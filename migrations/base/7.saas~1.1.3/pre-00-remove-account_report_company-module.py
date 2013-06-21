2af85c5e08b7c3d37696d65d97692cb7ba1fdc0b
Christophe Simonis <chs@openerp.com>
2013-06-21 17:09:56 +0200

    [FIX] base migration script that "remove-account_report_company-module"

---
+++ migrations/base/7.saas~1.1.3/pre-00-remove-account_report_company-module.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+def migrate(cr, version):
+    """In 7.0, the module "account_report_company" was a temporary module and
+       its functionnality is now part of the "base" and "account" modules.
+    """
+
+    MODULE = 'account_report_company'
+
+    # remove module
+    cr.execute("""
+        DELETE FROM ir_module_module
+              WHERE name=%s
+          RETURNING state
+    """, (MODULE,))
+
+    state = cr.fetchone()
+    if not state or state[0] not in ('installed', 'to upgrade', 'to remove'):
+        return
+
+    # remove views
+    cr.execute("""
+        SELECT res_id
+          FROM ir_model_data
+         WHERE module=%s
+           AND model=%s
+    """, (MODULE, 'ir.ui.view'))
+
+    view_ids = tuple(x[0] for x in cr.fetchall())
+    cr.execute('DELETE FROM ir_ui_view WHERE id IN %s', (view_ids,))
+
+    # reassign fields
+    cr.execute("""
+        UPDATE ir_model_data
+           SET module=%s
+         WHERE module=%s
+           AND name=%s
+    """, ('base', MODULE, 'field_res_partner_display_name'))
+
+    cr.execute("""
+        UPDATE ir_model_data
+           SET module=%s
+         WHERE module=%s
+           AND name IN %s
+    """, ('account', MODULE, ('field_account_invoice_commercial_partner_id', 'field_account_invoice_report_commercial_partner_id')))
+
+    # remove all ir.model.data
+    cr.execute("DELETE FROM ir_model_data WHERE module=%s", (MODULE,))
