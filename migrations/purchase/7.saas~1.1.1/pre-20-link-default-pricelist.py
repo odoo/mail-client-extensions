3f7d837826e89e86263ca0c3b7f59ddba5a0b2e8
Christophe Simonis <chs@openerp.com>
2013-06-24 12:35:52 +0200

    [ADD] migration script to match purchase pricelist

---
+++ migrations/purchase/7.saas~1.1.1/pre-20-link-default-pricelist.py
@@ -0,0 +1,24 @@
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    """
+        A default pricelist is created by xml data.
+        If user deleted and recreated pricelist, we can't recreate a new one that overlap.
+        We need to match xmlids, to the existing pricelists.
+        As pricelists are in "noupdate" mode, that's not a problem to reassign xmlids...
+    """
+    pl_id = util.ensure_xmlid_match_record(cr, 'purchase.list0', 'product.pricelist', {'type': 'purchase'})
+    if pl_id:
+        plv_id = util.ensure_xmlid_match_record(cr, 'purchase.ver0', 'product.pricelist.version', {
+            'pricelist_id': pl_id,
+            'date_start': None,
+            'date_end': None,
+        })
+        if not plv_id:
+            util.remove_record(cr, 'purchase.ver0')
+            util.remove_record(cr, 'purchase.item0')
+        else:
+            util.ensure_xmlid_match_record(cr, 'purchase.item0', 'product.pricelist.item', {
+                'price_version_id': plv_id,
+                'base': util.ref(cr, 'product.standard_price'),
+            })
