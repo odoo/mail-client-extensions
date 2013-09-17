6c692a3e59aeb0742ad4ea2faa903869d7f797a5
Christophe Simonis <chs@openerp.com>
2013-09-17 14:03:32 +0200

    [IMP] factorize module deletion

--- migrations/base/7.saas~1.1.3/pre-00-remove-account_report_company-module.py
+++ migrations/base/7.saas~1.1.3/pre-00-remove-account_report_company-module.py
@@ -1,40 +1,11 @@
 # -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     """In 7.0, the module "account_report_company" was a temporary module and
        its functionnality is now part of the "base" and "account" modules.
     """
 
     MODULE = 'account_report_company'
-
-    # delete constraints and relations...
-    for table in ['constraint', 'relation']:
-        cr.execute("""DELETE FROM ir_model_%s
-                            WHERE module = (SELECT id
-                                              FROM ir_module_module
-                                             WHERE name=%%s)
-                   """ % table, (MODULE,))
-
-    # remove module
-    cr.execute("""
-        DELETE FROM ir_module_module
-              WHERE name=%s
-          RETURNING state
-    """, (MODULE,))
-
-    state = cr.fetchone()
-    if not state or state[0] not in ('installed', 'to upgrade', 'to remove'):
-        return
-
-    # remove views
-    cr.execute("""
-        SELECT res_id
-          FROM ir_model_data
-         WHERE module=%s
-           AND model=%s
-    """, (MODULE, 'ir.ui.view'))
-
-    view_ids = tuple(x[0] for x in cr.fetchall())
-    cr.execute('DELETE FROM ir_ui_view WHERE id IN %s', (view_ids,))
 
     # reassign fields
     cr.execute("""
@@ -49,7 +20,7 @@ def migrate(cr, version):
            SET module=%s
          WHERE module=%s
            AND name IN %s
-    """, ('account', MODULE, ('field_account_invoice_commercial_partner_id', 'field_account_invoice_report_commercial_partner_id')))
+    """, ('account', MODULE, ('field_account_invoice_commercial_partner_id',
+                              'field_account_invoice_report_commercial_partner_id')))
 
-    # remove all ir.model.data
-    cr.execute("DELETE FROM ir_model_data WHERE module=%s", (MODULE,))
+    util.remove_module(cr, MODULE)
