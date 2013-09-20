40f60e071c137a5f0a9686140b9b2d105471549c
Christophe Simonis <chs@openerp.com>
2013-09-20 14:04:42 +0200

    [ADD] saas-2 migration script for base_action_rule

---
+++ migrations/base_action_rule/7.saas~2.1.0/pre-10-fill-kind-field.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.create_column(cr, 'base_action_rule', 'kind', 'varchar')
+
+    cr.execute("""UPDATE base_action_rule
+                     SET kind = CASE
+                                  WHEN trg_date_id IS NULL AND filter_pre_id IS NULL     THEN 'on_create'
+                                  WHEN trg_date_id IS NULL AND filter_pre_id IS NOT NULL THEN 'on_write'
+                                  WHEN trg_date_id IS NOT NULL                           THEN 'on_time'
+                                  ELSE 'on_write'
+                                END
+                   WHERE kind IS NULL
+               """)
