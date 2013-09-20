ac88a7c5cb2d34126375c02a81f4108da5b87e42
Christophe Simonis <chs@openerp.com>
2013-09-20 14:33:49 +0200

    [ADD] saas-2 migration scripts for new field user_id on idea.idea

---
+++ migrations/idea/7.saas~2.1.0/pre-10-fill-user_id.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+def migrate(cr, version):
+    util.create_column(cr, 'idea.idea', 'user_id', 'int4')
+    cr.execute("UPDATE idea_idea SET user_id = create_uid WHERE user_id IS NULL")
