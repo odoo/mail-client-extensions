56b3043a8dcbed4797c3a5d55e638ec1e0bf21da
Christophe Simonis <chs@openerp.com>
2013-10-16 16:33:30 +0200

    [FIX] base_action_rule: set kind to "on_create_or_write" which match v7 behavior

--- migrations/base_action_rule/7.saas~2.1.0/pre-10-fill-kind-field.py
+++ migrations/base_action_rule/7.saas~2.1.0/pre-10-fill-kind-field.py
@@ -6,7 +6,7 @@ def migrate(cr, version):
 
     cr.execute("""UPDATE base_action_rule
                      SET kind = CASE
-                                  WHEN trg_date_id IS NULL AND filter_pre_id IS NULL     THEN 'on_create'
+                                  WHEN trg_date_id IS NULL AND filter_pre_id IS NULL     THEN 'on_create_or_write'
                                   WHEN trg_date_id IS NULL AND filter_pre_id IS NOT NULL THEN 'on_write'
                                   WHEN trg_date_id IS NOT NULL                           THEN 'on_time'
                                   ELSE 'on_write'
