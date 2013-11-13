d5e9a2bb0849277406f99a9597a18abcf7967fef
Christophe Simonis <chs@openerp.com>
2013-11-13 18:24:50 +0100

    [IMP] improve saas-2 announce message

--- migrations/mail/7.saas~2.1.0/post-999-announce-new-version.py
+++ migrations/mail/7.saas~2.1.0/post-999-announce-new-version.py
@@ -5,14 +5,16 @@ def migrate(cr, version):
     # NOTE message is in RST
     message = """
         * New tool to merge duplicated partners.
-        * New Sales Team Dashboard.
-        * Improve geolocalisation of leads.
-        * New lead mass forward wizard.
-        * Dynamic stages on leads, opportunities, tasks, issues and job applicants.
+        * New Sales Team Dashboard, including spark lines.
+        * Improved geolocalisation of Leads.
+        * New mass-forward wizard for Leads.
+        * Dynamic stages on leads, opportunities, tasks, issues and job applicants. You may need to adapt some of your custom search filters.
+        * Folded stages are now available as a drop-down menu in the top right stage bar on Leads, Opportunities, Issues and Applicants.
         * Better leave pre-validation in HR module.
         * Social aspects for partners and employees.
         * Remove ``Shop`` model. The location is directly selectable on the Sale Order.
         * You can now choose the currency directly on the Purchase Order.
+        * Improvement management of marketing campaigns with bounce processing.
         * Better Google Doc integration, including new ``google_spreadsheet`` module.
         * Easier Server and Automated Actions configuration.
     """
