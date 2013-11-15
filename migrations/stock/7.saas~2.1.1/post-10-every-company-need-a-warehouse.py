0bb6614e10b05e9bff6032a25e629288285e449a
Christophe Simonis <chs@openerp.com>
2013-11-15 19:50:39 +0100

    [ADD] stock: script to ensure each company have a warehouse

---
+++ migrations/stock/7.saas~2.1.1/post-10-every-company-need-a-warehouse.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from openerp import SUPERUSER_ID
+from openerp.modules.registry import RegistryManager
+
+def migrate(cr, version):
+    # Ensure every company have a warehouse
+    registry = RegistryManager.get(cr.dbname)
+    Warehouse = registry['stock.warehouse']
+
+    cr.execute("""SELECT id, name, partner_id
+                    FROM res_company c
+                   WHERE NOT EXISTS(SELECT id
+                                      FROM stock_warehouse
+                                     WHERE company_id = c.id)
+               """)
+
+    for company_id, name, partner_id in cr.fetchall():
+        Warehouse.create(cr, SUPERUSER_ID, {
+            'name': name,
+            'company_id': company_id,
+            'partner_id': partner_id,
+        })
