bee31042774567215e590baee5e5bb5be77d2dc8
Christophe Simonis <chs@openerp.com>
2013-11-15 23:57:13 +0100

    [ADD] idea: add script to rename demo xmlids

---
+++ migrations/idea/7.saas~2.1.0/pre-20-rename_demo_xmlid.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+def migrate(cr, version):
+    renames = {
+        'idea_category_sales': 'idea_cat_0',
+        'idea_category_general': 'idea_cat_1',
+        'idea_category_technical': 'idea_cat_2',
+    }
+
+    for f, t in renames.values():
+        cr.execute("UPDATE ir_model_data SET name=%s WHERE module=%s AND name=%s", (t, 'idea', f))
