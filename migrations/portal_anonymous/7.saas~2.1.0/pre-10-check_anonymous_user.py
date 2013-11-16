3f9510fe0563dc83517e661a3b4667c1346cd31d
Christophe Simonis <chs@openerp.com>
2013-11-16 01:19:40 +0100

    [COPY] {portal_anonymous,training_base}: saas-1 cleaning scripts still valid for saas-2

---
+++ migrations/portal_anonymous/7.saas~2.1.0/pre-10-check_anonymous_user.py
@@ -0,0 +1,5 @@
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    if not util.ensure_xmlid_match_record(cr, 'portal_anonymous.anonymous_user', 'res.users', {'login': 'anonymous'}):
+        util.remove_record(cr, 'portal_anonymous.anonymous_user_res_partner', deactivate=True)
---
+++ migrations/training_base/7.saas~2.1.1/pre-10-check_users.py
@@ -0,0 +1,22 @@
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    users = """
+nick_hawson
+luke_stason
+maria_pulco
+thomas_brown
+michael_parker
+cheryl_jenkings
+pete_hurley
+greg_fletcher
+fred_lafaille
+james_fisher
+phil_harkins
+brandon_light
+""".split()
+    for u in users:
+        login = u.split('_')[0] + '@openwood.com'
+        xmlid = 'training_base.' + u
+        if not util.ensure_xmlid_match_record(cr, xmlid, 'res.users', {'login': login}):
+            util.remove_record(cr, 'training_base.%s_res_partner' % u, deactivate=True)
