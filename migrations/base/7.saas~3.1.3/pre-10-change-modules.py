184d9828237492a4e54048e0f84dfff3a2360981
Christophe Simonis <chs@openerp.com>
2014-01-20 17:23:32 +0100

    [IMP] base: merge saas-3 pre scripts [ADD] base: new dep for calendar module

--- migrations/base/7.saas~3.1.3/pre-10-change-modules.py
+++ migrations/base/7.saas~3.1.3/pre-10-change-modules.py
@@ -4,3 +4,7 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.rename_module(cr, 'base_calendar', 'calendar')
     util.rename_module(cr, 'google_base_account', 'google_account')
+
+    util.new_module_dep(cr, 'calendar', 'web_calendar')
+
+    util.remove_module(cr, 'web_shortcuts')
--- migrations/base/7.saas~3.1.3/pre-10-remove-modules.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp.addons.base.maintenance.migrations import util
-
-def migrate(cr, version):
-    util.remove_module(cr, 'web_shortcuts')
