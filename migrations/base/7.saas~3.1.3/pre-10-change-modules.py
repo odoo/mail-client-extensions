341cc2a3b25865eddc617e7863956fdbad7a12c4
Christophe Simonis <chs@openerp.com>
2014-01-22 19:11:35 +0100

    [FIX] "update module list" is call AFTER post scripts of base module, thus we can't auto-install new modules in post, we need to insert the module with the right state ourself

--- migrations/base/7.saas~3.1.3/pre-10-change-modules.py
+++ migrations/base/7.saas~3.1.3/pre-10-change-modules.py
@@ -10,3 +10,15 @@ def migrate(cr, version):
     util.new_module_dep(cr, 'hr_contract', 'base_action_rule')
 
     util.remove_module(cr, 'web_shortcuts')
+
+    # new module auto_installed
+    deps = ('hr_recruitment', 'document')
+    cr.execute("""SELECT count(1)
+                    FROM ir_module_module
+                   WHERE name IN %s
+                     AND state IN %s
+               """, (deps, ('to install', 'to upgrade')))
+
+    state = 'to install' if cr.fetchone()[0] == len(deps) else 'uninstalled'
+    cr.execute("INSERT INTO ir_module_module(name, state) VALUES (%s, %s)",
+               ('hr_applicant_document', state))
