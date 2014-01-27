3c2638b6fb5ef87ed513ec099d06958c3a9ade04
Christophe Simonis <chs@openerp.com>
2014-01-27 12:50:25 +0100

    [ADD] product: image move from product to template

---
+++ migrations/product/7.saas~3.1.1/post-move-images.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance import util
+
+def migrate(cr, version):
+    cr.execute("""UPDATE product_template
+                     SET image = p.image,
+                         image_medium = p.image_medium,
+                         image_small = p.image_small
+                    FROM product_product p
+                   WHERE p.product_template_id = id
+                """)
+
+    util.remove_field(cr, 'product.product', 'image')
+    util.remove_field(cr, 'product.product', 'image_medium')
+    util.remove_field(cr, 'product.product', 'image_small')
