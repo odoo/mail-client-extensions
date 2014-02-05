41f4ffd2de4dd6e2e456569b5bf147bec7b70c08
Christophe Simonis <chs@openerp.com>
2014-02-05 13:53:58 +0100

    [FIX] product: do not remove pos.category model if not exists

--- migrations/product/7.saas~3.1.1/pre-public_category-from-pos.py
+++ migrations/product/7.saas~3.1.1/pre-public_category-from-pos.py
@@ -4,4 +4,5 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # in saas-3, the model pos.category has been renamed to product.public.category and promoted
     # to product module
-    util.rename_model(cr, 'pos.category', 'product.public.category', module='product')
+    if util.table_exists(cr, 'pos_category'):
+        util.rename_model(cr, 'pos.category', 'product.public.category', module='product')
