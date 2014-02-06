93a396784445b94ab389366d85b0fcd39927ec78
Christophe Simonis <chs@openerp.com>
2014-02-06 13:04:47 +0100

    [FIX] limit custom script to some databases

--- migrations/base/7.saas~3.1.3/pre-99-update-custom.py
+++ migrations/base/7.saas~3.1.3/pre-99-update-custom.py
@@ -3,8 +3,10 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     """This migration script is only useful on saas."""
+    if util.table_exists(cr, 'payment_provider_account'):
+        util.rename_module(cr, 'payment', 'openerp_payment')
+        util.rename_model(cr, 'payment.payment', 'openerp_payment.payment')
+        util.rename_model(cr, 'payment.provider.account', 'openerp_payment.provider.account')
+        util.rename_model(cr, 'payment.creditcard', 'openerp_payment.creditcard')
 
-    util.rename_module(cr, 'payment', 'openerp_payment')
-    util.rename_model(cr, 'payment.payment', 'openerp_payment.payment')
-    util.rename_model(cr, 'payment.provider.account', 'openerp_payment.provider.account')
-    util.rename_model(cr, 'payment.creditcard', 'openerp_payment.creditcard')
+        util.new_module_dep(cr, 'openerp_enterprise', 'website')
