cdf45765aa9126861410b40ef8a76245c8fccb67
Christophe Simonis <chs@openerp.com>
2014-02-18 17:41:20 +0100

    [FIX] base: saas-3: rename "anonymous" user and group to "public"

--- migrations/base/7.saas~3.1.3/pre-05-rename-groups+users.py
+++ migrations/base/7.saas~3.1.3/pre-05-rename-groups+users.py
@@ -3,14 +3,27 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
 
-    groups = {
-        'portal.group_anonymous': 'base.group_public',
-        'portal.group_portal': 'base.group_portal',
-        'portal_anonymous.anonymous_user': 'base.public_user',
-        'portal_anonymous.anonymous_user_res_partner': 'base.public_partner',
+    _group_public = {
+        'name': 'Public',
+        'comment': 'Public users have specific access rights (such as record rules and restricted menus). \
+                    They usually do not belong to the usual OpenERP groups.',
     }
 
-    for old, new in groups.items():
+    _user_public = {
+        'login': 'public',
+        'password': '',
+        'active': False,
+        # image stay the same
+    }
+
+    updates = {
+        'portal.group_anonymous': ('base.group_public', _group_public),
+        'portal.group_portal': ('base.group_portal', None),
+        'portal_anonymous.anonymous_user': ('base.public_user', _user_public),
+        'portal_anonymous.anonymous_user_res_partner': ('base.public_partner', dict(name='Public user', active=False)),
+    }
+
+    for old, (new, data) in updates.items():
         old_module, _, old_name = old.partition('.')
         new_module, _, new_name = new.partition('.')
 
@@ -20,7 +33,16 @@ def migrate(cr, version):
                              name=%s
                        WHERE module=%s
                          AND name=%s
+                   RETURNING model, res_id
                    """, (False, new_module, new_name, old_module, old_name))
+
+        model, res_id = cr.fetchone() or [None, None]
+        if not res_id or not data:
+            continue
+        upd, params = zip(*(('%s=%%s' % k, v) for k, v in data.iteritems()))
+        query = 'UPDATE "%s" SET %s WHERE id=%%s' % (util.table_of_model(cr, model), ','.join(upd))
+        params = tuple(params) + (res_id,)
+        cr.execute(query, params)
 
     todel = [
         'portal_anonymous.anonymous_user_mail_alias',
