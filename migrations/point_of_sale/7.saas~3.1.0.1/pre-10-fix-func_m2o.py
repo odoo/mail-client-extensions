8fb23f61e0429f7f3cb95db4c9a361bd2fcc5b3b
Christophe Simonis <chs@openerp.com>
2014-03-24 15:31:00 +0100

    [FIX] point_of_sale: extract drop of wrong m2o func column in its util method

--- migrations/point_of_sale/7.saas~3.1.0.1/pre-10-fix-func_m2o.py
+++ migrations/point_of_sale/7.saas~3.1.0.1/pre-10-fix-func_m2o.py
@@ -2,19 +2,5 @@
 from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # m2o function fields now have foreign keys...
-
-    cr.execute("""SELECT count(1)
-                    FROM pos_session
-                   WHERE cash_register_id NOT IN (SELECT id
-                                                    FROM account_bank_statement)
-               """)
-    if cr.fetchone()[0]:
-        util.remove_column(cr, 'pos_session', 'cash_register_id')
-
-    cr.execute("""SELECT count(1)
-                    FROM pos_session
-                   WHERE cash_journal_id NOT IN (SELECT id
-                                                   FROM account_journal)
-               """)
-    if cr.fetchone()[0]:
-        util.remove_column(cr, 'pos_session', 'cash_journal_id')
+    util.ensure_m2o_func_field_data(cr, 'pos_session', 'cash_register_id', 'account_bank_statement')
+    util.ensure_m2o_func_field_data(cr, 'pos_session', 'cash_journal_id', 'account_journal')
