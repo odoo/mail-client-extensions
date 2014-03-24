d30878fd8f72336ef64b15752be4b0e5e1c3f8d1
Christophe Simonis <chs@openerp.com>
2014-03-24 11:40:21 +0100

    [FIX] point_of_sale: drop column when there is wrong m2o references to force recomputation

--- migrations/point_of_sale/7.saas~3.1.0.1/pre-10-fix-func_m2o.py
+++ migrations/point_of_sale/7.saas~3.1.0.1/pre-10-fix-func_m2o.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # m2o function fields now have foreign keys...
-    cr.execute("""UPDATE pos_session
-                     SET cash_register_id = NULL
+
+    cr.execute("""SELECT count(1)
+                    FROM pos_session
                    WHERE cash_register_id NOT IN (SELECT id
                                                     FROM account_bank_statement)
-                     AND state = 'closed'
                """)
+    if cr.fetchone()[0]:
+        util.remove_column(cr, 'pos_session', 'cash_register_id')
 
-    cr.execute("""UPDATE pos_session
-                     SET cash_journal_id = NULL
+    cr.execute("""SELECT count(1)
+                    FROM pos_session
                    WHERE cash_journal_id NOT IN (SELECT id
                                                    FROM account_journal)
-                     AND state = 'closed'
                """)
+    if cr.fetchone()[0]:
+        util.remove_column(cr, 'pos_session', 'cash_journal_id')
