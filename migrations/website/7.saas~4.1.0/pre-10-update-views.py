f74fba0870e4bb1090a8004acc0e4d5815996088
Christophe Simonis <chs@openerp.com>
2014-04-16 14:06:03 +0200

    [ADD] saas-4 migration script that alter website.layout view

---
+++ migrations/website/7.saas~4.1.0/pre-10-update-views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    view_id = util.ref(cr, 'website.layout')
+    cr.execute("""UPDATE ir_ui_view
+                     SET arch=replace(replace(arch, %s, %s), %s, %s)
+                   WHERE id=%s
+               """, ('<script type="text/javascript" src="/website/static/lib/bootstrap/js/bootstrap.js"/>',
+                     '',
+                     ' src="/website/static/src/js/website.snippets.animation.js"/>',
+                     'src="/website/static/src/js/website.snippets.animation.js"><script>\n\
+                      <script type="text/javascript" src="/web/static/lib/bootstrap/js/bootstrap.js"></script>',
+                     view_id))
