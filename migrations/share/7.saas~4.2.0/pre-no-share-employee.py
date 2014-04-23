01b84e9f18466cbaac52617302607164ac834328
Christophe Simonis <chs@openerp.com>
2014-04-23 17:52:34 +0200

    [FIX] share: do not change users groups

--- migrations/share/7.saas~4.2.0/pre-no-share-employee.py
+++ migrations/share/7.saas~4.2.0/pre-no-share-employee.py
@@ -2,23 +2,10 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    # remove "employee" group from share users
-    cr.execute("""DELETE FROM res_groups_users_rel
-                        WHERE gid=(SELECT res_id
-                                     FROM ir_model_data
-                                    WHERE module=%s
-                                      AND name=%s)
-                          AND uid IN (SELECT id
-                                        FROM res_users
-                                       WHERE share=%s)
-                """, ('base', 'group_user', True))
-
+    gid = util.ref(cr, 'base.group_user')
     cr.execute("""UPDATE res_users u
                      SET share = NOT EXISTS(SELECT 1
                                               FROM res_groups_users_rel
-                                             WHERE gid=(SELECT res_id
-                                                          FROM ir_model_data
-                                                         WHERE module=%s
-                                                           AND name=%s)
+                                             WHERE gid = %s
                                                AND uid = u.id)
-                """, ('base', 'group_user'))
+                """, (gid,))
