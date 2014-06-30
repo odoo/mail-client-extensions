63d9668c88241d4a1d80915f48eda4c327c1d313
Christophe Simonis <christophe@kn.gl>
2014-07-01 01:20:11 +0200

    [IMP] base@saas-5: product module depend on report + lint file

--- migrations/base/7.saas~5.1.3/pre-10-change-modules.py
+++ migrations/base/7.saas~5.1.3/pre-10-change-modules.py
@@ -2,26 +2,20 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    #Remove process
+
+    util.new_module_dep(cr, 'product', 'report')
+
     util.remove_module(cr, 'process')
-    
-    dependencies = ['account', 'crm', 'hr_expense', 'hr_holidays', 'hr', 'hr_timesheet', 'hr_timesheet_sheet', 
-                                            'membership', 'mrp_operations', 'mrp', 'procurement', 'product', 'project', 'project_mrp',
-                                            'project_timesheet', 'purchase',
-                                            'sale', 'sale_crm', 'sale_stock', ]
-    for depend in dependencies:
-        util.remove_module_deps(cr, depend, ('process',))
-    
-    
+
     # Remove stock_location and stock_no_auto_picking
     util.remove_module(cr, 'stock_location')
     util.remove_module(cr, 'stock_no_auto_picking')
-    
+
     # Inverse dependency of procurement from stock
     util.remove_module_deps(cr, 'procurement', ('stock',))
     util.new_module_dep(cr, 'stock', 'procurement')
 
-    #Add dependencies from stock on web modules
+    # Add dependencies from stock on web modules
     util.new_module_dep(cr, 'stock', 'web_kanban_gauge')
     util.new_module_dep(cr, 'stock', 'web_kanban_sparkline')
     # Rename module mrp_jit
@@ -29,18 +23,17 @@ def migrate(cr, version):
 
     # Add stock_account moodule, automatically installed when stock and account are
     util.new_module(cr, 'stock_account', auto_install_deps=('stock', 'account'))
-    
-    
-    #Sale stock should depend on stock_account
+
+    # Sale stock should depend on stock_account
     util.new_module_dep(cr, 'sale_stock', 'stock_account')
-    
+
     # Remove dependency of purchase from mrp
     util.remove_module_deps(cr, 'purchase', ('mrp',))
-    
+
     util.new_module_dep(cr, 'crm', 'sales_team')
     util.new_module_dep(cr, 'sale', 'sales_team')
-    
-    cr.execute("update ir_model_data set module=%s where module=%s and model=%s", 
+
+    cr.execute("update ir_model_data set module=%s where module=%s and model=%s",
                ('sales_team', 'crm', 'crm.case.section',))
-    
+
     util.remove_record(cr, 'base.user_groups_view')
