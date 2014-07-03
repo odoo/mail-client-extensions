584305432edef978dc17cf9ff36e8e3f7772f8b1
Christophe Simonis <christophe@kn.gl>
2014-07-03 11:48:50 +0200

    [FIX] calendar@saas-5

--- migrations/calendar/7.saas~5.1.0/pre-10-adjustdates.py
+++ migrations/calendar/7.saas~5.1.0/pre-10-adjustdates.py
@@ -2,14 +2,27 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
+    util.rename_field(cr, 'calendar.event', 'end_date', 'final_date')
+
     util.rename_field(cr, 'calendar.event', 'date', 'start_datetime')
     util.rename_field(cr, 'calendar.event', 'date_deadline', 'stop_datetime')
     util.create_column(cr, 'calendar_event', 'start_date', 'date')
     util.create_column(cr, 'calendar_event', 'stop_date', 'date')
-    #TODO: add 12 hours
-    cr.execute("""UPDATE calendar_event SET start_date = start_datetime WHERE allday = 't'
-    """)
-    cr.execute("""UPDATE calendar_event SET stop_date = stop_datetime WHERE allday = 't'
-    """)
-    util.rename_field(cr, 'calendar.event', 'end_date', 'final_date')
-    #TODO: if duration is null -> add datestop - datestart or datetime_stop - datetime_start
+
+    cr.execute("""UPDATE calendar_event
+                     SET start_date = start_datetime + interval '12 hours',
+                     SET stop_date = stop_datetime + interval '12 hours'
+                   WHERE allday = 't'
+               """)
+    cr.execute("""UPDATE calendar_event
+                     SET stop_date = start_date + (duration * interval '1 hour')
+                   WHERE duration IS NOT NULL
+                     AND stop_date IS NULL
+                     AND allday = 't'
+               """)
+    cr.execute("""UPDATE calendar_event
+                     SET stop_datetime = start_datetime + (duration * interval '1 hour')
+                   WHERE duration IS NOT NULL
+                     AND stop_datetime IS NULL
+                     AND allday = 'f'
+               """)
