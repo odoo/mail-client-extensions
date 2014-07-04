383d8f8f64990a08580fe8b3f40375ac93b6e8a6
Christophe Simonis <christophe@kn.gl>
2014-07-04 13:45:07 +0200

    [FIX] product@saas-3: as pos.category is back in saas-5, only rename it to product.public.categogy in saas-3,saas-4

--- migrations/product/7.saas~3.1.1/post-public_category-from-pos.py
+++ migrations/product/7.saas~3.1.1/post-public_category-from-pos.py
@@ -4,6 +4,9 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # in saas-3, the model pos.category has been renamed to product.public.category and promoted
     # to product module
+    # but it's back in saas-5.
+    if version and not version.startswith(('7.saas~3', '7.saas~4')):
+        return
     if util.column_exists(cr, 'product_product', 'pos_categ_id'):
         cr.execute("""UPDATE product_template t
                          SET public_categ_id = (SELECT pos_categ_id
--- migrations/product/7.saas~3.1.1/pre-public_category-from-pos.py
+++ migrations/product/7.saas~3.1.1/pre-public_category-from-pos.py
@@ -4,5 +4,8 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # in saas-3, the model pos.category has been renamed to product.public.category and promoted
     # to product module
+    # but it's back in saas-5.
+    if version and not version.startswith(('7.saas~3', '7.saas~4')):
+        return
     if util.table_exists(cr, 'pos_category'):
         util.rename_model(cr, 'pos.category', 'product.public.category', module='product')
