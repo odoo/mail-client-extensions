272d3651c5f9706891c53abe60abfd86f893dfc7
Christophe Simonis <christophe@kn.gl>
2014-07-04 11:59:53 +0200

    [IMP] sale@saas-5: rewrite wkf query

--- migrations/sale/7.saas~5.1.0/pre-10-movewkf.py
+++ migrations/sale/7.saas~5.1.0/pre-10-movewkf.py
@@ -1,11 +1,11 @@
-from openerp.addons.base.maintenance.migrations import util
+# -*- coding: utf-8 -*-
 
 def migrate(cr, version):
     """
     Adapt sale order workflow to be entirely in sale module instead of sale_stock
     """
-    cr.execute("update ir_model_data set module=%s where module=%s and model=%s", 
-               ('sale', 'sale_stock', 'workflow.activity',))
-    cr.execute("update ir_model_data set module=%s where module=%s and model=%s", 
-               ('sale', 'sale_stock', 'workflow.transition',))
-
+    cr.execute("""UPDATE ir_model_data
+                     SET module = %s
+                   WHERE module = %s
+                     AND model IN %s
+               """, ('sale', 'sale_stock', ('workflow.activity', 'workflow.transition')))
