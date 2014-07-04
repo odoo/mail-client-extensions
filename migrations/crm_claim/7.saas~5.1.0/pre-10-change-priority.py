c8a980388727580c226a724f5beb5fb032a0b987
hsa-odoo <hsa@tinyerp.com>
2014-07-04 16:08:41 +0530

    [FIX] crm_claim@saas-5: Adapt priority
    (rebase of PR #13)

---
+++ migrations/crm_claim/7.saas~5.1.0/pre-10-change-priority.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Priority Mapping For crm.claim
+# Low, Lowest = Low
+# Normal = Normal
+# High, Highest = Highest
+
+def migrate(cr, version):
+	cr.execute("""UPDATE crm_claim
+			SET priority = CASE WHEN priority in ('5','4') THEN '0'
+                            WHEN priority in ('3') THEN '1'
+                            WHEN priority in ('2','1') THEN '2'
+                            END
+            WHERE priority IS NOT NULL """)
