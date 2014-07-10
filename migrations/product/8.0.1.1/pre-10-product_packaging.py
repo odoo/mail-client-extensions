76104654bd27b457c257d418acbe2b855eefd469
ssu-odoo <ssu@openerp.com>
2014-07-10 14:28:08 +0530

    [ADD] product@8.0: on product.packaging, product_id replaced by product_tmpl_id

---
+++ migrations/product/8.0.1.1/pre-10-product_packaging.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.rename_field(cr, 'product_packaging', 'product_id', 'product_tmpl_id')
+    cr.execute("ALTER TABLE product_packaging DROP CONSTRAINT product_packaging_product_id_fkey")
+    cr.execute("""UPDATE product_packaging pa
+                     SET product_tmpl_id = pp.product_tmpl_id
+                    FROM product_product pp
+                   WHERE pa.product_tmpl_id = pp.id
+               """)
