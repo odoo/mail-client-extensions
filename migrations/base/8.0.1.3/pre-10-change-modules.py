6e8f473d2ca36a89fd9ebce73422c612c1f92e7c
Christophe Simonis <christophe@kn.gl>
2014-07-16 17:04:30 +0200

    [FIX] base@8.0: more module changes

--- migrations/base/8.0.1.3/pre-10-change-modules.py
+++ migrations/base/8.0.1.3/pre-10-change-modules.py
@@ -5,8 +5,24 @@ def migrate(cr, version):
 
     util.new_module(cr, 'bus')
     util.rename_module(cr, 'im', 'im_chat')
-    util.new_module_dep(cr, 'im_chat', ('bus',))
+    util.new_module_dep(cr, 'im_chat', 'bus')
 
     util.rename_module(cr, 'project_mrp', 'sale_service')
 
-    util.new_module_dep(cr, 'l10n_us', ('account_anglo_saxon',))
+    util.new_module_dep(cr, 'l10n_uk', 'account_anglo_saxon')
+    util.new_module_dep(cr, 'l10n_us', 'account_anglo_saxon')
+
+    util.remove_module_deps(cr, 'project_issue', ('crm',))
+    util.new_module_dep(cr, 'project_issue', 'sales_team')
+
+    util.new_module(cr, 'crm_mass_mailing', auto_install_deps=('crm', 'mass_mailing'))
+
+    util.mew_module(cr, 'website_instantclick')
+    util.mew_module(cr, 'website_sale_options')
+
+    # new l10n modules
+    for cc in 'ae hu sg'.split():
+        l10n = 'l10n_' + cc
+        util.new_module(cr, l10n)
+        util.new_module_dep(cr, l10n, 'account')
+        util.new_module_dep(cr, l10n, 'account_chart')
