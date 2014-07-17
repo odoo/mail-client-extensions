6a1e62e0d2b58319b745eb8ecf8320e0a294d03f
Christophe Simonis <christophe@kn.gl>
2014-07-17 19:07:35 +0200

    [FIX] stock{,_account}8.0: move (and fix) "valuation" script

---
+++ migrations/stock_account/8.0.1.1/pre-20-product-valuation.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+def migrate(cr, version):
+    # move "valuation" property field from product to template
+    cr.execute("""UPDATE ir_model_data where
+                     SET name=
+                   WHERE model=%s
+                     AND name=%s
+               """, ('field_product_template_valuation', 'ir.model.fields',
+                     'field_product_product_valuation'))
+
+    cr.execute("""UPDATE ir_model_fields
+                     SET model=%s,
+                         model_id=(SELECT id
+                                     FROM ir_model
+                                    WHERE model=%s)
+                   WHERE model=%s
+                     AND name=%s
+               RETURNING id
+               """, ('product.template', 'product.template', 'product.product', 'valuation'))
+    field_id = [cr.fetchone()]
+
+    cr.execute("""update ir_property ip
+                     set res_id = CONCAT('product.template,', p.product_tmpl_id)
+                    from product_product p
+                   where ip.res_id = CONCAT('product.product,', p.id)
+                     and ip.fields_id = %s
+               """, (field_id,))
