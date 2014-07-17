fd81ec348bd7f3ef2d986ca70201db7a991451e4
Christophe Simonis <christophe@kn.gl>
2014-07-17 18:02:30 +0200

    [FIX] point_of_sale@saas-5: also set the company on pos.config + avoid using the orm in post script

--- migrations/point_of_sale/7.saas~5.1.0.1/post-10-pospickingtype.py
+++ migrations/point_of_sale/7.saas~5.1.0.1/post-10-pospickingtype.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
 from openerp.addons.base.maintenance.migrations import util
-from openerp.modules.registry import RegistryManager
-from openerp import SUPERUSER_ID
 
 def migrate(cr, version):
-    registry = RegistryManager.get(cr.dbname)
-    mod_obj = registry['ir.model.data']
-    result = mod_obj.xmlid_to_res_id(cr, SUPERUSER_ID, 'point_of_sale.picking_type_posout')
-    if result:
-        cr.execute("""
-            UPDATE pos_config SET picking_type_id = %s
-        """, (result,))
+    pt = util.ref(cr, 'point_of_sale.picking_type_posout')
+    if pt:
+        cr.execute("UPDATE pos_config SET picking_type_id=%s", (pt,))
--- migrations/point_of_sale/7.saas~5.1.0.1/pre-30-poslocation.py
+++ migrations/point_of_sale/7.saas~5.1.0.1/pre-30-poslocation.py
@@ -3,8 +3,10 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.create_column(cr, 'pos_config', 'stock_location_id', 'int4')
-    cr.execute("""
-        UPDATE pos_config SET stock_location_id = sw.lot_stock_id FROM stock_warehouse sw 
-        WHERE sw.id = pos_config.warehouse_id
+    util.create_column(cr, 'pos_config', 'company_id', 'int4')
+    cr.execute("""UPDATE pos_config
+                     SET stock_location_id = sw.lot_stock_id.
+                         company_id = sw.company_id
+                    FROM stock_warehouse sw
+                   WHERE sw.id = pos_config.warehouse_id
     """)
-    
