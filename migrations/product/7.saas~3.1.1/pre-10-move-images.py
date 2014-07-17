f22af6b85dd5eb29389e8877bc9379ace5751ced
Olivier Laurent <olt@openerp.com>
2014-07-17 13:17:24 +0200

    [FIX] product@saas-3: moved 'image' stuff from post to pre
    image, image_small and image_medium are already deleted in post

---
+++ migrations/product/7.saas~3.1.1/pre-10-move-images.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+from openerp.release import series
+
+def migrate(cr, version):
+    util.create_column(cr, 'product_template', 'image', 'bytea')
+    util.create_column(cr, 'product_template', 'image_medium', 'bytea')
+    util.create_column(cr, 'product_template', 'image_small', 'bytea')
+
+    if series != '7.saas~3':
+        util.create_column(cr, 'product_product', 'image_variant', 'bytea')
+        cr.execute("""UPDATE product_product
+                         SET image_variant = image
+                         where product_tmpl_id in (select product_tmpl_id from product_product group by product_tmpl_id having count(id)>1)""")
+
+    cr.execute("""UPDATE product_template
+                     SET image = p.image,
+                         image_medium = p.image_medium,
+                         image_small = p.image_small
+                    FROM product_product p
+                   WHERE p.product_tmpl_id = product_template.id
+                """)
+
