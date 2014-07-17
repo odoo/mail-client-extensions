c72f21663bc970667d99215ec4c9d5f1f7154607
Olivier Laurent <olt@openerp.com>
2014-07-17 07:57:24 +0200

    [ADD] base@saas-3: remove obsolete views from google_account module

---
+++ migrations/base/7.saas~3.1.3/pre-20-remove-obsolete-views.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.remove_record(cr, 'google_account.view_users_gogole_form')
+    util.remove_record(cr, 'google_account.view_google_login_form')
+
