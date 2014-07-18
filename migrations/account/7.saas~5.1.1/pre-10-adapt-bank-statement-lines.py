367a9d1b0bd5497991a8c120623a717c81b1400b
Christophe Simonis <christophe@kn.gl>
2014-07-18 12:11:03 +0200

    [FIX] account@saas-5: correct bank_statement_line migration

--- migrations/account/7.saas~5.1.1/pre-10-adapt-bank-statement-lines.py
+++ migrations/account/7.saas~5.1.1/pre-10-adapt-bank-statement-lines.py
@@ -5,11 +5,13 @@ def migrate(cr, version):
 
     # Even if there was a many2many between statement lines and account move,
     # it must have at most one statement line per move.
-    # Fail if we found more.
+    # Due to a bug in 7.0, the move was wrongly copied when duplicating a bank statement.
+    # We need to keep only the generated move (we discrad the one with the lowest id)
+    # We fail if we found more than 2 (1 = normal situation, 2 = duplicated bank statement)
     cr.execute("""SELECT 1
                     FROM account_bank_statement_line_move_rel
                 GROUP BY move_id
-                  HAVING count(statement_line_id) > 1
+                  HAVING count(statement_line_id) > 2
                """)
     if cr.rowcount:
         raise util.MigrationError("More than one bank statement line per move found")
@@ -18,6 +20,8 @@ def migrate(cr, version):
 
     cr.execute("""UPDATE account_bank_statement_line l
                      SET journal_entry_id = m.move_id
-                    FROM account_bank_statement_line_move_rel m
+                    FROM (SELECT statement_line_id, MAX(move_id) as move_id
+                            FROM account_bank_statement_line_move_rel
+                           GROUP BY statement_line_id) m
                    WHERE m.statement_line_id = l.id
                """)
