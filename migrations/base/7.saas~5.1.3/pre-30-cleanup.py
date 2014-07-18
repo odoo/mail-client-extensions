66811fa33d16c10e775e3b36116a1a90129b06dd
Olivier Laurent <olt@openerp.com>
2014-07-18 07:40:46 +0200

    [FIX] base@saas-5: remove 'view_users_form' view
    prevents updating res.users form view because of inheritance

--- migrations/base/7.saas~5.1.3/pre-30-cleanup.py
+++ migrations/base/7.saas~5.1.3/pre-30-cleanup.py
@@ -4,3 +4,7 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # this views is auto-generated when updating res.groups
     util.remove_record(cr, 'base.user_groups_view')
+
+    # this view contains deleted fields (gmail_user, ...)
+    util.remove_record(cr, 'base.view_users_form')
+
