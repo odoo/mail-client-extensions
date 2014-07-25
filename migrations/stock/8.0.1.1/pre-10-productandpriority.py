5b56e4a8a4e88856d94b7062b6c93c8e5186ab23
Olivier Dony <odo@openerp.com>
2014-07-25 14:44:37 +0200

    [IMP] stock: pre: fix stock.move with incompatible UoM + warn

--- migrations/stock/8.0.1.1/pre-10-productandpriority.py
+++ migrations/stock/8.0.1.1/pre-10-productandpriority.py
@@ -13,3 +13,22 @@ def migrate(cr, version):
         UPDATE stock_move SET priority = '2' 
         WHERE priority = '1'
     """)
+
+    # Verify incompatible UoM on stock.move vs product UoM and warn about it
+    cr.execute("""
+        SELECT count(*) FROM stock_move m
+            JOIN product_product p ON (m.product_id = p.id)
+            JOIN product_template t ON (t.id=p.product_tmpl_id)
+            JOIN product_uom u1 ON (u1.id = m.product_uom)
+            JOIN product_uom u2 ON (u2.id = t.uom_id)
+        WHERE u1.category_id != u2.category_id;
+    """)
+    count = cr.fetchone()[0]
+    if count:
+        util.announce(cr, version, 'Found %s stock.move entries with a Units of Measure incompatible with the default product UoM (different categories). Forcing them to the default product UoM' % count)
+        cr.execute("""
+            UPDATE stock_move m SET product_uom = t.uom_id
+                FROM product_product p, product_template t, product_uom u1, product_uom u2
+                WHERE (m.product_id = p.id) AND (t.id=p.product_tmpl_id) AND (u1.id = m.product_uom) AND
+                      (u2.id = t.uom_id) AND u1.category_id != u2.category_id;
+        """)
