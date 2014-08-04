c5dd98d5739b2fb271b927000839cb086449f498
Christophe Simonis <christophe@kn.gl>
2014-08-04 15:19:42 +0200

    [ADD] base@8.0: cleanup falsey properties

---
+++ migrations/base/8.0.1.3/pre-40-clean-properties.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+def migrate(cr, version):
+    # before 8.0, falsey properties mean "default property"
+    # in 8.0, this mean "no value"
+    cr.execute("""DELETE FROM ir_property
+                        WHERE res_is IS NOT NULL
+                          AND value_text IS NULL
+                          AND value_float IS NULL
+                          AND value_integer IS NULL
+                          AND value_datetime IS NULL
+                          AND value_binary IS NULL
+                          AND value_reference IS NULL
+               """)
