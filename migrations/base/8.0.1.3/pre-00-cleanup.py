693cfd2b4459189588d51ff998266e96f2e72aa8
Christophe Simonis <christophe@kn.gl>
2014-08-11 15:57:24 +0200

    [FIX] util: delete_model also remove references to this model

--- migrations/base/8.0.1.3/pre-00-cleanup.py
+++ migrations/base/8.0.1.3/pre-00-cleanup.py
@@ -6,13 +6,5 @@ def migrate(cr, version):
     # remove them + all referencing data
     models = ['document.ftp.browse', 'plugin_thunderbird.installer', 'ir.ui.view_sc',
               'google.docs.config']
-
-    ignore = ['ir.model', 'ir.model.data']
-    for dest_model, res_model, _ in util.res_model_res_id(cr):
-        if dest_model in ignore:
-            continue
-        table = util.table_of_model(cr, dest_model)
-        query = 'DELETE FROM "{0}" WHERE "{1}"=%s'.format(table, res_model)
-        for model in models:
-            cr.execute(query, (model,))
+    for model in models:
         util.delete_model(cr, model)
