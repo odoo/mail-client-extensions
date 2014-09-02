9ede549826aadc45b0495a3487495581f13aff97
Christophe Simonis <christophe@kn.gl>
2014-09-02 14:55:07 +0200

    [FIX] base@saas-4: when removing default empty string set varchar to something if NULL cannot be set

--- migrations/base/7.saas~4.1.3/pre-00-rm-default-empty-strings.py
+++ migrations/base/7.saas~4.1.3/pre-00-rm-default-empty-strings.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     """remove empty string default values. This behavior cannot be reproduced via the ORM"""
@@ -11,4 +11,8 @@ def migrate(cr, version):
 
     for table, column in cr.fetchall():
         cr.execute("ALTER TABLE {0} ALTER COLUMN {1} DROP DEFAULT".format(table, column))
-        cr.execute("UPDATE {0} SET {1}=NULL WHERE {1}=''".format(table, column))
+        try:
+            with util.savepoint(cr):
+                cr.execute("UPDATE {0} SET {1}=NULL WHERE {1}=''".format(table, column))
+        except Exception:
+            cr.execute("UPDATE {0} SET {1}=CONCAT('id#', id) WHERE {1}=''".format(table, column))
