8b538545fbdee4b24870ed2e6d34bdd0019e964c
Olivier Laurent <olt@odoo.com>
2014-09-04 14:28:28 +0200

    [FIX] {hr,product}@saas-1: check 'ir_ui_view_sc' table existence (some dbs does not have it)

--- migrations/hr/7.saas~1.1.1/pre-10-ensure-shortcut.py
+++ migrations/hr/7.saas~1.1.1/pre-10-ensure-shortcut.py
@@ -1,8 +1,9 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.ensure_xmlid_match_record(cr, 'hr.ir_ui_view_sc_employee', 'ir.ui.view_sc', {
-        'resource': 'ir.ui.menu',
-        'res_id': util.ref(cr, 'hr.menu_open_view_employee_list_my'),
-        'user_id': util.ref(cr, 'base.user_root'),
-    })
+    if util.table_exists(cr, 'ir_ui_view_sc'):
+        util.ensure_xmlid_match_record(cr, 'hr.ir_ui_view_sc_employee', 'ir.ui.view_sc', {
+            'resource': 'ir.ui.menu',
+            'res_id': util.ref(cr, 'hr.menu_open_view_employee_list_my'),
+            'user_id': util.ref(cr, 'base.user_root'),
+        })
--- migrations/product/7.saas~1.1.1/pre-10-ensure-shortcut.py
+++ migrations/product/7.saas~1.1.1/pre-10-ensure-shortcut.py
@@ -1,8 +1,9 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.ensure_xmlid_match_record(cr, 'product.ir_ui_view_sc_product0', 'ir.ui.view_sc', {
-        'resource': 'ir.ui.menu',
-        'res_id': util.ref(cr, 'product.menu_products'),
-        'user_id': util.ref(cr, 'base.user_root'),
-    })
+    if util.table_exists(cr, 'ir_ui_view_sc'):
+        util.ensure_xmlid_match_record(cr, 'product.ir_ui_view_sc_product0', 'ir.ui.view_sc', {
+            'resource': 'ir.ui.menu',
+            'res_id': util.ref(cr, 'product.menu_products'),
+            'user_id': util.ref(cr, 'base.user_root'),
+        })
