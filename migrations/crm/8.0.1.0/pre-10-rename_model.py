24ca9fec45fd31a8f0a504688f2bbd10a297f70e
hpa-odoo <hpa@tinyerp.com>
2014-09-05 14:30:51 +0530

    [ADD] crm@8.0: rename models

---
+++ migrations/crm/8.0.1.0/pre-10-rename_model.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.rename_model(cr, 'crm.case.channel', 'crm.tracking.medium')
+    util.rename_model(cr, 'crm.case.resource.type', 'crm.tracking.campaign')
