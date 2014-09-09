05f8064a073cdeeb853307679d9c1f8fb200e2ab
Christophe Simonis <christophe@kn.gl>
2014-09-09 15:23:23 +0200

    [FIX] {base,website}@saas-5: initialize colunm "application" correctly

--- migrations/base/7.saas~5.1.3/pre-20-view-mode-application.py
+++ migrations/base/7.saas~5.1.3/pre-20-view-mode-application.py
@@ -13,3 +13,19 @@ def migrate(cr, version):
                               END,
                          application=%s
                 """, ('primary', 'extension', 'always'))
+
+    if util.column_exists(cr, 'ir_ui_view', 'inherit_option_id'):
+        # As script are executed module by module and not version by version,
+        # if we migrate for saas-4 (or lower) to 8.0 (or higher), the initialisation
+        # of the column `active` (see base/8.0.1.3/pre-60-view-application.py)
+        # will not be correct as all inherited views are marked to be always applied.
+        cr.execute("""UPDATE ir_ui_view
+                         SET mode=%s,
+                             application=CASE
+                                           WHEN inherit_id IS NULL
+                                           THEN %s
+                                           ELSE %s
+                                         END,
+                             inherit_id=inherit_option_id
+                      WHERE inherit_option_id IS NOT NULL""",
+                   ('extension', 'disabled', 'enabled'))
