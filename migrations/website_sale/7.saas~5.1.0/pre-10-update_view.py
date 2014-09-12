1f2efd8c8052aae5a57cf660c432c93ecb723e7d
hpa-odoo <hpa@tinyerp.com>
2014-09-12 11:38:08 +0530

    [FIX] website_sale@saas-5: remove outdated view

--- migrations/website_sale/7.saas~5.1.0/pre-10-update_view.py
+++ migrations/website_sale/7.saas~5.1.0/pre-10-update_view.py
@@ -3,3 +3,4 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.remove_record(cr, 'website_sale.product')
+    util.remove_record(cr, 'website_sale.view_sales_config_website_sale')
