65633840f9b28895767ebe9db445ee9d50d9875c
Christophe Simonis <christophe@kn.gl>
2014-09-24 14:13:59 +0200

    [ADD] l10n_es@8.0: keep old account types

---
+++ migrations/l10n_es/8.0.4.0/pre-noupdate-user-types.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+def migrate(cr, version):
+    cr.execute("""UPDATE ir_model_data
+                     SET noupdate = true
+                   WHERE model = 'account.account.type'
+                     AND module = 'l10n_es'
+               """)
