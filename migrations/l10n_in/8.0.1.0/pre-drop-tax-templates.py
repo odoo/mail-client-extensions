4276dc704e4401e0e6cea55fbbeda6167619cd95
Olivier Dony <odo@odoo.com>
2014-09-24 15:02:50 +0200

    [ADD] l10n_in@8.0: remove tax templates and fiscal positions tax templates

---
+++ migrations/l10n_in/8.0.1.0/pre-drop-tax-templates.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+def migrate(cr, version):
+    cr.execute("""DELETE FROM account_fiscal_position_tax_template
+                        WHERE id IN (SELECT res_id
+                                       FROM ir_model_data
+                                      WHERE module = 'l10n_in'
+                                        AND model = 'account.fiscal.position.tax.template');
+                  DELETE FROM account_tax_template
+                        WHERE id IN (SELECT res_id
+                                       FROM ir_model_data
+                                      WHERE module = 'l10n_in'
+                                        AND model = 'account.tax.template');
+                  DELETE FROM ir_model_data
+                        WHERE module = 'l10n_in'
+                          AND model IN ('account.tax.template', 'account.fiscal.position.tax.template');
+               """)
