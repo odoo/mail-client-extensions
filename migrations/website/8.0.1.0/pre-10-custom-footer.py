e79d5b69ac57cef3d4c7df5e07b8765447b26429
Olivier Dony <odo@openerp.com>
2014-10-01 18:22:20 +0200

    [FIX] website: force reset of main layout, everything else would break if not upgraded
    It is unlikely to have received real customizations, as they should
    have landed in blocks inside the layout, not in the layout itself.

--- migrations/website/8.0.1.0/pre-10-custom-footer.py
+++ migrations/website/8.0.1.0/pre-10-custom-footer.py
@@ -3,6 +3,12 @@ from openerp.addons.base.maintenance.migrations import util
 from lxml import etree
 
 def migrate(cr, version):
+
+    # Force reset of main layout ... it was changed too much due to
+    # the new bundles, as of rev. 74d4d1f.
+    util.force_noupdate(cr, 'website.layout', False)
+
+    # Fix custom footer by adding an @id='footer' to make it compatible w/ new one
     footer_custom_view_id = util.ref(cr, 'website.footer_custom')
     if not footer_custom_view_id:
         return
