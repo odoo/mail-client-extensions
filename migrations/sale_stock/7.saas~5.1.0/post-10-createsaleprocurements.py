83751a84a21519f1bfd0e811e69a75fbf0981371
Josse Colpaert <jco@openerp.com>
2014-10-01 11:09:58 +0200

    [IMP] Procurement group needed on sales orders that are still open

--- migrations/sale_stock/7.saas~5.1.0/post-10-createsaleprocurements.py
+++ migrations/sale_stock/7.saas~5.1.0/post-10-createsaleprocurements.py
@@ -11,14 +11,17 @@ def migrate(cr, version):
         to these lines
     """
     
-    # Check all sale order lines where the sales order is not done and not shipped and not cancelled
-    # and create procurements for it and add moves to them.   
+    # Check all related moves to sale order lines where the sales order is not done and not shipped and not cancelled
+    # Create a procurement group for every sales order related, create a procurement in customers and
+    # create a procurement in
+    #
     registry = RegistryManager.get(cr.dbname)
     move_obj = registry["stock.move"]
     sol_obj = registry['sale.order.line']
+    so_obj = registry['sale.order']
     proc_obj = registry['procurement.order']
     cr.execute("""SELECT sm.sale_line_id, sm.id FROM stock_move sm, sale_order_line sol, sale_order so, product_product pp, product_template pt WHERE so.shipped = False
-    and so.state <> 'done' and so.state <> 'cancel' and pp.product_tmpl_id = pt.id
+    and so.state not in ('done', 'cancel') and pp.product_tmpl_id = pt.id
     and pt.type <> 'service' and pp.id = sol.product_id and so.id = sol.order_id and sm.sale_line_id = sol.id 
     """)
     sol_dict = {}
@@ -26,16 +29,20 @@ def migrate(cr, version):
         if not sol_dict.get(item[0]):
             sol_dict[item[0]] = [item[1]]
         else:
-            sol_dict[item[0]] += [item[1]] 
-    
+            sol_dict[item[0]] += [item[1]]
     for line in sol_obj.browse(cr, SUPERUSER_ID, sol_dict.keys()):
-        vals = registry['sale.order']._prepare_order_line_procurement(cr, SUPERUSER_ID, line.order_id, line)
+        order = line.order_id
+        if not order.procurement_group_id:
+            vals = so_obj._prepare_procurement_group(cr, SUPERUSER_ID, order)
+            group_id = registry["procurement.group"].create(cr, SUPERUSER_ID, vals)
+            order.write({'procurement_group_id': group_id})
+        else:
+            group_id = order.procurement_group_id.id
+        vals = so_obj._prepare_order_line_procurement(cr, SUPERUSER_ID, line.order_id, line, group_id=group_id)
         # Search related moves
         related_moves = sol_dict[line.id]
         vals.update({'move_ids': [(6, 0, related_moves)]})
-        moves = move_obj.browse(cr, SUPERUSER_ID, related_moves)
-        if all([x.state == 'done' for x in moves]):
-            vals.update({'state': 'done'})
-        else:
-            vals.update({'state': 'running'})
+        vals.update({'state': 'running'})
         proc_id = proc_obj.create(cr, SUPERUSER_ID, vals)
+        proc_obj.check(cr, SUPERUSER_ID, [proc_id])
+        move_obj.write(cr, SUPERUSER_ID, related_moves, {'group_id': group_id})
