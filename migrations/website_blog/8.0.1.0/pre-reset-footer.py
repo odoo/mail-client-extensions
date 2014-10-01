79c1f8ec971ad8a72292635d09d1846dee350edc
Olivier Dony <odo@openerp.com>
2014-10-01 18:34:33 +0200

    [ADD] website_*: force reset of footer links due to structural changes in footer system
    Those templates only added links in the footer so
    customizations are unlikely.

---
+++ migrations/website_blog/8.0.1.0/pre-reset-footer.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+
+    # website.layout changed in an incompatible manner at fe3775b and
+    # will always be forced-reset, so we need to reset this one too,
+    # as it inherits from a different template now!
+    # Theoretically it only adds a small "News" link in the footer,
+    # so customizations are unlikely.
+    util.force_noupdate(cr, 'website_blog.header_footer_custom', False)
---
+++ migrations/website_hr_recruitment/8.0.1.0/pre-reset-footer.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+
+    # website.layout changed in an incompatible manner at fe3775b and
+    # will always be forced-reset, so we need to reset this one too,
+    # as it inherits from a different template now!
+    # Theoretically it only adds a small "Jobs" link in the
+    # footer, so customizations are unlikely.
+    util.force_noupdate(cr, 'website_hr_recruitment.job_footer_custom', False)
+
