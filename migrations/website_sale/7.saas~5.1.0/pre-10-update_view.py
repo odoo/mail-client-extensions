edc503b384e46230ff97b90a49d37ac168fedc03
Olivier Dony <odo@openerp.com>
2014-10-09 09:49:23 +0200

    [FIX] website_sale: backwards-incompatible changes in templates - need to force reset

--- migrations/website_sale/7.saas~5.1.0/pre-10-update_view.py
+++ migrations/website_sale/7.saas~5.1.0/pre-10-update_view.py
@@ -4,3 +4,8 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.remove_record(cr, 'website_sale.product')
     util.remove_record(cr, 'website_sale.view_sales_config_website_sale')
+
+    # Shop templates need to be reset - they're not compatible anymore
+    util.force_noupdate(cr, 'website_sale.products', False)
+    util.force_noupdate(cr, 'website_sale.categories_recursive', False)
+    util.force_noupdate(cr, 'website_sale.products_categories', False)
