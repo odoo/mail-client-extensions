f6d9c40b2a50c9c259da72ddbc8f127abb0cea9f
Olivier Dony <odo@openerp.com>
2014-10-21 14:11:05 +0200

    [FIX] calendar: cron needs proper function too

--- migrations/calendar/7.saas~3.1.0/post-10-fix-alarm-cron.py
+++ migrations/calendar/7.saas~3.1.0/post-10-fix-alarm-cron.py
@@ -4,4 +4,4 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     cid = util.ref(cr, 'calendar.ir_cron_scheduler_alarm')
     if cid:
-        cr.execute("UPDATE ir_cron SET model=%s WHERE id=%s", ('calendar.alarm_manager', cid))
+        cr.execute("""UPDATE ir_cron SET model=%s, "function"=%s WHERE id=%s""", ('calendar.alarm_manager', 'get_next_mail', cid))
