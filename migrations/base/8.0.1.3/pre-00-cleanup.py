727aea2c06e9e3e7980eb17c90e3f17cfa129854
Olivier Dony <odo@openerp.com>
2014-10-30 19:54:32 +0100

    [FIX] ir.actions: fix dangling {create,write}_uid before adding FKs

--- migrations/base/8.0.1.3/pre-00-cleanup.py
+++ migrations/base/8.0.1.3/pre-00-cleanup.py
@@ -7,3 +7,18 @@ def migrate(cr, version):
     models = ['document.ftp.browse', 'ir.ui.view_sc', 'google.docs.config']
     for model in models:
         util.delete_model(cr, model)
+
+    # 8.0 adds FK on ir_actions submodels (were missing before), so we need to
+    # cleanup the frequently dangling ones: write_uid/create_uid
+    for table in ('ir_act_client', 'ir_act_url', 'ir_act_window',
+                  'ir_act_window_view', 'ir_act_report_xml',
+                  'ir_act_server'):
+        cr.execute("""
+                    UPDATE "%s" SET create_uid = 1 WHERE NOT EXISTS
+                        (SELECT 1 FROM res_users WHERE id = "%s".create_uid)
+                   """ % (table, table))
+
+        cr.execute("""
+                    UPDATE "%s" SET write_uid = 1 WHERE NOT EXISTS
+                        (SELECT 1 FROM res_users WHERE id = "%s".write_uid)
+                   """ % (table, table))
