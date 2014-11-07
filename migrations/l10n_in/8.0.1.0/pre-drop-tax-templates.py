7defe1adbe538b3b12474108667601f4e5264af1
Christophe Simonis <chs@odoo.com>
2014-11-07 15:16:37 +0100

    [FIX] l10n_in@8.0: only delete account.tax.template but delete all the one linked to a l10n_in account.chart.template

--- migrations/l10n_in/8.0.1.0/pre-drop-tax-templates.py
+++ migrations/l10n_in/8.0.1.0/pre-drop-tax-templates.py
@@ -1,18 +1,12 @@
 # -*- coding: utf-8 -*-
 def migrate(cr, version):
-    cr.execute("""DELETE FROM account_fiscal_position_tax_template
-                        WHERE id IN (SELECT res_id
-                                       FROM ir_model_data
-                                      WHERE module = 'l10n_in'
-                                        AND model = 'account.fiscal.position.tax.template');
-               """)
     cr.execute("""DELETE FROM account_tax_template
-                        WHERE id IN (SELECT res_id
-                                       FROM ir_model_data
-                                      WHERE module = 'l10n_in'
-                                        AND model = 'account.tax.template');
+                        WHERE chart_template_id IN (SELECT res_id
+                                                      FROM ir_model_data
+                                                     WHERE module = 'l10n_in'
+                                                       AND model = 'account.chart.template');
                """)
     cr.execute("""DELETE FROM ir_model_data
                         WHERE module = 'l10n_in'
-                          AND model IN ('account.tax.template', 'account.fiscal.position.tax.template');
+                          AND model = 'account.tax.template'
                """)
