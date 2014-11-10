a2922763a7afd8b0feed4528abea2a6d053f9c39
Christophe Simonis <chs@odoo.com>
2014-11-10 17:25:05 +0100

    [ADD] l10n_es@8.0: drop tax templates

---
+++ migrations/l10n_es/8.0.4.0/pre-drop-tax-templates.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+def migrate(cr, version):
+    cr.execute("""DELETE FROM account_fiscal_position_tax_template
+                        WHERE chart_template_id IN (SELECT res_id
+                                                      FROM ir_model_data
+                                                     WHERE module = 'l10n_es'
+                                                       AND model = 'account.chart.template');
+               """)
+    cr.execute("""DELETE FROM account_tax_template
+                        WHERE chart_template_id IN (SELECT res_id
+                                                      FROM ir_model_data
+                                                     WHERE module = 'l10n_es'
+                                                       AND model = 'account.chart.template');
+               """)
+    cr.execute("""DELETE FROM ir_model_data
+                        WHERE module = 'l10n_es'
+                          AND model IN ('account.tax.template', 'account.fiscal.position.tax.template')
+               """)
