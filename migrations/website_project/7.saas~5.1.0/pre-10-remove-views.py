dad282a017bfd39fcaf4b5a4961598d731ce3a2b
Christophe Simonis <christophe@kn.gl>
2014-11-20 17:18:58 +0100

    [ADD] website_project@saas-5: remove views

---
+++ migrations/website_project/7.saas~5.1.0/pre-10-remove-views.py
@@ -0,0 +1,9 @@
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    """As deleted xmlid are deleted only at the end of the update, deleted
+       views still exists in database during the update process and used when
+       applying the view inheritance. If the views use a deleted field,
+       this raise an error. We delete it ourself to avoid this.
+    """
+    util.remove_record(cr, 'website_project.footer_custom')
