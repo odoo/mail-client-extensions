0f2dd33b0fe4729c6d4b9ed0c9418bb07bb77bda
Christophe Simonis <christophe@kn.gl>
2014-11-26 17:36:09 +0100

    [FIX] website_sale@saas-5: force noupdate of view instead of deleting it

--- migrations/website_sale/7.saas~5.1.0/pre-10-update_view.py
+++ migrations/website_sale/7.saas~5.1.0/pre-10-update_view.py
@@ -1,11 +1,18 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.remove_record(cr, 'website_sale.product')
     util.remove_record(cr, 'website_sale.view_sales_config_website_sale')
 
     # Shop templates need to be reset - they're not compatible anymore
+    cr.execute("""UPDATE ir_model_data
+                     SET name='product_comment'
+                   WHERE model='website_sale'
+                     AND name='product_option_openchatter'
+               """)
+
+    util.force_noupdate(cr, 'website_sale.product', False)
+
     util.force_noupdate(cr, 'website_sale.products', False)
     util.force_noupdate(cr, 'website_sale.categories_recursive', False)
     util.force_noupdate(cr, 'website_sale.products_categories', False)
