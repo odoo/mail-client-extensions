bc4e965c8643660f2f2d789ce3026680173377f6
Christophe Simonis <christophe@kn.gl>
2014-11-27 18:46:57 +0100

    [ADD] website_partner@saas-4: force update view

---
+++ migrations/website_partner/7.saas~4.0.1/pre-reset-view.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.force_noupdate(cr, 'website_partner.partner_detail', False)
