c0bbec2c85a5e25522dca67e5b630ddafaf29df4
Olivier Dony <odo@openerp.com>
2014-12-09 20:02:24 +0100

    [IMP] util, stock: add util.remove_view to allow a safe cascade-deletion of deprecated views
    In migration environments where deprecated custom views should be
    automatically disabled, the environment variable
    OE_DROP_DEPRECATED_CUSTOM should be set to a non-empty value.
    
    In stock migration for saas-5, remove the explicit listing of
    inherited views to delete, and use the new remove_view()
    mechanism instead.

--- migrations/stock/7.saas~5.1.1/pre-00-rm-views.py
+++ migrations/stock/7.saas~5.1.1/pre-00-rm-views.py
@@ -8,39 +8,11 @@ def migrate(cr, version):
        applying the view inheritance. If the views use a deleted field,
        this raise an error. We delete it ourself to avoid this.
     """
-
-    views = filter(None, map(methodcaller('strip'), """
-            sale_journal.sale_journal_picking_order_tree_in
-        stock.view_picking_in_tree
-
-            sale_journal.sale_journal_picking_order_in
-        stock.view_picking_in_form
-
-            claim_from_delivery.crm_claim_from_delivery
-            sale_stock.stock_picking_out_inherit_sale
-            sale_journal.sale_journal_picking_order_tree_out
-        stock.view_picking_out_tree
-
-            sale_journal.sale_journal_picking_order_out
-        stock.view_picking_out_form
-
-                mrp.product_product_normal_form_supply_view
-                project_mrp.product_product_normal_form_supply_view
-                sale_service.product_product_normal_form_supply_view
-                purchase.product_product_normal_form_procurement_help_view
-                purchase_requisition.product_normal_form_view_inherit
-                stock_no_autopicking.view_product_form_auto_pick
-            procurement.product_form_view_procurement_button
-            point_of_sale.product_normal_form_view
-            product_expiry.view_product_form_expiry
-            project_mrp.view_product_task_form
-            sale_service.view_product_task_form
-        stock.view_normal_procurement_locations_form
-
-        stock.view_normal_property_acc_form
-        stock.view_category_property_form
-
-    """.splitlines()))
-
-    for v in views:
-        util.remove_record(cr, v)
+    for view in ('stock.view_picking_in_tree',
+                 'stock.view_picking_in_form',
+                 'stock.view_picking_out_tree',
+                 'stock.view_picking_out_form',
+                 'stock.view_normal_procurement_locations_form',
+                 'stock.view_normal_property_acc_form',
+                 'stock.view_category_property_form'):
+        util.remove_view(cr, view)
