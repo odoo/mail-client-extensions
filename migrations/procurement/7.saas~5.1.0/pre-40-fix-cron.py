61f27107b5e7a2df6c7a96a6ea5932f794a81282
Christophe Simonis <christophe@kn.gl>
2014-12-11 11:18:15 +0100

    [FIX] procurement@saas-5: force deletion of fast cron

--- migrations/procurement/7.saas~5.1.0/pre-40-fix-cron.py
+++ migrations/procurement/7.saas~5.1.0/pre-40-fix-cron.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
+
+    # created in noupdate, so wont be deleted automatically by `_auto_end`
+    util.remove_record(cr, 'procurement.ir_cron_scheduler_action_fast')
+
     cr.execute("""UPDATE ir_cron
                      SET args='(True,)'
                    WHERE id IN (SELECT res_id
