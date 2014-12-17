930593035727a362b01d780568c8ae3e9702b3e2
Samus CTO <cto@openerp.com>
2014-12-17 14:51:18 +0100

    [FIX] optionally mark SaaS modules as "to install"
    This is supposed to be used only in SaaS. We are going to use the
    special flag OE_SAAS_MIGRATION in order to permit on-site databases to
    be migrated to our SaaS service.

--- migrations/base/7.saas~1.1.3/post-00-ensure_saas_trial_deps.py
+++ migrations/base/7.saas~1.1.3/post-00-ensure_saas_trial_deps.py
@@ -1,10 +1,15 @@
+from os import environ as ENV
+
+
+SAAS = ENV.get("OE_SAAS_MIGRATION")
 
 def migrate(cr, version):
-    # ensure saas_trial and its dependencies are (to) installed
-    states = ('installed', 'to install', 'to upgrade')
-    modules = tuple("saas_trial auth_oauth auth_signup web_analytics base_import".split())
-    cr.execute("""UPDATE ir_module_module
-                     SET state=%s
-                   WHERE name IN %s
-                     AND state NOT IN %s
-               """, ('to install', modules, states))
+    if SAAS:
+        # ensure saas_trial and its dependencies are (to) installed
+        states = ('installed', 'to install', 'to upgrade')
+        modules = tuple("saas_trial auth_oauth auth_signup web_analytics base_import".split())
+        cr.execute("""UPDATE ir_module_module
+                         SET state=%s
+                       WHERE name IN %s
+                         AND state NOT IN %s
+                   """, ('to install', modules, states))
