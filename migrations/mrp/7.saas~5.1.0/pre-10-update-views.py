afe3c2214cac56f2ec1f864985c0d90ee3ca06dc
Samus CTO <cto@openerp.com>
2015-01-09 17:58:24 +0100

    [IMP] mrp@7.saas~5: the view has been renamed

--- migrations/mrp/7.saas~5.1.0/pre-10-update-views.py
+++ migrations/mrp/7.saas~5.1.0/pre-10-update-views.py
@@ -3,3 +3,5 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.remove_view(cr, 'mrp.product_product_normal_form_supply_view')
+    util.rename_xmlid(cr,         'mrp.product_form_view_bom_button',
+                          'mrp.product_product_form_view_bom_button')
