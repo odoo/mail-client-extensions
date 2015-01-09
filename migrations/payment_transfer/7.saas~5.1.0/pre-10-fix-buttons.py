8579f8e4b0f5d9ee86b5bad62db19aa96c4b353f
Christophe Simonis <christophe@kn.gl>
2015-01-09 15:28:02 +0100

    [ADD] payment_{adyen,ogone,paypal,transfer}@saas-5: fix acquirer buttons

---
+++ migrations/payment_transfer/7.saas~5.1.0/pre-10-fix-buttons.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    vid = util.ref(cr, 'payment_transfer.transfer_acquirer_button')
+    if vid:
+        cr.execute("""UPDATE ir_ui_view
+                         SET arch=replace(arch, 'type="image" name="submit"', 'type="submit"')
+                       WHERE id=%s
+                   """, [vid])
