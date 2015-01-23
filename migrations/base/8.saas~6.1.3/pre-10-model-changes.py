ee5a9b87afb6a9cf8f3fdf96c8ec46f60c90fe03
Christophe Simonis <christophe@kn.gl>
2015-01-23 18:18:39 +0100

    [ADD] base@saas-6: create new mandatory column `ir_model_constraint.definition`

---
+++ migrations/base/8.saas~6.1.3/pre-10-model-changes.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.create_column(cr, 'ir_model_constraint', 'definition', 'varchar')
