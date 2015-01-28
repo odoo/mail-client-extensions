7fb6a502ce1f4833e6453d253d83310934b3d175
Christophe Simonis <christophe@kn.gl>
2015-01-28 16:43:19 +0100

    [ADD] base@saas-6: merge_module `email_template` into `mail` module

--- migrations/base/8.saas~6.1.3/pre-00-modules-changes.py
+++ migrations/base/8.saas~6.1.3/pre-00-modules-changes.py
@@ -3,6 +3,8 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
 
+    util.merge_module(cr, 'email_template', 'mail')
+
     util.new_module(cr, 'website_links')
     util.new_module_dep(cr, 'mass_mailing', 'website_links')
 
