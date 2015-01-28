b7a404f69aacf83b4a63d27f5752d7f94f38b9a8
Christophe Simonis <christophe@kn.gl>
2015-01-28 16:16:51 +0100

    [ADD] base@saas-6: register new module `web_tip` and its dependencies

---
+++ migrations/base/8.saas~6.1.3/pre-00-modules-changes.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.new_module(cr, 'web_tip')
+    util.new_module_dep(cr, 'web_tip', 'web')
+    for m in 'account crm event hr project purchase website_quote'.split():
+        util.new_module_dep(cr, m, 'web_tip')
