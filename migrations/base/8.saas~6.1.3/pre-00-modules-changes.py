dab6d6a426ba58ab96da893eaec377d4236a978c
Christophe Simonis <christophe@kn.gl>
2015-01-28 16:20:03 +0100

    [ADD] base@saas-6: register new module `web_links` and its dependencies

--- migrations/base/8.saas~6.1.3/pre-00-modules-changes.py
+++ migrations/base/8.saas~6.1.3/pre-00-modules-changes.py
@@ -2,6 +2,10 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
+
+    util.new_module(cr, 'website_links')
+    util.new_module_dep(cr, 'mass_mailing', 'website_links')
+
     util.new_module(cr, 'web_tip')
     util.new_module_dep(cr, 'web_tip', 'web')
     for m in 'account crm event hr project purchase website_quote'.split():
