d8cc496980eb3070aae6156826252d12b19c8d98
Christophe Simonis <christophe@kn.gl>
2015-01-30 17:28:12 +0100

    [ADD] base@saas-6: delete model `ir.sequence.type`

--- migrations/base/8.saas~6.1.3/pre-10-model-changes.py
+++ migrations/base/8.saas~6.1.3/pre-10-model-changes.py
@@ -3,3 +3,4 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.create_column(cr, 'ir_model_constraint', 'definition', 'varchar')
+    util.delete_model(cr, 'ir.sequence.type')
