cc14f54db614468348ea926d7a7716ad268fae73
Christophe Simonis <christophe@kn.gl>
2015-02-03 11:33:33 +0100

    [ADD] base@saas-6: register new module `barcodes` and its dependencies

--- migrations/base/8.saas~6.1.3/pre-00-modules-changes.py
+++ migrations/base/8.saas~6.1.3/pre-00-modules-changes.py
@@ -3,6 +3,11 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
 
+    util.new_module(cr, 'barcodes')
+    util.new_module_dep(cr, 'barcodes', 'web')
+    util.new_module_dep(cr, 'point_of_sale', 'barcodes')
+    util.new_module_dep(cr, 'stock', 'barcodes')
+
     util.merge_module(cr, 'email_template', 'mail')
 
     util.new_module(cr, 'website_links')
