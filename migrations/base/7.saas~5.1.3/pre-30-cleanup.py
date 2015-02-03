6f8e9413bb9a0cf023dc052e834a274f7397565a
Christophe Simonis <christophe@kn.gl>
2015-02-03 15:26:59 +0100

    [FIX] base@saas-5: correct cleanup
     - use `remove_view` for user group view
     - do not use `remove_view` for user form view

--- migrations/base/7.saas~5.1.3/pre-30-cleanup.py
+++ migrations/base/7.saas~5.1.3/pre-30-cleanup.py
@@ -3,7 +3,8 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     # this views is auto-generated when updating res.groups
-    util.remove_record(cr, 'base.user_groups_view')
+    util.remove_view(cr, 'base.user_groups_view')
 
-    # this view contains deleted fields (gmail_user, ...)
-    util.remove_view(cr, 'base.view_users_form')
+    # change in this view is minimal (c7e68feca83c0ce52fe815e827db3a588facaf9a)
+    # removing the view is unnecessary and overkill.
+    util.force_noupdate(cr, 'base.view_users_form', False)
