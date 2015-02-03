02aeaf1fcec68c395aab8bf7229162cb288be13a
Christophe Simonis <christophe@kn.gl>
2015-02-03 13:11:30 +0100

    [ADD] base@saas-6: remove old modules

--- migrations/base/8.saas~6.1.3/pre-00-modules-changes.py
+++ migrations/base/8.saas~6.1.3/pre-00-modules-changes.py
@@ -3,6 +3,14 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
 
+    util.remove_module(cr, 'base_report_designer')
+    util.remove_module(cr, 'crm_mass_mailing')
+    util.remove_module(cr, 'crm_profiling')     # FIXME convert to a survey?
+    util.remove_module(cr, 'portal_claim')
+    util.remove_module(cr, 'portal_project_issue')
+    util.remove_module(cr, 'web_graph')
+    util.remove_module(cr, 'website_instantclick')
+
     util.new_module(cr, 'barcodes')
     util.new_module_dep(cr, 'barcodes', 'web')
     util.new_module_dep(cr, 'point_of_sale', 'barcodes')
