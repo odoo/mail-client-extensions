da3f02a36104ef82f6203e42b5510cca8c7f4444
Samus CTO <cto@openerp.com>
2015-02-10 12:59:50 +0100

    [IMP] stock@7.saas~5: automatically fix UoM factor's lack of precision

---
+++ migrations/product/7.saas~5.1.1/pre-10-fix-most-uom.py
@@ -0,0 +1,23 @@
+import logging
+
+from openerp.addons.base.maintenance.migrations import util
+
+NS = 'openerp.addons.base.maintenance.migrations.product.saas-5.'
+_logger = logging.getLogger(NS + __name__)
+
+def fix_uom_factor_lack_precision(cr):
+    cr.execute("""
+        UPDATE  product_uom
+        SET     factor = 1.0 / round(1.0 / factor, 2)
+        WHERE   factor != 0
+        AND     round(1.0 / factor, 2) != 0
+        AND     abs((1.0 / round(1.0 / factor, 2)) - factor) != 0
+        AND     abs((1.0 / round(1.0 / factor, 2)) - factor) < 0.000001
+        RETURNING id, name, factor;
+        """, [])
+    for id, name, factor in cr.fetchall():
+        _logger.info("[UoM %s (%s) must be more accurate] factor is now %s",
+                     id, name, factor)
+
+def migrate(cr, version):
+    fix_uom_factor_lack_precision(cr)
