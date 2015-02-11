fc7be216f93dbb5e105faf8c7417a972cf6958cb
Christophe Simonis <christophe@kn.gl>
2015-02-11 13:37:22 +0100

    [IMP] base@saas-6: move ir.attachment fields from `website` module

--- migrations/base/8.saas~6.1.3/pre-10-model-changes.py
+++ migrations/base/8.saas~6.1.3/pre-10-model-changes.py
@@ -4,3 +4,10 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.create_column(cr, 'ir_model_constraint', 'definition', 'varchar')
     util.delete_model(cr, 'ir.sequence.type')
+
+    if util.column_exists(cr, 'ir_attachment', 'datas_checksum'):
+        util.move_field_to_module(cr, 'ir.attachment', 'datas_checksum', 'website', 'base')
+        util.rename_field(cr, 'ir.attachment', 'datas_checksum', 'checksum')
+
+    if util.column_exists(cr, 'ir_attachment', 'mimetype'):
+        util.move_field_to_module(cr, 'ir.attachment', 'mimetype', 'website', 'base')
