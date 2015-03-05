98c30152e512405e88c670b742091d7cd659bfb3
Jalpesh <patel@pja-odoo.(none)>
2015-03-05 17:55:49 +0530

    [FIX] calendar@saas~3: view's xmlid has changed
    Fixes #94

---
+++ migrations/calendar/7.saas~3.1.0/pre-10-rename-xmlid.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.rename_xmlid(cr, 'calendar.view_crm_meeting_form', 'calendar.view_calendar_event_form')
