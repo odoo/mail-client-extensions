ef5c74323f06941cba140b9787745a6d6b9f1f7a
Christophe Simonis <chs@odoo.com>
2015-03-11 19:11:50 +0100

    [ADD] mass_mailing@saas-6

---
+++ migrations/mass_mailing/8.saas~6.2.0/pre-10-adapt-models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.create_column(cr, 'mail_mail_statistics', 'state', 'varchar')
+    util.create_column(cr, 'mail_mail_statistics', 'state_update', 'timestamp without time zone')
+    cr.execute("""UPDATE mail_mail_statistics
+                     SET state_update=write_date,
+                         state=CASE WHEN bounced IS NOT NULL THEN 'bounced'
+                                    WHEN replied IS NOT NULL THEN 'replied'
+                                    WHEN opened IS NOT NULL THEN 'opened'
+                                    WHEN sent IS NOT NULL THEN 'sent'
+                                    WHEN "exception" IS NOT NULL THEN 'exception'
+                                    ELSE NULL
+                                END
+               """)
