ce5f305fb7365bcbd5771d934cf570c6f61666bc
Christophe Simonis <chs@odoo.com>
2015-03-13 11:32:46 +0100

    [ADD] hr_holidays@saas-6: compute payslip_status

--- migrations/hr_holidays/8.saas~6.1.5/pre-20-adpat-models.py
+++ migrations/hr_holidays/8.saas~6.1.5/pre-20-adpat-models.py
@@ -3,3 +3,6 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.drop_depending_views(cr, 'hr_holidays', 'state')
+
+    util.create_column(cr, 'hr_holidays', 'payslip_status', 'boolean')
+    cr.execute("UPDATE hr_holidays SET payslip_status=true WHERE state='validate'")
