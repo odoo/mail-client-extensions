7c65d796bb2e0941b51e1ab0516debe6ec9f286f
Christophe Simonis <chs@odoo.com>
2015-03-17 19:02:25 +0100

    [FIX] base@8.0: remove `edi.document` model
    This model has been deleted in 7.0 but may still be in the database
    being forgotten by previous migration.

--- migrations/base/8.0.1.3/pre-00-cleanup.py
+++ migrations/base/8.0.1.3/pre-00-cleanup.py
@@ -4,7 +4,7 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # there is still some referencing menu to old models (models still there)
     # remove them + all referencing data
-    models = ['document.ftp.browse', 'ir.ui.view_sc', 'google.docs.config']
+    models = ['document.ftp.browse', 'ir.ui.view_sc', 'google.docs.config', 'edi.document']
     for model in models:
         util.delete_model(cr, model)
 
