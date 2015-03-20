b78c4dfacd43a9d084dba1b994cdbd4b8c3d13ac
hpa-odoo <hpa@openerp.com>
2015-03-20 16:27:44 +0530

    [IMP] base@saas-2: set name and model in email_template created from ir_act_server

--- migrations/base/7.saas~2.1.3/pre-10-migrate_server_actions.py
+++ migrations/base/7.saas~2.1.3/pre-10-migrate_server_actions.py
@@ -23,7 +23,7 @@ def migrate(cr, v):
     cr.execute('DELETE from ir_act_server where state=%s', ('loop',))
 
     # email actions are defined by email_template module
-    cr.execute("SELECT id, email, subject, message, model_id FROM ir_act_server WHERE state=%s", ('email',))
+    cr.execute("SELECT id, name, email, subject, message, model_id FROM ir_act_server WHERE state=%s", ('email',))
     email_actions = list(cr.fetchall())
     if not email_actions:
         return
@@ -34,6 +34,8 @@ def migrate(cr, v):
     if not util.table_exists(cr, 'email_template'):
         cr.execute("""CREATE TABLE email_template(
                         id SERIAL NOT NULL,
+                        name varchar,
+                        model varchar,
                         model_id integer,
                         subject varchar,
                         email_from varchar,
@@ -44,18 +46,21 @@ def migrate(cr, v):
     email_from = '${user.email}'
     to_jinja = partial(re.sub, r'(\[\[.+\]\])', r'${\1}')
 
-    for aid, email_to, subject, message, model_id in email_actions:
+    for aid, name, email_to, subject, message, model_id in email_actions:
 
         email_to = (email_to or '""').strip() or '""'
         if email_to[0] == "[" and email_to[-1] == "]":
             email_to = "${',',join(%s)}" % (email_to,)
         else:
             email_to = "${%s}" % (email_to,)
+            
+        cr.execute("""select model from ir_model where id = %s""", [model_id])
+        model_name, = cr.fetchone()
 
-        cr.execute("""INSERT INTO email_template(model_id, subject, email_from, email_to, body_html)
-                                          VALUES(%s, %s, %s, %s, %s)
+        cr.execute("""INSERT INTO email_template(name, model, model_id, subject, email_from, email_to, body_html)
+                                          VALUES(%s, %s, %s, %s, %s, %s, %s)
                       RETURNING id
-                   """, (model_id, to_jinja(subject or ''), email_from, email_to, to_jinja(message or '')))
+                   """, (name, model_name, model_id, to_jinja(subject or ''), email_from, email_to, to_jinja(message or '')))
 
         tpl_id, = cr.fetchone()
         cr.execute("UPDATE ir_act_server SET template_id=%s WHERE id=%s", (tpl_id, aid))
