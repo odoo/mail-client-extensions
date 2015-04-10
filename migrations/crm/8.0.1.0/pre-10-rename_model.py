472559b94bfcdc8ff0198659f169aadd311083e5
Christophe Simonis <chs@odoo.com>
2015-04-10 12:57:23 +0200

    [FIX] crm@8.0: rename crm.lead.{type_id,campaign_id}

--- migrations/crm/8.0.1.0/pre-10-rename_model.py
+++ migrations/crm/8.0.1.0/pre-10-rename_model.py
@@ -4,3 +4,5 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.rename_model(cr, 'crm.case.channel', 'crm.tracking.medium')
     util.rename_model(cr, 'crm.case.resource.type', 'crm.tracking.campaign')
+
+    util.rename_field(cr, 'crm.lead', 'type_id', 'campaign_id')
