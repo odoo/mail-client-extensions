fb907bb4870faab26535a98e9fc22ece6e81b2cf
Samus CTO <cto@openerp.com>
2015-06-05 17:02:11 +0200

    [FIX] audittrail@7.saas~5: module disappeared but constraints exist

--- migrations/base/7.saas~5.1.3/pre-30-cleanup.py
+++ migrations/base/7.saas~5.1.3/pre-30-cleanup.py
@@ -8,3 +8,14 @@ def migrate(cr, version):
     # change in this view is minimal (c7e68feca83c0ce52fe815e827db3a588facaf9a)
     # removing the view is unnecessary and overkill.
     util.force_noupdate(cr, 'base.view_users_form', False)
+
+    if util.table_exists(cr, 'audittrail_log_line'):
+        cr.execute("""
+            ALTER TABLE audittrail_log_line
+                DROP CONSTRAINT audittrail_log_line_field_id_fkey;
+            ALTER TABLE audittrail_log_line
+                ADD CONSTRAINT audittrail_log_line_field_id_fkey
+                FOREIGN KEY (field_id)
+                REFERENCES ir_model_fields(id)
+                ON DELETE CASCADE;
+            """)
