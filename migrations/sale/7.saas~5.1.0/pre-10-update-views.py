a56de03700d9fde7729b4004ab2fe824ed5e03d4
hsa-odoo <hsa@openerp.com>
2015-06-26 10:26:44 +0530

    [FIX] sale@7.saas~5: rename xmlid and set proper sequence
    Fixes #117

--- migrations/sale/7.saas~5.1.0/pre-10-update-views.py
+++ migrations/sale/7.saas~5.1.0/pre-10-update-views.py
@@ -4,3 +4,19 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.rename_xmlid(cr, 'sale_crm.account_invoice_groupby_inherit',
                               'sale.account_invoice_groupby_inherit')
+    util.rename_xmlid(cr, 'sale_crm.mt_salesteam_order_sent', 'sale.mt_salesteam_order_sent')
+    util.rename_xmlid(cr, 'sale_crm.mt_salesteam_order_confirmed', 'sale.mt_salesteam_order_confirmed')
+
+    # Set correct sequnce id for 'Quotation Send' and 'Sales Order Confirmed'
+    cr.execute("""
+        UPDATE mail_message_subtype
+        SET sequence =
+                (CASE
+                    WHEN name='Quotation Send'
+                    THEN 20
+                    WHEN name='Sales Order Confirmed'
+                    THEN 21
+                ELSE sequence
+                END)
+        WHERE res_model='crm.case.section'
+        """)
