a65a0b1b06070e118d6487aa4c0468b814c7d97e
Samus CTO <cto@openerp.com>
2015-07-23 11:47:09 +0200

    [FIX] mail~8.0: memory overload
    Inserting all the id of a table in an array can occasionally cause an
    overload of the memory.

--- migrations/mail/8.0.1.0/pre-10-rm-dup-followers.py
+++ migrations/mail/8.0.1.0/pre-10-rm-dup-followers.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 def migrate(cr, version):
-    cr.execute("""DELETE FROM mail_followers
-                        WHERE id IN (SELECT unnest(x[2:array_upper(x, 1)])
-                                       FROM (SELECT array_agg(id) x
-                                               FROM mail_followers
-                                           GROUP BY res_model, res_id, partner_id
-                                             HAVING count(id) > 1
-                                            ) t
-                                    )
-               """)
+    cr.execute("""\
+        WITH mail_followers_to_keep AS (
+            SELECT min(id) AS id FROM mail_followers
+                GROUP BY res_model, res_id, partner_id
+        )
+        DELETE FROM mail_followers WHERE NOT EXISTS (
+            SELECT 1 FROM mail_followers_to_keep WHERE
+                mail_followers_to_keep.id = mail_followers.id
+        );
+        """)
