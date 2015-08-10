1221348d5e80ab945883a435b78b0f7a2b0be23a
Christophe Simonis <chs@odoo.com>
2015-08-10 15:12:05 +0200

    [FIX] crm@saas-6: rewrite mass_mailing domains

--- migrations/crm/8.saas~6.1.0/pre-30-change-filters.py
+++ migrations/crm/8.saas~6.1.0/pre-30-change-filters.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     cr.execute("""
@@ -6,3 +7,10 @@ def migrate(cr, version):
            SET domain = regexp_replace(domain, '\ycateg_ids\y', 'tag_ids', 'g')
            WHERE model_id = 'crm.lead'
     """)
+    if util.table_exists(cr, 'mail_mass_mailing'):
+        cr.execute("""
+            UPDATE mail_mass_mailing
+               SET mailing_domain = regexp_replace(mailing_domain, '\ycateg_ids\y', 'tag_ids', 'g')
+             WHERE mailing_model='crm.lead'
+               AND mailing_domain IS NOT NULL
+        """)
