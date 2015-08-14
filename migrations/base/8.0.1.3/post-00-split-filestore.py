d14110cbe72ebfa69cac6df4fbac3b78bda6971e
Christophe Simonis <chs@odoo.com>
2015-08-14 11:37:48 +0200

    [IMP] base@8.0: add main() to script that move attachments to filestore

--- migrations/base/8.0.1.3/post-00-split-filestore.py
+++ migrations/base/8.0.1.3/post-00-split-filestore.py
@@ -51,3 +51,15 @@ def move_db_attachments_to_disk(cr):
 def migrate(cr, version):
     if MOVE_TO_FILESTORE:
         move_db_attachments_to_disk(cr)
+
+if __name__ == '__main__':
+    import logging
+    logging.basicConfig(level=logging.ERROR)
+    from openerp.addons.base.maintenance.migrations import util
+    from openerp.api import Environment
+
+    def main(cr, _):
+        with Environment.manage():
+            move_db_attachments_to_disk(cr)
+
+    util.main(main)
