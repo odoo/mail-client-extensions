bde7c0fbc0120f25545d26a814c6f6a5d3dcf7e4
Christophe Simonis <chs@odoo.com>
2015-09-28 15:35:27 +0200

    [ADD] project_issue@9.0

---
+++ migrations/project_issue/9.0.1.0/pre-10-adapt-models.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    cr.execute("UPDATE project_issue SET kanban_state='normal' WHERE kanban_state IS NULL")
+
+    # categories -> tags
+    cr.execute("""ALTER TABLE project_category_project_issue_rel
+                    RENAME TO project_issue_project_tags_rel""")
+    cr.execute("""ALTER TABLE project_issue_project_tags_rel
+                RENAME COLUMN project_category_id TO project_tags_id""")
+
+    cr.execute("""
+        INSERT INTO project_tags(name)
+             SELECT v.name
+               FROM project_issue i JOIN project_issue_version v ON (v.id = i.version_id)
+              WHERE NOT EXISTS(SELECT 1 FROM project_tags WHERE name=v.name)
+           GROUP BY v.name
+    """)
+    cr.execute("""
+        INSERT INTO project_issue_project_tags_rel(project_issue_id, project_tags_id)
+             SELECT i.id, t.id
+               FROM project_issue i
+               JOIN project_issue_version v ON (v.id = i.version_id)
+               JOIN project_tags t ON (t.name = v.name)
+              WHERE NOT EXISTS(SELECT 1
+                                 FROM project_issue_project_tags_rel s
+                                WHERE s.project_issue_id = i.id
+                                  AND s.project_tags_id = t.id)
+    """)
+
+    util.remove_field(cr, 'project_issue', 'version_id')
+    util.delete_model(cr, 'project.issue.version')
