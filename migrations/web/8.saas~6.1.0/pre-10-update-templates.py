286890fcc23f32494054d9c83254868724d91c55
Christophe Simonis <chs@odoo.com>
2015-10-21 15:36:30 +0200

    [ADD] web_*: force update of assets_backend view

--- migrations/web/8.saas~6.1.0/pre-10-update-templates.py
+++ migrations/web/8.saas~6.1.0/pre-10-update-templates.py
@@ -4,3 +4,7 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     for t in 'assets_common assets_backend menu'.split():
         util.force_noupdate(cr, 'web.' + t, False)
+
+    # reset backend asset for other web_* modules (no need to create 1 line migreation scripts)
+    for m in 'calendar diagram gantt kanban tip'.split():
+        util.force_noupdate(cr, 'web_%s.assets_backend' % m, False)
