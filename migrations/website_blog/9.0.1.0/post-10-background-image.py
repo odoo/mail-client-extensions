4cc2d11a93fa7f25b5ac8b1966f7631795c7c490
Christophe Simonis <chs@odoo.com>
2015-10-21 19:13:20 +0200

    [ADD] website_blog@9.0

---
+++ migrations/website_blog/9.0.1.0/post-10-background-image.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    # FIXME background url will not work in 9.0
+    # /website/image is not a valid route anymore
+    cr.execute("""
+        UPDATE blog_post
+           SET cover_properties = concat('{"resize_class": "cover cover-full", "background-color": "oe_none", "background-image": "', encode(background_image, 'escape'), '"}')
+         WHERE background_image IS NOT NULL
+    """)
+
+    util.remove_field(cr, 'blog.post', 'background_image')
