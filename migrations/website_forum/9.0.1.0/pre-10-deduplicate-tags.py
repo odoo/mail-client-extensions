ecc8474061b1a7d7e3fa03c5cdfbaad5f337fabe
Christophe Simonis <chs@odoo.com>
2015-10-28 12:17:50 +0100

    [ADD] website_forum@9.0

---
+++ migrations/website_forum/9.0.1.0/pre-10-deduplicate-tags.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    # temporarily allow duplicated tags per post
+    cr.execute("ALTER TABLE forum_tag_rel DROP CONSTRAINT forum_tag_rel_forum_id_forum_tag_id_key")
+
+    cr.execute("""SELECT array_agg(id) as ids
+                    FROM forum_tag
+                   GROUP BY forum_id, trim(lower(name))
+                  HAVING COUNT(id) > 1""")
+
+    todel = []
+    for row in cr.fetchall():
+        ids = row[0]
+        ids.sort()
+        cid, other_ids = ids[0], ids[1:]
+        todel.extend(other_ids)
+
+        for oid in other_ids:
+            util.replace_record_references(cr, ('forum.tag', oid), ('forum.tag', cid))
+
+    if todel:
+        cr.execute('DELETE FROM forum_tag WHERE id IN %s', [tuple(todel)])
+        fix_m2m = util.import_script('base/9.0.1.3/pre-00-fixup-m2m.py')
+        fix_m2m.fixup_m2m(cr, 'forum_tag_rel',
+                          'forum_post', 'forum_tag', 'forum_id', 'forum_tag_id')
+
+    cr.execute("UPDATE forum_tag SET name = trim(name)")
