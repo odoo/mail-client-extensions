28dad8dbca586a3b17ff8a9baff27dff7b78cb8b
Christophe Simonis <chs@odoo.com>
2015-10-28 17:26:39 +0100

    [ADD] website_sale_delivery@9.0

---
+++ migrations/website_sale_delivery/9.0.1.0/pre-10-keep-carrier-description.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    cr.execute("""
+        UPDATE product_template t
+           SET description_sale = c.website_description
+          FROM product_product p, delivery_carrier c
+         WHERE p.product_tmpl_id = t.id
+           AND c.product_id = p.id
+           AND t.description_sale IS NULL
+           AND c.website_description IS NOT NULL
+    """)
+    util.remove_columns(cr, 'delivery_carrier', 'website_description')
