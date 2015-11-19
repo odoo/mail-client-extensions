d6c9e36b779585a16972b913f7c4f457accfdbb1
csn <csn@openerp.com>
2015-11-19 11:22:49 +0100

    [FIX] accounting@9.0: forgot to migrate payment_method on journal

---
+++ migrations/account/9.0.1.1/post-60-journal.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    cr.execute("""SELECT j.id AS journal_id, i.id AS payment_method_id, i.payment_type 
+                    FROM account_journal j, account_payment_method i 
+                    WHERE j.type IN ('bank', 'cash') AND i.payment_type IN ('inbound', 'outbound')
+                """)
+
+    for record in cr.dictfetchall():
+        if record['payment_type'] == 'inbound':
+            cr.execute("""INSERT into account_journal_inbound_payment_method_rel(journal_id, inbound_payment_method)
+                            VALUES (%s, %s)
+                        """, (record['journal_id'], record['payment_method_id']))
+        else:
+            cr.execute("""INSERT into account_journal_outbound_payment_method_rel(journal_id, outbound_payment_method)
+                            VALUES (%s, %s)
+                        """, (record['journal_id'], record['payment_method_id']))
