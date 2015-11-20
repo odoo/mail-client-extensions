2fe133c91ff1931d5988102b31d76cbb12a8bd44
csn <csn@openerp.com>
2015-11-20 11:10:10 +0100

    [FIX] accounting@9.0: missing migration of invoice_id on account_move_line

--- migrations/account/9.0.1.1/pre-35-aml.py
+++ migrations/account/9.0.1.1/pre-35-aml.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     cr.execute("""UPDATE account_move_line aml
@@ -12,3 +12,10 @@ def migrate(cr, version):
         SET date_maturity = date
         WHERE aml.date_maturity IS NULL
         """)
+
+    util.create_column(cr, 'account_move_line', 'invoice_id', 'int4')
+    cr.execute("""UPDATE account_move_line aml
+                    SET invoice_id = inv.id
+                    FROM account_invoice inv
+                    WHERE inv.move_id = aml.move_id
+                """)
