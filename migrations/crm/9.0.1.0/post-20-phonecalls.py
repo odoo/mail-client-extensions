ce64e2ba368705535f92905cf54fa8efc1ee3c60
Christophe Simonis <chs@odoo.com>
2015-11-20 11:02:52 +0100

    [FIX] crm@9.0: correct phonecall conversion.

--- migrations/crm/9.0.1.0/post-20-phonecalls.py
+++ migrations/crm/9.0.1.0/post-20-phonecalls.py
@@ -23,27 +23,35 @@ def migrate(cr, version):
 # Note about your Phonecalls
 
 Your phonecalls have been converted to comments on the related lead/opportunities.
-If you are using these phonecalls as a call center, you may want to install the [VOIP](/web#id=%d&view_type=form&model=ir.module.module) Application.
+If you are using these phonecalls as a call center, you may want to install the [VOIP](/web#id=%d&view_type=form&model=ir.module.module) App.
 [Contact us](mailto:online@odoo.com) if you want to avoid the conversion to comments.
 """ % voip_id
 
         util.announce(cr, '9.0', message, format='md', header=None)
 
     env = util.env(cr)
-
     activity = env.ref('crm.crm_activity_data_call')
-    stg_new = util.ref(cr, 'crm.stage_1')
+    stg_new = util.ref(cr, 'crm.stage_lead1')
+    stg_done = util.ref(cr, 'crm.stage_lead5')
 
-    stg_dead = env['crm.stage'].search([('type', 'in', ['lead', 'both']),
-                                        ('probability', '=', 0),
-                                        ('fold', '=', True)])[0].id
-    stg_done = env['crm.stage'].search([('type', '=', 'opportunity')], order='sequence')[0].id
+    stg_dead = env['crm.stage'].search([('type', 'in', ['lead', 'both']), ('probability', '=', 0)],
+                                       order='fold desc, sequence desc', limit=1)
+    if not stg_dead:
+        stg_dead = env['crm.stage'].create({
+            'name': 'Dead',
+            'type': 'lead',
+            'probability': 0,
+            'sequence': 999,
+            'fold': True,
+        })
+    stg_dead = stg_dead.id
 
+    team_col = 'team_id' if util.column_exists(cr, 'crm_phonecall', 'team_id') else 'section_id'
     cr.execute("""
       WITH new_leads AS (
         INSERT INTO crm_lead(name, partner_id, team_id, user_id, description, priority,
                              phone, mobile, active, type,stage_id, message_bounce)
-             SELECT 'Phonecall', partner_id, team_id, user_id, description, priority,
+             SELECT 'Phonecall', partner_id, {0}, user_id, description, priority,
                     partner_phone, partner_mobile, active,
                     case when state = 'done' then 'opportunity' else 'lead' end,
                     case when state = 'done' then %s when state = 'cancel' then %s else %s end,
@@ -62,7 +70,7 @@ If you are using these phonecalls as a call center, you may want to install the 
          SET message_bounce = 0
         FROM new_leads n
        WHERE l.id = n.id
-    """, [stg_done, stg_dead, stg_new])
+    """.format(team_col), [stg_done, stg_dead, stg_new])
 
     cr.execute("""
       WITH new_tags AS (
--- migrations/crm/9.0.1.0/pre-10-adapt-models.py
+++ migrations/crm/9.0.1.0/pre-10-adapt-models.py
@@ -7,3 +7,14 @@ def migrate(cr, version):
     # keep NULL as default value, no need to set them to 0
     util.create_column(cr, 'res_users', 'target_sales_done', 'int4')
     util.create_column(cr, 'res_users', 'target_sales_won', 'int4')
+
+    # rename some xmlids
+    cr.execute("""
+        DELETE FROM ir_model_data
+              WHERE model='crm.stage'
+                AND module='crm'
+                AND name IN ('stage_lead2', 'stage_lead5', 'stage_lead7')
+    """)
+    util.rename_xmlid(cr, 'crm.stage_lead3', 'crm.stage_lead2')
+    util.rename_xmlid(cr, 'crm.stage_lead4', 'crm.stage_lead3')
+    util.rename_xmlid(cr, 'crm.stage_lead6', 'crm.stage_lead5')
