ff9caa4d9e20165988a241f16d6b6308b1ec2c1a
Christophe Simonis <chs@odoo.com>
2015-11-23 15:45:15 +0100

    [FIX] mail@9.: force update record rule

--- migrations/mail/9.0.1.0/pre-10-model-changes.py
+++ migrations/mail/9.0.1.0/pre-10-model-changes.py
@@ -45,6 +45,7 @@ def migrate(cr, version):
     util.remove_view(cr, 'mail.view_users_form_mail')
 
     util.force_noupdate(cr, 'mail.mail_followers_read_write_others', False)
+    util.force_noupdate(cr, 'mail.mail_followers_read_write_own', False)
 
     util.drop_depending_views(cr, 'mail_message', 'model')
     cr.execute("UPDATE mail_message SET type='email' WHERE type IS NULL AND message_id IS NOT NULL")
