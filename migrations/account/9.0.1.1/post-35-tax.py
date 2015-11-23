a8180b97965dc1ba0ccef623ba6ce7f0e5c37c46
pvy <pvy@odoo.com>
2015-11-23 15:11:41 +0100

    [IMP] accounting@9.0: delete all child taxes with amount = 0, and then flatten tax groups with only 1 child tax

--- migrations/account/9.0.1.1/post-35-tax.py
+++ migrations/account/9.0.1.1/post-35-tax.py
@@ -12,3 +12,20 @@ def migrate(cr, version):
     cr.execute("""DELETE FROM account_tax_group
         WHERE name='migration'
         """)
+
+    cr.execute("""UPDATE account_tax
+        SET active = false
+        WHERE amount = 0 AND amount_type = 'percent' AND id in (SELECT child_tax from account_tax_filiation_rel where parent_tax IS NOT NULL)
+        """)
+
+    env = util.env(cr)
+
+    for parent_tax in env['account.tax'].search([]):
+        childs = parent_tax.children_tax_ids.filtered(lambda t: t.active)
+        if len(childs) == 1:
+            parent_tax.amount_type = 'percent'
+            parent_tax.amount = childs.amount
+            childs.active = False
+        if len(childs) == 0:
+            parent_tax.amount_type = 'percent'
+            parent_tax.amount = 0
