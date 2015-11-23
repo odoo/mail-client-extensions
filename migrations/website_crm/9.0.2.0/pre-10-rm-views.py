c196251be702aff75fd5ce3950401a29f9efbf77
Christophe Simonis <chs@odoo.com>
2015-11-23 12:32:34 +0100

    [FIX] website_crm@9.0: force update of templates

--- migrations/website_crm/9.0.2.0/pre-10-rm-views.py
+++ migrations/website_crm/9.0.2.0/pre-10-rm-views.py
@@ -3,3 +3,5 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.remove_view(cr, 'website_crm.contactus_form_company_name')
+    util.force_noupdate(cr, 'website_crm.contactus_form', False)
+    util.force_noupdate(cr, 'website_crm.contactus_thanks', False)
