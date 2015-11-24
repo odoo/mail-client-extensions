75ea0af0d261c93c244be8c8fabd1035f0b160b4
Christophe Simonis <chs@odoo.com>
2015-11-24 18:18:10 +0100

    [FIX] account@9.0: rename property fields (fixup)

--- migrations/account/9.0.1.1/post-25-property.py
+++ migrations/account/9.0.1.1/post-25-property.py
@@ -2,21 +2,11 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    props = util.splitlines("""
-        # res.partner
-        account_payable
-        account_receivable
-        account_position
-        payment_term
-        supplier_payment_term
-
-        # product.category
-        account_income_categ
-        account_expense_categ
-
-        # product.template
-        account_income
-        account_expense
-    """)
-    for p in props:
-        util.rename_field(cr, 'property_%s' % p, 'property_%s_id' % p)
+    prop_map = {
+        'res.partner': "account_payable account_receivable account_position payment_term supplier_payment_term".split(),
+        'product.category': 'account_income_categ account_expense_categ'.split(),
+        'product.template': 'account_income account_expense'.split(),
+    }
+    for model, props in prop_map.items():
+        for p in props:
+            util.rename_field(cr, model, 'property_%s' % p, 'property_%s_id' % p)
