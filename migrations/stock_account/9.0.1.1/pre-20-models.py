24b9f2b12dc469a97696f7f3262531d82801e545
Olivier Laurent <olt@odoo.com>
2015-11-24 08:25:32 +0100

    [FIX] stock_account@9.0: update property_stock_account_output_categ
    In addition to 'property_stock_account_input_categ',
    'property_stock_account_output_categ' should also be handled.

--- migrations/stock_account/9.0.1.1/pre-20-models.py
+++ migrations/stock_account/9.0.1.1/pre-20-models.py
@@ -4,9 +4,11 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.rename_field(cr, 'product_template', 'valuation', 'property_valuation')
     util.rename_field(cr, 'product_template', 'cost_method', 'property_cost_method')
-    util.rename_field(cr, 'product_category',
-                      'property_stock_account_input_categ',
-                      'property_stock_account_input_categ_id')
+
+    for prop in ['input', 'output']:
+        util.rename_field(cr, 'product.category',
+                        'property_stock_account_%s_categ' % prop,
+                        'property_stock_account_%s_categ_id' % prop)
 
     util.create_column(cr, 'stock_inventory', 'accounting_date', 'timestamp without time zone')
     cr.execute("""
