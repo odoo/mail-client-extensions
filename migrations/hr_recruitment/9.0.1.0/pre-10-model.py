a6ca1430c075789eb9aee7b7bfac4e1adb96dae6
Christophe Simonis <chs@odoo.com>
2015-11-27 18:06:56 +0100

    [FIX] hr_recruitment@9.0: keep stages

--- migrations/hr_recruitment/9.0.1.0/pre-10-model.py
+++ migrations/hr_recruitment/9.0.1.0/pre-10-model.py
@@ -3,3 +3,7 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.rename_model(cr, 'hr.applicant_category', 'hr.applicant.category', rename_table=False)
+
+    # keep stages in case they are actually used
+    for x in range(1, 7):
+        util.force_noupdate(cr, 'hr_recruitment.stage_job%d' % x, True)
