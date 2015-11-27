6456323844fee2d2933c1c147966522b2550f5a0
Christophe Simonis <chs@odoo.com>
2015-11-27 17:49:45 +0100

    [FIX] base@9.0: force update of groups and rules

--- migrations/base/9.0.1.3/pre-40-misc.py
+++ migrations/base/9.0.1.3/pre-40-misc.py
@@ -3,3 +3,9 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.remove_view(cr, 'base.user_groups_view', silent=True)
+
+    for fu in "group_erp_manager group_user group_multi_company".split():
+        util.force_noupdate(cr, 'base.' + fu, False)
+
+    util.remove_record(cr, 'base.multi_company_default_rule')
+    util.remove_record(cr, 'base.res_currency_rule')
