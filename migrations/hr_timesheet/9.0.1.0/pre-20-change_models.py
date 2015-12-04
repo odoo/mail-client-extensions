0cb58e917f207f4965a736603e4f2a285222f496
Christophe Simonis <chs@odoo.com>
2015-12-04 16:42:37 +0100

    [FIX] hr_timesheet@9.0: replace references of analytic timesheet

--- migrations/hr_timesheet/9.0.1.0/pre-20-change_models.py
+++ migrations/hr_timesheet/9.0.1.0/pre-20-change_models.py
@@ -10,5 +10,29 @@ def migrate(cr, version):
            SET is_timesheet = EXISTS(SELECT 1 FROM hr_analytic_timesheet WHERE line_id=l.id)
     """)
 
+    cr.execute("SELECT id, line_id FROM hr_analytic_timesheet")
+    for tid, lid in cr.fetchall():
+        util.replace_record_references(cr,
+                                       ('hr.analytic.timesheet', tid),
+                                       ('account.analytic.line', lid))
+
+    for model, res_model, res_id in util.res_model_res_id(cr):
+        if res_id and model != 'ir.values':
+            continue
+        cr.execute("UPDATE {0} SET {1}='account.analytic.line' WHERE {1}='hr.analytic.timesheet'"
+                   .format(model, res_model))
+
+    # only on real many2one that may be changes
+    if util.column_exists(cr, 'base_action_rule', 'model_id'):
+        cr.execute("""
+            SELECT id
+              FROM ir_model
+             WHERE model IN ('account.analytic.line', 'hr.analytic.timesheet')
+          ORDER BY 1
+        """)
+        aal, hat = [x[0] for x in cr.fetchall()]
+        cr.execute("UPDATE base_action_rule SET model_id=%s WHERE model_id=%s", [aal, hat])
+
+    util.delete_model(cr, 'hr.analytic.timesheet')
     util.delete_model(cr, 'hr.sign.out.project')
     util.delete_model(cr, 'hr.sign.in.project')
