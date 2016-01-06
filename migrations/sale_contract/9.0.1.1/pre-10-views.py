841404a038c5536e84f69ff2813d1cff7a36b9ff
Julien Legros <jle@odoo.com>
2016-01-06 11:31:15 +0100

    [FIX] sale_contract@9.0: remove inherited view

--- migrations/sale_contract/9.0.1.1/pre-10-views.py
+++ migrations/sale_contract/9.0.1.1/pre-10-views.py
@@ -3,3 +3,4 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.remove_view(cr, 'sale_contract.account_analytic_account_form_form')
+    util.remove_view(cr, 'sale_contract.account_analytic_analysis_form_form')
