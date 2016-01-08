02e5a775b59cf7b7a15e6aebe08635aedeae97d8
Christophe Simonis <chs@odoo.com>
2016-01-08 16:37:46 +0100

    [ADD] hr_recruitment@saas-9

---
+++ migrations/hr_recruitment/9.saas~9.1.0/pre-10-stages.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.create_column(cr, 'hr_recruitment_stage', 'job_id', 'int4')
+    util.create_column(cr, 'hr_recruitment_stage', '_tmp', 'int4')
+    cols, s_cols = map(','.join, util.get_columns(cr, 'hr_recruitment_stage', ('id', 'job_id', '_tmp'), ['s']))
+
+    cr.execute("""
+        WITH only_one_job AS (
+            SELECT stage_id, (array_agg(job_id))[1] as job_id
+              FROM job_stage_rel
+          GROUP BY 1
+            HAVING count(job_id) = 1
+        ),
+        _ AS (
+            DELETE FROM job_stage_rel r
+                  USING only_one_job o
+                  WHERE r.job_id=o.job_id
+                    AND r.stage_id=o.stage_id
+        )
+        UPDATE hr_recruitment_stage s
+           SET job_id = o.job_id
+          FROM only_one_job o
+         WHERE s.id = o.stage_id
+    """)
+
+    cr.execute("""
+
+    WITH stage_jobs AS (
+        SELECT stage_id, array_agg(job_id order by job_id) as jobs
+          FROM job_stage_rel
+      GROUP BY 1
+        HAVING array_agg(job_id order by job_id) != (SELECT array_agg(id order by id) FROM hr_job)
+    ),
+    _upd AS (
+      UPDATE hr_recruitment_stage s
+         SET job_id = t.jobs[1]
+        FROM stage_jobs t
+       WHERE t.stage_id = s.id
+    ),
+    new_stages AS (
+        INSERT INTO hr_recruitment_stage({cols}, job_id, _tmp)
+             SELECT {s_cols}, unnest(t.jobs[2:array_length(t.jobs, 1)]), s.id
+               FROM hr_recruitment_stage s
+               JOIN stage_jobs t ON (s.id = t.stage_id)
+          RETURNING id, job_id, _tmp
+    ),
+
+    -- reassign applicants to correct stage depending on job
+    -- users may seen duplicated stage in kanban view depending on applicant job
+    _upd_applicant_stage AS (
+        UPDATE hr_applicant l
+           SET stage_id = n.id
+          FROM new_stages n
+         WHERE l.stage_id = n._tmp
+           AND l.job_id = n.job_id
+    )
+    UPDATE hr_applicant l
+       SET last_stage_id = n.id
+      FROM new_stages n
+     WHERE l.last_stage_id = n._tmp
+       AND l.job_id = n.job_id
+
+    """.format(**locals()))
+
+    util.remove_column(cr, 'hr_recruitment_stage', '_tmp')
