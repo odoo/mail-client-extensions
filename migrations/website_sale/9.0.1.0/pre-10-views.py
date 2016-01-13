db0792910a2a656b8dd833633b280658a586583f
Christophe Simonis <chs@odoo.com>
2016-01-13 13:36:53 +0100

    [FIX] website_sale@9.0: force update of another view

--- migrations/website_sale/9.0.1.0/pre-10-views.py
+++ migrations/website_sale/9.0.1.0/pre-10-views.py
@@ -2,6 +2,6 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    views = "cart total product_item product extra_info reduction_code checkout payment".split()
+    views = "cart total product_item product suggested_products_list extra_info reduction_code checkout payment".split()
     for v in views:
         util.force_noupdate(cr, 'website_sale.' + v, False)
