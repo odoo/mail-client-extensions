9e8634903f4cd621a1ff9cd434032cee30a61748
Cedric Snauwaert <csn@openerp.com>
2016-01-15 10:13:44 +0100

    [FIX] accounting@9.0: migrate account_invoice_tax

---
+++ migrations/account/9.0.1.1/post-91-recompute-invoice-tax.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    """ Migration of account_invoice_tax object, easiest way is to call the _onchange_invoice_line_ids
+        on account_invoice as it will correctly recreate the account_invoice_tax with the correct values
+
+        Note: this will also call the recompute of some field on account_invoice (amount_untaxed, amount_untaxed_signed,
+            amount_tax, amount_total, amount_total_signed, amount_total_company_signed).
+
+            If this file is to slow to migrate, it is possible to speed it up by just recomputing the account_invoice_tax 
+            for all invoices that are in draft,proforma2 format. The other ones already have their account move and recomputing
+            account_invoice_tax for those does not have any effect (except if user cancel invoice and then reset to draft)
+    """ 
+
+    env = util.env(cr)
+    invoices = env['account.invoice'].search([])
+    for invoice in invoices:
+        invoice._onchange_invoice_line_ids()
