c5345ca958ac0628f5c097d9ba20f9a98c7d844f
Cedric Snauwaert <csn@openerp.com>
2016-01-21 16:48:36 +0100

    [FIX] sale_contract@9.0: remove some views that are not present anymore

--- migrations/sale_contract/9.0.1.1/pre-10-views.py
+++ migrations/sale_contract/9.0.1.1/pre-10-views.py
@@ -4,3 +4,5 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.remove_view(cr, 'sale_contract.account_analytic_account_form_form')
     util.remove_view(cr, 'sale_contract.account_analytic_analysis_form_form')
+    util.remove_view(cr, 'sale_contract.account_analytic_account_view_form_inherit')
+    util.remove_view(cr, 'sale_contract.view_account_analytic_account_template_required')
