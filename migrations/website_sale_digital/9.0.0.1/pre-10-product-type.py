e96edc3a95647b2e7d69d3be55c368dfcd08cd16
Julien Legros <jle@odoo.com>
2016-01-27 14:06:51 +0100

    [FIX] website_sale_digital@9.0: remove view

--- migrations/website_sale_digital/9.0.0.1/pre-10-product-type.py
+++ migrations/website_sale_digital/9.0.0.1/pre-10-product-type.py
@@ -4,3 +4,4 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     cr.execute("UPDATE product_template SET type='digital' WHERE digital_content=true")
     util.remove_field(cr, 'product.template', 'digital_content')
+    util.remove_view(cr, 'website_sale_digital.view_product_form_expiry')
