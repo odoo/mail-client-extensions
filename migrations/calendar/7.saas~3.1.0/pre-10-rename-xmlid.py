8bbe3986bfbc033bfe4f9884cee79c8697b46558
hip-odoo <hip@tinyerp.com>
2016-01-28 12:04:28 +0530

    [FIX] calendar@saas-3 : rename xmlid

--- migrations/calendar/7.saas~3.1.0/pre-10-rename-xmlid.py
+++ migrations/calendar/7.saas~3.1.0/pre-10-rename-xmlid.py
@@ -3,3 +3,4 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.rename_xmlid(cr, 'calendar.view_crm_meeting_form', 'calendar.view_calendar_event_form')
+    util.rename_xmlid(cr, 'calendar.view_crm_meeting_search', 'calendar.view_calendar_event_search')
