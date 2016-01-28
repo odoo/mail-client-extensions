cafd6755cdf226a7cd0d2b1a7dfe713900e0cadf
Christophe Simonis <chs@odoo.com>
2016-01-28 14:19:16 +0100

    [IMP] analytic@9.0: remove some report views

--- migrations/analytic/9.0.1.1/pre-20-views.py
+++ migrations/analytic/9.0.1.1/pre-20-views.py
@@ -4,3 +4,8 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.remove_view(cr, "analytic.account_analytic_line_extended_form")
+    util.remove_view(cr, "analytic.report_invertedanalyticbalance")
+    util.remove_view(cr, "analytic.report_analyticcostledger")
+    util.remove_view(cr, "analytic.report_analyticcostledgerquantity")
+    util.remove_view(cr, "analytic.report_analyticjournal")
+    util.remove_view(cr, "analytic.report_analyticbalance")
