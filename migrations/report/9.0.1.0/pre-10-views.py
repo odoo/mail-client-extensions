7e16acdcb8e827f198ee7c360ea904e5e635852e
Christophe Simonis <chs@odoo.com>
2016-02-01 19:22:28 +0100

    [ADD] report@9.0: update layouts

---
+++ migrations/report/9.0.1.0/pre-10-views.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from lxml import etree
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.force_noupdate(cr, 'report.layout', False)
+    for x in 'external internal minimal'.split():
+        util.force_noupdate(cr, 'report.%s_layout' % x, False)
+
+    # XXX should we just force update?
+    with util.skippable_cm(), util.edit_view(cr, 'report.external_layout_header') as arch:
+        node = arch.find('.//div[@t-field="company.partner_id"]/..')
+        if node:
+            node.attrib['name'] = 'company_address'
+        else:
+            node = arch.find('./div')
+            etree.SubElement(node, 'div', name='company_address')
+
+    with util.skippable_cm(), util.edit_view(cr, 'report.external_layout_footer') as arch:
+        new_node = etree.fromsting("""
+            <ul t-if="not company.custom_footer" class="list-inline" name="financial_infos">
+                <li t-if="company.vat">TIN: <span t-field="company.vat"/></li>
+            </ul>
+        """)
+        node = arch.find('.//ul')
+        if node:
+            node.addnext(new_node)
+        else:
+            node = arch.find('./div')
+            node.append(new_node)
