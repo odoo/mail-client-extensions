893997999fc65187756de732155ff81d8dc5b3fb
hip-odoo <hip@tinyerp.com>
2016-02-04 18:48:43 +0530

    [FIX] account_voucher@9.0: remove deprecated view

--- migrations/account_voucher/9.0.1.0/pre-20-views.py
+++ migrations/account_voucher/9.0.1.0/pre-20-views.py
@@ -2,7 +2,14 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    view_list = ["view_vendor_receipt_form", "view_vendor_payment_form", "view_voucher_filter_vendor_pay",
-                 "view_voucher_filter_customer_pay", "view_voucher_form"]
+    view_list = util.splitlines("""
+        view_vendor_receipt_form
+        view_vendor_payment_form
+        view_voucher_filter_vendor_pay
+        view_voucher_filter_customer_pay
+        view_voucher_form
+        view_invoice_customer
+        view_invoice_supplier
+    """)
     for view in view_list:
         util.remove_view(cr, "account_voucher." + view)
