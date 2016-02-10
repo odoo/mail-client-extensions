fa90d6a9c1b3802c3a69bf5280869c2fb34067c8
hip-odoo <hip@tinyerp.com>
2016-02-10 11:44:12 +0530

    [FIX] sale_contract@9.0: remove deprecated view

--- migrations/sale_contract/9.0.1.1/pre-10-views.py
+++ migrations/sale_contract/9.0.1.1/pre-10-views.py
@@ -2,7 +2,13 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.remove_view(cr, 'sale_contract.account_analytic_account_form_form')
-    util.remove_view(cr, 'sale_contract.account_analytic_analysis_form_form')
-    util.remove_view(cr, 'sale_contract.account_analytic_account_view_form_inherit')
-    util.remove_view(cr, 'sale_contract.view_account_analytic_account_template_required')
+    view_list = util.splitlines("""
+        account_analytic_account_form_form
+        account_analytic_analysis_form_form
+        account_analytic_account_view_form_inherit
+        view_account_analytic_account_template_required
+        view_account_analytic_account_overdue_search
+        view_account_analytic_account_tree_c2c_3
+    """)
+    for view in view_list:
+        util.remove_view(cr, "sale_contract." + view)
