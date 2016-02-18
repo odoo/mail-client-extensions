a76153c2542685dac0380a4d796855d9691fdf47
Christophe Simonis <chs@odoo.com>
2016-02-18 14:54:00 +0100

    [FIX] base@saas~8: non-computed non-related custom fields should be stored

--- migrations/base/9.saas~8.1.3/pre-10-models.py
+++ migrations/base/9.saas~8.1.3/pre-10-models.py
@@ -3,6 +3,11 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.create_column(cr, 'ir_model_fields', 'store', 'boolean')
+    cr.execute("""
+        UPDATE ir_model_fields
+           SET store=(compute IS NULL AND related IS NULL)
+         WHERE state='manual'
+    """)
 
     util.force_noupdate(cr, 'base.view_model_form', False)
     util.force_noupdate(cr, 'base.view_model_fields_form', False)
