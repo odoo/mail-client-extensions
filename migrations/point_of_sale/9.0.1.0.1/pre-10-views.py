3d8400c4559e6c2c34de1cd8c271421e8bd7ff6a
hip-odoo <hip@tinyerp.com>
2016-02-23 17:37:38 +0530

    [FIX] point_of_sale@9.0: use remove_view function

--- migrations/point_of_sale/9.0.1.0.1/pre-10-views.py
+++ migrations/point_of_sale/9.0.1.0.1/pre-10-views.py
@@ -4,4 +4,4 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.force_noupdate(cr, 'point_of_sale.index', False)
     cr.execute("DROP TABLE IF EXISTS pos_config_settings")
-    util.drop_view(cr, 'point_of_sale.view_sale_config_settings_form_pos')
+    util.remove_view(cr, 'point_of_sale.view_sale_config_settings_form_pos')
