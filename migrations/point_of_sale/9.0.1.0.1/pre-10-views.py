f5e428aef1d2f1dfdf8c16bbaf4bee6fa5ed9fd5
hip-odoo <hip@tinyerp.com>
2016-02-26 16:01:20 +0530

    [FIX] point_of_sale@9.0: remove deprecated view

--- migrations/point_of_sale/9.0.1.0.1/pre-10-views.py
+++ migrations/point_of_sale/9.0.1.0.1/pre-10-views.py
@@ -4,4 +4,5 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.force_noupdate(cr, 'point_of_sale.index', False)
     cr.execute("DROP TABLE IF EXISTS pos_config_settings")
-    util.remove_view(cr, 'point_of_sale.view_sale_config_settings_form_pos')
+    for view in "view_sale_config_settings_form_pos product_template_form_view_inherit_ean product_normal_form_view_inherit_ean".split():
+        util.remove_view(cr, "point_of_sale." + view)
