ba8d1eee2b33396fec5648b090e8753ab65ab66c
Christophe Simonis <chs@odoo.com>
2016-03-10 16:14:57 +0100

    [FIX] sale_stock@9.0: more views to rename

--- migrations/sale_stock/9.0.1.0/pre-10-views.py
+++ migrations/sale_stock/9.0.1.0/pre-10-views.py
@@ -2,8 +2,13 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.rename_xmlid(cr, 'sale_stock.view_order_form_inherit',
-                          'sale_stock.view_order_form_inherit_sale_stock')   # noqa
+    renames = {
+        'view_order_form_inherit': 'view_order_form_inherit_sale_stock',
+        'view_invoice_form_inherit': 'invoice_form_inherit_sale_stock',
+        'report_invoice_incoterm': 'report_invoice_document_inherit_sale_stock',
+    }
+    for f, t in renames.items():
+        util.rename_xmlid(cr, 'sale_stock.' + f, 'sale_stock.' + t)
 
     todel = util.splitlines("""
         view_picking_internal_search_inherit
