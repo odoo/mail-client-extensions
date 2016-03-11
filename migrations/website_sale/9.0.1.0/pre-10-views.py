d699063f5eab3b8e299fb3d0e4409e0621b900ae
Christophe Simonis <chs@odoo.com>
2016-03-11 13:21:13 +0100

    [FIX] website_sale@9.0: remove old view

--- migrations/website_sale/9.0.1.0/pre-10-views.py
+++ migrations/website_sale/9.0.1.0/pre-10-views.py
@@ -5,3 +5,5 @@ def migrate(cr, version):
     views = "cart total product_item product suggested_products_list extra_info reduction_code checkout payment".split()
     for v in views:
         util.force_noupdate(cr, 'website_sale.' + v, False)
+
+    util.remove_view(cr, 'website_sale.variants_template_tree_view')
