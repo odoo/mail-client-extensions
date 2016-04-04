395bb83042164c966723473628ecca10dd0d73f2
Christophe Simonis <chs@odoo.com>
2016-04-04 17:24:06 +0200

    [FIX] l10n_be@9.0: typo

--- migrations/l10n_be/9.0.1.1/post-20-account-tags.py
+++ migrations/l10n_be/9.0.1.1/post-20-account-tags.py
@@ -7,7 +7,7 @@ def migrate(cr, version):
         ('[67]5%', util.ref(cr, 'account.account_tag_financing')),
         ('[67]6%', util.ref(cr, 'account.account_tag_investing')),
     ]
-    for code, tag in mapping_table.items():
+    for code, tag in mapping_table:
         cr.execute("""
             INSERT INTO account_account_account_tag
                  SELECT id, %s
