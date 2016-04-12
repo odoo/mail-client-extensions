5b49747a5f19e5c572192b878bee1f374aa3a544
Christophe Simonis <chs@odoo.com>
2016-04-12 20:11:43 +0200

    [ADD] base@saas~10: db-specific scripts

---
+++ migrations/base/9.saas~10.1.3/pre-00-db-specific.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def _db_openerp(cr, version):
+    util.force_noupdate(cr, 'openerp_website.odoo_blog_sidebar_blogs', False)
+    util.remove_view(cr, view_id=8816)      # custom inherited view now builtin
+
+
+def migrate(cr, version):
+    util.dispatch_by_dbuuid(cr, version, {
+        '05a64ced-5b98-488d-a833-a994f9b1dd80': _db_openerp,    # test
+        '8851207e-1ff9-11e0-a147-001cc0f2115e': _db_openerp,    # prod
+    })
