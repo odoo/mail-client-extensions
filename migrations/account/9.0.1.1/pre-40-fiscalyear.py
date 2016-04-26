f5943a2d9491ad418765b56820fc380284c3223f
Christophe Simonis <chs@odoo.com>
2016-04-26 13:43:38 +0200

    [FIX] account@9.0: correct fiscalyear_last_{day,month} and {fiscalyear,period}_lock_date computation

--- migrations/account/9.0.1.1/pre-40-fiscalyear.py
+++ migrations/account/9.0.1.1/pre-40-fiscalyear.py
@@ -12,26 +12,33 @@ def migrate(cr, version):
     util.create_column(cr, 'res_company', 'period_lock_date', 'date')
     util.create_column(cr, 'res_company', 'fiscalyear_lock_date', 'date')
 
-    cr.execute("""UPDATE res_company c 
-                    SET fiscalyear_last_day = fy.fy_last_day, 
-                        fiscalyear_last_month = fy.fy_last_month,
-                        fiscalyear_lock_date = fy.fy_lock_date,
-                        period_lock_date = fy.p_lock_date
-                    FROM (SELECT date_part('day', max(f.date_stop)) AS fy_last_day,
-                        f.company_id,
-                        date_part('month', max(f.date_stop)) AS fy_last_month,
-                        max(p.date_stop) AS p_lock_date,
-                        max(f.date_stop) AS fy_lock_date
-                        FROM account_fiscalyear f 
-                        LEFT JOIN account_period p ON p.state = 'done' 
-                        WHERE f.state = 'done' GROUP BY f.company_id) fy
-                    WHERE fy.company_id = c.id
-                """)
-    #It is possible that some company did not have any close FY meaning that recquires field 
-    # fiscalyear_last_day/month has not been setted by previous request and need to be set using
-    # default value
+    cr.execute("""
+        UPDATE res_company c
+           SET fiscalyear_last_day = fy.fy_last_day,
+               fiscalyear_last_month = fy.fy_last_month
+          FROM (SELECT company_id,
+                       date_part('day', max(date_stop)) AS fy_last_day,
+                       date_part('month', max(date_stop)) AS fy_last_month
+                  FROM account_fiscalyear
+              GROUP BY company_id) fy
+         WHERE fy.company_id = c.id
+    """)
+    # default values for companies without any fiscalyear set
     cr.execute("""UPDATE res_company
                     SET fiscalyear_last_day = 31,
                         fiscalyear_last_month = 12
                     WHERE fiscalyear_last_day IS NULL AND fiscalyear_last_month IS NULL
                 """)
+
+    # lock_dates are max(date_stop of done fy/p) OR min(date_start-1day of draft fy/p)
+    for tbl in ('fiscalyear', 'period'):
+        cr.execute("""
+          UPDATE res_company c
+             SET {0}_lock_date = coalesce(s.max, s.min)
+            FROM (SELECT company_id,
+                         max(CASE WHEN state='done' THEN date_stop ELSE NULL END),
+                         min(CASE WHEN state='draft' THEN date_start - interval '1 day' ELSE NULL END)
+                    FROM account_{0}
+                GROUP BY company_id) s
+           WHERE s.company_id = c.id
+        """.format(tbl))
