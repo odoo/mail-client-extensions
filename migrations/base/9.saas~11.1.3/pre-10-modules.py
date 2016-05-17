69c4f8e731763e351e395bef9954a6d427330bed
Christophe Simonis <chs@odoo.com>
2016-05-17 16:16:42 +0200

    [ADD] base@saas~11: new modules, module deps changes, module merges...

---
+++ migrations/base/9.saas~11.1.3/pre-10-modules.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+
+    if util.has_enterprise():
+        util.new_module(cr, 'account_bank_statement_import_camt',
+                        deps=('account_bank_statement_import',))
+        util.new_module(cr, 'account_bank_statement_import_csv',
+                        deps=('account_bank_statement_import', 'base_import'))
+
+        util.new_module(cr, 'web_enterprise', deps=('web',), auto_install=True)
+
+        util.new_module_dep(cr, 'crm_voip', 'web_enterprise')
+
+        util.new_module(cr, 'grid', deps=('web',))
+        util.new_module(cr, 'timesheet_grid', deps=('grid', 'hr_timesheet'))
+        util.new_module(cr, 'hr_timesheet_sheet_timesheet_grid',
+                        deps=('hr_timesheet_sheet', 'timesheet_grid'), auto_install=True)
+        util.new_module(cr, 'timesheet_grid_sale',
+                        deps=('timesheet_grid', 'sale_timesheet'), auto_install=True)
+
+    util.new_module_dep(cr, 'base_import', 'web_kanban')
+    util.new_module_dep(cr, 'hr_holidays', 'product')
+
+    util.new_module(cr, 'hr_timesheet_attendance',
+                    deps=('hr_timesheet', 'hr_attendance'),
+                    auto_install=True)
+
+    util.new_module(cr, 'payment_payumoney', deps=('payment',))
+    util.new_module(cr, 'payment_stripe', deps=('payment',))
+
+    util.rename_model(cr, 'sale_layout.category', 'sale.layout_category', rename_table=False)
+    util.ENVIRON['sale_layout_installed'] = installed = util.module_installed(cr, 'sale_layout')
+    if installed:
+        for model in 'sale.order.line account.invoice.line'.split():
+            util.rename_field(cr, model, 'sale_layout_cat_id', 'layout_category_id')
+            util.rename_field(cr, model, 'categ_sequence', 'layout_category_sequence')
+    util.merge_module(cr, 'sale_layout', 'sale')
+
+    util.merge_module(cr, 'sale_service', 'sale_timesheet')
+
+    # reassign fields to related modules
+    warn_fields = {
+        'sale': ('sale', 'res.partner'),
+        'purchase': ('purchase', 'res.partner'),
+        'picking': ('stock', 'res.partner'),
+        'invoice': ('account', 'res.partner'),
+        'sale_line': ('sale', 'product.template'),
+        'purchase_line': ('purchase', 'product.template'),
+    }
+    for prefix, (module, model) in warn_fields.items():
+        util.move_field_to_module(cr, model, prefix + '_warn', 'warning', module)
+        util.move_field_to_module(cr, model, prefix + '_warn_msg', 'warning', module)
+
+    util.ENVIRON['warning_installed'] = util.module_installed(cr, 'warning')
+    util.remove_module(cr, 'warning')
