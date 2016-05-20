40ffc401ca0bada56e71b0a6153ce0d23536a8d1
Christophe Simonis <chs@odoo.com>
2016-05-20 10:43:30 +0200

    [ADD] product@saas~11

---
+++ migrations/product/9.saas~11.1.2/pre-migrate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.rename_xmlid(cr, 'product.product_template_action_product',
+                      'product.product_template_action_all')
+
+    # column change type: remove it to let the ORM recreate it correctly
+    cr.execute("ALTER TABLE base_config_settings DROP COLUMN group_product_variant")
