9402a28c2abd519488986353872e6d0f223a909e
Christophe Simonis <chs@odoo.com>
2016-05-25 17:46:03 +0200

    [IMP] account@9.0: speed up tax search

--- migrations/account/9.0.1.1/post-35-tax.py
+++ migrations/account/9.0.1.1/post-35-tax.py
@@ -1,101 +1,78 @@
 # -*- coding: utf-8 -*-
 from openerp.addons.base.maintenance.migrations import util
 
-def findline(cr, move, tax_amount):
-    cr.execute("""SELECT id, tax_line_id, tax_code_id
-                    FROM account_move_line
-                    WHERE move_id = %s AND tax_amount = %s
-                """, (move, tax_amount))
-    return cr.dictfetchone()
+def findtax(cr, code1, code2, base, cache):
+    codes = (code1, code2)
+    key = (frozenset(codes), base)
+    if key in cache:
+        return cache[key]
 
-def findtax(cr, code1, code2, base=True):
-    code = [code1, code2]
-    if base:
-        cr.execute("""SELECT parent_id 
-                        FROM (SELECT parent_id, count(*) 
-                            FROM account_tax 
-                            WHERE parent_id IS NOT NULL
-                                AND base_code_id in %s GROUP BY parent_id) AS e
-                        WHERE e.count > 1
-                    """, (tuple(code),))
-    else:
-        cr.execute("""SELECT parent_id 
-                        FROM (SELECT parent_id, count(*) 
-                            FROM account_tax 
-                            WHERE parent_id IS NOT NULL
-                                AND tax_code_id in %s GROUP BY parent_id) AS e
-                        WHERE e.count > 1
-                    """, (tuple(code),))
-    tax = cr.dictfetchone()
-    if tax:
-        return tax['parent_id']
-    # Try finding for a refund instead
-    if base:
-        cr.execute("""SELECT parent_id 
-                        FROM (SELECT parent_id, count(*) 
-                            FROM account_tax 
-                            WHERE parent_id IS NOT NULL
-                                AND ref_base_code_id in %s GROUP BY parent_id) AS e
-                        WHERE e.count > 1
-                    """, (tuple(code),))
-    else:
-        cr.execute("""SELECT parent_id 
-                        FROM (SELECT parent_id, count(*) 
-                            FROM account_tax 
-                            WHERE parent_id IS NOT NULL
-                                AND ref_tax_code_id in %s GROUP BY parent_id) AS e
-                        WHERE e.count > 1
-                    """, (tuple(code),))
-    tax = cr.dictfetchone()
-    if tax:
-        return tax['parent_id']
-    return False
-
+    col = 'base_code_id' if base else 'tax_code_id'
+    for prefix in ['', 'ref_']:
+        cr.execute("""
+            SELECT parent_id
+              FROM account_tax
+             WHERE parent_id IS NOT NULL
+               AND {0} IN %s
+          GROUP BY parent_id
+            HAVING count(*) > 1
+        """.format(prefix + col), [codes])
+        tax = cr.fetchone()
+        if tax:
+            cache[key] = tax[0]
+            return tax[0]
+    cache[key] = None
+    return None
 
 def migrate(cr, version):
     env = util.env(cr)
 
     # Some tax assignation on account_move_line have been wrongly computed, the way to fix those is the following:
     # if one of the line (line0) in an account_move has a debit=credit=0, take the tax_amount of that line, find the other
-    # line in that account_move with the same tax_amount, if that line has a tax_line_id: the tax referenced by that should 
+    # line in that account_move with the same tax_amount, if that line has a tax_line_id: the tax referenced by that should
     # be a tax with 2 children, one with base/tax_code_id = line.tax_code_id and the other with base/tax_code_id = line0.tax_code_id
-    cr.execute("""SELECT move_id, tax_amount, tax_code_id, tax_line_id  
-                    FROM account_move_line 
-                    WHERE debit = 0 
-                        AND credit = 0 
-                        AND tax_code_id IS NOT NULL 
-                        AND tax_amount IS NOT NULL""")
 
     tax_line_ids_to_update = []
     tax_line_id_to_update = []
-    for move in cr.dictfetchall():
-        line = findline(cr, move['move_id'], move['tax_amount'])
-        if not line:
-            continue
-        #Update tax_line_id
-        if line['tax_line_id']:
-            tax = findtax(cr, line['tax_code_id'], move['tax_code_id'], base=False)
+    tax_cache = {}
+    cr.execute("""
+        WITH lines0 AS (
+            SELECT id, move_id, tax_code_id, tax_amount
+              FROM account_move_line
+             WHERE credit = 0
+               AND debit = 0
+               AND tax_amount IS NOT NULL
+               AND tax_code_id IS NOT NULL
+        )
+        SELECT l1.id, l0.tax_code_id, l1.tax_code_id, l1.tax_line_id
+          FROM account_move_line l1
+          JOIN lines0 l0 ON (l1.id != l0.id AND l0.move_id=l1.move_id)
+         WHERE l0.tax_amount = l1.tax_amount
+           AND l1.tax_code_id IS NOT NULL
+    """)
+    for line_id, tax_code1, tax_code2, tax_line_id in cr.fetchall():
+        if tax_line_id:
+            # Update tax_line_id
+            tax = findtax(cr, tax_code1, tax_code2, base=False, cache=tax_cache)
             if tax:
-                tax_line_id_to_update.append((line['id'], tax))
-        #Update tax_line_ids
+                tax_line_id_to_update.append((line_id, tax))
         else:
-            tax = findtax(cr, line['tax_code_id'], move['tax_code_id'], base=True)
+            # Update tax_line_ids
+            tax = findtax(cr, tax_code1, tax_code2, base=True, cache=tax_cache)
             if tax:
-                tax_line_ids_to_update.append((line['id'], tax))
+                tax_line_ids_to_update.append((line_id, tax))
 
     for tuple_update in tax_line_ids_to_update:
         cr.execute("""UPDATE account_move_line_account_tax_rel
                         SET account_tax_id = %s
                         WHERE account_move_line_id = %s
                     """, (tuple_update[1], tuple_update[0]))
-    
+
     for tuple_update in tax_line_id_to_update:
         cr.execute("""UPDATE account_move_line
                         SET tax_line_id = %s
                         WHERE id = %s
                     """, (tuple_update[1], tuple_update[0]))
-
-
 
     # Finish migration on tax: if only one child, deactivate child and put value on parent
 
@@ -111,7 +88,7 @@ def migrate(cr, version):
 
     cr.execute("""UPDATE account_tax
         SET active = false
-        WHERE amount = 0 AND amount_type = 'percent' AND id in 
+        WHERE amount = 0 AND amount_type = 'percent' AND id in
             (SELECT child_tax from account_tax_filiation_rel where parent_tax IS NOT NULL)
         """)
 
@@ -133,7 +110,7 @@ def migrate(cr, version):
                         SET account_id = t2.account_id,
                             refund_account_id = t2.refund_account_id
                         FROM account_tax t2
-                        WHERE t2.parent_id = t.id 
+                        WHERE t2.parent_id = t.id
                             AND t.id IN %s
                     """, (tuple(ids_to_get_account),))
 
@@ -142,17 +119,17 @@ def migrate(cr, version):
     cr.execute("""UPDATE account_move_line aml
                     SET tax_line_id = tax.parent_id
                     FROM account_tax tax
-                    WHERE tax.id = aml.tax_line_id 
-                        AND tax.active = false 
-                        AND tax.amount_type = 'percent' 
+                    WHERE tax.id = aml.tax_line_id
+                        AND tax.active = false
+                        AND tax.amount_type = 'percent'
                         AND tax.parent_id IS NOT NULL
                 """)
 
     cr.execute("""UPDATE account_move_line_account_tax_rel a
                     SET account_tax_id = tax.parent_id
                     FROM account_tax tax
-                    WHERE tax.id = a.account_tax_id 
-                        AND tax.active = false 
-                        AND tax.amount_type = 'percent' 
+                    WHERE tax.id = a.account_tax_id
+                        AND tax.active = false
+                        AND tax.amount_type = 'percent'
                         AND tax.parent_id IS NOT NULL
                 """)
