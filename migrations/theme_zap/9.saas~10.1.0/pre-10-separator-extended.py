1a6d1a6dc4ff20a1f6731f6df69642e7f38837a1
Christophe Simonis <chs@odoo.com>
2016-06-09 17:06:03 +0200

    [ADD] theme_{nano,treehouse,zap}@saas~10: some wicked views have been disinherited...

---
+++ migrations/theme_zap/9.saas~10.1.0/pre-10-separator-extended.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    view = util.ref(cr, 'theme_zap.s_separator_extended')
+    if view:
+        cr.execute("UPDATE ir_ui_view SET inherit_id=NULL, mode='primary' WHERE id=%s", [view])
