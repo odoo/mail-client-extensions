bec953165419ffeb166e6fa89e79b12525d37443
Christophe Simonis <chs@odoo.com>
2016-06-15 17:27:41 +0200

    [FIX] stock@saas~10: correct group rename and settings default value

--- migrations/stock/9.saas~10.1.0/post-10-groups.py
+++ migrations/stock/9.saas~10.1.0/post-10-groups.py
@@ -2,12 +2,6 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    group_multi_warehouse = util.ref(cr, 'stock.group_stock_multi_warehouses')
-    group_multi_locations = util.ref(cr, 'stock.group_stock_multi_locations')
-
-    cr.execute("""
-        INSERT INTO res_groups_users_rel(gid, uid)
-             SELECT %s, uid
-               FROM res_groups_users_rel
-              WHERE gid = %s
-    """, [group_multi_warehouse, group_multi_locations])
+    config = util.env(cr)['stock.config.settings'].create({})
+    config.group_stock_multi_warehouses = config.group_stock_multi_locations
+    config.execute()
--- migrations/stock/9.saas~10.1.0/pre-10-groups.py
+++ migrations/stock/9.saas~10.1.0/pre-10-groups.py
@@ -2,4 +2,4 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.rename_xmlid(cr, 'stock.group_locations', 'stock.group_multi_locations')
+    util.rename_xmlid(cr, 'stock.group_locations', 'stock.group_stock_multi_locations')
