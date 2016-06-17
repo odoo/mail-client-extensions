29b63dbc14f280e851a9ea5546ea64c5834b1728
Christophe Simonis <chs@odoo.com>
2016-06-17 16:37:15 +0200

    [FIX] website@saas~11: search <head> in main layout

--- migrations/website/9.saas~11.1.0/pre-10-favicon.py
+++ migrations/website/9.saas~11.1.0/pre-10-favicon.py
@@ -3,7 +3,7 @@ from lxml import etree
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    with util.skippable_cm(), util.edit_view(cr, 'website.footer_custom') as arch:
+    with util.skippable_cm(), util.edit_view(cr, 'website.layout') as arch:
         favicon = etree.fromstring('''
             <link rel="shortcut icon" type="image/x-icon" t-attf-href="/web/image/website/{{website.id}}/favicon/"/>
         ''')
