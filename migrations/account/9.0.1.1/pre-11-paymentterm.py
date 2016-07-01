378607dd29574b69e25ab20b3979673b58c08971
Christophe Simonis <chs@odoo.com>
2016-07-01 10:46:55 +0200

    [FIX] account@9.0: correct payment term migration

--- migrations/account/9.0.1.1/pre-11-paymentterm.py
+++ migrations/account/9.0.1.1/pre-11-paymentterm.py
@@ -2,27 +2,15 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    """
-        account.payment.term.line: value field, rename procent selection into percent + 
-            day2 == -1  => set option to 'last_day_current_month'
-            day2 == 0 => do nothing
-            day2 >0 => set option to 'fix_day_following_month'
-    """
-
-    cr.execute("""UPDATE account_payment_term_line
-                   SET value = 'percent' WHERE value = 'procent'
-               """)
+    cr.execute("UPDATE account_payment_term_line SET value = 'percent' WHERE value = 'procent'")
 
     util.create_column(cr, 'account_payment_term_line', 'option', 'varchar')
-
-    cr.execute("""UPDATE account_payment_term_line
-                    SET option = 'last_day_current_month' WHERE days2 = -1
-            """)
-
-    cr.execute("""UPDATE account_payment_term_line
-                    SET option = 'fix_day_following_month' WHERE days2 > 0
-            """)
-
-    cr.execute("""UPDATE account_payment_term_line
-                    SET option = 'day_after_invoice_date' WHERE days2 = 0
-            """)
+    cr.execute("""
+        UPDATE account_payment_term_line
+           SET days = CASE WHEN days2 > 0 THEN COALESCE(days, 0) + days2
+                           ELSE days
+                       END,
+               option = CASE WHEN COALESCE(days2, 0) = 0 THEN 'day_after_invoice_date'
+                             ELSE 'fix_day_following_month'
+                         END
+    """)
