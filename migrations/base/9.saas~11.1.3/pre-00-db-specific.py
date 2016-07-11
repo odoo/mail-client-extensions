bae4518ee402a0105abec4f3463d38e0c0cc1ee1
Christophe Simonis <chs@odoo.com>
2016-07-11 12:43:12 +0200

    [ADD] base@saas~11: db-specific script for prod

---
+++ migrations/base/9.saas~11.1.3/pre-00-db-specific.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def _db_openerp(cr, version):
+    # bootstrap warn fields on partners
+    cr.execute("""
+        ALTER TABLE res_partner
+         ADD COLUMN invoice_warn varchar,
+         ADD COLUMN invoice_warn_msg text,
+         ADD COLUMN sale_warn varchar,
+         ADD COLUMN sale_warn_msg text,
+         ADD COLUMN picking_warn varchar,
+         ADD COLUMN picking_warn_msg text,
+         ADD COLUMN purchase_warn varchar,
+         ADD COLUMN purchase_warn_msg text
+    """)
+    cr.execute("""
+        UPDATE res_partner
+           SET invoice_warn = 'no-message',
+               sale_warn = 'no-message',
+               picking_warn = 'no-message',
+               purchase_warn = 'no-message'
+    """)
+    cr.execute("""
+        ALTER TABLE res_partner
+       ALTER COLUMN invoice_warn SET NOT NULL,
+       ALTER COLUMN sale_warn SET NOT NULL,
+       ALTER COLUMN picking_warn SET NOT NULL,
+       ALTER COLUMN purchase_warn SET NOT NULL
+    """)
+
+def migrate(cr, version):
+    util.dispatch_by_dbuuid(cr, version, {
+        '05a64ced-5b98-488d-a833-a994f9b1dd80': _db_openerp,    # test
+        '8851207e-1ff9-11e0-a147-001cc0f2115e': _db_openerp,    # prod
+    })
