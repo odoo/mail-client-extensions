5242d90bf252870f1629e9b8ed896a7d12b09b45
Christophe Simonis <chs@odoo.com>
2016-08-05 16:03:57 +0200

    [ADD] base@saas~10: boldom db-specific script

--- migrations/base/9.saas~10.1.3/pre-00-db-specific.py
+++ migrations/base/9.saas~10.1.3/pre-00-db-specific.py
@@ -12,10 +12,15 @@ def _db_natural_fresh(cr, version):
     for vid, in cr.fetchall():
         util.remove_view(cr, view_id=vid)
 
+def _boldom(cr, version):
+    cr.execute("DELETE FROM ir_server_object_lines WHERE id=403")
+    cr.execute("UPDATE ir_ui_view SET arch_db=%s WHERE id=837",
+               ["""<xpath expr="//field[@name='fax']/.."><group/></xpath>"""])
 
 def migrate(cr, version):
     util.dispatch_by_dbuuid(cr, version, {
         '05a64ced-5b98-488d-a833-a994f9b1dd80': _db_openerp,    # test
         '8851207e-1ff9-11e0-a147-001cc0f2115e': _db_openerp,    # prod
         '676796d7-881a-42c1-b5c8-ecae4452ec23': _db_natural_fresh,
+        '39b15f26-b737-4420-94bb-156191526fbf': _boldom,
     })
