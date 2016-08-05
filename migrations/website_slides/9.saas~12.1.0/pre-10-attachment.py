7b46811d4e6bfcc0024fa70e37b61a1fe38f8f9b
Christophe Simonis <chs@odoo.com>
2016-08-05 16:45:12 +0200

    [ADD] website_slides@saas~12

---
+++ migrations/website_slides/9.saas~12.1.0/pre-10-attachment.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    imp = util.import_script('base/9.0.1.3/post-20-binary_field_as_attachment.py')
+    imp.convert(cr, 'slide.slide', 'datas')     # seriously?
