33665a237729ca3e6760803eb51ca8d73174a311
Christophe Simonis <chs@odoo.com>
2016-08-09 12:35:47 +0200

    [ADD] rating_project@saas~12

---
+++ migrations/rating_project/9.saas~12.1.0/pre-10-models.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.create_column(cr, 'project_project', 'rating_status', 'varchar')
+    util.create_column(cr, 'project_project', 'rating_request_deadline', 'timestamp without time zone')
+
+    cr.execute("""
+        WITH stages AS (
+            UPDATE mail_template m
+               SET model = 'project.task'
+              FROM project_task_type t
+             WHERE m.id = t.rating_template_id
+         RETURNING t.id
+        )
+        UPDATE project_project p
+           SET rating_status = 'stage'
+          FROM stages s, project_task_type_rel r
+         WHERE p.id = r.project_id
+           AND r.type_id = s.id
+        )
+    """)
+    cr.execute("UPDATE project_project SET rating_status = 'no' WHERE rating_status IS NULL")
