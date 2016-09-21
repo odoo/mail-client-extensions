350a0d92413f6eef68e57391aba8fbe169995d9f
Olivier Dony <odo@openerp.com>
2016-09-21 18:16:49 +0200

    [IMP] 9.saas~12: improve release notes for migrated customers

--- migrations/mail/9.saas~12.1.0/post-999-announce-new-version.py
+++ migrations/mail/9.saas~12.1.0/post-999-announce-new-version.py
@@ -4,25 +4,28 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     domain = util.env(cr)['ir.config_parameter'].get_param('mail.catchall.domain', 'localhost')
     message = """
-- New modules:
-    - Equipements: Track equipment and manage maintenance requests.
-    + Quality Control: Quality Alerts and Control Points.
-    + Account TaxCloud: Compute sales tax automatically using TaxCloud based on customer address in United States.
-- MRP:
-    - Complete new MRP module set.
-- Point of Sale: manage lot/serial number from POS.
-- Expenses:
-    - Can now regroup expenses in sheets.
-    - New email interface: just send a picture of you ticket via email to expense@{domain} with total price in subject.
-    - Improved checkout process.
-    - Simple "Company Name" field on partners helps creation from checkout.
-    - Option to show a warning when a product is out of stock.
-    - Be more B2C friendly.
+- Manufacturing:
+    - Major redesign of the MRP App for v10
+    - New MRP Apps: Quality Control, PLM, MPS, Work Orders planning, Equipments & Maintenance
+- Expenses: updated App with many improvements, support for submitting batch expenses as Expense Reports, creation via email, etc.
+- Accounting: new TaxCloud App, to compute sales tax based on customer address in the USA
+- Events: new App to add barcodes on printed badges for faster check-in
+- Point of Sale: manage lots/serial numbers directly from POS
+- eCommerce, Sales & Payments:
+    - Improved checkout process (design + extensibility)
+    - "Sold out" warnings can be displayed on the Shop
+    - B2C/B2B tax display option (show prices with/without taxes)
+    - Customers: new "company name" field on customers for simpler B2C signup
+    - Payment methods can now be configured to generate paid invoices after acquirer confirmation
+- Mailing-list: unsubscription now possible without logging in
+- Project: improved front-end portal (issue rating + view projects)
 - Usability:
-    - Qweb templates are now compiled, speeding up page rendering.
-    - Basic python error detection in server actions.
-    - Ease search of date range.
-    - Many performance improvements.
+    - Chatter: source document is now a link in the message timeline
+    - New advanced search operator for date range: "between <begin> and <end>"
+    - QWeb templates are now compiled, website rendering is much faster
+    - Basic python error detection in server actions
+    - Many smaller performance improvements
+- And much more...
 """.format(domain=domain)
 
     util.announce(cr, '9.saas~12', message, format='md')
