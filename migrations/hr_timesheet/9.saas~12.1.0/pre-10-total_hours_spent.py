ad435df4c27ebf4436f56c1fd0b34f8a4c2b40be
Olivier Laurent <olt@odoo.com>
2016-09-23 08:17:43 +0200

    [FIX] hr_timesheet@saas-12: handle not always existing column
    If you are migrating from v9.0, the 'children_hours' column on
    'project.task' will not be existing. It's created in saas-9

--- migrations/hr_timesheet/9.saas~12.1.0/pre-10-total_hours_spent.py
+++ migrations/hr_timesheet/9.saas~12.1.0/pre-10-total_hours_spent.py
@@ -3,4 +3,5 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.create_column(cr, 'project_task', 'total_hours_spent', 'double precision')
-    cr.execute("UPDATE project_task SET total_hours_spent = effective_hours + children_hours")
+    util.create_column(cr, 'project_task', 'children_hours', 'double precision')
+    cr.execute("UPDATE project_task SET total_hours_spent = COALESCE(effective_hours, 0) + COALESCE(children_hours, 0)"
