1953ead1bc2e156a1d2bd46a373d5dc747619374
Christophe Simonis <chs@odoo.com>
2016-09-23 15:17:24 +0200

    [ADD] base@saas~13: xmlidpocalypse!

---
+++ migrations/base/9.saas~13.1.3/pre-xmlidpocalypse.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+
+    # XXX for pos_data_drinks, see script it pos_data_drinks module
+    # TODO demo data for website_sale
+
+    # no braces = from base module
+    renames = util.splitlines("""
+        account.group_account_user
+        association.menu_event_config
+        base_setup.menu_config
+        crm.menu_import_crm
+
+        {sales_team,crm}.menu_crm_config_lead
+        {sales_team,crm}.menu_crm_config_opportunity
+
+        hr.group_hr_user
+        hr.group_hr_manager
+        hr.group_hr_attendance
+        hr_expense.res_partner_address_fp
+
+        {hr,hr_recruitment}.menu_hr_job_position
+        {hr,hr_recruitment}.menu_hr_job_position_config
+
+        mail.action_partner_mass_mail
+
+        maintenance.group_equipment_manager
+        membership.menu_association
+        membership.menu_marketing_config_association
+
+        {product,mrp}.product_assembly
+        mrp.menu_mrp_config
+        mrp.menu_mrp_root
+
+        project.menu_main_pm
+        project.menu_project_config_project
+        project.menu_project_report
+        project.menu_project_general_settings
+
+        purchase.purchase_report
+        purchase.menu_purchase_root
+
+        {sales_team,sale}.menu_sales_config
+
+        sale_stock.menu_aftersale
+        sale_stock.menu_invoiced
+
+        sales_team.menu_action_currency_form
+        sales_team.menu_action_res_bank_form
+        sales_team.menu_action_res_partner_bank_form
+        sales_team.menu_sale_report
+        sales_team.sales_team_config
+
+        survey.group_survey_user
+        survey.group_survey_manager
+
+        {survey,survey_crm}.action_partner_survey_mail_crm
+
+        website.group_website_publisher
+        website.group_website_designer
+
+        {website,sales_team}.salesteam_website_sales
+
+        # not really part of xmlidpocalypse, but we can rename it here...
+        {mass_mailing,website_mass_mailing}.group_website_popup_on_exit
+
+        {project,website_project}.portal_project_rule
+        {project,website_project}.portal_task_rule
+
+        # enterprise
+        {stock,stock_barcode}.demo_package
+        {document,website_sign}.menu_document
+
+    """)
+
+    # This menu has been deduplicated into 2 modules. We have to choose one.
+    if util.module_installed(cr, 'account'):
+        renames.append('account.menu_action_currency_form')
+    else:
+        renames.append('sales_team.menu_action_currency_form')
+
+    for rename in renames:
+        try:
+            src_id, dest_id = util.expand_braces(rename)
+        except ValueError:
+            src_id = 'base.' + rename.partition('.')[2]
+            dest_id = rename
+
+        util.rename_xmlid(cr, src_id, dest_id)
