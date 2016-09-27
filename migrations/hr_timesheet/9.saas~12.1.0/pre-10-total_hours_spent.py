2851cc319cadddd4bd2b755047dc1badfcc24cf3
Christophe Simonis <chs@odoo.com>
2016-09-27 16:35:03 +0200

    [FIX] hr_timesheet@saas-12: typo

--- migrations/hr_timesheet/9.saas~12.1.0/pre-10-total_hours_spent.py
+++ migrations/hr_timesheet/9.saas~12.1.0/pre-10-total_hours_spent.py
@@ -4,4 +4,4 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.create_column(cr, 'project_task', 'total_hours_spent', 'double precision')
     util.create_column(cr, 'project_task', 'children_hours', 'double precision')
-    cr.execute("UPDATE project_task SET total_hours_spent = COALESCE(effective_hours, 0) + COALESCE(children_hours, 0)"
+    cr.execute("UPDATE project_task SET total_hours_spent = COALESCE(effective_hours, 0) + COALESCE(children_hours, 0)")
