06afbc5fe83a7ab5d63b9e293829eccfad4813b2
Christophe Simonis <chs@odoo.com>
2016-10-05 16:19:07 +0200

    [IMP] util, base@saas~10: extract method to split group into util

--- migrations/base/9.saas~10.1.3/post-10-light-multi-company.py
+++ migrations/base/9.saas~10.1.3/post-10-light-multi-company.py
@@ -2,15 +2,4 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    lmc = util.ref(cr, 'base.group_light_multi_company')
-    mc = util.ref(cr, 'base.group_multi_company')
-    cr.execute("""
-        INSERT INTO res_groups_users_rel(uid, gid)
-             SELECT uid, %s
-               FROM res_groups_users_rel
-              WHERE gid = %s
-             EXCEPT
-             SELECT uid, gid
-               FROM res_groups_users_rel
-              WHERE gid = %s
-    """, [mc, lmc, mc])
+    util.split_group(cr, 'base.group_light_multi_company', 'base.group_multi_company')
