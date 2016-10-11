34fdf7b9de46ef34252b97b8632686ff278af6f0
Christophe Simonis <chs@odoo.com>
2016-10-11 14:09:56 +0200

    [ADD] l10n_fr_payroll@saas~13

---
+++ migrations/l10n_fr_hr_payroll/9.saas~13.1.0/pre-models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.rename_model(cr,
+                      'report.l10n_fr_hr_payroll.report_l10nfrfichepaye',
+                      'report.l10n_fr_hr_payroll.report_l10n_fr_fiche_paye')
+
+    util.rename_xmlid(cr,
+                      'l10n_fr_hr_payroll.report_l10nfrfichepaye',
+                      'l10n_fr_hr_payroll.report_l10n_fr_fiche_paye')
+
+    util.remove_view(cr, 'l10n_fr_hr_payroll.res_company_form_l10n_fr_payroll')
