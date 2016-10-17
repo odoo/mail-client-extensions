d355970df4eff2373158141b5294b7fb7b2713b6
Olivier Laurent <olt@odoo.com>
2016-10-17 16:28:34 +0200

    [FIX] sale_timesheet@saas-12: check column exist
    When migrating from an Odoo 9.0 to saas-12, the 'project_id' on
    'product_template' does not yet exist in 'pre' section. It's only
    created in saas-11.

--- migrations/sale_timesheet/9.saas~12.1.0/pre-models.py
+++ migrations/sale_timesheet/9.saas~12.1.0/pre-models.py
@@ -2,5 +2,6 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.convert_field_to_property(cr, 'product.template', 'project_id',
-                                   'many2one', 'project.project')
+    if util.column_exists(cr, 'product_template', 'project_id'):
+        util.convert_field_to_property(cr, 'product.template', 'project_id',
+                                       'many2one', 'project.project')
