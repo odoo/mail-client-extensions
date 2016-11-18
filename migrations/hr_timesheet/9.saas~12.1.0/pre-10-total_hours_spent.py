328464fbd5e790649b564dd9088888663129e4da
Ravi Gohil <rgo@odoo.com>
2016-11-18 15:54:16 +0530

    [FIX] hr_timesheet@saas~12: Execute 'UPDATE' only if column exists,     Fields like total_hours_spent, children_hours, effective_hours won't be there in the db being migrated currently if previous version dbs has got 'hr_timesheet' module installed but hasn't got 'project_timesheet' module installed.

--- migrations/hr_timesheet/9.saas~12.1.0/pre-10-total_hours_spent.py
+++ migrations/hr_timesheet/9.saas~12.1.0/pre-10-total_hours_spent.py
@@ -2,6 +2,7 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.create_column(cr, 'project_task', 'total_hours_spent', 'double precision')
-    util.create_column(cr, 'project_task', 'children_hours', 'double precision')
-    cr.execute("UPDATE project_task SET total_hours_spent = COALESCE(effective_hours, 0) + COALESCE(children_hours, 0)")
+    if util.column_exists(cr, 'project_task', 'effective_hours'):
+        util.create_column(cr, 'project_task', 'total_hours_spent', 'double precision')
+        util.create_column(cr, 'project_task', 'children_hours', 'double precision')
+        cr.execute("UPDATE project_task SET total_hours_spent = COALESCE(effective_hours, 0) + COALESCE(children_hours, 0)")
