1b6c363b05baea77241c000ad9331d7a5ef19c01
Julien Legros <jle@odoo.com>
2016-11-21 16:33:20 +0100

    [FIX] mail@saas-13: missing primary key

--- migrations/mail/9.saas~13.1.0/pre-10-migrate.py
+++ migrations/mail/9.saas~13.1.0/pre-10-migrate.py
@@ -6,7 +6,7 @@ def migrate(cr, version):
     util.force_noupdate(cr, 'mail.mail_template_data_notification_email_default', False)
 
     cr.execute('ALTER TABLE res_users ALTER COLUMN alias_id DROP NOT NULL')
-    util.create_column(cr, 'mail_message_res_partner_needaction_rel', 'id', 'SERIAL')
+    util.create_column(cr, 'mail_message_res_partner_needaction_rel', 'id', 'SERIAL PRIMARY KEY')
 
     # fix m2m
     cr.execute("ALTER TABLE mail_channel_res_group_rel RENAME COLUMN groups_id TO res_groups_id")
