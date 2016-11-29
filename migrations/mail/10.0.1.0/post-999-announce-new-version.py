0481fb9d8f36e43e7761a72ee3a38ae6e7a32c34
Christophe Simonis <chs@odoo.com>
2016-11-29 19:26:41 +0100

    [ADD] mail@10.0: announce message

---
+++ migrations/mail/10.0.1.0/post-999-announce-new-version.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    # NOTE message is in Markdown \o/
+    message = """
+
+## Odoo has been upgraded to version 10.0.
+
+Read the [release notes on our website](https://www.odoo.com/page/odoo-10-release-notes).
+
+"""
+
+    util.announce(cr, '10.0', message, format='md', header=None)
+
+if __name__ == '__main__':
+    # odoo must be in PYTHONPATH
+    def echo(_cr, version, message, *a, **kw):
+        print util.md2html(message)
+    util.announce = echo
+    migrate(None, None)
