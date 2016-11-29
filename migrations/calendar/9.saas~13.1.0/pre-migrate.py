5e2fca0314a1606b478d00e5347f0172a2d5a217
Christophe Simonis <chs@odoo.com>
2016-11-29 20:10:39 +0100

    [FIX] calendar@saas~13: adapt mail templates

--- migrations/calendar/9.saas~13.1.0/pre-migrate.py
+++ migrations/calendar/9.saas~13.1.0/pre-migrate.py
@@ -6,3 +6,15 @@ def migrate(cr, version):
         util.force_noupdate(cr, 'calendar.calendar_template_meeting_' + t, False)
 
     util.rename_field(cr, 'calendar.attendee', 'cn', 'common_name')
+
+    # signature of method `get_interval` used in template has change.
+    # Adpat them in case they are in noupdate.
+    cr.execute("""
+        UPDATE mail_template
+           SET body_html=regexp_replace(regexp_replace(body_html, '\ycn\y', 'common_name', 'g'),
+                                        '\.get_interval\([^,]+,\s*', '.get_interval(', 'g')
+               , subject=regexp_replace(regexp_replace(subject, '\ycn\y', 'common_name', 'g'),
+                                        '\.get_interval\([^,]+,\s*', '.get_interval(', 'g')
+
+         WHERE model_id = (SELECT id FROM ir_model WHERE model='calendar.attendee')
+    """)
