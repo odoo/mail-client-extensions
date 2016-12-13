1df3809f756aaece6043e299d9e45353718ac106
Christophe Simonis <chs@odoo.com>
2016-12-13 13:18:09 +0100

    [ADD] base@saas~11: db-specific scripts

--- migrations/base/9.saas~11.1.3/pre-00-db-specific.py
+++ migrations/base/9.saas~11.1.3/pre-00-db-specific.py
@@ -29,8 +29,21 @@ def _db_openerp(cr, version):
        ALTER COLUMN purchase_warn SET NOT NULL
     """)
 
+def _db_watermark_collection(cr, version):
+    util.remove_view(cr, view_id=823)
+    cr.execute("UPDATE ir_ui_view SET inherit_id=610 WHERE inherit_id=800")
+
+def _db_tvpaint(cr, version):
+    cr.execute("UPDATE ir_ui_view SET priority=42 WHERE id=832")
+
+def _db_houtmerk(cr, version):
+    cr.execute("UPDATE ir_ui_view SET inherit_id=633 WHERE id=2491")
+
 def migrate(cr, version):
     util.dispatch_by_dbuuid(cr, version, {
         '05a64ced-5b98-488d-a833-a994f9b1dd80': _db_openerp,    # test
         '8851207e-1ff9-11e0-a147-001cc0f2115e': _db_openerp,    # prod
+        '3dc91671-94e5-4018-9f14-6251c822e93b': _db_watermark_collection,
+        '454555a6-d931-47a9-bc25-bbc11dab3a67': _db_tvpaint,
+        'ce2bbc1f-5d81-4140-909e-1204e9f743b7': _db_houtmerk,
     })
