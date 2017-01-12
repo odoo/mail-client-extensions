c6f9753ea7d844ebd898aeea0791b5e745bb3a07
pja-odoo <pja@tinyerp.com>
2017-01-12 12:32:36 +0530

    [FIX] l10n_fr_hr_payroll@9.saas~13.1.0: rename model with argument 'rename_table=False'

--- migrations/l10n_fr_hr_payroll/9.saas~13.1.0/pre-models.py
+++ migrations/l10n_fr_hr_payroll/9.saas~13.1.0/pre-models.py
@@ -4,7 +4,7 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.rename_model(cr,
                       'report.l10n_fr_hr_payroll.report_l10nfrfichepaye',
-                      'report.l10n_fr_hr_payroll.report_l10n_fr_fiche_paye')
+                      'report.l10n_fr_hr_payroll.report_l10n_fr_fiche_paye',rename_table=False)
 
     util.rename_xmlid(cr,
                       'l10n_fr_hr_payroll.report_l10nfrfichepaye',
