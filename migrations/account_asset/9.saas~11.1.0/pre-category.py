172c9f10ad984fc4d7751e92ce64434c31178bfa
Christophe Simonis <chs@odoo.com>
2017-01-18 18:00:58 +0100

    [FIX] account_asset@{saas-11,10.0}: only rename field in saas~11

--- migrations/account_asset/9.saas~11.1.0/pre-category.py
+++ migrations/account_asset/9.saas~11.1.0/pre-category.py
@@ -2,9 +2,10 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    # rename is onlu relevant in saas-11 (no-op after)
-    util.rename_field(cr, 'account.asset.category',
-                      'account_income_recognition_id', 'account_depreciation_expense_id')
+    # rename is only relevant in saas-11
+    if not util.column_exists(cr, 'account_asset_category', 'account_depreciation_expense_id'):
+        util.rename_field(cr, 'account.asset.category',
+                          'account_income_recognition_id', 'account_depreciation_expense_id')
     # field now required in saas-11
     # as it was wrongly named (and used) in 9.0, re-map related fields.
     cr.execute("""
