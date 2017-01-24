3ca1f818b5910145dae674bd5587b8bfe205d412
Christophe Simonis <chs@odoo.com>
2017-01-24 13:01:58 +0100

    [FIX] website_sale@saas~13: do not convert de-facto website_pricelist's country group

--- migrations/website_sale/9.saas~13.1.0/pre-10-models.py
+++ migrations/website_sale/9.saas~13.1.0/pre-10-models.py
@@ -5,6 +5,22 @@ def migrate(cr, version):
     util.create_column(cr, 'product_pricelist', 'website_id', 'int4')
     util.create_column(cr, 'product_pricelist', 'selectable', 'boolean')
 
+    # A default website_pricelist is created by module data. Users may not realise it, but it
+    # restrict main pricelist to europe by default (only on ecommerce < saas-13).
+    # This is not necessary what users want in backend. Only keep it if modified by user (as the
+    # record is in noupdate, only user can modify the write date).
+    wlist0 = util.ref(cr, 'website_sale.wlist0')
+    europe = util.ref(cr, 'base.europe')
+    if wlist0 and europe:
+        cr.execute("""
+            DELETE FROM res_country_group_website_pricelist_rel r
+                  USING website_pricelist p
+                  WHERE r.res_country_group_id = %s
+                    AND r.website_pricelist_id = p.id
+                    AND p.id = %s
+                    AND p.create_date = p.write_date
+        """, [europe, wlist0])
+
     cr.execute("""
       WITH upd AS (
             UPDATE product_pricelist pl
