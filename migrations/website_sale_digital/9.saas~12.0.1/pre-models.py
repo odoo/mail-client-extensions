de03d30fedeb01d2ebb06b7bb422f80084b3f85b
Christophe Simonis <chs@odoo.com>
2017-02-13 12:01:16 +0100

    [FIX] website_sale_digital@saas~12: it's the product.template type that should be changed...
    Closes #219

--- migrations/website_sale_digital/9.saas~12.0.1/pre-models.py
+++ migrations/website_sale_digital/9.saas~12.0.1/pre-models.py
@@ -4,7 +4,21 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.create_column(cr, 'ir_attachment', 'product_downloadable', 'boolean')
     cr.execute("""
-        UPDATE ir_attachment
-           SET type='service', product_downloadable=true
-         WHERE type='digital'
+        WITH products AS (
+            SELECT 'product.template' as model, id
+              FROM product_template
+             WHERE type = 'digital'
+             UNION
+            SELECT 'product.product' as model, p.id
+              FROM product_product p
+              JOIN product_template t ON (t.id = p.product_tmpl_id)
+             WHERE t.type = 'digital'
+        )
+        UPDATE ir_attachment a
+           SET product_downloadable=true
+          FROM products p
+         WHERE a.res_model = p.model
+           AND a.res_id = p.id
     """)
+
+    cr.execute("UPDATE product_template SET type='service' WHERE type='digital'")
