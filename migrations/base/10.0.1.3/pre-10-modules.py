c72be7d0ff8974775d66d9cfb3fb063b1cbff730
Christophe Simonis <chs@odoo.com>
2017-02-20 14:58:54 +0100

    [FIX] base@10.0: new link module: hr_expense_sepa

--- migrations/base/10.0.1.3/pre-10-modules.py
+++ migrations/base/10.0.1.3/pre-10-modules.py
@@ -7,3 +7,5 @@ def migrate(cr, version):
         util.new_module(cr, 'mail_push', deps=('mail', 'web_mobile'), auto_install=True)
         util.new_module(cr, 'stock_barcode_mobile', deps=('stock_barcode', 'web_mobile'),
                         auto_install=True)
+        util.new_module(cr, 'hr_expense_sepa', deps=('account_sepa', 'hr_expense'),
+                        auto_install=True)
