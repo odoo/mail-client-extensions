83e663e9d35ae7f2d29ea3ec3b8c6784a89545d7
Christophe Simonis <chs@odoo.com>
2017-02-24 17:24:45 +0100

    [FIX] theme_common@10.0: remove more views

--- migrations/theme_common/10.0.1.1/pre-10-remove-views.py
+++ migrations/theme_common/10.0.1.1/pre-10-remove-views.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from openerp.addons.base.maintenance.migrations import util
 
-
 def migrate(cr, version):
-    util.remove_view(cr, 'theme_common.s_showcase_image_caption-opt')
+
+    views = util.splitlines("""
+        theme_common.s_showcase_image_caption-opt
+        theme_common.s_options_discount
+
+        theme_clean.options_colorpicker
+        theme_enark.options_colorpicker
+        theme_nano.editor_colorpicker
+        theme_treehouse.editor_colorpicker
+    """)
+    for v in views:
+        util.remove_view(cr, v)
+
+    for t in 'anelusia clean kea kiddo loftspace monglia notes'.split():
+        util.remove_view(cr, 'theme_%s.colorpicker' % t)
