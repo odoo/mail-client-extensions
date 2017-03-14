8b7b61eeb75d5a832788a9722220158b17049fb0
Christophe Simonis <chs@odoo.com>
2017-03-14 17:06:51 +0100

    [FIX] base@saas~12: handle the case where customer delete countries...

--- migrations/base/9.saas~12.1.3/pre-3166-2.py
+++ migrations/base/9.saas~12.1.3/pre-3166-2.py
@@ -5,10 +5,16 @@ def migrate(cr, version):
     # forbid the creation of `name_code_uniq` constraint on `res.country.state` to ensure
     # csv file will be imported.
     # The constraint will be created after cleaning in post script.
+    cc = util.ref(cr, 'base.cc')
+    if not cc:
+        # Why do people remove countries?
+        cr.execute("SELECT min(id) FROM res_country")
+        [cc] = cr.fetchone() or [None]
+
     cr.execute("""
         INSERT INTO res_country_state(country_id, code, name)
              VALUES (%(country)s, 'CC', 'CC'),
                     (%(country)s, 'CC', 'CC')
           RETURNING id
-    """, dict(country=util.ref(cr, 'base.cc')))
+    """, dict(country=cc))
     util.ENVIRON['cc_states'] = [s[0] for s in cr.fetchall()]
