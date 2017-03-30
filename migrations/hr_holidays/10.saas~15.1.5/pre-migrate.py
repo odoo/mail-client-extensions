df34f2faf4c2f452f81f4d70c24e905eb64fa484
Christophe Simonis <chs@odoo.com>
2017-03-30 13:27:10 +0200

    [ADD] hr_holidays@saas~15

---
+++ migrations/hr_holidays/10.saas~15.1.5/pre-migrate.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.remove_view(cr, 'hr_holidays.resource_calendar_form_inherit')
