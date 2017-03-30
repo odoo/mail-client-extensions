f5429d6fe5c7a918610b53bbb56c9560efd0f2ad
Christophe Simonis <chs@odoo.com>
2017-03-30 13:27:37 +0200

    [ADD] hr_payroll@saas~15

---
+++ migrations/hr_payroll/10.saas~15.1.0/pre-settings.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.remove_field(cr, 'hr.payroll.config.settings', 'module_hr_payroll_account')
