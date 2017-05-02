7d4d3a097e5b55abefd4a821d95b1ad6763adbc8
Christophe Simonis <chs@odoo.com>
2017-05-02 16:45:47 +0200

    [FIX] website@saas~15

--- migrations/website/10.saas~15.1.0/pre-menu.py
+++ migrations/website/10.saas~15.1.0/pre-menu.py
@@ -2,4 +2,5 @@
 from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.force_noupdate(cr, 'website.menu_website', False)      # force removal
+    util.force_noupdate(cr, 'website.menu_website', False)                  # force removal
+    util.force_noupdate(cr, 'website.menu_website_configuration', False)    # force rename
