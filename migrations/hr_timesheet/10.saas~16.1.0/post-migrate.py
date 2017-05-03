660ee60ca090e791e362eb577d41a27d30efa17e
Christophe Simonis <chs@odoo.com>
2017-05-03 16:25:33 +0200

    [ADD] hr_timesheet@saas~16

---
+++ migrations/hr_timesheet/10.saas~16.1.0/post-migrate.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    gusr = util.ref(cr, 'hr_timesheet.group_hr_timesheet_user')
+    gmng = util.ref(cr, 'hr_timesheet.group_timesheet_manager')
+    gemp = util.ref(cr, 'base.group_user')
+
+    # users are actually mangagers
+    cr.execute("""
+        INSERT INTO res_groups_users_rel
+             SELECT uid, %(gmng)s
+               FROM res_groups_users_rel
+              WHERE gid = %(gusr)s
+             EXCEPT
+             SELECT uid, gid
+               FROM res_groups_users_rel
+              WHERE gid = %(gmng)s
+    """, locals())
+
+    # employees are also users
+    cr.execute("""
+        INSERT INTO res_groups_users_rel
+             SELECT uid, %(gusr)s
+               FROM res_groups_users_rel
+              WHERE gid = %(gemp)s
+             EXCEPT
+             SELECT uid, gid
+               FROM res_groups_users_rel
+              WHERE gid = %(gusr)s
+    """, locals())
