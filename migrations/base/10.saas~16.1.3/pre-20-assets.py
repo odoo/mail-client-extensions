a7604dddf99ef1e200c21f35d45255c94d6d3c6a
Christophe Simonis <chs@odoo.com>
2017-05-03 14:54:07 +0200

    [ADD] base@saas~16: force update backend assets

---
+++ migrations/base/10.saas~16.1.3/pre-20-assets.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+def migrate(cr, version):
+    cr.execute("""
+        UPDATE ir_model_data
+           SET noupdate = false
+         WHERE model = 'ir.ui.view'
+           AND COALESCE(module, '') NOT IN ('', '__export__')
+           AND name IN ('assets_common', 'assets_backend')
+    """)
