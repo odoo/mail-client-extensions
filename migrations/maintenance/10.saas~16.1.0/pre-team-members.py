f647b797ae8f2d7b879c36113a293575bd51f5bb
Christophe Simonis <chs@odoo.com>
2017-05-03 17:25:11 +0200

    [ADD] maintenance@saas~16

---
+++ migrations/maintenance/10.saas~16.1.0/pre-team-members.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.create_m2m(cr, 'maintenance_team_users_rel', 'maintenance_team', 'res_users')
+    cr.execute("""
+        INSERT INTO maintenance_team_users_rel(maintenance_team_id, res_users_id)
+             SELECT t.id, u.id
+               FROM maintenance_team t
+               JOIN res_users u ON (t.partner_id = u.partner_id)
+    """)
+    util.remove_field(cr, 'maintenance_team', 'partner_id')
