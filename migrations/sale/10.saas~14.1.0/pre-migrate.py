32da5ba62129479df9d20f3a5582fe30358d1aad
Julien Legros <jle@odoo.com>
2017-05-16 15:55:49 +0200

    [FIX] {sale,sales_team}@saas~14: split crm_team migration

--- migrations/sale/10.saas~14.1.0/pre-migrate.py
+++ migrations/sale/10.saas~14.1.0/pre-migrate.py
@@ -3,10 +3,9 @@ from openerp.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    util.create_column(cr, 'crm_team', 'team_type', 'varchar')
-    cr.execute("UPDATE crm_team SET team_type='sales'")
+    cr.execute("UPDATE crm_team SET use_invoices=true")
 
     team = util.ref(cr, 'sales_team.salesteam_website_sales')
     if team:
-        cr.execute("UPDATE crm_team SET team_type='website' WHERE id=%s",
+        cr.execute("UPDATE crm_team SET use_invoices=false WHERE id=%s",
                    [team])
