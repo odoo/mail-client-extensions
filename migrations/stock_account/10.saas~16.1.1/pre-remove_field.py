f3a5c604de032c627367f45cfecae523560dcaf0
Yannick Tivisse <yti@odoo.com>
2017-06-15 12:13:20 +0200

    [ADD] stock_account@saas~16

---
+++ migrations/stock_account/10.saas~16.1.1/pre-remove_field.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.remove_field(cr, 'stock.config.settings', 'group_stock_inventory_valuation')
+    util.remove_record(cr, 'stock.account', 'group_inventory_valuation')
