6d705acf38eb8fe17e38f8c3d3e8dbbc6f9a76ec
Christophe Simonis <chs@odoo.com>
2017-07-13 18:50:37 +0200

    [FIX] theme_common@saas~13: adapt snippet options

--- migrations/theme_common/9.saas~13.1.1/pre-rm-views.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp.addons.base.maintenance.migrations import util
-
-def migrate(cr, version):
-    for t in 'anelusia kea loftspace monglia'.split():
-        util.remove_view(cr, 'theme_%s.font_size' % t)
