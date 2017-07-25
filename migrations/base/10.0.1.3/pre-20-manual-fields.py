fa6e1d7c6d3c241da00d543e46bda8bfceb3a520
Christophe Simonis <chs@odoo.com>
2017-07-25 19:16:22 +0200

    [ADD] base@10: correct wrongly defined manual fields

---
+++ migrations/base/10.0.1.3/pre-20-manual-fields.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+def migrate(cr, version):
+    cr.execute("""
+        SELECT id, model, related
+          FROM ir_model_fields
+         WHERE name LIKE 'x\_%'
+           AND state = 'base'
+           AND related IS NOT NULL
+    """)
+
+    toup = []
+
+    for fid, model, related in cr.fetchall():
+        path = related.split('.')
+        comodel = model
+        for field in path:
+            cr.execute("SELECT relation, state FROM ir_model_fields WHERE model=%s AND name=%s",
+                       [comodel, field])
+            if not cr.rowcount:
+                # badly defined related, ignore
+                break
+            comodel, state = cr.fetchone()
+            if state == 'manual':
+                toup.append(fid)
+                break
+            if not comodel:
+                # badly defined related, ignore
+                break
+
+    cr.execute("UPDATE ir_model_fields SET state='manual' WHERE id=ANY(%s)", [toup])
+
+    cr.execute(
+        "UPDATE ir_model_fields SET selection='[]' WHERE selection IS NULL AND ttype='selection'"
+    )
