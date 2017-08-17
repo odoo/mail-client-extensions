cbad71bd7586da5eb140561cdc5ae6a133e62fe3
pja-odoo <pja@tinyerp.com>
2017-08-17 14:09:01 +0200

    [FIX] account_check_printing@saas~13: Correct code indentation

--- migrations/account_check_printing/9.saas~13.1.0/pre-migrate.py
+++ migrations/account_check_printing/9.saas~13.1.0/pre-migrate.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+from textwrap import dedent
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    code = """
+    code = dedent("""
        if context.get('active_model') == 'account.payment' and context.get('active_ids'):
            action = env['account.payment'].browse(context['active_ids']).print_checks()
-    """
+    """)
     action = util.ref(cr, 'account_check_printing.action_account_print_checks')
     if action:
         cr.execute("UPDATE ir_act_server SET code=%s WHERE id=%s", [code, action])
