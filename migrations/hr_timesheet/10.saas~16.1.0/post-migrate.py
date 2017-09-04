28d1399339584f656cc39af7370015aeaa08afe6
Christophe Simonis <chs@odoo.com>
2017-09-04 13:08:57 +0200

    [FIX] hr_timesheet@saas~16: adapt group deps

--- migrations/hr_timesheet/10.saas~16.1.0/post-migrate.py
+++ migrations/hr_timesheet/10.saas~16.1.0/post-migrate.py
@@ -4,11 +4,12 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     gusr = util.ref(cr, 'hr_timesheet.group_hr_timesheet_user')
     gmng = util.ref(cr, 'hr_timesheet.group_timesheet_manager')
+    ghru = util.ref(cr, 'hr.group_hr_user')
     gemp = util.ref(cr, 'base.group_user')
 
     # correct implied_id on gmng (use orm to handle users)
     util.env(cr)['res.users'].browse(gmng).write({
-        'implied_ids': [(6, 0, [gusr])],
+        'implied_ids': [(6, 0, [gusr, ghru])],
     })
 
     # employees are users
