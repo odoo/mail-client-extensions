57a6fdf7c038f5f1305d58dec635021c44fa0d7e
Christophe Simonis <chs@odoo.com>
2017-09-08 18:20:46 +0200

    [ADD] website_sale@saas~17

---
+++ migrations/website_sale/10.saas~17.1.0/pre-migrate.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.create_column(cr, 'sale_order', 'can_directly_mark_as_paid', 'boolean')
+    cr.execute("""
+        UPDATE sale_order s
+           SET can_directly_mark_as_paid = (s.state IN ('sent', 'sale') AND a.provider IN ('transfer', 'manual'))
+          FROM payment_acquirer a
+         WHERE a.id = s.payment_acquirer_id
+           AND s.payment_tx_id IS NOT NULL
+    """)
+    cr.execute("UPDATE sale_order SET can_directly_mark_as_paid = false WHERE can_directly_mark_as_paid IS NULL")
+
+    util.rename_field(cr, 'website.config.settings', 'sale_pricelist_setting_split_1', 'multi_sales_price')
+    util.rename_field(cr, 'website.config.settings', 'sale_pricelist_setting_split_2', 'multi_sales_price_method')
