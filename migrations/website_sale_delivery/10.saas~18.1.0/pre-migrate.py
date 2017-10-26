424f87ad0af7c1149390e62a07cc9382b096fbff
Christophe Simonis <chs@odoo.com>
2017-10-26 15:49:08 +0200

    [ADD] website_sale_delivery@saas~18

---
+++ migrations/website_sale_delivery/10.saas~18.1.0/pre-migrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    eb = util.expand_braces
+    util.rename_xmlid(cr, *eb('website_sale_delivery.{,portal_order_page_}shipping_tracking'))
