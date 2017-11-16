235ab1b10f5c43cf62d8b9b1cd87d8f1223ebc9b
Christophe Simonis <chs@odoo.com>
2017-11-16 13:27:16 +0100

    [ADD] base@11.0: modules changes

---
+++ migrations/base/11.0.1.3/pre-10-modules.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.new_module(cr, 'iap', deps={'web'}, auto_install=True)
+
+    if not util.has_enterprise():
+        # moved from enterprise \o/
+        util.new_module(cr, 'sms', deps={'iap', 'mail'}, auto_install=True)
+        util.new_module(cr, 'calendar_sms', deps={'calendar', 'sms'}, auto_install=True)
+    else:
+        util.new_module_dep(cr, 'account_accountant', 'account_reports')
+        util.new_module_dep(cr, 'sms', 'iap')
+        util.remove_module(cr, 'sms_fortytwo')
