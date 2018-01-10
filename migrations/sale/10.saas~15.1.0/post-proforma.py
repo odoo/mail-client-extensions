06115ce79835b1fd9e49e33bd12725827afe6f89
Christophe Simonis <chs@odoo.com>
2018-01-10 15:55:56 +0100

    [FIX] sale@saas~15: handle saas~18 model rename

--- migrations/sale/10.saas~15.1.0/post-proforma.py
+++ migrations/sale/10.saas~15.1.0/post-proforma.py
@@ -2,4 +2,5 @@
 from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.env(cr)['sale.config.settings'].create({'group_proforma_sales': True}).execute()
+    model = 'res.config.settings' if util.version_gte('10.saas~18') else 'sale.config.settings'
+    util.env(cr)[model].create({'group_proforma_sales': True}).execute()
