e0af7c05f2456b9fcbfe0da5030badbf292df57f
Olivier Laurent <olt@odoo.com>
2018-01-24 10:28:22 +0100

    [FIX] point_of_sale@saas~16: works on multiple records

--- migrations/point_of_sale/10.saas~16.1.0.1/post-pos_config.py
+++ migrations/point_of_sale/10.saas~16.1.0.1/post-pos_config.py
@@ -4,5 +4,6 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     pos_configs = util.env(cr)['pos.config'].search([('customer_facing_display_html', '=', False)])
-    default_values = pos_configs.default_get(['customer_facing_display_html'])
-    pos_configs.write(default_values)
+    for pos_config in pos_configs:
+        default_values = pos_config.default_get(['customer_facing_display_html'])
+        pos_config.write(default_values)
