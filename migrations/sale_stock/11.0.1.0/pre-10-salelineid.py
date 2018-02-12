32205fe6d2273a2ff2dab0b2990d8234f0b528ad
Josse Colpaert <jco@odoo.com>
2018-02-12 11:36:37 +0100

    [IMP] purchase, purchase_requisition, sale_stock: procurement removal consequences
    As we removed the procurement.order object, we need to provide some
    extra links on stock move or purchase (requistion) lines to know
    whether a purchase line is foreseen for an MTO e.g., although
    the stock.move has not been created yet.

---
+++ migrations/sale_stock/11.0.1.0/pre-10-salelineid.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    # Need sale_line_id on stock move instead of procurement.sale_line_id
+    util.create_column(cr, 'stock_move', 'sale_line_id', 'int4')
+    cr.execute("""UPDATE stock_move 
+                SET sale_line_id = p.sale_line_id
+                FROM procurement_order p WHERE procurement_id = p.id
+    """)
