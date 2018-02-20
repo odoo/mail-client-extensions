bece1893f7e78c6550cc7f9ddfbda5174c60e7e6
Christophe Simonis <chs@odoo.com>
2018-02-20 17:08:13 +0100

    [IMP] voip@saas~18: batch mode when called from odoo-shell

--- migrations/voip/10.saas~18.2.0/post-compute-fields.py
+++ migrations/voip/10.saas~18.2.0/post-compute-fields.py
@@ -8,10 +8,11 @@ def fmt(number, cc):
         pn = phonenumbers.parse(number, region=cc, keep_raw_input=True)
         if phonenumbers.is_possible_number(pn) and phonenumbers.is_valid_number(pn):
             return phonenumbers.format_number(pn, phonenumbers.PhoneNumberFormat.E164)
+        return number.strip()
     except phonenumbers.phonenumberutil.NumberParseException:
         return number.strip()
 
-def _sanitize(cr, field):
+def _sanitize(cr, field, batch=None):
     cr.execute("""
         SELECT p.id, p.{0}, c.code
           FROM res_partner p
@@ -19,11 +20,14 @@ def _sanitize(cr, field):
          WHERE trim(coalesce(p.{0}, '')) != ''
            AND p.sanitized_{0} IS NULL
     """.format(field))
-    for pid, number, country_code in util.log_progress(cr.fetchall(), qualifier='res.partner ' + field):
+
+    it = util.log_progress(cr.fetchall(), qualifier='res.partner ' + field)
+    for idx, (pid, number, country_code) in enumerate(it, 1):
         cr.execute("UPDATE res_partner SET sanitized_{0}=%s WHERE id=%s".format(field),
                    [fmt(number, country_code), pid])
-    # ids = (x[0] for x in cr.fetchall())
-    # util.recompute_fields(cr, 'res.partner', ids, 'sanitized_' + field)
+        if batch and idx % batch == 0:
+            cr.commit()
+
     cr.execute("""
         UPDATE res_partner
            SET sanitized_{0} = trim({0})
@@ -31,12 +35,12 @@ def _sanitize(cr, field):
            AND trim(coalesce({0}, '')) != ''
     """ .format(field))
 
-def migrate(cr, version):
-    _sanitize(cr, 'phone')
-    _sanitize(cr, 'mobile')
+def migrate(cr, version, batch=None):
+    _sanitize(cr, 'phone', batch)
+    _sanitize(cr, 'mobile', batch)
 
 
 if __name__ == '__main__':
     env = env   # noqa: F821
-    migrate(env.cr, None)
+    migrate(env.cr, None, 1000)
     env.cr.commit()
