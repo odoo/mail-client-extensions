403c726bd07a8a9537b4697ff1ad778cfa0c0b90
Christophe Simonis <chs@odoo.com>
2018-02-21 10:51:48 +0100

    [FIX] l10n_be@9.0: avoid adding duplicated tax-tag m2m
    Happen when there is another l10n_ module that have been loaded before.
    
    opw-815105

--- migrations/l10n_be/9.0.1.1/post-10-tax-tags.py
+++ migrations/l10n_be/9.0.1.1/post-10-tax-tags.py
@@ -2,11 +2,17 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    cr.execute("""INSERT INTO account_tax_account_tag(account_tax_id, account_account_tag_id)
-                  SELECT tax.id, tag.account_account_tag_id
-                  FROM account_tax as tax
-                  LEFT JOIN account_tax_template as template ON tax.name = template._old_name AND tax.type_tax_use = template.type_tax_use
-                  JOIN account_account_tag_account_tax_template_rel as tag ON template.id = tag.account_tax_template_id
+    cr.execute("""
+        INSERT INTO account_tax_account_tag(account_tax_id, account_account_tag_id)
+             SELECT tax.id, tag.account_account_tag_id
+               FROM account_tax as tax
+          LEFT JOIN account_tax_template as template
+                 ON tax.name = template._old_name AND tax.type_tax_use = template.type_tax_use
+               JOIN account_account_tag_account_tax_template_rel as tag
+                 ON template.id = tag.account_tax_template_id
+             EXCEPT
+             SELECT account_tax_id, account_account_tag_id
+               FROM account_tax_account_tag
         """)
 
     util.remove_column(cr, 'account_tax_template', '_old_name')
