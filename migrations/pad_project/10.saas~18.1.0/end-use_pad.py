8e5b449a96e7d762b0d8d712e920be2a5cbc7b13
Christophe Simonis <chs@odoo.com>
2018-02-22 16:07:07 +0100

    [FIX] pad_project@saas~18: correct `use_pads` computation

--- migrations/pad_project/10.saas~18.1.0/end-use_pad.py
+++ migrations/pad_project/10.saas~18.1.0/end-use_pad.py
@@ -4,8 +4,7 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     cr.execute("""
         UPDATE project_project p
-           SET use_pads = id NOT IN (SELECT project_id
-                                       FROM project_task
-                                      WHERE id > %s
-                                   GROUP BY project_id)
+           SET use_pads = id IN (SELECT project_id
+                                   FROM project_task
+                                  WHERE id <= %s)
     """, [util.ENVIRON.get('issue_offset', 0)])
