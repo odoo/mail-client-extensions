afdf18097e82e86b81bc4e8b320f82c7e85f0a18
Christophe Simonis <chs@odoo.com>
2018-02-26 19:28:59 +0100

    [ADD] hr_attendance@saas~11.1

---
+++ migrations/hr_attendance/saas~11.1.2.0/pre-migrate.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.create_column(cr, 'hr_employee', 'last_attendance_id', 'int4')
+
+    cr.execute("""
+        WITH lastatt AS (
+            SELECT employee_id,
+                   max(id ORDER BY check_in desc) as id
+              FROM hr_attendance
+            GROUP BY employee_id
+        )
+        UPDATE hr_employee e
+           SET last_attendance_id = l.id
+          FROM lastatt l
+         WHERE l.employee_id = e.id
+    """)
