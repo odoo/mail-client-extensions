69b21063c055975f548901fd2cebea4685401a9d
Christophe Simonis <chs@odoo.com>
2018-02-26 14:01:39 +0100

    [ADD] payment_strip@11: enable tokenisation

---
+++ migrations/payment_stipe/11.0.1.0/pre-tokenize.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+def migrate(cr, version):
+    cr.execute("""
+        UPDATE payment_acquirer
+           SET save_token = 'always'
+         WHERE provider = 'stripe'
+    """)
