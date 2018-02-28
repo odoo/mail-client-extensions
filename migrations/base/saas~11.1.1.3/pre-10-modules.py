6e5dfd88718cb247fcbddbadcdae654babbbbf04
Christophe Simonis <chs@odoo.com>
2018-02-28 19:38:37 +0100

    [IMP] base@saas~11.1: module changes (EE)

--- migrations/base/saas~11.1.1.3/pre-10-modules.py
+++ migrations/base/saas~11.1.1.3/pre-10-modules.py
@@ -16,4 +16,13 @@ def migrate(cr, version):
 
     util.new_module(cr, 'website_sale_link_tracker', deps={'website_sale', 'website_links'}, auto_install=True)
 
+    if util.has_enterprise():
+        util.module_deps_diff(cr, 'mrp_maintenance', plus={'mrp_workorder'}, minus={'quality_mrp'})
+        util.module_deps_diff(cr, 'mrp_workorder', plus={'quality'})
+        util.new_module(cr, 'quality_control', deps=('quality',), auto_install=True)
+        util.module_deps_diff(cr, 'quality_mrp', plus={'quality_control', 'mrp'}, minus={'mrp_workorder', 'quality'})
+        util.new_module(cr, 'quality_mrp_workorder', deps=('quality_control', 'mrp_workorder'), auto_install=True)
+
+        util.new_module(cr, 'test_marketing_automation', deps=('marketing_automation', 'test_mail'))
+
     util.remove_module(cr, 'sale_service_rating')
