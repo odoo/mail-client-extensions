b8398594638f36d59874c55e7c390f3946349c97
Christophe Simonis <chs@odoo.com>
2018-03-01 16:44:35 +0100

    [FIX] website@saas~17: verify column existance before use

--- migrations/website/10.saas~17.1.0/pre-migrate.py
+++ migrations/website/10.saas~17.1.0/pre-migrate.py
@@ -8,9 +8,10 @@ def migrate(cr, version):
     for g in 'maps analytics analytics_dashboard'.split():
         imp.default_to_icp(cr, 'website.config.settings', 'has_google_' + g, 'website.has_google' + g)
 
-    cr.execute("SELECT count(1) FROM website WHERE COALESCE(google_management_client_secret, '') != ''")
-    has = cr.fetchall()[0] > 0
-    util.env(cr)['ir.config_parameter'].set_param('website_has_google_analytics_dashboard', has)
+    if util.column_exists(cr, 'website', 'google_management_client_secret'):    # only appear in saas~14
+        cr.execute("SELECT count(1) FROM website WHERE COALESCE(google_management_client_secret, '') != ''")
+        has = cr.fetchall()[0] > 0
+        util.env(cr)['ir.config_parameter'].set_param('website_has_google_analytics_dashboard', has)
 
     cr.execute("SELECT count(1) FROM website WHERE COALESCE(google_analytics_key, '') != ''")
     has = cr.fetchall()[0] > 0
