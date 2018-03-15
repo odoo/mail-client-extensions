57dd5e51a27c5e6da994cbf577284ec40527fcb5
Christophe Simonis <chs@odoo.com>
2018-03-15 20:30:24 +0100

    [ADD] hr_holidays@saas~11.2

---
+++ migrations/hr_holidays/saas~11.2.1.5/post-createallocations.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    # create missing indexes
+    index_query = "CREATE INDEX {0}_{1}_index ON {0}({1})".format
+    cr.execute(index_query('resource_calendar_leaves', 'holiday_id'))
+    cr.execute(index_query('hr_leave', 'parent_id'))
+    cr.execute(index_query('hr_leave_allocation', 'parent_id'))
+    if util.column_exists(cr, 'account_analytic_line', 'holiday_id'):
+        cr.execute(index_query('account_analytic_line', 'holiday_id'))
+
+    columns = ','.join(util.get_columns(cr, 'hr_leave_allocation', ignore=('id', 'parent_id'))[0])
+    util.create_column(cr, 'hr_leave_allocation', '_tmp', 'int4')
+    cr.execute("""
+        INSERT INTO hr_leave_allocation(_tmp, {0})
+             SELECT id, {0}
+               FROM hr_leave
+              WHERE type='add'
+    """.format(columns))
+    cr.execute("""
+        UPDATE hr_leave_allocation a
+           SET parent_id = p.id
+          FROM hr_leave l,
+               hr_leave_allocation p
+         WHERE l.id = a._tmp
+           AND p._tmp = l.parent_id
+    """)
+
+    cr.execute("DELETE FROM hr_leave WHERE type='add'")
+    util.remove_field(cr, 'hr.leave', 'type')
+    util.remove_column(cr, 'hr_leave_allocation', '_tmp')
+
+    util.env(cr)['hr.leave']._add_sql_constraints()
+
+    # TODO create followers for allocation message.type
