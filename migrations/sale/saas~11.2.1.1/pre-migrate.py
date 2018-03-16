be7a44633ffab47955c6549355f4fa9941edfbfb
Christophe Simonis <chs@odoo.com>
2018-03-16 18:02:55 +0100

    [ADD] sale@saas~11.2

---
+++ migrations/sale/saas~11.2.1.1/pre-migrate.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    byebye = util.splitlines("""
+        mail_template_data_notification_email_sale_order
+        filter_sale_report_sales_funnel
+        filter_sale_report_salespersons
+        filter_sale_report_salesteam
+        filter_isale_report_product
+    """)
+    for record in byebye:
+        util.remove_record(cr, 'sale.' + record)
+
+    util.remove_field(cr, 'sale.order.line', 'qty_delivered_updateable')
+    util.create_column(cr, 'sale_order_line', 'is_expense', 'boolean')
+    util.create_column(cr, 'sale_order_line', 'qty_delivered_manual', 'numeric')
+    util.create_column(cr, 'sale_order_line', 'qty_delivered_method', 'varchar')
+
+    with_sale_stock = '' if util.module_installed(cr, 'sale_stock') else '--'
+    with_sale_timesheet = '' if util.module_installed(cr, 'sale_timesheet') else '--'
+
+    cr.execute("""
+    WITH cte AS (
+        SELECT l.id, (t.expense_policy != 'no' and count(a.id) != 0) as is_expense
+          FROM sale_order_line l
+          JOIN product_product p ON (p.id = l.product_id)
+          JOIN product_template t ON (t.id = p.product_tmpl_id)
+     LEFT JOIN account_analytic_line a ON (a.so_line = l.id)
+      GROUP BY l.id, t.expense_policy
+    )
+        UPDATE sale_order_line l
+           SET is_expense = cte.is_expense
+          FROM cte
+         WHERE l.id = cte.id
+    """)
+
+    cr.execute("""
+        UPDATE sale_order_line l
+           SET qty_delivered_manual = 0,
+               qty_delivered_method = CASE WHEN l.is_expense THEN 'analytic'
+                         {with_sale_stock} WHEN t.type IN ('consu', 'product') THEN 'stock_move'
+                     {with_sale_timesheet} WHEN t.type = 'service' AND t.service_type = 'timesheet' THEN 'timesheet'
+                                           ELSE 'manual'
+                                       END
+          FROM product_product p
+          JOIN product_template t ON (t.id = p.product_tmpl_id)
+         WHERE p.id = l.product_id
+    """.format(**locals()))     # poor man's PEP498
