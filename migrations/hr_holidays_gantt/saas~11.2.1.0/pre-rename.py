1e7b676e094195e37ae3dd5599936517a64560df
Christophe Simonis <chs@odoo.com>
2018-03-19 17:27:55 +0100

    [ADD] hr_holidays_gantt@saas~11.2

---
+++ migrations/hr_holidays_gantt/saas~11.2.1.0/pre-rename.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    eb = util.expand_braces
+    util.rename_xmlid(cr, *eb('hr_holidays_gantt.hr_{holidays,leave}_gantt_view'))
