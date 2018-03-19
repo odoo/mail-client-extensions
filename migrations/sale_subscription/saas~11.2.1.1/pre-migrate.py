cd0d0243c8f29385b57a897bb006ed9fb0d447d0
Christophe Simonis <chs@odoo.com>
2018-03-19 18:49:27 +0100

    [ADD] sale_subscription@saas~11.2

---
+++ migrations/sale_subscription/saas~11.2.1.1/pre-migrate.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    util.create_column(cr, 'sale_subscription_line', 'price_subtotal', 'numeric')
