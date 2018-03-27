e80469a7bf17e4ee5f8b2e700f0c63d44933a813
Christophe Simonis <chs@odoo.com>
2018-03-27 12:18:05 +0200

    [FIX] account_report_followup@9.0: model cleaning

---
+++ migrations/account_reports_followup/9.0.1.0/pre-20-models.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.rename_field(cr, 'account_followup.followup', 'followup_line', 'followup_line_ids')
+    util.remove_field(cr, 'account_followup.followup.line', 'email_template_id')
+
+    gone = util.splitlines("""
+        latest_followup_date
+        latest_followup_level_id
+        latest_followup_level_id_without_lit
+
+        payment_amount_due
+        payment_amount_overdue
+        payment_earliest_due_date
+    """)
+    for f in gone:
+        util.remove_field(cr, 'res.partner', f)
+
+    util.remove_model(cr, 'account_followup.stat')
+    util.remove_model(cr, 'account_followup.stat.by.partner')
+    util.remove_model(cr, 'account_followup.sending.results')
+    util.remove_model(cr, 'account_followup.print')
+    util.remove_model(cr, 'report.account_followup.report_followup')
