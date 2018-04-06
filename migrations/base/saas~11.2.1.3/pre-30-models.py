6dc9fe605a3fb6d9b8fb021cba62bd103f60c226
Christophe Simonis <chs@odoo.com>
2018-04-06 11:44:04 +0200

    [IMP] base@saas~11.2: move model to `base`

--- migrations/base/saas~11.2.1.3/pre-30-models.py
+++ migrations/base/saas~11.2.1.3/pre-30-models.py
@@ -4,3 +4,11 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.remove_field(cr, 'res.partner', 'commercial_partner_country_id')
     util.remove_field(cr, 'res.group', 'is_portal')
+
+    util.move_model(cr, 'base.partner.merge.line', 'crm', 'base')
+    util.move_model(cr, 'base.partner.merge.automatic.wizard', 'crm', 'base')
+
+    eb = util.expand_braces
+    util.rename_xmlid(cr, *eb('{crm,base}.action_partner_deduplicate'))
+    util.rename_xmlid(cr, *eb('{crm,base}.base_partner_merge_automatic_wizard_form'))
+    util.rename_xmlid(cr, *eb('{crm,base}.action_partner_merge'))
