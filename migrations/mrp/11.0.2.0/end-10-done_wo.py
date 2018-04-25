aa543cca4f6c0380eaa0bb20980a733e3b434d69
Josse Colpaert <jco@odoo.com>
2018-04-25 14:05:11 +0200

    [FIX] mrp: add the production_id on the stock.move.line in all cases
    Before it was not set mostly in migration and it is in handy e.g. in
    the traceability report.
    
    opw-1816938

--- migrations/mrp/11.0.2.0/end-10-done_wo.py
+++ migrations/mrp/11.0.2.0/end-10-done_wo.py
@@ -4,3 +4,8 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # Set done_wo fields
     cr.execute("""UPDATE stock_move_line ml SET done_wo='t' WHERE done_wo IS NULL""")
+    cr.execute("""UPDATE stock_move_line ml SET production_id = CASE WHEN m.raw_material_production_id IS NOT NULL
+                                                                    THEN m.raw_material_production_id
+                                                                    ELSE m.production_id END
+                    FROM stock_move m WHERE ml.move_id = m.id AND ml.production_id IS NULL
+                        AND (m.production_id IS NOT NULL OR m.raw_material_production_id IS NOT NULL)""")
