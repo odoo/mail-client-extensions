5e6807b09869d27e3a58b7aaec147c3cb714c1eb
Christophe Simonis <chs@odoo.com>
2018-05-02 10:30:04 +0200

    [FIX] mail_push@saas~17: rename more ICP

--- migrations/mail_push/10.saas~17.1.0/pre-settings.py
+++ migrations/mail_push/10.saas~17.1.0/pre-settings.py
@@ -4,8 +4,12 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     eb = util.expand_braces
     util.rename_field(cr, 'base.config.settings', *eb('{default_,}mail_push_notification'))
-    cr.execute("""
-        UPDATE ir_config_parameter
-           SET key='mail_push.mail_push_notification'
-         WHERE key='mail_push.default_mail_push_notification'
+
+    renames = util.splitlines("""
+        mail_push.{default_,}mail_push_notification
+        {,mail_push.}fcm_api_key
+        {,mail_push.}fcm_project_id
     """)
+    for r in renames:
+        from_, to = eb(r)
+        cr.execute("UPDATE ir_config_parameter SET key=%s WHERE key=%s", [to, from_])
