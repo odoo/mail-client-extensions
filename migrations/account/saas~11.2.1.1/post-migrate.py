db1eabf0b982930203812b5e0d6cae0870ebd7d4
Christophe Simonis <chs@odoo.com>
2018-05-03 19:08:36 +0200

    [FIX] account@saas~11.2: force group directly. Avoid using settings.
    Using settings may have unwanted side effects.

--- migrations/account/saas~11.2.1.1/post-migrate.py
+++ migrations/account/saas~11.2.1.1/post-migrate.py
@@ -2,6 +2,6 @@
 from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.env(cr)['res.config.settings'].create({
-        'group_analytic_tags': True,
-    }).execute()
+    util.env(cr).ref('base.group_user').write({
+        'implied_ids': [(4, util.ref('analytic.group_analytic_tags'))],
+    })
