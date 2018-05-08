abfb6cf3348071c46b5d6bbab5d856bf93df051a
Christophe Simonis <chs@odoo.com>
2018-05-08 11:14:20 +0200

    [FIX] account@saas~11.2: missing argument

--- migrations/account/saas~11.2.1.1/post-migrate.py
+++ migrations/account/saas~11.2.1.1/post-migrate.py
@@ -3,5 +3,5 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.env(cr).ref('base.group_user').write({
-        'implied_ids': [(4, util.ref('analytic.group_analytic_tags'))],
+        'implied_ids': [(4, util.ref(cr, 'analytic.group_analytic_tags'))],
     })
