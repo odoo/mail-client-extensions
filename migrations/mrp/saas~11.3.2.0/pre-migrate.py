081410e238477898fc5fca1c13e5cbc33697fec9
Christophe Simonis <chs@odoo.com>
2018-05-18 13:17:09 +0200

    [ADD] mrp@saas~11.3

---
+++ migrations/mrp/saas~11.3.2.0/pre-migrate.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "mrp.mrp_bom_cost_report")
+    util.remove_record(cr, "mrp.action_report_bom_price")
+    util.remove_model(cr, "report.mrp.mrp_bom_cost_report")
+    util.remove_view(cr, "mrp.mrp_bom_structure_report")
+    util.remove_record(cr, "mrp.action_report_bom_structure")
+    util.remove_model(cr, "report.mrp.mrp_bom_structure_report")
+
+    util.remove_view(cr, "mrp.mrp_bom_component_tree_view")
+    util.remove_view(cr, "mrp.view_mrp_bom_line_filter")
+
+    eb = util.expand_braces
+    util.rename_xmlid(cr, *eb("mrp.access_{product,uom}_uom_user"))
+    util.rename_xmlid(cr, "mrp.access_product_puom_categ", "mrp.access_uom_category")
+    util.rename_xmlid(cr, *eb("mrp.access_{product_uom_categ,uom_category}_mrp_manager"))
+    util.rename_xmlid(cr, *eb("mrp.access_{product,uom}_uom_mrp_manager"))
+
+    util.rename_xmlid(cr, *eb("mrp.production_work{center,order}_tree_view_inherit"))
+    util.rename_xmlid(cr, *eb("mrp.production_work{center,order}_form_view_inherit"))
+    util.rename_xmlid(cr, *eb("mrp.production_work{center,order}_form_view_filter"))
