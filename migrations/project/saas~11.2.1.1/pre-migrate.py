9d3bfaaf980b2943aeb330b2ca1c79b0134388e1
Christophe Simonis <chs@odoo.com>
2018-05-22 14:14:44 +0200

    [FIX] project@saas~11.2: `subtask_project_id` is back
    See odoo/odoo@b709e22fa2911b7a2ffd8efda7e00845a887a03d

--- migrations/project/saas~11.2.1.1/pre-migrate.py
+++ migrations/project/saas~11.2.1.1/pre-migrate.py
@@ -4,6 +4,3 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.remove_model(cr, 'project.task.merge.wizard')
     util.remove_view(cr, 'project.mail_template_task_merge')
-
-    util.remove_field(cr, 'project.project', 'subtask_project_id')
-    util.remove_field(cr, 'project.task', 'subtask_project_id')
