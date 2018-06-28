188009ee62877be644d4adf61f381524c3de871e
Christophe Simonis <chs@odoo.com>
2018-06-28 18:12:29 +0200

    [FIX] {mrp,mrp_plm}@10.saas~16: correctly migrate mrp.document

---
+++ migrations/mrp/10.saas~16.2.0/end-migrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "ir.attachment", "priority")
--- migrations/mrp_plm/10.saas~16.1.0/pre-migrate.py
+++ migrations/mrp_plm/10.saas~16.1.0/pre-migrate.py
@@ -12,3 +12,27 @@ def migrate(cr, version):
     util.remove_field(cr, 'mrp.eco.bom.change', 'product_uom_id')
 
     util.remove_view(cr, 'mrp_plm.mrp_eco_tag_view_form')
+
+    # create mrp.document
+    cr.execute("""
+        INSERT INTO mrp_document(ir_attachment_id, priority, active)
+             SELECT id, priority, true
+               FROM ir_attachment
+              WHERE model = 'mrp.eco'
+    """)
+    cr.execute("ALTERT TABLE mrp_eco DROP CONSTRAINT mrp_eco_displayed_image_id_fkey")
+    cr.execute("""
+        WITH _up AS (
+            UPDATE mrp_eco e
+               SET diplayed_image_id = d.id
+              FROM mrp_document d
+             WHERE d.ir_attachment_id = e.displayed_image_id
+         RETURNING e.id
+        )
+        UPDATE mrp_eco
+           SET displayed_image_id = NULL
+         WHERE id NOT IN (SELECT id FROM _up)
+    """)
+
+    util.rename_field(cr, 'mrp.eco', 'attachment_count', 'mrp_document_count')
+    util.rename_field(cr, 'mrp.eco', 'attachment_ids', 'mrp_document_ids')
