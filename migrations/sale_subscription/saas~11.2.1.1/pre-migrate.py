f7de97d652f020f87174a566244f419630d642b7
Christophe Simonis <chs@odoo.com>
2018-07-17 14:21:26 +0200

    [FIX] sale_subscription@saas~11.2: force non update of mail template

--- migrations/sale_subscription/saas~11.2.1.1/pre-migrate.py
+++ migrations/sale_subscription/saas~11.2.1.1/pre-migrate.py
@@ -3,3 +3,6 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.create_column(cr, 'sale_subscription_line', 'price_subtotal', 'numeric')
+
+    # template is not correct in data, keep actual
+    util.force_noupdate(cr, 'sale_subscription.email_subscription_open', True)
