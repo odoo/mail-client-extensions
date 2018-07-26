3352ac5fda3f3149bfe34f67768c65158553bcdd
Christophe Simonis <chs@odoo.com>
2018-07-26 19:39:40 +0200

    [ADD] mrp@saas~11.4

---
+++ migrations/mrp/saas~11.4.2.0/pre-migrate.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    # model `mrp.message` still there?
+    views = ["view_mrp_message_kanban"] + [
+        "mrp_message_view_%s" % suffix
+        for suffix in {"form", "form_embedded_product", "form_embedded_bom", "tree", "search"}
+    ]
+    for v in views:
+        util.remove_view(cr, "mrp." + v)
+
+    util.remove_record(cr, "mrp.mrp_message_action_main")
+    util.remove_menus(cr, [util.ref(cr, "mrp.message_menu")])
