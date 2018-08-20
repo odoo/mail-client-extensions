b5b78d1eb6624e5c4434be80617b48fea1f6ce08
Christophe Simonis <chs@odoo.com>
2018-08-20 17:10:48 +0200

    [ADD] base@saas~11.4: module changes (EE)

--- migrations/base/saas~11.4.1.3/pre-10-modules.py
+++ migrations/base/saas~11.4.1.3/pre-10-modules.py
@@ -15,4 +15,14 @@ def migrate(cr, version):
 
     util.module_deps_diff(cr, "website_quote", minus={"payment"})
 
+    if util.has_enterprise():
+        util.new_module(cr, "delivery_easypost", deps={"delivery", "mail"})
+        util.new_module(cr, "web_cohort", deps={"web"}, auto_install=True)
+
+        util.module_deps_diff(cr, "inter_company_rules", plus={"purchase_stock"}, minus={"purchase"})
+        util.module_deps_diff(cr, "mrp_workorder", plus={"barcodes"})
+        util.module_deps_diff(cr, "quality_mrp_workorder", plus={"barcodes"})
+        util.module_deps_diff(cr, "sale_subscription", plus={"web_cohort"})
+        util.module_deps_diff(cr, "stock_barcode", plus={"web_tour"})
+
     util.remove_module(cr, "auth_crypt")
