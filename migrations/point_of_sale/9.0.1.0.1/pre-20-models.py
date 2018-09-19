76a4285d6c8173d63322066cccfdaf3513a31b44
Christophe Simonis <chs@odoo.com>
2018-09-19 14:19:37 +0200

    [FIX] point_of_sale@9.0: set cash_control

---
+++ migrations/point_of_sale/9.0.1.0.1/pre-20-models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "pos.config", "cash_control", "boolean")
+    cr.execute("UPDATE pos_config SET cash_control=true")
