fb478bed9d359504cc6b58b068259d77cab61c35
Nicolas Seinlet <nse@odoo.com>
2018-09-20 18:56:29 +0530

    [IMP] stock@9.saas~12: speedup
    Disable indices on tables when performing mass updates

--- migrations/stock/9.saas~12.1.1/pre-10-models.py
+++ migrations/stock/9.saas~12.1.1/pre-10-models.py
@@ -2,19 +2,22 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.create_column(cr, 'stock_move', 'scrapped', 'boolean')
-    cr.execute("""
-        UPDATE stock_move m
-           SET scrapped = l.scrap_location
-          FROM stock_location l
-         WHERE l.id = m.location_dest_id
-    """)
+    #Disable indices makes this running in 27 Minutes instead of 36.
+    with util.disabled_index_on(cr, 'stock_move'):
+        util.create_column(cr, 'stock_move', 'scrapped', 'boolean')
+        cr.execute("""
+            UPDATE stock_move m
+               SET scrapped = l.scrap_location
+              FROM stock_location l
+             WHERE l.id = m.location_dest_id
+        """)
 
-    util.create_column(cr, 'stock_production_lot', 'product_uom_id', 'int4')
-    cr.execute("""
-        UPDATE stock_production_lot l
-           SET product_uom_id = t.uom_id
-          FROM product_product p, product_template t
-         WHERE p.id = l.product_id
-           AND t.id = p.product_tmpl_id
-    """)
+    with util.disabled_index_on(cr, 'stock_production_lot'):
+        util.create_column(cr, 'stock_production_lot', 'product_uom_id', 'int4')
+        cr.execute("""
+            UPDATE stock_production_lot l
+               SET product_uom_id = t.uom_id
+              FROM product_product p, product_template t
+             WHERE p.id = l.product_id
+               AND t.id = p.product_tmpl_id
+        """)
