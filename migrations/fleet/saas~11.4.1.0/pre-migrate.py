06f31c13a440139b85fb058df9193be2b819c342
Nicolas Seinlet <nse@odoo.com>
2018-09-27 10:44:57 +0200

    [FIX] Fleet: version is 0.1 not 1.0

--- migrations/fleet/saas~11.4.1.0/pre-migrate.py
+++ migrations/fleet/saas~11.4.1.0/pre-migrate.py
@@ -5,4 +5,4 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.create_column(cr, "fleet_vehicle", "first_contract_date", "date")
 
-    cr.execute("ALTER TABLE fleet_vehicle DROP CONSTRAINT driver_id_unique")
+    cr.execute("ALTER TABLE fleet_vehicle DROP CONSTRAINT IF EXISTS driver_id_unique")
