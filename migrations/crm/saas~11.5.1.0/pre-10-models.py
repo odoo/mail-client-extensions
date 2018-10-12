cd5cb75ff214469d6913d22cdd4c3a9f14de06b6
Christophe Simonis <chs@odoo.com>
2018-10-12 15:02:43 +0200

    [ADD] crm@saas~11.5
    Co-authored-by: Nicolas Seinlet <nse@odoo.com>

---
+++ migrations/crm/saas~11.5.1.0/pre-10-models.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "res.config.settings", "module_web_clearbit")
---
+++ migrations/crm/saas~11.5.1.0/pre-30-blacklist.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+        INSERT INTO mail_blacklist (email, active)
+             SELECT lower(substring(email_from, '([^ ,;<@]+@[^> ,;]+)')), true
+               FROM crm_lead
+              WHERE opt_out = true
+                AND coalesce(substring(email_from, '([^ ,;<@]+@[^> ,;]+)'), '') != ''
+        ON CONFLICT DO NOTHING
+    """
+    )
+    util.remove_field(cr, "crm.lead", "opt_out")
