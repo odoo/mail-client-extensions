4717ae1d7f192f7158d02bfd2d623b87bdc34668
Christophe Simonis <chs@odoo.com>
2018-10-12 15:08:09 +0200

    [ADD] ocn_client@saas~11.5
    Co-authored-by: Nicolas Seinlet <nse@odoo.com>

---
+++ migrations/ocn_client/saas~11.5.1.0/end-migrate.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+from odoo.tools.misc import str2bool
+from odoo.addons.iap import jsonrpc
+
+
+def migrate(cr, version):
+    cr.execute("DELETE FROM ir_config_parameter WHERE key LIKE 'mail_push.fcm_%'")
+    cr.execute("DELETE FROM ir_config_parameter WHERE key='mail_push.mail_push_notification' RETURNING value")
+    [value] = cr.fetchone() or ["False"]
+    if str2bool(value, default=False):
+        RCS = util.env(cr)["res.config.settings"]
+        RCS.create({"ocn_push_notification": True}).excecute()
+
+        endpoint = RCS._get_endpoint()
+        ocnuuid = RCS._get_ocn_uuid()
+
+        # re-register devices
+        def iap(pid, pname, device, subscription):
+            params = {
+                "ocn_uuid": ocnuuid,
+                "user_name": pname,
+                "user_login": str(pid),
+                "device_name": device,
+                "device_key": subscription,
+            }
+            return jsonrpc(endpoint + "/iap/ocn/register_device", params=params)
+
+        cr.execute("""
+            SELECT p.id, p.name,
+                   d.name, d.subscription_id
+              FROM mail_push_device d
+              JOIN res_partner p ON p.id = d.partner_id
+             WHERE d.service_type = 'fcm'
+          ORDER BY d.id
+        """)
+        for pid, pname, device, subscription_id in cr.fetchall():
+            token = iap(pid, pname, device, subscription_id)
+            if token:
+                cr.execute("UPDATE res_partner SET ocn_token = %s WHERE id = %s", [token, pid])
+
+    util.remove_model(cr, "mail_push.device")
