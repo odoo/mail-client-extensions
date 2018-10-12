e937e408c4cd62d1d905f9f578568b623f114471
Christophe Simonis <chs@odoo.com>
2018-10-12 15:08:13 +0200

    [ADD] portal@saas~11.5
    Co-authored-by: Nicolas Seinlet <nse@odoo.com>

---
+++ migrations/portal/saas~11.5.1.0/pre-migrate.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "portal.my_account_link")
+    util.remove_view(cr, "portal.portal_my_home")
+    util.remove_record_if_unchanged(cr, "portal.mail_template_data_portal_welcome")
+
+    models = {"account.invoice", "project.project", "project.task", "purchase.order", "sale.order", "helpdesk.ticket"}
+    for model in models:
+        util.rename_field(cr, model, "portal_url", "access_url")
