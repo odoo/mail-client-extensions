263355fab04ca354a2e0ebbe9daf5e32f6e382d4
Christophe Simonis <chs@odoo.com>
2018-10-12 15:18:30 +0200

    [ADD] website_slides@saas~11.5
    Co-authored-by: Nicolas Seinlet <nse@odoo.com>

---
+++ migrations/website_slides/saas~11.5.1.0/pre-migrate.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "res.config.settings", "website_slide_google_app_key")
+    util.create_column(cr, "website", "website_slide_google_app_key", "varchar")
+    util.create_column(cr, "slide_channel", "website_id", "int4")
+
+    ICP = util.env(cr)["ir.config_parameter"]
+    param_value = ICP.get_param("website_slides.google_app_key")
+    if param_value:
+        cr.execute("UPDATE website set website_slide_google_app_key=%s", (param_value,))
+    util.remove_record(cr, "website_slides.google_app_key")
+
+    util.remove_record_if_unchanged(cr, "website_slides.slide_template_published")
+    util.remove_record_if_unchanged(cr, "website_slides.slide_template_shared")
