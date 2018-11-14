50442892f5d12cc09142706616d4c65a96ded3f5
Christophe Simonis <chs@odoo.com>
2018-11-14 10:16:28 +0100

    [FIX] base@saas~11.5: only fill column if we just create it, else trust its value

--- migrations/base/saas~11.5.1.3/pre-40-models.py
+++ migrations/base/saas~11.5.1.3/pre-40-models.py
@@ -7,18 +7,17 @@ def migrate(cr, version):
 
     util.rename_field(cr, "ir.model.fields", "copy", "copied")
 
-    util.create_column(cr, "ir_attachment", "res_model_name", "varchar")
-    util.create_column(cr, "ir_attachment", "active", "boolean")
-    cr.execute(
+    if util.create_column(cr, "ir_attachment", "res_model_name", "varchar"):
+        cr.execute(
+            """
+            UPDATE ir_attachment a
+               SET res_model_name = m.name
+              FROM ir_model m
+             WHERE m.model = a.res_model
         """
-        UPDATE ir_attachment a
-           SET res_model_name = m.name
-          FROM ir_model m
-         WHERE m.model = a.res_model
-           AND a.res_model_name IS NULL
-    """
-    )
-    cr.execute("UPDATE ir_attachment SET active=true WHERE active IS NULL")
+        )
+    if util.create_column(cr, "ir_attachment", "active", "boolean"):
+        cr.execute("UPDATE ir_attachment SET active=true")
 
     util.create_column(cr, "ir_module_module", "to_buy", "boolean")
     cr.execute("UPDATE ir_module_module SET to_buy=false")
