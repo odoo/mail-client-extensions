8f79451d248a0f9e5733cb421fc6840a76437a21
Christophe Simonis <chs@odoo.com>
2018-11-26 16:23:24 +0100

    [FIX] hr_holidays@saas~11.5: handle versions < saas~11.3

--- migrations/hr_holidays/saas~11.5.1.5/pre-migrate.py
+++ migrations/hr_holidays/saas~11.5.1.5/pre-migrate.py
@@ -5,10 +5,12 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     for model in {"hr.leave", "hr.leave.allocation"}:
         table = util.table_of_model(cr, model)
+        if not util.table_exists(cr, table):
+            continue
         util.rename_field(cr, model, "number_of_days", "number_of_days_display")
         util.rename_field(cr, model, "number_of_days_temp", "number_of_days")
         util.rename_field(cr, model, "number_of_hours", "number_of_hours_display")
-        util.remove_column(cr, model, "number_of_days_display")
+        util.remove_column(cr, table, "number_of_days_display")
 
         util.create_column(cr, table, "mode_company_id", "int4")
 
@@ -30,11 +32,12 @@ def migrate(cr, version):
                                       ELSE 'fixed'
                                       END
     """)
-    cr.execute("""
-        UPDATE hr_leave_type
-           SET request_unit='day'
-         WHERE request_unit='half'
-    """)
+    if util.column_exists(cr, "hr_leave_type", "request_unit"):
+        cr.execute("""
+            UPDATE hr_leave_type
+               SET request_unit='day'
+             WHERE request_unit='half'
+        """)
 
     for field in {"limit", "employee_applicability", "accrual", "negative", "balance_limit"}:
         util.remove_field(cr, "hr.leave.type", field)
