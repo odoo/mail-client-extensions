b27fd52c0ca44773e98a5ab8a6263dd95779cfd1
Christophe Simonis <chs@odoo.com>
2018-11-29 12:04:47 +0100

    [FIX] l10n_ch@saas~11.2: keep old tax groups and tags
    closes #302

--- migrations/l10n_ch/saas~11.2.10.0/pre-migrate.py
+++ migrations/l10n_ch/saas~11.2.10.0/pre-migrate.py
@@ -9,7 +9,7 @@ def migrate(cr, version):
     util.remove_record(cr, 'l10n_ch.fiscal_position_tax_template_12')
     util.remove_record(cr, 'l10n_ch.fiscal_position_tax_template_16')
     util.remove_record(cr, 'l10n_ch.fiscal_position_tax_template_18')
-    
+
     util.remove_record(cr, 'l10n_ch.vat_38')
     util.remove_record(cr, 'l10n_ch.vat_38_incl')
     util.remove_record(cr, 'l10n_ch.vat_38_purchase')
@@ -22,11 +22,12 @@ def migrate(cr, version):
     util.remove_record(cr, 'l10n_ch.vat_80_purchase_incl')
     util.remove_record(cr, 'l10n_ch.vat_80_invest')
     util.remove_record(cr, 'l10n_ch.vat_80_invest_incl')
-    
-    util.remove_record(cr, 'l10n_ch.tax_group_tva_38')
-    util.remove_record(cr, 'l10n_ch.tax_group_tva_8')
 
-    util.remove_record(cr, 'l10n_ch.vat_tag_301_a')
-    util.remove_record(cr, 'l10n_ch.vat_tag_341_a')
-    util.remove_record(cr, 'l10n_ch.vat_tag_301_b')
-    util.remove_record(cr, 'l10n_ch.vat_tag_341_b')
+    # keep old tax groups and tags
+    util.force_noupdate(cr, 'l10n_ch.tax_group_tva_38', True)
+    util.force_noupdate(cr, 'l10n_ch.tax_group_tva_8', True)
+
+    util.force_noupdate(cr, 'l10n_ch.vat_tag_301_a', True)
+    util.force_noupdate(cr, 'l10n_ch.vat_tag_341_a', True)
+    util.force_noupdate(cr, 'l10n_ch.vat_tag_301_b', True)
+    util.force_noupdate(cr, 'l10n_ch.vat_tag_341_b', True)
