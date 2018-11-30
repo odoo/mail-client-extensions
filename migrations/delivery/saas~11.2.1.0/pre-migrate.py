72613ec5f4ed22cecd58991d489ec55338c92b2d
Yesha <ysh@openerp.com>
2018-11-30 05:56:37 +0100

    [FIX]deliery@saas~11.2 fix sleepydeveloper error

--- migrations/delivery/saas~11.2.1.0/pre-migrate.py
+++ migrations/delivery/saas~11.2.1.0/pre-migrate.py
@@ -5,5 +5,5 @@ def migrate(cr, version):
     util.remove_record(cr, 'delivery.mail_template_data_delivery_notification')
 
     util.remove_field(cr, 'stock.move', 'weight_uom_id')
-    util.remove_column(cr, 'stock.picking', 'weight_uom_id')    # column still exists (non stored)
+    util.remove_column(cr, 'stock_picking', 'weight_uom_id')    # column still exists (non stored)
     util.remove_field(cr, 'stock.picking', 'number_of_packages')
