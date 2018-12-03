1a0de2500558d9f67cd4f334595fcb59af65e33f
Christophe Simonis <chs@odoo.com>
2018-12-03 10:26:55 +0100

    [FIX] l10n_be_invoice_bba@saas~11.5: keep email template
    We can't actually force update of this record by removing it because
    it's belong to another module.
    
    Closes #314

--- migrations/l10n_be_invoice_bba/saas~11.5.1.0/pre-20-email-templates.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo.addons.base.maintenance.migrations import util
-
-
-def migrate(cr, version):
-    util.remove_record_if_unchanged(cr, "account.email_template_edi_invoice")
