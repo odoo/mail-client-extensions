a5ef4be4ecc1aa30f038843687501c6d97e55e0e
Sanjay Jamod <sja@odoo.com>
2018-12-03 18:13:28 +0530

    [FIX] gamification@saas~11.5: fix SleepyDeveloperError developer please stay awake while codding :p

--- migrations/gamification/saas~11.5.1.0/pre-migrate.py
+++ migrations/gamification/saas~11.5.1.0/pre-migrate.py
@@ -3,4 +3,4 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    util.remove_column(cr, "gamification.badge.user.wizard", "user_id")  # field still there
+    util.remove_column(cr, "gamification_badge_user_wizard", "user_id")  # field still there
