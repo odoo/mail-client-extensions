a93aa0d7e31a4abe9f02b36bb56f4608b53bbd8f
Yesha <ysh@openerp.com>
2018-12-04 06:12:03 +0100

    [FIX] website_sale@saas~11.5: correct table name

--- migrations/website_sale/saas~11.5.1.0/post-10-migrate.py
+++ migrations/website_sale/saas~11.5.1.0/post-10-migrate.py
@@ -5,6 +5,6 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     if util.ENVIRON.get("S115_default_cart_recovery_template"):
         cr.execute(
-            "UPDATE res_company SET cart_recovery_mail_template_id=%s",
+            "UPDATE website SET cart_recovery_mail_template_id=%s",
             [util.ref(cr, "website_sale.mail_template_sale_cart_recovery")],
         )
