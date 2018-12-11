fa090f3419c0368414e04a6b9ccb89be45c33891
Christophe Simonis <chs@odoo.com>
2018-12-11 14:57:06 +0100

    [FIX] sale_management@saas~11.5: set value for required fields
    Closes #326

--- migrations/sale_management/saas~11.5.1.0/post-10-recompute.py
+++ migrations/sale_management/saas~11.5.1.0/post-10-recompute.py
@@ -9,8 +9,8 @@ def migrate(cr, version):
         cr.execute(
             """
             INSERT INTO sale_order_template_line
-                (sale_order_template_id, layout_category_id, sequence, display_type, name)
-                SELECT s.sale_order_template_id, s.layout_category_id, min(s.sequence)-5, 'line_section', l.name
+                (sale_order_template_id, layout_category_id, sequence, display_type, name, price_unit, product_uom_qty)
+                SELECT s.sale_order_template_id, s.layout_category_id, min(s.sequence)-5, 'line_section', l.name, 0, 0
                 FROM sale_order_template_line s INNER JOIN sale_layout_category l on s.layout_category_id=l.id
                 GROUP BY s.sale_order_template_id, s.layout_category_id, l.name
                 ORDER BY s.sale_order_template_id, s.layout_category_id
