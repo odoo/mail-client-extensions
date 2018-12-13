fb1dac39982e46251cecaa2ddb63d7b16cdbea20
Christophe Simonis <chs@odoo.com>
2018-12-13 08:51:52 +0100

    [ADD] fetchmail@saas~12.1

---
+++ migrations/fetchmail/saas~12.1.1.0/pre-migrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.rename_field(cr, "fetchmail.server", "type", "server_type")
