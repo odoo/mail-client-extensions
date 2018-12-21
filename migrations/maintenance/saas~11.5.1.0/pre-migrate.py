c947e49a47b0fd20ad033bc4359b3d1100499914
Atul Patel <atp@openerp.com>
2018-12-21 08:04:50 +0100

    [FIX][maintenance]: no need to rename field as oldname attribute do that

--- migrations/maintenance/saas~11.5.1.0/pre-migrate.py
+++ migrations/maintenance/saas~11.5.1.0/pre-migrate.py
@@ -6,4 +6,3 @@ def migrate(cr, version):
     util.create_column(cr, "maintenance_equipment", "effective_date", "date")
 
     cr.execute("UPDATE maintenance_equipment set effective_date=create_date::date")
-    util.rename_field(cr, "maintenance.request", "technician_user_id", "user_id")
