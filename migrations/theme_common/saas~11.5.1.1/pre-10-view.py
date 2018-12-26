7e1c725c7e10f179ccafd0dbd40b208a5cff186f
Damien Bouvy <dbo@odoo.com>
2018-12-26 13:44:33 +0100

    [FIX] theme_common@saas~11.5: drop views

---
+++ migrations/theme_common/saas~11.5.1.1/pre-10-view.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+
+    views = util.splitlines("""
+        theme_common.assets_frontend
+        theme_common.compatibility-saas-10-1
+        theme_common.compatibility-saas-10-2
+    """)
+
+    for v in views:
+        util.remove_view(cr, v)
+
+    # nearly all options have changed structure, but some were simply removed and listing all of them is gonna take ages
+    # delete them all and let the update do the rest
+    cr.execute("SELECT module,name FROM ir_model_data WHERE model='ir.ui.view' AND module like '%theme%' and name like 'option_%'")
+    for v in cr.dictfetchall():
+        util.remove_view(cr, '.'.join([v['module'], v['name']]))
