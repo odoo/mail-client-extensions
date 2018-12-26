d2deaded491c80d8ea6d54d3ba5d987a3ebe127f
Nicolas Seinlet <nse@odoo.com>
2018-12-26 11:47:23 +0100

    [ADD] stock_account@saas~12.1

---
+++ migrations/stock_account/saas~12.1.1.1/pre-10-models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    # Columns needed on stock_move to support FIFO
+    util.remove_field(cr, 'product.template', 'property_valuation')
+    util.remove_field(cr, 'product.template', 'valuation')
+    util.remove_field(cr, 'product.template', 'property_cost_method')
+    util.remove_field(cr, 'product.template', 'cost_method')
+    util.remove_field(cr, 'product.template', 'property_stock_account_input')
+    util.remove_field(cr, 'product.template', 'property_stock_account_output')
+
+    util.remove_view(cr, "stock_account.view_template_property_form")
