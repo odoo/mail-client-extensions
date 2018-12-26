8e084caa53de0aa9f35dbbe3ad73b22d2a47c7d3
Damien Bouvy <dbo@odoo.com>
2018-12-26 19:16:10 +0100

    [IMP] purchase@saas~11.5: some things can't be computed
    If the uoms of the purchase line and product template are not in the same category,
    then the recompute will fail. Given that having a mess of uom categs is pretty much
    a given for any database older than, I don't know, 2 months, let's be a bit preventive
    here.
    
    If the uoms are not compatible, we behave in the same way as `_compute_quantity`
    would if `raise_if_failure` were set to `False`: we just use the same quantity.
    This new field seems to be there for reporting only, and fixing all past entries because
    of uoms problems is not realistic anyway.

--- migrations/purchase/saas~11.5.1.1/post-10-recompute.py
+++ migrations/purchase/saas~11.5.1.1/post-10-recompute.py
@@ -11,6 +11,24 @@ def migrate(cr, version):
            AND t.id = p.product_tmpl_id
            AND t.uom_id = l.product_uom
     """)
+    # behave somewhat like purchase: if the uoms are not in the same category
+    # then we say that the quantity is the same (as if we had called _compute_quantity
+    # on the uom with raise_if_failure=False); otherwise it will raise a shitload of errors
+    # for data in the past for a field that seems only to be there for reporting purposes...
+    cr.execute("""
+        UPDATE purchase_order_line l
+           SET product_uom_qty = l.product_qty
+          FROM product_product p, product_template t,
+               uom_uom u1, uom_uom u2
+         WHERE p.id = l.product_id
+           AND t.id = p.product_tmpl_id
+           AND t.uom_id != l.product_uom
+           AND u1.category_id != u2.category_id
+           AND u1.id = l.product_uom
+           AND u2.id = t.uom_id
+    """)
+    # lines with uom different than the one on the product template but in a compatible
+    # category: let the orm recompute
     cr.execute("SELECT id FROM purchase_order_line WHERE product_uom_qty IS NULL")
     ids = [l[0] for l in cr.fetchall()]
     util.recompute_fields(cr, "purchase.order.line", ["product_uom_qty"], ids=ids)
