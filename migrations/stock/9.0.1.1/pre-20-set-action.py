2256ce315d9873bf5a2b1feca1c8509480e3529e
Christophe Simonis <chs@odoo.com>
2019-01-04 12:34:14 +0100

    [FIX] stock@9.0: typo

--- migrations/stock/9.0.1.1/pre-20-set-action.py
+++ migrations/stock/9.0.1.1/pre-20-set-action.py
@@ -5,4 +5,4 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # force empty src_model; see #277
     cr.execute("UPDATE ir_act_window SET src_model = NULL WHERE id = %s",
-               [util.ref("stock.action_procurement_compute")])
+               [util.ref(cr, "stock.action_procurement_compute")])
