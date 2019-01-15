8044068d5a10c9d434efa5053620f24898de90ef
Christophe Simonis <chs@odoo.com>
2019-01-15 14:50:15 +0100

    [FIX] crm@10.saas~14: match known crm.activity when converting to mail.activity.type
    Closes #361

--- migrations/crm/10.saas~14.1.0/post-activities.py
+++ migrations/crm/10.saas~14.1.0/post-activities.py
@@ -1,17 +1,40 @@
 # -*- coding: utf-8 -*-
 from openerp.addons.base.maintenance.migrations import util
 
+
 def migrate(cr, version):
     # convert crm.activity to mail.activity.type
     util.create_column(cr, 'mail_activity_type', '_caid', 'int4')
+
+    id_map = {}
+    name_map = dict(util.expand_braces(l) for l in util.splitlines("""
+        {crm.crm,mail.mail}_activity_data_email
+        {crm.crm,mail.mail}_activity_data_call
+        {crm.crm,mail.mail}_activity_data_meeting
+
+        crm.{crm,mail}_activity_demo_followup_quote
+        crm.{crm,mail}_activity_demo_make_quote
+        crm.{crm,mail}_activity_demo_call_demo
+    """))
+    # __import__('pudb').set_trace()
+    for ca, mat in name_map.items():
+        caid = util.ref(cr, ca)
+        matid = util.ref(cr, mat)
+        if caid and matid:
+            id_map[caid] = matid
+            cr.execute("UPDATE mail_activity_type SET _caid = %s WHERE id = %s", [caid, matid])
+
+    util.replace_record_references_batch(cr, id_map, "crm.activity", "mail.activity.type", replace_xmlid=False)
+
     cr.execute("""
         INSERT INTO mail_activity_type(_caid, name, sequence, icon, summary, days, res_model_id)
              SELECT a.id, s.name, a.sequence, 'fa-tasks', s.description, a.days,
                     (SELECT id FROM ir_model WHERE model='crm.lead')
                FROM crm_activity a
                JOIN mail_message_subtype s ON (s.id = a.subtype_id)
+              WHERE a.id != ALL(%s)
           RETURNING _caid, id
-    """)
+    """, [list(id_map)])
 
     for caid, maid in cr.fetchall():
         util.replace_record_references(cr, ('crm.activity', caid), ('mail.activity.type', maid))
@@ -29,23 +52,13 @@ def migrate(cr, version):
                                     l.write_uid,
                                     l.create_uid,
                                     1),
-                    ma.res_model_id, l.id, 'crm.lead', l.name,
-                    l.title_action, COALESCE(l.date_action, l.write_date, l.create_date)
+                    (SELECT id FROM ir_model WHERE model='crm.lead'),
+                    l.id, 'crm.lead', l.name,
+                    l.title_action, COALESCE(l.activity_date_deadline, l.write_date, l.create_date)
                FROM crm_lead l
                JOIN mail_activity_type ma ON (ma._caid = l.next_activity_id)
                LEFT JOIN crm_team t ON (t.id = l.team_id)
     """)
-
-    util.rename_field(cr, 'crm.lead', 'date_action', 'activity_date_deadline')
-
-    fields_refs = {
-        'next_activity_id': 'activity_ids',
-        'date_action_next': 'activity_date_deadline',
-        'date_action_last': 'write_date',       # no better alternative...
-        'title_action': 'activity_summary',
-    }
-    for old, new in fields_refs.items():
-        util.update_field_references(cr, old, new, only_models=['crm.lead'])
 
     util.remove_column(cr, 'mail_activity_type', '_caid')
     util.delete_model(cr, 'crm.activity')
---
+++ migrations/crm/10.saas~14.1.0/pre-crm-lead.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.rename_field(cr, 'crm.lead', 'date_action', 'activity_date_deadline')
+
+    fields_refs = {
+        'next_activity_id': 'activity_ids',
+        'date_action_next': 'activity_date_deadline',
+        'date_action_last': 'write_date',       # no better alternative...
+        'title_action': 'activity_summary',
+    }
+    for old, new in fields_refs.items():
+        util.update_field_references(cr, old, new, only_models=['crm.lead'])
