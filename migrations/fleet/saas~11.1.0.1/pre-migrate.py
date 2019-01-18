b97dcab8b47a389f56de66eb99a7afa18c9a8098
Christophe Simonis <chs@odoo.com>
2019-01-18 16:58:28 +0100

    [FIX] fleet@saas~11.1: keep Q7 cars...
    Closes #365

--- migrations/fleet/saas~11.1.0.1/pre-migrate.py
+++ migrations/fleet/saas~11.1.0.1/pre-migrate.py
@@ -11,7 +11,7 @@ def migrate(cr, version):
     util.delete_unused(cr, 'fleet_vehicle_tag', ['fleet.vehicle_tag_%s' % t for t in tags.split()])
 
     models = 'corsa astra agila combotour meriva astragtc zafira zafiratourer insignia mokka antara ampera ' \
-             'a1 a3 a4 a5 a6 a7 a8 q3 q5 a7 tt ' \
+             'a1 a3 a4 a5 a6 a7 a8 q3 q5 q7 tt ' \
              'serie1 serie3 serie5 serie6 serie7 seriex seriez4 seriem seriehybrid ' \
              'classa classb classc classcl classcls classe classm classgl classglk classr classs classslk classsls'
     util.delete_unused(cr, 'fleet_vehicle_model', ['fleet.model_%s' % x for x in models.split()])
