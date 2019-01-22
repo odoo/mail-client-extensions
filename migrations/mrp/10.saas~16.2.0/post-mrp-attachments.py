34b699f2f94c452a7107add123317959ef3c0386
Sanjay Jamod <sja@odoo.com>
2019-01-22 11:19:38 +0530

    [FIX] mrp@saas~16.2: fix typo

--- migrations/mrp/10.saas~16.2.0/post-mrp-attachments.py
+++ migrations/mrp/10.saas~16.2.0/post-mrp-attachments.py
@@ -6,12 +6,12 @@ def migrate(cr, version):
         INSERT INTO mrp_document(ir_attachment_id, priority, active)
              SELECT a.id, a.priority, true
                FROM ir_attachment a, mrp_bom_line l
-              WHERE a.model = 'product.product'
+              WHERE a.res_model = 'product.product'
                 AND a.res_id = l.product_id
               UNION
              SELECT a.id, a.priority, true
                FROM ir_attachment a, mrp_bom_line l, product_product p
-              WHERE a.model = 'product.template'
+              WHERE a.res_model = 'product.template'
                 AND p.id = l.product_id
                 AND a.res_id = p.product_tmpl_id
     """)
