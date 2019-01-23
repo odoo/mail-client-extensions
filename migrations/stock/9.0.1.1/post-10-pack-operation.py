cb8013a5e245452d6b30aba7a723615120f86c66
Atul Patel <atp@openerp.com>
2019-01-23 12:50:34 +0100

    [FIX] stock@9.0: skip do partial picking if except occur

--- migrations/stock/9.0.1.1/post-10-pack-operation.py
+++ migrations/stock/9.0.1.1/post-10-pack-operation.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 from openerp.addons.base.maintenance.migrations import util
+from openerp.exceptions import UserError
+
+NS = 'openerp.addons.base.maintenance.migrations.base.9.'
+_logger = logging.getLogger(NS + __name__)
 
 def migrate(cr, version):
     # Pack operations were actually created when opening the wizard to move product.
@@ -8,5 +14,10 @@ def migrate(cr, version):
 
     pickings = util.env(cr)['stock.picking'].search([
         ('state','in', ('assigned','partially_available')),
-        ('pack_operation_ids','=', False)]) 
-    pickings.do_prepare_partial()
+        ('pack_operation_ids','=', False)])
+
+    for picking in util.iter_browse(util.env(cr)["stock.picking"], pickings.ids, logger=_logger):
+        try:
+            picking.do_prepare_partial()
+        except UserError as e:
+            _logger.warning("Cannot prepare partial picking %s: %s" % (picking.name_get()[0], str(e)))
