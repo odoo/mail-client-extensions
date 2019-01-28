c9bf6a21048597603e19a1ee964721a1ef5533bc
Damien Bouvy <dbo@odoo.com>
2019-01-28 18:10:09 +0100

    [FIX] website_{helpdesk,forum}@saas~11.5: drop views
    continuation of #386

--- migrations/website_forum/saas~11.5.1.0/pre-migrate.py
+++ migrations/website_forum/saas~11.5.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.if_unchanged(cr, "website_forum.validation_email", util.update_record_from_xml)
+    util.remove_view(cr, "website_forum.content_new_forum")
---
+++ migrations/website_helpdesk/saas~11.5.1.0/pre-migrate.py
@@ -0,0 +1,5 @@
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "website_helpdesk.content_new_team")
