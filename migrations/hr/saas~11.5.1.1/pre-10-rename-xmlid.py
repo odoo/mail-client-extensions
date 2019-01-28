334e84286b23dfaf93b0ca767439f422318d673e
Jalpesh Patel <pja@tinyerp.com>
2019-01-28 21:14:27 +0530

    [FIX] hr@saas~11.5: rename xmlid

---
+++ migrations/hr/saas~11.5.1.1/pre-10-rename-xmlid.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.rename_xmlid(cr, 'hr.employee_root', 'hr.employee_admin')
+
