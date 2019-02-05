59205beae1e60bb795fe177e9fbcf69fdc165854
Sanjay Jamod <sja@odoo.com>
2019-02-05 16:20:09 +0530

    [FIX] stock@11.0: add lot on stock move line from stock move

--- migrations/stock/11.0.1.1/end-10-cleanmergequantsreservation.py
+++ migrations/stock/11.0.1.1/end-10-cleanmergequantsreservation.py
@@ -90,7 +90,6 @@ def migrate(cr, version):
     util.remove_field(cr, 'stock.move', 'procurement_id')
     util.remove_field(cr, 'stock.move', 'partially_available')
     util.remove_field(cr, 'stock.move', 'split_from')
-    util.remove_field(cr, 'stock.move', 'restrict_lot_id')
     # Use super-efficient merge algo, but also adjust for in_date
     cr.execute("""WITH
                         dupes AS (
@@ -124,7 +123,7 @@ def migrate(cr, version):
         (product_id, location_id, location_dest_id, product_qty, product_uom_qty, qty_done, product_uom_id,
         package_id, result_package_id, owner_id, picking_id, date, lot_id, move_id, state)
         SELECT m.product_id, m.location_id, m.location_dest_id, 0.0, 0.0, m.product_uom_qty, m.product_uom,
-            NULL, NULL, m.restrict_partner_id, m.picking_id, m.date, NULL, m.id, m.state
+            NULL, NULL, m.restrict_partner_id, m.picking_id, m.date, m.restrict_lot_id, m.id, m.state
         FROM stock_move m
         WHERE m.state = 'done' AND m.inventory_id IS NULL AND m.product_qty > 0.0 
             AND NOT EXISTS (SELECT id FROM stock_move_line WHERE move_id = m.id) 
@@ -133,6 +132,7 @@ def migrate(cr, version):
     res = cr.fetchall()
     if res:
         _logger.warning('We added some stock move line for move (move, picking, product): %s', res)
+    util.remove_field(cr, 'stock.move', 'restrict_lot_id')
     # compute state on move line if not set already
     cr.execute("""
     UPDATE stock_move_line ml
