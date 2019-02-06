cb9f511277d5c52269ce6e263429dd67c5cd0220
Christophe Simonis <chs@odoo.com>
2019-02-06 11:08:13 +0100

    [FIX] account_sepa@saas~11.5: typo in model name

--- migrations/account_sepa/saas~11.5.1.0/pre-migrate.py
+++ migrations/account_sepa/saas~11.5.1.0/pre-migrate.py
@@ -6,4 +6,4 @@ def migrate(cr, version):
     util.remove_view(cr, "account_sepa.view_account_payment_form_inherited")
     util.remove_view(cr, "account_sepa.view_account_payment_invoice_form_inherited")
     util.remove_view(cr, "account_sepa.view_account_payment_from_invoices_inherited")
-    util.remove_model(cr, "account.sepa.credit.transfert")
+    util.remove_model(cr, "account.sepa.credit.transfer")
