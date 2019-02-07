e8bf2a5938d62994f9c9c2c4d1363c72ea0ff45b
Sanjay Jamod <sja@odoo.com>
2019-02-07 15:47:19 +0530

    [FIX] sale_contract@saas~13: fix traceback only fetch partner_id which is not null

--- migrations/sale_contract/9.saas~13.1.1/pre-models.py
+++ migrations/sale_contract/9.saas~13.1.1/pre-models.py
@@ -17,6 +17,7 @@ def migrate(cr, version):
           JOIN account_analytic_account a
             ON s.analytic_account_id = a.id
          WHERE s.pricelist_id IS NULL
+           AND a.partner_id IS NOT NULL
     """)
     for sid, pid, cid in cr.fetchall():
         # XXX browse all partners at once?
