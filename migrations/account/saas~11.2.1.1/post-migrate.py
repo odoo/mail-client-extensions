ae34e2357fcef55140033a414ac153e1e0f8f4f9
Christophe Simonis <chs@odoo.com>
2019-02-12 17:02:18 +0100

    [FIX] account@saas~11.2: conditionally force group imply
    Closes #405

--- migrations/account/saas~11.2.1.1/post-migrate.py
+++ migrations/account/saas~11.2.1.1/post-migrate.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo.addons.base.maintenance.migrations import util
 
+
 def migrate(cr, version):
-    util.env(cr).ref('base.group_user').write({
-        'implied_ids': [(4, util.ref(cr, 'analytic.group_analytic_tags'))],
-    })
+    group = util.env(cr).ref("base.group_user")
+    if util.ref(cr, "analytic.group_analytic_accounting") in group.implied_ids.ids:
+        group.write({"implied_ids": [(4, util.ref(cr, "analytic.group_analytic_tags"))]})
