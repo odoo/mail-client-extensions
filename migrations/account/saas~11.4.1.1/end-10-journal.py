056eb76a005e624ff0f7472a6a04111552c31842
Christophe Simonis <chs@odoo.com>
2019-02-14 10:59:27 +0100

    [FIX] account@saas~11.4: verify access right before writing on journal
    Closes #410

--- migrations/account/saas~11.4.1.1/end-10-journal.py
+++ migrations/account/saas~11.4.1.1/end-10-journal.py
@@ -5,14 +5,22 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     Journal = util.env(cr)["account.journal"]
     User = util.env(cr)["res.users"]
-    cr.execute("SELECT id, COALESCE(write_uid, create_uid, 1), company_id FROM account_journal WHERE type='purchase'")
+    cr.execute("SELECT id, COALESCE(write_uid, create_uid, 1), company_id FROM account_journal WHERE type = 'purchase'")
     for jid, uid, cid in cr.fetchall():
         u = User.browse(uid)
         old_cid = u.company_id.id
-        if cid!=old_cid:
-            if cid not in u.company_ids.ids:
-                u.write({'company_ids': [(4, cid)]})
-            u.company_id = cid
+        cid_changed = False
+
+        if cid != old_cid:
+            if cid in u.company_ids.ids:
+                u.company_id = cid
+                cid_changed = True
+            else:
+                uid = 1
+        elif not Journal.check_access_rights("write", raise_exception=False):
+            uid = 1
+
         Journal.browse(jid).sudo(user=uid)._update_mail_alias({})
-        if cid!=old_cid:
+
+        if cid_changed:
             u.company_id = old_cid
