cec608881937e5651d6db76c1f8a9c06ad02b178
Sanjay Jamod <sja@odoo.com>
2019-02-15 19:29:39 +0530

    [FIX] mrp@saas~11.5: recompute product_uom_qty correctly to avoid UoM categories different

--- migrations/mrp/saas~11.5.2.0/post-10-compute.py
+++ migrations/mrp/saas~11.5.2.0/post-10-compute.py
@@ -13,14 +13,28 @@ def migrate(cr, version):
         AND m.product_id=p.id
     """
     )
+    cr.execute("""
+        UPDATE mrp_production m
+           SET product_uom_qty = m.product_qty
+          FROM product_product p, product_template t,
+               uom_uom u1, uom_uom u2
+         WHERE p.id = m.product_id
+           AND t.id = p.product_tmpl_id
+           AND t.uom_id != m.product_uom_id
+           AND u1.category_id != u2.category_id
+           AND u1.id = m.product_uom_id
+           AND u2.id = t.uom_id
+    """)
+    cr.commit()
     # Then let ORM compute remaining records
     cr.execute(
         """
         SELECT m.id
-        FROM mrp_production m
-        LEFT JOIN product_product p on m.product_id=p.id
-        LEFT JOIN product_template pt ON p.product_tmpl_id=pt.id
-        WHERE NOT pt.uom_id=m.product_uom_id
+          FROM mrp_production m
+          LEFT JOIN product_product p on m.product_id=p.id
+          LEFT JOIN product_template pt ON p.product_tmpl_id=pt.id
+         WHERE NOT pt.uom_id=m.product_uom_id
+           AND m.product_uom_qty IS NULL
     """
     )
     ids = [x[0] for x in cr.fetchall()]
