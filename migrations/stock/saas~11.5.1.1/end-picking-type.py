32302501c1ec2782206640f86d32dda45a518e42
Christophe Simonis <chs@odoo.com>
2019-02-19 16:33:05 +0100

    [FIX] stock@saas~11.5: create/update picking types in post AND in end

--- migrations/stock/saas~11.5.1.1/end-picking-type.py
+++ migrations/stock/saas~11.5.1.1/end-picking-type.py
@@ -1,13 +1,1 @@
-# -*- coding: utf-8 -*-
-from openerp.addons.base.maintenance.migrations import util
-
-def migrate(cr, version):
-    env = util.env(cr)
-
-    for warehouse in env['stock.warehouse'].with_context(active_test=False).search([]):
-        picking_type_vals = warehouse._create_or_update_sequences_and_picking_types()
-        if picking_type_vals:
-            warehouse.write(picking_type_vals)
-        route_vals = warehouse._create_or_update_route()
-        if route_vals:
-            warehouse.write(route_vals)
+./post-picking-type.py
---
+++ migrations/stock/saas~11.5.1.1/post-picking-type.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+# NOTE
+# this script is called twice: in post- and in end- (symlink) to allow depending modules to overwrite values
+
+
+def migrate(cr, version):
+    env = util.env(cr)
+
+    for warehouse in env["stock.warehouse"].with_context(active_test=False).search([]):
+        picking_type_vals = warehouse._create_or_update_sequences_and_picking_types()
+        if picking_type_vals:
+            warehouse.write(picking_type_vals)
+        route_vals = warehouse._create_or_update_route()
+        if route_vals:
+            warehouse.write(route_vals)
+        warehouse._create_or_update_global_routes_rules()
