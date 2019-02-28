96ddacc4552c9f4c6800df6f0204c8262f71405c
Christophe Simonis <chs@odoo.com>
2019-02-28 14:13:34 +0100

    [ADD] base@saas~12.2: model changes

---
+++ migrations/base/saas~12.2.1.3/pre-20-models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    cr.execute("ALTER TABLE res_lang ALTER COLUMN week_start TYPE varchar")
+
+    if util.module_installed(cr, "point_of_sale"):
+        # in previous versions, field was declared in both modules
+        # free xmlid to avoid duplicate
+        cr.execute("""
+            DELETE FROM ir_model_data
+                  WHERE module = 'point_of_sale'
+                    AND name = 'field_res_partner__barcode'
+        """)
+        util.move_field_to_module(cr, "res.partner", "barcode", "base", "point_of_sale")
+    else:
+        util.remove_field(cr, "res.partner", "barcode")
