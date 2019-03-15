41e66389fd18b5fb602ae859e1293cf10e18c0e1
Damien Bouvy <dbo@odoo.com>
2019-03-15 17:27:26 +0100

    [IMP] stock@saas~18: wasted time
    Same request is done in stock@11.0: pre-10-basicmodels:339

--- migrations/stock/10.saas~18.1.1/pre-10-models.py
+++ migrations/stock/10.saas~18.1.1/pre-10-models.py
@@ -8,18 +8,4 @@ def migrate(cr, version):
                [SUPERUSER_ID])
 
     util.remove_field(cr, 'product_template', 'property_stock_procurement')
-
-    util.create_column(cr, 'stock_move', 'reference', 'varchar')
-    cr.execute("""
-      WITH refs AS (
-            SELECT m.id as id, COALESCE(p.name, m.name) as name
-              FROM stock_move m
-         LEFT JOIN stock_picking p ON (p.id = m.picking_id)
-      )
-        UPDATE stock_move m
-           SET "reference" = refs.name
-          FROM refs
-         WHERE refs.id = m.id
-    """)
-
     util.remove_model(cr, 'procurement.orderpoint.compute')
