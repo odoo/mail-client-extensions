74153e55d29e77237f862d7270a52fb189f8c37d
Christophe Simonis <chs@odoo.com>
2019-03-18 15:51:53 +0100

    [FIX] base@saas~11.3: do not recompute `_related_field_id` on unknown models

--- migrations/base/saas~11.3.1.3/end-models.py
+++ migrations/base/saas~11.3.1.3/end-models.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo.addons.base.maintenance.migrations import util
 
+
 def migrate(cr, version):
+    env = util.env(cr)
     cr.execute("""
-        SELECT id
+        SELECT model, array_agg(id)
           FROM ir_model_fields
-         WHERE state='manual'
+         WHERE state = 'manual'
            AND related IS NOT NULL
            AND related_field_id IS NULL
+      GROUP BY model
     """)
-    fields = util.env(cr)['ir.model.fields'].browse([x[0] for x in cr.fetchall()])
-    fields._compute_related_field_id()
+    for model, ids in cr.fetchall():
+        if model in env:
+            env["ir.model.fields"].browse(ids)._compute_related_field_id()
+        else:
+            # TODO handle manually unknow models
+            pass
