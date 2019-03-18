4cbefae93c65f29ccb301ee47e26206a66953f71
Atul Patel <atp@openerp.com>
2019-03-18 19:26:51 +0100

    [FIX] base@12.0: keep portal users only on portal group and keep public users only on public group

--- migrations/base/12.0.1.3/post-only-one-user-type-group.py
+++ migrations/base/12.0.1.3/post-only-one-user-type-group.py
@@ -6,35 +6,38 @@ def migrate(cr, version):
     user = util.ref(cr, "base.group_user")
     portal = util.ref(cr, "base.group_portal")
     public = util.ref(cr, "base.group_public")
+    usability = util.ref(cr, "base.module_category_usability")
 
-    u2id = util.ENVIRON['user2_id']
+    u2id = util.ENVIRON["user2_id"]
     assert isinstance(u2id, int)
 
-    # remove public users from `portal` and `employee` groups
+    # remove public users from all other non usability groups
     cr.execute("""
         DELETE FROM res_groups_users_rel
-              WHERE gid IN %s
-                AND uid IN (SELECT uid FROM res_groups_users_rel WHERE gid = %s)
+              WHERE gid != %(public)s
+                AND gid NOT IN (SELECT id FROM res_groups WHERE category_id = %(usability)s)
+                AND uid IN (SELECT uid FROM res_groups_users_rel WHERE gid = %(public)s)
                 AND uid != 1
-    """, [(user, portal), public])
+    """, locals())
 
-    # remove portal users from `employee` group
+    # remove portal users from all other non usability group
     cr.execute("""
         DELETE FROM res_groups_users_rel
-              WHERE gid = %s
-                AND uid IN (SELECT uid FROM res_groups_users_rel WHERE gid = %s)
+              WHERE gid != %(portal)s
+                AND gid NOT IN (SELECT id FROM res_groups WHERE category_id = %(usability)s)
+                AND uid IN (SELECT uid FROM res_groups_users_rel WHERE gid = %(portal)s)
                 AND uid != 1
-    """, [user, portal])
+    """, locals())
 
-    # remove SUPERUSER from `portal` and `public` groups
+    # remove SUPERUSER and user2 from `portal` and `public` groups
     cr.execute("""
         DELETE FROM res_groups_users_rel
-              WHERE gid IN %s
-                AND uid IN (1, %s)
-    """, [(portal, public), u2id])
-    # and force it to `employee` group
+              WHERE gid IN (%(portal)s, %(public)s))
+                AND uid IN (1, %(u2id)s)
+    """, locals())
+    # and force them to `employee` group
     cr.execute("""
         INSERT INTO res_groups_users_rel(uid, gid)
-             VALUES (1, %s), (%s, %s)
+             VALUES (1, %(user)s), (%(u2id)s, %(user)s)
         ON CONFLICT DO NOTHING
-    """, [user, u2id, user])
+    """, locals())
