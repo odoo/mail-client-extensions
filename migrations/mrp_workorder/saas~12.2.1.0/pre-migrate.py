d4428bd6246cef01ede71cd554e37c9d0d15a6a7
Nicolas Seinlet <nse@odoo.com>
2019-03-19 16:52:45 +0100

    [ADD] mrp_workorder@saas~12.2

---
+++ migrations/mrp_workorder/saas~12.2.1.0/pre-migrate.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "mrp.production", "date_planned_start_wo")
+    util.remove_field(cr, "mrp.production", "date_planned_finished_wo")
+    util.rename_column(cr, "quality_check", "lot_id", "final_lot_id")
+
+    util.remove_view(cr, "mrp_workorder.mrp_production_view_form_inherit_planning")
