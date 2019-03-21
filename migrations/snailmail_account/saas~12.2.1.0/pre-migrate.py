0bbf47de7322aee9e02d69ad644553ee0a421c2e
Christophe Simonis <chs@odoo.com>
2019-03-21 19:23:13 +0100

    [ADD] snailmail_account@saas~12.2

---
+++ migrations/snailmail_account/saas~12.2.1.0/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_column(cr, "account_invoice_send", "snailmail_cost")  # field is now computed
+    util.remove_field(cr, "account.invoice.send", "letter_ids")
+    cr.execute("DELETE TABLE snailmail_letter_account_invoice_send_rel")
