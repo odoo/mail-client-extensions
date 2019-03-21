589a52c3c063c60f5a6c9043133898573546f404
Christophe Simonis <chs@odoo.com>
2019-03-21 17:46:19 +0100

    [ADD] purchase_requisition@saas~12.2

---
+++ migrations/purchase_requisition/saas~12.2.0.1/pre-move-stuff.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    eb = util.expand_braces
+    mods = "purchase_requisition{,_stock}"
+
+    util.move_field_to_module(cr, "purchase.requisition", "warehouse_id", *eb(mods))
+    util.move_field_to_module(cr, "purchase.requisition", "picking_type_idg", *eb(mods))
+    util.move_field_to_module(cr, "purchase.requisition.line", "move_dest_id", *eb(mods))
+    util.move_field_to_module(cr, "stock.move", "requisition_line_ids", *eb(mods))
+
+    util.rename_xmlid(cr, *eb(mods + ".access_purchase_requisition_stock_manager"))
+    util.rename_xmlid(cr, *eb(mods + ".access_purchase_requisition_line_stock_manager"))
