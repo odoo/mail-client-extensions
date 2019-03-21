c120786bc8f1b65eab2234f63714107647690636
Christophe Simonis <chs@odoo.com>
2019-03-21 18:28:38 +0100

    [ADD] sale_crm@saas~12.2

---
+++ migrations/sale_crm/saas~12.2.1.0/pre-migrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.rename_field(cr, "crm.lead", "sale_number", "quotation_count")
