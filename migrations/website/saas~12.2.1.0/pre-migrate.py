e9c5b35f185e10ebc387db1dc91426fa04b3922b
Christophe Simonis <chs@odoo.com>
2019-03-27 19:16:29 +0100

    [ADD] website@saas~12.2

---
+++ migrations/website/saas~12.2.1.0/pre-migrate.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.move_field_to_module(cr, "res.partner", "is_published", "website_partner", "website")
+    util.create_column(cr, "res_partner", "is_published", "boolean")
+
+    cr.execute(
+        "UPDATE ir_ui_view SET customize_show = FALSE WHERE id = %s", [util.ref(cr, "portal.portal_show_sign_in")]
+    )
