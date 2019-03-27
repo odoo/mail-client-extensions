88328fb03ee76ed691272d8ce7daf8fdf809ca67
Christophe Simonis <chs@odoo.com>
2019-03-27 18:55:08 +0100

    [ADD] web_unsplash@saas~12.2

---
+++ migrations/web_unsplash/saas~12.2.1.1/pre-migrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.rename_xmlid(cr, *util.expand_braces("web_unsplash.assets_{common,frontend}"))
