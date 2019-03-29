5ba4c990bbc56a3acb27938dd2be51f000a0da8f
Christophe Simonis <chs@odoo.com>
2019-03-29 10:36:06 +0100

    [FIX] base@12.0: ensure admin has admin rights...

--- migrations/base/12.0.1.3/post-only-one-user-type-group.py
+++ migrations/base/12.0.1.3/post-only-one-user-type-group.py
@@ -6,6 +6,8 @@ def migrate(cr, version):
     user = util.ref(cr, "base.group_user")
     portal = util.ref(cr, "base.group_portal")
     public = util.ref(cr, "base.group_public")
+    settings = util.ref(cr, "base.group_system")
+    accessrights = util.ref(cr, "base.group_erp_manager")
     usability = util.ref(cr, "base.module_category_usability")
 
     u2id = util.ENVIRON["user2_id"]
@@ -17,7 +19,7 @@ def migrate(cr, version):
               WHERE gid != %(public)s
                 AND gid NOT IN (SELECT id FROM res_groups WHERE category_id = %(usability)s)
                 AND uid IN (SELECT uid FROM res_groups_users_rel WHERE gid = %(public)s)
-                AND uid != 1
+                AND uid NOT IN (1, %(u2id)s)
     """, locals())
 
     # remove portal users from all other non usability group
@@ -26,7 +28,7 @@ def migrate(cr, version):
               WHERE gid != %(portal)s
                 AND gid NOT IN (SELECT id FROM res_groups WHERE category_id = %(usability)s)
                 AND uid IN (SELECT uid FROM res_groups_users_rel WHERE gid = %(portal)s)
-                AND uid != 1
+                AND uid NOT IN (1, %(u2id)s)
     """, locals())
 
     # remove SUPERUSER and user2 from `portal` and `public` groups
@@ -35,9 +37,11 @@ def migrate(cr, version):
               WHERE gid IN (%(portal)s, %(public)s)
                 AND uid IN (1, %(u2id)s)
     """, locals())
-    # and force them to `employee` group
+    # and force them to `employee`, `settings` and `access rights` groups
     cr.execute("""
         INSERT INTO res_groups_users_rel(uid, gid)
              VALUES (1, %(user)s), (%(u2id)s, %(user)s)
+                  , (1, %(settings)s), (%(u2id)s, %(settings)s)
+                  , (1, %(accessrights)s), (%(u2id)s, %(accessrights)s)
         ON CONFLICT DO NOTHING
     """, locals())
