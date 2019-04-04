e7dac2e1709738433e89bd4690b6ac2ec5e06546
Christophe Simonis <chs@odoo.com>
2019-04-04 13:13:40 +0200

    Revert "[ADD] base@12.0: force update lang name of ar_SY"
    This is not needed. See https://git.io/fjLJ6
    (I made enough `push --force-with-lease`)
    
    This reverts commit da5a9c4fddf3aa060b5fdee06a9eec229eaaa7e1.

--- migrations/base/12.0.1.3/pre-arabic.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo.addons.base.maintenance.migrations import util
-
-
-def migrate(cr, version):
-    # Avoid duplicated language name (see odoo/odoo#28640)
-    util.force_noupdate(cr, "base.lang_ar_SY", False)
