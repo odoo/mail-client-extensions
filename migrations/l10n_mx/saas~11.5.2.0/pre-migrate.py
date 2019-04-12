a7649d4c9741bff8d7f20fac1915b7d2968359b5
Nicolas Seinlet <nse@odoo.com>
2019-04-12 09:12:49 +0200

    [FIX] l10n_mx@saas~11.5

--- migrations/l10n_mx/saas~11.5.2.0/pre-migrate.py
+++ migrations/l10n_mx/saas~11.5.2.0/pre-migrate.py
@@ -5,5 +5,5 @@ def migrate(cr, version):
     for tax_int in ("1", "2", "3", "5", "7", "8", "9", "10", "12", "13", "14"):
         tax = "l10n_mx.tax%s" % tax_int
         tax_id = util.ref(cr, tax)
-        cr.execute("delete from account_fiscal_position_tax_template where tax_dest_id=? or tax_src_id=?", (tax_id, tax_id))
+        cr.execute("delete from account_fiscal_position_tax_template where tax_dest_id=%s or tax_src_id=%s", (tax_id, tax_id))
         util.remove_record(cr, tax)
