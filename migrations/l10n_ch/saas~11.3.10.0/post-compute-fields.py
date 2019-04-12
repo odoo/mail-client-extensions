36e5920f46839adfc084e12883001a1044630859
Nicolas Seinlet <nse@odoo.com>
2019-04-12 09:15:08 +0200

    [FIX] l10n_ch@saas~11.3

--- migrations/l10n_ch/saas~11.3.10.0/post-compute-fields.py
+++ migrations/l10n_ch/saas~11.3.10.0/post-compute-fields.py
@@ -3,11 +3,16 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    #Not using recompute because it's no more a computed field
-    cr.execute("SELECT id FROM res_partner_bank WHERE l10n_ch_postal IS NULL")
+    #Using SQL queries to avoid a recomputation hell due to _compute_l10n_ch_isr_number
+    cr.execute("""
+        UPDATE res_partner_bank
+           SET l10n_ch_postal=sanitized_acc_number
+         WHERE l10n_ch_postal IS NULL AND acc_type!='iban'
+    """)
+    cr.execute("SELECT id FROM res_partner_bank WHERE l10n_ch_postal IS NULL and acc_type='iban' AND sanitized_acc_number like 'CH%'")
     for rec in util.iter_browse(util.env(cr)['res.partner.bank'], [b[0] for b in cr.fetchall()]):
-        if rec.acc_type == 'iban':
-            rec.l10n_ch_postal = rec._retrieve_l10n_ch_postal(rec.sanitized_acc_number)
-        else:
-            rec.l10n_ch_postal = rec.sanitized_acc_number
-    
+        cr.execute("""
+            UPDATE res_partner_bank
+               SET l10n_ch_postal=%s
+             WHERE id=%s
+        """, (rec.id, rec._retrieve_l10n_ch_postal(rec.sanitized_acc_number)))
