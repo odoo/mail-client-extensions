ec56c8eb4f849f56e879715a64b900df41af7ccf
Nicolas Seinlet <nse@odoo.com>
2019-05-31 16:45:17 +0200

    [ADD] l10n_be@saas~12.3

---
+++ migrations/l10n_be/saas~12.3.2.0/pre-migrate.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    cr.execute("select module,name from ir_model_data where model='account.tax' and module='l10n_be'")
+    for module, id in cr.fetchall():
+        util.update_record_from_xml(cr, "%s.%s" % (module, id))
+
+    util.replace_record_references_batch(
+        cr,
+        {
+            util.ref(cr, "l10n_be.tax_group_tva_0_eu"): util.ref(cr, "l10n_be.tax_group_tva_0"),
+            util.ref(cr, "l10n_be.tax_group_tva_6_eu"): util.ref(cr, "l10n_be.tax_group_tva_6"),
+            util.ref(cr, "l10n_be.tax_group_tva_12_eu"): util.ref(cr, "l10n_be.tax_group_tva_12"),
+            util.ref(cr, "l10n_be.tax_group_tva_21_eu"): util.ref(cr, "l10n_be.tax_group_tva_21"),
+            util.ref(cr, "l10n_be.tax_group_tva_6_non_eu"): util.ref(cr, "l10n_be.tax_group_tva_6"),
+            util.ref(cr, "l10n_be.tax_group_tva_12_non_eu"): util.ref(cr, "l10n_be.tax_group_tva_12"),
+            util.ref(cr, "l10n_be.tax_group_tva_21_non_eu"): util.ref(cr, "l10n_be.tax_group_tva_21"),
+            util.ref(cr, "l10n_be.tax_group_tva_6_cocont"): util.ref(cr, "l10n_be.tax_group_tva_6"),
+            util.ref(cr, "l10n_be.tax_group_tva_12_cocont"): util.ref(cr, "l10n_be.tax_group_tva_12"),
+            util.ref(cr, "l10n_be.tax_group_tva_21_cocont"): util.ref(cr, "l10n_be.tax_group_tva_21"),
+            util.ref(cr, "l10n_be.tax_group_tva_21_50_deductible"): util.ref(cr, "l10n_be.tax_group_tva_21"),
+        },
+        "account.tax.group",
+        replace_xmlid=False,
+    )
+
+    util.remove_record(cr, "l10n_be.tax_group_tva_0_eu")
+    util.remove_record(cr, "l10n_be.tax_group_tva_6_eu")
+    util.remove_record(cr, "l10n_be.tax_group_tva_12_eu")
+    util.remove_record(cr, "l10n_be.tax_group_tva_21_eu")
+    util.remove_record(cr, "l10n_be.tax_group_tva_6_non_eu")
+    util.remove_record(cr, "l10n_be.tax_group_tva_12_non_eu")
+    util.remove_record(cr, "l10n_be.tax_group_tva_21_non_eu")
+    util.remove_record(cr, "l10n_be.tax_group_tva_6_cocont")
+    util.remove_record(cr, "l10n_be.tax_group_tva_12_cocont")
+    util.remove_record(cr, "l10n_be.tax_group_tva_21_cocont")
+    util.remove_record(cr, "l10n_be.tax_group_tva_21_50_deductible")
