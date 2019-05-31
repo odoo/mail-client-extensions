de8e52a8e7bdda9672444b964a43420aa3599330
Nicolas Seinlet <nse@odoo.com>
2019-05-31 16:44:42 +0200

    [ADD] hr_holidays@saas~12.3

---
+++ migrations/hr_holidays/saas~12.3.1.5/pre-migrate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    eb = util.expand_braces
+    util.remove_field(cr, "hr.leave", "duration_display")
+    util.create_column(cr, "hr_leave_type", 'responsible_id', 'int4')
+    util.rename_xmlid(cr, *eb("hr_holidays.{h,}hr_leave_action_new_request_view_form"))
