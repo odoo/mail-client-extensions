48e99f4783745f1cb917501160f95dd17fcea5f6
Nicolas Seinlet <nse@odoo.com>
2019-05-31 16:46:17 +0200

    [ADD] sale_stock@saas~12.3

---
+++ migrations/sale_stock/saas~12.3.1.0/pre-migrate.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    cr.execute("""
+        UPDATE account_invoice
+           SET incoterm_id = incoterms_id
+         WHERE incoterm_id IS NULL
+           AND incoterms_id IS NOT NULL
+    """)
+
+    util.remove_field(cr, "account.invoice", "incoterms_id")
+    util.remove_view(cr, 'sale_stock.invoice_form_inherit_sale_stock')
