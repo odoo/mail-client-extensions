ecd710945da684da75e5b19f4e7216f40dfc53b8
Nicolas Seinlet <nse@odoo.com>
2019-06-04 09:41:08 +0200

    [ADD] account_sepa@saas~12.3

---
+++ migrations/account_sepa/saas~12.3.1.0/pre-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, 'account.register.payments', 'partner_bank_account_id')
+    util.remove_field(cr, 'account.payment', 'partner_bank_account_id')
