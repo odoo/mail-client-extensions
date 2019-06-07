801cc870d624e144554a8ca610805648301c6de2
Nicolas Seinlet <nse@odoo.com>
2019-06-07 14:56:34 +0200

    [FIX] base@9.0

--- migrations/base/9.0.1.3/pre-20-deduplicate-banks.py
+++ migrations/base/9.0.1.3/pre-20-deduplicate-banks.py
@@ -14,7 +14,7 @@ def migrate(cr, version):
     # XXX what if duplicated are not linked to the same partner?
     cr.execute("""SELECT array_agg(id ORDER BY coalesce(write_date, create_date) DESC)
                     FROM res_partner_bank
-                GROUP BY sanitized_acc_number
+                GROUP BY sanitized_acc_number, company_id
                   HAVING count(id) > 1
                 """)
     for ids, in cr.fetchall():
