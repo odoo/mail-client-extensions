6ff311efb215ea977664f19eafb138ad74a3ab5c
ysh-odoo <39479808+ysh-odoo@users.noreply.github.com>
2019-06-11 17:39:45 +0530

    [FIX] website_sale_stock@saas~18: set default value for inventory_availability
    Closes #463

--- migrations/website_sale_stock/10.saas~18.1.0/pre-migrate.py
+++ migrations/website_sale_stock/10.saas~18.1.0/pre-migrate.py
@@ -11,5 +11,17 @@ def migrate(cr, version):
                                          END
     """)
 
+    cr.execute("""
+        UPDATE ir_default
+           SET json_value = CASE WHEN json_value = '"empty"' THEN '"never"'
+                                 WHEN json_value = '"in_stock"' THEN '"always"'
+                                 WHEN json_value = '"warning"' THEN '"custom"'
+                                 ELSE json_value
+                             END
+         WHERE field_id = (SELECT id FROM ir_model_fields
+                            WHERE name='inventory_availability'
+                             AND model='product.template')
+    """)
+
     eb = util.expand_braces
     util.rename_xmlid(cr, *eb('website_sale_stock.{orders_followup,portal_order_page}_shipping'))
