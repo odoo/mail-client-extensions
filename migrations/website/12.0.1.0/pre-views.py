76acd501be10e8f9df518457d7135e0dda49a6cd
Christophe Simonis <chs@odoo.com>
2019-06-17 19:21:31 +0200

    [FIX] website@12.0: correctly choose which view to forceupdate
    In 12.0, qweb views are copy-on-write. This wasn't the case before. We
    should then update to source view only if views hasn't been modified.
    
    Closes #503

--- migrations/website/12.0.1.0/pre-views.py
+++ migrations/website/12.0.1.0/pre-views.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
+from functools import partial
 from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
+    force_update = partial(util.force_noupdate, noupdate=False)
+
     for view in {"homepage", "contactus", "aboutus"}:
-        util.force_noupdate(cr, "website." + view, False)
+        xid = "website." + view
+        util.force_noupdate(cr, xid)
+        util.if_unchanged(cr, xid, callback=force_update)
