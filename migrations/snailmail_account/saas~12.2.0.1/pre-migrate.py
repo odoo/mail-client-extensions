93678f71ea13667c3a5a8ebaa7cefff118b7aa33
Christophe Simonis <chs@odoo.com>
2019-06-18 15:51:17 +0200

    [FIX] snailmail_account@saas~12.2: correctly DROP TABLE

--- migrations/snailmail_account/saas~12.2.0.1/pre-migrate.py
+++ migrations/snailmail_account/saas~12.2.0.1/pre-migrate.py
@@ -5,4 +5,4 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.remove_column(cr, "account_invoice_send", "snailmail_cost")  # field is now computed
     util.remove_field(cr, "account.invoice.send", "letter_ids")
-    cr.execute("DELETE TABLE snailmail_letter_account_invoice_send_rel")
+    cr.execute("DROP TABLE snailmail_letter_account_invoice_send_rel")
