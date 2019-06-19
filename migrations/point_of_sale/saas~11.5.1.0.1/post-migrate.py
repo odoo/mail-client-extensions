4fcc852afe31a9f74dc403dd6a0d693dc1e4673c
Damien Bouvy <dbo@odoo.com>
2019-06-19 17:01:58 +0200

    [IMP] point_of_sale@saas~11.5: performance for big tables
    * POS Performance (#451)

--- migrations/point_of_sale/saas~11.5.1.0.1/post-migrate.py
+++ migrations/point_of_sale/saas~11.5.1.0.1/post-migrate.py
@@ -1,58 +1,144 @@
 # -*- coding: utf-8 -*-
 from operator import itemgetter
 from odoo.addons.base.maintenance.migrations import util
+from odoo.sql_db import connection_info_for
 import logging
+import psycopg2
 import os
+
+_logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
     env = util.env(cr)
-    _logger = logging.getLogger(__name__)
-    env_variable = 'ODOO_MIG_12_POS_MIGRATION_SQL'
-    if os.environ.get(env_variable):
+    env_big_pos = 'ODOO_MIG_12_POS_MIGRATION_SQL'
+    if not os.environ.get(env_big_pos):
+        # standard pos migration
+        cr.execute("SELECT id FROM pos_order_line")
+        if cr.rowcount > 300000:
+            _logger.warning("""There are %s pos order lines, this is considered as a big amount,
+                maybe you should consider to set the following environement variable %s : """ % (cr.rowcount, env_big_pos))
+        ids = list(map(itemgetter(0), cr.fetchall()))
+        orderlines = util.iter_browse(env["pos.order.line"], ids)
+        for orderline in orderlines:
+            orderline._onchange_amount_line_all()
+
+        cr.execute("SELECT id FROM pos_order")
+        ids = list(map(itemgetter(0), cr.fetchall()))
+        orders = util.iter_browse(env["pos.order"], ids)
+        for order in orders:
+            order._onchange_amount_all()
+    else:
+        # Alternate flow for pos processing
+        # We categorize all pos lines based on params that can affect the price computation (fpox, taxes, etc.)
+        # We store that categorization in a temporary table
+        # We process one line from each category and compute its price, then push the result to all line of the same category
+        # Since the amount of lines can be ginormous, we use a server-side cursor to avoid a MemoryError
+        _logger.info('creating temporary table')
+        # Store the categorization, all columns are important params that impact the computation except
+        # the last one which contains the matching lines as an array
+        cr.execute("""CREATE TABLE mig_temp_post_order_compute (
+             id serial,
+             pricelist_id integer,
+             fiscal_position_id integer,
+             product_id integer,
+             price_unit float8,
+             qty float8,
+             tax_ids integer[],
+             discount float8,
+             line_ids integer[],
+             price_subtotal float8,
+             price_subtotal_incl float8
+            )""")
         cr.execute("""
-                    WITH lines as (SELECT o.pricelist_id,o.fiscal_position_id, l.product_id, l.qty,array_agg(DISTINCT t.account_tax_id) as tax_ids,l.id, l.discount, l.price_unit
-                    from pos_order_line l
-                    left join pos_order o on o.id=l.id
-                    LEFT JOIN account_tax_pos_order_line_rel t on l.id=t.pos_order_line_id
-                    GROUP BY 1,2,3,4,6,7, 8)
-                    SELECT pricelist_id,fiscal_position_id,product_id,price_unit, qty,tax_ids,discount,array_agg(id) agg
-                    FROM lines
-                    group by 1,2,3,4,5,6,7
+            WITH lines as (
+                    SELECT o.pricelist_id,
+                            o.fiscal_position_id,
+                            l.product_id,
+                            l.qty,
+                            array_agg(DISTINCT t.account_tax_id) as tax_ids,
+                            l.id,
+                            l.discount,
+                            l.price_unit
+                    FROM pos_order_line l
+                    LEFT JOIN pos_order o ON o.id=l.id
+                    LEFT JOIN account_tax_pos_order_line_rel t ON l.id=t.pos_order_line_id
+                    GROUP BY 1,2,3,4,6,7,8
+                )
+            INSERT INTO mig_temp_post_order_compute (
+                pricelist_id,
+                fiscal_position_id,
+                product_id,
+                price_unit,
+                qty,
+                tax_ids,
+                discount,
+                line_ids
+            )
+            SELECT pricelist_id,
+                fiscal_position_id,
+                product_id,
+                price_unit,
+                qty,
+                tax_ids,
+                discount,
+                array_agg(id) agg
+            FROM lines
+            group by 1,2,3,4,5,6,7
         """)
-        for row in cr.fetchall():
-            ids = row[7] # column 8 in SQL is 7 in Python :-)
+        def compute_for_group(group):
+            # process a category of lines: compute amounts based on first line of group, then push the result via postgres
+            group_id = group[0]  # column 1 in SQL is 0 in Python :-)
+            ids = group[7]
             orderlines = env["pos.order.line"].browse([ids[0], ])
             res = orderlines[0]._compute_amount_line_all()
             cr.execute("""
-                UPDATE pos_order_line
-                SET price_subtotal_incl=%s,
-                    price_subtotal=%s
-                WHERE id=ANY(%s) 
-            """, [res['price_subtotal_incl'], res['price_subtotal'], list(ids)])
+                UPDATE mig_temp_post_order_compute
+                SET price_subtotal_incl=%s,price_subtotal=%s
+                WHERE id=%s
+            """, [res['price_subtotal_incl'], res['price_subtotal'], group_id])
 
+        # We need to make sure that the cursor we're about to open has the temp table available to it
+        cr.commit()
+        # Open an alternate cursor that will remain postgres-side instead of trying to load everything in memory
+        db, conn_info = connection_info_for(cr.dbname)
+        with psycopg2.connect(**conn_info) as conn:
+            with conn.cursor(name='hyuge_pos') as alt_cr:
+                done = 0
+                cr.execute("SELECT count(*) FROM mig_temp_post_order_compute")
+                total_groups = cr.fetchone()[0]
+                ROWS_TO_FETCH = 10**5
+                alt_cr.execute("""
+                    SELECT pricelist_id,fiscal_position_id,product_id,price_unit,qty,tax_ids,discount,agg
+                    FROM mig_temp_post_order_compute
+                """)
+                rows = alt_cr.fetchmany(ROWS_TO_FETCH)
+                while rows:
+                    _logger.info('Computing total for order line groups (%s/%s)', done + len(rows), total_groups)
+                    for group in rows:
+                        compute_for_group(group)
+                    env['pos.order.line'].invalidate_cache()
+                    rows = alt_cr.fetchmany(ROWS_TO_FETCH)
+                    done += len(rows)
         cr.execute("""
-            WITH paid as (
-                SELECT pos_statement_id, sum(amount) as paid
-                FROM account_bank_statement_line
-                group by pos_statement_id
-            )
-            UPDATE pos_order o
-            SET amount_paid=paid.paid
-            FROM paid
-            WHERE o.id=paid.pos_statement_id
+            UPDATE pos_order_line
+            SET price_subtotal_incl=tmp.price_subtotal_incl,
+                price_subtotal=tmp.price_subtotal
+            FROM mig_temp_post_order_compute tmp
+            WHERE pos_order_line.id = ANY(tmp.line_ids)
         """)
+        cr.execute(""" DROP TABLE mig_temp_post_order_compute """)  # clean up that shit
+        # Now let's process the orders
         cr.execute("""
-            WITH paid as (
-                SELECT pos_statement_id, sum(amount) as paid
-                FROM account_bank_statement_line
-                WHERE amount<0
-                group by pos_statement_id
-            )
+            WITH payments AS (
+                    SELECT pos_statement_id, SUM(amount) as paid, SUM(CASE WHEN amount < 0 THEN amount END) as returned
+                    FROM account_bank_statement_line
+                    GROUP BY pos_statement_id)
             UPDATE pos_order o
-            SET amount_return=paid.paid
-            FROM paid
-            WHERE o.id=paid.pos_statement_id
+            SET amount_paid = payments.paid,
+                amount_return = COALESCE(payments.returned, 0)
+            FROM payments
+            WHERE o.id=payments.pos_statement_id
         """)
         cr.execute("""
             WITH amount as (
@@ -66,26 +152,17 @@ def migrate(cr, version):
             FROM amount
             WHERE o.id=amount.order_id
         """)
-
-        cr.execute(""" update pos_order set amount_paid = 0 where amount_paid is NULL""")
-        cr.execute(""" update pos_order set amount_return = 0 where amount_return is NULL""")
-
-    else:
-        # standard pos migration
-        cr.execute("SELECT count(*) cnt FROM pos_order_line")
-        pol_count= cr.fetchone()[0]
-        if pol_count > 300000:
-            _logger.warning("""there are %s pos order lines, this is considered as a big amount,
-                maybe you should consider to set the following environement variable %s : """ % (pol_count, env_variable))
-
-        cr.execute("SELECT id FROM pos_order_line")
-        ids = list(map(itemgetter(0), cr.fetchall()))
-        orderlines = util.iter_browse(env["pos.order.line"], ids)
-        for orderline in orderlines:
-            orderline._onchange_amount_line_all()
-
-        cr.execute("SELECT id FROM pos_order")
-        ids = list(map(itemgetter(0), cr.fetchall()))
-        orders = util.iter_browse(env["pos.order"], ids)
-        for order in orders:
-            order._onchange_amount_all()
+        # Clean up, ensure value since this is a required field
+        cr.execute("""ALTER TABLE pos_order DISABLE TRIGGER ALL""")
+        cr.execute("""
+            UPDATE pos_order
+            SET amount_paid=COALESCE(amount_paid, 0),
+                amount_return=COALESCE(amount_return, 0),
+                amount_tax=COALESCE(amount_tax, 0),
+                amount_total=COALESCE(amount_total, 0)
+            WHERE amount_paid IS NULL
+               OR amount_return IS NULL
+               OR amount_tax IS NULL
+               OR amount_total IS NULL
+        """)
+        cr.execute("""ALTER TABLE pos_order ENABLE TRIGGER ALL""")
--- migrations/pos_sale/saas~11.5.1.0/post-pos-config.py
+++ migrations/pos_sale/saas~11.5.1.0/post-pos-config.py
@@ -3,4 +3,17 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    util.recompute_fields(cr, "pos.order", ["currency_rate"])
+    # only recompute for order with a currency != company currency, otherwise 1.0
+    cr.execute("""
+        UPDATE pos_order
+        SET currency_rate = 1
+        FROM product_pricelist p,res_company c
+        WHERE pos_order.company_id=c.id
+          AND pos_order.pricelist_id=p.id
+          AND p.currency_id = c.currency_id
+    """)
+    cr.execute("""
+        SELECT id FROM pos_order WHERE currency_rate IS NULL
+    """)
+    recompute_ids = [o[0] for o in cr.fetchall()]
+    util.recompute_fields(cr, "pos.order", ["currency_rate"], ids=recompute_ids)
