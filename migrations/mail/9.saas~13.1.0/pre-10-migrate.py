f0414b8292edfa568927305eda5e16eef8a8379a
Christophe Simonis <chs@odoo.com>
2019-06-27 11:43:18 +0200

    [FIX] mail@9.saas~13: avoid duplicated xmlid
    `mail.icp_mail_bounce_alias` may be already created by new 0.0.0 script
    when migrating to >= 10.0

--- migrations/mail/9.saas~13.1.0/pre-10-migrate.py
+++ migrations/mail/9.saas~13.1.0/pre-10-migrate.py
@@ -2,7 +2,12 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.rename_xmlid(cr, *util.expand_braces('{mass_mailing,mail}.icp_mail_bounce_alias'))
+    if util.version_gte("10.0") and util.ref(cr, "mail.icp_mail_bounce_alias"):
+        # a duplicated xmlid has already be created by `mail/0.0.0/pre-icp.py`.
+        cr.execute("DELETE FROM ir_model_data WHERE module = 'mass_mailing' AND name = 'icp_mail_bounce_alias'")
+    else:
+        util.rename_xmlid(cr, *util.expand_braces('{mass_mailing,mail}.icp_mail_bounce_alias'))
+
     util.force_noupdate(cr, 'mail.mail_template_data_notification_email_default', False)
 
     cr.execute('ALTER TABLE res_users ALTER COLUMN alias_id DROP NOT NULL')
