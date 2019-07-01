f829f1fac01bcdb9a2818bd75ad9ec5a6188f433
Christophe Simonis <chs@odoo.com>
2019-07-01 18:51:45 +0200

    [FIX] website_portal_sale@10.saas~16, sale@saas~18: link script instead of copy

--- migrations/sale/10.saas~18.1.1/pre-before-saas-16.py
+++ migrations/sale/10.saas~18.1.1/pre-before-saas-16.py
@@ -1,18 +1,1 @@
-# -*- coding: utf-8 -*-
-from odoo.addons.base.maintenance.migrations import util
-
-def migrate(cr, version):
-    # if database was older than saas~16, the script website_portal_sale@saas~16 hasn't be run
-    # do it now
-    util.remove_record(cr, 'sale.access_account_invoice_tax')
-    util.remove_record(cr, 'sale.access_account_journal')
-    util.remove_record(cr, 'sale.access_product_list')
-    util.remove_record(cr, 'sale.access_res_partner')
-    util.remove_record(cr, 'sale.access_account_tax')
-    util.remove_record(cr, 'sale.access_account_tax_group')
-    util.remove_record(cr, 'sale.access_res_partner_category')
-    util.remove_record(cr, 'sale.access_product_attribute_portal')
-    util.remove_record(cr, 'sale.access_product_attribute_value_portal')
-    util.remove_record(cr, 'sale.access_product_attribute_price_portal')
-    util.remove_record(cr, 'sale.access_product_attribute_line_portal')
-    util.remove_record(cr, 'sale.portal_personal_contact')
+../../website_portal_sale/10.saas~16.1.0/pre-remove_record.py
