cbe022f86ec3144f8d1b94b0d83d21c99fd7ae5f
Christophe Simonis <chs@odoo.com>
2019-07-02 15:49:59 +0200

    [FIX] project{,_issue}@10.saas~*: correctly handle module merge

--- migrations/project_issue/10.saas~15.1.0/pre-migrate.py
+++ migrations/project_issue/10.saas~15.1.0/pre-migrate.py
@@ -4,4 +4,6 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.remove_field(cr, 'project.config.settings', 'module_project_issue_sheet')
     util.remove_field(cr, 'project.config.settings', 'module_rating_project_issue')
-    util.force_noupdate(cr, 'project_issue.project_config_settings_view_form_inherit_project_issue', False)
+
+    module = "project" if util.version_gte("10.saas~18") else "project_issue"
+    util.remove_view(cr, module + ".project_config_settings_view_form_inherit_project_issue")
