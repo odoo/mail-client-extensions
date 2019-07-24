7fd3247564582481bf9c04c9c3510be8a50ee6f7
Nicolas Seinlet <nse@odoo.com>
2019-07-24 14:33:56 +0200

    [FIX] documents@saas~12.1

--- migrations/documents/saas~12.1.1.0/pre-migrate.py
+++ migrations/documents/saas~12.1.1.0/pre-migrate.py
@@ -3,7 +3,7 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     eb = util.expand_braces
-    
+
     cr.execute("""
         CREATE TABLE documents_document(
             id SERIAL PRIMARY KEY,
@@ -20,15 +20,16 @@ def migrate(cr, version):
             res_model varchar,
             name varchar,
             type varchar,
-            folder_id integer
+            folder_id integer,
+            attachment_id integer
         )
     """)
 
     cr.execute("""
         INSERT INTO documents_document
-        (id, attachment_id, active, thumbnail, partner_id, owner_id, lock_uid, file_size, res_model, name, type, folder_id)
-            SELECT id, id, active, thumbnail, partner_id, owner_id, lock_uid, file_size, res_model, coalesce(attachment_name, datas_fname), 'binary', folder_id
-              FROM ir.attachment
+        (id, attachment_id, active, partner_id, owner_id, lock_uid, file_size, res_model, name, type, folder_id)
+            SELECT id, id, active, partner_id, owner_id, lock_uid, file_size, res_model, datas_fname, 'binary', folder_id
+              FROM ir_attachment
              WHERE folder_id IS NOT NULL
     """)
     cr.execute("SELECT setval('documents_document_id_seq', (SELECT MAX(id)+1 FROM documents_document))")
@@ -39,8 +40,32 @@ def migrate(cr, version):
     util.create_column(cr, 'documents_workflow_rule', 'sequence', 'int4')
     cr.execute("UPDATE documents_workflow_rule set sequence=10")
 
-    util.rename_xmlid(cr, eb("documents.{documents_,}settings_action"))
-    util.rename_xmlid(cr, eb("documents.{documents_view_search_inherit,document_view_search}"))
-    util.rename_xmlid(cr, eb("documents.document{s,}_view_kanban"))
-    util.rename_xmlid(cr, eb("documents.document{s,}_view_form"))
-    util.rename_xmlid(cr, eb("documents.configuration{,_action}"))
+    util.rename_xmlid(cr, *eb("documents.{documents_,}settings_action"))
+    util.rename_xmlid(cr, *eb("documents.document{s,}_view_kanban"))
+    util.rename_xmlid(cr, *eb("documents.document{s,}_view_form"))
+    util.rename_xmlid(cr, *eb("documents.configuration{,_action}"))
+    util.rename_xmlid(cr, *eb("documents.documents_internal_status{_folder,}"))
+    util.rename_xmlid(cr, *eb("documents.documents_internal_knowledge{_facet,}"))
+    util.rename_xmlid(cr, *eb("documents.documents_internal_template{_facet,}"))
+    util.rename_xmlid(cr, *eb("documents.documents_finance_status{_folder,}"))
+    util.rename_xmlid(cr, *eb("documents.documents_finance_{Documents_folder,documents}"))
+    util.rename_xmlid(cr, *eb("documents.documents_finance_{Fiscal_year_folder,fiscal_year}"))
+    util.rename_xmlid(cr, *eb("documents.documents_hr_documents{_facet,}"))
+    util.rename_xmlid(cr, *eb("documents.documents_marketing_assets{_facet,}"))
+    util.rename_xmlid(cr, *eb("documents.documents_internal_knowledge_{S,s}ales"))
+    util.rename_xmlid(cr, *eb("documents.documents_internal_template_{P,p}resentations"))
+
+    cr.execute("""ALTER TABLE document_tag_rel RENAME TO document_tag_rel_old""")
+    util.create_m2m(cr, "document_tag_rel", "documents_document", "documents_tag")
+    cr.execute("""
+        INSERT INTO document_tag_rel (documents_document_id, documents_tag_id)
+        SELECT ir_attachment_id, documents_tag_id FROM document_tag_rel_old
+    """)
+    cr.execute("DROP TABLE document_tag_rel_old")
+
+    util.remove_view(cr, "documents.documents_view_search_inherit")
+    util.remove_view(cr, "documents.inherit_mail_attachment_view_kanban")
+    util.remove_view(cr, "documents.documents_view_kanban")
+    util.remove_view(cr, "documents.documents_view_form")
+    util.remove_view(cr, "documents.documents_upload_url_view")
+    util.remove_view(cr, "documents.documents_view_list")
