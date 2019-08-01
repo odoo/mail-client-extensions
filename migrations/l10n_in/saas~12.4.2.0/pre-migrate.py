b0b07af7e57e2e85e52ac92b0cc4efbcd8a13a4e
Nicolas Seinlet <nse@odoo.com>
2019-08-01 10:14:56 +0200

    [ADD] l10n_in@saas~12.4

---
+++ migrations/l10n_in/saas~12.4.2.0/pre-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "l10n_in.account.invoice.report", "invoice_id")
+    util.remove_field(cr, "l10n_in.account.invoice.report", "refund_invoice_id")
