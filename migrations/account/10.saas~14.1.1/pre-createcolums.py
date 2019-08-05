fb09ad067ac155bea7b9b7a1d39a8995e80dd5e2
Christophe Simonis <chs@odoo.com>
2019-08-05 14:16:21 +0200

    [FIX] account@10.saas~14: end-compute stored field
    Closes #477

--- migrations/account/10.saas~14.1.1/pre-createcolums.py
+++ migrations/account/10.saas~14.1.1/pre-createcolums.py
@@ -3,3 +3,4 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.create_column(cr, 'account_invoice', 'activity_date_deadline', 'date')
+    util.create_column(cr, "account_invoice_tax", "base", "numeric")
