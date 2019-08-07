55437ce0d5a70a9a635a220e22819dff6546edb1
Nicolas Seinlet <nse@odoo.com>
2019-08-07 14:19:03 +0200

    [FIX] {account,account_sepa}@saas~12.3: field is moved, not deleted

--- migrations/account_sepa/saas~12.3.1.0/pre-migrate.py
+++ migrations/account_sepa/saas~12.3.1.0/pre-migrate.py
@@ -4,4 +4,3 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.remove_field(cr, 'account.register.payments', 'partner_bank_account_id')
-    util.remove_field(cr, 'account.payment', 'partner_bank_account_id')
