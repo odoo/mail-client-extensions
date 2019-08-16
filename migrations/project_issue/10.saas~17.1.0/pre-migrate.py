7cc4d2713b2f0d8db20aa3785f468f0e8073d0b0
Jalpesh Patel <pja@odoo.com>
2019-08-16 17:20:22 +0530

    [FIX] project_issue@10.saas~17: check table existence before use
    Closes #555

--- migrations/project_issue/10.saas~17.1.0/pre-migrate.py
+++ migrations/project_issue/10.saas~17.1.0/pre-migrate.py
@@ -2,10 +2,14 @@
 from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    cr.execute("""
-        UPDATE project_issue
-           SET description = {}
-         WHERE description IS NOT NULL
-    """.format(util.pg_text2html('description')))
+    # If 'project_issue' module is not installed and 'project' is installed
+    # That time need to check table 'project_issue' is exist
+    # Because Symlinks file 'pre-00-17-issues.py' use in 'project' module
+    if util.table_exists(cr, "project_issue"):
+        cr.execute("""
+            UPDATE project_issue
+               SET description = {}
+             WHERE description IS NOT NULL
+        """.format(util.pg_text2html('description')))
 
     util.remove_model(cr, 'project.issue.report')
