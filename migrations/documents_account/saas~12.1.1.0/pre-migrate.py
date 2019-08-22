8b6db9000367209096da08dd38402aa543489415
Christophe Simonis <chs@odoo.com>
2019-08-22 14:00:44 +0200

    [FIX] documents_account@saas~12.1

--- migrations/documents_account/saas~12.1.1.0/pre-migrate.py
+++ migrations/documents_account/saas~12.1.1.0/pre-migrate.py
@@ -3,6 +3,6 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     eb = util.expand_braces
-    util.rename_xmlid(cr, eb("documents.documents_replace_{inbox,documents}_vendor_bill"))
+    util.rename_xmlid(cr, *eb("documents.documents_replace_{inbox,documents}_vendor_bill"))
     util.rename_field(cr, 'res.company', 'dms_account_settings', 'documents_account_settings')
     util.rename_field(cr, 'res.config.settings', 'dms_account_settings', 'documents_account_settings')
