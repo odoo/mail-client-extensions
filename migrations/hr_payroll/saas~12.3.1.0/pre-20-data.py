7ec73c49128ae080c78a655ae310bc6068936c36
Christophe Simonis <chs@odoo.com>
2019-08-22 18:13:42 +0200

    [FIX] hr_payroll@saas~12.3: avoid migrating base structure if unused

--- migrations/hr_payroll/saas~12.3.1.0/pre-20-data.py
+++ migrations/hr_payroll/saas~12.3.1.0/pre-20-data.py
@@ -10,6 +10,15 @@ def migrate(cr, version):
     # Main idea is to flatten the structures (which is hierarchical)
     # And convert the M2M (from structure to rules) to a O2M (by duplicating rules)
 
+    base_struct = util.ref(cr, "hr_payroll.structure_base")
+    if base_struct:
+        cr.execute("SELECT count(1) FROM hr_payslip WHERE struct_id = %s", [base_struct])
+        if not cr.fetchone()[0]:
+            # structure not used, we can remove it safely
+            util.remove_record(cr, "hr_payroll.structure_base")
+        else:
+            util.force_noupdate(cr, "hr_payroll.structure_base", True)
+
     cr.execute("""
         UPDATE hr_salary_rule
            SET appears_on_payslip=FALSE
