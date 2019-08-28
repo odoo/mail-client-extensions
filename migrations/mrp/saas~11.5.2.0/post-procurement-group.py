43ba5b0b44e44c4314e77c72695edf13ca0f8d04
Nicolas Seinlet <nse@odoo.com>
2019-08-28 10:22:35 +0200

    [FIX] mrp@saas~11.5
    sale_id field is defined in sale_stock module, which is not a dependency 
    of mrp.

--- migrations/mrp/saas~11.5.2.0/post-procurement-group.py
+++ migrations/mrp/saas~11.5.2.0/post-procurement-group.py
@@ -3,18 +3,23 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    env = util.env(cr)
-    cr.execute("""
-        SELECT m.id,m.name
-          FROM mrp_production m
-          JOIN procurement_group pg on m.procurement_group_id = pg.id
-         WHERE pg.sale_id IS NOT NULL
-    """)
-    for mid,mo_name in cr.fetchall():
-        procurement_group_id = env["procurement.group"].create({'name': mo_name})
-        cr.execute("""
-            UPDATE  mrp_production m
-              SET procurement_group_id=%s
-            WHERE id=%s
-        """,[procurement_group_id.id,mid])
-
+    if util.module_installed(cr, "sale_stock"):
+        env = util.env(cr)
+        cr.execute(
+            """
+            SELECT m.id,m.name
+              FROM mrp_production m
+              JOIN procurement_group pg on m.procurement_group_id = pg.id
+             WHERE pg.sale_id IS NOT NULL
+        """
+        )
+        for mid, mo_name in cr.fetchall():
+            procurement_group_id = env["procurement.group"].create({"name": mo_name})
+            cr.execute(
+                """
+                UPDATE  mrp_production m
+                  SET procurement_group_id=%s
+                WHERE id=%s
+            """,
+                [procurement_group_id.id, mid],
+            )
