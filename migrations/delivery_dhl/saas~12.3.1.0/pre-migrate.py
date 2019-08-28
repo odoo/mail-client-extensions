22a7afb6d7420ff4aaf69377b4fc241ffa598488
Nicolas Seinlet <nse@odoo.com>
2019-08-28 10:50:57 +0200

    [FIX] delivery_dhl@saas~12.3: SleepyDeveloperException

--- migrations/delivery_dhl/saas~12.3.1.0/pre-migrate.py
+++ migrations/delivery_dhl/saas~12.3.1.0/pre-migrate.py
@@ -4,7 +4,7 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     cr.execute("""
-        UPDATE delivery.carrier
+        UPDATE delivery_carrier
            SET dhl_package_dimension_unit=left(dhl_package_dimension_unit, 1),
                dhl_package_weight_unit=left(dhl_package_weight_unit, 1)
     """)
