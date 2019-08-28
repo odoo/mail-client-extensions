20e4eecaf91edddab3c5b53c231dfd5eaf750331
Nicolas Seinlet <nse@odoo.com>
2019-08-28 11:50:08 +0200

    [FIX] base@saas~12.3: lang ar_001/ar_AR

--- migrations/base/saas~12.3.1.3/pre-40-lang.py
+++ migrations/base/saas~12.3.1.3/pre-40-lang.py
@@ -1,6 +1,33 @@
 # -*- coding: utf-8 -*-
 
+
 def migrate(cr, version):
+    cr.execute(
+        """
+        INSERT INTO res_lang (name, code, iso_code, translatable, active, direction,
+                              date_format, time_format, week_start, grouping, decimal_point, thousands_sep)
+             SELECT 'ar_AA_tmp', 'ar_AA_tmp', iso_code, translatable, active, direction,
+                    date_format, time_format, week_start, grouping, decimal_point, thousands_sep
+               FROM res_lang
+              WHERE code = 'ar_AA'
+    """
+    )
+    cr.execute(
+        """
+        UPDATE res_lang
+           SET name = case code
+                 when 'ar_AA_tmp' then 'ar_AA'
+                 when 'ar_AA' then 'ar_001'
+               end,
+               code = case code
+                  when 'ar_AA_tmp' then 'ar_AA'
+                  when 'ar_AA' then 'ar_001'
+               end
+         WHERE code in ('ar_AA_tmp', 'ar_AA')
+    """
+    )
+
     cr.execute("UPDATE ir_translation SET lang='ar_001' WHERE lang = 'ar_AA' ")
-    cr.execute("UPDATE res_lang SET code = 'ar_001' WHERE code = 'ar_AA' ")
     cr.execute("UPDATE res_partner SET lang = 'ar_001' WHERE lang = 'ar_AA' ")
+
+    cr.execute("DELETE FROM res_lang WHERE code = 'ar_AA'")
