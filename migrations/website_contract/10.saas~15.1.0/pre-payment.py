0bb2abcc28bdadca412eb61dee57b2df375ba6bc
Christophe Simonis <chs@odoo.com>
2019-09-18 15:04:00 +0200

    [FIX] website_contract@10.saas~15: handle calls when module not installed
    This happen due to symlinks and explicts calls.
    
    Closes #595

--- migrations/website_contract/10.saas~15.1.0/pre-payment.py
+++ migrations/website_contract/10.saas~15.1.0/pre-payment.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
+    if not util.column_exists(cr, "payment_transaction", "callback_eval"):
+        # NOTE: This script is symlinked in website_subscription@saas~17.
+        #       If the database is already in saas~15, this script has already been run.
+        #       It is also explicitly called in sale_subscription@saas~18.
+        #       However, installation of `sale_subscription` doesn't imply the installation
+        #       of `payment`.
+        return
+
     cr.execute(r"""
         WITH tx AS (
             SELECT id, (regexp_matches(callback_eval, '\d+'))[1]::integer AS res_id
