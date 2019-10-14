de0dc1a57f112f12d4dd43fd7dbd8510b6c43121
Christophe Simonis <chs@odoo.com>
2019-10-14 15:53:41 +0200

    [ADD] hr_attendance@saas~12.5

---
+++ migrations/hr_attendance/saas~12.5.2.0/pre-migrate.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "hr_employee", "last_check_in", "timestamp without time zone")
+    util.create_column(cr, "hr_employee", "last_check_out", "timestamp without time zone")
+    cr.execute(
+        """
+        UPDATE hr_employee e
+           SET last_check_in = check_in,
+               last_check_out = check_out
+          FROM hr_attendance a
+         WHERE a.id = e.last_attendance_id
+    """
+    )
+
+    util.remove_view(cr, "hr_attendance.view_employee_filter_inherit_hr_attendance")
+    util.remove_view(cr, "hr_attendance.view_employee_kanban_inherit_hr_attendance")
