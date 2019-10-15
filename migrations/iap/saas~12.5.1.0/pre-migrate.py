8b14e11cd92b8beb8b031fa1f7a67f35c0a9dd69
Christophe Simonis <chs@odoo.com>
2019-10-15 11:14:15 +0200

    [ADD] iap@saas~12.5

---
+++ migrations/iap/saas~12.5.1.0/pre-migrate.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.create_m2m(cr, "iap_account_res_company_rel", "iap_account", "res_company")
+    cr.execute(
+        """
+        INSERT INTO iap_account_res_company_rel(iap_account_id, res_company_id)
+             SELECT id, company_id
+               FROM iap_account
+    """
+    )
+    util.remove_column(cr, "iap_account", "company_id")
+    util.rename_field(cr, "iap.account", "company_id", "company_ids")
