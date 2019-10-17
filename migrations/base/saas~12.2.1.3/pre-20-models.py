dce04a412a742b2257ffe5f92ea977c346f5e24e
Denis Ledoux <dle@odoo.com>
2019-10-17 14:41:11 +0200

    [FIX] base@saas~12.2: required many2one ondelete set null

--- migrations/base/saas~12.2.1.3/pre-20-models.py
+++ migrations/base/saas~12.2.1.3/pre-20-models.py
@@ -18,3 +18,14 @@ def migrate(cr, version):
         util.remove_field(cr, "res.partner", "barcode")
 
     util.force_noupdate(cr, "base.view_partner_form", False)
+
+    # Since odoo/odoo@ee6dc40a607626147429389d8f4746f697fadaa6
+    # This is forbidden to have a required many2one field defined with `on_delete` to `set null`
+    cr.execute("""
+        UPDATE ir_model_fields
+           SET on_delete = 'restrict'
+         WHERE state = 'manual'
+           AND on_delete = 'set null'
+           AND ttype = 'many2one'
+           AND required = 't'
+    """)
