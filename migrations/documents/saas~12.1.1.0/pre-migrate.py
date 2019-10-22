f1d0612ce5013dd413d106f1826e6124122db824
Christophe Simonis <chs@odoo.com>
2019-10-22 11:17:22 +0200

    [FIX] documents@saas~12.1: correctly convert attachments to documents
    Fixes #624
    Closes #627

--- migrations/documents/saas~12.1.1.0/pre-migrate.py
+++ migrations/documents/saas~12.1.1.0/pre-migrate.py
@@ -12,31 +12,99 @@ def migrate(cr, version):
             write_uid integer,
             write_date timestamp without time zone,
             active boolean,
-            thumbnail bytea,
             partner_id integer,
             owner_id integer,
             lock_uid integer,
             file_size integer,
             res_model varchar,
             name varchar,
+            url varchar,
             type varchar,
             folder_id integer,
             attachment_id integer
         )
     """)
+    cr.execute("INSERT INTO ir_model(name, model) VALUES ('Document', 'documents.document') RETURNING id")
+    model_id = cr.fetchone()[0]
 
-    fname_col = 'name' if util.version_gte('saas~12.4') else 'datas_fname'
+    fname_col = "name" if util.version_gte("saas~12.4") else "COALESCE(name, datas_fname)"
     cr.execute("""
-        INSERT INTO documents_document
-        (id, attachment_id, active, partner_id, owner_id, lock_uid, file_size, res_model, name, type, folder_id)
-            SELECT id, id, active, partner_id, owner_id, lock_uid, file_size, res_model, %s, 'binary', folder_id
-              FROM ir_attachment
-             WHERE folder_id IS NOT NULL OR id IN (SELECT ir_attachment_id FROM document_tag_rel)
-    """ % (fname_col,))
+        INSERT INTO documents_document(id, create_uid, create_date, write_uid, write_date,
+                                       attachment_id, active, partner_id, owner_id, lock_uid,
+                                       file_size, res_model, name, url, type, folder_id)
+             SELECT id, create_uid, create_date, write_uid, write_date,
+                    id, active, partner_id, owner_id, lock_uid,
+                    file_size, res_model, {}, url, type, folder_id
+               FROM ir_attachment
+              WHERE folder_id IS NOT NULL
+                 OR id IN (SELECT ir_attachment_id FROM document_tag_rel)
+    """.format(fname_col))
     cr.execute("SELECT setval('documents_document_id_seq', (SELECT MAX(id)+1 FROM documents_document))")
+
+    # thumbnail was an attachment on attachments, move them to document
+    cr.execute("""
+        UPDATE ir_attachment
+           SET res_model = 'documents.document'
+         WHERE res_model = 'ir.attachment'
+           AND res_field = 'thumbnail'
+           AND res_id IN (SELECT id FROM documents_document)
+    """)
+
+    util.create_m2m(cr, "documents_document_res_users_rel", "documents_document", "res_users")
+    cr.execute("""
+        INSERT INTO documents_document_res_users_rel(documents_document_id, res_users_id)
+             SELECT ir_attachment_id, res_users_id
+               FROM ir_attachment_res_users_rel
+              WHERE ir_attachment_id IN (SELECT id FROM documents_document)
+    """)
+    cr.execute("DROP TABLE ir_attachment_res_users_rel")
+
+    cr.execute("ALTER TABLE document_tag_rel RENAME TO document_tag_rel_old")
+    util.create_m2m(cr, "document_tag_rel", "documents_document", "documents_tag")
+    cr.execute("""
+        INSERT INTO document_tag_rel(documents_document_id, documents_tag_id)
+             SELECT ir_attachment_id, documents_tag_id
+               FROM document_tag_rel_old
+    """)
+    cr.execute("DROP TABLE document_tag_rel_old")
+
+    cr.execute("SELECT id FROM documents_document")
+    ids = {d: d for d, in cr.fetchall()}
+    util.replace_record_references_batch(cr, ids, "ir.attachment", "documents.document")
+
+    for field in {
+        "active",
+        "thumbnail",
+        "folder_id",
+        "lock_uid",
+        "partner_id",
+        "owner_id",
+        "favorited_ids",
+        "tag_ids",
+        "available_rule_ids",
+    }:
+        util.remove_field(cr, "ir.attachment", field)
+
+    util.remove_mixin_from_model(cr, "ir.attachment", "mail.thread")
+    util.remove_mixin_from_model(cr, "ir.attachment", "mail.activity.mixin")
+
+    util.create_m2m(cr, "documents_document_documents_share_rel", "documents_document", "documents_share")
+    cr.execute("""
+        INSERT INTO documents_document_documents_share_rel(documents_document_id, documents_share_id)
+             SELECT ir_attachment_id, documents_share_id
+               FROM documents_share_ir_attachment_rel
+              WHERE ir_attachment_id IN (SELECT id FROM documents_document)
+    """)
+    cr.execute("DROP TABLE documents_share_ir_attachment_rel")
 
     util.rename_field(cr, 'documents.folder', 'attachment_ids', 'document_ids')
     util.rename_field(cr, 'documents.share', 'attachment_ids', 'document_ids')
+
+    cr.execute("""
+        UPDATE mail_alias
+           SET model_id = %s
+         WHERE id IN (SELECT alias_id FROM documents_share)
+    """, [model_id])
 
     util.create_column(cr, 'documents_workflow_rule', 'sequence', 'int4')
     cr.execute("UPDATE documents_workflow_rule set sequence=10")
@@ -55,14 +123,6 @@ def migrate(cr, version):
     util.rename_xmlid(cr, *eb("documents.documents_marketing_assets{_facet,}"))
     util.rename_xmlid(cr, *eb("documents.documents_internal_knowledge_{S,s}ales"))
     util.rename_xmlid(cr, *eb("documents.documents_internal_template_{P,p}resentations"))
-
-    cr.execute("""ALTER TABLE document_tag_rel RENAME TO document_tag_rel_old""")
-    util.create_m2m(cr, "document_tag_rel", "documents_document", "documents_tag")
-    cr.execute("""
-        INSERT INTO document_tag_rel (documents_document_id, documents_tag_id)
-        SELECT ir_attachment_id, documents_tag_id FROM document_tag_rel_old
-    """)
-    cr.execute("DROP TABLE document_tag_rel_old")
 
     util.remove_view(cr, "documents.documents_view_search_inherit")
     util.remove_view(cr, "documents.inherit_mail_attachment_view_kanban")
