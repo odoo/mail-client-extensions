1e7f0c9c135ceaab9a622071fd408e65dc55a780
Nicolas Seinlet <nse@odoo.com>
2019-10-22 14:28:13 +0200

    [FIX] l10n_ch@saas~11.3
    Fix #622
    Fix #626
    Close #629

--- migrations/l10n_ch/saas~11.3.10.0/post-compute-fields.py
+++ migrations/l10n_ch/saas~11.3.10.0/post-compute-fields.py
@@ -3,16 +3,27 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    #Using SQL queries to avoid a recomputation hell due to _compute_l10n_ch_isr_number
-    cr.execute("""
-        UPDATE res_partner_bank
-           SET l10n_ch_postal=sanitized_acc_number
-         WHERE l10n_ch_postal IS NULL AND acc_type!='iban'
-    """)
-    cr.execute("SELECT id FROM res_partner_bank WHERE l10n_ch_postal IS NULL and acc_type='iban' AND sanitized_acc_number like 'CH%'")
-    for rec in util.iter_browse(util.env(cr)['res.partner.bank'], [b[0] for b in cr.fetchall()]):
-        cr.execute("""
+    ids = []
+    cr.execute("SELECT id FROM res_partner_bank WHERE l10n_ch_postal IS NULL")
+    for rec in util.iter_browse(util.env(cr)["res.partner.bank"], [b[0] for b in cr.fetchall()]):
+        if rec.sanitized_acc_number.startswith('CH') and rec.acc_type == "iban":
+            cr.execute(
+                """
+                UPDATE res_partner_bank
+                   SET l10n_ch_postal=%s
+                 WHERE id=%s
+                """,
+                (rec._retrieve_l10n_ch_postal(rec.sanitized_acc_number), rec.id),
+            )
+        else:
+            ids.append(rec.id)
+
+    if ids:
+        cr.execute(
+            """
             UPDATE res_partner_bank
-               SET l10n_ch_postal=%s
-             WHERE id=%s
-        """, (rec.id, rec._retrieve_l10n_ch_postal(rec.sanitized_acc_number)))
+               SET l10n_ch_postal=sanitized_acc_number
+             WHERE id in %s
+        """,
+            (tuple(ids)),
+        )
