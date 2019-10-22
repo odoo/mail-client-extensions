28e0c62895a0387f2c869afa8beb7465ac19e883
Nicolas Seinlet <nse@odoo.com>
2019-10-22 13:54:22 +0200

    [ADD] project_enterprise@saas~12.5

---
+++ migrations/project_enterprise/saas~12.5.1.0/pre-migrate.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "project.project", "allow_planning")
+    util.remove_field(cr, "project.task", "allow_planning")
+
+    util.remove_view(cr, "project_enterprise.project_view_kanban_inherit")
+    util.remove_view(cr, "project_enterprise.project_task_view_form_allow_planning_domain")
+    util.remove_view(cr, "project_enterprise.project_view_form_simplified_inherit")
+    util.remove_view(cr, "project_enterprise.project_view_form_inherit")
