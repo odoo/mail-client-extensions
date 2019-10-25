0cae9d1edd113f6daedab0a459253984a4e7e78d
Nicolas Seinlet <nse@odoo.com>
2019-10-25 11:24:17 +0200

    [ADD] sale_timesheet_enterprise@saas~12.5

---
+++ migrations/sale_timesheet_enterprise/saas~12.5.1.0/pre-migrate.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.move_field_to_module(cr, "project.project", "allow_billable", "industry_fsm", "sale_timesheet_enterprise")
+    util.move_field_to_module(cr, "project.task", "allow_billable", "industry_fsm", "sale_timesheet_enterprise")
+
+    util.create_column(cr, "project_project", "allow_timesheet_timer", "boolean")
+    util.create_column(cr, "project_project", "allow_billable", "boolean")
+    util.create_column(cr, "project_task", "timesheet_timer_first_start", "timestamp without time zone")
+    util.create_column(cr, "project_task", "timesheet_timer_last_stop", "timestamp without time zone")
+    util.create_column(cr, "project_task", "allow_billable", "boolean")
+
+    util.remove_field(cr, "res.company", "use_timesheet_timer")
+    util.remove_field(cr, "res.config.settings", "use_timesheet_timer")
