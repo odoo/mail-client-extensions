366583e31ea07f3b9a43f1720f714b213f1d3190
Christophe Simonis <chs@odoo.com>
2019-10-29 15:34:12 +0100

    [FIX] product@saas~12.2: correctly rename image fields
    Closes #625

--- migrations/product/saas~12.2.1.2/post-image.py
+++ migrations/product/saas~12.2.1.2/post-image.py
@@ -2,12 +2,15 @@
 from odoo.addons.base.maintenance.migrations import util
 
 
-def image_mixin_recompute_fields(cr, model, image_field_fix=""):
-    fields = [f % image_field_fix for f in ["image%s_big", "image%s_large", "image%s_medium", "image%s_small", "can_image%s_be_zoomed"]]
-    ids = util.env(cr)[model].search([("image%s_original" % image_field_fix, "!=", False)]).ids
-    util.recompute_fields(cr, model, fields, ids)
-
-
 def migrate(cr, version):
-    image_mixin_recompute_fields(cr, "product.template")
-    image_mixin_recompute_fields(cr, "product.product", "_raw")
+    env = util.env(cr)
+    suffixes = ["big", "large", "medium", "small"]
+
+    # `image_medium` and `image_small` where already there...
+    fields = ["image_{}".format(s) for s in suffixes[:2]] + ["can_image_be_zoomed"]
+    ids = env["product.template"].search([("image_original", "!=", False)]).ids
+    util.recompute_fields(cr, "product.template", fields, ids)
+
+    fields = ["image_raw_{}".format(s) for s in suffixes] + ["can_image_raw_be_zoomed"]
+    ids = env["product.product"].search([("image_raw_original", "!=", False)]).ids
+    util.recompute_fields(cr, "product.product", fields, ids)
