4666fc6587a9fd8518a354795169a38c115f1b56
Christophe Simonis <chs@odoo.com>
2019-11-05 17:12:09 +0100

    [ADD] website_mass_mailing@saas~12.5

---
+++ migrations/website_mass_mailing/saas~12.5.1.0/pre-migrate.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+        CREATE TABLE website_mass_mailing_popup (
+            id SERIAL PRIMARY KEY,
+            create_uid integer,
+            create_date timestamp without time zone,
+            write_uid integer,
+            write_date timestamp without time zone,
+            mailing_list_id int4,
+            website_id int4,
+            popup_content text
+        )
+    """
+    )
+    cr.execute(
+        """
+        INSERT INTO website_mass_mailing_popup(mailing_list_id, popup_content)
+             SELECT id, popup_content
+               FROM mailing_list
+    """
+    )
+
+    util.remove_field(cr, "mailing.list", "popup_content")
+    util.remove_field(cr, "mailing.list", "popup_redirect_url")
+    util.remove_field(cr, "res_config_settings", "group_website_popup_on_exit")
+
+    util.remove_record(cr, "website_mass_mailing.group_website_popup_on_exit")
+    util.remove_view(cr, "website_mass_mailing.view_mail_mass_mailing_list_form")
+    util.remove_view(cr, "website_mass_mailing.res_config_settings_view_form")
