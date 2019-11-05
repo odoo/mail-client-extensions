c507e929bd26382ab70739b43f27c314dc80ac8f
Laurent Contzen <lco@odoo.com>
2019-11-05 15:56:52 +0100

    [FIX] base@saas~12.3
    Only create the accounting backup tables if `account_financial_html_report_line`
    has a `name` column.
    
    Uninstalling of `account_reports` module can leave the table in place but with only `id` and
    log_access columns. In this case, checking table existence is not enough.
    
    Closes #640
    Fixes #611

--- migrations/base/saas~12.3.1.3/pre-20-taxes.py
+++ migrations/base/saas~12.3.1.3/pre-20-taxes.py
@@ -3,7 +3,7 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    if util.table_exists(cr, 'account_financial_html_report_line'):
+    if util.column_exists(cr, 'account_financial_html_report_line', 'name'):
         cr.execute("""
             SELECT account_financial_html_report_line.*, ir_model_data.name as xmlid, ir_model_data.module as module
           INTO financial_report_lines_v12_bckp
