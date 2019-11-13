2eb7e5f03233be7f10c989844b0396d2ccdc5bb2
Nicolas Seinlet <nse@odoo.com>
2019-11-13 16:25:23 +0100

    [ADD] account_analytic_default@saas~12.5

---
+++ migrations/account_analytic_default/saas~12.5.1.0/pre-migrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "account_analytic_default.res_config_settings_view_form")
