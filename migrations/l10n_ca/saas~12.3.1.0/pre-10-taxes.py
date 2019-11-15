ed21cadd0c9b2db8bd440bc2fa8f87c8ac9ee1e8
Nicolas Seinlet <nse@odoo.com>
2019-11-15 14:02:35 +0100

    [FIX] l10n_ca@saas~12.3

--- migrations/l10n_ca/saas~12.3.1.0/pre-10-taxes.py
+++ migrations/l10n_ca/saas~12.3.1.0/pre-10-taxes.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
 
 def migrate(cr, version):
-    cr.execute("""
-        INSERT INTO taxes_not_to_merge(tax_id)
-        SELECT res_id
-        FROM ir_model_data
-        WHERE
-        (name LIKE '%gstpst\_bc\_sale\_en'
-            OR name LIKE '%gstpst\_mb\_sale\_en'
-            OR name LIKE '%gstqst\_sale\_en'
-            OR name LIKE '%gstpst\_sk\_sale\_en'
-            OR name LIKE '%gstpst\_bc\_purc\_en'
-            OR name LIKE '%gstpst\_mb\_purc\_en'
-            OR name LIKE '%gstqst\_purc\_en'
-            OR name LIKE '%gstpst\_sk\_purc\_en')
-        AND model = 'account.tax'
-        AND module = 'l10n_ca'
-    """)
+    if util.table_exists(cr, "taxes_not_to_merge"):
+        cr.execute("""
+            INSERT INTO taxes_not_to_merge(tax_id)
+            SELECT res_id
+            FROM ir_model_data
+            WHERE
+            (name LIKE '%gstpst\_bc\_sale\_en'
+                OR name LIKE '%gstpst\_mb\_sale\_en'
+                OR name LIKE '%gstqst\_sale\_en'
+                OR name LIKE '%gstpst\_sk\_sale\_en'
+                OR name LIKE '%gstpst\_bc\_purc\_en'
+                OR name LIKE '%gstpst\_mb\_purc\_en'
+                OR name LIKE '%gstqst\_purc\_en'
+                OR name LIKE '%gstpst\_sk\_purc\_en')
+            AND model = 'account.tax'
+            AND module = 'l10n_ca'
+        """)
