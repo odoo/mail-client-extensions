575913b23a91bff4623dbe946e6a1594d7dc09e6
Nicolas Seinlet <nse@odoo.com>
2019-11-19 14:24:05 +0100

    [FIX] industry_fsm@saas~12.4

--- migrations/industry_fsm/saas~12.4.1.0/pre-migrate.py
+++ migrations/industry_fsm/saas~12.4.1.0/pre-migrate.py
@@ -3,21 +3,21 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    util.create_column(cr, "project.task", "fsm_state", "varchar")
+    util.create_column(cr, "project_task", "fsm_state", "varchar")
     cr.execute(
         """
         UPDATE project_task t
-           SET fsm_state = CASE WHEN t.fsm_done AND t.sale_line_id IS NOT NULL
-                                     AND f.allow_billable AND p.timesheet_product_id IS NOT NULL
+           SET fsm_state = CASE WHEN t.fsm_is_done AND t.sale_line_id IS NOT NULL
+                                     AND p.allow_billable AND p.timesheet_product_id IS NOT NULL
                                 THEN 'sold'
-                                WHEN t.fsm_done THEN 'validated'
+                                WHEN t.fsm_is_done THEN 'validated'
                                 ELSE 'draft'
                             END
           FROM project_project p
          WHERE p.id = t.project_id
     """
     )
-    util.remove_field(cr, "project.task", "fsm_done")
+    util.remove_field(cr, "project.task", "fsm_is_done")
     util.remove_field(cr, "project.task.create.sale.order", "material_line_ids")
     util.remove_field(cr, "project.task.create.sale.order", "product_template_ids")
 
@@ -29,3 +29,5 @@ def migrate(cr, version):
     util.remove_view(cr, "industry_fsm.view_form_project_inherit_quotation")
     util.remove_view(cr, "industry_fsm.view_form_fsm_inherit_quotation")
     util.remove_view(cr, "industry_fsm.project_task_create_sale_order_view_form_inherit")
+    util.remove_view(cr, "industry_fsm.view_task_form2_inherit")
+    util.remove_view(cr, "industry_fsm.project_view_form_simplified_inherit")
