bac33592e49cfb6ec66f112dbbcf5519684faa36
Nicolas Seinlet <nse@odoo.com>
2019-11-19 12:10:09 +0100

    [FIX] helpdesk_account@saas~12.4

--- migrations/helpdesk_account/saas~12.4.1.0/pre-10-account-pocalypse.py
+++ migrations/helpdesk_account/saas~12.4.1.0/pre-10-account-pocalypse.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
     cr.execute('ALTER TABLE account_invoice_helpdesk_ticket_rel RENAME TO account_invoice_helpdesk_ticket_rel_old')
+    util.create_m2m(cr, "account_move_helpdesk_ticket_rel", "helpdesk_ticket", "account_move")
