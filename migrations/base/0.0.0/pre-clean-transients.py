95829ef24816f2442a81e154bbc5997bdda84827
Christophe Simonis <chs@odoo.com>
2019-12-20 10:58:15 +0100

    [FIX] base@0.0.0: ignore bad queries when cleaning transients
    Fixes #676

--- migrations/base/0.0.0/pre-clean-transients.py
+++ migrations/base/0.0.0/pre-clean-transients.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import psycopg2
+
+from odoo.tools import ignore
 from odoo.addons.base.maintenance.migrations import util
 
 
@@ -6,7 +9,9 @@ def migrate(cr, version):
     cr.execute("SELECT model FROM ir_model WHERE transient = true")
     models = cr.fetchall()
     for model, in models:
-        cr.execute("DELETE FROM " + util.table_of_model(cr, model))
+        with ignore(psycopg2.Error), util.savepoint(cr):
+            cr.execute("DELETE FROM " + util.table_of_model(cr, model))
     for ir in util.indirect_references(cr, bound_only=True):
         query = 'DELETE FROM "{0}" WHERE {1}'.format(ir.table, ir.model_filter())
-        cr.executemany(query, models)
+        with ignore(psycopg2.Error), util.savepoint(cr):
+            cr.executemany(query, models)
