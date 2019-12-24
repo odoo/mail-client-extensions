0b5f73f10be305587448dd1003a1098f5a3e10fd
Nicolas Vannieuwerburgh <nvn@odoo.com>
2019-12-24 12:04:55 +0100

    [IMP] product@9.saas~13: pre-compute product_template.default_code
    more details at https://github.com/odoo/odoo/blob/d038cc1a0a6e1d939318763aa9dba48aa1eafcf5/addons/product/models/product_template.py#L260-L265

--- migrations/product/9.saas~13.1.2/pre-migrate.py
+++ migrations/product/9.saas~13.1.2/pre-migrate.py
@@ -18,3 +18,22 @@ def migrate(cr, version):
         ALTER TABLE product_attribute_line_product_attribute_value_rel RENAME COLUMN line_id TO product_attribute_line_id;
         ALTER TABLE product_attribute_line_product_attribute_value_rel RENAME COLUMN val_id TO product_attribute_value_id;
     """)
+
+    # pre compute the field product_template.default_code
+    util.create_column(cr, 'product_template', 'default_code', 'varchar')
+
+    cr.execute("""
+    UPDATE product_template pt SET default_code = A.default_code
+    FROM (
+        SELECT pt.id pt_id, min(pp.default_code) default_code
+        FROM product_template pt
+        JOIN product_product pp ON pt.id = pp.product_tmpl_id
+        WHERE pp.active = 't'
+        GROUP BY pt.id
+        HAVING count(*) = 1 AND min(pp.default_code) IS NOT NULL
+    )A
+    WHERE pt.id = A.pt_id
+    """)
+
+    cr.execute("UPDATE product_template pt SET default_code = '' WHERE default_code IS NULL")
+
