851503484b55acc597c1903f0fe95627859c6d1f
Ankit Joshi <ajo@odoo.com>
2020-01-02 15:23:47 +0530

    [REM]account_online_sync@9.saas~13.2.0: Remove 'online.institution' object. (#678)

--- migrations/account_online_sync/9.saas~13.2.0/end-cleanup.py
+++ migrations/account_online_sync/9.saas~13.2.0/end-cleanup.py
@@ -4,3 +4,4 @@ from openerp.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.remove_field(cr, 'account.online.journal', 'institution_id')
     util.delete_model(cr, 'account.institution')
+    util.delete_model(cr, 'online.institution')
