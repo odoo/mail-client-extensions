b97f0679c0a32e46303a342c5509c2e1f4ce0d56
Denis Ledoux <dle@odoo.com>
2020-01-09 11:58:30 +0100

    [FIX] mail@0.0.0: report migration not posted with only "in-database" custom views
    When there was only custom views created manually,
    in database (not coming from a custom module),
    the code in `end-report-migration.py`
    was called before the view were disabled,
    because in the loading process the validation of the custom
    views happens after the processing of the `end` migration scripts.
    
    Process end migration scripts:
    https://github.com/odoo/odoo/blob/91807404e7d5c314ea30926b44bd16e878417ec1/odoo/modules/loading.py#L381-L384
    Validate custom views:
    https://github.com/odoo/odoo/blob/91807404e7d5c314ea30926b44bd16e878417ec1/odoo/modules/loading.py#L449-L456
    
    On the other hand, `_register_hook()` is always called after the custom view validation,
    so I choose to use
    - that because this is quite simple to override
    - it's not called especially only when there is upgrade,
      but when its called for a loading without upgrade,
      then this migration script wouldn't be loaded,
      and util.migration_reports would be empty anyway.

--- migrations/mail/0.0.0/end-report-migration.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp.addons.base.maintenance.migrations import util
-
-
-def migrate(cr, version):
-    if len(util.migration_reports):
-        util.announce_migration_report(cr)
