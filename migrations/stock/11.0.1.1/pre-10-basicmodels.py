c4dad8c8d3ed196a24efeaa0d74bcb8784915a2e
Christophe Simonis <chs@odoo.com>
2020-01-15 18:00:29 +0100

    [FIX] stock@11.0: drop all possible constraints on m2m
    As the m2m constraint are not explictly named [1], their named are
    autogenerated by postgresql based on argument order, which depends on
    which m2m field is setup first.
    As it may change between versions, drop all possible candidates.
    
    Was actually a "fix" on the plateform side [2]
    
    [1] https://github.com/odoo/odoo/blob/91807404e7d5c314ea30926b44bd16e878417ec1/odoo/fields.py#L2380
    [2] https://github.com/odoo/migration-scripts/blob/b1d222b923ed0d36ca17090028629913913ef386/scripts/versions/11.0/addons/pre/stock/stock.py#L15

--- migrations/stock/11.0.1.1/pre-10-basicmodels.py
+++ migrations/stock/11.0.1.1/pre-10-basicmodels.py
@@ -143,8 +143,13 @@ def migrate(cr, version):
     if util.table_exists(cr, 'stock_valuation_adjustment_lines'):
         if not util.get_index_on(cr, 'stock_valuation_adjustment_lines', 'move_id'):
             cr.execute("""CREATE INDEX stock_valuation_adjustment_lines_move_id ON stock_valuation_adjustment_lines (move_id)""") #might be deleted in the end
-    cr.execute("""ALTER TABLE stock_quant_move_rel
-                  DROP CONSTRAINT IF EXISTS stock_quant_move_rel_quant_id_move_id_key;""")
+    cr.execute(
+        """
+        ALTER TABLE stock_quant_move_rel
+            -- constraint name is not stable. drop all possible exising ones
+            DROP CONSTRAINT IF EXISTS stock_quant_move_rel_quant_id_move_id_key,
+            DROP CONSTRAINT IF EXISTS stock_quant_move_rel_move_id_quant_id_key
+    """)
 
     cr.commit()
     # Update: partially_available state has been removed from picking
