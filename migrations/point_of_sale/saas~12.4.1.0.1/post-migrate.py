13070276241f815dcfb1958fcf63456c934dfa81
Christophe Simonis <chs@odoo.com>
2020-01-24 16:38:29 +0100

    [FIX] point_of_sale@saas~12.4

--- migrations/point_of_sale/saas~12.4.1.0.1/post-migrate.py
+++ migrations/point_of_sale/saas~12.4.1.0.1/post-migrate.py
@@ -5,16 +5,6 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    cr.execute(
-        """
-            SELECT o.id
-              FROM pos_order o
-        INNER JOIN pos_session s ON s.id = o.session_id
-        INNER JOIN pos_config c ON c.id = s.config_id
-        INNER JOIN res_company cmpy ON cmpy.id=o.company_id
-        INNER JOIN product_pricelist pp ON pp.id=o.pricelist_id
-             WHERE NOT pp.currency_id = cmpy.currency_id
-        """
-    )
+    cr.execute("SELECT id FROM pos_order WHERE currency_rate IS NULL")
     ids = list(map(itemgetter(0), cr.fetchall()))
     util.recompute_fields(cr, "pos.order", fields=["currency_rate"], ids=ids)
