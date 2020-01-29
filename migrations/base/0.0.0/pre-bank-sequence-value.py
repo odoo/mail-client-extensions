1b0e0b9262e21a182683d4f82b4053e9af5d5660
Thibault Francois <tfr@openerp.com>
2020-01-29 15:49:07 +0100

    [ADD] base@0.0.0: force sequence on res.partner.bank
    On res.partner.bank there is a field sequence that defines the order of the records.
    The default value has been added later. On some databases, the value is NULL which is
    considered as higher than any integer in SQL but it is sent as 0 by `read` to the web client,
    so they appear to be the first through the web client but the code sees it as the last one.
    We set a sequence to the empty one to match the code behavior.
    
    Closes #720

---
+++ migrations/base/0.0.0/pre-bank-sequence-value.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+def migrate(cr, version):
+    cr.execute("""
+        UPDATE res_partner_bank SET sequence = s.seq
+        FROM (
+            SELECT id, COALESCE(sequence, (
+                    SELECT coalesce(max(sequence),0)
+                    FROM res_partner_bank
+                    WHERE partner_id = b.partner_id) + row_number() OVER(PARTITION BY partner_id ORDER BY sequence, id)) AS seq
+            FROM res_partner_bank b
+        ) AS s
+        WHERE
+            s.id = res_partner_bank.id
+        AND
+            res_partner_bank.sequence is NULL
+    """)
