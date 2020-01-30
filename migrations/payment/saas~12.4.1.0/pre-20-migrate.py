3596d0399ba8621312ec4f7704676b7da68202ea
Thibault Francois <tfr@openerp.com>
2020-01-30 15:05:07 +0100

    [FIX] payment@saas~12.4 Some payment token are linked with the public partner and since the record rule give access to token base on the partner it lead to an issue with the page pay.php that shows all the payment_token
    A first query try to find the appropriate partner from the payment_transaction
    remaining token are assigned to odoobot

--- migrations/payment/saas~12.4.1.0/pre-20-migrate.py
+++ migrations/payment/saas~12.4.1.0/pre-20-migrate.py
@@ -4,3 +4,22 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.create_column(cr, "payment_acquirer", "check_validity", "boolean")
+
+    cr.execute("""
+        UPDATE
+            payment_token SET partner_id = p.partner_id
+        FROM
+            payment_transaction AS p
+        WHERE
+                payment_token.id = p.payment_token_id
+            AND
+                payment_token.partner_id =
+                    (select res_id from ir_model_data where name = 'public_partner' and module = 'base')
+    """)
+    cr.execute("""
+        UPDATE
+            payment_token SET partner_id = (select res_id from ir_model_data where name = 'partner_root' and module = 'base')
+        WHERE partner_id =
+                (select res_id from ir_model_data where name = 'public_partner' and module = 'base')
+    """)
+
