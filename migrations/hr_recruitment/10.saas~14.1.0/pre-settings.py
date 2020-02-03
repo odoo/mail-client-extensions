27a905cf973a14d17b164a61e7858bc2b6a6999f
Hiral Patel <hip@tinyerp.com>
2020-02-03 17:44:08 +0530

    [FIX] hr_recruitment@saas~14: remove menu's group
    Closes #728

--- migrations/hr_recruitment/10.saas~14.1.0/pre-settings.py
+++ migrations/hr_recruitment/10.saas~14.1.0/pre-settings.py
@@ -5,3 +5,7 @@ def migrate(cr, version):
     # field change type
     util.remove_field(cr, 'hr.recruitment.config.settings', 'module_hr_recruitment_survey')
     util.remove_view(cr, 'hr_recruitment.view_hr_recruitment_configuration')
+    cr.execute(
+        "DELETE FROM ir_ui_menu_group_rel WHERE menu_id = %s",
+        [util.ref(cr, "hr_recruitment.menu_crm_case_categ0_act_job")],
+    )
