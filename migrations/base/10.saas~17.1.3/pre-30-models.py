ec465534d9fc8954b4a7ff4b7bfbd15da9afd732
Denis Ledoux <dle@odoo.com>
2020-02-05 12:31:27 +0100

    [FIX] base@10.saas~17: preserve res.partner.fax when used
    Otherwise, users complain when they loose
    their beloved fax numbers of their customers.
    
    opw-2180752
    Where is the fax field? The fax field is in the kitchen.
    
    Closes #723

--- migrations/base/10.saas~17.1.3/pre-30-models.py
+++ migrations/base/10.saas~17.1.3/pre-30-models.py
@@ -21,6 +21,37 @@ def migrate(cr, version):
     cr.execute("UPDATE res_country SET name_position='after' WHERE id=%s",
                [util.ref(cr, 'base.jp')])
 
+    cr.execute("SELECT sum((TRIM(COALESCE(fax, '')) != '')::integer) / count(*)::float > 0.05 FROM res_partner;")
+    if cr.fetchone()[0]:
+        column_name = util.find_new_table_column_name(cr, 'res_partner', 'x_fax')
+        for model in ['res.partner', 'res.users']:
+            util.rename_field(cr, model, 'fax', column_name)
+            util.move_field_to_module(cr, model, column_name, 'base', '__upgrade__')
+            cr.execute("""
+                UPDATE ir_model_fields
+                   SET state = 'manual'
+                 WHERE model = %s AND name = %s
+             """, (model, column_name))
+        try:
+            util.add_view(cr, 'res.partner.form.tax.upgrade', 'res.partner', 'form', """
+            <data>
+              <field name="mobile" position="after">
+                <field name="%s"/>
+              </field>
+            </data>
+            """ % column_name, inherit_xml_id='base.view_partner_form')
+        except ValueError:
+            util.add_to_migration_reports("""
+                The fax field has been removed from the contact form in Odoo 11.0.
+                Nevertheless, we detected you were using it
+                and we therefore automatically preserved your data in a custom field "%s".
+                Unfortunately, we were not able to add this field automatically in your contact form view,
+                so you might want to add it manually yourself.
+            """ % column_name, "Removed fields")
+    else:
+        util.remove_field(cr, 'res.partner', 'fax')
+        util.remove_field(cr, 'res.users', 'fax')
+
     # ir.values,value text -> binary
     cr.execute("SELECT character_set_name FROM information_schema.character_sets")
     charset, = cr.fetchone()
