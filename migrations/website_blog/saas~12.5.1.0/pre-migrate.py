249c8a926799c378fbaf71dec87501e8f796648f
Paul Morelle <pmo@odoo.com>
2020-02-05 14:59:50 +0100

    [FIX] website_blog@saas~12.5: upgrade cover classes
    Cover-related CSS classes have changed from saas~12.5:
    - records that have a cover used to have `cover` and now have
      `o_record_has_cover`
    - records whose cover occupied the full browser used to have
      `container-fluid cover_full` and now just have `cover_full`
    - records whose cover occupied the half the browser window used to have
      `container-fluid cover_narrow` and now just have `cover_mid`
    - records whose cover occupied even a smaller space used to have
      `container cover_narrow` and now just have `cover_auto`
    
    This commit tells the migration how to replace the classes in each blog
    post.

--- migrations/website_blog/saas~12.5.1.0/pre-migrate.py
+++ migrations/website_blog/saas~12.5.1.0/pre-migrate.py
@@ -19,9 +19,64 @@ def migrate(cr, version):
 
     # update "resize_class" in blog_post.cover_properties json
     cr.execute(
-        """
+        # bb0cdec renamed .cover to .o_wblog_has_cover
+        # 08cc073 replaced .o_wblog_has_cover by the more general .o_record_has_cover
+        # empty resize_class are initialized with 'o_record_has_cover cover_mid' if there is a background image
+        r"""
         UPDATE blog_post
-           SET cover_properties = jsonb_pretty(jsonb_set(cover_properties::jsonb, '{resize_class}', '"cover_mid"'))
-         WHERE cover_properties::json->>'resize_class' = ''
-    """
-    )
+            SET cover_properties = jsonb_set(cover_properties::jsonb, '{resize_class}',
+                to_jsonb(REGEXP_REPLACE(
+                    coalesce(cover_properties::json->>'resize_class',
+                        case when cover_properties::json->>'background-image' <> 'none' then 'o_record_has_cover cover_mid'
+                        else '' end
+                    ),
+                    '\m(cover(?!-)|o_wblog_has_cover)\M', 'o_record_has_cover', 'g'
+                ))
+            );
+    """)
+    cr.execute(
+        # bb0cdec renamed the cover size 'container-fluid cover_full' -> 'cover_full'
+        # => Remove container-fluid when resize_class contains 'container-fluid' and 'cover_full'
+        r"""
+        UPDATE blog_post
+            SET cover_properties = jsonb_set(cover_properties::jsonb, '{resize_class}',
+                to_jsonb(REGEXP_REPLACE(
+                    cover_properties::json->>'resize_class',
+                    '\mcontainer-fluid\M', '', 'g')
+            )
+            WHERE cover_properties::json->>'resize_class' ~ '\mcontainer-fluid\M'
+                AND cover_properties::json->>'resize_class' ~ '\mcover_full\M';
+
+    """)
+    cr.execute(
+        # bb0cdec renamed the cover size 'container-fluid cover_narrow' -> 'cover_mid'
+        # => Remove container-fluid and rename 'cover_narrow' to 'cover_mid'
+        # when resize_class contains 'container-fluid' and 'cover_narrow'
+        r"""
+        UPDATE blog_post
+            SET cover_properties = jsonb_set(cover_properties::jsonb, '{resize_class}',
+                to_jsonb(REGEXP_REPLACE(REGEXP_REPLACE(
+                    cover_properties::json->>'resize_class',
+                    '\mcontainer-fluid\M', '', 'g'),
+                    '\mcover_narrow\M', 'cover_mid', 'g')
+                )
+            )
+            WHERE cover_properties::json->>'resize_class' ~ '\mcontainer-fluid\M'
+                AND cover_properties::json->>'resize_class' ~ '\mcover_narrow\M';
+    """)
+    cr.execute(
+        # bb0cdec renamed the cover size 'container cover_narrow' -> 'cover_auto'
+        # => Remove container and rename 'cover_narrow' to 'cover_auto'
+        # when resize_class contains 'container' and 'cover_narrow'
+        r"""
+        UPDATE blog_post
+            SET cover_properties = jsonb_set(cover_properties::jsonb, '{resize_class}',
+                to_jsonb(REGEXP_REPLACE(REGEXP_REPLACE(
+                    cover_properties::json->>'resize_class',
+                    '\mcontainer(?!-)\M', '', 'g'),
+                    '\mcover_narrow\M', 'cover_auto', 'g')
+                )
+            )
+            WHERE cover_properties::json->>'resize_class' ~ '\mcontainer(?!-)\M'
+                AND cover_properties::json->>'resize_class' ~ '\mcover_narrow\M';
+    """)
