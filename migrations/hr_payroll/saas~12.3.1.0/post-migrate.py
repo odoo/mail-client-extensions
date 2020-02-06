7dcb091e64bb83676a741526996b3740b0f9c799
Christophe Simonis <chs@odoo.com>
2020-02-06 16:52:26 +0100

    [FIX] hr_payroll@saas~12.3: force country of created structure type

--- migrations/hr_payroll/saas~12.3.1.0/post-migrate.py
+++ migrations/hr_payroll/saas~12.3.1.0/post-migrate.py
@@ -9,7 +9,11 @@ def migrate(cr, version):
     type_id = util.ref(cr, "hr_payroll.structure_type_employee")
     if not type_id:
         # Since saas~12.5, default type "employee" as been moved to demo data. See odoo/enterprise#4584
-        type_id = util.env(cr)["hr.payroll.structure.type"].create({"name": "Employee"}).id
+        type_id = (
+            util.env(cr)["hr.payroll.structure.type"]
+            .create({"name": "Employee", "country_id": False})
+            .id
+        )
 
     cr.execute("UPDATE hr_payroll_structure SET type_id=%s WHERE type_id IS NULL", [type_id])
     cr.execute('ALTER TABLE "hr_payroll_structure" ALTER COLUMN "type_id" SET NOT NULL')
