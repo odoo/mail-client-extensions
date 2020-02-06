493836863b83d391431dc463a478dd9ee7404169
Thibault Francois <tfr@openerp.com>
2020-02-06 11:32:33 +0100

    [FIX] base@saas~12.5: correct db-specific script
    Handle design of event and blog pages
    
    
    Co-authored-by: Jeremy Kersten <jke@odoo.com>
    Co-authored-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~12.5.1.3/end-00-db-specifix.py
+++ migrations/base/saas~12.5.1.3/end-00-db-specifix.py
@@ -10,6 +10,41 @@ def _openerp(cr, version):
          WHERE company_id = 1
     """
     )
+    cr.execute(
+        """
+        UPDATE event_event
+           SET cover_properties = REPLACE(REPLACE(cover_properties,
+                                                  'cover_mid',
+                                                  'o_half_screen_height o_record_has_cover'),
+                                          '"background-image": "none"',
+                                          '"background-image": "' || custom_banner_url || '"')
+    """
+    )
+    cr.execute(
+        """
+        UPDATE ir_ui_view
+           SET active = false
+         WHERE id IN (SELECT res_id
+                        FROM ir_model_data
+                       WHERE model = 'ir.ui.view'
+                         AND module IN ('website_event', 'website_blog')
+                         AND name IN ('opt_index_sidebar', 'opt_blog_post_sidebar',
+                                      'view_blog_post_list', 'opt_posts_loop_show_teaser')
+                     )
+    """
+    )
+    cr.execute(
+        """
+        UPDATE ir_ui_view
+           SET active = true
+         WHERE id IN (SELECT res_id
+                        FROM ir_model_data
+                       WHERE model = 'ir.ui.view'
+                         AND module IN ('website_event', 'website_blog')
+                         AND name = 'opt_posts_loop_show_cover'
+                     )
+    """
+    )
 
 
 def migrate(cr, version):
