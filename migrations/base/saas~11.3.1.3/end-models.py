34b92472546fd55992099a37407b1eeab1b5b6c5
Denis Ledoux <dle@odoo.com>
2020-02-07 13:55:20 +0000

    [FIX] base@saas~11.3.1.3: do not recompute invalid custom related field
    Fixes #734
    
    closes odoo/upgrade#742
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/base/saas~11.3.1.3/end-models.py
+++ migrations/base/saas~11.3.1.3/end-models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo.addons.base.maintenance.migrations import util
+from odoo.exceptions import UserError
 
 
 def migrate(cr, version):
@@ -12,9 +13,22 @@ def migrate(cr, version):
            AND related_field_id IS NULL
       GROUP BY model
     """)
+    fields_left = env["ir.model.fields"]
     for model, ids in cr.fetchall():
+        fields = env["ir.model.fields"].browse(ids)
         if model in env:
-            env["ir.model.fields"].browse(ids)._compute_related_field_id()
+            for field in fields:
+                try:
+                    field._compute_related_field_id()
+                # UserError: Unknown field name '..' in related field
+                # KeyError: model of field.relation not in registry models
+                except (UserError, KeyError):
+                    fields_left |= field
         else:
-            # TODO handle manually unknow models
-            pass
+            fields_left |= fields
+    if fields_left:
+        fields_desc_list = ', '.join('%s.%s' % (field.model, field.name) for field in fields)
+        util.add_to_migration_reports("""
+            Some custom related fields have not been computed because their model or fields they rely on
+            are defined in modules which were not available during the upgrade: %s
+        """ % fields_desc_list, "Custom fields")
