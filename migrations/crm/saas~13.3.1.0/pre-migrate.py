d6031be7cf076bba5e9208be153b1f2c9566865d
Thibault Delavallée <tde@odoo.com>
2020-02-10 15:51:07 +0000

    [IMP] crm@saas~13.3, crm_helpdesk@saas~13.3: add migration scripts for crm.partner.binding removal
    PURPOSE
    
    This commit is linked to a community refactoring that removes the crm.partner.
    binding mixin. Indeed it defines a mixin for unnecessary fields, generally
    either redefined in inheriting models, either not used at all. It also holds
    an utility method we moved directly on crm.lead model.
    
    SPECIFICATIONS
    
    crm.partner.binding defines two fields: partner_id and action.
    
    In crm
      * crm.lead2opportunity.partner: fields still exist and lead_id is added
      * crm.lead2opportunity.partner.mass: fields still exist and lead_id is added
        through inherit
      * crm.lead.convert2ticket: fields still exist
    
    In crm_helpdesk
      * crm.lead.convert2ticket: action is not used and field is removed
    
    LINKS
    
    Part of Task ID 2056759 (remove crm.partner.binding mixin)
    Part of Task ID 2088565 (crm onchange -> compute)
    Community PR odoo/odoo#44970
    Enterprise PR odoo/enterprise#8307
    Upgrade PR odoo/upgrade#750
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/crm/saas~13.3.1.0/pre-migrate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, 'crm_lead2opportunity_partner', 'lead_id', 'int4')
+    util.create_column(cr, 'crm_lead2opportunity_partner_mass', 'lead_id', 'int4')
+    util.remove_model(cr, 'crm.partner.binding')
