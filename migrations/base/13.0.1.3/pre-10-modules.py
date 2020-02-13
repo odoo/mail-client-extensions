c7b61c98a137af4f1d0c1105dd784c82f49e7a59
Christophe Simonis <chs@odoo.com>
2020-02-13 13:24:33 +0000

    [FIX] base@13.0: mew modules (deps)
    See odoo/enterprise@18d052acc6cb332139f0a210309c365100159b73
    
    closes odoo/upgrade#779
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/13.0.1.3/pre-10-modules.py
+++ migrations/base/13.0.1.3/pre-10-modules.py
@@ -7,6 +7,8 @@ def migrate(cr, version):
     util.new_module(cr, "l10n_lt", deps={"l10n_multilang"})
     util.new_module(cr, "l10n_dk", deps={"account", "base_iban", "base_vat"})
     if util.has_enterprise():
+        util.new_module(cr, "pos_hr_l10n_be", deps={"pos_hr", "pos_blackbox_be"}, auto_install=True)
+        util.module_deps_diff(cr, "pos_blackbox_be", plus={"pos_iot"})
         util.module_deps_diff(cr, "crm_enterprise", plus={"web_map"})
         util.merge_module(cr, "l10n_mx_edi_payment", "l10n_mx_edi")
         util.merge_module(cr, "account_reports_cash_flow", "account_reports")
