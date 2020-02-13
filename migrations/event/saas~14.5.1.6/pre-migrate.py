8b80d47e8a0ccd0e0be4eb03fe394cda9d8688ac
Debauche Stéphane <std@odoo.com>
2020-02-13 14:27:31 +0000

    [FIX] event(_sms)/saas~14.5: use a reference field the the template
    Purpose
    =======
    The template fields (template_id and sms_template_id) have
    been replaced by a unique reference field.
    
    Task 2127615
    PR #848
    See odoo/odoo/pull/46304
    See odoo/enterprise/pull/7701

--- migrations/event/saas~14.5.1.6/pre-migrate.py
+++ migrations/event/saas~14.5.1.6/pre-migrate.py
@@ -8,3 +8,26 @@ def migrate(cr, version):
     util.remove_field(cr, "event.type", "use_mail_schedule")
     util.remove_field(cr, "event.type", "use_timezone")
     util.create_column(cr, "event_type", "note", "text")
+
+    # move the M2O field `template_id` to the new reference field `template_ref`
+    for table_name in ["event_mail", "event_type_mail"]:
+        # Remove inconsistent records from the database
+        cr.execute(
+            f"""
+        DELETE FROM {table_name}
+              WHERE notification_type = 'mail'
+                AND template_id IS NULL
+         """,
+        )
+
+        util.create_column(cr, table_name, "template_ref", "varchar")
+
+        cr.execute(
+            f"""
+        UPDATE {table_name}
+           SET template_ref = CONCAT('mail.template,', template_id)
+         WHERE notification_type = 'mail'
+         """,
+        )
+        model_name = util.model_of_table(cr, table_name)
+        util.remove_field(cr, model_name, "template_id")
---
+++ migrations/event_sms/saas~14.5.1.0/pre-migrate.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # remove views
+    util.remove_view(cr, "event_sms.event_mail_view_tree")
+    util.remove_view(cr, "event_sms.event_mail_view_form")
+    util.remove_view(cr, "event_sms.event_event_view_form_inherit_sms")
+    util.remove_view(cr, "event_sms.event_type_view_form")
+
+    # move the M2O field `template_id` to the new reference field `template_ref`
+    for table_name in ["event_mail", "event_type_mail"]:
+        # Remove inconsistent records from the database
+        cr.execute(
+            f"""
+        DELETE FROM {table_name}
+              WHERE notification_type = 'sms'
+                AND sms_template_id IS NULL
+         """,
+        )
+
+        cr.execute(
+            f"""
+        UPDATE {table_name}
+           SET template_ref = CONCAT('sms.template,', sms_template_id)
+         WHERE notification_type = 'sms'
+         """,
+        )
+        model_name = util.model_of_table(cr, table_name)
+        util.remove_field(cr, model_name, "sms_template_id")
