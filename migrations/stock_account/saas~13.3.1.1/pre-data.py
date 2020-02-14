183e3015c03598ae63f573ff4f26510ee845d545
Rémy Voet (ryv) <ryv@odoo.com>
2020-02-14 12:23:09 +0000

    [IMP] stock_account@saas~13.3: clean unused actions/views
    Remove unused action and view of stock_account of
    stock.valuation.layer model.
    
    See odoo/odoo#44109

---
+++ migrations/stock_account/saas~13.3.1.1/pre-data.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, xml_id="stock_account.stock_valuation_layer_search")
+    util.remove_record(cr, "stock_account.action_stock_inventory_valuation")
