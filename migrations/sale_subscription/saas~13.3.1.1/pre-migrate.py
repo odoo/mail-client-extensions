07f73fc79bb8d911062ffe779432512d91e80ac4
Victor Feyens <vfe@odoo.com>
2020-02-17 13:48:21 +0000

    [ADD] sale_subscription@saas~13.3: duplicate rules removal
    Rules already defined in sales module.
    
    See odoo/enterprise#8213
    
    closes odoo/upgrade#790
    
    Related: odoo/odoo#44719
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/sale_subscription/saas~13.3.1.1/pre-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_record(cr, "sale_subscription.payment_transaction_salesman_rule")
+    util.remove_record(cr, "sale_subscription.payment_token_salesman_rule")
