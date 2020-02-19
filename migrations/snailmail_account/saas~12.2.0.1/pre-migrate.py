e9a97634ddc36d55a0017f62b1a1caa11436f65e
ysh-odoo <ysh@odoo.com>
2020-02-19 06:46:27 +0000

    [FIX] snailmail_account@saas~12.2: Field letter_ids is available in vsaas12.3 in module account and accessed in related through model mail.compose.message - composer_id.
    closes odoo/upgrade#805
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/snailmail_account/saas~12.2.0.1/pre-migrate.py
+++ migrations/snailmail_account/saas~12.2.0.1/pre-migrate.py
@@ -4,5 +4,5 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.remove_column(cr, "account_invoice_send", "snailmail_cost")  # field is now computed
-    util.remove_field(cr, "account.invoice.send", "letter_ids")
+    # m2m field `letter_ids` has been removed but a o2m field with the same name is created indirectly via the inherits on mail.compose.message.
     cr.execute("DROP TABLE snailmail_letter_account_invoice_send_rel")
