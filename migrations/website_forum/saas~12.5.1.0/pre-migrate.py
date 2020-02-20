b49a010357f8660da4b391a89ea022215a7ae310
Rémi Rahir <rar@odoo.com>
2020-02-20 12:16:05 +0000

    [FIX] website_forum@saas-12.5
    THe default value on field mode is 'questions' and not 'discussions'.
    
    closes odoo/upgrade#815
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website_forum/saas~12.5.1.0/pre-migrate.py
+++ migrations/website_forum/saas~12.5.1.0/pre-migrate.py
@@ -5,7 +5,7 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.create_column(cr, "forum_forum", "sequence", "int4")
     util.create_column(cr, "forum_forum", "mode", "varchar")
-    cr.execute("UPDATE forum_forum SET sequence=id, mode='discussions'")
+    cr.execute("UPDATE forum_forum SET sequence=id, mode='questions'")
 
     util.remove_view(cr, "website_forum.res_users_view_form_preference")
     util.remove_view(cr, "website_forum.forum_user_tooltip")
