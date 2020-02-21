22c7c3680369e384c45b3a773ce18845a4a271dd
Christophe Simonis <chs@odoo.com>
2020-02-21 16:56:44 +0100

    [FIX] hr_payroll@saas~12.3: correct calls to `remove_field`

--- migrations/hr_payroll/saas~12.3.1.0/pre-30-fieldsremoval.py
+++ migrations/hr_payroll/saas~12.3.1.0/pre-30-fieldsremoval.py
@@ -3,8 +3,8 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    util.remove_field(cr, "hr_contract", 'struct_id')
-    util.remove_field(cr, 'hr_payslip_line', 'register_id', 'int4')
+    util.remove_field(cr, "hr.contract", "struct_id")
+    util.remove_field(cr, "hr.payslip.line", "register_id")
     util.remove_field(cr, 'hr.payroll.structure', 'code')
     util.remove_field(cr, 'hr.payroll.structure', 'parent_id')
     util.remove_field(cr, 'hr.payroll.structure', 'children_ids')
