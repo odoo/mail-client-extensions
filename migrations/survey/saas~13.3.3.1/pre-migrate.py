0684d6095545edd12f853f994cf2c97bc026ca20
Tiffany Chang (tic) <tic@odoo.com>
2020-02-24 09:03:49 +0000

    [REF] survey@saas~13.3: add new scoring capabilities on survey questions
    PURPOSE
    
    Allow to score more question types in addition to single and multiple choice
    in survey: numerical_box, date and datetime are now scoreable.
    
    SPECIFICATIONS
    
    Adds 6 new columns to support new scorable question types (store correct values,
    computed question is_scored_question column and store answer answer_is_correct).
    is_scored_question and answer_is_correct are bootstrapped to take into account
    existing scored questions (simple / multiple choices).
    
    Additionally, one view ID is renamed to better reflect its updated
    contents.
    
    LINKS
    
    Task ID 2061937
    Community PR odoo/odoo#43014
    Upgrade PR odoo/upgrade#835
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/survey/saas~13.3.3.1/pre-migrate.py
+++ migrations/survey/saas~13.3.3.1/pre-migrate.py
@@ -12,3 +12,36 @@ def migrate(cr, version):
     util.create_column(cr, "survey_question", "is_conditional", "boolean")
     util.create_column(cr, "survey_question", "triggering_question_id", "int4")
     util.create_column(cr, "survey_question", "triggering_answer_id", "int4")
+
+    # scoring improvement
+    util.create_column(cr, "survey_question", "answer_numerical_box", "float8")
+    util.create_column(cr, "survey_question", "answer_date", "date")
+    util.create_column(cr, "survey_question", "answer_datetime", "timestamp without time zone")
+    util.create_column(cr, "survey_question", "answer_score", "float8")
+    util.create_column(cr, "survey_question", "is_scored_question", "bool")
+    # when bootstrapping no question is scored as it is activated by user on new questions
+    cr.execute("""
+        UPDATE survey_question question
+        SET is_scored_question = CASE
+            WHEN survey.scoring_type = 'no_scoring' THEN FALSE
+            WHEN question.question_type = 'simple_choice' THEN TRUE
+            WHEN question.question_type = 'multiple_choice' THEN TRUE
+            ELSE FALSE END
+        FROM survey_survey survey
+        WHERE question.survey_id = survey.id
+    """)
+
+    util.create_column(cr, "survey_user_input_line", "answer_is_correct", "bool")
+    # previously only answer_type='suggestion' could be correct/incorrect
+    # => make all previously existing non-suggestion answer_types FALSE
+    cr.execute("""
+        UPDATE survey_user_input_line input_line
+        SET answer_is_correct = CASE
+            WHEN input_line.answer_type = 'suggestion' THEN answer.is_correct
+            ELSE FALSE
+            END
+        FROM survey_question_answer answer
+        WHERE input_line.suggested_answer_id = answer.id
+    """)
+
+    util.rename_xmlid(cr, "survey.question_result_number", "survey.question_result_number_or_date")
