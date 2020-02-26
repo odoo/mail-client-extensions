ed83aecf3a39269bd7fec7784d0385ae1911be8c
Christophe Simonis <chs@odoo.com>
2020-02-26 11:53:18 +0100

    [ADD] hr_holidays@saas~13.1

---
+++ migrations/hr_holidays/saas~13.1.1.5/pre-migrate.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "hr_leave", "private_name", "varchar")
+    util.create_column(cr, "hr_leave_allocation", "private_name", "varchar")
+    cr.execute("UPDATE hr_leave SET private_name = name WHERE private_name IS NULL")
+    cr.execute("UPDATE hr_leave_allocation SET private_name = name WHERE private_name IS NULL")
+
+    util.rename_field(cr, "hr.leave.type", "validation_type", "leave_validation_type")
+    util.create_column(cr, "hr_leave_type", "allocation_validation_type", "varchar")
+    cr.execute(
+        """
+        UPDATE hr_leave_type
+           SET allocation_validation_type = leave_validation_type
+         WHERE allocation_validation_type IS NULL
+    """
+    )
