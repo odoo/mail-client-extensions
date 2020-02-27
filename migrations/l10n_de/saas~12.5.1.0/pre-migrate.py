7a94bbad53eca343bfa97083512f6c9d1402bf51
Nicolas Seinlet <nse@odoo.com>
2020-02-27 10:03:49 +0000

    [FIX] l10n_de@saas~12.5: keep used country states
    Fixes #843
    
    closes odoo/upgrade#859
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/l10n_de/saas~12.5.1.0/pre-migrate.py
+++ migrations/l10n_de/saas~12.5.1.0/pre-migrate.py
@@ -4,5 +4,4 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     de = "bw by be bb hb hh he mv ni nw rp sl sn st sh th"
-    for state in de.split():
-        util.remove_record(cr, "l10n_de.state_de_" + state)
+    util.delete_unused(cr, "res_country_state", ["l10n_de.state_de_" + state for state in de.split()])
