1284cdd5ca5350706309282c060360ecf1670888
Thibault Delavallée <tde@odoo.com>
2020-03-02 14:11:04 +0000

    [IMP] sms@saas~13.3: update to composer model cleaning
    To prepare future update of sms composer, some cleaning is performed in the
    composer: fields renamed to better indicate their use (single recipient mode
    notably), unused fields removed.
    
    LINKS
    
    Task ID 2088303
    Community PR odoo/odoo#40482
    Upgrade PR odoo/upgrade#873

--- migrations/sms/saas~13.3.2.0/pre-migrate.py
+++ migrations/sms/saas~13.3.2.0/pre-migrate.py
@@ -4,6 +4,8 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    # Template preview model
+    # ----------------------------------------------------------------------
     # Remove fields linked to _inherit of sms.template removal
     util.remove_field(cr, "sms.template.preview", "name")
     util.remove_field(cr, "sms.template.preview", "model")
@@ -17,3 +19,11 @@ def migrate(cr, version):
     util.remove_field(cr, "sms.template.preview", "res_id")
     # Remove column linked to _inherit of sms.template removal, now a computed field
     util.remove_column(cr, "sms_template_preview", "body")
+
+    # Composer model
+    # ----------------------------------------------------------------------
+    util.remove_field(cr, "sms.composer", "partner_ids")
+    util.rename_field(cr, "sms.composer", "recipient_count", "recipient_valid_count")
+    util.rename_field(cr, "sms.composer", "recipient_description", "recipient_single_description")
+    # force view update as it is quite changed
+    util.force_noupdate(cr, "sms.sms_composer_view_form", False)
