1561c6fd26375bba1d4960645e7a55eec70d9631
Thibault Delavallée <tde@odoo.com>
2020-03-02 11:05:50 +0000

    [IMP] mass_mailing{_sms}@saas~13.3: update to merge contact list views to have coherent views
    Also perform some renaming in views.
    
    Task ID 2088303
    Community PR odoo/odoo#40482
    Upgrade PR odoo/upgrade#873

--- migrations/mass_mailing/saas~13.3.2.1/pre-migrate.py
+++ migrations/mass_mailing/saas~13.3.2.1/pre-migrate.py
@@ -15,3 +15,10 @@ def migrate(cr, version):
     util.remove_field(cr, 'utm.campaign', 'opened')
     util.remove_field(cr, 'utm.campaign', 'replied')
     util.remove_field(cr, 'utm.campaign', 'bounced')
+
+    util.rename_xmlid(cr, 'mass_mailing.view_mail_mass_mailing_contact_search', 'mass_mailing.mailing_contact_view_search')
+    util.rename_xmlid(cr, 'mass_mailing.view_mail_mass_mailing_contact_form', 'mass_mailing.mailing_contact_view_form')
+    util.rename_xmlid(cr, 'mass_mailing.view_mail_mass_mailing_contact_tree', 'mass_mailing.mailing_contact_view_tree')
+    util.rename_xmlid(cr, 'mass_mailing.view_mail_mass_mailing_contact_kanban', 'mass_mailing.mailing_contact_view_kanban')
+    util.rename_xmlid(cr, 'mass_mailing.view_mail_mass_mailing_contact_pivot', 'mass_mailing.mailing_contact_view_pivot')
+    util.rename_xmlid(cr, 'mass_mailing.view_mail_mass_mailing_contact_graph', 'mass_mailing.mailing_contact_view_graph')
