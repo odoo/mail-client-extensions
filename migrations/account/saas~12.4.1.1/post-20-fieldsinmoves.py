ea6554089dabf5875fd7ed8897caa6513b89327e
Nicolas Seinlet <nse@odoo.com>
2020-03-02 08:02:44 +0000

    [FIX] account@saas~12.4
    Custom fields should be transfered in post to avoid "missing column" 
    exception Issue #824
    
    closes odoo/upgrade#830
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/saas~12.4.1.1/post-20-fieldsinmoves.py
+++ migrations/account/saas~12.4.1.1/post-20-fieldsinmoves.py
@@ -8,15 +8,32 @@ def _get_linking(table):
     return "INNER JOIN invl_aml_mapping map ON map.invl_id=inv.id WHERE am.id = map.aml_id"
 
 
+def _get_model_id(cr, model):
+    cr.execute("SELECT id FROM ir_model WHERE model=%s", (model,))
+    return cr.fetchone()[0]
+
+
 def migrate(cr, version):
+    cr.execute(
+        """
+        SELECT name, src_model, dst_model
+          FROM mig_s124_accountfieldstotransfer
+         WHERE state='manual'
+        """
+    )
+    for field, src_model, dst_model in cr.fetchall():
+        cr.execute(
+            "UPDATE ir_model_fields SET model=%s, model_id=%s WHERE model=%s AND name=%s RETURNING id",
+            (dst_model, _get_model_id(cr, dst_model), src_model, field),
+        )
     cr.execute(
         """
         SELECT name, ttype, src_model, dst_model
           FROM mig_s124_accountfieldstotransfer
-         WHERE ttype='custom'
+         WHERE ttype='custom' or state='manual'
         """
     )
-    for field, type, src_model, dst_model in [(p[0], p[1], p[2], p[3]) for p in cr.fetchall()]:
+    for field, type, src_model, dst_model in cr.fetchall():
         src_table = util.table_of_model(cr, src_model)
         dst_table = util.table_of_model(cr, dst_model)
         if type == "many2many":
@@ -25,9 +42,9 @@ def migrate(cr, version):
             util.create_column(cr, dst_table, field, util.column_type(cr, src_table, field))
             cr.execute(
                 """
-                UPDATE %(dst_table)s am
-                   SET %(field)s=inv.%(field)s
-                  FROM %(src_table)s inv
+                UPDATE "%(dst_table)s" am
+                   SET "%(field)s"=inv."%(field)s"
+                  FROM "%(src_table)s" inv
                   %(link)s
                 """
                 % {"dst_table": dst_table, "src_table": src_table, "field": field, "link": _get_linking(src_table)}
