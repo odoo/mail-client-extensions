c113c989cf94cadf0bd0be74f6ffbd5daa4fdbd1
David Beguin <dbe@odoo.com>
2020-03-05 14:50:41 +0000

    [IMP] survey@saas~13.3: add conditional questions columns
    Add the 3 columns needed for conditional questions configuration
    
      * is_conditional: mark the question as depending from another;
      * triggering_question_id: id of the question containing the answer
        that will trigger the display of the current question;
      * triggering_answer_id: id of the answer that will, if selected, trigger
        the display of the current question;
    
    Task ID 23509
    See odoo/odoo#44950
    
    closes odoo/upgrade#906
    
    Related: odoo/odoo#44950
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/survey/saas~13.3.3.1/pre-migrate.py
+++ migrations/survey/saas~13.3.3.1/pre-migrate.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
-from odoo.addons.base.maintenance.migrations import util
+
+from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    # session support
     util.rename_field(cr, 'survey.survey', 'session_show_ranking', 'session_show_leaderboard')
     util.create_column(cr, 'survey_survey', 'session_start_time', 'timestamp without time zone')
+
+    # conditional questions support
+    util.create_column(cr, "survey_question", "is_conditional", "boolean")
+    util.create_column(cr, "survey_question", "triggering_question_id", "int4")
+    util.create_column(cr, "survey_question", "triggering_answer_id", "int4")
