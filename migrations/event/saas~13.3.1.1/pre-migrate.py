08d7c7af384e33937d1a51706bbb742841a61b62
Patrick Hoste <pko@odoo.com>
2020-03-05 15:41:09 +0000

    [IMP] {event, website_event_track_gantt}@saas~13.3: change event form view display and remove duplicate view in website_event_track
    - create a new boolean field 'seats_limited' in event_event, event_type_ticket and event_event_ticket models
    - remove the selection field 'seats_availability' in event_event, event_type_ticket and event_event_ticket models
    - remove the boolean field 'is_online' in event_event and event_type models
    - remove a duplicate view in website_event_track_gantt module
    
    see odoo/odoo#46659
    see odoo/enterprise#8954
    
    closes odoo/upgrade#1002
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/event/saas~13.3.1.1/pre-migrate.py
+++ migrations/event/saas~13.3.1.1/pre-migrate.py
@@ -5,11 +5,33 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "event.type", "default_registration_min")
+    util.remove_field(cr, "event.type", "is_online")
+    util.rename_field(cr, "event.type", "default_registration_max", "seats_max")
     util.remove_field(cr, "event.event", "seats_min")
+    util.remove_field(cr, "event.event", "is_online")
+    util.create_column(cr, "event_event", "seats_limited", "boolean")
+    cr.execute("""
+        UPDATE event_event AS event
+        SET seats_limited = (event.seats_availability = 'limited')
+    """)
+    util.remove_field(cr, "event.event", "seats_availability")
+
+    util.create_column(cr, "event_type_ticket", "seats_limited", "boolean")
+    cr.execute("""
+        UPDATE event_type_ticket AS ticket
+        SET seats_limited = (ticket.seats_availability = 'limited')
+    """)
+    util.remove_field(cr, "event.type.ticket", "seats_availability")
+
+    util.create_column(cr, "event_event_ticket", "seats_limited", "boolean")
+    cr.execute("""
+        UPDATE event_event_ticket AS ticket
+        SET seats_limited = (ticket.seats_availability = 'limited')
+    """)
+    util.remove_field(cr, "event.event.ticket", "seats_availability")
 
     util.delete_unused(cr, "event.event_type_data_physical")
     util.rename_xmlid(cr, "website_event_track.event_type_data_tracks", "event.event_type_data_conference")
-    util.rename_field(cr, "event.type", "default_registration_max", "seats_max")
 
     util.remove_field(cr, "event.registration", "origin")
     for old_name in ["campaign_id", "source_id", "medium_id"]:
