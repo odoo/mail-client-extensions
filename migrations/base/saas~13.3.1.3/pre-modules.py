43a386ae1266a4ada512baed5701f7f12a24b55d
Victor Feyens <vfe@odoo.com>
2020-03-09 14:52:33 +0000

    [IMP] base@saas~13.3: sale_coupon dependency (sale_management --> sale).
    See odoo/odoo#46416
    
    closes odoo/upgrade#861
    
    Related: odoo/odoo#46416
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~13.3.1.3/pre-modules.py
+++ migrations/base/saas~13.3.1.3/pre-modules.py
@@ -5,3 +5,4 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.new_module(cr, "mrp_landed_costs", deps={"stock_landed_costs", "mrp"}, auto_install=True)
+    util.module_deps_diff(cr, "sale_coupon", plus={"sale"}, minus={"sale_management"})
