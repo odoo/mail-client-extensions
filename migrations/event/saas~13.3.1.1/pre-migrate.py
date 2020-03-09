8594adaf8abdb9832cec370bee5df10e299e80c0
Thibault Delavallée <tde@odoo.com>
2020-03-09 16:14:12 +0000

    [IMP] event@saas~13.3: update event type data and rename event type default_registration_max to seats_max
    Purpose is to
    
      * have clean a bit event type data, notably having them defined in event
        (updated in other modules) and less demo / data;
      * have matching names between event type and event to ease code understanding;,
    
    Task ID 2089156
    See community PR odoo/odoo#42911
    
    closes odoo/upgrade#912
    
    Related: odoo/odoo#42911
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/event/saas~13.3.1.1/pre-migrate.py
+++ migrations/event/saas~13.3.1.1/pre-migrate.py
@@ -6,3 +6,7 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "event.type", "default_registration_min")
     util.remove_field(cr, "event.event", "seats_min")
+
+    util.delete_unused(cr, "event.event_type_data_physical")
+    util.rename_xmlid(cr, "website_event_track.event_type_data_tracks", "event.event_type_data_conference")
+    util.rename_field(cr, "event.type", "default_registration_max", "seats_max")
