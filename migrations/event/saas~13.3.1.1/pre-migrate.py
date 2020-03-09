f50f7ade7a0b4013bb1a3a819e7208ff8723be3b
Thibault Delavallée <tde@odoo.com>
2020-03-09 12:07:46 +0000

    [REF] {base, event}@saas~13.3: move registration UTMs fields in event and rename them
    PURPOSE
    
    Allow to use UTMs directly in event, notably to be able to sort or make
    statistics on registrations.
    
    SPECIFICATIONS
    
    Move all UTMs fields definition to event. In base event module they are
    set by hand or through links (when using mass mailing for example).Shared
    links with utm bits defined in it allows to track marketing statistics
    on registrations.
    
    In event_sale registrations can be linked to a sale order. UTM information
    are copied from the sale order to the registration itself. Order defined utm
    always erase default one coming from event.
    
    Also move utm module dependency in event to benefit from utm models.
    
    SPECIFICATIONS: ORIGIN FIELD
    
    Remove origin field. This field has no use, as explained in community commit.
    
    LINKS
    
    Task ID 2212297
    Community PR odoo/odoo#47229
    
    closes odoo/upgrade#913
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/event/saas~13.3.1.1/pre-migrate.py
+++ migrations/event/saas~13.3.1.1/pre-migrate.py
@@ -10,3 +10,8 @@ def migrate(cr, version):
     util.delete_unused(cr, "event.event_type_data_physical")
     util.rename_xmlid(cr, "website_event_track.event_type_data_tracks", "event.event_type_data_conference")
     util.rename_field(cr, "event.type", "default_registration_max", "seats_max")
+
+    util.remove_field(cr, "event.registration", "origin")
+    for old_name in ["campaign_id", "source_id", "medium_id"]:
+        util.move_field_to_module(cr, "event.registration", old_name, "event_sale", "event")
+        util.rename_field(cr, "event.registration", old_name, f"utm_{old_name}")
