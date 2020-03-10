a9b6ebc379fb466c532542844d9d6295f186ca9f
Antoine Vandevenne (anv) <anv@odoo.com>
2020-03-10 16:53:39 +0000

    [FIX] sale_amazon@saas~13.3: update marketplaces' tax policy
    See odoo/enterprise#9097
    
    closes odoo/upgrade#921
    
    Related: odoo/enterprise#9097
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/sale_amazon/saas~13.3.1.0/pre-marketplaces.py
@@ -0,0 +1,13 @@
+from odoo.upgrade import util
+
+def migrate(cr, version):
+    util.create_column(cr, "amazon_marketplace", "tax_included", "bool")
+    cr.execute(
+        """
+        UPDATE amazon_marketplace
+        SET tax_included = true
+        WHERE api_ref IN (
+            'A1PA6795UKMFR9', 'A1RKKUPIHCS9HS', 'A13V1IB3VIYZZH', 'APJ6JRA9NG5V4', 'A1F83G8C2ARO7P'
+        )
+        """
+    )
