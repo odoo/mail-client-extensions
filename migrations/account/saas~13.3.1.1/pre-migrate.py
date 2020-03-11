59ab4c3dae04a85402b5ed0391eacf75e208f3e7
Mitali Patel <mpa@odoo.com>
2020-03-11 06:59:49 +0000

    [IMP] account@saas~13.3: remove the incoterms list
    update incoterms data to noupdate true and set it as inactive because
    deleting from xml file will also deleted from system during upgrade
    
    Community PR: https://github.com/odoo/odoo/pull/43883
    
    closes odoo/upgrade#922
    
    Taskid: 2179236
    Related: odoo/odoo#43883
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/account/saas~13.3.1.1/pre-migrate.py
+++ migrations/account/saas~13.3.1.1/pre-migrate.py
@@ -6,3 +6,17 @@ def migrate(cr, version):
     util.rename_field(cr, "account.move", "type", "move_type")
     util.rename_field(cr, "account.invoice.report", "type", "move_type")
     util.rename_field(cr, "account.move.line", "tag_ids", "tax_tag_ids")
+
+    incoterm_codes = "DAF,DES,DEQ,DDU,DAT".split(",")
+
+    for code in incoterm_codes:
+        util.force_noupdate(cr, "account.incoterm_%s" % code)
+
+    cr.execute(
+        """
+        UPDATE account_incoterms
+        set active = 'f'
+        where id in %s
+    """,
+        [tuple(util.ref(cr, "account.incoterm_%s" % code) for code in incoterm_codes)],
+    )
