96b6acfe574211f3fed542d5d736160d00166769
Jérémy Hennecart <jeh@odoo.com>
2020-03-16 11:59:30 +0000

    [IMP] website_calendar@saas~14.3: add new fields, remove views and update data
    Adapt the old slots with the new field end_hour. This field allows to create
    multiple appointment slot with one record: (end_hour - start_hour) / duration
    
    Remove views that was no longer used and edit some views to a better naming
    
    Add new fields website_id and cover properties, which comes from the new inherits.
    
    task-2190526
    PR ENT: odoo/enterprise#9456

---
+++ migrations/website_calendar/saas~14.3.1.0/pre-migrate.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "calendar_appointment_type", "cover_properties", "text")
+    background = """{
+        'background-image': 'url("/website_calendar/static/src/img/appointment_cover_0.jpg")',
+        'resize_class': 'o_record_has_cover o_half_screen_height',
+        'opacity': '0.4'
+    }"""
+    cr.execute(
+        """
+            UPDATE calendar_appointment_type
+            SET cover_properties = %s
+        """,
+        (background,),
+    )
+    util.create_column(cr, "calendar_appointment_slot", "end_hour", "float8")
+    cr.execute(
+        """
+            UPDATE calendar_appointment_slot slot
+            SET end_hour = slot.hour + t.appointment_duration
+            FROM calendar_appointment_type t
+            WHERE t.id = slot.appointment_type_id
+        """
+    )
+    util.remove_view(cr, "website_calendar.appointment")
+    util.remove_view(cr, "website_calendar.index")
+    util.remove_view(cr, "website_calendar.setup")
