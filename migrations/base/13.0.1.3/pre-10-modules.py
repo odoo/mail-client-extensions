085febae2281324c346560ac48275812ad4a3ecb
Christophe Simonis <chs@odoo.com>
2020-03-17 13:52:06 +0000

    [FIX] base@13.0: new module
    closes odoo/upgrade#955
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/13.0.1.3/pre-10-modules.py
+++ migrations/base/13.0.1.3/pre-10-modules.py
@@ -6,9 +6,16 @@ def migrate(cr, version):
     util.new_module(cr, "l10n_il", deps={"account"})
     util.new_module(cr, "l10n_lt", deps={"l10n_multilang"})
     util.new_module(cr, "l10n_dk", deps={"account", "base_iban", "base_vat"})
+
+    # odoo/odoo@24b57a377fec77178aad2b433da8f743be9db747
     util.new_module(cr, "odoo_referral", deps={"base", "web"}, auto_install=True)
     util.new_module(cr, "odoo_referral_portal", deps={"website", "odoo_referral"}, auto_install=True)
+
     util.new_module(cr, "hr_holidays_calendar", deps={"hr_holidays", "calendar"}, auto_install=True)
+
+    # odoo/odoo@3d43e65b4333c5491375f49feffbd1370b2ab4f9
+    util.new_module(cr, "pos_six", deps={"point_of_sale"})
+
     if util.has_enterprise():
         util.new_module(cr, "pos_hr_l10n_be", deps={"pos_hr", "pos_blackbox_be"}, auto_install=True)
         util.module_deps_diff(cr, "pos_blackbox_be", plus={"pos_iot"})
