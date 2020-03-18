26160471200dae023dd4f19a7e01d9343a1bada1
SEINLET Nicolas <nicolas@seinlet.com>
2020-03-18 15:44:44 +0000

    [FIX] stock_dropshipping@saas~12.5
    closes odoo/upgrade#962
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/stock_dropshipping/saas~12.5.1.0/post-migrate.py
+++ migrations/stock_dropshipping/saas~12.5.1.0/post-migrate.py
@@ -3,6 +3,7 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
+    util.delete_unused(cr, "stock_dropshipping.picking_type_dropship")
     C = util.env(cr)["res.company"]
     C.create_missing_dropship_sequence()
     C.create_missing_dropship_picking_type()
