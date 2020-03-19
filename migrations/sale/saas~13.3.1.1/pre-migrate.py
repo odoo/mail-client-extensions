716eba04fd72dd0a9e6d3849a678a5b2078f1d8a
Victor Feyens <vfe@odoo.com>
2020-03-19 16:09:25 +0000

    [IMP] sale@saas~13.3: store sale_order currency_id
    See odoo/odoo#47999
    
    closes odoo/upgrade#963
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/sale/saas~13.3.1.1/pre-migrate.py
+++ migrations/sale/saas~13.3.1.1/pre-migrate.py
@@ -6,3 +6,11 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "sale.order", "remaining_validity_days")
     util.move_field_to_module(cr, "sale_order", "tag_ids", "sale_crm", "sale")
+    util.create_column(cr, "sale_order", "currency_id", "int4")
+
+    cr.execute("""
+        UPDATE sale_order s
+        SET currency_id = p.currency_id
+        FROM product_pricelist p
+        WHERE p.id = s.pricelist_id
+    """)
