d01922203bbdb091e0b8f9dc29e2521983276b4c
Thibault Delavallée <tde@odoo.com>
2020-03-20 13:36:37 +0000

    [REF] mail@saas~13.3: remove mail.address.mixin
    PURPOSE
    
    Perform some code cleaning in mail module
    
    SPECIFICATIONS
    
    mail.address.mixin is used only for mail.thread.blacklist mixin. Let us
    merge those two and simplify class organization.
    
    LINKS
    
    Task ID 2222033
    Community PR odoo/odoo#48115
    
    closes odoo/upgrade#967
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/mail/saas~13.3.1.0/pre-migrate.py
+++ migrations/mail/saas~13.3.1.0/pre-migrate.py
@@ -7,3 +7,4 @@ def migrate(cr, version):
     util.remove_field(cr, "res.users", "out_of_office_message")
     util.remove_field(cr, "mail.template", "user_signature")
     util.create_column(cr, "mail_alias", "alias_bounced_content", "text")
+    util.remove_model(cr, "mail.address.mixin", drop_table=False)
