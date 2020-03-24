241a6cf0082127585c271c28c33c394065d6575c
Nicolas Vannieuwerburgh <nvn@odoo.com>
2020-03-24 08:45:04 +0000

    [FIX] mrp@9.saas~12: simplify
    closes odoo/upgrade#976
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/mrp/9.saas~12.2.0/post-40-mrpproduction.py
+++ migrations/mrp/9.saas~12.2.0/post-40-mrpproduction.py
@@ -6,15 +6,5 @@ from collections import defaultdict
 def migrate(cr, version):
     cr.execute("""update stock_move set is_done=state=ANY(array['done', 'cancel'])""")
 
-    to_recompute = [
-        ['mrp.production', ('availability',)],
-        ['mrp.workcenter', ('working_state',)],
-    ]
-    for model, fields in to_recompute:
-        cr.execute("""SELECT id FROM %s""" % util.table_of_model(cr, model))
-        ids = [row[0] for row in cr.fetchall()]
-        util.recompute_fields(
-            cr, model,
-            fields=fields,
-            ids=ids,
-        )
+    util.recompute_fields(cr, "mrp.production", ("availability",))
+    util.recompute_fields(cr, "mrp.workcenter", ("working_state",))
