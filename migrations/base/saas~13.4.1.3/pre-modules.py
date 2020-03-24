c8b16bc281eb5373752b4f391abf39a528ba9ddd
Jérémy Hennecart <jeh@odoo.com>
2020-03-24 08:48:06 +0000

    [IMP] base@saas~13.4: add mass_mailing dependency on web_tour
    Add web_tour dependency to mass_mailing module, as we are adding a tour
    in mass mailing.
    
    Task ID 2194876
    Community PR odoo/odoo#48259
    Upgrade PR #983
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/base/saas~13.4.1.3/pre-modules.py
+++ migrations/base/saas~13.4.1.3/pre-modules.py
@@ -4,10 +4,12 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.remove_module(cr, "odoo_referral_portal")
+
     util.new_module_dep(cr, "crm_iap_lead", "partner_autocomplete")
     util.new_module_dep(cr, "crm_iap_lead_enrich", "partner_autocomplete")
-    util.remove_module(cr, "odoo_referral_portal")
     util.new_module(cr, "mail_bot_hr", deps={"mail_bot", "hr"}, auto_install=True)
+    util.module_deps_diff(cr, "mass_mailing", plus={"web_tour"})
     util.module_deps_diff(cr, "hr", minus={"mail_bot"})
     util.module_deps_diff(cr, "test_mail", minus={"mail_bot"})
     util.new_module_dep(cr, 'l10n_in', 'base_vat')
