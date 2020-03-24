901ac7b60497513bf0c4272c65a58b93cca59c61
dmonzonis <mod@odoo.com>
2020-03-24 11:54:26 +0000

    [IMP] website_event_track@saas~13.3: modifications to event sponsors
    Add new field 'active' to event.sponsor model
    
    See odoo/odoo#48217
    
    closes odoo/upgrade#981
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/website_event_track/saas~13.3.1.0/pre-migrate.py
+++ migrations/website_event_track/saas~13.3.1.0/pre-migrate.py
@@ -6,3 +6,7 @@ from odoo.upgrade import util
 def migrate(cr, version):
     # Removal of twitter hashtag leads to some views (templates) being removed
     util.remove_view(cr, "website_event_track.event_track_social")
+
+    # Allow archiving of sponsors and get sponsor URL from partner
+    util.create_column(cr, "event_sponsor", "active", "boolean")
+    cr.execute("UPDATE event_sponsor SET active = true")
