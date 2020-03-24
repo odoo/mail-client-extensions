2cdbc8f8072dcfeaccdd50ec6ad458200d1336ca
Yannick Tivisse <yti@odoo.com>
2020-03-24 13:18:43 +0000

    [REF] hr_contract@saas~13.3: Remove advantages field
    TaskID: 2222984
    Community PR: https://github.com/odoo/odoo/pull/48258
    Enterprise PR: https://github.com/odoo/enterprise/pull/9454
    
    closes odoo/upgrade#980
    
    Related: odoo/odoo#48258
    Related: odoo/enterprise#9454
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/hr_contract/saas~13.3.1.0/pre-migrate.py
+++ migrations/hr_contract/saas~13.3.1.0/pre-migrate.py
@@ -6,3 +6,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
 
     util.move_field_to_module(cr, "hr.contract", "structure_type_id", "hr_payroll", "hr_contract")
+    util.remove_field(cr, 'hr.contract', 'advantages')
