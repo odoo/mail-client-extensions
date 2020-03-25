05fec4eb2b32aee453ee697dfd108e902e39d0cf
Rémy Baranx (bar) <bar@odoo.com>
2020-03-25 07:49:50 +0000

    [FIX] sales_team@saas~13.3: do not move crm tag model if CRM is not installed
    This is related to a mistake done in the upgrade PR #947.
    
    If the CRM app is not installed, the crm.tag model shall not be moved
    by the sales_team migration script, but the script should let the ORM
    creating the model itself when installing sales_team.
    
    Task ID: #2224387
    
    closes odoo/upgrade#987
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/sales_team/saas~13.3.1.0/pre-migrate.py
+++ migrations/sales_team/saas~13.3.1.0/pre-migrate.py
@@ -4,5 +4,6 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.move_model(cr, "crm.lead.tag", "crm", "sales_team", move_data=True)
-    util.rename_model(cr, "crm.lead.tag", "crm.tag")
+    if util.module_installed(cr, "crm"):
+        util.move_model(cr, "crm.lead.tag", "crm", "sales_team", move_data=True)
+        util.rename_model(cr, "crm.lead.tag", "crm.tag")
