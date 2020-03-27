15fbc44c03c1f6559f998b9080c1f45ee1f19303
Christophe Simonis <chs@odoo.com>
2020-03-27 12:23:11 +0000

    [FIX] l10n_cn@saas~12.5: correct call to `util.delete_unused`
    closes odoo/upgrade#1007
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/l10n_cn/saas~12.5.1.8/post-migrate.py
+++ migrations/l10n_cn/saas~12.5.1.8/post-migrate.py
@@ -3,4 +3,4 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    util.delete_unused(cr, "account_account_type", ["l10n_cn.user_type_all"])
+    util.delete_unused(cr, "l10n_cn.user_type_all")
