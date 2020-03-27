0f1983cca4021b2a6eced93c4e660f1252eb0842
Thibault Delavallée <tde@odoo.com>
2020-03-27 08:22:29 +0000

    [IMP] base@saas~13.3: prepare module addition by removing it
    Temporary commit to merge a bridge module in 13.2 .
    
    Task ID 2156000
    Enterprise PR odoo/enterprise#9505
    
    closes odoo/upgrade#1000
    
    X-original-commit: https://github.com/odoo/enterprise/commit/21828a48325a66b14af201c1c1c810b8f187f5dd
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>

--- migrations/base/saas~13.3.1.3/pre-modules.py
+++ migrations/base/saas~13.3.1.3/pre-modules.py
@@ -8,6 +8,8 @@ def migrate(cr, version):
     util.module_deps_diff(cr, "link_tracker", plus={"mail"})
     util.new_module(cr, "mrp_landed_costs", deps={"stock_landed_costs", "mrp"}, auto_install=True)
     util.module_deps_diff(cr, "sale_coupon", plus={"sale"}, minus={"sale_management"})
+    util.remove_module(cr, "social_linkedin_company_support")
+
     if util.has_enterprise():
         util.new_module(cr, "hr_contract_salary_payroll", deps={"hr_contract_salary", "hr_payroll"}, auto_install=True)
         util.new_module(
