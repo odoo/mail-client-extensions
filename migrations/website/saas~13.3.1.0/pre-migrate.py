f37183b9146c541020bb167e1409247b76f5a3f1
Jeremy Kersten <jke@odoo.com>
2020-03-30 14:48:07 +0000

    website@saas~13.3: set new stored field has_social_default_image
    see odoo/odoo#47848
    
    closes odoo/upgrade#1012
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/website/saas~13.3.1.0/pre-migrate.py
+++ migrations/website/saas~13.3.1.0/pre-migrate.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
     cr.execute("DROP TABLE IF EXISTS website_visitor_partner_rel")
+
+    util.create_column(cr, "website", "has_social_default_image", "boolean")
+    cr.execute(
+        """
+        UPDATE
+            website w
+        SET
+            has_social_default_image=EXISTS(
+                SELECT 1
+                FROM ir_attachment a
+                WHERE a.res_id = w.id
+                  AND a.res_model = 'website'
+                  AND a.res_field = 'social_default_image'
+            )
+    """
+    )
