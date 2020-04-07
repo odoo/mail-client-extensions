69dee5ec11a8a166d0478e591e0759325be9fd46
Xavier Morel <xmo@odoo.com>
2020-04-07 09:14:02 +0000

    [FIX] mail@0.0.0: remove tracking flag on untrackable fields
    Before odoo/odoo#47917 more or less all attributes would get copied
    onto related fields from their target even when that made no sense
    e.g. tracking attribute on fields which can't be tracked (not stored,
    not mail.thread fields, ...).
    
    For normal fields this is not an issue because after odoo/odoo#47917
    gets merged, the related fields would get initialized without the
    attributes and everything would be fine. However for *custom* fields,
    the `tracking` property gets stored in database and reflected /
    restored in every case, and it starts warning that there are
    nonsensical attributes on fields.
    
    Fix up the database by removing the tracking flag on fields (including
    custom but might as well fix them all) for which this makes no sense:
    
    * fields which are not stored can't be tracked
    * fields associated with models which don't extend mail.thread can't
      be tracked
    
    closes odoo/upgrade#1047
    
    Related: odoo/enterprise#9346
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/mail/0.0.0/pre-untracking.py
@@ -0,0 +1,24 @@
+"""
+Historically tracking could get copied onto related fields even when that made
+no sense (it rarely did). Remove tracking attribute if that's an option.
+"""
+
+def migrate(cr, version):
+    cr.execute("""
+        SELECT 1
+          FROM information_schema.columns
+         WHERE table_name = 'ir_model_fields'
+           AND column_name = 'tracking'
+    """)
+    if not cr.rowcount:
+        return
+
+    cr.execute("""
+        UPDATE ir_model_fields f
+           SET tracking = NULL
+          FROM ir_model m
+         WHERE m.id = f.model_id
+           AND f.tracking IS NOT NULL
+           AND (   NOT coalesce(f.store, false)
+                OR NOT coalesce(m.is_mail_thread, false))
+    """)
