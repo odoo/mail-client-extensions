38c48c1e0a7bea00045f5735268096553f88d99c
Rémy Baranx (bar) <bar@odoo.com>
2020-04-08 08:57:11 +0000

    [FIX] account@saas~12.4 migrate old account_invoice[_line] fields references in ir_server_object_lines
    Some fields have been moved/copied from account_invoice[_line] models to
    account_move[_line] models.
    
    This script handles the migration of these old fields/old models references
    in the ir_server_object_lines and ir_act_server tables.
    
    Odoo upgrade : #45288
    
    Co-Authored-By: Christophe Simonis <KangOl@users.noreply.github.com>

---
+++ migrations/account/saas~12.4.1.1/post-19-serveractions.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+
+    # for all fields that have been copied from an old model to a new one
+    # we have to update the ir_server_object_lines table with new field ids.
+    for suffix in ["", ".line"]:
+        src_model = "account.invoice%s" % suffix
+        dst_model = "account.move%s" % suffix
+
+        util.update_server_actions_fields(cr, src_model, dst_model)
+
+        # at the same time, update all server actions of these models
+        cr.execute(
+            """
+            UPDATE ir_act_server
+               SET model_name = %s, model_id = ir_model.id
+              FROM ir_model
+             WHERE ir_model.model = %s
+               AND ir_act_server.model_name = %s
+            """,
+            [dst_model, dst_model, src_model],
+        )
+
+    # For the 'account.invoice' model some field names have changed, so the following mapping list is used.
+    field_name_mapping = [
+        ("reference", "invoice_payment_ref"),
+        ("vendor_display_name", "invoice_partner_display_name"),
+        ("date_invoice", "invoice_date"),
+        ("date_due", "invoice_date_due"),
+        ("origin", "invoice_origin"),
+        ("payments_widget", "invoice_payments_widget"),
+        ("payment_term_id", "invoice_payment_term_id"),
+    ]
+
+    util.update_server_actions_fields(
+        cr, src_model="account.invoice", dst_model="account.move", fields_mapping=field_name_mapping
+    )
+
+    # some old fields from 'account.invoice' have no equivalent in 'account.move'
+    # for them, we remove all the corresponding rows in ir_server_object_lines.
+    cr.execute(
+        """
+        DELETE
+          FROM ir_server_object_lines l
+         USING ir_model_fields mf
+         WHERE mf.id = l.col1
+           AND mf.model = 'account.invoice'
+           AND mf.name IN ('move_id', 'number')
+        """
+    )
