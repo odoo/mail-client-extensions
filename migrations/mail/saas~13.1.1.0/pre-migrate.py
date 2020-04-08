57a8af0c031db3ba6c6c2e8da819c65e2a2f8afa
Christophe Simonis <chs@odoo.com>
2020-04-08 11:43:37 +0200

    [IMP] mail@saas~13.1: parallelize queries

--- migrations/mail/saas~13.1.1.0/pre-migrate.py
+++ migrations/mail/saas~13.1.1.0/pre-migrate.py
@@ -4,25 +4,46 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "mail_message", "is_internal", "boolean")
-    cr.execute("""
-        UPDATE mail_message m
-           SET is_internal = s.internal
-          FROM mail_message_subtype s
-         WHERE s.id = m.subtype_id
-           AND m.is_internal IS NULL
-    """)
-    cr.execute("UPDATE mail_message SET is_internal = false WHERE is_internal IS NULL")
+    # on next-ob2:
+    # parallel queries in 2 pass: ~44 minutes
+    # one sequential query: ~41 minutes (looks more io-bound that cpu-bound)
+    # parallel queries in 1 pass (current code): ~27 minutes
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            """
+               UPDATE mail_message m
+                  SET is_internal = EXISTS (SELECT 1
+                                              FROM mail_message_subtype s
+                                             WHERE s.id = m.subtype_id
+                                               AND s.internal=true)
+                WHERE m.is_internal IS NULL
+                  AND {parallel_filter}
+            """,
+            table="mail_message",
+            prefix="m.",
+        ),
+    )
 
     cr.execute("ALTER TABLE mail_tracking_value RENAME COLUMN field TO _field")
     util.create_column(cr, "mail_tracking_value", "field", "int4")
-    cr.execute("""
-        UPDATE mail_tracking_value t
-           SET field = f.id
-          FROM mail_message m, ir_model_fields f
-         WHERE t.mail_message_id = m.id
-           AND f.name = t._field
-           AND m.model = f.model
-    """)
+    util.parallel_execute(
+        cr,
+        util.explode_query(
+            cr,
+            """
+                UPDATE mail_tracking_value t
+                   SET field = f.id
+                  FROM mail_message m, ir_model_fields f
+                 WHERE t.mail_message_id = m.id
+                   AND f.name = t._field
+                   AND m.model = f.model
+            """,
+            prefix="t.",
+        ),
+    )
+
     util.remove_column(cr, "mail_tracking_value", "_field")
     cr.execute("CREATE INDEX ON mail_tracking_value(field)")
     cr.execute("DELETE FROM mail_tracking_value WHERE field IS NULL")
