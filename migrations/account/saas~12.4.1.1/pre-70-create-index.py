1d2e10d18200cdd4557e5ab1c266a379569ef012
Nicolas Vannieuwerburgh <nvn@odoo.com>
2020-04-09 14:24:12 +0000

    [FIX] account@saas~12.4: index using hash
    Using hash index allows to index very long varchar.
    When using regular index we may get the error below:
    psycopg2.errors.ProgramLimitExceeded: index row size 2792 exceeds maximum 2712 for index "_mig124_il_name"
    
    closes odoo/upgrade#1054
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/account/saas~12.4.1.1/pre-70-create-index.py
+++ migrations/account/saas~12.4.1.1/pre-70-create-index.py
@@ -9,10 +9,8 @@ def migrate(cr, version):
 
     idx_list = [
         ("_mig124_ai_currency_id", "account_invoice", "currency_id"),
-        ("_mig124_il_name", "account_invoice_line", "name"),
         ("_mig124_il_invoice_id", "account_invoice_line", "invoice_id"),
         ("_mig124_il_account_id", "account_invoice_line", "account_id"),
-        ("_mig124_ml_name", "account_move_line", "name"),
         ("_mig124_ml_account_id", "account_move_line", "account_id"),
     ]
 
@@ -22,5 +20,8 @@ def migrate(cr, version):
     for index_name, table_name, column_name in idx_list:
         util.ENVIRON["__created_accounting_idx"].append(index_name)
         create_index_queries.append("CREATE INDEX %s ON %s(%s)" % (index_name, table_name, column_name))
+    create_index_queries.append("CREATE INDEX _mig124_il_name ON account_invoice_line USING HASH (name)")
+    create_index_queries.append("CREATE INDEX _mig124_ml_name ON account_move_line USING HASH (name)")
+    util.ENVIRON["__created_accounting_idx"].extend(["_mig124_il_name", "_mig124_ml_name"])
     _logger.info("creating %s indexes (might be slow)", len(create_index_queries))
     util.parallel_execute(cr, create_index_queries)
