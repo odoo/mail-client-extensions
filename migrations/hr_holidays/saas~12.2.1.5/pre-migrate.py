a0d08eb1f91fd1bf1b020ef4e22ea4a4f268da8d
Stanislas Sobieski <sts@odoo.com>
2020-04-10 07:32:48 +0000

    [FIX] hr_holidays@saas~12.2.1.5: unfix typo
    closes odoo/upgrade#1060
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/hr_holidays/saas~12.2.1.5/pre-migrate.py
+++ migrations/hr_holidays/saas~12.2.1.5/pre-migrate.py
@@ -3,7 +3,7 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    util.rename_field(cr, "hr.employee", "is_absent_today", "is_absent")
+    util.rename_field(cr, "hr.employee", "is_absent_totay", "is_absent")
 
     util.remove_record(cr, "hr_holidays.open_company_allocation")
     util.remove_record(cr, "hr_holidays.menu_open_company_allocation")
