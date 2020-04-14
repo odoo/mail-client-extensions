8c0d3d567844a9fd8109cd0a5d11093bcfbe7cfe
Nicolas Vannieuwerburgh <nvn@odoo.com>
2020-04-14 09:47:14 +0000

    [FIX] account_3way_match@saas~12.4: can_be_paid copied from invl
    closes odoo/upgrade#1063
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/account_3way_match/saas~12.4.1.0/post-10-account-pocalypse.py
+++ migrations/account_3way_match/saas~12.4.1.0/post-10-account-pocalypse.py
@@ -1,13 +1,30 @@
 # -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
+
+    queries = []
     # Update account_move from existing account_invoice.
-    cr.execute('''
+    queries.append(
+        """
         UPDATE account_move am
         SET release_to_pay = inv.release_to_pay,
             release_to_pay_manual = inv.release_to_pay_manual,
             force_release_to_pay = inv.force_release_to_pay
         FROM account_invoice inv
         WHERE move_id IS NOT NULL AND am.id = inv.move_id
-    ''')
+    """
+    )
+
+    # Update account_move_line from existing account_invoice_line.
+    queries.append(
+        """
+        UPDATE account_move_line aml
+        SET can_be_paid = invl.can_be_paid
+        FROM invl_aml_mapping m, account_invoice_line invl
+        WHERE aml.id = m.aml_id AND invl.id = m.invl_id
+    """
+    )
+
+    util.parallel_execute(cr, queries)
--- migrations/account_3way_match/saas~12.4.1.0/pre-10-account-pocalypse.py
+++ migrations/account_3way_match/saas~12.4.1.0/pre-10-account-pocalypse.py
@@ -3,6 +3,7 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    util.create_column(cr, 'account_move', 'release_to_pay', 'varchar')
-    util.create_column(cr, 'account_move', 'release_to_pay_manual', 'varchar')
-    util.create_column(cr, 'account_move', 'force_release_to_pay', 'boolean')
+    util.create_column(cr, "account_move", "release_to_pay", "varchar")
+    util.create_column(cr, "account_move", "release_to_pay_manual", "varchar")
+    util.create_column(cr, "account_move", "force_release_to_pay", "boolean")
+    util.create_column(cr, "account_move_line", "can_be_paid", "varchar")
