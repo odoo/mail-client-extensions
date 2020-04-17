d87ac405fd5358bbda95dd643e912e05243aedbd
Christophe Simonis <chs@odoo.com>
2020-04-17 19:00:42 +0200

    [FIX] base@saas~13.3: correct modules changes

--- migrations/base/saas~13.3.1.3/pre-modules.py
+++ migrations/base/saas~13.3.1.3/pre-modules.py
@@ -9,10 +9,20 @@ def migrate(cr, version):
     util.new_module(cr, "mrp_landed_costs", deps={"stock_landed_costs", "mrp"}, auto_install=True)
     util.new_module(cr, "account_qr_code_sepa", deps={"account", "base_iban"}, auto_install=True)
     util.module_deps_diff(cr, "sale_coupon", plus={"sale"}, minus={"sale_management"})
-    util.remove_module(cr, "social_linkedin_company_support")
+
+    util.module_auto_install(cr, "crm_iap_lead", True)
+    util.module_auto_install(cr, "crm_iap_lead_enrich", True)
 
     if util.has_enterprise():
+        util.new_module(
+            cr, "documents_l10n_be_hr_payroll", deps={"documents_hr_payroll", "l10n_be_hr_payroll"}, auto_install=True
+        )
         util.new_module(cr, "hr_contract_salary_payroll", deps={"hr_contract_salary", "hr_payroll"}, auto_install=True)
+        util.new_module(
+            cr, "purchase_stock_enterprise", deps={"purchase_enterprise", "purchase_stock"}, auto_install=True
+        )
+        util.new_module(cr, "sale_renting_crm", deps={"sale_renting", "crm"}, auto_install=True)
+
         util.new_module(
             cr,
             "l10n_be_hr_contract_salary",
@@ -31,4 +41,5 @@ def migrate(cr, version):
             plus={"hr_contract_salary_payroll", "l10n_be_hr_contract_salary"},
             minus={"hr_contract_salary"},
         )
-        util.new_module(cr, "purchase_stock_enterprise", deps={"purchase_enterprise", "purchase_stock"}, auto_install=True)
+
+        util.merge_module(cr, "social_linkedin_company_support", "social_linkedin")
