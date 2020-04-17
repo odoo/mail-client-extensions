b0e586179581fe29ad88e939d3d0895de9cf1811
Denis Ledoux <dle@odoo.com>
2020-04-17 15:39:50 +0000

    [FIX] website_event@saas~12.5: do not remove `is_published` column
    It's still used by the `website.published.multi.mixin`
    See discussion in issue odoo/odoo#49354
    to understand why the field is being removed.
    This is basically a limitation of the ORM,
    that may be corrected in master.
    
    closes odoo/upgrade#1085
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/website_event/saas~12.5.1.0/pre-migrate.py
+++ migrations/website_event/saas~12.5.1.0/pre-migrate.py
@@ -12,3 +12,5 @@ def migrate(cr, version):
                  '{"background-image": "none", "background-color": "oe_blue", "opacity": "0.4", "resize_class": "cover_mid"}'
     """
     )
+    # https://github.com/odoo/odoo/issues/49354
+    cr.execute("DELETE FROM ir_model_data WHERE module = 'website_event' AND name = 'field_event_event__is_published'")
