10d897bfca728bc6b749652a971daae24301e7db
Christophe Simonis <chs@odoo.com>
2020-04-17 15:47:52 +0200

    [FIX] base@saas~13.3: handle existing module categories

--- migrations/base/saas~13.3.1.3/pre-migrate.py
+++ migrations/base/saas~13.3.1.3/pre-migrate.py
@@ -18,11 +18,24 @@ def migrate(cr, version):
     util.rename_xmlid(cr, *eb("base.module_category_{localization,accounting_localizations}"))
     util.rename_xmlid(cr, *eb("base.module_category_sales_{subscription,subscriptions}"))
     util.rename_xmlid(cr, *eb("base.module_category_marketing_{social,social_marketing}"))
-    util.rename_xmlid(cr, *eb("base.module_category_{tools,hidden_tools}"))
-    util.rename_xmlid(cr, *eb("base.module_category_{tests,hidden_tests}"))
     util.rename_xmlid(cr, *eb("base.module_category_{accounting,human_resources}_expense"))
     util.rename_xmlid(cr, *eb("base.module_category_accounting_{payment,payment_acquirers}"))
     util.rename_xmlid(cr, *eb("base.module_category_{discuss,productivity_discuss}"))
+
+    # categories moved to `hidden/` prefix may already exists, deduplicate them
+    for cat in {"tools", "tests"}:
+        root_name = f"base.module_category_{cat}"
+        hidden_name = f"base.module_category_hidden_{cat}"
+        root_id = util.ref(cr, root_name)
+        hidden_id = util.ref(cr, hidden_name)
+        if root_id and hidden_id:
+            if root_id != hidden_id:
+                util.replace_record_references(
+                    cr, ("ir.module.category", root_id), ("ir.module.category", hidden_id), replace_xmlid=False
+                )
+            cr.execute("DELETE FROM ir_model_data WHERE module=%s AND name=%s", root_name.split("."))
+        elif root_id:
+            util.rename_xmlid(cr, root_name, hidden_name)
 
     util.remove_view(cr, "base.view_partner_short_form")
 
