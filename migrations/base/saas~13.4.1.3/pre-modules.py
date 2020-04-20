799d4b376641c1987ff13bb61ed24ed7990b135b
dmonzonis <mod@odoo.com>
2020-04-20 10:48:20 +0000

    [IMP] base@saas~13.4: Add mail_bot_hr module
    The mail_bot_hr module is introduced, which is a bridge module between
    mail_bot and hr to remove the mail_bot dependency from hr.
    
    closes odoo/upgrade#1089
    
    Related: odoo/odoo#48521
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/base/saas~13.4.1.3/pre-modules.py
+++ migrations/base/saas~13.4.1.3/pre-modules.py
@@ -7,3 +7,6 @@ def migrate(cr, version):
     util.new_module_dep(cr, "crm_iap_lead", "partner_autocomplete")
     util.new_module_dep(cr, "crm_iap_lead_enrich", "partner_autocomplete")
     util.remove_module(cr, "odoo_referral_portal")
+    util.new_module(cr, "mail_bot_hr", deps={"mail_bot", "hr"}, auto_install=True)
+    util.module_deps_diff(cr, "hr", minus={"mail_bot"})
+    util.module_deps_diff(cr, "test_mail", minus={"mail_bot"})
