b42be3623a3c52fe8701ba5925e7e08f5d57d30e
Sébastien Theys <seb@odoo.com>
2020-04-20 09:41:00 +0000

    [FIX] snailmail@saas~13.4: upgrade failure type and reason if error
    Follow up on https://github.com/odoo/upgrade/pull/918
    
    closes odoo/upgrade#1088
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/snailmail/saas~13.4.0.2/pre-migrate.py
+++ migrations/snailmail/saas~13.4.0.2/pre-migrate.py
@@ -17,7 +17,9 @@ def migrate(cr, version):
             notification_type,
             letter_id,
             is_read,
-            notification_status
+            notification_status,
+            failure_type,
+            failure_reason
         )
         SELECT
             l.message_id,
@@ -30,6 +32,22 @@ def migrate(cr, version):
                 WHEN l.state = 'sent' THEN 'sent'
                 WHEN l.state = 'error' THEN 'exception'
                 WHEN l.state = 'canceled' THEN 'cancelled'
+            END,
+            CASE
+                WHEN l.state = 'error' THEN
+                    CASE
+                        WHEN l.error_code = 'CREDIT_ERROR' THEN 'sn_credit'
+                        WHEN l.error_code = 'TRIAL_ERROR' THEN 'sn_trial'
+                        WHEN l.error_code = 'NO_PRICE_AVAILABLE' THEN 'sn_price'
+                        WHEN l.error_code = 'MISSING_REQUIRED_FIELDS' THEN 'sn_fields'
+                        WHEN l.error_code = 'FORMAT_ERROR' THEN 'sn_format'
+                        ELSE 'sn_error'
+                    END
+                ELSE NULL
+            END,
+            CASE
+                WHEN l.state = 'error' THEN l.info_msg
+                ELSE NULL
             END
         FROM snailmail_letter l
         WHERE NOT EXISTS(SELECT 1 FROM mail_message_res_partner_needaction_rel WHERE letter_id = l.id)
--- migrations/snailmail/tests/test_snailmail.py
+++ migrations/snailmail/tests/test_snailmail.py
@@ -10,7 +10,14 @@ class TestSnailmailNotification(UpgradeCase):
         dest_partner = self.env["res.partner"].create([{"name": "dest_partner"}])
         letters = self.env["snailmail.letter"].create(
             [
-                {"model": dest_partner._name, "res_id": dest_partner.id, "partner_id": dest_partner.id, "state": state}
+                {
+                    "model": dest_partner._name,
+                    "res_id": dest_partner.id,
+                    "partner_id": dest_partner.id,
+                    "state": state,
+                    "error_code": "NO_PRICE_AVAILABLE" if state == "error" else False,
+                    "info_msg": "more info about %s" % state,
+                }
                 for state in ["pending", "sent", "error", "canceled"]
             ]
         )
@@ -30,3 +37,51 @@ class TestSnailmailNotification(UpgradeCase):
             self.assertEqual(notif.mail_message_id.id, letter.message_id.id)
             self.assertEqual(notif.notification_status, notification_status)
             self.assertEqual(notif.is_read, True)
+            if notif.notification_status != "exception":
+                self.assertFalse(notif.failure_type)
+                self.assertFalse(notif.failure_reason)
+            else:
+                self.assertEqual(notif.failure_type, "sn_price")
+                self.assertEqual(notif.failure_reason, "more info about error")
+
+
+@change_version("13.4")
+class TestSnailmailNotificationError(UpgradeCase):
+    def prepare(self):
+        dest_partner = self.env["res.partner"].create([{"name": "dest_partner"}])
+        letters = self.env["snailmail.letter"].create(
+            [
+                {
+                    "model": dest_partner._name,
+                    "res_id": dest_partner.id,
+                    "partner_id": dest_partner.id,
+                    "state": "error",
+                    "error_code": error_code,
+                    "info_msg": "more info about error",
+                }
+                for error_code in [
+                    "CREDIT_ERROR",
+                    "TRIAL_ERROR",
+                    "NO_PRICE_AVAILABLE",
+                    "MISSING_REQUIRED_FIELDS",
+                    "FORMAT_ERROR",
+                    "UNKNOWN_ERROR",
+                ]
+            ]
+        )
+        return {
+            "dest_partner_id": dest_partner.id,
+            "letter_ids": letters.ids,
+        }
+
+    def check(self, init):
+        letters = self.env["snailmail.letter"].browse(init["letter_ids"])
+        self.assertEqual(len(letters), 6)
+        for letter, failure_type in zip(
+            letters, ["sn_credit", "sn_trial", "sn_price", "sn_fields", "sn_format", "sn_error"]
+        ):
+            notif = letter.notification_ids
+            self.assertEqual(len(notif), 1)
+            self.assertEqual(notif.notification_status, "exception")
+            self.assertEqual(notif.failure_type, failure_type)
+            self.assertEqual(notif.failure_reason, "more info about error")
