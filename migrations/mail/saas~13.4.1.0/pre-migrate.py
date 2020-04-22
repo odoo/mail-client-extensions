bd67d416025df6c240a8c7c84c99de8156d2bb4c
Patrick Hoste <pko@odoo.com>
2020-04-22 08:03:39 +0000

    [IMP] mail@saas~13.4: add request_partner_id field to mail.activity
    Purpose is to store the source or requesting partner linked to an activity
    like customer requesting access to a channel in slides.
    
    Same commit adds mail.activity.mixin to slides but no migration script is
    required as no column is added.
    
    Task ID : 2127364
    Community PR odoo/odoo#40708
    Upgrade PR odoo/upgrade#1101
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/mail/saas~13.4.1.0/pre-migrate.py
+++ migrations/mail/saas~13.4.1.0/pre-migrate.py
@@ -6,3 +6,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "res.users", "alias_id")
     util.remove_field(cr, "res.users", "alias_contact")
+    util.create_column(cr, "mail_activity", "request_partner_id", "int4")
