688936b5c9e99ab5be138a0222358d115e91a9a0
SEINLET Nicolas <nicolas@seinlet.com>
2020-04-22 12:17:05 +0200

    [FIX] helpdesk@saas~13.3

--- migrations/helpdesk/saas~13.3.1.3/pre-10-migrate.py
+++ migrations/helpdesk/saas~13.3.1.3/pre-10-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_m2m(cr, "helpdesk_visibility_team", "helpdesk_team", "res_users")
+    util.create_column(cr, "helpdesk_sla", "exclude_stage_id", "int4")
