cf4c8690454dc14244c3ebef1e73669f3857a851
Christophe Simonis <chs@odoo.com>
2020-04-22 14:23:52 +0200

    [FIX] mrp_account@saas~12.3: rename moved model and handle upgrade without enterprise

--- migrations/mrp_account/saas~12.3.1.0/pre-20-split.py
+++ migrations/mrp_account/saas~12.3.1.0/pre-20-split.py
@@ -1,14 +1,33 @@
 # -*- coding: utf-8 -*-
 from odoo.addons.base.maintenance.migrations import util
 
+
 def migrate(cr, version):
     eb = util.expand_braces
-    util.move_model(cr, 'report.mrp_account.mrp_cost_structure', 'mrp_account', 'mrp_account_enterprise')
-    util.move_model(cr, 'report.mrp_account.product_template_cost_structure', 'mrp_account', 'mrp_account_enterprise')
-    util.rename_xmlid(cr, *eb("mrp_account{,_enterprise}.mrp_cost_structure"))
-    util.rename_xmlid(cr, *eb("mrp_account{,_enterprise}.product_template_cost_structure"))
-    util.rename_xmlid(cr, *eb("mrp_account{,_enterprise}.mrp_workcenter_view_inherit"))
-    util.rename_xmlid(cr, *eb("mrp_account{,_enterprise}.action_cost_struct_mrp_production"))
-    util.rename_xmlid(cr, *eb("mrp_account{,_enterprise}.mrp_production_form_inherit_view6"))
-    util.rename_xmlid(cr, *eb("mrp_account{,_enterprise}.action_cost_struct_product_template"))
-    util.rename_xmlid(cr, *eb("mrp_account{,_enterprise}.product_product_inherit_form_view_cost_structure"))
+    models = ["mrp_cost_structure", "product_template_cost_structure"]
+    records = """
+        action_cost_struct_mrp_production
+        action_cost_struct_product_template
+    """
+    views = """
+        mrp_cost_structure
+        product_template_cost_structure
+        mrp_workcenter_view_inherit
+        mrp_production_form_inherit_view6
+        product_product_inherit_form_view_cost_structure
+    """
+
+    if util.has_enterprise():
+        for model in models:
+            util.move_model(cr, "report.mrp_account.%s" % model, "mrp_account", "mrp_account_enterprise")
+            util.rename_model(cr, *eb("report.mrp_account{,_enterprise}.%s" % model), rename_table=False)
+
+        for xid in util.splitlines(records + "\n" + views):
+            util.rename_xmlid(cr, *eb("mrp_account{,_enterprise}.%s" % xid))
+    else:
+        for record in util.splitlines(records):
+            util.remove_record(cr, "mrp_account.%s" % record)
+        for view in util.splitlines(views):
+            util.remove_view(cr, "mrp_account.%s" % view)
+        for model in models:
+            util.remove_model(cr, "report.mrp_account.%s" % model)
