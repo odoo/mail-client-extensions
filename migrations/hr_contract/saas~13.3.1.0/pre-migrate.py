975cad58ea6b8313c7972eb5cc0055540d64feaf
SEINLET Nicolas <nicolas@seinlet.com>
2020-04-22 14:38:23 +0200

    [FIX] hr_contract@saas~13.3

--- migrations/hr_contract/saas~13.3.1.0/pre-migrate.py
+++ migrations/hr_contract/saas~13.3.1.0/pre-migrate.py
@@ -4,6 +4,17 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    eb = util.expand_braces
+    fields = {
+        "hr.contract": ["structure_type_id", "company_country_id"],
+        "hr.payroll.structure.type": ["name", "default_resource_calendar_id", "country_id"],
+    }
+    for model in fields:
+        for field in fields[model]:
+            util.move_field_to_module(cr, model, field, "hr_payroll", "hr_contract")
 
-    util.move_field_to_module(cr, "hr.contract", "structure_type_id", "hr_payroll", "hr_contract")
-    util.remove_field(cr, 'hr.contract', 'advantages')
+    util.remove_field(cr, "hr.contract", "advantages")
+
+    util.rename_xmlid(cr, *eb("hr_{payroll,contract}.access_hr_payroll_structure_type_hr_contract_manager"))
+    util.rename_xmlid(cr, *eb("hr_{payroll,contract}.ir_rule_hr_payroll_structure_type_multi_company"))
+    util.rename_xmlid(cr, *eb("{l10n_be_hr_payroll,hr_contract}.structure_type_employee_cp200"))
