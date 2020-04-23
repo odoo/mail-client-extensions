2e571b31293f57f0371f038ef7044c93090f06f2
Christophe Simonis <chs@odoo.com>
2020-04-23 18:31:35 +0200

    [FIX] website_slides@saas~13.3

--- migrations/website_slides/saas~13.3.2.0/pre-migrate.py
+++ migrations/website_slides/saas~13.3.2.0/pre-migrate.py
@@ -3,6 +3,8 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.create_column(cr, "slide_channel_tag", "color", "int4")
+    util.create_column(cr, "slide_channel_tag", "color", "int4", default=0)
+    util.fixup_m2m(cr, "rel_slide_tag", "slide_slide", "slide_tag", "slide_id", "tag_id")
 
-    cr.execute("UPDATE slide_channel_tag SET color = 0")
+    eb = util.expand_braces
+    util.rename_xmlid(cr, *eb("website_slides.website_slides_menu_config_course_{tags,groups}"))
