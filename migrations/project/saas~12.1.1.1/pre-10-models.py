33ab1a12d9aa4700696858127ec34695f7d6c6c0
Christophe Simonis <chs@odoo.com>
2020-04-23 14:30:00 +0200

    [FIX] project@saas~12.1

--- migrations/project/saas~12.1.1.1/pre-10-models.py
+++ migrations/project/saas~12.1.1.1/pre-10-models.py
@@ -4,3 +4,6 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     cr.execute("DELETE FROM ir_translation where name='project.project,label_tasks'")
+
+    util.remove_field(cr, "project.project", "percentage_satisfaction_task")
+    util.remove_field(cr, "project.project", "percentage_satisfaction_project")
