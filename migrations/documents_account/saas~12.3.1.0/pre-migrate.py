f47f560efc03ddbd1af7bb74289a84b582c11159
Christophe Simonis <chs@odoo.com>
2020-04-28 19:48:00 +0200

    [FIX] documents_account@saas~12.3: remove field on config wizard

--- migrations/documents_account/saas~12.3.1.0/pre-migrate.py
+++ migrations/documents_account/saas~12.3.1.0/pre-migrate.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo.addons.base.maintenance.migrations import util
 
+
 def migrate(cr, version):
-    util.remove_field(cr, 'res.company', 'account_tags')
+    util.remove_field(cr, "res.company", "account_tags")
+    util.remove_field(cr, "res.config.settings", "account_tags")
