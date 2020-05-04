a9c67085495899f0f98bb7d33e837d0b14e954b2
Denis Ledoux <dle@odoo.com>
2020-05-04 10:18:26 +0000

    [FIX] stock: test on hand quantity for bom kits
    The available quantity computation for bom kits changed
    in 13.0. It now computes the maximum quantity of that
    kit that can be produced.
    
    Therefore, for these products, the quantity can
    pass from 0 to something else (1, 2, ...)
    when passing from 12.0 to 13.0.
    
    As a solution, we simply ignore bom kits
    if the source is before 13.0 and the target after 13.0, included.
    
    closes odoo/upgrade#1137
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/stock/tests/test_on_hand_quantity.py
+++ migrations/stock/tests/test_on_hand_quantity.py
@@ -2,17 +2,37 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.addons.base.maintenance.migrations.testing import IntegrityCase
+from odoo.tools.parse_version import parse_version
+from odoo import release
 
 
 class TestOnHandQuantityUnchanged(IntegrityCase):
-    def invariant(self):
+    def check(self, value):
+        before_version, before_results = value
+        ignore_kits = "mrp.bom" in self.env.registry and parse_version(before_version) < parse_version("13.0")
+        after_version, after_results = self.invariant(ignore_kits=ignore_kits)
+        self.assertEqual(before_results, self.convert_check(after_results), self.message)
+
+    def invariant(self, ignore_kits=False):
+        ignore_kits = ignore_kits or (
+            "mrp.bom" in self.env.registry and parse_version(release.series) < parse_version("13.0")
+        )
         self.env.cr.execute(
-            """ SELECT pp.id
+            """
+                SELECT DISTINCT ON (pp.id) pp.id
                 FROM product_product pp
                 JOIN product_template pt ON pp.product_tmpl_id = pt.id
                 AND pp.active = TRUE AND pt.TYPE = 'product'
-                ORDER BY pp.id
+                %s
+                ORDER BY pp.id%s
             """
+            % (
+                """
+                    LEFT JOIN mrp_bom bom ON bom.product_id = pp.id OR bom.product_tmpl_id = pt.id
+                    WHERE coalesce(bom.type, '') != 'phantom'
+                """, ", bom.sequence"
+            ) if ignore_kits else ("", "")
         )
         products = self.env["product.product"].browse([row[0] for row in self.env.cr.fetchall()])
-        return products.mapped(lambda p: [p.id, p.qty_available])
+        results = products.mapped(lambda p: [p.id, p.qty_available])
+        return [release.series, results]
