3649fb5d7afbe18a60cfb7d09e292eb72c64836e
Christophe Simonis <chs@odoo.com>
2020-05-06 15:46:34 +0200

    [FIX] mail@10.saas~18

--- migrations/mail/10.saas~18.1.0/pre-migrate.py
+++ migrations/mail/10.saas~18.1.0/pre-migrate.py
@@ -3,6 +3,7 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.remove_field(cr, 'mail.followers', 'res_model_id')
+    util.remove_field(cr, "mail.template", "ref_ir_value")
 
     eb = util.expand_braces
     util.rename_xmlid(cr, *eb('mail.mail_smiley_{,normal_}smile'))
