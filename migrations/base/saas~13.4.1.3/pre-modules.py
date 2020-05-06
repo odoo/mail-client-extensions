859e6e97ea83240667deb9287472d541687d9d1d
Jigar Vaghela <jva@odoo.com>
2020-05-06 13:15:11 +0000

    [IMP] base@saas-13.4: add base_vat dependency in l10n_in

--- migrations/base/saas~13.4.1.3/pre-modules.py
+++ migrations/base/saas~13.4.1.3/pre-modules.py
@@ -10,3 +10,4 @@ def migrate(cr, version):
     util.new_module(cr, "mail_bot_hr", deps={"mail_bot", "hr"}, auto_install=True)
     util.module_deps_diff(cr, "hr", minus={"mail_bot"})
     util.module_deps_diff(cr, "test_mail", minus={"mail_bot"})
+    util.new_module_dep(cr, 'l10n_in', 'base_vat')
