b7a6c31f64ecc9448ab210e8666fb10661cc2799
Christophe Simonis <chs@odoo.com>
2020-05-07 14:32:15 +0200

    [FIX] sale@saas~13.3: use model name instead of table name

--- migrations/sale/saas~13.3.1.1/pre-migrate.py
+++ migrations/sale/saas~13.3.1.1/pre-migrate.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.remove_field(cr, "sale.order", "remaining_validity_days")
-    util.move_field_to_module(cr, "sale_order", "tag_ids", "sale_crm", "sale")
+    util.move_field_to_module(cr, "sale.order", "tag_ids", "sale_crm", "sale")
     util.create_column(cr, "sale_order", "currency_id", "int4")
 
-    cr.execute("""
-        UPDATE sale_order s
-        SET currency_id = p.currency_id
-        FROM product_pricelist p
-        WHERE p.id = s.pricelist_id
-    """)
+    cr.execute(
+        """
+            UPDATE sale_order s
+               SET currency_id = p.currency_id
+              FROM product_pricelist p
+             WHERE p.id = s.pricelist_id
+        """
+    )
