1b5627e9b20a55e414d1581710a61813322df537
Christophe Simonis <chs@odoo.com>
2020-05-08 12:37:27 +0000

    [FIX] mail@0.0.0: fixup mail aliases before cleaning up them
    closes odoo/upgrade#1169
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/mail/0.0.0/pre-rm-unreachable.py
+++ migrations/mail/0.0.0/pre-rm-unreachable.py
@@ -7,6 +7,41 @@ def migrate(cr, version):
     #   - As there is 2 res_model/res_id combinations, they cannot be run in
     #     parallel in one pass,otherwise we got concurrent access errors.
     #   - A null `res_id` is ok. Records should not be removed in this case.
+
+    # start by fixing the one2one of mail.alias.mixin records
+    alias_queries = []
+    cr.execute(
+        """
+            SELECT m.id, m.model
+              FROM mail_alias a
+              JOIN ir_model m ON m.id = a.alias_parent_model_id
+             WHERE COALESCE(a.alias_parent_thread_id, 0) != 0
+          GROUP BY m.id, m.model
+        """
+    )
+    for mid, model in cr.fetchall():
+        table = util.table_of_model(cr, model)
+        if not util.column_exists(cr, table, "alias_id"):
+            # XXX delete all aliases?
+            continue
+
+        alias_queries.append(
+            cr.mogrify(
+                """
+                    UPDATE mail_alias a
+                       SET alias_parent_thread_id = t.id
+                      FROM {table} t
+                     WHERE t.alias_id = a.id
+                       AND a.alias_parent_model_id = %s
+                       AND a.alias_parent_thread_id != t.id
+                """.format(
+                    table=table
+                ),
+                [mid],
+            )
+        )
+    util.parallel_execute(cr, alias_queries)
+
     mail_queries = []
     for ir in util.indirect_references(cr, bound_only=True):
         if ir.table == "mail_alias":
