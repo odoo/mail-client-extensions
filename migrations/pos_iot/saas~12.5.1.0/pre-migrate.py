cfa6d4431764a27c0b59d938f3dce6838f700531
Denis Ledoux <dle@odoo.com>
2020-05-11 14:25:05 +0000

    [FIX] pos_iot@saas~12.5: iface_printer_id references iot_device
    Requests:
     - 45710
     - 45690
     - 45490
    
    The field `iotbox_id` is not only renamed `iface_printer_id`,
    but it also changes of referenced table, from `iot_box` to `iot_device`.
    
    Its foreign key values must therefore be updated accordingly,
    otherwise when the ORM tries to add the foreign key on the
    column, it might fails because the id referenced in the column
    `iface_printer_id` is not within the table `iot_device`.
    
    Beyond the foreign key, its also nice to point to the correct records,
    and not some potentially random records from the table `iot_device`.
    
    ```
      File "/src/odoo/13.0/odoo/modules/loading.py", line 202, in load_module_graph
        registry.init_models(cr, model_names, {'module': package.name}, new_install)
      File "/src/odoo/13.0/odoo/modules/registry.py", line 353, in init_models
        func()
      File "/src/odoo/13.0/odoo/fields.py", line 2436, in update_db_foreign_key
        new = process(model._cr, model._table, self.name, comodel._table, 'id', self.ondelete or 'set null')
      File "/src/odoo/13.0/odoo/tools/sql.py", line 184, in fix_foreign_key
        return add_foreign_key(cr, tablename1, columnname1, tablename2, columnname2, ondelete)
      File "/src/odoo/13.0/odoo/tools/sql.py", line 157, in add_foreign_key
        cr.execute(query.format(tablename1, columnname1, tablename2, columnname2, ondelete))
      File "/src/odoo/13.0/odoo/sql_db.py", line 164, in wrapper
        return f(self, *args, **kwargs)
      File "/src/odoo/13.0/odoo/sql_db.py", line 241, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.ForeignKeyViolation: insert or update on table "pos_config" violates foreign key constraint "pos_config_iface_printer_id_fkey"
    ```
    
    closes odoo/upgrade#1177
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/pos_iot/saas~12.5.1.0/pre-migrate.py
+++ migrations/pos_iot/saas~12.5.1.0/pre-migrate.py
@@ -3,10 +3,52 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    util.rename_field(cr, "pos.config", "iotbox_id", "iface_printer_id")
+    util.create_column(cr, "pos_config", "iface_printer_id", "int4")
     util.create_column(cr, "pos_config", "iface_display_id", "int4")
     util.create_column(cr, "pos_config", "iface_scale_id", "int4")
     util.create_column(cr, "pos_payment_method", "iot_device_id", "int4")
 
     util.remove_field(cr, "account.journal", "use_payment_terminal")
     util.remove_field(cr, "pos.config", "iface_payment_terminal")
+
+    # `iface_printer_id`, `iface_display_id` and `iface_scale_id` are added, and can be determined from the `iotbox_id`,
+    # which has been removed.
+    # A same iot box can have multiple devices, multiple printers, multiple displays, multiple scales.
+    # in 12.0, the device was chosen by the Javascript, it took the first device available,
+    # in 13.0, you can choose by pos config the device to use on the iot box.
+
+    # 1. Determine the printer, display and scale to use by pos config. It's an heuristic,
+    #    the user might want to change this after upgrade if it does not suit his requirements.
+    cr.execute(
+        """
+        UPDATE pos_config
+           SET iface_printer_id = (
+                     SELECT id
+                       FROM iot_device
+                      WHERE iot_id = iotbox_id
+                        AND type = 'printer'
+                   ORDER BY connection, id -- order by connect because `direct` (USB) is better than `network`.
+                      LIMIT 1
+               ),
+               iface_display_id = (
+                      SELECT id
+                        FROM iot_device
+                       WHERE iot_id = iotbox_id
+                         AND type = 'display'
+                    ORDER BY connection, id
+                       LIMIT 1
+               ),
+               iface_scale_id = (
+                      SELECT id
+                        FROM iot_device
+                       WHERE iot_id = iotbox_id
+                         AND type = 'scale'
+                    ORDER BY connection, id
+                       LIMIT 1
+               )
+         WHERE iotbox_id IS NOT NULL
+        """
+    )
+
+    # 2. Remove the field and column `iotbox_id`
+    util.remove_field(cr, "pos.config", "iotbox_id")
