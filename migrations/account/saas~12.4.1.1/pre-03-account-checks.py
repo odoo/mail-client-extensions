4d019dc82092c84b5013933491e860156503ded0
Rémy Baranx (bar) <bar@odoo.com>
2020-05-12 05:34:15 +0000

    [FIX] account@saas~12.4: open/paid invoices not linked to an account move
    As having an open/paid invoice not linked to an account move is not legal,
    but also block the account upgrade, a new checking has been added to raise
    a migration error in this case.
    
    Upgrade request: #46563
    
    closes odoo/upgrade#1179
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/saas~12.4.1.1/pre-03-account-checks.py
+++ migrations/account/saas~12.4.1.1/pre-03-account-checks.py
@@ -22,6 +22,24 @@ def migrate(cr, version):
 
     cr.execute(
         """
+        SELECT count(*), array_agg(id)
+          FROM account_invoice
+         WHERE state IN ('open', 'paid')
+           AND move_id IS NULL
+        """
+    )
+    invoices_count, invoices_ids = cr.fetchone()
+
+    if invoices_count:
+        ids = " Invoice ids: ({})".format(", ".join([str(r) for r in invoices_ids])) if invoices_count < 1000 else ""
+
+        raise util.MigrationError(
+            "%s invoices are in 'open' or 'paid' state but not linked to an account move in pre-migration database. "
+            "Please investigate and fix it in pre-migration db.%s" % (invoices_count, ids)
+        )
+
+    cr.execute(
+        """
     UPDATE account_account a
        SET internal_type=t.type
       FROM account_account_type t
