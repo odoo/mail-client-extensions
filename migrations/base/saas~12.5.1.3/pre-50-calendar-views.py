280b09739d96369e41fca60bada2940f152b9103
Denis Ledoux <dle@odoo.com>
2020-05-13 15:09:41 +0000

    [FIX] base@saas~12.5: attr `readonly_form_view_id` removed from calendar views
    The use of the attribute `readonly_form_view_id` has been removed
    from calendar views in
    odoo/odoo@369c20a99bd25172f72f4efde870172a4729aadc
    
    To avoid unnecessary manual work,
    remove them automatically from views where its still there,
    to automatically correct custom views using it.
    
    closes odoo/upgrade#1187
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/base/saas~12.5.1.3/pre-50-calendar-views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+        SELECT id
+          FROM ir_ui_view
+         WHERE type='calendar'
+           AND arch_db ilike '%readonly_form_view_id%'
+        """
+    )
+    for (view_id,) in cr.fetchall():
+        with util.edit_view(cr, view_id=view_id) as view:
+            for node in view.xpath("//calendar[@readonly_form_view_id]"):
+                del node.attrib["readonly_form_view_id"]
