751059914059b941a4209c55300a3b7f3bf2a03b
Nicolas Vannieuwerburgh <nvn@odoo.com>
2020-05-15 12:03:55 +0000

    [FIX] account@saas~12.4: Performance improvement
    Sometimes PostreSQL uses the indexes while it shouldn't.
    The mapping is processed by ids using https://github.com/odoo/upgrade/blob/92aea09f14e0d29af4e15447da30d3f0643060a2/migrations/account/saas~12.4.1.1/post-10-account-pocalypse.py#L380
    Therefore, those indexes shouldn't be used.
    
    With indexes (costs: 9788) -> explain plan https://explain.depesz.com/s/CIbZ
    without indexes: (costs: 6) -> explain plan https://explain.depesz.com/s/ochG
    
    Tested on problematic two databases and goes faster.
    
    closes odoo/upgrade#1203
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/account/saas~12.4.1.1/pre-70-create-index.py
+++ migrations/account/saas~12.4.1.1/pre-70-create-index.py
@@ -8,10 +8,7 @@ _logger = logging.getLogger("odoo.addons.base.maintenance.migrations.account.saa
 def migrate(cr, version):
 
     idx_list = [
-        ("_mig124_ai_currency_id", "account_invoice", "currency_id"),
         ("_mig124_il_invoice_id", "account_invoice_line", "invoice_id"),
-        ("_mig124_il_account_id", "account_invoice_line", "account_id"),
-        ("_mig124_ml_account_id", "account_move_line", "account_id"),
     ]
 
     # indexes will be dropped at end-10-account-pocalypse.py
@@ -20,8 +17,5 @@ def migrate(cr, version):
     for index_name, table_name, column_name in idx_list:
         util.ENVIRON["__created_accounting_idx"].append(index_name)
         create_index_queries.append("CREATE INDEX %s ON %s(%s)" % (index_name, table_name, column_name))
-    create_index_queries.append("CREATE INDEX _mig124_il_name ON account_invoice_line USING HASH (name)")
-    create_index_queries.append("CREATE INDEX _mig124_ml_name ON account_move_line USING HASH (name)")
-    util.ENVIRON["__created_accounting_idx"].extend(["_mig124_il_name", "_mig124_ml_name"])
     _logger.info("creating %s indexes (might be slow)", len(create_index_queries))
     util.parallel_execute(cr, create_index_queries)
