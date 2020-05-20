6caf9e5a21ee7987f8b8ada912c4690df9d69660
Christophe Simonis <chs@odoo.com>
2020-05-20 12:40:11 +0200

    [FIX] mass_mailing@saas~13.3

--- migrations/mass_mailing/saas~13.3.2.1/pre-migrate.py
+++ migrations/mass_mailing/saas~13.3.2.1/pre-migrate.py
@@ -1,27 +1,19 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.remove_field(cr, 'utm.campaign', 'mailing_clicks_ratio')
-    util.remove_field(cr, 'utm.campaign', 'mailing_items')
-    util.remove_field(cr, 'utm.campaign', 'mailing_clicked')
-    util.remove_field(cr, 'utm.campaign', 'total')
-    util.remove_field(cr, 'utm.campaign', 'scheduled')
-    util.remove_field(cr, 'utm.campaign', 'failed')
-    util.remove_field(cr, 'utm.campaign', 'ignored')
-    util.remove_field(cr, 'utm.campaign', 'sent')
-    util.remove_field(cr, 'utm.campaign', 'delivered')
-    util.remove_field(cr, 'utm.campaign', 'opened')
-    util.remove_field(cr, 'utm.campaign', 'replied')
-    util.remove_field(cr, 'utm.campaign', 'bounced')
-
-    util.rename_xmlid(cr, 'mass_mailing.view_mail_mass_mailing_contact_search', 'mass_mailing.mailing_contact_view_search')
-    util.rename_xmlid(cr, 'mass_mailing.view_mail_mass_mailing_contact_form', 'mass_mailing.mailing_contact_view_form')
-    util.rename_xmlid(cr, 'mass_mailing.view_mail_mass_mailing_contact_tree', 'mass_mailing.mailing_contact_view_tree')
-    util.rename_xmlid(cr, 'mass_mailing.view_mail_mass_mailing_contact_kanban', 'mass_mailing.mailing_contact_view_kanban')
-    util.rename_xmlid(cr, 'mass_mailing.view_mail_mass_mailing_contact_pivot', 'mass_mailing.mailing_contact_view_pivot')
-    util.rename_xmlid(cr, 'mass_mailing.view_mail_mass_mailing_contact_graph', 'mass_mailing.mailing_contact_view_graph')
-
     util.create_column(cr, "mailing_mailing", "lang", "varchar")
+
+    for field in "clicks_ratio items clicked".split():
+        util.remove_field(cr, "utm.campaign", f"mailing_{field}")
+    for field in "total scheduled failed ignored sent delivered opened replied bounced".split():
+        util.remove_field(cr, "utm.campaign", field)
+
+    # data
+    eb = util.expand_braces
+    for view in "search form tree kanban pivot graph".split():
+        util.rename_xmlid(cr, *eb(f"mass_mailing.{{view_mail_mass_mailing_contact,mailing_contact_view}}_{view}"))
+
+    util.remove_record(cr, "mass_mailing.link_tracker_action_mass_mailing")
+    util.remove_record(cr, "mass_mailing.link_tracker_action_mass_mailing_campaign")
