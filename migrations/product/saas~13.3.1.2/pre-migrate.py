90aac8c14d82d4719d7b431eef945b9e7e99ed60
Christophe Simonis <chs@odoo.com>
2020-05-20 15:04:47 +0200

    [FIX] product@saas~13.3

--- migrations/product/saas~13.3.1.2/pre-migrate.py
+++ migrations/product/saas~13.3.1.2/pre-migrate.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
-def migrate(cr, version):
-    util.remove_field(cr, 'product.template', 'rental')
-    util.remove_field(cr, 'product.product', 'rental')
 
-    cr.execute("""
-        UPDATE product_pricelist
-        SET discount_policy='with_discount'
-        WHERE discount_policy IS NULL
-    """)
+def migrate(cr, version):
+    util.remove_field(cr, "product.template", "rental")
+
+    # Now required
+    cr.execute("UPDATE product_pricelist SET discount_policy='with_discount' WHERE discount_policy IS NULL")
