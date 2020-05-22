5d2873909352d0f53473e902710e11d7c44b0553
SEINLET Nicolas <nicolas@seinlet.com>
2020-05-22 09:10:14 +0000

    [IMP] inter_company_rules@saas~12.5: set default value at column creation
    closes odoo/upgrade#1243
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/inter_company_rules/saas~12.5.1.1/pre-migrate.py
+++ migrations/inter_company_rules/saas~12.5.1.1/pre-migrate.py
@@ -4,8 +4,7 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.rename_field(cr, "res.company", "auto_validation", "auto_validation_old")
-    util.create_column(cr, "res_company", "auto_validation", "boolean")
+    util.create_column(cr, "res_company", "auto_validation", "boolean", default=False)
     cr.execute("UPDATE res_company SET auto_validation=TRUE WHERE auto_validation_old='validated'")
-    cr.execute("UPDATE res_company SET auto_validation=FALSE WHERE auto_validation IS NULL")
     util.remove_field(cr, "res.company", "auto_validation_old")
     util.remove_field(cr, "res.config.settings", "auto_validation")
