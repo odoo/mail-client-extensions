3ef9edd00525ad3bfcc257ef3e786667591b2891
fja-odoo <fja@odoo.com>
2020-05-26 15:00:58 +0000

    [IMP] website@saas~13.4: remove visibility_group
    visibility_group which was a M2O has been deleted. We now use the
    groups_id field which is a M2M.
    
    Related to: https://github.com/odoo/odoo/pull/51846
    
    task-2264580
    
    closes odoo/upgrade#1260
    
    Related: odoo/odoo#51846
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website/saas~13.4.1.0/pre-migrate.py
+++ migrations/website/saas~13.4.1.0/pre-migrate.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from odoo.upgrade import util
 
 
 def migrate(cr, version):
@@ -11,3 +12,14 @@ def migrate(cr, version):
            AND res_model = 'ir.module.module'
     """
     )
+
+    cr.execute(
+        """
+        INSERT INTO ir_ui_view_group_rel
+             SELECT id, visibility_group
+               FROM ir_ui_view
+              WHERE visibility_group is not null
+        ON CONFLICT DO NOTHING
+        """
+    )
+    util.remove_field(cr, "ir.ui.view", "visibility_group")
