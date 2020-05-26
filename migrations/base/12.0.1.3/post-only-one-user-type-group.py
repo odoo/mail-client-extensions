6cf132825051d0fc13052d2ee3e4e4a8d40c01cc
Denis Ledoux <dle@odoo.com>
2020-05-26 09:45:26 +0000

    [FIX] base@13.0: one group type per user
    A constraint added in 12.0 implies all users to be in only one group type
    (either employee, either portal, either public)
    
    Since odoo/odoo@459e6dc1d296c6fca11984c88046aa17ad32b026 (Odoo 13.0)
    The constraint is checked as soon as you had a group
    to a user or a user to a group,
    for instance through the data xml files.
    In addition, the constraint checks the groups of ALL users
    as soon as the constraint is called on mutiple users
    https://github.com/odoo/odoo/commit/459e6dc1d296c6fca11984c88046aa17ad32b026#diff-c087b09b8455bbf3fa546a56c6e271bcR435
    
    And in some data files, we sometimes had two users to a group in the same call.
    
    Therefore:
     - in 12.0, it was possible to bypass the constraint because of the reasons explained in revision
       odoo/odoo@459e6dc1d296c6fca11984c88046aa17ad32b026
       therefore leading to databases not respecting the constraint
     - in 13.0, as soon as two users are added to a group in a data xml files,
       the constraint is checked on all users
    
    We therefore need in upgrade scripts to make the constraint respected.
    
    Besides,
     - for 13.0, it needs to be done in a pre script,
       because it must be done before the data xml files are loaded,
     - for 12.0, it must be done in a post script, or at least after
       `base/saas~11.5.1.3/pre-20-user_system.py` so the user "2" is created
    
    The initial idea was to put this in a `0.0.0` script,
    but it cant be a post `0.0.0` for 13.0,
    and it cant be a pre `0.0.0` for 12.0, because `0.0.0` script are executed
    before specific version pre scripts, and it requires the script `base/saas~11.5.1.3/pre-20-user_system.py`.
    e.g.
    `base/0.0.0/pre-30-only-one-user-type-group.py` would be executed before
    `base/saas~11.5.1.3/pre-20-user_system.py`
    
    The idea is therefore to execute this cleaning script as a post in 12.0,
    and as a pre in 13.0.
    
    In the future, if its still possible to bypass the constraint during the 13.0 lifetime
    and we therefore need as well to clean this when upgrading to 14.0, we can
    then convert `base/13.0.1.3/pre-30-one-user-type-group.py`
    to `base/0.0.0/pre-30-one-user-type-group.py`
    with a `if util.version_gte("13.0"):`
    
    Requests:
    45366
    45326
    46901
    45207
    43428
    46067
    45521
    45233
    
    closes odoo/upgrade#1255
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/12.0.1.3/post-only-one-user-type-group.py
+++ migrations/base/12.0.1.3/post-only-one-user-type-group.py
@@ -3,23 +3,26 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
+    u2id = util.ENVIRON.get("user2_id")
+    assert isinstance(u2id, int)
+    one_user_type_group(cr, [1, u2id])
+
+
+def one_user_type_group(cr, admin_ids):
+    admin_ids = tuple(admin_ids)
     user = util.ref(cr, "base.group_user")
     portal = util.ref(cr, "base.group_portal")
     public = util.ref(cr, "base.group_public")
     settings = util.ref(cr, "base.group_system")
     accessrights = util.ref(cr, "base.group_erp_manager")
     usability = util.ref(cr, "base.module_category_usability")
-
-    u2id = util.ENVIRON["user2_id"]
-    assert isinstance(u2id, int)
-
     # remove public users from all other non usability groups
     cr.execute("""
         DELETE FROM res_groups_users_rel
               WHERE gid != %(public)s
                 AND gid NOT IN (SELECT id FROM res_groups WHERE category_id = %(usability)s)
                 AND uid IN (SELECT uid FROM res_groups_users_rel WHERE gid = %(public)s)
-                AND uid NOT IN (1, %(u2id)s)
+                AND uid NOT IN %(admin_ids)s
     """, locals())
 
     # remove portal users from all other non usability group
@@ -28,21 +31,20 @@ def migrate(cr, version):
               WHERE gid != %(portal)s
                 AND gid NOT IN (SELECT id FROM res_groups WHERE category_id = %(usability)s)
                 AND uid IN (SELECT uid FROM res_groups_users_rel WHERE gid = %(portal)s)
-                AND uid NOT IN (1, %(u2id)s)
+                AND uid NOT IN %(admin_ids)s
     """, locals())
 
     # remove SUPERUSER and user2 from `portal` and `public` groups
     cr.execute("""
         DELETE FROM res_groups_users_rel
               WHERE gid IN (%(portal)s, %(public)s)
-                AND uid IN (1, %(u2id)s)
+                AND uid IN %(admin_ids)s
     """, locals())
     # and force them to `employee`, `settings` and `access rights` groups
     util.fixup_m2m(cr, 'res_groups_users_rel', 'res_users', 'res_groups', 'uid', 'gid')
-    cr.execute("""
-        INSERT INTO res_groups_users_rel(uid, gid)
-             VALUES (1, %(user)s), (%(u2id)s, %(user)s)
-                  , (1, %(settings)s), (%(u2id)s, %(settings)s)
-                  , (1, %(accessrights)s), (%(u2id)s, %(accessrights)s)
-        ON CONFLICT DO NOTHING
-    """, locals())
+    for admin_id in admin_ids:
+        cr.execute("""
+            INSERT INTO res_groups_users_rel(uid, gid)
+                 VALUES (%(admin_id)s, %(user)s), (%(admin_id)s, %(settings)s), (%(admin_id)s, %(accessrights)s)
+            ON CONFLICT DO NOTHING
+        """, locals())
