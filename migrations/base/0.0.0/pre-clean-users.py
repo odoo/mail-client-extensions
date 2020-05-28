6f66ab1b43bec096063735565cfc57de36843442
Denis Ledoux <dle@odoo.com>
2020-05-28 12:00:25 +0000

    [FIX] base@0.0.0: Key (login)=(admin) already exists.
    M19092512352011
    Request 46159
    
    ```
    odoo.tools.convert.ParseError: "duplicate key value violates unique constraint "res_users_login_key"
    DETAIL:  Key (login)=(admin) already exists.
    ```
    
    closes odoo/upgrade#1273
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/base/0.0.0/pre-clean-users.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    if util.version_gte("saas~11.5"):
+        user_id = util.ref(cr, "base.user_admin")
+        if not user_id:
+            user_id = util.ensure_xmlid_match_record(cr, "base.user_admin", "res.users", {"login": "admin"})
+        if user_id:
+            util.force_noupdate(cr, "base.user_admin", True)
