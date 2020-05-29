2ceb16109cb847d59b004adec6a5fdf25f0e1a9f
Denis Ledoux <dle@odoo.com>
2020-05-29 15:09:11 +0000

    [FIX] l10n_be_invoice_bba: `structured` is no longer a valid value for `invoice_reference_type`
    Requests 48343, 48326, 48337, 48303, 45697, 45697, 48220, 45207, 48160, 47875
    
    To relate with
    odoo/upgrade@0e819053569d6e2e39fa97573481aac312ac78b5
    
    ```
    File "/src/odoo/saas-12.3/odoo/addons/base/maintenance/migrations/account/saas~12.3.1.1/end-10-taxes.py", line 84, in migrate
    migration_dicts_list = get_v13_migration_dicts(cr)
    File "/src/odoo/saas-12.3/odoo/addons/base/maintenance/migrations/account/saas~12.3.1.1/end-10-taxes.py", line 1698, in get_v13_migration_dicts
    inv = create_invoice(cr, partner, tax, type=tax.type_tax_use == "sale" and "out_invoice" or "in_invoice")
    File "/src/odoo/saas-12.3/odoo/addons/base/maintenance/migrations/account/saas~12.3.1.1/end-10-taxes.py", line 546, in create_invoice
    invoice_ctx.action_invoice_open()
    File "/src/odoo/saas-12.3/addons/sale/models/account_invoice.py", line 53, in action_invoice_open
    res = super(AccountInvoice, self).action_invoice_open()
    File "/src/odoo/saas-12.3/addons/account/models/account_invoice.py", line 1079, in action_invoice_open
    to_open_invoices.invoice_validate()
    File "/src/enterprise/saas-12.3/account_invoice_extract/models/account_invoice.py", line 271, in invoice_validate
    res = super(AccountInvoice, self).invoice_validate()
    File "/src/odoo/saas-12.3/addons/account/models/account_invoice.py", line 1520, in invoice_validate
    vals['reference'] = invoice._get_computed_reference()
    File "/src/odoo/saas-12.3/addons/account/models/account_invoice.py", line 510, in _get_computed_reference
    ref_function = getattr(self, '_get_reference_{}_{}'.format(self.journal_id.invoice_reference_model, self.journal_id.invoice_reference_type))
    AttributeError: 'account.invoice' object has no attribute '_get_reference_odoo_structured'
    ```
    
    closes #1271
    
    closes odoo/upgrade#1282
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/l10n_be_invoice_bba/saas~12.3.1.0/pre-10-models.py
+++ migrations/l10n_be_invoice_bba/saas~12.3.1.0/pre-10-models.py
@@ -3,6 +3,20 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
+    cr.execute(
+        """
+        UPDATE account_journal j
+           SET invoice_reference_type = CASE c.l10n_be_structured_comm
+                                             WHEN 'partner_ref' THEN 'partner'
+                                             ELSE 'invoice'
+                                         END,
+               invoice_reference_model  = 'be'
+          FROM res_company c
+         WHERE c.id = j.company_id
+           AND invoice_reference_type = 'structured'
+        """
+    )
+
     util.remove_field(cr, "res.company", 'l10n_be_structured_comm')
     util.remove_field(cr, "res.config.settings", 'l10n_be_structured_comm')
     util.remove_view(cr, 'l10n_be_invoice_bba.res_config_settings_view_form')
