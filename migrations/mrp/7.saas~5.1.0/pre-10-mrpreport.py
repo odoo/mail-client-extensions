00c97cc4d00e226e6bc78372e73f0a9297717c56
Christophe Simonis <chs@odoo.com>
2020-06-03 17:30:46 +0000

    [FIX] mrp@7.saas~5: avoid deleting unknown model
    No need to correct the call to use the model name, the deletion of the
    view is enough.

--- migrations/mrp/7.saas~5.1.0/pre-10-mrpreport.py
+++ migrations/mrp/7.saas~5.1.0/pre-10-mrpreport.py
@@ -5,11 +5,10 @@ def migrate(cr, version):
     # Need to change many2many for consume lines to one2many
     util.create_column(cr, 'stock_move', 'raw_material_production_id', 'int4')
     cr.execute("""
-        UPDATE stock_move 
+        UPDATE stock_move
         SET raw_material_production_id = mrp.id
         FROM mrp_production mrp, mrp_production_move_ids mrpmove
         WHERE mrpmove.move_id = stock_move.id AND mrpmove.production_id = mrp.id
     """)
     # It is easiest to rebuild this report as the value field does not get well migrated
-    util.delete_model(cr, 'report_mrp_inout', drop_table=False)
     cr.execute("DROP VIEW IF EXISTS report_mrp_inout")
