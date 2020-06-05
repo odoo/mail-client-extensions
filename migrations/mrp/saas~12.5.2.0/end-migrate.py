64f35e7933e473c31790e2e776dc8149107c9577
Rémy Baranx (bar) <bar@odoo.com>
2020-06-05 08:07:17 +0000

    [FIX] mrp@saas-12.5: some MTO stock rules linked to company were missing
    Some stock locations shared in 12.0 are now specific to each company.
    In consequence, new MTO stock rules have to be created to point to these locations.
    
    This PR fixes 2 issues from the PR #1204:
    - stock rules creation must be done after the creation of new locations,
      which is done during the `end` phase. So stock rules creation has been moved to
      a `end` script.
    - created stock rules were not active => set them active in the `INSERT` query.
    
    closes odoo/upgrade#1297
    
    Issue: #1155
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/mrp/saas~12.5.2.0/end-migrate.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    """
+    A new MTO stock rule has to be created for each warehouse company.
+    New 'production' locations linked to compagnies must be created before (this is done in stock/saas~12.1.1.1/end-migrate.py).
+    """
+    try:
+        mto_route = util.env(cr)["stock.warehouse"]._find_global_route("stock.route_warehouse0_mto", "Make To Order").id
+    except Exception:
+        mto_route = False
+
+    if mto_route:
+        cr.execute(
+            """
+            INSERT INTO stock_rule (
+                        name,
+                        procure_method,
+                        company_id,
+                        action,
+                        auto,
+                        route_id,
+                        location_id,
+                        location_src_id,
+                        picking_type_id,
+                        active
+                   )
+                   SELECT
+                        CASE
+                            WHEN sw.manufacture_steps = 'mrp_one_step' THEN sw.code || ': ' || stock_loc.name
+                                                        || ' → ' || prod_loc.name || ' (MTO)'
+                            ELSE sw.code || ': ' || pbm_loc.name || ' → ' || prod_loc.name || ' (MTO)'
+                        END,
+                        'make_to_order',
+                        sw.company_id,
+                        'pull',
+                        'manual',
+                        %s,
+                        prod_loc.id,
+                        CASE
+                            WHEN sw.manufacture_steps = 'mrp_one_step' THEN sw.lot_stock_id
+                            ELSE sw.pbm_loc_id
+                        END,
+                        spt.id,
+                        true
+              FROM stock_warehouse sw
+              JOIN stock_location prod_loc ON prod_loc.company_id = sw.company_id AND prod_loc.usage = 'production'
+              JOIN stock_location stock_loc ON stock_loc.id = sw.lot_stock_id
+              JOIN stock_location pbm_loc ON pbm_loc.id = sw.pbm_loc_id
+              JOIN stock_picking_type spt ON spt.warehouse_id = sw.id AND spt.code = 'mrp_operation'
+            """,
+            (mto_route,),
+        )
