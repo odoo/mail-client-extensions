6ec443c276d9eb301bc0d24211553a3c30f960c1
Rémy Baranx (bar) <bar@odoo.com>
2020-06-05 12:39:45 +0000

    [FIX] purchase@9.0: remove the view purchase.view_request_for_quotation_filter
    The view `view_request_for_quotation_filter` has been removed from purchase
    in `9.0`.
    
    Upgrade request: #47276
    
    closes odoo/upgrade#1368
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/purchase/9.0.1.1/pre-05-views.py
+++ migrations/purchase/9.0.1.1/pre-05-views.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 from openerp.addons.base.maintenance.migrations import util
 
+
 def migrate(cr, version):
-    view_list = ["view_purchase_config", "purchase_order_2_stock_picking", "view_product_normal_purchase_buttons_from",
-                 "purchase_order_line_form", "view_purchase_line_invoice"]
+    view_list = [
+        "view_purchase_config",
+        "purchase_order_2_stock_picking",
+        "view_product_normal_purchase_buttons_from",
+        "purchase_order_line_form",
+        "view_purchase_line_invoice",
+        "view_request_for_quotation_filter",
+    ]
     for view in view_list:
         util.remove_view(cr, "purchase." + view)
 
-    for r in 'order quotation'.split():
-        util.force_noupdate(cr, 'purchase.report_purchase%s' % r, False)
-        util.force_noupdate(cr, 'purchase.report_purchase%s_document' % r, False)
+    for r in "order quotation".split():
+        util.force_noupdate(cr, "purchase.report_purchase%s" % r, False)
+        util.force_noupdate(cr, "purchase.report_purchase%s_document" % r, False)
