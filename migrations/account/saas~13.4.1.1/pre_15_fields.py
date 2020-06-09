59e7b3dc5ed6b3c11330edb3726aaa5dcec38bad
oco-odoo <oco@odoo.com>
2020-06-09 13:45:40 +0000

    [IMP] account@saas-13.4: introduce new 'past_months_limit' field on reconciliation models
    From PR:
    https://github.com/odoo/odoo/pull/50083
    https://github.com/odoo/enterprise/pull/10166
    
    closes odoo/upgrade#1317
    
    Related: odoo/enterprise#10166
    Related: odoo/odoo#50083
    Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>

---
+++ migrations/account/saas~13.4.1.1/pre_15_fields.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+def migrate(cr, version):
+    # Reconciliation models refactoring (Task 2182807)
+    util.create_column(cr, "account_reconcile_model", "past_months_limit", "int4", default=18)
