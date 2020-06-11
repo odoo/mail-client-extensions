d3972af64040c09c3f98518bb3afe32a2cc05ea2
Denis Ledoux <dle@odoo.com>
2020-06-11 11:36:26 +0000

    [FIX] account@saas~12.5: ensure chart templates are up-to-date
    Related to odoo/odoo#52816
    See the explanation in the above PR description
    
    closes odoo/upgrade#1340
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/account/saas~12.5.1.1/pre-data.py
+++ migrations/account/saas~12.5.1.1/pre-data.py
@@ -25,3 +25,5 @@ def migrate(cr, version):
     util.remove_view(cr, "account.view_account_bnk_stmt_cashbox")
 
     util.force_noupdate(cr, "account.open_account_journal_dashboard_kanban", False)
+
+    cr.execute("UPDATE ir_model_data SET noupdate=false WHERE model LIKE 'account.chart.template'")
