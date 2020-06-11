3a8d90b335330d3df7f6cc29470914836c539014
Christophe Simonis <chs@odoo.com>
2020-06-11 15:52:00 +0200

    [FIX] industry_fsm_sale@saas~13.3

--- migrations/industry_fsm_sale/saas~13.3.1.0/pre-migrate.py
+++ migrations/industry_fsm_sale/saas~13.3.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "project.task", "fsm_to_invoice")
+    util.remove_view(cr, "industry_fsm_sale.project_task_view_search")
