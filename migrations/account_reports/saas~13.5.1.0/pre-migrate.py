4af78c67f4b64b2d36671bfd0001bfa3e96753f5
fja-odoo <fja@odoo.com>
2020-06-15 13:11:31 +0000

    [IMP] account_reports@saas~13.5: update fields
    Introduction of tax_closing_end_date, which replaces is_tax_closing.
    
    Related to: https://github.com/odoo/odoo/pull/52988
    Related to: https://github.com/odoo/enterprise/pull/11160
    
    task-2201995
    
    closes odoo/upgrade#1347
    
    Related: odoo/odoo#52988
    Related: odoo/enterprise#11160
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/account_reports/saas~13.5.1.0/pre-migrate.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+def migrate(cr, version):
+    util.create_column(cr, 'account_move', 'tax_closing_end_date', 'date')
+    cr.execute(
+        """
+        UPDATE account_move
+           SET tax_closing_end_date = date
+         WHERE is_tax_closing
+    """
+    )
+    util.remove_field(cr, 'account.move', 'is_tax_closing')
