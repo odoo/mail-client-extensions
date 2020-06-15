6cfe3217ce15d643efbbb0ced9f64c5082386061
Christophe Simonis <chs@odoo.com>
2020-06-15 09:43:33 +0000

    FIX] pos_cache@10.saas~18: correctly encode/decode base64
    The methods `encodestring`/`decodestring` doesn't actually accept
    strings. They are aliases to `encodebytes`/`decodebytes`.
    
    Handle case of filestore absence.
    
    Also browse pos caches one by one as they can be heavy.
    
    closes odoo/upgrade#1343
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/pos_cache/10.saas~18.1.0/post-no-pickle.py
+++ migrations/pos_cache/10.saas~18.1.0/post-no-pickle.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import base64
 import json
+
 try:
     from odoo.tools import pickle as cPickle
 except ImportError:
@@ -11,9 +12,15 @@ except ImportError:
 
 from odoo.addons.base.maintenance.migrations import util
 
+
 def migrate(cr, version):
-    for cache in util.env(cr)['pos.cache'].search([]):
-        data = cPickle.loads(base64.decodestring(cache.cache))
-        cache.write({
-            'cache': base64.encodestring(json.dumps(data).encode('utf-8'))
-        })
+    PosCache = util.env(cr)["pos.cache"]
+    ids = PosCache.search([]).ids
+    for cache in util.iter_browse(PosCache, ids, chunk_size=1):
+        if not cache.cache:
+            # We don't have the data because the filestore is not available.
+            # force a refresh at next loading.
+            cache.write({"cache": False})
+        else:
+            data = cPickle.loads(base64.decodebytes(cache.cache))
+            cache.write({"cache": base64.encodebytes(json.dumps(data).encode("utf-8"))})
