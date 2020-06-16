592f36e60eec3727915b5493a0db6cd7fa60eb4b
Nicolas Vannieuwerburgh <nvn@odoo.com>
2020-06-16 12:48:56 +0000

    [FIX] website_sale@saas~12.5: compute product_image.can_image_1024_be_zoomed
    The filestore is not available during the upgrade.
    product_image.can_image_1024_be_zoomed is always false
    
    computing this allows to remove a lot of logs like
    
    2020-06-13 22:46:10,455 3136 INFO template_e29c70d2a084_46328_13.0 odoo.addons.base.models.ir_attachment: _read_file reading /home/odoo/.local/share/Odoo/filestore/template_e29c70d2a084_46328_13.0/f2/f270c3e9f00bed7eada671f62fa800ec4dd35583
    Traceback (most recent call last):
    File "/src/odoo/13.0/odoo/api.py", line 745, in get
    value = self._data[field][record._ids[0]]
    KeyError: 1
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
    File "/src/odoo/13.0/odoo/fields.py", line 1002, in get
    value = env.cache.get(record, self)
    File "/src/odoo/13.0/odoo/api.py", line 751, in get
    raise CacheMiss(record, field)
    odoo.exceptions.CacheMiss: ('product.image(1,).image_1920', None)
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
    File "/src/odoo/13.0/odoo/api.py", line 745, in get
    value = self._data[field][record._ids[0]]
    KeyError: 796339
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
    File "/src/odoo/13.0/odoo/fields.py", line 1002, in get
    value = env.cache.get(record, self)
    File "/src/odoo/13.0/odoo/api.py", line 751, in get
    raise CacheMiss(record, field)
    odoo.exceptions.CacheMiss: ('ir.attachment(796339,).datas', None)
    
    closes odoo/upgrade#1353
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/website_sale/saas~12.5.1.0/end-10-compute.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    imp = util.import_script("product/saas~12.2.1.2/post-image.py")
+    imp.image_mixin_recompute_fields(cr, "product.image", suffixes=["1024"])
--- migrations/website_sale/saas~12.5.1.0/pre-images.py
+++ migrations/website_sale/saas~12.5.1.0/pre-images.py
@@ -6,3 +6,5 @@ def migrate(cr, version):
     image = util.import_script("base/saas~12.5.1.3/pre-21-images.py")
     image.rename_mixin_fields(cr, "product.image")
     image.new_inherit_mixin(cr, "product.public.category")
+    util.create_column(cr, "product_image", "can_image_1024_be_zoomed", "bool")
+    cr.execute("UPDATE product_image set can_image_1024_be_zoomed = 'f'")
