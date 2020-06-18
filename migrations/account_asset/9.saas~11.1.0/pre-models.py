786dbe4521b1bc53d0bd384515f73ec4b061e60f
Denis Ledoux <dle@odoo.com>
2020-06-18 19:53:15 +0000

    [FIX] account_asset@9.saas~11: remove field `account.move` `asset_id`
    The field `asset_id` on `account.move`
    was there in 9.0,
    was removed 9.saas~11 with odoo/odoo@eb8d4aaa59562b41704c715202d0c794fd950a0b
    and then added back in saas~12.3 with odoo/enterprise@6cb107c42c1e3a679f7c7b2f41466f39d042bd16
    
    If a database upgrades from 9.0 to saas~12.3,
    and has leftover values for `asset_id` in its accounting moves,
    it might add depreciation lines because they are a one2many on `account.move` with `asset_id`:
    https://github.com/odoo/enterprise/blob/c8958989bcbb1539df11ee617b8b8204ea980c24/account_asset/models/account_asset.py#L49
    therefore computing wrongly the residual value of the asset.
    
    Request 48329
    
    closes odoo/upgrade#1365
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/account_asset/9.saas~11.1.0/pre-models.py
+++ migrations/account_asset/9.saas~11.1.0/pre-models.py
@@ -6,3 +6,8 @@ def migrate(cr, version):
                                    target_model='account.asset.category', default_value=None)
     util.convert_field_to_property(cr, 'product.template', 'deferred_revenue_category_id', 'many2one',
                                    target_model='account.asset.category', default_value=None)
+    # https://github.com/odoo/odoo/commit/eb8d4aaa59562b41704c715202d0c794fd950a0b#diff-e35933343c229da0afceb2a3b9e3639bL503
+    # It's important to remove it, because the field is re-introduced in saas-12.3
+    # https://github.com/odoo/enterprise/commit/6cb107c42c1e3a679f7c7b2f41466f39d042bd16#diff-d0dc174b28be71f15f8654cd9a9f5a20R13
+    # And it might lead to inconsistencies, adding new depreciation lines to the asset if there are leftovers
+    util.remove_field(cr, 'account.move', 'asset_id')
