bb9c7092dc3a25b82a61aae53298aa768786f373
Christophe Simonis <chs@odoo.com>
2020-06-19 15:35:24 +0000

    [FIX] account_sepa_direct_debit: field is still in database
    opw-2276662
    opw-2274430
    Closes #1373
    
    closes odoo/upgrade#1375
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account_sepa_direct_debit/saas~12.2.1.0/pre-migrate.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo.addons.base.maintenance.migrations import util
-
-
-def migrate(cr, version):
-    util.convert_binary_field_to_attachment(cr, "sdd.mandate", "original_doc")
