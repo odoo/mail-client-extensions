3f39a70a8eb2bd42d834ff8d634c875c2a640a9e
Mahendra Barad <mba@odoo.com>
2020-06-22 06:26:07 +0000

    [IMP] planning,project_forecast@saas~13.5: Remove the planing slot analysis report model
    In the task removed planning.slot.report.analysis model and report
    and moved it to the plannning based on planning.slot.
    
    closes odoo/upgrade#1377
    
    Taskid: 2172226
    Related: https://github.com/odoo/enterprise/pull/10943
    Related: odoo/enterprise#10943
    Closes: #1377
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/project_forecast/saas~13.5.1.0/pre-10-migrate.py
+++ migrations/project_forecast/saas~13.5.1.0/pre-10-migrate.py
@@ -4,3 +4,11 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "project_forecast.project_forecast_view_gantt")
+    util.remove_view(cr, "project_forecast.project_forecast_view_pivot")
+    util.remove_view(cr, "project_forecast.project_forecast_view_graph")
+    util.remove_view(cr, "project_forecast.project_forecast_view_grid")
+    util.remove_record(cr, "project_forecast.project_forecast_action_analysis")
+    util.remove_record(cr, "project_forecast.project_forecast_action_analysis_pivot")
+    util.remove_record(cr, "project_forecast.project_forecast_action_analysis_graph")
+    util.remove_record(cr, "project_forecast.project_forecast_action_analysis_gantt")
+    util.remove_menus(cr, [util.ref(cr, "project_forecast.project_forecast_report_activities")])
