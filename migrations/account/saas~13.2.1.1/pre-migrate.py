a8e77e57903190987d59ed65e048e53d85f3289e
william <wan@odoo.com>
2020-06-22 12:31:34 +0000

    [FIX] account@saas~13.3: sequence fetch next sequence correctly
    Before, if we had PREFIX/9 and PREFIX/10, the highest number was the
    lexicographic order, leading to proposing PREFIX/10 again and again.
    
    Because of performances, we can't execute the regex in the SELECT query
    and we need to store it in the database. This will allow to do an
    indexed search.
    
    See https://github.com/odoo/odoo/pull/53393

--- migrations/account/saas~13.2.1.1/pre-migrate.py
+++ migrations/account/saas~13.2.1.1/pre-migrate.py
@@ -1,14 +1,83 @@
 # -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
+import re
+
 
 def migrate(cr, version):
+    # ===========================================================
+    # Sequence Performances (PR:53393)
+    # ===========================================================
+    util.create_column(cr, "account_journal", "sequence_override_regex", "varchar")
+    for table in ['account_move', 'account_bank_statement']:
+        util.create_column(cr, table, 'sequence_prefix', 'varchar')
+        util.create_column(cr, table, 'sequence_number', 'int4')
+
+    cr.execute("""
+        SELECT journal.id,
+               journal.name,
+               COALESCE(prefix, '') AS prefix,
+               COALESCE(suffix, '') AS suffix
+          FROM account_journal journal
+          JOIN ir_sequence sequence ON journal.sequence_id = sequence.id
+    """)
+
+    for res in cr.dictfetchall():
+        substitute = {'year': 0, 'month': 0, 'range_year': 0, 'range_month': 0, 'y': 0, 'range_y': 0}
+        try:
+            res['prefix'] % substitute
+            res['suffix'] % substitute
+        except KeyError:
+            raise util.MigrationError(
+                "Only 'range_year', 'range_y', 'range_month', 'year' and 'month' are valid"
+                " placeholders in prefix and suffix for ir.sequence on %s." % res['name']
+            )
+        if re.search(r'%\(\w+\)s', res['prefix']) and re.search(r'%\(\w+\)s', res['suffix']):
+            raise util.MigrationError("Placeholders can not be in both prefix and suffix")
+
+        if res['suffix'] and re.search(r'(\d|%\(\w+\)s)', res['suffix']):
+            suffix_regex = res['suffix'].replace('%(year)s', r'(?P<year>(\d{4})?)')\
+                                        .replace('%(y)s', r'(?P<year>(\d{2})?)')\
+                                        .replace('%(month)s', r'(?P<month>(\d{2})?)')\
+                                        .replace('%(range_year)s', r'(?P<year>(\d{4})?)')\
+                                        .replace('%(range_y)s', r'(?P<year>(\d{2})?)')\
+                                        .replace('%(range_month)s', r'(?P<month>(\d{2})?)')
+            regex = r'^(?P<prefix1>.*?)(?P<seq>\d*)(?P<suffix>{})$'.format(suffix_regex)
+            cr.execute("""
+                UPDATE account_journal
+                   SET sequence_override_regex = %(regex)s
+                 WHERE id = %(journal_id)s;
+            """, {
+                'regex': regex,
+                'journal_id': res['id'],
+            })
+            number = re.sub(r'\?P<\w+>', '?:', regex.replace(r'?P<seq>', ''))
+            prefix = re.sub(r'\?P<\w+>', '', re.sub(r'(\?<seq>.*)', '(?:.*)', regex))
+            for table in ['account_move', 'account_bank_statement']:
+                cr.execute("""
+                    UPDATE {}
+                       SET sequence_prefix = (regexp_match(name, %(prefix)s))[1],
+                           sequence_number = ('0' || (regexp_match(name, %(number)s))[1])::integer
+                     WHERE journal_id = %(journal_id)s;
+                """.format(table), {
+                    'prefix': prefix,
+                    'number': number,
+                    'journal_id': res['id'],
+                })
+
+    for table in ['account_move', 'account_bank_statement']:
+        cr.execute(r"""
+            UPDATE %s
+               SET sequence_prefix = (regexp_match(name, '^(.*?)(?:\d*)(?:\D*?)$'))[1],
+                   sequence_number = ('0' || (regexp_match(name, '^(?:.*?)(\d*)(?:\D*?)$'))[1])::integer
+             WHERE sequence_number IS NULL;
+        """ % (table,))
+
     util.remove_field(cr, "account.journal", "sequence_id")
     util.remove_field(cr, "account.journal", "refund_sequence_id")
     util.remove_field(cr, "account.journal", "sequence_number_next")
     util.remove_field(cr, "account.journal", "refund_sequence_number_next")
 
-    util.create_column(cr, "account_journal", "sequence_override_regex", "varchar")
     util.create_column(cr, "account_journal", "sale_activity_type_id", "int4")
     util.create_column(cr, "account_journal", "sale_activity_user_id", "int4")
     util.create_column(cr, "account_journal", "sale_activity_note", "text")
