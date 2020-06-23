d00ff99e0573a251c901f49e9804639300319139
Denis Ledoux <dle@odoo.com>
2020-06-23 17:10:37 +0000

    [FIX] base@11.0: incompatible theme modules
    Somehow, this is possible to have the default or bootswatch theme
    installed in addition to one other theme
    if you come from Odoo 9.0 or Odoo 10.0, but Odoo 11.0 doesn't support it.
    
    - odoo/migration-scripts@39495dc672faa07cdc3c43b002d9a8ed7d642109
    - odoo/migration-scripts@da8824056cc4c36973a2c5239ed03e3c11edd10a
    - odoo/migration-scripts@54d59903fff6b135745ab941712cdc5c23762f2e
    - odoo/migration-scripts@bae1dba4feb2e28890b9199f50c969099823f408
    - odoo/migration-scripts@4406625aadb6d5d7cfffc0504b6f95e630811205
    - odoo/migration-scripts@d48fa0a38d53a22e27c4179787f429e061ddd5d9
    - odoo/migration-scripts@2ce3660b7a0290f2ccadfc39e9d5b7577b5bff80
    - odoo/migration-scripts@db8f83dec34c1f0d37740f03b636a86f24ca4b84
    - odoo/migration-scripts@90e27e356eaff5ac381d34577097620e9151b8ed
    - odoo/migration-scripts@1e0cafe1bdc8d4d43dcd81bdb2ae6675c37848b1
    - odoo/migration-scripts@27871585f67178d7c83e742a57fe0f1d9bf5e83f
    - odoo/migration-scripts@4f3cfdd7083bd15382fee7a864a26219497c4cd2
    - odoo/migration-scripts@188508f25d41eeb75ff0c45013b7a72ff4b364bc
    - odoo/migration-scripts@0afa9042c74efb26d309a5f71a62c681ac9ce42a
    - odoo/migration-scripts@e52e0ba266aa96b23f209dd058cbccd966f895b2
    
    closes odoo/upgrade#1391
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/base/11.0.1.3/pre-10-modules.py
+++ migrations/base/11.0.1.3/pre-10-modules.py
@@ -20,3 +20,21 @@ def migrate(cr, version):
 
         # https://github.com/odoo/enterprise/pull/6291
         util.new_module(cr, "account_reports_cash_flow", deps={"account_reports"}, auto_install=True)
+
+    # Somehow, this is possible to have the default or bootswatch theme installed in addition to one other theme
+    # if you come from Odoo 9.0 or Odoo 10.0, but Odoo 11.0 doesn't support it.
+    if util.module_installed(cr, "website"):
+        cr.execute(
+            """
+                SELECT name
+                FROM ir_module_module
+                WHERE name like 'theme_%%'
+                AND state in %s
+            """,
+            (util._INSTALLED_MODULE_STATES,),
+        )
+        installed_themes = [t[0] for t in cr.fetchall()]
+        for theme in ["theme_default", "theme_bootswatch"]:
+            if len(installed_themes) > 1 and theme in installed_themes:
+                util.uninstall_module(cr, theme)
+                installed_themes.remove(theme)
