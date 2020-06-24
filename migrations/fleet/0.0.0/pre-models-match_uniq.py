a9390f67bbb1e4304864dcd72d061873edf96427
Denis Ledoux <dle@odoo.com>
2020-06-24 12:35:55 +0000

    [FIX] fleet@0.0.0: In 10.0, `fleet.vehicle.state` is in `fleet.py`
    closes odoo/upgrade#1400
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/fleet/0.0.0/pre-models-match_uniq.py
+++ migrations/fleet/0.0.0/pre-models-match_uniq.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from odoo import models
-import odoo.addons.fleet.models.fleet_vehicle as _ignored  # noqa
+
+try:
+    import odoo.addons.fleet.models.fleet_vehicle as _ignored  # noqa
+except Exception:
+    import odoo.addons.fleet.models.fleet as _ignored  # noqa
 
 
 def migrate(cr, version):
