d645cad14a4e9be9ca81d692dea7d0c8853be2c3
Christophe Simonis <chs@odoo.com>
2020-06-24 11:40:49 +0000

    [FIX] documents_hr_payroll@saas~13.3: handle multi-company
    closes odoo/upgrade#1399
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/documents_hr_payroll/saas~13.3.1.0/post-migrate.py
+++ migrations/documents_hr_payroll/saas~13.3.1.0/post-migrate.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
-from odoo.addons.base.maintenance.migrations import util
+from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    payroll_folder = util.ref(cr, "documents_hr_payroll.documents_payroll_folder")
+
     cr.execute(
         "UPDATE res_company SET documents_payroll_folder_id=%s WHERE documents_payroll_folder_id IS NULL",
-        [util.ref(cr, "documents_hr_payroll.documents_payroll_folder")],
+        [payroll_folder],
     )
+    if cr.rowcount > 1:
+        # if we are in multi-company
+        # If the parent folder of "Payroll" is limited to a company, move "Payroll" at root
+        cr.execute(
+            """
+                UPDATE documents_folder f
+                   SET parent_folder_id = NULL
+                  FROM documents_folder p
+                 WHERE p.id = f.parent_folder_id
+                   AND p.company_id IS NOT NULL
+                   AND f.id = %s
+            """,
+            [payroll_folder],
+        )
