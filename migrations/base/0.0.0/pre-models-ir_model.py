e85eaaa84539198f0f4dafd36846ca0dd55040a5
Denis Ledoux <dle@odoo.com>
2020-06-25 09:32:06 +0000

    [FIX] base@0.0.0: no hard fail for fields being deleted because of the delegate bug
    See odoo/odoo#53632
    
    closes odoo/upgrade#1408
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/0.0.0/pre-models-ir_model.py
+++ migrations/base/0.0.0/pre-models-ir_model.py
@@ -30,8 +30,23 @@ class Field(models.Model):
     _module = "base"
 
     def unlink(self):
-        fields = ["%s.%s" % (f.model, f.name) for f in self]
-        raise util.MigrationError(
-            "💥 It's look like you forgot to call `util.remove_field` on the following fields: %s" % ", ".join(fields)
-        )
-        return super(Field, self).unlink()
+        unlink_fields = self.env["ir.model.fields"]
+        for field in self:
+            if field.model in self.env:
+                f = self.env[field.model]._fields.get(field.name)
+                if f and f.inherited:
+                    # See https://github.com/odoo/odoo/pull/53632
+                    util._logger.critical(
+                        "The field %s.%s is deleted but is still in the registry. It may come from a delegated field.",
+                        field.model,
+                        field.name,
+                    )
+                    continue
+            unlink_fields |= field
+        if unlink_fields:
+            fields = ["%s.%s" % (f.model, f.name) for f in unlink_fields]
+            raise util.MigrationError(
+                "💥 It's look like you forgot to call `util.remove_field` on the following fields: %s"
+                % ", ".join(fields)
+            )
+        return super(Field, unlink_fields).unlink()
