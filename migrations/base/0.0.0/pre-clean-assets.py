da74bdb129e0ac1650ec3910968eeab858c0e186
Christophe Simonis <chs@odoo.com>
2020-06-26 08:22:47 +0000

    [ADD] base@0.0.0: remove assets attachements
    closes odoo/upgrade#1416
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/base/0.0.0/pre-clean-assets.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+def migrate(cr, version):
+    # bundles
+    cr.execute(
+        r"""
+            DELETE FROM ir_attachment
+                  WHERE res_model = 'ir.ui.view'
+                    AND COALESCE(res_id, 0) = 0
+                    AND res_field IS NULL
+                    AND mimetype IN ('text/css', 'application/javascript')
+                    AND url ~ '^/web/content/\d+-[0-9a-f]+/.*\.(css|js)$'
+                    AND url LIKE '%assets%'
+    """
+    )
+    # individual css files
+    cr.execute(
+        r"""
+            DELETE FROM ir_attachment
+                  WHERE res_model IS NULL
+                    AND COALESCE(res_id, 0) = 0
+                    AND res_field IS NULL
+                    AND mimetype = 'text/css'
+                    AND url ~ '^/[\w_]+/static/.*\.(scss|less|sass).css$'
+        """
+    )
