5bb87277b29e1c63eeba69ddff34a966a5e424cf
dmonzonis <mod@odoo.com>
2020-07-01 12:43:08 +0000

    [FIX] mail@10.saas~15: missing create_column for is_mail_thread
    is_mail_thread became a column of the ir_model table but a create_column
    was missing. If the database tried to update ir_model records from the
    new version, the upgrade would fail since it would try to set the
    is_mail_thread column to something, but it is not created yet.
    
    closes odoo/upgrade#1438
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/mail/10.saas~15.1.0/pre-models.py
+++ migrations/mail/10.saas~15.1.0/pre-models.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
 from odoo.addons.base.maintenance.migrations import util
 
-def migrate(cr, version):
-    if util.module_installed(cr, 'web_studio'):
-        util.move_field_to_module(cr, 'ir.model', 'mail_thread', 'web_studio', 'mail')
-        util.rename_field(cr, 'ir.model', 'mail_thread', 'is_mail_thread')
-        util.move_field_to_module(cr, 'ir.model.fields', 'track_visibility', 'web_studio', 'mail')
-        util.rename_xmlid(cr, 'web_studio.model_form_view','mail.model_form_view')
-        util.rename_xmlid(cr, 'web_studio.model_search_view','mail.model_search_view')
-        util.rename_xmlid(cr, 'web_studio.field_form_view','mail.field_form_view')
 
-    util.create_column(cr, 'res_users', 'notification_type', 'varchar')
-    cr.execute("""
+def migrate(cr, version):
+    if util.module_installed(cr, "web_studio"):
+        util.move_field_to_module(cr, "ir.model", "mail_thread", "web_studio", "mail")
+        util.rename_field(cr, "ir.model", "mail_thread", "is_mail_thread")
+        util.move_field_to_module(cr, "ir.model.fields", "track_visibility", "web_studio", "mail")
+        util.rename_xmlid(cr, "web_studio.model_form_view", "mail.model_form_view")
+        util.rename_xmlid(cr, "web_studio.model_search_view", "mail.model_search_view")
+        util.rename_xmlid(cr, "web_studio.field_form_view", "mail.field_form_view")
+
+    # No need to set values for this column as they will be set by the _reflect_model_params method
+    # when models are loaded
+    util.create_column(cr, "ir_model", "is_mail_thread", "boolean")
+
+    util.create_column(cr, "res_users", "notification_type", "varchar")
+    cr.execute(
+        """
         UPDATE res_users u
            SET notification_type = CASE WHEN p.notify_email = 'none' THEN 'inbox' ELSE 'email' END
           FROM res_partner p
          WHERE p.id = u.partner_id
-    """)
-    util.remove_field(cr, 'res.partner', 'notify_email')
+    """
+    )
+    util.remove_field(cr, "res.partner", "notify_email")
