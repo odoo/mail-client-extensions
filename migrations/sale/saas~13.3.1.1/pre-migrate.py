f51977b86dfd2b04e557b517ea9bcbbe49229a77
SEINLET Nicolas <nicolas@seinlet.com>
2020-07-06 11:48:27 +0000

    [FIX] sale@saas~13.3

--- migrations/sale/saas~13.3.1.1/pre-migrate.py
+++ migrations/sale/saas~13.3.1.1/pre-migrate.py
@@ -7,14 +7,19 @@ def migrate(cr, version):
     util.move_field_to_module(cr, "sale.order", "tag_ids", "sale_crm", "sale")
     util.create_column(cr, "sale_order", "currency_id", "int4")
 
-    cr.execute(
+    util.parallel_execute(
+        cr,
+        util.explode_query(
+            cr,
         """
             UPDATE sale_order s
                SET currency_id = p.currency_id
               FROM product_pricelist p
              WHERE p.id = s.pricelist_id
-        """
-    )
+        """,
+        prefix="s.",
+    ),
+)
 
     gone = """
         access_sale_order_invoicing_payments_readonly
