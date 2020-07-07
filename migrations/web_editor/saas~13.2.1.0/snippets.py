88ade6e7adcbd24433ffcc416ede51ec0fc5aa13
Christophe Simonis <chs@odoo.com>
2020-07-07 13:44:02 +0000

    [IMP] web_editor@saas~13.2: log progress html field modification

--- migrations/web_editor/saas~13.2.1.0/snippets.py
+++ migrations/web_editor/saas~13.2.1.0/snippets.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import logging
 from lxml import etree, html
+from odoo.upgrade import util
 
+_logger = logging.getLogger("odoo.upgrade.web_editor.132." + __name__)
 utf8_parser = html.HTMLParser(encoding="utf-8")
 
 
@@ -22,8 +25,12 @@ def add_snippet_names(cr, table, column, snippets, select_query):
     :param list snippets: list of all snippets to migrate
     :param str select_query: a query that when executed will return (id, list of snippets contained in the arch, arch)
     """
+    _logger.info("Add snippet names on %s.%s", table, column)
     cr.execute(select_query)
-    for res_id, regex_matches, arch in cr.fetchall():
+
+    it = util.log_progress(cr.fetchall(), qualifier="rows", logger=_logger, size=cr.rowcount, log_hundred_percent=True)
+
+    for res_id, regex_matches, arch in it:
         regex_matches = [match[0] for match in regex_matches]
         body = html.fromstring(arch, parser=utf8_parser)
         for snippet in snippets:
