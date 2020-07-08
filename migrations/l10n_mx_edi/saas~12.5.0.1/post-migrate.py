dc400f31917092a0fc38c14723e1c4d01890b4b6
Christophe Simonis <chs@odoo.com>
2020-07-08 16:44:41 +0000

    [FIX] l10n_mx_edi@saas~12.5: force update of record rule
    Fixes #1324
    
    closes odoo/upgrade#1465
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/l10n_mx_edi/saas~12.5.0.1/post-migrate.py
+++ migrations/l10n_mx_edi/saas~12.5.0.1/post-migrate.py
@@ -3,4 +3,5 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
+    util.update_record_from_xml(cr, "l10n_mx_edi.sat_digital_certificate")
     util.recompute_fields(cr, "account.move", ["l10n_mx_edi_external_trade"])
