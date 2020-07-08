f65895b0cf4d8441f383755b93dcaa1f51e7468d
Jigar Vaghela <jva@odoo.com>
2020-07-08 13:02:33 +0000

    [IMP] l10n_th@saas-12.5: migrate new tax report
    New tax report introduces from this commit https://github.com/odoo/odoo/commit/914a722f814f5d922a930f2e9554497825a0ad90
    
    closes odoo/upgrade#1469
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

---
+++ migrations/l10n_th/saas~12.5.2.0/post-migrate.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    # Old tax report line xml ids and new tax report line xml ids
+    tax_report_ref_map = {
+        'tax_report_ta_vat_in': 'tax_report_input_tax_purchase_from_out_tax',
+        'tax_report_tp_vat_in': 'tax_report_input_tax',
+        'tax_report_ta_vat_out': 'tax_report_out_tax_sale',
+        'tax_report_tp_vat_out': 'tax_report_out_tax',
+        }
+    if util.table_exists(cr, "l10n_th_new_tax_report_tag_map"):
+        for old_ref, new_ref in tax_report_ref_map.items():
+            # Update plus tags related to new tax report line
+            cr.execute("""
+                UPDATE l10n_th_new_tax_report_tag_map trm
+                    SET new_plus_tag_id = tag.id,
+                        new_tax_report_line_xml_ref = ir_md.name,
+                        new_tax_report_line_id = atrl_tag.account_tax_report_line_id
+                FROM account_tax_report_line_tags_rel atrl_tag
+                JOIN ir_model_data ir_md ON res_id = atrl_tag.account_tax_report_line_id and model = 'account.tax.report.line'
+                JOIN account_account_tag tag ON tag.id = atrl_tag.account_account_tag_id
+                WHERE tag.tax_negate != true AND trm.old_tax_report_line_xml_ref = %s AND ir_md.name = %s
+            """, (old_ref, new_ref)
+            )
+
+            # Update minus tags related to new tax report line
+            cr.execute("""
+                UPDATE l10n_th_new_tax_report_tag_map trm
+                    SET new_minus_tag_id = tag.id,
+                        new_tax_report_line_xml_ref = ir_md.name,
+                        new_tax_report_line_id = atrl_tag.account_tax_report_line_id
+                FROM account_tax_report_line_tags_rel atrl_tag
+                JOIN ir_model_data ir_md ON res_id = atrl_tag.account_tax_report_line_id and model = 'account.tax.report.line'
+                JOIN account_account_tag tag ON tag.id = atrl_tag.account_account_tag_id
+                WHERE tag.tax_negate != true AND trm.old_tax_report_line_xml_ref = %s AND ir_md.name = %s
+            """, (old_ref, new_ref)
+            )
+
+        # Update tag_ids in account.move.line
+        cr.execute("""
+            UPDATE account_account_tag_account_move_line_rel aml_tag
+                SET account_account_tag_id = CASE
+                  WHEN aml_tag.account_account_tag_id = tag_map.old_plus_tag_id THEN tag_map.new_plus_tag_id
+                  ELSE tag_map.new_minus_tag_id END
+                FROM l10n_th_new_tax_report_tag_map tag_map
+                WHERE (aml_tag.account_account_tag_id = tag_map.old_plus_tag_id AND tag_map.new_plus_tag_id IS NOT NULL)
+                  OR (aml_tag.account_account_tag_id = tag_map.old_minus_tag_id AND tag_map.new_minus_tag_id IS NOT NULL)
+        """
+        )
+
+        # Update tag_ids in account.tax.repartition.line
+        cr.execute("""
+            UPDATE account_account_tag_account_tax_repartition_line_rel atrl_tag
+                SET account_account_tag_id = CASE
+                  WHEN atrl_tag.account_account_tag_id = tag_map.old_plus_tag_id THEN tag_map.new_plus_tag_id
+                  ELSE tag_map.new_minus_tag_id END
+                FROM l10n_th_new_tax_report_tag_map tag_map
+                WHERE (atrl_tag.account_account_tag_id = tag_map.old_plus_tag_id AND tag_map.new_plus_tag_id IS NOT NULL)
+                  OR (atrl_tag.account_account_tag_id = tag_map.old_minus_tag_id AND tag_map.new_minus_tag_id IS NOT NULL)
+        """
+        )
+
+        # Remove unused tags
+        cr.execute("""
+            DELETE FROM account_account_tag aat USING l10n_th_new_tax_report_tag_map tag_map
+            WHERE (aat.id = tag_map.old_plus_tag_id AND tag_map.new_plus_tag_id IS NOT NULL)
+              OR (aat.id = tag_map.old_minus_tag_id AND tag_map.new_minus_tag_id IS NOT NULL)
+        """
+        )
+        # Remove temporary map table
+        cr.execute("DROP TABLE l10n_th_new_tax_report_tag_map")
--- migrations/l10n_th/saas~12.5.2.0/pre-migrate.py
+++ migrations/l10n_th/saas~12.5.2.0/pre-migrate.py
@@ -3,6 +3,53 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
+    # Create temporary map table for tax report tag
+    cr.execute(
+        """
+        CREATE TABLE  l10n_th_new_tax_report_tag_map(
+            old_tax_report_line_xml_ref VARCHAR,
+            old_tax_report_line_id INTEGER,
+            old_plus_tag_id INTEGER,
+            old_minus_tag_id INTEGER,
+            new_tax_report_line_xml_ref VARCHAR,
+            new_tax_report_line_id INTEGER,
+            new_plus_tag_id INTEGER,
+            new_minus_tag_id INTEGER
+            );
+    """
+    )
+    # Add Tax report lines in temporary map table
+    cr.execute(
+        """
+        INSERT INTO l10n_th_new_tax_report_tag_map(old_tax_report_line_xml_ref, old_tax_report_line_id)
+            SELECT ir_md.name, atrl.id
+            FROM account_tax_report_line atrl
+            JOIN ir_model_data ir_md ON res_id = atrl.id and model = 'account.tax.report.line'
+            WHERE atrl.tag_name IS NOT NULL
+    """
+    )
+
+    # Update plus tags related to tax report line
+    cr.execute(
+        """
+        UPDATE l10n_th_new_tax_report_tag_map trm
+            SET old_plus_tag_id = tag.id
+        FROM account_tax_report_line_tags_rel atrl_tag
+        JOIN account_account_tag tag ON tag.id = atrl_tag.account_account_tag_id
+        WHERE atrl_tag.account_tax_report_line_id = trm.old_tax_report_line_id and tag.tax_negate != true
+    """
+    )
+
+    # Update minus tags related to tax report line
+    cr.execute("""
+        UPDATE l10n_th_new_tax_report_tag_map trm
+            SET old_minus_tag_id = tag.id
+        FROM account_tax_report_line_tags_rel atrl_tag
+        JOIN account_account_tag tag ON tag.id = atrl_tag.account_account_tag_id
+        WHERE atrl_tag.account_tax_report_line_id = trm.old_tax_report_line_id and tag.tax_negate = true
+    """
+    )
+
     cr.execute(
         """
         SELECT name
