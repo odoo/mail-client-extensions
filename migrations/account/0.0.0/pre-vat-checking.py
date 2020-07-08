8292bb9e8efc2f9ce6b1c962e9b0c12019eb49b9
dmonzonis <mod@odoo.com>
2020-07-08 14:56:02 +0000

    [FIX] base@13.0: avoid crash on invalid VAT
    Some databases contain invalid VATs (according to the check_vat method
    introduced in the `base_vat` module), which causes an upgrade failure if
    a the check is triggered during upgrade (this happens, for example, with
    the installation of the `l10n_latam_base` module, which triggers it
    within its install hook).
    
    To avoid this, the definition of the check_vat method is changed during
    the upgrade. But if this is done within the upgrade script for the
    `base_vat` module, it will not be executed if this module is also
    installed during the upgrade, which is the case in some of the databases
    having this problem. Therefore, the definition of the change_vat model
    is done in the upgrade script of `account`. If `base_vat` is installed,
    the method will be overridden, and if it is not, the unneeded change_vat
    method will be defined during upgrade, but it will be harmless.
    
    Request ID: 45304
    
    closes odoo/upgrade#1462
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

---
+++ migrations/account/0.0.0/pre-vat-checking.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+from odoo.addons.base_vat.models import res_partner  # noqa
+
+
+def migrate(cr, version):
+    pass
+
+
+# A method from the base_vat module is overridden here because it needs this definition during
+# upgrade to avoid crashes, even if the base_vat module is installed during the upgrade process
+# If the base_vat module is not installed, defining this method during the upgrade is harmless
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+    _module = "base_vat"
+
+    # Completely skip VAT check during upgrade
+    def check_vat(self):
+        pass
