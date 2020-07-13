3cfbca96542f8b2eb9352ebc1dee6fe4c948c8fd
Joseph Caburnay <jcb@odoo.com>
2020-07-13 08:02:59 +0000

    [IMP] point_of_sale@saas-13.5: allow users to manage orders in the pos ui
    We now allow invoicing of finalized orders in an open session in this first
    iteration of order management in the pos frontend. Additionally, receipt
    reprinting is also introduced. With this feature, we can now reprint the
    receipt of old orders.
    
    closes odoo/upgrade#1472
    
    Related: https://github.com/odoo/odoo/pull/51141
    Related: https://github.com/odoo/enterprise/pull/11690
    Related: odoo/odoo#51141
    Related: odoo/enterprise#11690
    Task-id: 1981354
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~13.5.1.3/pre-modules.py
+++ migrations/base/saas~13.5.1.3/pre-modules.py
@@ -4,5 +4,7 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.remove_module(cr, "pos_reprint")
+
     if util.has_enterprise():
         util.merge_module(cr, "iot_pairing", "iot")
---
+++ migrations/point_of_sale/saas~13.5.1.0.1/pre-migrate.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "pos_payment", "is_change", "boolean", default=False)
+    util.create_column(cr, "pos_payment", "ticket", "varchar")
+
+    # Change payments are the negative cash payments in orders with positive amount.
+    # We set the is_change to TRUE for these payments.
+    cr.execute(
+        """
+        WITH change_payments AS (
+            SELECT payment.id as id
+              FROM pos_payment payment
+              JOIN pos_payment_method method ON payment.payment_method_id = method.id
+              JOIN pos_order _order ON payment.pos_order_id = _order.id
+             WHERE method.is_cash_count = TRUE AND _order.amount_total > 0 AND payment.amount < 0
+        )
+        UPDATE pos_payment
+           SET is_change = TRUE
+          FROM change_payments
+         WHERE change_payments.id = pos_payment.id
+    """
+    )
+
+    util.rename_field(cr, "pos.config", "module_pos_reprint", "manage_orders")
