d3d4835022a65f0d481a434683636e6e0f03b8a3
Denis Ledoux <dle@odoo.com>
2020-07-14 09:59:36 +0000

    [FIX] base@0.0.0: do not raise for deleting of mixin fields
    There are a bunch of databases for which the upgrade fails
    because of fields coming from mixins being deleted
    which no longer exists in the database.
    
    For instance, if you install `mass_mailing`,
    it adds fields on `mail.compose.message`:
    https://github.com/odoo/odoo/blob/c87db90d5dbab4d02a9378a1fc905c9b2935cbe2/addons/mass_mailing/wizard/mail_compose_message.py#L11-L14
    `account.invoice.send` inherits from `mail.compose.message`:
    https://github.com/odoo/odoo/blob/c87db90d5dbab4d02a9378a1fc905c9b2935cbe2/addons/account/wizard/account_invoice_send.py#L12
    
    and therefore the fields `mass_mailing_id`, `campaign_id`, `mass_mailing_name`, `mailing_list_ids` are added to the model `account.invoice.send`
    once `mass_mailing` installed, but because of a bug in the ORM, the fields are marked as belonging to the module `account`.
    
    If you then uninstall `mass_mailing`, the `ir.model.fields` records remains, as it was `mass_mailing` which got uninstalled,
    not `account`.
    
    Then when the upgrade happens, the upgrade detects the fields should no longer be there (and this is correct)
    and therefore the ORM decides to remove it.
    
    The same thing happens with `alias_contact` of `crm.team` when `crm` gets uninstalled.
    `crm.team` inherits of the `mail.alias` mixin:
    ```python
    _inherit = ['mail.alias.mixin', 'crm.team']
    ```
    There it's even weirder: the field is sometimes marked as belonging to the `hr` module:
    ```sql
    select * from ir_model_data where model = 'ir.model.fields' and res_id = 2707;
      id   | create_uid | create_date | write_date | write_uid | noupdate |             name              |        date_init         |       date_update        | module |      model      | res_id
    -------+------------+-------------+------------+-----------+----------+-------------------------------+--------------------------+--------------------------+--------+-----------------+--------
     24657 |            |             |            |           | f        | field_crm_team__alias_contact | 2019-07-23 01:24:57.3966 | 2019-07-23 01:24:57.3966 | hr     | ir.model.fields |   2707
    (1 row)
    ```
    
    This may comes from the fact `hr` overrides the selection of `alias_contact`:
    https://github.com/odoo/odoo/blob/09dbbd77d83332c48f62b1c36c3b33ddbfa3a4eb/addons/hr/models/mail_alias.py#L10
    And suddenly the ORM says this field comes from the module `hr`.
    
    Request 49747
    
    `It looks like you forgot to call `util.remove_field` on the following fields: account.invoice.send.mailing_list_ids`
    
    closes odoo/upgrade#1474
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/base/0.0.0/pre-models-ir_model.py
+++ migrations/base/0.0.0/pre-models-ir_model.py
@@ -31,32 +31,52 @@ class Field(models.Model):
 
     def unlink(self):
         unlink_fields = self.env["ir.model.fields"]
+        ignore_fields = self.env["ir.model.fields"]
         for field in self:
             if field.model in self.env:
                 model = self.env[field.model]
                 f = model._fields.get(field.name)
-                if f:
-                    if f.inherited:
-                        # See https://github.com/odoo/odoo/pull/53632
-                        util._logger.critical(
-                            "The field %s.%s is deleted but is still in the registry. It may come from a delegated field.",
-                            field.model,
-                            field.name,
-                        )
-                        continue
-                    elif any("mixin" in m and f.name in self.env[m]._fields for m in model._inherit):
-                        # See https://github.com/odoo/odoo/issues/49354
-                        util._logger.critical(
-                            "The field %s.%s is deleted but is still in the registry. It comes from a mixin model.",
-                            field.model,
-                            field.name,
-                        )
-                        continue
+                if f and f.inherited:
+                    # See https://github.com/odoo/odoo/pull/53632
+                    util._logger.critical(
+                        "The field %s.%s is deleted but is still in the registry. It may come from a delegated field.",
+                        field.model,
+                        field.name,
+                    )
+                    continue
+                elif f and any("mixin" in m and f.name in self.env[m]._fields for m in model._inherit):
+                    # See https://github.com/odoo/odoo/issues/49354
+                    util._logger.critical(
+                        "The field %s.%s is deleted but is still in the registry. It comes from a mixin model.",
+                        field.model,
+                        field.name,
+                    )
+                    continue
+                elif not f and field.related:
+                    model = field.model
+                    for name in field.related.split("."):
+                        r = self.env["ir.model.fields"].search([("model", "=", model), ("name", "=", name)])
+                        if not r:
+                            # See https://github.com/odoo/odoo/issues/49354
+                            util._logger.critical(
+                                """
+                                    The field %s.%s is deleted during the upgrade.
+                                    Since this is a related field for which the related doesn't exist,
+                                    this might be due to the bug of the mixins
+                                    not marking the fields on their rightful module"
+                                """,
+                                field.model,
+                                field.name,
+                            )
+                            ignore_fields |= field
+                            break
+                        model = r.relation
+
             unlink_fields |= field
-        if unlink_fields:
-            fields = ["%s.%s" % (f.model, f.name) for f in unlink_fields]
+        invalid_unlink_fields = unlink_fields - ignore_fields
+        if invalid_unlink_fields:
+            fields = ["%s.%s" % (f.model, f.name) for f in invalid_unlink_fields]
             raise util.MigrationError(
-                "💥 It looks like you forgot to call `util.remove_field` on the following fields: %s"
-                % ", ".join(fields)
+                "💥 It looks like you forgot to call `util.remove_field` on the following fields: %s" % ", ".join(fields)
             )
         return super(Field, unlink_fields).unlink()
