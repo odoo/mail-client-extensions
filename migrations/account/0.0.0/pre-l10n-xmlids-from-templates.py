c1dcbc5fc26baaf305d3d6da8c9e864000ec56d9
Denis Ledoux <dle@odoo.com>
2020-07-16 20:41:16 +0000

    [FIX] account@0.0.0: mark noupdate all `account.account.type`
    These kind of data should never be deleted
    except maybe when not used,
    but the standard account types are already set to noupdate,
    its only if non-standard types are added that it could be not set to noupdate.
    
    Request 47189
    
    closes odoo/upgrade#1490
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/account/0.0.0/pre-l10n-xmlids-from-templates.py
+++ migrations/account/0.0.0/pre-l10n-xmlids-from-templates.py
@@ -7,7 +7,8 @@ def migrate(cr, version):
     # they are all supposed to be generated from templates. We can therefore assume all accounts, taxes, ...
     # can be automatically set to noupdate True, in case the user somehow reset them to noupdate False.
     # Otherwise, they get deleted at the end of the upgrade, and if they are referenced, it raises.
-    cr.execute("""
+    cr.execute(
+        """
         UPDATE ir_model_data
            SET noupdate = 't'
          WHERE module like 'l10n_%'
@@ -18,4 +19,14 @@ def migrate(cr, version):
                 'account.fiscal.position.account',
                 'account.fiscal.position.tax'
              )
-    """)
+    """
+    )
+    cr.execute(
+        """
+        UPDATE ir_model_data
+           SET noupdate = 't'
+         WHERE model in (
+                 'account.account.type'
+               )
+      """
+    )
