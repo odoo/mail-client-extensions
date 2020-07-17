72c7bfb3683efa821a9b910e5d3a06f9a47ec554
Denis Ledoux <dle@odoo.com>
2020-07-17 13:37:14 +0000

    [FIX] l10n_es_reports: typo in c2421dc2a21b5c86e95b6fdf479c5b8ec9015374
    ¯\_(ツ)_/¯
    
    closes odoo/upgrade#1500
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/l10n_es_reports/saas~12.3.0.1/pre-10-financial-reports.py
+++ migrations/l10n_es_reports/saas~12.3.0.1/pre-10-financial-reports.py
@@ -5,8 +5,8 @@ def migrate(cr, version):
     # Records with XML ids mod_347_operations_real_estates_bought and mod_347_operations_regular_sold
     # were swapped in 12.0
     for xmlid, codes in [
-        ("mod_347_operations_regular_sold", ("_mod_347_temp", "aeat_mod_347_operations_regular_bought")),
-        ("mod_347_operations_regular_bought", ("aeat_mod_347_operations_regular_sold",)),
+        ("mod_347_operations_regular_bought", ("_mod_347_temp", "aeat_mod_347_operations_regular_bought")),
+        ("mod_347_operations_regular_sold", ("aeat_mod_347_operations_regular_sold",)),
     ]:
         cr.execute(
             """
