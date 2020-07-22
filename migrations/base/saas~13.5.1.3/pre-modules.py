c03732287628ff94923486ebd1c20d356ef191f4
Jeremy Kersten <jke@odoo.com>
2020-07-22 09:25:06 +0000

    [FIX] base@saas~12.5: add zip_req and state_req field on res.country
    pr-54746
    
    closes odoo/upgrade#1509
    
    Related: odoo/odoo#54746
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/base/saas~13.5.1.3/pre-modules.py
+++ migrations/base/saas~13.5.1.3/pre-modules.py
@@ -8,3 +8,6 @@ def migrate(cr, version):
 
     if util.has_enterprise():
         util.merge_module(cr, "iot_pairing", "iot")
+
+    util.create_column(cr, 'res_country', 'zip_required', 'boolean', default=True)
+    util.create_column(cr, 'res_country', 'state_required', 'boolean', default=False)
