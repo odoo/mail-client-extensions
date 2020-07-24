d3bc8b9f69774441a50ad709df33bd33217ad958
Christophe Simonis <chs@odoo.com>
2020-07-24 17:59:58 +0200

    [FIX] crm@saas~13.4

--- migrations/crm/saas~13.4.1.1/pre-migrate.py
+++ migrations/crm/saas~13.4.1.1/pre-migrate.py
@@ -9,3 +9,31 @@ def migrate(cr, version):
     util.update_record_from_xml(cr, "crm.digest_tip_crm_0")
     util.update_record_from_xml(cr, "crm.digest_tip_crm_1")
     util.update_record_from_xml(cr, "crm.digest_tip_crm_2")
+
+    # copy opportunity_id on recurrent events
+    cr.execute(
+        """
+        WITH events AS (
+            SELECT e.id, b.opportunity_id
+              FROM calendar_event e
+              JOIN calendar_recurrence r ON r.id = e.recurrence_id
+              JOIN calendar_event b ON b.id = r.base_event_id
+             WHERE b.opportunity_id IS NOT NULL
+               AND e.opportunity_id IS NULL
+        )
+        UPDATE calendar_event e
+           SET opportunity_id = c.opportunity_id
+          FROM events c
+         WHERE c.id = c.id
+    """
+    )
+
+    records = util.splitlines(
+        """
+        relate_partner_opportunities_kanban
+        relate_partner_opportunities_tree
+        relate_partner_opportunities
+    """
+    )
+    for record in records:
+        util.remove_record(cr, f"crm.{record}")
