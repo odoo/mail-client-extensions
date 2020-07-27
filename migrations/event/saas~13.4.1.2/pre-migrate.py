fe21277e60ea1d120ed8a34bbe88411faeab8400
Christophe Simonis <chs@odoo.com>
2020-07-27 10:23:09 +0200

    [FIX] event@saas~13.4

--- migrations/event/saas~13.4.1.2/pre-migrate.py
+++ migrations/event/saas~13.4.1.2/pre-migrate.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.remove_field(cr, "event.event", "color")
+    util.delete_unused(cr, "event.event_type_data_online")
