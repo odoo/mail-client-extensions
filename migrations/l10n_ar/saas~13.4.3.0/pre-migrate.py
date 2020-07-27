70180df91670bb4fd8e2c76b95ecd5755326785d
Christophe Simonis <chs@odoo.com>
2020-07-27 15:24:46 +0200

    [FIX] l10n_ar@{saas~13.3,saas~13.4}

--- migrations/l10n_ar/saas~13.4.3.0/pre-migrate.py
+++ migrations/l10n_ar/saas~13.4.3.0/pre-migrate.py
@@ -5,7 +5,3 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "account.journal", "l10n_ar_sequence_ids")
     util.remove_field(cr, "ir.sequence", "l10n_ar_letter")
-
-    util.remove_view(cr, "l10n_ar.sequence_view")
-    util.remove_view(cr, "l10n_ar.sequence_view_tree")
-    util.remove_view(cr, "l10n_ar.view_sequence_search")
