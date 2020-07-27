849a40bdd9fdb575ab8c9c93da517a6de24e1b03
Christophe Simonis <chs@odoo.com>
2020-07-27 11:57:07 +0200

    [FIX] hr@saas~13.4

--- migrations/hr/saas~13.4.1.1/pre-migrate.py
+++ migrations/hr/saas~13.4.1.1/pre-migrate.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.remove_field(cr, 'res.config.settings', 'module_hr_org_chart')
+    util.remove_field(cr, "res.config.settings", "module_hr_org_chart")
+    util.rename_xmlid(cr, "hr.open_module_tree_department", "hr.hr_department_kanban_action")
