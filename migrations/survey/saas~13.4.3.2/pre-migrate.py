c1cd425d3e721795614040d50b100b0bb18ae688
Christophe Simonis <chs@odoo.com>
2020-07-28 18:53:34 +0200

    [FIX] survey@saas~13.4

--- migrations/survey/saas~13.4.3.2/pre-migrate.py
+++ migrations/survey/saas~13.4.3.2/pre-migrate.py
@@ -3,8 +3,10 @@
 
 def migrate(cr, version):
     # prepare new constraint
-    cr.execute("""
-        UPDATE survey_survey
-           SET scoring_success_min = 0
-         WHERE COALESCE(scoring_success_min, 0) NOT BETWEEN 0 AND 100
-    """)
+    cr.execute(
+        """
+            UPDATE survey_survey
+               SET scoring_success_min = GREATEST(0, LEAST(100, COALESCE(scoring_success_min, 0)))
+             WHERE COALESCE(scoring_success_min, -1) NOT BETWEEN 0 AND 100
+        """
+    )
