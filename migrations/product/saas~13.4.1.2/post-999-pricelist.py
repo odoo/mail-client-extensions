cbc4e87c039f00a85d5bf61743725706d63a081c
Christophe Simonis <chs@odoo.com>
2020-07-28 14:52:54 +0200

    [FIX] product@saas~13.4

--- migrations/product/saas~13.4.1.2/post-999-pricelist.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def migrate(cr, version):
-    cr.execute("""UPDATE product_pricelist_item
-                     SET date_end = date_end + interval '23 hours 59 minutes 59 seconds'
-                   WHERE date_end is not null
-               """)
