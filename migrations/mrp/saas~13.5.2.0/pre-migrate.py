bb64b9fe65817ce36accbc7c5c4ff5324efe5184
William Henrotin <whe@odoo.com>
2020-07-30 13:11:25 +0000

    [IMP] mrp@saas~13.5: remove chatter on workorder
    closes odoo/upgrade#1588
    
    Related: odoo/enterprise#12332
    Related: odoo/odoo#54366
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/mrp/saas~13.5.2.0/pre-migrate.py
+++ migrations/mrp/saas~13.5.2.0/pre-migrate.py
@@ -4,3 +4,6 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "mrp.mrp_production_workorder_form_view_inherit_gantt")
+    # remove mail.thread and mail.activity.mixin inheritance
+    util.remove_inherit_from_model(cr, 'mrp.workorder', 'mail.thread')
+    util.remove_inherit_from_model(cr, 'mrp.workorder', 'mail.activity.mixin')
