1117d0e5190fdcc6e0362d04469ee4524b2a10de
Nicolas (vin) <vin@odoo.com>
2020-08-03 13:23:18 +0000

    [IMP] account@13.5: Misc. improvements
    Pr : odoo/odoo/pull/54494
    Task : 2250349
    
    closes odoo/upgrade#1546
    
    Related: odoo/odoo#54494
    Related: odoo/enterprise#11862
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/account/13.5.1.1/pre-migrate.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+
+    # ===========================================================
+    # Misc-improvements (PR: 54494 & 11862)
+    # ===========================================================
+
+    # Res_company
+    util.create_column(cr, "res_company", "expense_currency_exchange_account_id", "int4")
+    util.create_column(cr, "res_company", "income_currency_exchange_account_id", "int4")
+
+    cr.execute(
+        """
+                UPDATE res_company
+                SET
+                   expense_currency_exchange_account_id = journal.default_debit_account_id,
+                   income_currency_exchange_account_id = journal.default_credit_account_id
+                FROM account_journal journal
+                WHERE journal.id = res_company.currency_exchange_journal_id
+            """
+    )
+
+    # Account_payment
+    util.create_column(cr, "account_payment_method", "sequence", "int4")
+
+    cr.execute(
+        """
+                UPDATE account_payment_method
+                SET
+                   sequence = id
+            """
+    )
+
+    # Account_journal
+    util.create_column(cr, "account_journal", "default_account_id", "int4")
+
+    cr.execute(
+        """
+                UPDATE account_journal
+                SET
+                   default_account_id = COALESCE(default_credit_account_id,default_debit_account_id)
+            """
+    )
+
+    util.remove_field(cr, "account.journal", "default_debit_account_id")
+    util.remove_field(cr, "account.journal", "default_credit_account_id")
+
+    # Setup_wizard
+    util.remove_field(cr, "account.setup.bank.manual.config", "new_journal_code")
+    util.remove_field(cr, "account.setup.bank.manual.config", "related_acc_type")
