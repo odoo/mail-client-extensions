35893ab25b35f81f848ee7bbf70b4143dd4ce423
Christophe Simonis <chs@odoo.com>
2020-08-05 14:21:38 +0200

    [FIX] helpdesk_sale_timesheet@saas~13.4

--- migrations/helpdesk_sale_timesheet/saas~13.4.1.0/pre-migrate.py
+++ migrations/helpdesk_sale_timesheet/saas~13.4.1.0/pre-migrate.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
@@ -7,3 +6,6 @@ def migrate(cr, version):
     util.remove_field(cr, "helpdesk.ticket", "invoice_status")
     util.remove_field(cr, "helpdesk.ticket", "display_create_invoice_primary")
     util.remove_field(cr, "helpdesk.ticket", "display_create_invoice_secondary")
+
+    util.remove_view(cr, "helpdesk_sale_timesheet.helpdesk_tickets_view_search_inherit_helpdesk_sale_timesheet")
+    util.remove_record(cr, "helpdesk_sale_timesheet.project_task_server_action_batch_invoice_helpdesk")
