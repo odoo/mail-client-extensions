6ef0cc494fe3a4a4fae3164abc5347b8b8982e08
Christophe Simonis <chs@odoo.com>
2020-08-05 15:02:53 +0000

    [FIX] website_event_track@saas~13.3
    Renamed xmlids after odoo/odoo#53944
    
    closes odoo/upgrade#1559
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website_event_track/saas~13.3.1.0/pre-migrate.py
+++ migrations/website_event_track/saas~13.3.1.0/pre-migrate.py
@@ -10,3 +10,11 @@ def migrate(cr, version):
     util.create_column(cr, "event_sponsor", "active", "boolean", default=True)
 
     util.fixup_m2m(cr, "event_track_event_track_tag_rel", "event_track", "event_track_tag")
+
+    # Data (changed by odoo/odoo#53944)
+    eb = util.expand_braces
+    # Don't change label of existing tags (demo data)
+    util.rename_xmlid(cr, *eb("website_event_track.event_track_tag{1,_technical}"), noupdate=True)
+    util.rename_xmlid(cr, *eb("website_event_track.event_track_tag{2,_business}"), noupdate=True)
+    util.rename_xmlid(cr, *eb("website_event_track.event_track_tag{3,11}"), noupdate=True)
+    util.rename_xmlid(cr, *eb("website_event_track.event_track_tag{4,12}"), noupdate=True)
