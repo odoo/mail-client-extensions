027c8ad591ce0e765edbd15f16f580899157c4b9
Christophe Simonis <chs@odoo.com>
2020-08-06 16:57:47 +0200

    [FIX] l10n_be_hr_contract_salary@saas~13.4

--- migrations/l10n_be_hr_contract_salary/saas~13.4.1.0/pre-migrate.py
+++ migrations/l10n_be_hr_contract_salary/saas~13.4.1.0/pre-migrate.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
@@ -7,3 +6,7 @@ def migrate(cr, version):
     util.remove_field(cr, "generate.simulation.link", "car_id")
     util.remove_field(cr, "generate.simulation.link", "new_car")
     util.remove_field(cr, "generate.simulation.link", "customer_relation")
+
+    util.delete_unused(cr, "l10n_be_hr_contract_salary.l10n_be_representation_fees_value_1")
+    util.remove_view(cr, "l10n_be_hr_contract_salary.assets_frontend")
+    util.remove_view(cr, "l10n_be_hr_contract_salary.salary_package")
