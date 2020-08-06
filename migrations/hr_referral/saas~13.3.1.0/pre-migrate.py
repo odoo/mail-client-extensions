e93646435974f0d089f0a74ad41dadf0028f6935
Christophe Simonis <chs@odoo.com>
2020-08-06 11:31:45 +0000

    [FIX] hr_referral@saas~13.3: define script in correct directory
    It worked by accident hr_referral@saas~13.3: define script in correct
    directory
    
    It worked by accident..

--- migrations/hr_referral/saas~13.3.1.0/pre-migrate.py
+++ migrations/hr_referral/saas~13.3.1.0/pre-migrate.py
@@ -4,6 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "hr_applicant", "last_valuable_stage_id", "int4")
-    util.create_column(cr, "hr_recruitment_stage", "not_hired_stage", "boolean")
-
-    cr.execute("UPDATE hr_recruitment_stage SET not_hired_stage = FALSE")
+    util.create_column(cr, "hr_recruitment_stage", "not_hired_stage", "boolean", default=False)
