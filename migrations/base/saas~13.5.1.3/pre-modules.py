de478d9511906fda6943a4c4e8327a3b2bc1ec21
wan <wan@odoo.com>
2020-08-06 09:21:19 +0000

    [IMP] base@saas~13.5: update l10n module categories
    Task 2309613
    Community PR: https://github.com/odoo/odoo/pull/55384
    Enterprise PR: https://github.com/odoo/enterprise/pull/12179
    
    closes odoo/upgrade#1568
    
    Related: odoo/enterprise#12179
    Related: odoo/odoo#55384
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~13.5.1.3/pre-modules.py
+++ migrations/base/saas~13.5.1.3/pre-modules.py
@@ -4,6 +4,7 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    eb = util.expand_braces
     util.remove_module(cr, "pos_reprint")
 
     if util.has_enterprise():
@@ -13,3 +14,6 @@ def migrate(cr, version):
     util.create_column(cr, 'res_country', 'state_required', 'boolean', default=False)
     util.new_module(cr, "microsoft_account", deps={"base_setup"})
     util.new_module(cr, "microsoft_calendar", deps={"microsoft_account", "calendar"})
+
+    util.rename_xmlid(cr, *eb('base.module_category_{localization,accounting_localizations}_account_charts'))
+    util.remove_record(cr, 'base.module_category_localization')
