30aa5d8976dbb64202aae82efb7a1a154f764709
Rémy Baranx (bar) <bar@odoo.com>
2020-08-07 14:21:08 +0000

    [FIX] pos_restaurant_iot@saas~12.5: iotbox_id has been replaced by device_id
    In `13.0`, the `iotbox_id` field has been removed and a new `device_id` field
    has been added.
    
    The `device_id` field may be filled using existing printer devices linked to the
    same `iotbox` than the `restaurant_printer`.
    
    Upgrade request: 47452
    
    closes odoo/upgrade#1582
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/pos_restaurant_iot/saas~12.5.1.0/pre-migrate.py
+++ migrations/pos_restaurant_iot/saas~12.5.1.0/pre-migrate.py
@@ -3,4 +3,25 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    util.rename_field(cr, "restaurant.printer", "iotbox_id", "device_id")
+    util.create_column(cr, "restaurant_printer", "device_id", "int4")
+
+    # 1. `device_id` is added, and can be determined from the `iotbox_id`, which has been removed.
+    # A same iot box can have multiple printers. Here, the selection criteria is the connection and the id
+    # but the customer might want to select another device after the upgrade.
+    cr.execute(
+        """
+        UPDATE restaurant_printer
+           SET device_id = (
+                     SELECT id
+                       FROM iot_device
+                      WHERE iot_id = iotbox_id
+                        AND type = 'printer'
+                   ORDER BY connection, id -- order by connect because `direct` (USB) is better than `network`.
+                      LIMIT 1
+               )
+         WHERE iotbox_id IS NOT NULL
+        """
+    )
+
+    # 2. Remove the field and column `iotbox_id`
+    util.remove_field(cr, "restaurant.printer", "iotbox_id")
