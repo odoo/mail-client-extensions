f3216c3eb497324418a0c7452b353354a2b1cd31
Denis Ledoux <dle@odoo.com>
2020-08-11 13:24:11 +0000

    [FIX] sale@9.0: additional removed views
    These views have been removed in
    odoo/odoo@94716a3f14d929574c46ef165e4364d12a5903ed
    
    Request 49194
    M140416306132
    
    closes odoo/upgrade#1598
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/sale/9.0.1.0/pre-15-rm-views.py
+++ migrations/sale/9.0.1.0/pre-15-rm-views.py
@@ -6,3 +6,5 @@ def migrate(cr, version):
     util.remove_view(cr, 'sale.view_order_form')
     util.remove_view(cr, 'sale.view_sales_config')
     util.remove_view(cr, 'sale.account_invoice_tree')
+    util.remove_view(cr, 'sale.view_order_line_form2')
+    util.remove_view(cr, 'sale.view_sales_order_uninvoiced_line_filter')
