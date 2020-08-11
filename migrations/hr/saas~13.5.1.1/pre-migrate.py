24e396d1b6a2470602a60ebb3eb20a794f86adb1
Baptiste Vergote <bve@odoo.com>
2020-08-11 11:16:44 +0000

    [FIX] hr@saas~13.5:value to company_id required
    Adding the constraint required=True on company_id defined on the
    hr_employee.
    
    Avoid issue in case this data is missing and get the value from the
    resource_calendar of the same employee.
    
    closes odoo/odoo#53109
    
    closes odoo/upgrade#1596
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/hr/saas~13.5.1.1/pre-migrate.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+def migrate(cr, version):
+	"""
+		Take company_id on resource_calendar if exists
+		Fallback: Take the company_id of the hr_contrat based on the latest start_date linked to the employee_id
+	"""
+
+	cr.execute(
+		"""
+			UPDATE hr_employee e
+			   SET company_id = rc.company_id
+			  FROM resource_calendar rc
+			 WHERE e.company_id IS NULL  
+			   AND rc.id = e.resource_calendar_id;
+   		"""
+	)
+
+	cr.execute(
+		"""
+			WITH last_contracts AS (
+    			SELECT employee_id, (array_agg(company_id ORDER BY date_start desc))[1] AS company_id
+      			  FROM hr_contract
+  			  GROUP BY employee_id
+			)
+			UPDATE hr_employee e
+   			   SET company_id = c.company_id
+  			  FROM last_contracts c
+ 			 WHERE c.employee_id = e.id
+   			   AND e.company_id IS NULL
+		"""
+	)
