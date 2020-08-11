6a46aaaab432f870ebfd439453c88ea070fbe1c9
Denis Ledoux <dle@odoo.com>
2020-08-11 13:17:23 +0000

    [FIX] account_reports_followup: additional removed views
    During the move and the refactoring of the followup module
    in odoo/enterprise@8893960ef374e56942c1ed751e7cb36b39c2418c
    the view on the model `res.partner` have been deleted
    and have not been replaced.
    
    Request 49194
    M140416306132
    
    closes odoo/upgrade#1597
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/account_reports_followup/9.0.1.0/pre-10-views.py
+++ migrations/account_reports_followup/9.0.1.0/pre-10-views.py
@@ -3,4 +3,7 @@ from openerp.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
+    util.remove_view(cr, xml_id="account_reports_followup.customer_followup_tree")
+    util.remove_view(cr, xml_id="account_reports_followup.view_partner_inherit_customer_followup_tree")
+    util.remove_view(cr, xml_id="account_reports_followup.customer_followup_search_view")
     util.remove_view(cr, xml_id="account_reports_followup.view_partner_inherit_followup_form")
