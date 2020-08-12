3554623af2cd96539be1e46ea092d8bbd33dfdb5
Francois (fge) <fge@odoo.com>
2020-08-12 07:40:37 +0000

    [IMP] helpdesk@saas~13.5: Deleting a view and actions that is no longer used
    closes odoo/upgrade#1605
    
    Related: odoo/enterprise#12123
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/helpdesk/saas~13.5.1.3/pre-10-migrate.py
+++ migrations/helpdesk/saas~13.5.1.3/pre-10-migrate.py
@@ -15,3 +15,16 @@ def migrate(cr, version):
     )
 
     util.remove_field(cr, "helpdesk.sla", "exclude_stage_id")
+
+    util.remove_view(cr, 'helpdesk.helpdesk_ticket_view_kanban_no_create')
+
+    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_main')
+    util.remove_record(cr, 'helpdesk.action_upcoming_sla_fail_tickets')
+    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_high_priorities')
+    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_urgent')
+    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_sla_high')
+    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_sla_urgent')
+    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_Archived')
+    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_slafailed')
+    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_dashboard_high_priority')
+    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_dashboard_urgent')
