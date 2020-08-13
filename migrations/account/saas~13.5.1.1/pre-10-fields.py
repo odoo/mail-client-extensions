bd030fe0b8b9b3bd002ace061763f46f8de6f79f
oco-odoo <oco@odoo.com>
2020-08-13 17:57:24 +0000

    [IMP] account@saas-13.5: migrate new account_tax_fiscal_country_id field of res.company
    In stable, we added a config parameter to allow using a different country for the tax report of a company than the one defined in its address:
    https://github.com/odoo/odoo/pull/51973
    https://github.com/odoo/enterprise/pull/10792
    
    The following PRs change that in master (13.5) by replacing this config parameter by a field on res.company, used in settings:
    https://github.com/odoo/odoo/pull/55308
    https://github.com/odoo/enterprise/pull/12156
    
    closes odoo/upgrade#1624
    
    Related: odoo/odoo#55308
    Related: odoo/enterprise#12156
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/account/saas~13.5.1.1/pre-10-fields.py
+++ migrations/account/saas~13.5.1.1/pre-10-fields.py
@@ -45,3 +45,31 @@ def migrate(cr, version):
             )
         )
     ''')
+
+    # ===========================================================
+    # Fiscal country field on companies (PR 55308 & 12156)
+    # ===========================================================
+    util.create_column(cr, 'res_company', 'account_tax_fiscal_country_id', 'int4')
+
+    # If a fiscal country was set with the dedicated config parameter, use it; else use the company's country.
+    cr.execute("""
+        UPDATE res_company
+        SET account_tax_fiscal_country_id = COALESCE(parameter_country.id, company_partner.country_id)
+        FROM res_partner company_partner
+        JOIN res_company company on company.partner_id = company.partner_id
+        LEFT JOIN ir_config_parameter fiscal_country_param ON fiscal_country_param.key = CONCAT('account_fiscal_country_', company.id)
+        LEFT JOIN res_country parameter_country ON LOWER(parameter_country.code) = LOWER(fiscal_country_param.value)
+        WHERE res_company.id = company.id
+
+    """)
+
+    # Remove the fiscal country config parameters; now useless.
+    cr.execute("""
+        DELETE FROM ir_config_parameter
+        WHERE key LIKE 'account_fiscal_country_%'
+    """)
+
+    # Replace the old related fields which pointed towards company country for use in taxes
+    util.rename_field(cr, 'account.move.line', 'country_id', 'tax_fiscal_country_id')
+    util.rename_field(cr, 'account.tax', 'country_id', 'tax_fiscal_country_id')
+    util.rename_field(cr, 'account.tax.repartition.line', 'country_id', 'tax_fiscal_country_id')
