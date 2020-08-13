cd70bb9517d2d6a6669ba7c2341759dd7bde67d6
Arnaud Joset <arj@odoo.com>
2020-08-13 13:12:19 +0000

    [IMP] base@saas~13.5: remove module website_project
    Community PR: 55549
    
    taskid: 2299401

--- migrations/base/saas~13.5.1.3/pre-modules.py
+++ migrations/base/saas~13.5.1.3/pre-modules.py
@@ -20,6 +20,7 @@ def migrate(cr, version):
     util.remove_record(cr, 'base.module_category_operations')
 
     util.remove_module(cr, 'hr_expense_check')
+    util.remove_module(cr, 'website_project')
 
     util.module_deps_diff(cr, "l10n_ec", plus={"l10n_latam_invoice_document", "l10n_latam_base"})
 
