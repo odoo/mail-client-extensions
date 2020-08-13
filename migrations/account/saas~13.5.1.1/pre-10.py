2d6f366f13fbe66d76a47911083b073cb07c972a
wan <wan@odoo.com>
2020-08-13 13:08:41 +0000

    [IMP] account@saas13.5: misc improvements of reconcile models
    Task 2292368
    Bundle
    https://runbot.odoo.com/runbot/bundle/master-reconcile-model-boost-wan-60105
    
    closes odoo/upgrade#1619
    
    Related: odoo/odoo#55031
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/account/saas~13.5.1.1/pre-10.py
+++ migrations/account/saas~13.5.1.1/pre-10.py
@@ -11,3 +11,13 @@ def migrate(cr, version):
     util.remove_column(cr, 'account_move_line', 'account_internal_type')
     util.remove_record(cr, 'account.action_accrual_entry')
     util.remove_record(cr, 'account.action_move_transfer_accounts')
+
+    # ===========================================================
+    # Reconciliation model improvements (PR:55031)
+    # ===========================================================
+    util.create_column(cr, 'account_reconcile_model', 'active', 'boolean', default=True)
+    for table in ('account_reconcile_model', 'account_reconcile_model_template'):
+        util.create_column(cr, table, 'matching_order', 'varchar', default='old_first')
+        util.create_column(cr, table, 'match_text_location_label', 'boolean', default=True)
+        util.create_column(cr, table, 'match_text_location_note', 'boolean', default=False)
+        util.create_column(cr, table, 'match_text_location_reference', 'boolean', default=False)
