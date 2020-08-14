fd5613630c2e7adac74ed0133a12e8f7a82c1a22
Atul Patel <atp@openerp.com>
2020-08-14 12:13:40 +0000

    [FIX] quality@saas~12.3: update test_type_id field from quality point
    closes odoo/upgrade#1631
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/quality/saas~12.3.1.0/pre-migrate.py
+++ migrations/quality/saas~12.3.1.0/pre-migrate.py
@@ -4,3 +4,13 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.create_column(cr, "quality_alert", 'email_cc', 'varchar')
+    util.create_column(cr, "quality_check", 'test_type_id', 'integer')
+    cr.execute(
+        """
+        UPDATE quality_check qc
+           SET test_type_id = qp.test_type_id
+          FROM quality_point qp
+         WHERE qp.id = qc.point_id
+           AND qc.test_type_id IS NULL
+        """
+    )
