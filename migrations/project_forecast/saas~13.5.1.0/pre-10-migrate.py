0935286970cc95d5ea5bec20bfe2a2a0f69962d6
Mahendra Barad <mba@odoo.com>
2020-08-17 08:48:54 +0000

    [IMP] project_forecast@saas~13.5: delete unused action and menus
    Purpose of the commit is to remove the actions and menus from
    project_forecast.
    
    TaskID: 2318479
    Related Enterprise: https://github.com/odoo/enterprise/pull/12422
    
    closes odoo/upgrade#1642
    
    Closes: #1642
    Related: odoo/enterprise#12422
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/project_forecast/saas~13.5.1.0/pre-10-migrate.py
+++ migrations/project_forecast/saas~13.5.1.0/pre-10-migrate.py
@@ -11,4 +11,12 @@ def migrate(cr, version):
     util.remove_record(cr, "project_forecast.project_forecast_action_analysis_pivot")
     util.remove_record(cr, "project_forecast.project_forecast_action_analysis_graph")
     util.remove_record(cr, "project_forecast.project_forecast_action_analysis_gantt")
-    util.remove_menus(cr, [util.ref(cr, "project_forecast.project_forecast_report_activities")])
+    util.remove_menus(cr, [
+        util.ref(cr, "project_forecast.project_forecast_report_activities"),
+        util.ref(cr, "project_forecast.project_forecast_group_by_user"),
+        util.ref(cr, "project_forecast.project_forecast_group_by_project"),
+        util.ref(cr, "project_forecast.project_forecast_menu")])
+    util.remove_record(cr, "project_forecast.project_forecast_action_by_user")
+    util.remove_record(cr, "project_forecast.project_forecast_action_view_by_user_gantt")
+    util.remove_record(cr, "project_forecast.project_forecast_action_by_project")
+    util.remove_record(cr, "project_forecast.project_forecast_action_view_by_project_gantt")
