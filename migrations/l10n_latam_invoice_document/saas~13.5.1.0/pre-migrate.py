ba8ecb0e68af3b5f52e4105b92aaaee1d2834e3d
oco-odoo <oco@odoo.com>
2020-08-19 15:00:29 +0000

    [IMP] l10n_latam_invoice_document@saas-13.5: remove now useless l10n_latam_country_code field on journals
    This field has been replaced by a more generic country_code field, directly defined in account.
    
    closes odoo/upgrade#1666
    
    Related: odoo/odoo#56181
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/l10n_latam_invoice_document/saas~13.5.1.0/pre-migrate.py
+++ migrations/l10n_latam_invoice_document/saas~13.5.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view('l10n_latam_invoice_document.report_invoice_document')
+    util.remove_field(cr, 'account.journal', 'l10n_latam_country_code')
