3c547f89c248c5781efd9577ae1a98f8f03a8712
Denis Ledoux <dle@odoo.com>
2020-08-19 08:49:56 +0000

    [FIX] sms@saas~12.5: sanitize 400.000 phone numbers
    The database of the observed case has about 400.000 numbers.
    Fetching all the numbers in once (with `cr.fetchall`)
    leads to a `MemoryError`.
    
    Therefore, split the numbers to fetch in chunks
    Despite the heavy count of phone numbers to sanitize,
    it's quite quick, something like 5 minutes,
    so I don't think there is a need to output
    a progress status.
    
    Request 49898
    M1509293041532
    
    closes odoo/upgrade#1655
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/sms/saas~12.5.2.0/post-migrate.py
+++ migrations/sms/saas~12.5.2.0/post-migrate.py
@@ -10,49 +10,55 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
 
-    cr.execute(
-        """
-        WITH partner_country AS (
+    limit = 10 ** 5
+    there_is_moar = True
+    while there_is_moar:
+        cr.execute(
+            """
+            WITH partner_country AS (
+                SELECT p.id,
+                       COALESCE(c.id, comp_c.id, main_comp_c.id) as country_id
+
+                  FROM res_partner p
+             LEFT JOIN res_country c ON c.id = p.country_id
+
+             LEFT JOIN res_company comp ON comp.id = p.company_id
+                  JOIN res_partner comp_p ON comp_p.id = comp.partner_id
+             LEFT JOIN res_country comp_c ON comp_c.id = comp_p.country_id
+
+                  JOIN res_company main_comp ON main_comp.id = %s
+                  JOIN res_partner main_comp_p ON main_comp_p.id = main_comp.partner_id
+             LEFT JOIN res_country main_comp_c ON main_comp_c.id = main_comp_p.country_id
+
+            )
+
             SELECT p.id,
-                   COALESCE(c.id, comp_c.id, main_comp_c.id) as country_id
-
+                   trim(COALESCE(p.mobile, p.phone)) as phone,
+                   c.code,
+                   c.phone_code
               FROM res_partner p
-         LEFT JOIN res_country c ON c.id = p.country_id
-
-         LEFT JOIN res_company comp ON comp.id = p.company_id
-              JOIN res_partner comp_p ON comp_p.id = comp.partner_id
-         LEFT JOIN res_country comp_c ON comp_c.id = comp_p.country_id
-
-              JOIN res_company main_comp ON main_comp.id = %s
-              JOIN res_partner main_comp_p ON main_comp_p.id = main_comp.partner_id
-         LEFT JOIN res_country main_comp_c ON main_comp_c.id = main_comp_p.country_id
-
+              JOIN partner_country r ON r.id = p.id
+         LEFT JOIN res_country c ON c.id = r.country_id
+             WHERE p.phone_sanitized IS NULL
+               AND length(trim(COALESCE(p.mobile, p.phone))) > 3
+             LIMIT %s
+        """,
+            [util.ref(cr, "base.main_company"), limit],
         )
+        if cr.rowcount < limit:
+            there_is_moar = False
 
-        SELECT p.id,
-               trim(COALESCE(p.mobile, p.phone)) as phone,
-               c.code,
-               c.phone_code
-          FROM res_partner p
-          JOIN partner_country r ON r.id = p.id
-     LEFT JOIN res_country c ON c.id = r.country_id
-         WHERE p.phone_sanitized IS NULL
-           AND length(trim(COALESCE(p.mobile, p.phone))) > 3
-    """,
-        [util.ref(cr, "base.main_company")],
-    )
+        # NOTE
+        # `ProcessPoolExecutor.map` arguments needs to be pickleable
+        # Functions can only be pickle if they are importable.
+        # However, the current file is not importable due to the dash in the filename.
+        # We should then put the executed function in its own importable file.
+        name = f"_upgrade_{uuid.uuid4().hex}"
+        san = sys.modules[name] = util.import_script("sms/saas~12.5.2.0/sanitize.py", name=name)
 
-    # NOTE
-    # `ProcessPoolExecutor.map` arguments needs to be pickleable
-    # Functions can only be pickle if they are importable.
-    # However, the current file is not importable due to the dash in the filename.
-    # We should then put the executed function in its own importable file.
-    name = f"_upgrade_{uuid.uuid4().hex}"
-    san = sys.modules[name] = util.import_script("sms/saas~12.5.2.0/sanitize.py", name=name)
-
-    with ProcessPoolExecutor() as executor:
-        execute_batch(
-            cr._obj,
-            "UPDATE res_partner SET phone_sanitized = %s WHERE id = %s",
-            executor.map(san.sanitize, *zip(*cr.fetchall())),
-        )
+        with ProcessPoolExecutor() as executor:
+            execute_batch(
+                cr._obj,
+                "UPDATE res_partner SET phone_sanitized = %s WHERE id = %s",
+                executor.map(san.sanitize, *zip(*cr.fetchall())),
+            )
