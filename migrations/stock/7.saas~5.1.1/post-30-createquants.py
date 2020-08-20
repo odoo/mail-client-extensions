3679819af60fb71709e429cd138c2ee50a14ebe3
Denis Ledoux <dle@odoo.com>
2020-08-20 12:58:59 +0000

    [FIX] stock@7.saas~5: quants creation performance improvement
    Create the quants of `done` moves using plain SQL.
    Anticipate the fact the quants are merged in the migration to 11.0 in
    https://github.com/odoo/upgrade/blob/9b4447a20bd2d495519a3839ed2a0cbd56ebd316/migrations/stock/11.0.1.1/end-10-cleanmergequantsreservation.py#L104-L122
    
    This is still considered as beta-testing until the customer tests this is correct.
    
    From my side, I tested:
     - with a smaller database (lyncee, request 51826), that the quants in the database upgraded to 11.0 were identical
       using the former script and this SQL optimization
     - with the larger databases for which this revision is the subject (request 45239 & 48253)
       that the quantity on hand were identical before and after upgrade.
    
    Request 45239, 48253
    M1712216061367
    M110812259-couavksy
    
    closes odoo/upgrade#1672
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/stock/7.saas~5.1.1/post-30-createquants.py
+++ migrations/stock/7.saas~5.1.1/post-30-createquants.py
@@ -23,23 +23,55 @@ def migrate(cr, version):
     registry = RegistryManager.get(cr.dbname)
     move_obj = registry['stock.move']
     quant_obj = registry['stock.quant']
-    moves = move_obj.search(cr, SUPERUSER_ID, [('state', '=', 'done'), ('product_qty', '>', 0.0)], order='company_id, date')
-    for move in util.iter_browse(move_obj, cr, SUPERUSER_ID, moves, logger=_logger):
-        preferred_domain = []
-        if move.move_orig_ids:
-            preferred_domain = [[('history_ids', 'in', [x.id for x in move.move_orig_ids])], [('history_ids', 'not in', [x.id for x in move.move_orig_ids])]]
-        quants = quant_obj.quants_get_prefered_domain(cr, SUPERUSER_ID, move.location_id, move.product_id, move.product_qty, domain=[('qty', '>', 0.0)],
-                                                      prefered_domain_list=preferred_domain, restrict_lot_id=move.restrict_lot_id.id, context={'force_company': move.company_id.id})
-        quant_obj.quants_move(cr, SUPERUSER_ID, quants, move, move.location_dest_id, lot_id=move.restrict_lot_id.id)
+    if os.environ.get('ODOO_MIG_S5_SQL_QUANTS_CREATION'):
+        cr.execute(
+            """
+                INSERT INTO stock_quant(product_id, location_id, company_id, lot_id, qty, in_date)
+                     SELECT product_id, location_id, company_id, lot_id, SUM(qty), MIN(date) FROM (
 
-    #Take in_date as first date of stock_move
-    cr.execute("""
-    UPDATE stock_quant SET in_date = qdate.date
-    FROM
-    (SELECT sq.id as id, MIN(sm.date) as date FROM stock_quant sq, stock_quant_move_rel sqmr, stock_move sm
-    WHERE sq.id = sqmr.quant_id AND sm.id = sqmr.move_id AND sm.state = 'done' AND sm.date is not null GROUP BY sq.id) AS qdate
-    WHERE stock_quant.id = qdate.id
-    """)
+                          SELECT m.product_id, m.location_dest_id as location_id, m.company_id,
+                                 m.restrict_lot_id as lot_id, m.product_qty as qty, m.date as date
+                            FROM stock_move m
+                            JOIN product_product pp ON pp.id = m.product_id
+                            JOIN product_template pt ON pt.id = pp.product_tmpl_id
+                           WHERE m.state = 'done'
+                             AND pt.type != 'consu'
+
+                        UNION ALL
+
+                          SELECT m.product_id, m.location_id, m.company_id,
+                                 m.restrict_lot_id as lot_id, -m.product_qty as qty, m.date as date
+                            FROM stock_move m
+                            JOIN stock_location l on l.id = m.location_id
+                            JOIN product_product pp ON pp.id = m.product_id
+                            JOIN product_template pt ON pt.id = pp.product_tmpl_id
+                           WHERE m.state = 'done'
+                             AND l.usage = 'internal'
+                             AND pt.type != 'consu'
+
+                     ) as quants
+                GROUP BY product_id, location_id, company_id, lot_id
+                  HAVING SUM(qty) != 0
+            """
+        )
+    else:
+        moves = move_obj.search(cr, SUPERUSER_ID, [('state', '=', 'done'), ('product_qty', '>', 0.0)], order='company_id, date')
+        for move in util.iter_browse(move_obj, cr, SUPERUSER_ID, moves, logger=_logger):
+            preferred_domain = []
+            if move.move_orig_ids:
+                preferred_domain = [[('history_ids', 'in', [x.id for x in move.move_orig_ids])], [('history_ids', 'not in', [x.id for x in move.move_orig_ids])]]
+            quants = quant_obj.quants_get_prefered_domain(cr, SUPERUSER_ID, move.location_id, move.product_id, move.product_qty, domain=[('qty', '>', 0.0)],
+                                                        prefered_domain_list=preferred_domain, restrict_lot_id=move.restrict_lot_id.id, context={'force_company': move.company_id.id})
+            quant_obj.quants_move(cr, SUPERUSER_ID, quants, move, move.location_dest_id, lot_id=move.restrict_lot_id.id)
+
+        #Take in_date as first date of stock_move
+        cr.execute("""
+        UPDATE stock_quant SET in_date = qdate.date
+        FROM
+        (SELECT sq.id as id, MIN(sm.date) as date FROM stock_quant sq, stock_quant_move_rel sqmr, stock_move sm
+        WHERE sq.id = sqmr.quant_id AND sm.id = sqmr.move_id AND sm.state = 'done' AND sm.date is not null GROUP BY sq.id) AS qdate
+        WHERE stock_quant.id = qdate.id
+        """)
 
     # Assigned moves should be reassigned to give them to appropriate quants
     moves = move_obj.search(cr, SUPERUSER_ID, [('state', '=', 'assigned')])
