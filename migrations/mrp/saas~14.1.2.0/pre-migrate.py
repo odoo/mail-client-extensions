79c78871c71b016a61be523fbb4e100cd0cbed86
William Henrotin <whe@odoo.com>
2020-08-27 13:06:57 +0000

    [IMP] mrp@saas~14.1: remove field
    This commit remove the field is_partially_planned related to https://github.com/odoo/odoo/pull/54845
    Also `is_planned` become a stored field. This commit also filled the
    column with the computed value.
    
    closes odoo/upgrade#1710
    
    Related: odoo/odoo#54845
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/mrp/saas~14.1.2.0/pre-migrate.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, 'mrp.production', 'is_partially_planned')
+    # is_planned become stored. This fills the computed values the first time
+    util.create_column(cr, 'mrp_production', 'is_planned', 'boolean')
+    cr.execute("""
+        UPDATE mrp_production mo
+        SET is_planned = wo.date_planned_start IS NOT NULL and wo.date_planned_finished IS NOT NULL
+        FROM mrp_workorder wo
+        WHERE wo.production_id = mo.id
+    """)
