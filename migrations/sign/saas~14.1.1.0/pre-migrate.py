a818243f4a9ec0f5029a9b1415312f4df2cf75bc
Mahendra Barad <mba@odoo.com>
2020-08-31 11:12:40 +0000

    [IMP]sign@saas~14.1: Add responsible user and set default as create_uid
    Related PR: https://github.com/odoo/enterprise/pull/11765
    
    closes odoo/upgrade#1720
    
    Taskid: 2277566
    Related: odoo/enterprise#11765
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/sign/saas~14.1.1.0/pre-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, 'sign_template', 'user_id', 'int4')
+    cr.execute("UPDATE sign_template SET user_id = create_uid WHERE user_id IS NULL")
