bed03a5023b9a137c217f6c35111fc3e3bc7632c
Francois (fge) <fge@odoo.com>
2020-09-10 07:49:27 +0000

    [FIX] documents@saas~14.2: Link existing records to document
    Task-2195019
    See https://github.com/odoo/enterprise/pull/12680
    
    closes odoo/upgrade#1810
    
    Related: odoo/enterprise#12680
    Related: odoo/odoo#56554
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/documents/saas~14.2.1.0/pre-migrate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, 'documents.workflow.rule', 'has_business_option')
+    util.create_column(cr, "documents_workflow_rule", "link_model", "varchar")
+    util.create_column(cr, "documents_document", "is_editable_attachment", "bool")
