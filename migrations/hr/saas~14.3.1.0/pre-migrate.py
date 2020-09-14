68d302838dfbe9889e60d2bd2fb787ed7d59604b
Pedro (pno) <pno@odoo.com>
2020-09-14 13:44:28 +0000

    [IMP] hr@saas~14.3: add hr.work.location
    Change the work location of an employee to a Many2One.
    
    closes odoo/upgrade#2020
    
    Related: odoo/odoo#63192
    Related: odoo/enterprise#15235
    Related: odoo/enterprise#15235
    Related: odoo/odoo#63192
    Taskid: 2335646
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/hr/saas~14.3.1.0/pre-migrate.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+            CREATE TABLE IF NOT EXISTS hr_work_location (
+                id SERIAL PRIMARY KEY,
+                active BOOLEAN,
+                name varchar,
+                company_id int,
+                address_id int,
+                create_uid integer,
+                create_date timestamp without time zone,
+                write_uid integer,
+                write_date timestamp without time zone
+            )
+        """
+    )
+
+    util.create_column(cr, "hr_employee", "work_location_id", "int4")
+
+    cr.execute(
+        """
+            INSERT INTO hr_work_location(name, company_id, address_id, active, create_uid, create_date, write_uid, write_date)
+                 SELECT work_location, company_id, address_id, true, 1, now() at time zone 'utc', 1, now() at time zone 'utc'
+                   FROM hr_employee
+                  WHERE work_location IS NOT NULL
+               GROUP BY work_location, company_id, address_id
+        """
+    )
+
+    cr.execute(
+        """
+        UPDATE hr_employee empl
+           SET work_location_id = wl.id
+          FROM hr_work_location wl
+         WHERE empl.work_location = wl.name
+           AND empl.company_id = wl.company_id
+           AND empl.address_id = wl.address_id
+        """
+    )
+
+    util.remove_field(cr, "res.users", "work_location")
+    util.remove_field(cr, "hr.employee.base", "work_location")
