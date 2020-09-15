ea02de87607437b71c3829fae8926126f0b5ae4d
Denis Ledoux <dle@odoo.com>
2020-09-15 15:14:09 +0000

    [FIX] crm@10.saas~18: Force `crm.web_planner` menu assignation
    `crm.web_planner` menu gets changed in
    odoo/odoo@ea66192276921be1bafe73255fa77c55f3e10ac7
    and its former menu is deleted.
    
    If the new assignation is not forced,
    an SQL exception is raise because of the NOT NULL constraint
    on `menu_id` of `web_planner`
    
    ```sql
    SELECT module, name, model, res_id, noupdate FROM ir_model_data WHERE
    name = 'planner_crm';
     module |    name     |    model    | res_id | noupdate
    --------+-------------+-------------+--------+----------
     crm    | planner_crm | web.planner |      3 | t
    (1 row)
    ```
    
    Request 53354
    M19120913469839
    
    closes odoo/upgrade#1770
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/crm/10.saas~18.1.0/pre-rename.py
+++ migrations/crm/10.saas~18.1.0/pre-rename.py
@@ -3,3 +3,10 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.rename_xmlid(cr, 'sales_team.menu_sales', 'crm.crm_menu_pipeline')
+    # The `menu_id` of `crm.planner_crm` gets changed in
+    # odoo/odoo@ea66192276921be1bafe73255fa77c55f3e10ac7
+    # The former menu `sales_team.menu_base_partner` is deleted in the same commit
+    # As the field `menu_id` of `web.planner` is required,
+    # it's important it gets setted with the new menu
+    # before `sales_team.menu_base_partner` gets deleted
+    util.force_noupdate(cr, 'crm.planner_crm', False)
