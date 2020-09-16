dd215e250735dfd9eda1673a0643ff4896161a7b
Pierre Masereel <pim@odoo.com>
2020-09-16 06:48:47 +0000

    [REF] point_of_sale@saas~14.1: remove unused field
    The field 'pos_statement_id' is not used anymore since last refactoring
    of the session closure. So we removed it.
    
    closes odoo/upgrade#1772
    
    Related: odoo/odoo#57785
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/point_of_sale/saas~14.1.1.0.1/pre-migrate.py
+++ migrations/point_of_sale/saas~14.1.1.0.1/pre-migrate.py
@@ -7,3 +7,4 @@ def migrate(cr, version):
     util.rename_field(cr, "pos.config", "iface_customer_facing_display", "iface_customer_facing_display_via_proxy")
     util.remove_field(cr, "pos.config", "customer_facing_display_html")
     util.remove_view(cr, "point_of_sale.customer_facing_display_html")
+    util.remove_field(cr, "account.bank.statement.line", "pos_statement_id")
