b19aeaef76da058cf7510e9e04196f0e12f9a118
Denis Ledoux <dle@odoo.com>
2020-09-18 16:07:44 +0000

    [FIX] account@saas~12.4: fix draft/cancel invoice with same total moves
    If a draft/cancelled invoice has a `move_id`,
    and the move has the same amount,
    there are good chances the move is the right move for the invoice.
    
    If the move is posted, then the invoice should have been posted in the
    first place as well. And the invoice line <-> move line mapping will do
    its job.
    
    If the move is not posted, then the invoice can be left
    as draft/cancelled, but can be removed from the inconsistent
    invoices list. The invoice line <-> move line mapping will
    do its job to merge the draft/cancelled invoice with the unposted move.
    
    Request 43639
    Request 46864
    Request 46382
    Request 49863
    Request 47486
    Request 53420
    Request 43616
    Request 53120
    
    closes odoo/upgrade#1787
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/account/saas~12.4.1.1/pre-03-account-checks.py
+++ migrations/account/saas~12.4.1.1/pre-03-account-checks.py
@@ -14,10 +14,65 @@ def migrate(cr, version):
     )
     [inconsistent_invoice_ids] = cr.fetchone()
     if inconsistent_invoice_ids:
+        inconsistent_invoice_ids = set(inconsistent_invoice_ids)
         # Try to fix the inconsistencies automatically
 
-        # 1. The move associated to the invoice have no lines. It is pointless -> break the link (set move_id to null)
-        inconsistent_invoice_ids = set(inconsistent_invoice_ids)
+        # 1. The move associated to the invoice has the same total.
+        #    Mark the invoice has open instead of draft/cancelled if the move is posted,
+        #    otherwise let the former draft/cancel state, but remove it from the inconsistent invoices list
+        #    to let the invoice line <-> move line mapping do its job.
+        cr.execute(
+            """
+                   UPDATE account_invoice invoice
+                      SET state = CASE WHEN same_amount_move.state = 'posted' THEN 'open' ELSE invoice.state END
+                     FROM (
+                               SELECT invoice.id as invoice_id, move.state, invoice.number, invoice.date_invoice
+                                 FROM account_invoice invoice
+                                 JOIN account_move move ON move.id = invoice.move_id
+                                 JOIN account_journal journal ON journal.id = invoice.journal_id
+                                 JOIN res_company company ON company.id = journal.company_id
+                            LEFT JOIN account_move_line move_line ON move_line.move_id = move.id
+                                WHERE invoice.state in ('draft', 'cancel')
+                                  AND invoice.move_id IS NOT NULL
+                             GROUP BY invoice.id, move.id
+                               HAVING ROUND(SUM(
+                                            CASE
+                                            WHEN debit > 0
+                                            THEN
+                                                CASE
+                                                WHEN move_line.currency_id != company.currency_id
+                                                THEN amount_currency
+                                                ELSE debit
+                                                END
+                                            ELSE 0
+                                            END
+                                      ), 2) = ROUND(invoice.amount_total, 2)
+                          ) as same_amount_move
+                    WHERE same_amount_move.invoice_id = invoice.id
+                RETURNING id, invoice.number, invoice.date_invoice
+            """
+        )
+        same_amount = cr.fetchall()
+        if same_amount:
+            inconsistent_invoice_ids -= set([r[0] for r in same_amount])
+            util.add_to_migration_reports(
+                f"""
+                    <details>
+                    <summary>
+                        {len(same_amount)} draft and cancelled invoices were linked to accounting entries having the
+                        same total. Normally, draft and cancelled invoices should not have entries in the accounting.
+                        To be able to upgrade properly, these invoices have been marked as "Open" if their entry in the
+                        accounting was posted. If the entry was unposted, than the invoice has been left as
+                        draft/cancelled. Anyhow, you should really give it a look to make sure everything is correct.
+                    </summary>
+                    Invoices: {", ".join(f"{number}({date})" for _, number, date in same_amount)}
+                    </details>
+                """,
+                "Accounting",
+                format="html",
+            )
+
+        # 2. The move associated to the invoice have no lines. It is pointless -> break the link (set move_id to null)
         cr.execute(
             """
                 UPDATE account_invoice SET move_id = NULL WHERE id IN (
@@ -29,12 +84,12 @@ def migrate(cr, version):
                           AND invoice.move_id IS NOT NULL
                      GROUP BY invoice.id, move.id HAVING SUM(COALESCE(move_line.debit, 0)) = 0
                 )
-                RETURNING id
+                RETURNING id, number, date_invoice
             """
         )
-        unlink_because_empty = [r[0] for r in cr.fetchall()]
+        unlink_because_empty = cr.fetchall()
         if unlink_because_empty:
-            inconsistent_invoice_ids -= set(unlink_because_empty)
+            inconsistent_invoice_ids -= set([r[0] for r in unlink_because_empty])
             util.add_to_migration_reports(
                 f"""
                     <details>
@@ -44,7 +99,7 @@ def migrate(cr, version):
                         and their entry have been removed.
                         The moves, even if empty, have been kept, for history purposes.
                     </summary>
-                    Invoice ids: {unlink_because_empty}
+                    Invoices: {", ".join(f"{number}({date})" for _, number, date in unlink_because_empty)}
                     </details>
                 """,
                 "Accounting",
