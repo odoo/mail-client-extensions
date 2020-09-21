baa4262e73374f561702b218cf5596a993fa498c
Florimond Husquinet <florimond@husquinet.net>
2020-09-21 15:06:01 +0200

    CompanyContact fa icons

--- outlook/src/taskpane/components/Company/CompanyContact.tsx
+++ outlook/src/taskpane/components/Company/CompanyContact.tsx
@@ -1,18 +1,10 @@
 import * as React from 'react';
-import { mergeStyles, mergeStyleSets } from 'office-ui-fabric-react/lib/Styling';
 import { Separator } from "office-ui-fabric-react/lib/Separator";
 import AppContext from '../AppContext';
 import InfoCell from "../InfoCell/InfoCell";
 
-const iconClass = mergeStyles({
-    fontSize: 15,
-    margin: '0 15px',
-});
-const classNames = mergeStyleSets({
-    address: [{ color: '#31B3B5' }, iconClass],
-    phone: [{color: '#A24689'}, iconClass],
-    website: [{color: '#0645AD'}, iconClass]
-});
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGlobeEurope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 
 type CompanyContactProps = {
 };
@@ -26,31 +18,38 @@ class CompanyContact extends React.Component<CompanyContactProps, CompanyContact
 
     public render(): JSX.Element {
         const {company} = this.context.partner;
-        
-        let websiteSection = null;
-        if (company.getDomain()) {
-            websiteSection = <InfoCell hrefContent={company.getDomain()} iconName="Link" iconClassName={classNames.website} title="Website" value={company.getDomain()} />
-        }
-        
-        let phoneSection = null;
-        if (company.getPhone()){
-            phoneSection = <InfoCell hrefContent={`tel:${company.getPhone()}`} iconName="Phone" iconClassName={classNames.phone} title="Phone" value={company.getPhone()} />
-        }
 
         let addressSection = null;
         if (company.getLocation()) {
-            addressSection = <InfoCell hrefContent={`http://maps.google.com/?q=${company.getLocation()}`} iconName="MapPin" iconClassName={classNames.address} title="Address" value={company.getLocation()} />
+            // #31B3B5
+            const addressIcon = <div className='company-icon'><FontAwesomeIcon icon={faMapMarkerAlt} color='darkgrey' className="fa-fw"/></div>
+            addressSection = <InfoCell hrefContent={`http://maps.google.com/?q=${company.getLocation()}`} icon={addressIcon} title="Address" value={company.getLocation()} />
         }
+
+        let phoneSection = null;
+        if (company.getPhone()){
+            // A24689
+            const phoneIcon = <div className='company-icon'><FontAwesomeIcon icon={faPhone} color='darkgrey' className="fa-fw"/></div>
+            phoneSection = <InfoCell hrefContent={`tel:${company.getPhone()}`} icon={phoneIcon} title="Phone" value={company.getPhone()} />
+        }
+
+        let websiteSection = null;
+        if (company.getDomain()) {
+            // 0645AD
+            const websiteIcon = <div className='company-icon'><FontAwesomeIcon icon={faGlobeEurope} color='darkgrey' className="fa-fw"/></div>
+            websiteSection = <InfoCell hrefContent={company.getDomain()} icon={websiteIcon} title="Website" value={company.getDomain()} />
+        }
+
 
         if (!websiteSection && !phoneSection && !addressSection)
             return null;
         return (
             <div className='bounded-tile'>
-                {websiteSection}
-                {websiteSection && phoneSection ? <Separator /> : null}
-                {phoneSection}
-                {(websiteSection || phoneSection) && addressSection ? <Separator /> : null}
                 {addressSection}
+                {addressSection && (phoneSection || websiteSection) ? <Separator /> : null}
+                {phoneSection}
+                {phoneSection && websiteSection ? <Separator /> : null}
+                {websiteSection}
             </div>
         );
     }
--- outlook/src/taskpane/components/InfoCell/InfoCell.tsx
+++ outlook/src/taskpane/components/InfoCell/InfoCell.tsx
@@ -1,12 +1,9 @@
 import * as React from "react";
-import { FontIcon } from 'office-ui-fabric-react/lib/Icon';
-
 import "./InfoCell.css";
 
 type InfoCellProps = {
     hrefContent: string,
-    iconName: string,
-    iconClassName: string,
+    icon: any,
     title: string,
     value: string
 };
@@ -18,11 +15,11 @@ class InfoCell extends React.Component<InfoCellProps, InfoCellState> {
     }
 
     render() {
-        const {hrefContent, iconName, iconClassName, title, value} = this.props;
+        const {hrefContent, icon, title, value} = this.props;
         return <a className="button-link" href={hrefContent} target="_blank" rel="noreferrer noopener">
             <div className="info-cell">
                 <div className="info-cell-left">
-                <FontIcon iconName={iconName} className={iconClassName}/>
+                {icon}
                 </div>
                 <div className="info-cell-right">
                 <div className="info-cell-title">{title}</div>
