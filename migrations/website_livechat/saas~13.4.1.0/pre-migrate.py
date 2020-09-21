50faef034294e034797e2a2a85868e3dfb411aa2
Christophe Simonis <chs@odoo.com>
2020-09-21 18:46:25 +0200

    [FIX] website_livechat@saas~13.4: keep livechat website menu
    Yes, `menu_livechat` is a `website.menu`, not a `ir.ui.menu`.
    
    While the records has been removed [1], we should keep it in case it was
    used by the customer.
    
    [1] https://github.com/odoo/odoo/commit/9b9829416ba0b09901a3de1578ff172024ab4f35#diff-c386c9f87e6dbc6eae78e1525d714a3bL8

--- migrations/website_livechat/saas~13.4.1.0/pre-migrate.py
+++ migrations/website_livechat/saas~13.4.1.0/pre-migrate.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.remove_menus(cr, [util.ref(cr, "website_livechat.menu_livechat")])
+    util.force_noupdate(cr, "website_livechat.menu_livechat", noupdate=True)
