0a32914c6939451b1d6dc5afa0c9cb904fe821f0
Christophe Simonis <chs@odoo.com>
2020-09-23 11:20:51 +0000

    [FIX] base@saas~13.5: remove orphan currency rates
    See odoo/odoo#58303
    
    closes odoo/upgrade#1798
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~13.5.1.3/pre-models.py
+++ migrations/base/saas~13.5.1.3/pre-models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.remove_field(cr, "res.country", "image")
+
+    cr.execute("DELETE FROM res_currency_rate WHERE currency_id IS NULL")
