83f39a416f6c7420ee7cc8307bb513992d970a74
Christophe Simonis <chs@odoo.com>
2020-09-24 19:00:06 +0200

    [FIX] account@saas~13.5: be lenient about constraints
    Don't assume it exists and allow it to be non-applicable.
    
    Reformat file.

--- migrations/account/saas~13.5.1.1/pre-10-fields.py
+++ migrations/account/saas~13.5.1.1/pre-10-fields.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-from odoo.addons.base.maintenance.migrations import util
+from odoo.tools.misc import ignore
+from odoo.upgrade import util
+
+import psycopg2
 
 
 def migrate(cr, version):
@@ -8,68 +11,70 @@ def migrate(cr, version):
     # Currency on all account.move.line (PR:50711 & 10394)
     # ===========================================================
 
-    util.remove_field(cr, 'account.move.line', 'always_set_currency_id')
+    util.remove_field(cr, "account.move.line", "always_set_currency_id")
 
-    cr.execute('''
-        ALTER TABLE account_move_line
-        DROP CONSTRAINT account_move_line_check_amount_currency_balance_sign
-    ''')
+    cr.execute(
+        "ALTER TABLE account_move_line DROP CONSTRAINT IF EXISTS account_move_line_check_amount_currency_balance_sign"
+    )
 
-    cr.execute('''
-        UPDATE account_move_line
-        SET 
-            currency_id = company_currency_id,
-            amount_currency = balance
-        WHERE currency_id IS NULL
-    ''')
+    cr.execute(
+        """
+            UPDATE account_move_line
+               SET currency_id = company_currency_id,
+                   amount_currency = balance
+             WHERE currency_id IS NULL
+        """
+    )
 
-    # Prevent inconsistencies in others scripts by adding the updated constraint manually.
-    cr.execute('''
-        ALTER TABLE account_move_line 
-        ADD CONSTRAINT account_move_line_check_amount_currency_balance_sign 
-        CHECK(
-            (
-                (currency_id != company_currency_id)
-                AND
-                (
-                    (debit - credit <= 0 AND amount_currency <= 0)
+    # Prevent inconsistencies in others scripts by adding the updated constraint manually (if possible)
+    with ignore(psycopg2.Error), util.savepoint(cr):
+        cr.execute(
+            """
+                ALTER TABLE account_move_line
+                ADD CONSTRAINT account_move_line_check_amount_currency_balance_sign
+                CHECK(
+                    (
+                        (currency_id != company_currency_id)
+                        AND
+                        (
+                            (debit - credit <= 0 AND amount_currency <= 0)
+                            OR
+                            (debit - credit >= 0 AND amount_currency >= 0)
+                        )
+                    )
                     OR
-                    (debit - credit >= 0 AND amount_currency >= 0)
+                    (
+                        currency_id = company_currency_id
+                        AND
+                        ROUND(debit - credit - amount_currency, 2) = 0
+                    )
                 )
-            )
-            OR
-            (
-                currency_id = company_currency_id
-                AND
-                ROUND(debit - credit - amount_currency, 2) = 0
-            )
+            """
         )
-    ''')
 
     # ===========================================================
     # Fiscal country field on companies (PR 55308 & 12156)
     # ===========================================================
-    util.create_column(cr, 'res_company', 'account_tax_fiscal_country_id', 'int4')
+    util.create_column(cr, "res_company", "account_tax_fiscal_country_id", "int4")
 
     # If a fiscal country was set with the dedicated config parameter, use it; else use the company's country.
-    cr.execute("""
-        UPDATE res_company
-        SET account_tax_fiscal_country_id = COALESCE(parameter_country.id, company_partner.country_id)
-        FROM res_partner company_partner
-        JOIN res_company company on company.partner_id = company.partner_id
-        LEFT JOIN ir_config_parameter fiscal_country_param ON fiscal_country_param.key = CONCAT('account_fiscal_country_', company.id)
-        LEFT JOIN res_country parameter_country ON LOWER(parameter_country.code) = LOWER(fiscal_country_param.value)
-        WHERE res_company.id = company.id
-
-    """)
+    cr.execute(
+        """
+            UPDATE res_company
+               SET account_tax_fiscal_country_id = COALESCE(parameter_country.id, company_partner.country_id)
+              FROM res_partner company_partner
+              JOIN res_company company ON company.partner_id = company.partner_id
+         LEFT JOIN ir_config_parameter fiscal_country_param
+                ON fiscal_country_param.key = CONCAT('account_fiscal_country_', company.id)
+         LEFT JOIN res_country parameter_country ON LOWER(parameter_country.code) = LOWER(fiscal_country_param.value)
+             WHERE res_company.id = company.id
+        """
+    )
 
     # Remove the fiscal country config parameters; now useless.
-    cr.execute("""
-        DELETE FROM ir_config_parameter
-        WHERE key LIKE 'account_fiscal_country_%'
-    """)
+    cr.execute("DELETE FROM ir_config_parameter WHERE key LIKE 'account_fiscal_country_%'")
 
     # Replace the old related fields which pointed towards company country for use in taxes
-    util.rename_field(cr, 'account.move.line', 'country_id', 'tax_fiscal_country_id')
-    util.rename_field(cr, 'account.tax', 'country_id', 'tax_fiscal_country_id')
-    util.rename_field(cr, 'account.tax.repartition.line', 'country_id', 'tax_fiscal_country_id')
+    util.rename_field(cr, "account.move.line", "country_id", "tax_fiscal_country_id")
+    util.rename_field(cr, "account.tax", "country_id", "tax_fiscal_country_id")
+    util.rename_field(cr, "account.tax.repartition.line", "country_id", "tax_fiscal_country_id")
