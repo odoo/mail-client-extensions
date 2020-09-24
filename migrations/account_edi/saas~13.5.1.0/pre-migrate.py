9f80852d7bb87974b23abad56f218eb3a841eb14
Christophe Simonis <chs@odoo.com>
2020-09-24 19:02:37 +0200

    [FIX] {account_edi,base}@saas~13.5: force upgrade of `account_edi`
    Allow SQL bootstrap of new column

--- migrations/account_edi/saas~13.5.1.0/pre-migrate.py
+++ migrations/account_edi/saas~13.5.1.0/pre-migrate.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
-from odoo.addons.base.maintenance.migrations import util
+from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    if util.table_exists(cr, "account_edi_format"):
+        util.create_column(cr, "account_edi_format", "has_web_service", "boolean", default=False)
+    util.create_column(cr, "account_move", "edi_state", "varchar")
 
-    # ===========================================================
-    # account_edi + refactoring l10n_mx_edi (PR: 52407 & 12226)
-    # ===========================================================
-
-    util.create_column(cr, 'account_edi_format', 'has_web_service', 'boolean', default=False)
-    util.create_column(cr, 'account_move', 'edi_state', 'varchar')
-    util.remove_field(cr, 'ir.attachment', 'edi_format_id')
-    util.remove_field(cr, 'account.edi.format', 'hide_on_journal')
+    util.remove_field(cr, "ir.attachment", "edi_format_id")
+    util.remove_field(cr, "account.edi.format", "hide_on_journal")
