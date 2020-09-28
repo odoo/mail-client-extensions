7d26f11b091723d117f7841561e1b2af04f5abe8
Christophe Simonis <chs@odoo.com>
2020-09-28 19:06:25 +0200

    [FIX] base@saas~13.5

--- migrations/base/saas~13.5.1.3/pre-30-data.py
+++ migrations/base/saas~13.5.1.3/pre-30-data.py
@@ -8,10 +8,6 @@ def migrate(cr, version):
     util.rename_xmlid(cr, *eb("{base,web}.action_base_document_layout_configurator"))
     util.remove_view(cr, "base.layout_preview")
 
-    util.move_model(cr, "base.document.layout", "base", "web")
-    util.create_column(cr, "report_layout", "sequence", "integer")
-    cr.execute("UPDATE report_layout SET sequence = id")
-
     util.rename_xmlid(cr, *eb("base.module_category_{localization,accounting_localizations}_account_charts"))
     util.remove_record(cr, "base.module_category_localization")
     util.remove_record(cr, "base.module_category_operations")
