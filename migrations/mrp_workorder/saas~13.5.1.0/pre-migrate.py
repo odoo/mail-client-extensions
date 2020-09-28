21927e341ea19868ded7b452ceb312b4c145b636
Xavier-Do <xdo@odoo.com>
2020-09-28 08:05:31 +0000

    [FIX] mrp_workorder@saas~13.5: remove view
    View removed in odoo/enterprise#13397
    
    closes odoo/upgrade#1809
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/mrp_workorder/saas~13.5.1.0/pre-migrate.py
+++ migrations/mrp_workorder/saas~13.5.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_column(cr, "quality_check", "finished_lot_id")
+    util.remove_view(cr, "mrp_workorder.mrp_production_view_search_inherit_planning")
