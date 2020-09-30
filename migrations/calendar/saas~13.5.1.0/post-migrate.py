71d1ca8607a227caafa4bf5f23c5b876d18f6c7b
Christophe Simonis <chs@odoo.com>
2020-09-30 12:44:13 +0200

    [FIX] calendar@saas~13.5: iter browse recurrences

--- migrations/calendar/saas~13.5.1.0/post-migrate.py
+++ migrations/calendar/saas~13.5.1.0/post-migrate.py
@@ -1,26 +1,21 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-
     env = util.env(cr)
     recurrences = env["calendar.recurrence"].search([])
     not_all_day_ranges = []
     all_day_ranges = []
-    recurrences_all_day = recurrences.filtered(lambda r: r._is_allday())
-    for recurrence in recurrences - recurrences_all_day:
-        event = recurrence.base_event_id
-        duration = event.stop - event.start
-        ranges = recurrence._get_ranges(event.start, duration)
-        not_all_day_ranges += [(recurrence.id, start, stop) for start, stop in ranges]
 
-    for recurrence in recurrences_all_day:
+    for recurrence in util.iter_browse(env["calendar.recurrence"], recurrences.ids):
         event = recurrence.base_event_id
         duration = event.stop - event.start
         ranges = recurrence._get_ranges(event.start, duration)
-        all_day_ranges += [(recurrence.id, start.date(), stop.date()) for start, stop in ranges]
+        if recurrence._is_allday():
+            all_day_ranges += [(recurrence.id, start.date(), stop.date()) for start, stop in ranges]
+        else:
+            not_all_day_ranges += [(recurrence.id, start, stop) for start, stop in ranges]
 
     cr.executemany(
         """
