693896cdd48684b4fce0caaac7d5455f89f15140
Denis Ledoux <dle@odoo.com>
2020-10-02 14:41:54 +0000

    [FIX] base@0.0.0: missing already encrypted passwords
    When there are more than 10.000 passwords to encrypt,
    we delay the encryption in a cron.
    
    See 7e38c2484132014499b7a0fec423505b7b99ef63
    
    However, when updrading from 11.0 to 12.0,
    the script `saas~11.4.1.3/pre-05-auth_crypt.py` took
    care to put the encrypted passwords from
    `password_crypt` to `password`.
    
    As 0.0.0 scripts are executed before the regular ones,
    `0.0.0/pre-user-crypt-password.py`
    is executed before
    `saas~11.4.1.3/pre-05-auth_crypt.py`
    and therefore we missed the passwords from the
    `password_crypt` in the column `_upg_password_to_crypt`.
    
    So, at the end, when `_upg_password_to_crypt` is renamed
    to `password` in `0.0.0/end-user-crypt-password.py`,
    the already encrypted passwords were missing.
    
    This revision simply aims to execute the script of
    `saas~11.4.1.3/pre-05-auth_crypt.py` before renaming
    the table `password` to `_upg_password_to_crypt`,
    so they are included.
    
    opw-2347006
    Request 53292
    M1707114531580
    
    closes odoo/upgrade#1816
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/base/0.0.0/pre-user-crypt-password.py
+++ migrations/base/0.0.0/pre-user-crypt-password.py
@@ -5,6 +5,11 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     if not util.version_gte("saas~11.4"):
         return
+    if not util.version_gte("saas~12.1") and util.module_installed(cr, "auth_crypt"):
+        cr.execute("UPDATE res_users SET password_crypt=password WHERE password_crypt IS NULL AND password IS NOT NULL")
+        util.remove_column(cr, "res_users", "password")
+        cr.execute("ALTER TABLE res_users RENAME COLUMN password_crypt TO password")
+        util.remove_field(cr, "res.users", "password_crypt")
 
     # Encrypting hundred of thousands passwords takes times. Delay the encryption after upgrade for such a case.
     cr.execute(
