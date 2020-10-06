b1d01314474555f6c4201370fc1d0e0a7d178218
Xavier BOL (xbo) <xbo@odoo.com>
2020-10-06 13:36:52 +0000

    [IMP] l10n_be_hr_payroll@saas~14.1: update fields in payroll.alloc.paid.leave
    In this wizard, the date_start and date_end fields have been replaced
    by the year field to select only the year of the period to compute the
    legal leaves of each employee for the next year.
    
    This migration script updates the record (if there are) and removes both
    fields to create the year field.
    
    closes odoo/upgrade#1832
    
    Pr: https://github.com/odoo/enterprise/pull/13953
    Taskid: 2314223
    Related: odoo/enterprise#13953
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/l10n_be_hr_payroll/saas~14.1.1.0/pre-migrate.py
+++ migrations/l10n_be_hr_payroll/saas~14.1.1.0/pre-migrate.py
@@ -6,3 +6,22 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
 
     util.remove_view(cr, "l10n_be_hr_payroll.hr_payslip_run_tree")
+
+    util.create_column(cr, "hr_leave_allocation", "max_leaves_allocated", "float", default=20)
+
+    util.create_column(cr, "hr_payroll_alloc_paid_leave", "year", "integer")
+    util.remove_field(cr, "hr.payroll.alloc.paid.leave", "date_start")
+    util.remove_field(cr, "hr.payroll.alloc.paid.leave", "date_end")
+    util.remove_field(cr, "hr.payroll.alloc.paid.leave", "alloc_employee_ids", cascade=True)
+
+    util.remove_field(cr, "hr.payroll.alloc.employee", "worked_days")
+    util.remove_field(cr, "hr.payroll.alloc.employee", "paid_time_off")
+    util.create_column(cr, "hr_payroll_alloc_employee", "paid_time_off", "real")
+    util.create_column(cr, "hr_payroll_alloc_employee", "paid_time_off_to_allocate", "real")
+
+    util.remove_field(cr, "l10n_be.hr.payroll.credit.time.wizard", "work_time")
+    util.create_column(cr, "l10n_be_hr_payroll_credit_time_wizard", "time_off_allocation", "real")
+    util.create_column(cr, "l10n_be_hr_payroll_credit_time_wizard", "remaining_allocated_time_off", "real")
+
+    util.create_column(cr, "l10n_be_hr_payroll_exit_credit_time_wizard", "time_off_allocation", "real")
+    util.create_column(cr, "l10n_be_hr_payroll_exit_credit_time_wizard", "remaining_allocated_time_off", "real")
