e3dedc72fcb08e588b220c61b31a3eef5588c138
Christophe Simonis <chs@odoo.com>
2020-10-06 19:16:57 +0200

    [FIX] iap_crm@saas~13.5

--- migrations/iap_crm/saas~13.5.1.0/pre-migrate.py
+++ migrations/iap_crm/saas~13.5.1.0/pre-migrate.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    if util.module_installed(cr, "crm_iap_lead"):
-        util.move_field_to_module(cr, "crm.lead", "reveal_id",
-                                  "crm_iap_lead", "iap_crm")
-    elif util.module_installed(cr, "crm_iap_lead_enrich"):
-        util.move_field_to_module(cr, "crm.lead", "reveal_id",
-                                  "crm_iap_lead_enrich", "iap_crm")
+    util.move_field_to_module(cr, "crm.lead", "reveal_id", "crm_iap_lead", "iap_crm")
+    util.move_field_to_module(cr, "crm.lead", "reveal_id", "crm_iap_lead_enrich", "iap_crm")
