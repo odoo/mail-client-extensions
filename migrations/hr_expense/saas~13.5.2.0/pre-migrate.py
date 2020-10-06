77bde790c7be639653988ae01c778b5ddc3a893d
Christophe Simonis <chs@odoo.com>
2020-10-06 17:23:29 +0200

    [FIX] hr_expense@saas~13.5

--- migrations/hr_expense/saas~13.5.2.0/pre-migrate.py
+++ migrations/hr_expense/saas~13.5.2.0/pre-migrate.py
@@ -3,48 +3,39 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.create_column(cr, "hr_expense", "sample", "boolean")
-
-    util.remove_model(cr, 'hr.expense.sheet.register.payment.wizard')
-    util.create_column(cr, 'hr_expense_sheet', 'amount_residual', 'numeric')
+    util.create_column(cr, "hr_expense", "sample", "boolean", default=False)
+    util.create_column(cr, "hr_expense_sheet", "amount_residual", "numeric")
 
     # Compute the residual amount on expense sheets using the same currency as the one from the company.
-    cr.execute('''
-        WITH amount_residual_per_sheet AS (
-            SELECT
-                sheet.id AS sheet_id,
-                -ROUND(SUM(line.amount_residual), currency.decimal_places) AS amount_residual
-            FROM hr_expense_sheet sheet
-            JOIN res_company company ON company.id = sheet.company_id
-            JOIN res_currency currency ON currency.id = company.currency_id
-            JOIN account_move_line line ON line.move_id = sheet.account_move_id
-            JOIN account_account account ON account.id = line.account_id
-            WHERE account.internal_type IN ('receivable', 'payable')
-            AND line.currency_id != sheet.currency_id
-            GROUP BY sheet.id, currency.decimal_places
-        )
-        UPDATE hr_expense_sheet
-        SET amount_residual = amount_residual_per_sheet.amount_residual
-        FROM amount_residual_per_sheet
-        WHERE amount_residual_per_sheet.sheet_id = hr_expense_sheet.id
-    ''')
+    cr.execute(
+        """
+            WITH cte AS (
+                SELECT s.id, -ROUND(SUM(
+                    CASE WHEN s.currency_id = c.currency_id
+                        THEN l.amount_residual
+                        ELSE l.amount_residual_currency
+                         END
+                    ), curr.decimal_places) AS residual
+                FROM hr_expense_sheet s
+                JOIN res_company c ON c.id = s.company_id
+                JOIN res_currency curr ON curr.id = s.currency_id
+                JOIN account_move_line l ON l.move_id = s.account_move_id
+                JOIN account_account account ON account.id = l.account_id
+               WHERE account.internal_type IN ('receivable', 'payable')
+            GROUP BY s.id, curr.decimal_places
+            )
+            UPDATE hr_expense_sheet s
+               SET amount_residual = cte.residual
+              FROM cte
+             WHERE cte.id = s.id
+        """
+    )
 
-    # Compute the residual amount on expense sheets using a foreign currency.
-    cr.execute('''
-        WITH amount_residual_per_sheet AS (
-            SELECT
-                sheet.id AS sheet_id,
-                -ROUND(SUM(line.amount_residual_currency), currency.decimal_places) AS amount_residual
-            FROM hr_expense_sheet sheet
-            JOIN res_currency currency ON currency.id = sheet.currency_id
-            JOIN account_move_line line ON line.move_id = sheet.account_move_id
-            JOIN account_account account ON account.id = line.account_id
-            WHERE account.internal_type IN ('receivable', 'payable')
-            AND line.currency_id = sheet.currency_id
-            GROUP BY sheet.id, currency.decimal_places
-        )
-        UPDATE hr_expense_sheet
-        SET amount_residual = amount_residual_per_sheet.amount_residual
-        FROM amount_residual_per_sheet
-        WHERE amount_residual_per_sheet.sheet_id = hr_expense_sheet.id
-    ''')
+    util.remove_model(cr, "hr.expense.sheet.register.payment.wizard")
+
+    util.rename_xmlid(cr, "hr_expense.view_expenses_tree", "hr_expense.hr_expense_view_expenses_analysis_tree")
+    util.rename_xmlid(cr, "hr_expense.view_expense_kanban_view", "hr_expense.hr_expense_view_expenses_analysis_kanban")
+
+    util.remove_record(cr, "hr_expense.action_unsubmitted_expense")
+    util.remove_record(cr, "hr_expense.action_request_approve_expense_sheet")
+    util.remove_record(cr, "hr_expense.action_request_to_post_expense_sheet")
