eb2502d011cdcb5ceaadf183042208dd1f6d05a5
YOGRAJ TANDEL <yta@odoo.com>
2020-10-07 09:52:06 +0000

    [IMP] calendar@saas~14.3: rename weekdays field name
    With this commit we are renaming weekdays fields like MO, TU, WE etc to MON,
    TUE, WED... also with this commit we also update values of weekday field from
    MO to MON, TU to TUE and so on.
    
    Community PR: https://github.com/odoo/odoo/pull/60277
    
    task-2335399
    
    Co-authored-by: Mohammed Shekha <msh@odoo.com>

---
+++ migrations/calendar/saas~14.3.1.0/pre-migrate.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    models = ["calendar.event", "calendar.recurrence"]
+    rename_fields = {
+        "mo": "mon",
+        "tu": "tue",
+        "we": "wed",
+        "th": "thu",
+        "fr": "fri",
+        "sa": "sat",
+        "su": "sun",
+    }
+    for model in models:
+        for old_name, new_name in rename_fields.items():
+            util.rename_field(cr, model, old_name, new_name)
+
+    # Update values of weekday selection field
+    cr.execute(
+        """
+           UPDATE calendar_recurrence
+              SET weekday = CASE weekday
+                            WHEN 'MO' THEN 'MON'
+                            WHEN 'TU' THEN 'TUE'
+                            WHEN 'WE' THEN 'WED'
+                            WHEN 'TH' THEN 'THU'
+                            WHEN 'FR' THEN 'FRI'
+                            WHEN 'SA' THEN 'SAT'
+                            WHEN 'SU' THEN 'SUN'
+                            ELSE weekday
+                             END
+            WHERE weekday IS NOT NULL
+        """
+    )
