2a8cd15886af117e4cb5a4fd3a1e6669a7e147d7
Christophe Simonis <chs@odoo.com>
2020-10-08 15:26:06 +0200

    [FIX] l10n_co@saas~13.5: reformat files

--- migrations/l10n_co/saas~13.5.0.8/pre-migrate.py
+++ migrations/l10n_co/saas~13.5.0.8/pre-migrate.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.create_column(cr, 'l10n_latam_identification_type', 'l10n_co_document_code', 'varchar')
-
-    util.remove_view(cr, 'l10n_co.view_partner_form_inherit_l10n_co')
+    util.create_column(cr, "l10n_latam_identification_type", "l10n_co_document_code", "varchar")
+    util.remove_view(cr, "l10n_co.view_partner_form_inherit_l10n_co")
