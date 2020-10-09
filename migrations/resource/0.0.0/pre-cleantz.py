cd0c85b8d17499c257fd3e63b608706aee043abb
SEINLET Nicolas <nicolas@seinlet.com>
2020-10-09 10:53:25 +0200

    [ADD] resource@0.0.0

---
+++ migrations/resource/0.0.0/pre-cleantz.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    """
+    some timezones doesn't exists in all pytz versions
+    """
+    if not util.column_exists(cr, "resource_resource", "tz"):
+        return
+
+    tz_convert = [
+        ["US/Pacific-New", "US/Pacific"],
+    ]
+    for old, new in tz_convert:
+        cr.execute("UPDATE resource_resource SET tz=%s WHERE tz=%s", [new, old])
