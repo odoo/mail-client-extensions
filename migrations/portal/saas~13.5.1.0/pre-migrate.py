14c9b0cf05da462b780103425ed8df9a1e4e69cf
Christophe Simonis <chs@odoo.com>
2020-10-13 17:27:56 +0200

    [FIX] portal@saas~13.5

--- migrations/portal/saas~13.5.1.0/pre-migrate.py
+++ migrations/portal/saas~13.5.1.0/pre-migrate.py
@@ -3,5 +3,8 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.move_field_to_module(cr, "ir.ui.view", "customize_show", "website", "portal")
+    util.create_column(cr, "ir_ui_view", "customize_show", "boolean", default=False)
+
     util.remove_view(cr, "portal.portal_archive_groups")
     util.rename_xmlid(cr, "portal.portal_show_sign_in", "portal.user_sign_in")
