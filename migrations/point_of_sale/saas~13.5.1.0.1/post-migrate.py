bf724bb85199e941b20f000442f45519da8ca85a
Christophe Simonis <chs@odoo.com>
2020-10-13 17:10:26 +0200

    [FIX] point_of_sale@saas~13.5

--- migrations/point_of_sale/saas~13.5.1.0.1/post-migrate.py
+++ migrations/point_of_sale/saas~13.5.1.0.1/post-migrate.py
@@ -3,21 +3,13 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    cr.execute(
-        """
-        SELECT DISTINCT product_id
-        FROM pos_order_line
-    """
-    )
+    cr.execute("SELECT product_id FROM pos_order_line WHERE product_id IS NOT NULL GROUP BY product_id")
 
-    product_ids = [r[0] for r in cr.fetchall()]
-    products = util.env(cr)["product.product"].browse(product_ids)
-    product_names = [(p.display_name, p.id) for p in products]
-    cr.executemany(
-        """
-        UPDATE pos_order_line pl
-        SET full_product_name = %s
-        WHERE product_id = %s
-    """,
-        product_names,
+    ids = [r[0] for r in cr.fetchall()]
+    util.parallel_execute(
+        cr,
+        [
+            cr.mogrify("UPDATE pos_order_line SET full_product_name = %s WHERE product_id = %s", [p.display_name, p.id])
+            for p in util.iter_browse(util.env(cr)["product.product"], ids)
+        ],
     )
