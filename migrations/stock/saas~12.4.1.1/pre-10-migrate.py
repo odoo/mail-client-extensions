6ec18e534b94634528eaa56bb63eccbbd393a877
dmonzonis <mod@odoo.com>
2020-10-13 12:26:56 +0000

    [FIX] stock@saas~12.4: some inventory locations not being migrated
    In the upgrade to `saas~12.4`, in the model `stock.inventory`, the
    `Many2one` field `location_id` is replaced by the `Many2many` field
    `location_ids`.
    
    In the upgrade scripts, only locations from `stock.inventory` records
    which have state `draft` or `confirm`. However, some clients complain
    that the locations set for records in other states have disappeared.
    
    To fix this, all locations are migrated regardless of the state of the
    `stock.inventory` record.
    
    Request ID: 53088
    
    closes odoo/upgrade#1840
    
    Opw: 2323479
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/stock/saas~12.4.1.1/pre-10-migrate.py
+++ migrations/stock/saas~12.4.1.1/pre-10-migrate.py
@@ -9,47 +9,57 @@ def migrate(cr, version):
     cr.execute("DELETE FROM ir_config_parameter WHERE key='stock.use_propagation_minimum_delta'")
 
     # new inventories
-    util.create_m2m(cr, 'stock_inventory_stock_location_rel', 'stock_inventory', 'stock_location')
-    cr.execute("""
+    util.create_m2m(cr, "stock_inventory_stock_location_rel", "stock_inventory", "stock_location")
+    cr.execute(
+        """
         INSERT INTO stock_inventory_stock_location_rel (stock_inventory_id,stock_location_id)
             SELECT id, location_id
-              FROM stock_inventory WHERE state in ('draft', 'confirm')
-    """)
-    util.create_m2m(cr, 'product_product_stock_inventory_rel', 'stock_inventory', 'product_product')
-    cr.execute("""
+              FROM stock_inventory
+    """
+    )
+    util.create_m2m(cr, "product_product_stock_inventory_rel", "stock_inventory", "product_product")
+    cr.execute(
+        """
         INSERT INTO product_product_stock_inventory_rel (stock_inventory_id, product_product_id)
             SELECT id, product_id
               FROM stock_inventory
              WHERE state in ('draft', 'confirm')
                AND product_id IS NOT NULL
-    """)
+    """
+    )
 
     # convert categ_id into product_ids
-    cr.execute("""
+    cr.execute(
+        """
         INSERT INTO product_product_stock_inventory_rel (stock_inventory_id, product_product_id)
             SELECT si.id, pp.id FROM product_product pp
             INNER JOIN product_template pt ON pt.id = pp.product_tmpl_id
             INNER JOIN product_category pc ON pc.id = pt.categ_id
             INNER JOIN stock_inventory si ON si.category_id = pc.id
             WHERE si.category_id is not null AND si.state='confirm'
-    """)
+    """
+    )
     # convert lot_id into product_ids
-    cr.execute("""
+    cr.execute(
+        """
         INSERT INTO product_product_stock_inventory_rel (stock_inventory_id, product_product_id)
             SELECT si.id, pp.id FROM product_product pp
             INNER JOIN stock_production_lot spl ON spl.product_id = pp.id
             INNER JOIN stock_inventory si on si.lot_id = spl.id
             WHERE si.lot_id is not null AND si.state='confirm'
-    """)
+    """
+    )
     # convert package_id into product_ids
-    cr.execute("""
+    cr.execute(
+        """
         INSERT INTO product_product_stock_inventory_rel (stock_inventory_id, product_product_id)
             SELECT si.id, pp.id FROM product_product pp
             INNER JOIN stock_quant sq ON sq.product_id = pp.id
             INNER JOIN stock_quant_package sqp ON sqp.id = sq.package_id
             INNER JOIN stock_inventory si on si.package_id = sqp.id
             WHERE si.package_id is not null AND si.state='confirm'
-    """)
+    """
+    )
 
     util.remove_field(cr, "stock.inventory", "location_id")
     util.remove_field(cr, "stock.inventory", "product_id")
@@ -83,12 +93,14 @@ def migrate(cr, version):
 
     # fill company_id with the warehouse one on stock_picking_type
     util.create_column(cr, "stock_picking_type", "company_id", "int4")
-    cr.execute("""
+    cr.execute(
+        """
         UPDATE stock_picking_type spt
            SET company_id = sw.company_id
           FROM stock_warehouse sw
          WHERE spt.warehouse_id = sw.id
-    """)
+    """
+    )
     cr.execute(
         "SELECT 1 FROM res_groups_implied_rel WHERE gid=%s AND hid IN %s",
         [
@@ -103,11 +115,11 @@ def migrate(cr, version):
     if cr.rowcount:
         cr.execute("UPDATE stock_picking_type SET show_operations=true")
 
-    util.remove_record(cr, 'stock.action_inventory_line_tree')
-    util.remove_view(cr, 'stock.view_stock_move_nosuggest_operations')
-    util.remove_record(cr, 'stock.lot_open_quants')
-    util.remove_record(cr, 'stock.product_open_quants')
-    util.remove_view(cr, 'stock.view_production_lot_form_simple')
+    util.remove_record(cr, "stock.action_inventory_line_tree")
+    util.remove_view(cr, "stock.view_stock_move_nosuggest_operations")
+    util.remove_record(cr, "stock.lot_open_quants")
+    util.remove_record(cr, "stock.product_open_quants")
+    util.remove_view(cr, "stock.view_production_lot_form_simple")
 
-    util.remove_model(cr, 'stock.change.product.qty')
-    util.remove_view(cr, 'stock.view_change_product_quantity')
+    util.remove_model(cr, "stock.change.product.qty")
+    util.remove_view(cr, "stock.view_change_product_quantity")
