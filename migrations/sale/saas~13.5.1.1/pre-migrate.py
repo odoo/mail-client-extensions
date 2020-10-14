accd30337604f1a4c9e37c8f5ea1bb4594d5f128
Christophe Simonis <chs@odoo.com>
2020-10-14 14:20:08 +0200

    [FIX] sale@saas~13.5

--- migrations/sale/saas~13.5.1.1/pre-migrate.py
+++ migrations/sale/saas~13.5.1.1/pre-migrate.py
@@ -3,6 +3,9 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    eb = util.expand_braces
+    util.rename_xmlid(cr, *eb("sale.access_product_product_attribute_custom_value{,_sale_manager}"))
+
     util.remove_view(cr, "sale.attribute_tree_view")
     util.remove_view(cr, "sale.product_template_attribute_line_form")
     util.remove_view(cr, "sale.product_attribute_view_form")
