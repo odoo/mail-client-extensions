8869f6c12086421529ae711f4a65dd13c99d9d3b
Christophe Simonis <chs@odoo.com>
2020-10-14 17:20:48 +0200

    [FIX] sale_timesheet@saas~13.5

--- migrations/sale_timesheet/saas~13.5.1.0/pre-migrate.py
+++ migrations/sale_timesheet/saas~13.5.1.0/pre-migrate.py
@@ -11,6 +11,13 @@ def migrate(cr, version):
     util.remove_field(cr, "project.create.sale.order", "price_unit")
     util.remove_field(cr, "project.create.sale.order", "currency_id")
     util.remove_field(cr, "project.create.sale.order", "billable_type")
+    util.create_column(cr, "project_create_sale_order", "link_selection", "varchar")
+    util.create_column(cr, "project_create_sale_order", "sale_order_id", "int4")
+    util.create_column(cr, "project_create_sale_order_line", "sale_line_id", "int4")
+
+    util.create_column(cr, "project_task_create_sale_order", "link_selection", "varchar")
+    util.create_column(cr, "project_task_create_sale_order", "sale_order_id", "int4")
+    util.create_column(cr, "project_task_create_sale_order", "sale_line_id", "int4")
 
     util.create_column(cr, "project_project", "bill_type", "varchar")
     util.create_column(cr, "project_project", "pricing_type", "varchar")
@@ -38,8 +45,8 @@ def migrate(cr, version):
     util.remove_field(cr, "project.project", "billable_type")
 
     util.remove_field(cr, "project.task", "billable_type")
+    util.create_column(cr, "project_task", "non_allow_billable", "boolean")
     util.create_column(cr, "project_task", "timesheet_product_id", "int4")
+
     util.create_column(cr, "account_analytic_line", "non_allow_billable", "boolean")
     util.create_column(cr, "project_sale_line_employee_map", "timesheet_product_id", "int4")
-
-    util.create_column(cr, "project_task", "non_allow_billable", "boolean")
