87f830d56ebe036f6e1c7759f68d181d57e9824e
Christophe Simonis <chs@odoo.com>
2020-10-19 18:01:58 +0200

    [FIX] website_sale@saas~13.5

--- migrations/website_sale/saas~13.5.1.0/pre-migrate.py
+++ migrations/website_sale/saas~13.5.1.0/pre-migrate.py
@@ -5,3 +5,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "website_sale.header")
     util.rename_xmlid(cr, "website_sale.hide_cart_if_empty", "website_sale.header_hide_empty_cart_link")
+    util.remove_record(cr, "website_sale.action_abandoned_orders_ecommerce")
