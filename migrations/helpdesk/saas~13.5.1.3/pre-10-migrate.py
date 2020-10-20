f93faf24a589cb49eb4dbdfab9d7804f8c67a5d5
Christophe Simonis <chs@odoo.com>
2020-10-20 18:46:50 +0200

    [FIX] helpdesk@saas~13.5

--- migrations/helpdesk/saas~13.5.1.3/pre-10-migrate.py
+++ migrations/helpdesk/saas~13.5.1.3/pre-10-migrate.py
@@ -7,24 +7,24 @@ def migrate(cr, version):
 
     cr.execute(
         """
-        INSERT INTO helpdesk_sla_helpdesk_stage_rel
-               (helpdesk_sla_id, helpdesk_stage_id)
-        SELECT s.id, s.exclude_stage_id
-        FROM helpdesk_sla s
-        WHERE s.exclude_stage_id IS NOT NULL"""
+            INSERT INTO helpdesk_sla_helpdesk_stage_rel(helpdesk_sla_id, helpdesk_stage_id)
+                 SELECT id, exclude_stage_id
+                   FROM helpdesk_sla
+                  WHERE exclude_stage_id IS NOT NULL
+        """
     )
 
     util.remove_field(cr, "helpdesk.sla", "exclude_stage_id")
 
-    util.remove_view(cr, 'helpdesk.helpdesk_ticket_view_kanban_no_create')
+    util.remove_view(cr, "helpdesk.helpdesk_ticket_view_kanban_no_create")
 
-    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_main')
-    util.remove_record(cr, 'helpdesk.action_upcoming_sla_fail_tickets')
-    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_high_priorities')
-    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_urgent')
-    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_sla_high')
-    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_sla_urgent')
-    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_Archived')
-    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_slafailed')
-    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_dashboard_high_priority')
-    util.remove_record(cr, 'helpdesk.helpdesk_ticket_action_dashboard_urgent')
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_main")
+    util.remove_record(cr, "helpdesk.action_upcoming_sla_fail_tickets")
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_high_priorities")
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_urgent")
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_sla_high")
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_sla_urgent")
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_Archived")
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_slafailed")
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_dashboard_high_priority")
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_dashboard_urgent")
