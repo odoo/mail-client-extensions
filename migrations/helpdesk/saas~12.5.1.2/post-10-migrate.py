97c0599473b0522349a58758f441f9ca776fc1c1
dmonzonis <mod@odoo.com>
2020-10-21 14:59:27 +0000

    [FIX] helpdesk@saas~12.5: Recompute deadline before exceeded_days
    In the upgrade to `saas~12.5`, a recompute of the field `exceeded_days` of
    the model `helpdesk.sla.status` is executed. This model did not exist in
    `saas~12.3`. The compute of this field uses another field, `deadline`,
    which is also computed, but if it has not been computed before, it may
    contain null values, causing the computation of `exceeded_days` to fail.
    
    To fix this, the `deadline` field is also forced to recompute, before computing
    `exceeded_days`.
    
    Request ID: 54189
    
    closes odoo/upgrade#1865
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/helpdesk/saas~12.5.1.2/post-10-migrate.py
+++ migrations/helpdesk/saas~12.5.1.2/post-10-migrate.py
@@ -3,5 +3,4 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    util.recompute_fields(cr, 'helpdesk.sla.status', ['exceeded_days'])
-
+    util.recompute_fields(cr, "helpdesk.sla.status", ["deadline", "exceeded_days"])
