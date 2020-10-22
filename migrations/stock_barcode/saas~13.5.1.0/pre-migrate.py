f167815f5312c93b0c6db375030e5101bfd05ebc
Christophe Simonis <chs@odoo.com>
2020-10-22 17:07:00 +0200

    [FIX] stock_barcode@saas~13.5

--- migrations/stock_barcode/saas~13.5.1.0/pre-migrate.py
+++ migrations/stock_barcode/saas~13.5.1.0/pre-migrate.py
@@ -3,4 +3,5 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.create_column(cr, "res_company", "keyboard_layout", "character varying", default="qwerty")
+    util.create_column(cr, "res_company", "keyboard_layout", "varchar", default="qwerty")
+    util.create_column(cr, "res_config_settings", "group_barcode_keyboard_shortcuts", "boolean")
