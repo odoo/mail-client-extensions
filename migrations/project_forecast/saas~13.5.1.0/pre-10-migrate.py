2227e2051ffdcb5f9a463eec900dc61ba1fdbda7
Christophe Simonis <chs@odoo.com>
2020-10-22 15:52:46 +0200

    [FIX] project_forecast@saas~13.5

--- migrations/project_forecast/saas~13.5.1.0/pre-10-migrate.py
+++ migrations/project_forecast/saas~13.5.1.0/pre-10-migrate.py
@@ -3,20 +3,44 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.remove_view(cr, "project_forecast.project_forecast_view_gantt")
-    util.remove_view(cr, "project_forecast.project_forecast_view_pivot")
-    util.remove_view(cr, "project_forecast.project_forecast_view_graph")
-    util.remove_view(cr, "project_forecast.project_forecast_view_grid")
-    util.remove_record(cr, "project_forecast.project_forecast_action_analysis")
-    util.remove_record(cr, "project_forecast.project_forecast_action_analysis_pivot")
-    util.remove_record(cr, "project_forecast.project_forecast_action_analysis_graph")
-    util.remove_record(cr, "project_forecast.project_forecast_action_analysis_gantt")
-    util.remove_menus(cr, [
-        util.ref(cr, "project_forecast.project_forecast_report_activities"),
-        util.ref(cr, "project_forecast.project_forecast_group_by_user"),
-        util.ref(cr, "project_forecast.project_forecast_group_by_project"),
-        util.ref(cr, "project_forecast.project_forecast_menu")])
-    util.remove_record(cr, "project_forecast.project_forecast_action_by_user")
-    util.remove_record(cr, "project_forecast.project_forecast_action_view_by_user_gantt")
-    util.remove_record(cr, "project_forecast.project_forecast_action_by_project")
-    util.remove_record(cr, "project_forecast.project_forecast_action_view_by_project_gantt")
+    util.create_column(cr, "planning_slot", "parent_id", "int4")
+    cr.execute(
+        """
+             UPDATE planning_slot s
+                SET parent_id = t.parent_id
+               FROM project_task t
+              WHERE t.id = s.task_id
+        """
+    )
+    util.create_column(cr, "res_config_settings", "group_project_forecast_display_allocate_time", "boolean")
+
+    for view in {"gantt", "pivot", "graph", "grid"}:
+        util.remove_view(cr, f"project_forecast.project_forecast_view_{view}")
+
+    util.remove_record(cr, "project_forecast.planning_rule_project_adminis_published")
+
+    actions = """
+        analysis_pivot
+        analysis_graph
+        analysis_gantt
+        analysis
+
+        view_by_user_gantt
+        by_user
+
+        view_by_project_gantt
+        by_project
+    """
+
+    for action in util.splitlines(actions):
+        util.remove_record(cr, f"project_forecast.project_forecast_action_{action}")
+
+    util.remove_menus(
+        cr,
+        [
+            util.ref(cr, "project_forecast.project_forecast_report_activities"),
+            util.ref(cr, "project_forecast.project_forecast_group_by_user"),
+            util.ref(cr, "project_forecast.project_forecast_group_by_project"),
+            util.ref(cr, "project_forecast.project_forecast_menu"),
+        ],
+    )
