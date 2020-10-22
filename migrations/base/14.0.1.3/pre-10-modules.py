a6a04ed3ca75e06c862a0064be9d109dce39f6c7
Laurent Smet <las@odoo.com>
2020-10-22 06:43:28 +0000

    [ADD] base@14.0: Add 'payment_fix_register_token' module
    related community PR: https://github.com/odoo/odoo/pull/60289
    
    closes odoo/upgrade#1879
    
    Task: 2352628
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/14.0.1.3/pre-10-modules.py
+++ migrations/base/14.0.1.3/pre-10-modules.py
@@ -7,3 +7,4 @@ def migrate(cr, version):
     util.new_module(cr, "adyen_platforms", deps={"mail", "web"})
     util.new_module(cr, "payment_odoo_by_adyen", deps={"payment", "adyen_platforms"})
     util.module_deps_diff(cr, "pos_adyen", plus={"adyen_platforms"})
+    util.new_module(cr, 'payment_fix_register_token', deps={'payment'}, auto_install=True)
--- migrations/base/saas~14.1.1.3/pre-10-modules.py
+++ migrations/base/saas~14.1.1.3/pre-10-modules.py
@@ -5,6 +5,7 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.new_module(cr, "sale_sms", deps={"sale", "sms"}, auto_install=True)
+    util.merge_module(cr, 'payment_fix_register_token', 'payment')
 
     if util.has_enterprise():
         util.module_deps_diff(cr, "l10n_be_hr_payroll", plus={"hr_payroll_holidays"})
