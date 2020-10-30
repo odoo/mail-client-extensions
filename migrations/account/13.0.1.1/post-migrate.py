b5909643ccd7dfaff591359f6ac0c755ac5963c9
dmonzonis <mod@odoo.com>
2020-10-30 12:27:54 +0000

    [FIX] account@13.0: update mail aliases linked to journals
    When upgrading to `13.0`, several customers have complained that their
    mail aliases associated to accounting journals no longer work. This is
    because in `13.0`, the `alias_defaults` needs to have the `journal_id`
    of the journal linked to the alias.
    
    Until now, the support team has been telling customers to manually
    modify these defaults to fix this issue when it has been arisen.
    However, it is something that should be taken care of during the
    upgrade.
    
    To fix this, aliases linked to journals are updated at the end of the
    upgrade to `13.0`.
    
    closes odoo/upgrade#1886
    
    Opw: 2368825
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/account/13.0.1.1/post-migrate.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    Journal = util.env(cr)["account.journal"]
+    cr.execute(
+        """
+        SELECT j.id, a.alias_name
+          FROM account_journal j
+          JOIN mail_alias a ON j.alias_id = a.id
+    """
+    )
+    for journal_id, alias_name in cr.fetchall():
+        # We want to update the alias but leave the name intact
+        Journal.browse(journal_id)._update_mail_alias({"alias_name": alias_name})
