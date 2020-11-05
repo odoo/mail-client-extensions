dc013447da8556e654b9b3fb7e6aca9deedd296f
Julian Roussieau <jur@odoo.com>
2020-11-05 16:18:18 +0000

    [FIX] base@14.0: foreign key in Many2many
    Before odoo 14. Foreign keys constraints were not checked on many2many relationship tables.
    Since version 14, we check that the relationship tables do indeed have a foreign keys constraint towards each model. If not, the constraint is added.
    In some tables, values already present can violate the constraint that will be added.
    
    The solution is to run `fixup_m2m` on each relation table. We will therefore delete the id's which have a reference to non-existent records.
    
    closes odoo/upgrade#1940
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

---
+++ migrations/base/14.0.1.3/pre-many2many.py
@@ -0,0 +1,20 @@
+from odoo.fields import Many2many
+
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+
+    original_update_db = Many2many.update_db
+
+    def update_db(self, model, columns):
+        comodel = model.env[self.comodel_name]
+        if (
+            util.table_exists(cr, self.relation)
+            and len(util.get_columns(cr, self.relation, ignore=(self.column1, self.column2))) == 0
+        ):
+            util.fixup_m2m(cr, self.relation, model._table, comodel._table, self.column1, self.column2)
+
+        original_update_db(self, model, columns)
+
+    Many2many.update_db = update_db
