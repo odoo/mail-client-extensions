440bbc1571d3e2d52d3e2da2975d6fbeda2346c0
Christophe Simonis <chs@odoo.com>
2020-11-09 16:16:20 +0000

    [FIX] stock@9.saas~11: do not use `util.disabled_index_on`
    Run queries in //

--- migrations/stock/9.saas~11.1.0/pre-10-models.py
+++ migrations/stock/9.saas~11.1.0/pre-10-models.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 from openerp.addons.base.maintenance.migrations import util
 
+
 def migrate(cr, version):
     # new field `ordered_qty` added on stock.move and stock.pack.operation
     # They aimed to store the initially ordered quantity in case the move/operation qty change
     # This can be invalid but we will initiate these columns with the currently processed qty
 
-    with util.disabled_index_on(cr, 'stock_move'):
-        util.create_column(cr, 'stock_move', 'ordered_qty', 'numeric')
-        cr.execute("UPDATE stock_move SET ordered_qty = product_uom_qty")
+    util.create_column(cr, "stock_move", "ordered_qty", "numeric")
+    query = "UPDATE stock_move SET ordered_qty = product_uom_qty"
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="stock_move"))
 
-    with util.disabled_index_on(cr, 'stock_pack_operation'):
-        util.create_column(cr, 'stock_pack_operation', 'ordered_qty', 'numeric')
-        cr.execute("UPDATE stock_pack_operation SET ordered_qty = product_qty")
+    util.create_column(cr, "stock_pack_operation", "ordered_qty", "numeric")
+    query = "UPDATE stock_pack_operation SET ordered_qty = product_qty"
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="stock_pack_operation"))
 
     # beside the logic would say when should go from `auto` (Automatic)
     # to `transparent` (Automatic No Step Added), the code in `_apply` function only treat
@@ -20,7 +21,7 @@ def migrate(cr, version):
     # behaving the same way...
     cr.execute("UPDATE stock_location_path SET auto='manual' WHERE auto='auto'")
 
-    util.rename_field(cr, 'stock.pack.operation', 'processed_boolean', 'is_done')
+    util.rename_field(cr, "stock.pack.operation", "processed_boolean", "is_done")
 
     # related fields without type set -> default to float, which worked.
     cr.execute("ALTER TABLE procurement_rule ALTER COLUMN route_sequence TYPE integer")
