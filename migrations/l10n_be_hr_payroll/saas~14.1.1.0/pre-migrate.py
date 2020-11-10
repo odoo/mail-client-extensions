323ec75ad12b06197ec3d9a134df1c09630a869c
Xavier BOL (xbo) <xbo@odoo.com>
2020-11-10 13:08:05 +0000

    [FIX] l10n_be_hr_payroll@saas~14.1: update work_time_rate in hr.contract
    The work time rate field in hr.contract has been changed to be the same
    than the one in the resource.calendar.
    
    This migration script updates the records containing in the hr_contract
    table to update the value of work_time_rate from varchar to real.
    
    This commit also fixes previous commit issue where real has been used in
    place of float8
    
    See odoo/odoo#61512
    See odoo/enterprise#14647
    
    closes odoo/upgrade#1939
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/l10n_be_hr_payroll/saas~14.1.1.0/pre-migrate.py
+++ migrations/l10n_be_hr_payroll/saas~14.1.1.0/pre-migrate.py
@@ -7,7 +7,7 @@ def migrate(cr, version):
 
     util.remove_view(cr, "l10n_be_hr_payroll.hr_payslip_run_tree")
 
-    util.create_column(cr, "hr_leave_allocation", "max_leaves_allocated", "float", default=20)
+    util.create_column(cr, "hr_leave_allocation", "max_leaves_allocated", "float8", default=20)
 
     util.create_column(cr, "hr_payroll_alloc_paid_leave", "year", "integer")
     util.remove_field(cr, "hr.payroll.alloc.paid.leave", "date_start")
@@ -16,12 +16,25 @@ def migrate(cr, version):
 
     util.remove_field(cr, "hr.payroll.alloc.employee", "worked_days")
     util.remove_field(cr, "hr.payroll.alloc.employee", "paid_time_off")
-    util.create_column(cr, "hr_payroll_alloc_employee", "paid_time_off", "real")
-    util.create_column(cr, "hr_payroll_alloc_employee", "paid_time_off_to_allocate", "real")
+    util.create_column(cr, "hr_payroll_alloc_employee", "paid_time_off", "float8")
+    util.create_column(cr, "hr_payroll_alloc_employee", "paid_time_off_to_allocate", "float8")
 
     util.remove_field(cr, "l10n_be.hr.payroll.credit.time.wizard", "work_time")
-    util.create_column(cr, "l10n_be_hr_payroll_credit_time_wizard", "time_off_allocation", "real")
-    util.create_column(cr, "l10n_be_hr_payroll_credit_time_wizard", "remaining_allocated_time_off", "real")
+    util.create_column(cr, "l10n_be_hr_payroll_credit_time_wizard", "time_off_allocation", "float8")
+    util.create_column(cr, "l10n_be_hr_payroll_credit_time_wizard", "remaining_allocated_time_off", "float8")
 
-    util.create_column(cr, "l10n_be_hr_payroll_exit_credit_time_wizard", "time_off_allocation", "real")
-    util.create_column(cr, "l10n_be_hr_payroll_exit_credit_time_wizard", "remaining_allocated_time_off", "real")
+    util.create_column(cr, "l10n_be_hr_payroll_exit_credit_time_wizard", "time_off_allocation", "float8")
+    util.create_column(cr, "l10n_be_hr_payroll_exit_credit_time_wizard", "remaining_allocated_time_off", "float8")
+
+    util.drop_depending_views(cr, "hr_contract", "work_time_rate")
+    cr.execute("ALTER TABLE hr_contract ALTER COLUMN work_time_rate TYPE float8 USING work_time_rate::float8")
+    # Drop selection values associated with hr.contract.work_time_rate
+    cr.execute("""
+        DELETE
+          FROM ir_model_fields_selection
+         WHERE field_id = (
+            SELECT id
+              FROM ir_model_fields
+             WHERE name = 'work_time_rate' AND model = 'hr.contract'
+        )
+    """)
