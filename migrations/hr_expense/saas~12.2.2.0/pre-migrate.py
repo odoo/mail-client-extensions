7c0090d4a1c6e817fff12e5831aec937f8c7378a
Denis Ledoux <dle@odoo.com>
2020-11-17 18:03:55 +0000

    [FIX] hr_expense@saas~12.2: data converted to demo data
    `product_product_fixed_cost` has been converted
    from a data to a demo data in
    odoo/odoo@4ba8f51d87be394988e6e2889cf03f351afea1a0
    
    If not used in the database,
    it can be deleted,
    otherwise it needs to be kept in case `noupdate` is not set.
    
    Request 1469 (beta platform)
    
    closes odoo/upgrade#1943
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/hr_expense/saas~12.2.2.0/pre-migrate.py
+++ migrations/hr_expense/saas~12.2.2.0/pre-migrate.py
@@ -6,3 +6,4 @@ def migrate(cr, version):
     # become default...
     util.update_record_from_xml(cr, "hr_expense.mt_expense_paid")
     # XXX should all followers follow this type?
+    util.delete_unused(cr, "hr_expense.product_product_fixed_cost")
