ca8401ef7b3c6c2585b3bcae44e873c0abd7ec62
Thibault Delavallée <tde@odoo.com>
2020-11-23 10:30:54 +0000

    [REF] website_event_exhibitor@saas~14.1: improve upgrade of sponsors to (online) exhibitors
    PURPOSE
    
    Improve sponsor management, especially when dealing with publication at
    sponsor level, exhibitor level, or exhibitor using chat (jitsi).
    
    SPECIFICATIONS
    
    Introduce a new category field containing sponsor, exhibitor or online
    exhibitor. Meaning is
    
      * sponsor: display as a sponsor (sponsor cards at the bottom of events).
        No publish management is done at sponsor level: a sponsor is always
        displayed in the sponsor widget;
      * exhibitor: display as a sponsor in sponsor cards. An exhibitor is also
        displayed in /exhibitors page and allow publish management. Also allow
        to edit description as it is used in front-end page view of exhibitor;
      * online exhibitor: same as exhibitor and allow to configure jitsi (chat)
        capabilities. It also activates display of jitsi in frontend;
    
    LINKS
    
    Task ID-2326433
    COM PR odoo/odoo#61781
    ENT pr odoo/enterprise#14752
    UPG PR odoo/upgrade#1931
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website_event_exhibitor/saas~14.1.1.1/pre-migrate.py
+++ migrations/website_event_exhibitor/saas~14.1.1.1/pre-migrate.py
@@ -9,3 +9,20 @@ def migrate(cr, version):
     util.move_model(cr, "event.sponsor.type", "website_event_track", "website_event_exhibitor", move_data=True)
     util.move_field_to_module(cr, "event.event", "sponsor_ids", "website_event_track", "website_event_exhibitor")
     util.move_field_to_module(cr, "event.event", "sponsor_count", "website_event_track", "website_event_exhibitor")
+
+    # migrate from is_exhibitor (defined in website_event_track_exhibitor)
+    # to exhibitor_type selection. Do it only if column exists aka if module
+    # was installed before migrating (as migrating could install website_event_exhibitor
+    # if website_event_track was installed).
+    util.create_column(cr, "event_sponsor", "exhibitor_type", "varchar", default="sponsor")
+    if util.column_exists(cr, "event_sponsor", "is_exhibitor"):
+        cr.execute(
+            """
+            UPDATE event_sponsor
+               SET exhibitor_type = CASE WHEN chat_room_id IS NOT NULL THEN 'online'
+                                         ELSE 'exhibitor'
+                                    END
+             WHERE is_exhibitor = true
+        """
+        )
+        util.remove_field(cr, "event.sponsor", "is_exhibitor")
