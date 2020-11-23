9f2f2e53bfcbd7770eb9766957b2e343b9d3e079
Denis Ledoux <dle@odoo.com>
2020-11-23 17:38:53 +0000

    [FIX] sign@saas~13.4: deleted demo records if unused
    Data are converted to demo data.
    For that reason,
    f57f5ac0f5971f1c91faa5bd7fbb1604cf1a5b72
    aims to remove them.
    
    However, if these templates are actually used in the database,
    they should not be removed.
    
    ```
      File "/tmp/tmp7xb3288e/migrations/sign/saas~13.4.1.0/pre-rm-data.py", line 15, in migrate
        util.remove_record(cr, f"sign.template_sign_{id_}")
      File "/tmp/tmp7xb3288e/migrations/util.py", line 782, in remove_record
        cr.execute('DELETE FROM "%s" WHERE id=%%s' % table, (res_id,))
      File "<decorator-gen-3>", line 2, in execute
      File "/home/odoo/src/odoo/14.0/odoo/sql_db.py", line 101, in check
        return f(self, *args, **kwargs)
      File "/home/odoo/src/odoo/14.0/odoo/sql_db.py", line 298, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.ForeignKeyViolation: update or delete on table "sign_template" violates foreign key constraint "sign_request_template_id_fkey" on table "sign_request"
    DETAIL:  Key (id)=(2) is still referenced from table "sign_request".
    ```
    
    `attachment_sign_*` records are deleted when their parent `sign_template` is,
    rather than using `delete_unused` on them as well
    because `delete_unused` ignore `ondelete="cascade"` FKs,
    and as `attachment_id` of `sign.template` is `ondelete="cascade"`,
    it's not detected as used with `delete_unused`.
    
    upg-765
    upg-917
    upg-1068
    
    closes odoo/upgrade#1968
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/sign/saas~13.4.1.0/pre-rm-data.py
+++ migrations/sign/saas~13.4.1.0/pre-rm-data.py
@@ -10,7 +10,10 @@ def migrate(cr, version):
     [demo] = cr.fetchone()
     if not demo:
         for item in [1, 2, 3, 4, 5, 8, 9, 20, 21, 22, 23, 24, 25]:
-            util.remove_record(cr, f"sign.sign_item_{item}")
+            util.delete_unused(cr, f"sign.sign_item_{item}")
         for id_ in [1, 3]:
-            util.remove_record(cr, f"sign.template_sign_{id_}")
-            util.remove_record(cr, f"sign.attachment_sign_{id_}")
+            util.delete_unused(cr, f"sign.template_sign_{id_}")
+            if util.ref(cr, f"sign.template_sign_{id_}"):
+                util.force_noupdate(cr, f"sign.attachment_sign_{id_}", True)
+            else:
+                util.remove_record(cr, f"sign.attachment_sign_{id_}")
