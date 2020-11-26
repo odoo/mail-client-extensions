1f3869c3e808f6aebc41395667fd1e728e09990f
Thibault Libioulle <tle@odoo.com>
2020-11-26 17:31:43 +0000

    [IMP] project@saas~14.3 : remove fields regarding sub-tasks
    PR Community: odoo/odoo#62577
    PR Enterprise: odoo/enterprise#15043
    PR Upgrade: odoo/upgrade#1984
    
    task-2388034
    Part of task-2379043
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/project/saas~14.3.1.2/pre-migrate.py
+++ migrations/project/saas~14.3.1.2/pre-migrate.py
@@ -5,3 +5,13 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.if_unchanged(cr, "project.mail_channel_project_task", util.remove_record)
+
+    util.create_column(cr, "project_task", "display_project_id", "int4")
+    # compute field
+    # here we set the display_project_id for all the previous tasks, including subtasks to do not change
+    # behaviour in various views for historic data's.
+    query = "UPDATE project_task SET display_project_id = project_id"
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="project_task"))
+
+    util.remove_field(cr, "project.task", "subtask_project_id")
+    util.remove_field(cr, "project.project", "subtask_project_id")
