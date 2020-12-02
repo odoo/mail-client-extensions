5785ecd3890643a8fc282422639bcb0909df0673
Rémy Voet (ryv) <ryv@odoo.com>
2020-12-02 16:17:13 +0000

    [IMP] {stock,mrp}@saas~14.2: clean stock,mrp
    - Remove all operation without bom_id because bom_id become required
    - Remove column of fields that became related no-store.
    - Remove unused `sequence_tracking`
    - Set picking_type_id of stock_move to be consistent with
    the new compute store.
    
    odoo/odoo#61027
    task-2373638
    
    closes odoo/upgrade#1998
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/mrp/saas~14.2.2.0/pre-migrate.py
+++ migrations/mrp/saas~14.2.2.0/pre-migrate.py
@@ -4,3 +4,17 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, 'mrp.view_production_gantt')
+
+    cr.execute("DELETE FROM mrp_routing_workcenter WHERE bom_id IS NULL")
+
+    util.remove_column(cr, "mrp_routing_workcenter", "company_id")
+    util.remove_column(cr, "mrp_workorder", "consumption")
+
+    cr.execute(
+        """
+        UPDATE stock_move AS sm
+           SET picking_type_id = mp.picking_type_id
+          FROM mrp_production AS mp
+         WHERE COALESCE(sm.raw_material_production_id, sm.production_id) = mp.id
+        """
+    )
--- migrations/stock/saas~14.2.1.1/pre-migrate.py
+++ migrations/stock/saas~14.2.1.1/pre-migrate.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from odoo.upgrade import util
 
 
 def migrate(cr, version):
@@ -9,5 +10,15 @@ def migrate(cr, version):
     UPDATE stock_quant
        SET in_date = (SELECT coalesce(min(in_date) - interval '1s', now() at time zone 'utc') FROM stock_quant)
      WHERE in_date IS NULL
+        """
+    )
+
+    util.delete_unused(cr, "stock.sequence_tracking")
+    cr.execute(
+        """
+        UPDATE stock_move AS sm
+           SET picking_type_id = sp.picking_type_id
+          FROM stock_picking AS sp
+         WHERE sp.id = sm.picking_id
         """
     )
