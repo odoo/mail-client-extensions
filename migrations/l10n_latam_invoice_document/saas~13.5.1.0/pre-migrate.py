3506f6f5e56a07ff1ead2cd4655879fd863b3fc3
Christophe Simonis <chs@odoo.com>
2020-12-04 11:38:25 +0100

    [FIX] l10n_cl/14.0,l10n_latam_invoice_document/saas~13.5: remove views

--- migrations/l10n_latam_invoice_document/saas~13.5.1.0/pre-migrate.py
+++ migrations/l10n_latam_invoice_document/saas~13.5.1.0/pre-migrate.py
@@ -6,3 +6,4 @@ def migrate(cr, version):
     util.remove_field(cr, "account.journal", "l10n_latam_country_code")
     util.remove_view(cr, "l10n_latam_invoice_document.report_invoice")
     util.remove_view(cr, "l10n_latam_invoice_document.report_invoice_with_payments")
+    util.remove_view(cr, "l10n_latam_invoice_document.report_invoice_document")
