c2aa8c56aaba08d4191f446f1a8b0723d7ffbf4f
Christophe Simonis <chs@odoo.com>
2020-12-04 11:24:20 +0100

    [FIX] website/saas~13.2: hide some view deletion warnings

--- migrations/website/saas~13.2.1.0/pre-migrate.py
+++ migrations/website/saas~13.2.1.0/pre-migrate.py
@@ -9,13 +9,11 @@ def migrate(cr, version):
     util.create_column(cr, "website", "cookies_bar", "boolean")
     util.create_column(cr, "website", "robots_txt", "text")
 
-    gone = """
-        s_btn
-        theme_customize
-
-        website_visitor_view_graph
-    """
-    for view in util.splitlines(gone):
-        util.remove_view(cr, f"website.{view}")
+    util.remove_view(cr, "website.s_btn")
+    util.remove_view(cr, "website.website_visitor_view_graph")
+    # The module `test_themes` create a bunch of inherited view without xmlid.
+    # We ignore the warnings if the module is installed.
+    # Tant pis for the customers that have this test module installed.
+    util.remove_view(cr, "website.theme_customize", silent=util.module_installed(cr, "test_themes"))
 
     util.remove_record(cr, "website.theme_install_todo_action")
