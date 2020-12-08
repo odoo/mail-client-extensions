ef0224d77a8c9a6e474cbff0ee519e1f824c95d2
Christophe Simonis <chs@odoo.com>
2020-12-08 16:25:21 +0000

    [FIX] base/14.0: update deps of `l10n_be_edi`
    closes odoo/upgrade#2012
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/14.0.1.3/pre-10-modules.py
+++ migrations/base/14.0.1.3/pre-10-modules.py
@@ -8,6 +8,9 @@ def migrate(cr, version):
     util.module_deps_diff(cr, "pos_adyen", plus={"adyen_platforms"})
     util.new_module(cr, "payment_fix_register_token", deps={"payment"}, auto_install=True)
 
+    # deps chamged by odoo/odoo@59d16513a019d52dd090e09c09be4675aa868baf (odoo/odoo#62730)
+    util.module_deps_diff(cr, "l10n_be_edi", plus={"account_edi_ubl"}, minus={"account_edi"})
+
     if util.has_enterprise():
         util.new_module(cr, "account_reports_tax", deps={"account_reports"}, auto_install=True)
 
