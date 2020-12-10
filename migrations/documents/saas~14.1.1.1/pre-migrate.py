2a0f1795fbc370d85d51c2f0500baa052887b908
Christophe Simonis <chs@odoo.com>
2020-12-10 14:45:17 +0100

    [FIX] documents/saas~14.1: use latest version number

--- migrations/documents/saas~14.1.1.1/pre-migrate.py
+++ migrations/documents/saas~14.1.1.1/pre-migrate.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.rename_xmlid(cr, "documents.documents_invoice_png", "documents.documents_mail_png")
+
     util.force_noupdate(cr, "documents.mail_template_document_request", noupdate=True)
