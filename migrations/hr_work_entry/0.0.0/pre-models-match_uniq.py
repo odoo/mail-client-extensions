fe0759e4aec622552e0cff7777965876d6c9d077
Nicolas Seinlet <nicolas@seinlet.com>
2020-12-10 11:52:38 +0100

    [ADD] hr_work_entry@0.0.0

---
+++ migrations/hr_work_entry/0.0.0/pre-models-match_uniq.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+
+import odoo.addons.hr_work_entry.models.hr_work_entry as _ignored  # noqa
+
+
+def migrate(cr, version):
+    pass
+
+
+class WorkEntryType(models.Model):
+    _inherit = "hr.work.entry.type"
+    _module = "hr_work_entry"
+    _match_uniq = True
