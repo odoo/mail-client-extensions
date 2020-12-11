2cb6a99158f46af174ccac7c9f20f742dce3959e
Christophe Simonis <chs@odoo.com>
2020-12-11 17:49:58 +0100

    [IMP] fleet/saas~14.1: update both `plan_to_change_*` columns at once

--- migrations/fleet/saas~14.1.0.1/pre-migrate.py
+++ migrations/fleet/saas~14.1.0.1/pre-migrate.py
@@ -8,43 +8,48 @@ def migrate(cr, version):
     util.create_column(cr, "fleet_vehicle", "frame_type", "varchar")
     util.create_column(cr, "fleet_vehicle", "electric_assistance", "bool")
     util.create_column(cr, "fleet_vehicle", "frame_size", "float8")
-    util.remove_field(cr, 'res.config.settings', 'module_fleet_account')
+    util.remove_field(cr, "res.config.settings", "module_fleet_account")
 
-    util.parallel_execute(cr, util.explode_query(cr, "UPDATE res_partner SET plan_to_change_car=False WHERE plan_to_change_car=True"))
-    cr.execute("UPDATE fleet_vehicle SET plan_to_change_car=False WHERE plan_to_change_car=False")
+    # recompute the fields plan_to_change_{bike,car}
+    query = "UPDATE res_partner SET plan_to_change_car = false WHERE plan_to_change_car = true"
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="res_partner"))
+    cr.execute("UPDATE fleet_vehicle SET plan_to_change_car = false WHERE plan_to_change_car = true")
 
-    cr.execute("SELECT res_id FROM ir_model_data WHERE name='fleet_vehicle_state_waiting_list'")
-    row = cr.fetchone()
-    res_id = row[0] if row else -1
+    state_filter = "v.state_id IS NULL"
+    waiting_list = util.ref(cr, "fleet.fleet_vehicle_state_waiting_list")
+    if waiting_list:
+        # as it's a demo data, I expect this branch will only be taken by CI checks
+        state_filter = cr.mogrify(f"({state_filter} OR v.state_id = %s)", [waiting_list]).decode()
 
-    vehicle_field = [("bike", "plan_to_change_bike"), ("car", "plan_to_change_car")]
-    for (vehicule_type, field_to_update) in vehicle_field:
-        cr.execute(
-            f"""
-            WITH f AS (
-                SELECT future_driver_id, vehicle_type
-                FROM fleet_vehicle
-                JOIN fleet_vehicle_model ON fleet_vehicle.model_id=fleet_vehicle_model.id
-                WHERE future_driver_id is not null AND vehicle_type=%s AND (state_id IS NULL OR state_id <> %s))
-            UPDATE res_partner
-            SET {field_to_update}=True
-            FROM f
-            WHERE res_partner.id=f.future_driver_id;
-        """,
-            (vehicule_type, res_id),
+    cte = f"""\
+        cte AS (
+            SELECT v.future_driver_id, array_agg(m.vehicle_type) as types
+              FROM fleet_vehicle v
+              JOIN fleet_vehicle_model m ON v.model_id = m.id
+             WHERE v.future_driver_id IS NOT NULL
+               AND vehicle_type IN ('bike', 'car')
+               AND {state_filter}
+          GROUP BY v.future_driver_id
         )
+    """
 
-        cr.execute(
-            f"""
-            WITH f AS (
-                SELECT future_driver_id, vehicle_type
-                FROM fleet_vehicle
-                JOIN fleet_vehicle_model ON fleet_vehicle.model_id=fleet_vehicle_model.id
-                WHERE future_driver_id is not null AND vehicle_type=%s AND (state_id IS NULL OR state_id <> %s))
-            UPDATE fleet_vehicle
-            SET {field_to_update}=True
-            FROM f
-            WHERE fleet_vehicle.driver_id=f.future_driver_id;
-        """,
-            (vehicule_type, res_id),
-        )
+    query = f"""
+          WITH {cte}
+        UPDATE res_partner p
+           SET plan_to_change_car = cte.types @> '{{car}}',
+               plan_to_change_bike = cte.types @> '{{bike}}'
+          FROM cte
+         WHERE cte.future_driver_id = p.id
+    """
+    # XXX explode query?
+    cr.execute(query)
+
+    query = f"""
+          WITH {cte}
+        UPDATE fleet_vehicle v
+           SET plan_to_change_car = cte.types @> '{{car}}',
+               plan_to_change_bike = cte.types @> '{{bike}}'
+          FROM cte
+         WHERE cte.future_driver_id = v.driver_id
+    """
+    cr.execute(query)
