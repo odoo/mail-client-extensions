e7f92d061c2cae94c2608a0188691303ba19d296
Christophe Simonis <chs@odoo.com>
2020-12-14 18:27:58 +0100

    [FIX] pad/saas~14.1: better message in case on multiple config

--- migrations/pad/saas~14.1.2.0/pre-10-migrate.py
+++ migrations/pad/saas~14.1.2.0/pre-10-migrate.py
@@ -3,11 +3,26 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    cr.execute("SELECT DISTINCT pad_server, pad_key FROM res_company WHERE pad_server IS NOT NULL")
-    if cr.rowcount > 1:
-        util.add_to_migration_reports("Only one pad server is supported", "Pad")
-    elif cr.rowcount == 1:
-        pad_server, pad_key = cr.fetchone()
+    cr.execute(
+        """
+        SELECT (array_agg(name ORDER BY id))[1] company_name, pad_server, pad_key
+          FROM res_company
+         WHERE pad_server IS NOT NULL
+      GROUP BY pad_server, pad_key
+      ORDER BY min(id)
+      """
+    )
+    rc = cr.rowcount
+    if rc:
+        company_name, pad_server, pad_key = cr.fetchone()
+
+        if rc > 1:
+            util.add_to_migration_reports(
+                "Using different pad configurations across companies is no more supported. "
+                f"Only the configuration of {company_name!r} has been kept.",
+                "Pad",
+            )
+
         ICP = util.env(cr)["ir.config_parameter"]
         ICP.set_param("pad.pad_server", pad_server)
         ICP.set_param("pad.pad_key", pad_key)
