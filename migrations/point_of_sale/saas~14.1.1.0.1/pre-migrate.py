0090f37466dbeabed1ddb960d814c9590ad1e9e3
Christophe Simonis <chs@odoo.com>
2020-12-14 18:36:19 +0100

    [FIX] point_of_sale/saas~14.1

--- migrations/point_of_sale/saas~14.1.1.0.1/pre-migrate.py
+++ migrations/point_of_sale/saas~14.1.1.0.1/pre-migrate.py
@@ -4,8 +4,10 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.create_column(cr, "pos_config", "iface_customer_facing_display_local", "boolean")
     util.rename_field(cr, "pos.config", "iface_customer_facing_display", "iface_customer_facing_display_via_proxy")
     util.remove_field(cr, "pos.config", "customer_facing_display_html")
-    util.remove_view(cr, "point_of_sale.customer_facing_display_html")
-    util.remove_field(cr, "account.bank.statement.line", "pos_statement_id")
     util.remove_field(cr, "pos.config", "iface_vkeyboard")
+    util.remove_view(cr, "point_of_sale.customer_facing_display_html")
+
+    util.remove_field(cr, "account.bank.statement.line", "pos_statement_id")
