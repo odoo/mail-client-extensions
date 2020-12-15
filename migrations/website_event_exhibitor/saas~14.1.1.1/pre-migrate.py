7ff994e0c316bd5e6285480cf400a50306d085b1
Christophe Simonis <chs@odoo.com>
2020-12-15 15:46:59 +0100

    [FIX] website_event_{exhibitor,track}/saas~14.1: move more stuff

--- migrations/website_event_exhibitor/saas~14.1.1.1/pre-migrate.py
+++ migrations/website_event_exhibitor/saas~14.1.1.1/pre-migrate.py
@@ -4,9 +4,16 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    eb = util.expand_braces
     # move sponsor model and fields from _track to _exhibitor
     util.move_model(cr, "event.sponsor", "website_event_track", "website_event_exhibitor", move_data=True)
+    for suffix in {"manager", "public"}:
+        util.rename_xmlid(cr, *eb(f"website_event_exhibitor.access_event_{{track_,}}sponsor_{suffix}"))
+
     util.move_model(cr, "event.sponsor.type", "website_event_track", "website_event_exhibitor", move_data=True)
+    for suffix in {"manager", "public"}:
+        util.rename_xmlid(cr, *eb(f"website_event_exhibitor.access_event_{{track_,}}sponsor_type_{suffix}"))
+
     util.move_field_to_module(cr, "event.event", "sponsor_ids", "website_event_track", "website_event_exhibitor")
     util.move_field_to_module(cr, "event.event", "sponsor_count", "website_event_track", "website_event_exhibitor")
 
@@ -26,3 +33,25 @@ def migrate(cr, version):
         """
         )
         util.remove_field(cr, "event.sponsor", "is_exhibitor")
+
+    # rename records
+    util.rename_xmlid(cr, *eb("website_event_{track,exhibitor}.event_sponsor"), noupdate=False)
+    util.rename_xmlid(cr, *eb("website_event_{track,exhibitor}.event_sponsor_type_view_tree"), noupdate=False)
+    util.rename_xmlid(cr, *eb("website_event_{track,exhibitor}.event_sponsor_type_view_form"), noupdate=False)
+    util.rename_xmlid(cr, *eb("website_event_{track,exhibitor}.event_sponsor_type_action"), noupdate=False)
+    util.rename_xmlid(cr, *eb("website_event_{track,exhibitor}.menu_event_sponsor_type"), noupdate=False)
+    util.rename_xmlid(cr, *eb("website_event_{track,exhibitor}.event_sponsor_action_from_event"), noupdate=False)
+
+    views = """
+        assets_frontend
+
+        # force recreation of these views
+        event_event_view_form
+
+        event_sponsor_view_search
+        event_sponsor_view_tree
+        event_sponsor_view_kanban
+        event_sponsor_view_form
+    """
+    for view in util.splitlines(views):
+        util.remove_view(cr, f"website_event_exhibitor.{view}")
