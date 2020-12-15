770b091068fcdf35023d587defc537954b6e77d9
Christophe Simonis <chs@odoo.com>
2020-12-15 16:47:55 +0100

    [FIX] website_slides/saas~14.1: remove record

--- migrations/website_slides/saas~14.1.2.3/pre-migrate.py
+++ migrations/website_slides/saas~14.1.2.3/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "slide_channel", "completed_template_id", "int4")
+    util.remove_record(cr, "website_slides.slide_channel_partner_action")
