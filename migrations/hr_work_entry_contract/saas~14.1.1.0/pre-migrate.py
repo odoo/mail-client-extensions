1c36bb792f81d482cd9ce3356b4b76b31f8c6fc1
Christophe Simonis <chs@odoo.com>
2020-12-16 15:52:03 +0100

    [FIX] hr_work_entry_contract/saas~14.1: rename view, don't drop it

--- migrations/hr_work_entry_contract/saas~14.1.1.0/pre-migrate.py
+++ migrations/hr_work_entry_contract/saas~14.1.1.0/pre-migrate.py
@@ -8,5 +8,16 @@ def migrate(cr, version):
     util.rename_xmlid(cr, *eb("{hr_payroll,hr_work_entry_contract}.hr_work_entry_regeneration_wizard"))
     util.rename_xmlid(cr, *eb("{hr_payroll,hr_work_entry_contract}.hr_work_entry_regeneration_wizard_action"))
     util.move_model(cr, "hr.work.entry.regeneration.wizard", "hr_payroll", "hr_work_entry_contract")
+
     util.remove_view(cr, "hr_work_entry_contract.hr_contract_view_form_inherit")
-    util.remove_view(cr, "hr_work_entry_contract.resource_calendar_view_form_inherit")
+
+    view_id = util.rename_xmlid(
+        cr,
+        "hr_work_entry_contract.resource_calendar_view_form_inherit",
+        "hr_work_entry_contract.resource_calendar_view_form",
+    )
+    if view_id:
+        cr.execute(
+            "DELETE FROM ir_ui_view_group_rel WHERE view_id = %s AND group_id = %s",
+            [view_id, util.ref(cr, "hr_payroll.group_hr_payroll_user")],
+        )
