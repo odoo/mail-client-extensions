79fdcf27ccbe9e4e3405a2a056bf6f3d030be582
Christophe Simonis <chs@odoo.com>
2020-12-16 12:37:02 +0100

    [FIX] hr_payroll/saas~14.1

--- migrations/hr_payroll/saas~14.1.1.0/pre-migrate.py
+++ migrations/hr_payroll/saas~14.1.1.0/pre-migrate.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-
     util.create_column(cr, "hr_payslip", "has_negative_net_to_report", "boolean")
+    util.create_column(cr, "hr_payslip", "edited", "boolean")
 
     # Related fields that become stored
     util.create_column(cr, "hr_payslip_worked_days", "name", "varchar")
@@ -23,3 +22,13 @@ def migrate(cr, version):
            FROM hr_payslip_input_type hpit
            WHERE hpi.input_type_id = hpit.id"""
     )
+
+    # transients models
+    # This field was defined in the `l10n_be_hr_payroll_variable_revenue` module (merged in `l10n_be_hr_payroll`)
+    util.move_field_to_module(cr, "hr.payslip.emplees", "department_id", "l10n_be_hr_payroll", "hr_payroll")
+    util.create_column(cr, "hr_payslip_employees", "department_id", "int4")
+
+    eb = util.expand_braces
+    util.rename_xmlid(cr, "hr_payroll.action_view_account_move_line_reconcile", "hr_payroll.action_reset_work_entries")
+    util.rename_xmlid(cr, *eb("{l10n_be_,}hr_payroll.input_deduction"))
+    util.rename_xmlid(cr, *eb("{l10n_be_,}hr_payroll.input_reimbursement"))
