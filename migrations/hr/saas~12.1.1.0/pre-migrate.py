3874981356ee0e63668e50e4bfcb39855efe5c8a
Rémy Baranx (bar) <bar@odoo.com>
2020-12-16 09:36:14 +0000

    [FIX] hr/saas~12.1: move template/report to hr
    In `saas~12.1` `print_employee_badge` template and `hr_employee_print_badge`
    report have been moved from `hr_attendance` to `hr` module.
    
    odoo/odoo#30302
    Upgrade request: 57633
    
    closes odoo/upgrade#2032
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr/saas~12.1.1.0/pre-migrate.py
+++ migrations/hr/saas~12.1.1.0/pre-migrate.py
@@ -5,3 +5,5 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.move_field_to_module(cr, "hr.employee", "barcode", "hr_attendance", "hr")
     util.move_field_to_module(cr, "hr.employee", "pin", "hr_attendance", "hr")
+    util.rename_xmlid(cr, "hr_attendance.print_employee_badge", "hr.print_employee_badge")
+    util.rename_xmlid(cr, "hr_attendance.hr_employee_print_badge", "hr.hr_employee_print_badge")
