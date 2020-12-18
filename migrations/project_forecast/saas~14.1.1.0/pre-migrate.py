83b465cb4dd87fbae3e05e9fba76b5affae1c662
Christophe Simonis <chs@odoo.com>
2020-12-18 20:31:08 +0100

    [FIX] project_forecast/saas~14.1

--- migrations/project_forecast/saas~14.1.1.0/pre-migrate.py
+++ migrations/project_forecast/saas~14.1.1.0/pre-migrate.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from odoo.addons.base.maintenance.migrations import util
+from odoo.upgrade import util
+
 
 def migrate(cr, version):
-    cr.execute("""
-        UPDATE ir_act_window
-           SET binding_model_id = NULL
-         WHERE id=%s
-    """, [util.ref(cr, 'project_forecast.action_project_task_view_planning')])
+    cr.execute(
+        "UPDATE ir_act_window SET binding_model_id = NULL WHERE id=%s",
+        [util.ref(cr, "project_forecast.action_project_task_view_planning")],
+    )
+
+    util.remove_record(cr, "project_forecast.planning_rule_project_adminis_published")
