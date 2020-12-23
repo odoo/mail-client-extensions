53ff9905a0b10db16ac9f0b7cd9ca540b536dd87
Christophe Simonis <chs@odoo.com>
2020-12-23 12:56:50 +0000

    [FIX] base/saas~13.4: handle non base64-encoded datas in attachments
    upg-2413
    upg-2530
    upg-2531
    upg-2599
    
    closes odoo/upgrade#2043
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~13.4.1.3/pre-attachments-no-b64.py
+++ migrations/base/saas~13.4.1.3/pre-attachments-no-b64.py
@@ -3,11 +3,18 @@
 
 def migrate(cr, version):
     # attachments are now stored raw in database.
-    # decode the base64 data
-    cr.execute(
-        """
-            UPDATE ir_attachment
-               SET db_datas = decode(encode(db_datas, 'escape'), 'base64')
-             WHERE db_datas IS NOT NULL
-        """
-    )
+    # try to decode the base64 data
+    parallel_safe = "PARALLEL SAFE" if cr._cnx.server_version >= 90600 else ""
+    try_b64 = f"""
+        CREATE FUNCTION try_b64(bytea) RETURNS bytea AS $$
+        BEGIN
+            RETURN decode(encode($1, 'escape'), 'base64');
+        EXCEPTION WHEN OTHERS THEN
+            RETURN $1;
+        END
+        $$ LANGUAGE plpgsql IMMUTABLE RETURNS NULL ON NULL INPUT {parallel_safe};
+    """
+
+    cr.execute(try_b64)
+    cr.execute("UPDATE ir_attachment SET db_datas = try_b64(db_datas) WHERE db_datas IS NOT NULL")
+    cr.execute("DROP FUNCTION try_b64(bytea);")
