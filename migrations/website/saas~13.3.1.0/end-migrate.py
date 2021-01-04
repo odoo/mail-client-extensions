0cfabb5858f0a32c3851f0fb00b6c45ee464a9e3
Christophe Simonis <chs@odoo.com>
2021-01-04 17:11:44 +0000

    [FIX] website/saas~13.3: adapt to split assets.
    closes odoo/upgrade#2055
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website/saas~13.3.1.0/end-migrate.py
+++ migrations/website/saas~13.3.1.0/end-migrate.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from odoo.upgrade import util
 
 
 def migrate(cr, version):
@@ -8,4 +9,32 @@ def migrate(cr, version):
            SET active=true
          WHERE key ~ '\.[_]{0,1}assets_snippet_s_.*_000'
     """
+    )
+
+    # helper asset view has been split (see odoo/odoo@da46ba7bc1af7e192ffaed654eee82d0a8ef90be)
+    # change parent of potential custom views (created by web_editor (save_asset method))
+    cr.execute(
+        r"""
+        UPDATE ir_ui_view
+           SET inherit_id = %s
+         WHERE inherit_id = %s
+           AND arch_db LIKE '%%/website/static/src/scss/user\_custom\_bootstrap\_overridden.scss%%'
+        """,
+        [
+            util.ref(cr, "website._assets_frontend_helpers_user_custom"),
+            util.ref(cr, "website._assets_frontend_helpers"),
+        ],
+    )
+
+    cr.execute(
+        r"""
+        UPDATE ir_ui_view
+           SET inherit_id = %s
+         WHERE inherit_id = %s
+           AND arch_db LIKE '%%/website/static/src/scss/user\_custom\_rules.scss%%'
+        """,
+        [
+            util.ref(cr, "website.assets_frontend_user_custom"),
+            util.ref(cr, "website.assets_frontend"),
+        ],
     )
