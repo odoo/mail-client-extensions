34d5e97fdb5612b9a45dee38dc13c48b32361ff9
Christophe Simonis <chs@odoo.com>
2021-01-08 10:01:45 +0100

    [FIX] mrp_mps/saas~12.4
     - Improve creation of schedules
        - fallback on creator's company when the product doesn't have
        - Set a default warehouse
     - Handle demo data (remove warnings on CI)
     - Blacken file

--- migrations/mrp_mps/saas~12.4.1.0/pre-migrate.py
+++ migrations/mrp_mps/saas~12.4.1.0/pre-migrate.py
@@ -4,7 +4,8 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
 
-    cr.execute("""
+    cr.execute(
+        """
         CREATE TABLE mrp_production_schedule(
             id SERIAL PRIMARY KEY,
             create_uid integer,
@@ -14,13 +15,15 @@ def migrate(cr, version):
             company_id integer,
             product_id integer NOT NULL,
             sequence integer,
-            warehouse_id integer, -- NOT NULL,     -- should be required, but sale_forecast.warehouse_id wasn't, forbidding migration
+            warehouse_id integer, -- NOT NULL, -- should be required, but sale_forecast.warehouse_id wasn't, forbidding migration
             forecast_target_qty float8,
             min_to_replenish_qty float8,
             max_to_replenish_qty float8
         )
-    """)
-    cr.execute("""
+    """
+    )
+    cr.execute(
+        """
         CREATE TABLE mrp_product_forecast(
             id SERIAL PRIMARY KEY,
             create_uid integer,
@@ -34,21 +37,32 @@ def migrate(cr, version):
             replenish_qty_updated boolean,
             procurement_launched boolean
         )
-    """)
+    """
+    )
 
-    cr.execute("""
+    cr.execute(
+        """
+        WITH default_warehouses AS (
+            SELECT company_id, min(id) as id
+              FROM stock_warehouse
+          GROUP BY company_id
+        )
         INSERT INTO mrp_production_schedule(product_id, company_id, sequence, warehouse_id,
                                             forecast_target_qty, min_to_replenish_qty, max_to_replenish_qty)
-             SELECT p.id, t.company_id, t.sequence, f.warehouse_id,
+             SELECT p.id, COALESCE(t.company_id, u.company_id), t.sequence, COALESCE(f.warehouse_id, w.id),
                     p.mps_forecasted, p.mps_min_supply, p.mps_max_supply
                FROM sale_forecast f
                JOIN product_product p ON p.id = f.product_id
                JOIN product_template t ON t.id = p.product_tmpl_id
-           GROUP BY p.id, t.company_id, t.sequence, f.warehouse_id,
+          LEFT JOIN res_users u ON u.id = COALESCE(f.create_uid, f.write_uid)
+          LEFT JOIN default_warehouses w ON w.company_id = COALESCE(t.company_id, u.company_id)
+           GROUP BY p.id, COALESCE(t.company_id, u.company_id), t.sequence, COALESCE(f.warehouse_id, w.id),
                     p.mps_forecasted, p.mps_min_supply, p.mps_max_supply
-    """)
+    """
+    )
 
-    cr.execute("""
+    cr.execute(
+        """
         INSERT INTO mrp_product_forecast(production_schedule_id, "date", forecast_qty,
                                          replenish_qty, replenish_qty_updated, procurement_launched)
 
@@ -56,7 +70,8 @@ def migrate(cr, version):
                     f.to_supply, (f.mode = 'manual'), (f.state = 'done')
                FROM sale_forecast f
                JOIN mrp_production_schedule s ON s.product_id = f.product_id AND s.warehouse_id = f.warehouse_id
-    """)
+    """
+    )
 
     # remove old stuff
     util.remove_model(cr, "sale.forecast")
@@ -73,3 +88,37 @@ def migrate(cr, version):
     util.remove_record(cr, "mrp_mps.mrp_mps_report_action_client")
     util.remove_record(cr, "mrp_mps.mrp_mps_menu_planning")
     util.remove_record(cr, "mrp_mps.mpr_mps_report_menu")
+
+    # demo data ¯\_(ツ)_/¯
+    cr.execute(
+        """
+        INSERT INTO ir_model_data(module,name,model,res_id,noupdate)
+             SELECT 'mrp_mps', 'mrp_mps_production_schedule_1', 'mrp.production.schedule', id, true
+               FROM mrp_production_schedule
+              WHERE warehouse_id = %(warehouse)s
+                AND product_id = %(product_12)s
+              UNION
+             SELECT 'mrp_mps', 'mrp_mps_production_schedule_2', 'mrp.production.schedule', id, true
+               FROM mrp_production_schedule
+              WHERE warehouse_id = %(warehouse)s
+                AND product_id = %(product_3)s
+              UNION
+             SELECT 'mrp_mps', 'mrp_mps_production_schedule_3', 'mrp.production.schedule', id, true
+               FROM mrp_production_schedule
+              WHERE warehouse_id = %(warehouse)s
+                AND product_id = %(product_13)s
+              UNION
+             SELECT 'mrp_mps', 'mrp_mps_production_schedule_5', 'mrp.production.schedule', id, true
+               FROM mrp_production_schedule
+              WHERE warehouse_id = %(warehouse)s
+                AND product_id = %(product_16)s
+        RETURNING id
+        """,
+        {
+            "warehouse": util.ref(cr, "stock.warehouse0"),
+            "product_12": util.ref(cr, "product.product_product_12"),
+            "product_3": util.ref(cr, "product.product_product_3"),
+            "product_13": util.ref(cr, "product.product_product_13"),
+            "product_16": util.ref(cr, "product.product_product_16"),
+        },
+    )
