9182012f2347924de295287bef09609eeea26bb1
Denis Ledoux <dle@odoo.com>
2021-01-12 12:42:26 +0000

    [FIX] web_gantt@0.0.0: do not validate nodes of gantt views
    This is basically the same idea than
    2ff3177fd110dc4923cfeb2451767d3d1de97635
    
    applied to the gantt view, which is added in its own
    module (in enterprise)
    
    upg-4325
    
    closes odoo/upgrade#2073
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/web_gantt/0.0.0/pre-models-ir-ui-view.py
@@ -0,0 +1,21 @@
+from odoo import models
+from odoo.addons.base.maintenance.migrations import util
+
+if util.version_gte("saas~13.3"):
+    from odoo.addons.web_gantt.models import ir_ui_view  # noqa
+
+
+def migrate(cr, version):
+    pass
+
+
+class IrUiView(models.Model):
+    _inherit = "ir.ui.view"
+    _module = "web_gantt"
+
+    if util.version_gte("saas~13.3"):
+
+        def _validate_tag_gantt(self, *args, **kwargs):
+            if self.env.context.get("_upgrade_custom_modules"):
+                return
+            return super()._validate_tag_gantt(*args, **kwargs)
