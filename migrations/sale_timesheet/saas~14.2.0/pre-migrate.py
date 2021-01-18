e287167902785c222678d6e3fbb47da1cae27861
Xavier BOL (xbo) <xbo@odoo.com>
2021-01-18 08:58:34 +0000

    [IMP] sale_timesheet/saas~14.2: only display one time the warning
    When the following condition for a SOL:
    (delivered quantity / ordered quantity) >= threshold set in the product
    is True, then we display an upsell warning for the corresponding SO.
    The problem is we don't check if the warning has already been displayed
    by the same SOL. Thus, each time we timesheet for this SOL, we display
    one more time a warning.
    
    This commit adds a new boolean field in sale.order.line to avoid to
    spam the salesman.
    
    odoo/odoo#64252
    task-2411291
    
    closes odoo/upgrade#2066
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/sale_timesheet/saas~14.2.0/pre-migrate.py
+++ migrations/sale_timesheet/saas~14.2.0/pre-migrate.py
@@ -5,3 +5,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.create_column(cr, "product_product", "service_upsell_warning", "boolean", default=False)
     util.create_column(cr, "product_product", "service_upsell_threshold", "float8")
+    util.create_column(cr, "sale_order_line", "has_displayed_warning_upsell", "boolean", default=False)
