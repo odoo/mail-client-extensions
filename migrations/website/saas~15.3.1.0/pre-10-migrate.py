a327996f152d9d517f8586d9e915c2f93b7aa15a
Aurélien Warnon <awa@odoo.com>
2021-01-27 14:10:55 +0000

    [IMP] website/saas~15.3: move website.visitor's demo data to website
    This commit cleans the website.visitor demo data of website_event since they
    have been moved to the website module where they belong.
    
    We also move the 'parent_id' field from the website_event module to the website
    module, see community commit for more information about that change.
    
    COM PR odoo/odoo#65113
    Task-2429652
    
    Part-of: odoo/upgrade#2111

--- migrations/website/saas~15.3.1.0/pre-10-migrate.py
+++ migrations/website/saas~15.3.1.0/pre-10-migrate.py
@@ -14,3 +14,12 @@ def migrate(cr, version):
     util.remove_field(cr, "res.config.settings", "social_instagram")
     util.remove_field(cr, "res.config.settings", "has_social_network")
     util.remove_field(cr, "res.config.settings", "specific_user_account")
+
+    if util.module_installed(cr, "website_event"):
+        util.move_field_to_module(cr, "website.visitor", "parent_id", "website_event", "website")
+        util.remove_record(cr, "website_event.website_visitor_event_0")
+        util.remove_record(cr, "website_event.website_visitor_event_1")
+        util.remove_record(cr, "website_event.website_visitor_event_2")
+        util.remove_record(cr, "website_event.website_visitor_event_2_1")
+    else:
+        util.create_column(cr, "website_visitor", "parent_id", "int4")
