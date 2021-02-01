8da72b08960307140ebbddd979b9b74a55855b89
Denis Ledoux <dle@odoo.com>
2021-02-01 11:45:01 +0000

    [FIX] sign@0.0.0: match_uniq on `sign.template.tag`
    Given the nature of tags,
    `_match_uniq` can be applied for `sign.template.tag`
    
    ```
    2021-01-30 22:01:05,117 25 INFO db_6169 odoo.modules.loading: loading sign/data/sign_data.xml
    2021-01-30 22:01:05,214 25 ERROR db_6169 odoo.sql_db: bad query: INSERT INTO "sign_template_tag" ("id", "create_uid", "create_date", "write_uid", "write_date", "color", "name") VALUES (nextval('sign_template_tag_id_seq'), 1, (now() at time zone 'UTC'), 1, (now() at time zone 'UTC'), 3, 'NDA') RETURNING id
    ERROR: duplicate key value violates unique constraint "sign_template_tag_name_uniq"
    DETAIL:  Key (name)=(NDA) already exists.
    ```
    
    upg-6169
    upg-5856
    
    closes odoo/upgrade#2124
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/sign/0.0.0/pre-models-match_uniq.py
@@ -0,0 +1,13 @@
+from odoo import models
+
+from odoo.addons.sign.models import sign_template  # noqa
+
+
+def migrate(cr, version):
+    pass
+
+
+class Tags(models.Model):
+    _inherit = "sign.template.tag"
+    _module = "sign"
+    _match_uniq = True
