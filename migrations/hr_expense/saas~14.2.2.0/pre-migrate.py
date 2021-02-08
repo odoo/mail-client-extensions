5009b473c06eb521b959da6c761bb8b0d291e8fd
Christophe Simonis <chs@odoo.com>
2021-02-08 19:17:33 +0100

    [FIX] hr_expense/saas~14.2

--- migrations/hr_expense/saas~14.2.2.0/pre-migrate.py
+++ migrations/hr_expense/saas~14.2.2.0/pre-migrate.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from odoo.upgrade import util
 
 
 def migrate(cr, version):
@@ -7,6 +8,8 @@ def migrate(cr, version):
             UPDATE hr_expense
                SET currency_id = res_company.currency_id
               FROM res_company
-             WHERE hr_expense.currency_id IS NULL AND hr_expense.company_id = res_company.id
+             WHERE hr_expense.company_id = res_company.id
+               AND hr_expense.currency_id IS NULL
         """
     )
+    util.remove_column(cr, "hr_expense", "company_currency_id")  # not stored anymore
