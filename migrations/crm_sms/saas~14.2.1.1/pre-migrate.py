e6825e2eaf9411fc1ef939f5d56339e0a0a31b31
Christophe Simonis <chs@odoo.com>
2021-02-08 19:02:19 +0100

    [FIX] crm_sms/saas~14.2

--- migrations/crm_sms/saas~14.2.1.1/pre-migrate.py
+++ migrations/crm_sms/saas~14.2.1.1/pre-migrate.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.remove_view(cr, "crm_sms.crm_lead_view_list_activities")
+    util.rename_xmlid(cr, "crm_sms.crm_lead_view_list_activities", "crm_sms.crm_case_tree_view_oppor", noupdate=False)
