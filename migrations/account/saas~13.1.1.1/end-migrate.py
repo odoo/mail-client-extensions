b59f13cc878dc2cc6f08c06bbd3eed6335ca59c3
Himanshu Domadiya <hdo@odoo.com>
2021-02-10 08:53:54 +0000

    [FIX] account/saas~13.1: Link custom tax report lines to the right report
    opw-2424067
    upg-5861
    
    closes odoo/upgrade#2147
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>
    Co-authored-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/saas~13.1.1.1/end-migrate.py
+++ migrations/account/saas~13.1.1.1/end-migrate.py
@@ -3,23 +3,29 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.create_column(cr, "account_tax_report", "_tmp", "int4")
     cr.execute(
         """
-        WITH new_reports AS (
-            INSERT INTO account_tax_report(name, country_id, _tmp)
-                 SELECT name, country_id, id
-                   FROM account_tax_report_line
-                  WHERE report_id IS NULL
-                    AND parent_id IS NULL
-              RETURNING id, _tmp
-        )
-        UPDATE account_tax_report_line l
-           SET report_id = n.id
-          FROM new_reports n
-         WHERE l.id = n._tmp
-    """
+        INSERT INTO account_tax_report(name, country_id)
+             SELECT 'Tax Report - ' || c.name, l.country_id
+               FROM account_tax_report_line l
+               JOIN res_country c ON c.id = l.country_id
+              WHERE l.report_id IS NULL
+                AND l.parent_id IS NULL
+                AND NOT EXISTS (SELECT 1 FROM account_tax_report WHERE country_id = l.country_id)
+           GROUP BY l.country_id, c.name
+        """
     )
+    cr.execute(
+        """
+        UPDATE account_tax_report_line l
+           SET report_id = r.id
+          FROM account_tax_report r
+         WHERE r.country_id = l.country_id
+           AND l.report_id IS NULL
+           AND l.parent_id IS NULL
+        """
+    )
+    # Assume the parent has the same country.
     cr.execute(
         r"""
         UPDATE account_tax_report_line l
@@ -27,8 +33,7 @@ def migrate(cr, version):
           FROM account_tax_report_line p
          WHERE l.report_id IS NULL
            AND p.id = (regexp_match(l.parent_path, '(\d+)/'))[1]::integer
-    """
+        """
     )
 
-    util.remove_column(cr, "account_tax_report", "_tmp")
     util.remove_column(cr, "account_tax_report_line", "country_id")
