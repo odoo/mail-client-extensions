a678c0ca2ad0df9a6c021f5cda9324e76eb0e04c
Christophe Simonis <chs@odoo.com>
2021-02-12 11:08:00 +0100

    [FIX] documents/saas~14.2

--- migrations/documents/saas~14.2.1.1/pre-migrate.py
+++ migrations/documents/saas~14.2.1.1/pre-migrate.py
@@ -5,5 +5,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "documents.workflow.rule", "has_business_option")
-    util.create_column(cr, "documents_workflow_rule", "link_model", "varchar")
+    util.create_column(cr, "documents_workflow_rule", "link_model", "int4")
     util.create_column(cr, "documents_document", "is_editable_attachment", "bool")
