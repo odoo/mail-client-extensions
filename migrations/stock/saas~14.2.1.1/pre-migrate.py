4c2d84ba12abaed0d18cec84c095617fef1e6f02
Christophe Simonis <chs@odoo.com>
2021-02-12 09:43:43 +0100

    [FIX] stock/saas~14.2: parallelise query

--- migrations/stock/saas~14.2.1.1/pre-migrate.py
+++ migrations/stock/saas~14.2.1.1/pre-migrate.py
@@ -5,13 +5,10 @@ from odoo.upgrade import util
 def migrate(cr, version):
 
     # Set a in_date, of quant without in_date, to the min of date because NULL was considered older
-    cr.execute(
-        """
-    UPDATE stock_quant
-       SET in_date = (SELECT coalesce(min(in_date) - interval '1s', now() at time zone 'utc') FROM stock_quant)
-     WHERE in_date IS NULL
-        """
-    )
+    cr.execute("SELECT coalesce(min(in_date) - interval '1s', now() at time zone 'utc') FROM stock_quant")
+    (min_date,) = cr.fetchone()
+    query = cr.mogrify("UPDATE stock_quant SET in_date = %s WHERE in_date IS NULL", [min_date]).decode()
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="stock_quant"))
 
     util.delete_unused(cr, "stock.sequence_tracking")
     cr.execute(
