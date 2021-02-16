7455cc179965a89a6e3efb79b8abe8f677f8edc8
Damien (abd) <abd@odoo.com>
2021-02-16 15:39:16 +0000

    [IMP] mail/saas~14.3: update some fields from activity_type
    * Reorganize and reword some of the fields for the activity form type view
    
    * The fields `force_next` from `mail.activity.type` and its related
      field from `mail.activity` are removed.
      Instead, a new field `chaining_type`, which is a `selection` will
      improve the readability of the activity_type form. (It is made more
      obvious that the user has to choose between 2 modes :
        - 'Trigger Next Activity': used when the user wants to specify the type of the
          next activity, which will be triggered once the current activity is done
        - 'Suggest Next Activity': used when the user wants to recommend the
          next activity for the user to schedule once the current activity is done
    
    * To be consistent with this change :
        - The field `default_next_type_id` is renamed `triggered_next_type_id`
        - The field `next_type_ids` is renamed `suggested_next_type_ids`
    
    * The field `default_description` is renamed `default_note` to better match the
      `note` field from `mail.activity`
    
    Task ID : 2410217
    COM PR : https://github.com/odoo/odoo/pull/63370
    ENT PR : https://github.com/odoo/enterprise/pull/15406
    
    closes odoo/upgrade#2167
    
    Related: odoo/odoo#63370
    Related: odoo/enterprise#15406
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/mail/saas~14.3.1.3/pre-migrate.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, 'mail.activity', 'force_next')
+    util.remove_field(cr, 'mail.activity.type', 'force_next')
+
+    util.rename_field(cr, 'mail.activity.type', 'default_next_type_id', 'triggered_next_type_id')
+    util.rename_field(cr, 'mail.activity.type', 'next_type_ids', 'suggested_next_type_ids')
+    util.rename_field(cr, 'mail.activity.type', 'default_description', 'default_note')
+
+    util.create_column(cr, 'mail_activity_type', 'chaining_type', 'varchar')
+
+    cr.execute(
+        """UPDATE mail_activity_type
+           SET chaining_type = CASE WHEN triggered_next_type_id IS NOT NULL
+                                    THEN 'trigger'
+                                    ELSE 'suggest'
+                               END"""
+    )
