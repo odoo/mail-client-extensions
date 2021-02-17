d11576bbe5b58cb10b1b12a0e267d1b843630906
Raj Modi <rai@odoo.com>
2021-02-17 11:14:38 +0000

    [FIX] l10n_it_edi@saas-13.5: Skipped duplicate records because the standard sql_constraint doesn't allow to insertion of non-unique records.
    The record is being inserted in 'account_edi_document' according to the new version standards. This is given in the reference below.
    https://github.com/odoo/upgrade/blob/508157d4d6e7ec43cc704c1f818e966c7731f172/migrations/l10n_it_edi/saas~13.5.0.3/post-migrate.py
    
    Due to which the sql_constaraint only allows unique records to get inserted. This is given in the following reference.
    https://github.com/odoo/odoo/blob/14.0/addons/account_edi/models/account_edi_document.py#L27
    
    but due to the insertion of duplicate records in some databases, migration gets failed.
    
    closes odoo/upgrade#2171
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/l10n_it_edi/saas~13.5.0.3/post-migrate.py
+++ migrations/l10n_it_edi/saas~13.5.0.3/post-migrate.py
@@ -9,12 +9,13 @@ def migrate(cr, version):
         cr.execute(
             """
             INSERT INTO account_edi_document (edi_format_id, move_id, state, attachment_id)
-            SELECT %s, move.id, 'sent', ir_attachment.id
+            SELECT %s, move.id, 'sent', (array_agg(ir_attachment.id ORDER BY ir_attachment.id DESC))[1]
               FROM account_move move
               JOIN ir_attachment ON ir_attachment.res_id = move.id AND ir_attachment.name = move.l10n_it_einvoice_name
              WHERE move.state = 'posted'
                AND move.l10n_it_einvoice_name IS NOT NULL
                AND ir_attachment.res_model = 'account.move'
+          GROUP BY move.id
             """,
             [it_edi_format_id],
         )
