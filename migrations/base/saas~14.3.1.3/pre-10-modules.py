a74557d9ff9842f212fbfbf4e086bf62516031fb
Christophe Simonis <chs@odoo.com>
2021-02-18 18:15:39 +0000

    [FIX] base/saas~14.3: correct module dependencies
    closes odoo/upgrade#2180
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~14.3.1.3/pre-10-modules.py
+++ migrations/base/saas~14.3.1.3/pre-10-modules.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.merge_module(cr, "account_edi_extended", "account_edi", without_deps=True)
 
-    if util.has_enterprise():
+    util.new_module(cr, "gift_card", deps={"sale"})
+    util.new_module(cr, "website_sale_gift_card", deps={"website_sale", "gift_card"})
 
+    util.module_deps_diff(cr, "l10n_pe", plus={"l10n_latam_invoice_document", "account_debit_note"})
+
+    if util.has_enterprise():
         util.merge_module(cr, "l10n_be_hr_payroll_273S_274", "l10n_be_hr_payroll")
         util.merge_module(cr, "l10n_be_hr_payroll_273S_274_account", "l10n_be_hr_payroll_account")
         util.merge_module(cr, "documents_l10n_be_hr_payroll_273S_274", "documents_l10n_be_hr_payroll")
         util.merge_module(cr, "l10n_be_hr_payroll_impulsion", "l10n_be_hr_payroll")
+
+        util.module_deps_diff(
+            cr,
+            "l10n_pe_edi",
+            plus={"account_edi"},
+            minus={"l10n_latam_invoice_document", "account_debit_note", "account_edi_extended"},
+        )
