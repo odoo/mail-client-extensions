90f38d7fbc4c430fdf956a3627871f0b783d37a1
Christophe Simonis <chs@odoo.com>
2021-02-19 13:33:18 +0000

    [FIX] account/saas~12.4: delete records directly
    Avoid passing through python. It's slower and may generate huge query
    which will throw the following error
    
    > psycopg2.errors.StatementTooComplex: stack depth limit exceeded
    
    upg-7658
    
    closes odoo/upgrade#2192
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/saas~12.4.1.1/pre-05-fix-invoices.py
+++ migrations/account/saas~12.4.1.1/pre-05-fix-invoices.py
@@ -29,28 +29,19 @@ def migrate(cr, version):
     # simply removing those taxes from the invoice lines.
     cr.execute(
         """
-        SELECT invltx.invoice_line_id, invltx.tax_id
-          FROM account_invoice_line_tax invltx
-          JOIN account_tax tax ON invltx.tax_id = tax.id
-          JOIN account_invoice_line invl ON invl.id = invltx.invoice_line_id
-          JOIN account_invoice inv ON inv.id = invl.invoice_id
-         WHERE inv.state IN ('draft', 'cancel')
-           AND tax.company_id != inv.company_id;
-    """
+        DELETE FROM account_invoice_line_tax lt
+              USING account_tax t,
+                    account_invoice_line l,
+                    account_invoice i
+              WHERE t.id = lt.tax_id
+                AND l.id = lt.invoice_line_id
+                AND i.id = l.invoice_id
+                AND i.state IN ('draft', 'cancel')
+                AND t.company_id != i.company_id
+        """
     )
-
-    wrong_invltx_data = cr.fetchall()
-
-    if wrong_invltx_data:
-        cr.execute(
-            """
-            DELETE FROM account_invoice_line_tax
-                  WHERE (invoice_line_id, tax_id) IN %(invltx_vals)s;
-        """,
-            {"invltx_vals": tuple(wrong_invltx_data)},
-        )
-
-        wrong_inv_count = len(wrong_invltx_data)
+    wrong_inv_count = cr.rowcount
+    if wrong_inv_count:
         util.add_to_migration_reports(
             f"{wrong_inv_count} draft of cancelled invoices referenced taxes from another company. "
             "These inconsistent taxes have been removed from those invoices.",
