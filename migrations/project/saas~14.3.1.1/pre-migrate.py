1c6d733dc47b81c848a0db528c8b26ef35f8e40f
Mahendra Barad <mba@odoo.com>
2021-02-22 12:53:02 +0000

    [IMP] project@saas~14.3: remove the ribbon field
    Purpose of the commit is to remove the ribbon_message
    field from the task.
    
    Related Community PR: https://github.com/odoo/odoo/pull/66589
    
    closes odoo/upgrade#2195
    
    Taskid: 2451287
    Related: odoo/enterprise#16559
    Related: odoo/odoo#66589
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/project/saas~14.3.1.1/pre-migrate.py
+++ migrations/project/saas~14.3.1.1/pre-migrate.py
@@ -5,3 +5,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     task_rating_id = util.ref(cr, 'project.mt_task_rating')
     cr.execute("UPDATE mail_message_subtype SET description = NULL WHERE id = %s", [task_rating_id])
+    util.remove_field(cr, "project.task", "ribbon_message")
