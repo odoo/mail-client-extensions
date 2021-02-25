726cb84239beae5015092d28890546289e9c3ad8
Yannick Tivisse <yti@odoo.com>
2021-02-25 13:10:31 +0000

    [IMP] l10n_be_hr_payroll@saas~14.3: Add hospital insurance advantage
    Com PR: https://github.com/odoo/odoo/pull/66837/files
    Ent PR: https://github.com/odoo/enterprise/pull/16678/files
    Internal PR (for 14.3 migration): https://github.com/odoo/internal/pull/1181
    
    closes odoo/upgrade#2209
    
    Taskid: 2467546
    Related: odoo/odoo#66837
    Related: odoo/enterprise#16678
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/l10n_be_hr_payroll/saas~14.3.1.0/pre-migrate.py
+++ migrations/l10n_be_hr_payroll/saas~14.3.1.0/pre-migrate.py
@@ -14,3 +14,13 @@ def migrate(cr, version):
     util.remove_view(cr, "l10n_be_hr_payroll.hr_contract_view_form_inherit")
 
     util.remove_model(cr, "l10n_be.meal.voucher.report")
+
+    util.create_column(cr, "hr_contract", "has_hospital_insurance", "boolean")
+    util.create_column(cr, "hr_contract", "insured_relative_children", "int4")
+    util.create_column(cr, "hr_contract", "insured_relative_adults", "int4")
+    util.create_column(cr, "hr_contract", "insured_relative_spouse", "boolean")
+    util.create_column(cr, "hr_contract", "hospital_insurance_amount_per_adult", "float8", default=20.5)
+    util.create_column(cr, "hr_contract", "hospital_insurance_amount_per_child", "float8", default=7.2)
+
+    util.create_column(cr, "res_config_settings", "hospital_insurance_amount_child", "float8")
+    util.create_column(cr, "res_config_settings", "hospital_insurance_amount_adult", "float8")
