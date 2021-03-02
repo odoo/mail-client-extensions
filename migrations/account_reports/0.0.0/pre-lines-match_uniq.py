ac158bc0cd73973585c9a6d4f62751d37225c9f7
Rémy Baranx (bar) <bar@odoo.com>
2021-03-02 12:14:26 +0000

    [FIX] account_reports/0.0.0: _match_uniq on financial report codes
    In some cases, while updating standard Odoo financial report lines,
    lines with same codes but with different formulas already exist, leading
    to a unicity constraint error. Use the `_match_uniq` attribute on that model.
    
    upg-2616
    upg-4903
    upg-5706
    upg-8101
    
    closes odoo/upgrade#2221
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/account_reports/0.0.0/pre-lines-match_uniq.py
@@ -0,0 +1,17 @@
+from odoo import models
+
+from odoo.addons.account_reports.models import account_financial_report  # noqa
+
+
+def migrate(cr, version):
+    pass
+
+
+class AccountFinancialHtmlReportLine(models.Model):
+    _inherit = "account.financial.html.report.line"
+    _module = "account_reports"
+    _match_uniq = True
+    _match_uniq_warning = (
+        "Your existing financial report line '{xmlid}' has been merged with the standard one "
+        "that has the same name '{name}'. You should check it to confirm that there is issue."
+    )
