97c60d91775b87d0f1902cc9c982666eed6e9782
Alvaro Fuentes <afu@odoo.com>
2021-03-02 08:42:16 +0000

    [FIX] website_helpdesk_form/saas~13.1.1.0: query deadlock
    The call to _ensure_submit_form_view on
    https://github.com/odoo/upgrade/blob/b2deb0a751d078a55f81886ca62321b753ffe534/migrations/website_helpdesk_form/saas%7E13.1.1.0/end-ensure-submit-form-view.py#L7
    triggers the installation of additional modules here
    https://github.com/odoo/enterprise/blob/be1ab29ba1f1aecb7370dfdd19ae7f4156c3b4d2/helpdesk/models/helpdesk.py#L178
    this provokes a deadlock on a query on ir_act_server (multiple
    transactions, one of which modifies that table).
    
    Here we uncheck for modules that have been uninstalled but are still marked as
    used by helpdesk teams. This ensures the installation does not happen later on
    the conflicting end script.
    
    upg-6983
    
    closes odoo/upgrade#2219
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website_helpdesk_form/saas~13.1.1.0/pre-migrate.py
+++ migrations/website_helpdesk_form/saas~13.1.1.0/pre-migrate.py
@@ -6,3 +6,28 @@ def migrate(cr, version):
     util.create_column(cr, "helpdesk_team", "website_form_view_id", "int4")
 
     util.remove_view(cr, "website_helpdesk_form.ticket_submit")
+
+    # https://github.com/odoo/enterprise/blob/be1ab29ba1f1aecb7370dfdd19ae7f4156c3b4d2/helpdesk/models/helpdesk.py#L180
+    MODULE_FIELD = {
+        "website_helpdesk_form": "use_website_helpdesk_form",
+        "website_helpdesk_livechat": "use_website_helpdesk_livechat",
+        "website_helpdesk_forum": "use_website_helpdesk_forum",
+        "website_helpdesk_slides": "use_website_helpdesk_slides",
+        "helpdesk_timesheet": "use_helpdesk_timesheet",
+        "helpdesk_sale_timesheet": "use_helpdesk_sale_timesheet",
+        "helpdesk_account": "use_credit_notes",
+        "helpdesk_stock": "use_product_returns",
+        "helpdesk_repair": "use_product_repairs",
+        "helpdesk_sale_coupon": "use_coupons",
+    }
+
+    cr.execute(
+        "SELECT name FROM ir_module_module WHERE state = 'uninstalled' AND name = any(%s)",
+        [list(MODULE_FIELD)],
+    )
+
+    fields_to_update = [MODULE_FIELD[module] for module, in cr.fetchall()]
+    if fields_to_update:
+        cr.execute(
+            "UPDATE helpdesk_team SET {}".format(", ".join("{} = False".format(field) for field in fields_to_update))
+        )
