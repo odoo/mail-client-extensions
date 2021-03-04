66dae489f0b3a1c369cd26b3d2dfa8b9089ccd53
Mahendra Barad <mba@odoo.com>
2021-03-04 10:59:31 +0000

    [IMP] helpdesk@saas~14.3: create new field in helpdek team
    Added the auto_close_ticket(boolean), auto_close_day(integer),
    from_stage_ids(m2m), to_stage_id(m2o) fields to helpdesk team.
    
    Related ent PR: https://github.com/odoo/enterprise/pull/16828
    
    closes odoo/upgrade#2230
    
    Taskid: 2466235
    Related: odoo/enterprise#16828
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/helpdesk/saas~14.3.1.4/pre-migrate.py
+++ migrations/helpdesk/saas~14.3.1.4/pre-migrate.py
@@ -5,3 +5,7 @@ from odoo.upgrade import util
 def migrate(cr, version):
     ticket_rating_id = util.ref(cr, 'helpdesk.mt_ticket_rated')
     cr.execute("UPDATE mail_message_subtype SET description = NULL WHERE id = %s", [ticket_rating_id])
+    util.create_column(cr, "helpdesk_team", "auto_close_ticket", "boolean")
+    util.create_column(cr, "helpdesk_team", "auto_close_day", "int4", default=7)
+    util.create_m2m(cr, "team_stage_auto_close_from_rel", "helpdesk_team", "helpdesk_stage")
+    util.create_column(cr, "helpdesk_team", "to_stage_id", "int4")
