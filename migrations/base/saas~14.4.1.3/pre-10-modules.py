84f0bea197066feb25689dda9d00d93d2a47ff1f
Thibault Delavallée <tde@odoo.com>
2021-03-05 09:19:47 +0000

    [ADD] mass_mailing_sale_subscription/saas~14.4: support marketing on subscriptions
    PURPOSE
    
    Support marketing on sale subscriptions.
    
    SPECIFICATIONS
    
    Add a bridge module between ``mass_mailing`` and ``sale_subscription`` apps.
    It should allow to perform a mailing (mail or sms) on subscriptions and make
    this model available in model selection on mailing view.
    
    LINKS
    
    Task ID-2431217
    COM PR odoo/odoo#67322
    ENT PR odoo/enterprise#16876
    UPG PR odoo/upgrade#2236
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~14.4.1.3/pre-10-modules.py
+++ migrations/base/saas~14.4.1.3/pre-10-modules.py
@@ -6,3 +6,7 @@ from odoo.upgrade import util
 def migrate(cr, version):
     if util.has_enterprise():
         util.merge_module(cr, "l10n_be_hr_payroll_eco_vouchers", "l10n_be_hr_payroll")
+
+        util.new_module(
+            cr, "mass_mailing_sale_subscription", deps={"mass_mailing", "sale_subscription"}, auto_install=True
+        )
