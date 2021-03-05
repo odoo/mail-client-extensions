c5fc8b3acc54dab3dcac3ec36788f98ff12e0558
Kevin Baptiste <kba@odoo.com>
2021-03-05 09:50:28 +0000

    [IMP] hr_holidays@saas~14.3: move current_leave_id to private employee
    Move the field current_leave_id from `hr.employee.base` to
    `hr.employee`.
    
    Related odoo/odoo#67332
    
    closes odoo/upgrade#2239
    
    Taskid: 2476677
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr_holidays/saas~14.3.1.5/pre-migrate.py
+++ migrations/hr_holidays/saas~14.3.1.5/pre-migrate.py
@@ -5,3 +5,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.update_record_from_xml(cr, "hr_holidays.hr_leave_rule_employee_unlink")
+    util.remove_field_metadata(cr, "hr.employee.base", "current_leave_id")
+    util.remove_field(cr, "hr.employee.public", "current_leave_id")
