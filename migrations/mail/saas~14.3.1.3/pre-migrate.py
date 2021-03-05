25bbc0349bd3a7d8b105b6721b5a50fe3e0d064e
Thibault Delavallée <tde@odoo.com>
2021-03-05 15:16:47 +0000

    [IMP] mail@saas~14.3: blackize file
    closes odoo/upgrade#2245
    
    Related: odoo/odoo#67382
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/mail/saas~14.3.1.3/pre-migrate.py
+++ migrations/mail/saas~14.3.1.3/pre-migrate.py
@@ -4,14 +4,14 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.remove_field(cr, 'mail.activity', 'force_next')
-    util.remove_field(cr, 'mail.activity.type', 'force_next')
+    util.remove_field(cr, "mail.activity", "force_next")
+    util.remove_field(cr, "mail.activity.type", "force_next")
 
-    util.rename_field(cr, 'mail.activity.type', 'default_next_type_id', 'triggered_next_type_id')
-    util.rename_field(cr, 'mail.activity.type', 'next_type_ids', 'suggested_next_type_ids')
-    util.rename_field(cr, 'mail.activity.type', 'default_description', 'default_note')
+    util.rename_field(cr, "mail.activity.type", "default_next_type_id", "triggered_next_type_id")
+    util.rename_field(cr, "mail.activity.type", "next_type_ids", "suggested_next_type_ids")
+    util.rename_field(cr, "mail.activity.type", "default_description", "default_note")
 
-    util.create_column(cr, 'mail_activity_type', 'chaining_type', 'varchar')
+    util.create_column(cr, "mail_activity_type", "chaining_type", "varchar")
 
     cr.execute(
         """UPDATE mail_activity_type
