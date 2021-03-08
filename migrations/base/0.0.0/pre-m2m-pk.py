f88acc9dc5d3584726f3150db8b483cc6cfe124a
Christophe Simonis <chs@odoo.com>
2021-03-08 16:04:08 +0000

    [FIX] base/0.0.0: fixup the m2m if we know the foreign keys
    closes odoo/upgrade#2248
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/0.0.0/pre-m2m-pk.py
+++ migrations/base/0.0.0/pre-m2m-pk.py
@@ -11,11 +11,13 @@ def migrate(cr, version):
     # needs a primary key (hence the initial commit).
     # Force this composite PK on all m2m tables.
     query = """
-        SELECT c.relname, array_agg(a.attname order by a.attname)
+        SELECT c.relname, array_agg(a.attname order by a.attname), array_agg(t.relname ORDER BY a.attname)
           FROM pg_class c
           JOIN pg_namespace ns on ns.oid = c.relnamespace
      LEFT JOIN pg_constraint p on p.conrelid = c.oid and p.contype = 'p'
           JOIN pg_attribute a ON a.attrelid = c.oid and a.attnum > 0
+     LEFT JOIN pg_constraint f ON f.conrelid = c.oid AND array_lower(f.conkey, 1) = 1 AND f.conkey[1] = a.attnum AND f.contype = 'f'
+     LEFT JOIN pg_class t ON f.confrelid = t.oid
          WHERE c.relkind IN ('r', 'p')
            AND ns.nspname = 'public'
            AND p.oid IS NULL
@@ -24,5 +26,9 @@ def migrate(cr, version):
     """
 
     cr.execute(query)
-    for m2m, columns in cr.fetchall():
-        util.fixup_m2m_indexes(cr, m2m, *columns)
+    for m2m, (col1, col2), (fk1, fk2) in cr.fetchall():
+        if fk1 and fk2:
+            util.fixup_m2m(cr, m2m, fk1, fk2, col1, col2)
+        else:
+            util.fixup_m2m_cleanup(cr, m2m, col1, col2)
+            util.fixup_m2m_indexes(cr, m2m, col1, col2)
