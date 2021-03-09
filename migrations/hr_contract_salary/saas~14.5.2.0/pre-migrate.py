f4ba1360b96c7e40f41352e86cf8a27178fd1fa2
jdoutreloux <jud@odoo.com>
2021-03-09 14:32:51 +0000

    [FIX] hr_contract_salary/saas~14.5: change type of fields
    Value fields were stored as chars but had to be floats.
    
    See odoo/enterprise#16945
    
    closes odoo/upgrade#2627
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr_contract_salary/saas~14.5.2.0/pre-migrate.py
+++ migrations/hr_contract_salary/saas~14.5.2.0/pre-migrate.py
@@ -5,4 +5,6 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.create_column(cr, "hr_contract_salary_advantage", "display_type", "varchar", default="line")
+    util.create_column(cr, "hr_contract_salary_advantage_value", "display_type", "varchar", default="line")
+    cr.execute("ALTER TABLE hr_contract_salary_advantage_value ALTER COLUMN value TYPE float8 USING value::float8")
+    cr.execute("ALTER TABLE hr_contract_salary_resume ALTER COLUMN fixed_value TYPE float8 USING fixed_value::float8")
