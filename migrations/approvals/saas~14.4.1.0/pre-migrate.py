9454de356137339e97872e778e21380f29b5f2fb
Mahendra Barad <mba@odoo.com>
2021-03-11 06:29:47 +0000

    [IMP] approvals@saas~14.4: update the mail activity type for approval
    Purpose of the commit is do not allow to create the approval activity
    manually so make it archieved.
    
    Related Ent PR: https://github.com/odoo/enterprise/pull/16987
    
    closes odoo/upgrade#2263
    
    Taskid: 2439015
    Related: odoo/enterprise#16987
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/approvals/saas~14.4.1.0/pre-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    approval_type_id = util.ref(cr, 'approvals.mail_activity_data_approval')
+    cr.execute("UPDATE mail_activity_type SET active = false WHERE id = %s", [approval_type_id])
