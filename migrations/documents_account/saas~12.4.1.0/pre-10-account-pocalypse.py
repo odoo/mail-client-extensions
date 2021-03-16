5fc1c5e3d0149555c3dac9622daece7e58e44698
Himanshu Domadiya <hdo@odoo.com>
2021-03-16 06:06:12 +0000

    [FIX] documents_account/saas~12.4: update documents workflow rule
    upg-4704
    opw-2424353
    
    closes odoo/upgrade#2274
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/documents_account/saas~12.4.1.0/pre-10-account-pocalypse.py
+++ migrations/documents_account/saas~12.4.1.0/pre-10-account-pocalypse.py
@@ -9,3 +9,13 @@ def migrate(cr, version):
     util.force_noupdate(cr, 'documents_account.credit_note_rule', noupdate=False)
 
     util.create_column(cr, 'account_move', 'document_request_line_id', 'int4')
+    cr.execute(
+        """
+        UPDATE documents_workflow_rule
+           SET create_model = replace(create_model, '.invoice', '.move')
+         WHERE create_model IN ('account.invoice.in_invoice',
+                                'account.invoice.out_invoice',
+                                'account.invoice.in_refund',
+                                'account.invoice.out_refund')
+        """
+    )
