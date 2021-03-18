506ed40f7a71f98155b09085f218560d5b23fafa
Thibault Delavallée <tde@odoo.com>
2021-03-18 14:36:46 +0000

    [IMP] event/saas~14.3: merge migration files to 1.5
    As models have changed minor version has been increased. We can safely merge
    both migration files for 14.3 .
    
    Task ID-2414658
    COM PR odoo/odoo#63093
    UPG PR odoo/upgrade#2014
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/event/saas~14.3.1.5/pre-migrate.py
+++ migrations/event/saas~14.3.1.5/pre-migrate.py
@@ -4,6 +4,20 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    # Clean the ACLs XMLID
+    acl_name_changes = {
+        "event_portal": "event",
+        "registration": "registration_user",
+        "registration_all": "registration",
+        "mail": "mail_user",
+        "type_mail_event_manager": "type_mail_manager",
+        "category_manager": "tag_category_manager",
+        "tag": "tag_user",
+    }
+    for old_name, new_name in acl_name_changes.items():
+        util.rename_xmlid(cr, f"event.access_event_{old_name}", f"event.access_event_{new_name}")
+
+    # event communication upgrade
     util.create_column(cr, "event_mail", "mail_count_done", "integer")
 
     # registration-based scheduler: count of event_mail_registration sent
