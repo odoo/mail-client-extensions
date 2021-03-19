f497b5b888ae4803ca52daf75e402330990534f3
Aurélien Warnon <awa@odoo.com>
2021-03-19 09:08:39 +0000

    [IMP] crm_iap_lead/SaaS~14.4: allow industries to match several reveal ids
    Follow-up of community commit that allows crm.iap.lead.industry to have several
    reveal_ids for each record.
    
    This commit simply renames the "reveal_id" field into "reveal_ids".
    And renames some xmlids to match data changes.
    
    See community commit for more details.
    
    COM PR odoo/odoo#68128
    Task-2471703

--- migrations/crm_iap_lead/saas~14.4.1.2/pre-migrate.py
+++ migrations/crm_iap_lead/saas~14.4.1.2/pre-migrate.py
@@ -13,3 +13,40 @@ def migrate(cr, version):
         """
     )
     util.remove_field(cr, "crm.iap.lead.mining.request", "error")
+
+    util.rename_field(cr, "crm.iap.lead.industry", "reveal_id", "reveal_ids")
+
+    # industry data have been updated modified in CSV file
+
+    # 1. update record references of merged industries
+    industry_ids_to_merge = {
+        "151": "150",
+        "154": "153",
+        "155": "30",
+        "156": "138",
+        "157": "69",
+        "159": "158",
+    }
+
+    def ref(num):
+        return util.ref(cr, f"crm_iap_lead.crm_iap_lead_industry_{num}")
+
+    util.replace_record_references_batch(
+        cr,
+        {ref(old_id): ref(new_id) for old_id, new_id in industry_ids_to_merge.items()},
+        "crm.iap.lead.industry",
+        replace_xmlid=False,
+    )
+
+    # 2. remove unused industries & merged ones
+    industry_ids_to_remove = ["146", "151", "154", "155", "156", "157", "159", "164"]
+    util.delete_unused(cr, *[f"crm_iap_lead.crm_iap_lead_industry_{num}" for num in industry_ids_to_remove])
+
+    # 3. rename the ones we keep with their new name (including multiple ids)
+    eb = util.expand_braces
+    util.rename_xmlid(cr, *eb("crm_iap_lead.crm_iap_lead_industry_30{,_155}"))
+    util.rename_xmlid(cr, *eb("crm_iap_lead.crm_iap_lead_industry_69{,_157}"))
+    util.rename_xmlid(cr, *eb("crm_iap_lead.crm_iap_lead_industry_138{,_156}"))
+    util.rename_xmlid(cr, *eb("crm_iap_lead.crm_iap_lead_industry_150{,_151}"))
+    util.rename_xmlid(cr, *eb("crm_iap_lead.crm_iap_lead_industry_153{,_154}"))
+    util.rename_xmlid(cr, *eb("crm_iap_lead.crm_iap_lead_industry_158{,_159}"))
