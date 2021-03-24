c28357f8042b41b68ede0cec13d535196458240e
Denis Ledoux <dle@odoo.com>
2021-03-24 15:51:54 +0000

    [FIX] stock@0.0.0: do not delete rules, picking types, warehouses
    ```
    2021-03-24 15:46:17,102 29 ERROR dle_8647_14.0 odoo.sql_db: bad query: DELETE FROM stock_rule WHERE id IN (23)
    ERROR: update or delete on table "stock_rule" violates foreign key constraint "stock_move_rule_id_fkey" on table "stock_move"
    DETAIL:  Key (id)=(23) is still referenced from table "stock_move".
    
    2021-03-24 15:46:17,112 29 WARNING dle_8647_14.0 odoo.modules.loading: Transient module states were reset
    2021-03-24 15:46:17,113 29 ERROR dle_8647_14.0 odoo.modules.registry: Failed to load registry
    2021-03-24 15:46:17,114 29 CRITICAL dle_8647_14.0 odoo.service.server: Failed to initialize database `dle_8647_14.0`.
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/14.0/odoo/service/server.py", line 1198, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "/home/odoo/src/odoo/14.0/odoo/modules/registry.py", line 89, in new
        odoo.modules.load_modules(registry._db, force_demo, status, update_module)
      File "/home/odoo/src/odoo/14.0/odoo/modules/loading.py", line 497, in load_modules
        env['ir.model.data']._process_end(processed_modules)
      File "/tmp/tmp59dtbcwp/migrations/base/0.0.0/pre-models-no-model-data-delete.py", line 90, in _process_end
        return super(IrModelData, self)._process_end(modules)
      File "/home/odoo/src/odoo/14.0/odoo/addons/base/models/ir_model.py", line 2266, in _process_end
        record.unlink()
      File "/home/odoo/src/odoo/14.0/odoo/models.py", line 3452, in unlink
        cr.execute(query, (sub_ids,))
      File "<decorator-gen-3>", line 2, in execute
      File "/home/odoo/src/odoo/14.0/odoo/sql_db.py", line 101, in check
        return f(self, *args, **kwargs)
      File "/home/odoo/src/odoo/14.0/odoo/sql_db.py", line 298, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.ForeignKeyViolation: update or delete on table "stock_rule" violates foreign key constraint "stock_move_rule_id_fkey" on table "stock_move"
    DETAIL:  Key (id)=(23) is still referenced from table "stock_move".
    ```
    
    upg-8647
    
    closes odoo/upgrade#2304
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/stock/0.0.0/pre-models-no-model-data-delete.py
+++ migrations/stock/0.0.0/pre-models-no-model-data-delete.py
@@ -4,3 +4,6 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.ENVIRON["no_model_data_delete"]["stock.location"] = "always"
+    util.ENVIRON["no_model_data_delete"]["stock.rule"] = "always"
+    util.ENVIRON["no_model_data_delete"]["stock.picking.type"] = "unused"
+    util.ENVIRON["no_model_data_delete"]["stock.warehouse"] = "unused"
