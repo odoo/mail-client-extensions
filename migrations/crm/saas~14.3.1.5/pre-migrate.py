6e93149517d9a1d3fa79087a72ac26b53d91c7ca
Thibault Delavallée <tde@odoo.com>
2021-03-29 14:23:42 +0000

    [IMP] crm/saas~14.3: allow team members to optout from assign
    Purpose is to allow members to quickly opt-out / opt-in from assign
    process. For example if for some reason (leaves, too much leads, temporarily
    working on something else) a salesperson cannot handle more leads he may
    opt-out from assign process.
    
    Task ID-2494749
    COM PR odoo/odoo#68476
    UPG PR odoo/upgrade#2318
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/crm/saas~14.3.1.5/pre-migrate.py
+++ migrations/crm/saas~14.3.1.5/pre-migrate.py
@@ -5,3 +5,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "crm.lead", "ribbon_message")
+    util.create_column(cr, "crm_team_member", "assignment_optout", "boolean")
