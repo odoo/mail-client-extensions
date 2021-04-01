26249e2acdb8afd487b248aa8cf5282e175382df
Denis Ledoux <dle@odoo.com>
2021-04-01 09:09:38 +0000

    [FIX] stock@7.saas~5: remove model `report.stock.move`
    This model has been removed in
    odoo/odoo@77ceb83f6983a8cc926eba551654a42597ec0757
    
    Calling `remove_model` allow to call `remove_view` on
    all this model views, hence handling custom views
    inheriting on those.
    
    upg-10542
    
    closes odoo/upgrade#2337
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/stock/7.saas~5.1.1/pre-00-rm-sql-views.py
+++ migrations/stock/7.saas~5.1.1/pre-00-rm-sql-views.py
@@ -2,10 +2,14 @@ from openerp.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    cr.execute("""
+    cr.execute(
+        """
         -- the following views are deprecated since 7.saas~5
         DROP VIEW IF EXISTS stock_report_tracklots;
         DROP VIEW IF EXISTS stock_report_prodlots;
         DROP VIEW IF EXISTS report_stock_inventory;
         DROP VIEW IF EXISTS report_stock_move;
-        """)
+        """
+    )
+
+    util.remove_model(cr, "report.stock.move")
