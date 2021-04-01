454bb024c1938b34005084739a68a1c35867d9ae
Denis Ledoux <dle@odoo.com>
2021-04-01 08:29:26 +0000

    [FIX] mrp_workorder@saas~12.2: performance improvment cleanup non-done wo on quality checks
    `NOT IN` with a huge list is never a good idea for performances.
    
    This revisions is just an alternative which speeds up
    the processing time from multiple hours (aborted the upgrade after
    7 hours and counting) to a few seconds in the case of the observed
    upgrade request.
    
    upg-11120
    
    closes odoo/upgrade#2334
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/mrp_workorder/saas~12.2.1.0/pre-migrate.py
+++ migrations/mrp_workorder/saas~12.2.1.0/pre-migrate.py
@@ -8,8 +8,16 @@ def migrate(cr, version):
     util.remove_view(cr, "mrp_workorder.mrp_production_view_form_inherit_planning")
 
     # cleanup non-done wo on quality checks
-    cr.execute("""
+    cr.execute(
+        """
         UPDATE quality_check c
            SET move_line_id = NULL
-         WHERE move_line_id NOT IN (SELECT id FROM mrp_workorder_line)
-    """)
+          FROM (
+                   SELECT qc.id
+                     FROM quality_check qc
+                LEFT JOIN mrp_workorder_line l ON qc.move_line_id = l.id
+                    WHERE l.id IS NULL
+               ) as to_update
+         WHERE c.id = to_update.id
+    """
+    )
