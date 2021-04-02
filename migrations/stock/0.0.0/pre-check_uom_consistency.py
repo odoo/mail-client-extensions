1d95a5cb7d99401181ce679287f75452e3928a00
Raj Modi <rai@odoo.com>
2021-04-02 10:20:58 +0000

    [FIX] stock@0.0.0: Handle UOM inconsistencies for archived products.
    In the database, stock move lines UoM category may be different from
    the corresponding archived product UoM category which is not correct.
    
    by default, archived products are not taken into account,
    to take them into account, the environment variable ODOO_MIG_DO_NOT_IGNORE_ARCHIVED_PRODUCTS_FOR_UOM_INCONSISTENCIES shall be set
    
    So, set the environment variable ODOO_MIG_DO_NOT_IGNORE_ARCHIVED_PRODUCTS_FOR_UOM_INCONSISTENCIES to update stock_move_line for archived products.
    
    closes odoo/upgrade#2347
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/stock/0.0.0/pre-check_uom_consistency.py
+++ migrations/stock/0.0.0/pre-check_uom_consistency.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
 
+from odoo.tools.misc import str2bool
+
 from odoo.addons.base.maintenance.migrations import util
 
 
@@ -44,7 +46,7 @@ def fix_product_templates(cr):
     return ["%s (id: %s)" % (name, str(id)) for id, name in cr.fetchall()]
 
 
-def fix_with_most_used_method(cr):
+def fix_with_most_used_method(cr, additional_conditions):
     """
     Fix stock move lines and product templates using the most used UoM category.
     """
@@ -59,7 +61,7 @@ def fix_with_most_used_method(cr):
               JOIN uom_uom uom1 ON uom1.id = sml.product_uom_id
               JOIN uom_uom uom2 ON uom2.id = pt.uom_id
              WHERE uom1.category_id != uom2.category_id
-               AND pp.active = true
+               AND {}
         ),
 
         -- for each product templates, get the main UoM category used in stock move lines
@@ -88,7 +90,9 @@ def fix_with_most_used_method(cr):
                 GROUP BY mc.tmpl_id,  uom.category_id, sml.product_uom_id
                 ORDER BY mc.tmpl_id, uom.category_id, COUNT(sml.product_uom_id) DESC, sml.product_uom_id
         ) as rows
-    """
+    """.format(
+            additional_conditions
+        )
     )
     moves = fix_moves(cr)
     templates = fix_product_templates(cr)
@@ -105,7 +109,7 @@ def fix_with_most_used_method(cr):
     )
 
 
-def fix_with_from_product_method(cr):
+def fix_with_from_product_method(cr, additional_conditions):
     """
     Fix stock move lines using the UoM of the corresponding product.
     """
@@ -119,8 +123,10 @@ def fix_with_from_product_method(cr):
           JOIN uom_uom uom1 ON uom1.id = sml.product_uom_id
           JOIN uom_uom uom2 ON uom2.id = pt.uom_id
          WHERE uom1.category_id != uom2.category_id
-           AND pp.active = true
-        """
+           AND {}
+        """.format(
+            additional_conditions
+        )
     )
     moves = fix_moves(cr)
     log_customer_report(
@@ -166,7 +172,7 @@ def log_customer_report(cr, explanation, moves=None, templates=None):
         )
 
 
-def log_faulty_objects(cr):
+def log_faulty_objects(cr, additional_conditions):
     """
     In case no fixing method has been chosen, stop the migration with the
     list of faulty moves to inform the customer.
@@ -180,9 +186,11 @@ def log_faulty_objects(cr):
             JOIN uom_uom uom1 ON uom1.id = sml.product_uom_id
             JOIN uom_uom uom2 ON uom2.id = pt.uom_id
            WHERE uom1.category_id != uom2.category_id
-             AND pp.active = true
+             AND {}
         GROUP BY sml.move_id
-        """
+        """.format(
+            additional_conditions
+        )
     )
     faulty_moves = ["%s (lines: %s)" % (str(move_id), str(lines)) for move_id, lines in cr.fetchall()]
 
@@ -202,6 +210,9 @@ def log_faulty_objects(cr):
             automatic fixes:
                 * MOST_USED: to automatically use the most used category,
                 * FROM_PRODUCT: to automatically use the UoM from the product
+
+            - Also you can set the environment variable
+                 ODOO_MIG_DO_NOT_IGNORE_ARCHIVED_PRODUCTS_FOR_UOM_INCONSISTENCIES to update stock_move_line for archived products
             """
             % ("\n".join(["                * %s" % sm for sm in faulty_moves]),)
         )
@@ -222,19 +233,22 @@ def migrate(cr, version):
     # - FROM_PRODUCT: update all the stock moves to use the UoM set on the product template
     allowed_methods = ["SKIP", "MOST_USED", "FROM_PRODUCT"]
     fix_inconsistencies_method = os.environ.get("ODOO_MIG_ENABLE_UOM_INCONSISTENCIES_FIX", "").upper()
-
+    archived_product_env = os.environ.get("ODOO_MIG_DO_NOT_IGNORE_ARCHIVED_PRODUCTS_FOR_UOM_INCONSISTENCIES")
+    update_uom_for_archived_product = str2bool(archived_product_env, default=False)
     if fix_inconsistencies_method and fix_inconsistencies_method not in allowed_methods:
         raise ValueError(
             "unknown value for environment variable ODOO_MIG_ENABLE_UOM_INCONSISTENCIES_FIX: %s"
             % fix_inconsistencies_method
         )
 
+    additional_conditions = "true" if update_uom_for_archived_product else "pp.active = true"
+
     if fix_inconsistencies_method == "SKIP":
         # if the customer absolutely wants to keep these inconsistencies as they are
         return
     elif fix_inconsistencies_method == "MOST_USED":
-        fix_with_most_used_method(cr)
+        fix_with_most_used_method(cr, additional_conditions)
     elif fix_inconsistencies_method == "FROM_PRODUCT":
-        fix_with_from_product_method(cr)
+        fix_with_from_product_method(cr, additional_conditions)
     else:
-        log_faulty_objects(cr)
+        log_faulty_objects(cr, additional_conditions)
