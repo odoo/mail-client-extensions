3c72b66cbcaf5f966d83e3dcc6b1ff465d103a78
Romain Derie <rde@odoo.com>
2021-04-09 12:39:48 +0000

    [IMP] website@saas~14.4: add website.sequence + company.website_id
    Also, clean the old domain coming from the xml records. Those where removed
    with [1], but DB create before that kept the domain.
    It was not really an issue before, but the related community PR is now
    replacing calls to `web.base.url` to `get_base_url()`, which will use more
    often the website domain.
    
    [1] https://github.com/odoo/odoo/commit/1f0edacdf29b911c44c074a57fcb8a50039d60b5
    
    task-2476101
    Related to https://github.com/odoo/odoo/pull/68201
    
    closes odoo/upgrade#2372
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website/saas~14.4.1.0/pre-migrate.py
+++ migrations/website/saas~14.4.1.0/pre-migrate.py
@@ -7,3 +7,11 @@ def migrate(cr, version):
     # TODO: remove later when renaming is backported
     if util.column_exists(cr, "theme_ir_asset", "glob"):
         util.rename_field(cr, "theme.ir.asset", "glob", "path")
+
+    util.create_column(cr, "website", "sequence", "int4")
+    cr.execute("UPDATE website SET sequence = id")
+    # 'localhost' is the old website default value (XML). Now, ICP calls are
+    # replaced by `get_base_url()` that will prefer website domain over ICP, we
+    # don't want links to be send with localhost, see commit msg.
+    cr.execute("UPDATE website SET domain = NULL WHERE domain = 'localhost'")
+    util.create_column(cr, "res_company", "website_id", "int4")
