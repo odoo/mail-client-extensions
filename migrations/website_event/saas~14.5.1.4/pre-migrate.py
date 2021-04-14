56ea1cd4ae1b399061d9c1b999bf4317f5461e7e
qmo-odoo <qmo@odoo.com>
2021-04-14 13:45:07 +0000

    [IMP] website_event/saas~14.5: remove dead template
    Filter by tags option has been removed in community. See odoo/odoo#68901
    
    Task-2488019
    
    closes odoo/upgrade#2395
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website_event/saas~14.5.1.4/pre-migrate.py
+++ migrations/website_event/saas~14.5.1.4/pre-migrate.py
@@ -7,3 +7,4 @@ def migrate(cr, version):
     util.create_column(cr, "event_event", "introduction_menu", "boolean", default=False)
     util.create_column(cr, "event_event", "location_menu", "boolean", default=False)
     util.create_column(cr, "event_event", "register_menu", "boolean", default=False)
+    util.remove_view(cr, "website_event.event_category_tag")
