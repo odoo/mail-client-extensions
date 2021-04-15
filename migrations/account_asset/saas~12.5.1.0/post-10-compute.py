917810768b525970e4f4bc7df268ec8b167eabf2
bda-odoo <bda@odoo.com>
2021-04-15 06:32:37 +0000

    [FIX]account@saas~12.5.1.0: Fix book_value calculation
    Before this commit:
    If salvage_value or value_residual is null then book_value null
    
    After this commit:
    If salvage_value or value_residual is null then book_value is not null but have some float value
    
    opw-2458044
    
    closes odoo/upgrade#2390
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account_asset/saas~12.5.1.0/post-10-compute.py
+++ migrations/account_asset/saas~12.5.1.0/post-10-compute.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
-from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    cr.execute("""
+    cr.execute(
+        """
           WITH childs AS (
              SELECT sum(book_value) as amt, parent_id
                FROM account_asset
            GROUP BY parent_id
           )
         UPDATE account_asset aa
-           SET book_value=a.value_residual+a.salvage_value+COALESCE(childs.amt, 0.0)
+           SET book_value = COALESCE(a.value_residual, 0.0) + COALESCE(a.salvage_value, 0.0) + COALESCE(childs.amt, 0.0)
           FROM account_asset a
      LEFT JOIN childs on a.id=childs.parent_id
          WHERE aa.id=a.id
-    """)
+    """
+    )
 
     # Acquisition date should be coming from the related move lines.
-    cr.execute("""
+    cr.execute(
+        """
         UPDATE account_asset aa
            SET acquisition_date = aml.acq_date
           FROM (
@@ -27,12 +29,13 @@ def migrate(cr, version):
                 GROUP BY asset_id
           ) AS aml
          WHERE aa.id = aml.asset_id AND aa.acquisition_date IS NULL
-    """)
+    """
+    )
 
     # fallback to prorata, first_depreciation, create date if there is no move line.
     # https://github.com/odoo/enterprise/blob/ef1286f6f96c998db5697806e691193b5eb1c9cf/account_asset/models/account_asset.py#L118
     cr.execute(
-      """
+        """
         UPDATE account_asset
            SET acquisition_date = COALESCE(prorata_date, first_depreciation_date, create_date)
          WHERE acquisition_date IS NULL
