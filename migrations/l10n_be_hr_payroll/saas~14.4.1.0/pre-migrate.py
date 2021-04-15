16e775c148142e5a2ba4309701510cd2eebf3981
Yannick Tivisse <yti@odoo.com>
2021-04-15 16:05:52 +0000

    [IMP] l10n_be_hr_payroll/saas~14.4: store 4 declaration wizards
    Note: There is no record to migrate, and the new models are different
    with new fields, and removed fields, so let's the ORM recreate everything
    properly.
    
    Ent PR: odoo/enterprise#17657
    
    closes odoo/upgrade#2394
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/l10n_be_hr_payroll/saas~14.4.1.0/pre-migrate.py
+++ migrations/l10n_be_hr_payroll/saas~14.4.1.0/pre-migrate.py
@@ -7,3 +7,10 @@ def migrate(cr, version):
     util.delete_unused(cr, "l10n_be_hr_payroll.cp200_pfi_reim_travel")
     util.delete_unused(cr, "l10n_be_hr_payroll.cp200_employees_double_holiday_reim_travel")
     util.remove_field(cr, "hr.contract", "others_reimbursed_amount")
+
+    util.remove_model(cr, "hr.payroll.281.base.wizard")
+    util.remove_model(cr, "hr.payroll.281.10.wizard")
+    util.remove_model(cr, "hr.payroll.281.45.wizard")
+    util.remove_model(cr, "l10n.be.withholding.tax.exemption")
+    util.remove_model(cr, "l10n.be.withholding.tax.exemption.line")
+    util.remove_model(cr, "l10n.be.withholding.tax.ip.declaration")
