51389925d8ecdfd2c092b9a431a5f8842b0cd073
Christophe Simonis <chs@odoo.com>
2021-04-20 18:10:22 +0200

    [FIX] hr_attendance/saas~14.3

--- migrations/hr_attendance/saas~14.3.2.0/pre-migrate.py
+++ migrations/hr_attendance/saas~14.3.2.0/pre-migrate.py
@@ -3,8 +3,11 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.update_record_from_xml(cr, "hr_attendance.menu_hr_attendance_attendances_overview")
-    util.remove_menus(cr, [
-        util.ref(cr, "hr_attendance.menu_hr_attendance_view_employees_kanban"),
-        util.ref(cr, "hr_attendance.menu_hr_attendance_manage_attendances"),
-    ])
+    util.update_record_from_xml(cr, "hr_attendance.menu_hr_attendance_view_attendances")
+    util.remove_menus(
+        cr,
+        [
+            util.ref(cr, "hr_attendance.menu_hr_attendance_view_employees_kanban"),
+            util.ref(cr, "hr_attendance.menu_hr_attendance_manage_attendances"),
+        ],
+    )
