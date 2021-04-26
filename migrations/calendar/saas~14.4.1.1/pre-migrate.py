e65945b121e1914301f98a06d10801cd44578cb2
nounoubensebia <neb@odoo.com>
2021-04-26 10:56:31 +0000

    [FIX] calendar/saas~14.4
    Related: https://github.com/odoo/odoo/pull/69846
    
    Task-2514473
    
    closes odoo/upgrade#2423
    
    Related: odoo/odoo#69846
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/calendar/saas~14.4.1.1/pre-migrate.py
+++ migrations/calendar/saas~14.4.1.1/pre-migrate.py
@@ -5,3 +5,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "calendar_contacts", "partner_checked", "bool", default=True)
+    util.move_field_to_module(cr, "res.partner", "meeting_ids", "crm", "calendar")
+    util.move_field_to_module(cr, "res.partner", "meeting_count", "crm", "calendar")
