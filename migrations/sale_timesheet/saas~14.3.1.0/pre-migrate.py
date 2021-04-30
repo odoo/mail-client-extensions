e38076da051ada70ef4b984dab65846c87280d0f
Christophe Simonis <chs@odoo.com>
2021-04-30 15:48:46 +0200

    [FIX] sale_timesheet/saas~14.3

--- migrations/sale_timesheet/saas~14.3.1.0/pre-migrate.py
+++ migrations/sale_timesheet/saas~14.3.1.0/pre-migrate.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.create_column(cr, "account_analytic_line", "order_id", "int4")
 
-    cr.execute("""
+    query = """
         UPDATE account_analytic_line l
            SET order_id = sol.order_id
           FROM sale_order_line sol
          WHERE sol.id = l.so_line
-    """)
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="account_analytic_line", prefix="l."))
 
     if util.module_installed(cr, "industry_fsm_sale"):
         util.move_field_to_module(
