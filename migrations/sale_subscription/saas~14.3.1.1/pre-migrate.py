577d0b975be57c154b37aca9143fe30e4580f6b4
Nicolas Seinlet <nse@openerp.com>
2021-05-05 09:43:49 +0200

    [FIX] sale_subscription/saas~14.3

--- migrations/sale_subscription/saas~14.3.1.1/pre-migrate.py
+++ migrations/sale_subscription/saas~14.3.1.1/pre-migrate.py
@@ -7,14 +7,13 @@ def migrate(cr, version):
     util.create_column(cr, "sale_subscription", "source_id", "integer")
     util.create_column(cr, "sale_subscription", "medium_id", "integer")
 
-    cr.execute(
-        """
-            UPDATE sale_subscription
+    query = """
+            UPDATE sale_subscription sub
                SET campaign_id = so.campaign_id,
                    source_id = so.source_id,
                    medium_id = so.medium_id
               FROM sale_order_line sol
               JOIN sale_order so ON sol.order_id = so.id
-             WHERE sale_subscription.id = sol.subscription_id
+             WHERE sub.id = sol.subscription_id
         """
-    )
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="sale_subscription", prefix="sub."))
