458d4d4e922c5029015f381564baa442e1f671a4
Christophe Simonis <chs@odoo.com>
2021-05-10 18:18:04 +0200

    [FIX] planning/saas~14.3

--- migrations/planning/saas~14.3.1.0/pre-migrate.py
+++ migrations/planning/saas~14.3.1.0/pre-migrate.py
@@ -3,4 +3,4 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.create_column(cr, "res_company", "planning_self_unassign_days_before", "integer")
+    util.create_column(cr, "res_company", "planning_self_unassign_days_before", "integer", default=0)
