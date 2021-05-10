143ee1e167146a7fec5edbdd12ea1bb676b9f945
Alvaro Fuentes <afu@odoo.com>
2021-05-10 07:50:59 +0000

    [FIX] stock/saas~13.4.1.1: deactivate cyclic rules
    When there is a cyclic reordering rule for a product on an orderpoint
    the computation of qty_to_order fails with
    ```
    Traceback (most recent call last):
      ...
      File "/home/odoo/src/odoo/14.0/addons/stock/models/product.py", line 563, in _get_rules_from_location
        return self._get_rules_from_location(rule.location_src_id, seen_rules=seen_rules | rule)
      File "/home/odoo/src/odoo/14.0/addons/stock/models/product.py", line 563, in _get_rules_from_location
        return self._get_rules_from_location(rule.location_src_id, seen_rules=seen_rules | rule)
      [Previous line repeated 942 more times]
      File "/home/odoo/src/odoo/14.0/addons/stock/models/product.py", line 556, in _get_rules_from_location
        'warehouse_id': location.get_warehouse()
      File "/home/odoo/src/odoo/14.0/addons/stock/models/stock_rule.py", line 463, in _get_rule
        result = self._search_rule(values.get('route_ids', False), product_id, values.get('warehouse_id', False), domain)
      File "/home/odoo/src/odoo/14.0/addons/stock/models/stock_rule.py", line 447, in _search_rule
        res = Rule.search(expression.AND([[('route_id', 'in', product_routes.ids)], domain]), order='route_sequence, sequence', limit=1)
      File "/home/odoo/src/odoo/14.0/odoo/models.py", line 1708, in search
        res = self._search(args, offset=offset, limit=limit, order=order, count=count)
      File "/home/odoo/src/odoo/14.0/odoo/models.py", line 4494, in _search
        query = self._where_calc(args)
      File "/home/odoo/src/odoo/14.0/odoo/models.py", line 4250, in _where_calc
        return expression.expression(domain, self).query
      File "/home/odoo/src/odoo/14.0/odoo/osv/expression.py", line 430, in __init__
        self._unaccent = get_unaccent_wrapper(model._cr)
      File "/home/odoo/src/odoo/14.0/odoo/osv/expression.py", line 404, in get_unaccent_wrapper
        if odoo.registry(cr.dbname).has_unaccent:
      File "/home/odoo/src/odoo/14.0/odoo/__init__.py", line 104, in registry
        return modules.registry.Registry(database_name)
      File "/home/odoo/src/odoo/14.0/odoo/modules/registry.py", line 63, in __new__
        return cls.registries[db_name]
      File "/home/odoo/src/odoo/14.0/odoo/tools/func.py", line 71, in wrapper
        return func(self, *args, **kwargs)
    RecursionError: maximum recursion depth exceeded
    ```
    
    upg-13567
    upg-11622
    upg-9569
    
    closes odoo/upgrade#2451
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/stock/saas~13.4.1.1/end-migrate.py
+++ migrations/stock/saas~13.4.1.1/end-migrate.py
@@ -1,8 +1,69 @@
 # -*- coding: utf-8 -*-
+from collections import namedtuple
+
 from odoo.upgrade import util
+
+
+def clean_recursive_rules(env):
+    # unraveling odoo/odoo/blob/9a4e792c38124e1c3902b37ea4604a17788c7710/addons/stock/models/product.py#L551
+    def get_recursive_rules(product, location, route_ids=False, seen_rules=False):
+        if not seen_rules:
+            seen_rules = env["stock.rule"]
+        rule = env["procurement.group"]._get_rule(
+            product, location, {"route_ids": route_ids, "warehouse_id": location.get_warehouse()}
+        )
+        if not rule:
+            return False
+        if rule in seen_rules:
+            return rule
+        if rule.procure_method == "make_to_stock" or rule.action not in ("pull_push", "pull"):
+            return False
+        else:
+            return get_recursive_rules(product, rule.location_src_id, seen_rules=(seen_rules | rule))
+
+    RuleInfo = namedtuple("RuleInfo", "rid desc pid pname")
+    info = []
+    env.cr.execute("SELECT id FROM stock_warehouse_orderpoint")
+    ids = [x[0] for x in env.cr.fetchall()]
+    for orderpoint in util.iter_browse(env["stock.warehouse.orderpoint"], ids):
+        product = orderpoint.product_id
+        location = orderpoint.location_id
+        if not product or not location:
+            continue
+        rule = get_recursive_rules(product, location, orderpoint.route_id)
+        if rule:
+            info.append(RuleInfo(rule.id, rule.rule_message, product.id, product.name))
+            rule.active = False
+
+    if not info:
+        return
+
+    # report the changes to the user
+    util.add_to_migration_reports(
+        f"""
+        <details>
+        <summary>
+        Cyclic reordering rules were disabled during the upgrade. If you still need them please check and fix them
+        manually.
+        </summary>
+        <h4>Disabled rules:</h4>
+        <ul>
+        {"".join(f"<li>Rule(id={r.rid}) for Product(id={r.pid}, name={r.pname}):<br/>{r.desc}</li>" for r in info)}
+        </ul>
+        </details>
+        """,
+        "Stock",
+        format="html",
+    )
 
 
 def migrate(cr, version):
     env = util.env(cr)
-    util.recompute_fields(cr, "stock.warehouse.orderpoint", ["qty_to_order"])
+    try:
+        util.recompute_fields(cr, "stock.warehouse.orderpoint", ["qty_to_order"])
+    except RecursionError:
+        env.clear()  # must do this since the ORM left things half-baked from the exception above
+        clean_recursive_rules(env)
+        util.recompute_fields(cr, "stock.warehouse.orderpoint", ["qty_to_order"])
+
     env["stock.warehouse"]._check_multiwarehouse_group()
