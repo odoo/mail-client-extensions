06fccf90374d47212437e61c288f833274be6522
Christophe Simonis <chs@odoo.com>
2021-05-11 10:06:09 +0200

    [chore] account/10.saas~14: blacken file

--- migrations/account/10.saas~14.1.1/pre-settings.py
+++ migrations/account/10.saas~14.1.1/pre-settings.py
@@ -2,9 +2,10 @@
 from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
-    util.remove_view(cr, 'account.view_account_config_settings')
+    util.remove_view(cr, "account.view_account_config_settings")
 
-    fields = util.splitlines("""
+    fields = util.splitlines(
+        """
         has_default_company
         excpects_chart_of_accounts
         company_footer
@@ -27,21 +28,23 @@ def migrate(cr, version):
 
         # this field change type (0/1 selection -> boolean)
         group_warning_account
-    """)
+        """
+    )
     for f in fields:
-        util.remove_field(cr, 'account.config.settings', f)
+        util.remove_field(cr, "account.config.settings", f)
 
-    moved = util.splitlines("""
+    moved = util.splitlines(
+        """
         use_anglo_saxon
         transfert_account_id
         tax_cash_basis_journal_id
         fiscalyear_last_day
         fiscalyear_last_month
-    """)
-    if util.module_installed(cr, 'account_accountant'):
+        """
+    )
+    if util.module_installed(cr, "account_accountant"):
         for m in moved:
-            util.move_field_to_module(cr, 'account.config.settings', m,
-                                      'account', 'account_accountant')
+            util.move_field_to_module(cr, "account.config.settings", m, "account", "account_accountant")
     else:
         for m in moved:
-            util.remove_field(cr, 'account.config.settings', m)
+            util.remove_field(cr, "account.config.settings", m)
