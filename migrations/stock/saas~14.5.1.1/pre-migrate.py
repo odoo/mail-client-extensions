dad2f3f85c550a6ab6968a98a1cf1ec5434bcd59
Tiffany Chang (tic) <tic@odoo.com>
2021-05-11 11:28:54 +0000

    [IMP] stock/saas~14.5: add new settings for reception report
    setting 1: display reception report to user
    setting 2: make reception report auto-open when picking is validated
    
    Task: 2500844
    COM PR: odoo/odoo#70669
    ENT PR: odoo/enterprise#18268
    
    closes odoo/upgrade#2731
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/stock/saas~14.5.1.1/pre-migrate.py
+++ migrations/stock/saas~14.5.1.1/pre-migrate.py
@@ -7,3 +7,5 @@ def migrate(cr, version):
     util.remove_field(cr, "stock.warehouse.orderpoint", "allowed_route_ids")
 
     util.remove_field(cr, "stock.move", "has_move_lines")
+    util.create_column(cr, "res_config_settings", "group_stock_reception_report", "boolean")
+    util.create_column(cr, "res_config_settings", "group_stock_auto_reception_report", "boolean")
