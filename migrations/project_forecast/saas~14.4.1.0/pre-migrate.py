c3e856b250398e6deede6e9218152c43d54c2299
Mahendra Barad <mba@odoo.com>
2021-05-12 13:35:08 +0000

    [IMP] project_forecast@saas~14.4: remove unused view and field
    Purpose of the commit is to remove unused view and field from
    project_forecst
    
    Related PR: https://github.com/odoo/enterprise/pull/18143
    
    closes odoo/upgrade#2458
    
    Taskid: 2523446
    Related: odoo/enterprise#18143
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/project_forecast/saas~14.4.1.0/pre-migrate.py
+++ migrations/project_forecast/saas~14.4.1.0/pre-migrate.py
@@ -3,5 +3,7 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.update_record_from_xml(cr, "planning.planning_rule_internal_user_read", from_module="project_forecast")
-    util.update_record_from_xml(cr, "planning.planning_rule_user_is_published", from_module="project_forecast")
+    util.update_record_from_xml(cr, "planning.planning_rule_internal_user_read", from_module="planning")
+    util.update_record_from_xml(cr, "planning.planning_rule_user_is_published", from_module="planning")
+    util.remove_view(cr, "project_forecast.planning_view_form_in_gantt")
+    util.remove_field(cr, "planning.slot", "is_private_project")
