b88b6df93f2d579c3f220b9e1ccdb4db46eb3fcc
Christophe Simonis <chs@odoo.com>
2021-05-21 10:09:18 +0200

    [chore] base/{11.0,saas~11.5},point_of_sale/7.saas~5: format files

--- migrations/base/11.0.1.3/pre-10-modules.py
+++ migrations/base/11.0.1.3/pre-10-modules.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 from odoo.addons.base.maintenance.migrations import util
 
+
 def migrate(cr, version):
-    util.new_module(cr, 'iap', deps={'web'}, auto_install=True)
+    util.new_module(cr, "iap", deps={"web"}, auto_install=True)
 
     if not util.has_enterprise():
         # moved from enterprise \o/
-        util.new_module(cr, 'sms', deps={'iap', 'mail'}, auto_install=True)
-        util.new_module(cr, 'calendar_sms', deps={'calendar', 'sms'}, auto_install=True)
+        util.new_module(cr, "sms", deps={"iap", "mail"}, auto_install=True)
+        util.new_module(cr, "calendar_sms", deps={"calendar", "sms"}, auto_install=True)
     else:
-        util.new_module_dep(cr, 'account_accountant', 'account_reports')
-        util.new_module_dep(cr, 'sms', 'iap')
-        util.remove_module(cr, 'sms_fortytwo')
+        util.new_module_dep(cr, "account_accountant", "account_reports")
+        util.new_module_dep(cr, "sms", "iap")
+        util.remove_module(cr, "sms_fortytwo")
 
-        util.new_module(cr, 'website_sale_taxcloud_delivery',
-                        deps=('website_sale_delivery', 'website_sale_account_taxcloud'),
-                        auto_install=True)
+        util.new_module(
+            cr,
+            "website_sale_taxcloud_delivery",
+            deps=("website_sale_delivery", "website_sale_account_taxcloud"),
+            auto_install=True,
+        )
         util.new_module(cr, "l10n_uk_reports_hmrc", deps={"l10n_uk_reports"}, auto_install=True)
 
         # https://github.com/odoo/enterprise/pull/6291
