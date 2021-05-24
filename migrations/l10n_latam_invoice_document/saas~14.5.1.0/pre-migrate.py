fcf611ea5a51ff013000539b6b5f03049345df28
Nikunj Ladava <nla@odoo.com>
2021-05-24 07:11:19 +0000

    [IMP] l10n_latam_invoice_document@saas~14.5: remove unused view and field
    Purpose of the commit is to remove unused view and field from
    l10n_latam_invoice_document
    
    Related PR: https://github.com/odoo/odoo/pull/71095
    
    closes odoo/upgrade#2478
    
    Taskid: 2427849
    Related: odoo/odoo#71095
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/l10n_latam_invoice_document/saas~14.5.1.0/pre-migrate.py
+++ migrations/l10n_latam_invoice_document/saas~14.5.1.0/pre-migrate.py
@@ -6,3 +6,9 @@ def migrate(cr, version):
     eb = util.expand_braces
     util.rename_xmlid(cr, *eb("l10n_latam_invoice_document.external_layout_{clean,bold}"))
     util.rename_xmlid(cr, *eb("l10n_latam_invoice_document.external_layout_{background,striped}"))
+
+    util.remove_view(cr, "l10n_latam_invoice_document.sequence_view")
+    util.remove_view(cr, "l10n_latam_invoice_document.sequence_view_tree")
+    util.remove_view(cr, "l10n_latam_invoice_document.view_sequence_search")
+
+    util.remove_field(cr, "ir.sequence", "l10n_latam_document_type_id")
