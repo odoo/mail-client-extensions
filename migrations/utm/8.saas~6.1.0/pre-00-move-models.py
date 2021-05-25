08e3ccbd586a535c022aa7174ba478fe088e530d
Christophe Simonis <chs@odoo.com>
2021-05-25 17:12:56 +0000

    [FIX] utm/8.saas~6: only move models if `crm` is installed
    closes odoo/upgrade#2462
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/utm/8.saas~6.1.0/pre-00-move-models.py
+++ migrations/utm/8.saas~6.1.0/pre-00-move-models.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from openerp.addons.base.maintenance.migrations import util
 
-def migrate(cr, version):
-    for model in 'medium campaign source mixin'.split():
-        rename_table = model != 'mixin'     # because it's an abstract model
-        util.rename_model(cr, 'crm.tracking.%s' % model, 'utm.%s' % model, rename_table)
-        util.move_model(cr, 'utm.%s' % model, 'crm', 'utm', move_data=True)
 
-    xids = filter(None, """
+def migrate(cr, version):
+    if not util.module_installed(cr, "crm"):
+        return
+    for model in "medium campaign source mixin".split():
+        rename_table = model != "mixin"  # because it's an abstract model
+        util.rename_model(cr, "crm.tracking.%s" % model, "utm.%s" % model, rename_table)
+        util.move_model(cr, "utm.%s" % model, "crm", "utm", move_data=True)
+
+    xids = """
         {crm,utm}_source_search_engine
         {crm,utm}_source_mailing
         {crm,utm}_source_newsletter
@@ -28,7 +31,7 @@ def migrate(cr, version):
         {crm_tracking,utm}_source_view_form
         {crm_tracking,utm}_source_action
         menu_{crm_tracking,utm}_source
-    """.strip().split())
+    """
 
-    for m in xids:
-        util.rename_xmlid(cr, *util.expand_braces('utm.' + m))
+    for m in util.splitlines(xids):
+        util.rename_xmlid(cr, *util.expand_braces("utm." + m))
