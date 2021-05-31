a268f660748fb66ba07e7cf9eec2d8744f7ad3a7
Kevin Baptiste <kba@odoo.com>
2021-05-31 09:18:06 +0000

    [IMP] pos_adyen/saas~14.4: Odoo Payments changes
    Remove `adyen.payout` and link `adyen.transaction` to POS order.
    
    odoo/odoo#68952

---
+++ migrations/pos_adyen/saas~14.4.1.0/pre-migrate.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "pos.payment.method", "adyen_payout_id")
+    util.remove_record(cr, "pos_adyen.access_adyen_payout_group_pos_manager")
+
+    util.create_column(cr, "adyen_transaction", "pos_payment_id", "int4")
+    util.create_column(cr, "adyen_transaction", "pos_order_id", "int4")
