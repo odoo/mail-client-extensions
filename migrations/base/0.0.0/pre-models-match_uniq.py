61152c84ee5820bc31aaba8afe3542fc21fc89d5
Denis Ledoux <dle@odoo.com>
2021-06-01 12:17:29 +0000

    [FIX] base@0.0.0: avoid `cr.savepoint` in `_match_uniq`
    As an effort to solve the performance issue
    related to open more than 64 savepoints in the same transaction,
    see https://github.com/odoo/odoo/pull/71395#issuecomment-851440042
    
    When creating a record for which the model is tagged `_match_uniq`,
    instead of asking for forgiveness in this case,
    search in advance the record before letting it be created,
    otherwise we can land in situation having hundreds of savepoints created,
    for instance when loading `res.country.state` files,
    creating hundred of country states in the same file.
    
    closes odoo/upgrade#2503
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/base/0.0.0/pre-models-match_uniq.py
+++ migrations/base/0.0.0/pre-models-match_uniq.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from psycopg2 import IntegrityError
-
 from odoo import api, models
 
 from odoo.addons.base.maintenance.migrations import util
@@ -20,52 +18,55 @@ class Base(models.AbstractModel):
 
     @api.model
     def create(self, values):
-        if not getattr(self, "_match_uniq", False):
+        if not getattr(self, "_match_uniq", False) or self.pool.ready:
             return super(Base, self).create(values)
 
-        try:
-            with self.env.cr.savepoint():
-                return super(Base, self).create(values)
-        except IntegrityError as e:
-            if e.pgcode == "23505" and not self.pool.ready:
-                self.env.cr.execute(
-                    """
-                    SELECT a.attname
-                      FROM pg_index i
-                      JOIN pg_attribute a ON a.attrelid = i.indrelid
-                                         AND a.attnum = ANY(i.indkey)
-                     WHERE i.indexrelid = %s::regclass
-                """,
-                    [e.diag.constraint_name],
-                )
-                constraint_fields = [f for f, in self.env.cr.fetchall()]
-                if constraint_fields:
-                    if isinstance(values, list):
-                        assert len(values) == 1
-                        values = values[0]
-                    default, missing = {}, [f for f in constraint_fields if f not in values]
-                    if missing:
-                        default = self.default_get(missing)
-                    domain = [(f, "=", values.get(f, default.get(f, False))) for f in constraint_fields]
-                    record = self.with_context(active_test=False).search(domain, limit=1)
-                    if record:
-                        xmlid = record.get_metadata()[0]["xmlid"]
-                        if xmlid and not xmlid.startswith("__export__."):
-                            # XXX raise?
-                            _logger.warning("Matching record %r has XMLID %r. Missing rename?", record, xmlid)
-                        store_values = {key: value for key, value in values.items() if record._fields[key].column_type}
-                        other_values = {key: value for key, value in values.items() if key not in store_values}
-                        record._write(store_values)
-                        record.write(other_values)
+        table = util.table_of_model(self.env.cr, self._name)
+        self.env.cr.execute(
+            """
+                  SELECT array_agg(a.attname)
+                    FROM pg_index i
+                    JOIN pg_attribute a ON a.attrelid = i.indrelid
+                                       AND a.attnum = ANY(i.indkey)
+                   WHERE i.indrelid = %s::regclass
+                     AND i.indisunique
+                     AND not i.indisprimary
+                GROUP BY i.indexrelid
+        """,
+            [table],
+        )
+        for [constraint_fields] in self.env.cr.fetchall():
+            if isinstance(values, list):
+                assert len(values) == 1
+                values = values[0]
+            missing = [f for f in constraint_fields if f not in values]
+            if missing:
+                values.update(self.default_get(missing))
+            if any(f not in values for f in constraint_fields):
+                continue
+            domain = [(f, "=", values[f]) for f in constraint_fields]
+            record = self.with_context(active_test=False).search(domain, limit=1)
+            if record:
+                xmlid = record.get_metadata()[0]["xmlid"]
+                if xmlid and not xmlid.startswith("__export__."):
+                    # XXX raise?
+                    _logger.warning(
+                        "Matching record %r using domain %r has XMLID %r. Missing rename?", record, domain, xmlid
+                    )
+                store_values = {key: value for key, value in values.items() if record._fields[key].column_type}
+                other_values = {key: value for key, value in values.items() if key not in store_values}
+                record._write(store_values)
+                record.write(other_values)
 
-                        match_warning = getattr(self, "_match_uniq_warning", False)
-                        if match_warning:
-                            util.add_to_migration_reports(
-                                message=match_warning.format(xmlid=xmlid, **values), category="Merged Records"
-                            )
+                match_warning = getattr(self, "_match_uniq_warning", False)
+                if match_warning:
+                    util.add_to_migration_reports(
+                        message=match_warning.format(xmlid=xmlid, **values), category="Merged Records"
+                    )
 
-                        return record
-            raise
+                return record
+
+        return super(Base, self).create(values)
 
 
 for model in {
