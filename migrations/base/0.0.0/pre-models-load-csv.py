61a3da2e3c4d9eb8c6fe848329e5817e27e6871a
Denis Ledoux <dle@odoo.com>
2021-06-01 14:12:18 +0000

    [FIX] base@0.0.0: avoid using savepoint on CSV file loading
    As an effort to solve the performance issue
    related to open more than 64 savepoints in the same transaction
    See https://github.com/odoo/odoo/pull/71395#issuecomment-851440042
    
    When loading module data from CSV file,
    load them without the overhard mechanism for the import feature,
    using savepoints to tell the user when there is a problem with
    his CSV import.
    As this is a CSV coming from a module, it's not useful.
    Besides, there is no reason to treat data from CSV file
    a different way than data from XML file
    (using savepoints, rollback in case of error, etc.)
    
    closes odoo/upgrade#2504
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

---
+++ migrations/base/0.0.0/pre-models-load-csv.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+from odoo.models import fix_import_export_id_paths
+
+
+def migrate(cr, version):
+    pass
+
+
+class Base(models.AbstractModel):
+    _inherit = "base"
+    _module = "base"
+
+    def load(self, fields, data):
+        # Avoid the overhead from the import feature we don't need to load module's CSV files
+        # Specifically, avoid the savepoints `model_load` and `model_load_save` which are useless
+        # for the load of module's CSV file, and cause performance issue
+        # because more than 64 savepoints can be created during the same transaction.
+        # There is no reason data from CSV file are not treated as data from XML files
+        if not self._context.get("install_module"):
+            return super(Base, self).load(fields, data)
+
+        current_module = self._context.get("module", "__import__")
+        mode = self._context.get("mode", "init")
+        noupdate = self._context.get("noupdate", False)
+
+        fields = [fix_import_export_id_paths(f) for f in fields]
+        # In case the xmlid for a many2one doesn't include its module in the xmlid
+        data = [
+            [
+                "%s.%s" % (current_module, value)
+                if value and len(field) == 2 and field[1] == "id" and "." not in value
+                else value
+                for field, value in zip(fields, d)
+            ]
+            for d in data
+        ]
+        extracted = self._extract_records(fields, data)
+        converted = self._convert_records(extracted)
+        ids = []
+        if hasattr(self, "_load_records"):
+            data_list = [
+                {
+                    "xml_id": xid if "." in xid else "%s.%s" % (current_module, xid),
+                    "values": vals,
+                    "info": info,
+                    "noupdate": noupdate,
+                }
+                for _id, xid, vals, info in converted
+            ]
+            records = self._load_records(data_list, mode == "update")
+            ids.extend(records.ids)
+        else:
+            for _id, xid, vals, _info in converted:
+                ids.append(
+                    self.env["ir.model.data"]._update(
+                        self._name, current_module, vals, mode=mode, xml_id=xid, noupdate=noupdate, res_id=_id
+                    )
+                )
+        return {
+            "ids": ids,
+            "messages": [],
+            "nextrow": 0,
+        }
