9ffeba897060fcbac1a35ea3261e3c83ec190088
Rémy Baranx (bar) <bar@odoo.com>
2021-06-02 06:39:12 +0000

    [FIX] mrp/saas~11.5: create missing procurement groups
    In Odoo `11.0`, when several MO were chained, the same procurement group
    was used for the whole chain.
    From Odoo `12.0` a new procurement group is created for each MO and linked
    elements such as stock moves. (see odoo/odoo@32fbf98).
    
    In Odoo `14.0` some smart buttons displaying children MO, sources MO and
    backorders MO have been added on the MO form. If a MO chain created in 11.0
    is not updated to have one procurement group per MO, these smart buttons
    display wrong values as they are based on procurement group (in fact,
    the same value is displayed for all the MO of the chain).
    
    This script parses MO chains, creates new procurements and updates related
    elements.
    
    request-59766
    
    closes odoo/upgrade#2418
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/mrp/saas~11.5.2.0/post-procurement-group.py
+++ migrations/mrp/saas~11.5.2.0/post-procurement-group.py
@@ -3,23 +3,110 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    if util.module_installed(cr, "sale_stock"):
-        env = util.env(cr)
-        cr.execute(
-            """
-            SELECT m.id,m.name
-              FROM mrp_production m
-              JOIN procurement_group pg on m.procurement_group_id = pg.id
-             WHERE pg.sale_id IS NOT NULL
+    # Get MOs linked to bill of materials
+    cr.execute(
         """
+          SELECT sm.raw_material_production_id
+            FROM stock_move sm
+            JOIN stock_move p ON p.production_id = sm.raw_material_production_id
+        GROUP BY sm.raw_material_production_id
+        """
+    )
+    mo_ids = tuple([id for id, in cr.fetchall()])
+
+    # we should create a new procurement group for each MO in the chain and update related elements.
+    if mo_ids:
+        util.create_column(cr, "procurement_group", "_tmp", "int4")
+
+        extra_move_cond = "AND sm.purchase_line_id IS NULL" if util.modules_installed(cr, "purchase") else ""
+        fields, pg_fields = map(
+            ",".join,
+            util.get_columns(cr, "procurement_group", ignore=("id", "name", "sale_id", "_tmp"), extra_prefixes=["pg"]),
         )
-        for mid, mo_name in cr.fetchall():
-            procurement_group_id = env["procurement.group"].create({"name": mo_name})
+        cr.execute(
+            f"""
+            INSERT INTO procurement_group (name, _tmp, {fields})
+                 SELECT mo.name, mo.id, {pg_fields}
+                   FROM mrp_production mo
+                   JOIN procurement_group pg ON pg.id = mo.procurement_group_id
+                  WHERE mo.id IN %s
+              RETURNING id
+            """,
+            (mo_ids,),
+        )
+        new_pg_ids = tuple([id for id, in cr.fetchall()])
+
+        # update MOs to use the new procurement groups
+        if new_pg_ids:
             cr.execute(
                 """
-                UPDATE  mrp_production m
-                  SET procurement_group_id=%s
-                WHERE id=%s
-            """,
-                [procurement_group_id.id, mid],
+                   UPDATE mrp_production mo
+                      SET procurement_group_id = pg.id
+                     FROM procurement_group pg
+                    WHERE mo.id = pg._tmp
+                      AND pg.id IN %s
+                RETURNING mo.id
+                """,
+                (new_pg_ids,),
             )
+            updated_mo_ids = tuple([id for id, in cr.fetchall()])
+
+            # update stock moves and related models to use the new procurement groups
+            if updated_mo_ids:
+                cr.execute(
+                    f"""
+                       UPDATE stock_move sm
+                          SET group_id = mo.procurement_group_id,
+                              name = mo.name
+                         FROM mrp_production mo
+                        WHERE (sm.raw_material_production_id = mo.id OR sm.production_id = mo.id)
+                          AND mo.id IN %s
+                          {extra_move_cond}
+                    RETURNING sm.id
+                    """,
+                    (updated_mo_ids,),
+                )
+                updated_move_ids = tuple([id for id, in cr.fetchall()])
+
+                cr.execute(
+                    f"""
+                    WITH RECURSIVE moves(id, group_id, picking_id) AS (
+                            SELECT sm.id, dm.group_id, sm.picking_id
+                              FROM stock_move sm
+                              JOIN stock_move_move_rel r ON r.move_orig_id = sm.id
+                              JOIN stock_move dm ON dm.id = r.move_dest_id
+                             WHERE dm.id IN %s
+                                   {extra_move_cond}
+                        UNION
+                            SELECT sm.id, m.group_id, sm.picking_id
+                              FROM stock_move sm
+                              JOIN stock_move_move_rel r ON r.move_orig_id = sm.id
+                              JOIN moves m ON m.id = r.move_dest_id
+                             WHERE true
+                                   {extra_move_cond}
+                    ),
+                    _upg AS (
+                        UPDATE stock_picking sp
+                           SET group_id = m.group_id
+                          FROM moves m
+                         WHERE sp.id = m.picking_id
+                    )
+                    UPDATE stock_move sm
+                       SET group_id = m.group_id
+                      FROM moves m
+                     WHERE sm.id = m.id
+                    """,
+                    (updated_move_ids,),
+                )
+                cr.execute(
+                    """
+                    UPDATE stock_picking p
+                       SET group_id = sm.group_id
+                      FROM stock_move sm
+                     WHERE p.id = sm.picking_id
+                       AND sm.id IN %s
+                    """,
+                    [updated_move_ids],
+                )
+
+        util.remove_column(cr, "procurement_group", "_tmp")
