b3b6da38d79ad5a540c32a57f8c6bb04989027ea
William Braeckman <wbr@odoo.com>
2021-06-04 10:06:38 +0000

    [IMP] hr_referral@saas~14.5: Allow users to dismiss alerts
    Adds an m2m containing the users which have dismissed the alert.
    
    See odoo/enterprise#18857
    
    Task ID: 2415337
    
    closes odoo/upgrade#2561
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/hr_referral/saas~14.5.1.0/pre-migrate.py
+++ migrations/hr_referral/saas~14.5.1.0/pre-migrate.py
@@ -6,3 +6,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.rename_field(cr, "hr.recruitment.stage", "not_hired_stage", "use_in_referral")
     cr.execute("UPDATE hr_recruitment_stage SET use_in_referral = NOT COALESCE(use_in_referral, false)")
+
+    util.create_m2m(cr, "hr_referral_alert_users_rel", "hr_referral_alert", "res_users", "alert_id", "user_id")
