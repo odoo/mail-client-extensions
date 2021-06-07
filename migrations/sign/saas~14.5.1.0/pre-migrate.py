a1cf6c16809c74c12a0740fa8e16fa66e0465f2b
Chong Wang (cwg) <cwg@odoo.com>
2021-06-07 13:19:43 +0000

    [IMP] sign/saas~14.5: add subject and attachments to sign request
    TaskID:2496596
    
    See odoo/enterprise#18633
    
    closes odoo/upgrade#2515
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/sign/saas~14.5.1.0/pre-migrate.py
+++ migrations/sign/saas~14.5.1.0/pre-migrate.py
@@ -5,6 +5,17 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "sign.sign_template_mail_follower")
 
+    util.create_column(cr, "sign_request", "subject", "varchar")
     util.create_column(cr, "sign_request", "message", "text")
     util.create_column(cr, "sign_request", "message_cc", "text")
     util.create_column(cr, "sign_send_request", "message_cc", "text")
+
+    cr.execute(
+        """
+        UPDATE sign_request
+           SET subject = 'Signature Request - ' || ia.name
+          FROM sign_template st
+          JOIN ir_attachment ia on st.attachment_id = ia.id
+         WHERE sign_request.template_id = st.id
+        """
+    )
