46d6797b139a398537201aaba08d5d99effa17fc
William Braeckman <wbr@odoo.com>
2021-06-08 14:05:12 +0000

    [IMP] fleet/saas~14.5: Improve general UX (Back2basics)
    Adds more information to copy from the model to the vehicle.
    
    See odoo/odoo#72943
    See odoo/enterprise#19356
    
    Task ID: 2468228
    
    closes odoo/upgrade#2601
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/fleet/saas~14.5.0.1/pre-migrate.py
+++ migrations/fleet/saas~14.5.0.1/pre-migrate.py
@@ -11,3 +11,18 @@ def migrate(cr, version):
     util.create_column(cr, "fleet_vehicle", "trailer_hook", "bool", default=False)
 
     util.create_column(cr, "fleet_vehicle", "fleet_id", "integer")
+
+    util.create_column(cr, "fleet_vehicle_model", "model_year", "int4")
+    util.create_column(cr, "fleet_vehicle_model", "color", "varchar")
+    util.create_column(cr, "fleet_vehicle_model", "seats", "int4")
+    util.create_column(cr, "fleet_vehicle_model", "doors", "int4")
+    util.create_column(cr, "fleet_vehicle_model", "trailer_hook", "bool", default=False)
+    util.move_field_to_module(cr, "fleet.vehicle.model", "default_co2", "l10n_be_hr_payroll_fleet", "fleet")
+    util.create_column(cr, "fleet_vehicle_model", "co2_standard", "varchar")
+    util.move_field_to_module(cr, "fleet.vehicle.model", "default_fuel_type", "l10n_be_hr_payroll_fleet", "fleet")
+    util.create_column(cr, "fleet_vehicle_model", "power", "int4")
+    util.create_column(cr, "fleet_vehicle_model", "horsepower", "int4")
+    util.create_column(cr, "fleet_vehicle_model", "horsepower_tax", "float")
+    util.create_column(cr, "fleet_vehicle_model", "electric_assistance", "bool", default=False)
+
+    util.create_column(cr, "fleet_vehicle", "co2_standard", "varchar")
