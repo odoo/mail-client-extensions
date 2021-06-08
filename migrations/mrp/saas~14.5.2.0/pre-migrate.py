c8e38afee6a4c2b76d72131d2d9e2a8282bc57eb
Tiffany Chang (tic) <tic@odoo.com>
2021-06-08 14:36:59 +0000

    [IMP] mrp/saas~14.5: rename+repurpose locked mo setting
    Existing setting (field) name/description was confusing so we are
    revsering the setting, i.e. instead of having MOs be locked by default
    with the setting active, we now have MOs unlocked by default when the
    setting is active.
    
    We repurposed the existing setting and therefore have to rename it +
    flip its boolean value in existing dbs so existing behavior stays the
    same.
    
    Task: 2518523
    COM PR: odoo/odoo#73939
    Relevant ENT PR (only fixes test): odoo/enterprise#19732

--- migrations/mrp/saas~14.5.2.0/pre-migrate.py
+++ migrations/mrp/saas~14.5.2.0/pre-migrate.py
@@ -4,3 +4,37 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "mrp.unbuild", "allowed_mo_ids")
+
+    util.rename_field(cr, "res.config.settings", "group_locked_by_default", "group_unlocked_by_default")
+    util.rename_xmlid(cr, "mrp.group_locked_by_default", "mrp.group_unlocked_by_default")
+
+    # we're renaming an implied group setting + the group's ID, but reversing its purpose =>
+    # need to record the opposite of the previous setting
+    usr_id = util.ref(cr, "base.group_user")
+    xml_id = util.ref(cr, "mrp.group_unlocked_by_default")
+    cr.execute(
+        """
+            DELETE
+              FROM res_groups_users_rel
+             WHERE gid=%s
+        """,
+        [xml_id],
+    )
+    cr.execute(
+        """
+            DELETE
+              FROM res_groups_implied_rel
+             WHERE gid=%s
+               AND hid=%s
+        """,
+        [usr_id, xml_id],
+    )
+    if not cr.rowcount:
+        cr.execute(
+            """
+                INSERT INTO res_groups_implied_rel (gid, hid)
+                     VALUES(%s, %s)
+            """,
+            [usr_id, xml_id],
+        )
+        util.env(cr)["res.groups"].browse(usr_id).write({"implied_ids": [(4, xml_id)]})
