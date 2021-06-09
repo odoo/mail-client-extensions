a5be3824e5f7e878217e0687e6357a861a5a2159
Ankita Raval <anr@odoo.com>
2021-06-09 06:02:32 +0000

    [FIX] stock@saas~12.5.2.0: Fix process of manufacturing orders in two steps and prevent the duplication location. create missing location it should work by this commit: https://github.com/odoo/upgrade/commit/17929c9ebb290e0a64cec6c8ea2fe8a77a23869b
    closes odoo/upgrade#2523
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/mrp/saas~12.5.2.0/end-migrate.py
+++ migrations/mrp/saas~12.5.2.0/end-migrate.py
@@ -53,3 +53,23 @@ def migrate(cr, version):
             """,
             (mto_route,),
         )
+
+    virtual_production_loc_id = util.ref(cr, "stock.location_production")
+    cr.execute(
+        """UPDATE stock_rule rule
+           SET location_id=prod_loc.id,
+               location_src_id = CASE
+                     WHEN sw.manufacture_steps = 'mrp_one_step' THEN sw.lot_stock_id
+                     ELSE sw.pbm_loc_id
+                     END,
+                     picking_type_id = spt.id
+          FROM stock_warehouse sw
+          JOIN stock_location prod_loc ON prod_loc.company_id = sw.company_id AND prod_loc.usage = 'production'
+          JOIN stock_location stock_loc ON stock_loc.id = sw.lot_stock_id
+          JOIN stock_location pbm_loc ON pbm_loc.id = sw.pbm_loc_id
+          JOIN stock_picking_type spt ON spt.warehouse_id = sw.id AND spt.code = 'mrp_operation'
+         WHERE rule.location_id = %s
+           AND rule.company_id = sw.company_id
+    """,
+        [virtual_production_loc_id],
+    )
--- migrations/stock/saas~12.1.1.1/end-migrate.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo.addons.base.maintenance.migrations import util
-
-def migrate(cr, version):
-    env = util.env(cr)
-    for company in env['res.company'].search([]):
-        company._create_production_location()
-        company._create_scrap_location()
-        company._create_inventory_loss_location()
