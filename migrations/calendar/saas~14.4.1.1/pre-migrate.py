844f8027f1f56a255c51386333c57859f205c101
Nicolas Seinlet <nse@openerp.com>
2021-06-17 10:59:08 +0000

    [FIX] calendar/saas~14.4
    closes odoo/upgrade#2557
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/calendar/saas~14.4.1.1/pre-migrate.py
+++ migrations/calendar/saas~14.4.1.1/pre-migrate.py
@@ -8,6 +8,5 @@ def migrate(cr, version):
     util.move_field_to_module(cr, "res.partner", "meeting_ids", "crm", "calendar")
     util.move_field_to_module(cr, "res.partner", "meeting_count", "crm", "calendar")
     util.convert_field_to_html(cr, "calendar.event", "description")
-    util.force_noupdate(cr, "calendar.calendar_template_meeting_invitation", False)
-    util.force_noupdate(cr, "calendar.calendar_template_meeting_changedate", False)
-    util.force_noupdate(cr, "calendar.calendar_template_meeting_reminder", False)
+    for template in ["invitation", "changedate", "reminder"]:
+        util.update_record_from_xml(cr, f"calendar.calendar_template_meeting_{template}")
