241584ad41136b9ef55484dca52b288cecdddbc9
Christophe Simonis <chs@odoo.com>
2021-06-18 09:38:33 +0000

    [FIX] account_sepa_direct_debit/saas~13.3: mandates are already b64 encoded
    upg-17157
    opw-2518739
    
    closes odoo/upgrade#2563
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account_sepa_direct_debit/saas~13.3.1.0/pre-migrate.py
+++ migrations/account_sepa_direct_debit/saas~13.3.1.0/pre-migrate.py
@@ -4,10 +4,12 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
 
-    """ sdd_mandate original_doc must be converted to ir.attachment """
-    util.convert_binary_field_to_attachment(cr, "sdd.mandate", "original_doc", False, "original_doc_filename")
+    # sdd_mandate original_doc must be converted to ir.attachment
+    util.convert_binary_field_to_attachment(cr, "sdd.mandate", "original_doc", name_field="original_doc_filename")
     # disconnect the attachment from the field
-    cr.execute("UPDATE ir_attachment SET res_field = NULL WHERE res_model = 'sdd.mandate' AND res_field = 'original_doc'")
+    cr.execute(
+        "UPDATE ir_attachment SET res_field = NULL WHERE res_model = 'sdd.mandate' AND res_field = 'original_doc'"
+    )
 
     util.remove_field(cr, "sdd.mandate", "original_doc")
     util.remove_field(cr, "sdd.mandate", "original_doc_filename")
