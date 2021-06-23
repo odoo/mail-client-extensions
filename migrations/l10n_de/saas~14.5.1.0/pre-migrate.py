ab5de637903e226c3156816651db4b1b022d0967
Jacky (trj) <trj@odoo.com>
2021-06-23 08:11:37 +0000

    [REF] l10n_de/saas~14.5: move `l10n_de_stnr` and `l10n_de_widnr` from `l10n_de_pos_cert` module
    The `l10n_de_stnr` and `l10n_de_widnr` are more relevant in the German localization rather than in the German POS localization.
    `l10n_de_nat_tax_nb` is merged with `l10n_de_stnr`.
    
    see https://github.com/odoo/odoo/pull/72590
    see https://github.com/odoo/enterprise/pull/19183
    
    closes odoo/upgrade#2577
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/l10n_de/saas~14.5.1.0/pre-migrate.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    if util.module_installed(cr, "l10n_de_pos_cert"):
+        util.move_field_to_module(cr, "res.company", "l10n_de_widnr", "l10n_de_pos_cert", "l10n_de")
+        # if the module is installed, `l10n_de_stnr` would always be filled
+        # so there's no need to update it with the `l10n_de_nat_tax_nb` value
+        util.move_field_to_module(cr, "res.company", "l10n_de_stnr", "l10n_de_pos_cert", "l10n_de")
+        util.update_field_references(cr, "l10n_de_nat_tax_nb", "l10n_de_stnr", ("res.company",))
+        util.remove_field(cr, "res.company", "l10n_de_nat_tax_nb")
+    else:
+        util.rename_field(cr, "res.company", "l10n_de_nat_tax_nb", "l10n_de_stnr")
+        util.create_column(cr, "res_company", "l10n_de_widnr", "varchar")
