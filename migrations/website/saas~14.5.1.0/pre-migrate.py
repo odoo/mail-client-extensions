43a6fccc09eec7f086ae01ccca842e531b1a1af5
qsm-odoo <qsm@odoo.com>
2021-06-24 11:30:04 +0000

    [IMP] {website, *}/saas~14.5: adapt to header templates review at [1]
    *: website_sale, website_sale_wishlist
    
    [1]: https://github.com/odoo/odoo/pull/68149
    
    task-2368576
    
    closes odoo/upgrade#2294
    
    Related: odoo/design-themes#11
    Related: odoo/odoo#68149
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/website/saas~14.5.1.0/pre-migrate.py
+++ migrations/website/saas~14.5.1.0/pre-migrate.py
@@ -20,6 +20,24 @@ def migrate(cr, version):
     util.remove_view(cr, "website.o_animate_options")
     util.remove_view(cr, "website.no-js_fallback")
 
+    # header template migration
+    util.remove_view(cr, "website.template_header_default_oe_structure_header_default_1")
+    util.remove_view(cr, "website.template_header_hamburger_oe_structure_header_hamburger_1")
+    util.remove_view(cr, "website.template_header_slogan_oe_structure_header_slogan_2")
+    util.remove_view(cr, "website.template_header_boxed_oe_structure_header_boxed_2")
+    util.remove_view(cr, "website.template_header_image_oe_structure_header_image_2")
+
+    # minimalist header to remove -> re-enable the default one on the right
+    # websites (which should already have a cow'ed view for it but disabled)
+    cr.execute("SELECT website_id FROM ir_ui_view WHERE key = 'website.template_header_minimalist' and active = TRUE")
+    website_ids = [website_id for (website_id,) in cr.fetchall()]
+    if website_ids:
+        cr.execute(
+            "UPDATE ir_ui_view SET active = TRUE WHERE key = 'website.template_header_default' and website_id IN %s",
+            [tuple(website_ids)],
+        )
+    util.remove_view(cr, "website.template_header_minimalist")
+
 
 def has_cow_view(cr, xml_ids):
     cr.execute(
--- migrations/website_sale/saas~14.5.1.0/pre-migrate.py
+++ migrations/website_sale/saas~14.5.1.0/pre-migrate.py
@@ -9,3 +9,5 @@ def migrate(cr, version):
     util.remove_view(cr, "website_sale.s_products_searchbar")
     util.remove_view(cr, "website_sale.s_products_searchbar_input")
     util.remove_view(cr, "website_sale.website_sale_products_search_box")
+
+    util.remove_view(cr, "website_sale.template_header_minimalist")
