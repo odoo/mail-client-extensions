eac19f744f84d7e934822573a2fe51aa78728e51
Christophe Simonis <chs@odoo.com>
2021-06-24 14:00:43 +0000

    [FIX] base/saas~13.1: avoid unique constraint violations when removing spaces from xmlids
    tg-63
    
    closes odoo/upgrade#2582
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~13.1.1.3/pre-20-models.py
+++ migrations/base/saas~13.1.1.3/pre-20-models.py
@@ -3,20 +3,40 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    cr.execute(r"""
+    # Avoid unique constraint violations by suffixing the create date to the xmlid (tg-63)
+    # (ok, there still can be conflicts, but they are less likely)
+    cr.execute(
+        r"""
+        WITH conflictuals AS (
+            SELECT unnest(array_agg(id) FILTER (WHERE name != new_name)) as id
+              FROM (SELECT id, module, name, replace(name, ' ', '_') as new_name FROM ir_model_data) imd
+          GROUP BY module, new_name
+            HAVING count(id) > 1
+        )
+        UPDATE ir_model_data d
+           SET name = replace(d.name, ' ', '_') || '_' || to_char(coalesce(d.date_init, d.create_date), 'J-SSSS-US')
+          FROM conflictuals c
+         WHERE d.id = c.id
+
+    """
+    )
+    cr.execute(
+        r"""
         UPDATE ir_model_data
            SET name = REPLACE(name, ' ', '_')
          WHERE name LIKE '% %'
-    """)
+    """
+    )
 
     cr.execute("SELECT id FROM ir_ui_view WHERE type='diagram'")
-    for vid, in cr.fetchall():
+    for (vid,) in cr.fetchall():
         util.remove_view(cr, view_id=vid)
 
     util.remove_field(cr, "ir.ui.view", "model_ids")
 
     # deduplicate properties
-    cr.execute("""
+    cr.execute(
+        """
        WITH dupes AS (
             SELECT unnest((array_agg(id ORDER BY id DESC))[2:]) as id
               FROM ir_property
@@ -26,4 +46,5 @@ def migrate(cr, version):
       DELETE FROM ir_property p
             USING dupes d
             WHERE p.id = d.id
-    """)
+    """
+    )
