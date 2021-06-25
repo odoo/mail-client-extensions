8e9a6739006cba9035183a5fa9d449e7faff8de1
William Braeckman <wbr@odoo.com>
2021-06-25 09:43:22 +0000

    [IMP] hr@saas~14.5: minor changes to hr.job
    Adds a new field 'sequence' and a new sql constraint on
    no_of_recruitment.
    
    See odoo/odoo#72769
    See odoo/enterprise#19264
    
    Task ID: 2581595
    
    closes odoo/upgrade#2585
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr/saas~14.5.1.1/pre-migrate.py
+++ migrations/hr/saas~14.5.1.1/pre-migrate.py
@@ -5,3 +5,12 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.move_field_to_module(cr, "hr.employee", "id_card", "l10n_be_hr_contract_salary", "hr")
     util.move_field_to_module(cr, "hr.employee", "driving_license", "l10n_be_hr_contract_salary", "hr")
+
+    util.create_column(cr, "hr_job", "sequence", "int4", default=10)
+    cr.execute(
+        """
+        UPDATE hr_job
+           SET sequence = id,
+               no_of_recruitment = GREATEST(no_of_recruitment, 0)
+    """
+    )
