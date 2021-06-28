2792f9e230a21a5e74c0c164f4f82d9492cef5de
Rémy Baranx (bar) <bar@odoo.com>
2021-06-28 08:10:04 +0000

    [FIX] sale_payment/saas~11.4: ignore missing sale orders
    While moving relationships between `sale_order` and `payment_transaction`
    in the `sale_order_transaction_rel` table, ignore `sale_order_id` that
    do not exist.
    
    upg-17793
    
    closes odoo/upgrade#2591
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/sale_payment/saas~11.4.1.0/pre-migrate.py
+++ migrations/sale_payment/saas~11.4.1.0/pre-migrate.py
@@ -4,17 +4,17 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version, module="sale_payment"):
     util.create_m2m(
-        cr, "sale_order_transaction_rel",
-        "payment_transaction", "sale_order",
-        "transaction_id", "sale_order_id"
+        cr, "sale_order_transaction_rel", "payment_transaction", "sale_order", "transaction_id", "sale_order_id"
     )
     if util.column_exists(cr, "payment_transaction", "sale_order_id"):
-        cr.execute("""
+        cr.execute(
+            """
             INSERT INTO sale_order_transaction_rel(transaction_id, sale_order_id)
-                 SELECT id, sale_order_id
-                   FROM payment_transaction
-                  WHERE sale_order_id IS NOT NULL
-        """)
+                 SELECT pt.id, pt.sale_order_id
+                   FROM payment_transaction pt
+                   JOIN sale_order so ON so.id = pt.sale_order_id
+        """
+        )
 
     util.remove_field(cr, "payment.transaction", "sale_order_id")
     util.remove_field(cr, "payment.transaction", "so_state")
