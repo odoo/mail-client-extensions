6ca5e537068ab43e83ef790d2792314ec4238652
Christophe Simonis <chs@odoo.com>
2021-07-06 15:07:00 +0000

    [FIX] base/0.0.0,util/pg.py: drop SQL default values for booleans
    Initially, the default values for boolean SQL columns were kept to match
    the ORM behavior [1]. However, the ORM code is actually dead as this
    method is never called [2] with `columntype` being `BOOLEAN`, but with
    `bool` [3].
    
    Also adds a `0.0.0` script to correct previously upgraded databases.
    
    [0] https://github.com/odoo/odoo/blob/dd3967088158833d3a9841eccfbb931fd7658f28/odoo/tools/sql.py#L79
    [1] https://github.com/odoo/odoo/blob/dd3967088158833d3a9841eccfbb931fd7658f28/odoo/fields.py#L887
    [2] https://github.com/odoo/odoo/blob/dd3967088158833d3a9841eccfbb931fd7658f28/odoo/fields.py#L1174
    
    opw-2520371
    opw-2558144
    
    closes odoo/upgrade#2620
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/base/0.0.0/pre-duplicitous-booleans.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+            SELECT table_name, column_name
+              FROM information_schema.columns
+             WHERE udt_name = 'bool'
+               AND column_default IS NOT NULL
+               -- And not the ones defined in `base_data.sql` (bootstrap sql)
+               AND NOT ARRAY[[table_name::text, column_name::text]] <@ ARRAY[
+                ['res_users', 'active'],
+                ['ir_module_module', 'application'],
+                ['ir_module_module', 'demo'],
+                ['ir_module_module', 'web'],
+                ['ir_module_module', 'auto_install'],
+                ['ir_module_module', 'to_buy'],
+                ['ir_module_module_dependency', 'auto_install_required'],
+                ['ir_model_data', 'noupdate']
+               ]
+        """
+    )
+    for table, column in cr.fetchall():
+        cr.execute('ALTER TABLE "{}" ALTER COLUMN "{}" DROP DEFAULT'.format(table, column))
