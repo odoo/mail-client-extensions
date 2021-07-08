a65152c84cac4136c5b1c82e665e6ac27507013b
William Braeckman <wbr@odoo.com>
2021-07-08 09:28:34 +0000

    [IMP] hr_recruitment/saas~14.5: improve job form view
    Remove unused `meeting_count` field.
    
    Relates to odoo/odoo#72649
    
    Task ID: 2578165
    
    closes odoo/upgrade#2642
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/hr_recruitment/saas~14.5.1.1/pre-migrate.py
+++ migrations/hr_recruitment/saas~14.5.1.1/pre-migrate.py
@@ -5,3 +5,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "hr_recruitment_stage", "hired_stage", "boolean")
+
+    util.remove_field(cr, "hr.applicant", "meeting_count")
