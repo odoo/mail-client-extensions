70c1822019da31c137ff56c4d134b20c2a944952
Yannick Tivisse <yti@odoo.com>
2021-07-12 10:24:06 +0000

    [IMP] hr_referral/saas~14.5: Add possibility to archive rewards
    Ent PR: https://github.com/odoo/enterprise/pull/19598
    
    closes odoo/upgrade#2639
    
    Taskid: 2586019
    Related: odoo/enterprise#19598
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr_referral/saas~14.5.1.0/pre-migrate.py
+++ migrations/hr_referral/saas~14.5.1.0/pre-migrate.py
@@ -8,3 +8,5 @@ def migrate(cr, version):
     cr.execute("UPDATE hr_recruitment_stage SET use_in_referral = NOT COALESCE(use_in_referral, false)")
 
     util.create_m2m(cr, "hr_referral_alert_users_rel", "hr_referral_alert", "res_users", "alert_id", "user_id")
+
+    util.create_column(cr, "hr_referral_reward", "active", "boolean", default=True)
