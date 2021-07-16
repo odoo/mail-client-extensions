2d16d9b9e3a78fbb25b6e759d7b6c65fb71686ae
Kevin Baptiste <kba@odoo.com>
2021-07-16 09:27:24 +0000

    [IMP] planning/saas~14.5: add resource_id on planning.slot
    Planning slots are now done on resource.resource instead of hr.employee.
    
    odoo/odoo#73822
    odoo/enterprise#19695
    
    closes odoo/upgrade#2649
    
    Taskid: 2566432
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/planning/saas~14.5.1.0/pre-migrate.py
+++ migrations/planning/saas~14.5.1.0/pre-migrate.py
@@ -16,3 +16,20 @@ def migrate(cr, version):
 
     util.update_field_references(cr, "is_published", "state", only_models=("planning.slot",), domain_adapter=adapter)
     util.remove_field(cr, "planning.slot", "is_published")
+
+    util.rename_xmlid(cr, *eb("planning.planning_menu_schedule_by_{employee,resource}"))
+    util.rename_xmlid(cr, *eb("planning.planning_action_schedule_by_{employee,resource}"))
+    util.rename_xmlid(cr, *eb("planning.planning_action_schedule_by_{employee,resource}_view_gantt"))
+
+    util.create_column(cr, "planning_slot", "resource_id", "int4")
+    cr.execute(
+        """
+            UPDATE planning_slot AS s
+               SET resource_id = e.resource_id
+              FROM hr_employee AS e
+             WHERE s.employee_id = e.id
+               AND s.company_id = e.company_id
+        """
+    )
+
+    util.create_column(cr, "resource_resource", "color", "int4", default=0)
