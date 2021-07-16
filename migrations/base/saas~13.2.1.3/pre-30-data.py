0aec7f78304b0b8ca6374cb37b573dd286099941
Alvaro Fuentes <afu@odoo.com>
2021-07-16 16:38:38 +0000

    [FIX] base/saas~13.2.1.3: avoid ir_translation contraints
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/14.0/odoo/service/server.py", line 1199, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "/home/odoo/src/odoo/14.0/odoo/modules/registry.py", line 89, in new
        odoo.modules.load_modules(registry._db, force_demo, status, update_module)
      File "/home/odoo/src/odoo/14.0/odoo/modules/loading.py", line 396, in load_modules
        report=report, models_to_check=models_to_check)
      File "/home/odoo/src/odoo/14.0/odoo/modules/loading.py", line 173, in load_module_graph
        migrations.migrate_module(package, 'pre')
      File "/home/odoo/src/odoo/14.0/odoo/modules/migration.py", line 180, in migrate_module
        migrate(self.cr, installed_version)
      File "/tmp/tmp50q12md8/migrations/base/saas~13.2.1.3/pre-30-data.py", line 10, in migrate
        cr.execute("UPDATE ir_translation SET lang = 'tl_PH' WHERE lang = 'fil_PH'")
      File "<decorator-gen-3>", line 2, in execute
      File "/home/odoo/src/odoo/14.0/odoo/sql_db.py", line 101, in check
        return f(self, *args, **kwargs)
      File "/home/odoo/src/odoo/14.0/odoo/sql_db.py", line 298, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.UniqueViolation: duplicate key value violates unique constraint "ir_translation_unique"
    DETAIL:  Key (type, name, lang, res_id, md5(src))=(model_terms, ir.ui.view,arch_db, tl_PH, 57, 1c76cbfe21c6f44c1d1e59d54f3e4420) already exists.
    ```
    
    https://github.com/odoo/odoo/blob/ad421baf18e7149e12f52869235ee653a6235f6e/odoo/addons/base/models/ir_translation.py#L193-L198
    
    upg-8404
    
    closes odoo/upgrade#2653
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~13.2.1.3/pre-30-data.py
+++ migrations/base/saas~13.2.1.3/pre-30-data.py
@@ -7,6 +7,44 @@ def migrate(cr, version):
     tl = util.ref(cr, "base.lang_tl")
     if fil and tl:
         util.replace_record_references(cr, ("res.lang", fil), ("res.lang", tl), replace_xmlid=False)
+
+    # avoid translation constraints
+    # "ir_translation_code_unique" UNIQUE, btree (type, lang, md5(src)) WHERE type::text = 'code'::text
+    # "ir_translation_model_unique" UNIQUE, btree (type, lang, name, res_id) WHERE type::text = 'model'::text
+    # "ir_translation_unique" UNIQUE, btree (type, name, lang, res_id, md5(src))
+    # https://github.com/odoo/odoo/blob/ad421baf18e7149e12f52869235ee653a6235f6e/odoo/addons/base/models/ir_translation.py#L193-L198
+    cr.execute(
+        """
+        WITH dup AS (
+            SELECT fil.id
+              FROM ir_translation fil
+              JOIN ir_translation tl
+                ON -- ir_translation_code_unique
+                    (    fil.type = 'code'
+                     AND  tl.type = 'code'
+                     AND md5(fil.src) = md5(tl.src)
+                    )
+                OR -- ir_translation_model_unique
+                    (    fil.type = 'model'
+                     AND  tl.type = 'model'
+                     AND fil.name = tl.name
+                     AND fil.res_id = tl.res_id
+                    )
+                OR -- ir_translation_unique
+                    (    fil.type = tl.type
+                     AND fil.name = tl.name
+                     AND fil.res_id = tl.res_id
+                     AND md5(fil.src) = md5(tl.src)
+                    )
+
+             WHERE fil.lang = 'fil_PH'
+               AND tl.lang = 'tl_PH'
+            )
+        DELETE FROM ir_translation tr
+              USING dup
+              WHERE tr.id=dup.id
+        """
+    )
     cr.execute("UPDATE ir_translation SET lang = 'tl_PH' WHERE lang = 'fil_PH'")
     cr.execute("UPDATE res_partner SET lang = 'tl_PH' WHERE lang = 'fil_PH'")
     util.remove_record(cr, "base.lang_fil")
