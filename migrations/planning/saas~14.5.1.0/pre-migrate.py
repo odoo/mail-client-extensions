ec0c9b371845d28f38564c0de84ecf6af3956fa8
Prakash Prajapati <ppr@odoo.com>
2021-07-19 10:10:41 +0000

    [IMP] planning/saas~14.5: remove the is_published field
    The purpose of the task is to add a state selection field and make
    is_published field non-store.
    
    Related Ent PR: odoo/enterprise#19260
    
    TaskID:2531460
    
    closes odoo/upgrade#2654
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/planning/saas~14.5.1.0/pre-migrate.py
+++ migrations/planning/saas~14.5.1.0/pre-migrate.py
@@ -6,3 +6,13 @@ from odoo.upgrade import util
 def migrate(cr, version):
     eb = util.expand_braces
     util.rename_xmlid(cr, *eb("{project_forecast,planning}.planning_view_gantt_no_sample"))
+    util.create_column(cr, "planning_slot", "state", "varchar", default="draft")
+    cr.execute("UPDATE planning_slot SET state = 'published' where is_published=True")
+
+    def adapter(leaf, _, __):
+        left, operator, right = leaf
+        operator = "=" if bool(right) else "!="
+        return [(left, operator, "published")]
+
+    util.update_field_references(cr, "is_published", "state", only_models=("planning.slot",), domain_adapter=adapter)
+    util.remove_field(cr, "planning.slot", "is_published")
