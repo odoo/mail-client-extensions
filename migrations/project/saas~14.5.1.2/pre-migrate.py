63f2e9f09f805c6b0d58a8ef1e5af8ce01db1dc5
William Braeckman <wbr@odoo.com>
2021-07-22 07:47:34 +0000

    [IMP] project/saas~14.5: add personal stages
    project.task:
     - Change many2one `user_id` to a many2many `user_ids` and migrate the
       data
    project.task.type:
     - Add a new field `is_personal`
    
    See odoo/odoo#74087
    See odoo/enterprise#19780
    
    Task ID: 2398734
    
    closes odoo/upgrade#2723
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>
    Co-authored-by: chs <chs@odoo.com>

--- migrations/project/saas~14.5.1.2/pre-migrate.py
+++ migrations/project/saas~14.5.1.2/pre-migrate.py
@@ -6,3 +6,74 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.create_m2m(cr, "project_project_project_tags_rel", "project_project", "project_tags")
     util.create_column(cr, "project_project", "stage_id", "int4")
+
+    # Create m2m with intermediate model `project.task.personal.stage`
+    # Let the orm take care of foreign keys and other rules
+    cr.execute(
+        """
+        CREATE TABLE project_task_user_rel (
+            id          SERIAL NOT NULL PRIMARY KEY,
+            create_uid  integer,
+            create_date timestamp without time zone,
+            write_uid   integer,
+            write_date  timestamp without time zone,
+
+            task_id     integer NOT NULL,
+            user_id     integer NOT NULL,
+            stage_id    integer
+        )
+    """
+    )
+    # Create our 6 default stage for every user having the group `group_project_user`
+    # Fetch the appropriate users
+    util.create_column(cr, "project_task_type", "user_id", "int4")
+    cr.execute(
+        """
+        WITH stages AS (
+           SELECT *
+             FROM (VALUES (1, 'Inbox', false),
+                                        (2, 'Today', false),
+                                        (3, 'This Week', false),
+                                        (4, 'This Month', false),
+                                        (5, 'Later', false),
+                                        (6, 'Done', true),
+                                        (7, 'Canceled', true)
+             ) x(seq, name, fold)
+        ),
+        assigned_users AS (
+            SELECT DISTINCT user_id
+              FROM project_task
+             WHERE user_id IS NOT NULL
+        )
+        INSERT INTO
+            project_task_type(
+                sequence,
+                name,
+                user_id,
+                fold,
+                legend_blocked,
+                legend_done,
+                legend_normal
+            )
+            SELECT s.seq, s.name, a.user_id, s.fold,
+                   'Blocked', 'Ready', 'In Progress'
+              FROM assigned_users a,
+                   stages s
+        """
+    )
+    # Populate our new triplet with the data we need,
+    # We migrate our assigned users and also assign their default stage.
+    cr.execute(
+        """
+        INSERT INTO project_task_user_rel(task_id, user_id, stage_id)
+             SELECT task.id, task.user_id, stage.id
+               FROM project_task task
+               JOIN project_task_type stage USING (user_id)
+              WHERE stage.sequence = 1
+                AND task.user_id IS NOT NULL
+        """
+    )
+    util.update_field_references(cr, "user_id", "user_ids", only_models=("project.task",))
+    util.remove_field(cr, "project.task", "user_id")
+    util.remove_field(cr, "project.task", "user_email")
+    util.update_record_from_xml(cr, "project.task_visibility_rule")
