8f827081beb23cd0d5914530d8f56cd2c6398c18
Denis Ledoux <dle@odoo.com>
2021-07-22 08:03:30 +0000

    [FIX] calendar@0.0.0: match_uniq `calendar.event.type`
    ```
    odoo.tools.convert.ParseError: "duplicate key value violates unique constraint "calendar_event_type_name_uniq"
    DETAIL:  Key (name)=(Customer Meeting) already exists.
    " while parsing /home/odoo/src/odoo/11.0/addons/calendar/data/calendar_data.xml:55, near
    <record id="categ_meet1" model="calendar.event.type">
                <field name="name">Customer Meeting</field>
            </record>
    ```
    
    upg-19864
    
    closes odoo/upgrade#2661
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

---
+++ migrations/calendar/0.0.0/pre-models-match_uniq.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+
+try:
+    from odoo.addons.calendar.models import calendar  # noqa
+except Exception:
+    from odoo.addons.calendar.models import calendar_event_type  # noqa
+
+
+def migrate(cr, version):
+    pass
+
+
+class Tax(models.Model):
+    _inherit = "calendar.event.type"
+    _module = "calendar"
+    _match_uniq = True
