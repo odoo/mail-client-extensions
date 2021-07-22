dcedff315462059540b8e09b5c64f366d6939edd
Denis Ledoux <dle@odoo.com>
2021-07-22 14:27:05 +0000

    [FIX] mrp@saas~13.4: avoid too many savepoints
    Calling `_generate_backorder_productions` leads to create a savepoint
    in `_procure_orderpoint_confirm`
    https://github.com/odoo/odoo/blob/a46f8d66965cce5759ca4af17c71b0aa18eda9ec/addons/stock/models/stock_orderpoint.py#L476
    
    Having more than 64 savepoints per transaction, even if they are released,
    is not good for the performance of the entire cluster,
    as explained in
    https://github.com/odoo/odoo/pull/71395#issuecomment-851440042
    
    Therefore, split the MOs every 50 records, and commit between each chunk,
    to avoid more than 50 savepoints in the transaction.
    
    ```
    2021-07-15 13:59:14,777 3689 INFO db_16846 odoo.upgrade.util.orm: [52.16%] 4700/9010 mrp.production processed in 0:39:50.077752 (total estimated time: 1:16:21.829903)
    2021-07-15 13:59:19,328 3689 WARNING db_16846 odoo.upgrade.util.pg: There are more than 64 savepoints opened (528): {'_procure_orderpoint_confirm': 528}
    ```
    
    upg-16846
    
    closes odoo/upgrade#2664
    
    Signed-off-by: Denis Ledoux (dle) <dle@odoo.com>

--- migrations/mrp/saas~13.4.2.0/end-migrate.py
+++ migrations/mrp/saas~13.4.2.0/end-migrate.py
@@ -59,7 +59,7 @@ def migrate(cr, version):
     # (3.) For MO in progress with the done move
     _logger.info("Create backorder for in progress MO")
     env = util.env(cr)
-    for mos in util.iter_browse(env["mrp.production"], ids_to_backorder):
+    for mos in util.iter_browse(env["mrp.production"], ids_to_backorder, strategy="commit", chunk_size=50):
         mos = mos.filtered(lambda mo: mo.state != "done")
         mos._generate_backorder_productions(close_mo=True)
         for mo in mos:
