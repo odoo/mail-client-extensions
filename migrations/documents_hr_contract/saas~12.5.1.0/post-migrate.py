fa6297e13d420747154a8c5a7707a0d983502c11
Denis Ledoux <dle@odoo.com>
2021-07-26 11:50:00 +0000

    [FIX] documents_hr_{contract,payroll}@saas~12.5: do not force creation of document tag
    The tag `documents_hr_documents_contracts` and `documents_hr_documents_Payslips`
    have been moved from `documents` to `documents_hr_contract` and `documents_hr_payroll`
    which are new modules.
    odoo/enterprise@f90218b7efda2d9fcccfb883a80e5646f0df3536
    
    As they are new modules, `force_migration_of_fresh_module` will load them by default
    in `init` mode, which will try to force their creation if they were not there.
    But, as `documents` was already installed and these tags initially comes from there,
    if these tags do not exist, it means they have been deleted by the users,
    and as they are configured as `forcecreate=0`, they shouldn't be created back
    during the upgrade.
    In addition, it raises an error if the facet `documents_hr.documents_hr_documents`
    was also removed by the users. See the below traceback.
    
    So, pass `init=False` so the creation of these tags is not enforced.
    However, the assignation in the one2many field `documents_hr_contracts_tags`
    was not there in 12.0/saas-12.3. This field didn't exist then.
    So, the upgrade should take care of the assignation of the tags in a script,
    as the `init=False` will have as side-effect to not load the `base.main_company`
    block as it's wrapped inside a `noupdate=1` block.
    
    ```
      File "/home/odoo/src/odoo/13.0/odoo/addons/base/models/ir_model.py", line 1685, in xmlid_lookup
        raise ValueError('External ID not found in the system: %s' % xmlid)
    odoo.tools.convert.ParseError: "External ID not found in the system: documents_hr.documents_hr_documents" while parsing /home/odoo/src/enterprise/13.0/documents_hr_contract/data/data.xml:2, near
    <odoo>
        <data noupdate="1">
    
            <record id="documents_hr_documents_contracts" model="documents.tag" forcecreate="0">
                <field name="name">Contracts</field>
                <field name="facet_id" ref="documents_hr.documents_hr_documents"/>
                <field name="sequence">11</field>
            </record>
    
            <record id="base.main_company" model="res.company">
                <field name="documents_hr_contracts_tags" eval="[(6,0,[ref('documents_hr_documents_contracts')])]"/>
            </record>
    
        </data>
    </odoo>
    ```
    
    upg-19759
    
    closes odoo/upgrade#2673
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/documents_hr_contract/saas~12.5.1.0/post-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    if util.ref(cr, "documents_hr_contract.documents_hr_documents_contracts"):
+        util.update_record_from_xml(cr, "base.main_company", from_module="documents_hr_contract")
