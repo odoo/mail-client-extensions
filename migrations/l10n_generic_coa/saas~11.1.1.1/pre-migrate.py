986f8c406f78d5ea2d31aa8074fbcb2303238daa
Christophe Simonis <chs@odoo.com>
2021-07-30 13:14:32 +0000

    [ADD] l10n_generic_coa/saas~11.1: rename xmlids of demo data
    Make upgradeci happy.
    
    closes odoo/upgrade#2696
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/l10n_generic_coa/saas~11.1.1.1/pre-migrate.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    # move demo data
+    lst = """
+        demo_bank_statement_1
+        demo_bank_statement_line_1
+        demo_bank_statement_line_2
+        demo_bank_statement_line_3
+        demo_bank_statement_line_4
+        demo_bank_statement_line_5
+
+        demo_invoice_1
+        demo_invoice_2
+        demo_invoice_3
+        demo_invoice_followup
+        demo_invoice_0
+
+        demo_invoice_january_wages
+        ceo_wages_line
+
+        demo_invoice_equipment_purchase
+        coffee_machine_line
+
+        demo_opening_move
+        opening_line_1
+        opening_line_2
+        opening_line_3
+    """
+
+    for xid in util.splitlines(lst):
+        util.rename_xmlid(cr, "account." + xid, "l10n_generic_coa." + xid)
