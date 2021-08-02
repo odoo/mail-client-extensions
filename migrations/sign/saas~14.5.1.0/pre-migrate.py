394d9a21062901be76f8f36c4492a4fa0307f644
Chong Wang (cwg) <cwg@odoo.com>
2021-08-02 10:26:24 +0000

    [IMP] sign/saas~14.5: add colors for roles
    add colors
    default color for existing roles:
        'Customer': 3
        'Company': 9
        'Employee': 10
        'HR Responsible': 11
        others: random color from 1 to 11 except 3, 9, 10, 11
    
    taskid:2614473
    
    see odoo/enterprise#19971
    
    closes odoo/upgrade#2703
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/sign/saas~14.5.1.0/pre-migrate.py
+++ migrations/sign/saas~14.5.1.0/pre-migrate.py
@@ -9,6 +9,21 @@ def migrate(cr, version):
     util.create_column(cr, "sign_request", "message", "text")
     util.create_column(cr, "sign_request", "message_cc", "text")
     util.create_column(cr, "sign_send_request", "message_cc", "text")
+    util.create_column(cr, "sign_item_role", "color", "integer")
+
+    # random color from [1, 2, 4, 5, 6, 7, 8]
+    cr.execute(
+        """
+        UPDATE sign_item_role
+        SET color = CASE name
+                        WHEN 'Customer' THEN 3
+                        WHEN 'Company' THEN 9
+                        WHEN 'Employee' THEN 10
+                        WHEN 'HR Responsible' THEN 11
+                        ELSE (array[1,2,4,5,6,7,8])[floor(random() * 7 + 1)]
+                    END
+        """
+    )
 
     cr.execute(
         """
