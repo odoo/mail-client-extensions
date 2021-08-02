6a96448985f39e5972aff120c7594b5722d25abf
Nicolas (vin) <vin@odoo.com>
2021-08-02 08:03:33 +0000

    [IMP] account/saas~14.5: french tax report carryover
    See odoo/odoo#69887
    See odoo/enterprise#17953
    
    closes odoo/upgrade#2708
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/saas~14.5.1.1/pre-migrate.py
+++ migrations/account/saas~14.5.1.1/pre-migrate.py
@@ -9,3 +9,9 @@ def migrate(cr, version):
 
     util.remove_view(cr, "account.dashboard_onboarding_bill_step")
     util.create_column(cr, "res_company", "account_setup_taxes_state", "varchar", default="not_done")
+
+    # ===============================================================
+    # French tax report: carry over tax grid 27 to tax grid 22 (PR: (odoo) 69887 & (enterprise) 17953)
+    # ===============================================================
+    util.create_column(cr, "account_tax_report_line", "is_carryover_persistent", "bool", default=True)
+    util.create_column(cr, "account_tax_report_line", "is_carryover_used_in_balance", "bool")
