c3e1d50cb6cfc1cad8a9f33481236a132d6290b8
Martin Trigaux <mat@odoo.com>
2021-08-13 11:57:51 +0000

    [IMP] mail/saas~14.5: convert res_model_id to res_model
    Following the removal of read access on ir.model (odoo/odoo#69120),
    the mail.activity.type model was not accessible to non-admin users due
    to the res_model_id many2one field.
    
    Convert it to a selection field with the selection values being
    computed in sudo.
    
    closes odoo/upgrade#2734
    
    Related: odoo/enterprise#20214
    Related: odoo/odoo#74981
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/mail/saas~14.5.1.5/pre-migrate.py
+++ migrations/mail/saas~14.5.1.5/pre-migrate.py
@@ -21,3 +21,16 @@ def migrate(cr, version):
         ),
     )
     util.rename_field(cr, "mail.mail", "notification", "is_notification")
+
+    util.create_column(cr, "mail_activity_type", "res_model", "varchar")
+    cr.execute(
+        """
+        UPDATE mail_activity_type mat
+           SET res_model = m.model
+          FROM ir_model m
+         WHERE m.id = mat.res_model_id
+    """
+    )
+
+    util.remove_field(cr, "mail.activity.type", "res_model_id")
+    util.remove_field(cr, "mail.activity.type", "initial_res_model_id")
