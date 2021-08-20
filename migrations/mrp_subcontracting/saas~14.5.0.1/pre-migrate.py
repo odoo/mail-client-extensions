d16e59c7128352081fc19474876d4b85532d7d4b
Rémy Voet (ryv) <ryv@odoo.com>
2021-08-20 08:43:11 +0000

    [IMP] mrp_subcontracting/saas~14.5: add new column
    Add `subcontracting_has_been_recorded` (and
    fill it) and`subcontracting_resupply_type_id`.
    
    See odoo/odoo#75041
    See odoo/enterprise#20350
    task-2486811
    
    closes odoo/upgrade#2750
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/mrp_subcontracting/saas~14.5.0.1/pre-migrate.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "mrp_production", "subcontracting_has_been_recorded", "boolean", default=False)
+    cr.execute(
+        """
+    WITH subcontracted_mo AS (
+        SELECT DISTINCT mp.id
+          FROM mrp_production mp
+          JOIN stock_move sm1 ON (sm1.production_id = mp.id AND sm1.product_id = mp.product_id)
+          JOIN stock_move_move_rel sm_rel ON sm_rel.move_orig_id = sm1.id
+          JOIN stock_move sm2 ON sm2.id = sm_rel.move_dest_id
+          JOIN stock_move_line sml ON sml.move_id = sm2.id
+         WHERE sm2.is_subcontract = TRUE AND sml.qty_done > 0
+    )
+    UPDATE mrp_production mp
+       SET subcontracting_has_been_recorded = TRUE
+      FROM subcontracted_mo sub_mo
+     WHERE mp.id = sub_mo.id
+        """
+    )
+
+    util.create_column(cr, "stock_warehouse", "subcontracting_resupply_type_id", "int4")
+    cr.execute("UPDATE stock_warehouse SET subcontracting_resupply_type_id = out_type_id")
