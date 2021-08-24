1d42ced06effff44d5bae506f1247e990fb0dce3
William Braeckman <wbr@odoo.com>
2021-08-24 09:56:16 +0000

    [IMP] hr,hr_payroll/saas~14.5: remove personal documents tab
    This commit makes sure to convert old binary fields into attachment in
    the chatter and/or documents in the documents app.
    The fields are kept but unused because the salary configurator needs
    fields to bind to in order to display and request information during the
    salary review.
    
    See odoo/odoo#75506
    See odoo/enterprise#20420
    
    Task ID: 2628750
    
    closes odoo/upgrade#2769
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ migrations/hr_payroll/saas~14.5.1.0/end-migrate.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # Those fields will no longer be used to hold the documents we need,
+    # instead we will use the chatter and the documents app.
+    # The reason it is done this way is to make sure that the documents app creates
+    # the documents for the related files accordingly.
+    # See documents/models/ir_attachment.py:write
+    # If documents_hr is not installed, just set res_field to NULL on the appropriate records.
+    binary_fields_to_process = ("mobile_invoice", "sim_card", "internet_invoice")
+    if util.module_installed(cr, "documents_hr"):
+        cr.execute(
+            """
+            SELECT id
+              FROM ir_attachment
+             WHERE res_model='hr.employee'
+               AND res_field in %s
+        """,
+            (binary_fields_to_process,),
+        )
+        ids = [id for id, in cr.fetchall()]
+        env = util.env(cr)
+        util.iter_browse(env["ir.attachment"], ids).write({"res_field": False})
+    else:
+        cr.execute(
+            """
+            UPDATE ir_attachment
+               SET res_field=NULL
+             WHERE res_model='hr.employee'
+               AND res_field in %s
+        """,
+            (binary_fields_to_process,),
+        )
