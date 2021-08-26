31c2828c509aafbce930ec6f6207bee15d86e38f
Julien Banken <jbn@odoo.com>
2021-08-26 13:44:41 +0000

    [IMP] website_event_track/saas~14.5.1.3: update track fields
    Purpose is to update fields related to event track management: stage display
    update (visibility), kanban custom display tracking as well as various
    frontend visibility update.
    
    See odoo/odoo#69585
    Task-2504216
    
    closes odoo/upgrade#2408
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website_event_track/saas~14.5.1.3/pre-migrate.py
+++ migrations/website_event_track/saas~14.5.1.3/pre-migrate.py
@@ -4,4 +4,26 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.create_column(cr, "event_track_stage", "description", "text")
+    util.create_column(cr, "event_track_stage", "legend_blocked", "varchar", default="Blocked")
+    util.create_column(cr, "event_track_stage", "legend_done", "varchar", default="Ready for Next Stage")
+    util.create_column(cr, "event_track_stage", "legend_normal", "varchar", default="In Progress")
+    util.rename_field(cr, "event.track.stage", "is_accepted", "is_visible_in_agenda")
+    util.rename_field(cr, "event.track.stage", "is_done", "is_fully_accessible")
+
+    util.remove_field(cr, "event.track", "is_accepted")
+    util.create_column(cr, "event_track", "kanban_state_label", "varchar")
+    # pre compute kanban state label; as fields are new, let us skip translations
+    # as anyway future changes will use right language in tracking
+    cr.execute(
+        """
+        UPDATE event_track
+           SET kanban_state_label = CASE WHEN kanban_state = 'done' THEN 'Ready for Next Stage'
+                                         WHEN kanban_state = 'blocked' THEN 'Blocked'
+                                         ELSE 'In Progress'
+                                         END
+        """
+    )
+
+    util.remove_view(cr, "website_event_track.tracks_cards_track")
     util.remove_view(cr, "website_event_track.event_track_proposal_success")
