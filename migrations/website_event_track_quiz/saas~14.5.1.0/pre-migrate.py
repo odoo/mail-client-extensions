78c0be8c71423801a4edd354098640ace62b6519
Julien Banken <jbn@odoo.com>
2021-08-31 13:13:21 +0000

    [IMP] website_event_track_quiz/saas~14.5.1.0: update quiz
    Purpose is to add fields related to event track quiz update. They are now
    repeatable. See odoo/odoo#69585
    
    Task-2504216
    
    Part-of: odoo/upgrade#2408

--- migrations/website_event_track_quiz/saas~14.5.1.0/pre-migrate.py
+++ migrations/website_event_track_quiz/saas~14.5.1.0/pre-migrate.py
@@ -5,3 +5,6 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "event.quiz", "event_track_ids")
     util.remove_view(cr, "website_event_track_quiz.event_quiz_view_form_from_track")
+    util.create_column(cr, "event_quiz", "repeatable", "boolean")
+    util.create_column(cr, "event_quiz_answer", "is_correct", "boolean")
+    cr.execute("UPDATE event_quiz_answer SET is_correct = (COALESCE(awarded_points, 0) > 0)")
