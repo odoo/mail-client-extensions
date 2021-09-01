2a2e92b7914667029738c6ab177acb3a4a654acd
Thibault Libioulle <tle@odoo.com>
2021-09-01 16:08:02 +0000

    [FIX] sale_project_forecast/saas~14.5: fix explode_query_range call
    This commit fixes the call to explode_query_range and ensures the id is
    used on the right tablei, i.e. planning_slot and not sale_order_line.
    
    See #2777
    
    closes odoo/upgrade#2616
    
    Related: odoo/odoo#70527
    Related: odoo/enterprise#19459
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/sale_project_forecast/saas~14.5.1.0/pre-migrate.py
+++ migrations/sale_project_forecast/saas~14.5.1.0/pre-migrate.py
@@ -22,9 +22,9 @@ def migrate(cr, version):
     util.parallel_execute(cr, util.explode_query_range(cr, query, table="planning_slot", prefix="slot."))
 
     query = """
-        UPDATE planning_slot
+        UPDATE planning_slot slot
            SET sale_order_id = sol.order_id
           FROM sale_order_line sol
-         WHERE sale_line_id = sol.id
+         WHERE slot.sale_line_id = sol.id
     """
-    util.parallel_execute(cr, util.explode_query_range(cr, query, table="planning_slot"))
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="planning_slot", prefix="slot."))
