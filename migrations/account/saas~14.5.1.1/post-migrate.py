0d5972ce3cb995adb5fc9f247355c591c92e7719
wan <wan@odoo.com>
2021-09-01 11:32:41 +0000

    [IMP] account/saas~14.5: resequence only in debug mode
    odoo/odoo#75819
    task-2604155
    
    closes odoo/upgrade#2794
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/saas~14.5.1.1/post-migrate.py
+++ migrations/account/saas~14.5.1.1/post-migrate.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.update_record_from_xml(cr, "account.action_account_resequence")
 
     # ==========================================================================
     # The Reconciliation Models usability imp (PR: odoo#73043, enterprise#19395)
