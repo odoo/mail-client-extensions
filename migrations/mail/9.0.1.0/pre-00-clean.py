ac7846a164d331b67112f8695517b057f763ada8
Christophe Simonis <chs@odoo.com>
2021-09-06 12:59:11 +0000

    [chore] mail/9.0: format file
    Part-of: odoo/upgrade#2720

--- migrations/mail/9.0.1.0/pre-00-clean.py
+++ migrations/mail/9.0.1.0/pre-00-clean.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 import logging
+
 from openerp.release import version
+
 from openerp.addons.base.maintenance.migrations import util
 
-NS = 'openerp.addons.base.maintenance.migrations.mail.{}.{}'
-_logger = logging.getLogger(NS.format(version.rstrip('.0'), __name__))
+NS = "openerp.addons.base.maintenance.migrations.mail.{}.{}"
+_logger = logging.getLogger(NS.format(version.rstrip(".0"), __name__))
 
-def clean(cr, table, model_col='res_model', id_col='res_id'):
-    cr.execute("SELECT {model_col} FROM {table} WHERE {model_col} IS NOT NULL GROUP BY {model_col}"
-               .format(**locals()))
-    for model, in cr.fetchall():
-        adj = ''
+
+def clean(cr, table, model_col="res_model", id_col="res_id"):
+    cr.execute("SELECT {model_col} FROM {table} WHERE {model_col} IS NOT NULL GROUP BY {model_col}".format(**locals()))
+    for (model,) in cr.fetchall():
+        adj = ""
         target_table = util.table_of_model(cr, model)
         if util.table_exists(cr, target_table):
             query = """
@@ -28,31 +30,30 @@ def clean(cr, table, model_col='res_model', id_col='res_id'):
             cr.execute(query.format(**locals()), [model])
 
         else:
-            adj = ' unexisting'
+            adj = " unexisting"
             cr.execute("DELETE FROM {table} WHERE {model_col}=%s".format(**locals()), [model])
 
         deleted = cr.rowcount
         if deleted:
-            _logger.info('DELETE %(deleted)d %(table)s unreachable rows for%(adj)s model %(model)s',
-                         locals())
+            _logger.info("DELETE %(deleted)d %(table)s unreachable rows for%(adj)s model %(model)s", locals())
 
 
 def migrate(cr, version):
-    clean(cr, 'mail_followers')
+    clean(cr, "mail_followers")
 
     # Create missing index.
     # Without it, deletion is way loooonger!
-    if not util.get_index_on(cr, 'mail_mail', 'mail_message_id'):
+    if not util.get_index_on(cr, "mail_mail", "mail_message_id"):
         cr.execute("CREATE INDEX mail_mail_mail_message_id_index ON mail_mail(mail_message_id)")
 
-    parent_id_fk = next((
-        fk for fk in util.get_fk(cr, 'mail_message') if fk[0] == "mail_message" and fk[1] == "parent_id"
-    ), None)
+    parent_id_fk = next(
+        (fk for fk in util.get_fk(cr, "mail_message") if fk[0] == "mail_message" and fk[1] == "parent_id"), None
+    )
     if parent_id_fk:
         _, _, constraint, on_delete = parent_id_fk
         cr.execute("ALTER TABLE mail_message DROP CONSTRAINT %s" % constraint)
 
-    clean(cr, 'mail_message', 'model')
+    clean(cr, "mail_message", "model")
 
     if parent_id_fk:
         _, _, constraint, on_delete = parent_id_fk
@@ -61,7 +62,7 @@ def migrate(cr, version):
             "c": "CASCADE",
             "r": "RESTRICT",
             "n": "SET NULL",
-            "d": "SET DEFAULT"
+            "d": "SET DEFAULT",
         }.get(on_delete)
         cr.execute(
             """
@@ -69,10 +70,12 @@ def migrate(cr, version):
                         ADD CONSTRAINT %s FOREIGN KEY (parent_id)
                             REFERENCES mail_message(id)
                             ON DELETE %s
-            """ % (constraint, on_delete))
+            """
+            % (constraint, on_delete)
+        )
 
 
-if __name__ == '__main__':
-    env = env   # noqa: F821
+if __name__ == "__main__":
+    env = env  # noqa: F821
     migrate(env.cr, None)
     env.cr.commit()
