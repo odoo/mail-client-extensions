3d5c9973d3fd946c2a58e3cb85c2363681d2d874
kbs-odoo <kbs@odoo.com>
2021-09-08 09:53:14 +0000

    [FIX] l10n_in_purchase/saas~13.4: update stored GST treatment value
    This is a stored computed field.
    
    upg-22922
    opw-2632516
    
    closes odoo/upgrade#2814
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/l10n_in_purchase/saas~13.4.1.0/pre-models.py
+++ migrations/l10n_in_purchase/saas~13.4.1.0/pre-models.py
@@ -5,3 +5,18 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # create column in db for new fields in purchase_order table
     util.create_column(cr, "purchase_order", "l10n_in_gst_treatment", "varchar")
+
+    # stored value for field 'l10n_in_gst_treatment' as per partner for existing purchase orders
+    cr.execute(
+        """
+        UPDATE purchase_order p
+           SET l10n_in_gst_treatment = rp.l10n_in_gst_treatment
+          FROM res_partner rp,
+               res_company comp
+          JOIN res_partner comp_p ON comp_p.id = comp.partner_id
+          JOIN res_country c ON c.id = comp_p.country_id
+         WHERE rp.id = p.partner_id
+           AND comp.id = p.company_id
+           AND c.code = 'IN'
+        """
+    )
