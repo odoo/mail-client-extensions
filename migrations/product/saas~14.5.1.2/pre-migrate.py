09f473f0bdc645d4d53b8c1de96c8dd8febfcbe6
Christophe Simonis <chs@odoo.com>
2021-09-08 16:04:47 +0000

    [FIX] {product,website_sale}/saas~14.5: correct modules version
    closes odoo/upgrade#2816
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/product/saas~14.5.1.2/pre-migrate.py
+++ migrations/product/saas~14.5.1.2/pre-migrate.py
@@ -11,13 +11,34 @@ def migrate(cr, version):
     util.remove_record(cr, "product.report_product_product_barcode")
     util.remove_record(cr, "product.report_product_template_barcode")
     util.remove_view(cr, "product.report_producttemplatebarcode")
+
     util.remove_field(cr, "product.attribute", "is_used_on_products")
     util.update_field_references(cr, "lst_price", "list_price", only_models=("product.template",))
     util.remove_field(cr, "product.template", "lst_price")
     util.remove_record(cr, "product.product_attribute_value_action")
+
     util.create_column(cr, "product_template", "detailed_type", "varchar")
-    util.parallel_execute(cr, util.explode_query_range(
+    util.parallel_execute(
         cr,
-        "UPDATE product_template SET detailed_type = type",
-        table="product_template",
-    ))
+        util.explode_query_range(
+            cr,
+            "UPDATE product_template SET detailed_type = type",
+            table="product_template",
+        ),
+    )
+
+    util.create_column(cr, "product_template_attribute_line", "value_count", "int4", default=0)
+
+    cr.execute(
+        """
+        WITH attcount AS (
+            SELECT product_template_attribute_line_id as id, count(*) as count
+              FROM product_attribute_value_product_template_attribute_line_rel
+          GROUP BY product_template_attribute_line_id
+        )
+        UPDATE product_template_attribute_line ptal
+           SET value_count = a.count
+          FROM attcount a
+         WHERE a.id = ptal.id
+    """
+    )
