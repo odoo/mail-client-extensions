fdc0ee581e76a1a9b078bbb417ac031eaa251bf8
Thibault Delavallée <tde@odoo.com>
2021-09-13 12:26:01 +0000

    [IMP] mail/saas~15.1: remove deprecated notification layout
    RIP ``mail_notification_borders``. You were a joyful comrade.
    
    Task-2621326
    Related to odoo/odoo#76418
    
    closes odoo/upgrade#2829
    
    Related: odoo/enterprise#20903
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/mail/saas~15.1.1.6/pre-migrate.py
+++ migrations/mail/saas~15.1.1.6/pre-migrate.py
@@ -4,6 +4,8 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.remove_view(cr, "mail.mail_notification_borders")
+
     util.rename_field(cr, "mail.resend.message", "has_cancel", "can_cancel")
 
     # Non-internal users can not receive notification in Odoo (only by email)
