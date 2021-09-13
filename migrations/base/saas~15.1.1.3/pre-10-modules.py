8f4de5721ca2149a38b50a6bffa0ae28db4a00ac
Yannick Tivisse <yti@odoo.com>
2021-09-13 08:54:00 +0000

    [IMP] pad/saas~14.5: Drop etherpad integration
    Purpose
    =======
    
    With the merge of https://github.com/odoo/odoo/pull/75768 , the new odoo editor
    is now collaborative, rendering pads useless. We are thus removing this
    feature in Project and Notes.
    
    Com PR: https://github.com/odoo/odoo/pull/76467
    TaskId:2643861
    
    closes odoo/upgrade#2826
    
    Related: odoo/odoo#76467
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/base/saas~15.1.1.3/pre-10-modules.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_module(cr, "note_pad")
+    util.remove_module(cr, "pad_project")
+    util.remove_module(cr, "pad")
