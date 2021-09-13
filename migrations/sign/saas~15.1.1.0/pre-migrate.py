4bfde03ee0372786687f1a36e675e7f6d392d007
Chong Wang (cwg) <cwg@odoo.com>
2021-09-13 20:53:44 +0000

    [IMP] sign/saas~15.1: add refusal_allowd for sign.request
    add refusal_allowd for sign.request and sign.send.request
    
    task-2614105
    
    see odoo/enterprise#19972
    
    closes odoo/upgrade#2704
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/sign/saas~15.1.1.0/pre-migrate.py
+++ migrations/sign/saas~15.1.1.0/pre-migrate.py
@@ -3,4 +3,6 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.create_column(cr, "sign_request", "refusal_allowed", "boolean")
+    util.create_column(cr, "sign_send_request", "refusal_allowed", "boolean")
     util.create_column(cr, "sign_template", "num_pages", "integer", default=None)
