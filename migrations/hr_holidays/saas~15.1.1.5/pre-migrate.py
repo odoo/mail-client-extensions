915b3c384997f9f4af3dcb39077ac67aeb0ce295
sofiagvaladze <sgv@odoo.com>
2021-09-22 09:45:21 +0000

    [IMP] hr_holidays/saas~15.1: Time Off smartbutton
    Community PR - odoo/odoo#75941
    task - 2634892
    
    closes odoo/upgrade#2852
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/hr_holidays/saas~15.1.1.5/pre-migrate.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+
+    util.remove_field(cr, "hr.employee.base", "allocation_used_display")
+    util.remove_field(cr, "hr.employee.base", "allocation_used_count")
+
+    util.remove_field(cr, "res.users", "allocation_used_display")
+    util.remove_field(cr, "res.users", "allocation_used_count")
