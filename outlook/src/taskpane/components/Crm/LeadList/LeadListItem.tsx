c39535be9c3cb3b675da8df2349aab6df435ed98
std-odoo <std@odoo.com>
2021-09-22 11:08:21 +0200

    [REF] outlook: simplify the "CRM" related components
    Purpose
    =======
    Simplify the code of the component "LeadListItem" by using async / await
    instead of promise and use more modern syntax.
    
    The "collapsed" logic should be on the CollapseSection component and
    not on the LeadSection component.
    
    Task-2601837

--- outlook/src/taskpane/components/Crm/LeadList/LeadListItem.tsx
+++ outlook/src/taskpane/components/Crm/LeadList/LeadListItem.tsx
@@ -15,36 +15,29 @@ type LeadsListItemProps = {
 class LeadListItem extends React.Component<LeadsListItemProps, {}> {
     openInOdoo = () => {
         const cids = this.context.getUserCompaniesString();
-        let url = api.baseURL + `/web#id=${this.props.lead.id}&model=crm.lead&view_type=form${cids}`;
+        const url = `${api.baseURL}/web#id=${this.props.lead.id}&model=crm.lead&view_type=form${cids}`;
         window.open(url, '_blank');
     };
 
     render() {
-        let expectedRevenueString = _t('%(expected_revenue)s at %(probability)s%', {
-            expected_revenue: this.props.lead.expectedRevenue,
-            probability: this.props.lead.probability,
-        });
-
-        if (this.props.lead.recurringPlan) {
-            expectedRevenueString = _t(
-                '%(expected_revenue)s + %(recurring_revenue)s %(recurring_plan)s at %(probability)s%',
-                {
-                    expected_revenue: this.props.lead.expectedRevenue,
-                    recurring_revenue: this.props.lead.recurringRevenue,
-                    recurring_plan: this.props.lead.recurringPlan,
-                    probability: this.props.lead.probability,
-                },
-            );
-        }
-
-        let expectedRevenuesText = <div className="lead-list-item-revenue-text">{expectedRevenueString}</div>;
+        const expectedRevenueString = _t(
+            this.props.lead.recurringPlan
+                ? '%(expected_revenue)s + %(recurring_revenue)s %(recurring_plan)s at %(probability)s%'
+                : '%(expected_revenue)s at %(probability)s%',
+            {
+                expected_revenue: this.props.lead.expectedRevenue,
+                recurring_revenue: this.props.lead.recurringRevenue,
+                recurring_plan: this.props.lead.recurringPlan,
+                probability: this.props.lead.probability,
+            },
+        );
 
         return (
             <div className="list-item-root-container" onClick={this.openInOdoo}>
                 <div className="list-item-container">
                     <div className="list-item-info-container">
                         <div className="list-item-title-text">{this.props.lead.name}</div>
-                        {expectedRevenuesText}
+                        <div className="lead-list-item-revenue-text">{expectedRevenueString}</div>
                     </div>
                     <Logger resId={this.props.lead.id} model="crm.lead" tooltipContent={_t('Log Email Into Lead')} />
                 </div>
--- outlook/src/taskpane/components/Crm/LeadsSection/LeadsSection.tsx
+++ outlook/src/taskpane/components/Crm/LeadsSection/LeadsSection.tsx
@@ -22,15 +22,12 @@ type LeadsSectionState = {
 class LeadsSection extends React.Component<LeadSectionProps, LeadsSectionState> {
     constructor(props, context) {
         super(props, context);
-        let isCollapsed = true;
-        if (props.partner.leads && props.partner.leads.length > 0) {
-            isCollapsed = false;
-        }
+        const isCollapsed = !props.partner.leads || !props.partner.leads.length;
         this.state = { leads: this.props.partner.leads, isCollapsed: isCollapsed };
     }
 
     private createOpportunityRequest = () => {
-        Office.context.mailbox.item.body.getAsync(Office.CoercionType.Html, (result) => {
+        Office.context.mailbox.item.body.getAsync(Office.CoercionType.Html, async (result) => {
             const message = result.value.split('<div id="x_appendonsend"></div>')[0]; // Remove the history and only log the most recent message.
             const subject = Office.context.mailbox.item.subject;
 
@@ -40,80 +37,62 @@ class LeadsSection extends React.Component<LeadSectionProps, LeadsSectionState> 
                 email_subject: subject,
             };
 
-            const logRequest = sendHttpRequest(
-                HttpVerb.POST,
-                api.baseURL + api.createLead,
-                ContentType.Json,
-                this.context.getConnectionToken(),
-                requestJson,
-                true,
-            );
-            logRequest.promise
-                .then((response) => {
-                    const parsed = JSON.parse(response);
-                    if (parsed['error']) {
-                        this.context.showTopBarMessage();
-                        return;
-                    } else {
-                        const cids = this.context.getUserCompaniesString();
-                        const action = 'crm_mail_plugin.crm_lead_action_form_edit';
-                        const url =
-                            api.baseURL +
-                            `/web#action=${action}&id=${parsed.result.lead_id}&model=crm.lead&view_type=form${cids}`;
-                        window.open(url);
-                    }
-                })
-                .catch((error) => {
-                    this.context.showHttpErrorMessage(error);
-                    console.log(error);
-                });
+            let response = null;
+            try {
+                response = await sendHttpRequest(
+                    HttpVerb.POST,
+                    api.baseURL + api.createLead,
+                    ContentType.Json,
+                    this.context.getConnectionToken(),
+                    requestJson,
+                    true,
+                ).promise;
+            } catch (error) {
+                this.context.showHttpErrorMessage(error);
+                return;
+            }
+
+            const parsed = JSON.parse(response);
+            if (parsed['error']) {
+                this.context.showTopBarMessage();
+                return;
+            }
+            const cids = this.context.getUserCompaniesString();
+            const action = 'crm_mail_plugin.crm_lead_action_form_edit';
+            const url = `${api.baseURL}/web#action=${action}&id=${parsed.result.lead_id}&model=crm.lead&view_type=form${cids}`;
+            window.open(url);
         });
     };
 
-    private onCollapseButtonClick = () => {
-        this.setState({ isCollapsed: !this.state.isCollapsed });
+    private getLeadsSection = () => {
+        if (!this.props.partner.isAddedToDatabase()) {
+            return <div className="list-text">{_t('Save Contact to create new Opportunities.')}</div>;
+        } else if (this.state.leads.length > 0) {
+            return (
+                <div className="section-content">
+                    {this.state.leads.map((lead) => (
+                        <LeadListItem lead={lead} key={lead.id} />
+                    ))}
+                </div>
+            );
+        }
+        return <div className="list-text">{_t('No opportunities found for this contact.')}</div>;
     };
 
     render() {
-        let leadsExpanded = null;
-
-        let title = _t('Opportunities');
-
-        if (!this.props.partner.isAddedToDatabase()) {
-            if (!this.state.isCollapsed) {
-                leadsExpanded = <div className="list-text">{_t('Save Contact to create new Opportunities.')}</div>;
-            }
-        } else {
-            if (!this.state.isCollapsed) {
-                let leadsContent = null;
-                if (this.state.leads.length > 0) {
-                    let leads = this.state.leads.map((lead) => {
-                        return <LeadListItem lead={lead} key={lead.id} />;
-                    });
-                    leadsContent = <div className="section-content">{leads}</div>;
-                } else {
-                    leadsContent = <div className="list-text">{_t('No opportunities found for this contact.')}</div>;
-                }
-                leadsExpanded = <div>{leadsContent}</div>;
-            }
-        }
-
-        if (this.state.leads)
-            title = _t('Opportunities (%(count)s)', {
-                count: this.props.partner.leads.length.toString(),
-            });
+        const leadCount = this.state.leads && this.state.leads.length;
+        const title = this.state.leads
+            ? _t('Opportunities (%(count)s)', { count: leadCount.toString() })
+            : _t('Opportunities');
 
         return (
-            <>
-                <CollapseSection
-                    onCollapseButtonClick={this.onCollapseButtonClick}
-                    isCollapsed={this.state.isCollapsed}
-                    title={title}
-                    hasAddButton={this.props.partner.isAddedToDatabase()}
-                    onAddButtonClick={this.createOpportunityRequest}>
-                    {leadsExpanded}
-                </CollapseSection>
-            </>
+            <CollapseSection
+                isCollapsed={this.state.isCollapsed}
+                title={title}
+                hasAddButton={this.props.partner.isAddedToDatabase()}
+                onAddButtonClick={this.createOpportunityRequest}>
+                {this.getLeadsSection()}
+            </CollapseSection>
         );
     }
 }
