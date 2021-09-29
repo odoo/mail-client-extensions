ff2b477940381ecc9304b5c59904c8dca5e78c63
Noe Antoine <nan@odoo.com>
2021-09-29 15:26:44 +0000

    [ADD] appointment/saas~15.2: Split appointment module on hr dependancy
    Remove useless layer between appointment types and its staff. Remove all
    hr.employee related fields and replace them with res.users related fields,
    throughout the module. Recover the stored-field data from previous existing
    table before deleting it.
    
    Rename template id according to module rewording.
    
    This comes with the splitting of appointment module in two parts:
    -appointment containing the main features, and new bridge module
    -appointment_hr containing the ones depending on hr (work schedules...)
    
    --- Links ---
    Task-2499566
    ENT PR - odoo/enterprise#17934
    COM PR - odoo/odoo#82234
    
    closes odoo/upgrade#2578
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

---
+++ migrations/appointment/saas~15.2.1.1/pre-migrate.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+
+    util.create_m2m(cr, "appointment_type_res_users_rel", "calendar_appointment_type", "res_users")
+
+    # Field employee_ids (hr.employee) is replaced with staff_user_ids (res.users) on calendar.apointment.type
+    # We fill the new table with users corresponding to the existing employees, then remove the table.
+    cr.execute(
+        """
+       INSERT INTO appointment_type_res_users_rel(calendar_appointment_type_id, res_users_id)
+            SELECT wct.calendar_appointment_type_id, emp.user_id
+              FROM appointment_type_employee_rel wct
+              JOIN hr_employee emp ON wct.hr_employee_id = emp.id
+             WHERE emp.user_id IS NOT NULL
+        """
+    )
+    cr.execute("DROP TABLE appointment_type_employee_rel")
+    util.remove_field(cr, "calendar.appointment.type", "employee_ids")
+
+    util.remove_field(cr, "calendar.appointment.share", "suggested_employee_ids")
+    util.remove_field(cr, "calendar.appointment.share", "employee_ids")
+    util.rename_xmlid(cr, "appointment.employee_select", "appointment.staff_user_select")
