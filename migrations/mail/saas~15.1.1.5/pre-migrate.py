75917da2239811a7a7eef3f39ebf421584c25ef8
std-odoo <std@odoo.com>
2021-10-05 06:16:13 +0000

    [IMP] mail/saas~15.1: non-internal users receive notification by email
    Purpose
    =======
    Non-internal users can now only receive notification by email. It make
    no sense for a portal user to receive notification in Discuss, so we
    added a constraint.
    
    Task-2508521
    COM odoo/odoo#77766
    ENT odoo/enterprise#21420
    UPG odoo/upgrade#2890
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>
    Co-authored-by: nounoubensebia <neb@odoo.com>

--- migrations/mail/saas~15.1.1.5/pre-migrate.py
+++ migrations/mail/saas~15.1.1.5/pre-migrate.py
@@ -5,3 +5,18 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.rename_field(cr, "mail.resend.message", "has_cancel", "can_cancel")
+
+    # Non-internal users can not receive notification in Odoo (only by email)
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            """
+        UPDATE res_users
+           SET notification_type = 'email'
+         WHERE share = TRUE
+           AND notification_type != 'email'
+           """,
+            table="res_users",
+        ),
+    )
