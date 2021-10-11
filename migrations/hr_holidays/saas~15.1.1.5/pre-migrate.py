7ae318d97d043a89ef185e30c82dafd376af634e
Kevin Baptiste <kba@odoo.com>
2021-10-11 08:10:33 +0000

    [IMP] hr_holidays/saas~15.1: rename group_days_allocation
    Rename to `allocation_group` to convey its proper meaning.
    
    Related: odoo/odoo#78128
    Taskid: 2665866
    Part-of: odoo/upgrade#2907

--- migrations/hr_holidays/saas~15.1.1.5/pre-migrate.py
+++ migrations/hr_holidays/saas~15.1.1.5/pre-migrate.py
@@ -10,3 +10,5 @@ def migrate(cr, version):
 
     util.remove_field(cr, "res.users", "allocation_used_display")
     util.remove_field(cr, "res.users", "allocation_used_count")
+
+    util.rename_field(cr, "hr.leave.type", "group_days_allocation", "allocation_count")
