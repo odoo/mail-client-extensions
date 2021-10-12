cb2e869816b9e6767c75b5174fdec76b5ecdb99b
Denis Ledoux <dle@odoo.com>
2021-10-12 08:57:42 +0000

    [IMP] account@10.saas~18: speed-up `price_total` computation
    Before this revision, the `price_total` field is computed invoice line
    by invoice line, which can take a while when there is a huge number
    of invoice lines.
    
    The strategy here is to compute together the lines that at the end
    will have the same price total,
    when they share the same price, quantity and taxes.
    Because there is a high chance when there is a lot of invoice lines
    that a lot of them sells the same thing at the same price within the same
    quantity.
    
    By default, `compute_all` doesn't depend on the product or the partner,
    it's only when the invoice lines uses a python tax that potentially
    it could uses the product or the partner, as its passed in the
    evaluation context so users could potentially use them.
    https://github.com/odoo/odoo/blob/11.0/addons/account/models/account.py#L913-L1020
    https://github.com/odoo/odoo/blob/11.0/addons/account/models/account.py#L876-L899
    https://github.com/odoo/odoo/blob/11.0/addons/account_tax_python/models/account_tax.py#L39-L47
    
    So, group the invoice line by taxes, price, currency and quantity
    when the line doesn't use a python taxes, and compute them together,
    otherwise add the product and partner in addition.
    
    This allows to speed up the processing speed from 5 hours to 30 minutes
    for the request 31554
    2021-09-21 19:26:05,987 28 INFO db_31554 odoo.addons.base.maintenance.migrations.account.saas-18.end-invoice-line-total: [99.83%] 1145820/1147781 invoice lines processed in 5:06:08.807473 (total estimated time: 5:06:40.244550)
    
    upg-31554
    
    closes odoo/upgrade#2915
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/10.saas~18.1.1/end-invoice-line-total.py
+++ migrations/account/10.saas~18.1.1/end-invoice-line-total.py
@@ -9,31 +9,56 @@ _logger = logging.getLogger(NS + __name__)
 
 def migrate(cr, version):
     env = util.env(cr)
-    cr.execute(
-        """
-        SELECT l.id, array_agg(r.tax_id) as taxes,
-               l.price_unit * (1 - COALESCE(l.discount, 0) / 100.0) as price,
-               i.currency_id, l.quantity, l.product_id, i.partner_id
-          FROM account_invoice_line_tax r
-          JOIN account_invoice_line l ON (l.id = r.invoice_line_id)
-          JOIN account_invoice i ON (i.id = l.invoice_id)
-         WHERE l.price_total IS NULL
-      GROUP BY l.id, price, i.currency_id, l.quantity, l.product_id, i.partner_id
-    """
-    )
-
-    size = cr.rowcount
-    for row in util.log_progress(cr.fetchall(), _logger, qualifier="invoice lines", size=size):
-        lid, taxes, price, currency, quantity, product, partner = row
-
-        tca = (
-            env["account.tax"]
-            .browse(taxes)
-            .json_friendly_compute_all(
-                price, currency_id=currency, quantity=quantity, product_id=product, partner_id=partner
+    for name, amount_type, group_by in [
+        (
+            "invoice lines without python taxes",
+            "bool_and(tax.amount_type != 'code')",
+            "taxes, price, currency_id, quantity",
+        ),
+        (
+            "invoice lines with python taxes",
+            "bool_or(tax.amount_type = 'code')",
+            "taxes, price, currency_id, quantity, product_id, partner_id",
+        ),
+    ]:
+        cr.execute(
+            """
+            WITH lines AS (
+                SELECT l.id, array_agg(r.tax_id ORDER BY r.tax_id) as taxes,
+                    l.price_unit * (1 - COALESCE(l.discount, 0) / 100.0) as price,
+                    i.currency_id, l.quantity, l.product_id, i.partner_id
+                FROM account_invoice_line_tax r
+                JOIN account_tax tax ON tax.id = r.tax_id
+                JOIN account_invoice_line l ON (l.id = r.invoice_line_id)
+                JOIN account_invoice i ON (i.id = l.invoice_id)
+                WHERE l.price_total IS NULL
+            GROUP BY l.id, price, i.currency_id, l.quantity, l.product_id, i.partner_id
+            HAVING %(amount_type)s
             )
+            SELECT array_agg(id) as line_ids, %(group_by)s
+                FROM lines
+            GROUP BY %(group_by)s
+        """
+            % {"amount_type": amount_type, "group_by": group_by}
         )
-        cr.execute("UPDATE account_invoice_line SET price_total=%s WHERE id=%s", [tca["total_included"], lid])
+
+        size = cr.rowcount
+        for row in util.log_progress(cr.dictfetchall(), _logger, qualifier=name, size=size):
+            tca = (
+                env["account.tax"]
+                .browse(row["taxes"])
+                .json_friendly_compute_all(
+                    row["price"],
+                    currency_id=row["currency_id"],
+                    quantity=row["quantity"],
+                    product_id=row.get("product_id"),
+                    partner_id=row.get("partner_id"),
+                )
+            )
+            cr.execute(
+                "UPDATE account_invoice_line SET price_total=%s WHERE id IN %s",
+                [tca["total_included"], tuple(row["line_ids"])],
+            )
 
     cr.execute("UPDATE account_invoice_line SET price_total=price_subtotal WHERE price_total IS NULL")
 
