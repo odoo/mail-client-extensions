22ba606e26218323079859d9bdfeff55da77ec33
Christophe Simonis <chs@odoo.com>
2021-10-12 15:54:12 +0000

    [FIX] sales_team/saas~14.2: correctly map the `sales_team.crm_team_member_admin_sales` xmlid
    It should point to an existing record. And it should not be created by
    the ORM to avoid issues
    
    tg-93
    upg-41274
    upg-41392
    upg-41573
    
    closes odoo/upgrade#2918
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/sales_team/saas~14.2.1.1/pre-migrate.py
+++ migrations/sales_team/saas~14.2.1.1/pre-migrate.py
@@ -81,15 +81,37 @@ def migrate(cr, version):
     # avoid issues with admin due to sales_team data crm_team_member_admin_sales
     # that makes admin member of team_sales_department. We should not try to create
     # it twice as a constraint will raise.
+    # We should not let the ORM create the record as it may not be possible due to multi-companies issues.
+    admin_id = util.ref(cr, "base.user_admin")
+    cr.execute("SELECT crm_team_id FROM crm_team_member WHERE user_id = %s AND active", [admin_id])
+    if cr.rowcount:
+        team_id = cr.fetchone()[0]
+    else:
+        cr.execute(
+            """
+            INSERT INTO crm_team_member (crm_team_id, user_id, active)
+                 SELECT t.id, u.id, true
+                   FROM res_users u
+                   JOIN crm_team t ON t.company_id = u.company_id OR t.company_id IS NULL
+                  WHERE u.id = %s
+               ORDER BY t.sequence
+                  LIMIT 1
+              RETURNING crm_team_id
+            """,
+            [admin_id],
+        )
+        team_id = cr.fetchone()[0]
+
     util.ensure_xmlid_match_record(
         cr,
         "sales_team.crm_team_member_admin_sales",
         "crm.team.member",
         {
-            "user_id": util.ref(cr, "base.user_admin"),
-            "crm_team_id": util.ref(cr, "sales_team.team_sales_department"),
+            "user_id": admin_id,
+            "crm_team_id": team_id,
         },
     )
+    # for the demo data, we can hardcode the team.
     user_demo, team_demo = util.ref(cr, "base.user_demo"), util.ref(cr, "sales_team.crm_team_1")
     if user_demo and team_demo:
         util.ensure_xmlid_match_record(
