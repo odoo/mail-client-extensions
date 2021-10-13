1a3ab7fd3ad983050ad4277667744303a528815a
Christophe Simonis <chs@odoo.com>
2021-10-13 08:26:12 +0000

    [FIX] base/saas~15.1: auto discovery modules
    closes odoo/upgrade#2899
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~15.1.1.3/pre-10-modules.py
+++ migrations/base/saas~15.1.1.3/pre-10-modules.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.modules_auto_discovery(cr)
+
     util.remove_module(cr, "note_pad")
     util.remove_module(cr, "pad_project")
     util.remove_module(cr, "pad")
