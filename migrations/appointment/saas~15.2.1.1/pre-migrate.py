8ff4ac602adf9906efc5776f48e8e54a7cd3fdf1
Fabio Barbero <faba@odoo.com>
2021-10-22 07:20:15 +0000

    [IMP] appointment/saas~15.2.1.1: remove appointment_select_template
    Purpose
    =======
    Remove template appointment_select_timezone as it is now displayed by
    default.
    
    Upgrade: odoo/upgrade/pull/2954
    Community: odoo/odoo/pull/78771
    Enterprise: odoo/enterprise/pull/21824
    Task-2672237
    
    closes odoo/upgrade#2954
    
    Related: odoo/enterprise#21824
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/appointment/saas~15.2.1.1/pre-migrate.py
+++ migrations/appointment/saas~15.2.1.1/pre-migrate.py
@@ -4,7 +4,6 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-
     util.create_m2m(cr, "appointment_type_res_users_rel", "calendar_appointment_type", "res_users")
 
     # Field employee_ids (hr.employee) is replaced with staff_user_ids (res.users) on calendar.apointment.type
@@ -24,3 +23,4 @@ def migrate(cr, version):
     util.remove_field(cr, "calendar.appointment.share", "suggested_employee_ids")
     util.remove_field(cr, "calendar.appointment.share", "employee_ids")
     util.rename_xmlid(cr, "appointment.employee_select", "appointment.staff_user_select")
+    util.remove_view(cr, "appointment.appointment_select_timezone")
