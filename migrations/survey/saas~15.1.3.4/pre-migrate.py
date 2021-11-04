379c273ece8cd40916e501ae6643de4730e0525d
David Beguin <dbe@odoo.com>
2021-11-04 10:48:41 +0000

    [IMP] survey/saas~15.1: add placeholder field to survey questions.
    This commit create the column for the new placeholder field before migration
    by ORM. That way it won't add the column itself and will skip the compute
    method that would have set False to all records, which is a bit useless.
    
    Task-2671388
    COM PR: odoo/odoo#78682
    
    closes odoo/upgrade#2998
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/survey/saas~15.1.3.4/pre-migrate.py
+++ migrations/survey/saas~15.1.3.4/pre-migrate.py
@@ -6,3 +6,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     for view_type in ["form", "tree", "kanban"]:
         util.rename_xmlid(cr, f"survey.survey_{view_type}", f"survey.survey_survey_view_{view_type}")
+    util.create_column(cr, "survey_question", "question_placeholder", "varchar")
