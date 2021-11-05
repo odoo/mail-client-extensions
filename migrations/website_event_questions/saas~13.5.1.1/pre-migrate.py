019b982ca098d5828914e62ced4fa50d09dc8bf3
Christophe Simonis <chs@odoo.com>
2021-11-05 10:58:58 +0000

    [chore] website_event_questions/saas~13.5: format file
    Part-of: odoo/upgrade#3007

--- migrations/website_event_questions/saas~13.5.1.1/pre-migrate.py
+++ migrations/website_event_questions/saas~13.5.1.1/pre-migrate.py
@@ -5,5 +5,8 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.rename_xmlid(cr, "website_event_questions.event_event_view_form_inherit_question",
-                      "website_event_questions.event_event_view_form")
+    util.rename_xmlid(
+        cr,
+        "website_event_questions.event_event_view_form_inherit_question",
+        "website_event_questions.event_event_view_form",
+    )
