86f03d0be25a7c408613f3878a2a0ee47a4b52bb
Christophe Simonis <chs@odoo.com>
2021-11-05 11:45:48 +0000

    [chore] *: remove useless `LICENSE` comments
    There is no `LICENSE` file in this repository
    
    Part-of: odoo/upgrade#3007

--- migrations/mail/tests/__init__.py
+++ migrations/mail/tests/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import test_jinja_to_qweb, test_mail_v15
--- migrations/quality_mrp_workorder/tests/test_mrp_v14.py
+++ migrations/quality_mrp_workorder/tests/test_mrp_v14.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import release
 from odoo.tests import Form, tagged
