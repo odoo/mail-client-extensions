06fd525f0550977564845d76fbce6b7a54b38200
wde-odoo <wde@odoo.com>
2021-11-09 13:56:30 +0000

    [IMP] survey/saas~15.1.3.4: remove obsolete actions & fields
    Community commit polished the survey module and removed obsolete actions
    menus and fields. Notably "allow value image" field was used as a toggle
    which is not desired anymore.
    
    Task-2600241
    See odoo/odoo#79813
    
    closes odoo/upgrade#3033
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/survey/saas~15.1.3.4/pre-migrate.py
+++ migrations/survey/saas~15.1.3.4/pre-migrate.py
@@ -6,4 +6,9 @@ from odoo.upgrade import util
 def migrate(cr, version):
     for view_type in ["form", "tree", "kanban"]:
         util.rename_xmlid(cr, f"survey.survey_{view_type}", f"survey.survey_survey_view_{view_type}")
+    util.remove_record(cr, "survey.survey_action_server_clean_test_answers")
+    util.remove_record(cr, "survey.survey_menu_user_inputs")
+
     util.create_column(cr, "survey_question", "question_placeholder", "varchar")
+
+    util.remove_field(cr, "survey.question", "allow_value_image")
