660ecfae1e52c77a9062e9904ceb5b4dbf8e090a
Alvaro Fuentes <afu@odoo.com>
2021-11-17 08:17:37 +0000

    [chore] sale_subscription/saas~12.4.1.1: blacked
    String format.
    
    closes odoo/upgrade#3039
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/sale_subscription/saas~12.4.1.1/pre-10-account-pocalypse.py
+++ migrations/sale_subscription/saas~12.4.1.1/pre-10-account-pocalypse.py
@@ -3,7 +3,7 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    util.create_column(cr, 'account_move_line', 'subscription_id', 'int4')
-    util.create_column(cr, 'account_move_line', 'subscription_start_date', 'date')
-    util.create_column(cr, 'account_move_line', 'subscription_end_date', 'date')
-    util.create_column(cr, 'account_move_line', 'subscription_mrr', 'numeric')
+    util.create_column(cr, "account_move_line", "subscription_id", "int4")
+    util.create_column(cr, "account_move_line", "subscription_start_date", "date")
+    util.create_column(cr, "account_move_line", "subscription_end_date", "date")
+    util.create_column(cr, "account_move_line", "subscription_mrr", "numeric")
