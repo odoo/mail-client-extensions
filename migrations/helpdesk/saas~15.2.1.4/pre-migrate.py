2cf655d9d8007f236552558f37c4020114098e8f
Xavier BOL (xbo) <xbo@odoo.com>
2021-12-03 11:37:54 +0000

    [IMP] helpdesk/saas~15.2: remove customer_satisfaction field
    This field has been removed because we will use the rating average
    defined in the rating mixin.
    
    task-2671848
    
    Community PR: odoo/odoo#81027
    Enterprise PR: odoo/enterprise#22773
    
    closes odoo/upgrade#3094
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/helpdesk/saas~15.2.1.4/pre-migrate.py
+++ migrations/helpdesk/saas~15.2.1.4/pre-migrate.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from odoo.upgrade import util
+
 
 def migrate(cr, version):
-
+    util.remove_field(cr, "helpdesk.team", "customer_satisfaction")
     cr.execute("ALTER TABLE res_users ALTER COLUMN helpdesk_target_closed TYPE int4")
