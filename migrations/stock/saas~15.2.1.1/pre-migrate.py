2d1fa1a814ffb5f861ae7121c35a505fc91f1bf0
William Henrotin <whe@odoo.com>
2021-12-06 10:02:04 +0000

    [IMP] stock/saas~15.2: rename route model
    Rename model `stock.location.route` into `stock.route` to have better
    naming convention.
    
    See odoo/odoo#80434
    odoo/enterprise#22535
    
    Task: 2648449
    Part-of: odoo/upgrade#3068

--- migrations/stock/saas~15.2.1.1/pre-migrate.py
+++ migrations/stock/saas~15.2.1.1/pre-migrate.py
@@ -6,3 +6,8 @@ def migrate(cr, version):
 
     util.rename_xmlid(cr, "stock.access_stock_production_lot", "stock.access_stock_lot")
     util.rename_model(cr, "stock.production.lot", "stock.lot")
+
+    util.rename_model(cr, "stock.location.route", "stock.route")
+    cr.execute("ALTER TABLE stock_location_route_categ RENAME TO stock_route_categ")
+    cr.execute("ALTER TABLE stock_location_route_packaging RENAME TO stock_route_packaging")
+    cr.execute("ALTER TABLE stock_location_route_move RENAME TO stock_route_move")
