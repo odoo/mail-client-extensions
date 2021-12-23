dba9bd88d9621ee8d59c12b37b76667a61a950f5
Thibault Francois <tfr@odoo.com>
2021-12-23 11:18:49 +0000

    [ADD] l10n_mx_edi_extended/saas~15.1: xml_id for cities
    Since Mexico cities loading has been moved
    from l10n_mx_edi to l10n_mx_edi_extended
    in stable (15.0 https://github.com/odoo/enterprise/pull/22951)
    we take the opportunity of the next migration
    to clean the xml_id module
    
    closes odoo/upgrade#3164
    
    Signed-off-by: Thibault Francois <tfr@odoo.com>

---
+++ migrations/l10n_mx_edi_extended/saas~15.1.0.1/post-01-city.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+def migrate(cr, version):
+    # Move external ids of city in the right module in case they were in the wrong one
+    cr.execute(
+        """
+        UPDATE ir_model_data
+          SET module = 'l10n_mx_edi_extended'
+        WHERE module = 'l10n_mx_edi'
+          AND model = 'res.city'
+        """
+    )
