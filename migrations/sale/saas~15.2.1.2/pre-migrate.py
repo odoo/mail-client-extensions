e4e4193643e1dca5527da23ea499e04440e1f526
Victor Feyens <vfe@odoo.com>
2021-12-27 14:25:22 +0000

    [IMP] sale/saas~15.2: remove old unused reporting models
    See odoo/odoo#82070
    See odoo/enterprise#23183
    
    Part-of: odoo/upgrade#3135

--- migrations/sale/saas~15.2.1.2/pre-migrate.py
+++ migrations/sale/saas~15.2.1.2/pre-migrate.py
@@ -6,3 +6,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "sale.order.line", "qty_delivered_manual")
     util.remove_view(cr, "sale.report_invoice_document_inherit_sale")
+    util.remove_model(cr, "report.all.channels.sales")
+    util.remove_model(cr, "report.sale.report_saleproforma")
