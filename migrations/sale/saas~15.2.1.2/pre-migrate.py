85c1e89479e5e77638196436698a608d909444f6
Victor Feyens <vfe@odoo.com>
2021-12-30 13:34:57 +0000

    [IMP] sale/saas~15.2: move sale_mngt menu to sale module
    See odoo/odoo#82070
    See odoo/enterprise#23183
    
    Part-of: odoo/upgrade#3135

--- migrations/sale/saas~15.2.1.2/pre-migrate.py
+++ migrations/sale/saas~15.2.1.2/pre-migrate.py
@@ -8,3 +8,5 @@ def migrate(cr, version):
     util.remove_view(cr, "sale.report_invoice_document_inherit_sale")
     util.remove_model(cr, "report.all.channels.sales")
     util.remove_model(cr, "report.sale.report_saleproforma")
+
+    util.rename_xmlid(cr, "sale_management.menu_product_attribute_action", "sale.menu_product_attribute_action")
