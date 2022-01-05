f5ea8c0ffd142a8c076aafe05dd37dbeba4377fb
Mahendra Barad <mba@odoo.com>
2022-01-05 12:39:35 +0000

    [IMP] hr_timesheet/saas~15.3: remove the unused field
    Remove the unused field has_planned_hours_tasks and recompute the
    allocated_hours from the sale_timesheet.
    
    Related Community PR: https://github.com/odoo/odoo/pull/82019
    Related Enterprise PR: https://github.com/odoo/enterprise/pull/23148
    
    task-2692856
    
    closes odoo/upgrade#3149
    
    Related: odoo/odoo#82019
    Related: odoo/enterprise#23148
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr_timesheet/saas~15.3.1.0/pre-migrate.py
+++ migrations/hr_timesheet/saas~15.3.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "hr_timesheet.project_sharing_inherit_project_task_view_search")
+    util.remove_field(cr, "project.project", "has_planned_hours_tasks")
