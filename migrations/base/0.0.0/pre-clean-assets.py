d34684239c42b304f4daf1ba2f20ccd4f7d2b18a
Alvaro Fuentes <afu@odoo.com>
2022-01-05 17:34:21 +0000

    [FIX] base/0.0.0: prallelize queries
    Before (~10h):
    ```
    2021-12-21 03:47:03,850 35 INFO db_70714 odoo.modules.migration: module base: Running migration [>0.0.0] pre-clean-assets
    2021-12-21 13:13:35,609 35 INFO db_70714 odoo.modules.migration: module base: Running migration [>0.0.0] pre-clean-imd
    ```
    
    After (~1.5h):
    ```
    2022-01-05 15:58:17,385 29 INFO 70714_afu_13.0 odoo.modules.migration: module base: Running migration [>0.0.0] pre-clean-assets
    2022-01-05 15:58:32,379 29 INFO 70714_afu_13.0 odoo.addons.base.maintenance.migrations.util.pg: [100.00%] 635/635 queries processed in 0:00:14.974151
    2022-01-05 15:59:42,409 29 INFO 70714_afu_13.0 odoo.addons.base.maintenance.migrations.util.pg: [0.31%] 2/635 queries processed in 0:01:10.018724
    ...
    2022-01-05 17:31:11,617 29 INFO 70714_afu_13.0 odoo.addons.base.maintenance.migrations.util.pg: [100.00%] 635/635 queries processed in 1:32:39.227231
    2022-01-05 17:31:11,620 29 INFO 70714_afu_13.0 odoo.modules.migration: module base: Running migration [>0.0.0] pre-clean-imd
    ```
    
    upg-70714
    
    closes odoo/upgrade#3151
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/0.0.0/pre-clean-assets.py
+++ migrations/base/0.0.0/pre-clean-assets.py
@@ -1,27 +1,37 @@
 # -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
     # bundles
-    cr.execute(
-        r"""
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            r"""
             DELETE FROM ir_attachment
                   WHERE res_model = 'ir.ui.view'
                     AND COALESCE(res_id, 0) = 0
                     AND res_field IS NULL
                     AND mimetype IN ('text/css', 'application/javascript')
                     AND url ~ '^/web/(content|assets)/\d+-[0-9a-f]+/.*\.(css|js)$'
-                    AND url LIKE '%assets%'
-    """
+            """,
+            table="ir_attachment",
+        ),
     )
     # individual css files
-    cr.execute(
-        r"""
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            r"""
             DELETE FROM ir_attachment
                   WHERE res_model IS NULL
                     AND COALESCE(res_id, 0) = 0
                     AND res_field IS NULL
                     AND mimetype = 'text/css'
                     AND url ~ '^/[\w_]+/static/.*\.(scss|less|sass).css$'
-        """
+            """,
+            table="ir_attachment",
+        ),
     )
