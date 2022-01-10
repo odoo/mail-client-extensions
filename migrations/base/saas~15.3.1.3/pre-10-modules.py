cb5d06fa0c3494ddfba4c050694961ecce77a5b9
Arnaud Joset <arj@odoo.com>
2022-01-10 13:15:44 +0000

    [IMP] base/saas~15.3: remove sale_subscription_timesheet
    sale_subscription_timesheet module is no longer after
    sale_subscription becomes sale_order.
    
    Taskid: 2568007
    Enterprise: odoo/enterprise#21825
    Community: odoo/odoo#82632
    Part-of: odoo/upgrade#3174

--- migrations/base/saas~15.3.1.3/pre-10-modules.py
+++ migrations/base/saas~15.3.1.3/pre-10-modules.py
@@ -8,6 +8,6 @@ def migrate(cr, version):
     util.merge_module(cr, "sale_project_account", "sale_project")
     util.merge_module(cr, "account_sale_timesheet", "sale_project")
     util.modules_auto_discovery(cr)
-
     if util.has_enterprise():
         util.remove_module(cr, "delivery_barcode")
+        util.remove_module(cr, "sale_subscription_timesheet")
