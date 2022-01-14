d077ce962d83bd02c0a2d7fe8b6ee4025a09670a
h4818 <ayh@odoo.com>
2022-01-14 16:40:43 +0000

    [IMP] account_asset/saas~15.2: merge wizards
    related odoo/enterprise#23514
    
    closes odoo/upgrade#3179
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/account_asset/saas~15.2.1.0/pre-migrate.py
+++ migrations/account_asset/saas~15.2.1.0/pre-migrate.py
@@ -4,3 +4,9 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "account_asset", "non_deductible_tax_value", "numeric", default=0.0)
+
+    util.remove_view(cr, "account_asset.asset_sell_form")
+    util.remove_view(cr, "account_asset.asset_pause_form")
+
+    util.remove_model(cr, "account.asset.sell")
+    util.remove_model(cr, "account.asset.pause")
