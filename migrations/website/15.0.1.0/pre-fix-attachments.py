074aea3eb3c3469c75973a69a23f4838a75ab634
Augusto Perez (aup) <aup@odoo.com>
2022-01-20 14:27:19 +0000

    [FIX] website/15.0.1.0: fix attachments
    A fix was made to nullyfy theme_template_id on records that has original_id set
    https://github.com/odoo/upgrade/pull/3110
    
    Script is was executed in pre but field was not always created in the db.
    This attemps to fix it by executing the script after the field is created.
    
    closes odoo/upgrade#3186
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website/15.0.1.0/pre-fix-attachments.py
+++ migrations/website/15.0.1.0/pre-fix-attachments.py
@@ -2,10 +2,13 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    query = """
-            UPDATE ir_attachment
-               SET theme_template_id = NULL
-             WHERE original_id IS NOT NULL
-            """
 
-    util.parallel_execute(cr, util.explode_query_range(cr, query, table="ir_attachment"))
+    # Column check intended to handle databases from 13.x that have website_theme_install uninstalled
+    if util.column_exists(cr, "ir_attachment", "theme_template_id"):
+        query = """
+                UPDATE ir_attachment
+                   SET theme_template_id = NULL
+                 WHERE original_id IS NOT NULL
+                """
+
+        util.parallel_execute(cr, util.explode_query_range(cr, query, table="ir_attachment"))
