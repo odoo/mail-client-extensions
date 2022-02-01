2e15986b23b1e63886ff525221ff9c3ac0d45a18
Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>
2022-02-01 11:49:28 +0000

    [IMP] account/saas~15.2.1.1: add necessary fields for storno accounting
    This commits add the necessary fields for storno accounting
    
    See odoo/odoo#79581
    
    closes odoo/upgrade#3212
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/saas~15.2.1.1/pre-migrate.py
+++ migrations/account/saas~15.2.1.1/pre-migrate.py
@@ -5,3 +5,6 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.move_field_to_module(cr, "account.move", "partner_shipping_id", "sale", "account")
     util.move_field_to_module(cr, "res.config.settings", "group_delivery_invoice_address", "sale", "account")
+    util.create_column(cr, "account_move", "is_storno", "bool", default=False)
+    util.create_column(cr, "account_chart_template", "use_storno_accounting", "bool", default=False)
+    util.create_column(cr, "res_company", "account_storno", "bool", default=False)
