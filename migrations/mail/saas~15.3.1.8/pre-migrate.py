cda9c9dde82494e0a7c700a8041fdaec3b63d71e
Didier (did) <did@odoo.com>
2022-02-03 12:50:43 +0000

    [IMP] mail/saas~15.3: Remove field in mail.thread
    Remove message_unread_counter and message_unread.
    
    See odoo/odoo#83765
    
    closes odoo/upgrade#3222
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/mail/saas~15.3.1.8/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "mail.thread", "message_unread")
+    util.remove_field(cr, "mail.thread", "message_unread_counter")
