878a06d36cd057ebc70d372b8fa4cdb349a564bf
Christophe Simonis <chs@odoo.com>
2022-02-07 09:53:17 +0000

    [FIX] base/saas~15.2: Correctly handle merge of payroll modules
    Part-of: odoo/upgrade#3349

--- migrations/base/saas~15.2.1.3/pre-10-modules.py
+++ migrations/base/saas~15.2.1.3/pre-10-modules.py
@@ -10,6 +10,13 @@ def migrate(cr, version):
         util.merge_module(cr, "l10n_be_hr_payroll_december", "l10n_be_hr_payroll")
         util.merge_module(cr, "l10n_be_hr_payroll_holiday_pay_recovery", "l10n_be_hr_payroll")
         util.merge_module(cr, "l10n_be_hr_payroll_report_measures", "l10n_be_hr_payroll")
+
+        # merge common salary rules
+        r1 = util.ref(cr, "l10n_be_hr_contract_salary_group_insurance.cp200_employees_termination_fees_group_insurance")
+        r2 = util.ref(cr, "l10n_be_hr_payroll.cp200_employees_termination_fees_group_insurance")
+        if r1 and r2:
+            util.replace_record_references_batch(cr, {r1: r2}, "hr.salary.rule", replace_xmlid=False)
+
         util.merge_module(cr, "l10n_be_hr_contract_salary_group_insurance", "l10n_be_hr_payroll")
 
     util.merge_module(cr, "base_address_city", "base_address_extended")
