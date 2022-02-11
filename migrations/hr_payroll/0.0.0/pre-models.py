283daec90d1c22326c6a83160f5da66c953f4b67
Christophe Simonis <chs@odoo.com>
2022-02-11 09:26:28 +0000

    [ADD] hr_payroll/0.0.0: `_match_uniq` parameters and values
    Part-of: odoo/upgrade#3349

---
+++ migrations/hr_payroll/0.0.0/pre-models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+if util.version_gte("saas~12.5"):
+    from odoo import models
+
+    from odoo.addons.hr_payroll.models import hr_rule_parameter as _ignore  # noqa: F401
+
+    class HRPV(models.Model):
+        _inherit = "hr.rule.parameter.value"
+        _module = "hr_payroll"
+        _match_uniq = True
+
+    class HRP(models.Model):
+        _inherit = "hr.rule.parameter"
+        _module = "hr_payroll"
+        _match_uniq = True
+
+
+def migrate(cr, version):
+    pass
