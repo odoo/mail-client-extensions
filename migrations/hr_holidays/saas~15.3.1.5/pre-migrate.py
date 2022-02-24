d1adeb0d606188a7bb54e4fcb374a037a72e9363
William Braeckman <wbr@odoo.com>
2022-02-24 11:06:14 +0000

    [IMP] hr_holidays/saas~15.3: improved accrual plan level
    Relates to odoo/odoo#85281
    
    TaskId-2768674
    
    closes odoo/upgrade#3279
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr_holidays/saas~15.3.1.5/pre-migrate.py
+++ migrations/hr_holidays/saas~15.3.1.5/pre-migrate.py
@@ -5,3 +5,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.change_field_selection_values(cr, "hr.leave.type", "allocation_validation_type", {"set": "officer"})
+    util.remove_field(cr, "hr.leave.accrual.level", "level")
