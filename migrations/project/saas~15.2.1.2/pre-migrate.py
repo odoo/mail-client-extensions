a41ab99998739a51eabc9801690571e7db51b56f
Laurent Stukkens (LTU) <ltu@odoo.com>
2022-02-28 13:54:22 +0000

    [IMP] project/saas~15.2: pre-compute stored is_closed
    Related PRs:
    - odoo/odoo#85513
    - odoo/enterprise#24828
    
    closes odoo/upgrade#3295
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/project/saas~15.2.1.2/pre-migrate.py
+++ migrations/project/saas~15.2.1.2/pre-migrate.py
@@ -15,3 +15,11 @@ def migrate(cr, version):
     )
     util.remove_record(cr, "project.menu_projects_group_stage")
     util.remove_record(cr, "project.open_view_project_all_group_stage")
+    util.create_column(cr, "project_task", "is_closed", "boolean", default=False)
+    query = """
+        UPDATE project_task t
+           SET is_closed = s.fold
+          FROM project_task_type s
+         WHERE s.id = t.stage_id
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="project_task", prefix="t."))
