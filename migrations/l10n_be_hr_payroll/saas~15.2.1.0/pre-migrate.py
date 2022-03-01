f1f77e2aa97168d82079b8fa77d5f2b8c5439288
Christophe Simonis <chs@odoo.com>
2022-03-01 15:42:43 +0000

    [FIX] l10n_be_hr_payroll/saas~15.2: move data around
    closes odoo/upgrade#3349
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/l10n_be_hr_payroll/saas~15.2.1.0/pre-migrate.py
+++ migrations/l10n_be_hr_payroll/saas~15.2.1.0/pre-migrate.py
@@ -4,6 +4,7 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    eb = util.expand_braces
 
     util.remove_view(cr, "l10n_be_hr_payroll.hr_employee_view_form")
     util.remove_view(cr, "l10n_be_hr_payroll.hr_payroll_report_view_search")
@@ -13,7 +14,29 @@ def migrate(cr, version):
     util.remove_view(cr, "l10n_be_hr_payroll.dmfa_xml_report_insurance_group")
     util.remove_view(cr, "l10n_be_hr_payroll.l10n_be_individual_account_wizard_view_form")
 
-    # Split l10n_be_hr_payroll_canteen into l10n_be_hr_payroll/l10n_be_hr_contract_salary
+    # Move data from the merge of `l10n_be_hr_contract_salary_group_insurance` to `l10n_be_hr_contract_salary`.
+    # As `l10n_be_hr_contract_salary_group_insurance` depended on `l10n_be_hr_contract_salary`, no need to verify if
+    # `l10n_be_hr_contract_salary` is installed. If the xmlid is present, we consider that `l10n_be_hr_contract_salary`
+    # is installed.
+    advantages = """
+        group_insurance
+        group_insurance_value_0
+        group_insurance_value_2
+        group_insurance_value_3
+
+        ambulatory_insurance
+        ambulatory_insurance_value_0
+        ambulatory_insurance_value_1
+
+        ambulatory_insured_spouse
+        ambulatory_insured_children
+        ambulatory_insured_adults
+    """
+    for adv in util.splitlines(advantages):
+        util.rename_xmlid(cr, *eb(f"l10n_be_hr_{{payroll,contract_salary}}.l10n_be_{adv}"))
+
+    # and in the other way around from the merge of `l10n_be_hr_payroll_canteen` into `l10n_be_hr_contract_salary`
+    util.rename_xmlid(cr, *eb("l10n_be_hr_{contract_salary,payroll}.cp200_employees_salary_canteen"))
     util.move_field_to_module(
         cr, "hr.contract", "l10n_be_canteen_cost", "l10n_be_hr_contract_salary", "l10n_be_hr_payroll"
     )
