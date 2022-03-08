ecbc51bb3a9cc9d3ae719253a54343b8825d365a
Nicolas Seinlet <nse@openerp.com>
2022-03-08 11:55:31 +0000

    [IMP] account/saas~11.4: parallel update invoices
    closes odoo/upgrade#3312
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/saas~11.4.1.1/post-models.py
+++ migrations/account/saas~11.4.1.1/post-models.py
@@ -3,11 +3,16 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    #Partner_id was a required field until this version
-    #In every migration, every invoice will have a partner_id
-    cr.execute("""
-        UPDATE account_invoice a
-        SET vendor_display_name = p.name
-        FROM res_partner p
-        WHERE p.id=a.partner_id
-    """)
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            """
+                UPDATE account_invoice a
+                SET vendor_display_name = p.name
+                FROM res_partner p
+                WHERE p.id=a.partner_id
+            """,
+            prefix="a."
+        )
+    )
