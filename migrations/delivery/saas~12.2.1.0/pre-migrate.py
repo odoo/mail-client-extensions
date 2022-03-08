7422d99e7462cb7a9513b052e1b83458a7f869b5
Nicolas Seinlet <nse@openerp.com>
2022-03-08 12:54:55 +0000

    [IMP] delivery/saas~12.2
    Parallelize UPDATE query
    
    closes odoo/upgrade#3316
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/delivery/saas~12.2.1.0/pre-migrate.py
+++ migrations/delivery/saas~12.2.1.0/pre-migrate.py
@@ -7,14 +7,19 @@ def migrate(cr, version):
     util.remove_field(cr, "sale.order", "available_carrier_ids")
 
     util.create_column(cr, "sale_order", "recompute_delivery_price", "boolean")
-    cr.execute(
-        """
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            """
         UPDATE sale_order s
            SET recompute_delivery_price = TRUE
           FROM sale_order_line l
          WHERE s.state IN ('draft', 'sent')
            AND l.order_id = s.id
            AND l.is_delivery = TRUE
-    """
+            """,
+            prefix="s.",
+        ),
     )
     cr.execute("UPDATE sale_order SET recompute_delivery_price = FALSE WHERE recompute_delivery_price IS NULL")
