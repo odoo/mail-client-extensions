b4caea44e31803872850f9a3e40d3367beab5952
jbw <jbw@odoo.com>
2022-03-10 12:15:26 +0000

    [IMP] {base,account_intrastat}/saas~15.5: merge account_intrastat_expiry into account_intrastat
    See: odoo/enterprise#25161
    
    The account_intrastat_expiry module contains fields that represent the
    lifetime of intrastat codes within the database, and mechanisms for
    showing/hiding codes depending on whether they are within their active
    timeframe. These features were written as a module so that they would be
    available to users in stable, but can now be merged in master as they
    have utility in the core intrastat module.
    
    closes odoo/upgrade#3337
    
    Ticket-id: 2732930
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account_intrastat/saas~15.5.1.0/pre-migrate.py
+++ migrations/account_intrastat/saas~15.5.1.0/pre-migrate.py
@@ -5,3 +5,28 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_model(cr, "account.intrastat.report")
     util.remove_view(cr, "account_intrastat.search_template")
+    util.remove_view(cr, "account_intrastat.view_intrastat_code_expiry_search")
+    util.remove_view(cr, "account_intrastat.view_report_intrastat_code_expiry_tree")
+    util.remove_view(cr, "account_intrastat.view_report_intrastat_code_expiry_form")
+    util.remove_view(cr, "account_intrastat.invoice_form_inherit_account_intrastat_expiry")
+    util.remove_view(cr, "account_intrastat.account_intrastat_expiry_product_template_search_view")
+    util.rename_xmlid(
+        cr,
+        "account_intrastat.product_product_tree_view_account_intrastat_expiry",
+        "account_intrastat.product_product_tree_view_account_intrastat",
+    )
+    util.rename_xmlid(
+        cr,
+        "account_intrastat.account_intrastat_expiry_product_category_search_view",
+        "account_intrastat.account_intrastat_product_category_search_view",
+    )
+    util.rename_xmlid(
+        cr,
+        "account_intrastat.product_category_tree_view_account_intrastat_expiry",
+        "account_intrastat.product_category_tree_view_account_intrastat",
+    )
+    util.rename_xmlid(
+        cr,
+        "account_intrastat.account_intrastat_expiry_main_template",
+        "account_intrastat.account_intrastat_main_template",
+    )
--- migrations/base/saas~15.5.1.3/pre-10-modules.py
+++ migrations/base/saas~15.5.1.3/pre-10-modules.py
@@ -42,6 +42,7 @@ def migrate(cr, version):
         util.force_migration_of_fresh_module(cr, "spreadsheet_edition")
         util.remove_module(cr, "project_account_accountant")
         util.remove_module(cr, "sale_project_enterprise")
+        util.merge_module(cr, "account_intrastat_expiry", "account_intrastat")
 
     util.remove_module(cr, "l10n_nl_report_intrastat")
     util.remove_module(cr, "l10n_es_reports_2021")
