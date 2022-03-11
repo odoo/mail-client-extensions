e71834a8016080ec11cf938e2e81a7de9b06e7d2
Vincent Larcin <vila@odoo.com>
2022-03-11 11:59:10 +0000

    [IMP] project_timesheet_forecast/saas~15.3 update percentage_hours
    The field `percentage_hours` in `planning.slot`, which was previously
    unused, is now used for the planning slots progress bar.
    
    However, it was computed on a scale of 0 to 1, whereas the progress bar
    needs a number from 0 to 100.
    
    This commit multiplies all the previous values of this field by 100.
    
    See https://github.com/odoo/enterprise/pull/23342
    
    Task-2664116
    
    closes odoo/upgrade#3344
    
    Related: odoo/enterprise#23342
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/project_timesheet_forecast/saas~15.3.1.0/pre-migrate.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    query = """
+        UPDATE planning_slot s
+           SET percentage_hours = s.percentage_hours * 100
+         WHERE s.percentage_hours != 0
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="planning_slot", prefix="s."))
