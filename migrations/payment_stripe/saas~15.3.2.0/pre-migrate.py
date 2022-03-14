2559d94c9fc76247dead0e0e408f969a221ffe16
Demesmaeker <edm@odoo.com>
2022-03-14 13:58:52 +0000

    [IMP] payment_stripe/saas~15.3.2.0: report the events change
    task-2733725
    see https://github.com/odoo/odoo/pull/84150
    see https://github.com/odoo/documentation/pull/1605
    
    closes odoo/upgrade#3350
    
    Related: odoo/odoo#84150
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/payment_stripe/saas~15.3.2.0/pre-migrate.py
+++ migrations/payment_stripe/saas~15.3.2.0/pre-migrate.py
@@ -1,2 +1,12 @@
+from odoo.upgrade import util
+
+
 def migrate(cr, version):
     cr.execute("UPDATE payment_acquirer SET support_authorization = TRUE WHERE provider = 'stripe'")
+
+    if cr.rowcount:
+        util.add_to_migration_reports(
+            "Odoo is now listening to other events coming from the payment provider Stripe. In order to configure this, please "
+            "follow the following documentation: "
+            "https://www.odoo.com/documentation/saas-15.3/applications/finance/payment_acquirers/stripe.html#webhook-signing-secret"
+        )
