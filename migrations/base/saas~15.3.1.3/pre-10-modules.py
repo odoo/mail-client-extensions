236ad09d03aaaa49a2f7a7ba2b54d7e86e532f5c
Julien Alardot (jual) <jual@odoo.com>
2022-03-14 13:03:30 +0000

    [IMP] base/saas~15.3: merge l10n_sa_invoice module into l10n_sa
    The l10n_sa_invoices changes do not need to be in a separate module as
    they are always required for l10n_sa.
    This merges the two modules into l10n_sa
    
    COM odoo/odoo#85215
    
    closes odoo/upgrade#3303
    
    Task: task-2723443
    Signed-off-by: Julien Alardot (jual) <jual@odoo.com>
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~15.3.1.3/pre-10-modules.py
+++ migrations/base/saas~15.3.1.3/pre-10-modules.py
@@ -4,6 +4,7 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.merge_module(cr, "l10n_sa_invoice", "l10n_sa")
     util.modules_auto_discovery(cr)
 
     if util.has_enterprise():
