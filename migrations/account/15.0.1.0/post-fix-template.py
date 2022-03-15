a9f2d135f055f5feb174d15879ce83c907a027d9
kajal-odoo <kago@odoo.com>
2022-03-15 09:49:32 +0000

    [FIX] account/{saas~12.4,15.0}: Update account mail template
    The field `origin` should be referred now as `invoice_origin`
    This fixes a missing field update on a9bcf01bda9d9609c74862b41c2a6897d5da4832
    
    closes odoo/upgrade#3358
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/15.0.1.0/post-fix-template.py
+++ migrations/account/15.0.1.0/post-fix-template.py
@@ -7,11 +7,13 @@ def migrate(cr, version):
         r"""
         UPDATE mail_template
            SET subject = regexp_replace(subject, '\yobject.number\y', 'object.name', 'g'),
-               body_html = regexp_replace(body_html, '\yobject.number\y', 'object.name', 'g'),
+               body_html = regexp_replace(regexp_replace(body_html, '\yobject.number\y', 'object.name', 'g'),
+                           '\yobject.origin\y', 'object.invoice_origin', 'g'),
                report_name = regexp_replace(report_name, '\yobject.number\y', 'object.name', 'g')
          WHERE id = %s
             AND (  subject like '%%object.number%%'
                 OR body_html like '%%object.number%%'
+                OR body_html like '%%object.origin%%'
                 OR report_name like '%%object.number%%'
                 )
         """,
