d07cc66ba489f6828decf01088d0709e8bae1ae0
kcv-odoo <kcv@odoo.com>
2022-03-15 05:38:59 +0000

    [IMP] project/saas~15.4: add new field in project_task
    This commit add 'ancestor_id' field in project_task and set
    it's value based on parent of task.
    
    task-2725525
    Related PR: https://github.com/odoo/odoo/pull/82305
    
    Part-of: odoo/upgrade#3355

---
+++ migrations/project/saas~15.4.1.2/pre-migrate.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "project_task", "ancestor_id", "int4")
+
+    cr.execute(
+        """
+        WITH RECURSIVE task_tree AS (
+            SELECT id, id as ancestor_id
+              FROM project_task
+             WHERE parent_id IS NULL
+             UNION
+            SELECT t.id, tree.ancestor_id
+              FROM project_task t
+              JOIN task_tree tree ON tree.id = t.parent_id
+        )
+        UPDATE project_task t
+           SET ancestor_id = tree.ancestor_id
+          FROM task_tree tree
+         WHERE tree.id = t.id
+           AND tree.id != tree.ancestor_id
+    """
+    )
