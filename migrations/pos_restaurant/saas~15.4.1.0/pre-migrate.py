c08bd3c8910c273aeec95f7128a8a1db576951a7
Jacky (trj) <trj@odoo.com>
2022-03-25 12:45:40 +0000

    [IMP] pos_restaurant/saas~15.4: update kitchen printer fields
    Adding an uuid to every orderline in order to differentiate them
    during the differences printing.
    
    Removing the `mp_dirty` field, it doesn't need to be stored because
    it can easily be computed in the front end thanks to the `multiprint_resume`.
    
    Resetting the `multiprint_resume` value of every draft order which represents
    the printing state of the order since the structure has changed in 15.3.
    
    See https://github.com/odoo/odoo/pull/85956
    See https://github.com/odoo/enterprise/pull/25051
    
    closes odoo/upgrade#3375
    
    Related: odoo/odoo#85956
    Related: odoo/enterprise#25051
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/pos_restaurant/saas~15.4.1.0/pre-migrate.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "pos_order_line", "uuid", "varchar")
+    util.remove_field(cr, "pos.order.line", "mp_dirty")
+    query = """
+        UPDATE pos_order
+        SET multiprint_resume = NULL
+        WHERE state = 'draft'
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="pos_order"))
