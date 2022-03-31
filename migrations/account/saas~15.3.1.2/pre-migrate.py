5d37d517a070136c5409700880240722584a041d
Camille Spiritus <casp@odoo.com>
2022-03-31 14:06:25 +0000

    [IMP] account/saas~15.3.2.1: display cpny's total amount when grouping
    Relates to odoo/odoo#87535
    
    When looking at the list of the payments and grouping by company,
    the total amount per company is no longer displayed since v15.
    
    This is due to the fact that the new variable used to represent
    this data was no longer stored.
    
    By storing this data, we can regain this ability.
    
    task-2801688
    
    closes odoo/upgrade#3401
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/account/saas~15.3.1.2/pre-migrate.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "account_payment", "amount_company_currency_signed", "numeric")
+    query = """
+        UPDATE account_payment ap
+        SET ap.amount_company_currency_signed =
+            (CASE
+                WHEN ap.payment_type = 'outbound'
+                THEN -1
+                ELSE 1
+            END)
+            * am.amount_total_signed
+        FROM account_move am
+        WHERE ap.move_id = am.id
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="account_payment", alias="ap"))
