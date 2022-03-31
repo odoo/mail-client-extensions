6ab347c6709c1faf38e76e72cbb7583af01c5b19
Kevin Baptiste <kba@odoo.com>
2022-03-31 11:14:32 +0000

    [IMP] hr/saas~15.3: convert plan activities m2m to m2o
    Related odoo/odoo#87082
    Related odoo/enterprise#25511
    
    Taskid: 2796407
    Part-of: odoo/upgrade#3398

--- migrations/hr/saas~15.3.1.1/pre-migrate.py
+++ migrations/hr/saas~15.3.1.1/pre-migrate.py
@@ -14,3 +14,52 @@ def migrate(cr, version):
     )
 
     util.remove_field(cr, "hr.job", "state")
+    util.remove_record(cr, "hr.menu_config_plan_types")
+
+    # Convert m2m to m2o
+    util.create_column(cr, "hr_plan_activity_type", "plan_id", "int4")
+
+    cr.execute(
+        """
+            WITH no_dupes AS (
+                SELECT hr_plan_activity_type_id, min(hr_plan_id) AS hr_plan_id
+                  FROM hr_plan_hr_plan_activity_type_rel
+              GROUP BY hr_plan_activity_type_id
+                HAVING COUNT(hr_plan_activity_type_id) = 1
+            ),
+            _ AS (
+                DELETE FROM hr_plan_hr_plan_activity_type_rel r
+                      USING no_dupes d
+                      WHERE d.hr_plan_id = r.hr_plan_id
+                        AND d.hr_plan_activity_type_id = r.hr_plan_activity_type_id
+            )
+            UPDATE hr_plan_activity_type act
+               SET plan_id = d.hr_plan_id
+              FROM no_dupes d
+             WHERE act.id = d.hr_plan_activity_type_id
+        """
+    )
+
+    pa_cols, pa_pref_cols = map(",".join, util.get_columns(cr, "hr_plan_activity_type", ("id", "plan_id"), ["p"]))
+    cr.execute(
+        f"""
+            WITH plan_activities AS (
+                SELECT hr_plan_activity_type_id, ARRAY_AGG(hr_plan_id ORDER BY hr_plan_id) AS plans
+                  FROM hr_plan_hr_plan_activity_type_rel
+              GROUP BY hr_plan_activity_type_id
+            ),
+            _upd AS (
+                UPDATE hr_plan_activity_type a
+                   SET plan_id = pa.plans[1]
+                  FROM plan_activities pa
+                 WHERE pa.hr_plan_activity_type_id = a.id
+            )
+            INSERT INTO hr_plan_activity_type({pa_cols}, plan_id)
+                 SELECT {pa_pref_cols}, UNNEST(pa.plans[2:])
+                   FROM hr_plan_activity_type p
+                   JOIN plan_activities pa
+                     ON p.id = pa.hr_plan_activity_type_id
+        """
+    )
+
+    cr.execute("DROP TABLE hr_plan_hr_plan_activity_type_rel")
