0b83546e4aeb781c95d9ae82b662eb7368036ac6
Xavier BOL (xbo) <xbo@odoo.com>
2022-04-01 07:32:33 +0000

    [IMP] sale_timesheet/saas~15.3: update timesheet_invoice_type field
    This `timesheet_invoice_type` has 2 new values availables:
    1. `billable_milestones`: correspond to the timesheets linked to SOL
    containing a product with `invoice_policy='delivery'` and
    `service_type='milestones'`.
    2. `billable_manual`: correspond to the timesheets linked to SOL
    containing a product with `invoice_policy='delivery'` and
    `service_type='manual'`.
    
    This commit updates the `timesheet_invoice_type` field accordingly to
    avoid recomputing the compute for that.
    
    task-2710808
    
    closes odoo/upgrade#3388
    
    Related: odoo/enterprise#25710
    Related: odoo/odoo#87480
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/sale_timesheet/saas~15.3.1.0/pre-migrate.py
+++ migrations/sale_timesheet/saas~15.3.1.0/pre-migrate.py
@@ -18,3 +18,30 @@ def migrate(cr, version):
     util.remove_view(cr, "sale_timesheet.project_profitability_report_view_search")
     util.remove_record(cr, "sale_timesheet.ir_filter_project_profitability_report_costs_and_revenues")
     util.remove_model(cr, "project.profitability.report")
+
+    query = """
+        WITH timesheets AS (
+            SELECT aal.id,
+                   pt.service_type
+              FROM account_analytic_line aal
+              JOIN sale_order_line sol ON sol.id = aal.so_line
+              JOIN product_product pro ON pro.id = sol.product_id
+              JOIN product_template pt ON pt.id = pro.product_tmpl_id
+             WHERE aal.timesheet_invoice_type = 'billable_fixed'
+               AND pt.service_type in ('milestones', 'manual')
+               AND {parallel_filter}
+        )
+        UPDATE account_analytic_line
+           SET timesheet_invoice_type = 'billable_' || t.service_type
+          FROM timesheets t
+         WHERE t.id = account_analytic_line.id
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="account_analytic_line", alias="aal"))
+
+    query = """
+        UPDATE account_analytic_line
+           SET timesheet_invoice_type = 'other_costs'
+         WHERE timesheet_invoice_type = 'service_revenues'
+           AND amount < 0
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="account_analytic_line"))
