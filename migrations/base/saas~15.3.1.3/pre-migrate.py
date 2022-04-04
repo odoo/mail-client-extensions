39d58a9e80ed6b3107c7b7faf706b4d33c12d3ef
Christophe Simonis <chs@odoo.com>
2022-04-04 11:32:01 +0000

    [IMP] base/saas~15.3: (re)move field `credit_limit`
    See odoo/odoo#83205
    
    closes odoo/upgrade#3407
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~15.3.1.3/pre-migrate.py
+++ migrations/base/saas~15.3.1.3/pre-migrate.py
@@ -7,3 +7,8 @@ def migrate(cr, version):
     util.remove_field(cr, "base.language.install", "lang")
     util.create_m2m(cr, "res_lang_install_rel", "base_language_install", "res_lang", "language_wizard_id", "lang_id")
     util.remove_field(cr, "base.language.install", "state")
+
+    if util.module_installed(cr, "account"):
+        util.move_field_to_module(cr, "res.partner", "credit_limit", "base", "account")
+    else:
+        util.remove_field(cr, "res.partner", "credit_limit")
