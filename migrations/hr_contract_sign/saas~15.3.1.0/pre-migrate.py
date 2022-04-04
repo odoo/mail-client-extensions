398b2c280252c04c0658b7323d6dc217fcce34fe
Kevin Baptiste <kba@odoo.com>
2022-04-04 08:58:33 +0000

    [IMP] hr_contract_sign/saas~15.3: compute employee_role_id
    Related odoo/odoo#87082
    Related odoo/enterprise#25511
    
    closes odoo/upgrade#3398
    
    Taskid: 2796407
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr_contract_sign/saas~15.3.1.0/pre-migrate.py
+++ migrations/hr_contract_sign/saas~15.3.1.0/pre-migrate.py
@@ -4,3 +4,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.rename_field(cr, "hr.contract.sign.document.wizard", "follower_ids", "cc_partner_ids")
+
+    util.create_column(cr, "hr_plan_activity_type", "employee_role_id", "int4")
