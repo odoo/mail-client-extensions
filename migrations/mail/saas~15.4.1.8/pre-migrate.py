e1ecec94b00168f2793673cdf7effcdbca8bb1e3
Didier (did) <did@odoo.com>
2022-04-08 12:56:22 +0000

    [IMP] mail/saas~15.4.1.8: update channel name
    Use the language of the user that created the channel to determines the name of
    the channel.
    
    see https://github.com/odoo/odoo/pull/88239
    
    closes odoo/upgrade#3423
    
    Related: odoo/odoo#88239
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/mail/saas~15.4.1.8/pre-migrate.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+        UPDATE mail_channel c
+           SET name = t.value
+          FROM ir_translation t,
+               res_users u
+          JOIN res_partner p ON p.id = u.partner_id
+         WHERE t.name = 'mail.channel,name'
+           AND t.res_id = c.id
+           AND u.id = c.create_uid
+           AND t.lang = p.lang
+        """
+    )
+    cr.execute(
+        """
+        DELETE
+          FROM ir_translation
+         WHERE name = 'mail.channel,name'
+        """
+    )
