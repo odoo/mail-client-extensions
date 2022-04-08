9b0798fdb1ae83622d3f536d407c7e879f964eda
Prakash Prajapati <ppr@odoo.com>
2022-04-08 05:49:41 +0000

    [IMP] helpdesk_timesheet/saas~15.5: imply new helpdesk_timesheet group
    closes odoo/upgrade#3426
    
    Related: odoo/enterprise#24609
    Task-id: 2729677
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/helpdesk_timesheet/saas~15.5.1.0/post-migrate.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+        SELECT 1
+          FROM helpdesk_team
+         WHERE use_helpdesk_timesheet = true
+         LIMIT 1
+    """
+    )
+    if cr.rowcount:
+        groups = util.env(cr).ref("helpdesk.group_helpdesk_user") + util.env(cr).ref(
+            "hr_timesheet.group_hr_timesheet_user"
+        )
+        groups.write(
+            {
+                "implied_ids": [(4, util.ref(cr, "helpdesk_timesheet.group_use_helpdesk_timesheet"))],
+            }
+        )
