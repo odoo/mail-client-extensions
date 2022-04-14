3117b287eb3881b3433631f05209bd1d25a3408d
dht-odoo <dht@odoo.com>
2022-04-14 12:54:44 +0000

    [IMP] website_slides/saas~15.5: rename the field share_template_id
    This commit will rename the field share_template_id to
    share_slide_template_id.
    
    Com PR:  https://github.com/odoo/odoo/pull/78139
    
    task:2627364
    
    closes odoo/upgrade#3444
    
    Related: odoo/odoo#78139
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/website_slides/saas~15.5.2.6/pre-migrate.py
+++ migrations/website_slides/saas~15.5.2.6/pre-migrate.py
@@ -5,3 +5,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "website_slides.slide_edit_options")
     util.remove_view(cr, "website_slides.user_navbar_inherit_website_slides")
+
+    util.rename_field(cr, "slide.channel", "share_template_id", "share_slide_template_id")
