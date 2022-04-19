fc80f4012c44e33e54d8c4ea343eb87819b44748
Nicolas (vin) <vin@odoo.com>
2022-04-19 14:22:00 +0000

    [FIX] account/saas~14.5: Zero tax line CABA
    When creating Zero tax lines, there is an issue for cash basis lines
    where two tax line would be created on both the invoice and the cash
    basis move.
    On the invoice though, only one line should be created, with the cash
    basis transition account, while on the cash basis move we should find
    the two lines as expected.
    Another specificity is that we shouldn't find the tax_line_id and
    tax_repartition_line_id on the line on the cash basis move that is
    using the cash basis account. It should only be found on the line
    using the final account (from the tax repartition line or base line)
    
    Also populate some balances with zero instead of leaving
    them empty, as I heard it could cause some issues with specific reports.
    
    closes odoo/upgrade#3450
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/saas~14.5.1.1/post-10-migrate.py
+++ migrations/account/saas~14.5.1.1/post-10-migrate.py
@@ -16,25 +16,38 @@ def migrate(cr, version):
         """
     )
 
-    # Handles regular 0% taxes.
+    # Handles 0% taxes for cash basis moves.
     cr.execute(
         """
         INSERT INTO account_move_line (move_id, move_name, date, parent_state, journal_id, company_id,
                                        company_currency_id, account_id, account_root_id, sequence, name,
                                        partner_id, analytic_account_id, reconciled, blocked,
-                                       currency_id, tax_repartition_line_id, tax_line_id, tax_group_id, exclude_from_invoice_tab,
-                                       tax_base_amount)
+                                       currency_id,
+                                       tax_repartition_line_id,
+                                       tax_line_id,
+                                       tax_group_id,
+                                       exclude_from_invoice_tab,
+                                       tax_base_amount,
+                                       debit, credit, balance, amount_currency)
         SELECT move.id, move.name, base_line.date, base_line.parent_state, base_line.journal_id, base_line.company_id,
                base_line.company_currency_id, account.id, account.root_id, 10, COALESCE(NULLIF(ir_translation.value, ''), tax.name),
                base_line.partner_id, CASE WHEN tax.analytic THEN base_line.analytic_account_id END, account.reconcile, FALSE,
-               base_line.currency_id, atrl.id, tax.id, tax.tax_group_id, TRUE,
-               CASE
-                   WHEN
-                           (tax.type_tax_use = 'sale' AND atrl.invoice_tax_id IS NOT NULL)
-                       OR
-                           (tax.type_tax_use = 'purchase' AND atrl.refund_tax_id IS NOT NULL)
-                   THEN -SUM(base_line.balance)
-                   ELSE SUM(base_line.balance) END
+               base_line.currency_id,
+               CASE WHEN account.id != tax.cash_basis_transition_account_id THEN atrl.id END,
+               CASE WHEN account.id != tax.cash_basis_transition_account_id THEN tax.id END,
+               CASE WHEN account.id != tax.cash_basis_transition_account_id THEN tax.tax_group_id END,
+               TRUE,
+               CASE WHEN account.id != tax.cash_basis_transition_account_id THEN
+                   CASE
+                       WHEN
+                               (tax.type_tax_use = 'sale' AND atrl.invoice_tax_id IS NOT NULL)
+                           OR
+                               (tax.type_tax_use = 'purchase' AND atrl.refund_tax_id IS NOT NULL)
+                       THEN -SUM(base_line.balance)
+                       ELSE SUM(base_line.balance)
+                   END
+               END,
+               0.0, 0.0, 0.0, 0.0
         FROM account_move_line_account_tax_rel rel
         JOIN account_tax tax ON rel.account_tax_id = tax.id AND tax.amount_type != 'group' AND tax.amount = 0.0
         JOIN account_move_line base_line ON base_line.id = rel.account_move_line_id
@@ -46,13 +59,79 @@ def migrate(cr, version):
                 (move.move_type NOT IN ('out_refund', 'in_refund') AND atrl.invoice_tax_id = tax.id)
             )
             AND repartition_type = 'tax'
-        JOIN account_account account ON tax.tax_exigibility = 'on_payment'
-            AND account.id = tax.cash_basis_transition_account_id OR account.id = COALESCE(atrl.account_id, base_line.account_id)
+        JOIN account_account account ON
+            (
+                tax.tax_exigibility = 'on_payment' AND account.id = tax.cash_basis_transition_account_id
+            )
+                OR
+            (
+                account.id = COALESCE(atrl.account_id, base_line.account_id)
+            )
         JOIN res_company ON move.company_id = res_company.id
         JOIN res_partner ON res_company.partner_id = res_partner.id
         LEFT JOIN ir_translation ON ir_translation.name = 'account.tax,name'
             AND ir_translation.res_id = tax.id AND ir_translation.type = 'model' AND ir_translation.lang = res_partner.lang
         WHERE NOT EXISTS (SELECT 1 FROM move_ids_to_exclude WHERE move_ids_to_exclude.move_id = base_line.move_id)
+            AND move.tax_cash_basis_rec_id IS NOT NULL
+        GROUP BY base_line.company_id, base_line.company_currency_id, base_line.currency_id, base_line.partner_id,
+                 base_line.account_id, base_line.date, base_line.parent_state, move.id, base_line.journal_id,
+                 COALESCE(NULLIF(ir_translation.value, ''), tax.name), tax.tax_exigibility, tax.type_tax_use,
+                 CASE WHEN account.id != tax.cash_basis_transition_account_id THEN atrl.id END,
+                 CASE WHEN account.id != tax.cash_basis_transition_account_id THEN tax.id END,
+                 CASE WHEN account.id != tax.cash_basis_transition_account_id THEN tax.tax_group_id END,
+                 atrl.invoice_tax_id, atrl.refund_tax_id,
+                 tax.cash_basis_transition_account_id, account.id, CASE WHEN tax.analytic THEN base_line.analytic_account_id END
+
+    """
+    )
+
+    # Handles regular 0% taxes for account move.
+    cr.execute(
+        """
+        INSERT INTO account_move_line (move_id, move_name, date, parent_state, journal_id, company_id,
+                                       company_currency_id, account_id, account_root_id, sequence, name,
+                                       partner_id, analytic_account_id, reconciled, blocked,
+                                       currency_id, tax_repartition_line_id, tax_line_id, tax_group_id, exclude_from_invoice_tab,
+                                       tax_base_amount,
+                                       debit, credit, balance, amount_currency)
+        SELECT move.id, move.name, base_line.date, base_line.parent_state, base_line.journal_id, base_line.company_id,
+               base_line.company_currency_id, account.id, account.root_id, 10, COALESCE(NULLIF(ir_translation.value, ''), tax.name),
+               base_line.partner_id, CASE WHEN tax.analytic THEN base_line.analytic_account_id END, account.reconcile, FALSE,
+               base_line.currency_id, atrl.id, tax.id, tax.tax_group_id, TRUE,
+               CASE
+                   WHEN
+                           (tax.type_tax_use = 'sale' AND atrl.invoice_tax_id IS NOT NULL)
+                       OR
+                           (tax.type_tax_use = 'purchase' AND atrl.refund_tax_id IS NOT NULL)
+                   THEN -SUM(base_line.balance)
+                   ELSE SUM(base_line.balance)
+               END,
+               0.0, 0.0, 0.0, 0.0
+        FROM account_move_line_account_tax_rel rel
+        JOIN account_tax tax ON rel.account_tax_id = tax.id AND tax.amount_type != 'group' AND tax.amount = 0.0
+        JOIN account_move_line base_line ON base_line.id = rel.account_move_line_id
+        JOIN account_move move ON base_line.move_id = move.id
+        JOIN account_tax_repartition_line atrl ON
+            (
+                (move.move_type IN ('out_refund', 'in_refund') AND atrl.refund_tax_id = tax.id)
+            OR
+                (move.move_type NOT IN ('out_refund', 'in_refund') AND atrl.invoice_tax_id = tax.id)
+            )
+            AND repartition_type = 'tax'
+        JOIN account_account account ON
+            (
+                tax.tax_exigibility = 'on_payment' AND account.id = tax.cash_basis_transition_account_id
+            )
+                OR
+            (
+                tax.tax_exigibility = 'on_invoice' AND account.id = COALESCE(atrl.account_id, base_line.account_id)
+            )
+        JOIN res_company ON move.company_id = res_company.id
+        JOIN res_partner ON res_company.partner_id = res_partner.id
+        LEFT JOIN ir_translation ON ir_translation.name = 'account.tax,name'
+            AND ir_translation.res_id = tax.id AND ir_translation.type = 'model' AND ir_translation.lang = res_partner.lang
+        WHERE NOT EXISTS (SELECT 1 FROM move_ids_to_exclude WHERE move_ids_to_exclude.move_id = base_line.move_id)
+            AND move.tax_cash_basis_rec_id IS NULL
         GROUP BY base_line.company_id, base_line.company_currency_id, base_line.currency_id, base_line.partner_id,
                  base_line.account_id, base_line.date, base_line.parent_state, move.id, base_line.journal_id,
                  COALESCE(NULLIF(ir_translation.value, ''), tax.name), tax.tax_exigibility, tax.type_tax_use, tax.id,
@@ -70,7 +149,7 @@ def migrate(cr, version):
                                        partner_id, analytic_account_id, reconciled, blocked,
                                        currency_id, tax_repartition_line_id, tax_line_id, tax_group_id, exclude_from_invoice_tab,
                                        tax_base_amount,
-                                       group_tax_id)
+                                       group_tax_id, debit, credit, balance, amount_currency)
         SELECT move.id, move.name, base_line.date, base_line.parent_state, base_line.journal_id, base_line.company_id,
                base_line.company_currency_id, account.id, account.root_id, 10, COALESCE(NULLIF(ir_translation.value, ''), tax.name),
                base_line.partner_id, CASE WHEN tax.analytic THEN base_line.analytic_account_id END, account.reconcile, FALSE,
@@ -83,8 +162,9 @@ def migrate(cr, version):
                            (tax.type_tax_use = 'purchase' OR (tax.type_tax_use = 'none' AND parent_tax.type_tax_use = 'purchase'))
                            AND atrl.refund_tax_id IS NOT NULL
                    THEN -SUM(base_line.balance)
-                   ELSE SUM(base_line.balance) END,
-               parent_tax.id
+                   ELSE SUM(base_line.balance)
+               END,
+               parent_tax.id, 0.0, 0.0, 0.0, 0.0
         FROM account_move_line_account_tax_rel rel
         JOIN account_tax_filiation_rel ON account_tax_filiation_rel.parent_tax = rel.account_tax_id
         JOIN account_tax parent_tax ON parent_tax.id = account_tax_filiation_rel.parent_tax AND parent_tax.amount_type = 'group'
@@ -98,13 +178,20 @@ def migrate(cr, version):
                 (move.move_type NOT IN ('out_refund', 'in_refund') AND atrl.invoice_tax_id = tax.id)
             )
             AND repartition_type = 'tax'
-        JOIN account_account account ON tax.tax_exigibility = 'on_payment'
-            AND account.id = tax.cash_basis_transition_account_id OR account.id = COALESCE(atrl.account_id, base_line.account_id)
+        JOIN account_account account ON
+            (
+                tax.tax_exigibility = 'on_payment' AND account.id = tax.cash_basis_transition_account_id
+            )
+                OR
+            (
+                tax.tax_exigibility = 'on_invoice' AND account.id = COALESCE(atrl.account_id, base_line.account_id)
+            )
         JOIN res_company ON move.company_id = res_company.id
         JOIN res_partner ON res_company.partner_id = res_partner.id
         LEFT JOIN ir_translation ON ir_translation.name = 'account.tax,name'
             AND ir_translation.res_id = tax.id AND ir_translation.type = 'model' AND ir_translation.lang = res_partner.lang
         WHERE NOT EXISTS (SELECT 1 FROM move_ids_to_exclude WHERE move_ids_to_exclude.move_id = base_line.move_id)
+            AND move.tax_cash_basis_rec_id IS NULL
         GROUP BY base_line.company_id, base_line.company_currency_id, base_line.currency_id, base_line.partner_id, move.id,
                  base_line.account_id, base_line.date, base_line.parent_state, base_line.journal_id,
                  COALESCE(NULLIF(ir_translation.value, ''), tax.name), tax.tax_exigibility, tax.type_tax_use, tax.id,
--- migrations/account/tests/test_14_5_aml_zero_tax.py
+++ migrations/account/tests/test_14_5_aml_zero_tax.py
@@ -127,6 +127,43 @@ class TestAmlZeroTax(UpgradeCase):
         )
         return {"invoice": invoice.id, "zero_percent_tax": self.zero_percent_tax_sale.id}
 
+    def _prepare_test_4_CABA(self):
+        move_type_field = "move_type" if util.version_gte("saas~13.3") else "type"
+        invoice = self.env["account.move"].create(
+            {
+                move_type_field: "out_invoice",
+                "partner_id": self.partner.id,
+                "invoice_date": "2016-01-01",
+                "date": "2016-01-01",
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": "line",
+                            "quantity": 1.0,
+                            "price_unit": 100.0,
+                            "account_id": self.account_income.id,
+                            "tax_ids": [(6, 0, self.zero_percent_tax_caba.ids)],
+                        },
+                    )
+                ],
+            }
+        )
+        invoice.action_post()
+        # To create the CABA entry, we have to register a payment
+        self.env["account.payment.register"].with_context(active_ids=invoice.ids, active_model="account.move").create(
+            {
+                "payment_date": invoice.date,
+            }
+        )._create_payments()
+        return {
+            "invoice": invoice.id,
+            "zero_percent_tax_caba": self.zero_percent_tax_caba.id,
+            "income_account_id": self.account_income.id,
+            "caba_account_id": self.account_caba.id,
+        }
+
     def _check_test_1_simple_line(self, data):
         invoice = self.env["account.move"].browse(data["invoice"])
         tax_lines = invoice.line_ids.filtered("tax_line_id")
@@ -190,6 +227,59 @@ class TestAmlZeroTax(UpgradeCase):
             ],
         )
 
+    def _check_test_4_caba(self, data):
+        invoice = self.env["account.move"].browse(data["invoice"])
+        if util.version_gte("saas~14.5"):
+            caba_origin_field = "tax_cash_basis_origin_move_id"
+        else:
+            caba_origin_field = "tax_cash_basis_move_id"
+        caba_move = self.env["account.move"].search([(caba_origin_field, "=", invoice.id)])
+        tax_lines = invoice.line_ids.filtered("tax_line_id")
+        self.assertRecordValues(
+            tax_lines,
+            [
+                {
+                    "tax_line_id": data["zero_percent_tax_caba"],
+                    "tax_base_amount": 100,
+                    "group_tax_id": False,
+                    "balance": 0,
+                    "account_id": data["caba_account_id"],
+                }
+            ],
+        )
+        self.assertRecordValues(
+            caba_move.line_ids,
+            [
+                # Regular lines
+                {
+                    "tax_line_id": False,
+                    "credit": 0,
+                    "debit": 100,
+                    "account_id": data["income_account_id"],
+                },
+                {
+                    "tax_line_id": False,
+                    "credit": 100,
+                    "debit": 0,
+                    "account_id": data["income_account_id"],
+                },
+                # CABA line to final account
+                {
+                    "tax_line_id": data["zero_percent_tax_caba"],
+                    "credit": 0,
+                    "debit": 0,
+                    "account_id": data["income_account_id"],
+                },
+                # CABA line from caba account
+                {
+                    "tax_line_id": False,
+                    "credit": 0,
+                    "debit": 0,
+                    "account_id": data["caba_account_id"],
+                },
+            ],
+        )
+
     # -------------------------------------------------------------------------
     # SETUP
     # -------------------------------------------------------------------------
@@ -200,6 +290,7 @@ class TestAmlZeroTax(UpgradeCase):
             {
                 "name": f"company for {test_name}",
                 "user_ids": [(4, self.env.ref("base.user_admin").id)],
+                "tax_exigibility": True,
             }
         )
 
@@ -240,6 +331,11 @@ class TestAmlZeroTax(UpgradeCase):
             [("company_id", "=", self.company.id), ("user_type_id.type", "=", "receivable")],
             limit=1,
         )
+        self.account_caba = self.env["account.account"].search(
+            [("company_id", "=", self.company.id), ("user_type_id", "=", revenue)],
+            order="id desc",
+            limit=1,
+        )
 
         # Setup partner.
         self.partner = self.env["res.partner"].create(
@@ -249,6 +345,19 @@ class TestAmlZeroTax(UpgradeCase):
                 "company_id": self.company.id,
             }
         )
+
+        self.zero_percent_tax_caba = self.env["account.tax"].create(
+            {
+                "name": "zero_percent_tax_caba",
+                "amount_type": "percent",
+                "amount": 0.0,
+                "type_tax_use": "sale",
+                "tax_exigibility": "on_payment",
+                "cash_basis_transition_account_id": self.account_caba.id,
+            }
+        )
+        # Just need to have two different ones
+        self.zero_percent_tax_caba.invoice_repartition_line_ids[1].account_id = self.account_income.id
 
         self.zero_percent_tax = self.env["account.tax"].create(
             {
@@ -293,6 +402,7 @@ class TestAmlZeroTax(UpgradeCase):
                 self._prepare_test_1_simple_line(),
                 self._prepare_test_2_group_of_taxes(),
                 self._prepare_test_3_existing_line(),
+                self._prepare_test_4_CABA(),
             ],
         }
 
@@ -300,3 +410,4 @@ class TestAmlZeroTax(UpgradeCase):
         self._check_test_1_simple_line(init["14.5-test-zero-tax-line"][0])
         self._check_test_2_group_of_taxes(init["14.5-test-zero-tax-line"][1])
         self._check_test_3_existing_line(init["14.5-test-zero-tax-line"][2])
+        self._check_test_4_caba(init["14.5-test-zero-tax-line"][3])
