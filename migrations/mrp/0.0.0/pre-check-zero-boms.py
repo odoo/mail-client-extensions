585740fc25718cad745662ec3cdf3dfc2c7ba0eb
Alvaro Fuentes <afu@odoo.com>
2022-04-29 11:17:46 +0000

    [FIX] mrp/0.0.0: check for zero qty BoMs
    From 13.0 up we do not allow zero quantity BoMs. In 13.0 we rely in code
    checks to enforce the restriction. Since 14.0[^1] we rely on an SQL
    constraint. If we start with a DB in 12.0 we may get to 14.0 without the
    constraint in place due to:
    ```
     odoo.schema: Table 'mrp_bom': unable to add constraint 'mrp_bom_qty_positive' as check (product_qty > 0)
    ```
    Thus allowing for zero quantity in BoMs. This in turn may fail with
    ZeroDivisionError[^2] on mrp production operations.
    
    upg-281524
    
    [^1]: https://github.com/odoo/odoo/blob/5df214b1a8160ccdfa73b926f1257d5a41de537c/addons/mrp/models/mrp_bom.py#L80
    [^2]: https://github.com/odoo/odoo/blob/f5553c550fc49dac92b4ae69a096c43a9e43a0d1/addons/mrp/models/mrp_production.py#L839
    
    closes odoo/upgrade#3484
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/mrp/0.0.0/pre-check-zero-boms.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import os
+
+from odoo.tools.misc import str2bool
+
+from odoo.addons.base.maintenance.migrations import util
+
+# BoMs with zero product_qty are not allowed from 13.0 up.
+# If this variable is set, we fix the BoMs automatically by changing the quantity
+# from 0 to 1
+UPDATE_BOMS = str2bool(os.environ.get("ODOO_MIG_UPDATE_ZERO_QUANTITY_BOMS", "0"))
+
+
+def migrate(cr, version):
+    if not util.version_gte("13.0"):
+        return
+
+    cr.execute(
+        """
+        SELECT b.id,
+               pt.name
+          FROM mrp_bom b
+          JOIN product_template pt
+            ON b.product_tmpl_id = pt.id
+         WHERE b.product_qty <= 0
+        """
+    )
+    info = cr.fetchall()
+    if not info:
+        return
+    if UPDATE_BOMS:
+        cr.execute("UPDATE mrp_bom SET product_qty=1 WHERE id IN %s", [tuple(r[0] for r in info)])
+        util.add_to_migration_reports(
+            message="""
+            <details>
+            <summary>
+                While upgrading your database we have detected some Bill of Materials that have zero
+                Quantity of produced products. To allow the upgrade, we have set the quantity to 1.
+            </summary>
+            Note: this action was activated with the environment variable ODOO_MIG_UPDATE_ZERO_QUANTITY_BOMS
+            List of updated BoMs:
+            <ol>{}</ol>
+            </details>
+            """.format(
+                "\n".join("<li>{}</li>".format(util.get_anchor_link_to_record("mrp.bom", r[0], r[1])) for r in info)
+            ),
+            category="MRP",
+            format="html",
+        )
+        return
+    msg = """
+    Bills of Materials (BoM) with zero Quantity are not allowed. To be able to continue the upgrade, you can:
+    - fix these inconsistencies manually (below the details of the affected BoMs)
+    OR
+    - let this script automatically set the quantity to 1 on the affected BoMs by setting the environment variable
+      ODOO_MIG_UPDATE_ZERO_QUANTITY_BOMS to 1
+
+    The BoMs with zero produced quantity are:
+    {}
+    """.format(
+        "\n".join("    * {} (product: {})".format(*r) for r in info)
+    )
+    raise util.MigrationError(msg)
