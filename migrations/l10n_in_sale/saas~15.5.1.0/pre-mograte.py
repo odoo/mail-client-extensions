3b13ff69e343a006d979b767cc1ae25bfe2471b3
Jigar Vaghela <jva@odoo.com>
2022-05-03 12:47:07 +0000

    [IMP] l10n_in_sale/15.5.1.0: remove l10n_in_shipping_gstin
    Always companies only get a tax credit on the bill to GST number(vat),
    not on the shipping GST number.
    
    So we remove this field but not the column,
    because if a customer still needs data then he can transfer it at his end.
    
    see https://github.com/odoo/odoo/pull/81977
    task - 2723111
    
    closes odoo/upgrade#3493
    
    Related: odoo/odoo#81977
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/l10n_in_sale/saas~15.5.1.0/pre-mograte.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute("SELECT 1 FROM res_partner WHERE l10n_in_shipping_gstin IS NOT NULL LIMIT 1")
+    drop_column = not bool(cr.rowcount)
+    util.remove_field(cr, "res.partner", "l10n_in_shipping_gstin", drop_column=drop_column)
+    util.remove_view(cr, "l10n_in_sale.l10n_in_view_partner_form")
