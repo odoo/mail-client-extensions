9baa8882d7324acac0b1270cda8273b2329ab8cf
Adesh Jolhe <adjo@odoo.com>
2022-05-03 11:22:04 +0000

    [IMP] industry_fsm_report/saas~15.5: remove view from fsm report
    This commit remove view from project_project in industry fsm report module
    
    Related Enterprise PR: https://github.com/odoo/enterprise/pull/26977
    Related Community PR: https://github.com/odoo/odoo/pull/90588
    
    task-2821488
    
    closes odoo/upgrade#3492
    
    Related: odoo/enterprise#26977
    Related: odoo/odoo#90588
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/industry_fsm_report/saas~15.5.1.0/pre-migrate.py
+++ migrations/industry_fsm_report/saas~15.5.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "industry_fsm_report.portal_my_worksheet")
+    util.remove_view(cr, "industry_fsm_report.project_project_view_form_simplified_inherit")
