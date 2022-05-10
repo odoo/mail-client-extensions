116871c6b1193ca18db7335594c6b739e7ddf5d8
kbs-odoo <kbs@odoo.com>
2022-05-10 13:06:38 +0000

    [FIX] account/saas~15.2.1.1: Move group from sale to account
    The field 'group_sale_delivery_address' has been moved to 'account'
    module but the group belonging to that field wasn't moved to
    'account'. The fix will avoid loss of settings for
    provision of customer address.
    
    closes odoo/upgrade#3510
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>
    Co-authored-by: Jinal Patel <jip@odoo.com>

--- migrations/account/saas~15.2.1.1/pre-migrate.py
+++ migrations/account/saas~15.2.1.1/pre-migrate.py
@@ -3,8 +3,10 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    eb = util.expand_braces
     util.move_field_to_module(cr, "account.move", "partner_shipping_id", "sale", "account")
     util.move_field_to_module(cr, "res.config.settings", "group_delivery_invoice_address", "sale", "account")
     util.create_column(cr, "account_move", "is_storno", "bool", default=False)
     util.create_column(cr, "account_chart_template", "use_storno_accounting", "bool", default=False)
     util.create_column(cr, "res_company", "account_storno", "bool", default=False)
+    util.rename_xmlid(cr, *eb("{sale,account}.group_delivery_invoice_address"))
