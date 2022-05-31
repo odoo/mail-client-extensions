6e6fda502560dcb585e7ceea31a2ab30eb0f1c18
MAHAMADASIF ANSARI <maan@odoo.com>
2022-05-31 08:42:04 +0000

    [IMP] calendar/saas~15.5: update mail_template data records
    With a linked community PR, we update some of the email template data
    records linked with calendar event(meeting).
    
    Those records are non-updatalble, so we forcefully update them.
    
    see related community PR https://github.com/odoo/odoo/pull/92628
    
    task-2821957
    
    closes odoo/upgrade#3558
    
    Related: odoo/odoo#92628
    Related: odoo/enterprise#27136
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/calendar/saas~15.5.1.1/post-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    for template_id_suffix in ["invitation", "changedate", "reminder", "update"]:
+        util.if_unchanged(cr, f"calendar.calendar_template_meeting_{template_id_suffix}", util.update_record_from_xml)
