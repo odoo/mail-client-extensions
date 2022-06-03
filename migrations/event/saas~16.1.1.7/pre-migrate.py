fa66232ce3cf2f3f7a33db930c5e670372b82573
Pratik Raval <prra@odoo.com>
2022-06-03 12:52:37 +0000

    [IMP] event/saas~16.1: Delete event_type data records
    We have deleted data records in the `event_type` in the community file
    `event_data`. here it is `noupdate=True`, so we need this upgrade script to
    delete the unused data records at upgrade time.
    
    see odoo/odoo#99421
    
    taskID-2854123
    
    closes odoo/upgrade#3849
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

---
+++ migrations/event/saas~16.1.1.7/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.delete_unused(cr, "event.event_type_data_ticket")
+    util.delete_unused(cr, "event.event_type_data_conference")
