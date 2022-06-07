f830440998b20a9149820347d60804b16db5a131
Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>
2022-06-07 15:03:07 +0000

    [IMP] base/saas~15.5.1.3: move `company_registry` to partner
    This commit moves the `company_registry` from res_company to res_partner
    
    Community PR: https://github.com/odoo/odoo/pull/91938
    Enterprise PR: https://github.com/odoo/enterprise/pull/27605
    Upgrade PR: https://github.com/odoo/upgrade/pull/3570
    
    closes odoo/upgrade#3570
    
    Related: odoo/enterprise#27605
    Related: odoo/odoo#91938
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~15.5.1.3/pre-20-models.py
+++ migrations/base/saas~15.5.1.3/pre-20-models.py
@@ -8,3 +8,14 @@ def migrate(cr, version):
     util.remove_field(cr, "ir.module.module.dependency", "create_date")
     util.remove_field(cr, "ir.module.module.dependency", "write_uid")
     util.remove_field(cr, "ir.module.module.dependency", "create_uid")
+
+    # moving the field company_registry from res_company to res_partner
+    util.create_column(cr, "res_partner", "company_registry", "varchar")
+    query = """
+        UPDATE res_partner p
+           SET company_registry = res_company.company_registry
+          FROM res_company
+         WHERE res_company.partner_id = p.id
+    """
+    cr.execute(query)
+    util.remove_column(cr, "res_company", "company_registry")
