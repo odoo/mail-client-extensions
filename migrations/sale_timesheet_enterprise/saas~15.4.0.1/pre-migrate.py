55cd85168fe4c7aed12aa4e15a62c3ddca8253c4
Christophe Monniez <moc@odoo.com>
2022-06-08 05:44:26 +0000

    [FIX] sale_timesheet_enterprise/saas~15.4: fix version
    As the sale_timesheet_enterprise is in version 0.1, it seems that there
    was a mistake in the directory naming.
    
    Also, as an action is removed with `remove_view`, the script crashes.
    So the whole function is commented and has to be fixed later in order to
    allow bumping Odoo master version.
    
    closes odoo/upgrade#3571
    
    Related: odoo/odoo#92997
    Signed-off-by: Christophe Monniez (moc) <moc@odoo.com>

---
+++ migrations/sale_timesheet_enterprise/saas~15.4.0.1/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+# TODO FIXME either give the correct view or use remove_record
+def migrate(cr, version):
+#    util.remove_view(cr, "sale_timesheet_enterprise.timesheet_action_view_report_by_billing_rate_grid")
+    ...
