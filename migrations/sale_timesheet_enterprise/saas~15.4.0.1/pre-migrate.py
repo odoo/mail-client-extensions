0530623667b43563121496eecacab43f608ea23e
Laurent Stukkens (LTU) <ltu@odoo.com>
2022-06-08 07:58:43 +0000

    [FIX] sale_timesheet_enterprise/*: fix version and remove_view
    Previous to this commit:
    - Migration script of version `12.*` where placed
      in a wrong folder name as the revision number of the module is `0.1`
      and not `1.0` as it was previously used.
    - `sale_timesheet_enterprise.timesheet_action_view_report_by_billing_rate_grid`
      was treated as an `ir.view` altough it is an `ir.actions.act_window.view`.
    
    This commit fixes those issues.
    
    closes odoo/upgrade#3573
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/sale_timesheet_enterprise/saas~15.4.0.1/pre-migrate.py
+++ migrations/sale_timesheet_enterprise/saas~15.4.0.1/pre-migrate.py
@@ -2,7 +2,6 @@
 
 from odoo.upgrade import util
 
-# TODO FIXME either give the correct view or use remove_record
+
 def migrate(cr, version):
-#    util.remove_view(cr, "sale_timesheet_enterprise.timesheet_action_view_report_by_billing_rate_grid")
-    ...
+    util.remove_record(cr, "sale_timesheet_enterprise.timesheet_action_view_report_by_billing_rate_grid")
