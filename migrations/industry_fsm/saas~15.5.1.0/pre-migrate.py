d0d1fc3426c8f04a85c238e381aea6c7739a65dc
Mahendra Barad <mba@odoo.com>
2022-06-09 08:34:29 +0000

    [IMP] industry_fsm/saas~15.5: remove unused fields
    Purpose of the commit is to remove the has_complete_partner_address
    field.
    
    Related Community PR: https://github.com/odoo/odoo/pull/92048
    Related Enterprise PR: https://github.com/odoo/enterprise/pull/27655
    
    task-2792027
    
    closes odoo/upgrade#3579
    
    Related: odoo/enterprise#27655
    Related: odoo/odoo#92048
    Signed-off-by: Maksoud Jinane (maji) <maji@odoo.com>

--- migrations/industry_fsm/saas~15.5.1.0/pre-migrate.py
+++ migrations/industry_fsm/saas~15.5.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "industry_fsm.portal_my_worksheet")
+    util.remove_field(cr, "project.task", "has_complete_partner_address")
