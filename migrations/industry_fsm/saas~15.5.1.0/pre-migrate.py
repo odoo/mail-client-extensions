7ef28c5b7e4280e1f5607a4eded294d61c561c20
Adesh Jolhe <adjo@odoo.com>
2022-06-15 08:44:33 +0000

    [IMP] industry_fsm/saas~15.5: remove view from fsm_views
    This commit remove view from project_task in industry_fsm module
    
    Related Enterprise PR: https://github.com/odoo/enterprise/pull/28419
    Related Community PR: https://github.com/odoo/odoo/pull/93676
    
    task-2821488
    
    closes odoo/upgrade#3593
    
    Related: odoo/enterprise#28419
    Related: odoo/odoo#93676
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/industry_fsm/saas~15.5.1.0/pre-migrate.py
+++ migrations/industry_fsm/saas~15.5.1.0/pre-migrate.py
@@ -5,3 +5,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "industry_fsm.portal_my_worksheet")
     util.remove_field(cr, "project.task", "has_complete_partner_address")
+    util.remove_view(cr, "industry_fsm.project_task_view_graph_fsm")
