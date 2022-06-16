f826e223ede0ac7c19f31be825fde9019f07a67a
sofiagvaladze <sgv@odoo.com>
2022-06-16 11:44:30 +0000

    [IMP] hr_expense/saas~15.5: remove unused data
    Odoo - odoo/odoo#93802
    Enterprise - odoo/enterprise#28474
    
    task - 2831036
    
    closes odoo/upgrade#3778
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/hr_expense/saas~15.5.2.0/pre-migrate.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "hr_expense.view_hr_expense_sheet_dashboard_tree")
+
+    if util.delete_unused(cr, "hr_expense.product_product_fixed_cost"):
+        util.delete_unused(cr, "hr_expense.product_product_fixed_cost_product_template")
+
+    if util.delete_unused(cr, "hr_expense.product_product_zero_cost"):
+        util.delete_unused(cr, "hr_expense.product_product_zero_cost_product_template")
+
+    util.remove_record(cr, "hr_expense.hr_expense_actions_my_unsubmitted")
+    util.remove_record(cr, "hr_expense.action_hr_expense_sheet_all_to_submit")
+    util.remove_record(cr, "hr_expense.action_hr_expense_sheet_all_to_approve")
+    util.remove_record(cr, "hr_expense.action_hr_expense_sheet_all_to_post")
+    util.remove_record(cr, "hr_expense.action_hr_expense_sheet_all_to_pay")
+
+    util.remove_menus(
+        cr,
+        [
+            util.ref(cr, "hr_expense.menu_hr_expense_my_expenses_to_submit"),
+            util.ref(cr, "hr_expense.menu_hr_expense_sheet_all_to_submit"),
+            util.ref(cr, "hr_expense.menu_hr_expense_sheet_all_to_approve"),
+            util.ref(cr, "hr_expense.menu_hr_expense_sheet_all_to_post"),
+            util.ref(cr, "hr_expense.menu_hr_expense_sheet_all_to_pay"),
+            util.ref(cr, "hr_expense.menu_hr_expense_sheet_all"),
+        ],
+    )
