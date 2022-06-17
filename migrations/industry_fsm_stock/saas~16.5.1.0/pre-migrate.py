024fe08ef849e553244690d1d0fc545fb022b5b7
Laurent Stukkens (LTU) <ltu@odoo.com>
2022-06-17 13:30:20 +0000

    [IMP] industry_fsm_stock/saas~16.1: rename wizard_tracking_line_valided
    As the field `wizard_tracking_line_valided` was wrongly spelled
    and corrected in the enterprise related PR, this commit ensures
    that the field gets correctly renamed prior to the migration.
    
    task-2714003
    
    Related enterprise PR: odoo/enterprise#23000
    
    closes odoo/upgrade#3607
    
    Related: odoo/odoo#114757
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/industry_fsm_stock/saas~16.5.1.0/pre-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.rename_field(cr, "fsm.stock.tracking.line", "wizard_tracking_line_valided", "wizard_tracking_line_validated")
+    util.remove_field(cr, "fsm.stock.tracking", "fsm_done")
