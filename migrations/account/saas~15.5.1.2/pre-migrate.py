ff8425635ed0e9525d3799670f02f934a81c77b1
Pierrot (prro) <prro@odoo.com>
2022-06-21 09:00:21 +0000

    [IMP] account/saas~15.5.1.2: remove view with seachpanel
    Removed `account.view_account_move_line_filter_with_root_selection`
    view.
    See https://github.com/odoo/odoo/pull/94076
    
    closes odoo/upgrade#3612
    
    Related: odoo/odoo#94076
    Related: odoo/enterprise#28644
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/saas~15.5.1.2/pre-migrate.py
+++ migrations/account/saas~15.5.1.2/pre-migrate.py
@@ -4,3 +4,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "account.view_move_line_tree_grouped")
+    util.remove_view(cr, "account.view_account_move_line_filter_with_root_selection")
+    util.remove_record(cr, "account.action_account_moves_ledger_general")
