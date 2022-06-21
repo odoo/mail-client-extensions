6b8266e8337c9683b0da75b2b43e355cd82c9293
Victor Feyens <vfe@odoo.com>
2022-06-21 13:07:20 +0000

    [ADD] sale_temporal/saas~15.5: move pricing_id field
    See odoo/enterprise#28899.
    
    closes odoo/upgrade#3632
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/sale_temporal/saas~15.5.1.0/pre-migrate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    if util.module_installed(cr, "sale_subscription"):
+        util.move_field_to_module(cr, "sale.order.line", "pricing_id", "sale_temporal", "sale_subscription")
+    else:
+        util.remove_field(cr, "sale.order.line", "pricing_id")
