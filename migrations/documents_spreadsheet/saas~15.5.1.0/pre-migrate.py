912cf95ff2df4f02e395a43cf260c5c235b2b034
Lucas Lefèvre <lul@odoo.com>
2022-06-24 14:57:55 +0000

    [IMP] documents_spreadsheet/saas~15.5: decouple revision from documents
    Spreadsheet revisions soon will be linked to other records than
    `documents.document`.
    The foreign key is transformed to generic fields res_model/res_id
    
    See odoo/enterprise#28859
    
    closes odoo/upgrade#3629
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/documents_spreadsheet/saas~15.5.1.0/pre-migrate.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "spreadsheet_revision", "res_model", "varchar")
+    util.create_column(cr, "spreadsheet_revision", "res_id", "int4")
+    query = """
+        UPDATE spreadsheet_revision r
+           SET res_model = 'documents.document',
+               res_id = r.document_id
+        """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="spreadsheet_revision", alias="r"))
+    util.remove_constraint(cr, "spreadsheet_revision", "parent_revision_unique")
+    util.remove_field(cr, "spreadsheet.revision", "document_id")
---
+++ migrations/documents_spreadsheet/tests/test_revision_model.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from odoo.addons.base.maintenance.migrations.testing import UpgradeCase, change_version
+
+
+@change_version("15.5")
+class TestSpreadsheetRevisionModel(UpgradeCase):
+    def prepare(self):
+        document = self.env["documents.document"].create()
+        revision = self.env["spreadsheet.revision"].create(
+            [
+                {
+                    "document_id": document.id,
+                    "commands": "",
+                    "parent_revision_id": "A",
+                    "revision_id": "B",
+                },
+            ]
+        )
+        return (revision.id, document.id)
+
+    def check(self, init):
+        revision_id, document_id = init
+        revision = self.env["spreadsheet.revision"].browse(revision_id)
+        self.assertEqual(revision.res_model, "documents.document")
+        self.assertEqual(revision.res_id, document_id)
