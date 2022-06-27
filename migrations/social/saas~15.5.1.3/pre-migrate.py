b3c8fa24e3ad4831b97f6c5b14c3c891e0d033e2
Pratik Raval <prra@odoo.com>
2022-06-27 10:29:36 +0000

    [IMP] social_twitter/saas~15.5: remove twitter_screen_name field
    In the commit, we removed the twitter_screen_name field in social.account model
    as it will now contain the same information as the `social_account_handle` field
    (introduced with linked PR).
    
    This script moves the historical data to the `social` module renaming it to
    `social_account_handle`.
    
    Note that, accounts other than Twitter will not be updated while Upgrading the
    DB and will remain void til the user reconnects/update their account.
    
    see odoo/enterprise#28067
    
    taskID-2857845
    
    closes odoo/upgrade#3631
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/social/saas~15.5.1.3/pre-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.move_field_to_module(cr, "social.account", "twitter_screen_name", "social_twitter", "social")
+    util.rename_field(cr, "social.account", "twitter_screen_name", "social_account_handle")
