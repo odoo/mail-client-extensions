347bc45700b7edf6055068ddf23b41553512ddd0
Stanislas Gueniffey <stgu@odoo.com>
2022-06-28 17:33:04 +0000

    [IMP] base/saas~15.5: bank account management
    Introduce the field allow_out_payment with a default of true
    for pre-existing accounts.
    Remove a view for res.partner.bank that was barely useful
    
    closes odoo/upgrade#3637
    
    See: odoo/odoo#94801
    Task: 2856231
    Related: odoo/enterprise#29916
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/base/saas~15.5.1.3/pre-allow-out-payment.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # The default for allow_out_payment is False except for pre-existing bank accounts
+    util.create_column(cr, "res_partner_bank", "allow_out_payment", "bool")
+    cr.execute("UPDATE res_partner_bank SET allow_out_payment = true")
