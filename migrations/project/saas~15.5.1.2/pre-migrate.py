645c8f82a88d28256b5ca7573a591c7adf9d94b2
Laurent Stukkens (LTU) <ltu@odoo.com>
2022-06-29 10:04:29 +0000

    [IMP] project/saas~15.5: remove burndown_chart pivot view
    As the pivot burndown chart view in the community related PR,
    this commit ensures that the view is removed prior to the
    migration.
    
    task-2845729
    
    Related community PR: odoo/odoo#93225
    
    closes odoo/upgrade#3640
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>
    Co-authored-by: Yannick Tivisse <yti@odoo.com>

--- migrations/project/saas~15.5.1.2/pre-migrate.py
+++ migrations/project/saas~15.5.1.2/pre-migrate.py
@@ -7,3 +7,8 @@ def migrate(cr, version):
     util.remove_field(cr, "res.config.settings", "rating_status")
     util.remove_field(cr, "res.config.settings", "rating_status_period")
     cr.execute("DELETE FROM ir_config_parameter WHERE key IN ('project.rating_status','project.rating_status_period')")
+
+    util.remove_view(cr, "project.project_task_burndown_chart_report_view_pivot")
+    util.convert_model_to_abstract(cr, "project.task.burndown.chart.report")
+    for field in ["date_group_by", "task_id"]:
+        util.remove_field(cr, "project.task.burndown.chart.report", field)
