3b742d4a54022d5fe6e47292e10d8154d3a5854d
Jinane Maksoud <maji@odoo.com>
2022-06-30 09:08:35 +0000

    [ADD] base/0.0.0: clean xmlids of manual fields
    Remove the xml_ids that belong to manual fields coming from standard modules
    and those marked as `__export__`
    
    Related PRs in odoo that prevent the creation of new xmlids for those fields:
    https://github.com/odoo/odoo/pull/34563
    
    closes odoo/upgrade#3644
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/base/0.0.0/post-clean-manual-xmlids.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from odoo import modules
+
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    standard_modules = modules.get_modules()
+    excluded_modules = {"studio_customization", "__cloc_exclude__"}
+    included_modules = {"__export__"}
+    modules_that_should_not_have_manual_fields_xmlid = set(standard_modules) - excluded_modules | included_modules
+
+    cr.execute(
+        """
+        DELETE
+          FROM ir_model_data d
+         USING ir_model_fields f,
+               ir_model m
+         WHERE d.model = 'ir.model.fields'
+           AND f.id = d.res_id
+           AND m.id = f.model_id
+           AND f.state = 'manual'
+           AND m.state = 'base'
+           AND d.module IN %s
+            {}
+    """.format(
+            "AND d.studio IS NOT TRUE" if util.column_exists(cr, "ir_model_data", "studio") else ""
+        ),
+        [tuple(modules_that_should_not_have_manual_fields_xmlid)],
+    )
