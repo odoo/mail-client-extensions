8031e180cf296ea84cd8961f4904016931fe6cc8
Christophe Simonis <chs@odoo.com>
2022-07-01 18:09:03 +0000

    [ADD] hr_payroll_account/saas~15.5: match existing journal
    The `SLR` journal is created by the various `l10n_*_hr_payroll_account`
    modules.
    
    closes odoo/upgrade#3648
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/hr_payroll_account/saas~15.5.1.0/pre-migrate.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.force_noupdate(cr, "hr_payroll_account.hr_salaries_account_journal", True)
+    util.ensure_xmlid_match_record(
+        cr,
+        "hr_payroll_account.hr_payroll_account_journal",
+        "account.journal",
+        {
+            "code": "SLR",
+            "company_id": util.env(cr).user.company_id.id,
+        },
+    )
