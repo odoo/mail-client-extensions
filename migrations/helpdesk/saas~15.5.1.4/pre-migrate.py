d07c14b0e5c7e732680f1ce8a64b58a46d5aa41d
kais-odoo <kais@odoo.com>
2022-07-05 11:26:40 +0000

    [IMP] helpdesk/saas~15.5: remove field 'is_close'
    in this commit, 'closing stage' field removed from
    helpdesk stage
    
    Related Enterprise PR: https://github.com/odoo/enterprise/pull/28949
    
    task-2869771
    
    closes odoo/upgrade#3657
    
    Related: odoo/enterprise#28949
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/helpdesk/saas~15.5.1.4/pre-migrate.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+            UPDATE helpdesk_stage
+                SET fold=true
+              WHERE is_close=true AND fold=false
+        """
+    )
+    util.update_field_references(cr, "is_close", "fold", only_models=("helpdesk.stage",))
+    util.remove_field(cr, "helpdesk.stage", "is_close")
