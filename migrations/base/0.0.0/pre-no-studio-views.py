29a31f6ce297d75d9593e58aad1d6fe06363dc18
Alvaro Fuentes <afu@odoo.com>
2022-07-06 07:04:42 +0000

    [FIX] base/0.0.0: don't remove industry_fsm_report's reports
    `industry_fsm_report` creates some reports in code and assign to them
    xmlids
    https://github.com/odoo/enterprise/blob/9ea9003746443bdbf5b41aa9bff38f1bf7378c48/industry_fsm_report/models/project_worksheet_template.py#L331-L337
    
    Although they are correctly created with `noupdate=True` this script was
    changing them to `noupdate=False`. The reason is that those reports are
    meant to be edited through the menu Field Services > Configuration >
    Worksheet Templates, i.e. they would have `studio=True` while the xmlid
    is still on `industry_fsm_report` module.
    
    We add here an exception to not change the noupdate flag for those
    reports.
    
    upg-361131
    Many more, see (part of) odoo/upgrade-specific#3886
    
    closes odoo/upgrade#3658
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/0.0.0/pre-no-studio-views.py
+++ migrations/base/0.0.0/pre-no-studio-views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+
 from odoo.addons.base.maintenance.migrations import util
 
 _logger = logging.getLogger("odoo.addons.base.maintenance.migrations.base." + __name__)
@@ -10,13 +11,16 @@ def migrate(cr, version):
         return
 
     cr.execute(
-        """
+        r"""
             UPDATE ir_model_data
                SET noupdate = false
              WHERE model = 'ir.ui.view'
                AND noupdate = true
                AND studio = true
                AND module != 'studio_customization'
+               AND NOT (   module = 'industry_fsm_report'
+                       AND name LIKE 'report\_custom\_%'
+                       )
          RETURNING module, name
         """
     )
