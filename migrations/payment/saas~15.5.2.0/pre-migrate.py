f31c0a5e9844f51b6865e67f0221b4aa67d5dfad
Laura Schauer <lasc@odoo.com>
2022-07-08 07:35:05 +0000

    [IMP] payment/saas~15.5: rename and update token name
    Before, token names were prefixed with leading X's.
    For token names to be dynamically computed, only the clear payment
    details are stored.
    
    See odoo/odoo#94978
    See odoo/enterprise#29190
    
    closes odoo/upgrade#3738
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/payment/saas~15.5.2.0/pre-migrate.py
+++ migrations/payment/saas~15.5.2.0/pre-migrate.py
@@ -4,3 +4,16 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.create_column(cr, "payment_acquirer", "is_published", "boolean", default=True)
     util.rename_field(cr, "payment.acquirer", "country_ids", "available_country_ids")
+    util.rename_field(cr, "payment.token", "name", "payment_details")
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            """
+                UPDATE payment_token
+                   SET payment_details = RIGHT(payment_details, 4)
+                 WHERE payment_details IS NOT NULL
+            """,
+            table="payment_token",
+        ),
+    )
