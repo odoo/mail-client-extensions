a7bfbeaada58f6fbf6bf7382e2ff8e80bb89c380
Agustín Castro Bugallo <agcb@odoo.com>
2022-07-08 13:05:16 +0000

    [FIX] base/saas~13.3.1.3: Use `on_collision="merge"`
    It is common for these module categories to have an already existing
    xmlid with the same name they are going to rename to, needing to be
    fixed with a specific script.
    
    To avoid the constraint violation, use is made of the `on_collision`
    argument to merge them instead.
    
    Specifix examples:
    odoo/upgrade-specific#2389
    odoo/upgrade-specific#3025
    odoo/upgrade-specific#1727
    
    upg-167597
    upg-361764
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/14.0/odoo/service/server.py", line 1201, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "/home/odoo/src/odoo/14.0/odoo/modules/registry.py", line 89, in new
        odoo.modules.load_modules(registry._db, force_demo, status, update_module)
      File "/home/odoo/src/odoo/14.0/odoo/modules/loading.py", line 402, in load_modules
        report=report, models_to_check=models_to_check)
      File "/home/odoo/src/odoo/14.0/odoo/modules/loading.py", line 174, in load_module_graph
        migrations.migrate_module(package, 'pre')
      File "/home/odoo/src/odoo/14.0/odoo/modules/migration.py", line 180, in migrate_module
        migrate(self.cr, installed_version)
      File "/tmp/tmp8u1ihxy7/migrations/base/saas~13.3.1.3/pre-migrate.py", line 18, in migrate
        util.rename_xmlid(cr, *eb("base.module_category_{localization,accounting_localizations}"))
      File "/tmp/tmp8u1ihxy7/migrations/util/records.py", line 438, in rename_xmlid
        (new_module, new_name, old_module, old_name),
      File "<decorator-gen-3>", line 2, in execute
      File "/home/odoo/src/odoo/14.0/odoo/sql_db.py", line 101, in check
        return f(self, *args, **kwargs)
      File "/home/odoo/src/odoo/14.0/odoo/sql_db.py", line 300, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.UniqueViolation: duplicate key value violates unique constraint "ir_model_data_module_name_uniq_index"
    DETAIL:  Key (module, name)=(base, module_category_accounting_localizations) already exists.
    ```
    
    closes odoo/upgrade#3667
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~13.3.1.3/pre-migrate.py
+++ migrations/base/saas~13.3.1.3/pre-migrate.py
@@ -5,37 +5,25 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     eb = util.expand_braces
-    util.rename_xmlid(cr, *eb("base.module_category_{operations,services}_timesheets"))
-    util.rename_xmlid(cr, *eb("base.module_category_{operations,services}_project"))
-    util.rename_xmlid(cr, *eb("base.module_category_{operations,inventory}_inventory"))
-    util.rename_xmlid(cr, *eb("base.module_category_{operations,inventory}_purchase"))
-    util.rename_xmlid(cr, *eb("base.module_category_{operations,manufacturing}_maintenance"))
-    util.rename_xmlid(cr, *eb("base.module_category_{operations_inventory,inventory}_delivery"))
-    util.rename_xmlid(cr, *eb("base.module_category_{operations,productivity}_documents"))
-    util.rename_xmlid(cr, *eb("base.module_category_{operations,services}_helpdesk"))
-    util.rename_xmlid(cr, *eb("base.module_category_{operations,services}_field_service"))
-    util.rename_xmlid(cr, *eb("base.module_category_manufacturing_{plm,product_lifecycle_management_(plm)}"))
-    util.rename_xmlid(cr, *eb("base.module_category_{localization,accounting_localizations}"))
-    util.rename_xmlid(cr, *eb("base.module_category_sales_{subscription,subscriptions}"))
-    util.rename_xmlid(cr, *eb("base.module_category_marketing_{social,social_marketing}"))
-    util.rename_xmlid(cr, *eb("base.module_category_{accounting,human_resources}_expense"))
-    util.rename_xmlid(cr, *eb("base.module_category_accounting_{payment,payment_acquirers}"))
-    util.rename_xmlid(cr, *eb("base.module_category_{discuss,productivity_discuss}"))
-
-    # categories moved to `hidden/` prefix may already exists, deduplicate them
-    for cat in {"tools", "tests"}:
-        root_name = f"base.module_category_{cat}"
-        hidden_name = f"base.module_category_hidden_{cat}"
-        root_id = util.ref(cr, root_name)
-        hidden_id = util.ref(cr, hidden_name)
-        if root_id and hidden_id:
-            if root_id != hidden_id:
-                util.replace_record_references(
-                    cr, ("ir.module.category", root_id), ("ir.module.category", hidden_id), replace_xmlid=False
-                )
-            cr.execute("DELETE FROM ir_model_data WHERE module=%s AND name=%s", root_name.split("."))
-        elif root_id:
-            util.rename_xmlid(cr, root_name, hidden_name)
+    oc = dict(on_collision="merge")
+    util.rename_xmlid(cr, *eb("base.module_category_{operations,services}_timesheets"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{operations,services}_project"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{operations,inventory}_inventory"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{operations,inventory}_purchase"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{operations,manufacturing}_maintenance"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{operations_inventory,inventory}_delivery"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{operations,productivity}_documents"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{operations,services}_helpdesk"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{operations,services}_field_service"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_manufacturing_{plm,product_lifecycle_management_(plm)}"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{localization,accounting_localizations}"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_sales_{subscription,subscriptions}"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_marketing_{social,social_marketing}"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{accounting,human_resources}_expense"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_accounting_{payment,payment_acquirers}"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{discuss,productivity_discuss}"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{,hidden}_tools"), **oc)
+    util.rename_xmlid(cr, *eb("base.module_category_{,hidden}_tests"), **oc)
 
     util.remove_view(cr, "base.view_menu")
     util.remove_view(cr, "base.view_partner_short_form")
