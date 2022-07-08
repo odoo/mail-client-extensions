021c56af4776fd2ea81398a3781034763755281b
Kevin Baptiste <kba@odoo.com>
2022-07-08 07:52:17 +0000

    [IMP] hr_recruitment/saas~15.5: update email templates
    odoo/odoo#95630
    
    task-2908038
    
    closes odoo/upgrade#3670
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/hr_recruitment/saas~15.5.1.0/post-migrate.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    rt = dict(reset_translations={"body_html"})
+    util.if_unchanged(
+        cr, "hr_recruitment.email_template_data_applicant_congratulations", util.update_record_from_xml, **rt
+    )
+    util.if_unchanged(cr, "hr_recruitment.email_template_data_applicant_interest", util.update_record_from_xml, **rt)
