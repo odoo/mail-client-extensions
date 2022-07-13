8b20e10ae31f8ba1882da51dcb84f8803bf1216e
Louis Wicket (wil) <wil@odoo.com>
2022-07-13 09:29:12 +0000

    [IMP] mail/saas~15.5: rename mail.channel.partner to mail.channel.member
    closes odoo/upgrade#3681
    
    Community: odoo/odoo#95912
    Enterprise: odoo/enterprise#29433
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/mail/0.0.0/pre-ensure-records.py
+++ migrations/mail/0.0.0/pre-ensure-records.py
@@ -5,10 +5,12 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # Prevents constraint mail_channel_partner_partner_unique from failing
     if util.version_gte("saas~15.1"):
+        # table mail_channel_partner was renamed to mail_channel_member in 15.5
+        name = "member" if util.table_exists(cr, "mail_channel_member") else "partner"
         util.ensure_xmlid_match_record(
             cr,
-            "mail.channel_partner_general_channel_for_admin",
-            "mail.channel.partner",
+            "mail.channel_{}_general_channel_for_admin".format(name),
+            "mail.channel.{}".format(name),
             {
                 "partner_id": util.ref(cr, "base.partner_admin"),
                 "channel_id": util.ref(cr, "mail.channel_all_employees"),
---
+++ migrations/mail/saas~15.5.1.9/pre-migrate.py
@@ -0,0 +1,34 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.rename_model(cr, "mail.channel.partner", "mail.channel.member")
+    util.rename_field(cr, "mail.channel.rtc.session", "channel_partner_id", "channel_member_id")
+    util.rename_field(cr, "mail.channel", "channel_last_seen_partner_ids", "channel_member_ids")
+
+    eb = util.expand_braces
+    renames = """
+        mail.channel_{partner,member}_general_channel_for_admin
+        mail.mail_channel_{partner,member}_view_tree
+        mail.mail_channel_{partner,member}_view_form
+        mail.mail_channel_{partner,member}_action
+        mail.mail_channel_{partner,member}_menu
+
+        mail.ir_rule_mail_channel_{partner,member}_group_user
+        mail.ir_rule_mail_channel_{partner,member}_group_system
+
+        mail.access_mail_channel_{partner,member}_public
+        mail.access_mail_channel_{partner,member}_portal
+        mail.access_mail_channel_{partner,member}_user
+    """
+    for rename in util.splitlines(renames):
+        util.rename_xmlid(cr, *eb(rename))
+
+    indexes = """
+        mail_channel_{partner,member}_guest_unique
+        mail_channel_{partner,member}_partner_unique
+        mail_channel_{partner,member}_seen_message_id_idx
+        mail_channel_rtc_session_channel_{partner,member}_unique
+    """
+    for old, new in map(eb, util.splitlines(indexes)):
+        cr.execute(f"ALTER INDEX IF EXISTS {old} RENAME TO {new}")
