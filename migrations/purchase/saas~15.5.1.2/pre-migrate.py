79f0c3e2c6ed7ec97e69b6b6232a0e1cfa9aad0b
Arnold Moyaux <arm@odoo.com>
2022-07-13 15:34:13 +0000

    [IMP] purchase_stock/saas~15.5: remove price different account
    The community PR is mainly to automatically correct stock value and
    accounting when there is a price difference between a purchase.order
    and the related invoice. Price difference account was use to
    partially fix this situtation.
    
    Price different account is not used anymore since the correction are
    automaticaly made with the layers. So remove the associated view.
    
    Task-2081607
    See odoo/odoo/pull/99411
    
    closes odoo/upgrade#3878
    
    Related: odoo/odoo#99411
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/purchase/saas~15.5.1.2/pre-migrate.py
+++ migrations/purchase/saas~15.5.1.2/pre-migrate.py
@@ -6,3 +6,7 @@ eb = util.expand_braces
 
 def migrate(cr, version):
     util.rename_field(cr, "purchase.order", *eb("tax_totals{_json,}"))
+    util.remove_view(cr, "purchase.product_template_form_view")
+    util.remove_view(cr, "purchase.view_category_property_form")
+    util.remove_field(cr, "product.template", "property_account_creditor_price_difference")
+    util.remove_field(cr, "product.category", "property_account_creditor_price_difference_categ")
---
+++ migrations/purchase_stock/saas~15.5.1.2/pre-migrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "purchase_stock.product_category_view_form")
