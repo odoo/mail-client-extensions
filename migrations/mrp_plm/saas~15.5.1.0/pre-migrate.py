88843b93b4302ac90cbd9a4374b628fa405a9719
clesgow <quwo@odoo.com>
2022-07-14 15:17:49 +0000

    [IMP] mrp_plm/saas~15.5: BoM Overview
    Remove now obsolete views of the 'Structure & Cost' report as it has
    been reworked into a brand new report, with its new templates.
    
    See odoo/enterprise#28191
    
    Part-of: odoo/upgrade#3686

--- migrations/mrp_plm/saas~15.5.1.0/pre-migrate.py
+++ migrations/mrp_plm/saas~15.5.1.0/pre-migrate.py
@@ -5,3 +5,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "mrp_plm.message_approval")
+    util.remove_view(cr, "mrp_plm.report_mrp_bom_line_inherit_mrp_plm")
