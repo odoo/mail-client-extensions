da2183aea4ecc431407ef6a240239cc63582e23f
Thibault Delavallée <tde@odoo.com>
2022-07-19 07:01:00 +0000

    [IMP] mail, sms/saas~15.5: improve 'email' and 'sms' server actions
    We now support posting on a thread with options in addition to directly sending
    emails or sms from templates. See odoo/odoo#75906 for more details.
    
    This script renames key (from email to mail_post to be more coherent with
    its usage) and bootstraps new columns
      * with mail_post_method being email as this was the only supported method
        previously;
      * with sms_method being 'sms' or 'note' depending on previous configuration
        coming from 'sms_mass_keep_log';
    
    Task-2613245 (Server actions mail update / cleaning)
    
    closes odoo/upgrade#3696
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/mail/saas~15.5.1.10/pre-migrate.py
+++ migrations/mail/saas~15.5.1.10/pre-migrate.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
+
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.remove_view(cr, "mail.email_message_tree_view")
+
+    util.change_field_selection_values(
+        cr,
+        "ir.actions.server",
+        "state",
+        {"email": "mail_post"},
+    )
+    util.create_column(cr, "ir_act_server", "mail_post_autofollow", "boolean")
+    util.create_column(cr, "ir_act_server", "mail_post_method", "varchar")
+    cr.execute(
+        """
+            UPDATE ir_act_server
+               SET mail_post_method = 'email'
+             WHERE state = 'mail_post'
+        """
+    )
---
+++ migrations/sms/saas~15.5.2.4/pre-migrate.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "ir_act_server", "sms_method", "varchar")
+    cr.execute(
+        """
+            UPDATE ir_act_server
+               SET sms_method = CASE WHEN sms_mass_keep_log = true THEN 'note'
+                                     ELSE 'sms'
+                                     END
+             WHERE state = 'sms'
+        """
+    )
+    util.remove_field(cr, "ir.actions.server", "sms_mass_keep_log")
