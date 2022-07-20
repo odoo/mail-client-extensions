dc7f0e6aec315a45e8da7faf559ab3d74edabae7
Alvaro Fuentes <afu@odoo.com>
2022-07-20 13:00:05 +0000

    [FIX] base/0.0.0: avoid not null constraints
    Some DBs in 12.0 may have not null constraints for some columns in
    res_partner table. See, for example, odoo/odoo@06a02ae4.
    
    We do not discard either that some custom modules may add not null
    columns.
    
    upg-363662 (and more)
    
    closes odoo/upgrade#3701
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/base/0.0.0/pre-res_partner_bank.py
+++ migrations/base/0.0.0/pre-res_partner_bank.py
@@ -10,33 +10,40 @@ def migrate(cr, version):
         return
     cr.execute(
         """
-        WITH new_partners AS (
-           INSERT
-             INTO res_partner(name, active, color)
-           SELECT 'Partner for bank account ' || b.acc_number,
-                  false,
-                  b.id
-             FROM res_partner_bank b
-            WHERE b.partner_id IS NULL
-        RETURNING id,
-                  color AS bank_id
-        )  UPDATE res_partner_bank b
-              SET partner_id = new_partners.id
-             FROM new_partners
-            WHERE new_partners.bank_id = b.id
-        RETURNING b.id
+        CREATE FUNCTION _upg_not_null_partner() RETURNS TABLE(id int)
+        AS $$
+            BEGIN
+                RETURN QUERY WITH new_partners AS (
+                   INSERT
+                     INTO res_partner(name, active, color)
+                   SELECT 'Partner for bank account ' || b.acc_number,
+                          false,
+                          b.id
+                     FROM res_partner_bank b
+                    WHERE b.partner_id IS NULL
+                RETURNING res_partner.id,
+                          res_partner.color AS bank_id
+                )  UPDATE res_partner_bank b
+                      SET partner_id = new_partners.id
+                     FROM new_partners
+                    WHERE new_partners.bank_id = b.id
+                RETURNING b.partner_id;
+                RETURN;
+            EXCEPTION
+                WHEN not_null_violation THEN RETURN;
+            END;
+        $$
+        LANGUAGE PLPGSQL
         """
     )
-    if not cr.rowcount:
-        return
-    bids = cr.fetchall()
-    cr.execute(
-        """
-        UPDATE res_partner p
-           SET color = NULL
-          FROM res_partner_bank b
-         WHERE b.partner_id = p.id
-           AND b.id IN %s
-        """,
-        [tuple(_id for _id, in bids)],
-    )
+    cr.execute("SELECT * FROM _upg_not_null_partner()")
+    if cr.rowcount:
+        cr.execute(
+            """
+            UPDATE res_partner p
+               SET color = NULL
+             WHERE p.id IN %s
+            """,
+            [tuple(_id for _id, in cr.fetchall())],
+        )
+    cr.execute("DROP FUNCTION _upg_not_null_partner")
