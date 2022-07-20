e215688590e8dac16f84afcadfac698f1c0c176b
Cyril (cyla) <cyla@odoo.com>
2022-07-20 08:13:35 +0000

    [IMP] base/14.0.1.3: avoid unnecessary calls to fixup_m2m
    `update_db()` is called many times per model in each upgrade. In the
    13->14 migration, this translates into multiple calls to `fixup_m2m()`
    on every m2m table.
    
    As per #1940, the aim of this is to enforce restrictions that were not
    enforced prior to 14.0. This does not need to be done more than once
    per relation.
    
    Tested on upg-244576, there are 1614 calls to `fixup_m2m()` coming from
    here, on 199 unique tables. This accounts for 9 minutes out of a 55
    minutes upgrade, down to 199 calls in 1 minute after this patch.
    
    closes odoo/upgrade#3699
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/14.0.1.3/pre-many2many.py
+++ migrations/base/14.0.1.3/pre-many2many.py
@@ -6,13 +6,16 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
 
     original_update_db = Many2many.update_db
+    fixed = set()
 
     def update_db(self, model, columns):
         comodel = model.env[self.comodel_name]
         if (
-            util.table_exists(cr, self.relation)
+            self.relation not in fixed
+            and util.table_exists(cr, self.relation)
             and len(util.get_columns(cr, self.relation, ignore=(self.column1, self.column2))) == 0
         ):
+            fixed.add(self.relation)
             util.fixup_m2m(cr, self.relation, model._table, comodel._table, self.column1, self.column2)
 
         original_update_db(self, model, columns)
