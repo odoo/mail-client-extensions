0b855e8f10c7ce6cc2cb5dfab80b5bc09c162c53
Horacio Tellez <hote@odoo.com>
2022-07-20 15:19:46 +0000

    [IMP] payment/saas~15.5: change of field's name and deletion of field
    Changing the field name "country_ids" to "available_country_ids" to
    be more on accord with its purpose.
    
    Removing the currency_id field from the sepa_direct_debit acquirer,
    as we are adding a field with the same definition to the
    payment.acquirer model, this field is no longer required.
    
    See the community task for more details.
    
    See - odoo/odoo#82411
    See - odoo/enterprise#24412
    
    closes odoo/upgrade#3703
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/payment/saas~15.5.2.0/pre-migrate.py
+++ migrations/payment/saas~15.5.2.0/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "payment_acquirer", "is_published", "boolean", default=True)
+    util.rename_field(cr, "payment.acquirer", "country_ids", "available_country_ids")
