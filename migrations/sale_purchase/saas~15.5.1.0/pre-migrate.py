52ca2dd19d91bcd09ff609aefc136fc54acf824c
Mahendra Barad <mba@odoo.com>
2022-07-26 10:28:47 +0000

    [IMP] sale_purchase/saas~15.5: convert the field to company_dependent
    Purpose of the commit is to convert the service_to_purchase field
    to the company_dependent field.
    
    Related Community PR: https://github.com/odoo/odoo/pull/96149
    Related Enterprise PR: https://github.com/odoo/enterprise/pull/29563
    
    task-2889829
    
    closes odoo/upgrade#3712
    
    Related: odoo/odoo#96149
    Related: odoo/enterprise#29563
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/sale_purchase/saas~15.5.1.0/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.convert_field_to_property(cr, "product.template", "service_to_purchase", "boolean")
+    util.remove_constraint(cr, "product_template", "product_template_service_to_purchase")
