b0f4b7ce2d0e317591583537514076aa36fe76f7
Jinane Maksoud <maji@odoo.com>
2022-07-27 10:31:27 +0000

    [FIX] base/0.0.0: change studio file names to char
    At some point in v12 if the user added a studio field of type binary, an
    additional field is created for the file name and it was saved as binary
    as well.
    https://github.com/odoo/enterprise/pull/3135 fixed this behavior and
    changed the filename to char, but the fields created before the fix are
    not updated.
    
    closes odoo/upgrade#3717
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/0.0.0/pre-00-fix-oversights.py
+++ migrations/base/0.0.0/pre-00-fix-oversights.py
@@ -9,3 +9,36 @@ def migrate(cr, version):
 
     # forgotten temporary table
     cr.execute("DROP TABLE IF EXISTS saas124_acc_mig_bad_mappings")
+
+    if util.version_gte("12.0"):
+        cr.execute(
+            r"""
+               UPDATE ir_model_fields fields
+                  SET ttype = 'char'
+                 FROM ir_model_fields f
+                 JOIN ir_model_data d
+                   ON d.res_id = f.id
+                  AND d.model = 'ir.model.fields'
+                WHERE d.module = 'studio_customization'
+                  AND f.state = 'manual'
+                  AND f.ttype = 'binary'
+                  AND f.name LIKE 'x\_%\_filename'
+                  AND fields.id = f.id
+            RETURNING fields.model,
+                      fields.name
+        """
+        )
+        for model, name in cr.fetchall():
+            table = util.table_of_model(cr, model)
+            if not util.column_exists(cr, table, name):
+                continue
+            cr.execute(
+                """
+                ALTER TABLE {table}
+                ALTER COLUMN "{name}"
+                 TYPE varchar
+                USING CONVERT_FROM("{name}",'UTF8')
+            """.format(
+                    **locals()
+                ),
+            )
