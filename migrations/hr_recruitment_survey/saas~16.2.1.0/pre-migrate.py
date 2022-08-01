af2d953c1e5756475c0f45596b01bd4063867a78
Dossogne Bertrand <bedo@odoo.com>
2022-08-01 13:06:50 +0000

    [IMP] hr_recruitment_survey/saas~16.2: change applicant-user_input relation
    This commit changes the relation between hr_applicant
    to survey_user_input from many2one to one2many, thus
    allowing the applicant to receive a survey with a new token
    without creating an error.
    
    taskID 2884526
    
    related to odoo/odoo#94178
    
    closes odoo/upgrade#3731
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr_recruitment_survey/saas~16.2.1.0/pre-migrate.py
+++ migrations/hr_recruitment_survey/saas~16.2.1.0/pre-migrate.py
@@ -4,4 +4,15 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.rename_field(cr, "survey.user_input", "applicant_id", "applicant_ids")
+    util.remove_field(cr, "hr.applicant", "response_state")
+    util.create_column(cr, "survey_user_input", "applicant_id", "int4")
+
+    cr.execute(
+        """
+        UPDATE survey_user_input i
+           SET applicant_id = a.id
+          FROM hr_applicant a
+         WHERE a.response_id = i.id
+        """
+    )
+    util.remove_field(cr, "hr.applicant", "response_id")
