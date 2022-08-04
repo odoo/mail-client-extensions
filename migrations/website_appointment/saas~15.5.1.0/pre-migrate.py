ff6e32cf098bc333ca5746fad325a14c3fca5c09
amdi-odoo <amdi@odoo.com>
2022-08-04 13:53:19 +0000

    [IMP] {website_}appointment/saas~15.5: remove old search box
    Old search box dropped for a new one handling search suggestions.
    
    Task-2888646
    
    closes odoo/upgrade#3665
    
    Related: odoo/enterprise#29219
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website_appointment/saas~15.5.1.0/pre-migrate.py
+++ migrations/website_appointment/saas~15.5.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "website_appointment.user_navbar_inherit_website_enterprise_inherit_website_appointment")
+    util.remove_view(cr, "website_appointment.appointments_search_box")
