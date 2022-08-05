5d7c2350a2a17531c5a6bc97998b483998e2ec88
Kevin Baptiste <kba@odoo.com>
2022-08-05 06:44:20 +0000

    [IMP] hr_appraisal/saas~15.5: remove previous_appraisal_id
    Related: odoo/enterprise#30147
    
    task-2945849
    
    closes odoo/upgrade#3747
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr_appraisal/saas~15.5.1.1/pre-migrate.py
+++ migrations/hr_appraisal/saas~15.5.1.1/pre-migrate.py
@@ -7,3 +7,6 @@ def migrate(cr, version):
     util.change_field_selection_values(
         cr, "hr.appraisal.goal", "progression", {"0": "000", "25": "025", "50": "050", "75": "075"}
     )
+
+    util.remove_field(cr, "hr.appraisal", "previous_appraisal_id")
+    util.remove_field(cr, "hr.appraisal", "previous_appraisal_date")
