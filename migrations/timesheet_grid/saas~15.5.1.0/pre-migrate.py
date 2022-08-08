ea7c204c577557b248cdaf9844159c22972b56f9
Raouf <abgh@odoo.com>
2022-08-08 09:21:29 +0000

    [ADD] timesheet_grid/saas~15.5: timesheets_past_days_encoding_limit
    In this commit:
    - timesheets_past_days_encoding_limit was
    removed from res.company.
    
    - last_validated_timesheet_date added
    in hr_employee
    
    - update query to calculate
    last_validated_timesheet_date
    
    See odoo/enterprise#30197
    
    Task-2946737
    
    closes odoo/upgrade#3756
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/timesheet_grid/saas~15.5.1.0/pre-migrate.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "res.config.settings", "timesheets_past_days_encoding_limit")
+    util.remove_field(cr, "res.company", "timesheets_past_days_encoding_limit")
+    util.create_column(cr, "hr_employee", "last_validated_timesheet_date", "date")
+    cr.execute(
+        """
+        WITH employee_last_validated_timesheet_date AS (
+            SELECT employee_id,
+                   MAX(A.date) as last_validated_timesheet_date
+              FROM account_analytic_line A
+             WHERE A.validated = true
+               AND A.project_id IS NOT NULL
+          GROUP BY employee_id
+        )
+        UPDATE hr_employee E
+           SET last_validated_timesheet_date = I.last_validated_timesheet_date
+          FROM employee_last_validated_timesheet_date I
+         WHERE I.employee_id = E.id
+    """
+    )
