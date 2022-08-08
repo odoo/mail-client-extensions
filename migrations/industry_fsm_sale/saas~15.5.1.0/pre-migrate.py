05cb2637949885fba20adcaefd6046b43b9c5b9c
Xavier BOL (xbo) <xbo@odoo.com>
2022-08-08 12:34:22 +0000

    [IMP] industry_fsm_sale_report/saas~15.5: move delivered_price_* fields
    With the changes in odoo/enterprise#25280, when the `industry_fsm_sale`
    module is installed but not the `industry_fsm_sale_report` one, the
    user cannot access portal view of a task because the
    `delivered_price_{subtotal,tax,total}` fields are not defined.
    
    This commit moves those fields from `industry_fsm_sale_report` in
    `industry_fsm_sale`.
    
    Related PR: odoo/enterprise#30205
    
    Related task-2743512
    
    closes odoo/upgrade#3758
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/industry_fsm_sale/saas~15.5.1.0/pre-migrate.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    field_name_list = [
+        "delivered_price_subtotal",
+        "delivered_price_tax",
+        "delivered_price_total",
+    ]
+    for field_name in field_name_list:
+        util.move_field_to_module(cr, "sale.order.line", field_name, "industry_fsm_sale_report", "industry_fsm_sale")
