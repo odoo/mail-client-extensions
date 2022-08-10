084798dbbd72de54e5a9ed9df293aa77f87c191d
Panagiotis Kyriakou <paky@odoo.com>
2022-08-10 12:26:35 +0000

    [IMP] project_*/saas~15.5: remove fields and views
    * = project_forecast, project_timesheet_forecast,
    project_timesheet_forecast_sale
    
    In this commit we specify the fields and views that have been removed
    from the project_timesheet_forecast_sale, the project_forecast and the
    project_timesheet_forecast modules.
    
    Related PRs:
    * Community: odoo/odoo#98035
    * Enterprise: odoo/enterprise#30404
    
    task-2941828
    
    closes odoo/upgrade#3771
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/project_forecast/saas~15.5.1.0/pre-migrate.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "planning.slot.template", "task_id")
+    util.remove_field(cr, "project.task", "allow_forecast")
+    util.remove_field(cr, "project.task", "forecast_hours")
+    util.remove_field(cr, "planning.slot", "task_id")
+    util.remove_field(cr, "planning.slot", "planned_hours")
+    util.remove_field(cr, "planning.slot", "forecast_hours")
+    util.remove_field(cr, "planning.slot", "parent_id")
+    util.remove_constraint(cr, "planning_slot", "planning_slot_project_required_if_task")
+    util.remove_field(cr, "planning.analysis.report", "task_id")
+    util.remove_field(cr, "planning.analysis.report", "parent_id")
+    util.remove_view(cr, "project_forecast.project_task_view_form")
+    util.remove_record(cr, "project_forecast.action_project_task_view_planning")
---
+++ migrations/project_timesheet_forecast_sale/saas~15.5.1.0/pre-migrate.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "planning.slot", "order_line_id")
+    util.remove_field(cr, "project.timesheet.forecast.report.analysis", "sale_order_id")
+    util.remove_field(cr, "project.timesheet.forecast.report.analysis", "sale_line_id")
+    util.remove_view(cr, "project_timesheet_forecast_sale.project_timesheet_forecast_sale_report_view_search")
+    util.remove_view(cr, "project_timesheet_forecast_sale.project_timesheet_forecast_sale_report_view_pivot")
