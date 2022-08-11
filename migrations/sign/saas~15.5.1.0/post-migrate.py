bdd5bbe2296c728503aea26ba6db2bd8b61d2004
Arnaud Joset <arj@odoo.com>
2022-08-11 09:50:03 +0000

    [IMP] sign/saas~15.5.1.0: update the default name of sign item
    Related enterprise PR: https://github.com/odoo/enterprise/pull/28303
    
    closes odoo/upgrade#3767
    
    Taskid: 2888453
    Related: odoo/enterprise#28303
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/sign/saas~15.5.1.0/post-migrate.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    query = """
+        UPDATE sign_item si
+           SET name = sit.name
+          FROM sign_item_type sit
+         WHERE sit.id = si.type_id
+           AND si.name IS NULL
+        """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="sign_item", alias="si"))
