9595528b7379f075d1644a4f00016ef915b863e1
Victor Feyens <vfe@odoo.com>
2022-08-12 12:51:05 +0000

    [IMP] sale/saas~15.5: distinguish down payment invoice lines
    See odoo/odoo#98600
    See odoo/enterprise#30653
    
    closes odoo/upgrade#3799
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/sale/saas~15.5.1.2/pre-migrate.py
+++ migrations/sale/saas~15.5.1.2/pre-migrate.py
@@ -6,3 +6,14 @@ eb = util.expand_braces
 
 def migrate(cr, version):
     util.rename_field(cr, "sale.order", *eb("tax_totals{_json,}"))
+
+    util.create_column(cr, "account_move_line", "is_downpayment", "boolean")
+    query = """
+        UPDATE account_move_line aml
+           SET is_downpayment = TRUE
+          FROM sale_order_line_invoice_rel r
+          JOIN sale_order_line sol ON sol.id = r.order_line_id
+         WHERE aml.id = r.invoice_line_id
+           AND sol.is_downpayment
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="account_move_line", alias="aml"))
