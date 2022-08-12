19ef28a3cb78da01a8fb625b9f83d2ad26e8bf36
Romain Derie <rde@odoo.com>
2022-08-12 15:31:40 +0000

    [FIX] website/saas~15.5: rename some google analytics related records
    Google Analytics dashboard was removed with [1]. But some records were
    still mentioning it, which was a bit confusing.
    Nothing critical but as the community counterpart PR [⅛] of this commit
    is modifying a tour related to the analytics dashboard, the chance is
    taken to clean it.
    
    [1]: https://github.com/odoo/odoo/commit/985e49bdb5e22fa197ba267aa41d7a8ba7e50550
    [2]: https://github.com/odoo/odoo/pull/98090
    
    closes odoo/upgrade#3774
    
    Related: odoo/odoo#98090
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website/saas~15.5.1.0/pre-website.py
+++ migrations/website/saas~15.5.1.0/pre-website.py
@@ -11,3 +11,7 @@ def migrate(cr, version):
     util.remove_field(cr, "res.config.settings", "google_management_client_id")
     util.remove_field(cr, "res.config.settings", "google_management_client_secret")
     util.remove_field(cr, "res.config.settings", "has_google_analytics_dashboard")
+    util.rename_xmlid(
+        cr, "website.ir_actions_server_website_google_analytics", "website.ir_actions_server_website_analytics"
+    )
+    util.rename_xmlid(cr, "website.menu_website_google_analytics", "website.menu_website_analytics")
