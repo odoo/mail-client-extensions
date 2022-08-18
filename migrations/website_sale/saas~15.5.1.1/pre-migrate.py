a1f42e9fedf534ab2071eef18a8ee3c20c8a6fa2
William Braeckman <wbr@odoo.com>
2022-08-18 07:52:07 +0000

    [IMP] website_sale/saas~15.5: improve image zoom on product pages
    Two templates have been renamed.
    
    Relates to odoo/odoo#98305
    
    TaskId-2922062
    
    closes odoo/upgrade#3785
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website_sale/saas~15.5.1.1/pre-migrate.py
+++ migrations/website_sale/saas~15.5.1.1/pre-migrate.py
@@ -8,3 +8,6 @@ def migrate(cr, version):
     util.remove_view(cr, "website_sale.search_count_box")
     util.remove_view(cr, "website_sale.user_navbar_inherit_website_sale")
     util.remove_view(cr, "website_sale.recommended_products")
+
+    util.rename_xmlid(cr, "website_sale.product_picture_magnify_auto", "website_sale.product_picture_magnify_hover")
+    util.rename_xmlid(cr, "website_sale.product_picture_magnify", "website_sale.product_picture_magnify_click")
