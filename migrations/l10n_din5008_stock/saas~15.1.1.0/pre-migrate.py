671374b9c8044b4ff2877d559971f341d4f6eb54
Christophe Simonis <chs@odoo.com>
2022-08-22 08:17:48 +0000

    [FIX] l10n_din5008_stock/saas~15.1: rename field
    Field added (and renamed) during forward-port odoo/odoo#86014
    
    Part-of: odoo/upgrade#3732

--- migrations/l10n_din5008_stock/saas~15.1.1.0/pre-migrate.py
+++ migrations/l10n_din5008_stock/saas~15.1.1.0/pre-migrate.py
@@ -6,3 +6,4 @@ def migrate(cr, version):
     eb = util.expand_braces
 
     util.rename_field(cr, "stock.picking", *eb("l10n_{de,din5008}_template_data"))
+    util.rename_field(cr, "stock.picking", *eb("l10n_{de,din5008}_addresses"))
