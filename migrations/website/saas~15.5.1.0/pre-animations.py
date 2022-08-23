c179410ee89060e87081a8e823ee3a712b37e7e1
Benjamin Vray <bvr@odoo.com>
2022-08-23 07:24:23 +0000

    [IMP] website/saas~15.5: add an "on scroll" option for animations
    This converts classnames which have been split into two classes. Also
    use an inline style for a specific animation.
    
    task-2862761
    See https://github.com/odoo/odoo/pull/94119
    
    closes odoo/upgrade#3858
    
    Related: odoo/odoo#94119
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Co-authored-by: qsm-odoo <qsm@odoo.com>

---
+++ migrations/website/saas~15.5.1.0/pre-animations.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import re
+
+import odoo.upgrade.util.snippets as snip
+
+ANIMATE_OLD_TO_NEW_CLASSES = {
+    "o_anim_bounce_in_down": ("o_anim_bounce_in", "o_anim_from_top"),
+    "o_anim_bounce_in_left": ("o_anim_bounce_in", "o_anim_from_left"),
+    "o_anim_bounce_in_right": ("o_anim_bounce_in", "o_anim_from_right"),
+    "o_anim_fade_in_down": ("o_anim_fade_in", "o_anim_from_top"),
+    "o_anim_fade_in_left": ("o_anim_fade_in", "o_anim_from_left"),
+    "o_anim_fade_in_right": ("o_anim_fade_in", "o_anim_from_right"),
+    "o_anim_fade_in_up": ("o_anim_fade_in", "o_anim_from_bottom"),
+    "o_anim_rotate_in_down_left": ("o_anim_rotate_in", "o_anim_from_bottom_left"),
+    "o_anim_rotate_in_down_right": ("o_anim_rotate_in", "o_anim_from_bottom_right"),
+    "o_anim_zoom_in_down": ("o_anim_zoom_in", "o_anim_from_top"),
+    "o_anim_zoom_in_left": ("o_anim_zoom_in", "o_anim_from_left"),
+    "o_anim_zoom_in_right": ("o_anim_zoom_in", "o_anim_from_right"),
+}
+
+
+def convert_animate(el):
+    el_class_attr = el.get("class", False)
+    if not el_class_attr:
+        return False
+
+    classes = re.split(r"\s+", el_class_attr)
+
+    for class_name in classes:
+        new_classes = ANIMATE_OLD_TO_NEW_CLASSES.get(class_name)
+        if new_classes is not None:
+            classes.remove(class_name)
+            classes.extend(new_classes)
+            el.set("class", " ".join(classes))
+            return True
+
+    if "o_anim_flash" in classes:
+        old_style = el.get("style", "")
+        el.set("style", f"--wanim-intensity: 100; {old_style}")
+        return True
+
+    return False
+
+
+def migrate(cr, version):
+    snip.convert_html_content(
+        cr,
+        snip.html_selector_converter(convert_animate, selector="//*[hasclass('o_animate')]"),
+        where_column=r"~ '\yo_animate\y'",
+    )
