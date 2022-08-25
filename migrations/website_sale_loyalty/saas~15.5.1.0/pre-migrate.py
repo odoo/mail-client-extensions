d19d0459c760a9555ce771f118087f868687a631
William Braeckman <wbr@odoo.com>
2022-08-25 12:19:07 +0000

    [IMP] website_sale_loyalty/saas~15.5: loyalty misc improvements
    See https://github.com/odoo/odoo/pull/99023
    See https://github.com/odoo/enterprise/pull/30794
    
    closes odoo/upgrade#3820
    
    Related: odoo/odoo#99023
    Related: odoo/enterprise#30794
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/website_sale_loyalty/saas~15.5.1.0/pre-migrate.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "website_sale_loyalty.pay_with_gift_card_form")
+    util.remove_view(cr, "website_sale_loyalty.loyalty_generate_wizard_view_form_inherit_website_sale_loyalty")
+
+    util.remove_field(cr, "loyalty.generate.wizard", "website_id")
