29f5479b90b9547f8831c0410f28ad0e58d24740
Alvaro Fuentes <afu@odoo.com>
2022-08-25 14:05:33 +0000

    [FIX] website/saas~14.5: check key before udpating page
    In website/saas~14.5.1.0/pre-10-contactus.py we only force-restore the
    standard views if there is a cowed view detected. Meaning that the key
    is also checked.
    https://github.com/odoo/upgrade/blob/df7854783ccd52428d12d92e6ee878200c19145f/migrations/website/saas%7E14.5.1.0/pre-10-contactus.py#L75
    
    Thus we cannot blindly assume that a new fresh contactus page/view was
    created. When there is a truly custom contactus view we better assume
    the data is already correct.
    
    Examples (different DBs):
    ```
    371616_afu_14.0=> SELECT p.id,p.view_id,p.url,p.website_id,v.key,v.name FROM website_page p JOIN ir_ui_view v ON p.view_id=v.id WHERE p.url = '/contactus'
    +------+-----------+------------+--------------+--------------------+------------+
    | id   | view_id   | url        | website_id   | key                | name       |
    |------+-----------+------------+--------------+--------------------+------------|
    | 2    | 1051      | /contactus | <null>       | website.contactus  | Contact Us |
    | 34   | 2963      | /contactus | 1            | website.contact-us | Contact Us |
    +------+-----------+------------+--------------+--------------------+------------+
    376046_afu=> SELECT p.id,p.view_id,p.url,p.website_id,v.key,v.name FROM website_page p JOIN ir_ui_view v ON p.view_id=v.id WHERE p.url = '/contactus'
    +------+-----------+------------+--------------+-----------------+---------+
    | id   | view_id   | url        | website_id   | key             | name    |
    |------+-----------+------------+--------------+-----------------+---------|
    | 18   | 2117      | /contactus | 4            | website.kontakt | Kontakt |
    +------+-----------+------------+--------------+-----------------+---------+
    375478_afu=> SELECT p.id,p.view_id,p.url,p.website_id,v.key,v.name FROM website_page p JOIN ir_ui_view v ON p.view_id=v.id WHERE p.url = '/contactus'
    +------+-----------+------------+--------------+---------------------+-----------+
    | id   | view_id   | url        | website_id   | key                 | name      |
    |------+-----------+------------+--------------+---------------------+-----------|
    | 61   | 4898      | /contactus | 6            | website.contactus-1 | contactus |
    +------+-----------+------------+--------------+---------------------+-----------+
    ```
    In this case the key of the page with not-null website is not standard.
    
    upg-371616
    upg-376046
    upg-375478
    tbg-307
    
    closes odoo/upgrade#3817
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website/saas~14.5.1.0/post-define-contactus-details.py
+++ migrations/website/saas~14.5.1.0/post-define-contactus-details.py
@@ -21,24 +21,33 @@ def migrate(cr, version):
     if not website_ids:
         return
 
-    pages = {
-        "contactus": util.ref(cr, "website.contactus_page"),
-        "contactus-thank-you": util.ref(cr, "website.contactus_thanks"),
-    }
+    pages = [
+        ("/contactus", "website.contactus_page", util.ref(cr, "website.contactus_page")),
+        ("/contactus-thank-you", "website.contactus_thanks", util.ref(cr, "website.contactus_thanks")),
+    ]
 
     for website_id, company_id in website_ids:
         partner = util.env(cr)["res.company"].browse(company_id).partner_id
 
-        for page in pages:
+        for url, key, page_id in pages:
             cr.execute(
-                "SELECT view_id FROM website_page WHERE url = %s AND website_id = %s",
-                (
-                    f"/{page}",
+                """
+                SELECT p.view_id,
+                       v.key
+                  FROM website_page p
+                  JOIN ir_ui_view v
+                    ON p.view_id = v.id
+                 WHERE p.url = %s
+                   AND p.website_id = %s
+                """,
+                [
+                    url,
                     website_id,
-                ),
+                ],
             )
+            view_id = next((vid for vid, vkey in cr.fetchall() if vkey == key), None)
             if not cr.rowcount:  # Avoid cloning the page if a specific view already exists
-                util.env(cr)["website.page"].clone_page(page_id=pages[page])
+                util.env(cr)["website.page"].clone_page(page_id=page_id)
                 # The url need to be reset to be sure it's correct. When calling the `clone_page` method,
                 # the new url may be renamed to be unique (with a `-x` sufix).
                 cr.execute(
@@ -49,19 +58,19 @@ def migrate(cr, version):
                         RETURNING view_id
                     """,
                     [
-                        f"/{page}",
+                        url,
                         website_id,
                     ],
                 )
-            view_id = cr.fetchone()[0]
+                view_id = cr.fetchone()[0]
 
             # As we backup the contactus page before (website/saas~14.5.1.0/pre-10-contactus.py), some fresh new
             # contactus pages are created before this file execution. So it's safe to update the <li> tags content
-            # according the company data.
+            # according the company data, if the key is the same. Otherwise we assume this is a truly custom view.
+            if not view_id:
+                continue
             with util.edit_view(cr, view_id=view_id) as arch:
                 nodes = arch.findall(".//li")
-                if not nodes:  # No <li> tags found, the page may remain custom
-                    continue
 
                 set_value(nodes[0], partner.name)
                 address = partner._display_address(without_company=True)
