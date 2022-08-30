cd17b2f81451a1529d812d7e8d300c0501552cb6
Nicolas Seinlet <nse@odoo.com>
2022-08-30 16:53:39 +0200

    [IMP] social_push_notif/saas~15.3

--- migrations/social_push_notifications/saas~15.3.1.1/post-migrate.py
+++ migrations/social_push_notifications/saas~15.3.1.1/post-migrate.py
@@ -9,9 +9,10 @@ def migrate(cr, version):
         INSERT INTO website_visitor_push_subscription(
             website_visitor_id, push_token
         )
-        SELECT id, push_token
+        SELECT min(id), push_token
           FROM website_visitor
          WHERE has_push_notifications = TRUE
+      GROUP BY push_token
     """
     )
     util.remove_field(cr, "website.visitor", "push_token")
--- migrations/social_push_notifications/saas~15.3.1.1/pre-migrate.py
+++ migrations/social_push_notifications/saas~15.3.1.1/pre-migrate.py
@@ -4,10 +4,16 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "website_visitor", "has_push_notifications", "bool")
-    cr.execute(
-        """
-        UPDATE website_visitor
-           SET has_push_notifications = TRUE
-         WHERE push_token IS NOT NULL
-    """
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            """
+            UPDATE website_visitor
+              SET has_push_notifications = TRUE
+            WHERE push_token IS NOT NULL
+              AND has_push_notifications = FALSE
+            """,
+            table="website_visitor",
+        ),
     )
