25b9169db733021330069a306f60282be7ff4d54
Nicolas Seinlet <nse@odoo.com>
2022-08-30 16:58:28 +0200

    [IMP] base/0.0.0: useless on some db's

--- migrations/base/0.0.0/pre-reindex-ir_model_data.py
+++ migrations/base/0.0.0/pre-reindex-ir_model_data.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
+
+from odoo.addons.base.maintenance.migrations import util
+
+
 def migrate(cr, version):
+    if util.str2bool(os.getenv("UPG_NO_REINDEX_IMD", "0")):
+        return False
     cr.execute("REINDEX TABLE ir_model_data")
