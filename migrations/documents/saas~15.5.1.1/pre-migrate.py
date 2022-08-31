86bd75ecbe3f553f2f856dee7120e7f6dd81af3b
William Braeckman <wbr@odoo.com>
2022-08-31 07:24:29 +0000

    [IMP] documents/saas~15.5: documents B2B
    Prefill a new computed field.
    
    See odoo/odoo#98389
    See odoo/enterprise#29807
    
    TaskId-2863861
    
    closes odoo/upgrade#3835
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/documents/saas~15.5.1.1/pre-migrate.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "documents_document", "thumbnail_status", "varchar")
+
+    cr.execute(
+        """
+        UPDATE documents_document doc
+           SET thumbnail_status = 'present'
+          FROM ir_attachment att
+         WHERE att.res_model = 'documents.document'
+           AND att.res_id = doc.id
+           AND att.res_field = 'thumbnail'
+        """
+    )
