35aab6cc2e2baf2b8459ecd301db911deef290bb
Eleftherios Kritsotakis <elkr@odoo.com>
2022-08-31 09:38:07 +0000

    [IMP] {pos_loyalty,point_of_sale}/saas~15.5: loyalty
    After this commit, the settings of coupons, promotions,
    gift cards and loyalty cards are now managed under a
    single one field in PoS Settings configuration. Moreover,
    this settings field is "global" and shared between the
    eCommerce and Sales app.
    
    See https://github.com/odoo/odoo/pull/99060
    
    task-2959620
    
    closes odoo/upgrade#3842
    
    Related: odoo/odoo#99060
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/point_of_sale/saas~15.5.1.0.1/pre-migrate.py
+++ migrations/point_of_sale/saas~15.5.1.0.1/pre-migrate.py
@@ -5,3 +5,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "pos.config", "barcode_nomenclature_id")
     util.remove_field(cr, "res.config.settings", "pos_barcode_nomenclature_id")
+    util.remove_field(cr, "pos.config", "module_pos_loyalty")
+    util.remove_field(cr, "res.config.settings", "pos_module_pos_loyalty")
---
+++ migrations/pos_loyalty/saas~15.5.1.0/pre-migrate.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.change_field_selection_values(cr, "pos.config", "gift_card_settings", {"scan_set": "scan_use"})
+
+    if not util.table_exists(cr, "loyalty_program_pos_config_rel"):
+        util.create_m2m(cr, "loyalty_program_pos_config_rel", "loyalty_program", "pos_config")
+
+    # Migrate `coupon_program_ids`, `promo_program_ids` -> properly link to pos_config_ids
+    tables = ["pos_config_coupon_program_rel", "pos_config_promo_program_rel"]
+    for m2m_relation_table in tables:
+        if util.table_exists(cr, m2m_relation_table):
+            cr.execute(
+                """
+                INSERT INTO loyalty_program_pos_config_rel (pos_config_id, loyalty_program_id)
+                     SELECT pos_config_id, loyalty_program_id
+                       FROM %s
+                ON CONFLICT DO NOTHING
+                """
+                % m2m_relation_table
+            )
+            cr.execute("DROP TABLE %s" % m2m_relation_table)
+
+    # Migrate `loyalty_program_id`, `gift_card_program_id`
+    for field_name in ["loyalty_program_id", "gift_card_program_id"]:
+        if util.column_exists(cr, "pos_config", field_name):
+            cr.execute(
+                """
+                INSERT INTO loyalty_program_pos_config_rel (pos_config_id, loyalty_program_id)
+                     SELECT id, %s
+                       FROM pos_config
+                      WHERE %s IS NOT NULL
+                ON CONFLICT DO NOTHING
+                """
+                % (field_name, field_name)
+            )
+
+    util.remove_field(cr, "pos.config", "use_coupon_programs")
+    util.remove_field(cr, "pos.config", "use_gift_card")
+    util.remove_field(cr, "pos.config", "coupon_program_ids", drop_column=False)
+    util.remove_field(cr, "pos.config", "promo_program_ids", drop_column=False)
+    util.remove_field(cr, "pos.config", "loyalty_program_id")
+    util.remove_field(cr, "pos.config", "gift_card_program_id")
+    util.remove_field(cr, "pos.config", "all_program_ids")
+    util.remove_field(cr, "res.config.settings", "pos_loyalty_program_id")
+    util.remove_field(cr, "res.config.settings", "pos_use_coupon_programs")
+    util.remove_field(cr, "res.config.settings", "pos_coupon_program_ids")
+    util.remove_field(cr, "res.config.settings", "pos_promo_program_ids")
+    util.remove_field(cr, "res.config.settings", "pos_use_gift_card")
+    util.remove_field(cr, "res.config.settings", "pos_gift_card_program_id")
