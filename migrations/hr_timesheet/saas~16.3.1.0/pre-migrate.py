4282ad98c3982f30c4ce6884ab794f3999490f45
kcv-odoo <kcv@odoo.com>
2022-09-02 06:43:30 +0000

    [IMP] timesheet_grid/saas~16.3: remove unused view
    This commit removes pivot view
    `timesheet_grid_pivot_view_weekly_validate` from timesheet
    (`account.analytic.line`) as we don't need a pivot
    we for last week validate menu which contains week as
    an interval.
    
    Community PR: https://github.com/odoo/odoo/pull/100141
    Enterprise PR:  https://github.com/odoo/enterprise/pull/31304
    
    task-2969791
    
    closes odoo/upgrade#3897
    
    Related: odoo/odoo#100141
    Related: odoo/enterprise#31304
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr_timesheet/saas~16.3.1.0/pre-migrate.py
+++ migrations/hr_timesheet/saas~16.3.1.0/pre-migrate.py
@@ -5,3 +5,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "project.project", "timesheet_count")
+    util.remove_record(cr, "hr_timesheet.act_hr_timesheet_line_view_pivot")
+    util.remove_record(cr, "hr_timesheet.act_hr_timesheet_line_view_graph")
--- migrations/timesheet_grid/saas~16.3.1.0/pre-migrate.py
+++ migrations/timesheet_grid/saas~16.3.1.0/pre-migrate.py
@@ -5,3 +5,6 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "account.analytic.line", "can_edit")
+    util.remove_view(cr, "timesheet_grid.timesheet_grid_pivot_view_weekly_validate")
+    util.remove_record(cr, "timesheet_grid.hr_timesheet_to_validate_action_pivot")
+    util.remove_record(cr, "timesheet_grid.hr_timesheet_to_validate_action_graph")
