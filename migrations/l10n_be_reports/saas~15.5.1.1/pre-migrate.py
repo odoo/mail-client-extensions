d7dae7fd2a79757b865f72f76fed404dda6f391c
Ricardo Gomes Rodrigues (rigr) <rigr@odoo.com>
2022-09-05 07:14:17 +0000

    [IMP] l10n_be_reports/saas~15.5.1.1: remove control lines
    This commit removes the control lines from the Balance Sheet which
    are no longer necessary.
    
    Entreprise PR: https://github.com/odoo/enterprise/pull/30799
    
    closes odoo/upgrade#3859
    
    Task-id: 2928459
    Related: odoo/enterprise#30799
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/l10n_be_reports/saas~15.5.1.1/pre-migrate.py
+++ migrations/l10n_be_reports/saas~15.5.1.1/pre-migrate.py
@@ -5,3 +5,8 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     util.remove_model(cr, "l10n.be.report.partner.vat.listing")
     util.remove_view(cr, "l10n_be_reports.line_caret_options")
+    for i in [0, 3, 4, 5, 8, 9, 10, 13, 14, 16, 17, 18, 29]:
+        util.remove_record(cr, f"l10n_be_reports.account_financial_report_be_{i}_control")
+        util.remove_record(cr, f"l10n_be_reports.account_financial_report_be_{i}_control_balance")
+        util.remove_record(cr, f"l10n_be_reports.account_financial_report_be_{i}_control_be_{i}_total_balance")
+    util.remove_record(cr, "l10n_be_reports.account_financial_report_be_off_sheet")
