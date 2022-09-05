7f96d4a1b52ad8d2f5c090a35284ecb64d1bde98
Dossogne Bertrand <bedo@odoo.com>
2022-09-05 13:00:28 +0000

    [IMP] {website_hr_recruitment,hr}/saas~15.5: move contract type
    Move the hr.contract.type model to hr so that it can be
    used in more places.
    
    Also set empty values to the new job_details field in
    website_hr_recruitment so that already set jobs
    doesn't get the default value.
    
    related PRs:
    odoo/odoo#96551
    odoo/enterprise#29777
    
    taskID 2898063
    
    closes odoo/upgrade#3861
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/hr/saas~15.5.1.1/pre-migrate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    eb = util.expand_braces
+    util.move_model(cr, "hr.contract.type", "hr_contract", "hr")
+    util.rename_xmlid(cr, *eb("hr{_contract,}.access_hr_contract_type_manager"))
--- migrations/website_hr_recruitment/saas~15.5.1.0/pre-migrate.py
+++ migrations/website_hr_recruitment/saas~15.5.1.0/pre-migrate.py
@@ -5,3 +5,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "website_hr_recruitment.job_edit_options")
     util.remove_view(cr, "website_hr_recruitment.user_navbar_inherit_website_hr_recruitment")
+
+    util.create_column(cr, "hr_job", "job_details", "text", default="")
