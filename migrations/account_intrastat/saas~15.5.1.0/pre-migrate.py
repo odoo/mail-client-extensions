f0a181d1958cad841408f0316299a9e67060bfd3
dbkosky <dako@odoo.com>
2022-09-09 14:58:04 +0000

    [IMP] account_intrastat/saas~15.5: supplementary units on products
    See https://github.com/odoo/enterprise/pull/30828
    
    In relation to a commit that adapts the way the supplementary units
    relate to products and intrastat fields on products are stored in
    general in 'account_intrastat', this commit:
    
    Removes fields on the product category, and removes their related views.
    
    Adapts fields on the product template and product variants. These
    adaptions represent the relocation of intrastat fields such that they
    exist exclusively on the product variant (rather than existing on the
    product variant, product template and product category). Data is moved
    from the template fields down to the variant fields when no value is
    present in the variant fields.
    
    closes odoo/upgrade#3886
    
    Task-id: 2884512
    Related: odoo/enterprise#30828
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account_intrastat/saas~15.5.1.0/pre-migrate.py
+++ migrations/account_intrastat/saas~15.5.1.0/pre-migrate.py
@@ -9,7 +9,10 @@ def migrate(cr, version):
     util.remove_view(cr, "account_intrastat.view_report_intrastat_code_expiry_tree")
     util.remove_view(cr, "account_intrastat.view_report_intrastat_code_expiry_form")
     util.remove_view(cr, "account_intrastat.invoice_form_inherit_account_intrastat_expiry")
+    util.remove_view(cr, "account_intrastat.account_intrastat_expiry_product_category_search_view")
     util.remove_view(cr, "account_intrastat.account_intrastat_expiry_product_template_search_view")
+    util.remove_view(cr, "account_intrastat.product_category_tree_view_account_intrastat_expiry")
+    util.remove_view(cr, "account_intrastat.product_category_form_view_inherit_account_intrastat")
     util.rename_xmlid(
         cr,
         "account_intrastat.product_product_tree_view_account_intrastat_expiry",
@@ -17,16 +20,33 @@ def migrate(cr, version):
     )
     util.rename_xmlid(
         cr,
-        "account_intrastat.account_intrastat_expiry_product_category_search_view",
-        "account_intrastat.account_intrastat_product_category_search_view",
-    )
-    util.rename_xmlid(
-        cr,
-        "account_intrastat.product_category_tree_view_account_intrastat_expiry",
-        "account_intrastat.product_category_tree_view_account_intrastat",
-    )
-    util.rename_xmlid(
-        cr,
         "account_intrastat.account_intrastat_expiry_main_template",
         "account_intrastat.account_intrastat_main_template",
     )
+
+    # Delete the product template field intrastat_id field, and assign its value to empty child product variants on
+    # the "intrastat_code_id" field. Update the origin country field on the variant with it's parent template's value.
+    util.create_column(cr, "product_product", "intrastat_code_id", "int4")
+    util.create_column(cr, "product_product", "intrastat_origin_country_id", "int4")
+
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            """
+            UPDATE product_product prod
+               SET intrastat_code_id = COALESCE(prod.intrastat_variant_id, prodt.intrastat_id),
+                   intrastat_origin_country_id = prodt.intrastat_origin_country_id
+              FROM product_template prodt
+             WHERE prod.product_tmpl_id = prodt.id
+            """,
+            table="product_product",
+            alias="prod",
+        ),
+    )
+
+    # Rename the intrastat code field on the product product
+    # Remove the commodity code fields on the product template and product category
+    util.remove_field(cr, "product.template", "intrastat_id")
+    util.remove_field(cr, "product.category", "intrastat_id")
+    util.remove_field(cr, "product.product", "intrastat_variant_id")
