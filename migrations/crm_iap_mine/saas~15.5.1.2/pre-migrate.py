76488fad77f8ba5c555e666446635227a0f75eb7
William Braeckman <wbr@odoo.com>
2022-09-14 08:25:19 +0000

    [IMP] crm_iap_mine/saas~15.5: migrate crm to wowl2
    Removes some views.
    
    See odoo/odoo#98837
    
    closes odoo/upgrade#3895
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ migrations/crm_iap_mine/saas~15.5.1.2/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "crm_iap_mine.crm_lead_view_kanban_lead")
+    util.remove_view(cr, "crm_iap_mine.crm_lead_view_kanban_opportunity")
