55f798171e2a69f89f4c752fec55a047d0d1acd4
Kevin Baptiste <kba@odoo.com>
2022-09-16 06:50:12 +0000

    [IMP] hr_recruitment/saas~15.5: transform interviewer_id into m2m
    Transform `interviewer_id` on `hr.applicant` into a many2many.
    
    odoo/odoo#100338
    task-2939388
    
    closes odoo/upgrade#3904
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/hr_recruitment/saas~15.5.1.0/pre-migrate.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_m2m(cr, "hr_applicant_res_users_interviewers_rel", "hr_applicant", "res_users")
+
+    util.update_record_from_xml(cr, "hr_recruitment.hr_applicant_interviewer_rule")
+    if util.column_exists(cr, "hr_applicant", "interviewer_id"):
+        cr.execute(
+            """
+                INSERT INTO hr_applicant_res_users_interviewers_rel
+                     SELECT id, interviewer_id
+                       FROM hr_applicant
+                      WHERE interviewer_id IS NOT NULL
+            """
+        )
+
+    util.remove_column(cr, "hr_applicant", "interviewer_id")
+    util.rename_field(cr, "hr.applicant", "interviewer_id", "interviewer_ids")
