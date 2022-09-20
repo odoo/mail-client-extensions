b7cf6dc380191f87bb6a45463d82c9fd06bef5ee
Pierre Paridans <app@odoo.com>
2022-09-20 19:22:40 +0000

    [ADD] web_enterprise/16.0.1.0: Remove webclient_bootstrap view
    Not needed anymore since community becomes responsive.
    
    Related odoo/odoo#100580
    Related odoo/enterprise#31511
    
    closes odoo/upgrade#3911
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/web_enterprise/16.0.1.0/pre-migrate.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        "UPDATE ir_ui_view SET inherit_id = %s WHERE inherit_id = %s",
+        [util.ref(cr, "web.webclient_bootstrap"), util.ref(cr, "web_enterprise.webclient_bootstrap")],
+    )
+    util.remove_view(cr, "web_enterprise.webclient_bootstrap")
