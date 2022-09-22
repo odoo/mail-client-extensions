6c839c991b846ea0d9113ac4822512227713d37f
Kamlesh Pathekar <kpt@odoo.com>
2022-09-22 04:58:10 +0000

    [IMP] planning/saas~16.4: remove view
    This commit remove unused view from planning and inherited view
    from project_timesheet_forecast module and rename their references.
    
    Related PR: https://github.com/odoo/enterprise/pull/33987
    
    task-2888248
    
    closes odoo/upgrade#4037
    
    Related: odoo/enterprise#33987
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/planning/saas~16.4.1.0/pre-migrate.py
+++ migrations/planning/saas~16.4.1.0/pre-migrate.py
@@ -6,3 +6,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     eb = util.expand_braces
     util.rename_xmlid(cr, *eb("planning.planning_view_search{,_base}"))
+    util.remove_record(cr, "planning.planning_action_my_planning_view_pivot")
+    util.remove_record(cr, "planning.planning_action_my_planning_view_graph")
