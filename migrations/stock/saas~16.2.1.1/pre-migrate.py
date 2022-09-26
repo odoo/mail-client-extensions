a2a66ed86f04c9c00d4889eb21e0aac0eda25d76
Mathias Mathy (MAMA) <mama@odoo.com>
2022-09-26 12:17:40 +0000

    [IMP] stock/16.2.1.1 report_stock_forecasted to owl stock_forecasted
    Refactoring of report_stock_forecasted to owl
    Since it wasn't really a report (no print action),
    it changed from a report to a client action.
    Task: 2885757
    See :
    odoo/odoo#101247
    
    closes odoo/upgrade#3926
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/stock/saas~16.2.1.1/pre-migrate.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "stock.report_product_template_replenishment")
+    util.remove_view(cr, "stock.report_product_product_replenishment")
+    util.remove_view(cr, "stock.report_replenishment_header")
+    util.remove_record(cr, "stock.stock_replenishment_report_product_product_action")
+    util.remove_record(cr, "stock.stock_replenishment_report_product_template_action")
+    util.remove_record(cr, "stock.stock_replenishment_product_product_action")
+    util.rename_model(
+        cr, "report.stock.report_product_product_replenishment", "stock.forecasted_product_product", False
+    )
+    util.rename_model(
+        cr, "report.stock.report_product_template_replenishment", "stock.forecasted_product_template", False
+    )
