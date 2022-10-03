03b7c2dd669240cafce337d7486fa2850d510a24
mth-odoo <mth@odoo.com>
2022-10-03 09:03:41 +0000

    [FIX] point_of_sale/saas~12.1.1.0.1: updated the employee pin from
    the user
    
    Before saas~12.1.1.0.1 pos_security_pin field was located in res_users table
    https://github.com/odoo/odoo/blob/saas-12/addons/point_of_sale/models/res_users.py#L10
    
    From saas~12.1.1.0.1 that field removed from res_users table
    https://github.com/odoo/upgrade/blob/c11a5d5415a7733112d1c1ce3aa4bdfb72ed66a7/migrations/point_of_sale/saas~12.1.1.0.1/pre-migrate.py#L7
    
    Introduced Pin field from the mentioned commit to change the functionality of
    Check In/Out in the Kiosk Mode of the Attendance application (if enabled in
    Configuration) and to change the cashier in the Point of Sale application
    https://github.com/odoo/odoo/pull/28567
    
    Before committing, the employee pin(hr_employee) field was empty so updated
    that field from the user(pos_security_pin)
    
    opw-2980970
    upg-383546
    
    closes odoo/upgrade#3939
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/point_of_sale/saas~12.1.1.0.1/pre-migrate.py
+++ migrations/point_of_sale/saas~12.1.1.0.1/pre-migrate.py
@@ -3,7 +3,21 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
+    if util.module_installed(cr, "hr"):
+        # force creation of the new field (see odoo/odoo#28567) in case the `hr` module hasn't been loaded yet.
+        util.create_column(cr, "hr_employee", "pin", "varchar")
+
+        cr.execute(
+            """
+            UPDATE hr_employee emp
+               SET pin = usr.pos_security_pin
+              FROM res_users usr
+             WHERE usr.id = emp.user_id
+               AND emp.pin IS NULL
+            """
+        )
+
     util.remove_field(cr, "pos.config", "barcode_scanner")
     util.remove_field(cr, "res.users", "pos_security_pin")
-    util.remove_view(cr, 'point_of_sale.res_users_view_form')
-    util.remove_view(cr, 'point_of_sale.pos_config_view_form')
+    util.remove_view(cr, "point_of_sale.res_users_view_form")
+    util.remove_view(cr, "point_of_sale.pos_config_view_form")
