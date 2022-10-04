2da32d26e956a8cbff0bae10cafd96196d55e3ee
Alvaro Fuentes <afu@odoo.com>
2022-10-04 15:30:15 +0000

    [chore] procurement/7.saas~5.1.0: blacked
    Part-of: odoo/upgrade#3950

--- migrations/procurement/7.saas~5.1.0/pre-30-procuremove.py
+++ migrations/procurement/7.saas~5.1.0/pre-30-procuremove.py
@@ -1,22 +1,26 @@
 from openerp.addons.base.maintenance.migrations import util
 
+
 def migrate(cr, version):
     """
-        1 Procurement method moved from procurements to stock moves
-        2 Change states from procurements
-        
-        If product of type service and mto, it should get auto_create_task
-    """
-    util.create_column(cr, 'stock_move', 'procure_method', 'varchar')
-    
-    cr.execute("""
-    UPDATE stock_move move
-    SET procure_method = p.procure_method
-    FROM procurement_order p WHERE p.move_id = move.id
-    """)
+    1 Procurement method moved from procurements to stock moves
+    2 Change states from procurements
 
-    if util.table_exists(cr, 'sale_order_line'):
-        cr.execute("""
+    If product of type service and mto, it should get auto_create_task
+    """
+    util.create_column(cr, "stock_move", "procure_method", "varchar")
+
+    cr.execute(
+        """
+        UPDATE stock_move move
+        SET procure_method = p.procure_method
+        FROM procurement_order p WHERE p.move_id = move.id
+        """
+    )
+
+    if util.table_exists(cr, "sale_order_line"):
+        cr.execute(
+            """
             ALTER TABLE sale_order_line
                 DROP CONSTRAINT sale_order_line_procurement_id_fkey;
             ALTER TABLE stock_warehouse_orderpoint
@@ -45,52 +49,63 @@ def migrate(cr, version):
                 FOREIGN KEY (procurement_id)
                 REFERENCES procurement_order(id)
                 ON DELETE SET NULL;
-            """)
+            """
+        )
     else:
         cr.execute("""delete from procurement_order where procure_method='make_to_stock'""")
-    
-    #Rename column move_dest_id
-    util.rename_field(cr, 'procurement.order', 'move_id', 'move_dest_id')
-    
-    # Change states of procurements: 
-    # draft, ready should be confirmed 
-    # waiting should pass to running 
-    cr.execute("""
+
+    # Rename column move_dest_id
+    util.rename_field(cr, "procurement.order", "move_id", "move_dest_id")
+
+    # Change states of procurements:
+    # draft, ready should be confirmed
+    # waiting should pass to running
+    cr.execute(
+        """
     UPDATE procurement_order po
     SET state = 'confirmed'
     WHERE po.state in ('draft', 'ready')
-    """)
-    cr.execute("""
+    """
+    )
+    cr.execute(
+        """
     UPDATE procurement_order po
     SET state = 'running'
     WHERE po.state = 'waiting'
-    """)
+    """
+    )
 
     # Migration of stock_location
-    if util.table_exists(cr, 'product_pulled_flow'):
-        moved = util.splitlines("""
+    if util.table_exists(cr, "product_pulled_flow"):
+        moved = util.splitlines(
+            """
         partner_address_id
         location_id
         location_src_id
         procure_method
         cancel_cascade
-    """)
+    """
+        )
         for m in moved:
-            util.move_field_to_module(cr, 'product.pulled.flow', m,'stock_location', 'stock')
-        util.move_field_to_module(cr, 'product.pulled.flow', 'invoice_state','stock_location', 'stock_account')
-        moved = util.splitlines("""
+            util.move_field_to_module(cr, "product.pulled.flow", m, "stock_location", "stock")
+        util.move_field_to_module(cr, "product.pulled.flow", "invoice_state", "stock_location", "stock_account")
+        moved = util.splitlines(
+            """
         name
         company_id
         type_proc
-    """)
+    """
+        )
         for m in moved:
-            util.move_field_to_module(cr, 'product.pulled.flow', m,'stock_location', 'procurement')
-        util.rename_model(cr, 'product.pulled.flow', 'procurement.rule', rename_table=True)
-        util.rename_field(cr, 'procurement.rule', 'cancel_cascade', 'propagate')
-        util.rename_field(cr, 'procurement.rule', 'type_proc', 'action')
+            util.move_field_to_module(cr, "product.pulled.flow", m, "stock_location", "procurement")
+        util.rename_model(cr, "product.pulled.flow", "procurement.rule", rename_table=True)
+        util.rename_field(cr, "procurement.rule", "cancel_cascade", "propagate")
+        util.rename_field(cr, "procurement.rule", "type_proc", "action")
 
-    util.create_column(cr, 'product_template', 'auto_create_task', 'boolean')
-    cr.execute("""
+    util.create_column(cr, "product_template", "auto_create_task", "boolean")
+    cr.execute(
+        """
     UPDATE product_template SET auto_create_task = 't'
     WHERE product_template.type = 'service' AND product_template.procure_method='make_to_order'
-    """)
+    """
+    )
