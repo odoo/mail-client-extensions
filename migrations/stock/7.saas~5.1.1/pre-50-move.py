b9d5d0bd1f01681b9245f59f8f05c055c8b390af
Alvaro Fuentes <afu@odoo.com>
2022-10-04 14:02:25 +0000

    [chore] stock/7.saas~5.1.1: blacked
    Part-of: odoo/upgrade#3950

--- migrations/stock/7.saas~5.1.1/pre-50-move.py
+++ migrations/stock/7.saas~5.1.1/pre-50-move.py
@@ -1,18 +1,20 @@
 from openerp.addons.base.maintenance.migrations import util
 
+
 def migrate(cr, version):
     """
-        As product_qty will become a function field, the old value should be in product_uom_qty
-        The lots of a stock move can still be used with restrict_lot_id
-        Inventory and stock moves are related with a one2many instead of many2many in v8
-        The invoice_state needs to be copied from the picking on the move
+    As product_qty will become a function field, the old value should be in product_uom_qty
+    The lots of a stock move can still be used with restrict_lot_id
+    Inventory and stock moves are related with a one2many instead of many2many in v8
+    The invoice_state needs to be copied from the picking on the move
     """
-    util.rename_field(cr, 'stock.move', 'product_qty', 'product_uom_qty')
-    util.rename_field(cr, 'stock.move', 'prodlot_id', 'restrict_lot_id')
+    util.rename_field(cr, "stock.move", "product_qty", "product_uom_qty")
+    util.rename_field(cr, "stock.move", "prodlot_id", "restrict_lot_id")
 
     # now bootstrap the new function field
-    util.create_column(cr, 'stock_move', 'product_qty', 'numeric')
-    cr.execute("""
+    util.create_column(cr, "stock_move", "product_qty", "numeric")
+    cr.execute(
+        """
         UPDATE  stock_move move
         SET     product_qty
                 = round(move.product_uom_qty * puom.factor / muom.factor,
@@ -25,23 +27,27 @@ def migrate(cr, version):
         AND     temp.id = prod.product_tmpl_id
         AND     muom.id = move.product_uom
         AND     puom.id = temp.uom_id;
-    """)
+    """
+    )
 
     # For inventories, we need to change the many2many to many2one for correcting the moves
-    util.rename_field(cr, 'stock.inventory.line', 'product_uom', 'product_uom_id')
-    util.create_column(cr, 'stock_move', 'inventory_id', 'int4')
-    cr.execute("""
-        UPDATE stock_move 
+    util.rename_field(cr, "stock.inventory.line", "product_uom", "product_uom_id")
+    util.create_column(cr, "stock_move", "inventory_id", "int4")
+    cr.execute(
+        """
+        UPDATE stock_move
         SET inventory_id = inv.id
         FROM stock_inventory inv, stock_inventory_move_rel invmove
         WHERE invmove.move_id = stock_move.id AND invmove.inventory_id = inv.id
-    """)
-    
+    """
+    )
 
-    util.create_column(cr, 'stock_move', 'invoice_state', 'varchar')
-    cr.execute("""
-        UPDATE stock_move 
-        SET invoice_state = pick.invoice_state 
+    util.create_column(cr, "stock_move", "invoice_state", "varchar")
+    cr.execute(
+        """
+        UPDATE stock_move
+        SET invoice_state = pick.invoice_state
         FROM stock_picking pick
         WHERE pick.id = stock_move.picking_id
-    """)
+        """
+    )
