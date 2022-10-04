e4f1b3a4328856b2dd4744855ce13bdf0900fc15
Alvaro Fuentes <afu@odoo.com>
2022-10-04 15:01:09 +0000

    [FIX] stock/7.saas~5.1.1: parallelize queries
    Part-of: odoo/upgrade#3950

--- migrations/stock/7.saas~5.1.1/pre-50-move.py
+++ migrations/stock/7.saas~5.1.1/pre-50-move.py
@@ -13,41 +13,59 @@ def migrate(cr, version):
 
     # now bootstrap the new function field
     util.create_column(cr, "stock_move", "product_qty", "numeric")
-    cr.execute(
-        """
-        UPDATE  stock_move move
-        SET     product_qty
-                = round(move.product_uom_qty * puom.factor / muom.factor,
-                        ceil(-log(puom.rounding))::integer)
-        FROM    product_product prod
-        ,       product_uom muom
-        ,       product_template temp
-        ,       product_uom puom
-        WHERE   prod.id = move.product_id
-        AND     temp.id = prod.product_tmpl_id
-        AND     muom.id = move.product_uom
-        AND     puom.id = temp.uom_id;
-    """
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            """
+            UPDATE  stock_move move
+            SET     product_qty
+                    = round(move.product_uom_qty * puom.factor / muom.factor,
+                            ceil(-log(puom.rounding))::integer)
+            FROM    product_product prod
+            ,       product_uom muom
+            ,       product_template temp
+            ,       product_uom puom
+            WHERE   prod.id = move.product_id
+            AND     temp.id = prod.product_tmpl_id
+            AND     muom.id = move.product_uom
+            AND     puom.id = temp.uom_id
+            """,
+            table="stock_move",
+            alias="move",
+        ),
     )
 
     # For inventories, we need to change the many2many to many2one for correcting the moves
     util.rename_field(cr, "stock.inventory.line", "product_uom", "product_uom_id")
     util.create_column(cr, "stock_move", "inventory_id", "int4")
-    cr.execute(
-        """
-        UPDATE stock_move
-        SET inventory_id = inv.id
-        FROM stock_inventory inv, stock_inventory_move_rel invmove
-        WHERE invmove.move_id = stock_move.id AND invmove.inventory_id = inv.id
-    """
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            """
+            UPDATE stock_move
+            SET inventory_id = inv.id
+            FROM stock_inventory inv, stock_inventory_move_rel invmove
+            WHERE invmove.move_id = stock_move.id AND invmove.inventory_id = inv.id
+            """,
+            table="stock_move",
+            alias="stock_move",
+        ),
     )
 
     util.create_column(cr, "stock_move", "invoice_state", "varchar")
-    cr.execute(
-        """
-        UPDATE stock_move
-        SET invoice_state = pick.invoice_state
-        FROM stock_picking pick
-        WHERE pick.id = stock_move.picking_id
-        """
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            """
+            UPDATE stock_move
+            SET invoice_state = pick.invoice_state
+            FROM stock_picking pick
+            WHERE pick.id = stock_move.picking_id
+            """,
+            table="stock_move",
+            alias="stock_move",
+        ),
     )
