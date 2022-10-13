e8148e886d1b9e57edc5b55e78b98bf7c196cb6b
shva-odoo <shva@odoo.com>
2022-10-13 07:18:50 +0000

    [chore] payment/9.saas~11: format file
    Part-of: odoo/upgrade#3963

--- migrations/payment/9.saas~11.1.0/pre-migrate.py
+++ migrations/payment/9.saas~11.1.0/pre-migrate.py
@@ -1,48 +1,58 @@
 # -*- coding: utf-8 -*-
-from lxml import etree
-from openerp.addons.base.maintenance.migrations import util
-from openerp.modules.module import get_resource_path
 import logging
+
+from lxml import etree
+
+from openerp.modules.module import get_resource_path
+
+from openerp.addons.base.maintenance.migrations import util
 
 _logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
-    util.create_column(cr, 'payment_acquirer', 'module_id', 'int4')
-    util.create_column(cr, 'payment_acquirer', 'description', 'text')
+    util.create_column(cr, "payment_acquirer", "module_id", "int4")
+    util.create_column(cr, "payment_acquirer", "description", "text")
 
     cr.execute("UPDATE payment_acquirer SET environment='test' WHERE environment != 'prod'")
 
     case_query = ""
     case_params = []
 
-    files = ['payment_acquirer_data.xml', 'payment_acquirer.xml']
+    files = ["payment_acquirer_data.xml", "payment_acquirer.xml"]
     for f in files:
-        file_path = get_resource_path('payment', 'data', f)  # return False if not found
+        file_path = get_resource_path("payment", "data", f)  # return False if not found
         if file_path:
             with open(file_path) as fp:
                 tree = etree.parse(fp)
                 for node in tree.xpath('//field[@name="description"]'):
-                    xid = node.getparent().get('id')
-                    acq = xid[len('payment_acquirer_'):]
+                    xid = node.getparent().get("id")
+                    acq = xid[len("payment_acquirer_") :]
 
-                    util.rename_xmlid(cr, '{0}.{1}'.format(acq, xid), 'payment.{0}'.format(xid))
+                    util.rename_xmlid(cr, "{0}.{1}".format(acq, xid), "payment.{0}".format(xid))
 
-                    desc = ''.join(map(etree.tostring, node.iterchildren()))
+                    desc = "".join(map(etree.tostring, node.iterchildren()))
                     case_query += "WHEN provider=%s THEN %s "
                     case_params.extend([acq, desc])
 
-    if not case_params: # file not found
-        _logger.warning('Cannot find file: %s' % ' or '.join(files))
+    if not case_params:  # file not found
+        _logger.warning("Cannot find file: %s" % " or ".join(files))
 
-    cr.execute("""
+    cr.execute(
+        """
         UPDATE payment_acquirer
            SET description = CASE {0} ELSE NULL END
-    """.format(case_query), case_params)
+    """.format(
+            case_query
+        ),
+        case_params,
+    )
 
-    cr.execute("""
+    cr.execute(
+        """
         UPDATE payment_acquirer a
            SET module_id = m.id
           FROM ir_module_module m
          WHERE m.name = 'payment_' || a.provider
-    """)
+    """
+    )
