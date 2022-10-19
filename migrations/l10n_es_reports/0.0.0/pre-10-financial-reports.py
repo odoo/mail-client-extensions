c99c6686ea36c4e371ba65b0dfcdbd568f7e92d8
diagnoza <owi@odoo.com>
2022-10-19 09:19:00 +0000

    [FIX] l10_es_reports/0.0.0: extend target
    Initial fix had a typo and hence there may be
    still dbs broken in 13.0.
    typo fix: https://github.com/odoo/upgrade/commit/72c7bfb3683efa821a9b910e5d3a06f9a47ec554
    
    closes odoo/upgrade#3972
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/l10n_es_reports/0.0.0/pre-10-financial-reports.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    # Records with XML ids mod_347_operations_real_estates_bought and mod_347_operations_regular_sold
+    # were swapped in 12.0
+    if util.version_between("saas~12.3", "14.0"):
+        for xmlid, codes in [
+            ("mod_347_operations_regular_bought", ("_mod_347_temp", "aeat_mod_347_operations_regular_bought")),
+            ("mod_347_operations_regular_sold", ("aeat_mod_347_operations_regular_sold",)),
+        ]:
+            cr.execute(
+                """
+                UPDATE ir_model_data
+                   SET res_id = (
+                        SELECT id
+                          FROM account_financial_html_report_line
+                         WHERE code in %s
+                        )
+                 WHERE module = 'l10n_es_reports' AND name = %s
+                """,
+                (codes, xmlid),
+            )
