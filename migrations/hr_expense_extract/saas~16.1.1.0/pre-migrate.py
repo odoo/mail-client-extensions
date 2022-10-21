35ec3e214a2dd4bbb4beeb21e752512f63e6b360
Louis Baudoux <lba@odoo.com>
2022-10-21 14:04:43 +0000

    [IMP] hr_expense_extract/saas~16.1: remove `hr.expense.extract.words`
    This model isn't used at all and can thus safely be removed.
    
    Enterprise PR: odoo/enterprise#33078
    
    closes odoo/upgrade#3984
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr_expense_extract/saas~16.1.1.0/pre-migrate.py
+++ migrations/hr_expense_extract/saas~16.1.1.0/pre-migrate.py
@@ -5,3 +5,6 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "hr.expense", "extract_can_show_resend_button")
+
+    util.remove_field(cr, "hr.expense", "extract_word_ids")
+    util.remove_model(cr, "hr.expense.extract.words")
