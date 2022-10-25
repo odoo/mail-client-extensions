efcb089c20213d0c02d501fb891cdffe7642df8a
Alvaro Fuentes <afu@odoo.com>
2022-10-25 12:58:40 +0000

    [FIX] stock/15.0: alter column quantity float8->numeric
    Due to odoo/odoo@0cf5c4e8 the column `quantity` in `sotck_quant` table
    changed from `float8` to `numeric`. Normally the ORM would handle this
    case on
    https://github.com/odoo/odoo/blob/ddf8f2c8c683f44e3dd9e4753276e6dada819bf2/odoo/tools/sql.py#L105-L117
    
    It seems that for borderline values of float8 the conversion to numeric
    is tricky.
    ```
    385065_afu_14.0=> select '5060342757999.006'::float8, '5060342757999.006'::float8::numeric, '5060342757999.006'::float8::text::numeric, '5060342757999.006'::numeric;
    +-------------------+------------------+-------------------+-------------------+
    | float8            | numeric          | numeric           | numeric           |
    |-------------------+------------------+-------------------+-------------------|
    | 5060342757999.006 | 5060342757999.01 | 5060342757999.006 | 5060342757999.006 |
    +-------------------+------------------+-------------------+-------------------+
    ```
    It's not even stable across versions, the above was in PG14, below in
    PG10
    ```
    postgres=> select '5060342757999.006'::float8, '5060342757999.006'::float8::numeric, '5060342757999.006'::float8::text::numeric, '5060342757999.006'::numeric;
    +------------------+------------------+------------------+-------------------+
    | float8           | numeric          | numeric          | numeric           |
    |------------------+------------------+------------------+-------------------|
    | 5060342757999.01 | 5060342757999.01 | 5060342757999.01 | 5060342757999.006 |
    +------------------+------------------+------------------+-------------------+
    ```
    
    From the examples above it seems that converting to `text` before
    converting to `numeric` keeps the expected output (first column equals
    third column in both cases).
    
    upg-385065
    
    closes odoo/upgrade#3990
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/stock/15.0.1.0/pre-migrate.py
+++ migrations/stock/15.0.1.0/pre-migrate.py
@@ -6,3 +6,8 @@ def migrate(cr, version):
     # https://github.com/odoo/odoo/commit/489e76ade600f7983a9a1976c04529ac542fdd44
     # This field was added in saas~14.3 but got to be a stored computed on 15.0
     util.create_column(cr, "stock_quant", "inventory_quantity_set", "bool")
+
+    # https://github.com/odoo/odoo/commit/0cf5c4e8241c46724defc0ea7e0d2c881ec2eeab
+    util.drop_depending_views(cr, "stock_quant", "quantity")
+    # converting float8 to numeric has some quirks, see commit introducing the change below
+    cr.execute("ALTER TABLE stock_quant ALTER COLUMN quantity TYPE numeric USING quantity::text::numeric")
