ee2032cef27f87a43e7d569cab98a845da8d28e3
Christophe Simonis <chs@odoo.com>
2022-10-31 14:40:34 +0100

    [REM] util
    Remove `util` package and files that have been extracted to the
    odoo/upgrade-util repository.

--- migrations/base/tests/__init__.py
+++ migrations/base/tests/__init__.py
@@ -1,16 +1,13 @@
 from . import (
     test_assets_management,
     test_attrs_views,
-    test_ensure_has_pk,
     test_fix_views,
     test_ir_act_server_conversion,
     test_manual_fields_start_with_x_,
     test_mock_crawl,
     test_module_state,
-    test_moved0,
     test_read_group_refactor,
     test_res_partner_bank,
     test_translate,
-    test_util,
     test_versions,
 )
