a27ea46e827b2d6d46cf35795e2398f831b7fc38
Rohit Kumar <roku@odoo.com>
2022-10-31 06:20:34 +0000

    [IMP] project/saas~16.1: rename some views/action xmlid
    Before this commit, the `My Tasks` action and its views linked
    were originally to display all tasks. Since those views and
    action have been changed to display `My Tasks` and a new
    menuitem for `All Tasks` will be added in the related PR.
    
    This commit rename the action and views related to `My Tasks`
    to directly know those views and action are for `My Tasks` menu
    and not for `All Tasks`.
    
    Related Community PR: https://github.com/odoo/odoo/pull/97669
    Related Enterprise PR: https://github.com/odoo/enterprise/pull/30195
    
    task-2917086
    
    closes odoo/upgrade#3893
    
    Related: odoo/enterprise#30195
    Related: odoo/odoo#97669
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/project/saas~16.1.1.2/pre-migrate.py
+++ migrations/project/saas~16.1.1.2/pre-migrate.py
@@ -5,3 +5,7 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "project.project_task_view_tree_activity")
+    util.rename_xmlid(cr, "project.action_view_all_task", "project.action_view_my_task")
+    util.rename_xmlid(cr, "project.open_view_all_task_list_kanban", "project.open_view_my_task_list_kanban")
+    util.rename_xmlid(cr, "project.open_view_all_task_list_tree", "project.open_view_my_task_list_tree")
+    util.rename_xmlid(cr, "project.open_view_all_task_list_calendar", "project.open_view_my_task_list_calendar")
--- migrations/project_enterprise/saas~16.1.1.0/pre-migrate.py
+++ migrations/project_enterprise/saas~16.1.1.0/pre-migrate.py
@@ -5,3 +5,6 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "project.task", "overlap_warning")
+    util.rename_xmlid(
+        cr, "project_enterprise.open_view_all_task_list_gantt", "project_enterprise.open_view_my_task_list_gantt"
+    )
