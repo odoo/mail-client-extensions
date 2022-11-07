e2fea5d9b1f83bd51657b38e1fb43572d2345c45
fdardenne <dafl@odoo.com>
2022-11-07 15:11:31 +0000

    [IMP] website/saas~16.1: view_hierarchy: QWeb views to client action
    Before this commit, `view_hierarchy` extends the QWeb view to see
    the hierarchy of views.
    
    With the evolution of the javascript framework, this use case has
    now more sense in a client action rather than an extension of a
    QWeb view.
    
    This commit refactor `view_hierarchy` into a client action.
    
    See odoo/odoo#103477
    
    closes odoo/upgrade#4016
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/website/saas~16.1.1.0/pre-migrate.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+
+    cr.execute("SELECT id FROM ir_act_window_view WHERE view_mode = 'qweb'")
+    act_window_view_ids = [r[0] for r in cr.fetchall()]
+    util.remove_records(cr, "ir.actions.act_window.view", act_window_view_ids)
+    util.remove_view(cr, "website.report_viewhierarchy_children")
+    util.remove_view(cr, "website.view_view_qweb")
