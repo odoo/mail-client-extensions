8b339646376c6bf620e082ec478d4eaa512843fc
Christophe Simonis <chs@odoo.com>
2022-11-14 14:27:41 +0000

    [FIX] test_mail/16.0
    Explicitly remove fields from mixin
    
    closes odoo/upgrade#4030
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/test_mail/16.0.1.0/pre-migrate.py
+++ migrations/test_mail/16.0.1.0/pre-migrate.py
@@ -4,3 +4,10 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.rename_field(cr, "mail.test.track.selection", "type", "selection_type")
+
+    # Remove fields from mixin
+    util.remove_field(cr, "mail.test.composer.mixin", "model_object_field")
+    util.remove_field(cr, "mail.test.composer.mixin", "sub_object")
+    util.remove_field(cr, "mail.test.composer.mixin", "sub_model_object_field")
+    util.remove_field(cr, "mail.test.composer.mixin", "null_value")
+    util.remove_field(cr, "mail.test.composer.mixin", "copyvalue")
