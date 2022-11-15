38db2a770a55965ef0839901375997067d54c968
Antoine (anso) <anso@odoo.com>
2022-11-15 13:44:40 +0000

    [FIX] mass_mailing_themes/saas~16.4: rename templates
    Template views to be renamed
    
    Tech -> Blogging
    VIP -> Coupon
    Bold -> Magazine
    Solar -> Promotion
    Newsletter -> Event
    
    See community odoo/odoo#99311
    
    Task-2714386
    
    closes odoo/upgrade#4035
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

---
+++ migrations/mass_mailing_themes/saas~16.4.1.2/pre-migrate.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    eb = util.expand_braces
+    util.rename_xmlid(cr, *eb("mass_mailing_themes.theme_{solar,promotion}_template"))
+    util.rename_xmlid(cr, *eb("mass_mailing_themes.theme_{bold,magazine}_template"))
+    util.rename_xmlid(cr, *eb("mass_mailing_themes.theme_{vip,coupon}_template"))
+    util.rename_xmlid(cr, *eb("mass_mailing_themes.theme_{tech,blogging}_template"))
+    util.rename_xmlid(cr, *eb("mass_mailing_themes.theme_{newsletter,event}_template"))
