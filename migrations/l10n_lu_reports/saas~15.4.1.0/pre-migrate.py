0227c17697e25c88fd832a9ddd491bf083cbf549
Alvaro Fuentes <afu@odoo.com>
2022-11-17 08:59:35 +0000

    [FIX] l10n_lu_reports/saas~15.4: module may be uninstalled
    When l10n_lu_reports_annual_vat is uninstalled (it is autoinstalled)
    the model l10n_lu_reports_annual_vat.report.appendix.expenditures
    is does not exist thus trying to rename it causes an error.
    ```
      File "/tmp/tmpx2cpfl8j/migrations/l10n_lu_reports/saas~15.4.1.0/pre-migrate.py", line 9, in migrate
        util.rename_model(
      File "/tmp/tmpx2cpfl8j/migrations/util/models.py", line 209, in rename_model
        cr.execute('ALTER TABLE "{0}" RENAME TO "{1}"'.format(old_table, new_table))
      File "/home/odoo/src/odoo/16.0/odoo/sql_db.py", line 315, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.UndefinedTable: relation "l10n_lu_reports_annual_vat_report_appendix_expenditures" does not exist
    ```
    ```
    417522_afu=> select id,name,latest_version,state,author,create_date,write_date from ir_module_module where name like 'l10n_lu_reports%'
    +-----+----------------------------+----------------+-------------+-----------+----------------------------+---------------------------+
    | id  | name                       | latest_version | state       | author    | create_date                | write_date                |
    |-----+----------------------------+----------------+-------------+-----------+----------------------------+---------------------------|
    | 155 | l10n_lu_reports            | 15.0.1.0       | installed   | Odoo S.A. | <null>                     | 2022-11-03 15:31:17.49619 |
    | 902 | l10n_lu_reports_annual_vat | <null>         | uninstalled | Odoo S.A. | 2022-03-28 17:00:41.049657 | 2022-11-03 15:31:17.49619 |
    +-----+----------------------------+----------------+-------------+-----------+----------------------------+---------------------------+
    ```
    
    upg-417522
    upg-417185
    tbg-383
    
    closes odoo/upgrade#4042
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/l10n_lu_reports/saas~15.4.1.0/pre-migrate.py
+++ migrations/l10n_lu_reports/saas~15.4.1.0/pre-migrate.py
@@ -6,9 +6,12 @@ def migrate(cr, version):
     util.remove_view(cr, "l10n_lu_reports.view_l10n_lu_generate_tax_report")
     util.remove_view(cr, "l10n_lu_reports.view_l10n_lu_yearly_tax_report_manual_export")
     util.remove_view(cr, "l10n_lu_reports.view_l10n_lu_yearly_tax_report_manual_extended")
-    util.rename_model(
-        cr, "l10n_lu_reports_annual_vat.report.appendix.expenditures", "l10n_lu_reports.report.appendix.expenditures"
-    )
+    if util.table_exists(cr, "l10n_lu_reports_annual_vat_report_appendix_expenditures"):
+        util.rename_model(
+            cr,
+            "l10n_lu_reports_annual_vat.report.appendix.expenditures",
+            "l10n_lu_reports.report.appendix.expenditures",
+        )
 
     util.remove_constraint(cr, "l10n_lu_yearly_tax_report_manual", "l10n_lu_yearly_tax_report_manual_year_unique")
     if not util.table_exists(cr, "l10n_lu_yearly_tax_report_manual_res_company_rel"):
