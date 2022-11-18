4546a39df2cbcb811abec3d0dd7617f06f4d23f8
Alvaro Fuentes <afu@odoo.com>
2022-11-18 13:42:18 +0000

    [FIX] base/16.0: convert translations of custom fields
    Adapt translated stored fields into JSONB. Otherwise custom fields fail
    in the crawler as soon they are used in a domain.
    
    Manual fields are special case because their column type is not changed
    to jsonb by the ORM, thus we need to do it before building the jsonb
    dict.
    
    Examples
    ```
     psycopg2.errors.UndefinedFunction: operator does not exist: character varying ->> unknown
    LINE 1: ..."x_rma_stage"."id" AS "id", "x_rma_stage"."x_name"->>'en_US'...
    ```
    ```
     psycopg2.errors.UndefinedFunction: operator does not exist: text ->> unknown
    LINE 1: ...products_count", "product_brand_ept"."description"->>'en_US'...
    ```
    
    closes odoo/upgrade#4054
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>
    Co-authored-by: Jinane Maksoud <maji@odoo.com>

--- migrations/base/16.0.1.3/end-migrate.py
+++ migrations/base/16.0.1.3/end-migrate.py
@@ -1,12 +1,77 @@
 # -*- coding: utf-8 -*-
+import collections
 import logging
 
 from odoo.modules.db import has_trigram, has_unaccent
+from odoo.tools.sql import convert_column_translatable
 from odoo.tools.translate import _get_translation_upgrade_queries
 
 from odoo.upgrade import util
 
 _logger = logging.getLogger("odoo.upgrade.16.0.1.3." + __name__)
+
+
+def migrate_custom_fields(cr, done, skip_models):
+    cr.execute(
+        """
+        SELECT model,
+               array_agg(name)
+          FROM ir_model_fields
+         WHERE store
+           AND translate
+           AND (model, name) NOT IN %s
+           AND model != ALL(%s)
+         GROUP BY model
+        """,
+        [tuple(done) or ("_", "_"), skip_models],
+    )
+    custom_fields = collections.defaultdict(list)
+    for model_name, field_names in cr.fetchall():
+        table = util.table_of_model(cr, model_name)
+        if not util.table_exists(cr, table):
+            continue
+        for field in field_names:
+            if util.column_type(cr, table, field) not in ("varchar", "text"):
+                continue
+            custom_fields[model_name].append(field)
+            if util.on_CI():
+                _logger.error("Attempting to modify translations for the standard field %s.%s", model_name, field)
+                continue
+            cr.execute(
+                f"""
+                ALTER TABLE "{table}" RENAME COLUMN "{field}" TO "{field}_upg";
+                ALTER TABLE "{table}" ADD COLUMN "{field}" jsonb
+                """
+            )
+            util.parallel_execute(
+                cr,
+                util.explode_query_range(
+                    cr,
+                    f"""
+                    UPDATE "{table}"
+                       SET "{field}" = jsonb_build_object('en_US', "{field}_upg")
+                     WHERE "{field}" IS NOT NULL
+                    """,
+                    table=table,
+                ),
+            )
+            cr.execute(f'ALTER TABLE "{table}" DROP COLUMN "{field}_upg" CASCADE')
+
+    msg = """
+    <details>
+        <summary>
+        In Odoo 16 translations are stored as JSON column. Please verify the upgraded translations for custom fields.
+        Since the custom code is not available during the upgrade some translations may be wrong, you should correct
+        them. Below the list of affected fields by model.
+        </summary>
+        <ul>
+            {}
+        </ul>
+    </details>
+    """.format(
+        "\n".join("<li>{}: {}</li>".format(model, ",".join(fields)) for model, fields in custom_fields.items())
+    )
+    util.add_to_migration_reports(msg, category="Translations", format="html")
 
 
 def migrate(cr, version):
@@ -54,15 +119,24 @@ def migrate(cr, version):
 
     # upgrade translations
     cleanup_queries = []
+    done = []
+    skip_models = []
     for Model in env.registry.values():
         if Model._auto and not Model._abstract:
             for field in Model._fields.values():
                 if field.store and field.translate:
+                    done.append((Model._name, field.name))
+                    if field.manual:
+                        convert_column_translatable(cr, Model._table, field.name, "jsonb")
                     migrate, cleanup = _get_translation_upgrade_queries(cr, field)
                     # don't parallelize data migration queries from different
                     # fields, as it may cause serialization errors
                     util.parallel_execute(cr, migrate)
                     cleanup_queries.extend(cleanup)
+        else:
+            skip_models.append(Model._name)
+
+    migrate_custom_fields(cr, done, skip_models)
 
     # remove translations; leftovers should be migrated manually
     cleanup_queries.append("DELETE FROM _ir_translation WHERE type = 'code'")
