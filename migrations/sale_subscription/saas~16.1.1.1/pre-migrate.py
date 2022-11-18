7c824b20d2caee788a7880f7e66746178ec492ab
Arnaud Joset <arj@odoo.com>
2022-11-18 09:52:50 +0000

    [IMP] sale_subscription/saas~16.1: empty origin_order_id of 1st contract
    Before this commit, origin_order_id of the first contract of a sequence is
    equal to the id of the contract. It prevent looking for new contract easily
    in reports.
    
    This PR make sure that the origin_order_id of the first contract is NULL.
    
    closes odoo/upgrade#4049
    
    Enterprise: https://github.com/odoo/enterprise/pull/33520
    Taskid: 3048181
    Related: odoo/enterprise#33520
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/sale_subscription/saas~16.1.1.1/pre-migrate.py
+++ migrations/sale_subscription/saas~16.1.1.1/pre-migrate.py
@@ -6,3 +6,14 @@ from odoo.upgrade import util
 def migrate(cr, version):
     cr.execute("DROP VIEW IF EXISTS sale_subscription_report CASCADE")
     util.change_field_selection_values(cr, "sale.order.alert", "action", {"email": "mail_post"})
+
+    # First order of a sequence should have an empty origin_order_id
+    # Duplicated orders should not share the same origin_order_id if they are not renew or upsell
+    query = """
+            UPDATE sale_order so
+            SET origin_order_id=null
+            WHERE so.is_subscription = true
+              AND so.subscription_management NOT IN ('renew', 'upsell')
+               OR so.subscription_management IS NULL
+            """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="sale_order", alias="so"))
