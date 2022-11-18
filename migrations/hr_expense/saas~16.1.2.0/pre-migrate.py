5b9560cc0ae1b13c275f50d53ad4bb638b0da5f5
Thibault Delavallée <tde@odoo.com>
2022-11-18 08:10:22 +0000

    [IMP] hr_expense/saas~16.1: rename alias configuration fields
    Better namespace expense configuration fields about aliases and mail gateway
    usage to lessen potential conflicts.
    
    See odoo/odoo#105992 as well as odoo/enterprise#34089 (not requiring migration).
    
    closes odoo/upgrade#4046
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

---
+++ migrations/hr_expense/saas~16.1.2.0/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.rename_field(cr, "res.config.settings", "use_mailgateway", "hr_expense_use_mailgateway")
+    util.rename_field(cr, "res.config.settings", "expense_alias_prefix", "hr_expense_alias_prefix")
