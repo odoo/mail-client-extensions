2164fe62f7d28d8d561adc75817dcbe7f7cf2c1e
Adesh Jolhe <adjo@odoo.com>
2022-11-21 09:22:40 +0000

    [IMP] base/saas~16.5: remove useless bridge module
    This commit removes sale_timesheet_account_budget module and move it's
    related code to project_account_budget module.
    
    See https://github.com/odoo/enterprise/pull/29467
    
    Task-2903637
    
    closes odoo/upgrade#4095
    
    Related: odoo/odoo#95971
    Related: odoo/enterprise#29467
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/saas~16.5.1.3/pre-10-modules.py
+++ migrations/base/saas~16.5.1.3/pre-10-modules.py
@@ -6,3 +6,5 @@ def migrate(cr, version):
     util.merge_module(cr, "l10n_ke_hr_payroll_bik", "l10n_ke_hr_payroll")
     util.remove_module(cr, "l10n_ae_pos")
     util.merge_module(cr, "loyalty_delivery", "sale_loyalty_delivery")
+    if util.has_enterprise():
+        util.merge_module(cr, "sale_timesheet_account_budget", "project_account_budget")
