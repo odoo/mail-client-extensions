8c8536bc803e0f49e77844c09a85ddf5930c3bc0
Leonardo Pavan Rocha <lpr@odoo.com>
2022-11-24 18:09:40 +0000

    [IMP] base/saas~16.1: remove association module
    This commit removes the association module from odoo, as it was only
    overwriting the main menu item of the membership app, adding a group to it.
    
    See https://github.com/odoo/odoo/pull/106472.
    
    task-3080120
    
    closes odoo/upgrade#4075
    
    Related: odoo/odoo#106472
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/saas~16.1.1.3/pre-10-modules.py
+++ migrations/base/saas~16.1.1.3/pre-10-modules.py
@@ -6,6 +6,7 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_module(cr, "pos_cache")
     util.remove_module(cr, "spreadsheet_dashboard_sale_expense")
+    util.remove_module(cr, "association")
 
     if util.has_enterprise():
         util.remove_module(cr, "project_timesheet_forecast_contract")
