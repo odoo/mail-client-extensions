039f96cc5569a3c371cac4a070126776285f3286
Jinane Maksoud <maji@odoo.com>
2022-11-28 15:55:51 +0000

    [FIX] base/0.0.0: cleanup dashboard view modes
    Many models do not have any dashboard type views anymore
    but they are still referenced in action windows if they
    are custom or noupdate.
    
    closes odoo/upgrade#4087
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/0.0.0/end-clean-menu-actions.py
+++ migrations/base/0.0.0/end-clean-menu-actions.py
@@ -14,3 +14,23 @@ def migrate(cr, version):
          )
     """
     )
+
+    # remove dashboard view_mode from action windows
+    # if no views of the same type remains
+    cr.execute(
+        """
+         WITH actions AS (
+          SELECT w.id
+            FROM ir_act_window w
+       LEFT JOIN ir_ui_view v
+              ON v.model = w.res_model
+             AND v.type = 'dashboard'
+           WHERE w.view_mode LIKE '%dashboard%'
+             AND v.id IS NULL
+           )
+       UPDATE ir_act_window w
+          SET view_mode = ARRAY_TO_STRING(ARRAY_REMOVE(STRING_TO_ARRAY(w.view_mode, ','), 'dashboard'), ',')
+         FROM actions a
+        WHERE a.id = w.id
+    """
+    )
