9118d718f501722339198cc7aced88719acca271
Ayman Adil <ayad@odoo.com>
2022-11-28 16:18:31 +0000

    [FIX] stock/16.0.1.1: pre-create column to avoid memory error
    Field `stock_move.quantity_done` became stored in v16 which raises a
    memory error.
    
    A solution is to pre-create the corresponding column.
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/16.0/odoo/service/server.py", line 1289, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "<decorator-gen-14>", line 2, in new
      File "/home/odoo/src/odoo/16.0/odoo/tools/func.py", line 87, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/16.0/odoo/modules/registry.py", line 91, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 478, in load_modules
        processed_modules += load_marked_modules(cr, graph,
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 371, in load_marked_modules
        loaded, processed = load_module_graph(
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 207, in load_module_graph
        registry.init_models(cr, model_names, {'module': package.name}, new_install)
      File "/home/odoo/src/odoo/16.0/odoo/modules/registry.py", line 465, in init_models
        func()
      File "/home/odoo/src/odoo/16.0/odoo/addons/base/models/ir_model.py", line 1720, in _reflect_relation
        self.env.invalidate_all()
      File "/home/odoo/src/odoo/16.0/odoo/api.py", line 722, in invalidate_all
        self.flush_all()
      File "/home/odoo/src/odoo/16.0/odoo/api.py", line 734, in flush_all
        self[model_name].flush_model()
      File "/home/odoo/src/odoo/16.0/odoo/models.py", line 5541, in flush_model
        self._flush(fnames)
      File "/home/odoo/src/odoo/16.0/odoo/models.py", line 5613, in _flush
        id_vals[record.id][field.name] = value
    MemoryError
    ```
    
    upg-419892
    
    closes odoo/upgrade#4079
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/stock/16.0.1.1/pre-migrate.py
+++ migrations/stock/16.0.1.1/pre-migrate.py
@@ -35,3 +35,5 @@ def migrate(cr, version):
     util.remove_record(cr, "stock.report_stock_quantity_action_product")
 
     util.remove_field(cr, "res.config.settings", "stock_mail_confirmation_template_id")
+
+    util.create_column(cr, "stock_move", "quantity_done", "float8")
