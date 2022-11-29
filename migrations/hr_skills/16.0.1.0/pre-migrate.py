85ec647678d8a6f80b8a9549ed442a60562bbb38
Christophe Simonis <chs@odoo.com>
2022-11-29 13:47:58 +0100

    [ADD] hr{_recruitment,}_skills/16.0: create idiotic m2m
    See odoo/odoo#93684

---
+++ migrations/hr_skills/16.0.1.0/pre-migrate.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_m2m(cr, "hr_employee_hr_skill_rel", "hr_employee", "hr_skill")
+    # yes, this m2m is idiotic ¯\_(ツ)_/¯
+    cr.execute(
+        """
+            INSERT into hr_employee_hr_skill_rel(hr_employee_id, hr_skill_id)
+            SELECT DISTINCT employee_id, skill_id
+              FROM hr_employee_skill
+        """
+    )
