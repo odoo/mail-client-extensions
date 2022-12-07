3cf78063e409f0bb221d3b8b0a46324aa1086b44
clesgow <quwo@odoo.com>
2022-12-07 14:33:24 +0000

    [IMP] purchase_stock/saas~16.1: Allow multiple purchase lines per move
    With the introduction of alternative Purchase Orders, having only a
    single Purchase Order Line linked to a move wouldn't keep track of the
    original moves that generated the Purchase Order once an alternative was
    created for it.
    
    In order to do that, it was necessary to transform the
    `created_purchase_line_id` m2o into a `created_purchase_line_ids` m2m.
    
    See odoo/odoo#106411
    
    closes odoo/upgrade#4116
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/purchase_stock/saas~16.1.1.2/pre-migrate.py
+++ migrations/purchase_stock/saas~16.1.1.2/pre-migrate.py
@@ -5,3 +5,21 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "purchase.report", "avg_receipt_delay")
+    util.create_m2m(
+        cr,
+        "stock_move_created_purchase_line_rel",
+        "stock_move",
+        "purchase_order_line",
+        "move_id",
+        "created_purchase_line_id",
+    )
+    cr.execute(
+        """
+        INSERT INTO stock_move_created_purchase_line_rel (move_id,created_purchase_line_id)
+             SELECT id, created_purchase_line_id
+               FROM stock_move
+              WHERE created_purchase_line_id IS NOT NULL
+    """
+    )
+    util.adapt_domains(cr, "stock.move", "created_purchase_line_id", "created_purchase_line_ids")
+    util.remove_field(cr, "stock.move", "created_purchase_line_id")
