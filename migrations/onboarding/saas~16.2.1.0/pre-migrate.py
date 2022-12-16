c665ed04a243528ed3399bb69c5ea536ec6e097f
Florian Charlier <flch@odoo.com>
2022-12-16 14:09:16 +0000

    [IMP] onboarding/saas~16.2: remove extra progress records
    It sometimes occurred from v16.0 that multiple
    onboarding_progress(_steps) records without set company for the same
    onboarding were created despite our measures in place to avoid that.
    
    This commit accompanies FIX commits in the onboarding module to prevent
    further occurrence of this issue.
    
    Task-3101666
    See odoo/odoo#108183 and odoo/odoo#109221
    
    closes odoo/upgrade#4142
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

---
+++ migrations/onboarding/saas~16.2.1.0/pre-migrate.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    # Remove extra onboarding progress (step) records without company
+    # created for the same onboarding.
+    cr.execute(
+        """
+        DELETE FROM onboarding_progress
+              WHERE company_id IS NULL
+                AND id NOT IN (
+                       SELECT max(id)
+                         FROM onboarding_progress
+                        WHERE company_id IS NULL
+                     GROUP BY onboarding_id
+                )
+        """
+    )
+    # Replaced with unique index handling NULL as identical values
+    util.remove_constraint(cr, "onboarding_progress", "onboarding_company_uniq")
