257425d0924fac64c17d78e406c4eaaf1a4e98e6
Alvaro Fuentes <afu@odoo.com>
2022-12-16 10:45:35 +0000

    [FIX] base/0.0.0: discard translated with null lang
    When upgrading translations to the new jsonb system we get an error for
    entries with `state=translated` and null `lang`.
    ```
                WITH t AS (
                    SELECT res_id, jsonb_object_agg(lang, value) AS value
                      FROM _ir_translation
                     WHERE type = 'model' AND name = 'product.template,name' AND state = 'translated'
                  GROUP BY res_id
                )
                UPDATE product_template m
                   SET "name" =  t.value || m."name"
                  FROM t
                 WHERE t.res_id = m.id
    
    ERROR: field name must not be null
    ```
    It seems there is not valid flow (other than bugs or custom code) that
    leads to this state. Moreover those terms are not taken into account for
    translations anwyay since the language is missing.
    
    upg-421998
    
    closes odoo/upgrade#4164
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/0.0.0/pre-clean-translations.py
+++ migrations/base/0.0.0/pre-clean-translations.py
@@ -6,6 +6,11 @@ def migrate(cr, version):
     if not util.table_exists(cr, "ir_translation"):
         return
 
-    cr.execute("DELETE FROM ir_translation WHERE value = src AND type IS DISTINCT FROM 'model'")
-    cr.execute("DELETE FROM ir_translation WHERE value IS NULL")
-    cr.execute("DELETE FROM ir_translation WHERE value = ''")
+    cr.execute(
+        """
+        DELETE FROM ir_translation
+         WHERE lang IS NULL
+            OR COALESCE(value, '') = ''
+            OR (value = src AND type IS DISTINCT FROM 'model')
+        """
+    )
