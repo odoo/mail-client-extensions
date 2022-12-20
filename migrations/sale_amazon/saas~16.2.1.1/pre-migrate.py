78fb81a4e6634ced7500ae836f36be4dfa475181
Demesmaeker <edm@odoo.com>
2022-12-20 10:21:48 +0000

    [IMP] sale_amazon/saas~16.2: track synchronization status
    See odoo/enterprise#28970
    See odoo/documentation#3193
    
    closes odoo/upgrade#4146
    
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- migrations/sale_amazon/saas~16.2.1.1/pre-migrate.py
+++ migrations/sale_amazon/saas~16.2.1.1/pre-migrate.py
@@ -6,3 +6,12 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "crm.team", "amazon_team")
     util.remove_field(cr, "stock.location", "amazon_location")
+
+    util.create_column(cr, "stock_picking", "amazon_sync_status", "varchar")
+    query = """
+        UPDATE stock_picking
+           SET amazon_sync_status = 'pending'
+         WHERE amazon_sync_pending = TRUE
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="stock_picking"))
+    util.remove_field(cr, "stock.picking", "amazon_sync_pending")
