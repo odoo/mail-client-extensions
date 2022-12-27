215edb7998e36fec1aa6ab88e14992035d4c5f9e
Horacio Tellez <hote@odoo.com>
2022-12-27 15:15:45 +0000

    [FIX] sale/16.0.1.2: mail templates correction
    [FIX] website_payment/16.0.1.0: mail templates correction
    
    After the change of `acquirer` on `provider` and all other
    changes it implied the templates where not correctly adapted:
        - the term 'acquirer' was still present;
        - the field 'provider' instead of 'provider_code' for transactions
          was still present.
    After this commit the templates will function as wanted.
    
    See odoo/odoo#90899
    
    closes odoo/upgrade#4159
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/sale/16.0.1.2/post-migrate.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import re
+
+
+def migrate(cr, version):
+    changes = [
+        ("acquirer_id", "provider_id"),
+        ("tx.provider", "tx.provider_code"),
+    ]
+    for old, new in changes:
+        re_old = rf"\y{re.escape(old)}\y"
+        match = f'exists($.* ? (@ like_regex "{re_old}"))'
+        cr.execute(
+            """
+WITH upd AS (
+     SELECT t.id,
+            jsonb_object_agg(v.key, regexp_replace(v.value, %s, %s, 'g')) AS body_html
+       FROM mail_template t
+       JOIN LATERAL jsonb_each_text(t.body_html) v
+         ON true
+      WHERE t.model = 'sale.order'
+        AND jsonb_path_match(t.body_html, %s)
+      GROUP BY t.id
+)
+UPDATE mail_template orig
+   SET body_html = upd.body_html
+  FROM upd
+ WHERE upd.id=orig.id
+""",
+            [re_old, new, match],
+        )
---
+++ migrations/website_payment/16.0.1.0/post-migrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.if_unchanged(cr, "website_payment.donation_mail_body", util.update_record_from_xml)
