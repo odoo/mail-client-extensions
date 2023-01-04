5ad722ae9fcae8dba7216c99953d69a195611a9c
ノウラ <nea@odoo.com>
2023-01-04 12:39:24 +0000

    [FIX] helpdesk/saas~16.1: prevent having duplicated record rule
    Associated enterprise PR prevents the record rule Team: multi-company
    from being duplicated.
    
    Related PR:
    - odoo/enterprise#35419
    
    opw-3096957
    
    closes odoo/upgrade#4174
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/helpdesk/saas~16.1.1.4/pre-migrate.py
+++ migrations/helpdesk/saas~16.1.1.4/pre-migrate.py
@@ -5,3 +5,13 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.update_field_references(cr, "email", "partner_email", only_models=("helpdesk.ticket",))
     util.remove_field(cr, "helpdesk.ticket", "email")
+
+    util.ensure_xmlid_match_record(
+        cr,
+        "helpdesk.helpdesk_team_company_rule",
+        "ir.rule",
+        {
+            "name": "Team: multi-company",
+            "model_id": util.ref(cr, "helpdesk.model_helpdesk_team"),
+        },
+    )
