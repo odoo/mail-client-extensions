cd13979af6ccd7810b7333ae2f4ef474b8250708
diagnoza <owi@odoo.com>
2023-01-09 22:54:35 +0000

    [FIX] l10n_mx_edi/saas~13.5.0.2: link folios after payment-pocalypse
    Links missing folios after account_payment refactoring.
    
    closes odoo/upgrade#4194
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/l10n_mx_edi/saas~13.5.0.2/pre-05-edi-documents.py
+++ migrations/l10n_mx_edi/saas~13.5.0.2/pre-05-edi-documents.py
@@ -53,3 +53,34 @@ def migrate(cr, version):
         """,
         [mx_edi_format_id],
     )
+
+    # Link from payments
+    cr.execute(
+        """
+        WITH cfdi_per_payment AS (
+            SELECT
+                move.id AS move_id,
+                attachment.id AS attachment_id,
+                ROW_NUMBER() OVER(PARTITION BY pay.id ORDER BY attachment.create_date DESC, attachment.id DESC) AS rank
+            FROM account_payment pay
+            JOIN ir_attachment attachment ON
+                attachment.res_model = 'account.payment'
+                AND
+                attachment.res_id = pay.id
+            JOIN account_move move ON
+                pay.move_id = move.id
+                AND
+                attachment.company_id = move.company_id
+           WHERE move.state = 'posted'
+        )
+        INSERT INTO account_edi_document (edi_format_id, move_id, state, attachment_id)
+        SELECT
+            %s,
+            cfdi_per_payment.move_id,
+            'sent',
+            cfdi_per_payment.attachment_id
+        FROM cfdi_per_payment
+        WHERE cfdi_per_payment.rank = 1
+        """,
+        [mx_edi_format_id],
+    )
