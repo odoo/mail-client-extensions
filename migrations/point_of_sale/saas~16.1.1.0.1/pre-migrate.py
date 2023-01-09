423c21a14c7e4e110d90a77a330495921d90422d
Adrien Guilliams (adgu) <adgu@odoo.com>
2023-01-09 16:44:06 +0000

    [IMP] point_of_sale/saas~16.1.1.0: Add shipping_date, remove to_ship
    The PR of the community version adds a date picker in the pos config
    so that the users can choose a date when they enable the options "ship later"
    for an order. It also display the delivery date on the receipt if there is one.
    The field to_ship is thus not needed anymore since the new field shipping_date
    has all the necessary information.
    
    See odoo/odoo#107115
    
    closes odoo/upgrade#4200
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/point_of_sale/saas~16.1.1.0.1/pre-migrate.py
+++ migrations/point_of_sale/saas~16.1.1.0.1/pre-migrate.py
@@ -4,3 +4,23 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.rename_field(cr, "pos.config", "number_of_opened_session", "number_of_rescue_session")
+    util.create_column(cr, "pos_order", "shipping_date", "date")
+    query = """
+        UPDATE pos_order
+           SET shipping_date = stock_picking.scheduled_date
+          FROM stock_picking
+         WHERE pos_order.id = stock_picking.pos_order_id
+           AND stock_picking.scheduled_date IS NOT null
+           AND pos_order.to_ship = true
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="pos_order"))
+
+    def adapter(leaf, _o, _n):
+        _, op, right = leaf
+        if op == "!=":
+            right = not right
+        new_op = "!=" if right else "="
+        return [("shipping_date", new_op, False)]
+
+    util.adapt_domains(cr, "pos.order", "to_ship", "shipping_date", adapter=adapter)
+    util.remove_field(cr, "pos.order", "to_ship")
