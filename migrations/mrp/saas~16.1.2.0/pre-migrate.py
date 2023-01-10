740d710a94ed76952dacea3b40550977c13fe891
JF Aubert <ajf@odoo.com>
2023-01-10 09:54:00 +0000

    [FIX] mrp/saas~16.1: Improvements
    remove mrp.immediate.production model
    
    task: 3094741
    
    see odoo/odoo#108446
    
    closes odoo/upgrade#4163
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/mrp/saas~16.1.2.0/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+
+    util.remove_model(cr, "mrp.immediate.production")
+    util.remove_model(cr, "mrp.immediate.production.line")
