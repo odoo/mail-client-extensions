89dbf8417eca483c2494c4f0fa8e0a3596020803
Arnold Moyaux <arm@odoo.com>
2023-01-11 16:57:09 +0000

    [FIX] purchase/16.0: do not remove price diff accounts
    Community PR odoo/odoo#108513
    
    We removed the price difference account fields on products and category
    following a functional mistake (explained in community PR).
    So we have to reintroduce them and keep them in the fix module
    during migration
    
    for 16.0. In 16.1 they will be put and migrate back to purchase_stock.
    
    Part-of: odoo/upgrade#4315

--- migrations/purchase/16.0.1.2/pre-migrate.py
+++ migrations/purchase/16.0.1.2/pre-migrate.py
@@ -9,8 +9,6 @@ def migrate(cr, version):
     util.rename_field(cr, "purchase.order", *eb("tax_totals{_json,}"))
     util.remove_view(cr, "purchase.product_template_form_view")
     util.remove_view(cr, "purchase.view_category_property_form")
-    util.remove_field(cr, "product.template", "property_account_creditor_price_difference")
-    util.remove_field(cr, "product.category", "property_account_creditor_price_difference_categ")
 
     upgrade_analytic_distribution(
         cr,
@@ -18,3 +16,17 @@ def migrate(cr, version):
         account_field="account_analytic_id",
     )
     util.remove_field(cr, "purchase.report", "account_analytic_id")
+    if util.module_installed(cr, "purchase_stock"):
+        util.move_field_to_module(
+            cr,
+            "product.category",
+            "property_account_creditor_price_difference_categ",
+            "purchase",
+            "purchase_price_diff",
+        )
+        util.move_field_to_module(
+            cr, "product.template", "property_account_creditor_price_difference", "purchase", "purchase_price_diff"
+        )
+    else:
+        util.remove_field(cr, "product.category", "property_account_creditor_price_difference_categ")
+        util.remove_field(cr, "product.template", "property_account_creditor_price_difference")
