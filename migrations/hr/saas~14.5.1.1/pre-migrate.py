060afb3751e16d450893a2dcb234f619c625088b
Aniss Grabsi (angr) <angr@odoo.com>
2023-01-13 17:26:40 +0000

    [FIX] hr/saas~14.5.1.1: custom/leftover constraint
    Some customers have a 'hr_job_hired_employee_check'
    constraint breaking the upgrade.
    that could either be leftover from Odoo 8 or custom.
    
    The UPDATE of the new `sequence` column in this script
    breaks the upgrade if the leftover constraint is present.
    
    upg-449779
    upg-438970
    
    closes odoo/upgrade#4229
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/hr/saas~14.5.1.1/pre-migrate.py
+++ migrations/hr/saas~14.5.1.1/pre-migrate.py
@@ -6,6 +6,10 @@ def migrate(cr, version):
     util.move_field_to_module(cr, "hr.employee", "id_card", "l10n_be_hr_contract_salary", "hr")
     util.move_field_to_module(cr, "hr.employee", "driving_license", "l10n_be_hr_contract_salary", "hr")
 
+    # some customers have a 'hr_job_hired_employee_check' constraint breaking the upgrade
+    # that could either be leftover from Odoo 8 or custom.
+    util.remove_constraint(cr, "hr_job", "hr_job_hired_employee_check")
+
     util.create_column(cr, "hr_job", "sequence", "int4", default=10)
     cr.execute(
         """
