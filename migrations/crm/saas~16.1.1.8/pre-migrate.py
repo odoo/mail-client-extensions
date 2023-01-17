cd368b61b91b20302e7894b4449c8dda39cd5b92
Nicolas Seinlet <nse@odoo.com>
2023-01-17 14:25:48 +0000

    [ADD] crm/saas~16.1
    Part-of: odoo/upgrade#4358

---
+++ migrations/crm/saas~16.1.1.8/pre-migrate.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "crm_lead", "recurring_revenue_prorated", "numeric")
+    util.parallel_execute(
+        cr,
+        util.explode_query_range(
+            cr,
+            """
+        UPDATE crm_lead
+           SET recurring_revenue_prorated = COALESCE(recurring_revenue, 0.0) * COALESCE(probability, 0.0) / 100.0
+            """,
+            table="crm_lead",
+        ),
+    )
