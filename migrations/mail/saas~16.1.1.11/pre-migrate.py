04e5599e7ee12f6e8310f6eb63776f7af1e1772e
Christophe Simonis <chs@odoo.com>
2023-01-19 18:54:33 +0000

    [FIX] mail/saas~16.1: create new field with default value
    Part-of: odoo/upgrade#4358

--- migrations/mail/saas~16.1.1.11/pre-migrate.py
+++ migrations/mail/saas~16.1.1.11/pre-migrate.py
@@ -5,3 +5,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.rename_field(cr, "mail.wizard.invite", "send_mail", "notify")
+    util.create_column(cr, "mail_alias", "alias_status", "varchar", default="not_tested")
