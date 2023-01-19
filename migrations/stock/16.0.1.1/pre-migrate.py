581d317668e2ece8034bf5c8281e0ce007aa56d9
Jinane Maksoud <maji@odoo.com>
2023-01-19 08:28:56 +0000

    [FIX] stock/16.0: remove replenishment flag from locations with wrong wh
    The warehouse id on stock locations is now stored, and the replenishment
    orders no longer get it from the stock quantity report.
    Which means now that the warehouse id must be correctly set on replenishment
    locations, which are the warehouses' lot_stock_id. The warehouses
    must have the correct view location as a location of usage 'view'
    and as a parent location to its lot stock location.
    
    When the replenishment location has a wrong warehouse id, the replenishment
    flag is removed and a note is added to the report to let the user fix
    their location hierarchies and warehouses
    
    https://github.com/odoo/odoo/pull/97109
    
    closes odoo/upgrade#4286
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/stock/16.0.1.1/pre-migrate.py
+++ migrations/stock/16.0.1.1/pre-migrate.py
@@ -22,6 +22,39 @@ def migrate(cr, version):
          WHERE l.parent_path LIKE sl.parent_path || '%'
     """
     util.parallel_execute(cr, util.explode_query_range(cr, query, table="stock_location", alias="l"))
+    cr.execute(
+        """
+        UPDATE stock_location l
+           SET replenish_location = false
+          FROM stock_warehouse w
+         WHERE l.id = w.lot_stock_id
+           AND l.replenish_location = true
+           AND l.warehouse_id IS DISTINCT FROM w.id
+     RETURNING l.id,
+               l.name,
+               w.id,
+               w.name
+    """
+    )
+    locations_with_wrong_wh = cr.fetchall()
+    if locations_with_wrong_wh:
+        util.add_to_migration_reports(
+            category="Stock Locations",
+            message="The following stock locations have a mismatch between their assigned "
+            "warehouse id, and the warehouse of their parent view location. They "
+            "are automatically marked as non replenishment locations to avoid "
+            "failure during the upgrade."
+            "\nPlease review the hierarchy of the stock locations and make sure "
+            "that the 'view' locations on the warehouses are correct.\n{}".format(
+                "\n".join(
+                    " - location id:{}, location name:{}, warehouse id:{}, warehouse name:{}".format(
+                        id, name, w_id, w_name
+                    )
+                    for id, name, w_id, w_name in locations_with_wrong_wh
+                )
+            ),
+        )
+
     util.remove_view(cr, "stock.stock_report_view_search")
     util.remove_menus(
         cr,
