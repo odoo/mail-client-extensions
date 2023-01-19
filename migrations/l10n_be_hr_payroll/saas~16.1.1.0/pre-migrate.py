049250c99222a42a7cc667d6b5ed7745be16008e
Nicolas Seinlet <nse@odoo.com>
2023-01-19 07:41:53 +0000

    [FIX] l10n_be_hr_payroll/saas~16.1
    Part-of: odoo/upgrade#4358

--- migrations/l10n_be_hr_payroll/saas~16.1.1.0/pre-migrate.py
+++ migrations/l10n_be_hr_payroll/saas~16.1.1.0/pre-migrate.py
@@ -5,3 +5,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.rename_xmlid(cr, "l10n_be_hr_payroll.hr_payslip_run_view_tree", "l10n_be_hr_payroll.hr_payslip_run_view_form")
+    util.delete_unused(cr, "l10n_be_hr_payroll.cp200_input_rep_fees_regularization")
