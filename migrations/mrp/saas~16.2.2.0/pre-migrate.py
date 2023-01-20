461b394945b04d31c78de3a2033d1a4ab3ed838a
JF Aubert <ajf@odoo.com>
2023-01-20 12:46:18 +0000

    [FIX] mrp/saas~16.2: Merge wo and mo scheduled/effective dates
    remove mrp.production fields
     - date_planned_start
     - date_planned_finished
    rename mrp.production fields
     - production_duration_expected
     - production_real_duration
    remove mrp.workorder fields
     - date_planned_start
     - date_planned_finished
    
    task: 3108291
    
    see odoo/odoo#110550
    see odoo/enterprise#36102
    
    closes odoo/upgrade#4247
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/mrp/saas~16.2.2.0/pre-migrate.py
+++ migrations/mrp/saas~16.2.2.0/pre-migrate.py
@@ -16,3 +16,40 @@ def migrate(cr, version):
     util.parallel_execute(cr, util.explode_query_range(cr, query, table="mrp_bom", alias="b"))
     util.remove_field(cr, "product.template", "produce_delay")
     util.remove_field(cr, "product.template", "days_to_prepare_mo")
+
+    util.explode_execute(
+        cr,
+        "UPDATE mrp_production SET date_start=date_planned_start WHERE date_start IS NULL",
+        table="mrp_production",
+    )
+    util.explode_execute(
+        cr,
+        "UPDATE mrp_production SET date_finished=date_planned_finished WHERE date_finished IS NULL",
+        table="mrp_production",
+    )
+
+    util.adapt_domains(cr, "mrp.production", "date_planned_start", "date_start")
+    util.adapt_domains(cr, "mrp.production", "date_planned_finished", "date_finished")
+
+    util.remove_field(cr, "mrp.production", "date_planned_start")
+    util.remove_field(cr, "mrp.production", "date_planned_finished")
+
+    util.rename_field(cr, "mrp.production", "production_duration_expected", "duration_expected")
+    util.rename_field(cr, "mrp.production", "production_real_duration", "duration")
+
+    util.explode_execute(
+        cr,
+        "UPDATE mrp_workorder SET date_start=date_planned_start WHERE date_start IS NULL",
+        table="mrp_workorder",
+    )
+    util.explode_execute(
+        cr,
+        "UPDATE mrp_workorder SET date_finished=date_planned_finished WHERE date_finished IS NULL",
+        table="mrp_workorder",
+    )
+
+    util.adapt_domains(cr, "mrp.workorder", "date_planned_start", "date_start")
+    util.adapt_domains(cr, "mrp.workorder", "date_planned_finished", "date_finished")
+
+    util.remove_field(cr, "mrp.workorder", "date_planned_start")
+    util.remove_field(cr, "mrp.workorder", "date_planned_finished")
