6c405dd111dc86e4dcb437ca3f44dd78301a4c93
svs-odoo <svs@odoo.com>
2023-01-25 13:07:22 +0000

    [IMP] product_expiry/saas~16.2: compute quant expiration_date
    task-3076044
    Community PR: odoo/odoo#109511
    
    Part-of: odoo/upgrade#4252

--- migrations/product_expiry/saas~16.2.1.0/pre-migrate.py
+++ migrations/product_expiry/saas~16.2.1.0/pre-migrate.py
@@ -6,3 +6,13 @@ def migrate(cr, version):
     eb = util.expand_braces
     util.rename_xmlid(cr, *eb("product_expiry.view_stock_quant_tree{_expiry,}"))
     util.rename_xmlid(cr, *eb("product_expiry.view_stock_quant_tree_editable{_expiry,}"))
+
+    # Updates quants for products using expiration date to add the `expiration_date` on the quants.
+    util.create_column(cr, "stock_quant", "expiration_date", "date")
+    query = """
+        UPDATE stock_quant AS qt
+           SET expiration_date = lot.expiration_date
+          FROM stock_lot AS lot
+         WHERE qt.lot_id = lot.id
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="stock_quant", alias="qt"))
