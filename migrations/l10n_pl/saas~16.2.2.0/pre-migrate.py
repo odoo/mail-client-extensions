37665242478f2507b0eb4f2a174b26392709a560
Christophe Simonis <chs@odoo.com>
2023-01-31 15:21:28 +0000

    [ADD] l10n_pl/saas~16.2
    Force update of some report line to avoid duplicated `code`.
    See odoo/odoo#109611
    
    closes odoo/upgrade#4280
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/l10n_pl/saas~16.2.2.0/pre-migrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.update_record_from_xml(cr, "l10n_pl.account_tax_report_line_podatek_uslug_pozostalych")
