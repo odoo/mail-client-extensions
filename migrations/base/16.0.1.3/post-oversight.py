7af878c9f6add377cd27623bce24755c492f7660
Alvaro Fuentes <afu@odoo.com>
2023-02-01 10:02:28 +0000

    [FIX] base/16.0: update saas_trial modules form view
    The `application` field is added as part of a `group_no_one` block in
    `base.modules_form_view`.
    https://github.com/odoo/odoo/blob/bee4d91d030e305ba8c44704a38c4fd8d614997b/odoo/addons/base/views/ir_module_views.xml#L88-L106
    
    The button `button_immediate_install` is outside that block
    https://github.com/odoo/odoo/blob/bee4d91d030e305ba8c44704a38c4fd8d614997b/odoo/addons/base/views/ir_module_views.xml#L76
    
    Thus we cannot use `application` field on the attrs of the button as
    done in the saas_trial view.
    
    closes odoo/upgrade#4285
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/16.0.1.3/post-oversight.py
+++ migrations/base/16.0.1.3/post-oversight.py
@@ -1,3 +1,5 @@
+from lxml.builder import E
+
 from odoo.upgrade import util
 
 
@@ -9,3 +11,22 @@ def migrate(cr, version):
             elems = arch.xpath("//button[@class='btn btn-primary button_immediate_install' and not(@name)]")
             if elems:
                 elems[0].attrib["name"] = "button_immediate_install"
+
+        with util.skippable_cm(), util.edit_view(
+            cr, "saas_trial.paid_apps_module_form", skip_if_not_noupdate=False
+        ) as arch:
+            if not arch.xpath("//field[@name='application']"):
+                elems = arch.xpath(
+                    """//xpath[@expr="//button[@name='button_immediate_install']" and @position='attributes']"""
+                )
+                if elems:
+                    elem = elems[0]
+                    parent = elem.getparent()
+                    parent.insert(
+                        parent.index(elem),
+                        E.xpath(
+                            E.field(name="application", invisible="1"),
+                            expr="//button[@name='button_immediate_install']",
+                            position="after",
+                        ),
+                    )
