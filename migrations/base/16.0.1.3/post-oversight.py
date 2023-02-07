782d6291759ccd6190045b8a748c25dbb72b25bc
Xavier ALT <xal@odoo.com>
2023-02-07 09:24:19 +0000

    [FIX] base/16.0: remove `saas_fetchmail`, add settings form view
    With the merge of `fetchmail` into `mail`, the `saas_fetchmail` will
    be uninstallable on 16.0 instances, so remove it.
    
    Also manually add missing `saas_trial` view to hide incoming mail
    server config settings on saas.
    
    closes odoo/upgrade#4306
    
    Related: odoo/internal#2054
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/base/16.0.1.3/post-oversight.py
+++ migrations/base/16.0.1.3/post-oversight.py
@@ -4,6 +4,27 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    if util.module_installed(cr, "saas_fetchmail"):
+        util.remove_module(cr, "saas_fetchmail")
+        email_server_tree_act_id = util.ref(cr, "mail.action_email_server_tree")
+        view_id = util.add_view(
+            cr,
+            "General Settings",
+            "res.config.settings",
+            "form",
+            """
+            <xpath expr="//button[@name='%(mail.action_email_server_tree)d']/.." position="replace"></xpath>
+            """.strip()
+            % {"mail.action_email_server_tree": email_server_tree_act_id},
+            inherit_xml_id="mail.res_config_settings_view_form",
+        )
+        util.ensure_xmlid_match_record(
+            cr,
+            "saas_trial.inherit_view_general_configuration",
+            "ir.ui.view",
+            {"id": view_id},
+        )
+
     if util.module_installed(cr, "saas_trial"):
         with util.skippable_cm(), util.edit_view(
             cr, "saas_trial.paid_apps_module_kanban", skip_if_not_noupdate=False
