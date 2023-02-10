53d97501e04f3eb27220f61ad998110a98e8414f
Nishant Jain (niai) <niai@odoo.com>
2023-02-10 05:13:59 +0000

    [IMP] l10n_in_edi/saas~16.2: remove settings view
    In the below commit, one setting view has been removed.
    https://github.com/odoo/odoo/commit/a5465a91a257ae55fb2239d415d2f3dcee4e846e
    
    closes odoo/upgrade#4321
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/l10n_in_edi/saas~16.2.1.03/pre-migrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "l10n_in_edi.res_config_settings_view_form_l10n_in_inherit_l10n_in_edi")
