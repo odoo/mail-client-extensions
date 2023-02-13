3648a99bddf19983c63b98a0604d615cec8655f1
Hardik Prajapati <hpr@odoo.com>
2023-02-13 13:23:27 +0000

    [FIX] mrp/16.0.2.0: fix memory error
    memory error when mrp is loading with huge number of stock moves
    
    ref:
    1. odoo/odoo@8d0b08752535e82a17af9166c9469b1bae8d5afa
    2. odoo/odoo@599b10a7cd4499f97efb86443971d165e5d12b4f
    
    ```
    2023-02-13 16:04:33,495 25 [1;37m[1;41mCRITICAL[0m 424349_15_16.0 odoo.service.server: Failed to initialize database `424349_15_16.0`.
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/16.0/odoo/api.py", line 983, in get
        cache_value = field_cache[record._ids[0]]
    KeyError: 11341964
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/16.0/odoo/fields.py", line 1139, in __get__
        value = env.cache.get(record, self)
      File "/home/odoo/src/odoo/16.0/odoo/api.py", line 990, in get
        raise CacheMiss(record, field)
    odoo.exceptions.CacheMiss: 'stock.move(11341964,).state'
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/16.0/odoo/service/server.py", line 1289, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "<decorator-gen-14>", line 2, in new
      File "/home/odoo/src/odoo/16.0/odoo/tools/func.py", line 87, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/16.0/odoo/modules/registry.py", line 91, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 478, in load_modules
        processed_modules += load_marked_modules(cr, graph,
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 371, in load_marked_modules
        loaded, processed = load_module_graph(
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 207, in load_module_graph
        registry.init_models(cr, model_names, {'module': package.name}, new_install)
      File "/home/odoo/src/odoo/16.0/odoo/modules/registry.py", line 465, in init_models
        func()
      File "/home/odoo/src/odoo/16.0/odoo/addons/base/models/ir_model.py", line 1720, in _reflect_relation
        self.env.invalidate_all()
      File "/home/odoo/src/odoo/16.0/odoo/api.py", line 722, in invalidate_all
        self.flush_all()
      File "/home/odoo/src/odoo/16.0/odoo/api.py", line 732, in flush_all
        self._recompute_all()
      File "/home/odoo/src/odoo/16.0/odoo/api.py", line 728, in _recompute_all
        self[field.model_name]._recompute_field(field)
      File "/home/odoo/src/odoo/16.0/odoo/models.py", line 6163, in _recompute_field
        field.recompute(records)
      File "/home/odoo/src/odoo/16.0/odoo/fields.py", line 1325, in recompute
        self.compute_value(recs)
      File "/home/odoo/src/odoo/16.0/odoo/fields.py", line 1347, in compute_value
        records._compute_field_value(self)
      File "/home/odoo/src/odoo/16.0/odoo/models.py", line 4196, in _compute_field_value
        getattr(self, field.compute)()
      File "/home/odoo/src/odoo/16.0/addons/mrp/models/stock_move.py", line 150, in _compute_manual_consumption
        if move.state != 'draft':
      File "/home/odoo/src/odoo/16.0/odoo/fields.py", line 1165, in __get__
        recs._fetch_field(self)
      File "/home/odoo/src/odoo/16.0/odoo/models.py", line 3158, in _fetch_field
        self._read(fnames)
      File "/home/odoo/src/odoo/16.0/odoo/models.py", line 3235, in _read
        cr.execute(query_str, params + [sub_ids])
      File "/home/odoo/src/odoo/16.0/odoo/sql_db.py", line 310, in execute
        _logger.debug("query: %s", self._format(query, params))
      File "/home/odoo/src/odoo/16.0/odoo/sql_db.py", line 302, in _format
        return self._obj.mogrify(query, params).decode(encoding, 'replace')
      File "/home/odoo/.odoo-venvs/15.0/lib/python3.8/site-packages/psycopg2/extensions.py", line 127, in getquoted
        return b'(' + b', '.join(qobjs) + b')'
    MemoryError
    ```
    
    opw-3105048
    upg-424349
    
    closes odoo/upgrade#4340
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/mrp/16.0.2.0/pre-migrate.py
+++ migrations/mrp/16.0.2.0/pre-migrate.py
@@ -7,3 +7,34 @@ def migrate(cr, version):
     util.remove_view(cr, "mrp.report_mrp_operation_line")
     util.remove_view(cr, "mrp.report_mrp_byproduct_line")
     util.remove_view(cr, "mrp.report_mrp_bom_pdf")
+
+    util.create_column(cr, "mrp_bom_line", "manual_consumption", "boolean", default=False)
+    cr.execute(
+        """
+            UPDATE mrp_bom_line line
+               SET manual_consumption = true
+              FROM product_product p
+              JOIN product_template pt
+                ON p.product_tmpl_id = pt.id
+             WHERE p.id = line.product_id
+               AND pt.tracking != 'none'
+        """
+    )
+
+    util.create_column(cr, "stock_picking_type", "use_auto_consume_components_lots", "boolean", default=False)
+    util.create_column(cr, "stock_move", "manual_consumption", "boolean", default=False)
+    query = """
+        UPDATE stock_move AS move
+           SET manual_consumption = true
+          FROM stock_move m
+          JOIN product_product p
+            ON p.id = m.product_id
+          JOIN product_template pt
+            ON pt.id = p.product_tmpl_id
+          JOIN mrp_bom_line bom
+            ON bom.id = m.bom_line_id
+         WHERE m.state = 'draft'
+           AND m.id = move.id
+           AND (bom.manual_consumption OR pt.tracking != 'none')
+        """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="stock_move", alias="move"))
