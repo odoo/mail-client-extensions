aadd7d8133d816202abaef821ae938d7789b85f9
Abderraouf Ghrissi (abgh) <abgh@odoo.com>
2023-02-16 17:03:05 +0000

    [IMP] hr_timesheet/saas~16.2: remove useless fields
    This commit remove fields related to changes in
    related PRs.
    
    Related PRs:
    community: odoo/odoo#110101
    
    task-3103701
    
    Part-of: odoo/upgrade#4269

--- migrations/hr_timesheet/saas~16.2.1.0/pre-migrate.py
+++ migrations/hr_timesheet/saas~16.2.1.0/pre-migrate.py
@@ -5,3 +5,15 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "res.config.settings", "module_project_timesheet_synchro")
+    util.remove_field(cr, "account.analytic.line", "ancestor_task_id")
+    util.remove_field(cr, "timesheets.analysis.report", "ancestor_task_id")
+    util.create_column(cr, "account_analytic_line", "parent_task_id", "int4")
+
+    query = """
+        UPDATE account_analytic_line l
+           SET parent_task_id = t.parent_id
+          FROM project_task t
+         WHERE t.id = l.task_id
+           AND t.parent_id IS NOT NULL
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="project_task", alias="l"))
