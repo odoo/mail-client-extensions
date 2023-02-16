3344dce0ed403d3bc13547eb8d38edaddd8af9c4
Abderraouf Ghrissi (abgh) <abgh@odoo.com>
2023-02-16 17:03:18 +0000

    [IMP] project/saas~16.2: remove useless fields
    This commit remove fields related to changes in
    related PRs.
    
    Related PRs:
    community: odoo/odoo#110101
    
    task-3103701
    
    Part-of: odoo/upgrade#4269

--- migrations/project/saas~16.2.1.2/pre-migrate.py
+++ migrations/project/saas~16.2.1.2/pre-migrate.py
@@ -18,3 +18,19 @@ def migrate(cr, version):
         ),
     )
     util.change_field_selection_values(cr, "project.task", "recurrence_update", {"subsequent": "future"})
+
+    fields_to_remove_per_model_name = {
+        "project.task": [
+            "partner_is_company",
+            "manager_id",
+            "email_from",
+            "ancestor_id",
+            "partner_email",
+            "project_analytic_account_id",
+            "is_analytic_account_id_changed",
+        ],
+        "project.project": ["partner_email", "partner_phone"],
+    }
+    for model_name, fields in fields_to_remove_per_model_name.items():
+        for field in fields:
+            util.remove_field(cr, model_name, field)
