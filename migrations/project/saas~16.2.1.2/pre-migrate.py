c97afbd8c1255edd5497a3d9a2cf04371674f643
Audric Onockx (auon) <auon@odoo.com>
2023-02-16 08:42:43 +0000

    [IMP] project/saas~16.2: revert task template creation
    Recent commit 6a93dabc3ab0137cb4caab35ac26004aa591e467,
    was about creating task template for recurrences, among other things.
    Since this improvement has been dropped and the task recurrence much
    simplified, this commit removes what had been done to allow it.
    
    Related: odoo/odoo#112764
    Related: odoo/enterprise#37114
    
    task-3084945
    
    closes odoo/upgrade#4343
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/project/saas~16.2.1.2/pre-migrate.py
+++ migrations/project/saas~16.2.1.2/pre-migrate.py
@@ -5,19 +5,49 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     # Remove recurrence from subtasks
-    util.parallel_execute(
+    util.explode_execute(
         cr,
-        util.explode_query_range(
-            cr,
-            """ UPDATE project_task pt
-                   SET recurrence_id = NULL
-                 WHERE parent_id IS NOT NULL
-            """,
-            table="project_task",
-            alias="pt",
-        ),
+        """ UPDATE project_task pt
+               SET recurrence_id = NULL
+             WHERE parent_id IS NOT NULL
+        """,
+        table="project_task",
+        alias="pt",
     )
-    util.change_field_selection_values(cr, "project.task", "recurrence_update", {"subsequent": "future"})
+
+    # Delete recurrences without tasks
+    cr.execute(
+        """
+        SELECT r.id
+          FROM project_task_recurrence r
+         WHERE NOT EXISTS (
+                    SELECT 1 FROM project_task t WHERE t.recurrence_id = r.id
+              )
+        """
+    )
+    util.remove_records(cr, "project.task.recurrence", [rid for rid, in cr.fetchall()])
+
+    # Remove the cron that creates recurring tasks, as they will now be created when marking the last task as done
+    util.remove_record(cr, "project.ir_cron_recurring_tasks")
+
+    # Change repeat n times to repeat until <last_task.create_date + n * rec.repeat_interval [rec.repeat_unit]>
+    cr.execute(
+        """ WITH rec_to_update AS (
+               SELECT r.id,
+                      max(t.create_date) + (r.repeat_interval * r.recurrence_left || ' ' || r.repeat_unit)::interval as repeat_until
+                 FROM project_task t
+                 JOIN project_task_recurrence r
+                   ON r.id = t.recurrence_id
+                WHERE r.repeat_type = 'after'
+             GROUP BY r.id
+          )
+          UPDATE project_task_recurrence r
+             SET repeat_until = u.repeat_until,
+                 repeat_type = 'until'
+            FROM rec_to_update u
+           WHERE u.id = r.id
+        """
+    )
 
     # Replace kanban_state field with state field
     util.create_column(cr, "project_task", "state", "character varying")
@@ -72,8 +102,25 @@ def migrate(cr, version):
     util.adapt_domains(cr, "project.task", "kanban_state", "state", adapter=state_adapter)
     util.adapt_domains(cr, "project.task", "is_blocked", "is_closed", adapter=is_blocked_adapter)
 
+    recurrence_fields = [
+        "sun",
+        "sat",
+        "fri",
+        "thu",
+        "wed",
+        "tue",
+        "mon",
+        "repeat_month",
+        "repeat_weekday",
+        "repeat_week",
+        "repeat_day",
+        "repeat_on_year",
+        "repeat_on_month",
+        "repeat_number",
+    ]
     fields_to_remove_per_model_name = {
         "project.task": [
+            *recurrence_fields,
             "partner_is_company",
             "manager_id",
             "email_from",
@@ -87,6 +134,12 @@ def migrate(cr, version):
             "legend_done",
             "legend_blocked",
             "legend_normal",
+            "repeat_show_month",
+            "repeat_show_week",
+            "repeat_show_day",
+            "repeat_show_dow",
+            "recurrence_message",
+            "recurrence_update",
         ],
         "project.task.type": [
             "legend_done",
@@ -98,7 +151,15 @@ def migrate(cr, version):
             "is_blocked",
         ],
         "project.project": ["partner_email", "partner_phone"],
+        "project.task.recurrence": [
+            *recurrence_fields,
+            "recurrence_left",
+            "next_recurrence_date",
+        ],
     }
     for model_name, fields in fields_to_remove_per_model_name.items():
         for field in fields:
             util.remove_field(cr, model_name, field)
+
+    util.update_record_from_xml(cr, "project.project_task_recurrence_1")
+    util.update_record_from_xml(cr, "project.project_task_recurrence_2")
