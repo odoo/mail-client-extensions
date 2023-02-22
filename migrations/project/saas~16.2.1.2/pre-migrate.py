3dd2945b9bfea5ddc3b91d064052160fcc51cecc
Bastien (bvdn) <bvdn@odoo.com>
2023-02-22 16:36:54 +0000

    [IMP] project/saas~16.2: replace kanban_state by state
    This commit completes the implementation of the new project.task.state
    which will replace the kanban_state
    the task.state has more selection choice
    and is linked to its dependent tasks states
    More info in the commit linked below
    
    This commit will:
    
    Create new column state in project.task table
    (base value linked to old kanban_state value)
    Then remove kanban_state field from project.task and report.project.task.user
    
    also remove/refactor the following fields:
    
    For project.task remove:
     - kanban_state_label
     - legend_blocked
     - legend_normal
     - legend_done
    
    For project.task.type (stage) remove:
     - legend_blocked
     - legend_normal
     - legend_done
     - auto_validation_kanban_state (not removed, renamed auto_validation_state)
    
    See odoo/odoo#107593
    See odoo/enterprise#35359
    
    Task-3084930
    
    closes odoo/upgrade#4367
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/project/saas~16.2.1.2/pre-migrate.py
+++ migrations/project/saas~16.2.1.2/pre-migrate.py
@@ -19,6 +19,59 @@ def migrate(cr, version):
     )
     util.change_field_selection_values(cr, "project.task", "recurrence_update", {"subsequent": "future"})
 
+    # Replace kanban_state field with state field
+    util.create_column(cr, "project_task", "state", "character varying")
+
+    query = """
+        UPDATE project_task
+           SET state = CASE
+          WHEN kanban_state = 'normal' THEN '01_in_progress'
+          WHEN kanban_state = 'blocked' THEN '02_changes_requested'
+          WHEN kanban_state = 'done' THEN '03_approved'
+          ELSE '01_in_progress'
+           END
+    """
+
+    util.explode_execute(cr, query, table="project_task")
+
+    # rename auto_validation_kanban_state
+    util.rename_field(cr, "project.task.type", "auto_validation_kanban_state", "auto_validation_state")
+
+    # remove digest_tip_project_0
+    util.remove_record(cr, "project.digest_tip_project_0")
+
+    # rename xmlid for subtypes
+    util.rename_xmlid(cr, "project.mt_task_progress", "project.mt_task_in_progress")
+    util.rename_xmlid(cr, "project.mt_task_blocked", "project.mt_task_changes_requested")
+    util.rename_xmlid(cr, "project.mt_task_ready", "project.mt_task_approved")
+
+    util.rename_xmlid(cr, "project.mt_project_task_progress", "project.mt_project_task_in_progress")
+    util.rename_xmlid(cr, "project.mt_project_task_blocked", "project.mt_project_task_changes_requested")
+    util.rename_xmlid(cr, "project.mt_project_task_ready", "project.mt_project_task_approved")
+
+    # adapt is_blocked and kanban_state domains
+    def state_adapter(leaf, _o, _n):
+        _, op, right = leaf
+        kanban_to_state_dict = {
+            "normal": "01_in_progress",
+            "blocked": "02_changes_requested",
+            "done": "03_approved",
+        }
+        if isinstance(right, str):
+            right = kanban_to_state_dict.get(right, right)
+        elif right:
+            right = [kanban_to_state_dict.get(r, r) for r in right]
+        return [("state", op, right)]
+
+    def is_blocked_adapter(leaf, _o, _n):
+        # here we will consider is_blocked as the inverse of is_closed
+        _, op, right = leaf
+        new_op = "!=" if bool(right) else "="
+        return [("is_closed", new_op, right)]
+
+    util.adapt_domains(cr, "project.task", "kanban_state", "state", adapter=state_adapter)
+    util.adapt_domains(cr, "project.task", "is_blocked", "is_closed", adapter=is_blocked_adapter)
+
     fields_to_remove_per_model_name = {
         "project.task": [
             "partner_is_company",
@@ -28,6 +81,21 @@ def migrate(cr, version):
             "partner_email",
             "project_analytic_account_id",
             "is_analytic_account_id_changed",
+            "kanban_state",
+            "is_blocked",
+            "kanban_state_label",
+            "legend_done",
+            "legend_blocked",
+            "legend_normal",
+        ],
+        "project.task.type": [
+            "legend_done",
+            "legend_blocked",
+            "legend_normal",
+        ],
+        "report.project.task.user": [
+            "kanban_state",
+            "is_blocked",
         ],
         "project.project": ["partner_email", "partner_phone"],
     }
