a28b492937a51d2dcfacad3bf795c424562c2488
Pierre Rousseau <pro@odoo.com>
2023-02-22 09:09:51 +0000

    [FIX] spreadsheet_edition/saas~16.2: remove revision views
    The views of `spreadsheet.revision` are moved from `documents_spreadsheet`
    to `spreadsheet_edition`. This migration script rename the xml ids from
    `documents_spreadsheet` to `spreadsheet_edition`.
    
    See https://github.com/odoo/enterprise/pull/37388
    
    Task-id 3199033
    
    closes odoo/upgrade#4364
    
    Related: odoo/enterprise#37388
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/spreadsheet_edition/saas~16.2.1.0/pre-migrate.py
+++ migrations/spreadsheet_edition/saas~16.2.1.0/pre-migrate.py
@@ -3,3 +3,13 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.rename_field(cr, "spreadsheet.collaborative.mixin", "raw", "spreadsheet_data")
+
+    renames = """
+        spreadsheet_revision_view_tree
+        spreadsheet_revision_view_search
+        spreadsheet_revision_action
+        menu_technical_spreadsheet
+        menu_technical_spreadsheet_revision
+    """
+    for name in util.splitlines(renames):
+        util.rename_xmlid(cr, f"documents_spreadsheet.{name}", f"spreadsheet_edition.{name}")
