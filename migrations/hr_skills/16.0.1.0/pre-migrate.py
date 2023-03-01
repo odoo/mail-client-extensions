ff7a1e7af616df0b6c0e7ba90a734a3e90c2f430
diagnoza <owi@odoo.com>
2023-03-01 16:01:46 +0000

    [FIX] hr_skills/16.0.1.0: do not use records with null hr_employee
    hr_employee is not required on hr_employee_skill, sometimes only
    applicant_id is set
    
    closes odoo/upgrade#4389
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/hr_skills/16.0.1.0/pre-migrate.py
+++ migrations/hr_skills/16.0.1.0/pre-migrate.py
@@ -10,5 +10,6 @@ def migrate(cr, version):
             INSERT into hr_employee_hr_skill_rel(hr_employee_id, hr_skill_id)
             SELECT DISTINCT employee_id, skill_id
               FROM hr_employee_skill
+             WHERE employee_id IS NOT NULL
         """
     )
