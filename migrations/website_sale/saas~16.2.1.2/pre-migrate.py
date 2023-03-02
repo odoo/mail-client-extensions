7e8d3d203cd319e22512e626c4f33e4d486be9c2
Maximilien (malb) <malb@odoo.com>
2023-03-02 12:53:26 +0000

    [FIX] website_sale/saas~16.2: misplaced script
    It seems that this migration scripts wasn't correctly placed
    
    closes odoo/upgrade#3826
    
    Related: odoo/odoo#99209
    Related: odoo/enterprise#30853
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website_sale/saas~16.2.1.2/pre-migrate.py
+++ migrations/website_sale/saas~16.2.1.2/pre-migrate.py
@@ -5,3 +5,4 @@ def migrate(cr, version):
     cr.execute("SELECT value FROM ir_config_parameter WHERE key='account.show_line_subtotals_tax_selection'")
     [show_line] = cr.fetchone() or [None]
     util.create_column(cr, "res_company", "show_line_subtotals_tax_selection", "varchar", default=show_line)
+    util.rename_xmlid(cr, "website_sale.menu_ecommerce_payment_icons", "website_sale.menu_ecommerce_payment_methods")
--- migrations/website_sale/saas~16.2.2.0/pre-migrate.py
+++
@@ -1,6 +0,0 @@
-from odoo.upgrade import util
-
-
-def migrate(cr, version):
-
-    util.rename_xmlid(cr, "website_sale.menu_ecommerce_payment_icons", "website_sale.menu_ecommerce_payment_methods")
