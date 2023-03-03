af088180861586d0f94b4963d6a68dbdd11c02cf
Panagiotis Kyriakou <paky@odoo.com>
2023-03-03 15:11:29 +0000

    [IMP] planning/saas~16.3: remove model and view
    In this commit we specify the model and
    view that we removed from the planning
    app. Both model and view were not used.
    
    task-3117580
    See odoo/enterprise#37806
    
    closes odoo/upgrade#4406
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/planning/saas~16.3.1.0/pre-migrate.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "planning.planning", "slot_ids")
+    util.remove_model(cr, "slot.planning.select.send")
+
+    util.remove_view(cr, "planning.slot_planning_select_send_view_form")
+    util.remove_record(cr, "planning.model_planning_slot_action_publish")
