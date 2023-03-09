add67668412262c06ecd2deabe167cd49f644525
Louis Baudoux <lba@odoo.com>
2023-03-09 09:56:07 +0000

    [IMP] *_extract/saas~16.2.1.0: remove duplicated IAP extract endpoints
    Enterprise PR: odoo/enterprise#37964
    
    Part-of: odoo/upgrade#4417

--- migrations/hr_expense_extract/saas~16.2.1.0/pre-migrate.py
+++ migrations/hr_expense_extract/saas~16.2.1.0/pre-migrate.py
@@ -4,5 +4,7 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.remove_record(cr, "hr_expense_extract.hr_expense_extract_endpoint")
+
     iap_extract = util.import_script("iap_extract/saas~16.2.1.0/end-status_code.py")
     iap_extract.migrate_status_code(cr, "hr.expense")
