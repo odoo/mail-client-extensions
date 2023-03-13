dd501ea6da4a31b2326083cf25582860c0e904f5
svs-odoo <svs@odoo.com>
2023-03-13 15:17:33 +0000

    [IMP] mrp_plm/saas~16.4: remove unused field
    Removes unused computed `mrp.bom`'s field `eco_inprogress_count`.
    
    See odoo/enterprise#35969
    Community PR: odoo/odoo#110227
    
    task-3089368
    
    closes odoo/upgrade#4428
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/mrp_plm/saas~16.4.1.0/pre-migrate.py
+++ migrations/mrp_plm/saas~16.4.1.0/pre-migrate.py
@@ -7,3 +7,4 @@ def migrate(cr, version):
     util.rename_xmlid(cr, "mrp_workorder_plm.ecostage_progress", "mrp_plm.ecostage_bom_update_progress")
     util.rename_xmlid(cr, "mrp_workorder_plm.ecostage_validated", "mrp_plm.ecostage_bom_update_validated")
     util.rename_xmlid(cr, "mrp_workorder_plm.ecostage_effective", "mrp_plm.ecostage_bom_update_effective")
+    util.remove_field(cr, "mrp.bom", "eco_inprogress_count")
