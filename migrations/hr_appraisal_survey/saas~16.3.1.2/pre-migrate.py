7c1e100b722762f97eee6c2f48916628a039a15f
Pierre-Yves Dufays <pydu@odoo.com>
2023-03-13 10:53:02 +0000

    [IMP] {hr_appraisal_,}survey/saas~16.3: remove survey is_appraisal field
    Following the simplification of the survey form and the introduction of the
    field survey_type in survey_survey, the field is_appraisal is now redundant.
    The migration sets survey_type to 'appraisal' for all survey having
    is_appraisal = true and remove the is_appraisal field.
    
    See odoo/odoo#115297
    See odoo/enterprise#38268
    
    Task-3061869
    
    closes odoo/upgrade#4464
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/hr_appraisal_survey/saas~16.3.1.2/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute("UPDATE survey_survey SET survey_type = 'appraisal' WHERE is_appraisal")
+    util.remove_field(cr, "survey.survey", "is_appraisal")
--- migrations/survey/saas~16.3.3.5/pre-migrate.py
+++ migrations/survey/saas~16.3.3.5/pre-migrate.py
@@ -6,3 +6,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "survey.certification_report_view_classic")
     util.remove_view(cr, "survey.certification_report_view_modern")
+
+    util.create_column(cr, "survey_survey", "survey_type", "varchar", default="custom")
