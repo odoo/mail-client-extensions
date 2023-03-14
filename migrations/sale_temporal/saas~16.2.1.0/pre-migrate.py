056201db5a010d895eb78a2918ac67275f4845be
Arnaud Joset <arj@odoo.com>
2023-03-14 16:28:20 +0000

    [IMP] sale_temoral/saas~16.2: convert internal_note to html field
    taskid: 3151840
    enterprise PR: https://github.com/odoo/enterprise/pull/36390
    
    closes odoo/upgrade#4401
    
    Related: odoo/enterprise#36390
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/sale_temporal/saas~16.2.1.0/pre-migrate.py
+++ migrations/sale_temporal/saas~16.2.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.rename_field(cr, "sale.temporal.recurrence", "subscription_unit_display", "temporal_unit_display")
+    util.convert_field_to_html(cr, "sale.order", "internal_note")
