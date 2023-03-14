0f089e32d7be3cb22384190b25b2cbe8002ba48d
Andro Gvivradze <angv@odoo.com>
2023-03-14 09:27:14 +0000

    [IMP] Survey/saas~16.3: make certification reports editable by studio
    This commit merges two certification reports into one so that we
    get rid of repetitive code and make certificates editable by studio.
    
    See https://github.com/odoo/odoo/pull/113469
    
    closes odoo/upgrade#4503
    
    Task: 3148819
    Related: odoo/odoo#113469
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/survey/saas~16.3.3.5/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "survey.certification_report_view_classic")
+    util.remove_view(cr, "survey.certification_report_view_modern")
