447130234814457b9c0d89cf8be1bc747f7aec31
Christophe Simonis <chs@odoo.com>
2023-03-16 16:48:23 +0100

    [FIX] hr_holidays/saas~16.2: sql-compute new field

--- migrations/hr_holidays/saas~16.2.1.5/pre-migrate.py
+++ migrations/hr_holidays/saas~16.2.1.5/pre-migrate.py
@@ -16,3 +16,13 @@ def migrate(cr, version):
     util.adapt_domains(cr, "hr.leave.type", "responsible_id", "responsible_ids")
     util.remove_field(cr, "hr.leave.type", "responsible_id")
     util.delete_unused(cr, "hr_holidays.mail_act_leave_allocation_second_approval")
+
+    util.create_column(cr, "hr_leave_accrual_plan", "company_id", "int4")
+    cr.execute(
+        """
+            UPDATE hr_leave_accrual_plan p
+               SET company_id = t.company_id
+              FROM hr_leave_type t
+             WHERE t.id = p.time_off_type_id
+        """
+    )
