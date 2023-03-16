61e3c2e27ca9e3c1919b9807cee38d075c169c42
Pieter Claeys (clpi) <clpi@odoo.com>
2023-03-16 09:25:02 +0000

    [IMP] stock/saas~16.4.1.1: allow scrapping of kits
    Convert the reference to a stock_move.id on a stock_scrap
    to a reference to a stock_scrap.id on stock_move.
    
    Community PR: https://github.com/odoo/odoo/pull/114315
    Enterprise PR: https://github.com/odoo/enterprise/pull/37764
    
    closes odoo/upgrade#4514
    
    Related: odoo/odoo#114315
    Related: odoo/enterprise#37764
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/stock/saas~16.4.1.1/pre-migrate.py
+++ migrations/stock/saas~16.4.1.1/pre-migrate.py
@@ -4,3 +4,14 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "product.replenish", "route_ids")
+    # Convert the reference to a stock_move.id on a stock_scrap to a reference to a stock_scrap.id on stock_move
+    util.create_column(cr, "stock_move", "scrap_id", "int4")
+    query = """
+        UPDATE stock_move sm
+           SET scrap_id = ss.id
+          FROM stock_scrap ss
+         WHERE ss.move_id = sm.id
+    """
+    util.explode_execute(cr, query, table="stock_move", alias="sm")
+    util.remove_field(cr, "stock.move", "scrap_ids")
+    util.remove_field(cr, "stock.scrap", "move_id")
