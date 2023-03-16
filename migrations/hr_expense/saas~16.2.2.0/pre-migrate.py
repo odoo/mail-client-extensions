64675614f1bcb015f486ae9580c918df759b3e78
Christophe Simonis <chs@odoo.com>
2023-03-16 17:44:49 +0100

    [FIX] hr_expense/saas~16.2: sql-compute new stored fields

--- migrations/hr_expense/saas~16.2.2.0/pre-migrate.py
+++ migrations/hr_expense/saas~16.2.2.0/pre-migrate.py
@@ -1,9 +1,31 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.create_column(cr, "hr_expense_sheet", "employee_journal_id", "int4")
+    util.create_column(cr, "hr_expense_sheet", "payment_method_line_id", "int4")
+
+    util.explode_execute(cr, "UPDATE hr_expense_sheet SET employee_journal_id = journal_id", table="hr_expense_sheet")
+    cr.execute(
+        """
+            WITH prefered AS (
+                SELECT j.company_id, (array_agg(l.id ORDER BY l.sequence, l.id))[1] as line_id
+                  FROM account_payment_method_line l
+                  JOIN account_journal j
+                    ON j.id = l.journal_id
+                  JOIN account_payment_method m
+                    ON m.id = l.payment_method_id
+                 WHERE m.payment_type = 'outbound'
+              GROUP BY j.company_id
+            )
+            UPDATE hr_expense_sheet s
+               SET payment_method_line_id = p.line_id
+              FROM prefered p
+             WHERE p.company_id = s.company_id
+        """
+    )
+
     util.remove_field(cr, "hr.expense.sheet", "bank_journal_id")
     util.remove_field(cr, "hr.expense.sheet", "attachment_number")
 
