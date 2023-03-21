2ef6904abc8e25f3b7b64e34523ccc1a547e33c9
dasz <dasz@odoo.com>
2023-03-21 09:21:09 +0000

    [FIX] hr_holidays/saas~16.5: remove fields not used anymore
    remove parent_id which is not used anymore
    move is_based_on_work_time to accrual plan.
    reword "postponed" key to "all"
    "days" and "hours" to singular form, to be consistent on the value type
    in both plan and level.
    
    see odoo/odoo#112383
    task 3217812
    
    closes odoo/upgrade#4597
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr_holidays/saas~16.5.1.5/pre-migrate.py
+++ migrations/hr_holidays/saas~16.5.1.5/pre-migrate.py
@@ -23,3 +23,82 @@ def migrate(cr, version):
 
     for record in rename_records:
         util.rename_xmlid(cr, record, record.replace("stress", "mandatory"))
+
+    util.change_field_selection_values(
+        cr,
+        "hr.leave.accrual.level",
+        "added_value_type",
+        {
+            "days": "day",
+            "hours": "hour",
+        },
+    )
+    util.change_field_selection_values(
+        cr, "hr.leave.accrual.level", "action_with_unused_accruals", {"postponed": "all"}
+    )
+    util.remove_field(cr, "hr.leave.accrual.level", "parent_id")
+    util.remove_column(cr, "hr_leave_accrual_level", "parent_path")  # if parent_store was set by any inheriting module.
+
+    util.create_column(cr, "hr_leave_accrual_plan", "is_based_on_worked_time", "boolean", default=False)
+    cr.execute(
+        """
+            WITH plans AS (
+          SELECT accrual_plan_id
+            FROM hr_leave_accrual_level
+           WHERE is_based_on_worked_time = true
+        GROUP BY accrual_plan_id
+            )
+            UPDATE hr_leave_accrual_plan p
+               SET is_based_on_worked_time = true
+              FROM plans o
+             WHERE o.accrual_plan_id = p.id
+        """
+    )
+
+    cr.execute(
+        """
+            UPDATE hr_leave_accrual_level l
+               SET added_value_type = CASE WHEN t.request_unit in ('day', 'half_day') THEN 'day' ELSE 'hour' END
+              FROM hr_leave_accrual_plan p
+              JOIN hr_leave_type t
+                ON t.id = p.time_off_type_id
+             WHERE p.id = l.accrual_plan_id
+        """
+    )
+    cr.execute(
+        """
+            WITH plan_no_type AS (
+                SELECT p.id,
+                       (array_agg(l.added_value_type ORDER BY l.sequence ASC))[1] as avt
+                  FROM hr_leave_accrual_level l
+                  JOIN hr_leave_accrual_plan p
+                    ON p.id = l.accrual_plan_id
+                 WHERE p.time_off_type_id IS NULL
+              GROUP BY p.id
+            )
+            UPDATE hr_leave_accrual_level l
+               SET added_value_type = p.avt
+              FROM plan_no_type p
+             WHERE p.id = l.accrual_plan_id
+        """
+    )
+
+    util.create_column(cr, "hr_leave_accrual_plan", "added_value_type", "varchar")
+    cr.execute(
+        """
+            WITH plans AS (
+                SELECT p.id,
+                       (array_agg(l.added_value_type ORDER BY l.sequence ASC))[1] as avt
+                  FROM hr_leave_accrual_level l
+                  JOIN hr_leave_accrual_plan p
+                    ON p.id = l.accrual_plan_id
+              GROUP BY p.id
+            )
+            UPDATE hr_leave_accrual_plan p
+               SET added_value_type = o.avt
+              FROM plans o
+             WHERE p.id = o.id
+        """
+    )
+
+    util.remove_field(cr, "hr.leave.accrual.level", "is_based_on_worked_time")
