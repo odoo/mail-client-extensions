a5f538b3be5c9810f6e060e52e5310f13d4b2d3f
diagnoza <owi@odoo.com>
2023-03-21 00:23:24 +0000

    [FIX] product/saas~14.5.1.2: move printnode fix
    Move printnode fix from local_util to a generic script.
    
    closes odoo/upgrade#4454
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/product/saas~14.5.1.2/pre-custom.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo.upgrade import util
+
+_logger = logging.getLogger("odoo.upgrade.saas-14.5.1.2." + __name__)
+
+
+def migrate(cr, version):
+    # A very common custom module, "printnode_base", that's always interfering with what's done during the upgrade.
+    # Existing reports are about to be removed in pre-migrate.py.
+    # Their deletion should cascade onto the printnode entries referring to them.
+    tables = ["printnode_report_policy", "printnode_rule", "printnode_scenario", "printnode_action_button"]
+    for table in tables:
+        if not util.column_exists(cr, table, "report_id"):
+            continue
+        cr.execute(
+            f"""
+            ALTER TABLE {table}
+             DROP CONSTRAINT IF EXISTS {table}_report_id_fkey,
+              ADD CONSTRAINT {table}_report_id_fkey
+                  FOREIGN KEY (report_id) REFERENCES ir_act_report_xml(id)
+                       ON DELETE CASCADE;
+            """
+        )
+        _logger.info("Change FK of %s.report_id to be `ON DELETE CASCADE`", table)
