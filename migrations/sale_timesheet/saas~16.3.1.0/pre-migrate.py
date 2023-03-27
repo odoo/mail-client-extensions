e5ed335ba2efee244c6ad28756095b59ddbb8945
Abderraouf Ghrissi (abgh) <abgh@odoo.com>
2023-03-27 12:25:42 +0000

    [IMP] sale_timesheet/saas~16.3: remove useless fields
    This commit remove fields related to changes in
    related PRs.
    
    Related PRs:
    Community: https://github.com/odoo/odoo/pull/116628
    Enterprise: https://github.com/odoo/enterprise/pull/38771
    
    task-3229873
    
    closes odoo/upgrade#4478
    
    Related: odoo/enterprise#38771
    Related: odoo/odoo#116628
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/sale_timesheet/saas~16.3.1.0/pre-migrate.py
+++ migrations/sale_timesheet/saas~16.3.1.0/pre-migrate.py
@@ -5,3 +5,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "sale.order", "timesheet_ids")
+    util.remove_view(cr, "sale_timesheet.view_sale_service_inherit_form2")
