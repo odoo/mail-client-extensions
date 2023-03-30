03462f02716b744fe6eba5fdc0c0897c1d48e35c
Alvaro Fuentes <afu@odoo.com>
2023-03-30 09:16:09 +0000

    [FIX] website_sale/saas~14.5: avoid MemoryError, create columns
    The compute of those fields may cause memory errors when the ORM loads
    all the product (template) records.
    
    The problematic fields are `base_unit_count`, default 1, and `base_unit_id`,
    default NULL.
    
    The fields `base_unit_price` and `base_unit_name` do not cause memory
    errors so far.
    
    ```
      File "/home/odoo/src/odoo/15.0/addons/website_sale/models/product_template.py", line 56, in _compute_base_unit_count
        for template in self.filtered(lambda template: len(template.product_variant_ids) == 1):
      File "/home/odoo/src/odoo/15.0/odoo/models.py", line 5503, in filtered
        return self.browse([rec.id for rec in self if func(rec)])
      File "/home/odoo/src/odoo/15.0/odoo/models.py", line 5503, in <listcomp>
        return self.browse([rec.id for rec in self if func(rec)])
      File "/home/odoo/src/odoo/15.0/addons/website_sale/models/product_template.py", line 56, in <lambda>
        for template in self.filtered(lambda template: len(template.product_variant_ids) == 1):
      File "/home/odoo/src/odoo/15.0/odoo/fields.py", line 3397, in __get__
        return super().__get__(records, owner)
      File "/home/odoo/src/odoo/15.0/odoo/fields.py", line 2611, in __get__
        return super().__get__(records, owner)
      File "/home/odoo/src/odoo/15.0/odoo/fields.py", line 1154, in __get__
        return self.convert_to_record(value, record)
      File "/home/odoo/src/odoo/15.0/odoo/fields.py", line 3208, in convert_to_record
        corecords = corecords.filtered(Comodel._active_name).with_prefetch(prefetch_ids)
      File "/home/odoo/src/odoo/15.0/odoo/models.py", line 5502, in filtered
        self.mapped(name)
      File "/home/odoo/src/odoo/15.0/odoo/models.py", line 5464, in mapped
        recs = recs._fields[name].mapped(recs)
      File "/home/odoo/src/odoo/15.0/odoo/fields.py", line 1180, in mapped
        self.__get__(first(remaining), type(remaining))
      File "/home/odoo/src/odoo/15.0/odoo/fields.py", line 1087, in __get__
        recs._fetch_field(self)
      File "/home/odoo/src/odoo/15.0/odoo/models.py", line 3276, in _fetch_field
        self._read(fnames)
      File "/home/odoo/src/odoo/15.0/odoo/models.py", line 3373, in _read
        self.env.cache.update(fetched, field, values)
      File "/home/odoo/src/odoo/15.0/odoo/api.py", line 900, in update
        field_cache.update(zip(records._ids, values))
    MemoryError
    ```
    
    See:
    https://github.com/odoo/odoo/commit/8fc2e229f954158d74df5232d62e7b498e8d34b2#diff-c90a84a31fe946fe096b9def41662c02281c181727cca38704b039047b88152cR210-R215
    https://github.com/odoo/odoo/commit/8fc2e229f954158d74df5232d62e7b498e8d34b2#diff-bcce8086c8655fe6671ccbb5b4739a891993e92380f67930ea24423e80941607R17-R18
    
    upg-580327
    opw-3129291
    
    closes odoo/upgrade#4499
    
    Note: the memory error doesn't happen always.
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/website_sale/saas~14.5.1.1/pre-migrate.py
+++ migrations/website_sale/saas~14.5.1.1/pre-migrate.py
@@ -12,3 +12,28 @@ def migrate(cr, version):
 
     util.remove_view(cr, "website_sale.template_header_minimalist")
     util.remove_view(cr, "website_sale.product_template_form_view_invoice_policy")
+
+    util.create_column(cr, "product_product", "base_unit_count", "float", default=1)
+    util.create_column(cr, "product_template", "base_unit_count", "float")
+    util.explode_execute(
+        cr,
+        """
+        WITH info AS (
+            SELECT t.id
+              FROM product_template t
+              JOIN product_product p
+                ON p.product_tmpl_id = t.id
+             WHERE {parallel_filter}
+             GROUP BY t.id
+            HAVING COUNT(*) = 1
+        ) UPDATE product_template t
+             SET base_unit_count = 1
+            FROM info
+           WHERE info.id=t.id
+        """,
+        table="product_template",
+        alias="t",
+    )
+
+    util.create_column(cr, "product_product", "base_unit_id", "int4")
+    util.create_column(cr, "product_template", "base_unit_id", "int4")
