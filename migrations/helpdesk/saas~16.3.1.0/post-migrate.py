27efdd8575269a098e7c309d4ba1f497388fccdd
Xavier Bol (xbo) <xbo@odoo.com>
2023-04-06 13:44:15 +0000

    [IMP] helpdesk/saas~16.3: implies new group to helpdesk user group
    This commit adds the new group in the one for Helpdesk User if the
    auto-assignment feature is enabled to at least one helpdesk team.
    The goal of that new group is to be able to display filter
    when the feature is enabled in at least one helpdesk team, if no
    helpdesk team has the feature enabled then the filter is useless and
    so it will be hidden thanks to that group.
    
    task-3052920
    
    Enterprise PR: odoo/enterprise#33835
    
    closes odoo/upgrade#4527
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/helpdesk/saas~16.3.1.0/post-migrate.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+            SELECT 1
+              FROM helpdesk_team team
+             WHERE team.auto_assignment IS TRUE
+             LIMIT 1
+        """
+    )
+    if cr.rowcount:
+        helpdesk_user_group = util.ref(cr, "helpdesk.group_helpdesk_user")
+        helpdesk_user_group.write({"implied_ids": [(4, util.ref(cr, "helpdesk.group_auto_assignment"))]})
