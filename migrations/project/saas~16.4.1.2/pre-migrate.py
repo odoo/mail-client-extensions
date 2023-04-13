fbc6943eb20caee375c53154e0009e22f8467439
Bastien (bvdn) <bvdn@odoo.com>
2023-04-13 14:56:55 +0000

    [FIX] project/saas~16.4: remove is_closed field
    This commit completes the implementation of the Mark as Done task followup.
    In this upgrade script we are removing the project.task *is_closed* field.
    In these models:
    - Project.task
    - Project.task.burndown.chart.report
    - report.project.task.user
    
    See: https://github.com/odoo/odoo/pull/117968
    See: https://github.com/odoo/enterprise/pull/39714
    
    Task-3213526
    
    closes odoo/upgrade#4548
    
    Related: odoo/enterprise#39714
    Related: odoo/odoo#117968
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/project/saas~16.4.1.2/pre-migrate.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    def is_closed_adapter(leaf, _o, _n):
+        # what was closed is now in one of the closed states
+        _, op, right = leaf
+        return [("state", "in" if (op == "=") == right else "not in", ["1_done", "1_canceled"])]
+
+    util.update_field_usage(cr, "project.task", "is_closed", "state", domain_adapter=is_closed_adapter)
+
+    util.remove_field(cr, "project.task", "is_closed", drop_column=False)
+    util.remove_field(cr, "project.task.burndown.chart.report", "is_closed")
+    util.remove_field(cr, "report.project.task.user", "is_closed")
