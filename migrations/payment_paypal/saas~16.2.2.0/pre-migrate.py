e8885017f1fd0d82d210496e8791446d8606bab1
Thibault Francois <tfr@odoo.com>
2023-04-19 10:19:52 +0000

    [FIX] payment_paypal/saas~16.2.2.0: do not disable other provider
    Before this commit all provider were disabled during upgrade to 16.2
    
    after this commit, only paypal provider with the missing token are
    disabled
    
    Since an action from the admin is required we add the disabled provider
    in the report
    
    closes odoo/upgrade#4566
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/payment_paypal/saas~16.2.2.0/pre-migrate.py
+++ migrations/payment_paypal/saas~16.2.2.0/pre-migrate.py
@@ -7,6 +7,33 @@ def migrate(cr, version):
     util.remove_field(cr, "payment.provider", "paypal_seller_account")
     util.remove_field(cr, "payment.provider", "paypal_use_ipn")
 
-    cr.execute("UPDATE payment_provider SET state='disabled' WHERE paypal_pdt_token IS NULL")
+    cr.execute(
+        """
+           UPDATE payment_provider
+              SET state='disabled'
+            WHERE paypal_pdt_token IS NULL
+              AND code = 'paypal'
+        RETURNING id, name->'en_US'
+        """
+    )
+
+    provider_disabled = cr.fetchall()
+    if provider_disabled:
+        util.add_to_migration_reports(
+            """
+                <details>
+                <summary>
+                    The following paypal provider have been disabled due to missing PDT Identity Token
+                </summary>
+                <ul>%s</ul>
+                </details>
+            """
+            % "\n".join(
+                f"<li>{util.get_anchor_link_to_record('payment.provider', provider_id, name)}</li>"
+                for provider_id, name in provider_disabled
+            ),
+            category="Payment",
+            format="html",
+        )
 
     util.remove_view(cr, "payment_paypal.mail_template_paypal_invite_user_to_configure")
