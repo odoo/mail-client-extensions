756d37c03b33bc3c32e2c5faae708dcfd2e89879
diagnoza <owi@odoo.com>
2023-04-19 18:54:15 +0000

    [FIX] website_sale/16.0.1.1: adapt a cowed view
    Adapts cowed website_sale.product to update the "row" div.
    
    closes odoo/upgrade#4568
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/website_sale/16.0.1.1/end-migrate.py
+++ migrations/website_sale/16.0.1.1/end-migrate.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+from functools import lru_cache
+
+from lxml import etree
+
+from odoo.tools import misc
+
 from odoo.upgrade import util
 
 
@@ -15,3 +21,21 @@ def migrate(cr, version):
         with util.edit_view(cr, view_id=view_id) as arch:
             for node in arch.xpath('//div[@id="wrap"][not(hasclass("o_wsale_product_page"))]'):
                 node.attrib["class"] = (node.attrib.get("class", "") + " o_wsale_product_page").lstrip()
+            for node in arch.xpath('//t[@t-call="website_sale.shop_product_carousel"]'):
+                # There are multiple divs with class='row' that have no unique ids, so
+                # it's difficult to obtain a particular div reliably. Instead, we obtain
+                # a unique (to this template) node of the div that interests us and
+                # refer to its parent
+                updated_node = get_node()
+                parent = node.getparent()
+                parent.getparent().replace(parent, updated_node)
+
+
+@lru_cache(1)
+def get_node():
+    with misc.file_open("website_sale/views/templates.xml") as fp:
+        tree = etree.parse(fp)
+        node = tree.find("//template[@id='product']//div[@id='product_detail_main']")
+        if node:
+            return node
+    raise RuntimeError("Node not found")
