4b3c0b24fc67428718b1c44bda985e769da806e7
Alvaro Fuentes <afu@odoo.com>
2023-04-21 07:19:23 +0000

    [FIX] stock/11.0: split query run into chunks
    Use left joint instead of sub-query.
    
    closes odoo/upgrade#4556
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/stock/11.0.1.1/pre-20-inventoryadjustment.py
+++ migrations/stock/11.0.1.1/pre-20-inventoryadjustment.py
@@ -3,7 +3,9 @@ from openerp.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    cr.execute("""
+    queries = util.explode_query_range(
+        cr,
+        """
 INSERT INTO stock_move_line	
     (move_id, product_id, product_uom_id, product_qty, product_uom_qty, qty_done, lot_id, owner_id, package_id, result_package_id, state,	
      location_id, location_dest_id, date, reference)
@@ -34,6 +36,7 @@ FROM stock_inventory_line il
      INNER JOIN stock_move m ON m.inventory_id = il.inventory_id AND m.product_id = il.product_id AND m.product_uom = il.product_uom_id
      INNER JOIN stock_location l1 ON l1.id = m.location_id
      INNER JOIN stock_location l2 ON l2.id = m.location_dest_id
+      LEFT JOIN stock_move_line sl ON sl.move_id = m.id 
 WHERE i.state = 'done'
   AND (
         (il.theoretical_qty>il.product_qty AND l1.usage!='inventory' AND l2.usage='inventory')
@@ -48,5 +51,10 @@ WHERE i.state = 'done'
         (il.partner_id IS NULL AND m.restrict_partner_id IS NULL)
         OR (il.partner_id IS NOT NULL AND il.partner_id=m.restrict_partner_id)
       )
-  AND NOT EXISTS (SELECT id FROM stock_move_line sl WHERE sl.move_id = m.id) 
-    """)
+  AND sl.id IS NULL
+        """,
+        table="stock_inventory_line",
+        alias="il",
+    )
+    for query in util.log_chunks(queries, util._logger, len(queries), qualifier="queries"):
+        cr.execute(query)
