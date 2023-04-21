cdb0860de4ab322f3898ec04479a66baa470c397
Julien Van Roy <juvr@odoo.com>
2023-04-21 14:54:39 +0000

    [IMP] account/saas~16.3: rename setting field `invoice_is_print`
    Rename the field `invoice_is_print` to `invoice_is_download` since it
    corresponds to the checkbox "Download".
    
    Community:https://github.com/odoo/odoo/pull/119397
    
    closes odoo/upgrade#4581
    
    Related: odoo/odoo#119397
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/saas~16.3.1.2/pre-migrate.py
+++ migrations/account/saas~16.3.1.2/pre-migrate.py
@@ -4,3 +4,6 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "account.move.line", "tax_audit")
+
+    util.rename_field(cr, "res.config.settings", "invoice_is_print", "invoice_is_download")
+    util.rename_field(cr, "res.company", "invoice_is_print", "invoice_is_download")
