feca262f4fcfef7e5def6b6040d4e805b13e4706
Alvaro Fuentes <afu@odoo.com>
2023-04-25 12:04:35 +0000

    [FIX] sale_stock/11.0: run query in //
    Part-of: odoo/upgrade#4590

--- migrations/sale_stock/11.0.1.0/pre-10-salelineid.py
+++ migrations/sale_stock/11.0.1.0/pre-10-salelineid.py
@@ -4,7 +4,12 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # Need sale_line_id on stock move instead of procurement.sale_line_id
     util.create_column(cr, 'stock_move', 'sale_line_id', 'int4')
-    cr.execute("""UPDATE stock_move sm
+    util.explode_execute(
+        cr,
+        """UPDATE stock_move sm
                 SET sale_line_id = p.sale_line_id
                 FROM procurement_order p WHERE sm.procurement_id = p.id
-    """)
+        """,
+        table="stock_move",
+        alias="sm",
+    )
