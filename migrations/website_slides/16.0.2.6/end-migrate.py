a1c93132a79eae6cdd17954580f47b073d3e5ca4
Atul Patel <atp@odoo.com>
2023-04-25 12:45:48 +0000

    [FIX] website_slides/16.0: set value for Channel Share Template
    `share_channel_template_id` new field introduced in Odoo 16
    here : https://github.com/odoo/odoo/commit/7d66dcbf544215b23033e85a6c3a0cf5e8ffe3e4#diff-d0df991fd453f06eef3ae33c2e82a89552d12c3debac422c774df9d051a323f3R254
    so for during upgrade for existing course(channel) record,
    default value not set as mail template `mail_template_channel_shared`
    which was created on same module, data file is loaded later
    before that it's try to set default value. so
    `share_channel_template_id` contain null value and that will
    create issue when we try to share the course.
    
    upg-588661
    unicorn task id: 3266178
    
    closes odoo/upgrade#4592
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/website_slides/16.0.2.6/end-migrate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        "UPDATE slide_channel SET share_channel_template_id = %s WHERE share_channel_template_id IS NULL",
+        [util.ref(cr, "website_slides.mail_template_channel_shared")],
+    )
