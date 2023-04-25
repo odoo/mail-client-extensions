ee8fe8add565345137c8cb4c9a672367785d2f70
Alvaro Fuentes <afu@odoo.com>
2023-04-25 12:02:28 +0000

    [FIX] mail/10.saas~14: // run query
    Set default value in //
    
    Part-of: odoo/upgrade#4590

--- migrations/mail/10.saas~14.1.0/pre-followers.py
+++ migrations/mail/10.saas~14.1.0/pre-followers.py
@@ -3,10 +3,15 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.create_column(cr, 'mail_followers', 'res_model_id', 'int4')
-    cr.execute("""
+    util.explode_execute(
+        cr,
+        """
         UPDATE mail_followers f
            SET res_model_id = m.id
           FROM ir_model m
          WHERE m.model = f.res_model
-    """)
+        """,
+        table="mail_followers",
+        alias="f",
+    )
     cr.execute("DELETE FROM mail_followers WHERE res_model_id IS NULL")
