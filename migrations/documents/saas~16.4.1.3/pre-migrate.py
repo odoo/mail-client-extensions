87f3a0dda02669f1d49da281dbe78d75dad2dce4
std-odoo <std@odoo.com>
2023-04-26 07:00:12 +0000

    [IMP] documents/saas~16.4: force all URLs to be valid
    Purpose
    =======
    Do not allow to store invalid URLs.
    
    Task-3283452
    See odoo/enterprise/pull/40344
    See odoo/enterprise/pull/40343
    
    closes odoo/upgrade#4596
    
    Related: odoo/enterprise#40344
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

---
+++ migrations/documents/saas~16.4.1.3/pre-migrate.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.explode_execute(
+        cr,
+        """
+        UPDATE documents_document
+           SET url = 'https://' || url
+         WHERE url IS NOT NULL
+           AND NOT starts_with(url, 'https://')
+           AND NOT starts_with(url, 'http://')
+           AND NOT starts_with(url, 'ftp://')
+        """,
+        table="documents_document",
+    )
