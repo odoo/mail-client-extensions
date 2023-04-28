674e70ad7ad70b1da247340a2bb83e13c1af6400
Loukas Wets (lowe) <lowe@odoo.com>
2023-04-28 10:42:52 +0000

    [ADD] pos_restaurant/saas~16.3.1.0: remove table management
    See https://github.com/odoo/odoo/pull/120117
    See https://github.com/odoo/enterprise/pull/40532
    
    closes odoo/upgrade#4622
    
    Related: odoo/enterprise#40532
    Related: odoo/odoo#120117
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/pos_restaurant/saas~16.3.1.0/pre-migrate.py
+++ migrations/pos_restaurant/saas~16.3.1.0/pre-migrate.py
@@ -11,3 +11,5 @@ def migrate(cr, version):
             util.ref(cr, "pos_restaurant.menu_restaurant_printer_all"),
         ],
     )
+    util.remove_field(cr, "pos.config", "is_table_management")
+    util.remove_field(cr, "res.config.settings", "pos_is_table_management")
