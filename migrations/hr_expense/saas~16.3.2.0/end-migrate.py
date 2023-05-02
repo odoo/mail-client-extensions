d84a40d09bafb955609f1b4b6a41863b5f40affe
Elias Regopoulos <elre@odoo.com>
2023-05-02 09:42:39 +0000

    [IMP] hr_expense/saas~16.3: recompute expense states
    The expense/expense report state rework implements a new state
    and requires recomputation
    
    related: odoo/odoo#120450
    task-3276501
    
    closes odoo/upgrade#4625
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/hr_expense/saas~16.3.2.0/end-migrate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # A new state is created, splitting the "reported" state into two new states
+    cr.execute("SELECT id FROM hr_expense WHERE state IN ('draft', 'reported')")
+    ids = [r[0] for r in cr.fetchall()]
+    util.recompute_fields(cr, "hr.expense", ["state"], ids=ids)
