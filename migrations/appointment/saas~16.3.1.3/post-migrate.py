f497009577e616ee2dff01f9ab71e2290016f1d2
Florian Charlier <flch@odoo.com>
2023-05-02 10:46:57 +0000

    [IMP] onboarding,appointment/saas~16.3: update panel layout fields
    Part of the webclient's redesign is an update of the onboarding panels.
    The appointment one uses the onboarding module to store layout
    information.
    There is no longer a background image nor color defined on the panel
    but images and alt text for each step.
    
    See odoo/odoo#116641
    See odoo/enterprise#38775
    
    Task-2818586
    
    closes odoo/upgrade#4652
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/appointment/saas~16.3.1.3/post-migrate.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    for onboarding_step_id in (
+        "appointment.appointment_onboarding_create_appointment_type_step",
+        "appointment.appointment_onboarding_preview_invite_step",
+        "appointment.appointment_onboarding_configure_calendar_provider_step",
+    ):
+        util.if_unchanged(cr, onboarding_step_id, util.update_record_from_xml)
