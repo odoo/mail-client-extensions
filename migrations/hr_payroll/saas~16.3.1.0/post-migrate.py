512db188137978fcda93c5871101cb4e451f7ab7
Kevin Baptiste <kba@odoo.com>
2023-05-02 09:10:32 +0000

    [IMP] hr_payroll/saas~16.3: use own model for payroll notes
    Do not depend anymore on note module, instead use a dedicated model.
    
    odoo/enterprise#40590
    
    task-3142411
    
    closes odoo/upgrade#4612
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ migrations/hr_payroll/saas~16.3.1.0/post-migrate.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    note_tag = util.ref(cr, "hr_payroll.payroll_note_tag")
+    if not note_tag:
+        return
+
+    cr.execute(
+        """
+        INSERT INTO hr_payroll_note(company_id, name, note, create_uid, write_uid, create_date, write_date)
+        SELECT n.company_id, n.name, n.memo, n.create_uid, n.write_uid, n.create_date, n.write_date
+          FROM note_note n
+          JOIN note_tags_rel nt
+            ON nt.note_id = n.id
+         WHERE nt.tag_id = %s
+    """,
+        [note_tag],
+    )
+
+    cr.execute("DELETE FROM note_note n USING note_tags_rel nt WHERE nt.note_id = n.id AND nt.tag_id = %s", [note_tag])
+
+    util.remove_record(cr, "hr_payroll.payroll_note_tag")
