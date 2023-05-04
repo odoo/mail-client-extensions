1efecd1f7104886dbcc6ab873047e9832694685e
Jurgen (jugj) <jugj@odoo.com>
2023-05-04 12:21:19 +0000

    [FIX] hr_appraisal/saas~16.4: Restrict manager viewing rights
    Enterprise PR: odoo/enterprise#40721
    
    task-3293253
    
    closes odoo/upgrade#4628
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/hr_appraisal/saas~16.4.1.1/pre-migrate.py
+++ migrations/hr_appraisal/saas~16.4.1.1/pre-migrate.py
@@ -7,3 +7,4 @@ def migrate(cr, version):
     util.remove_field(cr, "hr.appraisal.goal", "is_implicit_manager")
     util.remove_field(cr, "hr.appraisal", "is_appraisal_manager")
     util.remove_field(cr, "hr.appraisal", "is_implicit_manager")
+    util.if_unchanged(cr, "hr_appraisal.hr_appraisal_emp_rule", util.update_record_from_xml)
