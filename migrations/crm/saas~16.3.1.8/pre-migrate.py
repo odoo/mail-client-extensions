e3c5369d598ca28692608055be7dd2102cd4e660
Renaud Thiry <reth@odoo.com>
2023-05-08 07:25:15 +0000

    [FIX] crm/saas~16.3: undo mark_as_lost renaming
    In odoo/odoo#118494
    We replace the server action binding
    with a binding of the action window it called.
    This renders the action useless.
    
    We remove the renaming of the action added for odoo/odoo#116287 as
    it is pointless to rename it when it's not used.
    
    As the action remains functional, we don't remove it to allow users
    who use it in their customized flows can keep using it for now.
    Although it should be considered deprecated.
    
    task-3272955
    
    closes odoo/upgrade#4640
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/crm/saas~16.3.1.8/pre-migrate.py
+++ migrations/crm/saas~16.3.1.8/pre-migrate.py
@@ -3,7 +3,4 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    eb = util.expand_braces
-
-    util.rename_xmlid(cr, *eb("crm.action_{mark_as_lost,set_lost_with_reason}"))
-    util.update_record_from_xml(cr, "crm.set_lost_with_reason")
+    util.force_noupdate(cr, "crm.action_mark_as_lost", True)
