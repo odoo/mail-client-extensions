ea65f7b5306fbdd017c0e3138ee880a04fc66537
Alvaro Fuentes <afu@odoo.com>
2023-05-09 07:21:34 +0000

    [FIX] base/0.0.0: clean `__export__` entries in `ir_model_data`
    It speeds up considerably the indexing of this table and other
    operations with xmlids. We choose a high enough threshold to remove
    entries that seem to be automatically created per model.
    
    upg-761613
    
    ```
    761613_afu=> select model,count(*) from ir_model_data where module='__export__' group by model having count(*) > 1000000
    +--------------+-----------+
    | model        | count     |
    |--------------+-----------|
    | mrp.bom      | 7892216   |
    | mrp.bom.line | 198979018 |
    +--------------+-----------+
    ```
    
    closes odoo/upgrade#4648
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/0.0.0/pre-reindex-ir_model_data.py
+++ migrations/base/0.0.0/pre-reindex-ir_model_data.py
@@ -5,6 +5,29 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    if util.str2bool(os.getenv("UPG_NO_REINDEX_IMD", "0")):
-        return False
-    cr.execute("REINDEX TABLE ir_model_data")
+    if not util.str2bool(os.getenv("UPG_NO_CLEAN_EXPORT_IMD", "0")):
+        cr.execute(
+            """
+            WITH info AS (
+                SELECT model,
+                       count(*) AS rem
+                  FROM ir_model_data
+                 WHERE module = '__export__'
+              GROUP BY model
+                    -- more than 1 million imd intries ಠ_ಠ
+                HAVING count(*) > 1000000
+            ),
+            __ AS (
+            DELETE FROM ir_model_data d
+                  USING info
+                  WHERE d.module = '__export__'
+                    AND d.model = info.model
+            )
+            SELECT * FROM info
+            """
+        )
+        if cr.rowcount:
+            util._logger.info("Cleaned __export__ entries from ir_model_data: %s", cr.fetchall())
+
+    if not util.str2bool(os.getenv("UPG_NO_REINDEX_IMD", "0")):
+        cr.execute("REINDEX TABLE ir_model_data")
