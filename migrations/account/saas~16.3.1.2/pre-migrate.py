eafb921f52f44a1ab27f5ae94cb4372a24c84195
Sohail JAIDI (soja) <soja@odoo.com>
2023-05-11 14:44:27 +0000

    [IMP] account/saas~16.3: removing unused field
    See odoo/odoo#117961
    See odoo/enterprise#40919
    
    closes odoo/upgrade#4667
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/account/saas~16.3.1.2/pre-migrate.py
+++ migrations/account/saas~16.3.1.2/pre-migrate.py
@@ -4,7 +4,8 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "account.move.line", "tax_audit")
-
     util.rename_field(cr, "res.config.settings", "invoice_is_print", "invoice_is_download")
     util.rename_field(cr, "res.company", "invoice_is_print", "invoice_is_download")
     util.remove_record(cr, "account.action_automatic_entry")
+    util.remove_field(cr, "account.move.reversal", "date_mode")
+    util.remove_field(cr, "account.move.reversal", "refund_method")
