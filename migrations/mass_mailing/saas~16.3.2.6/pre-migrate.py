213421faca16e4a732b33fa373e3bcd9aa9c842d
qsm-odoo <qsm@odoo.com>
2023-05-11 16:09:14 +0000

    [REF] mass_mailing/saas~16.3: remove useless views
    task-3233465
    
    closes odoo/upgrade#4668
    
    Related: odoo/odoo#121202
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

---
+++ migrations/mass_mailing/saas~16.3.2.6/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "mass_mailing.snippet_options_extra_shapes")
+    util.remove_view(cr, "mass_mailing.snippet_options_image_styles")
