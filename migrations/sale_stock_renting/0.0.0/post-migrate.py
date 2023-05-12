b48ca713af870b58737b51e98c09459dfb61c20e
Atul Patel <atp@odoo.com>
2023-05-12 11:58:59 +0000

    [FIX] sale_stock_renting/0.0.0: fixed rental stock quant inconsistency
    whever customer upload database with demo data, and create sale order
    with retail product `rental_stock_product_1` with pick_up state
    which is not return yet then on hand qty mismatched with original
    database.
    
    In sale_stock_renting , we defined 1 rental product with 3 stock quant
    record in rental_stock_demo.xml file. so if we used one qty for rent
    using sale order , we had 2 on hand qty available in stock
    but after upgrade due to this , it will create 3 quant again
    and will get 3 on hand qty which is wrong. so fix that
    inconsistency in stock move we need to call post_int_hook
    method here forcefully.
    
    added symlink in sale_stock_rental app.
    see:
    https://github.com/odoo/enterprise/blob/6ea1824a84cdddb6c4dad2d7d055083aa8561089/sale_stock_renting/__init__.py#L10
    https://github.com/odoo/enterprise/blob/6ea1824a84cdddb6c4dad2d7d055083aa8561089/sale_stock_renting/data/rental_stock_demo.xml#L91
    
    upg-767455
    
    ```
    Traceback (most recent call last):
      File "/tmp/tmpo1ojrief/migrations/testing.py", line 211, in test_check
        self.check(value)
      File "/tmp/tmpo1ojrief/migrations/stock/tests/test_on_hand_quantity.py", line 41, in check
        self.assertEqual(before_results, self.convert_check(after_results), self.message)
    AssertionError: Lists differ: [[5, [313 chars]'], [111, '3'], [112, '6'], [113, '3'], [116, '2'], [122, '1']] != [[5, [313 chars]'], [111, '3'], [112, '6'], [113, '3'], [116, '3'], [122, '1']]
    
    First differing element 30:
    [116, '2']
    [116, '3']
    ```
    
    closes odoo/upgrade#4673
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/sale_stock_renting/0.0.0/post-migrate.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.sale_stock_renting import _ensure_rental_stock_moves_consistency
+
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    # Force the post_init_hook configuration
+    if util.version_gte("saas~16.1"):
+        env = util.env(cr)
+        _ensure_rental_stock_moves_consistency(env)
+
+    else:
+        _ensure_rental_stock_moves_consistency(cr, False)
