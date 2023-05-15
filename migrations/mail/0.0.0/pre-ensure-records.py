daa20760ee99abaeba5c9b746086f64e0022f1b7
Christophe Simonis <chs@odoo.com>
2023-05-15 13:28:20 +0000

    [FIX] mail/0.0.0: adapt script after model rename
    Oversight of 90625b95cb1163d64ae6de5ce543bb74fca6cf53
    
    closes odoo/upgrade#4676
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- migrations/mail/0.0.0/pre-ensure-records.py
+++ migrations/mail/0.0.0/pre-ensure-records.py
@@ -5,10 +5,15 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     # Prevents constraint mail_channel_partner_partner_unique from failing
     if util.version_gte("saas~15.1"):
-        # table mail_channel_partner was renamed to mail_channel_member in 15.5
-        name = "member" if util.table_exists(cr, "mail_channel_member") else "partner"
-        # table mail_channel_member was renamed to discuss_channel_member in 16.3
-        prefix = "discuss" if util.table_exists(cr, "discuss_channel_member") else "mail"
+        if util.table_exists(cr, "discuss_channel_member"):
+            # >= saas~16.3
+            prefix, name = "discuss", "member"
+        elif util.table_exists(cr, "mail_channel_member"):
+            # >= saas~15.5
+            prefix, name = "mail", "member"
+        else:
+            prefix, name = "mail", "partner"
+
         util.ensure_xmlid_match_record(
             cr,
             "mail.channel_{}_general_channel_for_admin".format(name),
