02732c5d3e520164e1578ed99a3bf5b73a438f65
William Henrotin <whe@odoo.com>
2023-05-16 12:32:13 +0000

    [IMP] stock/saas~16.3: picking workflow refactoring
    The creation and process of stock picking hase been reworked. This
    implies the deletion of the stock quant reserve wizard. as well as the
    the `show_mark_as_todo' field.
    
    See odoo/odoo#117513
    and odoo/enterprise#41111
    
    closes odoo/upgrade#4682
    
    Task: 3256447
    Signed-off-by: Christophe Simonis <chs@odoo.com>

---
+++ migrations/stock/saas~16.3.1.1/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "stock.picking", "show_mark_as_todo")
+    util.remove_model(cr, "stock.quant.reserve")
+    util.remove_model(cr, "stock.quant.reserve.line")
