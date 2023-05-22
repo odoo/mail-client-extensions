8fbf512754d9dac3c7e9b2187ce57692166c5aec
Antoine (anso) <anso@odoo.com>
2023-05-22 12:16:29 +0000

    [IMP] website_customer/saas~16.4: update classname
    The current "default" class name renders hardly visible (white text on
    white bg) tags in the UI -> It has to be changed to "light" to make
    the tags fully visible
    
    See https://github.com/odoo/odoo/pull/109752
    task-3083706
    
    closes odoo/upgrade#4690
    
    Related: odoo/odoo#109752
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/website_customer/saas~16.4.1.0/pre-migrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.change_field_selection_values(cr, "res.partner.tag", "classname", {"default": "light"})
