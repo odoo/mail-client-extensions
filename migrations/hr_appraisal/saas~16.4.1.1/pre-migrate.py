fbf0f68519b93febdcf2f9f93a92c7f8a37f3642
dasz <dasz@odoo.com>
2023-05-24 09:11:37 +0000

    [FIX] hr_appraisal/saas~16.4: remove fields not used anymore
    implicit/appraisal manager fields getting merged into is_manager single field
    see odoo/enterprise#41398
    task 3217812
    
    closes odoo/upgrade#4698
    
    Related: odoo/enterprise#41541
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/hr_appraisal/saas~16.4.1.1/pre-migrate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "hr.appraisal.goal", "is_implicit_manager")
+    util.remove_field(cr, "hr.appraisal", "is_appraisal_manager")
+    util.remove_field(cr, "hr.appraisal", "is_implicit_manager")
