8737adb2dafbd0c051902cca8f8321d2f54a3e4e
agrabsi <angr@odoo.com>
2023-05-25 13:52:01 +0000

    [FIX] l10n_in_hr_payroll/saas~16.2: `delete_unused` salary
    in saas~16.2 we removed a couple of salary rules, strctures and types.
    
    These we not handled in this migration script leading
    to foreign key violation (RESTRICT).
    
    upg-778236
    
    closes odoo/upgrade#4711
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/l10n_in_hr_payroll/saas~16.2.1.0/pre-migrate.py
+++ migrations/l10n_in_hr_payroll/saas~16.2.1.0/pre-migrate.py
@@ -6,7 +6,22 @@ def migrate(cr, version):
     util.remove_view(cr, "l10n_in_hr_payroll.report_india_payslip")
     util.remove_view(cr, "l10n_in_hr_payroll.view_res_company_da")
 
+    # Removed salary structures and types
     util.delete_unused(cr, "l10n_in_hr_payroll.structure_0002")
+    util.delete_unused(cr, "l10n_in_hr_payroll.structure_worker_0001")
+    util.delete_unused(cr, "l10n_in_hr_payroll.hr_payroll_salary_structure_ind_emp")
+    util.delete_unused(cr, "l10n_in_hr_payroll.hr_payroll_salary_structure_type_ind_emp")
+
+    # Removed salary rules
+    util.delete_unused(cr, "l10n_in_hr_payroll.hr_salary_rule_da")
+    util.delete_unused(cr, "l10n_in_hr_payroll.hr_salary_trans_allownce")
+    util.delete_unused(cr, "l10n_in_hr_payroll.hr_salary_rule_lta")
+    util.delete_unused(cr, "l10n_in_hr_payroll.hr_salary_rule_le")
+    util.delete_unused(cr, "l10n_in_hr_payroll.hr_salary_rule_performance")
+    util.delete_unused(cr, "l10n_in_hr_payroll.hr_salary_rule_bonus")
+    util.delete_unused(cr, "l10n_in_hr_payroll.hr_salary_rule_medical_allow")
+    util.delete_unused(cr, "l10n_in_hr_payroll.hr_payslip_line_professional_tax")
+    util.delete_unused(cr, "l10n_in_hr_payroll.hr_payslip_rule_employee_esic")
 
     util.rename_field(cr, "res.company", "dearness_allowance", "l10n_in_dearness_allowance")
 
