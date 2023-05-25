7f65394a97bdb300cc980f9b2036f295130986e1
Yannick Tivisse <yti@odoo.com>
2023-05-25 12:44:06 +0000

    [IMP] hr_payroll/saas~16.4: Improve salary attachments
    1/ Convert deduction_type from selection to M2O
    
    The idea is to allow using salary attachment to be linked to other inputs
    more easily to configure recurrent inputs (either positive or negative)
    on payslips.
    
    2/ Allow multi employees attachments
    
    3/ Remove note from hr.payslip.line
    
    Not visible anymore, and quite useless as this is a duplicate
    from the salary rule note.
    
    4/ Make salary rule note translatable
    
    Ent PR: https://github.com/odoo/enterprise/pull/41109
    
    closes odoo/upgrade#4679
    
    Taskid: 3029662
    Related: odoo/enterprise#41109
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ migrations/hr_payroll/saas~16.4.1.0/post-migrate.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    query = """
+        UPDATE hr_salary_attachment a
+        SET
+            deduction_type_id = CASE deduction_type
+                WHEN 'attachment' THEN %s
+                WHEN 'assignment' THEN %s
+                WHEN 'child_support' THEN %s
+                END,
+            no_end_date = (deduction_type = 'child_support')
+    """
+    query = cr.mogrify(
+        query,
+        [
+            util.ref(cr, "hr_payroll.hr_salary_attachment_type_attachment"),
+            util.ref(cr, "hr_payroll.hr_salary_attachment_type_assignment"),
+            util.ref(cr, "hr_payroll.hr_salary_attachment_type_child_support"),
+        ],
+    ).decode()
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="hr_salary_attachment", alias="a"))
+    util.remove_column(cr, "hr_salary_attachment", "deduction_type")
---
+++ migrations/hr_payroll/saas~16.4.1.0/pre-migrate.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "hr.salary.attachment", "deduction_type", drop_column=False)
+    util.create_column(cr, "hr_salary_attachment", "no_end_date", "boolean")
+
+    util.create_m2m(cr, "hr_employee_hr_salary_attachment_rel", "hr_employee", "hr_salary_attachment")
+    cr.execute(
+        """
+        INSERT INTO hr_employee_hr_salary_attachment_rel(hr_employee_id, hr_salary_attachment_id)
+             SELECT employee_id, id
+               FROM hr_salary_attachment
+              WHERE employee_id is NOT NULL
+    """
+    )
+    util.update_field_usage(cr, "hr.salary.attachment", "employee_id", "employee_ids")
+    util.remove_field(cr, "hr.salary.attachment", "employee_id")
+
+    util.remove_field(cr, "hr.payslip.line", "note")
+    util.remove_field(cr, "hr.payroll.edit.payslip.line", "note")
+
+    util.convert_field_to_translatable(cr, "hr.salary.rule", "note")
