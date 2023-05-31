6c7070a8644bacbc0ca0c99d13b5fd26f9f187d1
Julien Alardot (jual) <jual@odoo.com>
2023-05-31 15:29:30 +0000

    [IMP] hr_expense/saas~16.4: reverse move and expense_sheet relationship
    We need to allow several moves to be linked to one
    expense sheet, to simplify reconciliation
    and multi-currencies handling
    
    Prior to this change
    hr.expense.sheet.account_move_id -Many2One< account.move.id
    account.move.expense_sheet_id >One2Many- hr.expense.sheet.id
    This was in reality a One2One relationship
    
    After this change
    hr.expense.sheet.account_move_ids >One2Many- account.move.id
    account.move.expense_sheet_id -Many2One< hr.expense.sheet.id
    
    see odoo/odoo#126805
    Task-3292188
    
    closes odoo/upgrade#4873
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/hr_expense/saas~16.4.2.0/pre-migrate.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # Reverse the relation of hr.expense.sheet.account_move_id and account.move.expense_sheet_id
+    # to allow multiple moves linked to one expense.sheet
+
+    util.create_column(cr, "account_move", "expense_sheet_id", "int4")
+    util.explode_execute(
+        cr,
+        """
+        WITH am_hes AS (
+            SELECT hes.account_move_id AS move_id,
+                   min(hes.id) AS sheet_id
+              FROM hr_expense_sheet AS hes
+              JOIN account_move am -- for // filter
+                ON hes.account_move_id = am.id
+             WHERE {parallel_filter}
+             GROUP BY hes.account_move_id
+            )
+        UPDATE account_move am
+           SET expense_sheet_id = am_hes.sheet_id
+          FROM am_hes
+         WHERE am.id = am_hes.move_id
+        """,
+        table="account_move",
+        alias="am",
+    )
+    util.remove_field(cr, "hr.expense.sheet", "account_move_id")
