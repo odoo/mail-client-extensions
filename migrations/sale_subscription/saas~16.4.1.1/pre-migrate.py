2a49d50aeb09162b1ff392d48173e864334ba919
Florian Damhaut <flda@odoo.com>
2023-06-02 08:21:41 +0000

    [IMP] sale_subscription/saas~16.4.1.0: do todo, remove field
    enterprise PR : odoo/enterprise#39367
    
    Remove todo from sale_subscription.
    4 fields are removed in ent PR
    
    closes odoo/upgrade#4606
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/sale_subscription/saas~16.4.1.1/pre-migrate.py
+++ migrations/sale_subscription/saas~16.4.1.1/pre-migrate.py
@@ -5,3 +5,8 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.change_field_selection_values(cr, "sale.order.alert", "state", {"done": "sale"})
+    util.remove_field(cr, "sale.order", "industry_id")
+    util.remove_field(cr, "sale.order", "country_id")
+    util.remove_field(cr, "sale.order.template", "color")
+    util.remove_field(cr, "sale.order.line", "pricing_id")
+    util.remove_view(cr, "sale_subscription.sale_order_line_view_tree")
