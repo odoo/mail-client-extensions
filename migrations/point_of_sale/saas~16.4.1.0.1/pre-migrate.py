8f85c321d8b7d49280d0fb2ad5a589ca15a9cbcc
vlst <vlst@odoo.com>
2023-06-06 12:29:39 +0000

    [IMP] point_of_sale/saas~16.4: Create pos_categ_ids field
    Currently, a product can only be part of one `pos_category`.
    This PR changes the relationship between the `product`
    and the `pos_category` from a `One2many` to a `Many2many`.
    
    See odoo/odoo#122907
    See odoo/enterprise#41791
    
    closes odoo/upgrade#4739
    
    Task: 3138825
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/point_of_sale/saas~16.4.1.0.1/pre-migrate.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_m2m(cr, "pos_category_product_template_rel", "pos_category", "product_template")
+    cr.execute(
+        """
+            INSERT INTO pos_category_product_template_rel
+                        (product_template_id, pos_category_id)
+            SELECT id,
+                   pos_categ_id
+            FROM   product_template
+            WHERE  pos_categ_id IS NOT NULL
+        """
+    )
+    util.update_field_usage(cr, "product.template", "pos_categ_id", "pos_categ_ids")
+    util.remove_field(cr, "product.template", "pos_categ_id")
+    util.remove_field(cr, "report.pos.order", "pos_categ_id")
