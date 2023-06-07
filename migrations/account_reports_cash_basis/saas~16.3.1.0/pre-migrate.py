08818b3bdbcfb592ba9e53e32e746051e2f1e739
Christophe Simonis <chs@odoo.com>
2023-06-07 09:43:52 +0000

    [FIX] account_reports_cash_basis/saas~16.3: correct xmlid
    🤦
    
    closes odoo/upgrade#4744
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account_reports_cash_basis/saas~16.3.1.0/pre-migrate.py
+++ migrations/account_reports_cash_basis/saas~16.3.1.0/pre-migrate.py
@@ -3,5 +3,5 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.remove_view(cr, "account_report_cash_basis.search_template_extra_options")
-    util.remove_view(cr, "account_report_cash_basis.filter_cash_basis_template")
+    util.remove_view(cr, "account_reports_cash_basis.search_template_extra_options")
+    util.remove_view(cr, "account_reports_cash_basis.filter_cash_basis_template")
