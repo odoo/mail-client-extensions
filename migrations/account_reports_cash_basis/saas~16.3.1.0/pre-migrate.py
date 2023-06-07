0b9565d77206e594362ffd818e44779621829e3d
Christophe Simonis <chs@odoo.com>
2023-06-07 09:02:23 +0000

    [FIX] account_reports_cash_basis/saas~16.3
    Remove not forward-ported view.
    
    See odoo/enterprise#41680
    
    Part-of: odoo/upgrade#4744

--- migrations/account_reports_cash_basis/saas~16.3.1.0/pre-migrate.py
+++ migrations/account_reports_cash_basis/saas~16.3.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "account_report_cash_basis.search_template_extra_options")
+    util.remove_view(cr, "account_report_cash_basis.filter_cash_basis_template")
