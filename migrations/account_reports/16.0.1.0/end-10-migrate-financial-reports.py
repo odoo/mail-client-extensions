74a1e1b15c40ebcd6b6e5ab43a1b00278034e90c
Edoardo Piroli <pied@odoo.com>
2023-06-08 08:46:36 +0000

    [FIX] account_reports/16.0: Migrate custom financial reports
    [OPW-3110853](
    https://www.odoo.com/web#id=3110853&cids=1&model=project.task&view_type=form)
    [OPW-3215509](
    https://www.odoo.com/web#id=3215509&cids=1&model=project.task&view_type=form)
    
    After https://github.com/odoo/enterprise/pull/28648,
    `account.financial.html.report{,.line}` models are
    [dropped](https://github.com/odoo/upgrade/blob/master/migrations/account_reports/16.0.1.0/pre-migrate.py#L48)
    and replaced by `account.report{,.line,.expression}`, without saving
    their records.
    
    This causes custom reports (and their lines) to be lost when migrating
    to Odoo 16.
    
    The script introduced by this commit allows (when setting the proper
    environment variable) to migrate basic custom (and/or duplicated)
    reports, their lines and expressions to the new models/tables
    ('account.report{,.line,.expression,.column}').
    
    This script does not attempt to cover all corner cases and ensure
    perfect compatibility with the new engine. Customers are highly
    encouraged to double check the migrated reports and either fix or
    recreate their financial reports anew.
    
    closes odoo/upgrade#4495
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/account_reports/16.0.1.0/end-10-migrate-financial-reports.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+import logging
+import os
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    if not os.getenv("ODOO_MIG_16_MIGRATE_CUSTOM_FINANCIAL_REPORTS") or not util.column_exists(
+        cr, "account_report_line", "v15_line_id"
+    ):
+        return
+
+    _logger = logging.getLogger(__name__)
+
+    # constant regexes
+    TERM_CODE_REGEX = r"\w+"
+    DOMAIN_EXPR_REGEX = r"-?sum(?:_if_(?:pos|neg))?"
+
+    # There are 2 cases under which we need to migrate extra lines:
+    #   1. Migrated aggregation formulas make use of lines that used to be standard in v15 and are no longer there in Odoo 16.
+    #   2. Migrated aggregation formulas make use of lines with report_id = NULL
+    # In both cases, they are detected with the following query and migrated later on
+    cr.execute(
+        rf"""
+        WITH codes_by_id AS (
+            SELECT
+                REGEXP_SPLIT_TO_TABLE(
+                    REGEXP_REPLACE(
+                        are.formula,
+                        '(?:^-|\.balance|\(|\)|{DOMAIN_EXPR_REGEX})',
+                        ' ',
+                        'g'
+                    ),
+                    '[\s\+\*\-/]+'
+                ) AS code,
+                are.report_line_id
+              FROM account_report_expression are
+              JOIN account_report_line arl
+                ON arl.id = are.report_line_id
+               AND arl.v15_line_id IS NOT NULL
+             WHERE are.engine = 'aggregation'
+        ),
+        codes_to_migrate_by_id AS (
+            SELECT code, report_line_id
+              FROM codes_by_id c
+             WHERE NOT EXISTS (SELECT 1 FROM account_report_line WHERE code = c.code)
+               AND c.code != ''
+        )
+        SELECT report_line_id, ARRAY_AGG(code)
+          FROM codes_to_migrate_by_id
+         GROUP BY report_line_id
+        """
+    )
+
+    for report_line_id, codes_to_migrate in cr.fetchall():
+        migrated_codes = set()
+
+        while codes_to_migrate:
+            code = codes_to_migrate.pop()
+
+            # migrate term's formula to a subexpression of the faulty expression
+            cr.execute(
+                rf"""
+                INSERT INTO account_report_expression (
+                    create_uid, write_uid, create_date, write_date, report_line_id,
+                    label, date_scope, figure_type, green_on_positive, auditable,
+                    engine,
+                    formula,
+                    subformula
+                )
+                SELECT afhrl.create_uid, afhrl.write_uid, afhrl.create_date, NOW(), %s,
+                       LOWER(afhrl.code), afhrl.special_date_changer, afhrl.figure_type, afhrl.green_on_positive, True,
+                       -- engine
+                          CASE
+                                WHEN afhrl.formulas ~ ('^{DOMAIN_EXPR_REGEX}$') THEN 'domain'
+                                ELSE 'aggregation'
+                          END,
+                       -- formula
+                          CASE
+                                WHEN afhrl.formulas ~ ('^{DOMAIN_EXPR_REGEX}$') THEN afhrl.domain
+                                ELSE REGEXP_REPLACE(afhrl.formulas, '({TERM_CODE_REGEX})', '\1.balance', 'g')
+                          END,
+                       -- subformula
+                          CASE
+                                WHEN afhrl.formulas ~ ('^{DOMAIN_EXPR_REGEX}$') THEN afhrl.formulas
+                                ELSE 'cross_report'
+                          END
+                  FROM ___upgrade_afhrl afhrl
+                 WHERE afhrl.code = %s
+             RETURNING id
+                """,
+                [report_line_id, code],
+            )
+
+            if not cr.rowcount:
+                # no expression was added because of the `afhrl.code = %s` check, thus the code doesn't exist in the origin db
+                # either something was already wrong or the formula parsing ignores some corner case.
+                _logger.error(
+                    "term code `%s` was not found in the source db. Some reports may be affected.",
+                    code,
+                )
+                continue
+
+            (subexp_id,) = cr.fetchone()
+
+            # replace $term_code.balance in formula with $line_code.$term_code
+            cr.execute(
+                r"""
+                UPDATE account_report_expression are
+                   SET formula = REGEXP_REPLACE(
+                           are.formula,
+                           '(?<=\s|^)' || %s || '\.balance',
+                           arl.code || '.' || LOWER(%s),
+                           'g'
+                       )
+                  FROM account_report_line arl
+                 WHERE are.report_line_id = arl.id
+                   AND arl.id = %s
+                """,
+                [code, code, report_line_id],
+            )
+
+            # migrated code's formula might contain more codes to migrate separately
+            cr.execute(
+                rf"""
+                WITH new_codes AS (
+                    SELECT
+                        REGEXP_SPLIT_TO_TABLE(
+                            REGEXP_REPLACE(
+                                are.formula,
+                                '(?:^-|\.balance|\(|\)|{DOMAIN_EXPR_REGEX})',
+                                '',
+                                'g'
+                            ),
+                            '[\s\+\*\-/]+'
+                        ) AS code
+                      FROM account_report_expression are
+                     WHERE are.id = %s
+                       AND are.engine = 'aggregation'
+                )
+                SELECT code
+                  FROM new_codes c
+                 WHERE NOT EXISTS (SELECT 1 FROM account_report_line WHERE code = c.code)
+                   AND code IS NOT NULL
+                 GROUP BY code
+                """,
+                [subexp_id],
+            )
+
+            # keep track of already migrated codes for the current expression…
+            migrated_codes.add(code)
+            codes_to_migrate.extend(r[0] for r in cr.fetchall() if r[0] not in migrated_codes)
+
+    util.remove_column(cr, "account_report_line", "v15_line_id")
+
+    cr.execute("DROP TABLE ___upgrade_afhrl")
+
+    # some terms may include both aggregators and term codes - split them into different subexpressions
+    # also be aware that, in such mixed formulas, the aggregators will have an extra '.balance' that needs to be discarded
+    cr.execute(
+        rf"""
+        INSERT INTO account_report_expression (
+            create_uid, write_uid, create_date, write_date, report_line_id,
+            label, engine, formula, subformula,
+            date_scope, figure_type, green_on_positive, auditable
+        )
+        SELECT are.create_uid, are.write_uid, are.create_date, NOW(), arl.id,
+               'agg', 'domain', arl.v15_domain, SUBSTRING(are.formula FROM '({DOMAIN_EXPR_REGEX})'),
+               are.date_scope, are.figure_type, are.green_on_positive, are.auditable
+          FROM account_report_expression are
+          JOIN account_report_line arl
+            ON arl.id = are.report_line_id
+         WHERE engine = 'aggregation'
+           AND formula ~ '(?<=\s|^){DOMAIN_EXPR_REGEX}\.balance(?=\s|$)'
+        """
+    )
+
+    cr.execute(
+        rf"""
+        UPDATE account_report_expression are
+           SET formula = REGEXP_REPLACE(are.formula, '{DOMAIN_EXPR_REGEX}\.balance', arl.code || '.agg')
+          FROM account_report_line arl
+         WHERE are.engine = 'aggregation'
+           AND are.formula ~ '(?<=\s|^){DOMAIN_EXPR_REGEX}\.balance(?=\s|$)'
+           AND are.report_line_id = arl.id
+        """
+    )
+
+    util.remove_column(cr, "account_report_line", "v15_domain")
---
+++ migrations/account_reports/16.0.1.0/pre-10-migrate-financial-reports.py
@@ -0,0 +1,344 @@
+# -*- coding: utf-8 -*-
+import os
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # get ids of custom financial reports
+    cr.execute(
+        """
+        SELECT afhr.id, afhr.name
+          FROM account_financial_html_report afhr
+     LEFT JOIN ir_model_data imd
+            ON imd.res_id = afhr.id
+           AND imd.model = 'account.financial.html.report'
+         WHERE imd IS NULL
+        """
+    )
+
+    if not cr.rowcount:
+        return
+
+    custom_fin_reports = cr.fetchall()
+
+    if not os.getenv("ODOO_MIG_16_MIGRATE_CUSTOM_FINANCIAL_REPORTS"):
+        util.add_to_migration_reports(
+            """
+                <details>
+                    <summary>
+                        Due to a complete revamp of the reporting engine in Odoo 16, custom financial reports are not migrated by
+                        default.
+                        The general suggestion is to try to fallback on the new capabilities of the standard reports, and recreate
+                        your reports from scratch, if necessary.
+                        However, if this would be too inconvenient, there is a possibility that opening a support ticket might help,
+                        based on your specific case.
+                        The custom financial reports that are being lost in this migration are the following ones:
+                    </summary>
+                    <ul>%s</ul>
+                </details>
+            """
+            % " ".join([f"<li>{name} (id: {id})</li>" for id, name in custom_fin_reports]),
+            category="Financial Reports",
+            format="html",
+        )
+        return
+
+    fin_report_ids = tuple(res[0] for res in custom_fin_reports)
+
+    # create temporary column to have mapping between the old and the new report ids
+    util.create_column(cr, "account_report", "v15_fin_report_id", "integer")
+
+    # migrate custom financial reports from account_financial_html_report to account_report
+    cr.execute(
+        """
+        INSERT INTO account_report (
+            create_uid, write_uid, create_date, write_date, name,
+            filter_unfold_all, filter_analytic, filter_period_comparison, filter_journals, filter_multi_company,
+            filter_date_range, default_opening_date_filter,
+            country_id, availability_condition,
+            only_tax_exigible, v15_fin_report_id
+        )
+        SELECT create_uid, write_uid, create_date, NOW(), JSONB_BUILD_OBJECT('en_US', name),
+               unfold_all_filter, analytic, comparison, show_journal_filter, 'selector',
+               date_range, CASE WHEN date_range THEN 'this_year' ELSE 'today' END,
+               country_id, CASE WHEN country_id IS NOT NULL THEN 'country' ELSE 'always' END,
+               tax_report, id
+          FROM account_financial_html_report
+         WHERE id IN %s
+     RETURNING id, name->>'en_US'
+        """,
+        [fin_report_ids],
+    )
+
+    new_custom_fin_reports = cr.fetchall()
+
+    cr.execute(
+        """
+        INSERT INTO account_report_column (
+            create_uid, write_uid, create_date, write_date,
+            report_id, name, expression_label, figure_type,
+            sortable, blank_if_zero
+        )
+        SELECT create_uid, write_uid, create_date, NOW(),
+               id, JSONB_BUILD_OBJECT('en_US', 'Balance'), 'balance', 'monetary',
+               True, False
+          FROM account_report
+         WHERE v15_fin_report_id IS NOT NULL
+        """
+    )
+
+    # update ir_action_client context for migrated reports
+    cr.execute(
+        """
+        UPDATE ir_act_client iac
+           SET context = '{"report_id": ' || ar.id || '}'
+          FROM account_report ar
+          JOIN account_financial_html_report afhr
+            ON afhr.id = ar.v15_fin_report_id
+           AND ar.v15_fin_report_id IS NOT NULL
+          JOIN ir_ui_menu ium
+            ON ium.id = afhr.generated_menu_id
+         WHERE iac.id = SPLIT_PART(ium.action, ',', 2)::integer
+        """
+    )
+
+    # preserve menus and actions
+    cr.execute(
+        """
+        WITH to_preserve AS (
+            SELECT afhr.generated_menu_id,
+                   SPLIT_PART(ium.action, ',', 2)::integer AS action_id
+              FROM account_financial_html_report afhr
+              JOIN ir_ui_menu ium
+                ON ium.id = afhr.generated_menu_id
+             WHERE afhr.id IN %s
+        )
+        UPDATE ir_model_data imd
+           SET noupdate = True,
+               name = REPLACE(imd.name, 'account_financial_html_report', 'account_report')
+          FROM to_preserve
+         WHERE (imd.res_id, imd.model) IN (
+                   (to_preserve.generated_menu_id, 'ir.ui.menu'),
+                   (to_preserve.action_id, 'ir.actions.client')
+               )
+        """,
+        [fin_report_ids],
+    )
+
+    cr.execute(
+        "UPDATE account_financial_html_report SET generated_menu_id = NULL WHERE id IN %s",
+        [fin_report_ids],
+    )
+
+    # create temporary column to have mapping between the old and the new report line ids (for parent_id)
+    util.create_column(cr, "account_report_line", "v15_line_id", "integer")
+
+    # create temporary columns for fields that are to be migrated to account_report_expression
+    util.create_column(cr, "account_report_line", "v15_formulas", "varchar")
+    util.create_column(cr, "account_report_line", "v15_figure_type", "varchar")
+    util.create_column(cr, "account_report_line", "v15_green_on_positive", "bool")
+    util.create_column(cr, "account_report_line", "v15_special_date_changer", "varchar")
+    util.create_column(cr, "account_report_line", "v15_domain", "varchar")
+
+    # constant regexes
+    TERM_CODE_REGEX = r"\w+"
+    DOMAIN_EXPR_REGEX = r"-?sum(?:_if_(?:pos|neg))?"
+
+    # migrate custom reports' lines from account_financial_html_report_line to account_report_line
+    cr.execute(
+        rf"""
+        WITH mapping AS (
+            SELECT ar.id AS report_id,
+                   afhrl.id AS line_id
+              FROM account_report ar
+              JOIN account_financial_html_report_line afhrl
+                ON afhrl.financial_report_id = ar.v15_fin_report_id
+               AND ar.v15_fin_report_id IS NOT NULL
+        )
+        INSERT INTO account_report_line (
+            create_uid, write_uid, create_date, write_date,
+            report_id, name,
+            hierarchy_level, sequence, action_id, code, print_on_new_page, hide_if_zero,
+            groupby, foldable, v15_domain,
+            v15_green_on_positive, v15_special_date_changer, v15_line_id,
+            v15_formulas,
+            v15_figure_type
+        )
+        SELECT afhrl.create_uid, afhrl.write_uid, afhrl.create_date, NOW(),
+               m.report_id, JSONB_BUILD_OBJECT('en_US', afhrl.name),
+               0, afhrl.sequence, afhrl.action_id, afhrl.code, afhrl.print_on_new_page, afhrl.hide_if_zero,
+               afhrl.groupby, afhrl.groupby IS NOT NULL AND afhrl.show_domain = 'foldable', afhrl.domain,
+               afhrl.green_on_positive, afhrl.special_date_changer, afhrl.id,
+               REGEXP_REPLACE(afhrl.formulas, '({TERM_CODE_REGEX})', '\1.balance', 'g'),
+               CASE
+                    WHEN afhrl.figure_type = 'float' THEN NULL
+                    ELSE afhrl.figure_type
+               END
+          FROM mapping m
+          JOIN account_financial_html_report_line afhrl
+            ON afhrl.id = m.line_id
+        """
+    )
+
+    # migrate custom reports' lines, that were children of other migrated lines (for which financial_report_id was not set in v15)
+    while cr.rowcount:
+        cr.execute(
+            rf"""
+            WITH mapping AS (
+                SELECT arl.report_id,
+                       arl.id AS parent_id,
+                       arl.hierarchy_level AS parent_hierarchy_level,
+                       afhrl.id AS line_id
+                  FROM account_financial_html_report_line afhrl
+                  JOIN account_report_line arl
+                    ON arl.v15_line_id = afhrl.parent_id
+                   AND arl.v15_line_id IS NOT NULL
+                   AND NOT EXISTS (SELECT 1 FROM account_report_line WHERE v15_line_id = afhrl.id)
+            )
+            INSERT INTO account_report_line (
+                create_uid, write_uid, create_date, write_date,
+                report_id, name,
+                parent_id, hierarchy_level, sequence, action_id, code, print_on_new_page, hide_if_zero,
+                groupby, foldable, v15_domain,
+                v15_green_on_positive, v15_special_date_changer, v15_line_id,
+                v15_formulas,
+                v15_figure_type
+            )
+            SELECT afhrl.create_uid, afhrl.write_uid, afhrl.create_date, NOW(),
+                   m.report_id, JSONB_BUILD_OBJECT('en_US', afhrl.name),
+                   m.parent_id, m.parent_hierarchy_level + 2, afhrl.sequence, afhrl.action_id, afhrl.code, afhrl.print_on_new_page, afhrl.hide_if_zero,
+                   afhrl.groupby, afhrl.groupby IS NOT NULL AND afhrl.show_domain = 'foldable', afhrl.domain,
+                   afhrl.green_on_positive, afhrl.special_date_changer, afhrl.id,
+                   REGEXP_REPLACE(afhrl.formulas, '({TERM_CODE_REGEX})', '\1.balance', 'g'),
+                   CASE
+                        WHEN afhrl.figure_type = 'float' THEN NULL
+                        ELSE afhrl.figure_type
+                   END
+              FROM mapping m
+              JOIN account_financial_html_report_line afhrl
+                ON afhrl.id = m.line_id
+            """
+        )
+
+    # restore lines order (`sequence` field)
+    # in v15 a line's `sequence` would only compare to its siblings to establish the order among themselves
+    # in v16 lines are sorted purely by the (sequence, id). Meaning that if you have the following 3 lines:
+    #   * A, sequence 1
+    #   * A1, sequence 3, parent_id = A, hierarchy_level = 2
+    #   * B, sequence 2
+    # they will display as follows:
+    #   * A
+    #   * B
+    #       * A1
+    # which was never the case in v15 (parent-child relationship was alwasy respected no matter the value of `sequence`)
+    # The following query translates v15 conventions into v16 rules through the use of `sort` a column made up of each ancestors'
+    # sequence and their old `v15_line_id` (used as a tiebreaker for siblings with same `sequence`).
+    cr.execute(
+        """
+        WITH RECURSIVE _lines AS (
+            SELECT id,
+                   LPAD(sequence::text, 5, '0') || '.' || LPAD(v15_line_id::text, 5, '0') AS sort,
+                   report_id
+              FROM account_report_line
+             WHERE parent_id IS NULL
+
+             UNION ALL
+
+            SELECT arl.id,
+                   l.sort || '.' || LPAD(arl.sequence::text, 5, '0') || '.' || LPAD(arl.v15_line_id::text, 5, '0') AS sort,
+                   arl.report_id
+              FROM account_report_line arl
+              JOIN _lines l
+                ON l.id = arl.parent_id
+        ),
+        _lines_ordered AS (
+            SELECT id,
+                   (ROW_NUMBER() OVER (PARTITION BY report_id ORDER BY sort))*10 AS sequence
+              FROM _lines
+        )
+        UPDATE account_report_line arl
+           SET sequence = lo.sequence
+          FROM _lines_ordered lo
+         WHERE arl.id = lo.id
+        """
+    )
+
+    # v15_fin_report_id no longer needed
+    util.remove_column(cr, "account_report", "v15_fin_report_id")
+
+    # populate account_report_expression with formulas and other fields from report lines
+    cr.execute(
+        rf"""
+        INSERT INTO account_report_expression (
+            create_uid, write_uid, create_date, write_date,
+            report_line_id, label,
+            date_scope, figure_type, green_on_positive, auditable,
+            engine,
+            formula,
+            subformula
+        )
+        SELECT arl.create_uid, arl.write_uid, arl.create_date, NOW(),
+               arl.id, 'balance',
+               arl.v15_special_date_changer, arl.v15_figure_type, arl.v15_green_on_positive, True,
+               -- engine
+               -- note that some formulas might contain both domain and aggregation terms. Those will be labelled as 'aggregation'
+               -- and split in the end-10-migrate_financial_reports.py script.
+                  CASE WHEN arl.v15_formulas ~ ('^{DOMAIN_EXPR_REGEX}\.balance$') THEN 'domain'
+                       ELSE 'aggregation'
+                  END,
+
+               -- formula
+                  CASE
+                       WHEN arl.v15_formulas ~ ('^{DOMAIN_EXPR_REGEX}\.balance$') THEN arl.v15_domain
+                       ELSE arl.v15_formulas
+                  END,
+
+               -- subformula
+               -- 'cross_report' is slower but the standard behavior in v15
+                  CASE
+                       WHEN arl.v15_formulas ~ ('^{DOMAIN_EXPR_REGEX}\.balance$') THEN REPLACE(arl.v15_formulas, '.balance', '')
+                       ELSE 'cross_report'
+                  END
+          FROM account_report_line arl
+         WHERE arl.v15_formulas IS NOT NULL
+        """
+    )
+
+    # account.report.line v15 temporary columns are no longer needed
+    # note that `v15_line_id` and `v15_domain` columns are deliberately kept, because used and removed in an end- script
+    util.remove_column(cr, "account_report_line", "v15_formulas")
+    util.remove_column(cr, "account_report_line", "v15_figure_type")
+    util.remove_column(cr, "account_report_line", "v15_green_on_positive")
+    util.remove_column(cr, "account_report_line", "v15_special_date_changer")
+
+    # temp table, used and dropped in end- script
+    cr.execute(
+        """
+        CREATE UNLOGGED TABLE ___upgrade_afhrl AS
+            SELECT *
+              FROM account_financial_html_report_line afhrl
+             WHERE NOT EXISTS (SELECT 1 FROM account_report_line WHERE code = afhrl.code)
+        """
+    )
+
+    util.add_to_migration_reports(
+        """
+            <details>
+                <summary>
+                    The following custom financial reports have been migrated, note however they might be partially or substantially
+                    wrong.
+                    It is therefore crucial to analyze them thouroughlly and either fix the errors or create them anew.
+                </summary>
+                <ul>%s</ul>
+            </details>
+        """
+        % " ".join(
+            [
+                f"<li>{util.get_anchor_link_to_record('account.report', id, name)}</li>"
+                for id, name in new_custom_fin_reports
+            ]
+        ),
+        category="Accounting reports",
+        format="html",
+    )
