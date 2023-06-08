0754ac85d5c0f808cb2da79a02e7e0d7616b7239
Nicolas Bayet <nby@odoo.com>
2023-06-08 08:41:06 +0000

    [IMP] mass_mailing/saas~16.4: remove views
    Community PR: https://github.com/odoo/odoo/pull/118966
    Enterprise PR: https://github.com/odoo/enterprise/pull/42141
    task-3175256
    
    Part-of: odoo/upgrade#4750

--- migrations/mass_mailing/saas~16.4.2.6/pre-migrate.py
+++ migrations/mass_mailing/saas~16.4.2.6/pre-migrate.py
@@ -5,3 +5,6 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.if_unchanged(cr, "mass_mailing.mass_mailing_kpi_link_trackers", util.update_record_from_xml)
+    util.remove_view(cr, "mass_mailing.iframe_css_assets_readonly")
+    util.remove_view(cr, "mass_mailing.iframe_css_assets_edit")
+    util.remove_view(cr, "mass_mailing.mass_mailing_mail_style")
