eb9b2a9a9ed462b5f10ef38b61332fb29e29863e
Jinane Maksoud <maji@odoo.com>
2023-06-08 10:18:19 +0000

    [FIX] sale_temporal/saas~15.3: recompute currency_id
    Product.pricing currency_id is changed from a required field with a default
    value to a computed stored field.
    During the migration, many records are inserted in this model from
    sale_renting and sale_subscription. A recompute of the field is needed.
    
    closes odoo/upgrade#4753
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/sale_temporal/saas~15.3.1.0/end-compute.py
+++ migrations/sale_temporal/saas~15.3.1.0/end-compute.py
@@ -3,5 +3,31 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-
     util.recompute_fields(cr, "sale.temporal.recurrence", ["name"])
+    util.explode_execute(
+        cr,
+        """
+        UPDATE product_pricing pp
+           SET currency_id = pl.currency_id
+          FROM product_pricelist pl
+         WHERE pp.pricelist_id = pl.id
+           AND pp.currency_id IS NULL
+        """,
+        table="product_pricing",
+        alias="pp",
+    )
+    util.explode_execute(
+        cr,
+        """
+        UPDATE product_pricing pp
+           SET currency_id = c.currency_id
+          FROM product_template pt
+          JOIN res_company c
+            ON pt.company_id = c.id
+         WHERE pp.pricelist_id IS NULL
+           AND pp.currency_id IS NULL
+           AND pp.product_template_id = pt.id
+        """,
+        table="product_pricing",
+        alias="pp",
+    )
