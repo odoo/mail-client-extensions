973c75dc0022c4b23740dddf181f002564517960
Yannick Tivisse <yti@odoo.com>
2023-06-13 13:10:41 +0000

    [IMP] hr_recruitment/saas~16.4: Remove ir.rule
    Com PR: https://github.com/odoo/odoo/pull/120586
    Ent PR: https://github.com/odoo/enterprise/pull/40763
    
    Taskid: 3101400
    Part-of: odoo/upgrade#4678

--- migrations/hr_recruitment/saas~16.4.1.1/pre-migrate.py
+++ migrations/hr_recruitment/saas~16.4.1.1/pre-migrate.py
@@ -6,3 +6,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.update_record_from_xml(cr, "hr_recruitment.group_hr_recruitment_interviewer")
     util.update_record_from_xml(cr, "hr_recruitment.group_hr_recruitment_user")
+
+    util.remove_record(cr, "hr_recruitment.mail_message_interviewer_rule")
