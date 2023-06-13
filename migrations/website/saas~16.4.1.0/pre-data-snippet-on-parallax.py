25bb5d010a123bfa8dbab00283f8e86ac34a2d4a
Louis (loco) <loco@odoo.com>
2023-06-13 08:46:48 +0000

    [IMP] website/saas~16.4: remove data attributes on background
    Steps to reproduce the problem:
    - Add a "Text" snippet on the website.
    - Change the background image with one of your own.
    - Change the parallax to "Fixed".
    - Save.
    - Inspect the "Text" snippet.
    
    -> There is a "data-snippet" and a "data-name" attribute on the
    background image.
    
    The goal of this commit is to remove the "data-snippet" and "data-name"
    attributes on background images.
    
    On the community PR, the problem is solved by filtering the data
    attributes before transferring them to the new target at parallax
    change.
    
    See https://github.com/odoo/odoo/pull/123873
    
    closes odoo/upgrade#4767
    
    Related: odoo/odoo#123873
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/website/saas~16.4.1.0/pre-data-snippet-on-parallax.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import odoo.upgrade.util.snippets as snip
+
+
+def remove_data_snippet_and_name(el):
+    if el.get("data-snippet"):
+        el.attrib.pop("data-snippet")
+    if el.get("data-name"):
+        el.attrib.pop("data-name")
+    return True
+
+
+def migrate(cr, version):
+    """
+    Delete the "data-snippet" and "data-name" attributes on background images
+    that appeared at the change of parallax.
+
+    :param cr: database cursor.
+    :param version: version of the module.
+    """
+    snip.convert_html_content(
+        cr,
+        snip.html_converter(
+            remove_data_snippet_and_name, selector="//*[hasclass('s_parallax_bg')][@data-snippet or @data-name]"
+        ),
+        where_column=r"~ '\ys_parallax_bg\y'",
+    )
