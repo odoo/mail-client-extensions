9d041ac5c4d191f26d2104c1e5568310973e3d02
Xavier-Do <xdo@odoo.com>
2023-06-15 21:01:34 +0000

    [FIX] sale_stock_renting/0.0.0
    Introduced in b48ca713af870b58737b51e98c09459dfb61c20e, a small error
    was missed regarding the version breaking upgrades from
    16.0 to saas-16.1.
    
    closes odoo/upgrade#4789
    
    closes odoo/upgrade#4672
    
    Related: odoo/enterprise#40986
    Signed-off-by: Xavier Dollé (xdo) <xdo@odoo.com>
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/helpdesk/saas~16.4.1.5/pre-migrate.py
+++ migrations/helpdesk/saas~16.4.1.5/pre-migrate.py
@@ -13,3 +13,7 @@ def migrate(cr, version):
         "sla_status_ongoing",
     ]:
         util.remove_field(cr, "helpdesk.sla.report.analysis", field)
+    util.remove_view(cr, "helpdesk.helpdesk_ticket_view_graph_7days_analysis_inherit_dashboard")
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_7days_analysis")
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_7days_analysis_pivot")
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_7days_analysis_graph")
