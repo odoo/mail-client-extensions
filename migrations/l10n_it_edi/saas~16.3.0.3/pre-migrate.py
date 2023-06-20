adb03174a687b31fd436531689bc2dd13b427cb1
Jinane Maksoud <maji@odoo.com>
2023-06-20 13:53:00 +0000

    [FIX] l10n_it_edi/saas~16.3: fix query join
    The column country_id on res.company is computed and not stored.
    
    closes odoo/upgrade#4805
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/l10n_it_edi/saas~16.3.0.3/pre-migrate.py
+++ migrations/l10n_it_edi/saas~16.3.0.3/pre-migrate.py
@@ -7,10 +7,12 @@ def migrate(cr, version):
         UPDATE account_edi_proxy_client_user u
            SET proxy_type = 'l10n_it_edi'
           FROM res_company c
+          JOIN res_partner p
+            ON p.id = c.partner_id
           JOIN res_country t
-            ON t.id = c.country_id
+            ON t.id = p.country_id
          WHERE c.id = u.company_id
            AND t.code = 'IT'
     """
 
-    util.parallel_execute(cr, util.explode_query_range(cr, query, table="account_edi_proxy_client_user"))
+    util.explode_execute(cr, query, table="account_edi_proxy_client_user", alias="u")
