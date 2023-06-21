3985573781e07b65472951d934b770ab9a821aef
Demesmaeker <edm@odoo.com>
2023-06-21 16:02:51 +0000

    [ADD] sale_amazon/saas~16.4: synchronize Odoo stock with Amazon
    Odoo will now synchronize the stock with Amazon.
    
    The `unique_sku` constraint has been adapted and no longer depends on
    `marketplace_id`, so duplicate `account_id` and `sku` pairs must be
    removed from the database.
    
    See odoo/enterprise#40720
    
    task-2864098
    
    closes odoo/upgrade#4810
    
    Related: odoo/documentation#4773
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/sale_amazon/saas~16.4.1.1/pre-migrate.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+_logger = logging.getLogger("odoo.upgrade.sale_amazon.16.4.1.1." + __name__)
+
+
+def migrate(cr, version):
+    delete_query = """
+        DELETE FROM amazon_offer
+        WHERE id NOT IN (
+            SELECT MAX(id)
+            FROM amazon_offer
+            GROUP BY account_id, SKU
+        )
+    """
+    cr.execute(delete_query)
+
+    nb_offers_deleted = cr.rowcount
+
+    if nb_offers_deleted:
+        _logger.info(
+            "Drop %d duplicate offers (same SKU for the same Amazon account",
+            nb_offers_deleted,
+        )
