28ed0b72ab77cb1aaa8a476a7c2ad301fc4305cc
Valentin Vallaeys (vava) <vava@odoo.com>
2023-06-22 11:15:39 +0000

    [IMP] website_sale_stock_product_configurator/saas~16.4: remove module
    Community PR: https://github.com/odoo/odoo/pull/125960
    
    closes odoo/upgrade#4818
    
    Related: odoo/odoo#125960
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/saas~16.4.1.3/pre-10-modules.py
+++ migrations/base/saas~16.4.1.3/pre-10-modules.py
@@ -4,5 +4,6 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.merge_module(cr, "l10n_in_upi", "l10n_in")
+    util.remove_module(cr, "website_sale_stock_product_configurator")
     if util.has_enterprise():
         util.remove_module(cr, "industry_fsm_forecast")
