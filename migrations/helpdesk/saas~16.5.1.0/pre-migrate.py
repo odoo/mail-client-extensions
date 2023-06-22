e3a42cd6ee7c266162010eb10804aaa45b1d5de0
kais-odoo <kais@odoo.com>
2023-06-22 19:12:25 +0530

    [IMP] helpdesk/saas~16.5: rename field ticket deadline
    In this commit, rename field 'ticket_deadline' to 'sla_deadline'  in helpdesk
    ticket analysis report so that it can be use by search view  group by filter.
    
    Related Enterprise PR: https://github.com/odoo/enterprise/pull/40055
    
    task:3186578
    
    closes odoo/upgrade#4820
    
    Related: odoo/enterprise#40055
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/helpdesk/saas~16.5.1.0/pre-migrate.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.rename_field(cr, "helpdesk.ticket.report.analysis", "ticket_deadline", "sla_deadline")
