3d1a9d4ea16cfdc8802f30df226716adf8fb0637
Valeriya(vchu) <vchu@odoo.com>
2023-06-22 14:28:16 +0000

    [IMP] website_sale/saas~16.4: remove many2many
    remove link between product_tag and product_ribbon
    
    task-3299538
    
    see odoo/odoo#124299
    
    closes odoo/upgrade#4824
    
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- migrations/website_sale/saas~16.4.1.1/pre-migrate.py
+++ migrations/website_sale/saas~16.4.1.1/pre-migrate.py
@@ -6,5 +6,7 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "res.company", "website_sale_onboarding_payment_provider_state")
     util.remove_field(cr, "res.config.settings", "terms_url")
+    util.remove_field(cr, "product.tag", "ribbon_id")
+    util.remove_field(cr, "product.ribbon", "product_tag_ids")
 
     util.remove_view(cr, "website_sale.res_config_settings_view_form_web_terms")
