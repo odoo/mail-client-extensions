80ad643d22d956781a886877f0b8039dc953f495
vlst <vlst@odoo.com>
2023-06-26 09:47:26 +0200

    [IMP] point_of_sale/saas~17.1: One order refund at a time
    It's possible in POS to refund multiple products from multiple
    paid orders at the same time, even if any of those orders were
    originally invoiced. This causes issues; generating data discrepancy
    d even leading to tracebacks (ex: in Accounting, a reverse-invoice
    must be linked to only one invoice, not multiple).
    
    This PR makes it so only items from a single order can be refunded at a time.
    
    Thus, the `many2many` field `refunded_order_ids` is replaced with a many2one.
    
    Task: 3376828
    See : odoo/odoo#125780
    
    closes odoo/upgrade#4830
    
    Related: odoo/enterprise#54198
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/point_of_sale/saas~17.1.1.0.1/pre-migrate.py
+++ migrations/point_of_sale/saas~17.1.1.0.1/pre-migrate.py
@@ -7,3 +7,6 @@ def migrate(cr, version):
         util.rename_xmlid(cr, "point_of_sale.product_product_consumable", "pos_discount.product_product_consumable")
     else:
         util.delete_unused(cr, "point_of_sale.product_product_consumable")
+    util.remove_field(cr, "pos.order", "refunded_order_ids")
+    util.remove_field(cr, "pos.order", "refunded_orders_count")
+    util.remove_field(cr, "pos.order", "is_refunded")
