74380cbe4b1d2f4a64414a08e55d545007598175
Adrien Schoffeniels <adsc@odoo.com>
2023-06-28 13:33:32 +0200

    [IMP] (website_)knowledge/saas~16.5: (re)move views
    This commit removes unused views in knowledge and website_knowledge, and
    move some views from knowledge to website_knowledge.
    
    Task-3346593
    see odoo/enterprise#43304
    
    closes odoo/upgrade#4862
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/knowledge/saas~16.5.1.0/pre-migrate.py
+++ migrations/knowledge/saas~16.5.1.0/pre-migrate.py
@@ -5,3 +5,13 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "knowledge.knowledge_embedded_view")
     util.remove_view(cr, "knowledge.knowledge_view_link")
+    util.remove_view(cr, "knowledge.knowledge_article_tree_favorites")
+    util.remove_view(cr, "knowledge.knowledge_article_tree_frontend")
+    if util.module_installed(cr, "website_knowledge"):
+        util.rename_xmlid(cr, "knowledge.layout", "website_knowledge.layout")
+        util.rename_xmlid(cr, "knowledge.articles_template", "website_knowledge.articles_template")
+        util.rename_xmlid(cr, "knowledge.knowledge_article_view_frontend", "website_knowledge.article_view_public")
+    else:
+        util.remove_view(cr, "knowledge.layout")
+        util.remove_view(cr, "knowledge.articles_template")
+        util.remove_view(cr, "knowledge.knowledge_article_view_frontend")
