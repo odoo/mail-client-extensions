53ccac4557a4d9face17597416657068eef82550
Anita (anko) <anko@odoo.com>
2023-06-29 14:33:30 +0200

    [ADD] payment_adyen/saas~16.5.2.0 : remove and rename api fields
    Field 'adyen_recurring_api_url' is removed and 'adyen_checkout_api_url'
    is renamed to 'adyen_api_url_prefix'. Additionaly values in
    'adyen_api_url_prefix' will be adapted to new requirements.
    
    See odoo/odoo#126831
    
    closes odoo/upgrade#4876
    
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- migrations/payment_adyen/saas~16.5.2.0/pre-migrate.py
+++ migrations/payment_adyen/saas~16.5.2.0/pre-migrate.py
@@ -2,6 +2,17 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+
     util.remove_view(cr, "payment_adyen.checkout")
     util.remove_view(cr, "payment_adyen.manage")
     util.remove_view(cr, "payment_adyen.sdk_assets")
+
+    util.remove_field(cr, "payment.provider", "adyen_recurring_api_url")
+    util.rename_field(cr, "payment.provider", "adyen_checkout_api_url", "adyen_api_url_prefix")
+    cr.execute(
+        r"""
+       UPDATE payment_provider
+          SET adyen_api_url_prefix = (regexp_match(adyen_api_url_prefix, '(?:https://)?(\w+-\w+).*'))[1]
+        WHERE code = 'adyen'
+        """
+    )
