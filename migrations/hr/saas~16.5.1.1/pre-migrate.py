1fdf230189fffe0ab571361c521c3d8ac1673d35
Yannick Tivisse <yti@odoo.com>
2023-07-03 12:07:08 +0000

    [IMP] hr/saas~16.5.1.1: Add contract type code
    COM PR: https://github.com/odoo/odoo/pull/105119/files
    ENT PR: https://github.com/odoo/enterprise/pull/32888/files
    
    closes odoo/upgrade#4879
    
    Taskid: 3029662
    Related: odoo/enterprise#32888
    Related: odoo/odoo#105119
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/hr/saas~16.5.1.1/pre-migrate.py
+++ migrations/hr/saas~16.5.1.1/pre-migrate.py
@@ -5,3 +5,11 @@ def migrate(cr, version):
     if util.module_installed(cr, "hr_recruitment"):
         util.move_field_to_module(cr, "hr.employee", "newly_hired_employee", "hr_recruitment", "hr")
         util.rename_field(cr, "hr.employee", "newly_hired_employee", "newly_hired")
+
+    util.create_column(cr, "hr_contract_type", "code", "varchar")
+
+    util.explode_execute(
+        cr,
+        "UPDATE hr_contract_type SET code = name->>'en_US'",
+        table="hr_contract_type",
+    )
