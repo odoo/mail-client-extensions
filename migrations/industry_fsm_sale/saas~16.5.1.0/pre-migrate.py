dc01dab24ea5a40ee31d9ecfa3b88e7470321402
Bastien (bvdn) <bvdn@odoo.com>
2023-07-04 18:15:19 +0200

    [IMP] industry_fsm_*/saas~16.5: remove product.kanban views
    those views are no longer needed, we are now using the product.catalog
    view from the sale module
    
    Enterprise PR: https://github.com/odoo/enterprise/pull/43469
    Community PR: https://github.com/odoo/odoo/pull/127161
    
    Task-3343547
    
    closes odoo/upgrade#4887
    
    Related: odoo/odoo#127161
    Related: odoo/enterprise#43469
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/industry_fsm_sale/saas~16.5.1.0/pre-migrate.py
+++ migrations/industry_fsm_sale/saas~16.5.1.0/pre-migrate.py
@@ -4,3 +4,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "industry_fsm_sale.view_project_task_pivot_fsm_inherit_sale")
+    util.remove_view(cr, "industry_fsm_sale.view_product_product_kanban_material")
+    util.remove_view(cr, "industry_fsm_sale.product_search_form_view_inherit_fsm_sale")
--- migrations/industry_fsm_stock/saas~16.5.1.0/pre-migrate.py
+++ migrations/industry_fsm_stock/saas~16.5.1.0/pre-migrate.py
@@ -5,3 +5,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.rename_field(cr, "fsm.stock.tracking.line", "wizard_tracking_line_valided", "wizard_tracking_line_validated")
     util.remove_field(cr, "fsm.stock.tracking", "fsm_done")
+    util.remove_view(cr, "industry_fsm_stock.stock_product_product_kanban_material")
