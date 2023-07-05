45cb5f3c786370b2bcce01a71f286bb21a58d0ba
Kamlesh Pathekar <kpt@odoo.com>
2023-07-05 18:18:24 +0530

    [IMP] purchase_requisition,stock/saas~16.5: remove unused field
    Removes field on purchase.requisition.alternative.warning and
    stock.inventory.adjustment.name as it is no longer used.
    
    Community PR: https://github.com/odoo/odoo/pull/124820
    Enterprise PR: https://github.com/odoo/enterprise/pull/42472
    
    task-3356114
    
    closes odoo/upgrade#4899
    
    Related: odoo/odoo#124820
    Related: odoo/enterprise#42472
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/stock/saas~16.5.1.1/pre-migrate.py
+++ migrations/stock/saas~16.5.1.1/pre-migrate.py
@@ -6,3 +6,4 @@ def migrate(cr, version):
     util.remove_field(cr, "stock.move", "display_clear_serial")
     util.remove_model(cr, "stock.import.lot")
     util.remove_model(cr, "stock.generate.serial")
+    util.remove_field(cr, "stock.inventory.adjustment.name", "show_info")
