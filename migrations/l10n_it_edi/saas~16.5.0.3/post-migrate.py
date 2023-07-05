21132430c3e37f2711d46944b28e77597bcbcef5
Paolo Gatti (pgi) <pgi@odoo.com>
2023-07-05 13:53:20 +0200

    [IMP] l10n_it_edi/saas~16.5: remove account_edi from depends
    The account_edi framework is no more a dependancy.
    
    - Models account.edi.document and account.edi.format have been removed.
      All the functions have been moved to the new account.move.send,
      account.edi.proxy.client.user, l10n_it_edi.import, l10n_it_edi.export
    - The move now holds the l10n_it_edi_state which admits all values that
      the SdI sends us as a status update
    - The sending is now not handled by the cron but by the Send and Print
      dialog.
    - The move form will now hold the state of the EDI transaction, its
      number and the EDI attachment as a Binary field on the move itself.
    - A new banner will keep the latest message from the SdI, that's not the
      account_edi old banner.
    - All the user messages have been simplified.
    
    See odoo/odoo#122194
    See odoo/iap-apps#652
    
    closes odoo/upgrade#4893
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/l10n_it_edi/saas~16.5.0.3/post-migrate.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # Return the moves where we deleted the old edi document, so we can recompute the old edi state
+    # to avoid the field edi_state to appear when not necessary anymore after upgrade
+    query = """
+            DELETE FROM account_edi_document aed
+                  USING account_edi_format aef
+                  WHERE aed.edi_format_id = aef.id
+                    AND aef.code = 'fattura_pa'
+              RETURNING aed.move_id
+        """
+    cr.execute(query)
+    res = cr.fetchall()
+    if res:
+        util.recompute_fields(cr, "account.move", ["edi_state"], ids=[re[0] for re in res], strategy="commit")
+
+    util.remove_record(cr, "l10n_it_edi.edi_fatturaPA")
---
+++ migrations/l10n_it_edi/saas~16.5.0.3/pre-migrate.py
@@ -0,0 +1,44 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "account_move_send", "l10n_it_edi_checkbox_send", "boolean")
+    util.create_column(cr, "account_move", "l10n_it_edi_state", "varchar")
+    util.create_column(cr, "account_move", "l10n_it_edi_header", "varchar")
+
+    util.rename_field(cr, "res.config.settings", "l10n_it_edi_sdicoop_register", "l10n_it_edi_register")
+    util.rename_field(cr, "res.config.settings", "l10n_it_edi_sdicoop_demo_mode", "l10n_it_edi_demo_mode")
+
+    util.remove_view(cr, "l10n_it_edi.view_out_invoice_tree_inherit")
+    util.remove_view(cr, "l10n_it_edi.view_in_bill_tree_inherit")
+
+    query = """
+         UPDATE account_move am
+            SET l10n_it_edi_state = CASE
+                    WHEN aed.state = 'to_send' AND am.l10n_it_edi_transaction IS NULL AND aed.blocking_level = 'error' THEN 'rejected'
+                    WHEN aed.state = 'to_send' AND am.l10n_it_edi_transaction IS NULL THEN NULL
+                    WHEN aed.state = 'to_send' AND am.l10n_it_edi_transaction IS NOT NULL THEN 'processing'
+                    WHEN aed.state = 'sent' THEN 'forwarded'
+                END,
+                l10n_it_edi_header = REPLACE(aed.error, '<ul>', '<ul class="mb-0">')
+           FROM account_edi_document aed
+           JOIN account_edi_format aef
+             ON aef.id = aed.edi_format_id
+            AND aef.code = 'fattura_pa'
+          WHERE am.state = 'posted'
+            AND aed.move_id = am.id
+    """
+    util.explode_execute(cr, query, table="account_move", alias="am")
+
+    query = """
+        UPDATE ir_attachment ia
+           SET res_field = 'l10n_it_edi_attachment_file',
+               mimetype = 'application/xml'
+          FROM account_move am
+         WHERE ia.id = am.l10n_it_edi_attachment_id
+    """
+    util.explode_execute(cr, query, table="ir_attachment", alias="ia")
+
+    util.remove_field(cr, "account.move", "l10n_it_einvoice_id")
+    util.remove_field(cr, "account.move", "l10n_it_einvoice_name")
+    util.remove_field(cr, "account.move", "l10n_it_edi_attachment_id")
