ec148f757b4d6bc2629cc5ca6bf7040c1b424c11
Hugo Carlier (Huca) <huca@odoo.com>
2023-07-05 13:03:55 +0000

    [IMP] project_todo/saas~16.4: avoid evaluation of equivalent ir.rule
    Since the introduction of project_todo, access to the model project.task
    for interal users has been extended to read/write/create/unlink. The
    basic record rule for task visibility does in consequence need to manage
    only the read access. A new record rule was introduced to manage
    write/create/unlink access for project users. However, the read access
    for this group was also managed by this new rule, which led to
    evaluation of both ir.rule for project user in some cases.
    
    The new ir.rule has been modified to avoid such double evaluation in
    16.3, however, it still need to be updated for db already in 16.3.
    
    related-https://github.com/odoo/odoo/pull/127433
    
    closes odoo/upgrade#4894
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/project_todo/saas~16.4.1.0/pre-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.if_unchanged(cr, "project_todo.task_visibility_rule_project_user", util.update_record_from_xml)
