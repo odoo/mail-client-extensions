7f617446374ee63c62a38481e55dfbc714219fb3
sofiagvaladze <sgv@odoo.com>
2023-07-06 09:08:53 +0200

    [IMP] hr_recruitment/saas~16.5: sanitize fields
    Sanitize applicant's phone/mobile and normalize email.
    
    In order to have faster and more reliable search for similar applicants,
    1 - we add normalized email and sanitized phone and mobile fields
    on hr.applicant.
    2 - we use those fields for a search of similar applicants.
    
    Enterprise PR: odoo/odoo#120521
    
    task-3282883
    
    closes odoo/upgrade#4977
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/hr_recruitment/saas~16.5.1.1/post-migrate.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import sys
+import uuid
+from concurrent.futures import ProcessPoolExecutor
+
+from psycopg2.extras import execute_batch
+
+from odoo.upgrade import util
+
+
+def sanitize_fields(cr, san, phone_to_sanitize, phone_sanitized):
+    cr.execute(
+        f"""
+        WITH applicant AS (
+           SELECT a.id,
+        trim(a.{phone_to_sanitize}) as phone,
+        COALESCE( r_a.country_id , r.country_id) AS country_id,
+        a.company_id AS company_id
+      FROM hr_applicant a
+      JOIN res_company comp ON comp.id = a.company_id
+      JOIN res_partner r ON comp.partner_id = r.id
+      JOIN res_partner r_a ON a.partner_id = r_a.id
+     WHERE a.{phone_sanitized} IS NULL
+       AND length(trim(a.{phone_to_sanitize})) > 3
+    )
+    SELECT a.id,
+        a.phone,
+        c.code,
+        c.phone_code
+    FROM applicant a
+        JOIN res_country c ON a.country_id = c.id
+    """
+    )
+
+    with ProcessPoolExecutor() as executor:
+        execute_batch(
+            cr._obj,
+            f"UPDATE hr_applicant SET {phone_sanitized} = %s WHERE id = %s",
+            executor.map(san.sanitize, *zip(*cr.fetchall())),
+        )
+
+
+def migrate(cr, version):
+    # NOTE
+    # `ProcessPoolExecutor.map` arguments needs to be pickleable
+    # Functions can only be pickle if they are importable.
+    # However, the current file is not importable due to the dash in the filename.
+    # We should then put the executed function in its own importable file.
+    name = f"_upgrade_{uuid.uuid4().hex}"
+    san = sys.modules[name] = util.import_script("sms/saas~12.5.2.0/sanitize.py", name=name)
+
+    sanitize_fields(cr, san, "partner_phone", "partner_phone_sanitized")
+    sanitize_fields(cr, san, "partner_mobile", "partner_mobile_sanitized")
--- migrations/hr_recruitment/saas~16.5.1.1/pre-migrate.py
+++ migrations/hr_recruitment/saas~16.5.1.1/pre-migrate.py
@@ -4,3 +4,16 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "hr_recruitment.hr_employee_view_search")
     util.remove_record(cr, "hr_recruitment.hr_employee_action_from_department")
+    util.create_column(cr, "hr_applicant", "email_normalized", "varchar")
+
+    util.explode_execute(
+        cr,
+        """
+        UPDATE hr_applicant
+           SET email_normalized=lower(substring(email_from, '([^ ,;<@]+@[^> ,;]+)'))
+         WHERE email_from IS NOT NULL
+        """,
+        table="hr_applicant",
+    )
+    util.create_column(cr, "hr_applicant", "partner_phone_sanitized", "varchar")
+    util.create_column(cr, "hr_applicant", "partner_mobile_sanitized", "varchar")
