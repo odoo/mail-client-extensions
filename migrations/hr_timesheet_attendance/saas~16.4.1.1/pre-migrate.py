805ddcc4a960a1fb3f36d75d3a2a43354fd117e0
Audric Onockx (auon) <auon@odoo.com>
2023-07-06 08:00:49 +0000

    [IMP] hr_timesheet_attendance/saas~16.4: base report on employees
    Related:https://github.com/odoo/odoo/pull/127492
    The report is currently base on timesheets/attendances.user_id.employee_id.
    As a result, some data weren't taken into account.
    As both models have employee_id, there is no reason not to use it.
    
    Also, we had a view in this module that was not used
    and didn't seem to make any sense, so we delete it here.
    
    task-3251675
    
    closes odoo/upgrade#4900
    
    Related: odoo/odoo#127492
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/hr_timesheet_attendance/saas~16.4.1.1/pre-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "hr_timesheet_attendance.hr_timesheet_attendance_report_view_tree")
+    util.remove_field(cr, "hr.timesheet.attendance.report", "user_id")
