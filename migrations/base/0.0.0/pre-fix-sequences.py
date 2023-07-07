e46e4919df7e2c431179cdfe3b7aa6587e6eb755
arg-odoo <arg@odoo.com>
2023-07-07 13:18:03 +0000

    [IMP] base/0.0.0: Prevent error with default value for id sequences
    By default, the default value for id field is set to the next value of
    the id sequence for a specific table. Sometimes, the id default value
    isn't set or the sequence has been dropped. To prevent error when
    inserting new values in table that are missing a default value or a
    sequence, we can set a default value or/and create the missing
    sequence.
    
    For information, here's the list of actual `create_id_sequence` specific
    script. (23 uses)
    ```bash
    scripts/M120531617-vmdevrot/hr_payroll/saas~14.1.1.0/pre--migrate.py:    util.create_id_sequence(cr, "hr_salary_rule")
    scripts/M120531617-vmdevrot/hr_payroll/saas~14.1.1.0/pre--migrate.py:    util.create_id_sequence(cr, "hr_payroll_structure")
    scripts/M1608241572983/base/16.0.1.3/pre--migrate.py:        util.create_id_sequence(cr, table)
    scripts/M1704293858885/base/14.0.1.3/pre--migrate.py:        util.create_id_sequence(cr, table, set_as_default=True)
    scripts/M1707114525178/base/14.0.1.3/pre--migrate.py:    util.create_id_sequence(cr, "ir_model_fields_selection")
    scripts/M1707114525178/documents/14.0.1.0/pre--migrate.py:    util.create_id_sequence(cr, "documents_document")
    scripts/M1707114525178/hr_contract/14.0.1.0/pre--migrate.py:    util.create_id_sequence(cr, "hr_payroll_structure_type")
    scripts/M1707114525178/hr_payroll/14.0.1.0/pre--migrate.py:    util.create_id_sequence(cr, "hr_work_entry_type")
    scripts/M1803267073077/hr_payroll/14.0.1.0/pre--update-sequence.py:    util.create_id_sequence(cr, "hr_salary_rule")
    scripts/M1812119249078/account/16.0.1.2/pre--migration.py:    util.create_id_sequence(cr, table="account_payment_register")
    scripts/M1812119249078/base/13.1.1.3/pre--migrate.py:        util.create_id_sequence(cr, table, set_as_default=True)
    scripts/M1901149521551/hr_holidays/saas~12.3.1.5/pre--migrate.py:    util.create_id_sequence(cr, table="hr_leave_type")
    scripts/M1901149521551/hr_payroll/16.0.1.0/pre--migrate.py:    util.create_id_sequence(cr, table="hr_payroll_structure")
    scripts/M1901149521551/hr_payroll/saas~14.1.1.0/pre--migrate.py:    util.create_id_sequence(cr, table="hr_salary_rule")
    scripts/M1901149521551/im_livechat/saas~12.3.1.0/pre--migrate.py:    util.create_id_sequence(cr, table="im_livechat_channel")
    scripts/M1901149521551/website_form/13.0.1.0/pre--migrate.py:    util.create_id_sequence(cr, table="website_page")
    scripts/M1901149521551/website_sale/saas~14.1.1.0/pre--migrate.py:    util.create_id_sequence(cr, table="website_rewrite")
    scripts/M1902139752314/base/15.0.1.3/pre--migrate.py:        util.create_id_sequence(cr, table)
    scripts/M19032010057088/base/15.0.1.3/pre--migrate.py:    util.create_id_sequence(cr, table="product_product")
    scripts/M19032810129838/utm/13.0.1.0/pre--migrate.py:    util.create_id_sequence(cr, "utm_medium")
    scripts/M20122121995557/base/saas~13.4.1.3/pre--migrate.py:        util.create_id_sequence(cr, table)
    scripts/M21090230147340/event/14.0.1.0/pre--migrate.py:        util.create_id_sequence(cr, table)
    scripts/M23041278254393/base/11.0.1.3/pre--migrate.py:    util.create_id_sequence(cr, "survey_stage")
    ```
    
    tbg-787
    
    Exemple of errors (not exhausive).
    ```
    psycopg2.errors.UndefinedTable: relation "hr_leave_type_id_seq" does not exist
    psycopg2.errors.NotNullViolation: null value in column "id" violates not-null constraint
    ```
    
    closes odoo/upgrade#4911
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/0.0.0/pre-fix-sequences.py
+++ migrations/base/0.0.0/pre-fix-sequences.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
 
+from odoo.addons.base.maintenance.migrations import util
+
 
 def migrate(cr, version):
     _logger = logging.getLogger(__name__)
@@ -8,17 +10,20 @@ def migrate(cr, version):
     # with help from https://wiki.postgresql.org/wiki/Retrieve_primary_key_columns
     cr.execute(
         r"""
-        SELECT (regexp_matches(pg_get_expr(d.adbin,0),'nextval\(''([[:alpha:]][\w\$]*)''::regclass\)'))[1] seq_name,
-               t.relname table_name
-          FROM pg_attribute a
-          JOIN pg_class t ON t.oid=a.attrelid AND t.relkind='r'
-          JOIN pg_attrdef d ON d.adrelid=a.attrelid AND a.attnum=d.adnum
-          JOIN pg_index i ON a.attrelid=i.indrelid AND a.attnum=any(i.indkey)
-         WHERE a.attname='id'
-           AND a.atthasdef
+           SELECT SUBSTRING(pg_get_expr(d.adbin,0), 'nextval\(''([[:alpha:]][\w\$]*)''::regclass\)') as sequence_name,
+                t.relname table_name
+             FROM pg_attribute a
+             JOIN pg_class t ON t.oid=a.attrelid AND t.relkind='r'
+        FULL JOIN pg_attrdef d ON d.adrelid=a.attrelid AND a.attnum=d.adnum
+             JOIN pg_index i ON a.attrelid=i.indrelid AND a.attnum=any(i.indkey)
+            WHERE a.attname='id';
         """
     )
     for seq_name, table_name in cr.fetchall():
+        if seq_name is None:
+            util.create_id_sequence(cr, table=table_name)
+            continue
+
         cr.execute('SELECT max(id) FROM "{}"'.format(table_name))
         max_id = cr.fetchone()[0]
 
