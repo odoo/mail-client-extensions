e8f396f17f09a76b3388eb7db6148270bf4b92c4
moerradi <moer@odoo.com>
2023-07-07 16:10:48 +0000

    [IMP] website_sale/saas~16.4.1.1: 'Edit in Builder' for T&C with website
    Prveiously the "Edit in Website Builder" button was not shown for terms
    and conditions if website is installed, it was shown only if the
    website_sale module was installed. This commit fixes that.
    
    Community-PR: https://github.com/odoo/odoo/pull/127735
    
    task-3342876
    
    closes odoo/upgrade#4912
    
    Related: odoo/odoo#127735
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/website_sale/saas~16.4.1.1/pre-migrate.py
+++ migrations/website_sale/saas~16.4.1.1/pre-migrate.py
@@ -5,3 +5,6 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "res.company", "website_sale_onboarding_payment_provider_state")
+    util.remove_field(cr, "res.config.settings", "terms_url")
+
+    util.remove_view(cr, "website_sale.res_config_settings_view_form_web_terms")
