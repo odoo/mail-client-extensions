0bae008ea74b6fdffa6a177e7ab4932462f5997c
Audric Onockx (auon) <auon@odoo.com>
2023-07-12 12:39:20 +0000

    [IMP] project/saas~16.4: delete unnecessary fields
    In following Pull Requests, we introduce a boolean field,
    to determine if a task should be displayed in its project.
    By doing so, we remove the necessity
    to have a 2nd project field (`project_root_id`),
    and we ensure the main project field (`project_id`)
    is always set on non-private tasks,
    so `is_private` becomes useless.
    
    With this commit, we also set `project_id` of all non-private
    tasks to the project of their parent, or grand parent, etc recursively.
    
    COM: https://github.com/odoo/odoo/pull/128281/files
    ENT: https://github.com/odoo/enterprise/pull/43996
    task-3367246
    
    closes odoo/upgrade#4930
    
    Related: odoo/odoo#128281
    Related: odoo/enterprise#43996
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/project/saas~16.4.1.2/pre-migrate.py
+++ migrations/project/saas~16.4.1.2/pre-migrate.py
@@ -23,6 +23,43 @@ def migrate(cr, version):
         "report_project_task_user_report_comp_rule",
         "update_comp_rule",
         "milestone_comp_rule",
+        "project_manager_all_project_tasks_rule",
     ]
     for ir_rule_xmlid in ir_rule_xmlids_to_update:
         util.if_unchanged(cr, f"project.{ir_rule_xmlid}", util.update_record_from_xml)
+
+    util.remove_field(cr, "project.task", "is_private")
+    util.update_field_usage(cr, "project.task", "project_root_id", "project_id")
+    util.remove_field(cr, "project.task", "project_root_id")
+    # Create the column "display_in_project"
+    util.create_column(cr, "project_task", "display_in_project", "boolean", default=True)
+    # Set "project_id" of all non-private tasks to their project root
+    # and "display_in_project" to false
+    util.explode_execute(
+        cr,
+        """
+        WITH RECURSIVE project_hierarchy AS (
+                    SELECT pt.id,
+                           pt.parent_id,
+                           pt.project_id AS project_root_id
+                      FROM project_task pt
+                     WHERE pt.project_id IS NULL
+                       AND pt.parent_id IS NOT NULL
+                       AND {parallel_filter}
+                 UNION ALL
+                    SELECT ph.id,
+                           pt.parent_id,
+                           COALESCE(ph.project_root_id, pt.project_id) AS project_root_id
+                      FROM project_hierarchy ph
+                      JOIN project_task pt ON ph.parent_id = pt.id
+                       )
+                UPDATE project_task pt
+                   SET project_id = ph.project_root_id,
+                       display_in_project = false
+                  FROM project_hierarchy ph
+                 WHERE ph.project_root_id IS NOT NULL
+                   AND ph.id = pt.id
+        """,
+        table="project_task",
+        alias="pt",
+    )
