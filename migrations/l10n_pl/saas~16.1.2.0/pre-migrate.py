444947aa7181b92fd1df46affc5057bdf8cf0ec4
Edoardo Piroli <pied@odoo.com>
2023-07-13 10:05:28 +0000

    [FIX] l10n_pl/saas~16.1: Migrate updated tax reports
    [TBG](https://upgrade.odoo.com/web#id=791&action=178&model=upgrade.request.traceback.group&view_type=form&cids=1&menu_id=107)
    [upg-868024](
    https://upgrade.odoo.com/web#id=868024&active_id=868024&model=upgrade.request&view_type=form&cids=1&menu_id=107)
    [upg-845419](
    https://upgrade.odoo.com/web#id=845419&active_id=845419&model=upgrade.request&view_type=form&cids=1&menu_id=107)
    [upg-765801](
    https://upgrade.odoo.com/web#id=765801&active_id=765801&model=upgrade.request&view_type=form&cids=1&menu_id=107)
    
    The changes introduced in
    https://github.com/odoo/odoo/pull/108459/files#diff-4192d27fc406f02e4a9f906b394e7f1624d76d52e6a40782d8915d5af23b625c
    are not handled, triggering the following issue:
    
    ```
    psycopg2.errors.UniqueViolation: duplicate key value violates unique constraint "account_report_expression_line_label_uniq"
    DETAIL:  Key (report_line_id, label)=(65, balance) already exists.
    ```
    
    Since the changes were merged in 16.0 after the release, only dbs
    created/upgraded in a specific timeframe will need this script.
    
    closes odoo/upgrade#4937
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/l10n_pl/saas~16.1.2.0/pre-migrate.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # data file was changed after release, only apply this migration script to the dbs created before such change
+    if not util.ref(cr, "l10n_pl.account_tax_report_line_towary_art_129_formula"):
+        return
+
+    util.update_record_from_xml(cr, "l10n_pl.account_tax_report_line_uslugi_kraj_0_tag")
+
+    xmlids_to_remove = [
+        "account_tax_report_line_towary_art_129_formula",
+        "account_tax_report_line_uslug_s_trwale_formula",
+        "account_tax_report_line_kasy_rejestrujace_formula",
+        "account_tax_report_line_zaniechaniem_poboru_formula",
+        "account_tax_report_line_podatek_art_14_5_formula",
+        "account_tax_report_line_podatek_transp_termin_formula",
+        "account_tax_report_line_podatek_deklaracji_formula",
+        "account_tax_report_line_podatek_s_trwale_formula",
+        "account_tax_report_line_podatek_s_trwalych_formula",
+        "account_tax_report_line_podatek_pozostalych_nabyc_formula",
+        "account_tax_report_line_podatek_okresie_formula",
+    ]
+
+    for name in xmlids_to_remove:
+        util.remove_record(cr, "l10n_pl." + name)
