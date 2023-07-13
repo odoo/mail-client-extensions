92559055d251f55a8f6d5843196b000051de4901
arg-odoo <arg@odoo.com>
2023-07-13 12:41:37 +0000

    [IMP] l10n_cl/saas~15.1.3.0: rename xmlids
    Some records have been moved from module `l10n_cl_edi_stock` to module
    `l10n_cl`. We need to move the records (xmlids) to the right module to
    avoid them to be created in `l10n_cl` and then removed from
    `l10n_cl_edi_stock`.
    
    odoo: odoo/odoo@3150ad1
    enterprise: odoo/enterprise@25da76b
    
    upg-1040066
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/16.0/odoo/service/server.py", line 1299, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "<decorator-gen-14>", line 2, in new
      File "/home/odoo/src/odoo/16.0/odoo/tools/func.py", line 87, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/16.0/odoo/modules/registry.py", line 90, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 546, in load_modules
        env['ir.model.data']._process_end(processed_modules)
      File "/tmp/tmptwlqlu9p/migrations/base/0.0.0/pre-models-no-model-data-delete.py", line 103, in _process_end
        return super(IrModelData, self)._process_end(modules)
      File "/home/odoo/src/odoo/16.0/odoo/addons/base/models/ir_model.py", line 2364, in _process_end
        self._process_end_unlink_record(record)
      File "/home/odoo/src/odoo/16.0/odoo/addons/base/models/ir_model.py", line 2293, in _process_end_unlink_record
        record.unlink()
      File "/home/odoo/src/odoo/16.0/odoo/models.py", line 3568, in unlink
        cr.execute(query, (sub_ids,))
      File "/home/odoo/src/odoo/16.0/odoo/sql_db.py", line 321, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.ForeignKeyViolation: update or delete on table "l10n_latam_document_type" violates foreign key constraint "l10n_cl_dte_reference_tpo_doc_ref_fkey" on table "l10n_cl_dte_reference"
    DETAIL:  Key (id)=(53) is still referenced from table "l10n_cl_dte_reference".
    ```
    
    closes odoo/upgrade#4939
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/l10n_cl/saas~15.1.3.0/pre-migrate.py
+++ migrations/l10n_cl/saas~15.1.3.0/pre-migrate.py
@@ -4,3 +4,7 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "l10n_latam_document_type", "l10n_cl_active", "boolean")
+
+    eb = util.expand_braces
+    util.rename_xmlid(cr, *eb("l10n_cl{_edi_stock,}.dc_gd"))
+    util.rename_xmlid(cr, *eb("l10n_cl{_edi_stock,}.dc_gd_dte"))
