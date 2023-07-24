9b87ba129bc48696904b5a43e8114f2702a2a959
Xavier Bol (xbo) <xbo@odoo.com>
2023-07-24 13:22:12 +0200

    [IMP] timesheet_grid/saas~18.3: populate parent_res_{model,id} in timer
    This commit updates the running timer linked to a timesheet to fill the
    `parent_res_model` and `parent_res_id` columns.
    
    task-2276015
    
    Enterprise PR: odoo/enterprise#16084
    
    Part-of: odoo/upgrade#2208
    Related: odoo/upgrade-util#51
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/timesheet_grid/saas~18.3.1.0/pre-migrate.py
+++ migrations/timesheet_grid/saas~18.3.1.0/pre-migrate.py
@@ -1,6 +1,51 @@
+from psycopg2 import sql
+
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.remove_model(cr, "project.task.create.timesheet")
     util.remove_field(cr, "project.task", "display_timer_start_secondary")
+
+    if util.column_exists(cr, "account_analytic_line", "helpdesk_ticket_id"):
+        parent_res_model_query = """
+            CASE
+                WHEN AAL.helpdesk_ticket_id IS NOT NULL
+                THEN 'helpdesk.ticket'
+                ELSE 'project.task'
+            END
+        """
+        parent_res_id_query = """
+            CASE
+                WHEN AAL.helpdesk_ticket_id IS NOT NULL
+                THEN AAL.helpdesk_ticket_id
+                ELSE AAL.task_id
+             END
+        """
+        where_clause = """
+            COALESCE(AAL.helpdesk_ticket_id, AAL.task_id) IS NOT NULL
+        """
+    else:
+        parent_res_model_query = "'project.task'"
+        parent_res_id_query = "AAL.task_id"
+        where_clause = "AAL.task_id IS NOT NULL"
+    util.explode_execute(
+        cr,
+        util.format_query(
+            cr,
+            """
+                UPDATE timer_timer t
+                   SET parent_res_model = {parent_res_model},
+                       parent_res_id = {parent_res_id}
+                  FROM account_analytic_line AAL
+                  WHERE t.res_model = 'account.analytic.line'
+                    AND AAL.id = t.res_id
+                    AND {where_clause}
+            """,
+            parent_res_model=sql.SQL(parent_res_model_query),
+            parent_res_id=sql.SQL(parent_res_id_query),
+            where_clause=sql.SQL(where_clause),
+        ),
+        table="timer_timer",
+        alias="t",
+    )
