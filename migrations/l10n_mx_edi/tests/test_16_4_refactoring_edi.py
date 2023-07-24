e869419a4a796ef2dd4ac7e0d4cc4baa227769bc
Alvaro Fuentes <afu@odoo.com>
2023-07-24 13:24:48 +0000

    [chore] l10n_mx_edi/tests: automatic fixes
    Part-of: odoo/upgrade#4965

--- migrations/l10n_mx_edi/tests/test_16_4_refactoring_edi.py
+++ migrations/l10n_mx_edi/tests/test_16_4_refactoring_edi.py
@@ -15,61 +15,72 @@ from odoo.addons.base.maintenance.migrations.testing import change_version
 class TestRefactoringL10nMxEDI(TestAccountingSetupCommon, abstract=True):
     @contextmanager
     def with_mocked_pac_method(self, method_name, method_replacement):
-        """ Helper to mock an rpc call to the PAC.
+        """Helper to mock an rpc call to the PAC.
         :param method_name:         The name of the method to mock.
         :param method_replacement:  The method to be called instead.
         """
-        with patch.object(type(self.env['account.edi.format']), method_name, method_replacement):
+        with patch.object(type(self.env["account.edi.format"]), method_name, method_replacement):
             yield
 
     def prepare(self):
-        results = super().prepare(chart_template_ref='mx')
+        results = super().prepare(chart_template_ref="mx")
 
-        self.certificate = self.env['l10n_mx_edi.certificate'].create({
-            'content': base64.encodebytes(misc.file_open('l10n_mx_edi/demo/pac_credentials/certificate.cer', 'rb').read()),
-            'key': base64.encodebytes(misc.file_open('l10n_mx_edi/demo/pac_credentials/certificate.key', 'rb').read()),
-            'password': '12345678a',
-        })
-        self.certificate.write({
-            'date_start': '2016-01-01 01:00:00',
-            'date_end': '2018-01-01 01:00:00',
-        })
+        self.certificate = self.env["l10n_mx_edi.certificate"].create(
+            {
+                "content": base64.encodebytes(
+                    misc.file_open("l10n_mx_edi/demo/pac_credentials/certificate.cer", "rb").read()
+                ),
+                "key": base64.encodebytes(
+                    misc.file_open("l10n_mx_edi/demo/pac_credentials/certificate.key", "rb").read()
+                ),
+                "password": "12345678a",
+            }
+        )
+        self.certificate.write(
+            {
+                "date_start": "2016-01-01 01:00:00",
+                "date_end": "2018-01-01 01:00:00",
+            }
+        )
 
-        self.env.company.write({
-            'vat': 'EKU9003173C9',
-            'street': 'Campobasso Norte 3206 - 9000',
-            'street2': 'Fraccionamiento Montecarlo',
-            'zip': '85134',
-            'city': 'Ciudad Obregón',
-            'country_id': self.env.ref('base.mx').id,
-            'state_id': self.env.ref('base.state_mx_son').id,
-            'l10n_mx_edi_pac': 'solfact',
-            'l10n_mx_edi_pac_test_env': True,
-            'l10n_mx_edi_fiscal_regime': '601',
-            'l10n_mx_edi_certificate_ids': [(6, 0 , self.certificate.ids)],
-        })
+        self.env.company.write(
+            {
+                "vat": "EKU9003173C9",
+                "street": "Campobasso Norte 3206 - 9000",
+                "street2": "Fraccionamiento Montecarlo",
+                "zip": "85134",
+                "city": "Ciudad Obregón",
+                "country_id": self.env.ref("base.mx").id,
+                "state_id": self.env.ref("base.state_mx_son").id,
+                "l10n_mx_edi_pac": "solfact",
+                "l10n_mx_edi_pac_test_env": True,
+                "l10n_mx_edi_fiscal_regime": "601",
+                "l10n_mx_edi_certificate_ids": [(6, 0, self.certificate.ids)],
+            }
+        )
 
-        self.product = self.env['product.product'].create({
-            'name': 'product_mx',
-            'default_code': "product_mx",
-            'uom_po_id': self.env.ref('uom.product_uom_kgm').id,
-            'uom_id': self.env.ref('uom.product_uom_kgm').id,
-            'lst_price': 1000.0,
-            'property_account_income_id': self.account_income.id,
-            'unspsc_code_id': self.env.ref('product_unspsc.unspsc_code_01010101').id,
-        })
+        self.product = self.env["product.product"].create(
+            {
+                "name": "product_mx",
+                "default_code": "product_mx",
+                "uom_po_id": self.env.ref("uom.product_uom_kgm").id,
+                "uom_id": self.env.ref("uom.product_uom_kgm").id,
+                "lst_price": 1000.0,
+                "property_account_income_id": self.account_income.id,
+                "unspsc_code_id": self.env.ref("product_unspsc.unspsc_code_01010101").id,
+            }
+        )
         return results
 
 
-@change_version('saas~16.4')
+@change_version("saas~16.4")
 class TestRefactoringEDI(TestRefactoringL10nMxEDI):
-
     def with_mocked_pac_sign_success(self, method_name):
-        """ Fake the SAT cfdi stamping. """
+        """Fake the SAT cfdi stamping."""
 
         def method_replacement(_format, _record, exported):
             # Inject UUID.
-            tree = etree.fromstring(exported['cfdi_str'])
+            tree = etree.fromstring(exported["cfdi_str"])
             uuid = f"00000000-0000-0000-0000-{tree.attrib['Folio'].rjust(12, '0')}"
             stamp = f"""
                 <tfd:TimbreFiscalDigital
@@ -97,103 +108,117 @@ class TestRefactoringEDI(TestRefactoringL10nMxEDI):
                 """
                 tree.insert(len(tree), etree.fromstring(complemento_node))
                 tree[-1].attrib.clear()
-            cfdi_str = etree.tostring(tree, xml_declaration=True, encoding='UTF-8')
+            cfdi_str = etree.tostring(tree, xml_declaration=True, encoding="UTF-8")
 
             return {
-                'cfdi_signed': cfdi_str,
-                'cfdi_encoding': 'str',
+                "cfdi_signed": cfdi_str,
+                "cfdi_encoding": "str",
             }
 
         return self.with_mocked_pac_method(method_name, method_replacement)
 
     def with_mocked_pac_invoice_sign_success(self):
-        return self.with_mocked_pac_sign_success('_l10n_mx_edi_post_invoice_pac')
+        return self.with_mocked_pac_sign_success("_l10n_mx_edi_post_invoice_pac")
 
     def with_mocked_pac_payment_sign_success(self):
-        return self.with_mocked_pac_sign_success('_l10n_mx_edi_post_payment_pac')
+        return self.with_mocked_pac_sign_success("_l10n_mx_edi_post_payment_pac")
 
-    @freeze_time('2017-01-01')
+    @freeze_time("2017-01-01")
     def _prepare_test_invoice_with_payment(self):
-        inv = self.env['account.move'].create({
-            'move_type': 'out_invoice',
-            'partner_id': self.partner.id,
-            'invoice_date': '2017-01-01',
-            'date': '2017-01-01',
-            'invoice_date_due': '2017-02-01', # Force PPD by default
-            'l10n_mx_edi_payment_method_id': self.env.ref('l10n_mx_edi.payment_method_efectivo').id,
-            'invoice_line_ids': [(0, 0, {'product_id': self.product.id})],
-        })
+        inv = self.env["account.move"].create(
+            {
+                "move_type": "out_invoice",
+                "partner_id": self.partner.id,
+                "invoice_date": "2017-01-01",
+                "date": "2017-01-01",
+                "invoice_date_due": "2017-02-01",  # Force PPD by default
+                "l10n_mx_edi_payment_method_id": self.env.ref("l10n_mx_edi.payment_method_efectivo").id,
+                "invoice_line_ids": [(0, 0, {"product_id": self.product.id})],
+            }
+        )
         with self.with_mocked_pac_invoice_sign_success():
             inv.action_post()
             inv.edi_document_ids._process_documents_web_services(with_commit=False)
-            inv.l10n_mx_edi_sat_status = 'not_found'
+            inv.l10n_mx_edi_sat_status = "not_found"
         with self.with_mocked_pac_payment_sign_success():
-            pay = self.env['account.payment.register'] \
-                .with_context(active_model='account.move', active_ids=inv.ids).create({}) \
+            pay = (
+                self.env["account.payment.register"]
+                .with_context(active_model="account.move", active_ids=inv.ids)
+                .create({})
                 ._create_payments()
+            )
             pay.edi_document_ids._process_documents_web_services(with_commit=False)
-            pay.move_id.l10n_mx_edi_sat_status = 'valid'
+            pay.move_id.l10n_mx_edi_sat_status = "valid"
 
         # For some reason, this compute is not triggered at the right time in this setup.
         # Since it's a stored field, invalidating the value is not enough.
         pay.move_id._compute_l10n_mx_edi_cfdi_request()
 
-        self.assertRecordValues(inv + pay.move_id, [
-            {
-                'l10n_mx_edi_cfdi_request': 'on_invoice',
-                'l10n_mx_edi_sat_status': 'not_found',
-                'l10n_mx_edi_cfdi_uuid': '00000000-0000-0000-0000-000000000001',
-            },
-            {
-                'l10n_mx_edi_cfdi_request': 'on_payment',
-                'l10n_mx_edi_sat_status': 'valid',
-                'l10n_mx_edi_cfdi_uuid': '00000000-0000-0000-0000-000000000001',
-            },
-        ])
-        self.assertRecordValues(inv.edi_document_ids + pay.edi_document_ids, [{'state': 'sent'}] * 2)
+        self.assertRecordValues(
+            inv + pay.move_id,
+            [
+                {
+                    "l10n_mx_edi_cfdi_request": "on_invoice",
+                    "l10n_mx_edi_sat_status": "not_found",
+                    "l10n_mx_edi_cfdi_uuid": "00000000-0000-0000-0000-000000000001",
+                },
+                {
+                    "l10n_mx_edi_cfdi_request": "on_payment",
+                    "l10n_mx_edi_sat_status": "valid",
+                    "l10n_mx_edi_cfdi_uuid": "00000000-0000-0000-0000-000000000001",
+                },
+            ],
+        )
+        self.assertRecordValues(inv.edi_document_ids + pay.edi_document_ids, [{"state": "sent"}] * 2)
         self.assertTrue(inv.edi_document_ids.attachment_id)
         self.assertTrue(pay.edi_document_ids.attachment_id)
 
         return inv.id, pay.id, inv.edi_document_ids.attachment_id.id, pay.edi_document_ids.attachment_id.id
 
     def _check_invoice_with_payment(self, _config, inv_id, pay_id, inv_cfdi_id, pay_cfdi_id):
-        inv = self.env['account.move'].browse(inv_id)
-        pay = self.env['account.payment'].browse(pay_id)
+        inv = self.env["account.move"].browse(inv_id)
+        pay = self.env["account.payment"].browse(pay_id)
 
-        self.assertRecordValues(inv + pay.move_id, [
-            {
-                'l10n_mx_edi_is_cfdi_needed': True,
-                'l10n_mx_edi_cfdi_state': 'sent',
-                'l10n_mx_edi_cfdi_sat_state': 'not_found',
-                'l10n_mx_edi_cfdi_attachment_id': inv_cfdi_id,
-                'l10n_mx_edi_cfdi_uuid': '00000000-0000-0000-0000-000000000001',
-            },
-            {
-                'l10n_mx_edi_is_cfdi_needed': True,
-                'l10n_mx_edi_cfdi_state': 'sent',
-                'l10n_mx_edi_cfdi_sat_state': 'valid',
-                'l10n_mx_edi_cfdi_attachment_id': pay_cfdi_id,
-                'l10n_mx_edi_cfdi_uuid': '00000000-0000-0000-0000-000000000001',
-            },
-        ])
-        self.assertRecordValues(inv.l10n_mx_edi_invoice_document_ids, [
-            {
-                'invoice_ids': inv.ids,
-                'move_id': pay.move_id.id,
-                'attachment_id': pay_cfdi_id,
-                'state': 'payment_sent',
-                'sat_state': 'valid',
-            },
-            {
-                'invoice_ids': inv.ids,
-                'move_id': inv.id,
-                'attachment_id': inv_cfdi_id,
-                'state': 'invoice_sent',
-                'sat_state': 'not_found',
-            },
-        ])
+        self.assertRecordValues(
+            inv + pay.move_id,
+            [
+                {
+                    "l10n_mx_edi_is_cfdi_needed": True,
+                    "l10n_mx_edi_cfdi_state": "sent",
+                    "l10n_mx_edi_cfdi_sat_state": "not_found",
+                    "l10n_mx_edi_cfdi_attachment_id": inv_cfdi_id,
+                    "l10n_mx_edi_cfdi_uuid": "00000000-0000-0000-0000-000000000001",
+                },
+                {
+                    "l10n_mx_edi_is_cfdi_needed": True,
+                    "l10n_mx_edi_cfdi_state": "sent",
+                    "l10n_mx_edi_cfdi_sat_state": "valid",
+                    "l10n_mx_edi_cfdi_attachment_id": pay_cfdi_id,
+                    "l10n_mx_edi_cfdi_uuid": "00000000-0000-0000-0000-000000000001",
+                },
+            ],
+        )
+        self.assertRecordValues(
+            inv.l10n_mx_edi_invoice_document_ids,
+            [
+                {
+                    "invoice_ids": inv.ids,
+                    "move_id": pay.move_id.id,
+                    "attachment_id": pay_cfdi_id,
+                    "state": "payment_sent",
+                    "sat_state": "valid",
+                },
+                {
+                    "invoice_ids": inv.ids,
+                    "move_id": inv.id,
+                    "attachment_id": inv_cfdi_id,
+                    "state": "invoice_sent",
+                    "sat_state": "not_found",
+                },
+            ],
+        )
 
     def prepare(self):
         results = super().prepare()
-        results["tests"].append(('_check_invoice_with_payment', self._prepare_test_invoice_with_payment()))
+        results["tests"].append(("_check_invoice_with_payment", self._prepare_test_invoice_with_payment()))
         return results
--- migrations/l10n_mx_edi_stock/tests/test_16_4_refactoring_edi.py
+++ migrations/l10n_mx_edi_stock/tests/test_16_4_refactoring_edi.py
@@ -6,11 +6,10 @@ from odoo.addons.base.maintenance.migrations.l10n_mx_edi.tests.test_16_4_refacto
 from odoo.addons.base.maintenance.migrations.testing import change_version
 
 
-@change_version('saas~16.4')
+@change_version("saas~16.4")
 class TestRefactoringEDI(TestRefactoringL10nMxEDI):
-
     def with_mocked_pac_sign_success(self):
-        """ Fake the SAT cfdi stamping. """
+        """Fake the SAT cfdi stamping."""
 
         def method_replacement(_format, credentials, cfdi):
             # Inject UUID.
@@ -42,135 +41,165 @@ class TestRefactoringEDI(TestRefactoringL10nMxEDI):
                 """
                 tree.insert(len(tree), etree.fromstring(complemento_node))
                 tree[-1].attrib.clear()
-            cfdi_str = etree.tostring(tree, xml_declaration=True, encoding='UTF-8')
+            cfdi_str = etree.tostring(tree, xml_declaration=True, encoding="UTF-8")
 
             return {
-                'cfdi_signed': cfdi_str,
-                'cfdi_encoding': 'str',
+                "cfdi_signed": cfdi_str,
+                "cfdi_encoding": "str",
             }
 
-        return self.with_mocked_pac_method('_l10n_mx_edi_solfact_sign', method_replacement)
+        return self.with_mocked_pac_method("_l10n_mx_edi_solfact_sign", method_replacement)
 
-    @freeze_time('2017-01-01')
+    @freeze_time("2017-01-01")
     def _prepare_test_stock_picking(self):
-        picking = self.env['stock.picking'].create({
-            'location_id': self.new_wh.lot_stock_id.id,
-            'location_dest_id': self.customer_location.id,
-            'picking_type_id': self.new_wh.out_type_id.id,
-            'partner_id': self.partner.id,
-            'l10n_mx_edi_transport_type': '01',
-            'l10n_mx_edi_vehicle_id': self.vehicle_pedro.id,
-            'l10n_mx_edi_distance': 120,
-            'state': 'draft',
-            'immediate_transfer': False,
-        })
-        self.env['stock.move'].create({
-            'name': self.product.name,
-            'product_id': self.product.id,
-            'product_uom_qty': 10,
-            'product_uom': self.product.uom_id.id,
-            'picking_id': picking.id,
-            'location_id': self.new_wh.lot_stock_id.id,
-            'location_dest_id': self.customer_location.id,
-            'state': 'confirmed',
-            'description_picking': self.product.name,
-        })
+        picking = self.env["stock.picking"].create(
+            {
+                "location_id": self.new_wh.lot_stock_id.id,
+                "location_dest_id": self.customer_location.id,
+                "picking_type_id": self.new_wh.out_type_id.id,
+                "partner_id": self.partner.id,
+                "l10n_mx_edi_transport_type": "01",
+                "l10n_mx_edi_vehicle_id": self.vehicle_pedro.id,
+                "l10n_mx_edi_distance": 120,
+                "state": "draft",
+                "immediate_transfer": False,
+            }
+        )
+        self.env["stock.move"].create(
+            {
+                "name": self.product.name,
+                "product_id": self.product.id,
+                "product_uom_qty": 10,
+                "product_uom": self.product.uom_id.id,
+                "picking_id": picking.id,
+                "location_id": self.new_wh.lot_stock_id.id,
+                "location_dest_id": self.customer_location.id,
+                "state": "confirmed",
+                "description_picking": self.product.name,
+            }
+        )
 
-        self.env['stock.quant']._update_available_quantity(self.product, self.new_wh.lot_stock_id, 10.0)
+        self.env["stock.quant"]._update_available_quantity(self.product, self.new_wh.lot_stock_id, 10.0)
         picking.action_assign()
         picking.move_ids[0].move_line_ids[0].qty_done = 10
         picking._action_done()
 
         with self.with_mocked_pac_sign_success():
             picking.l10n_mx_edi_action_send_delivery_guide()
-            picking.l10n_mx_edi_sat_status = 'valid'
+            picking.l10n_mx_edi_sat_status = "valid"
 
-        self.assertRecordValues(picking, [{
-            'l10n_mx_edi_status': 'sent',
-            'l10n_mx_edi_sat_status': 'valid',
-        }])
+        self.assertRecordValues(
+            picking,
+            [
+                {
+                    "l10n_mx_edi_status": "sent",
+                    "l10n_mx_edi_sat_status": "valid",
+                }
+            ],
+        )
         self.assertTrue(picking.l10n_mx_edi_cfdi_file_id)
 
         return picking.id, picking.l10n_mx_edi_cfdi_file_id.id
 
     def _check_stock_picking(self, _config, picking_id, cfdi_id):
-        picking = self.env['stock.picking'].browse(picking_id)
-        self.assertRecordValues(picking, [{
-            'l10n_mx_edi_is_cfdi_needed': True,
-            'l10n_mx_edi_cfdi_state': 'sent',
-            'l10n_mx_edi_cfdi_sat_state': 'valid',
-            'l10n_mx_edi_cfdi_attachment_id': cfdi_id,
-            'l10n_mx_edi_cfdi_uuid': '00000000-0000-0000-0000-000000000001',
-        }])
-        self.assertRecordValues(picking.l10n_mx_edi_document_ids, [
-            {
-                'picking_id': picking_id,
-                'attachment_id': cfdi_id,
-                'state': 'picking_sent',
-                'sat_state': 'valid',
-            },
-        ])
+        picking = self.env["stock.picking"].browse(picking_id)
+        self.assertRecordValues(
+            picking,
+            [
+                {
+                    "l10n_mx_edi_is_cfdi_needed": True,
+                    "l10n_mx_edi_cfdi_state": "sent",
+                    "l10n_mx_edi_cfdi_sat_state": "valid",
+                    "l10n_mx_edi_cfdi_attachment_id": cfdi_id,
+                    "l10n_mx_edi_cfdi_uuid": "00000000-0000-0000-0000-000000000001",
+                }
+            ],
+        )
+        self.assertRecordValues(
+            picking.l10n_mx_edi_document_ids,
+            [
+                {
+                    "picking_id": picking_id,
+                    "attachment_id": cfdi_id,
+                    "state": "picking_sent",
+                    "sat_state": "valid",
+                },
+            ],
+        )
 
     def prepare(self):
         results = super().prepare()
 
-        self.new_wh = self.env['stock.warehouse'].create({
-            'name': 'New Warehouse',
-            'reception_steps': 'one_step',
-            'delivery_steps': 'ship_only',
-            'code': 'NWH'
-        })
+        self.new_wh = self.env["stock.warehouse"].create(
+            {"name": "New Warehouse", "reception_steps": "one_step", "delivery_steps": "ship_only", "code": "NWH"}
+        )
 
-        self.customer_location = self.env.ref('stock.stock_location_customers')
+        self.customer_location = self.env.ref("stock.stock_location_customers")
 
-        self.product.write({
-            'type': 'product',
-            'unspsc_code_id': self.env.ref('product_unspsc.unspsc_code_56101500').id,
-            'weight': 1,
-        })
+        self.product.write(
+            {
+                "type": "product",
+                "unspsc_code_id": self.env.ref("product_unspsc.unspsc_code_56101500").id,
+                "weight": 1,
+            }
+        )
 
-        self.partner = self.env['res.partner'].create({
-            'name': 'INMOBILIARIA',
-            'street': 'Street Calle',
-            'city': 'Hidalgo del Parral',
-            'country_id': self.env.ref('base.mx').id,
-            'state_id': self.env.ref('base.state_mx_chih').id,
-            'zip': '33826',
-            'vat': 'ICV060329BY0',
-        })
+        self.partner = self.env["res.partner"].create(
+            {
+                "name": "INMOBILIARIA",
+                "street": "Street Calle",
+                "city": "Hidalgo del Parral",
+                "country_id": self.env.ref("base.mx").id,
+                "state_id": self.env.ref("base.state_mx_chih").id,
+                "zip": "33826",
+                "vat": "ICV060329BY0",
+            }
+        )
 
-        self.operator_pedro = self.env['res.partner'].create({
-            'name': 'Amigo Pedro',
-            'vat': 'VAAM130719H60',
-            'street': 'JESUS VALDES SANCHEZ',
-            'city': 'Arteaga',
-            'country_id': self.env.ref('base.mx').id,
-            'state_id': self.env.ref('base.state_mx_coah').id,
-            'zip': '25350',
-            'l10n_mx_edi_operator_licence': 'a234567890',
-        })
+        self.operator_pedro = self.env["res.partner"].create(
+            {
+                "name": "Amigo Pedro",
+                "vat": "VAAM130719H60",
+                "street": "JESUS VALDES SANCHEZ",
+                "city": "Arteaga",
+                "country_id": self.env.ref("base.mx").id,
+                "state_id": self.env.ref("base.state_mx_coah").id,
+                "zip": "25350",
+                "l10n_mx_edi_operator_licence": "a234567890",
+            }
+        )
 
-        self.vehicle_pedro = self.env['l10n_mx_edi.vehicle'].create({
-            'name': 'DEMOPERMIT',
-            'transport_insurer': 'DEMO INSURER',
-            'transport_insurance_policy': 'DEMO POLICY',
-            'transport_perm_sct': 'TPAF10',
-            'vehicle_model': '2020',
-            'vehicle_config': 'T3S1',
-            'vehicle_licence': 'ABC123',
-            'trailer_ids': [(0, 0, {'name': 'trail1', 'sub_type': 'CTR003'})],
-            'figure_ids': [
-                (0, 0, {
-                    'type': '01',
-                    'operator_id': self.operator_pedro.id,
-                }),
-                (0, 0, {
-                    'type': '02',
-                    'operator_id': self.env.company.partner_id.id,
-                    'part_ids': [(6, 0, self.env.ref('l10n_mx_edi_stock.l10n_mx_edi_part_05').ids)],
-                }),
-            ],
-        })
+        self.vehicle_pedro = self.env["l10n_mx_edi.vehicle"].create(
+            {
+                "name": "DEMOPERMIT",
+                "transport_insurer": "DEMO INSURER",
+                "transport_insurance_policy": "DEMO POLICY",
+                "transport_perm_sct": "TPAF10",
+                "vehicle_model": "2020",
+                "vehicle_config": "T3S1",
+                "vehicle_licence": "ABC123",
+                "trailer_ids": [(0, 0, {"name": "trail1", "sub_type": "CTR003"})],
+                "figure_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "type": "01",
+                            "operator_id": self.operator_pedro.id,
+                        },
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "type": "02",
+                            "operator_id": self.env.company.partner_id.id,
+                            "part_ids": [(6, 0, self.env.ref("l10n_mx_edi_stock.l10n_mx_edi_part_05").ids)],
+                        },
+                    ),
+                ],
+            }
+        )
 
-        results["tests"].append(('_check_stock_picking', self._prepare_test_stock_picking()))
+        results["tests"].append(("_check_stock_picking", self._prepare_test_stock_picking()))
         return results
