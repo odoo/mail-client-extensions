2bc34aeea9c085984d27560117036213e60b5831
Valeriya(vchu) <vchu@odoo.com>
2023-07-25 10:53:49 +0200

    [FIX] website_sale/saas~16.4: set ribbon from product template
    before removing link between ribon and tag set website_ribbon_id if None
    from product tags that are linked to ribbons on product template.
    
     see odoo/odoo#124299
     fixes #4824
    
    closes odoo/upgrade#4969
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/website_sale/saas~16.4.1.1/pre-migrate.py
+++ migrations/website_sale/saas~16.4.1.1/pre-migrate.py
@@ -6,6 +6,24 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "res.company", "website_sale_onboarding_payment_provider_state")
     util.remove_field(cr, "res.config.settings", "terms_url")
+    cr.execute(
+        """
+        WITH tab AS (
+          SELECT rel.product_template_id AS ptid,
+                 min(tag.ribbon_id) AS rid
+            FROM product_tag_product_template_rel rel
+            JOIN product_tag tag
+              ON tag.id = rel.product_tag_id
+           WHERE tag.ribbon_id IS NOT NULL
+        GROUP BY rel.product_template_id
+        )
+        UPDATE product_template pt
+           SET website_ribbon_id = tab.rid
+          FROM tab
+         WHERE tab.ptid = pt.id
+           AND pt.website_ribbon_id IS NULL
+        """
+    )
     util.remove_field(cr, "product.tag", "ribbon_id")
     util.remove_field(cr, "product.ribbon", "product_tag_ids")
 
