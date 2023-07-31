b8871a5aae355e65c2ac0d83527e8f08903f1b9a
Mayurrajsinh Rathod <mrsr@odoo.com>
2023-07-31 10:32:43 +0000

    [IMP] documents/saas~16.5.1.3: associate owner to partner
    Now the owner_id refers to partner in documents.share model and
    the 'owner_id' field of 'documents.request_wizard' is rename to
    'requestee_id'.
    The purpose of the code is to synchronize documents.share records
    and to remove old field from documents.request_wizard.
    
    Related PR: https://github.com/odoo/enterprise/pull/41242
    
    Task-3290827
    
    closes odoo/upgrade#4822
    
    Related: odoo/enterprise#41242
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/documents/saas~16.5.1.3/pre-migrate.py
+++ migrations/documents/saas~16.5.1.3/pre-migrate.py
@@ -6,3 +6,15 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "documents.share_single")
     util.remove_view(cr, "documents.share_page")
+
+    util.remove_constraint(cr, "documents_share", "documents_share_owner_id_fkey")
+    query = """
+        UPDATE documents_share ds
+           SET owner_id = rp.id
+          FROM res_users ru
+          JOIN res_partner rp
+            ON rp.id = ru.partner_id
+         WHERE ru.id = ds.owner_id
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="documents_share", alias="ds"))
+    util.remove_field(cr, "documents.request_wizard", "owner_id")
