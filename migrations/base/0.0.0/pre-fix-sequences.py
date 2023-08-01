f08c7164d4a7df3e6afc921c9a920410de0c138a
arg-odoo <arg@odoo.com>
2023-08-01 13:45:23 +0000

    [FIX] base/0.0.0: Avoid sequence creation on identity columns
    Some database have (custom) identity columns. These already have
    implicit sequence[1] so should be skipped when creating missing
    sequence (as it's normal that no sequence exists for these tables).
    
    To check if a column is an identity one, it's possible to check the
    `pg_attribute.attidentity` value. As explained on psql doc [2]:
    ```
    If a zero byte (''), then not an identity column. Otherwise, a = generated always, d = generated by default.
    ```
    
    [1] https://www.postgresql.org/docs/current/sql-createtable.html
    [2] https://www.postgresql.org/docs/current/catalog-pg-attribute.html
    
    upg-1058936
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/14.0/odoo/service/server.py", line 1201, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "/home/odoo/src/odoo/14.0/odoo/modules/registry.py", line 89, in new
        odoo.modules.load_modules(registry._db, force_demo, status, update_module)
      File "/home/odoo/src/odoo/14.0/odoo/modules/loading.py", line 402, in load_modules
        report=report, models_to_check=models_to_check)
      File "/home/odoo/src/odoo/14.0/odoo/modules/loading.py", line 174, in load_module_graph
        migrations.migrate_module(package, 'pre')
      File "/home/odoo/src/odoo/14.0/odoo/modules/migration.py", line 186, in migrate_module
        migrate(self.cr, installed_version)
      File "/tmp/tmpnq7gg_ag/migrations/base/0.0.0/pre-fix-sequences.py", line 24, in migrate
        util.create_id_sequence(cr, table=table_name)
      File "/tmp/tmpnq7gg_ag/migrations/util/pg.py", line 1032, in create_id_sequence
        table=table_sql,
      File "<decorator-gen-3>", line 2, in execute
      File "/home/odoo/src/odoo/14.0/odoo/sql_db.py", line 101, in check
        return f(self, *args, **kwargs)
      File "/home/odoo/src/odoo/14.0/odoo/sql_db.py", line 301, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.SyntaxError: column "id" of relation "mmci_fsp_order" is an identity column
    ```
    
    closes odoo/upgrade#5001
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/base/0.0.0/pre-fix-sequences.py
+++ migrations/base/0.0.0/pre-fix-sequences.py
@@ -8,17 +8,24 @@ def migrate(cr, version):
     _logger = logging.getLogger(__name__)
 
     # with help from https://wiki.postgresql.org/wiki/Retrieve_primary_key_columns
-    cr.execute(
-        r"""
+    query = r"""
            SELECT SUBSTRING(pg_get_expr(d.adbin,0), 'nextval\(''([[:alpha:]][\w\$]*)''::regclass\)') as sequence_name,
-                t.relname table_name
+                  t.relname table_name
              FROM pg_attribute a
              JOIN pg_class t ON t.oid=a.attrelid AND t.relkind='r'
         FULL JOIN pg_attrdef d ON d.adrelid=a.attrelid AND a.attnum=d.adnum
              JOIN pg_index i ON a.attrelid=i.indrelid AND a.attnum=any(i.indkey)
-            WHERE a.attname='id';
+            WHERE a.attname='id'
         """
-    )
+
+    if cr._cnx.server_version >= 100000:
+        # identity column have been introduced in pg 10.0, these must be
+        # ignored as they already have their own implicit sequences.
+        # zero bytes ('') attidentity means columns isn't identity
+        query += " AND a.attidentity = ''"
+
+    cr.execute(query)
+
     for seq_name, table_name in cr.fetchall():
         if seq_name is None:
             util.create_id_sequence(cr, table=table_name)
