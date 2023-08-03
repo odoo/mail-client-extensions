bfa2b4386265b414d2ae5acf1556ea972adad50b
aliya <alta@odoo.com>
2023-08-03 08:06:52 +0000

    [IMP] l10n_mx_edi/saas~16.5.0.3: simplify l10n_mx_edi_certificate
    Currently, there is a m2m relationship between res_company
    and l10n_mx_edi_certificate
    (`l10n_mx_edi_certificate_res_company_rel`). This is going to change in 16.5,
    where we add a required m2o company_id field on the certificate.
    
    Even though certificates are not supposed to be shared between companies,
    the current design allows for it.
    So it is necessary to keep those certificates during upgrade.
    The idea is to copy the certificate data and insert a new row
    for every company that shares that certificate.
    First, the existing rows are updated with the company_id.
    Then we insert duplicate certificates for every other company.
    
    See:
    odoo/enterprise#44971
    
    closes odoo/upgrade#5011
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>
    Co-authored-by: dbkosky dako@odoo.com

---
+++ migrations/l10n_mx_edi/saas~16.5.0.3/pre-migrate.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "l10n_mx_edi_certificate", "company_id", "integer")
+    cr.execute(
+        """
+            WITH info AS (
+                SELECT l10n_mx_edi_certificate_id as certificate_id,
+                       array_agg(res_company_id) AS company_ids
+                  FROM l10n_mx_edi_certificate_res_company_rel
+              GROUP BY certificate_id
+            ),
+            _update AS (
+                UPDATE l10n_mx_edi_certificate c
+                   SET company_id = info.company_ids[1]
+                  FROM info
+                 WHERE c.id = info.certificate_id
+            )
+            INSERT INTO l10n_mx_edi_certificate(company_id, content, key, password, serial_number, date_start, date_end, create_uid, write_uid)
+                 SELECT unnest(info.company_ids[2:]), c.content, c.key, c.password, c.serial_number, c.date_start, c.date_end, c.create_uid, c.write_uid
+                   FROM l10n_mx_edi_certificate AS c
+                   JOIN info
+                     ON info.certificate_id = c.id
+                  WHERE array_length(info.company_ids, 1) > 1
+        """
+    )
+
+    # drop the m2m table as we remove the m2m relationship
+    cr.execute("DROP TABLE l10n_mx_edi_certificate_res_company_rel")
