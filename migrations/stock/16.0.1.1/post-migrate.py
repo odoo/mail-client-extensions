a056a734c74ac00d15ea54134791b3970e8212b0
arg-odoo <arg@odoo.com>
2023-08-03 14:22:25 +0000

    [FIX] stock/16.0.1.1: Add checks for uoms and products
    Add a check to ensure product from `stock.move` match the product from
    related `stock.move.line`.
    Add a check to ensure uom category from `stock.move.line` match the
    product template uom category.
    
    upg-1055834
    upg-1056248
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/16.0/odoo/service/server.py", line 1299, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "<decorator-gen-14>", line 2, in new
      File "/home/odoo/src/odoo/16.0/odoo/tools/func.py", line 87, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/16.0/odoo/modules/registry.py", line 90, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 484, in load_modules
        processed_modules += load_marked_modules(cr, graph,
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 372, in load_marked_modules
        loaded, processed = load_module_graph(
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 236, in load_module_graph
        migrations.migrate_module(package, 'post')
      File "/home/odoo/src/odoo/16.0/odoo/modules/migration.py", line 189, in migrate_module
        migrate(self.cr, installed_version)
      File "/tmp/tmpdfpow918/migrations/stock/16.0.1.1/post-migrate.py", line 8, in migrate
        util.recompute_fields(cr, "stock.move", ["quantity_done"])
      File "/tmp/tmpdfpow918/migrations/util/orm.py", line 210, in wrapper
        return f(*args, **kwargs)
      File "/tmp/tmpdfpow918/migrations/util/orm.py", line 241, in recompute_fields
        recompute(records)
      File "/tmp/tmpdfpow918/migrations/util/orm.py", line 184, in recompute
        (
      File "/home/odoo/src/odoo/16.0/odoo/api.py", line 728, in _recompute_all
        self[field.model_name]._recompute_field(field)
      File "/home/odoo/src/odoo/16.0/odoo/models.py", line 6178, in _recompute_field
        field.recompute(records)
      File "/home/odoo/src/odoo/16.0/odoo/fields.py", line 1365, in recompute
        apply_except_missing(self.compute_value, recs)
      File "/home/odoo/src/odoo/16.0/odoo/fields.py", line 1338, in apply_except_missing
        func(records)
      File "/home/odoo/src/odoo/16.0/odoo/fields.py", line 1387, in compute_value
        records._compute_field_value(self)
      File "/home/odoo/src/odoo/16.0/odoo/models.py", line 4211, in _compute_field_value
        fields.determine(field.compute, self)
      File "/home/odoo/src/odoo/16.0/odoo/fields.py", line 97, in determine
        return needle(*args)
      File "/home/odoo/src/odoo/16.0/addons/stock/models/stock_move.py", line 360, in _quantity_done_compute
        move.quantity_done = sum(
      File "/home/odoo/src/odoo/16.0/addons/stock/models/stock_move.py", line 361, in <genexpr>
        self.env['uom.uom'].browse(line_uom_id)._compute_quantity(qty, uom, round=False)
      File "/home/odoo/src/odoo/16.0/addons/uom/models/uom_uom.py", line 214, in _compute_quantity
        raise UserError(_('The unit of measure %s defined on the order line doesn\'t belong to the same category as the unit of measure %s defined on the product. Please correct the unit of measure defined on the order line or on the product, they should belong to the same category.') % (self.name, to_unit.name))
    odoo.exceptions.UserError: La unidad de medida Dozens que se definió en la otra linea no pertenece a la misma categoría que la unidad de medida Units que se definió en el producto. Corrija la unidad de medida que se definió en la línea de orden o en el producto, deben de ser de la misma categoría.
    ```
    
    closes odoo/upgrade#5017
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/stock/16.0.1.1/post-migrate.py
+++ migrations/stock/16.0.1.1/post-migrate.py
@@ -4,5 +4,7 @@ from odoo.upgrade.util import inconsistencies
 
 
 def migrate(cr, version):
+    inconsistencies.verify_products(cr, "stock.move", "stock.move.line", "move_id")
     inconsistencies.verify_uoms(cr, "stock.move", uom_field="product_uom")
+    inconsistencies.verify_uoms(cr, "stock.move.line", uom_field="product_uom_id")
     util.recompute_fields(cr, "stock.move", ["quantity_done"])
