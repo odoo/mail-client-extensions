31c44bf0cdbfbb1df19d98145fcba3fb35b72e5d
David Monnom (moda) <moda@odoo.com>
2023-08-07 15:08:18 +0200

    [IMP] pos_self_order/saas~16.5: rename view
    Allow end customers to place orders at a self-service kiosk (or totem).
    
    Main differences with the Self-order at Table:
    
    Kiosk shouldn't be accessible publicly
    Kiosk should support payments via card readers
    
    XLU - ADGU - MODA
    
    closes odoo/upgrade#5024
    
    Taskid: 3323163
    Community: https://github.com/odoo/odoo/pull/129562
    Related: odoo/odoo#129562
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/pos_self_order/saas~16.5.1.0/pre-migrate.py
+++ migrations/pos_self_order/saas~16.5.1.0/pre-migrate.py
@@ -5,3 +5,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "pos_self_order.first_letter_of_odoo_logo_svg")
     util.remove_view(cr, "pos_self_order.qr_code")
+    util.rename_xmlid(cr, "pos_self_order.index", "pos_self_order.mobile_index")
