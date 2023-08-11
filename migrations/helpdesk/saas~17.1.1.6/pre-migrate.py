cce16b160bacbbb54b23763d557a9f543a2a1ca2
Bastien (bvdn) <bvdn@odoo.com>
2023-08-11 16:00:24 +0200

    [FIX] helpdesk/saas~17.1: remove close_analysis_graph view
    also renamed mislabeled action.views record
    
    https://github.com/odoo/enterprise/pull/41739
    Task-3344837
    
    closes odoo/upgrade#5042
    
    Related: odoo/enterprise#41739
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/helpdesk/saas~17.1.1.6/pre-migrate.py
+++ migrations/helpdesk/saas~17.1.1.6/pre-migrate.py
@@ -4,4 +4,12 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    eb = util.expand_braces
+    util.remove_view(cr, "helpdesk.helpdesk_ticket_action_close_analysis_graph_inherit_dashboard")
+    util.rename_xmlid(cr, *eb("helpdesk.helpdesk_ticket_action_7days{,_}success_tree"))
+    util.rename_xmlid(cr, *eb("helpdesk.helpdesk_ticket_action_7days{,_}success_kanban"))
+    util.rename_xmlid(cr, *eb("helpdesk.helpdesk_ticket_action_7days{,_}success_activity"))
+    util.rename_xmlid(cr, *eb("helpdesk.helpdesk_ticket_action_7days{,_}success_pivot"))
+    util.rename_xmlid(cr, *eb("helpdesk.helpdesk_ticket_action_7days{,_}success_graph"))
+    util.remove_record(cr, "helpdesk.helpdesk_ticket_action_7dayssuccess_cohort")
     util.remove_record(cr, "helpdesk.model_helpdesk_ticket_action_share")
