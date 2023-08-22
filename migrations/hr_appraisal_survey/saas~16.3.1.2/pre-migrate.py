e901fa3efaea8eb583d0fc083b8351a643b28ad9
Lopes Marc (loma) <loma@odoo.com>
2023-08-22 15:17:08 +0200

    [IMP] hr_appraisal_survey/saas~16.3: update record rule
    When users with `User` access rights tries to access a survey form,
    they gets an Odoo server error.
    
    The error is caused by a record rule that calls on a field that doesn't
    exist in the survey model.
    
    The purpose of this PR is to update that record rule, after
    it has been fixed.
    
    see odoo/enterprise#46093
    task-3474933
    
    closes odoo/upgrade#5061
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr_appraisal_survey/saas~16.3.1.2/pre-migrate.py
+++ migrations/hr_appraisal_survey/saas~16.3.1.2/pre-migrate.py
@@ -6,3 +6,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     cr.execute("UPDATE survey_survey SET survey_type = 'appraisal' WHERE is_appraisal")
     util.remove_field(cr, "survey.survey", "is_appraisal")
+
+    util.update_record_from_xml(cr, "hr_appraisal_survey.survey_survey_rule_appraisal_manager")
