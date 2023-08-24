c6b1608032684ca02b67306b17b22d01651c47bf
Dhwani Patel <dwa@odoo.com>
2023-08-24 13:31:17 +0000

    [FIX] website_slides/saas~16.3.2.7: Fix standard view translation
    From version 16.2 to 16.3, the name of span tag has been changed from
    'members_done_count_label' to 'members_completed_count_label' but while
    upgrading that translation of this name is not been updated as per standard due
    to we update translation from old version transaltion directory that's why that
    not update. Therefore, when website_slide_survey mdoule is installed, So,
    'slide.channel.view.form.inherit.survey' used 'members_completed_count_label'
    named span in xpath that's why  while open elearning module with any other
    language except 'en_US' then it will raise error.
    
    We need to update this view manually for other languages.
    
    opw: 3377412, 3448888, 3429011, 3423448, 3441408
    upg-1051605, 1126421, 1066244, 1121484, 1076971
    
    closes odoo/upgrade#5074
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/website_slides/saas~16.3.2.7/post-migrate.py
+++ migrations/website_slides/saas~16.3.2.7/post-migrate.py
@@ -5,3 +5,12 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.if_unchanged(cr, "website_slides.mail_template_slide_channel_enroll", util.update_record_from_xml)
     util.if_unchanged(cr, "website_slides.mail_notification_channel_invite", util.update_record_from_xml)
+    extra_filter = cr.mogrify("t.id = %s", [util.ref(cr, "website_slides.view_slide_channel_form")]).decode()
+    util.replace_in_all_jsonb_values(
+        cr,
+        "ir_ui_view",
+        "arch_db",
+        "members_done_count_label",
+        "members_completed_count_label",
+        extra_filter=extra_filter,
+    )
