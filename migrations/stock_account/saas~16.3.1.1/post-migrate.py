6b48da34f1a95e184eb411edb4108adfc6bee37c
Tiffany Chang (tic) <tic@odoo.com>
2023-08-25 13:06:42 +0000

    [FIX] stock_account/saas~16.3: activate feature when already used
    Before COM PR: odoo/odoo#113973 was merged, the ability to use
    automatic accounting for stock valuation (i.e. ability to set
    `property_valuation='real_time'` for `product.category`) was always
    active when stock_account was installed. After the PR was merged, there
    was a res_config_setting added to activate this feature and was False by
    default (i.e. the setting hides the `property_valuation` field from the
    UI). This did not make sense for those who were already using the
    feature and could suddenly no longer see/change the field.
    
    Therefore we check if the db has any instance of
    `property_valuation='real_time'` and activate this setting so they can
    still see it and their db is more consistent.
    
    Ref for context (i.e. the part about not being able to see the field
    anymore)
    OPW-3474598
    
    closes odoo/upgrade#5075
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/stock_account/saas~16.3.1.1/post-migrate.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+            SELECT 1
+              FROM ir_property
+             WHERE name='property_valuation'
+               AND value_text='real_time'
+             LIMIT 1
+        """
+    )
+    if cr.rowcount:
+        # feature is already being used => activate the "setting" for it
+        usr_id = util.ref(cr, "base.group_user")
+        xml_id = util.ref(cr, "stock_account.group_stock_accounting_automatic")
+        util.env(cr)["res.groups"].browse(usr_id).write({"implied_ids": [(4, xml_id)]})
