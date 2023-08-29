c20ac1634016a9bc2569e5727e361ea0c7bd9465
Jinal Patel <jip@odoo.com>
2023-08-29 12:42:41 +0000

    [IMP] website_sale_loyalty/saas~15.3: Improve loyalty fix
    - Issue when coupon module is not installed then DB will be failed.
      So, added a condition
    
    closes odoo/upgrade#5084
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/website_sale_loyalty/saas~15.3.1.0/post-migrate.py
+++ migrations/website_sale_loyalty/saas~15.3.1.0/post-migrate.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
+from odoo.upgrade import util
+
 
 def migrate(cr, version):
-    cr.execute(
-        """
-        UPDATE loyalty_program loyalty
-           SET website_id = coupon.website_id
-          FROM coupon_program coupon
-         WHERE coupon.id = loyalty._upg_coupon_program_id
-           AND coupon.website_id IS NOT NULL
-        """
-    )
+    if util.column_exists(cr, "coupon_program", "website_id"):
+        cr.execute(
+            """
+            UPDATE loyalty_program loyalty
+               SET website_id = coupon.website_id
+              FROM coupon_program coupon
+             WHERE coupon.id = loyalty._upg_coupon_program_id
+               AND coupon.website_id IS NOT NULL
+            """
+        )
