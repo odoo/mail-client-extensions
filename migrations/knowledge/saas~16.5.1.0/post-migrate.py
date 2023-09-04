e332ea26a46a555a5fcc63b84babfd77e246c8da
Julien Banken <jbn@odoo.com>
2023-09-04 11:15:09 +0200

    [IMP] knowledge/saas~16.5: update knowledge templates
    This commit will update the default Knowledge templates and remove
    any irrelevant ones.
    
    See https://github.com/odoo/enterprise/pull/44394
    
    task-3423795
    
    closes odoo/upgrade#5097
    
    Related: odoo/enterprise#44394
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

---
+++ migrations/knowledge/saas~16.5.1.0/post-migrate.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.if_unchanged(cr, "knowledge.knowledge_template_my_todo_list", util.remove_record)
+    util.delete_unused(cr, "knowledge.knowledge_article_template_category_recommended")
+    util.if_unchanged(cr, "knowledge.knowledge_article_template_hr_faq", util.update_record_from_xml)
+    util.if_unchanged(cr, "knowledge.knowledge_article_template_brand_assets", util.update_record_from_xml)
+    util.delete_unused(cr, "knowledge.knowledge_article_template_category_sales_and_marketing")
+    util.if_unchanged(
+        cr, "knowledge.knowledge_article_template_category_company_organization", util.update_record_from_xml
+    )
