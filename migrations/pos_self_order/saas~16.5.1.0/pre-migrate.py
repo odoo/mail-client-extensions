a0c4d6965757d5f0bd8d6f3fb8ed6e6d7f057355
David Monnom (moda) <moda@odoo.com>
2023-09-05 14:43:00 +0200

    [IMP] pos_self_order/saas~16.5: remove unused column
    Before, fields in `res_config_settings.py` were not related to fields
    in `pos_config`. These have now been changed to related fields.
    
    The compute function managing these old fields has also been removed.
    
    Community PR: https://github.com/odoo/odoo/pull/133137
    
    closes odoo/upgrade#5112
    
    Related: odoo/odoo#133137
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/pos_self_order/saas~16.5.1.0/pre-migrate.py
+++ migrations/pos_self_order/saas~16.5.1.0/pre-migrate.py
@@ -6,3 +6,9 @@ def migrate(cr, version):
     util.remove_view(cr, "pos_self_order.first_letter_of_odoo_logo_svg")
     util.remove_view(cr, "pos_self_order.qr_code")
     util.rename_xmlid(cr, "pos_self_order.index", "pos_self_order.mobile_index")
+
+    util.remove_column(cr, "res_config_settings", "pos_self_order_view_mode")
+    util.remove_column(cr, "res_config_settings", "pos_self_order_table_mode")
+    util.remove_column(cr, "res_config_settings", "pos_self_order_pay_after")
+    util.remove_column(cr, "res_config_settings", "pos_self_order_image")
+    util.remove_column(cr, "res_config_settings", "pos_self_order_image_name")
