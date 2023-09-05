88af8877456e53e7661f860f74ae4d27b5565238
Mathias Mathy (MAMA) <mama@odoo.com>
2023-09-05 10:24:28 +0200

    [IMP] repair/16.5.1.0: inventory kanban view repair improvements
    1. Add Confirmed and Under Repair section to repair kanban cards.
    2. Modify default filters for repair orders.
    3. Modify the repair's location_id field's name
    4. Following recent changes on stock.picking.type, adapt return option
       creation on repair operation type defined as return.
    5. Only return transfers are proposed on the repair orders.
    6. Filter product_id selection based on set picking_id and vice-versa
    
    Task : 3369400
    Community : odoo/odoo#133932
    
    closes odoo/upgrade#5108
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/repair/saas~16.5.1.0/pre-migrate.py
+++ migrations/repair/saas~16.5.1.0/pre-migrate.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from odoo.upgrade import util
 
 
@@ -6,3 +7,15 @@ def migrate(cr, version):
     # Renaming those 2 fields for easier reading
     util.rename_field(cr, "stock.lot", "repair_order_ids", "repair_line_ids")
     util.rename_field(cr, "stock.lot", "repair_order_count", "repair_part_count")
+    util.remove_field(cr, "repair.order", "allowed_picking_type_ids")
+    util.remove_field(cr, "stock.picking.type", "count_repair_waiting")
+    util.remove_field(cr, "stock.picking.type", "count_repair_late")
+
+    cr.execute(
+        """
+          UPDATE stock_picking_type
+             SET is_repairable = False
+           WHERE is_repairable IS NOT False
+             AND id NOT IN (SELECT return_picking_type_id FROM stock_picking_type)
+        """,
+    )
