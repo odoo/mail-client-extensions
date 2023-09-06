3b46d686caacbdc45a8d1a8b1c4bc32ae0665db5
Himanshu Domadiya <hdo@odoo.com>
2023-09-06 10:11:06 +0000

    [FIX] mail/saas~12.1.1.0: improve performance at pre-10-blacklistmixin
    References:
    - opw- 1126839
    
    Summary:
    - With a huge number (3,64,55,835) of customers (res.partner) taking a
      bit more time to perform a query with "cr.execute", replaced with
      "util.parallel_execute" it's going smoothly and faster.
    
    Query:
    ```
    hdo_1126839=> SELECT count(*) FROM res_partner WHERE email IS NOT NULL;
      count
    ----------
     36455835
    (1 row)
    ```
    
    Without Fix: 2 hrs 43 min 4 sec
    ```
    2023-09-05 13:23:30,015 33 INFO hdo_1126839_14.0 odoo.modules.migration: module mail: Running migration [>0.0.0] pre-rm-unreachable
    2023-09-05 13:23:30,017 33 INFO hdo_1126839_14.0 odoo.modules.migration: module mail: Running migration [>0.0.0] pre-untracking
    2023-09-05 13:23:30,024 33 INFO hdo_1126839_14.0 odoo.modules.migration: module mail: Running migration [>saas~12.1.1.0] pre-10-blacklistmixin
    ```
    
    With Fix: 30 min 28 sec
    ```
    2023-09-06 12:20:30,015 33 INFO hdo_1126839_14.0 odoo.modules.migration: module mail: Running migration [>0.0.0] pre-rm-unreachable
    2023-09-06 12:20:30,017 33 INFO hdo_1126839_14.0 odoo.modules.migration: module mail: Running migration [>0.0.0] pre-untracking
    2023-09-06 12:20:30,024 33 INFO hdo_1126839_14.0 odoo.modules.migration: module mail: Running migration [>saas~12.1.1.0] pre-10-blacklistmixin
    ```
    
    closes odoo/upgrade#5117
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/mail/saas~12.1.1.0/pre-10-blacklistmixin.py
+++ migrations/mail/saas~12.1.1.0/pre-10-blacklistmixin.py
@@ -4,8 +4,13 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.create_column(cr, "res_partner", "email_normalized", "varchar")
-    cr.execute("""
+    util.explode_execute(
+        cr,
+        """
         UPDATE res_partner
            SET email_normalized=lower(substring(email, '([^ ,;<@]+@[^> ,;]+)'))
          WHERE email IS NOT NULL
-    """)
+        """,
+        table="res_partner",
+        bucket_size=50000,
+    )
