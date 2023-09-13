8d0f967ec8e4186afed840c70b4ff7a864982acf
William Henrotin <whe@odoo.com>
2023-09-13 07:41:58 +0200

    [IMP] stock/saas~16.5: no save detailed operation
    see https://github.com/odoo/odoo/pull/124409
    
    closes odoo/upgrade#5139
    
    Related: odoo/odoo#124409
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/stock/saas~16.5.1.1/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "stock.move", "display_clear_serial")
+    util.remove_model(cr, "stock.import.lot")
+    util.remove_model(cr, "stock.generate.serial")
