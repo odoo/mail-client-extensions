a8e5ccea97b7b9a2498bbcd5413d51b2958a66c4
sofiagvaladze <sgv@odoo.com>
2023-09-21 13:44:10 +0200

    [FIX] hr_appraisal_survey/saas~16.3: update rule
    enterprise - odoo/enterprise#47707
    
    task-3497285
    
    closes odoo/upgrade#5178
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/hr_appraisal_survey/saas~16.3.1.2/pre-migrate.py
+++ migrations/hr_appraisal_survey/saas~16.3.1.2/pre-migrate.py
@@ -8,3 +8,4 @@ def migrate(cr, version):
     util.remove_field(cr, "survey.survey", "is_appraisal")
 
     util.update_record_from_xml(cr, "hr_appraisal_survey.survey_survey_rule_appraisal_manager")
+    util.update_record_from_xml(cr, "survey.survey_user_input_rule_survey_user_read", from_module="hr_appraisal_survey")
