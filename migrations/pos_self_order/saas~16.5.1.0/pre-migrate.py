cdb3406b9d4af02c89fb17e50be2ba9c45ed9ed0
David Monnom (moda) <moda@odoo.com>
2023-09-21 12:40:10 +0200

    [IMP] pos_self_order/saas~16.5: merging kiosk and selfOrder mobile
    Previously, the self-order code and parameters were split into two parts
    mobile and kiosk.
    
    Now kiosk and mobile have been merged with each other to simplify
    maintainability.
    
    closes odoo/upgrade#5174
    
    Community: https://github.com/odoo/odoo/pull/136051
    Related: odoo/odoo#136051
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/pos_self_order/saas~16.5.1.0/pre-migrate.py
+++ migrations/pos_self_order/saas~16.5.1.0/pre-migrate.py
@@ -12,3 +12,30 @@ def migrate(cr, version):
     util.remove_column(cr, "res_config_settings", "pos_self_order_pay_after")
     util.remove_column(cr, "res_config_settings", "pos_self_order_image")
     util.remove_column(cr, "res_config_settings", "pos_self_order_image_name")
+
+    util.remove_field(cr, "pos.order.line", "selected_attributes")
+    util.create_column(cr, "pos_config", "self_ordering_mode", "varchar", default="nothing")
+    if util.column_exists(cr, "pos_config", "self_order_table_mode"):
+        cr.execute(
+            """
+                UPDATE pos_config
+                    SET self_ordering_mode = CASE
+                        WHEN self_order_table_mode = TRUE THEN 'mobile'
+                        WHEN self_order_view_mode = TRUE THEN 'consultation'
+                        ELSE 'nothing'
+                    END;"""
+        )
+
+    util.remove_field(cr, "pos.config", "self_order_image")
+    util.remove_field(cr, "pos.config", "self_order_table_mode")
+    util.remove_field(cr, "pos.config", "self_order_view_mode")
+    util.remove_field(cr, "pos.config", "self_order_image_name")
+
+    util.remove_field(cr, "res.config.settings", "pos_self_order_image")
+    util.remove_field(cr, "res.config.settings", "pos_self_order_table_mode")
+    util.remove_field(cr, "res.config.settings", "pos_self_order_view_mode")
+    util.remove_field(cr, "res.config.settings", "pos_self_order_image_name")
+
+    util.rename_field(cr, "pos.config", "self_order_pay_after", "self_ordering_pay_after")
+    util.rename_field(cr, "res.config.settings", "pos_self_order_pay_after", "pos_self_ordering_pay_after")
+    util.remove_view(cr, "pos_self_order.mobile_index")
