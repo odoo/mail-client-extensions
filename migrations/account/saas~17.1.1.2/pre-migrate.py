37dac84967782d81d71c4027015d2e09bd516abb
Joren Van Onder <jov@odoo.com>
2023-09-26 16:41:08 -0700

    [ADD] account{,_avatax}/saas~17.1: deprecate Taxcloud
    The accounting settings has a checkbox to install Taxcloud. Taxcloud
    will be deprecated in Odoo 17 so this checkbox is removed. Because of
    this we no longer need this field.
    
    On top of that Avatax is auto-installed for the appropriate countries
    so we removed that field from the accounting settings as well.
    
    Community PR: https://github.com/odoo/odoo/pull/136894
    Enterprise PR: https://github.com/odoo/enterprise/pull/48045
    
    task-3516826
    
    closes odoo/upgrade#5199
    
    Related: odoo/odoo#136894
    Related: odoo/enterprise#48045
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account/saas~17.1.1.2/pre-migrate.py
+++ migrations/account/saas~17.1.1.2/pre-migrate.py
@@ -5,3 +5,5 @@ def migrate(cr, version):
     util.remove_field(cr, "account.journal", "sale_activity_note")
     util.remove_field(cr, "account.journal", "sale_activity_user_id")
     util.remove_field(cr, "account.journal", "sale_activity_type_id")
+    util.remove_field(cr, "res.config.settings", "module_account_taxcloud")
+    util.remove_field(cr, "res.config.settings", "module_account_avatax")
