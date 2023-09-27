10e138329a276ede7d10f4cfa10583ace2324522
zel-odoo <zel@odoo.com>
2023-09-27 17:04:02 +0200

    [IMP] im_livechat/saas~16.5: move username
    Move livechat_username from res_users to res_users_settings
    
    Community PR: https://github.com/odoo/odoo/pull/133142
    
    closes odoo/upgrade#5113
    
    Related: odoo/odoo#133142
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/im_livechat/saas~16.5.1.0/pre-migrate.py
+++ migrations/im_livechat/saas~16.5.1.0/pre-migrate.py
@@ -24,3 +24,30 @@ def migrate(cr, version):
         table="discuss_channel_member",
         alias="m",
     )
+
+    # move livechat_username from res_users to res_users_settings
+    util.create_column(cr, "res_users_settings", "livechat_username", "varchar")
+    extra_cols, extra_vals = "", ""
+    if util.column_exists(cr, "res_users_settings", "how_to_call_on_mobile"):
+        # If VOIP is installed, set the required field 'how_to_call_on_mobile' to 'ask' as default
+        extra_cols = ", how_to_call_on_mobile"
+        extra_vals = ", 'ask'"
+    # update res_users_settings with livechat_username from res_users
+    # create res_users_settings for users that don't have one
+    util.explode_execute(
+        cr,
+        f"""
+        INSERT INTO res_users_settings (user_id, livechat_username {extra_cols})
+             SELECT u.id, u.livechat_username {extra_vals}
+               FROM res_users u
+              WHERE u.livechat_username IS NOT NULL
+                AND {{parallel_filter}}
+        ON CONFLICT (user_id) DO UPDATE
+                SET livechat_username = EXCLUDED.livechat_username;
+        """,
+        table="res_users",
+        alias="u",
+    )
+
+    # remove livechat_username from res_users
+    util.remove_column(cr, "res_users", "livechat_username")
