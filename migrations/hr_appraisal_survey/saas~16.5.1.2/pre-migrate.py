7e7ef27bb62b6ba783d7b5327b2fb3320b9d5291
jugj-odoo <jugj@odoo.com>
2023-10-03 11:24:13 +0200

    [IMP] hr_appraisal_survey/saas~16.5: Backport cleanup
    Enterprise PR: odoo/enterprise#48291
    
    closes odoo/upgrade#5213
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr_appraisal_survey/saas~16.5.1.2/pre-migrate.py
+++ migrations/hr_appraisal_survey/saas~16.5.1.2/pre-migrate.py
@@ -4,5 +4,15 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.update_record_from_xml(cr, "hr_appraisal_survey.mail_template_appraisal_ask_feedback")
+    eb = util.expand_braces
+    good = util.ref(cr, "hr_appraisal_survey.mail_template_appraisal_ask_feedback_formatted")
+    if good:
+        bad = util.ref(cr, "hr_appraisal_survey.mail_template_appraisal_ask_feedback")
+        if bad:
+            util.replace_record_references_batch(cr, {bad: good}, "mail.template", replace_xmlid=False)
+            util.remove_record(cr, "hr_appraisal_survey.mail_template_appraisal_ask_feedback")
+        util.rename_xmlid(cr, *eb("hr_appraisal_survey.mail_template_appraisal_ask_feedback{_formatted,}"))
+    else:
+        util.update_record_from_xml(cr, "hr_appraisal_survey.mail_template_appraisal_ask_feedback")
     util.remove_field(cr, "appraisal.ask.feedback", "user_body")
+    util.remove_view(cr, "hr_appraisal_survey.survey_user_input_view_tree")
