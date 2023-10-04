f9c9d8b0c53ee824a56e92f7e5c97078b20f0b49
Brieuc-brd <brd@odoo.com>
2023-10-04 09:53:48 +0200

    [IMP] website{_sale{_wishlist}}/saas~16.5: adapt header templates
    Since the redesign of header templates in [1], older templates have been
    removed or their xml id changed.
    
    [1]: https://github.com/odoo/odoo/pull/119650
    
    task-3060986
    
    closes odoo/upgrade#5214
    
    Related: odoo/design-themes#651
    Related: odoo/odoo#119650
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>
    Co-authored-by: qsm-odoo <qsm@odoo.com>

--- migrations/website/saas~16.5.1.0/pre-migrate.py
+++ migrations/website/saas~16.5.1.0/pre-migrate.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from odoo.upgrade import util
+
 
 def migrate(cr, version):
     cr.execute(
@@ -9,3 +11,51 @@ def migrate(cr, version):
          WHERE url IS NULL
         """
     )
+
+    util.remove_view(cr, "website_sale_wishlist.template_header_magazine")
+    util.remove_view(cr, "website_sale_wishlist.template_header_hamburger_full")
+    util.remove_view(cr, "website_sale_wishlist.template_header_image")
+
+    util.rename_xmlid(cr, "website_sale_wishlist.template_header_centered_logo", "website_sale_wishlist.template_header_search")
+    util.rename_xmlid(cr, "website_sale_wishlist.template_header_slogan", "website_sale_wishlist.template_header_sales_two")
+    util.rename_xmlid(cr, "website_sale_wishlist.template_header_contact", "website_sale_wishlist.template_header_sales_three")
+
+    util.remove_view(cr, "website_sale.template_header_magazine")
+    util.remove_view(cr, "website_sale.template_header_hamburger_full")
+    util.remove_view(cr, "website_sale.template_header_image")
+
+    util.rename_xmlid(cr, "website_sale.template_header_centered_logo", "website_sale.template_header_search")
+    util.rename_xmlid(cr, "website_sale.template_header_slogan", "website_sale.template_header_sales_two")
+    util.rename_xmlid(cr, "website_sale.template_header_contact", "website_sale.template_header_sales_three")
+
+    util.remove_view(cr, "website.option_header_no_mobile_hamburger")
+    util.remove_view(cr, "website.option_header_off_canvas_template_header_hamburger_full")
+    util.remove_view(cr, "website.option_header_off_canvas_template_header_sidebar")
+    util.remove_view(cr, "website.option_header_off_canvas_template_header_hamburger")
+    util.remove_view(cr, "website.option_header_off_canvas_logo_show")
+    util.remove_view(cr, "website.option_header_off_canvas")
+    util.remove_view(cr, "website.template_header_magazine_oe_structure_header_magazine_1")
+    util.remove_view(cr, "website.template_header_magazine")
+    util.remove_view(cr, "website.template_header_hamburger_full_oe_structure_header_hamburger_full_1")
+    util.remove_view(cr, "website.template_header_hamburger_full")
+    util.remove_view(cr, "website.template_header_image_oe_structure_header_image_1")
+    util.remove_view(cr, "website.template_header_image")
+    util.remove_view(cr, "website.template_header_boxed_oe_structure_header_boxed_1")
+    util.remove_view(cr, "website.template_header_contact_oe_structure_header_contact_1")
+    util.remove_view(cr, "website.template_header_slogan_oe_structure_header_slogan_3")
+    util.remove_view(cr, "website.template_header_slogan_oe_structure_header_slogan_1")
+    util.remove_view(cr, "website.template_header_slogan_align_right")
+    util.remove_view(cr, "website.template_header_slogan_align_center")
+    util.remove_view(cr, "website.template_header_sidebar_oe_structure_header_sidebar_1")
+    util.remove_view(cr, "website.template_header_vertical_oe_structure_header_vertical_3")
+    util.remove_view(cr, "website.template_header_vertical_oe_structure_header_vertical_2")
+    util.remove_view(cr, "website.template_header_vertical_oe_structure_header_vertical_1")
+    util.remove_view(cr, "website.template_header_hamburger_oe_structure_header_hamburger_3")
+    util.remove_view(cr, "website.template_header_hamburger_oe_structure_header_hamburger_2")
+    util.remove_view(cr, "website.template_header_hamburger_align_center")
+
+    util.rename_xmlid(cr, "website.template_header_centered_logo", "website.template_header_search")
+    util.rename_xmlid(cr, "website.template_header_slogan", "website.template_header_sales_two")
+    util.rename_xmlid(cr, "website.template_header_contact", "website.template_header_sales_three")
+
+    util.remove_view(cr, "website.language_selector_add_language")
