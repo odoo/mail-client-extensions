9966dc0c5cbea1467c0e16dc224f136c091585ed
amdi-odoo <amdi@odoo.com>
2023-10-05 16:10:18 +0200

    [IMP] appointment/saas~16.5: rename category and move anytime
    Rename the 'website' category of appointment type to
    'recurring'. This is done as with the arrival of
    the new 'punctual' appointment type category,
    the 'website' category isn't the only one available
    on the website anymore.
    
    Rename the xmlid of the action opening the Invitations
    menu to remove the 'anytime' part as anytime appointment
    types are now displayed in the Appointments menu.
    
    See odoo/enterprise#43298
    
    Task-3109254
    
    closes odoo/upgrade#5170
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/appointment/saas~16.5.1.3/pre-migrate.py
+++ migrations/appointment/saas~16.5.1.3/pre-migrate.py
@@ -4,4 +4,8 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.change_field_selection_values(cr, "appointment.type", "category", {"website": "recurring"})
     util.create_column(cr, "appointment_type", "event_videocall_source", "varchar")
+    util.rename_xmlid(
+        cr, "appointment.appointment_type_action_custom_and_anytime", "appointment.appointment_type_action_custom"
+    )
