ca6f8eb36a836e65f64220f9f36fe584b797cb4b
Noe Antoine <nan@odoo.com>
2023-10-06 17:30:12 +0200

    [FIX] website_slides/saas~16.5: remove useless field
    Remove field not used at all slide_channel_all_ids
    on res.partner. Do not remove table as it is a m2m
    reading from a model table slide.channel.partner.
    
    Task-3444633
    COM PR: odoo/odoo#130030
    
    closes odoo/upgrade#5218
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/website_slides/saas~16.5.2.7/pre-migrate.py
+++ migrations/website_slides/saas~16.5.2.7/pre-migrate.py
@@ -10,3 +10,5 @@ def migrate(cr, version):
     util.if_unchanged(cr, "website_slides.rule_slide_slide_signed_in_user", util.update_record_from_xml)
     util.if_unchanged(cr, "website_slides.rule_slide_slide_resource_downloadable", util.update_record_from_xml)
     util.remove_field(cr, "slide.channel", "partner_all_ids")
+
+    util.remove_field(cr, "res.partner", "slide_channel_all_ids", drop_column=False)
