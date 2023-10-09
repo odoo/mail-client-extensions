873197f5fdfa15d2eb16323d2325f2313b8db8cd
diagnoza <owi@odoo.com>
2023-10-09 22:54:30 +0000

    [FIX] sale/16.0: adapt cowed views
    Adapt cowed views of `sale.report_saleorder_document`.
    
    closes odoo/upgrade#5251
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/sale/16.0.1.2/post-migrate.py
+++ migrations/sale/16.0.1.2/post-migrate.py
@@ -12,3 +12,18 @@ def migrate(cr, version):
         util.replace_in_all_jsonb_values(
             cr, "mail_template", "body_html", old, new, extra_filter="t.model='sale.order'"
         )
+
+    cr.execute(
+        r"""
+        SELECT id
+          FROM ir_ui_view
+         WHERE key LIKE 'sale.report\_saleorder\_document%'
+           AND key <> 'sale.report_saleorder_document'
+        """
+    )
+    for view_id in cr.fetchall():
+        with util.edit_view(cr, view_id=view_id, active=None) as arch:
+            for node in arch.xpath(
+                '//div[@name="so_total_summary"]/div[@name="total"]/div[1]/table/t[@t-value="json.loads(doc.tax_totals_json)"]'
+            ):
+                node.attrib["t-value"] = "doc.tax_totals"
