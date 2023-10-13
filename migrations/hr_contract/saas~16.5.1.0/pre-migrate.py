b2769e6c0b782ec038a7e132da806e14baaa1950
Sina Chaichi Maleki (cmsi) <cmsi@odoo.com>
2023-10-13 16:41:02 +0200

    [IMP] hr_contract/saas~16.5.1.0: add contract menu
    As moving contract menu item from hr_work_entry_contract_enterprise
    to hr_contract, we need to rename the menu item xmlid.
    
    see https://github.com/odoo/enterprise/pull/46541
    see https://github.com/odoo/odoo/pull/133586
    
    closes odoo/upgrade#5275
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/hr_contract/saas~16.5.1.0/pre-migrate.py
+++ migrations/hr_contract/saas~16.5.1.0/pre-migrate.py
@@ -4,5 +4,7 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    eb = util.expand_braces
     util.update_field_usage(cr, "hr.contract", "visa_expire", "employee_id.work_permit_expiration_date")
     util.remove_field(cr, "hr.contract", "visa_expire")
+    util.rename_xmlid(cr, *eb("{hr_work_entry_contract_enterprise,hr_contract}.hr_menu_contract"))
