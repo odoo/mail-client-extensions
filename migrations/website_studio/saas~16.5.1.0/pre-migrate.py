f02eb9b25b4d0e064fc24a3c7d39f109b4d69a95
luvi <luvi@odoo.com>
2023-10-16 17:03:53 +0200

    [IMP] website_studio/saas~16.5,*: remove unused view
    Since the related PRs, this 'default_form_page' view from website_studio has
    been replaced by 'default_record_page'. The view should then be removed
    as well during the upgrade.
    
    See https://github.com/odoo/odoo/pull/130544
    See https://github.com/odoo/enterprise/pull/47116
    
    closes odoo/upgrade#5290
    
    Related: odoo/odoo#130544
    Related: odoo/enterprise#47116
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/website_studio/saas~16.5.1.0/pre-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "website_studio.default_form_page")
+    util.remove_record(cr, "website_studio.action_web_studio_form")
