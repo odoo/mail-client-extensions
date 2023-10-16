e510aad7589312ad4a2293a4c1af1d296867883c
Julien Banken <jbn@odoo.com>
2023-10-16 15:44:30 +0200

    [IMP] knowledge/saas~16.5: merge template model with article model
    To reduce the complexity of the code base, we will merge the models
    `knowledge.article` and `knowledge.article.template`. We will then
    avoid duplicating views and the code logic that were actually similar
    between the two models.
    
    See: https://github.com/odoo/enterprise/pull/48691
    
    task-3334717
    
    closes odoo/upgrade#5258
    
    Related: odoo/enterprise#48691
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/knowledge/saas~16.5.1.0/pre-migrate.py
+++ migrations/knowledge/saas~16.5.1.0/pre-migrate.py
@@ -29,3 +29,98 @@ def migrate(cr, version):
         );
         """
     )
+
+    util.remove_field(cr, "knowledge.cover", "article_template_ids")
+
+    util.create_column(cr, "knowledge_article", "is_template", "bool")
+    util.create_column(cr, "knowledge_article", "template_body", "jsonb")
+    util.create_column(cr, "knowledge_article", "template_category_id", "int4")
+    util.create_column(cr, "knowledge_article", "template_description", "jsonb")
+    util.create_column(cr, "knowledge_article", "template_name", "jsonb")
+    util.create_column(cr, "knowledge_article", "template_sequence", "int4")
+
+    if util.table_exists(cr, "knowledge_article_template"):
+        util.create_column(cr, "knowledge_article", "_upg_template_id", "int4")
+        util.create_column(cr, "knowledge_article", "_upg_template_parent_id", "int4")
+
+        cr.execute(
+            """
+            INSERT INTO knowledge_article (
+                active,
+                article_properties,
+                article_properties_definition,
+                cover_image_id,
+                create_date,
+                create_uid,
+                full_width,
+                icon,
+                internal_permission,
+                is_article_item,
+                is_template,
+                template_body,
+                template_category_id,
+                template_description,
+                template_name,
+                template_sequence,
+                _upg_template_id,
+                _upg_template_parent_id,
+                write_date,
+                write_uid
+            )
+            SELECT
+                TRUE                           AS active,
+                template_properties            AS article_properties,
+                template_properties_definition AS article_properties_definition,
+                cover_image_id,
+                create_date,
+                create_uid,
+                FALSE                          AS full_width,
+                icon,
+                'write'                        AS internal_permission,
+                is_template_item               AS is_article_item,
+                TRUE                           AS is_template,
+                body                           AS template_body,
+                category_id                    AS template_category,
+                description                    AS template_description,
+                name                           AS template_name,
+                sequence                       AS template_sequence,
+                id                             AS _upg_template_id,
+                parent_id                      AS _upg_template_parent_id,
+                write_date,
+                write_uid
+            FROM knowledge_article_template
+       RETURNING _upg_template_id, id;
+        """
+        )
+        mapping = dict(cr.fetchall())
+        if mapping:
+            util.replace_record_references_batch(
+                cr, mapping, model_src="knowledge.article.template", model_dst="knowledge.article"
+            )
+
+        cr.execute(
+            """
+            UPDATE knowledge_article AS child
+               SET parent_id = parent.id
+              FROM knowledge_article AS parent
+             WHERE child._upg_template_parent_id = parent._upg_template_id;
+            """
+        )
+
+        util.remove_column(cr, "knowledge_article", "_upg_template_id")
+        util.remove_column(cr, "knowledge_article", "_upg_template_parent_id")
+
+        util.merge_model(
+            cr,
+            "knowledge.article.template",
+            "knowledge.article",
+            fields_mapping={
+                "body": "template_body",
+                "category_id": "template_category_id",
+                "description": "template_description",
+                "name": "template_name",
+                "sequence": "template_sequence",
+                "template_properties": "article_properties",
+                "template_properties_definition": "article_properties_definition",
+            },
+        )
