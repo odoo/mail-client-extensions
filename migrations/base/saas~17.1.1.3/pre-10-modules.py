2c1ecb9b32e5f44f403ddf99d7d2d79d24d99757
Serge Bayet (seba) <seba@odoo.com>
2023-10-19 10:00:54 +0200

    [IMP] base/saas~17.1: rename website_form_project module
    This commit moves the website_form_project module to website_project
    related to [1]
    
    [1]: https://github.com/odoo/odoo/pull/139151
    task-3282988
    
    closes odoo/upgrade#5301
    
    Related: odoo/odoo#139151
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/base/saas~17.1.1.3/pre-10-modules.py
+++ migrations/base/saas~17.1.1.3/pre-10-modules.py
@@ -4,6 +4,7 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.rename_module(cr, "mrp_subonctracting_landed_costs", "mrp_subcontracting_landed_costs")
+    util.rename_module(cr, "website_form_project", "website_project")
 
     if util.has_enterprise():
         util.rename_module(cr, "account_bacs", "l10n_uk_bacs")
