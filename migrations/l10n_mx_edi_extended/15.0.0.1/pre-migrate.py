cc92e4df1b748ff401e83ac353f339c36b905938
pja-odoo <pja@odoo.com>
2023-10-23 10:54:07 +0000

    [FIX] l10n_mx_edi/15.0: avoid memory error
    We get a memory error when module 'l10n_mx_edi' is installed but module
    'l10n_mx_edi_extended' is uninstalled, it is force installed at
    `migrations/base/{15.0.1.3,saas~15.1.1.3}/pre-10-modules.py`.
    
    It then tries to compute the field 'l10n_mx_edi_locality', in 15.0:
    https://github.com/odoo/enterprise/blob/cdf8846/l10n_mx_edi_extended/models/res_partner.py#L10-L13
    but due to the large amount of partner data, it will crash.
    
    At force install of `l10n_mx_edi_extended` we do not need to compute
    `l10n_mx_edi_locality` because this field computation depends on
    `l10n_mx_edi_locality_id` which is a Many2one fields not set when module
    is uninstalled. In 15.0:
    https://github.com/odoo/enterprise/blob/cdf8846/l10n_mx_edi_extended/models/res_partner.py#L28
    
    upg-1155052
    tbg-337
    
    ugpgrade
    
    Part-of: odoo/upgrade#5313

--- migrations/l10n_mx_edi_extended/15.0.0.1/pre-migrate.py
+++ migrations/l10n_mx_edi_extended/15.0.0.1/pre-migrate.py
@@ -3,6 +3,7 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.create_column(cr, "res_partner", "l10n_mx_edi_locality", "varchar")
     util.move_field_to_module(cr, "res.city", "l10n_mx_edi_code", "l10n_mx_edi", "l10n_mx_edi_extended")
 
     # Move external ids of city in the right module in case they were in the wrong one
