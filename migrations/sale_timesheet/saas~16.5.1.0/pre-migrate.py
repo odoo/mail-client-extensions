06213a2eb2489aebd215becacc9f31eac3b1e843
Xavier Bol (xbo) <xbo@odoo.com>
2023-10-24 18:22:06 +0200

    [IMP] sale_timesheet/saas~16.5: add `billing_type` field in project
    This commit adds new field called `billing_type` in `project_project`
    model to allow users to flag the timesheets from a project as
    considering as billable even they have no SOL set.
    
    Community PR: odoo/odoo#139478
    
    task-3565762
    
    closes odoo/upgrade#5318
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/sale_timesheet/saas~16.5.1.0/pre-migrate.py
+++ migrations/sale_timesheet/saas~16.5.1.0/pre-migrate.py
@@ -6,3 +6,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "sale_timesheet.project_project_view_form_simplified_inherit")
     util.remove_field(cr, "project.project", "display_create_order")
+    util.create_column(cr, "project_project", "billing_type", "character varying", default="not_billable")
