93178d4a3e224c165bb379e9fb48581cb786f2d8
Alvaro Fuentes <afu@odoo.com>
2023-10-27 16:50:39 +0200

    [FIX] {base,l10n_mx_edi_extended}/15.0: fix force upgrade
    `force_upgrade_of_fresh_module` should be called in `base` pre scripts.
    
    Part-of: odoo/upgrade#5313

--- migrations/base/15.0.1.3/pre-10-modules.py
+++ migrations/base/15.0.1.3/pre-10-modules.py
@@ -10,3 +10,8 @@ def migrate(cr, version):
     util.merge_module(cr, "payment_adyen_paybylink", "payment_adyen")
     # https://github.com/odoo/enterprise/pull/23761
     util.force_upgrade_of_fresh_module(cr, "account_asset_ndt")
+
+    if util.module_installed(cr, "l10n_mx_edi"):
+        # res.city has been moved to l10n_mx_edi_extended in v15 https://github.com/odoo/upgrade/pull/3193
+        util.force_install_module(cr, "l10n_mx_edi_extended")
+        util.force_upgrade_of_fresh_module(cr, "l10n_mx_edi_extended")
--- migrations/base/saas~15.1.1.3/pre-10-modules.py
+++ migrations/base/saas~15.1.1.3/pre-10-modules.py
@@ -18,3 +18,8 @@ def migrate(cr, version):
     util.uninstall_module(cr, "saas_pad")
 
     util.force_upgrade_of_fresh_module(cr, "l10n_din5008")
+
+    if util.module_installed(cr, "l10n_mx_edi"):
+        # res.city has been moved to l10n_mx_edi_extended in v15 https://github.com/odoo/upgrade/pull/3193
+        util.force_install_module(cr, "l10n_mx_edi_extended")
+        util.force_upgrade_of_fresh_module(cr, "l10n_mx_edi_extended")
