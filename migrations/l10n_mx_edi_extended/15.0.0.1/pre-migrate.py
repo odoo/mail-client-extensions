f1c7c41befebba1034e47fc36beb8f1fcba56f2b
Alvaro Fuentes <afu@odoo.com>
2023-10-30 08:54:20 +0100

    [FIX] l10n_mx_edi_extended/15.0: fix move xmlids
    In odoo/upgrade#3164 we moved `res.city` xmlids from `l10n_mx_edi` to
    `l10n_mx_edi_extented`. Althought the creation of such entries is indeed
    done in the latter the xmlids are still associtated with `10n_mx_edi` in
    15.0:
    https://github.com/odoo/enterprise/blob/822c36b/l10n_mx_edi_extended/hooks.py#L36
    The new updated modules was done in saas~15.1:
    https://github.com/odoo/enterprise/blob/971aad3/l10n_mx_edi_extended/hooks.py#L36
    
    This patch avoid the error:
    ```
    Traceback (most recent call last):
    ...
      File "/data/build/odoo/odoo/addons/base/models/ir_model.py", line 1963, in _xmlid_lookup
        raise ValueError('External ID not found in the system: %s' % xmlid)
    ValueError: External ID not found in the system: l10n_mx_edi.res_city_mx_agu_005
    
    The above exception was the direct cause of the following exception:
    
    Traceback (most recent call last):
    ...
      File "/data/build/odoo/odoo/tools/convert.py", line 696, in _tag_root
        raise ParseError('while parsing %s:%s, somewhere inside\n%s' % (
    odoo.tools.convert.ParseError: while parsing /data/build/enterprise/l10n_mx_edi_extended_40/demo/res_partner.xml:4, somewhere inside
    <record id="l10n_mx.partner_demo_company_mx" model="res.partner">
            <field name="state_id" ref="base.state_mx_ags"/>
            <field name="city_id" ref="l10n_mx_edi.res_city_mx_agu_005"/>
            <field name="l10n_mx_edi_colony_code">0027</field>
        </record>
    ```
    when upgrading from 14 to 15.
    
    Renaming the xmlids is actually needed for saas-15.2.
    
    Part-of: odoo/upgrade#5313

--- migrations/l10n_mx_edi_extended/15.0.0.1/pre-migrate.py
+++ migrations/l10n_mx_edi_extended/15.0.0.1/pre-migrate.py
@@ -5,13 +5,3 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.create_column(cr, "res_partner", "l10n_mx_edi_locality", "varchar")
     util.move_field_to_module(cr, "res.city", "l10n_mx_edi_code", "l10n_mx_edi", "l10n_mx_edi_extended")
-
-    # Move external ids of city in the right module in case they were in the wrong one
-    cr.execute(
-        """
-        UPDATE ir_model_data
-          SET module = 'l10n_mx_edi_extended'
-        WHERE module = 'l10n_mx_edi'
-          AND model = 'res.city'
-        """
-    )
