a2c78a7a50c996aef84afc208df348d6cade0925
Arnold Moyaux <arm@odoo.com>
2023-11-13 13:52:43 +0100

    [IMP] stock/saas~16.5: mix of qty_done and reserved_qty
    Following pull request [1]
    The purpose is to merge both qty_done and product_qty on stock.move.line
    qty_done is always an input from the users while product_qty is mainly
    fill by the system and represent the quantity reserved for the picking.
    
    The purpose of the modificiation is explained in the community PR
    For the upgrade we choose to keep the qty_done and fill the quantity
    column with them. Also each move with qty_done is marked as picked to
    prevent modification later by the system. In order to achieve that, we
    drop the reservation on exisiting databases. They are added back later
    due to the scheduler action
    
    [1] #5310
    
    see odoo/odoo#137864
    see odoo/enterprise#48709
    
    closes odoo/upgrade#5366
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/stock/saas~16.5.1.1/pre-migrate.py
+++ migrations/stock/saas~16.5.1.1/pre-migrate.py
@@ -17,29 +17,135 @@ def migrate(cr, version):
     util.remove_model(cr, "stock.immediate.transfer")
     util.remove_model(cr, "stock.immediate.transfer.line")
     util.create_column(cr, "stock_move", "picked", "boolean")
+    util.create_column(cr, "stock_move_line", "picked", "boolean")
+    util.create_column(cr, "stock_move_line", "quantity_product_uom", "numeric")
 
-    # TODO: We miss the migration part for the real data
-    # the block of code bellow just allow to pass the repair test
-    # but will fail on real data. We will lose the quantity picked
-    # if it's different than the reservation.
-    # We will finish the script as soon as possible and we ensured it's
-    # possible to migrate the data.
-
+    query = """
+        UPDATE stock_move_line move_line
+            SET quantity_product_uom = move_line.qty_done / ml_uom.factor * p_uom.factor
+            FROM uom_uom as ml_uom,
+                 product_product AS product
+            JOIN product_template AS template
+              ON product.product_tmpl_id = template.id
+            JOIN uom_uom AS p_uom
+              ON template.uom_id = p_uom.id
+           WHERE product.id = move_line.product_id
+             AND move_line.product_uom_id = ml_uom.id
+             AND {parallel_filter}
+    """
+    util.explode_execute(cr, query, table="stock_move_line", alias="move_line")
     # Mark as picked all move that should not be unreserved/rereserved in the end-* script
+    query = """
+        UPDATE stock_move_line move_line
+            SET picked = 't'
+          WHERE (move_line.qty_done > 0
+             OR  state in ('done', 'cancel'))
+            AND {parallel_filter};
+    """
+    util.explode_execute(cr, query, table="stock_move_line", alias="move_line")
+    query = """
+        WITH picked_moves AS (
+            SELECT move.id
+              FROM stock_move move
+              JOIN stock_move_line line
+                ON line.move_id = move.id
+             WHERE line.picked
+               AND {parallel_filter}
+          GROUP BY move.id
+        )
+        UPDATE stock_move as move
+           SET picked = true
+          FROM picked_moves
+         WHERE move.id = picked_moves.id
+    """
+    util.explode_execute(cr, query, table="stock_move", alias="move")
+
+    # remove reservation
+    cr.execute("DELETE FROM stock_move_line WHERE picked = false")
+
+    util.rename_field(cr, "stock.move", "quantity_done", "quantity")
+    util.rename_field(cr, "stock.move.line", "qty_done", "quantity")
+
+    query = """
+        UPDATE stock_move
+                SET state =
+                    CASE
+                    WHEN quantity = 0 THEN 'confirmed'
+                    ELSE 'partially_available'
+                    END
+                WHERE quantity < product_uom_qty
+                AND state = 'assigned'
+                AND {parallel_filter};
+    """
+    util.explode_execute(cr, query, table="stock_move")
+
+    util.explode_execute(cr, "UPDATE stock_quant SET reserved_quantity = 0", table="stock_quant")
+
+    # Update the table stock quant base on the sum of quantity
+    # of stock move line table group by product_id, location_id, lot_id, package_id, owner_id
     cr.execute(
-        """ UPDATE stock_move as move
-                   SET picked = 't'
-                   FROM (
-                       SELECT
-                           move_id,
-                           sum(reserved_uom_qty) as reserved,
-                           sum(qty_done) as done
-                       FROM stock_move_line
-                       GROUP BY move_id
-                       ) as sub_sml
-                   WHERE move.id = sub_sml.move_id
-                       AND (move.state = 'done' OR sub_sml.reserved != sub_sml.done)
-               """
+        """
+        WITH move_line_reserved AS (
+          SELECT sml.product_id,
+                 sml.location_id,
+                 sml.lot_id,
+                 sml.package_id,
+                 sml.owner_id,
+                 sml.company_id,
+                 sum(sml.quantity_product_uom) as quantity
+            FROM stock_move_line sml
+            JOIN stock_location location
+              ON location.id = sml.location_id
+            JOIN product_product product
+              ON product.id = sml.product_id
+            JOIN product_template template
+              ON template.id = product.product_tmpl_id
+           WHERE sml.state NOT IN ('done', 'cancel')
+         AND NOT (
+                   location.usage IN ('supplier', 'customer', 'inventory', 'production') OR
+                   location.scrap_location = 't' OR
+                   (location.usage = 'transit' AND location.company_id IS NULL)
+                )
+             AND template.type = 'product'
+        GROUP BY sml.product_id, sml.location_id, sml.lot_id, sml.package_id, sml.owner_id, sml.company_id
+        )
+        INSERT INTO stock_quant (
+               product_id,
+               location_id,
+               lot_id,
+               package_id,
+               owner_id,
+               company_id,
+               reserved_quantity,
+               in_date
+        )
+        SELECT move_line.product_id,
+               move_line.location_id,
+               move_line.lot_id,
+               move_line.package_id,
+               move_line.owner_id,
+               move_line.company_id,
+               move_line.quantity,
+               now()
+          FROM move_line_reserved move_line;
+    """
+    )
+
+    # warn admins
+    util.add_to_migration_reports(
+        """
+<details>
+<summary>
+    Transfers and production orders has been unreserved during migration.
+    The quantities registered before the migration are now in the quantity
+    column and mark as picked.
+    You can use the run scheduler action in debug mode to re-reserve other
+    operations.
+</summary>
+</details>
+            """,
+        category="Inventory",
+        format="html",
     )
 
     util.remove_field(cr, "stock.move", "reserved_availability")
