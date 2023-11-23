027bf7f6b38328fd10cb1e386a4de09b59062f4d
Adrien Guilliams (adgu) <adgu@odoo.com>
2023-11-23 11:35:31 +0100

    [IMP] point_of_sale/saas~17.1: move xmlid from pos to pos_discount
    This commit moves the xmlid of the discount product from point_of_sale
    to pos_discount. The community PR has the same purpose.
    
    Community PR: odoo/odoo#143305
    
    closes odoo/upgrade#5382
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/point_of_sale/saas~17.1.1.0.1/pre-migrate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    if util.module_installed(cr, "pos_discount"):
+        util.rename_xmlid(cr, "point_of_sale.product_product_consumable", "pos_discount.product_product_consumable")
+    else:
+        util.delete_unused(cr, "point_of_sale.product_product_consumable")
