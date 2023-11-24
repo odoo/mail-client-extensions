316c7669428ee0607ceb4a4cfa53586cebc1fce7
Christophe Simonis <chs@odoo.com>
2023-11-24 12:12:53 +0100

    [FIX] l10n_be_hr_payroll/saas~16.5
    - use `util.table_of_model`
    - fully-qualify the columns in the SQL query
    
    closes odoo/upgrade#5383
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/l10n_be_hr_payroll/saas~16.5.1.0/post-migrate.py
+++ migrations/l10n_be_hr_payroll/saas~16.5.1.0/post-migrate.py
@@ -5,14 +5,10 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     pdf_to_post = ", pdf_to_post" if util.modules_installed(cr, "documents_l10n_be_hr_payroll") else ""
-    table_to_model = {
-        "l10n_be_individual_account": "l10n_be.individual.account",
-        "l10n_be_281_10": "l10n_be.281_10",
-        "l10n_be_281_45": "l10n_be.281_45",
-    }
-    for declaration_model in ["l10n_be_individual_account", "l10n_be_281_10", "l10n_be_281_45"]:
-        declaration_line_model = f"{declaration_model}_line"
-        res_model = table_to_model[declaration_model]
+
+    for declaration_model in ["l10n_be.individual.account", "l10n_be.281_10", "l10n_be.281_45"]:
+        declaration_table = util.table_of_model(cr, declaration_model)
+        declaration_line_table = f"{declaration_table}_line"
         cr.execute(
             f"""
             INSERT INTO hr_payroll_employee_declaration (
@@ -30,23 +26,23 @@ def migrate(cr, version):
                 {pdf_to_post}
             )
             SELECT
-                dlm.create_uid,
-                dlm.create_date,
-                dlm.write_uid,
-                dlm.write_date,
-                sheet_id,
-                employee_id,
-                dm.company_id,
+                l.create_uid,
+                l.create_date,
+                l.write_uid,
+                l.write_date,
+                l.sheet_id,
+                l.employee_id,
+                d.company_id,
                 %s,
-                pdf_filename,
-                pdf_to_generate,
-                pdf_file
+                l.pdf_filename,
+                l.pdf_to_generate,
+                l.pdf_file
                 {pdf_to_post}
-            FROM {declaration_line_model} dlm
-            JOIN {declaration_model} dm
-            ON dlm.sheet_id = dm.id
+            FROM {declaration_line_table} l
+            JOIN {declaration_table} d
+              ON l.sheet_id = d.id
             """,
-            [res_model],
+            [declaration_model],
         )
 
     cr.execute("DROP TABLE l10n_be_individual_account_line")
