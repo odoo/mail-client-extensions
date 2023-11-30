99941a021a7897a78cfa5eaa97969c4a5fd8817e
svs-odoo <svs@odoo.com>
2023-11-30 14:36:07 +0100

    [FIX] base/saas~17.1: subcontracting typo
    Renames a module to fix a typo.
    
    Fixes #141746
    Community PR: odoo/odoo#144387
    
    closes odoo/upgrade#5399
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/base/saas~17.1.1.3/pre-10-modules.py
+++ migrations/base/saas~17.1.1.3/pre-10-modules.py
@@ -3,5 +3,7 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.rename_module(cr, "mrp_subonctracting_landed_costs", "mrp_subcontracting_landed_costs")
+
     if util.has_enterprise():
         util.rename_module(cr, "account_bacs", "l10n_uk_bacs")
