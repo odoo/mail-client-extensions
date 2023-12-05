9b516e940486e8b5ce5fc6d0d6052447585a106c
Rémy Voet (ryv) <ryv@odoo.com>
2023-12-05 15:05:09 +0100

    [IMP] */saas~17.1: remove analytic_distribution_search
    https://github.com/odoo/odoo/pull/144747
    https://github.com/odoo/enterprise/pull/53380
    
    closes odoo/upgrade#5469
    
    Related: odoo/documentation#7194
    Related: odoo/odoo#144747
    Related: odoo/enterprise#53380
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account/saas~17.1.1.2/pre-migrate.py
+++ migrations/account/saas~17.1.1.2/pre-migrate.py
@@ -7,3 +7,4 @@ def migrate(cr, version):
     util.remove_field(cr, "account.journal", "sale_activity_type_id")
     util.remove_field(cr, "res.config.settings", "module_account_taxcloud")
     util.remove_field(cr, "res.config.settings", "module_account_avatax")
+    util.remove_field(cr, "analytic.mixin", "analytic_distribution_search")
