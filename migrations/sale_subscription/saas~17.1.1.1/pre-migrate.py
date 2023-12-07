5c0caa7a9817c5e0e93ce9a52c2dca54a908bf3e
AMZIL Ayoub <amay@odoo.com>
2023-12-07 13:39:37 +0100

    [FIX] sale_subscription:/saas~17.1: fix the corrupted recurring plans
    In versions before 17, a recurring plan with 0 months didn't make sense,
    so we added a Python constraint to avoid disrupting
    existing client subscriptions.
    starting from 17.1, we've enhanced this by including
    an SQL constraint in master,
    this PR is intended to update the wrong values.
    
    enterprise PR: odoo/enterprise#52329
    
    closes odoo/upgrade#5421
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/sale_subscription/saas~17.1.1.1/pre-migrate.py
+++ migrations/sale_subscription/saas~17.1.1.1/pre-migrate.py
@@ -15,3 +15,34 @@ def migrate(cr, version):
            AND pricelist.active IS NOT True
     """
     )
+
+    # Having a recurring bill of 0 doesn't make sense
+    cr.execute(
+        """
+           UPDATE sale_subscription_plan
+              SET billing_period_value = 1
+            WHERE billing_period_value <= 0
+        RETURNING id, name->>'en_US'
+        """
+    )
+
+    if cr.rowcount > 0:
+        subs_recurring_period = "\n".join(
+            "<li>{}</li>".format(util.get_anchor_link_to_record("sale.subscription.plan", _id, name))
+            for _id, name in cr.fetchall()
+        )
+        util.add_to_migration_reports(
+            message=f"""
+            <details>
+              <summary>
+                The billing period of Sale Subscription plans must be a positive quantity.
+                We have reset the following recurring plans to have a billing period of 1.
+              </summary>
+              <ul>
+                {subs_recurring_period}
+              </ul>
+            </details>
+            """,
+            format="html",
+            category="Subscription",
+        )
