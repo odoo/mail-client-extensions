9645b88b1bfb5f160059188e264ccfa6198a8a46
Hardik Prajapati <hpr@odoo.com>
2023-12-08 11:42:27 +0000

    [FIX] purchase/17.0.1.2: fix memory error
    - when database is upgrading with purchase module from
    lower then 17.0 to 17.0, memory error is raising because
    of new introduced compute stored field with 'purchase.order.line' named
    'discount'.
    - [PR](https://github.com/odoo/odoo/pull/126804/files)
    file: addons/purchase/models/purchase.py
    
    - Traceback:
    ```py
    2023-12-07 11:47:04,489 28 [1;32m[1;49mINFO[0m 1181112_org_17.0 odoo.modules.registry: module purchase: creating or updating database tables
    2023-12-07 11:47:04,555 28 [1;32m[1;49mINFO[0m 1181112_org_17.0 odoo.models: Prepare computation of purchase.order.line.discount
    2023-12-07 12:39:38,647 28 [1;33m[1;49mWARNING[0m 1181112_org_17.0 odoo.modules.loading: Transient module states were reset
    2023-12-07 12:39:38,648 28 [1;31m[1;49mERROR[0m 1181112_org_17.0 odoo.modules.registry: Failed to load registry
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/17.0/odoo/fields.py", line 1388, in compute_value
        records._compute_field_value(self)
      File "/home/odoo/src/odoo/17.0/odoo/models.py", line 4858, in _compute_field_value
        fields.determine(field.compute, self)
      File "/home/odoo/src/odoo/17.0/odoo/fields.py", line 101, in determine
        return needle(*args)
      File "/home/odoo/src/odoo/17.0/addons/purchase/models/purchase_order_line.py", line 369, in _compute_price_unit_and_date_planned_and_name
        default_names.append(line._get_product_purchase_description(line.product_id.with_context(product_ctx)))
      File "/home/odoo/src/odoo/17.0/addons/purchase/models/purchase_order_line.py", line 541, in _get_product_purchase_description
        name = product_lang.display_name
      File "/home/odoo/src/odoo/17.0/odoo/fields.py", line 1204, in __get__
        recs = record if self.recursive else record._in_cache_without(self)
      File "/home/odoo/src/odoo/17.0/odoo/models.py", line 6610, in _in_cache_without
        ids = self.env.cache.get_missing_ids(self.browse(ids), field)
      File "/home/odoo/src/odoo/17.0/odoo/models.py", line 5801, in browse
        ids = tuple(ids)
      File "/home/odoo/src/odoo/17.0/odoo/models.py", line 330, in expand_ids
        seen.add(id_)
    MemoryError
    ```
    
    - upg-1181112
    
    closes odoo/upgrade#5426
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/purchase/17.0.1.2/pre-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # https://github.com/odoo/odoo/pull/126804
+    util.create_column(cr, "purchase_order_line", "discount", "float8")
