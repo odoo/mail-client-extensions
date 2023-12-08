9ecfa546e02fa44b0aa4b02e4da5f6d64166493d
Christophe Simonis <chs@odoo.com>
2023-12-08 13:16:12 +0100

    [FIX] project/tests: skip test if `todo` is not installed
    The verification step is actually done in the new `project_todo` module,
    which is not installed if the `todo` module is not installed.
    
    closes odoo/upgrade#5424
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/project/tests/test_migrate_note_in_todo.py
+++ migrations/project/tests/test_migrate_note_in_todo.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from odoo.addons.base.maintenance.migrations import util
 from odoo.addons.base.maintenance.migrations.testing import UpgradeCase, change_version
 
 
 @change_version("saas~16.3")
 class TestMigrateNoteInTodo(UpgradeCase):
     def prepare(self):
+        if not util.module_installed(self.env.cr, "todo"):
+            self.skipTest("`todo` module not installed")
         personal_stages = self.env["project.task.type"].search([("user_id", "=", self.env.uid)])
         project_tag = self.env["project.tags"].create({"name": "UPGRADE_test_migrate_note_in_todo_test"})
         task = self.env["project.task"].create({"name": "Private Task", "personal_stage_id": personal_stages[-1].id})
