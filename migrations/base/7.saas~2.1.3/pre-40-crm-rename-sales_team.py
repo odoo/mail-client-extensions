ff1511d537f8ff2d824c03f1f9663e1f0d9403f3
Christophe Simonis <chs@odoo.com>
2023-12-12 16:16:19 +0100

    [chore] base/7.saas~2: format file
    Part-of: odoo/upgrade#5433

--- migrations/base/7.saas~2.1.3/pre-40-crm-rename-sales_team.py
+++ migrations/base/7.saas~2.1.3/pre-40-crm-rename-sales_team.py
@@ -5,16 +5,19 @@ from openerp.addons.base.maintenance.migrations import util
 #       and sales_team (but sales_team is a new module so it doesn't run the
 #       migration scripts)
 
+
 def migrate(cr, version):
     # NOTE: these data are conflicting because they are both using the
     #       code "DM" but crm.crm_case_section_3 exists only if the demo data
     #       are installed.
-    if util.ref(cr, 'crm.crm_case_section_3'):
+    if util.ref(cr, "crm.crm_case_section_3"):
         # remove duplicate record related sales_team
-        cr.execute("""DELETE FROM ir_model_data
+        cr.execute(
+            """DELETE FROM ir_model_data
                                 WHERE  model = 'crm.case.section'
                                   AND name = 'section_sales_department'
-                   """)
+                   """
+        )
 
         # rename xmlid of record related to sales_team
-        util.rename_xmlid(cr, 'crm.crm_case_section_3', 'crm.section_sales_department')
+        util.rename_xmlid(cr, "crm.crm_case_section_3", "crm.section_sales_department")
