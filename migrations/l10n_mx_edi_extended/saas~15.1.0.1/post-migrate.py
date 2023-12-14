7286c69e28242aaaadfc2cd63d51bdee46df0c28
Alvaro Fuentes <afu@odoo.com>
2023-12-14 15:59:31 +0000

    [FIX] l10n_mx_edi_extended/saas~15.1: skip post hook
    Since fecd546 we call post_init_hook to ensure some data is created
    correctly. Now this causes issues if the data is already there.
    
    Steps to reproduce in standard:
    * Install l10n_mx_edi_extended in 15.0
    * Uninstall l10n_mx_edi. Notice that res.city records remain since they
      are craeted with an xmlid in l10n_mx_edi by the post_init_hook from
      version 15.0 [1]
    * Try to upgrade to 16.0
    
    We get an error:
    ```
    Traceback (most recent call last):
    ...
      File "/tmp/tmpefkls54e/migrations/l10n_mx_edi_extended/saas~15.1.0.1/post-migrate.py", line 8, in migrate
        post_init_hook(cr, util.env(cr))
      File "/home/odoo/src/enterprise/16.0/l10n_mx_edi_extended/hooks.py", line 31, in post_init_hook
        cr.execute('''
      File "/home/odoo/src/odoo/16.0/odoo/sql_db.py", line 321, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.UniqueViolation: duplicate key value violates unique constraint "ir_model_data_module_name_uniq_index"
    DETAIL:  Key (module, name)=(l10n_mx_edi_extended, res_city_mx_agu_001) already exists.
    ```
    
    The reason for the issue is that we try to create entries in
    ir_model_data that already are associeted with l10n_mx_edi_extended
    since
    https://github.com/odoo/upgrade/blob/75af882/migrations/l10n_mx_edi_extended/saas~15.1.0.1/pre-move-xmlids.py#L3-L10
    
    From upg-1183726:
    ```
    1183726_afu=> select module,model,count(*) from ir_model_data where name like 'res_city_mx%' group by module,model
    +-------------+----------+-------+
    | module      | model    | count |
    |-------------+----------+-------|
    | l10n_mx_edi | res.city | 2447  |
    +-------------+----------+-------+
    1183726_afu=> select id,name,latest_version,state,author from ir_module_module where name like 'l10n_mx%' order by latest_version,name
    +-----+-------------------------------+----------------+-------------+-----------+
    | id  | name                          | latest_version | state       | author    |
    |-----+-------------------------------+----------------+-------------+-----------|
    | 363 | l10n_mx_reports_closing       | 15.0.0.1       | installed   | Vauxoo    |
    | 542 | l10n_mx_xml_polizas           | 15.0.0.1       | installed   | Odoo      |
    | 680 | l10n_mx_edi                   | 15.0.0.3       | installed   | Odoo S.A. |
    | 54  | l10n_mx_reports               | 15.0.1.0       | installed   | Vauxoo    |
    | 811 | l10n_mx_edi_stock             | 15.0.1.0.      | installed   | Odoo S.A. |
    | 359 | l10n_mx                       | 15.0.2.0       | installed   | Vauxoo    |
    | 844 | l10n_mx_edi_40                | <null>         | uninstalled | Odoo S.A. |
    | 745 | l10n_mx_edi_extended          | <null>         | uninstalled | Odoo S.A. |
    | 847 | l10n_mx_edi_extended_40       | <null>         | uninstalled | Odoo S.A. |
    | 767 | l10n_mx_edi_landing           | <null>         | uninstalled | Odoo S.A. |
    | 848 | l10n_mx_edi_stock_40          | <null>         | uninstalled | Odoo S.A. |
    | 810 | l10n_mx_edi_stock_extended    | <null>         | uninstalled | Odoo S.A. |
    | 842 | l10n_mx_edi_stock_extended_40 | <null>         | uninstalled | Odoo S.A. |
    | 865 | l10n_mx_xml_polizas_edi       | <null>         | uninstalled | Odoo      |
    +-----+-------------------------------+----------------+-------------+-----------+
    ```
    
    upg-1174104
    upg-1183726
    
    [1]: https://github.com/odoo/enterprise/blob/00a7b9d/l10n_mx_edi_extended/hooks.py#L36
    
    closes odoo/upgrade#5448
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/l10n_mx_edi_extended/saas~15.1.0.1/post-migrate.py
+++ migrations/l10n_mx_edi_extended/saas~15.1.0.1/post-migrate.py
@@ -5,4 +5,14 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    post_init_hook(cr, util.env(cr))
+    cr.execute(
+        """
+        SELECT 1
+          FROM ir_model_data
+         WHERE module = 'l10n_mx_edi_extended'
+           AND model = 'res.city'
+         LIMIT 1
+        """
+    )
+    if not cr.rowcount:
+        post_init_hook(cr, util.env(cr))
