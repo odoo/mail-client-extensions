53add3f8b6aa24c347b5562f026aebc18b9bc55b
Christophe Simonis <chs@odoo.com>
2023-12-18 11:51:54 +0100

    [FIX] project/tests: use correct module name
    Confusion bewteen the old and new names: `note` and `project_todo`.
    
    Oversight of 9ecfa546e02fa44b0aa4b02e4da5f6d64166493d
    
    closes odoo/upgrade#5453
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/project/tests/test_migrate_note_in_todo.py
+++ migrations/project/tests/test_migrate_note_in_todo.py
@@ -7,8 +7,8 @@ from odoo.addons.base.maintenance.migrations.testing import UpgradeCase, change_
 @change_version("saas~16.3")
 class TestMigrateNoteInTodo(UpgradeCase):
     def prepare(self):
-        if not util.module_installed(self.env.cr, "todo"):
-            self.skipTest("`todo` module not installed")
+        if not util.module_installed(self.env.cr, "note"):
+            self.skipTest("`note` module not installed")
         personal_stages = self.env["project.task.type"].search([("user_id", "=", self.env.uid)])
         project_tag = self.env["project.tags"].create({"name": "UPGRADE_test_migrate_note_in_todo_test"})
         task = self.env["project.task"].create({"name": "Private Task", "personal_stage_id": personal_stages[-1].id})
