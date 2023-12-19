5b3969643e912134fd29f8b318c180da159d5495
Christophe Simonis <chs@odoo.com>
2023-12-19 15:56:40 +0100

    [FIX] website_forum/saas~16.5: reload noupdate views
    Needed due to the new view schema (attrs).

--- migrations/website_forum/saas~16.5.1.2/pre-migrate.py
+++ migrations/website_forum/saas~16.5.1.2/pre-migrate.py
@@ -3,3 +3,7 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "website_forum.private_profile")
+
+    # explictly reload noupdate views
+    util.update_record_from_xml(cr, "website_forum.forum_post_view_form")
+    util.update_record_from_xml(cr, "website_forum.forum_post_view_tree")
