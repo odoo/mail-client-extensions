60fcf90869f3416a7413587a859aa59cfb0339d7
matu-odoo <matu@odoo.com>
2023-12-20 07:28:51 +0000

    [FIX] sale/16.0: some changes in mail template
    Moving the fix to the end, as in the post, will only replace
    in "en_US" and its translations will remain the same.
    But if this is moved to the end, then it will be replaced
    in translations as well.
    
    upg-1161242, 1184178, 1187178, 1142163
    
    closes odoo/upgrade#5456
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/sale/16.0.1.2/end-migrate.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    if util.is_changed(cr, "sale.mail_template_sale_confirmation"):
+        tid = util.ref(cr, "sale.mail_template_sale_confirmation")
+        changes = [
+            ("acquirer_id", "provider_id"),
+            ("tx.provider", "tx.provider_code"),
+            ("token_id.name", "token_id.display_name"),
+        ]
+        for old, new in changes:
+            util.replace_in_all_jsonb_values(
+                cr, "mail_template", "body_html", old, new, extra_filter=cr.mogrify("t.id = %s", [tid]).decode()
+            )
+    else:
+        util.update_record_from_xml(cr, "sale.mail_template_sale_confirmation", ensure_references=True)
--- migrations/sale/16.0.1.2/post-migrate.py
+++ migrations/sale/16.0.1.2/post-migrate.py
@@ -3,16 +3,6 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    changes = [
-        ("acquirer_id", "provider_id"),
-        ("tx.provider", "tx.provider_code"),
-        ("token_id.name", "token_id.display_name"),
-    ]
-    for old, new in changes:
-        util.replace_in_all_jsonb_values(
-            cr, "mail_template", "body_html", old, new, extra_filter="t.model='sale.order'"
-        )
-
     cr.execute(
         r"""
         SELECT id
