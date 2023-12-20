92f0511d48bc7e1bfdbd2dc4b5e3f3d3e3e859c2
nseinlet <nse@odoo.com>
2023-12-20 08:46:45 +0100

    [IMP] project/saas~16.5
    31k non-null values VS 1.5 million null values

--- migrations/project/saas~16.5.1.3/pre-migrate.py
+++ migrations/project/saas~16.5.1.3/pre-migrate.py
@@ -8,5 +8,5 @@ def migrate(cr, version):
     util.remove_field(cr, "project.project", "alias_value")
     if not util.module_installed(cr, "industry_fsm"):
         cr.execute("DROP VIEW IF EXISTS report_project_task_user CASCADE")
-        util.alter_column_type(cr, "project_task", "date_deadline", "timestamp without time zone")
+        util.alter_column_type(cr, "project_task", "date_deadline", "timestamp without time zone", using="{0}")
     util.remove_field(cr, "res.company", "analytic_plan_id")
