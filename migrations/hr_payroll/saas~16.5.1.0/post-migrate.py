02e0b62678322692e4a0b7fee5aa8d47557de517
Xavier ALT <xal@odoo.com>
2023-12-21 10:54:34 +0100

    [IMP] hr_payroll/saas~16.5: make salary rules update more generic
    Followed 2362c2f40, make the hr.salary.rule python expression update
    more generic - also considering `worked_days` var. and standard rule
    from `hr_payroll` and `l10n_be_hr_payroll`.

--- migrations/hr_payroll/saas~16.5.1.0/post-migrate.py
+++ migrations/hr_payroll/saas~16.5.1.0/post-migrate.py
@@ -20,16 +20,11 @@ def migrate(cr, version):
     util.update_record_from_xml(cr, "hr_payroll.default_reimbursement_salary_rule")
     util.update_record_from_xml(cr, "hr_payroll.default_net_salary")
 
-    # Replace master rules/categories/inputs that are coming from default values
-    updates = {f"categories.{code}": f'categories["{code}"]' for code in ["BASIC", "ALW", "DED", "NET"]}
-    updates.update(
-        (f"inputs.{code}", f'inputs.get("{code}")')
-        for code in ["ATTACH_SALARY", "ASSIG_SALARY", "CHILD_SUPPORT", "REIMBURSEMENT", "DEDUCTION"]
-    )
-    updates["rules.NET"] = "rules.get('NET')"
-
+    # Replace master rules/categories/inputs /worked_days that are coming from default values
+    # ex: inputs.VAR -> inputs.get("VAR")
+    localdict_vars = ["rules", "categories", "inputs", "worked_days"]
     fields = {
-        field: util.pg_replace(field, updates.items())
+        field: rf"""regexp_replace({field}, '({'|'.join(localdict_vars)}).([A-Z0-9_]+)', E'\\1.get("\\2")', 'g')"""
         for field in [
             "amount_python_compute",
             "quantity",
