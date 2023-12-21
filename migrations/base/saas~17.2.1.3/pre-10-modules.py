6d5822da7ec08b5c638a7f489fffca93658a9e05
Joren Van Onder <jov@odoo.com>
2023-12-20 16:28:49 -0800

    [IMP] base/saas~17.2: remove all Taxcloud modules
    This removes all Taxcloud modules in master, following the deprecation
    in Odoo 17 [1].
    
    Taxcloud fiscal positions are archived and kept. A notice is shown to
    inform users and direct them to our documentation.
    
    task-3516826
    
    Enterprise: https://github.com/odoo/enterprise/pull/53768
    
    [1] https://github.com/odoo/upgrade/pull/5199
    
    closes odoo/upgrade#5501
    
    Related: odoo/enterprise#53768
    Signed-off-by: William André (wan) <wan@odoo.com>

---
+++ migrations/base/saas~17.2.1.3/pre-10-modules.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    if util.module_installed(cr, "account_taxcloud"):
+        # Archive taxcloud fiscal positions because they will no longer work.
+        cr.execute("UPDATE account_fiscal_position SET active = false WHERE is_taxcloud")
+
+        # Don't delete the included fiscal position (could be used).
+        cr.execute(
+            """
+            DELETE FROM ir_model_data
+                  WHERE module = 'account_taxcloud'
+                    AND name = 'account_fiscal_position_taxcloud_us'
+            """
+        )
+
+        util.add_to_migration_reports(
+            """
+                <p><strong>IMPORTANT NOTICE</strong></p>
+                <p>
+                    Taxcloud is no longer supported in this version of Odoo and has been uninstalled. This doesn't
+                    affect existing, captured Taxcloud transactions that may exist in your database.
+
+                    See <a href="https://www.odoo.com/documentation/17.0/applications/finance/accounting/taxes/taxcloud.html">our documentation</a>
+                    for more info and recommendations.
+                </p>
+            """,
+            category="Accounting",
+            format="html",
+        )
+
+    taxcloud_modules = (
+        "account_taxcloud",
+        "sale_account_taxcloud",
+        "sale_amazon_taxcloud",
+        "sale_loyalty_taxcloud",
+        "sale_loyalty_taxcloud_delivery",
+        "sale_subscription_taxcloud",
+        "website_sale_account_taxcloud",
+    )
+    for module in taxcloud_modules:
+        util.remove_module(cr, module)
