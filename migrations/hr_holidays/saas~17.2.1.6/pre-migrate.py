48ddc4cd9022467014c90eb8f730e798fd8913d6
Rob Coekaerts <roco@odoo.com>
2024-01-03 08:53:47 +0100

    [IMP] hr_holidays/saas~17.2: compute newly stored field
    This commit computes the newly stored field `number_of_hours_display`
    for hr.leave.allocation so that it can be used in reporting.
    
    See odoo/odoo#120665
    
    closes odoo/upgrade#5485
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr_holidays/saas~17.2.1.6/pre-migrate.py
+++ migrations/hr_holidays/saas~17.2.1.6/pre-migrate.py
@@ -1,7 +1,44 @@
 # -*- coding: utf-8 -*-
+import itertools
+
+from odoo.addons.resource.models.utils import HOURS_PER_DAY
 
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.remove_field(cr, "hr.leave", "report_note")
+
+    util.create_column(cr, "hr_leave_allocation", "number_of_hours_display", "float8")
+    queries = [
+        cr.mogrify(query, [HOURS_PER_DAY]).decode()
+        for query in [
+            """
+            UPDATE hr_leave_allocation a
+               SET number_of_hours_display = a.number_of_days * COALESCE(rc.hours_per_day, %s)
+              FROM hr_employee e
+         LEFT JOIN resource_calendar rc
+                ON e.resource_calendar_id = rc.id
+             WHERE a.holiday_type = 'employee' AND a.employee_id = e.id
+            """,
+            """
+            UPDATE hr_leave_allocation a
+               SET number_of_hours_display = a.number_of_days * COALESCE(rc.hours_per_day, %s)
+              FROM hr_leave_type lt
+         LEFT JOIN res_company c
+                ON c.id = lt.company_id
+         LEFT JOIN resource_calendar rc
+                ON c.resource_calendar_id = rc.id
+             WHERE a.holiday_type != 'employee' AND a.holiday_status_id = lt.id
+            """,
+        ]
+    ]
+
+    util.parallel_execute(
+        cr,
+        list(
+            itertools.chain.from_iterable(
+                util.explode_query_range(cr, query, "hr_leave_allocation", alias="a") for query in queries
+            )
+        ),
+    )
