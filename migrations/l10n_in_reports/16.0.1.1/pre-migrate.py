4d8e9040ed41af9297b276d755decfb38e1c960b
pja-odoo <pja@odoo.com>
2024-01-03 06:43:39 +0000

    [FIX] l10n_in_reports/16.0: avoid memory error
    We get a memory error when the module 'l10n_in_reports'
    is updating, in version 16.0
    new compute fields 'l10n_in_transaction_type' is added
    [Ref task](https://github.com/odoo/enterprise/commit/f435654f70f3d6a1fec7fa696945f2c0eb9a7056)
    It's store compute field, when the field value compute via
    [method](https://github.com/odoo/enterprise/blob/16.0/l10n_in_reports/models/account_move.py#L21)
    it's causing the memory error.
    So compute that field value via SQL to avoid the memory error
    
    This patch avoid the error:
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/8.0/openerp/service/server.py", line 941, in preload_registries
        registry = RegistryManager.new(dbname, update_module=update_module)
      File "/home/odoo/src/odoo/8.0/openerp/modules/registry.py", line 370, in new
        openerp.modules.load_modules(registry._db, force_demo, status, update_module)
      File "/home/odoo/src/odoo/8.0/openerp/modules/loading.py", line 351, in load_modules
        force, status, report, loaded_modules, update_module)
      File "/home/odoo/src/odoo/8.0/openerp/modules/loading.py", line 255, in load_marked_modules
        loaded, processed = load_module_graph(cr, graph, progressdict, report=report, skip_modules=loaded_modules, perform_checks=perform_checks)
      File "/home/odoo/src/odoo/8.0/openerp/modules/loading.py", line 142, in load_module_graph
        migrations.migrate_module(package, 'pre')
      File "/home/odoo/src/odoo/8.0/openerp/modules/migration.py", line 171, in migrate_module
        migrate(self.cr, pkg.installed_version)
      File "/home/odoo/src/odoo/8.0/openerp/addons/base/maintenance/migrations/procurement/7.saas~5.1.0/pre-30-procuremove.py", line 58, in migrate
        """
      File "/home/odoo/src/odoo/8.0/openerp/sql_db.py", line 171, in wrapper
        return f(self, *args, **kwargs)
      File "/home/odoo/src/odoo/8.0/openerp/sql_db.py", line 247, in execute
        res = self._obj.execute(query, params)
    ProgrammingError: constraint "sale_order_line_procurement_id_fkey" of relation "sale_order_line" does not exist
    ```
    upg - 1194974
    TGB - 337
    
    closes odoo/upgrade#5484
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/l10n_in_reports/16.0.1.1/pre-migrate.py
+++ migrations/l10n_in_reports/16.0.1.1/pre-migrate.py
@@ -4,3 +4,19 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_model(cr, "l10n.in.report.account")
+    util.create_column(cr, "account_move", "l10n_in_transaction_type", "character varying")
+    query = """
+        UPDATE account_move m
+           SET l10n_in_transaction_type = CASE
+                   WHEN m.l10n_in_state_id = partner.state_id THEN 'intra_state'
+                   ELSE 'inter_state'
+               END
+          FROM res_company company
+          JOIN res_country country
+            ON company.account_fiscal_country_id = country.id
+          JOIN res_partner partner
+            ON partner.id = company.partner_id
+         WHERE m.company_id = company.id
+           AND country.code = 'IN'
+    """
+    util.explode_execute(cr, query, table="account_move", alias="m")
