20d429dfa581ca5b63bb5070a606d19fc1a5d031
Maxime de Neuville <mane@odoo.com>
2024-01-05 10:32:50 +0100

    [IMP] sale_timesheet_enterprise/saas~17.1.1.0: remove unused gantt view
    See odoo/odoo#148176
    See odoo/enterprise#53697
    
    closes odoo/upgrade#5498
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/sale_timesheet_enterprise/saas~17.1.0.1/pre-migrate.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "sale_timesheet_enterprise.project_task_view_form_in_gantt_inherit")
