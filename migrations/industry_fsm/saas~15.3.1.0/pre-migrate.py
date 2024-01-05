a844854d3f726f32bb6da6e3693309a38641a2f5
Christophe Simonis <chs@odoo.com>
2024-01-05 18:12:12 +0100

    [FIX] industry_fsm/saas~15.3
    Remove computed field added in stable.
    
    See https://github.com/odoo/enterprise/pull/53522
    
    closes odoo/upgrade#5499
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/industry_fsm/saas~15.3.1.0/pre-migrate.py
+++ migrations/industry_fsm/saas~15.3.1.0/pre-migrate.py
@@ -5,3 +5,6 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_record(cr, "industry_fsm.project_task_action_planning_groupby_user_fsm_view_pivot")
     util.remove_record(cr, "industry_fsm.project_task_action_planning_groupby_user_fsm_view_graph")
+
+    # remove computed field added in stable: https://github.com/odoo/enterprise/pull/53522
+    util.remove_field(cr, "project.task", "planned_date_begin_formatted")
