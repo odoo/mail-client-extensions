301274bb449f06fe29b61ec0e53d0ae0f06611e7
Nicolas Seinlet <nse@odoo.com>
2024-01-05 11:38:38 +0100

    [IMP] account/saas~16.5: accelerate reconciliation query
    Use a temporary table.

--- migrations/account/saas~16.5.1.2/pre-migrate.py
+++ migrations/account/saas~16.5.1.2/pre-migrate.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+
 from odoo.upgrade import util
 
 
@@ -100,41 +102,98 @@ def migrate(cr, version):
         table="account_move_line",
     )
 
-    util.explode_execute(
-        cr,
+    if os.environ.get("MIGRATE_PARTIAL_RECONCILE_SQL", "0") == "1":
+        cr.execute("CREATE TABLE matching (min_id integer,line_id integer)")
+        cr.execute("CREATE INDEX matching_min_id_idx ON matching USING btree (min_id)")
+        cr.execute("CREATE INDEX matching_line_id_idx ON matching USING btree (line_id)")
+        cr.execute(
+            """
+            CREATE OR REPLACE FUNCTION build_matching_table(integer, integer, integer) RETURNS void AS $$
+                DECLARE
+                    partial_id ALIAS FOR $1;
+                    debit_line_id ALIAS FOR $2;
+                    credit_line_id ALIAS FOR $3;
+                    debit_min_id integer;
+                    credit_min_id integer;
+                BEGIN
+                    SELECT min_id INTO debit_min_id FROM matching WHERE line_id = debit_line_id;
+                    SELECT min_id INTO credit_min_id FROM matching WHERE line_id = credit_line_id;
+                    IF debit_min_id IS NOT NULL AND credit_min_id IS NOT NULL AND credit_min_id != debit_min_id THEN
+                        UPDATE matching
+                        SET min_id = LEAST(credit_min_id, debit_min_id)
+                        WHERE min_id IN (credit_min_id, debit_min_id)
+                        AND min_id != LEAST(credit_min_id, debit_min_id);
+                    ELSIF debit_min_id IS NOT NULL THEN
+                        INSERT INTO matching (min_id, line_id) VALUES (debit_min_id, credit_line_id);
+                    ELSIF credit_min_id IS NOT NULL THEN
+                        INSERT INTO matching (min_id, line_id) VALUES (credit_min_id, debit_line_id);
+                    ELSE
+                        INSERT INTO matching (min_id, line_id) VALUES (partial_id, debit_line_id), (partial_id, credit_line_id);
+                    END IF;
+                END;
+                $$ LANGUAGE plpgsql;
         """
-            WITH RECURSIVE partials (line_id, current_id) AS (
-                    SELECT line.id,
-                           line.id
-                      FROM account_move_line line
-                      JOIN account_partial_reconcile partial
-                        ON line.id = partial.debit_move_id
-                        OR line.id = partial.credit_move_id
-                     WHERE line.full_reconcile_id IS NULL
-                       AND {parallel_filter}
-                                                UNION
-                    SELECT p.line_id,
-                           CASE WHEN partial.debit_move_id = p.current_id THEN partial.credit_move_id
-                                ELSE partial.debit_move_id
-                           END
-                      FROM partials p
-                      JOIN account_partial_reconcile partial ON p.current_id = partial.debit_move_id
-                                                             OR p.current_id = partial.credit_move_id
-            ), new_vals AS (
-                  SELECT line_id, 'P' || MIN(partial.id) AS matching_number
-                    FROM partials
-                    JOIN account_partial_reconcile partial ON current_id = partial.debit_move_id
-                                                           OR current_id = partial.credit_move_id
-                GROUP BY line_id
+        )
+        queries = []
+        cr.execute("SELECT id from res_company order by id")
+        for company_id in cr.fetchall():
+            queries.append(
+                cr.mogrify(
+                    "SELECT build_matching_table(id, debit_move_id, credit_move_id) FROM account_partial_reconcile WHERE company_id=%s ORDER BY id",
+                    company_id,
+                ).decode()
             )
+        util.parallel_execute(cr, queries)
+        util.explode_execute(
+            cr,
+            """
             UPDATE account_move_line line
-               SET matching_number = new_vals.matching_number
-              FROM new_vals
-             WHERE new_vals.line_id = line.id
-        """,
-        table="account_move_line",
-        alias="line",
-    )
+               SET matching_number = 'P' || matching.min_id
+              FROM matching
+             WHERE matching.line_id = line.id
+               AND {parallel_filter}
+            """,
+            table="account_move_line",
+            alias="line",
+        )
+        cr.execute("DROP FUNCTION build_matching_table")
+        cr.execute("DROP TABLE matching")
+    else:
+        util.explode_execute(
+            cr,
+            """
+                WITH RECURSIVE partials (line_id, current_id) AS (
+                        SELECT line.id,
+                            line.id
+                        FROM account_move_line line
+                        JOIN account_partial_reconcile partial
+                            ON line.id = partial.debit_move_id
+                            OR line.id = partial.credit_move_id
+                        WHERE line.full_reconcile_id IS NULL
+                        AND {parallel_filter}
+                                                    UNION
+                        SELECT p.line_id,
+                            CASE WHEN partial.debit_move_id = p.current_id THEN partial.credit_move_id
+                                    ELSE partial.debit_move_id
+                            END
+                        FROM partials p
+                        JOIN account_partial_reconcile partial ON p.current_id = partial.debit_move_id
+                                                                OR p.current_id = partial.credit_move_id
+                ), new_vals AS (
+                    SELECT line_id, 'P' || MIN(partial.id) AS matching_number
+                        FROM partials
+                        JOIN account_partial_reconcile partial ON current_id = partial.debit_move_id
+                                                            OR current_id = partial.credit_move_id
+                    GROUP BY line_id
+                )
+                UPDATE account_move_line line
+                SET matching_number = new_vals.matching_number
+                FROM new_vals
+                WHERE new_vals.line_id = line.id
+            """,
+            table="account_move_line",
+            alias="line",
+        )
 
     # ANALYTIC
     # Merge lines that can be merged when all the plans are at 100% and at most one plan is split on multiple accounts
