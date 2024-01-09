2129cb2e25a18b94e171d37e8fb576f88d0d5db3
ABE-Mark45 <aels@odoo.com>
2024-01-09 14:47:35 +0200

    [IMP] mrp{_workorder}/saas~17.2: remove unused fields and views
    1. remove field `manual_consumption_readonly` from `mrp.bom.line` model
    2. remove field `use_auto_consume_components_lots` from `mrp.production`
    3. remove field `stock.picking.type` from `mrp.production`
    4. remove redundant `mrp_workorder.view_picking_type_form_inherit_mrp_workorder`
    5. remove field `prefill_lot_tablet` from `stock.picking.type` model
    
    community PR: https://github.com/odoo/odoo/pull/148026
    Enterprise PR: https://github.com/odoo/enterprise/pull/54326
    
    task-3573667
    
    closes odoo/upgrade#5547
    
    Related: odoo/odoo#148026
    Related: odoo/enterprise#54326
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/mrp/saas~17.2.2.0/pre-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "mrp.bom.line", "manual_consumption_readonly")
+    util.remove_field(cr, "mrp.production", "use_auto_consume_components_lots")
+    util.remove_field(cr, "stock.picking.type", "use_auto_consume_components_lots")
--- migrations/mrp_workorder/saas~17.2.1.0/pre-upgrade.py
+++ migrations/mrp_workorder/saas~17.2.1.0/pre-upgrade.py
@@ -3,3 +3,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "mrp_workorder.mrp_bom_form_view_inherit")
+    util.remove_field(cr, "stock.picking.type", "prefill_lot_tablet")
+    util.remove_view(cr, "mrp_workorder.view_picking_type_form_inherit_mrp_workorder")
