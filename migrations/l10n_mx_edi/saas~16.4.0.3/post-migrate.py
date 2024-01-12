a00b1a413f7c4aa62f3357acbcb751654664d56f
diagnoza <owi@odoo.com>
2024-01-12 18:01:56 +0000

    [FIX] l10n_mx_edi/saas~16.4.0.3: adapt mx edi cron for 16.4
    This commit aims to add a cron adapted for changes introduced in 16.4.
    In 16.4, all MX edi documents are migrated from `account.edi.document`
    to `l10n_mx_edi.document`:
    https://github.com/odoo/upgrade/blob/a1bf5aedaa4b267b4307f49255f17cc02f138bee/migrations/l10n_mx_edi/saas~16.4.0.3/post-migrate.py#L7
    
    This means that in the existing cron, we have to adapt one of the SELECT
    statements to account for any changes between the two models, such as
    different selection values for the `state` field or that we may no
    longer need to include edi_format_id in the WHERE clause (especially
    that the edi_format "l10n_mx_edi.edi_cfdi_3_3" used prior
    to 16.4 is removed when upgrading to 16.4).
    
    closes odoo/upgrade#5532
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/l10n_mx_edi/saas~16.4.0.3/post-migrate.py
+++ migrations/l10n_mx_edi/saas~16.4.0.3/post-migrate.py
@@ -75,3 +75,52 @@ def migrate(cr, _version):
     # Cleanup.
     cr.execute("DELETE FROM account_edi_document WHERE edi_format_id = %s", [cfdi_format_id])
     util.remove_record(cr, "l10n_mx_edi.edi_cfdi_3_3")
+
+    # Cron recomputing UUIDs
+    # See cron on l10n_mx_edi/16.0.0.3/post-migrate.py
+    UUID_RECOMPUTATION_CRON_CODE = """
+env.cr.execute('''
+    SELECT to_update.move_id
+    FROM
+    (
+        SELECT move.id AS move_id, move.date AS move_date
+        FROM l10n_mx_edi_document doc
+        JOIN account_move move ON move.id = doc.move_id
+        JOIN res_company ON res_company.id = move.company_id
+        JOIN res_country ON res_country.id = res_company.account_fiscal_country_id
+        WHERE doc.state IN ('invoice_sent', 'payment_sent')
+        AND move.l10n_mx_edi_cfdi_uuid IS NULL
+        AND res_country.code = 'MX'
+
+        UNION
+
+        SELECT move.id AS move_id, move.date AS move_date
+        FROM account_move move
+        JOIN ir_attachment ON move.id = ir_attachment.res_id AND ir_attachment.res_model = 'account.move'
+        JOIN res_company ON res_company.id = move.company_id
+        JOIN res_country ON res_country.id = res_company.account_fiscal_country_id
+        WHERE move.state = 'posted'
+        AND move.move_type IN ('in_invoice', 'in_refund')
+        AND move.l10n_mx_edi_cfdi_uuid IS NULL
+        AND ir_attachment.mimetype = 'application/xml'
+        AND res_country.code = 'MX'
+    ) to_update
+    ORDER BY to_update.move_date DESC
+    LIMIT 1000
+''')
+
+move_ids = [move_id for (move_id,) in env.cr.fetchall()]
+env['account.move'].browse(move_ids)._compute_l10n_mx_edi_cfdi_uuid()
+    """
+
+    cron = util.ref(cr, "__upgrade__.cron_post_upgrade_l10n_mx_edi_recompute_uuid")
+    if cron:
+        util.env(cr)["ir.cron"].browse([cron]).ir_actions_server_id.code = UUID_RECOMPUTATION_CRON_CODE
+    else:
+        util.create_cron(
+            cr,
+            "l10n_mx_edi: recompute UUID",
+            "account.move",
+            UUID_RECOMPUTATION_CRON_CODE,
+            interval=(20, "minutes"),
+        )
