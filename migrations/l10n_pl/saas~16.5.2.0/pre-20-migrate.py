5e9bf47a2e627dca742fa553579a7f0f628042e4
Christophe Simonis <chs@odoo.com>
2024-01-15 16:37:44 +0100

    [FIX] l10n_pl/saas~16.5: rename xmlids
    See odoo/odoo#146452
    
    closes odoo/upgrade#5538
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/l10n_pl/saas~16.5.2.0/pre-20-migrate.py
+++ migrations/l10n_pl/saas~16.5.2.0/pre-20-migrate.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from itertools import product
+
 from odoo.upgrade import util
 
 
@@ -22,3 +24,13 @@ def migrate(cr, version):
     util.remove_field(cr, "account.move", "l10n_pl_delivery_date")
     util.remove_field(cr, "account.move", "l10n_pl_show_delivery_date")
     util.remove_view(cr, "l10n_pl.report_invoice_document")
+
+    renames = """
+        kraj_{3_lub_,}5
+        kraj_{7_lub_,}8
+        kraj_{22_lub_,}23
+    """
+    for xid in util.splitlines(renames):
+        for prefix, suffix in product(("", "podatek_"), ("", "_tag")):
+            from_, to = util.expand_braces(f"l10n_pl.account_tax_report_line_{prefix}{xid}{suffix}")
+            util.rename_xmlid(cr, from_, to)
