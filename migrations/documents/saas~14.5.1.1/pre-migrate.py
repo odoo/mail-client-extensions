efdfb3568018a422613fbb88a40df139ae75ff64
Edoardo Piroli <pied@odoo.com>
2024-01-15 10:52:03 +0000

    [FIX] documents/saas~14.5: update rule in post
    Force update of record rules in `post-`, once the used fields are registred.
    
    See https://github.com/odoo/odoo/pull/149154

--- migrations/documents/saas~14.5.1.1/pre-migrate.py
+++ migrations/documents/saas~14.5.1.1/pre-migrate.py
@@ -5,5 +5,3 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "documents_folder", "user_specific_write", "bool")
-
-    util.update_record_from_xml(cr, "documents.documents_document_write_rule")
