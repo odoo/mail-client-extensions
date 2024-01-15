ed0d8fe5baa11d75424130ba60b55db5ca00d540
Christophe Simonis <chs@odoo.com>
2024-01-15 18:15:55 +0100

    [FIX] hr_{recruitment_survey,referral}/saas~16.4: update correct record
    Oversight of the original commit.

--- migrations/hr_referral/saas~16.4.1.0/pre-migrate.py
+++ migrations/hr_referral/saas~16.4.1.0/pre-migrate.py
@@ -4,4 +4,4 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.update_record_from_xml(cr, "hr_recruitment.group_hr_recruitment_user", from_module="hr_referral")
+    util.update_record_from_xml(cr, "hr_recruitment.group_hr_recruitment_interviewer", from_module="hr_referral")
