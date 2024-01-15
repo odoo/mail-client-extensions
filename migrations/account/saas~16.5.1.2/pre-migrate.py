0777f11865b8de6792a6c9d6bb89d9cfd11a63d7
william-andre <wan@odoo.com>
2024-01-15 12:02:00 +0100

    [FIX] account/saas~16.5: complexity of matching number computation
    The recursive query was done in O(n^2) because it was going through the
    whole graph of matching, starting from each node.
    This new approach is only going through each link once, by using a
    temporary table to remember each subgraph and be able to find them with
    an index, speeding up considerably databases containing a lot of complex
    reconciliation of a big size.
    
    For the upgrade of a database with
    * 13M account_move_line
    *  3M account_partial_reconcile
    * one complex matching of 17k lines and multiple others of ~1k lines
    
    The time of this part of the upgrade went from more than 38 minutes to
    4s.
    
    closes odoo/upgrade#5537
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account/saas~16.5.1.2/pre-migrate.py
+++ migrations/account/saas~16.5.1.2/pre-migrate.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import os
-
 from odoo.upgrade import util
 
 
@@ -64,99 +62,60 @@ def migrate(cr, version):
         table="account_move_line",
     )
 
-    if os.environ.get("MIGRATE_PARTIAL_RECONCILE_SQL", "0") == "1":
-        cr.execute("CREATE TABLE matching (min_id integer,line_id integer)")
-        cr.execute("CREATE INDEX matching_min_id_idx ON matching USING btree (min_id)")
-        cr.execute("CREATE INDEX matching_line_id_idx ON matching USING btree (line_id)")
-        cr.execute(
-            """
-            CREATE OR REPLACE FUNCTION build_matching_table(integer, integer, integer) RETURNS void AS $$
-                DECLARE
-                    partial_id ALIAS FOR $1;
-                    debit_line_id ALIAS FOR $2;
-                    credit_line_id ALIAS FOR $3;
-                    debit_min_id integer;
-                    credit_min_id integer;
-                BEGIN
-                    SELECT min_id INTO debit_min_id FROM matching WHERE line_id = debit_line_id;
-                    SELECT min_id INTO credit_min_id FROM matching WHERE line_id = credit_line_id;
-                    IF debit_min_id IS NOT NULL AND credit_min_id IS NOT NULL AND credit_min_id != debit_min_id THEN
+    cr.execute("""
+        /*
+        Create and join graphs for all partial reconciliations.
+        Each `account_partial_reconcile` is an edge between 2 `account_move_line` nodes.
+        `matching` represents the link between each node and it's graph number (matching  number),
+        where the graph number is the minimam id of the partial reconcile.
+        */
+        CREATE UNLOGGED TABLE matching (min_id integer, line_id integer);
+        CREATE INDEX matching_min_id_idx ON matching USING btree (min_id);
+        CREATE INDEX matching_line_id_idx ON matching USING btree (line_id);
+
+        DO $$
+        DECLARE
+            r record;
+            debit_min_id integer;
+            credit_min_id integer;
+        BEGIN
+            FOR r IN SELECT id, debit_move_id, credit_move_id
+                       FROM account_partial_reconcile
+                      WHERE full_reconcile_id IS NULL
+                   ORDER BY id
+            LOOP
+                SELECT min_id INTO debit_min_id FROM matching WHERE line_id = r.debit_move_id;
+                SELECT min_id INTO credit_min_id FROM matching WHERE line_id = r.credit_move_id;
+                IF debit_min_id IS NOT NULL AND credit_min_id IS NOT NULL THEN
+                    IF credit_min_id != debit_min_id THEN  -- merge graphs
                         UPDATE matching
-                        SET min_id = LEAST(credit_min_id, debit_min_id)
-                        WHERE min_id IN (credit_min_id, debit_min_id)
-                        AND min_id != LEAST(credit_min_id, debit_min_id);
-                    ELSIF debit_min_id IS NOT NULL THEN
-                        INSERT INTO matching (min_id, line_id) VALUES (debit_min_id, credit_line_id);
-                    ELSIF credit_min_id IS NOT NULL THEN
-                        INSERT INTO matching (min_id, line_id) VALUES (credit_min_id, debit_line_id);
-                    ELSE
-                        INSERT INTO matching (min_id, line_id) VALUES (partial_id, debit_line_id), (partial_id, credit_line_id);
+                           SET min_id = LEAST(credit_min_id, debit_min_id)
+                         WHERE min_id IN (credit_min_id, debit_min_id)
+                           AND min_id != LEAST(credit_min_id, debit_min_id);
                     END IF;
-                END;
-                $$ LANGUAGE plpgsql;
+                ELSIF debit_min_id IS NOT NULL THEN  -- add node to graph
+                    INSERT INTO matching (min_id, line_id) VALUES (debit_min_id, r.credit_move_id);
+                ELSIF credit_min_id IS NOT NULL THEN  -- add node to graph
+                    INSERT INTO matching (min_id, line_id) VALUES (credit_min_id, r.debit_move_id);
+                ELSE  -- create new graph
+                    INSERT INTO matching (min_id, line_id) VALUES (r.id, r.debit_move_id), (r.id, r.credit_move_id);
+                END IF;
+            END LOOP;
+        END$$
+    """)
+    cr.execute("ANALYZE matching")
+    util.explode_execute(
+        cr,
         """
-        )
-        queries = []
-        cr.execute("SELECT id from res_company order by id")
-        for company_id in cr.fetchall():
-            queries.append(
-                cr.mogrify(
-                    "SELECT build_matching_table(id, debit_move_id, credit_move_id) FROM account_partial_reconcile WHERE company_id=%s ORDER BY id",
-                    company_id,
-                ).decode()
-            )
-        util.parallel_execute(cr, queries)
-        util.explode_execute(
-            cr,
-            """
             UPDATE account_move_line line
                SET matching_number = 'P' || matching.min_id
               FROM matching
              WHERE matching.line_id = line.id
-               AND line.full_reconcile_id IS NULL
-               AND {parallel_filter}
-            """,
-            table="account_move_line",
-            alias="line",
-        )
-        cr.execute("DROP FUNCTION build_matching_table")
-        cr.execute("DROP TABLE matching")
-    else:
-        util.explode_execute(
-            cr,
-            """
-                WITH RECURSIVE partials (line_id, current_id) AS (
-                        SELECT line.id,
-                            line.id
-                        FROM account_move_line line
-                        JOIN account_partial_reconcile partial
-                            ON line.id = partial.debit_move_id
-                            OR line.id = partial.credit_move_id
-                        WHERE line.full_reconcile_id IS NULL
-                        AND {parallel_filter}
-                                                    UNION
-                        SELECT p.line_id,
-                            CASE WHEN partial.debit_move_id = p.current_id THEN partial.credit_move_id
-                                    ELSE partial.debit_move_id
-                            END
-                        FROM partials p
-                        JOIN account_partial_reconcile partial ON p.current_id = partial.debit_move_id
-                                                                OR p.current_id = partial.credit_move_id
-                ), new_vals AS (
-                    SELECT line_id, 'P' || MIN(partial.id) AS matching_number
-                        FROM partials
-                        JOIN account_partial_reconcile partial ON current_id = partial.debit_move_id
-                                                            OR current_id = partial.credit_move_id
-                    GROUP BY line_id
-                )
-                UPDATE account_move_line line
-                SET matching_number = new_vals.matching_number
-                FROM new_vals
-                WHERE new_vals.line_id = line.id
-            """,
-            table="account_move_line",
-            alias="line",
-        )
+        """,
+        table="account_move_line",
+        alias="line",
+    )
+    cr.execute("DROP TABLE matching")
 
     # ANALYTIC
     # Merge lines that can be merged when all the plans are at 100% and at most one plan is split on multiple accounts
