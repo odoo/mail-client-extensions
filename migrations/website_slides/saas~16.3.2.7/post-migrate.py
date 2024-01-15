a782f7654c0cbab3da09a7168f1f4755dee459fc
Edoardo Piroli <pied@odoo.com>
2024-01-15 15:59:10 +0100

    [FIX] website_slides/saas~16.3: update rule in post
    Force update of record rules in post-, once the used fields are registred.
    
    See https://github.com/odoo/odoo/pull/149154

--- migrations/website_slides/saas~16.3.2.7/post-migrate.py
+++ migrations/website_slides/saas~16.3.2.7/post-migrate.py
@@ -3,6 +3,10 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.update_record_from_xml(cr, "website_slides.rule_slide_slide_signed_in_user")
+    util.update_record_from_xml(cr, "website_slides.rule_slide_channel_visibility_signed_in_user")
+
+    util.if_unchanged(cr, "website_slides.rule_slide_slide_resource_downloadable", util.update_record_from_xml)
     util.if_unchanged(cr, "website_slides.mail_template_slide_channel_enroll", util.update_record_from_xml)
     util.if_unchanged(cr, "website_slides.mail_notification_channel_invite", util.update_record_from_xml)
     extra_filter = cr.mogrify("t.id = %s", [util.ref(cr, "website_slides.view_slide_channel_form")]).decode()
