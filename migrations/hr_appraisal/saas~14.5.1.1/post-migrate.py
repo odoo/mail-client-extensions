2b3959e6c5b128a73b878011aca41f4e3c51340a
Edoardo Piroli <pied@odoo.com>
2024-01-16 10:17:52 +0100

    [FIX] hr_appraisal/saas~14.5: update rule in post
    Force update of record rules in post-, once the used fields are registred.
    
    See https://github.com/odoo/odoo/pull/149154

--- migrations/hr_appraisal/saas~14.5.1.1/post-migrate.py
+++ migrations/hr_appraisal/saas~14.5.1.1/post-migrate.py
@@ -3,6 +3,8 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.update_record_from_xml(cr, "hr_appraisal.hr_appraisal_goal_all")
+    util.update_record_from_xml(cr, "hr_appraisal.hr_appraisal_goal_own")
     util.if_unchanged(
         cr,
         "hr_appraisal.mail_template_appraisal_confirm",
--- migrations/hr_appraisal/saas~14.5.1.1/pre-20-data.py
+++ migrations/hr_appraisal/saas~14.5.1.1/pre-20-data.py
@@ -11,7 +11,6 @@ def migrate(cr, version):
     util.remove_view(cr, "hr_appraisal.hr_job_view_form")
     util.remove_record(cr, "hr_appraisal.mail_template_appraisal_confirm_manager")
     util.remove_record(cr, "hr_appraisal.hr_appraisal_plan_comp_rule")
-    util.update_record_from_xml(cr, "hr_appraisal.hr_appraisal_goal_all")
-    util.update_record_from_xml(cr, "hr_appraisal.hr_appraisal_goal_own")
+
     util.update_record_from_xml(cr, "hr_appraisal.group_hr_appraisal_manager")
     util.update_record_from_xml(cr, "hr_appraisal.ir_cron_scheduler_appraisal")
