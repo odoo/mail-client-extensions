97f15ebec9d1a982e6201df41943866560fa3285
Edoardo Piroli <pied@odoo.com>
2024-01-16 09:57:56 +0100

    [FIX] website_slides/saas~16.5: update rule in post
    Force update of record rules in post-, once the used fields are registred.
    
    See https://github.com/odoo/odoo/pull/149154

--- migrations/website_slides/saas~16.5.2.7/pre-migrate.py
+++ migrations/website_slides/saas~16.5.2.7/pre-migrate.py
@@ -6,9 +6,6 @@ def migrate(cr, version):
     util.remove_view(cr, "website_slides.slide_icon")
     util.remove_view(cr, "website_slides.private_profile")
 
-    util.if_unchanged(cr, "website_slides.rule_slide_channel_visibility_signed_in_user", util.update_record_from_xml)
-    util.if_unchanged(cr, "website_slides.rule_slide_slide_signed_in_user", util.update_record_from_xml)
-    util.if_unchanged(cr, "website_slides.rule_slide_slide_resource_downloadable", util.update_record_from_xml)
     util.remove_field(cr, "slide.channel", "partner_all_ids")
 
     util.remove_field(cr, "res.partner", "slide_channel_all_ids", drop_column=False)
