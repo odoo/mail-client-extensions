7450bb5343582121b5b39e2a9507b576229006ea
Edoardo Piroli <pied@odoo.com>
2024-01-16 09:53:43 +0100

    [FIX] helpdesk/saas~15.2: update rule in post
    Force update of record rules in post-, once the used fields are registred.
    
    See https://github.com/odoo/odoo/pull/149154

--- migrations/helpdesk/saas~15.2.1.4/post-migrate.py
+++ migrations/helpdesk/saas~15.2.1.4/post-migrate.py
@@ -3,6 +3,11 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    # https://github.com/odoo/enterprise/pull/26811
+    util.if_unchanged(cr, "helpdesk.helpdesk_portal_ticket_rule", util.update_record_from_xml)
+    # https://github.com/odoo/enterprise/pull/31804
+    util.if_unchanged(cr, "helpdesk.helpdesk_sla_report_analysis_rule_manager", util.update_record_from_xml)
+
     util.recompute_fields(cr, "helpdesk.sla.status", ["exceeded_hours"])
     cr.execute(
         """
