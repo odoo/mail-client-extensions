706d8cadc0616a6dcd96a1f839255bae26f49d18
Edoardo Piroli <pied@odoo.com>
2024-01-16 10:08:44 +0100

    [FIX] hr_recruitment_survey/saas~16.4: update rule in post
    Force update of record rules in post-, once the used fields are registred.
    
    See https://github.com/odoo/odoo/pull/149154

--- migrations/hr_recruitment_survey/saas~16.4.1.0/post-migrate.py
+++ migrations/hr_recruitment_survey/saas~16.4.1.0/post-migrate.py
@@ -4,5 +4,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.update_record_from_xml(
-        cr, "hr_recruitment.group_hr_recruitment_interviewer", from_module="hr_recruitment_survey"
+        cr, "survey.survey_user_input_rule_survey_user_read", from_module="hr_recruitment_survey"
     )
