09aba3eb668035f0cd1c6485e6c69b057c3f99ba
Maxime de Neuville <mane@odoo.com>
2024-01-17 16:21:25 +0100

    [IMP] project/saas~17.2: remove unused display_access_mode field
    See https://github.com/odoo/odoo/pull/149796
    
    closes odoo/upgrade#5549
    
    Related: odoo/odoo#149796
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/project/saas~17.2.1.3/pre-migrate.py
+++ migrations/project/saas~17.2.1.3/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "project.project", "closed_task_count")
+    util.remove_field(cr, "project.share.wizard", "display_access_mode")
