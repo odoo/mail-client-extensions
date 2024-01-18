e0c42af6a91cd79cbd58aea085b3fec933982fff
Xavier ALT <xal@odoo.com>
2024-01-18 18:06:48 +0100

    [FIX] account/saas~16.5: missing report line user_groupby
    In saas~16.5 a new field `user_groupby` is introduced on the
    account.report.line model, on creation this field receive the same value
    as the `groupby` field.
    
    So this commit set the `user_groupby` on existing report line by copying
    the value from `groupby` to ensure reports still works as expected
    (ex: Aged Receivable with no `user_groupby` will not show any lines)
    
    closes odoo/upgrade#5562
    
    Related: odoo/odoo#133830
    Related: odoo/enterprise#46657
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/account/saas~16.5.1.2/end-account-report-line.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.explode_execute(
+        cr,
+        """
+        UPDATE account_report_line
+           SET user_groupby = groupby
+         WHERE user_groupby IS NULL AND groupby IS NOT NULL
+        """,
+        table="account_report_line",
+    )
