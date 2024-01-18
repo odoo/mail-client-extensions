ebb08a1f5ed41b14b81b49e41f87e5197df63d39
Alvaro Fuentes <afu@odoo.com>
2024-01-18 11:58:25 +0100

    [FIX] {account,analytic}/saas~17.1: remove field in right module
    The field is removed from analytic module, not account.
    
    See: odoo/odoo#144747
    Part-of: odoo/upgrade#5552

--- migrations/account/saas~17.1.1.2/pre-migrate.py
+++ migrations/account/saas~17.1.1.2/pre-migrate.py
@@ -7,4 +7,3 @@ def migrate(cr, version):
     util.remove_field(cr, "account.journal", "sale_activity_type_id")
     util.remove_field(cr, "res.config.settings", "module_account_taxcloud")
     util.remove_field(cr, "res.config.settings", "module_account_avatax")
-    util.remove_field(cr, "analytic.mixin", "analytic_distribution_search")
