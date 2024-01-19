a4816446f2d3d1e9e0b620158700be023b210368
Christophe Simonis <chs@odoo.com>
2024-01-19 14:49:47 +0100

    [FIX] knowledge/saas~16.3
    Don't update gone records.
    They have been converted to article template.
    Those records were in noupdate and meant to be changed by users. Keep
    them.

--- migrations/knowledge/saas~16.3.1.0/post-migrate.py
+++ migrations/knowledge/saas~16.3.1.0/post-migrate.py
@@ -4,9 +4,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.if_unchanged(cr, "knowledge.knowledge_article_user_onboarding", util.update_record_from_xml)
-    util.if_unchanged(cr, "knowledge.knowledge_article_hr_faq", util.update_record_from_xml)
-    util.if_unchanged(cr, "knowledge.knowledge_article_sales_and_marketing", util.update_record_from_xml)
-    util.if_unchanged(cr, "knowledge.knowledge_article_sales_playbook", util.update_record_from_xml)
-    util.if_unchanged(cr, "knowledge.knowledge_article_brand_assets", util.update_record_from_xml)
-
     util.update_record_from_xml(cr, "knowledge.knowledge_article_mail_invite")
