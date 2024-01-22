1389a5bce1d186bf99d205c1819bf9a842897cd4
Christophe Simonis <chs@odoo.com>
2024-01-22 11:17:45 +0100

    [FIX] mass_mailing_{crm,sale}/saas~14.5
    Don't update demo data. It never work.

--- migrations/mass_mailing_sale/saas~14.5.1.0/post-migration.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo.upgrade import util
-
-
-def migrate(cr, version):
-    util.if_unchanged(
-        cr, "mass_mailing_sale.mass_mail_sale_order_0", util.update_record_from_xml, reset_translations={"body_html"}
-    )
