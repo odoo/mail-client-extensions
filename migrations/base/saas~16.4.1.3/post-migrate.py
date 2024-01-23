21c098b4e64fd37100f1228969588c93e47dbb91
Jinane Maksoud <maji@odoo.com>
2024-01-23 14:31:59 +0100

    [FIX] base/saas~16.4: remove parent company
    The parent_id on companies is now redefined to mean
    subsidaries/branches and the child companies will share
    their parents coa and other account and company configs.
    The parent company is hence removed.
    
    Community PR https://github.com/odoo/odoo/pull/125642
    
    Part-of: odoo/upgrade#5575

---
+++ migrations/base/saas~16.4.1.3/post-migrate.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # odoo/odoo#125642
+    # res.company.parent_id is repurposed to identify subsidiaries
+    cr.execute(
+        """
+        UPDATE res_company cc
+           SET parent_id = NULL
+          FROM res_company pc
+         WHERE cc.parent_id = pc.id
+     RETURNING cc.id,
+               cc.name,
+               pc.id,
+               pc.name
+        """
+    )
+    if cr.rowcount:
+        util.add_to_migration_reports(
+            category="Company Hierarchy",
+            message="""
+                <details>
+                    <summary>
+                        In Odoo 17, companies with a parent are considered branches
+                        of their parent companies, any company that previously had a
+                        parent is now set as an independent company to preserve its
+                        settings. Therefore the parent company is removed from the
+                        following:
+                    </summary>
+                    <ul>%s</ul>
+                </details>"""
+            % "".join(
+                f"<li>{name} (id={id}), had as parent company {p_name} (id={p_id})</li>"
+                for id, name, p_id, p_name in cr.fetchall()
+            ),
+            format="html",
+        )
+        util.env(cr)["res.company"]._parent_store_compute()
