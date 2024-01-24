47fa54fd86e678b449178588d17a752b26610424
vlst-odoo <vlst@odoo.com>
2024-01-24 15:21:00 +0100

    [IMP] point_of_sale/saas~17.3: remove old customer display fields
    In the corresponding commnity pr, the customer display feature of
    pos is completely refactored and also moved to a separate new module:
    `pos_customer_display`.
    
    In this commit we update the `point_of_sale` and `pos_iot` modules
    to reflect the new changes.
    
    Some fields are simply renamed. In other cases the modelling is changed.
    
    https://github.com/odoo/odoo/pull/148870
    https://github.com/odoo/enterprise/pull/54501
    
    closes odoo/upgrade#5585
    
    Related: odoo/odoo#148870
    Related: odoo/enterprise#54501
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/point_of_sale/saas~17.3.1.0.1/pre-migrate.py
+++ migrations/point_of_sale/saas~17.3.1.0.1/pre-migrate.py
@@ -4,3 +4,26 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.rename_field(cr, "pos.category", "child_id", "child_ids")
     util.remove_field(cr, "pos.session", "cash_register_total_entry_encoding")
+    util.create_column(cr, "pos_config", "customer_display_type", "varchar")
+    where_clause = (
+        "iface_display_id IS NOT NULL"
+        if util.module_installed(cr, "pos_iot")
+        else "iface_customer_facing_display_via_proxy = True"
+    )
+    cr.execute(f"UPDATE pos_config SET customer_display_type = 'proxy' WHERE {where_clause}")
+    cr.execute("UPDATE pos_config SET customer_display_type = 'local' WHERE iface_customer_facing_display_local = True")
+    util.rename_field(
+        cr,
+        "res.config.settings",
+        "pos_iface_customer_facing_display_background_image_1920",
+        "pos_customer_display_bg_img",
+    )
+    util.remove_field(cr, "res.config.settings", "pos_iface_customer_facing_display_via_proxy")
+    util.remove_field(cr, "res.config.settings", "pos_iface_customer_facing_display_local")
+    util.remove_column(cr, "res_config_settings", "pos_proxy_ip")
+    util.rename_field(
+        cr, "pos.config", "iface_customer_facing_display_background_image_1920", "customer_display_bg_img"
+    )
+    util.remove_field(cr, "pos.config", "iface_customer_facing_display_via_proxy")
+    util.remove_field(cr, "pos.config", "iface_customer_facing_display_local")
+    util.remove_field(cr, "pos.config", "iface_customer_facing_display")
