e290558c7e279af13773fa06faba86b5188b054d
Alvaro Fuentes <afu@odoo.com>
2024-01-24 13:50:49 +0000

    [FIX] stock/0.0.0: ensure correct groups
    See odoo/odoo@99b56ec7
    
    upg-1189857
    opw-3696080
    
    closes odoo/upgrade#5584
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/stock/0.0.0/pre-stable-fixes.py
+++ migrations/stock/0.0.0/pre-stable-fixes.py
@@ -7,3 +7,11 @@ def migrate(cr, version):
         # non-stored field remove in stable version.
         # See https://github.com/odoo/odoo/pull/120048
         util.remove_field(cr, "report.stock.quantity", "move_ids")
+
+    if util.version_between("16.0", "17.0"):
+        env = util.env(cr)
+        user_group = env.ref("base.group_user")
+        portal_group = env.ref("base.group_portal")
+        stock_group = env.ref("stock.group_production_lot")
+        if stock_group in user_group.implied_ids:
+            portal_group._apply_group(stock_group)
