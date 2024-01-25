2738081991ef1ea5f356abb9a7584a923a0ddc05
higo-odoo <higo@odoo.com>
2024-01-25 07:31:17 +0000

    [FIX] sms/saas~14.5.2.2: don't try to update demo records
    `util.update_record_from_xml` only handle `data` records.
    
    closes odoo/upgrade#5589
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/sms/saas~14.5.2.2/post-migrate.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo.upgrade import util
-
-
-def migrate(cr, version):
-    util.if_unchanged(cr, "sms.sms_template_demo_0", util.update_record_from_xml, reset_translations={"body"})
