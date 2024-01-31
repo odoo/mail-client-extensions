697e267aac17cb61c5e8edd84eaa5bafb99247e0
Gorash <chm@odoo.com>
2024-01-31 11:16:12 +0100

    [IMP] {hr_holidays,mail}/saas~17.2: remove fields
    Two fields were removed, and their use was reimplemented otherwise.
    
    closes odoo/upgrade#5616
    
    See: odoo/odoo#151597
    Related: odoo/enterprise#55523
    Related: odoo/documentation#8131
    Signed-off-by: Raphael Collet <rco@odoo.com>

--- migrations/mail/saas~17.2.1.17/pre-migrate.py
+++ migrations/mail/saas~17.2.1.17/pre-migrate.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
@@ -7,3 +6,4 @@ def migrate(cr, version):
     util.rename_field(cr, "mail.activity.schedule", "plan_date_deadline", "plan_date")
     util.rename_field(cr, "mail.activity.schedule", "plan_assignation_summary", "plan_summary")
     util.remove_field(cr, "mail.activity.plan", "assignation_summary")
+    util.remove_field(cr, "mail.tracking.value", "field_groups")
