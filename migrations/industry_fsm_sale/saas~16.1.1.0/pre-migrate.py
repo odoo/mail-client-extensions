9e20cca162b596dfc008dc6dacc9da9396e26236
Atul Patel <atp@odoo.com>
2024-01-31 14:49:42 +0000

    [FIX] industry_fsm_sale/saas~16.1: update record only if unchanged
    For `fsm_project`, there is constraint that
    The timesheet product is required when the fsm project
    can be billed and timesheets are allowed.
    
    `(allow_billable = 't' AND allow_timesheets = 't' AND is_fsm = 't' AND
    timesheet_product_id IS NOT NULL)`
    
    In customer original database,
    for this project `fsm_project`,
    
    fsm project can be billed but no timesheets allowed and timesheet
    product is also null.
    
    `allow_timesheets = False`, `allow_billable =True` and
    `is_fsm = True` and `timesheet_product_id IS NULL`.
    
    ```
    select allow_timesheets, is_fsm, allow_billable, timesheet_product_id from project_project where id=20;
     allow_timesheets | is_fsm | allow_billable | timesheet_product_id
    ------------------+--------+----------------+----------------------
     f                | t      | t              |
    (1 row)
    ```
    
    During upgrade, by this file,
    https://github.com/odoo/upgrade/blob/00db546005f1e8455fefd90cc7fe1bfc3651226c/migrations/industry_fsm_sale/saas~16.1.1.0/pre-migrate.py#L7
    
    It's try to update `fsm_project` data which update
    `allow_timesheet = True` and that raise Constraint error
    because now three field value become true
    
    Means now fsm_project can be billed and timesheets are allowed
    so timesheet product should be required.
    
    `allow_billable = 't' AND allow_timesheets = 't' AND is_fsm = 't'` but
    `timesheet_product_id` is still null that's why constraint raised.
    
    To resolved this we need to use `if_unchaged` to keep
    customer changed and update only if there is no change in record.
    
    TGB:
    https://upgrade.odoo.com/web#id=960&cids=1&menu_id=107&action=178&model=upgrade.request.traceback.group&view_type=form
    
    Part-of: odoo/upgrade#5617

--- migrations/industry_fsm_sale/saas~16.1.1.0/pre-migrate.py
+++ migrations/industry_fsm_sale/saas~16.1.1.0/pre-migrate.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.delete_unused(cr, "industry_fsm_sale.field_service_project_stage_0")
-    util.update_record_from_xml(cr, "industry_fsm.fsm_project", force_create=True)
+    util.if_unchanged(cr, "industry_fsm.fsm_project", util.update_record_from_xml, force_create=True)
     util.remove_view(cr, "industry_fsm_sale.report_project_task_user_fsm_view_search_inherit_sale")
