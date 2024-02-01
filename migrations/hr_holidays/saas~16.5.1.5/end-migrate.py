3462816b16250063c6767c558247e2aa5cfb79c5
Christophe Simonis <chs@odoo.com>
2024-02-01 15:03:10 +0100

    [FIX] hr_holidays/saas~16.5: always compute `number_of_hours`
    Contrary to what the R&D said, this field isn't used only for a report.
    It should be recomputed for all the records.
    
    Also, limiting to the current year is not valid as in some countries,
    employees are allowed to still use remaining leaves from last year.
    
    closes odoo/upgrade#5623
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr_holidays/saas~16.5.1.5/end-migrate.py
+++ migrations/hr_holidays/saas~16.5.1.5/end-migrate.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
-
-import os
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    # We compute the hours separately because we do not
-    # want to let the days be recomputed.
-    compute_leave_hours = os.getenv("UPG_COMPUTE_LEAVE_HOURS", "all")
-    if compute_leave_hours == "skip":
-        return
-    query = "SELECT id FROM hr_leave"
-    if compute_leave_hours == "current_year":
-        query += " WHERE extract(YEAR FROM date_to) >= extract(YEAR FROM current_date)"
-    cr.execute(query)
-    ids = [lid for (lid,) in cr.fetchall()]
-    Leave = util.env(cr)["hr.leave"].with_context(mail_notrack=True)
-    for leave in util.iter_browse(Leave, ids):
-        _, hours = leave._get_duration()
-        leave.number_of_hours = hours
+    util.recompute_fields(cr, "hr.leave", ["number_of_hours"])
