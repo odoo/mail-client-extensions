b3fc76b9ae80283242a8544b10f4d354fe6ebbed
Maxime de Neuville <mane@odoo.com>
2024-02-05 09:31:35 +0100

    [IMP] helpdesk/saas~17.2: convert some metrics from integer to float
    See https://github.com/odoo/enterprise/pull/55531
    
    closes odoo/upgrade#5631
    
    Related: odoo/enterprise#55531
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/helpdesk/saas~17.2.1.6/pre-migrate.py
+++ migrations/helpdesk/saas~17.2.1.6/pre-migrate.py
@@ -9,3 +9,8 @@ def migrate(cr, version):
 
     util.replace_record_references_batch(cr, {old: new}, "mail.activity.type", replace_xmlid=False)
     util.remove_record(cr, "helpdesk.mail_act_helpdesk_handle")
+
+    cr.execute("DROP VIEW IF EXISTS helpdesk_sla_report_analysis")
+    cr.execute("DROP VIEW IF EXISTS helpdesk_ticket_report_analysis")
+    util.alter_column_type(cr, "helpdesk_ticket", "assign_hours", "float8")
+    util.alter_column_type(cr, "helpdesk_ticket", "close_hours", "float8")
