e7377bbf93cb28f8aa68c0037b41025e6a68e218
mjvi-odoo <mjvi@odoo.com>
2024-02-06 14:44:06 +0530

    [IMP] sale/saas~17.3: remove the configuration bar
    - Remove the configuration bar from the sale module.
    
    Community: https://github.com/odoo/odoo/pull/152270
    
    task-3645924
    
    closes odoo/upgrade#5644
    
    Related: odoo/enterprise#61325
    Related: odoo/documentation#8391
    Related: odoo/odoo#152270
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- migrations/sale/saas~17.3.1.2/pre-migrate.py
+++ migrations/sale/saas~17.3.1.2/pre-migrate.py
@@ -3,3 +3,6 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "sale.sale_order_view_form")
+    util.remove_record(cr, "sale.onboarding_onboarding_sale_quotation")
+    util.remove_record(cr, "sale.onboarding_onboarding_step_sale_order_confirmation")
+    util.remove_record(cr, "sale.onboarding_onboarding_step_sample_quotation")
