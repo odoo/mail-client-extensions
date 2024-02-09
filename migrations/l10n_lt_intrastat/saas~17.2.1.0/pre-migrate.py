4b409dfcd7e8851f64413dba7a08bbbbe1053063
hupo-odoo <hupo@odoo.com>
2024-02-09 11:19:58 +0100

    [FIX] l10n_lt_intrastat/saas~17.2: remove view of intrastat region
    The view defining the intrastat region in the Accounting settings is now
    defined in a different module to enable other localization to benefit from it.
    
    See https://github.com/odoo/enterprise/pull/56238
    
    task-3691571
    
    closes odoo/upgrade#5666
    
    Related: odoo/enterprise#56238
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/l10n_lt_intrastat/saas~17.2.1.0/pre-migrate.py
@@ -0,0 +1,8 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "l10n_lt_intrastat.res_config_settings_view_form")
+    util.move_field_to_module(
+        cr, "res.config.settings", "intrastat_region_id", "l10n_lt_intrastat", "account_intrastat"
+    )
