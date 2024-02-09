35830148fdea7cdfe2cdba8eff4fbef48e701b26
mathysPaul <matp@odoo.com>
2024-02-09 10:45:35 +0100

    [IMP] payment/saas~17.3.2.0: remove fields
    Removes explicit configuration fields like show_credentials_page
    and require_currency from the payment module. This adjustment
    adopts XPath for direct view customization, simplifying the
    setup and eliminating the need for method overrides for view
    modifications.
    
    See odoo/odoo#152517
    See odoo/enterprise#56181
    See odoo/documentation#7686
    
    closes odoo/upgrade#5667
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/payment/saas~17.3.2.0/pre-migrate.py
+++ migrations/payment/saas~17.3.2.0/pre-migrate.py
@@ -5,3 +5,13 @@ def migrate(cr, version):
     util.remove_view(cr, "payment.pay_meth_link")
 
     util.rename_xmlid(cr, "payment.transaction_status", "payment.state_header")
+
+    util.remove_field(cr, "payment.provider", "show_credentials_page")
+    util.remove_field(cr, "payment.provider", "show_allow_tokenization")
+    util.remove_field(cr, "payment.provider", "show_allow_express_checkout")
+    util.remove_field(cr, "payment.provider", "show_pre_msg")
+    util.remove_field(cr, "payment.provider", "show_pending_msg")
+    util.remove_field(cr, "payment.provider", "show_auth_msg")
+    util.remove_field(cr, "payment.provider", "show_done_msg")
+    util.remove_field(cr, "payment.provider", "show_cancel_msg")
+    util.remove_field(cr, "payment.provider", "require_currency")
