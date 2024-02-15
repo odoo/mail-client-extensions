4f11b949a5612fdadadf80989b9d09beeeff0ec1
Sven Fuehr <svfu@odoo.com>
2024-02-15 17:05:09 +0100

    [FIX] l10n_lt_reports/saas~17.3: remove obsolete records
    In the related enterprise PR (see below) the reports were updated to
    use tags instead of codes.
    As part of this the profit and loss information in the BS was
    updated to refer directly to the P&L (cross_report).
    In the process some account.report.expression records were removed.
    
    task-3701151
    
    see odoo/odoo#154407
    see odoo/enterprise#56825
    
    closes odoo/upgrade#5706
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/l10n_lt_reports/saas~17.3.1.0.0/pre-migrate.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_record(cr, "l10n_lt_reports.account_financial_html_report_line_bs_lt_d_5_1_balance_account_codes")
+    util.remove_record(cr, "l10n_lt_reports.account_financial_html_report_line_bs_lt_d_5_1_balance_aggregate")
+    util.remove_record(cr, "l10n_lt_reports.account_financial_html_report_line_bs_lt_d_5_1_bs_pl_balance")
+    util.remove_record(cr, "l10n_lt_reports.account_financial_html_report_line_bs_lt_d_5_2_balance_aggregate")
