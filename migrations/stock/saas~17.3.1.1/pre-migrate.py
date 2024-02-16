17b59dd12de5c72f3ee807eb303836afbce9fbdb
ABE-Mark45 <aels@odoo.com>
2024-02-16 12:38:29 +0300

    [IMP] stock/saas~17.3: remove redundant records and update fields
    1. Remove `group_stock_storage_categories` field from `res.config.settings`
    2. Remove corresponding `group_stock_storage_categories` record
    3. Remove `menu_reordering_rules_config` menu item
    4. Putaway sublocation strategy will be set to closest location
    for rules having storage category set
    5. Putaway sublocation strategy will be set to 'no' for rules
    with a null storage category
    
    task-3718808
    community PR: https://github.com/odoo/odoo/pull/158638
    
    closes odoo/upgrade#5889
    
    Related: odoo/odoo#158638
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/stock/saas~17.3.1.1/pre-migrate.py
+++ migrations/stock/saas~17.3.1.1/pre-migrate.py
@@ -4,3 +4,16 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_column(cr, "stock_move_line", "product_category_name")
     util.remove_column(cr, "stock_warehouse_orderpoint", "qty_to_order")
+    util.remove_field(cr, "res.config.settings", "group_stock_storage_categories")
+    util.remove_record(cr, "stock.group_stock_storage_categories")
+    util.remove_record(cr, "stock.menu_reordering_rules_config")
+    util.create_column(cr, "stock_putaway_rule", "sublocation", "varchar", default="no")
+
+    # Putaway sublocation strategy will be set to 'closest location
+    # for rules having storage category set
+    query = """
+        UPDATE stock_putaway_rule
+           SET sublocation = 'closest_location'
+         WHERE storage_category_id IS NOT NULL
+    """
+    cr.execute(query)
