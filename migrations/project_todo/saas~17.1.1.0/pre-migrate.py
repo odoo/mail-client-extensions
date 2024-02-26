e058ff4b0b3f9b5a23b106df08b5a3f7014d1b8e
Alvaro Fuentes <afu@odoo.com>
2024-02-26 15:34:39 +0000

    [FIX] project_todo/saas~17.1: replace activity type everywhere
    There may be restriction keys in other tables, not just `mail_activity`
    
    ```
      File "/tmp/tmp9fonemz2/migrations/project_todo/saas~17.1.1.0/pre-migrate.py", line 19, in migrate
        util.remove_record(cr, "project_todo.mail_activity_data_reminder")
      File "/tmp/tmp9fonemz2/migrations/util/records.py", line 318, in remove_record
        return remove_records(cr, model, [res_id])
      File "/tmp/tmp9fonemz2/migrations/util/records.py", line 356, in remove_records
        cr.execute('DELETE FROM "{}" WHERE id IN %s'.format(table), [ids])
      File "/home/odoo/src/odoo/saas-17.1/odoo/sql_db.py", line 346, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.ForeignKeyViolation: update or delete on table "mail_activity_type" violates foreign key constraint "ir_act_server_activity_type_id_fkey" on table "ir_act_server"
    DETAIL:  Key (id)=(16) is still referenced from table "ir_act_server".
    ```
    
    Tested on: upg-1320438 upg-1387747
    More in: tbg-1084
    
    closes odoo/upgrade#5742
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/project_todo/saas~17.1.1.0/pre-migrate.py
+++ migrations/project_todo/saas~17.1.1.0/pre-migrate.py
@@ -4,17 +4,13 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    to_do_activity_type = util.ref(cr, "mail.mail_activity_data_todo")
     reminder_activity_type = util.ref(cr, "project_todo.mail_activity_data_reminder")
     if reminder_activity_type:
-        query = cr.mogrify(
-            """
-            UPDATE mail_activity
-               SET activity_type_id = %s
-             WHERE activity_type_id = %s
-            """,
-            [to_do_activity_type, reminder_activity_type],
-        ).decode()
-        util.explode_execute(cr, query, table="mail_activity")
-    util.remove_record(cr, "project_todo.mail_activity_data_reminder")
+        util.replace_record_references_batch(
+            cr,
+            {reminder_activity_type: util.ref(cr, "mail.mail_activity_data_todo")},
+            "mail.activity.type",
+            replace_xmlid=False,
+        )
     util.change_field_selection_values(cr, "mail.activity.type", "category", {"reminder": "default"})
+    util.remove_record(cr, "project_todo.mail_activity_data_reminder")
