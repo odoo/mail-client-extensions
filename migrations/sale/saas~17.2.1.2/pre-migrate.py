f6993fcdc966aea178926233d8fd1516e7975e5f
Lina (liew) <liew@odoo.com>
2024-02-26 16:01:26 +0100

    [IMP] sale/saas~17.2.1.2: fill null attached_on in product_document
    See odoo/odoo#153465
    
    closes odoo/upgrade#5741
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- migrations/sale/saas~17.2.1.2/pre-migrate.py
+++ migrations/sale/saas~17.2.1.2/pre-migrate.py
@@ -3,3 +3,10 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "account.analytic.line", "allowed_so_line_ids")
+
+    query = """
+        UPDATE product_document
+        SET attached_on = 'hidden'
+        WHERE attached_on is NULL
+    """
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="product_document"))
