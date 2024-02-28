7b3b9d32b975b6b1de1a406987d166cb3801ccd6
Jérémy Hennecart (jeh) <jeh@odoo.com>
2024-02-28 15:45:12 +0100

    [IMP] appointment/saas~17.3: remove calendar field on manage leaves
    - Remove the calendar_id field on the appointment_manage_leaves wizard.
    This field is no longer needed as it was hidden because of the
    possible creation of global public leaves for a company and/or calendar
    
    ENT PR: odoo/enterprise#58963
    
    task-3773104
    
    closes odoo/upgrade#5847
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/appointment/saas~17.3.1.3/pre-migrate.py
+++ migrations/appointment/saas~17.3.1.3/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "appointment.type", "user_assign_method")
+    util.remove_field(cr, "appointment.manage.leaves", "calendar_id")
