c321214d567b9cec9f49124063e9b9f8b7932a22
Dhwani Patel <dwa@odoo.com>
2024-02-29 06:01:49 +0000

    [FIX] knowledge/saas~16.2.0.1: make visible knowledge article
    In version saas~16.2, We introduced new field 'is_article_visible_by_everyone'
    to make restriction on knowledge articles like visible to some user or not. But
    if customer is upgrading from lower version to saas~16.2, the all knowledge
    article are invisible to any user because during upgrade we set default False
    in 'is_article_visible_by_everyone' field, that's why knowledge articles are not
    visible to user.
    
    To resolve, this issue I have make this field default true as it has to be show
    to user then after if customer want change articles visibility then they can
    change according to their requirement.
    
    req-1326641
    OPW-3741019
    
    closes odoo/upgrade#5761
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/knowledge/saas~16.2.0.1/pre-migrate.py
+++ migrations/knowledge/saas~16.2.0.1/pre-migrate.py
@@ -4,3 +4,10 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "knowledge_article", "is_article_visible_by_everyone", "boolean", default=False)
+    cr.execute(
+        """
+        UPDATE knowledge_article
+           SET is_article_visible_by_everyone = True
+         WHERE category = 'workspace'
+        """
+    )
