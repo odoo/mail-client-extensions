093562bc860a5ad6a7e09bff5cd8438cbb3cc707
william-andre <wan@odoo.com>
2024-03-05 16:13:33 +0100

    [IMP] account/saas~17.2: remove denormalized columns
    Both the fields `include_initial_balance` and `internal_group` are used
    only to filter the search on `account_account`. By implementing the
    `search` functions, we can give more flexibility to the query planner by
    allowing to use the index already defined on `account_type`
    
    Related to
    odoo/odoo#155390
    odoo/enterprise#57520
    
    closes odoo/upgrade#5783
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/account/saas~17.2.1.2/pre-migrate.py
+++ migrations/account/saas~17.2.1.2/pre-migrate.py
@@ -4,3 +4,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "account.account_payment_term_form")
     util.rename_xmlid(cr, "account.account_payment_term_advance", "account.account_payment_term_90days_on_the_10th")
+    util.remove_column(cr, "account_account", "include_initial_balance")
+    util.remove_column(cr, "account_account", "internal_group")
