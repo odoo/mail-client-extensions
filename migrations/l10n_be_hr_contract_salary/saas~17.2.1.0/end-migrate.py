17c98a638c7c0cf5dc5e9fd868698b6a1ebc5a5c
cmsi-odoo <cmsi@odoo.com>
2024-03-05 14:49:36 +0100

    [IMP] l10n_be_hr_contract_salary/saas~17.2:change benefit fields content
    - The "Company Car" benefit has been renamed to "Transportation"
    - The obsolete descriptions indicating mutual exclusivity with
      "Company Bike" and "Fuel Card" benefits have been removed.
    - The "Fuel Card" benefit's sequence has been adjusted to 22.
    
    reference PR:
    
    https://github.com/odoo/enterprise/pull/58020
    
    closes odoo/upgrade#5781
    
    Related: odoo/enterprise#58020
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ migrations/l10n_be_hr_contract_salary/saas~17.2.1.0/end-migrate.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.update_record_from_xml(cr, "l10n_be_hr_contract_salary.l10n_be_transport_company_car")
+    util.update_record_from_xml(cr, "l10n_be_hr_contract_salary.l10n_be_transport_company_bike")
+    util.update_record_from_xml(cr, "l10n_be_hr_contract_salary.l10n_be_fuel_card")
