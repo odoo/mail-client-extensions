8616af4547dfd5ba77dc0012906957e8e23aa8a9
hpr-odoo <hpr@odoo.com>
2024-03-06 12:40:53 +0000

    [FIX] event_sale/saas~16.5.1.3: fix serialize access error
    - traceback:
    ```py
    2024-03-05 10:40:33,587 28 INFO 1386443_org_16.0_17.0 odoo.modules.migration: module event_sale: Running migration [>saas~16.5.1.3] pre-migrate
    2024-03-05 10:40:34,497 28 ERROR ? odoo.sql_db: bad query:
                UPDATE event_registration reg
                   SET sale_status = CASE
                        WHEN so_l.price_total = 0 THEN 'free'
                        WHEN reg.is_paid = false AND reg.state = 'open' THEN 'free'
                        WHEN reg.is_paid = true THEN 'sold'
                        ELSE 'to_pay'
                        END
                  FROM sale_order_line so_l
                 WHERE so_l.id = reg.sale_order_line_id
                 AND reg.id IS NOT NULL
    ERROR: could not serialize access due to concurrent update
    
    2024-03-05 10:40:34,508 28 WARNING 1386443_org_16.0_17.0 odoo.modules.loading: Transient module states were reset
    2024-03-05 10:40:34,508 28 ERROR 1386443_org_16.0_17.0 odoo.modules.registry: Failed to load registry
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/17.0/odoo/modules/registry.py", line 113, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/17.0/odoo/modules/loading.py", line 476, in load_modules
        processed_modules += load_marked_modules(env, graph,
      File "/home/odoo/src/odoo/17.0/odoo/modules/loading.py", line 364, in load_marked_modules
        loaded, processed = load_module_graph(
      File "/home/odoo/src/odoo/17.0/odoo/modules/loading.py", line 181, in load_module_graph
        migrations.migrate_module(package, 'pre')
      File "/home/odoo/src/odoo/17.0/odoo/modules/migration.py", line 240, in migrate_module
        migrate(self.cr, installed_version)
      File "/tmp/tmp3gf_x8n9/migrations/event_sale/saas~16.5.1.3/pre-migrate.py", line 12, in migrate
        util.parallel_execute(
      File "/tmp/tmp3gf_x8n9/migrations/util/pg.py", line 131, in parallel_execute
        return parallel_execute_impl(cr, queries, logger=_logger)
      File "/tmp/tmp3gf_x8n9/migrations/util/pg.py", line 110, in _parallel_execute_threaded
        return sum(
      File "/tmp/tmp3gf_x8n9/migrations/util/misc.py", line 215, in log_progress
        for i, e in enumerate(it, 1):
      File "/usr/lib/python3.10/concurrent/futures/_base.py", line 621, in result_iterator
        yield _result_or_cancel(fs.pop())
      File "/usr/lib/python3.10/concurrent/futures/_base.py", line 319, in _result_or_cancel
        return fut.result(timeout)
      File "/usr/lib/python3.10/concurrent/futures/_base.py", line 458, in result
        return self.__get_result()
      File "/usr/lib/python3.10/concurrent/futures/_base.py", line 403, in __get_result
        raise self._exception
      File "/usr/lib/python3.10/concurrent/futures/thread.py", line 58, in run
        result = self.fn(*self.args, **self.kwargs)
      File "/tmp/tmp3gf_x8n9/migrations/util/pg.py", line 102, in execute
        cr.execute(query)
      File "/home/odoo/src/odoo/17.0/odoo/sql_db.py", line 332, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.SerializationFailure: could not serialize access due to concurrent update
    ```
    
    - UPG-1386443
    - TBG-881
    - OPW-3765226
    
    closes odoo/upgrade#5788
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/event_sale/saas~16.5.1.3/pre-migrate.py
+++ migrations/event_sale/saas~16.5.1.3/pre-migrate.py
@@ -9,29 +9,24 @@ def migrate(cr, version):
     util.change_field_selection_values(cr, "event.registration", "sale_status", {"paid": "sold"})
     util.create_column(cr, "event_registration", "sale_status", "varchar")
 
-    util.parallel_execute(
+    util.explode_execute(
+        cr, "UPDATE event_registration SET sale_status = 'free' WHERE sale_order_id IS NULL", table="event_registration"
+    )
+    util.explode_execute(
         cr,
-        util.explode_query_range(
-            cr,
-            "UPDATE event_registration SET sale_status = 'free' WHERE sale_order_id IS NULL",
-            table="event_registration",
-        )
-        + util.explode_query_range(
-            cr,
-            """
-            UPDATE event_registration reg
-               SET sale_status = CASE
-                    WHEN so_l.price_total = 0 THEN 'free'
-                    WHEN reg.is_paid = false AND reg.state = 'open' THEN 'free'
-                    WHEN reg.is_paid = true THEN 'sold'
-                    ELSE 'to_pay'
-                    END
-              FROM sale_order_line so_l
-             WHERE so_l.id = reg.sale_order_line_id
-            """,
-            table="event_registration",
-            alias="reg",
-        ),
+        """
+        UPDATE event_registration reg
+           SET sale_status = CASE
+                WHEN so_l.price_total = 0 THEN 'free'
+                WHEN reg.is_paid = false AND reg.state = 'open' THEN 'free'
+                WHEN reg.is_paid = true THEN 'sold'
+                ELSE 'to_pay'
+                END
+          FROM sale_order_line so_l
+         WHERE so_l.id = reg.sale_order_line_id
+        """,
+        table="event_registration",
+        alias="reg",
     )
 
     util.remove_field(cr, "event.registration", "is_paid")
