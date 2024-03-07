e8e64b71e6b47bb42f0d86ea3dcda30c31147940
Noe Antoine <nan@odoo.com>
2024-03-07 13:33:37 +0100

    [IMP] mass_mailing/saas~17.2: remove graph views
    Remove graph views for mailing.mailing, as reporting
    is more complete and appropriate.
    
    COM PR: odoo/odoo#155399
    ENT PR: odoo/enterprise#57526
    
    Task-3653289
    
    closes odoo/upgrade#5791
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/mass_mailing/saas~17.2.2.7/pre-migrate.py
+++ migrations/mass_mailing/saas~17.2.2.7/pre-migrate.py
@@ -5,3 +5,6 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "mass_mailing.mailing_mailing_view_form_full_width")
     util.remove_record(cr, "mass_mailing.mailing_mailing_action_mail_fullwidth_form")
+
+    util.remove_view(cr, "mass_mailing.view_mail_mass_mailing_graph")
+    util.remove_record(cr, "mass_mailing.mailing_mailing_action_mail_fullwidth_graph")
