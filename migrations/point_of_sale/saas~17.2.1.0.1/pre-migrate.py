f12e0afd4687fe54281e9fd4f2b1281bd2abb019
David Monnom (moda) <moda@odoo.com>
2024-03-07 16:03:14 +0100

    [IMP] point_of_sale/saas~17.2: move note fields in point_of_sale
    The note fields was moved from pos_restaurant module to point_of_sale.
    Following specs requirement.
    
    TaskId: 3758925
    Community PR: https://github.com/odoo/odoo/pull/155733
    
    closes odoo/upgrade#5792
    
    Related: odoo/odoo#155733
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/point_of_sale/saas~17.2.1.0.1/pre-migrate.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.move_field_to_module(
+        cr,
+        "pos.order.line",
+        "note",
+        "pos_restaurant",
+        "point_of_sale",
+    )
