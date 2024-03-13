89a55df265aa1f674b10c34542ac1faba4b56ea8
Gabriel (gdpf) <gdpf@odoo.com>
2024-03-13 14:33:13 +0100

    [IMP] {google,microsoft}_calendar/saas~17.2: move credentials
    Merge the Google and Microsoft Calendar Credentials into Res Users Settings.
    This move allows saving the both synchronization credentials in the same model
    and also creating new fields in the future related to the synchronizations
    (which should not be added in Res Users directly).
    
    Community PR: odoo/odoo#151537
    
    task-3707128
    
    closes odoo/upgrade#5611
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/google_calendar/saas~17.2.1.0/post-migrate.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    """
+    User settings table for calendar users (Community PR:151537).
+    """
+    # Upsert the Google Calendar Credentials into the Res Users Settings table.
+    cr.execute(
+        """
+        INSERT INTO res_users_settings (
+                        user_id,
+                        google_calendar_sync_token,
+                        google_calendar_token,
+                        google_calendar_rtoken,
+                        google_synchronization_stopped,
+                        google_calendar_token_validity,
+                        google_calendar_cal_id
+                    )
+             SELECT usr.id,
+                    cred_user.calendar_sync_token,
+                    cred_user.calendar_token,
+                    cred_user.calendar_rtoken,
+                    cred_user.synchronization_stopped,
+                    cred_user.calendar_token_validity,
+                    cred_user.calendar_cal_id
+               FROM google_calendar_credentials AS cred_user
+               JOIN res_users AS usr
+                 ON usr.google_calendar_account_id = cred_user.id
+              WHERE cred_user.calendar_rtoken IS NOT NULL
+        ON CONFLICT (user_id) DO UPDATE
+                SET google_calendar_sync_token = EXCLUDED.google_calendar_sync_token,
+                    google_calendar_token = EXCLUDED.google_calendar_token,
+                    google_calendar_rtoken = EXCLUDED.google_calendar_rtoken,
+                    google_synchronization_stopped = EXCLUDED.google_synchronization_stopped,
+                    google_calendar_token_validity = EXCLUDED.google_calendar_token_validity,
+                    google_calendar_cal_id = EXCLUDED.google_calendar_cal_id
+        """
+    )
+
+    # Remove model and its Many2one field in the res_users model.
+    util.remove_field(cr, "res.users", "google_calendar_account_id")
+    util.remove_model(cr, "google.calendar.credentials")
---
+++ migrations/microsoft_calendar/saas~17.2.1.0/post-migrate.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    """
+    User settings table for calendar users (Community PR:151537).
+    """
+    # Upsert the Microsoft Calendar Credentials into the Res Users Settings table.
+    cr.execute(
+        """
+        INSERT INTO res_users_settings (
+                        user_id,
+                        microsoft_calendar_sync_token,
+                        microsoft_synchronization_stopped,
+                        microsoft_last_sync_date
+                    )
+             SELECT usr.id,
+                    cred_user.calendar_sync_token,
+                    cred_user.synchronization_stopped,
+                    cred_user.last_sync_date
+               FROM microsoft_calendar_credentials AS cred_user
+               JOIN res_users AS usr
+                 ON usr.microsoft_calendar_account_id = cred_user.id
+              WHERE cred_user.calendar_sync_token IS NOT NULL
+        ON CONFLICT (user_id) DO UPDATE SET
+                    microsoft_calendar_sync_token = EXCLUDED.microsoft_calendar_sync_token,
+                    microsoft_synchronization_stopped = EXCLUDED.microsoft_synchronization_stopped,
+                    microsoft_last_sync_date = EXCLUDED.microsoft_last_sync_date
+        """
+    )
+
+    # Remove model and its Many2one field in the res_users model.
+    util.remove_field(cr, "res.users", "microsoft_calendar_account_id")
+    util.remove_model(cr, "microsoft.calendar.credentials")
