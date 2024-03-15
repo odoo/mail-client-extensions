07caf3534b6f47f093b1e35a9a83848acb279850
Vincent Larcin <vila@odoo.com>
2024-03-15 11:03:29 +0100

    [IMP] project/saas~17.2: update task message subtypes and data
    Since we change the name and description of message subtypes and
    data records, this commit forces the update for existing databases.
    
    Community: https://github.com/odoo/odoo/pull/152000
    Enterprise: https://github.com/odoo/enterprise/pull/55505
    
    Task-3713873
    
    closes odoo/upgrade#5825
    
    Related: odoo/odoo#152000
    Related: odoo/enterprise#55505
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/project/saas~17.2.1.3/post-migrate.py
+++ migrations/project/saas~17.2.1.3/post-migrate.py
@@ -6,3 +6,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.if_unchanged(cr, "project.mail_template_data_project_task", util.update_record_from_xml)
     util.if_unchanged(cr, "project.project_task_rule_portal", util.update_record_from_xml)
+    util.if_unchanged(cr, "project.mt_task_canceled", util.update_record_from_xml)
