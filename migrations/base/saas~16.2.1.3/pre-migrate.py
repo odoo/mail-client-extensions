7bbd409ff546b547794574718ed2fd8f87144b08
Bommisetty Narendra <bona@odoo.com>
2024-03-18 12:45:32 +0000

    [FIX] base/saas~16.2.1.3:Check before dropping foreign key constraint
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/17.0/odoo/service/server.py",
      line 1302, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "<decorator-gen-16>", line 2, in new
      File "/home/odoo/src/odoo/17.0/odoo/tools/func.py", line 87, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/17.0/odoo/modules/registry.py", line 113, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/17.0/odoo/modules/loading.py",
      line 422, in load_modules
        loaded_modules, processed_modules = load_module_graph(
      File "/home/odoo/src/odoo/17.0/odoo/modules/loading.py",
      line 181, in load_module_graph
        migrations.migrate_module(package, 'pre')
      File "/home/odoo/src/odoo/17.0/odoo/modules/migration.py",
      line 240, in migrate_module
        migrate(self.cr, installed_version)
      File "/tmp/tmprf9b1_ni/migrations/base/saas~16.2.1.3/pre-migrate.py",
      line 5, in migrate
        cr.execute
    ("ALTER TABLE res_users_apikeys DROP CONSTRAINT res_users_apikeys_user_id_fkey")
      File "/home/odoo/src/odoo/17.0/odoo/sql_db.py", line 332, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.UndefinedObject: constraint
    "res_users_apikeys_user_id_fkey" of relation "res_users_apikeys" does not exist
    
    Description:
    We encountered an error due to the absence of the
    `res_users_apikeys_user_id_fkey` constraint in the customer's database
    while attempting to drop it. To address this issue,
    we need to implement a check before proceeding with the drop operation.
    
    UPG - 1403690
    OPW - 3798437
    
    closes odoo/upgrade#5828
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/saas~16.2.1.3/pre-migrate.py
+++ migrations/base/saas~16.2.1.3/pre-migrate.py
@@ -2,5 +2,5 @@
 
 
 def migrate(cr, version):
-    cr.execute("ALTER TABLE res_users_apikeys DROP CONSTRAINT res_users_apikeys_user_id_fkey")
+    cr.execute("ALTER TABLE res_users_apikeys DROP CONSTRAINT IF EXISTS res_users_apikeys_user_id_fkey")
     cr.execute("ALTER TABLE res_users_apikeys ADD FOREIGN KEY (user_id) REFERENCES res_users(id) ON DELETE cascade")
