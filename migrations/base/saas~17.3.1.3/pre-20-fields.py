b1403e6581b7ceb7fbee1aa701076d89e72596c7
Julien Castiaux <juc@odoo.com>
2024-03-21 10:33:36 +0100

    [FIX] base/saas~17.3: ir.mail_server auth=cert requires encryption=tls
    From the two selections fields, the combination of
    `smtp_authentication='certificate'` and `smtp_encryption='none'` just
    doesn't makes sense. You need an established TLS connexion to send a
    user certificate. The actual behavior is that now authentication (nor
    via a certificate nor via a login) is performed, this basically acts
    like a login with the user/pass fields empty.
    
    A constraint has been added to refuse that specific combo, the SQL
    scripts  changes all bad rows to the equivalent auth=login with an
    empty user/pass pair.
    
    task-3820247
    
    closes odoo/upgrade#5845
    
    Related: odoo/odoo#158535
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/base/saas~17.3.1.3/pre-20-fields.py
+++ migrations/base/saas~17.3.1.3/pre-20-fields.py
@@ -1,7 +1,40 @@
 from odoo.upgrade import util
 
+SMTP_MESSAGE = """\
+The following Outgoing Mail Servers (`ir.mail_server`) were configured \
+to share certificates on an insecure connection. The system behavior \
+in that case was to ignore the certificates and to connect using an \
+empty password scheme. To reflect what the system was actually doing, \
+the following changes have been applied on those records:
+* smtp_authentication='login'
+* smtp_user=None
+* smtp_pass=None
+Impacted records:
+- {}
+Please review those records to make sure the configuration is valid."""
+
 
 def migrate(cr, version):
     cr.execute("UPDATE ir_cron SET active = false WHERE numbercall = 0 AND active")
     util.remove_field(cr, "ir.cron", "doall")
     util.remove_field(cr, "ir.cron", "numbercall")
+
+    cr.execute(
+        """
+           UPDATE ir_mail_server
+              SET smtp_authentication = 'login',
+                  smtp_user = null,
+                  smtp_pass = null
+            WHERE smtp_authentication = 'certificate'
+              AND smtp_encryption = 'none'
+        RETURNING id, name
+    """
+    )
+    rows = cr.dictfetchall()
+    if rows:
+        util.add_to_migration_reports(
+            SMTP_MESSAGE.format(
+                "\n- ".join(util.get_anchor_link_to_record("ir.mail_server", row["id"], row["name"]) for row in rows)
+            ),
+            format="md",
+        )
