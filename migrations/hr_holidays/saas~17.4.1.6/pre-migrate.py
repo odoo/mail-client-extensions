2f7781e6838d03b724de8b44196265c480d89258
Yannick Tivisse <yti@odoo.com>
2024-03-22 15:29:57 +0100

    [IMP] hr_holidays/saas~17.4: Remove multi leave/allocations
    Com PR: https://github.com/odoo/odoo/pull/158829
    Ent PR: https://github.com/odoo/enterprise/pull/59246
    
    closes odoo/upgrade#5852
    
    Taskid: 3323139
    Related: odoo/odoo#158829
    Related: odoo/enterprise#59246
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr_holidays/saas~17.4.1.6/pre-migrate.py
+++ migrations/hr_holidays/saas~17.4.1.6/pre-migrate.py
@@ -19,3 +19,37 @@ def migrate(cr, version):
             "no": "no_validation",
         },
     )
+
+    cr.execute(
+        "DELETE FROM hr_leave WHERE holiday_type in ('company', 'department', 'category') OR employee_id IS NULL"
+    )
+    cr.execute(
+        "DELETE FROM hr_leave_allocation WHERE holiday_type in ('company', 'department', 'category') OR employee_id IS NULL"
+    )
+
+    util.remove_constraint(cr, "hr_leave", "hr_leave_type_value")
+    util.remove_constraint(cr, "hr_leave_allocation", "hr_leave_allocation_type_value")
+
+    util.remove_field(cr, "hr.leave", "parent_id")
+    util.remove_field(cr, "hr.leave", "linked_request_ids")
+    util.remove_field(cr, "hr.leave", "holiday_type")
+    util.remove_field(cr, "hr.leave", "employee_ids")
+    util.remove_field(cr, "hr.leave", "multi_employee")
+    util.remove_field(cr, "hr.leave", "category_id")
+    util.remove_field(cr, "hr.leave", "mode_company_id")
+    util.remove_field(cr, "hr.leave", "all_employee_ids")
+
+    util.remove_field(cr, "hr.leave.allocation", "parent_id")
+    util.remove_field(cr, "hr.leave.allocation", "linked_request_ids")
+    util.remove_field(cr, "hr.leave.allocation", "holiday_type")
+    util.remove_field(cr, "hr.leave.allocation", "employee_ids")
+    util.remove_field(cr, "hr.leave.allocation", "multi_employee")
+    util.remove_field(cr, "hr.leave.allocation", "mode_company_id")
+    util.remove_field(cr, "hr.leave.allocation", "category_id")
+    util.remove_field(cr, "hr.leave.allocation", "has_accrual_plan")
+
+    util.remove_field(cr, "hr.leave.report", "holiday_type")
+    util.remove_field(cr, "hr.leave.report", "category_id")
+
+    eb = util.expand_braces
+    util.rename_xmlid(cr, *eb("hr_holidays.{act_hr_employee_holiday_type,action_hr_leave_report}"))
