8ecda639078b9bddb68d086e25dbc2763dc8d0d8
Lina (liew) <liew@odoo.com>
2024-03-29 10:06:37 +0100

    [IMP] product/saas~17.3: simplify pricelist fields
    - remove discount policy from pricelist
    - remove different pricelist groups and relevant records
    - remove selectable field on pricelist
    - remove configuration tab on pricelist
    
    See odoo/odoo#159746
    See odoo/enterprise#59680
    
    closes odoo/upgrade#5883
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- migrations/product/saas~17.4.1.2/pre-migrate.py
+++ migrations/product/saas~17.4.1.2/pre-migrate.py
@@ -6,3 +6,8 @@ def migrate(cr, version):
         util.move_field_to_module(cr, "product.template", "service_tracking", "sale", "product")
     else:
         util.create_column(cr, "product_template", "service_tracking", "varchar", default="no")
+    util.remove_record(cr, "product.pricelist_availability")
+    util.remove_record(cr, "product.pricelist_discounts")
+    util.remove_field(cr, "res.config.settings", "group_sale_pricelist")
+    util.remove_field(cr, "res.config.settings", "product_pricelist_setting")
+    util.remove_field(cr, "product.pricelist", "discount_policy")
