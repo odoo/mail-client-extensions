1f4c1342dcd7c4df123cf5bd6395578ce79595fd
Nicolas Seinlet <nse@odoo.com>
2024-03-29 14:25:52 +0100

    [FIX] account_edi/16.0: batch compute
    With millions of records, reading all records leads to a MemoryError in python.
    Batching solves this issues. But batching is not sufficient, as this computation
    trigger a tracking in mail class, which try to store all non-stored related
    fields in memory, and is solved by disabling tracking.
    
    closes odoo/upgrade#5881
    
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

---
+++ migrations/account_edi/16.0.1.0/pre-migrate.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from odoo import models
+
+from odoo.addons.account_edi.models import account_journal  # noqa
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    pass
+
+
+class AccountJournal(models.Model):
+    _inherit = "account.journal"
+    _module = "account_edi"
+
+    def _compute_edi_format_ids(self):
+        if self.env.context.get("_upg_iter_browse"):
+            return super()._compute_edi_format_ids()
+        util.iter_browse(
+            self.env["account.journal"].with_context(_upg_iter_browse=True), self.ids
+        )._compute_edi_format_ids()
+        return True
