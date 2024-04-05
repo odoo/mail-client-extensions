1a9e766f24b5bdc384b24214d029ff8817884e28
Logan Staelens <lost@odoo.com>
2024-04-05 11:19:41 +0200

    [IMP] account_reports/saas~17.3: Refactoring Journal reports
    closes odoo/upgrade#5897
    
    See: odoo/enterprise#59778
    Task: 3698843
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/account_reports/saas~17.3.1.0/pre-migrate.py
@@ -0,0 +1,11 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "account_reports.journal_report_pdf_export_filters")
+    util.remove_view(cr, "account_reports.journal_report_pdf_export_main_table_header")
+    util.remove_view(cr, "account_reports.journal_report_pdf_export_main_table_body")
+    util.remove_record(cr, "account_reports.journal_report_date")
+    util.remove_record(cr, "account_reports.journal_report_communication")
+    util.remove_record(cr, "account_reports.journal_report_partner_name")
+    util.remove_record(cr, "account_reports.journal_report_amount_currency")
