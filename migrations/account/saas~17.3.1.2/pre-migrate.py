04649017b1520ba1a37d64c05a182ee8f26be2fd
Ricardo Gomes Rodrigues <rigr@odoo.com>
2024-04-08 09:37:05 +0200

    [IMP] account/saas~17.3.1.2: delete secure_sequence_id
    The field `secure_sequence_id` has been deleted. We can remove it
    from the journal and completely delete the matching ir.sequence rows
    
    Community PR: https://github.com/odoo/odoo/pull/160875
    
    task-id 3820975
    
    Part-of: odoo/upgrade#5900

--- migrations/account/saas~17.3.1.2/pre-migrate.py
+++ migrations/account/saas~17.3.1.2/pre-migrate.py
@@ -3,3 +3,10 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.convert_field_to_translatable(cr, "account.tax.group", "preceding_subtotal")
+    cr.execute("""
+        DELETE
+          FROM ir_sequence s
+         USING account_journal j
+         WHERE s.id = j.secure_sequence_id
+    """)
+    util.remove_field(cr, "account.journal", "secure_sequence_id")
