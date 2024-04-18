932fe7559cd5f3af6fae2166e26b42188db7d93e
Mohammad Abdulmoneim <abdu@odoo.com>
2024-04-18 19:57:23 +0200

    [IMP] stock/saas~17.5: remove wave grouping setting
    In odoo/odoo#157869, batch and wave grouping are merged into one single
    setting. Therefore, the setting specific for wave grouping is no longer
    needed.
    
    Task-3724421
    
    closes odoo/upgrade#5936
    
    Related: odoo/enterprise#60308
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/stock/saas~17.5.1.1/pre-migrate.py
+++ migrations/stock/saas~17.5.1.1/pre-migrate.py
@@ -4,3 +4,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "stock.location", "sequence")
     util.remove_field(cr, "stock.picking", "hide_picking_type")
+    util.remove_field(cr, "res.config.settings", "group_stock_picking_wave")
+    util.remove_record(cr, "stock.group_stock_picking_wave")
