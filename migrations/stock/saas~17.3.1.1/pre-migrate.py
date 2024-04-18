0679e4c88859a8f816962b9567c659294d60cba9
Alvaro Fuentes <afu@odoo.com>
2024-04-18 13:47:01 +0200

    [IMP] stock/saas~17.3: remove non-stored column
    See odoo/odoo@156bed3f430d706e13822bbd95d91c8dfd3ea42d
    
    Part-of: odoo/upgrade#5915

--- migrations/stock/saas~17.3.1.1/pre-migrate.py
+++ migrations/stock/saas~17.3.1.1/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_column(cr, "stock_move_line", "product_category_name")
+    util.remove_column(cr, "stock_warehouse_orderpoint", "qty_to_order")
