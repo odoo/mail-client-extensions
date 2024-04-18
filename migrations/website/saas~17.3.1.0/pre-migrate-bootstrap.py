d773324a59f945ddf4040fff8ea4f48bc40e5883
Pierre Paridans <app@odoo.com>
2024-04-18 13:16:37 +0200

    [IMP] website/saas~17.3: Upgrade Bootstrap to version 5.3
    This commit complement the upgrade from Bootstrap 5.1 to 5.3 by applying
    the following changes:
    - Add the missing `alert` class for `s_alert` snippet.
    - Fix Bootstrap Carousel's `ride` & `interval` data attributes usage.
    
    Related: https://github.com/odoo/odoo/pull/158560
    Related: https://github.com/odoo/enterprise/pull/59122
    
    task-3600468
    
    closes odoo/upgrade#5931
    
    Related: odoo/odoo#158560
    Related: odoo/enterprise#59122
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/website/saas~17.3.1.0/pre-migrate-bootstrap.py
@@ -0,0 +1,25 @@
+from odoo.upgrade.util import snippets
+
+
+def add_alert_class(el):
+    if "alert" in el.classes:
+        return False
+    el.classes |= ["alert"]
+    return True
+
+
+def fix_carousel(el):
+    el.attrib["data-bs-ride"] = "true"
+    el.attrib.pop("data-bs-interval")
+    return True
+
+
+def migrate(cr, version):
+    snippets.convert_html_content(
+        cr,
+        snippets.html_converter(add_alert_class, selector="//*[hasclass('s_alert')]"),
+    )
+    snippets.convert_html_content(
+        cr,
+        snippets.html_converter(fix_carousel, selector="//div[@data-bs-ride='carousel'][@data-bs-interval='0']"),
+    )
