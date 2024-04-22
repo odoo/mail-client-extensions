758f9ccdbcd8222555980ec768a226d2de9eba59
Alvaro Fuentes <afu@odoo.com>
2024-04-22 14:19:41 +0200

    [FIX] helpdesk_timesheet/saas~15.2: remove constraint
    See odoo/enterprise@d2599730aca1a8006ed12b4ab61301ddf3eb3502
    
    Part-of: odoo/upgrade#5915

--- migrations/helpdesk_timesheet/saas~15.2.1.0/pre-migrate.py
+++ migrations/helpdesk_timesheet/saas~15.2.1.0/pre-migrate.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.remove_view(cr, "helpdesk_timesheet.view_helpdesk_ticket_pivot_analysis")
+    util.remove_constraint(cr, "helpdesk_ticket_create_timesheet", "helpdesk_ticket_create_timesheet_time_positive")
