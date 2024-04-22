f55565b6ea384286d5cb3319c4cc7eedf180249b
Alvaro Fuentes <afu@odoo.com>
2024-04-22 14:51:05 +0200

    [FIX] hr_contract_salary/saas~16.1: remove constraint
    See odoo/enterprise@e490d39b65a593dcbfe8bd5e3c5907c74aa7b07d
    
    Part-of: odoo/upgrade#5915

--- migrations/hr_contract_salary/saas~16.1.2.0/pre-migrate.py
+++ migrations/hr_contract_salary/saas~16.1.2.0/pre-migrate.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.remove_field(cr, "generate.simulation.link", "employee_contract_employee_id")
+    util.remove_column(cr, "generate_simulation_link", "employee_id")
