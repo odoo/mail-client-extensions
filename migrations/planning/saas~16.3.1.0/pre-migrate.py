0af11b9c94254c1e6246616a3610e04b13f4a774
Alvaro Fuentes <afu@odoo.com>
2024-04-23 09:28:28 +0200

    [FIX] planning/saas~16.3: remove non-stored column
    The field belongs to planning, we cannot remove the column only in
    sale_planning because the latte may not be installed.
    
    See odoo/enterprise@e94d37792a218a64ef23ad1612a1c9225d3e3e10
    
    Part-of: odoo/upgrade#5915

--- migrations/planning/saas~16.3.1.0/pre-migrate.py
+++ migrations/planning/saas~16.3.1.0/pre-migrate.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
@@ -9,3 +7,6 @@ def migrate(cr, version):
 
     util.remove_view(cr, "planning.slot_planning_select_send_view_form")
     util.remove_record(cr, "planning.model_planning_slot_action_publish")
+
+    if not util.module_installed(cr, "sale_planning"):
+        util.remove_column(cr, "hr_employee", "default_planning_role_id")
