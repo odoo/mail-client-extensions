d9cf6fdf2433ffcf2b9281bd4170d3e4b4abc4c8
Alvaro Fuentes <afu@odoo.com>
2024-04-23 15:00:06 +0200

    [FIX] sale_timesheet/saas~14.4: fix constraint name
    Add table name as prefix.
    
    Part-of: odoo/upgrade#5915

--- migrations/sale_timesheet/saas~14.4.1.0/pre-migrate.py
+++ migrations/sale_timesheet/saas~14.4.1.0/pre-migrate.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
@@ -7,5 +5,5 @@ def migrate(cr, version):
     util.remove_field(cr, "project.create.sale.order", "link_selection")
     util.remove_field(cr, "project.create.sale.order", "pricing_type")
     util.remove_field(cr, "project.create.sale.order.line", "sale_line_id")
-    util.remove_constraint(cr, "project_project", "sale_order_required_if_sale_line")
+    util.remove_constraint(cr, "project_project", "project_project_sale_order_required_if_sale_line")
     util.remove_column(cr, "project_project", "pricing_type")  # This field will be a compute non-stored field
