ddd8037ea80f85d5b3875d42f332461b64c6c020
Alvaro Fuentes <afu@odoo.com>
2024-04-23 13:03:43 +0200

    [FIX] sale_subscription/saas~15.2: fix constraint name
    It must use the table prefix.
    
    Part-of: odoo/upgrade#5915

--- migrations/sale_subscription/saas~15.2.1.1/pre-migrate.py
+++ migrations/sale_subscription/saas~15.2.1.1/pre-migrate.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.remove_constraint(cr, "sale_subscription", "sale_subscription_uuid_uniq")
     util.rename_field(cr, "sale.subscription", "uuid", "access_token")
-    util.remove_constraint(cr, "sale_subscription", "uuid_uniq")
     util.rename_field(cr, "sale.subscription", "website_url", "access_url")
