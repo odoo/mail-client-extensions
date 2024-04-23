b36f4d5dbb56e1b1618625b52a6d5962fdec02e5
Sanchit Gupta <sagu@odoo.com>
2024-04-23 18:01:10 +0530

    [FIX] sale_temporal/saas~16.2: Fix column type of name
    Description:
    In saas-16.2 field ``name`` of ``sale.temporal.recurrence`` translate
    becomes
    [true](https://github.com/odoo/enterprise/commit/5cabcce39702535f6539bf15c65f45a86da55c20)
    so, changing the column type.
    
    closes odoo/upgrade#5964
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/sale_temporal/saas~16.2.1.0/pre-migrate.py
+++ migrations/sale_temporal/saas~16.2.1.0/pre-migrate.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.rename_field(cr, "sale.temporal.recurrence", "subscription_unit_display", "temporal_unit_display")
     util.convert_field_to_html(cr, "sale.order", "internal_note")
+    util.convert_field_to_translatable(cr, "sale.temporal.recurrence", "name")
