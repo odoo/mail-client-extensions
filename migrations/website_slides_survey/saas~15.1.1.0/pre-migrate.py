68be3e913dda80269e4f9b37bf532e46da3c7199
Alvaro Fuentes <afu@odoo.com>
2024-04-23 14:02:26 +0200

    [FIX] website_slides_survey/saas~15.1: fix constraint name
    Prefix with table name.
    
    Part-of: odoo/upgrade#5915

--- migrations/website_slides_survey/saas~15.1.1.0/pre-migrate.py
+++ migrations/website_slides_survey/saas~15.1.1.0/pre-migrate.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.remove_record(cr, "website_slides_survey.survey_survey_action_slides_report")
     util.remove_menus(cr, [util.ref(cr, "website_slides_survey.website_slides_menu_report_certification")])
-    util.remove_constraint(cr, "slide_slide", "check_survey_id")
+    util.remove_constraint(cr, "slide_slide", "slide_slide_check_survey_id")
