3077c10b611f2d4b9321b11d49f9bc2bc9667e1a
Alvaro Fuentes <afu@odoo.com>
2024-04-23 16:29:19 +0200

    [FIX] account_edi_proxy_client/saas~16.5: remove conditionally
    These constraints appeared in saas~16.3.
    
    See odoo/odoo@8590c6a929b49db086b3a1ad64352b7d431e306a
    
    Part-of: odoo/upgrade#5915

--- migrations/account_edi_proxy_client/saas~16.5.1.0/pre-migrate.py
+++ migrations/account_edi_proxy_client/saas~16.5.1.0/pre-migrate.py
@@ -1,9 +1,11 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.remove_constraint(
-        cr, "account_edi_proxy_client_user", "account_edi_proxy_client_user_unique_edi_identification"
-    )
-    util.remove_constraint(cr, "account_edi_proxy_client_user", "account_edi_proxy_client_user_unique_company_proxy")
+    if util.parse_version(version) >= util.parse_version("saas~16.3"):
+        util.remove_constraint(
+            cr, "account_edi_proxy_client_user", "account_edi_proxy_client_user_unique_edi_identification"
+        )
+        util.remove_constraint(
+            cr, "account_edi_proxy_client_user", "account_edi_proxy_client_user_unique_company_proxy"
+        )
