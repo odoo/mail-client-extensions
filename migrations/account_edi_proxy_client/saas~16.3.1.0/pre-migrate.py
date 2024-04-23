78d6d9de2c39079d11cc2425bc729d924442a787
Alvaro Fuentes <afu@odoo.com>
2024-04-23 16:11:57 +0200

    [FIX] account_edi_proxy_client/saas~16.3: no need to remove constraint
    Removing the field `edi_format_id` already drops the constraint according
    to its definition:
    ```
    Indexes:
        "account_edi_proxy_client_user_pkey" PRIMARY KEY, btree (id)
        "account_edi_proxy_client_user_unique_edi_identification_per_for" UNIQUE CONSTRAINT, btree (edi_identification, edi_format_id)
        "account_edi_proxy_client_user_unique_id_client" UNIQUE CONSTRAINT, btree (id_client)
    ```
    
    Part-of: odoo/upgrade#5915

--- migrations/account_edi_proxy_client/saas~16.3.1.0/pre-migrate.py
+++ migrations/account_edi_proxy_client/saas~16.3.1.0/pre-migrate.py
@@ -1,13 +1,9 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.remove_field(cr, "account_edi_proxy_client.user", "edi_format_code")
     util.remove_field(cr, "account_edi_proxy_client.user", "edi_format_id")
-    util.remove_constraint(
-        cr, "account_edi_proxy_client_user", "account_edi_proxy_client_user_unique_edi_identification_per_for"
-    )
 
     util.create_column(cr, "account_edi_proxy_client_user", "proxy_type", "varchar")
     cr.execute("DELETE FROM ir_config_parameter WHERE key='account_edi_proxy_client.demo' RETURNING value")
