04d659fa9f41331c7daeedfcbc7e67f2766c2ff4
Alvaro Fuentes <afu@odoo.com>
2024-04-23 14:39:12 +0200

    [FIX] l10n_rs/16.0: fix constraint name
    Add table name as prefix.
    
    Part-of: odoo/upgrade#5915

--- migrations/l10n_rs/16.0.1.0/pre-migrate.py
+++ migrations/l10n_rs/16.0.1.0/pre-migrate.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
@@ -9,7 +8,7 @@ def migrate(cr, version):
         WHERE l10n_rs_company_registry IS NOT NULL
     """
     util.parallel_execute(cr, util.explode_query_range(cr, query, table="res_partner"))
-    util.remove_constraint(cr, "res_partner", "company_registry_country_uniq")
+    util.remove_constraint(cr, "res_partner", "res_partner_company_registry_country_uniq")
     util.remove_field(cr, "account.move", "l10n_rs_company_registry")
     util.remove_field(cr, "res.partner", "l10n_rs_company_registry")
     util.remove_view(cr, "l10n_rs.view_partner_form_inherit")
