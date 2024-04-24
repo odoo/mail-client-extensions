ac9c2d61b48f4f99e9ca83fd54ad4031daef75f8
Alvaro Fuentes <afu@odoo.com>
2024-04-24 13:46:45 +0200

    [FIX] account/saas~13.4: remove non-stored column
    Bank statements now inherit via move_id. Those fields come from the
    linked move now.
    
    See odoo/odoo@caeb782841fc5a7ad71a196e2c9ee67644ef9074
    
    Part-of: odoo/upgrade#5915

--- migrations/account/saas~13.4.1.1/end-10-payment-refactoring.py
+++ migrations/account/saas~13.4.1.1/end-10-payment-refactoring.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+from odoo.upgrade import util
 
 
 def migrate(cr, version):
@@ -10,3 +10,9 @@ def migrate(cr, version):
     cr.execute("DROP TABLE account_payment_pre_backup CASCADE")
     cr.execute("DROP TABLE account_bank_statement_line_pre_backup CASCADE")
     cr.execute("DROP TABLE account_journal_backup CASCADE")
+
+    # these fields come now from the parent account move
+    util.remove_column(cr, "account_bank_statement_line", "date")
+    util.remove_column(cr, "account_bank_statement_line", "journal_id")
+    util.remove_column(cr, "account_bank_statement_line", "ref")
+    util.remove_column(cr, "account_bank_statement_line", "company_id")
