f7c440acdbad9d0e40dda47652e0db0144f8ecd7
Alvaro Fuentes <afu@odoo.com>
2024-04-25 15:19:03 +0200

    [FIX] inter_company_rules/saas~12.5: remove non-stored columns
    See odoo/enterprise@46f1c95ac37dd196ef58cb1586176942669dcdd2
    
    Part-of: odoo/upgrade#5915

--- migrations/inter_company_rules/saas~12.5.1.1/pre-migrate.py
+++ migrations/inter_company_rules/saas~12.5.1.1/pre-migrate.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from odoo.addons.base.maintenance.migrations import util
 
 
@@ -8,3 +7,8 @@ def migrate(cr, version):
     cr.execute("UPDATE res_company SET auto_validation=TRUE WHERE auto_validation_old='validated'")
     util.remove_field(cr, "res.company", "auto_validation_old")
     util.remove_field(cr, "res.config.settings", "auto_validation")
+
+    util.remove_column(cr, "res_config_settings", "rule_type")
+    util.remove_column(cr, "res_config_settings", "applicable_on")
+    util.remove_column(cr, "res_config_settings", "rules_company_id")
+    util.remove_column(cr, "res_config_settings", "warehouse_id")
