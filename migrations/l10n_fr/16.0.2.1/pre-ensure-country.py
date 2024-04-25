5ef681c9c44fcc0eb50a1da9856a8433069ea0db
Christophe Simonis <chs@odoo.com>
2024-04-25 13:48:11 +0200

    [FIX] l10n_fr/16.0: handle upgrades from 16.0.2.0
    upg-1519435
    
    closes odoo/upgrade#5948
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>
    Co-authored-by: Eliott Loop <oplo@odoo.com>

--- migrations/l10n_fr/16.0.2.1/pre-ensure-country.py
+++ migrations/l10n_fr/16.0.2.1/pre-ensure-country.py
@@ -2,6 +2,14 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    if not util.table_exists(cr, "account_tax_template"):
+        # this script is also run for 16.0 database that have this module installed before the version bump[^1].
+        # However, the `account.tax.template model is removed by the saas~16.2 account script[^2] that is run before this script.
+        #
+        # [^1]: https://github.com/odoo/odoo/commit/55a487ff2be8b1d976d07ea50f2332a82cb73f9c
+        # [^2]: https://github.com/odoo/upgrade/blob/ac0b7445cbafa1e3c149aee40ce12fdddff91535/migrations/account/saas~16.2.1.2/pre-migrate.py#L62
+        return
+
     fr = util.ref(cr, "base.fr")
 
     cr.execute(
