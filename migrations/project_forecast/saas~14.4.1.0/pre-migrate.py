d0abd598bee752a1f51620f7260550ce957fb92b
Alvaro Fuentes <afu@odoo.com>
2024-04-26 15:12:13 +0200

    [FIX] project_forecast/saas~14.4: remove non-stored column
    See odoo/enterprise@85c6e2e0064ac5f132c9429008073966ac916bd2
    
    Part-of: odoo/upgrade#5915

--- migrations/project_forecast/saas~14.4.1.0/pre-migrate.py
+++ migrations/project_forecast/saas~14.4.1.0/pre-migrate.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
@@ -9,3 +8,7 @@ def migrate(cr, version):
     util.remove_view(cr, "project_forecast.res_config_settings_view_form")
     util.remove_field(cr, "res.config.settings", "group_project_forecast_display_allocate_time")
     util.remove_record(cr, "project_forecast.group_project_forecast_display_allocate_time")
+
+    # these fields are now company_dependent=True
+    util.remove_column(cr, "planning_slot_template", "project_id")
+    util.remove_column(cr, "planning_slot_template", "task_id")
