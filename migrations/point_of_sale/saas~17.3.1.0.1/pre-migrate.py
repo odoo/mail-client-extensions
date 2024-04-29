728df0735e311058afb9a3874552f8167856e733
Nesma Harraz <neha@odoo.com>
2024-04-29 12:29:29 +0300

    [IMP] point_of_sale/saas~17.3: remove cash_register_total_entry_encoding
    removed cash_register_total_entry_encoding for not being used
    
    Community: https://github.com/odoo/odoo/pull/154563
    
    Task ID: 3734448
    
    closes odoo/upgrade#5958
    
    Related: odoo/odoo#154563
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/point_of_sale/saas~17.3.1.0.1/pre-migrate.py
+++ migrations/point_of_sale/saas~17.3.1.0.1/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.rename_field(cr, "pos.category", "child_id", "child_ids")
+    util.remove_field(cr, "pos.session", "cash_register_total_entry_encoding")
