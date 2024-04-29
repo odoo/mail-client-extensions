9e5c20a38f43e198299061b64a77a8494604d2f9
Arthur Pierrot (arpi) <arpi@odoo.com>
2024-04-29 13:22:27 +0200

    [IMP] l10n_be_hr_payroll/saas~17.4: move fiscal voluntarism
    This will move the fiscal voluntarism rate from the contract to the employee.
    
    Task: 3877405
    
    See odoo/enterprise#61312
    
    closes odoo/upgrade#5959
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/l10n_be_hr_payroll/saas~17.4.1.0/pre-migrate.py
+++ migrations/l10n_be_hr_payroll/saas~17.4.1.0/pre-migrate.py
@@ -2,4 +2,19 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.create_column(cr, "hr_employee", "fiscal_voluntary_rate", "float8")
+    cr.execute("""
+    UPDATE hr_employee e
+       SET fiscal_voluntary_rate = c.fiscal_voluntary_rate
+      FROM hr_contract c
+     WHERE c.id = e.contract_id
+       AND c.fiscal_voluntarism = true
+    """)
+
+    util.remove_field(cr, "hr.contract", "fiscal_voluntarism")
+    util.remove_field(cr, "hr.contract", "fiscal_voluntary_rate")
+
+    util.remove_field(cr, "hr.contract.history", "fiscal_voluntarism")
+    util.remove_field(cr, "hr.contract.history", "fiscal_voluntary_rate")
+
     util.remove_field(cr, "l10n.be.double.pay.recovery.wizard", "classic_holiday_pay")
