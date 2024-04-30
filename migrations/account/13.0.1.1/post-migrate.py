6661de97ff519ec5908b74173b9eff8f8fe80564
Alvaro Fuentes <afu@odoo.com>
2024-04-30 15:32:29 +0200

    Revert "[FIX] account/13.0: ensure account type gets internal group"
    This reverts commit 40e39822347c4fd5bec87bd02aa4dc5ed91f9d63.
    
    It triggers recomputes that we don't need/want.
    
    Moreover it was already ensured by
    https://github.com/odoo/upgrade/blob/9bde4ded4b34ed0176d5c0edec3fa84a4ec3836e/migrations/account/saas~12.5.1.1/post-models.py#L18-L41
    
    closes odoo/upgrade#5965
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/account/13.0.1.1/post-migrate.py
+++ migrations/account/13.0.1.1/post-migrate.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from odoo.addons.base.maintenance.migrations import util
 
 
@@ -13,5 +14,3 @@ def migrate(cr, version):
     for journal_id, alias_name in cr.fetchall():
         # We want to update the alias but leave the name intact
         Journal.browse(journal_id)._update_mail_alias({"alias_name": alias_name})
-
-    util.if_unchanged(cr, "account.data_account_type_current_liabilities", util.update_record_from_xml)
