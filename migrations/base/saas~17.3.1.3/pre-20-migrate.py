a18244d7d4ec5ee7e10a0db0b00f2af7b310cdd2
Julien Castiaux <juc@odoo.com>
2024-04-30 15:42:17 +0200

    [FIX] base/saas~17.3: cron interval_{type,number} made required
    closes odoo/upgrade#5966
    
    Community: odoo/odoo#134763
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/saas~17.3.1.3/pre-20-migrate.py
+++ migrations/base/saas~17.3.1.3/pre-20-migrate.py
@@ -3,3 +3,31 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_model(cr, "res.config.installer")
+
+    # this cron doesn't support interval_type='months'
+    cron_id = util.ref(cr, "crm.ir_cron_crm_lead_assign")
+    if cron_id:
+        cr.execute(
+            """
+            UPDATE ir_cron
+               SET active = false,
+                   interval_type = 'weeks'
+             WHERE interval_type IS NULL
+               AND id = %s
+            """,
+            [cron_id],
+        )
+
+    cr.execute("""
+        UPDATE ir_cron
+           SET active = false,
+               interval_type = 'months'
+         WHERE interval_type IS NULL
+    """)
+
+    cr.execute("""
+        UPDATE ir_cron
+           SET active = false,
+               interval_number = 1
+         WHERE interval_number is NULL
+    """)
