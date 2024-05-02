b465d80de44a33de47a575a282dae115f1d5844b
Yash Vaishnav <yava@odoo.com>
2024-05-02 17:34:53 +0530

    [IMP] hr/saas~17.5: update request_hour_(from, to) in hr.leave
    The request_hour_(from, to) fields in hr.leave has been changed to
    the Float field
    
    This migration script updates the records containing in the hr_leave
    table to update the value of request_hour_(from, to) from varchar to real.
    
    Related Com PR: https://github.com/odoo/odoo/pull/164290
    Related Ent PR: https://github.com/odoo/enterprise/pull/61919
    Task-3888748
    
    closes odoo/upgrade#5972
    
    Related: odoo/odoo#164290
    Related: odoo/enterprise#61919
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr_holidays/saas~17.5.1.6/pre-migrate.py
+++ migrations/hr_holidays/saas~17.5.1.6/pre-migrate.py
@@ -3,3 +3,6 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.change_field_selection_values(cr, "hr.leave", "state", {"draft": "cancel"})
+
+    util.alter_column_type(cr, "hr_leave", "request_hour_from", "float8", using="{}::double precision")
+    util.alter_column_type(cr, "hr_leave", "request_hour_to", "float8", using="{}::double precision")
