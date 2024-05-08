be3a0e7b000502f7677fcb469d0d2f259690e59d
Julien Alardot (jual) <jual@odoo.com>
2024-05-08 17:06:42 +0200

    [IMP] {account,payment}_sepa_direct_debit/saas~17.5: SDD mandatalypse
    In the account_sepa_direct_debit UX overhaul,
    the `sdd.mandate` saw a few field changes.
    A mail template was also taken from payment_sepa_direct_debit,
    but as it was rewritten, we delete the old one instead
    
    closes odoo/upgrade#6493
    
    Related: odoo/enterprise#69867
    Task-id: 3722517
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/account_sepa_direct_debit/saas~17.5.1.0/pre-migrate.py
@@ -0,0 +1,25 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, model="sdd.mandate", fieldname="payment_journal_id")
+    util.remove_field(cr, model="sdd.mandate", fieldname="suitable_journal_ids")
+    util.create_column(cr, table="sdd_mandate", column="is_sent", definition="boolean")
+    util.create_column(cr, table="account_payment", column="sdd_mandate_id", definition="int4")
+
+    util.explode_execute(
+        cr,
+        query="UPDATE sdd_mandate SET is_sent = true WHERE state = 'active'",
+        table="sdd_mandate",
+    )
+    util.explode_execute(
+        cr,
+        query="""
+            UPDATE account_payment
+               SET sdd_mandate_id = account_move.sdd_mandate_id
+              FROM account_move
+             WHERE account_move.id = account_payment.move_id
+               AND account_move.sdd_mandate_id IS NOT NULL
+        """,
+        table="account_payment",
+    )
---
+++ migrations/payment_sepa_direct_debit/saas~17.5.2.0/pre-migrate.py
@@ -0,0 +1,5 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_record(cr, "payment_sepa_direct_debit.mail_template_sepa_notify_debit")
