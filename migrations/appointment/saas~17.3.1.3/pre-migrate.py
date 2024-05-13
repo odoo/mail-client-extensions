027db358bd4a3c973fb978c997bf96bc136978b4
aath-odoo <aath@odoo.com>
2024-05-13 11:26:22 +0530

    [IMP] website_appointment/saas~17.3: Remove appointment_progress_bar
    appointment_progress_bar has been moved from website_appointment to appointment
    
    closes odoo/upgrade#6004
    
    Related: https://github.com/odoo/enterprise/pull/62346
    Related: odoo/enterprise#62346
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/appointment/saas~17.3.1.3/pre-migrate.py
+++ migrations/appointment/saas~17.3.1.3/pre-migrate.py
@@ -4,3 +4,6 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "appointment.type", "user_assign_method")
     util.remove_field(cr, "appointment.manage.leaves", "calendar_id")
+
+    if util.module_installed(cr, "website_appointment"):
+        util.rename_xmlid(cr, "website_appointment.appointment_progress_bar", "appointment.appointment_progress_bar")
---
+++ migrations/appointment_account_payment/saas~17.3.1.0/pre-migrate.py
@@ -0,0 +1,10 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    if util.module_installed(cr, "website_appointment_account_payment"):
+        util.rename_xmlid(
+            cr,
+            "website_appointment_account_payment.appointment_progress_bar",
+            "appointment_account_payment.appointment_progress_bar",
+        )
