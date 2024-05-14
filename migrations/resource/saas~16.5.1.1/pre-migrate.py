2e03192203e0b1636cb36de87784cccf9fdd2968
Elias Regopoulos <elre@odoo.com>
2024-05-14 16:57:36 +0000

    [FIX] resource/saas~16.5: Explicitly mention possibly ambiguous table
    The field `hour_to` exists only in resource.calendar.attendance in standard
    Odoo, but custom modules can introduce a field of the same name to
    resource.calendar, making that field in any query that refers to both tables
    ambiguous.
    ```
    > SELECT imf.id,imf.name,imf.model,imd.module FROM ir_model_data imd JOIN ir_model_fields imf ON imd.model='ir.model.fields' AND imf.id=imd.res_id WHERE imf.name='hour_to'
    +-------+---------+------------------------------+---------------------------------+
    | id    | name    | model                        | module                          |
    |-------+---------+------------------------------+---------------------------------|
    | 12810 | hour_to | resource.calendar            | odt_hr_attendance_with_analytic |
    | 1670  | hour_to | resource.calendar.attendance | resource                        |
    +-------+---------+------------------------------+---------------------------------+
    ```
    
    Referring to the tables explicitly solves this issue.
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/17.0/odoo/service/server.py", line 1302, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "<decorator-gen-16>", line 2, in new
      File "/home/odoo/src/odoo/17.0/odoo/tools/func.py", line 87, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/17.0/odoo/modules/registry.py", line 113, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/17.0/odoo/modules/loading.py", line 476, in load_modules
        processed_modules += load_marked_modules(env, graph,
      File "/home/odoo/src/odoo/17.0/odoo/modules/loading.py", line 364, in load_marked_modules
        loaded, processed = load_module_graph(
      File "/home/odoo/src/odoo/17.0/odoo/modules/loading.py", line 181, in load_module_graph
        migrations.migrate_module(package, 'pre')
      File "/home/odoo/src/odoo/17.0/odoo/modules/migration.py", line 240, in migrate_module
        migrate(self.cr, installed_version)
      File "/tmp/tmps6n6f3ua/migrations/resource/saas~16.5.1.1/pre-migrate.py", line 16, in migrate
        util.parallel_execute(cr, util.explode_query_range(cr, query, table="resource_calendar_attendance"))
      File "/tmp/tmps6n6f3ua/migrations/util/pg.py", line 175, in parallel_execute
        return parallel_execute_impl(cr, queries, logger=_logger)
      File "/tmp/tmps6n6f3ua/migrations/util/pg.py", line 92, in _parallel_execute_threaded
        cr.execute(queries[0])
      File "/home/odoo/src/odoo/17.0/odoo/sql_db.py", line 332, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.AmbiguousColumn: column reference "hour_to" is ambiguous
    LINE 5: ...                  WHEN day_period != 'lunch' AND (hour_to - ...
                                                                 ^
    ```
    
    upg-1518317
    upg-1554196
    
    closes odoo/upgrade#6023
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/resource/saas~16.5.1.1/pre-migrate.py
+++ migrations/resource/saas~16.5.1.1/pre-migrate.py
@@ -4,13 +4,13 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.create_column(cr, "resource_calendar_attendance", "duration_days", "float8")
     query = """
-            UPDATE resource_calendar_attendance
+            UPDATE resource_calendar_attendance rca
                SET duration_days = CASE
-                                       WHEN day_period = 'lunch' THEN 0
-                                       WHEN day_period != 'lunch' AND (hour_to - hour_from) <= rc.hours_per_day * 3 / 4 THEN 0.5
+                                       WHEN rca.day_period = 'lunch' THEN 0
+                                       WHEN rca.day_period != 'lunch' AND (rca.hour_to - rca.hour_from) <= rc.hours_per_day * 3 / 4 THEN 0.5
                                        ELSE 1
                                    END
               FROM resource_calendar rc
-             WHERE resource_calendar_attendance.calendar_id = rc.id
+             WHERE rca.calendar_id = rc.id
             """
-    util.parallel_execute(cr, util.explode_query_range(cr, query, table="resource_calendar_attendance"))
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="resource_calendar_attendance", alias="rca"))
