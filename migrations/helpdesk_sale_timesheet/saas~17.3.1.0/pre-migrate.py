33ca3eefff70672480a1ecbb2bbdea98b022de01
kcv-odoo <kcv@odoo.com>
2024-05-14 12:22:11 +0530

    [IMP] helpdesk_sale_timesheet/saas~17.3: remove sla field and views
    Remove sla field from product and move those data in sla's
    product_ids field
    
    Related PR: https://github.com/odoo/enterprise/pull/62741
    
    task-3938198
    
    closes odoo/upgrade#6041
    
    Related: odoo/odoo#166341
    Related: odoo/enterprise#62741
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/helpdesk_sale_timesheet/saas~17.3.1.0/pre-migrate.py
@@ -0,0 +1,23 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # Link the products to slas.
+    util.create_m2m(cr, "helpdesk_sla_product_template_rel", "product_template", "helpdesk_sla")
+    cr.execute(
+        """
+        INSERT INTO helpdesk_sla_product_template_rel(product_template_id, helpdesk_sla_id)
+             SELECT DISTINCT SPLIT_PART(ip.res_id, ',', 2)::int AS product_template_id,
+                    SPLIT_PART(ip.value_reference, ',', 2)::int AS helpdesk_sla_id
+               FROM ir_property ip
+               JOIN product_template pt
+                 ON (SPLIT_PART(ip.res_id, ',', 2)::integer) = pt.id
+              WHERE ip.name = 'sla_id'
+                AND ip.value_reference LIKE 'helpdesk.sla,%'
+                AND ip.res_id LIKE 'product.template,%'
+        """
+    )
+
+    util.remove_field(cr, "helpdesk.sla", "sale_line_ids")
+    util.remove_field(cr, "product.template", "sla_id")
+    util.remove_view(cr, "helpdesk_sale_timesheet.product_template_form_view_invoice_policy_inherit_helpdesk")
