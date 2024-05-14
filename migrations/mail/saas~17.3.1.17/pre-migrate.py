1f25544569e01ab961bcc546592a74401c45e2a0
tsm-odoo <tsm@odoo.com>
2024-05-14 16:06:24 +0200

    [FIX] mail/saas~17.3: fix incorrect new message separator value
    In [1], the `new_message_separator` field was introduced to keep track
    of the user unread messages. The first unread message is the one after
    the last one the user saw (`seen_message_id` field).
    
    The upgrade script sets the value of this new field to the value of the
    `seen_message_id` while it should actually be `seen_message_id + 1`.
    
    This PR corrects this mistake.
    
    Steps to reproduce it:
    - create a db with mail installed
    - check the value of the `seen_message_id` field on the
    `discuss_channel_member` table
    - proceed to the upgrade to master
    - the `new_message_separator` field on the `discuss_channel_member`
    table was set to the value of `seen_message_id` while it should have
    been `seen_message_id + 1`.
    
    [1]: https://github.com/odoo/upgrade/pull/5994
    
    closes odoo/upgrade#6019
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/mail/saas~17.3.1.17/pre-migrate.py
+++ migrations/mail/saas~17.3.1.17/pre-migrate.py
@@ -5,7 +5,7 @@ def migrate(cr, version):
     util.create_column(cr, "discuss_channel_member", "new_message_separator", "integer", default=0)
     query = """
         UPDATE discuss_channel_member
-           SET new_message_separator = seen_message_id
+           SET new_message_separator = seen_message_id + 1
          WHERE seen_message_id IS NOT NULL
 
     """
