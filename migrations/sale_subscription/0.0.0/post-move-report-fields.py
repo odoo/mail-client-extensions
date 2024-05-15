43cc6978865efe754274bd1670bc481044135833
Jinane Maksoud <maji@odoo.com>
2024-05-15 10:19:07 +0200

    [FIX] sale_subscription/0.0.0: rename xmlids
    In version saas-16.2 the sale subscription report started to inherit
    sale.report which came possible with fields 'website_id' and
    'is_abandoned_cart' based on whether 'website_sale' or
    'website_sale_dashboard' were also installed.
    In version saas-16.3 both fields became part of 'website_sale'.
    The order of installation of modules dictates the module on the xmlid
    for inherited fields on the subscription report, so they can have
    the 'sale_subscription' as module which is not correct, and leads to
    issues when the website_sale module is uninstalled, because those
    inherited fields are not removed, causing failures in upgrades.
    
    closes odoo/upgrade#5085
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/sale_subscription/0.0.0/post-move-report-fields.py
@@ -0,0 +1,13 @@
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    if not util.version_between("saas~16.3", "18.0"):
+        return
+
+    for field in ["is_abandoned_cart", "website_id"]:
+        if util.ref(cr, "sale_subscription.field_sale_subscription_report__{}".format(field)):
+            if util.module_installed(cr, "website_sale"):
+                util.move_field_to_module(cr, "sale.subscription.report", field, "sale_subscription", "website_sale")
+            else:
+                util.remove_field(cr, "sale.subscription.report", field)
