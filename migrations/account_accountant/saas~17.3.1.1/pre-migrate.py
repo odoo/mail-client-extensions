d0fa2ce79b78a96e81fdf250a946a5b2c7b28c1e
Ricardo Gomes Rodrigues <rigr@odoo.com>
2024-05-17 15:18:07 +0200

    [FIX] account_accountant/saas~17.3.1.2 split deferred rev/exp
    Allow having different Deferred Journal and Computation Method
    for Deferred Expense/Revenue
    
    Entreprise PR: https://github.com/odoo/enterprise/pull/62620
    
    closes odoo/upgrade#6033
    
    Task-id: 3931137
    Related: odoo/enterprise#62620
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account_accountant/saas~17.3.1.1/pre-migrate.py
+++ migrations/account_accountant/saas~17.3.1.1/pre-migrate.py
@@ -3,3 +3,25 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "bank.rec.widget", "st_line_narration")
+
+    # Deferred
+    for deferred_type in ("expense", "revenue"):
+        method_col = f"deferred_{deferred_type}_amount_computation_method"
+        util.create_column(cr, "res_company", method_col, "varchar")
+        journal_col = f"deferred_{deferred_type}_journal_id"
+        util.create_column(cr, "res_company", journal_col, "int4")
+        query = util.format_query(
+            cr,
+            """
+            UPDATE res_company
+               SET {} = deferred_amount_computation_method,
+                   {} = deferred_journal_id
+            """,
+            method_col,
+            journal_col,
+        )
+        cr.execute(query)
+    util.remove_field(cr, "res.company", "deferred_amount_computation_method")
+    util.remove_field(cr, "res.company", "deferred_journal_id")
+    util.remove_field(cr, "res.config.settings", "deferred_amount_computation_method")
+    util.remove_field(cr, "res.config.settings", "deferred_journal_id")
