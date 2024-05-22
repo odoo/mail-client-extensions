c6a71b852087c665d74379227350ac0a7fae5ba3
Agustin Castro Bugallo <agcb@odoo.com>
2024-05-22 15:23:45 +0200

    [FIX] l10n_ar/saas~17.3: Remove field from res.partner
    The field `l10n_ar_special_purchase_document_type_ids` has been removed
    in `saas~17.3` by https://github.com/odoo/odoo/pull/162610.
    
    Found in https://upgradeci.odoo.com/upgradeci/run/136449
    
    ```
    2024-05-22 10:38:58,899 11984 CRITICAL matt-l10n_ar odoo.upgrade.util: 💥 It looks like you forgot to call `util.remove_field` on the following fields: res.partner.l10n_ar_special_purchase_document_type_ids
    ```
    
    closes odoo/upgrade#6054
    
    Signed-off-by: Agustín Castro Bugallo (agcb) <agcb@odoo.com>

--- migrations/l10n_ar/saas~17.3.1.2/pre-migrate.py
+++ migrations/l10n_ar/saas~17.3.1.2/pre-migrate.py
@@ -5,3 +5,5 @@ def migrate(cr, version):
     # A new computed stored field invoice_currency_rate was added to model 'account.move'.
     # This makes the l10n_ar_currency_rate field from this module obsolete
     util.remove_field(cr, "account.move", "l10n_ar_currency_rate")
+
+    util.remove_field(cr, "res.partner", "l10n_ar_special_purchase_document_type_ids")
