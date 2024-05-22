121663a1cbbe6f81ab85360e597c27a6c4cf00ba
Christophe Simonis <chs@odoo.com>
2024-05-22 17:49:28 +0200

    [FIX] hr_holidays/saas~16.5: only recompute `number_of_hours`
    As user may change the value of `number_of_days`, we should not
    recompute it. We however use the existing value to compute a ratio for a
    more accurate `number_of_hours`.
    
    opw-3898342
    opw-3856608
    
    closes odoo/upgrade#6057
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr_holidays/saas~16.5.1.5/end-migrate.py
+++ migrations/hr_holidays/saas~16.5.1.5/end-migrate.py
@@ -1,6 +1,20 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.recompute_fields(cr, "hr.leave", ["number_of_hours"])
+    # recompute `number_of_hours` without recomputing `number_of_days`, that can have been changed by the user.
+    cr.execute("""
+       SELECT id
+         FROM hr_leave
+        WHERE date_from IS NOT NULL
+          AND date_to IS NOT NULL
+          AND resource_calendar_id IS NOT NULL
+    """)
+    ids = [r[0] for r in cr.fetchall()]
+    Leave = util.env(cr)["hr.leave"].with_context(mail_notrack=True)
+    for leave in util.iter_browse(Leave, ids):
+        days, hours = leave._get_duration(check_leave_type=False)
+
+        # compute the ratio between the chosen and the guessed number of days
+        ratio = leave.number_of_days / days if days else 1
+        leave.number_of_hours = round(hours * ratio)
