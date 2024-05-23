7a3ca9647cf5954dea51df0c08aeb3b709d61750
pja-odoo <pja@odoo.com>
2024-05-23 12:41:20 +0000

    [FIX] account_asset_ndt/15.0.1.0: Unlink wrong Journal Items
    Unlink the wrong move line linked with the asset
    before re-computing the asset field ‘non_deductible_tax_val’
    because during the upgrade when it’s recomputing the asset
    field ‘non_deductible_tax_val’ at that time constraint is violated
    
    "You cannot create an asset from lines containing credit
    and debit on the account or with a null amount"
    
    This constraint was introduced in version saas-12-5:
    https://github.com/odoo-dev/enterprise/commit/6e0d855af162aacfbe80df9ef199d0c12d029154
    but the trigger the recompute, was introduced in version 15.0
    https://github.com/odoo/enterprise/pull/23761/files#diff-f997fd2b42170395940ad5abbd161d6d888fdcec3a402008de9c028023fd6d9eR162
    The problem is that we are recomputing a field here
    that will also trigger the check.
    Those assets are already wrong since long ago.
    This patch is unlink the wrong move line linked with the asset since long ago.
    
    upg - 1577976
    [TGB](https://upgrade.odoo.com/web#id=696&cids=1&menu_id=107&action=178&model=upgrade.request.traceback.group&view_type=form)
    
    closes odoo/upgrade#6060
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/account_asset_ndt/15.0.1.0/post-recompute.py
+++ migrations/account_asset_ndt/15.0.1.0/post-recompute.py
@@ -1,7 +1,13 @@
-# -*- coding: utf-8 -*-
 import contextlib
+import logging
+import os
+from textwrap import dedent
 
 from odoo.upgrade import util
+
+_logger = logging.getLogger(__name__)
+
+ODOO_UPG_UNLINK_CREDIT_LINES_FROM_ASSETS = util.str2bool(os.getenv("ODOO_UPG_UNLINK_CREDIT_LINES_FROM_ASSETS", "0"))
 
 
 @contextlib.contextmanager
@@ -42,5 +48,86 @@ def post_moves(cr):
 
 
 def migrate(cr, version):
+    cr.execute(
+        """
+        SELECT asset.id AS asset_id,
+               asset.name AS asset_name,
+               array_agg(ml.id ORDER BY ml.id) FILTER (WHERE ml.credit != 0) AS credit_line_ids,
+               array_agg(ml.name ORDER BY ml.id) FILTER (WHERE ml.credit != 0) AS credit_line_names
+          FROM account_asset AS asset
+          JOIN asset_move_line_rel AS mlr
+            ON mlr.asset_id = asset.id
+          JOIN account_move_line AS ml
+            ON ml.id = mlr.line_id
+      GROUP BY asset.id
+        HAVING (sum(ml.credit) != 0 AND sum(ml.debit) != 0)
+            OR sum(ml.credit + ml.debit) = 0
+         """
+    )
+    asset_move_line_ids = cr.fetchall()
+    credit_line_ids = []
+    for res in asset_move_line_ids:
+        credit_line_ids.extend(res[2])
+    if credit_line_ids:
+        if ODOO_UPG_UNLINK_CREDIT_LINES_FROM_ASSETS:
+            cr.execute(
+                "DELETE FROM asset_move_line_rel rel WHERE line_id IN %s",
+                [tuple(credit_line_ids)],
+            )
+            util.add_to_migration_reports(
+                """
+                  <details>
+                      <summary>
+                          Some assets have linked journal items with positive credit.
+                          Standard assets cannot be linked to journal items containing
+                          credit and debit on the account or with a null amount.
+                          The journal entries have been automatically unlinked from the assets.
+                          Here is the list of affected records.
+                      </summary>
+                      <ul>{}</ul>
+                      <p>
+                      This automatic fix was triggered by the environment variable
+                      ODOO_UPG_UNLINK_CREDIT_LINES_FROM_ASSETS=1
+                      <p>
+                  </details>
+                """.format(
+                    " ".join(
+                        "<li>The asset {} was unlinked from the journal item {}</li>".format(
+                            util.get_anchor_link_to_record("account.asset", asset_id, asset_name),
+                            ", ".join(
+                                util.get_anchor_link_to_record("account.move.line", line_id, line_name)
+                                for line_id, line_name in zip(line_ids, line_names)
+                            ),
+                        )
+                        for asset_id, asset_name, line_ids, line_names in asset_move_line_ids
+                    )
+                ),
+                category="Assets",
+                format="html",
+            )
+        else:
+            msg = dedent("""
+            Some assets have linked journal items with positive credit.
+            Standard assets cannot be linked to journal items containing
+            credit and debit on the account or with a null amount.
+            In order to avoid the issue set the environment variable
+            * ODOO_UPG_UNLINK_CREDIT_LINES_FROM_ASSETS to 1
+            When this variable is set the journal entries will be unlinked
+            from the assets, without removing any record.
+            Here is the list of affected records:
+            * {}
+            """).format(
+                "\n * ".join(
+                    "The asset (id={}, name=`{}`) is linked to the journal item (id={}, name=`{}`).".format(
+                        asset_id,
+                        asset_name,
+                        line_id,
+                        line_name,
+                    )
+                    for asset_id, asset_name, line_id, line_name in asset_move_line_ids
+                ),
+            )
+            _logger.warning("\n%s\n", msg)
+
     with post_moves(cr):
         util.recompute_fields(cr, "account.asset", ["non_deductible_tax_val"])
