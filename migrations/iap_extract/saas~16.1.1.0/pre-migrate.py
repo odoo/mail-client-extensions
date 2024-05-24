c80303a72cc28b603c37dfdc8d8405b2d632e53e
rudo-odoo <rudo@odoo.com>
2024-05-24 08:55:37 +0000

    [FIX] iap_extract/saas~16.1: Fix reset mail template
    - Updated the template_fs path in mail_template to the correct XML file.
    - Changed from 'account_invoice_extract/data/mail_template_data.xml'
      to 'iap_extract/data/mail_template_data.xml'.
    - Ensured the correct template ID is referenced to resolve the reset issue.
    
    Steps to reproduce:
    1. Go to the email template.
    2. Search for IAP Extract Notification.
    3. Click on the Reset Template button; an error will appear.
    
    Issue:
    The reset template was failing after migration due to the
    outdated XML file reference which was not updated during the
    migration process, leading to a `FileNotFoundError`.
    
    Cause:
    The `template_fs` path was not updated during migration,
    causing the reset template functionality to fail.
    
    Fix:
    Replaced the outdated XML file reference with the new XML file
    to ensure the reset functionality works as expected.
    
     Traceback
     ```
      File "/home/odoo/src/odoo/17.0/addons/mail/wizard/mail_template_reset.py",
      line 16, in reset_template
        self.template_ids.reset_template()
      File "/home/odoo/src/odoo/17.0/addons/mail/models/template_reset_mixin.py",
      line 81, in reset_template
        fullpath = file_path(template.template_fs)
      File "/home/odoo/src/odoo/17.0/odoo/tools/misc.py", line 189, in file_path
        raise FileNotFoundError("File not found: " + file_path)
     FileNotFoundError:
        File not found: account_invoice_extract/data/mail_template_data.xml
     ```
    
    closes odoo/upgrade#6063
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/iap_extract/saas~16.1.1.0/pre-migrate.py
+++ migrations/iap_extract/saas~16.1.1.0/pre-migrate.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from odoo.upgrade import util
 
 
@@ -9,4 +7,14 @@ def migrate(cr, version):
         "account_invoice_extract.account_invoice_extract_no_credit",
         "iap_extract.iap_extract_no_credit",
         noupdate=False,
+    )
+    # Update template_fs otherwise mail/16.0.1.10/end-migrate.py won't set it correctly
+    cr.execute(
+        """
+        UPDATE mail_template
+           SET template_fs = 'iap_extract/data/mail_template_data.xml'
+         WHERE id = %s
+           AND template_fs = 'account_invoice_extract/data/mail_template_data.xml'
+        """,
+        [util.ref(cr, "iap_extract.iap_extract_no_credit")],
     )
