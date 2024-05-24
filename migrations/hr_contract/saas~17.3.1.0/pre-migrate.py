0f85f5da23a46f6fe70daa22c5ede213f5b68400
sofiagvaladze <sgv@odoo.com>
2024-05-24 16:36:04 +0200

    [IMP] hr_contract/saas~17.3: update security rule
    We would like if hr_contract.group_hr_contract_employee_manager rights
    did not imply hr.group_hr_user. Generally, giving hr.group_hr_user rights
    to user is too much 'power', and if we could do without it, we should.
    
    Motivation:
    Employee manager that conducts appraisals, might benefit from having an
    access to employee contracts, without giving them HR officer rights.
    
    related: https://github.com/odoo/odoo/pull/166715
    
    task-3935801
    
    closes odoo/upgrade#6066
    
    Related: odoo/odoo#166715
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/hr_contract/saas~17.3.1.0/pre-migrate.py
+++ migrations/hr_contract/saas~17.3.1.0/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.update_record_from_xml(cr, "hr_contract.group_hr_contract_employee_manager", from_module="hr_contract")
+    util.update_record_from_xml(cr, "hr_contract.group_hr_contract_manager", from_module="hr_contract")
