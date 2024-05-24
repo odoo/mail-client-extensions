fa0adbc6ff78f06c79350637a452a2d0d7850835
Christophe Simonis <chs@odoo.com>
2024-05-24 15:44:19 +0200

    [IMP] mail/saas~17.3: Reuse existing admin channel
    Complement of odoo/upgrade#5786
    
    See odoo/upgrade-util#93
    
    closes odoo/upgrade#6105
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/mail/saas~17.3.1.17/pre-migrate.py
+++ migrations/mail/saas~17.3.1.17/pre-migrate.py
@@ -11,3 +11,6 @@ def migrate(cr, version):
     """
     util.explode_execute(cr, query, table="discuss_channel_member")
     util.remove_field(cr, "discuss.channel", "is_chat")
+
+    # reuse existing admin channel created by a previous upgrade.
+    util.rename_xmlid(cr, "__upgrade__.channel_administrators", "mail.channel_admin", noupdate=True)
