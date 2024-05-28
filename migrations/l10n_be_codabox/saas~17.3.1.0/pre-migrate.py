a104c0af493ae92a593b00f2e2ecdf51f0cedb24
Hesham <hsal@odoo.com>
2024-05-28 17:13:16 +0300

    [IMP] l10n_be_codabox/saas~17.3: removed soda import wizard view
    Removed the view soda_import_wizard_view_form_codabox from l10_be_codabox
    This is required for the Decoupling of soda and codabox modules
    
    Enterprise PR: https://github.com/odoo/enterprise/pull/58844
    
    task-3813212
    
    closes odoo/upgrade#6074
    
    Related: odoo/enterprise#58844
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/l10n_be_codabox/saas~17.3.1.0/pre-migrate.py
+++ migrations/l10n_be_codabox/saas~17.3.1.0/pre-migrate.py
@@ -3,3 +3,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_group(cr, "l10n_be_codabox.group_access_connection_settings")
+    util.remove_view(cr, "l10n_be_codabox.soda_import_wizard_view_form_codabox")
+    util.remove_record(cr, "l10n_be_codabox.action_open_accounting_settings")
