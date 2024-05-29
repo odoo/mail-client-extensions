6e145a148f8d8d2a64f77d0e4e71d8142d621788
Hesham <hsal@odoo.com>
2024-05-29 12:03:34 +0300

    [IMP] l10n_cl_edi/saas~17.4: Merged l10n_cl_edi_boletas
    Merged l10n_cl_edi_boletas module with l10n_cl_edi
    
    Enterprise PR: https://github.com/odoo/enterprise/pull/61265
    
    task-3919542
    
    closes odoo/upgrade#6082
    
    Related: odoo/enterprise#61265
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- migrations/base/saas~17.4.1.3/pre-10-modules.py
+++ migrations/base/saas~17.4.1.3/pre-10-modules.py
@@ -5,3 +5,4 @@ def migrate(cr, version):
     util.merge_module(cr, "data_merge", "data_cleaning")
     util.merge_module(cr, "im_livechat_mail_bot", "mail_bot")
     util.force_upgrade_of_fresh_module(cr, "event_product")
+    util.merge_module(cr, "l10n_cl_edi_boletas", "l10n_cl_edi")
