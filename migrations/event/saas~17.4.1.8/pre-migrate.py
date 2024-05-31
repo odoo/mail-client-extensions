df1d3a6b0cd7e8ee3b357daa0ec905519815e9a2
David Monnom (moda) <moda@odoo.com>
2024-05-31 09:53:07 +0200

    [IMP] event/saas~17.4: move question from website_event to event
    In order to use custom questions on events and event registrations
    without having to install the website_* module suite, this commit
    extracts this functionality from the website_event module to event.
    
    Community PR: https://github.com/odoo/odoo/pull/167384
    
    closes odoo/upgrade#6094
    
    Related: odoo/odoo#167384
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/event/saas~17.4.1.8/pre-migrate.py
@@ -0,0 +1,30 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.move_field_to_module(cr, "event.type", "question_ids", "website_event", "event")
+    util.move_field_to_module(cr, "event.event", "question_ids", "website_event", "event")
+    util.move_field_to_module(cr, "event.event", "general_question_ids", "website_event", "event")
+    util.move_field_to_module(cr, "event.event", "specific_question_ids", "website_event", "event")
+    util.move_field_to_module(cr, "event.registration", "registration_answer_ids", "website_event", "event")
+    util.move_field_to_module(cr, "event.registration", "registration_answer_choice_ids", "website_event", "event")
+
+    util.move_model(cr, "event.question", "website_event", "event")
+    util.move_model(cr, "event.question.answer", "website_event", "event")
+    util.move_model(cr, "event.registration.answer", "website_event", "event")
+
+    eb = util.expand_braces
+    # addons/event/views/event_question_views.xml
+    util.rename_xmlid(cr, *eb("{website_event,event}.event_question_view_form"))
+    # addons/event/views/event_registration_answer_views.xml
+    util.rename_xmlid(cr, *eb("{website_event,event}.event_registration_answer_view_search"))
+    util.rename_xmlid(cr, *eb("{website_event,event}.event_registration_answer_view_tree"))
+    util.rename_xmlid(cr, *eb("{website_event,event}.event_registration_answer_view_graph"))
+    util.rename_xmlid(cr, *eb("{website_event,event}.event_registration_answer_view_pivot"))
+    util.rename_xmlid(cr, *eb("{website_event,event}.action_event_registration_report"))
+    # addons/event/security/ir.model.access.csv
+    util.rename_xmlid(cr, *eb("{website_event,event}.access_event_question_user"))
+    util.rename_xmlid(cr, *eb("{website_event,event}.access_event_question_answer_employee"))
+    util.rename_xmlid(cr, *eb("{website_event,event}.access_event_question_answer_registration"))
+    util.rename_xmlid(cr, *eb("{website_event,event}.access_event_question_answer_user"))
+    util.rename_xmlid(cr, *eb("{website_event,event}.access_event_registration_answer"))
