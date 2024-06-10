bc07d824f84549b687c257ba361f0275e2b4b434
Yannick Tivisse <yti@odoo.com>
2024-06-10 11:47:53 +0200

    [IMP] hr_holidays/saas~17.4: Enhance validation workflow
    Com PR: https://github.com/odoo/odoo/pull/157724
    Ent PR: https://github.com/odoo/enterprise/pull/58687
    
    closes odoo/upgrade#6138
    
    Taskid: 3776596
    Related: odoo/odoo#157724
    Related: odoo/enterprise#58687
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/hr_holidays/saas~17.4.1.6/pre-migrate.py
+++ migrations/hr_holidays/saas~17.4.1.6/pre-migrate.py
@@ -9,3 +9,13 @@ def migrate(cr, version):
     """)
     util.update_field_usage(cr, "hr.leave.allocation", "private_name", "name")
     util.remove_field(cr, "hr.leave.allocation", "private_name")
+
+    util.change_field_selection_values(
+        cr,
+        "hr.leave.type",
+        "allocation_validation_type",
+        {
+            "officer": "hr",
+            "no": "no_validation",
+        },
+    )
