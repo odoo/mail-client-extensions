a04edaa7f5afcadfc95343a6321752f8708d222a
william-andre <wan@odoo.com>
2024-06-11 19:03:30 +0200

    [IMP] account/saas~17.5: store moves made gap
    It is slightly faster to store the fact that the move is a gap or not
    when readind, which is considered better.
    Allows to add a partial index targetting only gaps, which is more direct
    and less big.
    
    odoo/odoo#168951
    odoo/enterprise#66954
    
    closes odoo/upgrade#6146
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/account/saas~17.5.1.2/pre-migrate.py
@@ -0,0 +1,26 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.rename_field(cr, "account.move", "made_sequence_hole", "made_sequence_gap")
+    util.create_column(cr, "account_move", "made_sequence_gap", "bool")
+
+    query = """
+        WITH to_update AS (
+            SELECT this.id
+              FROM account_move this
+         LEFT JOIN account_move other ON this.journal_id = other.journal_id
+                                     AND this.sequence_prefix = other.sequence_prefix
+                                     AND this.sequence_number = other.sequence_number + 1
+             WHERE other.id IS NULL
+               AND this.sequence_number != 1
+               AND this.name != '/'
+               AND {parallel_filter}
+        )
+        UPDATE account_move
+           SET made_sequence_gap = TRUE
+          FROM to_update
+         WHERE account_move.id = to_update.id
+    """
+    util.explode_execute(cr, query, table="account_move", alias="this")
+    cr.execute("DROP INDEX IF EXISTS account_move_sequence_index3")
