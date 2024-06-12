657f1afaae0d10f2059caf43dddf21feecbefe10
Claire Bretton (clbr) <clbr@odoo.com>
2024-06-12 09:11:27 +0200

    [FIX] account/saas~17.4: fix hash test that uses company from env
    The test uses the company from `self.env.company` which could lead
    to problems. In our case, l10n_cl companies have specific `_post()`
    validations that would fail if you try to create invoices in that company
    without setting certain fields, thus the prepare of the hash test would fail.
    
    To fix this, we instead use the Accounting common setup that will give everything
    needed, and makes sure we use a company from the US with generic Chart of Accounts.
    
    Part-of: odoo/upgrade#6082
    Related: odoo/enterprise#61265
    Signed-off-by: Claire Bretton (clbr) <clbr@odoo.com>

--- migrations/account/tests/test_inalterable_hash_v3_to_v4.py
+++ migrations/account/tests/test_inalterable_hash_v3_to_v4.py
@@ -1,56 +1,52 @@
 from odoo import fields
 from odoo.tools import format_date
 
-from odoo.addons.base.maintenance.migrations.testing import UpgradeCase, change_version
+from odoo.addons.base.maintenance.migrations.account.tests.test_common import TestAccountingSetupCommon
+from odoo.addons.base.maintenance.migrations.testing import change_version
 from odoo.addons.base.maintenance.migrations.util.accounting import no_fiscal_lock
 
 
 @change_version("saas~17.2")
-class TestAccountMoveInalterableHashV3ToV4Upgrade(UpgradeCase):
-    def create_invoice(self, invoice_date, account, partner):
+class TestAccountMoveInalterableHashV3ToV4Upgrade(TestAccountingSetupCommon):
+    def _prepare_invoices(self, dates, account_id, partner_id):
         return self.env["account.move"].create(
-            {
-                "move_type": "out_invoice",
-                "partner_id": partner.id,
-                "invoice_date": fields.Date.from_string(invoice_date),
-                "invoice_line_ids": [
-                    fields.Command.create(
-                        {
-                            "name": "Product",
-                            "quantity": 1,
-                            "price_unit": 100,
-                            "account_id": account.id,
-                        },
-                    )
-                ],
-            }
+            [
+                {
+                    "move_type": "out_invoice",
+                    "partner_id": partner_id,
+                    "invoice_date": fields.Date.from_string(date),
+                    "invoice_line_ids": [
+                        fields.Command.create(
+                            {
+                                "name": "Product",
+                                "quantity": 1,
+                                "price_unit": 100,
+                                "account_id": account_id,
+                            },
+                        )
+                    ],
+                }
+                for date in dates
+            ]
         )
 
     def prepare(self):
+        res = super().prepare()
+        dates = ["2024-01-01", "2024-01-02", "2024-01-03", "2024-01-04", "2024-01-05"]
+        account_id = res["config"]["account_receivable_id"]
+        partner_id = res["config"]["partner_id"]
         with no_fiscal_lock(self.env.cr):
-            account = self.env["account.account"].create(
-                {
-                    "name": "Account",
-                    "code": "ACC",
-                    "account_type": "asset_current",
-                }
-            )
-            partner = self.env["res.partner"].create({"name": "Partner"})
-            moves = (
-                self.create_invoice("2024-01-01", account, partner)
-                | self.create_invoice("2024-01-02", account, partner)
-                | self.create_invoice("2024-01-03", account, partner)
-                | self.create_invoice("2024-01-04", account, partner)
-                | self.create_invoice("2024-01-05", account, partner)
-            )
+            moves = self._prepare_invoices(dates, account_id, partner_id)
             moves[0].journal_id.restrict_mode_hash_table = True
             moves.action_post()
-        return {"move_ids": moves.ids, "account_id": account.id, "partner_id": partner.id}
+        res["tests"].append(("_check_inalterable_hash", [moves.ids]))
+        return res
 
-    def check(self, result):
-        moves = self.env["account.move"].browse(result["move_ids"])
-        account = self.env["account.account"].browse(result["account_id"])
-        partner = self.env["res.partner"].browse(result["partner_id"])
+    def _check_inalterable_hash(self, config, move_ids):
+        self.env.company = self.env["res.company"].browse(config["company_id"])
+        moves = self.env["account.move"].browse(move_ids)
+        account_id = config["account_receivable_id"]
+        partner_id = config["partner_id"]
         moves.env.user.groups_id |= self.env.ref("account.group_account_user")
 
         # Check that the hashing chain check is still correct after the upgrade
@@ -61,9 +57,7 @@ class TestAccountMoveInalterableHashV3ToV4Upgrade(UpgradeCase):
         self.assertEqual(result["last_move_date"], format_date(self.env, fields.Date.to_string(moves[-1].date)))
 
         with no_fiscal_lock(self.env.cr):
-            new_moves = self.create_invoice("2024-01-06", account, partner) | self.create_invoice(
-                "2024-01-07", account, partner
-            )
+            new_moves = self._prepare_invoices(["2024-01-06", "2024-01-07"], account_id, partner_id)
             moves |= new_moves
             new_moves.action_post()
             new_moves.button_hash()
