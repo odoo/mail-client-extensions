84dfa7cb41c576af8d414cb4a39c5386a8c88796
Chrysanthe (chgo) <chgo@odoo.com>
2024-06-17 14:06:50 +0200

    [IMP] {web,*}/saas~17.4: review z-index utilities
    Remove the custom z-index utilities in favor of Bootstrap default.
    
    See odoo/odoo#165568
    and odoo/enterprise#62524
    
    closes odoo/upgrade#6078
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/website/saas~17.4.1.0/pre-migrate-bootstrap.py
@@ -0,0 +1,19 @@
+from odoo.upgrade.util import snippets
+
+
+def replace_z_index_classes(el):
+    zindex_classes = {"z-index-0": "z-0", "z-index-1": "z-1"}
+
+    for old_utility, new_utility in zindex_classes.items():
+        if old_utility in el.classes:
+            el.classes -= [old_utility]
+            el.classes |= [new_utility]
+
+
+def migrate(cr, version):
+    snippets.convert_html_content(
+        cr,
+        snippets.html_converter(
+            replace_z_index_classes, selector="//*[contains(@class, 'z-index-0') or contains(@class, 'z-index-1')]"
+        ),
+    )
