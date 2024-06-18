b824f0cf42f3935ffcba0915788a3fa6be9203fa
aliya <alta@odoo.com>
2024-06-18 10:31:45 +0200

    [IMP] account_peppol/saas~17.4: refactor partner check
    Currently, verifying whether a partner exists on Peppol network
    and whether they can receive a particular document type is
    unnecessarily complex.
    We have three fields for the same purpose,
    even though we could use just one.
    
    Additionally, partners are shared between companies.
    Each company in the database can be registered on Peppol separately
    in different edi modes: demo, test, prod.
    
    Each edi mode has its own partner verification check, so the
    verification state needs to be company-dependent.
    At the same time, it needs to be computed on the fly
    when a user updates peppol_eas, peppol_endpoint or ubl_cii_format
    on that partner.
    In that moment, we recompute the verification state for every
    company in the db.
    
    See also:
    odoo/odoo#169650
    
    closes odoo/upgrade#6164
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/account_peppol/saas~17.5.1.0/pre-migrate.py
@@ -0,0 +1,30 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    def adapter(leaf, _or, _neg):
+        # (..., '=', True) => (..., '=', 'valid')
+        # (..., '=', False) => (..., '!=', 'valid')
+        # (..., '!=', True) => (..., '!=', 'valid')
+        # (..., '!=', False) => (..., '=', 'valid')
+        left, op, right = leaf
+        if (op, bool(right)) in [("=", False), ("!=", True)]:
+            op = "!="
+        return [(left, op, "valid")]
+
+    for field in ("account_peppol_validity_last_check", "account_peppol_verification_label"):
+        util.remove_field(cr, "res.partner", field)
+
+    util.rename_field(
+        cr, "res.partner", "account_peppol_is_endpoint_valid", "peppol_verification_state", domain_adapter=adapter
+    )
+    util.alter_column_type(
+        cr,
+        "res_partner",
+        "peppol_verification_state",
+        "varchar",
+        using="CASE {0} WHEN true THEN 'valid' ELSE 'not_verified' END",
+    )
+    util.make_field_company_dependent(
+        cr, "res.partner", "peppol_verification_state", "selection", default_value="not_verified"
+    )
