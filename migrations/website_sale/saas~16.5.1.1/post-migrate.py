095211f34740a75ce1fe6806b888325dbf3c9ef7
diagnoza <owi@odoo.com>
2024-06-18 20:43:00 +0000

    [FIX] website_sale/saas~16.5: carry over extra step setting
    Until now, the "Extra Step" setting was correctly carried over only for databases that didn't have a multi-website setup. In databases with multi-website and prior to 17.0, a cowed view 'website_sale.extra_info_option' was created for every website that had this setting enabled. In 17.0, these views ('website_sale.extra_info_option') were removed in favor or relying only on 'website_sale.extra_info'. Therefore we create new 'website_sale.extra_info' cowed views for every 'website_sale.extra_info_option' there has been.
    
    Part-of: odoo/upgrade#6168
    Signed-off-by: Oskar Wielgos (owi) <owi@odoo.com>

--- migrations/website_sale/saas~16.5.1.1/post-migrate.py
+++ migrations/website_sale/saas~16.5.1.1/post-migrate.py
@@ -5,3 +5,16 @@ def migrate(cr, version):
     util.if_unchanged(
         cr, "website_sale.payment_delivery_methods", util.update_record_from_xml, reset_translations={"arch_db"}
     )
+
+    # Set the arch for carried over cowed views, see pre-migrate.py
+    cr.execute(
+        """
+        UPDATE ir_ui_view v
+           SET arch_db = vv.arch_db
+          FROM ir_ui_view vv
+         WHERE vv.key = 'website_sale.extra_info'
+           AND vv.website_id IS NULL -- the "main" view, not a cowed one
+           AND v.key = 'website_sale.extra_info'
+           AND v.website_id IS NOT NULL
+        """
+    )
--- migrations/website_sale/saas~16.5.1.1/pre-migrate.py
+++ migrations/website_sale/saas~16.5.1.1/pre-migrate.py
@@ -18,6 +18,26 @@ def migrate(cr, version):
             (extra_info_option_view_id, extra_info_view_id),
         )
 
+        # In multi-website environments, cowed views were created for each
+        # website that had this setting enabled. Here we only create "extra_info"
+        # views based on existing "extra_info_option" views. The arch will be set in
+        # a post script so that it's up to date.
+        cr.execute(
+            """
+            INSERT INTO ir_ui_view (
+                name, key, active,
+                priority, mode, type, website_id
+            )
+            SELECT
+                'Checkout Extra Info', 'website_sale.extra_info', active,
+                16, 'primary', 'qweb', website_id
+              FROM ir_ui_view
+             WHERE key = 'website_sale.extra_info_option'
+               AND website_id IS NOT NULL
+               AND active
+            """,
+        )
+
     util.remove_field(cr, "res.config.settings", "module_website_sale_digital")
     util.remove_view(cr, "website_sale.cart_popover")
     util.remove_view(cr, "website_sale.cart_summary")
