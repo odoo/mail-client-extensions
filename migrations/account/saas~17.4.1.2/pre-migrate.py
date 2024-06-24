a176fe311c86f5c137ddcc694283a7cd5be3667e
Maximilien (malb) <malb@odoo.com>
2024-06-24 13:40:34 +0200

    [IMP] account/saas~17.4: replace last by previous
    This upgrade script is needed cause we replaced the last_... by period with the
    number of period.
    
    see:
    https://github.com/odoo/odoo/pull/168875
    https://github.com/odoo/enterprise/pull/63743
    
    closes odoo/upgrade#6178
    
    Task: 3961087
    Related: odoo/odoo#168875
    Related: odoo/enterprise#63743
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account/saas~17.4.1.2/pre-migrate.py
+++ migrations/account/saas~17.4.1.2/pre-migrate.py
@@ -6,3 +6,14 @@ def migrate(cr, version):
     util.remove_field(cr, "account.move.send", "sequence_gap_warning")
     util.rename_field(cr, "mail.message", "show_audit_log", "account_audit_log_activated")
     util.remove_view(cr, "account.res_config_settings_view_form_inherit_account_audit_trail")
+    util.change_field_selection_values(
+        cr,
+        "account.report",
+        "default_opening_date_filter",
+        {
+            "last_month": "previous_month",
+            "last_quarter": "previous_quarter",
+            "last_year": "previous_year",
+            "last_tax_period": "previous_tax_period",
+        },
+    )
