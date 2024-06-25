c9caf9878ced2d63079f06e81a76a24f5a6ffcc7
jeep-odoo <jeep@odoo.com>
2024-06-25 10:44:17 +0530

    [IMP] payment/saas~17.5: add emi for razorpay
    This commit adds the payment method `emi` in the Razorpay provider
    
    COM PR: https://github.com/odoo/odoo/pull/170677
    
    task-3958299
    
    closes odoo/upgrade#6180
    
    Related: odoo/odoo#170677
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/payment/saas~17.5.2.0/post-migrate.py
@@ -0,0 +1,6 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.if_unchanged(cr, "payment.payment_provider_razorpay", util.update_record_from_xml)
+    util.if_unchanged(cr, "payment.payment_method_emi_india", util.update_record_from_xml)
--- migrations/payment/saas~17.5.2.0/pre-migrate.py
+++ migrations/payment/saas~17.5.2.0/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.change_field_selection_values(cr, "payment.method", "support_refund", {None: "none"})
+    util.rename_xmlid(cr, "payment.payment_method_emi", "payment.payment_method_emi_india")
