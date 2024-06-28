3091b3139f98bbc73bacb1336da6fe264bedc792
asno-odoo <asno@odoo.com>
2024-06-28 08:42:01 +0000

    [FIX] account/saas~17.4: remove view
    This view was added in stable as bugfix and merged till saas~17.3
    but master was not merged: https://github.com/odoo/odoo/pull/170274,
    because a fix is already there in master and these views are different.
    
    closes odoo/upgrade#6191
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account/saas~17.4.1.2/pre-migrate.py
+++ migrations/account/saas~17.4.1.2/pre-migrate.py
@@ -6,6 +6,7 @@ def migrate(cr, version):
     util.remove_field(cr, "account.move.send", "sequence_gap_warning")
     util.rename_field(cr, "mail.message", "show_audit_log", "account_audit_log_activated")
     util.remove_view(cr, "account.res_config_settings_view_form_inherit_account_audit_trail")
+    util.remove_view(cr, "account.view_account_payment_method_line_kanban")
     util.change_field_selection_values(
         cr,
         "account.report",
