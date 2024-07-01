dfbc96f6c50ab4f4b7192a7e658d36181727969b
Agustín Castro Bugallo <agcb@odoo.com>
2024-07-01 14:33:28 +0000

    [FIX] l10n_cl_reports/saas~16.1: Apply patch when needed
    The patch https://github.com/odoo/enterprise/commit/66412a4180a631b5b492b5c63e3372b293e2f003
    changed the records in stable 16. However the fw-port to 17 is still not done.
    This makes that records may trigger a duplicate code.
    
    Databases coming from versions earlier that 16, have the new values from that
    patch, and are handled by https://github.com/odoo/upgrade/pull/6046
    However, when the database comes from 16, the records still hold the old values
    so that patch shouldn't be applied.
    
    The fix in standard is in https://github.com/odoo/enterprise/pull/62693
    
    tbg-1347
    
    ```
    psycopg2.errors.UniqueViolation: duplicate key value violates unique constraint "account_report_line_code_uniq"
    DETAIL:  Key (report_id, code)=(20, CL_TOTVEN) already exists.
    [...]
    odoo.tools.convert.ParseError: while parsing /home/odoo/src/enterprise/17.0/l10n_cl_reports/data/f29_report.xml:3, somewhere inside
    ```
    
    closes odoo/upgrade#6199
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/l10n_cl_reports/saas~16.1.1.1/pre-migrate.py
+++ migrations/l10n_cl_reports/saas~16.1.1.1/pre-migrate.py
@@ -2,6 +2,13 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    cr.execute(
+        "SELECT 1 FROM account_report_line WHERE id = %s AND code='CL_VEXPO'",
+        [util.ref(cr, "l10n_cl_reports.account_financial_report_f29_line_0103")],
+    )
+    if not cr.rowcount:
+        return
+
     util.remove_record(cr, "l10n_cl_reports.account_financial_report_f29_line_0103")
     util.remove_record(cr, "l10n_cl_reports.account_financial_report_f29_line_0103_balance")
 
