86eeee55cc1686cf7f2e72f69e57aaa10462604e
Alvaro Fuentes <afu@odoo.com>
2024-07-05 14:53:41 +0200

    [chore] ruffed
    Fixes:
    * Iteration over set
    * Encoding declaration
    
    closes odoo/upgrade#6216
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/l10n_din5008/saas~15.1.1.0/pre-migrate.py
+++ migrations/l10n_din5008/saas~15.1.1.0/pre-migrate.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     eb = util.expand_braces
 
-    for model in {"account.move", "base.document.layout", "account.analytic.line"}:
-        for suffix in {"template_data", "document_title"}:
+    for model in ["account.move", "base.document.layout", "account.analytic.line"]:
+        for suffix in ["template_data", "document_title"]:
             util.move_field_to_module(cr, model, f"l10n_de_{suffix}", *eb("l10n_{de,din5008}"))
             util.rename_field(cr, model, *eb(f"l10n_{{de,din5008}}_{suffix}"))
 
