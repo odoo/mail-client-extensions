3cb22308cf1f604db042fc635a6eb82951398852
Christophe Simonis <chs@odoo.com>
2024-07-08 13:51:20 +0200

    [ADD] base/0.0.0: force explicit removal of (root) menus
    In order to not loose the order of root menus on users's homepage, we
    should forbid implicit deletions, in case a `rename_xmlid` is missing.
    
    closes odoo/upgrade#6225
    
    Related: odoo/upgrade-util#109
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/base/0.0.0/pre-models-ir_ui_menu.py
@@ -0,0 +1,35 @@
+import logging
+import os
+
+from odoo import models
+
+from odoo.addons.base.maintenance.migrations import util
+
+_logger = logging.getLogger("odoo.addons.base.maintenance.migrations.base." + __name__)
+
+
+def migrate(cr, version):
+    pass
+
+
+if util.version_gte("saas~16.4"):
+    from odoo.addons.base.models.ir_ui_menu import IrUiMenu as _ignore  # noqa
+
+    class Menu(models.Model):
+        _inherit = "ir.ui.menu"
+        _module = "base"
+
+        def unlink(self):
+            to_ignore = os.getenv("suppress_upgrade_warnings", "").split(",")  # noqa: SIM112
+            for menu in self:
+                if menu.parent_id:
+                    continue
+                xmlid = menu.get_metadata()[0]["xmlid"]
+                if not xmlid:
+                    continue
+                if "xmlid:{}".format(xmlid) in to_ignore:
+                    _logger.log(util.NEARLY_WARN, "menu unlink %s explicitly ignored", xmlid)
+                else:
+                    _logger.critical("It looks like you forgot to call `util.remove_record(cr, %r)`", xmlid)
+
+            return super().unlink()
