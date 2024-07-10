2452156c5369e329abe92b5aaa66e8c9eefd85b6
Atul Patel <atp@odoo.com>
2024-07-10 08:14:39 +0000

    [FIX] auth_totp/saas~16.2: check if exist before drop constraint
    IN some of databases , constraints does not exist, so it will
    raise error , constraint does not exist.
    so check will drop constraint only if it exists.
    for safer side.
    
    I know for this database it's specific database issue
    as FOREIGN KEY does not exist in original database.
    
    upg-1799785
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/17.0/odoo/service/server.py", line 1313, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "<decorator-gen-16>", line 2, in new
      File "/home/odoo/src/odoo/17.0/odoo/tools/func.py", line 87, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/17.0/odoo/modules/registry.py", line 113, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/17.0/odoo/modules/loading.py", line 476, in load_modules
        processed_modules += load_marked_modules(env, graph,
      File "/home/odoo/src/odoo/17.0/odoo/modules/loading.py", line 364, in load_marked_modules
        loaded, processed = load_module_graph(
      File "/home/odoo/src/odoo/17.0/odoo/modules/loading.py", line 181, in load_module_graph
        migrations.migrate_module(package, 'pre')
      File "/home/odoo/src/odoo/17.0/odoo/modules/migration.py", line 240, in migrate_module
        migrate(self.cr, installed_version)
      File "/tmp/tmphofqldps/migrations/auth_totp/saas~16.2.1.0/pre-migrate.py", line 5, in migrate
        cr.execute("ALTER TABLE auth_totp_device DROP CONSTRAINT auth_totp_device_user_id_fkey")
      File "/home/odoo/src/odoo/17.0/odoo/sql_db.py", line 332, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.UndefinedObject: constraint "auth_totp_device_user_id_fkey" of relation "auth_totp_device" does not exist
    
    ```
    
    closes odoo/upgrade#6239
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/auth_totp/saas~16.2.1.0/pre-migrate.py
+++ migrations/auth_totp/saas~16.2.1.0/pre-migrate.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-
-
 def migrate(cr, version):
-    cr.execute("ALTER TABLE auth_totp_device DROP CONSTRAINT auth_totp_device_user_id_fkey")
+    cr.execute("ALTER TABLE auth_totp_device DROP CONSTRAINT IF EXISTS auth_totp_device_user_id_fkey")
     cr.execute("ALTER TABLE auth_totp_device ADD FOREIGN KEY (user_id) REFERENCES res_users(id) ON DELETE cascade")
