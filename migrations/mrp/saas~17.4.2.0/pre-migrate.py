5942980a143f5024a6435fa260a93b4877cf7bd0
Mathieu Duckerts-Antoine <dam@odoo.com>
2024-07-11 13:45:46 +0200

    [IMP] mrp/saas~17.4.2.0: delete 2 gantt views
    The gant views deleted here have been moved to mrp_workorder.
    See commit "[FIX] *: no gantt in community" in related PRs:
    - https://github.com/odoo/odoo/pull/172737
    - https://github.com/odoo/enterprise/pull/66481
    
    closes odoo/upgrade#6263
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/mrp/saas~17.4.2.0/pre-migrate.py
+++ migrations/mrp/saas~17.4.2.0/pre-migrate.py
@@ -3,3 +3,11 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "mrp.production", "confirm_cancel")
+    if util.module_installed(cr, "mrp_workorder"):
+        eb = util.expand_braces
+        util.rename_xmlid(cr, *eb("mrp{,_workorder}.workcenter_line_gantt_production"))
+        util.rename_xmlid(cr, *eb("mrp{,_workorder}.mrp_workorder_view_gantt"))
+    else:
+        util.remove_view(cr, "mrp.workcenter_line_gantt_production")
+        util.remove_view(cr, "mrp.mrp_workorder_view_gantt")
+        util.remove_act_window_view_mode(cr, "mrp.workorder", "gantt")
