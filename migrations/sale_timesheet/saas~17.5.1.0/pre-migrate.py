dd022ce5c73d3810b8a1932d50b47c0a043743aa
Xavier Bol (xbo) <xbo@odoo.com>
2024-07-11 15:33:33 +0200

    [IMP] sale_timesheet/saas~17.5: update timesheet_invoice_type
    This commit updates timesheet_invoice_type according the changes made in
    the related PR in community.
    
    Community PR: odoo/odoo#172788
    Enterprise PR: odoo/enterprise#66507
    
    closes odoo/upgrade#6258
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/sale_timesheet/saas~17.5.1.0/pre-migrate.py
+++ migrations/sale_timesheet/saas~17.5.1.0/pre-migrate.py
@@ -3,3 +3,19 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "project.task", "so_analytic_account_id")
+    util.explode_execute(
+        cr,
+        """
+        UPDATE account_analytic_line aal
+           SET timesheet_invoice_type = CASE timesheet_invoice_type
+                 WHEN 'timesheet_revenues' THEN 'billable_time'
+                 ELSE 'other_costs'
+               END
+         WHERE (    timesheet_invoice_type = 'timesheet_revenues'
+                AND unit_amount <= 0)
+            OR (    timesheet_invoice_type IN ('service_revenues', 'other_revenues')
+                AND unit_amount < 0)
+        """,
+        table="account_analytic_line",
+        alias="aal",
+    )
