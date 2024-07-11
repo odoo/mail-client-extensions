277a681e9ca26f64da585d4122e8e0471bc2dbbe
qsm-odoo <qsm@odoo.com>
2024-07-11 13:43:08 +0200

    [IMP] base/saas~17.5: remove twitter apps
    Community PR: https://github.com/odoo/odoo/pull/172755
    Enterprise PR: https://github.com/odoo/enterprise/pull/66488
    Related upgrade-util PR: https://github.com/odoo/upgrade-util/pull/113
    
    task-4008369
    
    closes odoo/upgrade#6254
    
    Related: odoo/odoo#172755
    Related: odoo/enterprise#66488
    Related: odoo/upgrade-util#113
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/base/saas~17.5.1.3/pre-twitter.py
@@ -0,0 +1,33 @@
+from odoo.upgrade import util
+from odoo.upgrade.util import snippets
+
+
+def remove_s_twitter(el):
+    el.getparent().remove(el)
+    return True
+
+
+def migrate(cr, version):
+    if util.module_installed(cr, "website_twitter"):
+        # Remove all twitter snippets that were added by users
+        snippets.convert_html_content(
+            cr,
+            snippets.html_converter(remove_s_twitter, selector="//*[@data-snippet='s_twitter']"),
+        )
+
+    if util.module_installed(cr, "website_event_twitter_wall"):
+        # Remove event menus that would lead to a 404.
+        cr.execute(
+            """
+            DELETE FROM website_menu m
+                  USING website_event_menu e
+                  WHERE e.menu_id = m.id
+                    AND e.menu_type = 'social'
+                    AND m.url LIKE '/event/%/social'
+            """
+        )
+
+    # Remove twitter apps
+    util.remove_module(cr, "website_event_twitter_wall")
+    util.remove_module(cr, "website_twitter_wall")
+    util.remove_module(cr, "website_twitter")
