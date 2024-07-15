49697a6f49c92ab2370b5cdabc4b96c0d62cd294
adsh-odoo <adsh@odoo.com>
2024-07-15 14:34:30 +0530

    [IMP] appointment/saas~17.5: rename XML id
    - Rename the XML id for some record rules as there purpose have been changed.
    - Remove menu as it is no longer present.
    Related pr:
    https://github.com/odoo/enterprise/pull/63046
    
    Task-3905109
    
    closes odoo/upgrade#6266
    
    Related: odoo/enterprise#63046
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/appointment/saas~17.5.1.3/pre-migrate.py
+++ migrations/appointment/saas~17.5.1.3/pre-migrate.py
@@ -2,4 +2,17 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    eb = util.expand_braces
+
     util.remove_field(cr, "appointment.type", "appointment_count_report")
+
+    renames = [
+        "appointment.appointment_type_rule_user_{,write_}unlink",
+        "appointment.appointment_invite_rule_user{,_internal}",
+        "appointment.appointment_invite_rule_user_{,internal_write_}unlink",
+        "appointment.appointment_slot_rule_user_{,write_}unlink",
+    ]
+    for rename in renames:
+        util.rename_xmlid(cr, *eb(rename))
+
+    util.remove_menus(cr, [util.ref(cr, "appointment.menu_schedule_report_all")])
