199ec087154b06874d2be4053ff011ffb50d0bee
Christophe Simonis <chs@odoo.com>
2024-07-15 15:24:47 +0200

    [ADD] sale_stock/saas~17.4: new compute stored field
    Don't let the ORM compute it, else it MemoryError.
    
    See odoo/odoo#169036

---
+++ migrations/sale_stock/saas~17.4.1.0/post-upgrade.py
@@ -0,0 +1,17 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    query = """
+        UPDATE sale_order_line l
+           SET warehouse_id = o.warehouse_id
+          FROM sale_order o
+         WHERE o.id = l.order_id
+           AND l.route_id IS NULL
+    """
+    util.explode_execute(cr, query, table="sale_order_line", alias="l")
+
+    cr.execute("SELECT id FROM sale_order_line WHERE route_id IS NOT NULL")
+    ids = [lid for (lid,) in cr.fetchall()]
+    if ids:
+        util.recompute_fields(cr, "sale.order.line", "warehouse_id", ids=ids)
