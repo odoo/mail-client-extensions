06d0227311e3346f78f142b1931e8ebc38738655
Aurélien Warnon <awa@odoo.com>
2024-07-15 11:05:37 +0200

    [IMP] website_event_exhibitor/saas~17.5: cleanup unused templates
    Cleanup of odoo/odoo@a06d20e1c6842639a1496d74ac93a9ab0cc9ddc8
    As we needed to merge in the recently freezed saas-17.4.
    
    COM PR odoo/odoo#173105
    Task-3908389
    
    closes odoo/upgrade#6265
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/website_event_exhibitor/saas~17.5.1.1/pre-migrate.py
+++ migrations/website_event_exhibitor/saas~17.5.1.1/pre-migrate.py
@@ -3,3 +3,6 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_record(cr, "website_event_exhibitor.event_report_template_full_page_ticket_inherit_exhibitor")
+
+    util.remove_view(cr, "website_event_exhibitor.exhibitors_search_tag")
+    util.remove_view(cr, "website_event_exhibitor.exhibitor_search_box")
