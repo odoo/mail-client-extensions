odoo/mail-client-extensions

commit c7f6666a9150de05d25b45476d67bc562c67906b

[IMP] spreadsheet_edition/saas~17.3: change pivot measures and groups
This commit changes the way pivot measures and groups are stored in the
ADD_PIVOT command. This is done to be able to add properties to measures
and groups in the future. (e.g. the measure aggregate or the order of a group)

Before this commit:
```js
{
  type: "ADD_PIVOT",
  pivotId: 1,
  pivot: {
    colGroupBys: ["A"],
    rowGroupBys: ["B"],
    measures: ["C"]
    ...
  }
}
```

After this commit:
```js
{
  type: "ADD_PIVOT",
  pivotId: 1,
  pivot: {
    colGroupBys: [{ name: "A" }],
    rowGroupBys: [{ name: "B" }],
    measures: [{ name: "C" }]
    ...
  }
}
```

See https://github.com/odoo/odoo/pull/158253
and https://github.com/odoo/enterprise/pull/58957

Task-id 3813563

closes odoo/upgrade#5834

Related: odoo/odoo#158253
Related: odoo/enterprise#58957
Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>
Co-authored-by: Lucas Lefevre <lul@odoo.com>
Co-authored-by: Pierre Rousseau <pro@odoo.com>

migrations/spreadsheet_edition/saas~17.3.1.0/pre-migrate.py
@@ -9,7 +9,7 @@ def migrate(cr, version):
         r"""
         SELECT id, commands
           FROM spreadsheet_revision
-         WHERE commands LIKE '%UPDATE\_CELL%'
+         WHERE commands LIKE '%UPDATE\_CELL%' OR commands LIKE '%ADD\_PIVOT%'
         """
     )
 
@@ -20,11 +20,14 @@ def migrate(cr, version):
             continue
 
         for command in commands:
-            if command["type"] != "UPDATE_CELL":
-                continue
-            command["content"] = re.sub(r"\bODOO\.PIVOT\(", "PIVOT.VALUE(", command["content"], flags=re.I)
-            command["content"] = re.sub(r"\bODOO\.PIVOT\.TABLE\b", "PIVOT", command["content"], flags=re.I)
-            command["content"] = re.sub(r"\bODOO\.PIVOT\.HEADER\b", "PIVOT.HEADER", command["content"], flags=re.I)
+            if command["type"] == "UPDATE_CELL":
+                command["content"] = re.sub(r"\bODOO\.PIVOT\(", "PIVOT.VALUE(", command["content"], flags=re.I)
+                command["content"] = re.sub(r"\bODOO\.PIVOT\.TABLE\b", "PIVOT", command["content"], flags=re.I)
+                command["content"] = re.sub(r"\bODOO\.PIVOT\.HEADER\b", "PIVOT.HEADER", command["content"], flags=re.I)
+            elif command["type"] == "ADD_PIVOT":
+                command["pivot"]["measures"] = [{"name": measure} for measure in command["pivot"]["measures"]]
+                command["pivot"]["colGroupBys"] = [parse_dimension(col) for col in command["pivot"]["colGroupBys"]]
+                command["pivot"]["rowGroupBys"] = [parse_dimension(row) for row in command["pivot"]["rowGroupBys"]]
 
         data_loaded["commands"] = commands
         cr.execute(
@@ -35,3 +38,10 @@ def migrate(cr, version):
             """,
             [json.dumps(data_loaded), revision_id],
         )
+
+
+def parse_dimension(dimension):
+    values = dimension.split(":")
+    if len(values) > 1:
+        return {"name": values[0], "granularity": values[1]}
+    return {"name": values[0]}

migrations/spreadsheet_edition/tests/test_add_pivot.py
@@ -1,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+from odoo.addons.base.maintenance.migrations import util
+from odoo.addons.base.maintenance.migrations.testing import UpgradeCase, change_version
+
+
+@change_version("17.3")
+class TestSpreadsheetInsertPivot(UpgradeCase):
+    def prepare(self):
+        if not util.table_exists(self.env.cr, "spreadsheet_revision"):
+            return None
+        pivot = {
+            "type": "ODOO",
+            "colGroupBys": ["A", "B"],
+            "rowGroupBys": ["C", "D"],
+            "measures": ["MEASURE"],
+            "model": "MODEL",
+            "context": {"context_key": "context_value"},
+            "domain": [["A", "=", "test"]],
+            "name": "MODEL",
+            "sortedColumn": None,
+        }
+        commands = {
+            "type": "REMOTE_REVISION",
+            "commands": [
+                {
+                    "type": "ADD_PIVOT",
+                    "pivot": pivot,
+                    "pivotId": "1",
+                },
+            ],
+        }
+        field_name = "revision_uuid" if util.version_gte("saas~17.2") else "revision_id"
+        revision_id = self.env["spreadsheet.revision"].create(
+            [
+                {
+                    "res_id": 1000,
+                    "res_model": "spreadsheet",
+                    "commands": json.dumps(commands),
+                    field_name: "B",
+                    "parent_revision_id": False if util.version_gte("saas~17.2") else "START_REVISION",
+                },
+            ]
+        )
+        return revision_id.id
+
+    def check(self, revision_id):
+        if not revision_id:
+            return
+        revisions = self.env["spreadsheet.revision"].browse(revision_id)
+
+        data = json.loads(revisions[0].commands)
+        pivot = data["commands"][0]["pivot"]
+        self.assertEqual(pivot["measures"], [{"name": "MEASURE"}])
+        self.assertEqual(pivot["colGroupBys"], [{"name": "A"}, {"name": "B"}])
+        self.assertEqual(pivot["rowGroupBys"], [{"name": "C"}, {"name": "D"}])

migrations/spreadsheet_edition/tests/test_insert_pivot.py
@@ -13,7 +13,7 @@ class TestSpreadsheetInsertPivot(UpgradeCase):
             return None
         definition = {
             "metaData": {
-                "colGroupBys": ["A", "B"],
+                "colGroupBys": ["A:month", "B"],
                 "rowGroupBys": ["C", "D"],
                 "activeMeasures": ["MEASURE"],
                 "resModel": "MODEL",
@@ -75,7 +75,7 @@ class TestSpreadsheetInsertPivot(UpgradeCase):
 
         pivot = {
             "type": "ODOO",
-            "colGroupBys": ["A", "B"],
+            "colGroupBys": ["A:month", "B"],
             "rowGroupBys": ["C", "D"],
             "measures": ["MEASURE"],
             "model": "MODEL",
@@ -84,6 +84,12 @@ class TestSpreadsheetInsertPivot(UpgradeCase):
             "name": "MODEL",
             "sortedColumn": None,
         }
+
+        if util.version_gte("saas~17.3"):
+            pivot["colGroupBys"] = [{"name": "A", "granularity": "month"}, {"name": "B"}]
+            pivot["rowGroupBys"] = [{"name": "C"}, {"name": "D"}]
+            pivot["measures"] = [{"name": "MEASURE"}]
+
         data = json.loads(revisions[0].commands)
         self.assertEqual(
             data,

migrations/spreadsheet_edition/tests/__init__.py
@@ -1,4 +1,5 @@
 from . import (
+    test_add_pivot,
     test_create_table_command_change,
     test_image_attachment,
     test_insert_pivot,

commit bfce59fce789330684b7020f1e4ad8af53d4a083

[FIX] spreadsheet_edition/saas~17.3: wrong renaming of pivot field
This commit fixes the migration introduced in c7f6666a9150de.
The attributes `colGroupBys` and `rowGroupBys` should have been renamed
to `columns` and `rows`, respectively. It should also be applied to the
`UPDATE_PIVOT` command.

Steps to reproduce:
- Saas-17.2: go to CRM -> Insert pivot in spreadsheet
- Update the pivot (right click on pivot cell -> See pivot properties,
  add a dimension -> Update)
- Shutdown the server (to avoid the snapshot to be created)
- Upgrade to saas-17.3
- Open the spreadsheet
=> All the cells are in error.

See https://github.com/odoo/odoo/pull/158253/files#diff-c2d29c9b2883c8b3de41568ee5b390ee4819c8ffe943dca85a721b1fb4ccb74cR319-R322

Spotted while working on another migration, not related to an opw right
now.

closes odoo/upgrade#6287

Task: 4059939
Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

migrations/spreadsheet_edition/saas~17.3.1.0/pre-migrate.py
@@ -15,10 +15,12 @@ def migrate(cr, version):
                 command["content"] = re.sub(r"\bODOO\.PIVOT\(", "PIVOT.VALUE(", command["content"], flags=re.I)
                 command["content"] = re.sub(r"\bODOO\.PIVOT\.TABLE\b", "PIVOT", command["content"], flags=re.I)
                 command["content"] = re.sub(r"\bODOO\.PIVOT\.HEADER\b", "PIVOT.HEADER", command["content"], flags=re.I)
-            elif command["type"] == "ADD_PIVOT":
+            elif command["type"] in ["ADD_PIVOT", "UPDATE_PIVOT"]:
                 command["pivot"]["measures"] = [{"name": measure} for measure in command["pivot"]["measures"]]
-                command["pivot"]["colGroupBys"] = [parse_dimension(col) for col in command["pivot"]["colGroupBys"]]
-                command["pivot"]["rowGroupBys"] = [parse_dimension(row) for row in command["pivot"]["rowGroupBys"]]
+                command["pivot"]["columns"] = [parse_dimension(col) for col in command["pivot"]["colGroupBys"]]
+                command["pivot"]["rows"] = [parse_dimension(row) for row in command["pivot"]["rowGroupBys"]]
+                del command["pivot"]["colGroupBys"]
+                del command["pivot"]["rowGroupBys"]
         cr.execute(
             """
             UPDATE spreadsheet_revision
@@ -32,7 +34,7 @@ def migrate(cr, version):
     SELECT id, commands
       FROM spreadsheet_revision
      WHERE commands LIKE '%UPDATE\_CELL%'
-        OR commands LIKE '%ADD\_PIVOT%'
+        OR commands LIKE '%\_PIVOT%'
     """
     with util.named_cursor(cr, BATCH_SIZE) as ncr:
         ncr.execute(query)

migrations/spreadsheet_edition/tests/test_add_pivot.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import json
 
 from odoo.addons.base.maintenance.migrations import util
@@ -30,6 +28,11 @@ class TestSpreadsheetInsertPivot(UpgradeCase):
                     "pivot": pivot,
                     "pivotId": "1",
                 },
+                {
+                    "type": "UPDATE_PIVOT",
+                    "pivot": pivot,
+                    "pivotId": "1",
+                },
             ],
         }
         field_name = "revision_uuid" if util.version_gte("saas~17.2") else "revision_id"
@@ -54,5 +57,9 @@ class TestSpreadsheetInsertPivot(UpgradeCase):
         data = json.loads(revisions[0].commands)
         pivot = data["commands"][0]["pivot"]
         self.assertEqual(pivot["measures"], [{"name": "MEASURE"}])
-        self.assertEqual(pivot["colGroupBys"], [{"name": "A"}, {"name": "B"}])
-        self.assertEqual(pivot["rowGroupBys"], [{"name": "C"}, {"name": "D"}])
+        self.assertEqual(pivot["columns"], [{"name": "A"}, {"name": "B"}])
+        self.assertEqual(pivot["rows"], [{"name": "C"}, {"name": "D"}])
+        updated_pivot = data["commands"][1]["pivot"]
+        self.assertEqual(updated_pivot["measures"], [{"name": "MEASURE"}])
+        self.assertEqual(updated_pivot["columns"], [{"name": "A"}, {"name": "B"}])
+        self.assertEqual(updated_pivot["rows"], [{"name": "C"}, {"name": "D"}])

migrations/spreadsheet_edition/tests/test_insert_pivot.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import json
 
 from odoo.addons.base.maintenance.migrations import util
@@ -86,9 +84,11 @@ class TestSpreadsheetInsertPivot(UpgradeCase):
         }
 
         if util.version_gte("saas~17.3"):
-            pivot["colGroupBys"] = [{"name": "A", "granularity": "month"}, {"name": "B"}]
-            pivot["rowGroupBys"] = [{"name": "C"}, {"name": "D"}]
+            pivot["columns"] = [{"name": "A", "granularity": "month"}, {"name": "B"}]
+            pivot["rows"] = [{"name": "C"}, {"name": "D"}]
             pivot["measures"] = [{"name": "MEASURE"}]
+            del pivot["colGroupBys"]
+            del pivot["rowGroupBys"]
 
         data = json.loads(revisions[0].commands)
         self.assertEqual(