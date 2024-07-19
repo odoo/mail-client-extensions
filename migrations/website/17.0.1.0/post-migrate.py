613f2986c7d4d9a54157be678b1be7b6b9132287
Dhwani Patel <dwa@odoo.com>
2024-07-19 11:04:35 +0000

    [FIX] website/17.0.1.0: activate/create a cowed view
    The database upgraded from a lower version to version 17, If a website has a
    header menu layout of "Hamburger menu" then a list of menus is not visible to
    the customer.
    
    Due to 'no_autohide_menu' view is deactivated(In case, If the customer has once
    used the header layout of 'Sidebar' then 'no_autohide_menu' is created for that
    website and after unused of that header the view is deactivated. because that
    view is used in that header layout) or unavailable in views as it was not
    required in the previous version for that header.
    From v17, 'template_header_hamburger' header layout should require an active
    "no_autohide_menu" view for that website.
    
    To solve this issue, I have created a cowed view for that particular website and
    for the already created cowed view, just activate them.
    
    opw - 4054201
    Req - 1840005
    
    closes odoo/upgrade#6294
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/website/17.0.1.0/post-migrate.py
@@ -0,0 +1,21 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    view = util.env(cr).ref("website.no_autohide_menu")
+    cr.execute(
+        """
+        SELECT h.website_id
+          FROM ir_ui_view h
+     LEFT JOIN ir_ui_view a
+            ON h.website_id = a.website_id
+           AND a.key = 'website.no_autohide_menu'
+           AND a.active
+         WHERE h.key = 'website.template_header_hamburger'
+           AND h.active
+           AND a.id is NULL
+      GROUP BY h.website_id
+        """
+    )
+    for (website_id,) in cr.fetchall():
+        view.copy({"website_id": website_id, "active": True})
