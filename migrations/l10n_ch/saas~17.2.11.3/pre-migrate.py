e06e7a6f73ead4aec73ba2b7145e628cadf84a30
Jinane Maksoud <maji@odoo.com>
2024-07-23 15:24:25 +0200

    [FIX] l10n_ch/saas~17.2: fix repartition lines order
    For dbs that had repartition lines created by the upgrade
    the sequence field was null and the order of insert is not
    the needed order.
    This will fix those lines that fail validation.
    
    closes odoo/upgrade#6303
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/l10n_ch/saas~17.2.11.3/pre-migrate.py
@@ -0,0 +1,51 @@
+import itertools
+
+
+def migrate(cr, version):
+    tax_names = [
+        "vat_sale_26",
+        "vat_sale_26_incl",
+        "vat_purchase_26",
+        "vat_purchase_26_incl",
+        "vat_purchase_26_invest",
+        "vat_purchase_26_invest_incl",
+        "vat_sale_38",
+        "vat_sale_38_incl",
+        "vat_purchase_38",
+        "vat_purchase_38_incl",
+        "vat_purchase_38_invest",
+        "vat_purchase_38_invest_incl",
+        "vat_sale_81",
+        "vat_sale_81_incl",
+        "vat_purchase_81",
+        "vat_purchase_81_incl",
+        "vat_purchase_81_invest",
+        "vat_purchase_81_invest_incl",
+        "vat_purchase_81_return",
+        "vat_purchase_81_reverse",
+    ]
+    cr.execute("SELECT id FROM res_company WHERE chart_template = 'ch'")
+    if not cr.rowcount:
+        return
+    companies = [row[0] for row in cr.fetchall()]
+    xmlids = tuple(f"{c_id}_{name}" for c_id, name in itertools.product(companies, tax_names))
+    cr.execute(
+        """
+        WITH tax_ids AS (
+             SELECT res_id
+               FROM ir_model_data
+              WHERE module = 'account'
+                AND name IN %s
+           )
+        UPDATE account_tax_repartition_line l
+           SET sequence = CASE WHEN l.repartition_type = 'base' THEN 1
+                          ELSE 2
+                          END
+          FROM tax_ids
+         WHERE l.tax_id = tax_ids.res_id
+           AND l.sequence IS NULL
+        """,
+        [
+            xmlids,
+        ],
+    )
