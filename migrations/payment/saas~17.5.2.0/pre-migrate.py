966621bb0d2c4940b8f6652a77f968ee74db8608
Lina (liew) <liew@odoo.com>
2024-07-29 15:10:57 +0200

    [IMP] payment_paypal/saas~17.5: migrate to new paypal apis
    See odoo/odoo#167402
    
     #  Please enter the commit message for your changes. Lines starting
    
    closes odoo/upgrade#6323
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/payment/saas~17.5.2.0/pre-migrate.py
+++ migrations/payment/saas~17.5.2.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.change_field_selection_values(cr, "payment.method", "support_refund", {None: "none"})
     util.rename_xmlid(cr, "payment.payment_method_emi", "payment.payment_method_emi_india")
+    util.remove_field(cr, "payment.provider.onboarding.wizard", "paypal_pdt_token")
---
+++ migrations/payment_paypal/saas~17.5.2.0/pre-migrate.py
@@ -0,0 +1,40 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # === PAYMENT PROVIDER === #
+
+    util.remove_field(cr, "payment.provider", "paypal_pdt_token")
+
+    cr.execute(
+        """
+        UPDATE payment_provider pp
+           SET redirect_form_view_id = NULL,
+               state='disabled'
+         WHERE code = 'paypal'
+        RETURNING pp.id,
+                  pp.name
+        """,
+    )
+    provider_disabled = cr.fetchall()
+    if provider_disabled:
+        util.add_to_migration_reports(
+            message=f"""
+                <details>
+                    <summary>
+                        Odoo updated to a new version of PayPal, you will need to update your configuration according to the documentation available at <a href="https://www.odoo.com/documentation/18.0/applications/finance/payment_providers/paypal.html">https://www.odoo.com/documentation/18.0/applications/finance/payment_providers/paypal.html</a><br>
+                        The following payment providers have been disabled:
+                    </summary>
+                <ul>
+                    {"".join(
+                        f"<li>{util.get_anchor_link_to_record('payment.provider', provider_id, name)}</li>"
+                        for provider_id, name in provider_disabled
+                    )}
+                </ul>
+                </details>
+            """,
+            category="Payment",
+            format="html",
+        )
+
+    util.remove_view(cr, "payment_paypal.redirect_form")
