fc186db5c91a5175c4e704589078c05fcf6f6f1a
std-odoo <std@odoo.com>
2024-07-30 15:51:40 +0200

    [IMP] documents_spreadsheet/saas~17.5: migrate documents spreadsheet after the document refactor
    Purpose
    =======
    The documents application has been refactored (share and folder have been removed, etc),
    and so we need to adapt documents_spreadsheet.
    
    Frozen spreadsheets
    ===================
    Before, we could freeze a spreadsheet, and share the frozen version of the spreadsheet.
    It was a bit weird, because if an internal user opens the link, he was redirected to the
    real spreadsheet and not to the frozen one (and so, he saw a different document if he
    was logged in).
    
    Now, everybody will see the same document (as long as they can access it). To freeze a
    spreadsheet, we duplicate the document, and so we can choose to share the "live"
    spreadsheet, or a frozen version (this is done with 2 different action).
    
    We also had a model `documents.shared.spreadsheet` to store the frozen spreadsheets,
    this is not needed anymore because we store the information on the documents.
    
    We migrate those `documents.shared.spreadsheet` only if there's only one spreadsheet,
    since the access token can not be shared.
    
    Default folder
    ==============
    There's a default folder for spreadsheet created outside of the documents application.
    
    Users can only see their own spreadsheets in that folder.
    
    Task-4014621
    COM odoo/odoo/pull/180264
    ENT odoo/enterprise/pull/65655
    
    Part-of: odoo/upgrade#6226
    Related: odoo/odoo#180264
    Related: odoo/enterprise#65655
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/documents_spreadsheet/saas~17.5.1.0/pre-migrate.py
@@ -0,0 +1,177 @@
+from odoo.upgrade import util
+
+documents_pre_migrate = util.import_script("documents/saas~17.5.1.4/pre-migrate.py")
+
+
+def migrate(cr, version):
+    # Migrate frozen spreadsheet on share, only if there's 1 and only 1 spreadsheet
+    # (because the token can not be reused)
+
+    # let the ORM create the alias
+    cr.execute("ALTER TABLE documents_document ALTER COLUMN alias_id DROP NOT NULL")
+    cr.execute("ALTER TABLE documents_document ALTER COLUMN document_token DROP NOT NULL")
+
+    util.create_column(
+        cr,
+        "documents_document",
+        "_upg_old_shared_spreadsheet_id",
+        "int4",
+    )
+
+    frozen_spreadsheets = """
+    WITH frozen_spreadsheets AS (
+       SELECT min(shared_spreadsheet.document_id) AS id,
+              share.access_token,
+              min(shared_spreadsheet.id) AS documents_shared_spreadsheet_id
+         FROM documents_shared_spreadsheet AS shared_spreadsheet
+         JOIN documents_share AS share
+           ON share.id = shared_spreadsheet.share_id
+     GROUP BY shared_spreadsheet.share_id, share.access_token, share.alias_id
+       HAVING COUNT(shared_spreadsheet.document_id) = 1
+    )
+    """
+
+    # create the frozen spreadsheet folder
+    cr.execute(
+        frozen_spreadsheets
+        + """
+        INSERT INTO documents_document (
+                    name, type, handler, folder_id, owner_id,
+                    access_via_link, access_internal, active
+        )
+             SELECT DISTINCT ON (doc.folder_id)
+                    'Frozen spreadsheets',  -- TODO: should we translate the name ?
+                    'folder',
+                    'frozen_folder',
+                    doc.folder_id,
+                    %(odoobot)s,
+                    'none',
+                    'view',
+                    TRUE
+               FROM frozen_spreadsheets
+               JOIN documents_document AS doc
+                 ON doc.id = frozen_spreadsheets.id
+        """,
+        {"odoobot": util.ref(cr, "base.user_root")},
+    )
+
+    documents_pre_migrate.fix_missing_document_tokens(cr)
+
+    # create one document per frozen spreadsheet in that folder
+    cr.execute(
+        frozen_spreadsheets
+        + """
+        INSERT INTO documents_document (
+                    name, type, handler, folder_id, owner_id, document_token,
+                    access_via_link, access_internal, is_access_via_link_hidden, active,
+                    _upg_old_shared_spreadsheet_id, attachment_id
+        )
+             SELECT 'Frozen: ' || spreadsheet.name,  -- TODO: should we translate the name ?
+                    'binary',
+                    'frozen_spreadsheet',
+                    frozen_folder.id,
+                    spreadsheet.owner_id,
+                    frozen.access_token,  -- temporary keep the token to ease the migration to documents_redirect
+                    'view',
+                    'none',
+                    TRUE,
+                    TRUE,
+                    documents_shared_spreadsheet_id,
+                    attachment.id
+               FROM frozen_spreadsheets AS frozen
+               JOIN documents_document AS spreadsheet
+                 ON spreadsheet.id = frozen.id
+               JOIN documents_document AS frozen_folder
+                 ON frozen_folder.folder_id = spreadsheet.folder_id
+                AND frozen_folder.handler = 'frozen_folder'
+          LEFT JOIN ir_attachment AS attachment
+                 ON attachment.res_model = 'documents.shared.spreadsheet'
+                AND attachment.res_field = 'spreadsheet_binary_data'
+                AND attachment.res_id = documents_shared_spreadsheet_id
+        """,
+        {"odoobot": util.ref(cr, "base.user_root")},
+    )
+
+    # move excel_export, datas, ... from <documents.shared.spreadsheet> to <documents.document>
+    util.explode_execute(
+        cr,
+        """
+        UPDATE ir_attachment
+           SET res_model = 'documents.document',
+               res_id = frozen_spreadsheet.id,
+               res_field = (
+                   CASE WHEN ir_attachment.res_field = 'spreadsheet_binary_data'
+                        THEN NULL
+                        ELSE ir_attachment.res_field
+                         END
+               ),
+               name = (
+                   CASE WHEN ir_attachment.name = 'spreadsheet_binary_data'
+                        THEN frozen_spreadsheet.name
+                        ELSE ir_attachment.name
+                         END
+               )
+          FROM documents_document AS frozen_spreadsheet
+         WHERE frozen_spreadsheet.handler = 'frozen_spreadsheet'
+           AND ir_attachment.res_model = 'documents.shared.spreadsheet'
+           AND ir_attachment.res_id = frozen_spreadsheet._upg_old_shared_spreadsheet_id
+           AND {parallel_filter}
+        """,
+        table="documents_document",
+        alias="frozen_spreadsheet",
+    )
+
+    documents_pre_migrate.fix_missing_document_tokens(cr)
+
+    # Move token from `documents.document` to `document.redirect`
+    util.explode_execute(
+        cr,
+        """
+     INSERT INTO documents_redirect (document_id, access_token)
+          SELECT document.id,
+                 document.document_token
+            FROM documents_document AS document
+           WHERE document.handler = 'frozen_spreadsheet'
+             AND {parallel_filter}
+         """,
+        table="documents_document",
+        alias="document",
+    )
+
+    # Now that the tokens have been moved to `documents_redirect`, reset the token on the `documents_document` table
+    cr.execute("UPDATE documents_document SET document_token = NULL WHERE handler = 'frozen_spreadsheet'")
+    documents_pre_migrate.fix_missing_document_tokens(cr)
+
+    cr.execute("ALTER TABLE documents_document ALTER COLUMN document_token SET NOT NULL")
+
+    # Standard spreadsheet have been redirected in `documents.redirect`,
+    # in the main documents migration script, we only want to keep frozen spreadsheet
+    cr.execute(
+        """
+        DELETE FROM documents_redirect
+              USING documents_document
+              WHERE documents_document.handler = 'spreadsheet'
+                AND documents_redirect.document_id = documents_document.id
+        """
+    )
+
+    util.rename_field(
+        cr,
+        "res.company",
+        "documents_spreadsheet_folder_id",
+        "document_spreadsheet_folder_id",
+    )
+    util.rename_field(
+        cr,
+        "res.config.settings",
+        "documents_spreadsheet_folder_id",
+        "document_spreadsheet_folder_id",
+    )
+
+    util.remove_model(cr, "documents.shared.spreadsheet")
+    util.remove_column(cr, "documents_document", "_upg_old_shared_spreadsheet_id")
+
+    xmlid_mapping = {
+        "documents_spreadsheet_folder": "document_spreadsheet_folder",
+    }
+    documents_pre_migrate.migrate_folders_xmlid(cr, "documents_project", xmlid_mapping)
---
+++ migrations/documents_spreadsheet/tests/test_17_5.py
@@ -0,0 +1,175 @@
+import base64
+import json
+
+from odoo.addons.base.maintenance.migrations import util
+from odoo.addons.base.maintenance.migrations.testing import UpgradeCase, change_version
+
+
+@change_version("saas~17.5")
+class TestSpreadsheetShareMigration(UpgradeCase):
+    def prepare(self):
+        if not util.version_gte("saas~17.4"):
+            return None
+
+        folders = self.env["documents.folder"].create(
+            [
+                {"name": "Spreadsheet_main"},
+                {"name": "Spreadsheet_other"},
+            ]
+        )
+
+        spreadsheets = self.env["documents.document"].create(
+            [
+                {
+                    "name": "spreadsheet",
+                    "handler": "spreadsheet",
+                    "folder_id": folder.id,
+                    "spreadsheet_binary_data": base64.b64encode(json.dumps({"test": "data"}).encode()).decode(),
+                    "is_multipage": False,
+                }
+                for folder in folders
+            ]
+        )
+
+        spreadsheet_shares = json.dumps(
+            [
+                {
+                    "excel_files": [],
+                    "spreadsheet_data": json.dumps({"test": "data A"}),
+                    "document_id": spreadsheets[0].id,
+                }
+            ]
+        )
+        share_single_doc_1 = (
+            self.env["documents.share"]
+            .with_context(
+                **spreadsheets[0].create_share()["context"],
+                default_spreadsheet_shares=spreadsheet_shares,
+            )
+            .create({})
+        )
+
+        spreadsheet_shares = json.dumps(
+            [
+                {
+                    "excel_files": [],
+                    "spreadsheet_data": json.dumps({"test": "data B"}),
+                    "document_id": spreadsheets[1].id,
+                }
+            ]
+        )
+        share_single_doc_2 = (
+            self.env["documents.share"]
+            .with_context(
+                **spreadsheets[1].create_share()["context"],
+                default_spreadsheet_shares=spreadsheet_shares,
+            )
+            .create({})
+        )
+
+        self.assertEqual(len(share_single_doc_2.freezed_spreadsheet_ids), 1)
+
+        spreadsheet_shares = json.dumps(
+            [
+                {
+                    "excel_files": [],
+                    "spreadsheet_data": json.dumps({"test": f"data {i}"}),
+                    "document_id": spreadsheet.id,
+                }
+                for i, spreadsheet in enumerate(spreadsheets)
+            ]
+        )
+        share_two_docs = self.env["documents.share"].create(
+            {
+                "type": "domain",
+                "folder_id": folders[0].id,
+                "domain": repr([("id", "in", spreadsheets.ids)]),
+                "document_ids": spreadsheets.ids,
+                "spreadsheet_shares": spreadsheet_shares,
+            }
+        )
+        self.assertEqual(len(share_two_docs.freezed_spreadsheet_ids), 2)
+
+        company = self.env["res.company"].create(
+            {
+                "name": "Test Company",
+                "documents_spreadsheet_folder_id": folders[1].id,
+            }
+        )
+
+        return {
+            "share_single_doc_1": share_single_doc_1.access_token,
+            "share_single_doc_2": share_single_doc_2.access_token,
+            "share_two_docs": share_two_docs.access_token,
+            "company": company.id,
+        }
+
+    def check(self, init):
+        if not init:
+            return
+
+        # TODO: check how that function behave on the runbot with the documents module tests
+        Document = self.env["documents.document"]
+        share_single_doc_1_token = init["share_single_doc_1"]
+        share_single_doc_2_token = init["share_single_doc_2"]
+        share_two_docs_token = init["share_two_docs"]
+
+        # Check that the old tokens are not on the `documents.document`
+        result = self.env["documents.document"].search(
+            [
+                (
+                    "document_token",
+                    "in",
+                    (
+                        share_single_doc_1_token,
+                        share_single_doc_2_token,
+                        share_two_docs_token,
+                    ),
+                )
+            ]
+        )
+        self.assertFalse(result)
+
+        # Check that no standard spreadsheets are in the `documents_redirect` table
+        spreadsheets = self.env["documents.document"].search([("handler", "=", "spreadsheet")])
+        self.assertFalse(self.env["documents.redirect"].search([("document_id", "in", spreadsheets.ids)]))
+
+        _get_redirection = self.env["documents.redirect"]._get_redirection
+
+        frozen_folders = Document.search([("name", "=", "Frozen spreadsheets")])
+        self.assertEqual(len(frozen_folders), 2)
+        for frozen_folder in frozen_folders:
+            self.assertTrue(frozen_folder.active)
+            self.assertEqual(frozen_folder.handler, "frozen_folder")
+            self.assertEqual(frozen_folder.access_via_link, "none")
+            self.assertEqual(frozen_folder.access_internal, "view")
+
+        self.assertEqual(
+            set(frozen_folders.mapped("folder_id.name")),
+            {"Spreadsheet_main", "Spreadsheet_other"},
+        )
+        self.assertEqual(Document.search_count([("handler", "=", "frozen_folder")]), 2)
+
+        self.assertFalse(_get_redirection(share_two_docs_token))
+        document_1 = _get_redirection(share_single_doc_1_token)
+        self.assertEqual(len(document_1), 1)
+        self.assertEqual(document_1.handler, "frozen_spreadsheet")
+        self.assertEqual(document_1.attachment_id.name, "Frozen: spreadsheet")
+        self.assertEqual(base64.b64decode(document_1.datas), b'{"test": "data A"}')
+        self.assertEqual(document_1.excel_export, b"UEsFBgAAAAAAAAAAAAAAAAAAAAAAAA==")
+        self.assertEqual(document_1.spreadsheet_data, json.dumps({"test": "data A"}))
+        self.assertIn(document_1.folder_id, frozen_folders)
+
+        document_2 = _get_redirection(share_single_doc_2_token)
+        self.assertEqual(len(document_2), 1)
+        self.assertEqual(document_2.handler, "frozen_spreadsheet")
+        self.assertEqual(document_2.attachment_id.name, "Frozen: spreadsheet")
+        self.assertEqual(base64.b64decode(document_2.datas), b'{"test": "data B"}')
+        self.assertEqual(document_2.spreadsheet_data, json.dumps({"test": "data B"}))
+        self.assertIn(document_2.folder_id, frozen_folders)
+
+        company = self.env["res.company"].browse(init["company"])
+        self.assertEqual(
+            company.document_spreadsheet_folder_id.name,
+            "Spreadsheet_other",
+        )
