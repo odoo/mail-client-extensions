829b479778ee91964500444a0859ecab45260297
Nikhil Kajavadra <nika@odoo.com>
2024-07-31 16:06:58 +0000

    [FIX] mrp_workorder/saas~16.4: compute employee_id
    Since odoo/enterprise@d4f65146 (16.0) there is a new field 'employee_id' in
    'mrp.workcenter.productivity'. Later in odoo/enterprise@d5faf8653 (16.0) the
    field was made a computed stored one.And then also changed in
    odoo/enterprise@dd5c31331 (17.0).
    In version 16, the compute method relies also on the boolean field
    'allow_employee',which was introduced with a default value false. Consequently,
    this condition of compute method will not be satisfied. So it can't be computed
    during the upgrade process from version 15 to 16.
    
    Additionally, we should consider records where the 'employee_id' field is null
    without computed in those dbs which are already in version 16 .
    So it needs to update in version 17.
    
    opw-4051305
    upg-1826818,1868458
    
    closes odoo/upgrade#6291
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/mrp_workorder/saas~16.4.1.0/pre-migrate.py
+++ migrations/mrp_workorder/saas~16.4.1.0/pre-migrate.py
@@ -1,11 +1,25 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.remove_field(cr, "mrp.workcenter", "allow_employee")
     util.remove_field(cr, "mrp.workorder", "allow_employee")
-
+    if not util.column_exists(cr, "mrp_workcenter_productivity", "employee_id"):
+        # mrp_workorder_hr was merged into mrp_workorder in this version
+        util.create_column(cr, "mrp_workcenter_productivity", "employee_id", "int4")
+    util.explode_execute(
+        cr,
+        """
+        UPDATE mrp_workcenter_productivity mwp
+           SET employee_id = emp.id
+          FROM hr_employee emp
+         WHERE mwp.user_id = emp.user_id
+           AND mwp.company_id = emp.company_id
+           AND mwp.employee_id IS NULL
+        """,
+        table="mrp_workcenter_productivity",
+        alias="mwp",
+    )
     util.remove_view(cr, "mrp_workorder.mrp_routing_workcenter_form_view_inherit")
     util.remove_view(cr, "mrp_workorder.mrp_workcenter_tree_view_inherit")
     util.remove_view(cr, "mrp_workorder.mrp_production_workorder_tree_editable_view_connect")
