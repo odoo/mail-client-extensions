84a6a6d5924487dd2e65d493c12a7174ba704761
Christophe Simonis <chs@odoo.com>
2024-08-07 11:24:15 +0200

    [FIX] auth_signup/saas~17.4: adapt mail template
    The signup URL is now obtained via the `_get_signup_url()` function.

--- migrations/auth_signup/saas~17.4.1.0/pre-migrate.py
+++ migrations/auth_signup/saas~17.4.1.0/pre-migrate.py
@@ -1,8 +1,32 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    # replace the usage of `signup_url` in mail templates
+    replacements = [
+        ("user_id", ".partner_id", "res.users"),
+        ("partner_id", "", "res.partner"),
+    ]
+    for varname, subfield, model in replacements:
+        util.replace_in_all_jsonb_values(
+            cr,
+            "mail_template",
+            "body_html",
+            f"{varname}.signup_url",
+            f"{varname}{subfield}._get_signup_url()",
+        )
+        util.replace_in_all_jsonb_values(
+            cr,
+            "mail_template",
+            "body_html",
+            "object.signup_url",
+            "object{subfield}.partner_id._get_signup_url()",
+            extra_filter=cr.mogrify("t.model = %s", [model]).decode(),
+        )
+
+    # bare replacement for other cases, asuming they are on `res.users`.
+    util.replace_in_all_jsonb_values(cr, "mail_template", "body_html", ".signup_url", ".partner_id._get_signup_url()")
+
     util.remove_field(cr, "res.partner", "signup_url")
     util.remove_field(cr, "res.partner", "signup_valid")
     util.remove_field(cr, "res.partner", "signup_token")
