8df1276981a76d485729212464485ec657012e0c
Thibault Francois <tfr@odoo.com>
2024-08-07 17:43:15 +0200

    [FIX] mail/saas~17.5: improve activity state search method
    Store TZ on user model to ease various mail related queries.
    
    See https://github.com/odoo/odoo/pull/175927
    
    Task-4105401
    
    closes odoo/upgrade#6358
    
    Related: odoo/odoo#175927
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/mail/saas~17.5.1.18/pre-migrate.py
+++ migrations/mail/saas~17.5.1.18/pre-migrate.py
@@ -3,3 +3,19 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.if_unchanged(cr, "mail.ir_cron_send_scheduled_message", util.update_record_from_xml)
+
+    util.create_column(cr, "mail_activity", "user_tz", "varchar")
+    util.explode_execute(
+        cr,
+        """
+        UPDATE mail_activity activity
+           SET user_tz = partner.tz
+          FROM res_users users
+          JOIN res_partner partner
+            ON partner.id = users.partner_id
+         WHERE users.id = activity.user_id
+           AND partner.tz IS DISTINCT FROM activity.user_tz
+        """,
+        table="mail_activity",
+        alias="activity",
+    )
