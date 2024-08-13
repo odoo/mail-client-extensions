54f638e15ec51f480e27a20b5fa208eefc068049
Hardy <hpr@odoo.com>
2024-08-13 14:31:17 +0530

    [FIX] pos_sale/saas~17.3: prevent memory error
    - after [pr](https://github.com/odoo/odoo/pull/144943/files)
    introduced compute stored field 'qty_delivered' for 'pos.order.line'
    
    - traceback:
    ```py
    2024-08-09 10:44:40,569 27 INFO db_1878656 odoo.modules.registry: module pos_sale: creating or updating database tables
    2024-08-09 10:44:41,511 27 INFO db_1878656 odoo.models: Prepare computation of pos.order.line.qty_delivered
    2024-08-09 11:26:54,349 27 WARNING db_1878656 odoo.modules.loading: Transient module states were reset
    2024-08-09 11:26:54,349 27 ERROR db_1878656 odoo.modules.registry: Failed to load registry
    2024-08-09 11:26:54,349 27 CRITICAL db_1878656 odoo.service.server: Failed to initialize database `db_1878656`.
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/saas-17.4/odoo/service/server.py", line 1302, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "<decorator-gen-16>", line 2, in new
      File "/home/odoo/src/odoo/saas-17.4/odoo/tools/func.py", line 84, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/saas-17.4/odoo/modules/registry.py", line 123, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/saas-17.4/odoo/modules/loading.py", line 476, in load_modules
        processed_modules += load_marked_modules(env, graph,
      File "/home/odoo/src/odoo/saas-17.4/odoo/modules/loading.py", line 364, in load_marked_modules
        loaded, processed = load_module_graph(
      File "/home/odoo/src/odoo/saas-17.4/odoo/modules/loading.py", line 205, in load_module_graph
        registry.init_models(env.cr, model_names, {'module': package.name}, new_install)
      File "/home/odoo/src/odoo/saas-17.4/odoo/modules/registry.py", line 603, in init_models
        func()
      File "/home/odoo/src/odoo/saas-17.4/odoo/addons/base/models/ir_model.py", line 1953, in _reflect_relation
        self.env.invalidate_all()
      File "/home/odoo/src/odoo/saas-17.4/odoo/api.py", line 727, in invalidate_all
        self.flush_all()
      File "/home/odoo/src/odoo/saas-17.4/odoo/api.py", line 739, in flush_all
        self[model_name].flush_model()
      File "/home/odoo/src/odoo/saas-17.4/odoo/models.py", line 6426, in flush_model
        self._flush(fnames)
      File "/home/odoo/src/odoo/saas-17.4/odoo/models.py", line 6482, in _flush
        OrderedSet(id_ for ids in dirty_field_ids.values() for id_ in ids),
      File "/home/odoo/src/odoo/saas-17.4/odoo/tools/misc.py", line 1026, in __init__
        self._map = dict.fromkeys(elems)
    MemoryError
    ```
    
    - UPG-1878656
    
    closes odoo/upgrade#6375
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/pos_sale/saas~17.3.1.1/pre-migrate.py
@@ -0,0 +1,34 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "pos_order_line", "qty_delivered", "float8", default=0)
+    util.explode_execute(
+        cr,
+        """
+        WITH lines AS (
+            SELECT line.id as line_id, sum(move.quantity) as qty_delivered
+              FROM pos_order_line line
+              JOIN pos_order pos
+                ON line.order_id = pos.id
+              JOIN stock_picking pick
+                ON pick.pos_order_id = pos.id
+              JOIN stock_picking_type p_type
+                ON pick.picking_type_id = p_type.id
+              JOIN stock_move move
+                ON move.picking_id = pick.id
+             WHERE pick.state = 'done'
+               AND p_type.code = 'outgoing'
+               AND move.state = 'done'
+               AND pos.state IN ('paid', 'done', 'invoiced')
+               AND move.product_id = line.product_id
+          GROUP BY line.id
+        )
+        UPDATE pos_order_line line
+           SET qty_delivered = lines.qty_delivered
+          FROM lines
+         WHERE lines.line_id = line.id
+        """,
+        table="pos_order_line",
+        alias="line",
+    )
