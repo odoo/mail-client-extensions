529f29c3577b8f5ab6e83010f41f5b4fa315c2c1
asno-odoo <asno@odoo.com>
2024-08-16 08:28:12 +0000

    [FIX] payment/0.0.0: defuse check required fields
    Some fields are required for specific providers. The check
    was correctly defused for saas~16.5 only, but any force update
    of record for any other place than saas~16.5 will lead to failure
    in case false as clients would have to fix their providers anyways
    and this also happens when test databases are neutralized.
    
    [TBG-1424](https://upgrade.odoo.com/web#id=1424&cids=1&menu_id=107&action=178&model=upgrade.request.traceback.group&view_type=form)
    
    closes odoo/upgrade#6387
    
    See: #5710
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/payment/0.0.0/pre-migrate.py
+++ migrations/payment/0.0.0/pre-migrate.py
@@ -1,5 +1,26 @@
 # -*- coding: utf-8 -*-
+
 from odoo.addons.base.maintenance.migrations import util
+
+if util.version_gte("saas~16.5"):
+    import logging
+
+    from odoo import exceptions, models
+
+    from odoo.addons.payment.models import payment_provider  # noqa
+
+    _logger = logging.getLogger("odoo.addons.base.maintenance.migrations.payment.0.0.0." + __name__)
+
+    class PaymentProvider(models.Model):
+        _inherit = "payment.provider"
+        _name = "payment.provider"
+        _module = "payment"
+
+        def _check_required_if_provider(self):
+            try:
+                super()._check_required_if_provider()
+            except exceptions.ValidationError as e:
+                _logger.warning("Invalid payment providers %s: %s", self.mapped("name"), e.args[0])
 
 
 def migrate(cr, version):
