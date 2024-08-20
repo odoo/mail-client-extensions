978678286735247bd4bba82aecde26ff0ca62600
UemuS <yaih@odoo.com>
2024-08-20 11:53:01 +0000

    [FIX] base/0.0.0: remove invalid company_id in partners
    Related PR: odoo/odoo#177221
    
    This change ensures data consistency between res_partner and res_company
    by updating the company_id field in res_partner to NULL
    when it doesn't align with the associated company.
    This prevents potential issues where partners belong to a company
    they don't represent.
    
    TBG-1428
    
    ```
    yaih@(none):yaih_1900953> SELECT id, name, company_id, partner_id FROM stock_warehouse WHERE Id = 2
    +----+------+------------+------------+
    | id | name | company_id | partner_id |
    |----+------+------------+------------|
    | 2  | DEMO | 2          | 819        |
    +----+------+------------+------------+
    SELECT 1
    Time: 0.034s
    yaih@(none):yaih_1900953> SELECT id, name, partner_id FROM res_company where id = 2
    +----+------+------------+
    | id | name | partner_id |
    |----+------+------------|
    | 2  | DEMO | 819        |
    +----+------+------------+
    SELECT 1
    Time: 0.054s
    yaih@(none):yaih_1900953> SELECT id, name, company_id FROM res_partner where id = 819
    +-----+------+------------+
    | id  | name | company_id |
    |-----+------+------------|
    | 819 | DEMO | 1          |
    +-----+------+------------+
    ```
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/saas-17.4/odoo/service/server.py", line 1302, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "<decorator-gen-16>", line 2, in new
      File "/home/odoo/src/odoo/saas-17.4/odoo/tools/func.py", line 84, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/saas-17.4/odoo/modules/registry.py", line 124, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/saas-17.4/odoo/modules/loading.py", line 514, in load_modules
        migrations.migrate_module(package, 'end')
      File "/home/odoo/src/odoo/saas-17.4/odoo/modules/migration.py", line 240, in migrate_module
        migrate(self.cr, installed_version)
      File "/tmp/tmp4fk_eovb/migrations/stock/saas~17.3.1.1/end-migrate.py", line 12, in migrate
        warehouse.write(new_types_vals)
      File "/home/odoo/src/odoo/saas-17.4/addons/mrp/models/stock_warehouse.py", line 264, in write
        return super(StockWarehouse, self).write(vals)
      File "/home/odoo/src/odoo/saas-17.4/addons/stock/models/stock_warehouse.py", line 199, in write
        res = super().write(vals)
      File "/home/odoo/src/odoo/saas-17.4/odoo/models.py", line 4597, in write
        self._check_company()
      File "/home/odoo/src/odoo/saas-17.4/odoo/models.py", line 4233, in _check_company
        raise UserError("\n".join(lines))
    odoo.exceptions.UserError: Incompatible companies on records:
    - “Freelancer” belongs to company “Freelancer” and “Address” (partner_id: 'Freelancer') belongs to another company.
    ```
    
    closes odoo/upgrade#6396
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/base/0.0.0/pre-remove-invalid-partner-company.py
@@ -0,0 +1,12 @@
+def migrate(cr, version):
+    cr.execute(
+        """
+        UPDATE res_partner p
+           SET company_id = NULL
+          FROM res_company c
+         WHERE p.id = c.partner_id
+           AND p.company_id != c.id
+           AND p.is_company IS TRUE
+           AND p.company_id IS NOT NULL
+        """
+    )
