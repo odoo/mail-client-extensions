de879b48cdeaddcca3bb08a2e830b9e7ce6c8050
Stéphane Diez <snd@odoo.com>
2024-08-22 13:35:41 +0200

    [IMP] mrp_mps/saas~17.5: remove manufacturing_period_to_display
    This PR remove `manufacturing_period_to_display` from `res.company` and
    `res.config.settings` and replaces it with 4 new integer fields that will
    fulfil the same functions:
    - manufacturing_period_to_display_year
    - manufacturing_period_to_display_month
    - manufacturing_period_to_display_week
    - manufacturing_period_to_display_day
    
    Thus, the original value it contains needs to be moved to the field
    corresponding to the setting selected in `manufacturing_period`.
    We don't include 'year' in the script since it is an entirely new option.
    
    see odoo/enterprise#68548
    
    closes odoo/upgrade#6406
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/mrp_mps/saas~17.5.1.0/pre-migrate.py
+++ migrations/mrp_mps/saas~17.5.1.0/pre-migrate.py
@@ -3,3 +3,24 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "mrp.bom", "schedule_count")
+
+    util.create_column(cr, "res_company", "manufacturing_period_to_display_month", "int4", default=12)
+    util.create_column(cr, "res_company", "manufacturing_period_to_display_week", "int4", default=12)
+    util.create_column(cr, "res_company", "manufacturing_period_to_display_day", "int4", default=30)
+    cr.execute("""
+        UPDATE res_company
+           SET manufacturing_period_to_display_month = manufacturing_period_to_display
+         WHERE manufacturing_period = 'month'
+    """)
+    cr.execute("""
+        UPDATE res_company
+           SET manufacturing_period_to_display_week = manufacturing_period_to_display
+         WHERE manufacturing_period = 'week'
+    """)
+    cr.execute("""
+        UPDATE res_company
+           SET manufacturing_period_to_display_day = manufacturing_period_to_display
+         WHERE manufacturing_period = 'day'
+    """)
+    util.remove_field(cr, "res.company", "manufacturing_period_to_display")
+    util.remove_field(cr, "res.config.settings", "manufacturing_period_to_display")
