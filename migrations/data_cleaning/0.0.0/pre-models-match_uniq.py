4c6f6a0aee80c52cdda03da7099038eea0d67fb7
Agustín Castro Bugallo <agcb@odoo.com>
2024-08-23 12:18:10 +0000

    [FIX] data_cleaning/0.0.0: Match unique for data_merge.rule
    The module `data_merge` has been merged into `data_cleaning` in saas~17.4.
    It's very easy for data merge rules to trigger the unique name constraint,
    so there was some script in `data_merge` to avoid that.
    
    Now, the class is marked as `_match_uniq` to tackle the issue in this module.
    
    ```
    psycopg2.errors.UniqueViolation: duplicate key value violates unique constraint "data_merge_model_uniq_name"
    DETAIL:  Key (name)=(Contact) already exists.
    ```
    
    tbg-1459
    
    closes odoo/upgrade#6412
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/data_cleaning/0.0.0/pre-models-match_uniq.py
@@ -0,0 +1,16 @@
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    pass
+
+
+if util.version_gte("saas~17.4"):
+    from odoo import models
+
+    from odoo.addons.data_cleaning.models import data_merge_rule  # noqa
+
+    class DataMergeRule(models.Model):
+        _inherit = "data_merge.rule"
+        _module = "data_cleaning"
+        _match_uniq = True
