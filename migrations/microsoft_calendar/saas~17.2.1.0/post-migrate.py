994ec5cd7502e8051c728430474a9fc00a89642a
diagnoza <owi@odoo.com>
2024-08-23 12:23:21 +0000

    [FIX] microsoft_calendar/saas~17.2: set calendar_default_privacy
    There is a required field called 'calendar_default_privacy' on 'res.users.settings' that
    must be set when inserting any records into res_users_settings. The value that we set is
    the default value for this field:
    https://github.com/odoo/odoo/blob/0b2492e8d7698d999b64a8b9c8eccd99e98dbfe0/addons/calendar/models/res_users_settings.py\#L13
    
    closes odoo/upgrade#6413
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/microsoft_calendar/saas~17.2.1.0/post-migrate.py
+++ migrations/microsoft_calendar/saas~17.2.1.0/post-migrate.py
@@ -6,18 +6,25 @@ def migrate(cr, version):
     User settings table for calendar users (Community PR:151537).
     """
     # Upsert the Microsoft Calendar Credentials into the Res Users Settings table.
+    p, q = (
+        (", calendar_default_privacy", ", 'public'")
+        if util.column_exists(cr, "res_users_settings", "calendar_default_privacy")
+        else ("", "")
+    )
     cr.execute(
-        """
+        f"""
         INSERT INTO res_users_settings (
                         user_id,
                         microsoft_calendar_sync_token,
                         microsoft_synchronization_stopped,
                         microsoft_last_sync_date
+                        {p}
                     )
              SELECT usr.id,
                     cred_user.calendar_sync_token,
                     cred_user.synchronization_stopped,
                     cred_user.last_sync_date
+                    {q}
                FROM microsoft_calendar_credentials AS cred_user
                JOIN res_users AS usr
                  ON usr.microsoft_calendar_account_id = cred_user.id
