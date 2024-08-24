904f69d1dbd408397f8b5657812b13d952415b56
diagnoza <owi@odoo.com>
2024-08-24 22:06:20 +0000

    [IMP] base/saas~16.2: limit update to standard modules
    We should only care about updating the 'module' column for records coming
    from standard modules. Custom modules may not neceserilly need this change,
    they should be updated separately with custom migrations scripts.
    
    closes odoo/upgrade#6415
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/base/saas~16.2.1.3/pre-05-l10n-to-account.py
+++ migrations/base/saas~16.2.1.3/pre-05-l10n-to-account.py
@@ -1,24 +1,31 @@
-# -*- coding: utf-8 -*-
+from odoo import modules
 
 
 def migrate(cr, version):
     # stay consistent with how the xml ids are dynamically generated
     # (now attached to account instead of the l10n module)
-    cr.execute("""
+    cr.execute(
+        """
         UPDATE ir_model_data
            SET module = 'account'
          WHERE model IN %s
            AND module LIKE 'l10n%%'
            AND module NOT LIKE '%%demo%%'
-    """, [(
-        'account.account',
-        'account.group',
-        'account.tax',
-        'account.tax.repartition.line',
-        # 'account.tax.group', # Those are handled in `account/saas~16.2.1.2/pre-migrate.py`
-        'account.fiscal.position.tax',
-        'account.fiscal.position.account',
-        'account.fiscal.position',
-        'account.reconcile.model',
-        'account.reconcile.model.line',
-    )])
+           AND module IN %s -- limit to standard modules
+        """,
+        [
+            (
+                "account.account",
+                "account.group",
+                "account.tax",
+                "account.tax.repartition.line",
+                # 'account.tax.group', # Those are handled in `account/saas~16.2.1.2/pre-migrate.py`
+                "account.fiscal.position.tax",
+                "account.fiscal.position.account",
+                "account.fiscal.position",
+                "account.reconcile.model",
+                "account.reconcile.model.line",
+            ),
+            tuple(modules.get_modules()),
+        ],
+    )
