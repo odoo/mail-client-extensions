42a999a43388a0c2c1caa9fa1fb90610314b526d
diagnoza <owi@odoo.com>
2024-08-27 12:26:16 +0000

    [FIX] payment/0.0.0: execute script until 18.0
    Script should also execute for upgrades to 18.0, once
    18.0 is finally released.
    
    closes odoo/upgrade#6423
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/payment/0.0.0/pre-migrate.py
+++ migrations/payment/0.0.0/pre-migrate.py
@@ -24,5 +24,5 @@ if util.version_gte("saas~16.5"):
 
 
 def migrate(cr, version):
-    if util.version_between("17.0", "saas~17.5"):
+    if util.version_between("17.0", "18.0"):
         util.delete_unused(cr, "payment.payment_method_acss_debit")
