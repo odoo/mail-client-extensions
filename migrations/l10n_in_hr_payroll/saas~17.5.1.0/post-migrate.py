eea94751c933226a8377b19e52d9c194af887d76
ravb-odoo <ravb@odoo.com>
2024-08-29 15:24:13 +0530

    [IMP] l10n_in_hr_payroll/saas~17.5: update structure type in contracts
    This PR focuses on updating the structure type in contracts for Indian
    employees. Previously, the `Indian: Regular` structure was under the
    employee structure type, but now distinct structure types have been
    created for all Indian structures. As a result, the structure type
    in contracts must be set based on the default_struc_id.
    
    ENT PR: https://github.com/odoo/enterprise/pull/68136
    
    Task-3505344
    
    closes odoo/upgrade#6441
    
    Related: odoo/enterprise#68136
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/l10n_in_hr_payroll/saas~17.5.1.0/post-migrate.py
+++ migrations/l10n_in_hr_payroll/saas~17.5.1.0/post-migrate.py
@@ -1,3 +1,5 @@
+from psycopg2.extras import Json
+
 from odoo.upgrade import util
 
 
@@ -9,4 +11,33 @@ def migrate(cr, version):
            WHERE id = %s
         """,
         [util.ref(cr, "l10n_in_hr_payroll.seq_payment_advice")],
+    )
+
+    regular_pay_structure = util.ref(cr, "l10n_in_hr_payroll.hr_payroll_structure_in_employee_salary")
+    worker_pay_structure = util.ref(cr, "l10n_in_hr_payroll.structure_worker_0001")
+    regular_pay_structure_type = util.ref(cr, "l10n_in_hr_payroll.hr_payroll_salary_structure_type_ind_emp_pay")
+    worker_pay_structure_type = util.ref(cr, "l10n_in_hr_payroll.hr_payroll_salary_structure_type_ind_worker")
+
+    mapping = {
+        regular_pay_structure: regular_pay_structure_type,
+        worker_pay_structure: worker_pay_structure_type,
+    }
+    cr.execute(
+        """
+        UPDATE hr_contract
+           SET structure_type_id = (%s::jsonb->hc.structure_type_id::text)::int
+          FROM hr_contract hc
+          JOIN res_company rc
+            ON hc.company_id = rc.id
+          JOIN res_partner rp
+            ON rc.partner_id = rp.id
+          JOIN res_country re
+            ON rp.country_id = re.id
+          JOIN hr_payroll_structure_type st
+            ON st.id = hc.structure_type_id
+         WHERE hr_contract.id = hc.id
+           AND re.code = 'IN'
+           AND st.default_struct_id IN %s
+        """,
+        [Json(mapping), tuple(mapping)],
     )
