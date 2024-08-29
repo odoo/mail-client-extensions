298791161e6734dd1b05354bf4ac598876d19207
Shaan Thakkar (THSH) <thsh@odoo.com>
2024-08-29 17:50:05 +0530

    [IMP] event_sale/saas~18.1: rename field
    Rename the field sale_price_subtotal to sale_price_total
    Because now we want to include taxes and discount also.
    
    Related: https://github.com/odoo/odoo/pull/178326
    
    Task-4107360
    
    closes odoo/upgrade#6443
    
    Related: odoo/odoo#178326
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/event_sale/saas~18.1.1.3/pre-migrate.py
+++ migrations/event_sale/saas~18.1.1.3/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "event_sale.event_registration_view_kanban")
+    util.rename_field(cr, "event.event", "sale_price_subtotal", "sale_price_total")
