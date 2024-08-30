acb3168d971cbf222532d78f62ce51eff5e18e6e
Jitendra Prajapat <jipr@odoo.com>
2024-08-30 14:28:28 +0530

    [IMP] point_of_sale/saas~17.5: remove start category fields
    In this commit:
    ================
    `iface_start_categ_id` and `start_category` fields have been removed from the
    point_of _sale module due to the removing of the start category feature.
    
    task - 4147445
    
    Related Community PR: https://github.com/odoo/odoo/pull/178556
    
    closes odoo/upgrade#6449
    
    Related: odoo/odoo#178556
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/point_of_sale/saas~17.5.1.0.1/pre-migrate.py
+++ migrations/point_of_sale/saas~17.5.1.0.1/pre-migrate.py
@@ -33,3 +33,8 @@ def migrate(cr, version):
     if util.module_installed(cr, "pos_self_order"):
         util.move_field_to_module(cr, "product.template", "description_self_order", "pos_self_order", "point_of_sale")
         util.rename_field(cr, "product.template", "description_self_order", "public_description")
+
+    util.remove_field(cr, "pos.config", "iface_start_categ_id")
+    util.remove_field(cr, "pos.config", "start_category")
+    util.remove_field(cr, "res.config.settings", "pos_iface_start_categ_id")
+    util.remove_field(cr, "res.config.settings", "pos_start_category")
