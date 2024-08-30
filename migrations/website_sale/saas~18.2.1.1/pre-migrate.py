94045106720bc736d3ad55b4f43825ad8a1342b2
Victor Feyens <vfe@odoo.com>
2024-08-30 14:28:25 +0200

    [IMP] website_sale/saas~18.2: remove fields
    See odoo/odoo#186923
    See odoo/enterprise#73652
    
    closes odoo/upgrade#6754
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- migrations/website_sale/saas~18.2.1.1/pre-migrate.py
+++ migrations/website_sale/saas~18.2.1.1/pre-migrate.py
@@ -7,3 +7,8 @@ def migrate(cr, version):
 
     util.remove_record(cr, "website_sale.s_dynamic_snippet_products_000_js")
     util.remove_record(cr, "website_sale.s_popup_000_js")
+
+    util.remove_field(cr, "website", "all_pricelist_ids")
+    util.remove_field(cr, "website", "fiscal_position_id")
+    util.remove_field(cr, "website", "pricelist_id")
+    util.remove_field(cr, "res.partner", "last_website_so_id")
