ce1f690d51aa2651bfc9a1cb759aefc3bdb97bd9
kpt-odoo <kpt@odoo.com>
2024-09-02 10:09:40 +0530

    [IMP] website_sale/saas~17.5: remove action
    In this commit, removed action "ir_actions_server_sale_cart_recovery_email"
    
    Related PR: https://github.com/odoo/odoo/pull/179448
    
    task:4102978
    
    closes odoo/upgrade#6470
    
    Related: odoo/odoo#179448
    Related: odoo/enterprise#69616
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- migrations/website_sale/saas~17.5.1.1/pre-migrate.py
+++ migrations/website_sale/saas~17.5.1.1/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.rename_xmlid(cr, "website_sale.row_addresses", "website_sale.address_row")
     util.rename_field(cr, "res.config.settings", "module_website_sale_picking", "module_website_sale_collect")
+    util.remove_record(cr, "website_sale.ir_actions_server_sale_cart_recovery_email")
