5945272bb041b52725fd7633cd83d0a2e4bb2182
Arnold Moyaux <arm@odoo.com>
2024-09-05 16:51:01 +0200

    [IMP] stock/saas~17.5.1.1: Remove the schduler wizard
    We removed the scheduler wizard and instead call the cron
    action directly
    
    odoo/odoo#179005
    
    closes odoo/upgrade#6469
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>
    Co-authored-by: Christophe Simonis <KangOl@users.noreply.github.com>

--- migrations/stock/saas~17.5.1.1/pre-migrate.py
+++ migrations/stock/saas~17.5.1.1/pre-migrate.py
@@ -6,3 +6,5 @@ def migrate(cr, version):
     util.remove_field(cr, "stock.picking", "hide_picking_type")
     util.remove_field(cr, "res.config.settings", "group_stock_picking_wave")
     util.remove_record(cr, "stock.group_stock_picking_wave")
+    util.remove_model(cr, "stock.scheduler.compute")
+    util.remove_record(cr, "stock.menu_procurement_compute")
