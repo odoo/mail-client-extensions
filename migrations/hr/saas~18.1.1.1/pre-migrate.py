15891dc37b613c8ce603de812f010169fa03f061
Nathaniel (naja) <naja@odoo.com>
2024-09-09 12:02:57 +0200

    [IMP] hr/saas~18.1: make unarchive wizard work with multiple employees
    Related: odoo/odoo#179609
    Related: odoo/enterprise#69734
    
    Change to the model `hr.departure.wizard`:
    * remove `employee_id` m2o field to replace with m2m (`employee_ids`
    * in documents_hr overload remove related `private_email`
    
    task-3987430
    
    closes odoo/upgrade#6476
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr/saas~18.1.1.1/pre-migrate.py
+++ migrations/hr/saas~18.1.1.1/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_record(cr, "hr.hr_employee_comp_rule")
     util.remove_record(cr, "hr.hr_employee_public_comp_rule")
+    util.remove_field(cr, "hr.departure.wizard", "employee_id")
