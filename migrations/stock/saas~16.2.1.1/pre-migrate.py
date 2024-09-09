89b01f077f677d72ce7bc4b31a8e5560ed9055e4
Carsten Wolff (cawo) <cawo@odoo.com>
2024-09-09 07:09:09 +0000

    [IMP] stock/saas~16.2.1.1: explode query on product_packaging
    upg-2020451
    
    This table can be large. Found through
    https://internal-monitoring.odoo.com/goto/z2Zl1teIg?orgId=3
    
    Note that `util.parallel_execute()` has become safe(r) for `DELETE` recently.
    
    before (prod):
    ```
    2024-09-06 07:42:18,276 7948 INFO db_2020451 odoo.upgrade.util.records: remove deprecated built-in view stock.report_mrp_line (ID 671)
    2024-09-06 11:38:06,330 7948 INFO db_2020451 odoo.modules.migration: module stock: Running migration [>saas~16.3.1.1] pre-migrate
    ```
    
    after (test.u.o.c):
    ```
    2024-09-06 16:49:49,256 7945 INFO cawo_2020451_17.0 odoo.upgrade.util.records: remove deprecated built-in view stock.report_mrp_line (ID 671)
    2024-09-06 18:23:41,240 7945 INFO cawo_2020451_17.0 odoo.upgrade.util.pg: [100.00%] 169/169 queries processed in 1:33:51.647062
    2024-09-06 18:23:41,284 7945 INFO cawo_2020451_17.0 odoo.modules.migration: module stock: Running migration [>saas~16.3.1.1] pre-migrate
    ```
    
    closes odoo/upgrade#6474
    
    Signed-off-by: Jinane Maksoud (maji) <maji@odoo.com>

--- migrations/stock/saas~16.2.1.1/pre-migrate.py
+++ migrations/stock/saas~16.2.1.1/pre-migrate.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
@@ -13,15 +12,21 @@ def migrate(cr, version):
     util.remove_record(cr, "stock.stock_replenishment_report_product_template_action")
     util.remove_record(cr, "stock.stock_replenishment_product_product_action")
     util.rename_model(
-        cr, "report.stock.report_product_product_replenishment", "stock.forecasted_product_product", False
+        cr,
+        "report.stock.report_product_product_replenishment",
+        "stock.forecasted_product_product",
+        False,  # noqa: FBT003
     )
     util.rename_model(
-        cr, "report.stock.report_product_template_replenishment", "stock.forecasted_product_template", False
+        cr,
+        "report.stock.report_product_template_replenishment",
+        "stock.forecasted_product_template",
+        False,  # noqa: FBT003
     )
 
     # `product_id` is required on packagings, deletes the ones without this
     # field as they shouldn't exist at the first place.
-    cr.execute("DELETE FROM product_packaging WHERE product_id IS NULL")
+    util.explode_execute(cr, "DELETE FROM product_packaging WHERE product_id IS NULL", table="product_packaging")
 
     # Set the right code for dropship's pickings.
     query = """
