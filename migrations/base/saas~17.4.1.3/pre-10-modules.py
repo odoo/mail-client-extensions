7be49cf664055e1af348a69fa20e0f8705346100
Agustín Castro Bugallo <agcb@odoo.com>
2024-09-10 13:00:36 +0000

    [FIX] base/saas~17.4: Don't init module `event_product`
    The record `event_sale.product_product_event` has the noupdate flag and it's
    moved to the new module `event_product`. The xmlid is renamed during the
    upgrade.
    
    Using the init mode might add an unwanted update of the record, which might
    trigger validation errors.
    
    upg-2095577
    upg-1878961
    
    ```
    odoo.tools.convert.ParseError: while parsing /home/odoo/src/odoo/saas-17.4/addons/event_product/data/event_product_data.xml:9
    This product is already being used in posted Journal Entries.
    If you want to change its Unit of Measure, please archive this product and create a new one.
    ```
    
    closes odoo/upgrade#6483
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/saas~17.4.1.3/pre-10-modules.py
+++ migrations/base/saas~17.4.1.3/pre-10-modules.py
@@ -4,7 +4,7 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.merge_module(cr, "data_merge", "data_cleaning")
     util.merge_module(cr, "im_livechat_mail_bot", "mail_bot")
-    util.force_upgrade_of_fresh_module(cr, "event_product")
+    util.force_upgrade_of_fresh_module(cr, "event_product", init=False)
     util.merge_module(cr, "l10n_cl_edi_boletas", "l10n_cl_edi")
     util.merge_module(cr, "account_audit_trail", "account")
     util.remove_module(cr, "l10n_de_audit_trail")
