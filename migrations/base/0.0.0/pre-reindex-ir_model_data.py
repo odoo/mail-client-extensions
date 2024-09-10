030d61866b6db9a57754dfb542b03b68cfffc0c6
Carsten Wolff (cawo) <cawo@odoo.com>
2024-09-10 08:14:20 +0000

    [IMP] base/0.0.0: run cleanup of export imd in parallel
    upg-2021456
    
    Found in the PG log:
    ```
    2024-09-08 04:15:59.362 CEST [1888052] db_2021456@db_2021456 LOG:  duration: 4162078.999 ms  statement:
                        WITH info AS (
                            SELECT model,
                                   count(*) AS rem
                              FROM ir_model_data
                             WHERE module = '__export__'
                          GROUP BY model
                            HAVING count(*) > 10000
                        ),
                        __ AS (
                        DELETE FROM ir_model_data d
                              USING info
                              WHERE d.module = '__export__'
                                AND d.model = info.model
                        )
                        SELECT * FROM info
    ```
    The reason being the sheer number of deleted rows:
    ```
    cawo_2021456=> SELECT model, count(*) AS rem FROM ir_model_data WHERE module = '__export__' GROUP BY model HAVING count(*) > 10000;
                 model             |   rem
    -------------------------------+---------
     account.move                  |  279204
     account.move.line             | 2047596
     account.payment               |   63588
     crm.event.log                 |   17625
     crm.lead                      |   76092
     ddmrp.product.planner         |   10309
     inventory.account.report.line |   16793
     mail.message                  |  102324
     mailing.contact               |   96981
     mrp.automation.result         |  137822
     mrp.automation.result.line    |   46569
     mrp.bom                       |  148960
     mrp.bom.line                  |   57337
     mrp.production                |   52009
     mrp.workorder                 |   22414
     product.attribute.value       |   22206
     product.product               |   48236
     product.supplierinfo          |   10613
     product.template              |   19083
     project.task                  |   16944
     purchase.order                |   38948
     res.partner                   |  336906
     sale.order                    |  169737
     sale.order.line               |  126964
     sale.workflow.hold            |   45181
     stock.inventory.line          |   23961
     stock.move                    | 1211436
     stock.picking                 |  140618
     stock.production.lot          |  156679
     stock.quant                   |   17260
     stock.scrap                   |   65849
     voip.phonecall                |   77951
    (32 rows)
    ```
    To speed this up on DBs with such large numbers, parallelize the deletion.
    
    closes odoo/upgrade#6479
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/0.0.0/pre-reindex-ir_model_data.py
+++ migrations/base/0.0.0/pre-reindex-ir_model_data.py
@@ -8,25 +8,26 @@ def migrate(cr, version):
     if not util.str2bool(os.getenv("UPG_NO_CLEAN_EXPORT_IMD", "0")):
         cr.execute(
             """
-            WITH info AS (
                 SELECT model,
                        count(*) AS rem
                   FROM ir_model_data
                  WHERE module = '__export__'
               GROUP BY model
                 HAVING count(*) > 10000
-            ),
-            __ AS (
-            DELETE FROM ir_model_data d
-                  USING info
-                  WHERE d.module = '__export__'
-                    AND d.model = info.model
-            )
-            SELECT * FROM info
             """
         )
         if cr.rowcount:
-            util._logger.info("Cleaned __export__ entries from ir_model_data: %s", cr.fetchall())
+            to_delete = cr.fetchall()
+            qry = cr.mogrify(
+                """
+                DELETE FROM ir_model_data
+                      WHERE module = '__export__'
+                        AND model IN %s
+                """,
+                [tuple(t[0] for t in to_delete)],
+            ).decode()
+            util.explode_execute(cr, qry, table="ir_model_data")
+            util._logger.info("Cleaned __export__ entries from ir_model_data: %s", to_delete)
 
     if not util.str2bool(os.getenv("UPG_NO_REINDEX_IMD", "0")):
         cr.execute("REINDEX TABLE ir_model_data")
