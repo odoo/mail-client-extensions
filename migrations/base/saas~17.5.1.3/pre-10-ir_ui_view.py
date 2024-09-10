962da4a879e23d3c36d7750b84e5706a901997c2
Christophe Simonis <chs@odoo.com>
2024-09-10 17:04:54 +0200

    [FIX] base/saas~17.5
    Update selection value with dedicated funciton instead of direct query.
    
    Also lint file.
    
    closes odoo/upgrade#6484
    
    Related: odoo/upgrade-util#134
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/saas~17.5.1.3/pre-10-ir_ui_view.py
+++ migrations/base/saas~17.5.1.3/pre-10-ir_ui_view.py
@@ -1,11 +1,12 @@
 from odoo.upgrade import util
 
+
 def migrate(cr, version):
     util.replace_in_all_jsonb_values(
         cr,
         "ir_ui_view",
         "arch_db",
-        util.PGRegexp(r'(</?)tree([ >])'),
+        util.PGRegexp(r"(</?)tree([ >])"),
         r"\1list\2",
     )
     util.replace_in_all_jsonb_values(
@@ -13,8 +14,12 @@ def migrate(cr, version):
         "ir_ui_view",
         "arch_db",
         util.PGRegexp(r"\ytree_view_ref\y"),
-        'list_view_ref',
+        "list_view_ref",
     )
-    cr.execute(r"UPDATE ir_ui_view SET type = 'list' WHERE type = 'tree'")
-    cr.execute(r"UPDATE ir_act_window SET view_mode = regexp_replace(view_mode, '\ytree\y', 'list', 'g') WHERE view_mode LIKE '%tree%'")
-    cr.execute(r"UPDATE ir_act_window SET context = regexp_replace(context, '\ytree_view_ref\y', 'list_view_ref', 'g') WHERE context LIKE '%tree\_view\_ref%'")
+    util.change_field_selection_values(cr, "ir.ui.view", "type", {"tree": "list"})
+    cr.execute(
+        r"UPDATE ir_act_window SET view_mode = regexp_replace(view_mode, '\ytree\y', 'list', 'g') WHERE view_mode LIKE '%tree%'"
+    )
+    cr.execute(
+        r"UPDATE ir_act_window SET context = regexp_replace(context, '\ytree_view_ref\y', 'list_view_ref', 'g') WHERE context LIKE '%tree\_view\_ref%'"
+    )
