c49c3a5baf4c998a1fa2bd1c85f65912e87fd687
Yannick Tivisse <yti@odoo.com>
2024-09-11 04:17:14 +0200

    [IMP] base/saas~17.5: Move res.bank.country_code to base
    Com PR: https://github.com/odoo/odoo/pull/179837
    Ent PR: https://github.com/odoo/enterprise/pull/68103
    
    closes odoo/upgrade#6485
    
    Taskid: 3326388
    Related: odoo/odoo#179837
    Related: odoo/enterprise#68103
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/base/saas~17.5.1.3/pre-migrate.py
+++ migrations/base/saas~17.5.1.3/pre-migrate.py
@@ -12,3 +12,6 @@ def migrate(cr, version):
     )
     util.rename_xmlid(cr, "base.lang_sr_RS", "base.lang_sr@Cyrl")
     util.remove_view(cr, "base.res_users_identitycheck_view_form_revokedevices")
+
+    util.move_field_to_module(cr, "res.bank", "country_code", "l10n_pe", "base")
+    util.move_field_to_module(cr, "res.bank", "country_code", "l10n_hk_hr_payroll", "base")
