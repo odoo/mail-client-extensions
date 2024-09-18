c35b0e5bcfa893b5ad737168a1255daf3e4c3f4e
Gabriel (gdpf) <gdpf@odoo.com>
2024-09-18 11:08:05 +0200

    [IMP] sale_subscription/saas~18.1: remove sale order alert
    This commit relates to the 'base_automation' module dependency removal
    from 'sale_subscription' as from now on all the automation will be done
    in Studio. As a result, we are dropping the 'sale.order.alert' model
    and its derived views. In addition, we are also removing the 'health'
    field from sale orders and its reports, as it will be no longer used.
    
    task: 4181465
    See odoo/enterprise#70180
    
    closes odoo/upgrade#6527
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/sale_subscription/saas~18.1.1.1/pre-migrate.py
+++ migrations/sale_subscription/saas~18.1.1.1/pre-migrate.py
@@ -9,3 +9,14 @@ def migrate(cr, version):
             cr, {reason_3: end_of_contract}, "sale.order.close.reason", replace_xmlid=False
         )
         util.remove_record(cr, "sale_subscription.close_reason_3")
+
+    # Remove SaleOrderAlert and its views. From now on, automations will be made in Studio.
+    util.remove_field(cr, "base.automation", "is_sale_order_alert")
+    util.remove_model(cr, "sale.order.alert")
+    util.remove_view(cr, "sale_subscription.sale_subscription_base_automation_form")
+    util.remove_menus(cr, [util.ref(cr, "sale_subscription.menu_sale_subscription_alert")])
+
+    # Remove the subscription health field from sale orders and reports.
+    util.remove_field(cr, "sale.order", "health")
+    util.remove_field(cr, "sale.order.log.report", "health")
+    util.remove_field(cr, "sale.subscription.report", "health")
