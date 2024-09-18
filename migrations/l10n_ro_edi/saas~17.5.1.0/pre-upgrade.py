d4e1b55ee964462db6864b6ce048ce1d01856fea
Yosua Nicolaus <yoni@odoo.com>
2024-09-18 17:11:07 +0200

    [IMP] l10n_ro_edi/saas~17.5: update state selection values
    This commit does 2 things for the l10n_ro_edi module:
    
    - Update the selection values of the 2 state fields in l10n_ro_edi.
    - Update all move l10n_ro_edi_index if the move have a document with
      index (saved in key_loading)
    
    related community PR: https://github.com/odoo/odoo/pull/180419
    
    closes odoo/upgrade#6531
    
    Task-id: 4059522
    Related: odoo/odoo#180419
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/l10n_ro_edi/saas~17.5.1.0/pre-upgrade.py
+++ migrations/l10n_ro_edi/saas~17.5.1.0/pre-upgrade.py
@@ -5,3 +5,39 @@ def migrate(cr, version):
     # computed stored field from `l10n_ro_efactura`; don't recompute it
     util.create_column(cr, "account_move", "l10n_ro_edi_state", "varchar")
     util.remove_field(cr, "account.move.send", "l10n_ro_edi_warnings")
+
+    # change selection values of romanian move and document's state from:
+    # "invoice_sending" -> "invoice_sent"
+    # "invoice_sent" -> "invoice_validated"
+    # the update order must be done starting from sent->validated, and sending->sent after
+    util.change_field_selection_values(
+        cr,
+        "account.move",
+        "l10n_ro_edi_state",
+        {"invoice_sent": "invoice_validated", "invoice_sending": "invoice_sent"},
+    )
+    util.change_field_selection_values(
+        cr,
+        "l10n_ro_edi.document",
+        "state",
+        {"invoice_sent": "invoice_validated", "invoice_sending": "invoice_sent"},
+    )
+
+    # set account_move.l10n_ro_edi_index to the first found related document with key_loading information
+    util.create_column(cr, "account_move", "l10n_ro_edi_index", "varchar")
+    cr.execute("""
+        WITH document_with_index AS (
+            SELECT DISTINCT ON (invoice_id)
+                   invoice_id,
+                   key_loading
+              FROM l10n_ro_edi_document
+             WHERE key_loading IS NOT NULL
+          ORDER BY invoice_id,
+                   datetime DESC,
+                   id DESC
+        )
+        UPDATE account_move move
+           SET l10n_ro_edi_index = document_with_index.key_loading
+          FROM document_with_index
+         WHERE document_with_index.invoice_id = move.id;
+    """)
