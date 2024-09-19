a611efe4c6fd9117140d32c086fc8f8b5a141e26
amdi-odoo <amdi@odoo.com>
2024-09-19 15:00:45 +0200

    [IMP] appointment/saas~17.5: add a field for the appointment status
    We now use the new field `appointment_status` to better manage the
    status of appointments.
    Remove the `appointment_attended` field as it's now a specific status
    of the new field (attended).
    While at it, we also allow the manual confirmation option for staff user.
    This leads to the renaming of the field `resource_manual_confirmation`
    into `appointment_manual_confirmation`.
    
    ENT PR: odoo/enterprise#66076
    
    Task-4028357
    
    closes odoo/upgrade#6214
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>
    Co-authored-by: amdi-odoo amdi@odoo.com
    Co-authored-by: jeh-odoo jeh@odoo.com

--- migrations/appointment/saas~17.5.1.3/pre-migrate.py
+++ migrations/appointment/saas~17.5.1.3/pre-migrate.py
@@ -16,3 +16,40 @@ def migrate(cr, version):
         util.rename_xmlid(cr, *eb(rename))
 
     util.remove_menus(cr, [util.ref(cr, "appointment.menu_schedule_report_all")])
+
+    util.create_column(cr, "calendar_event", "appointment_status", "varchar")
+    util.explode_execute(
+        cr,
+        """
+        WITH manual_confirmation_events AS (
+            SELECT attendee.event_id
+              FROM calendar_attendee attendee
+              JOIN calendar_event event
+                ON event.id = attendee.event_id
+              JOIN appointment_type app_type
+                ON app_type.id = event.appointment_type_id
+               AND app_type.resource_manual_confirmation
+             WHERE attendee.state = 'needsAction'
+               AND {parallel_filter}
+          GROUP BY attendee.event_id
+        )
+        UPDATE calendar_event event
+           SET appointment_status = CASE
+                    WHEN event.appointment_type_id IS NULL THEN NULL
+                    WHEN event.appointment_attended THEN 'attended'
+                    WHEN NOT event.active THEN 'cancelled'
+                    WHEN mce IS NOT NULL THEN 'request'
+                    ELSE 'booked'
+               END
+          FROM calendar_event event2
+     LEFT JOIN manual_confirmation_events mce
+            ON mce.event_id = event2.id
+         WHERE event.id = event2.id
+           AND {parallel_filter}
+        """,
+        table="calendar_event",
+        alias="event",
+    )
+    util.remove_field(cr, "calendar.event", "appointment_attended")
+    util.rename_field(cr, "appointment.type", *eb("{resource,appointment}_manual_confirmation"))
+    util.update_record_from_xml(cr, "calendar.alarm_notif_1")
