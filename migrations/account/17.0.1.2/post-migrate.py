f5068e77692e9fe4d07f5d9816946c0b20f528f9
kajal-odoo <kago@odoo.com>
2024-09-20 12:59:48 +0000

    [FIX] account/17.0: reset translation
    Issue with translation where & is being rendered like this &amp; in the view.
    
    Step to reproduce.
    
    1. create database in version 16.
    2. install account module.
    3. set language english(UK).
    4. migrate 16 to 17.
    
    closes odoo/upgrade#6547
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account/17.0.1.2/post-migrate.py
+++ migrations/account/17.0.1.2/post-migrate.py
@@ -5,3 +5,6 @@ def migrate(cr, version):
     util.if_unchanged(
         cr, "account.report_invoice_document", util.update_record_from_xml, reset_translations={"arch_db"}
     )
+    util.if_unchanged(
+        cr, "account.res_config_settings_view_form", util.update_record_from_xml, reset_translations={"arch_db"}
+    )
