fdb67d00dcb3634ee96381782a7ef2b08c380e3f
Rémy Voet (ryv) <ryv@odoo.com>
2024-09-24 10:40:18 +0200

    [IMP] mail/saas~18.1: remove unused field
    Remove alias_domain_name from res.company
    
    See odoo/odoo#153463
    See odoo/enterprise#58293
    
    Part-of: odoo/upgrade#5793
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/mail/saas~18.1.1.18/pre-migrate.py
+++ migrations/mail/saas~18.1.1.18/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "mail.mail_template_view_form_confirm_delete")
+    util.remove_field(cr, "res.company", "alias_domain_name")
