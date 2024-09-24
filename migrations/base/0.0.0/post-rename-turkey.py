3240305d3df04aa7006c0127800405c9f08dd2b7
Hesham <hsal@odoo.com>
2024-09-24 10:37:26 +0200

    [IMP] base/0.0.0: Updated Turkey to Türkiye
    In 2022, Republic of Turkey officially renamed itself to Republic of Türkiye.
    So, this commit updates the name of the Turkish republic.
    Community PR: https://github.com/odoo/odoo/pull/180758
    Enterprise PR: https://github.com/odoo/enterprise/pull/70304
    
    task-4182931
    
    closes odoo/upgrade#6558
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/base/0.0.0/post-rename-turkey.py
@@ -0,0 +1,6 @@
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    if util.version_between("16.0", "18.0"):
+        util.update_record_from_xml(cr, "base.tr")
