24b0a176c72b35f27407def39ee74dfcec34157c
Jinane Maksoud <maji@odoo.com>
2024-09-24 17:04:23 +0200

    [FIX] account/0.0.0: ensure deferred accounts exist
    Some charts of account modules set deferred accounts on the
    company in a post init hook, and if the accounts are not found
    this results in a warning. When this happens during the upgrade
    on the CI it flags a failure, so the solution is to ensure the
    needed accounts are loaded before they are used on the company.
    
    closes odoo/upgrade#6563
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/account/0.0.0/pre-ensure-deferred-accounts.py
@@ -0,0 +1,34 @@
+from odoo import models
+
+from odoo.addons.account.models import chart_template  # noqa
+
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    pass
+
+
+if util.version_gte("saas~16.4"):
+
+    class ChartTemplate(models.AbstractModel):
+        _inherit = "account.chart.template"
+        _module = "account"
+
+        def _load_data(self, data, *args, **kwargs):
+            if set(data.keys()) == {"res.company"}:
+                needed_account_xmlids = {
+                    xmlid
+                    for (c_id, vals) in data["res.company"].items()
+                    for (field_name, xmlid) in vals.items()
+                    if field_name in ("deferred_expense_account_id", "deferred_revenue_account_id")
+                    and not self.ref(xmlid, raise_if_not_found=False)
+                }
+                if needed_account_xmlids:
+                    chart_code = self.env.company.chart_template
+                    account_data = self._get_chart_template_data(chart_code)
+                    if "account.account" in account_data:
+                        data["account.account"] = {
+                            account: account_data["account.account"][account] for account in needed_account_xmlids
+                        }
+            return super()._load_data(data, *args, **kwargs)
