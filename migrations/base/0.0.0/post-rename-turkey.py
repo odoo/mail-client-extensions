e9a4f94b3801c436f9dfcfaf470bdbd3b542dd43
Hesham <hsal@odoo.com>
2024-10-14 09:44:13 +0200

    [IMP] base/0.0.0: Updated Turkey to Türkiye
    This is a followup PR of this one: https://github.com/odoo/upgrade/pull/6558
    Community PR: https://github.com/odoo/odoo/pull/180758
    Enterprise PR: https://github.com/odoo/enterprise/pull/70304
    It accomodates for the DBs in 18 for the country name update.
    Before this commit, DBs in v18.0, with the old name of the Turkish republic,
    when upgraded to higher versions would not have the country name upgraded.
    
    task-4182931
    
    closes odoo/upgrade#6610
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/0.0.0/post-rename-turkey.py
+++ migrations/base/0.0.0/post-rename-turkey.py
@@ -2,5 +2,5 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    if util.version_between("16.0", "18.0"):
+    if util.version_between("16.0", "19.0"):
         util.update_record_from_xml(cr, "base.tr")
