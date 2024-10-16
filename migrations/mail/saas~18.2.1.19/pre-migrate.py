ee1125673507a27f6309deccf02cc27f1582731a
zel-odoo <zel@odoo.com>
2024-10-16 17:11:38 +0200

    [IMP] mail/saas~18.2: mass edit subscriptions
    1. remove mail.wizard.invite model
    
    https://github.com/odoo/odoo/pull/167045
    
    closes odoo/upgrade#6620
    
    Related: odoo/odoo#167045
    Related: odoo/enterprise#71532
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/mail/saas~18.2.1.19/pre-migrate.py
+++ migrations/mail/saas~18.2.1.19/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "discuss.channel", "allow_public_upload")
     util.remove_field(cr, "discuss.channel.member", "fold_state")
+    util.remove_model(cr, "mail.wizard.invite")
