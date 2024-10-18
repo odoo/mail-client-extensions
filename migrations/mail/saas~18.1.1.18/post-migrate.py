de7c9a46da19a887307d686069f794bdb2d526d7
zel-odoo <zel@odoo.com>
2024-10-18 14:13:33 +0200

    [IMP] mail/saas~18.1: support sub thread in group
    This commit updates the ACL for groups.
    This change adds the support for sub thread in group chat.
    It allows to create a sub thread in the action list or create from a
    specific message in the group chat.
    
    https://github.com/odoo/odoo/pull/184289
    
    closes odoo/upgrade#6638
    
    Related: odoo/odoo#184289
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

---
+++ migrations/mail/saas~18.1.1.18/post-migrate.py
@@ -0,0 +1,6 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.update_record_from_xml(cr, "mail.ir_rule_discuss_channel_all")
+    util.update_record_from_xml(cr, "mail.ir_rule_discuss_channel_member_read_all")
