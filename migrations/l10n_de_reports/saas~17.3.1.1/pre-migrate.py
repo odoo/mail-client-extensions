6fa340c09dfd3860489dec77eee4485bb261b7ae
Louis Gobert <loug@odoo.com>
2024-10-21 17:23:06 +0200

    [FIX] l10n_de_reports/saas~17.3: convert fields to company dependent
    Prior to this fix, upgrading the l10n_de_reports module to version 17.3
    caused the fields in l10n_de_datev_identifier/customer to be dropped.
    
    This fix resolves the issue by converting these fields to company-dependent,
    preventing them from being removed during the upgrade.
    
    To reproduce, just add a value in field l10n_de_datev_identifier or
    l10n_de_datev_identifier_customer for a customer in a DE company in
    saas-17.2 and upgrade to saas-17.3.
    
    odoo/enterprise@cab6b2e
    
    opw-4199466
    
    closes odoo/upgrade#6647
    
    Signed-off-by: Jinane Maksoud (maji) <maji@odoo.com>

--- migrations/l10n_de_reports/saas~17.3.1.1/pre-migrate.py
+++ migrations/l10n_de_reports/saas~17.3.1.1/pre-migrate.py
@@ -2,5 +2,5 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.remove_column(cr, "res_partner", "l10n_de_datev_identifier")
-    util.remove_column(cr, "res_partner", "l10n_de_datev_identifier_customer")
+    util.make_field_company_dependent(cr, "res.partner", "l10n_de_datev_identifier", "integer")
+    util.make_field_company_dependent(cr, "res.partner", "l10n_de_datev_identifier_customer", "integer")
