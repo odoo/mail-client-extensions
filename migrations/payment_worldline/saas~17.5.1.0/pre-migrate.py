9dbf48fbf23361ccc9b406147f2882173fb741fd
Christophe Simonis <chs@odoo.com>
2024-10-21 14:27:20 +0200

    [FIX] payment_worldline/saas~17.5
    Remove `ogone_hash_function` field.

--- migrations/payment_worldline/saas~17.5.1.0/pre-migrate.py
+++ migrations/payment_worldline/saas~17.5.1.0/pre-migrate.py
@@ -7,3 +7,4 @@ def migrate(cr, version):
     util.rename_field(cr, "payment.provider", "ogone_password", "worldline_api_secret")
     util.rename_field(cr, "payment.provider", "ogone_shakey_in", "worldline_webhook_key")
     util.rename_field(cr, "payment.provider", "ogone_shakey_out", "worldline_webhook_secret")
+    util.remove_field(cr, "payment.provider", "ogone_hash_function")
