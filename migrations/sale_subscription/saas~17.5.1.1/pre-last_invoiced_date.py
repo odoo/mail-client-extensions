241ba858e516730c5b48b6aff412930c5cd4a9c0
Arnaud Joset <arj@odoo.com>
2024-10-22 16:55:47 +0200

    [IMP] sale_subscription/saas~17.5: add last_invoiced_date
    Before this commit, the last_invoiced_date (last date covered by a
    deferred revenue) of a sale.order.line was unknown.
    
    task 3071118
    
    Enterprise: odoo/enterprise#41796
    
    PR making last_invoiced_date computed:
    odoo/enterprise#70628 and
    odoo/enterprise#71807 (master fw port)
    
    closes odoo/upgrade#6612
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/sale_subscription/saas~17.5.1.1/pre-last_invoiced_date.py
+++ migrations/sale_subscription/saas~17.5.1.1/pre-last_invoiced_date.py
@@ -7,11 +7,44 @@ def migrate(cr, version):
     util.explode_execute(
         cr,
         """
-            UPDATE sale_order
+            UPDATE sale_order so
                SET require_payment=False
              WHERE subscription_state IN ('3_progress', '4_paused')
                AND payment_token_id is NULL
-            """,
+        """,
         table="sale_order",
         alias="so",
     )
+
+    # fill the last_invoiced_date value based on last deferred_end_date of posted invoice
+    util.create_column(cr, "sale_order_line", "last_invoiced_date", "date")
+    query = """
+        WITH _cte AS (
+            SELECT r.order_line_id,
+                   (
+                      SELECT max(d)
+                        FROM (
+                            SELECT unnest(array_agg(ml.deferred_end_date) FILTER (WHERE m.move_type = 'out_invoice')) as d
+                           EXCEPT
+                            SELECT unnest(array_agg(ml.deferred_end_date) FILTER (WHERE m.move_type = 'out_refund')) as d
+                       ) as _
+                  ) as last_invoiced_date
+              FROM sale_order_line_invoice_rel r
+              JOIN sale_order_line ol       -- needed for explode
+                ON ol.id = r.order_line_id
+              JOIN account_move_line ml
+                ON ml.id = r.invoice_line_id
+              JOIN account_move m
+                ON m.id = ml.move_id
+             WHERE m.state = 'posted'
+               AND m.move_type IN ('out_invoice', 'out_refund')
+               AND ml.deferred_end_date IS NOT NULL
+               AND {parallel_filter}
+          GROUP BY r.order_line_id
+        )
+        UPDATE sale_order_line ol
+           SET last_invoiced_date = _cte.last_invoiced_date
+          FROM _cte
+         WHERE _cte.order_line_id = ol.id
+    """
+    util.explode_execute(cr, query, table="sale_order_line", alias="ol")
