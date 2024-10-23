58f98cad976ad5942fb9c13de594cc988bc6dd87
Dhwani Patel <dwa@odoo.com>
2024-10-23 13:18:15 +0000

    [FIX] website/17.0.1.0: set a cowed view in inherit for related website
    The database upgraded from a lower version to version 17, If a website has a
    header menu layout of "Hamburger menu" then a list of menus is not visible to
    the customer.
    
    Because 'no_autohide_menu' views inherit from the 'Main layout' view and during
    upgrade we create a cowed view for 'no_autohide_menu' view by copying from
    related standard view also inherited view is the standard view of 'Main Layout'
    but if a cowed view is already available of 'Main Layout' in the customer
    database then that needs to be set as an inherited view for a newly created cowd
    view of 'no_autohide_menu' to resolve this issue.
    
    opw - 4246249
    Req - 2114055
    
    closes odoo/upgrade#6651
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/website/17.0.1.0/post-migrate.py
+++ migrations/website/17.0.1.0/post-migrate.py
@@ -5,20 +5,29 @@ def migrate(cr, version):
     view = util.env(cr).ref("website.no_autohide_menu")
     cr.execute(
         """
-        SELECT h.website_id
+        SELECT h.website_id,
+               MIN(i.id)
           FROM ir_ui_view h
      LEFT JOIN ir_ui_view a
             ON h.website_id = a.website_id
            AND a.key = 'website.no_autohide_menu'
            AND a.active
+     LEFT JOIN ir_ui_view i
+            ON i.website_id = h.website_id
+           AND i.key='website.layout'
+           AND i.active
          WHERE h.key = 'website.template_header_hamburger'
            AND h.active
            AND a.id is NULL
       GROUP BY h.website_id
         """
     )
-    for (website_id,) in cr.fetchall():
-        view.copy({"website_id": website_id, "active": True})
+    for website_id, inherit_id in cr.fetchall():
+        raw_data = {"website_id": website_id, "active": True}
+        if inherit_id:
+            raw_data["inherit_id"] = inherit_id
+        view.copy(raw_data)
+
     util.if_unchanged(
         cr, "website.res_config_settings_view_form", util.update_record_from_xml, reset_translations={"arch_db"}
     )
