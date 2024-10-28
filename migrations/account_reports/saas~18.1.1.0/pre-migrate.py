4cebf0e7bcd1832e16df2ad164bf3452746e1d40
oco-odoo <oco@odoo.com>
2024-10-28 17:21:55 +0100

    [FIX] account_report/saas~18.1: revert totals below sections removal
    This reverts commit 956b236ed288145453cd35b64ca11ccc5e5da93a.
    
    We got reports that the "Totals Below Sections" setting is actually important in the US and LATAM countries. The usage is there to always display amounts like that on reports, and there is a lot of resistance to softwares not allowing that. We hence reintroduce this feature.
    
    closes odoo/upgrade#6678
    
    Related: https://github.com/odoo/enterprise/pull/72930
    Related: odoo/enterprise#72930
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account_reports/saas~18.1.1.0/pre-migrate.py
+++
@@ -1,6 +0,0 @@
-from odoo.upgrade import util
-
-
-def migrate(cr, version):
-    util.remove_field(cr, "res.company", "totals_below_sections")
-    util.remove_field(cr, "res.config.settings", "totals_below_sections")
