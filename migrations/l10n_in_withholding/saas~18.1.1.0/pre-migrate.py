52390f9fb1a514b17e2f0480af8c312c122784dc
Nishant Jain (NIAI) <niai@odoo.com>
2024-10-29 12:29:02 +0530

    [IMP] l10n_in_withholding/saas~18.1: remove warning field
    Remove field `l10n_in_tcs_tds_warning` from `account.move`
    
    Community PR - https://github.com/odoo/odoo/pull/178001
    
    closes odoo/upgrade#6667
    
    Related: odoo/odoo#185462
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/l10n_in_withholding/saas~18.1.1.0/pre-migrate.py
+++ migrations/l10n_in_withholding/saas~18.1.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "l10n_in.withhold.wizard", "withhold_line_ids")
     util.remove_model(cr, "l10n_in.withhold.wizard.line")
+    util.remove_field(cr, "account.move", "l10n_in_tcs_tds_warning")
