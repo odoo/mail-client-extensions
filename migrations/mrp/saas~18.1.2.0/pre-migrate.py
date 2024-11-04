cfafdb8b003826e3457e37f3bc13ad68f7fc2242
Lulu Grimalkin (lugr) <lugr@odoo.com>
2024-11-04 11:04:24 +0100

    [FIX] mrp{,_workorder}/saas~18.1: remove unused WO view action
    This action was orphaned after the last view to use it in
    `mrp.workcenter` changed in behaviour and now uses a different action.
    
    See odoo/odoo#186040
    See odoo/enterprise#73160
    
    closes odoo/upgrade#6711
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/mrp/saas~18.1.2.0/pre-migrate.py
+++ migrations/mrp/saas~18.1.2.0/pre-migrate.py
@@ -6,3 +6,4 @@ def migrate(cr, version):
     util.remove_column(cr, "mrp_workcenter_productivity", "loss_type")
     util.remove_column(cr, "mrp_workorder", "product_id")
     util.remove_column(cr, "stock_move", "order_finished_lot_id")
+    util.remove_record(cr, "mrp.act_product_mrp_production_workcenter")
