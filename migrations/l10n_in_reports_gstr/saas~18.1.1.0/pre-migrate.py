650bf1061540d3bdb097701a6c969f4a36c23ee3
hatr-odoo <hatr@odoo.com>
2024-11-05 11:30:56 +0530

    [FIX] l10n_in_reports_gstr/saas~18.1.1.0: rectify gstr2b_status field
    This script rectifies gstr2b_status's selection field from 'not_recived' to
    'not_received'.
    
    see https://github.com/odoo/enterprise/pull/73266
    
    task-4306959
    
    closes odoo/upgrade#6717
    
    Related: odoo/enterprise#73266
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/l10n_in_reports_gstr/saas~18.1.1.0/pre-migrate.py
+++ migrations/l10n_in_reports_gstr/saas~18.1.1.0/pre-migrate.py
@@ -4,3 +4,6 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "l10n_in_reports_gstr.l10n_in_gst_return_period_view_inherit_spreadsheet")
     util.rename_field(cr, "l10n_in.gst.return.period", "gstr1_spreadsheet", "gstr1_spreadsheet_id")
+    util.change_field_selection_values(
+        cr, "l10n_in.gst.return.period", "gstr2b_status", {"not_recived": "not_received"}
+    )
