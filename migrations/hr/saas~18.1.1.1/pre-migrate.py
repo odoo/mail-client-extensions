8589849be23b36ce100ffd5f27bf971c6aa8f087
Ahmad Khanalizadeh <khah@odoo.com>
2024-11-06 10:12:23 +0100

    [IMP] hr/saas~18.1: update targets on stage change
    The PR https://github.com/odoo/odoo/pull/185959 makes two fields
    non-stored. This commit removes the related columns.
    
    Community PR: https://github.com/odoo/odoo/pull/185959
    
    task-4283592
    
    closes odoo/upgrade#6731
    
    Related: odoo/odoo#185959
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr/saas~18.1.1.1/pre-migrate.py
+++ migrations/hr/saas~18.1.1.1/pre-migrate.py
@@ -5,3 +5,5 @@ def migrate(cr, version):
     util.remove_record(cr, "hr.hr_employee_comp_rule")
     util.remove_record(cr, "hr.hr_employee_public_comp_rule")
     util.remove_field(cr, "hr.departure.wizard", "employee_id")
+    util.remove_column(cr, "hr_job", "expected_employees")
+    util.remove_column(cr, "hr_job", "no_of_employee")
