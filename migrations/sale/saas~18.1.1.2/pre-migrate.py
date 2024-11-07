78bd075a2601d21c41d0d6c1edb2e3d38e233e7f
kcv-odoo <kcv@odoo.com>
2024-11-07 17:29:30 +0530

    [IMP] sale/saas~18.1: rename fields
    This commit rename tax_id field on sale.order.line
    to tax_ids.
    
    community PR: https://github.com/odoo/odoo/pull/186869
    enterprise PR: https://github.com/odoo/enterprise/pull/73624
    
    task-4206350
    
    closes odoo/upgrade#6746
    
    Related: odoo/odoo#186869
    Related: odoo/enterprise#73624
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/sale/saas~18.1.1.2/pre-migrate.py
+++ migrations/sale/saas~18.1.1.2/pre-migrate.py
@@ -6,3 +6,4 @@ def migrate(cr, version):
     util.remove_field(cr, "sale.advance.payment.inv", "display_invoice_amount_warning")
     util.rename_field(cr, "sale.order.line", "product_uom", "product_uom_id")
     util.rename_field(cr, "sale.report", "product_uom", "product_uom_id")
+    util.rename_field(cr, "sale.order.line", "tax_id", "tax_ids")
