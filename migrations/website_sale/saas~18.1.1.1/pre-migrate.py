d9730071805164d0cb8584fab032ecd5aa2094c1
Levi Siuzdak (sile) <sile@odoo.com>
2024-11-12 11:12:02 +0100

    [FIX] website_sale/saas~18.1: add `publish_date` to `product.template`
    See https://github.com/odoo/odoo/pull/185987
    
    Add the `publish_date` field to the `product.template` model, and
    initialize its value to `create_date`.
    This keeps "sort by Newest Arrivals" unchanged post-migration.
    
    closes odoo/upgrade#6749
    
    Related: odoo/odoo#185987
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/website_sale/saas~18.1.1.1/pre-migrate.py
+++ migrations/website_sale/saas~18.1.1.1/pre-migrate.py
@@ -3,3 +3,13 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.if_unchanged(cr, "website_sale.mail_template_sale_cart_recovery", util.update_record_from_xml)
+
+    util.create_column(cr, "product_template", "publish_date", "timestamp without time zone")
+    query = "UPDATE product_template SET publish_date = create_date"
+    util.parallel_execute(cr, util.explode_query_range(cr, query, table="product_template"))
+    util.change_field_selection_values(
+        cr,
+        "website",
+        "shop_default_sort",
+        {"create_date desc": "publish_date desc"},
+    )
