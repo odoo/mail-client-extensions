70c3e41ea5e3e8b538e639a331d4739bf6306eac
Krzysztof Magusiak (krma) <krma@odoo.com>
2024-11-18 06:45:25 +0000

    [IMP] base/saas~18.1: check indexes and WHERE bool
    Warn about indexes that may use a boolean field in their
    condition because the generated queries changed for boolean fields.
    The ORM will translate queries on booleans as `field IS [NOT] TRUE`
    instead of comparisons with a boolean value or null checks.
    We want to identify indexes that may not be useful anymore:
    these contain a WHERE clause with a comparison with a boolean value
    or "active" field while ignoring conditions that use the new syntax.
    
    Remove indexes that will be recreated by the ORM with a different name.
    
    task-4067946
    odoo/odoo#187538
    
    closes odoo/upgrade#6776
    
    Related: odoo/enterprise#73981
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/saas~18.1.1.3/pre-migrate.py
+++ migrations/base/saas~18.1.1.3/pre-migrate.py
@@ -4,3 +4,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.change_field_selection_values(cr, "ir.actions.act_window", "target", {"inline": "current"})
     util.remove_column(cr, "ir_act_server", "model_name")
+
+    cr.execute("DROP INDEX IF EXISTS res_device_log__composite_idx")
--- migrations/mail/saas~18.1.1.18/pre-migrate.py
+++ migrations/mail/saas~18.1.1.18/pre-migrate.py
@@ -4,3 +4,5 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "mail.mail_template_view_form_confirm_delete")
     util.remove_field(cr, "res.company", "alias_domain_name")
+
+    cr.execute("DROP INDEX IF EXISTS unique_mail_message_id_res_partner_id_if_set")
