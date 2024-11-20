20741405d855926cb1d759da31e15c1da1e519ab
UemuS <yaih@odoo.com>
2024-11-20 10:07:57 +0000

    [FIX] documents/saas~17.5: forcecreate potential dependency
    upg-2192400
    
    Issue 1:
    The record documents_tag_draft:
    https://github.com/odoo/enterprise/blob/7ed31945c5a4cec7ca61978c1c58ea4c211658e4/documents/data/documents_tag_data.xml#L4)
    added in 18.0 is not created during the update of the module,
    but is needed in another module if that one is newly added.
    
    In this case:
    https://github.com/odoo/enterprise/blob/6073f0486565664c1f124d37b01a06e89ab41ffe/documents_project/data/ir_actions_server_data.xml
    
    Since both are `forcecreate="0"`, the issue only happens if one module is
    installed before the update and the second is added during or after.
    
    Issue 2:
    In 18.0, document tags became unique by `name`, a record that has the
    same name existed in 17.0 and was removed in 18.0:
    https://github.com/odoo/enterprise/blob/c50ad9e6e541900d4be3d5f0b8966aa7219e4af7/documents_project/data/documents_tag_data.xml#L4
    
    But since it's noupdate, we get 2 records with the same name and different
    xmlids after the upgrade, so we rename the old one before the update.
    
    ```
    ValueError: External ID not found in the system: documents.documents_tag_draft
    
    The above exception was the direct cause of the following exception:
    
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/18.0/odoo/service/server.py", line 1305, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "<decorator-gen-13>", line 2, in new
      File "/home/odoo/src/odoo/18.0/odoo/tools/func.py", line 97, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/18.0/odoo/modules/registry.py", line 131, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/18.0/odoo/modules/loading.py", line 479, in load_modules
        processed_modules += load_marked_modules(env, graph,
      File "/home/odoo/src/odoo/18.0/odoo/modules/loading.py", line 363, in load_marked_modules
        loaded, processed = load_module_graph(
      File "/home/odoo/src/odoo/18.0/odoo/modules/loading.py", line 226, in load_module_graph
        load_data(env, idref, mode, kind='data', package=package)
      File "/home/odoo/src/odoo/18.0/odoo/modules/loading.py", line 70, in load_data
        tools.convert_file(env, package.name, filename, idref, mode, noupdate, kind)
      File "/home/odoo/src/odoo/18.0/odoo/tools/convert.py", line 608, in convert_file
        convert_xml_import(env, module, fp, idref, mode, noupdate)
      File "/home/odoo/src/odoo/18.0/odoo/tools/convert.py", line 679, in convert_xml_import
        obj.parse(doc.getroot())
      File "/home/odoo/src/odoo/18.0/odoo/tools/convert.py", line 594, in parse
        self._tag_root(de)
      File "/home/odoo/src/odoo/18.0/odoo/tools/convert.py", line 537, in _tag_root
        f(rec)
      File "/home/odoo/src/odoo/18.0/odoo/tools/convert.py", line 550, in _tag_root
        raise ParseError('while parsing %s:%s, somewhere inside\n%s' % (
    odoo.tools.convert.ParseError: while parsing /home/odoo/src/enterprise/18.0/documents_project/data/ir_actions_server_data.xml:6, somewhere inside
    <record id="ir_actions_server_tag_remove_draft" model="ir.actions.server" forcecreate="0">
                <field name="name">Remove Tag Draft</field>
                <field name="model_id" ref="documents.model_documents_document"/>
                <field name="groups_id" eval="[Command.link(ref('base.group_user'))]"/>
                <field name="sequence">7</field> <!-- Higher than the sequence of "_code" checks -->
                <field name="state">object_write</field>
                <field name="update_m2m_operation">remove</field>
                <field name="update_path">tag_ids</field>
                <field name="resource_ref" ref="documents.documents_tag_draft"/>
            </record>
    ```
    
    closes odoo/upgrade#6785
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/documents/saas~17.5.1.4/post-migrate.py
+++ migrations/documents/saas~17.5.1.4/post-migrate.py
@@ -5,3 +5,11 @@ def migrate(cr, version):
     util.if_unchanged(cr, "documents.mail_template_document_request", util.update_record_from_xml)
     util.if_unchanged(cr, "documents.mail_template_document_request_reminder", util.update_record_from_xml)
     util.if_unchanged(cr, "documents.public_page_layout", util.update_record_from_xml)
+
+    # The `name` of documents tags is unique in 18.0.
+    # The record in documents.projects has been deleted in 18.0
+    # And a new one was added in documents.
+    # Since the record is `forcecreate=0`, dependant modules installed
+    # during or after the upgrade will fail, so we update it and force create it.
+    util.rename_xmlid(cr, "documents_project.documents_project_status_draft", "documents.documents_tag_draft")
+    util.update_record_from_xml(cr, "documents.documents_tag_draft")
