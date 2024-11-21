850f685144ec03aad8aa89b190ac01d13b6b13c2
Xavier Bol (xbo) <xbo@odoo.com>
2024-11-21 16:33:49 +0100

    [REV] hr/saas~18.1: remove multi company rules in employee
    This commit reverts the changes made in #6507 because the current
    behavior (without multi-company rules defined in `hr.employee`) caused
    many issues in other records linked to an employee.
    
    closes odoo/upgrade#6806
    
    Related: odoo/odoo#188096
    Related: odoo/enterprise#74301
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr/saas~18.1.1.1/pre-migrate.py
+++ migrations/hr/saas~18.1.1.1/pre-migrate.py
@@ -2,8 +2,6 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.remove_record(cr, "hr.hr_employee_comp_rule")
-    util.remove_record(cr, "hr.hr_employee_public_comp_rule")
     util.remove_field(cr, "hr.departure.wizard", "employee_id")
     util.remove_column(cr, "hr_job", "expected_employees")
     util.remove_column(cr, "hr_job", "no_of_employee")
