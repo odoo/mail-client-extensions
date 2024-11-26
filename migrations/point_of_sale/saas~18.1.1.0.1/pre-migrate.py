a393c71450818bbe0038ff075e65fe9e9c8dab24
paan-odoo <paan@odoo.com>
2024-11-26 14:21:39 +0530

    [IMP] point_of_sale/saas~18.1: remove module_pos_paytm field
    In this commit:
    
    - We are removing the `module_pos_paytm` field from the `res.config.settings`
      model. The entire module will be removed in the future.
    
    task-4360269
    
    Related PR: odoo/odoo#188586
    
    closes odoo/upgrade#6821
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/point_of_sale/saas~18.1.1.0.1/pre-migrate.py
+++ migrations/point_of_sale/saas~18.1.1.0.1/pre-migrate.py
@@ -6,3 +6,4 @@ def migrate(cr, version):
     util.remove_field(cr, "pos.session", "sequence_number")
     util.rename_field(cr, "pos.order", "general_note", "general_customer_note")
     util.delete_unused(cr, "point_of_sale.product_category_pos")
+    util.remove_field(cr, "res.config.settings", "module_pos_paytm")
