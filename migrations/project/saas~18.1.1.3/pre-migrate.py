8e95e665aa7849f8f8c14224b089fa942e191985
Maxime de Neuville <mane@odoo.com>
2024-11-26 12:11:50 +0100

    [IMP] project/saas~18.1: move partner_phone in project
    https://github.com/odoo/odoo/pull/188581
    https://github.com/odoo/enterprise/pull/74567
    
    closes odoo/upgrade#6824
    
    Related: odoo/odoo#188581
    Related: odoo/enterprise#74567
    Signed-off-by: Jinane Maksoud (maji) <maji@odoo.com>

--- migrations/project/saas~18.1.1.3/pre-migrate.py
+++ migrations/project/saas~18.1.1.3/pre-migrate.py
@@ -7,3 +7,23 @@ def migrate(cr, version):
     util.remove_view(cr, "project.project_sharing_portal")
     util.rename_xmlid(cr, *eb("project.project_sharing_{embed,portal}"))
     util.remove_view(cr, "project.project_sharing")
+
+    is_industry_fsm_installed = util.module_installed(cr, "industry_fsm")
+    is_website_project_installed = util.module_installed(cr, "website_project")
+    if is_industry_fsm_installed:
+        util.move_field_to_module(cr, "project.task", "partner_phone", "industry_fsm", "project")
+    if is_website_project_installed:
+        util.move_field_to_module(cr, "project.task", "partner_phone", "website_project", "project")
+    if not (is_industry_fsm_installed or is_website_project_installed):
+        util.create_column(cr, "project_task", "partner_phone", "varchar")
+        util.explode_execute(
+            cr,
+            """
+            UPDATE project_task t
+               SET partner_phone = COALESCE(p.mobile, p.phone)
+              FROM res_partner p
+             WHERE t.partner_id = p.id
+            """,
+            table="project_task",
+            alias="t",
+        )
