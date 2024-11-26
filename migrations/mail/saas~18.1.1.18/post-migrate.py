dbf361818abdce5cd8e2ed9c73f37aac5b0295ad
tsm-odoo <tsm@odoo.com>
2024-11-26 12:35:35 +0100

    [IMP] mail/saas~18.1: simplify sub-channels ACLs
    The community counterpart of this PR simplify sub-channel
    ACLs. As a result, ACL records should be updated and the
    constraint on sub-channels not having group public id must
    be removed.
    
    closes odoo/upgrade#6778
    
    See: https://github.com/odoo/odoo/pull/187566
    Related: odoo/odoo#187566
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/mail/saas~18.1.1.18/post-migrate.py
+++ migrations/mail/saas~18.1.1.18/post-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.update_record_from_xml(cr, "mail.ir_rule_discuss_channel_all")
     util.update_record_from_xml(cr, "mail.ir_rule_discuss_channel_member_read_all")
+    util.update_record_from_xml(cr, "mail.ir_rule_discuss_channel_member_is_self_all")
--- migrations/mail/saas~18.1.1.18/pre-migrate.py
+++ migrations/mail/saas~18.1.1.18/pre-migrate.py
@@ -6,3 +6,14 @@ def migrate(cr, version):
     util.remove_field(cr, "res.company", "alias_domain_name")
 
     cr.execute("DROP INDEX IF EXISTS unique_mail_message_id_res_partner_id_if_set")
+    cr.execute("ALTER TABLE discuss_channel DROP CONSTRAINT IF EXISTS discuss_channel_sub_channel_no_group_public_id")
+    util.explode_execute(
+        cr,
+        """
+        UPDATE discuss_channel
+           SET group_public_id = parent.group_public_id
+          FROM discuss_channel parent
+         WHERE discuss_channel.parent_channel_id = parent.id
+    """,
+        table="discuss_channel",
+    )
