9369a309a49d0cdbc6b22e2a20de04ca06c2981d
Elias Regopoulos <elre@odoo.com>
2024-11-27 20:31:52 +0000

    [FIX] base/9.saas~7: Allow empty isocode lang
    During the upgrade to Odoo 10.0, res_lang entries' `iso_code` is used to insert
    those entries' respective xmlid (if not already existing) into ir_model_data.
    The xmlid's `name` is set to the concatenation of "lang_" and the res_lang's
    iso_code. `res_lang.iso_code` is not a required field though, so a NULL value
    would result in a NULL imd name which is invalid.
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/10.0/odoo/service/server.py", line 974, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "/home/odoo/src/odoo/10.0/odoo/modules/registry.py", line 83, in new
        odoo.modules.load_modules(registry._db, force_demo, status, update_module)
      File "/home/odoo/src/odoo/10.0/odoo/modules/loading.py", line 318, in load_modules
        report=report, models_to_check=models_to_check)
      File "/home/odoo/src/odoo/10.0/odoo/modules/loading.py", line 136, in load_module_graph
        migrations.migrate_module(package, 'pre')
      File "/home/odoo/src/odoo/10.0/odoo/modules/migration.py", line 169, in migrate_module
        migrate(self.cr, installed_version)
      File "/home/odoo/src/odoo/10.0/odoo/addons/base/maintenance/migrations/base/9.saas~7.1.3/pre-20-lang.py", line 13, in migrate
        """)
      File "/home/odoo/src/odoo/10.0/odoo/sql_db.py", line 154, in wrapper
        return f(self, *args, **kwargs)
      File "/home/odoo/src/odoo/10.0/odoo/sql_db.py", line 231, in execute
        res = self._obj.execute(query, params)
    IntegrityError: null value in column "name" of relation "ir_model_data" violates not-null constraint
    DETAIL:  Failing row contains (5675, null, null, null, null, f, null, null, null, base, res.lang, 2).
    ```
    
    In the absence of iso_code, we can try to deduce it from `code`, as Odoo is
    doing in tools/misc.py:get_iso_codes().
    If even that doesn't yield an iso_code, we refrain from creating the xmlid.
    upg-2310414
    
    closes odoo/upgrade#6840
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/9.saas~7.1.3/pre-20-lang.py
+++ migrations/base/9.saas~7.1.3/pre-20-lang.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
+
 def migrate(cr, version):
     cr.execute("UPDATE res_lang SET iso_code='en' WHERE code='en_US'")
     cr.execute("""
+        WITH lang_table AS (
+            SELECT id, 'lang_' || COALESCE(iso_code,substring(LOWER(code),'^([a-z]{2})_\\1$')) AS langcode
+              FROM res_lang
+        )
         INSERT INTO ir_model_data(module, name, model, res_id, noupdate)
-           SELECT 'base', 'lang_' || iso_code, 'res.lang', id, false
-             FROM res_lang l
+           SELECT 'base', langcode, 'res.lang', id, false
+             FROM lang_table
             WHERE NOT EXISTS(SELECT 1
                                FROM ir_model_data
                               WHERE module='base'
-                                AND name='lang_' || l.iso_code)
+                                AND name=langcode
+                            )
+              AND langcode IS NOT NULL
     """)
