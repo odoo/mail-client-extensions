7cb42fbee0360397ea5ce830e0f4f3e0e57b5c90
temo-odoo <temo@odoo.com>
2024-11-28 11:22:58 +0530

    [IMP] mrp/saas~18.3: remove counter field from model
    - 'counter' field removed from 'mrp.production.split'
    
    Related PR:
    community- https://github.com/odoo/odoo/pull/188940
    enterprise- https://github.com/odoo/enterprise/pull/74757
    
    task- 4274773
    
    closes odoo/upgrade#6835
    
    Related: odoo/odoo#188940
    Related: odoo/enterprise#74757
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/mrp/saas~18.3.2.0/pre-migrate.py
+++ migrations/mrp/saas~18.3.2.0/pre-migrate.py
@@ -13,3 +13,4 @@ def migrate(cr, version):
     util.remove_field(cr, "mrp.workorder", "worksheet_type")
     util.remove_field(cr, "mrp.workorder", "worksheet_google_slide")
     util.remove_field(cr, "mrp.workorder", "operation_note")
+    util.remove_field(cr, "mrp.production.split", "counter")
