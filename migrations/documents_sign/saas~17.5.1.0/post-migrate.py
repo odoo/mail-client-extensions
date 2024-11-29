b62765180cb55f7d50738ca9ca2282bb5cc43ae3
syna-odoo <syna@odoo.com>
2024-11-29 10:42:17 +0000

    [FIX] documents_sign/saas~17.5 : Update Default Record
    Issue:
    ------
    When migrating a database to version 18.0 having 'documents_sign' module
    installed and trying to save any modification in the settings will raise
    Invalid field 'Sign Base Folder' error.
    
    Solution:
    -----------
    Updated the default record in the field 'documents_sign_folder_id'
    as per standard.
    
    Steps to reproduce:
    ------------------------
    1. Create a database in lower versions than 18.0  with 'documents_sign'
       module installed.
    2. Go to settings and try to make changes and save.
    3. You'll see the Invalid fields error.
    
    Enterprise PR:
    1. https://github.com/odoo/enterprise/commit/a32825ee00f2b330d99113f4d8c1488903fe744e#diff-478d2ca433e66687d14803ca4f3a2e7040cfeced0d4b4158e1bc8c4b39184faeR9
    2. https://github.com/odoo/enterprise/commit/a32825ee00f2b330d99113f4d8c1488903fe744e#diff-a18eb8f9cbf54c0029e69761c6bbd7c4962ed53c72bc33afda71e994abdab9b8R9
    
    closes odoo/upgrade#6851
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/documents_sign/saas~17.5.1.0/post-migrate.py
+++ migrations/documents_sign/saas~17.5.1.0/post-migrate.py
@@ -5,3 +5,7 @@ def migrate(cr, version):
     util.if_unchanged(cr, "documents_sign.documents_sign_template_form_inherit", util.update_record_from_xml)
     util.update_record_from_xml(cr, "documents_sign.ir_actions_server_create_sign_template_direct_create")
     util.update_record_from_xml(cr, "documents_sign.ir_actions_server_create_sign_template_direct")
+    cr.execute(
+        "UPDATE res_company SET documents_sign_folder_id = %s WHERE documents_sign_folder_id IS NULL",
+        [util.ref(cr, "documents_sign.document_sign_folder")],
+    )
