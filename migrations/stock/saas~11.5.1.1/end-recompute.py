cbdc81e07bba820ab31ae5811c490680a2f8c2c4
fja-odoo <fja@odoo.com>
2024-12-02 16:27:19 +0800

    [FIX] stock/saas~11.5: compute location's complete names in SQL
    Before:
    [98.48%] 621/630 stock.location 256-bucket processed in 7:15:41.357831
    
    After:
    08:36:34 stock: Running migration [$saas~11.5.1.1] end-recompute
    08:36:41 account: Running migration [$saas~11.4.1.1] end-10-journal
    
    task-4343728
    
    closes odoo/upgrade#6852
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/stock/saas~11.5.1.1/end-recompute.py
+++ migrations/stock/saas~11.5.1.1/end-recompute.py
@@ -1,8 +1,27 @@
-# -*- coding: utf-8 -*-
-from odoo.addons.base.maintenance.migrations import util
+from odoo.addons.base.maintenance.migrations.util.inconsistencies import break_recursive_loops
 
 
 def migrate(cr, version):
     # Necessary because translation is removed from stock.location.name
     # Check if multiple languages are installed befre triggering the recompute?
-    util.recompute_fields(cr, "stock.location", ["complete_name"])
+    # Overriding python compute for better performance:
+    # https://github.com/odoo/odoo/blob/12.0/addons/stock/models/stock_location.py#L74
+    break_recursive_loops(cr, "stock.location", "location_id")
+    cr.execute("""
+        WITH RECURSIVE location_hierarchy AS (
+            SELECT id,
+                   name AS complete_name
+              FROM stock_location
+             WHERE location_id IS NULL
+             UNION ALL
+            SELECT child.id,
+                   parent.complete_name || '/' || child.name
+              FROM stock_location child
+              JOIN location_hierarchy parent
+                ON child.location_id = parent.id
+        )
+        UPDATE stock_location
+           SET complete_name = location_hierarchy.complete_name
+          FROM location_hierarchy
+         WHERE stock_location.id = location_hierarchy.id;
+    """)
