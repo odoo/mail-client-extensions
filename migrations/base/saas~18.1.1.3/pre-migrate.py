f1f1f6724ca24a557fcfa4e419d3253fed7d8c04
Maximilien (malb) <malb@odoo.com>
2024-12-02 15:39:53 +0100

    [IMP] base/saas~18.1: remove res.partner.title model and field
    The enterprise and community PR does a lot of changes on the partner form.
    One of the change was to remove the title field and so the model.
    
    closes odoo/upgrade#6779
    
    See: odoo/odoo#187357
    See: odoo/enterprise#73879
    See: https://github.com/odoo/upgrade-util/pull/177
    Task: 4306429
    Related: odoo/upgrade-util#177
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/base/saas~18.1.1.3/pre-migrate.py
+++ migrations/base/saas~18.1.1.3/pre-migrate.py
@@ -6,3 +6,6 @@ def migrate(cr, version):
     util.remove_column(cr, "ir_act_server", "model_name")
 
     cr.execute("DROP INDEX IF EXISTS res_device_log__composite_idx")
+
+    util.remove_field(cr, "res.partner", "title")
+    util.remove_model(cr, "res.partner.title")
--- migrations/crm/saas~18.1.1.8/pre-migrate.py
+++ migrations/crm/saas~18.1.1.8/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "res.users", "target_sales_won")
     util.remove_field(cr, "res.users", "target_sales_done")
+    util.remove_field(cr, "crm.lead", "title")
