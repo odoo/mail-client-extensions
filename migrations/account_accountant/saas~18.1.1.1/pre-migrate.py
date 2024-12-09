5ef1482f3c21e89c5852efaa552c2d10b46cd929
Yosua Nicolaus <yoni@odoo.com>
2024-12-09 11:34:36 +0100

    [IMP] account_accountant/saas~18.1: remove `exception_needed` field
    This commit removes the unused field after a replacement of it was added
    in stable. `exception_needed` will be replaced by `exception_needed_fields`.
    
    related previous-stable-PR: https://github.com/odoo/enterprise/pull/74262
    related enterprise-PR: https://github.com/odoo/enterprise/pull/75358
    
    closes odoo/upgrade#6889
    
    Task-id: 4297145
    Related: odoo/enterprise#75358
    Signed-off-by: William André (wan) <wan@odoo.com>

--- migrations/account_accountant/saas~18.1.1.1/pre-migrate.py
+++ migrations/account_accountant/saas~18.1.1.1/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "account_accountant.product_template_form_view")
+    util.remove_field(cr, "account.change.lock.date", "exception_needed")
