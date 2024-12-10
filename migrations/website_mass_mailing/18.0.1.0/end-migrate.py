a77b506247699b21f571231f0b5c4c4728fd8445
Akash Pandey <apan@odoo.com>
2024-12-10 18:00:56 +0530

    [FIX] website_mass_mailing/18.0: update newsletter input form snippet
    Newsletter input form snippet has been changed in v18 with new classes which
    is required for the JS logic to handle the visibilty of subscribe or not.
    
    As these newsletter snippet is used in many website pages which has been
    created by end user, many instances of the snippet still use the old version
    which lacks the required classes. After upgrading to `v18` a error message
    pop-ups on pages where this snippets is used.
    
    ```
    UncaughtPromiseError > TypeError
    
    Uncaught Promise > Cannot read properties of null (reading 'classList')
    
    Occured on localhost:8069 on 2024-12-10 08:35:46 GMT
    
    TypeError: Cannot read properties of null (reading 'classList')
        at Class._updateSubscribeControlsStatus (http://localhost:8069/web/assets/1/6e6c0c6/web.assets_frontend_lazy.min.js:9444:866)
        at Class._updateView (http://localhost:8069/web/assets/1/6e6c0c6/web.assets_frontend_lazy.min.js:9444:355)
        at async Promise.all (index 1)
        at async Promise.all (index 0)
        at async Promise.all (index 73)
        at async Promise.all (index 1)
        at async Promise.all (index 1)
        at async createPublicRoot (http://localhost:8069/web/assets/1/6e6c0c6/web.assets_frontend_lazy.min.js:6157:1047)
    ```
    
    Related commit: odoo/odoo@a1622ce
    
    OPW - 4372680
    
    closes odoo/upgrade#6904
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/website_mass_mailing/18.0.1.0/end-migrate.py
@@ -0,0 +1,39 @@
+from copy import deepcopy
+
+from lxml import etree
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # fetch the new arch
+    cr.execute(
+        """
+        SELECT arch_db->>'en_US'
+          FROM ir_ui_view view
+          JOIN ir_model_data imd
+            ON imd.res_id = view.id
+         WHERE imd.name = 's_newsletter_subscribe_form'
+           AND imd.module = 'website_mass_mailing'
+        """
+    )
+    if cr.rowcount:
+        arch = etree.fromstring(cr.fetchone()[0])
+        new_subs_divs = arch.xpath(".//div[contains(@class, 's_newsletter_subscribe_form')]")[0]
+    else:
+        return  # couldn't find reference view
+
+    # fetch the views where old arch is present and update them
+    cr.execute(
+        r"""
+        SELECT id
+          FROM ir_ui_view
+         WHERE arch_db->>'en_US' ILIKE '%s\_newsletter\_subscribe\_form%'
+           AND website_id IS NOT NULL
+        """
+    )
+    children = new_subs_divs.getchildren()
+    for (view,) in cr.fetchall():
+        with util.skippable_cm(), util.edit_view(cr, view_id=view) as arch:
+            for newsletter_div in arch.xpath("//div[@data-snippet='s_newsletter_subscribe_form']"):
+                newsletter_div[:] = deepcopy(children)
