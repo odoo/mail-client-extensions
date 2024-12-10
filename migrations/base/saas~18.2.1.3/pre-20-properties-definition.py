278eabdb28a268bfdde9a70351064d5e243a0a21
Chong Wang (cwg) <cwg@odoo.com>
2024-12-10 15:32:27 +0100

    [FIX] core/saas~18.2: cleanup properties definition
    The redundant definition parameters are dropped when write and create instead of
    flush and read. Cleanup database for existing properties definition data.
    
    See odoo/odoo#182599
    
    closes odoo/upgrade#6906
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/base/saas~18.2.1.3/pre-20-properties-definition.py
@@ -0,0 +1,32 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute("SELECT model, name FROM ir_model_fields WHERE ttype = 'properties_definition' and store = true")
+    for model_name, field_name in cr.fetchall():
+        table_name = util.table_of_model(cr, model_name)
+        if not util.column_updatable(cr, table_name, field_name):
+            continue
+        util.explode_execute(
+            cr,
+            util.format_query(
+                cr,
+                """
+                UPDATE {table_name}
+                   SET {field_name} = (
+                            SELECT jsonb_agg(
+                                CASE
+                                    WHEN elem->>'type' = 'tag' THEN elem - ARRAY['comodel', 'domain', 'selection']
+                                    WHEN elem->>'type' = 'selection' THEN elem - ARRAY['comodel', 'domain', 'tags']
+                                    WHEN elem->>'type' IN ('many2one', 'many2many') THEN elem - ARRAY['selection', 'tags']
+                                    ELSE elem - ARRAY['comodel', 'domain', 'tags', 'selection']
+                                END)
+                            FROM jsonb_array_elements({field_name}) AS elem
+                        )
+                WHERE {field_name} IS NOT NULL
+                """,
+                table_name=table_name,
+                field_name=field_name,
+            ),
+            table=table_name,
+        )
