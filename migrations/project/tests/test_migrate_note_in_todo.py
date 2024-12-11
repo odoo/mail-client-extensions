d20630cff909bbc85de119aab6a16de6ad32a9fb
Jinane Maksoud <maji@odoo.com>
2024-12-11 17:10:52 +0100

    [FIX] project/tests: ensure personal stages exist
    The test case TestMigrateNoteInTodo in project requires
    the env user to have at least 1 personal stage before
    the execution of the test_prepare. This condition is not
    always met based on the order of running of other test
    cases. The test_check also ensures that the number of
    personal stages is the same between multiple users,
    so here we propose creating a dummy task to trigger
    the creation of any missing standard personal stages
    
    closes odoo/upgrade#6922
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/project/tests/test_migrate_note_in_todo.py
+++ migrations/project/tests/test_migrate_note_in_todo.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from odoo.addons.base.maintenance.migrations import util
 from odoo.addons.base.maintenance.migrations.testing import UpgradeCase, change_version
 
@@ -9,6 +7,11 @@ class TestMigrateNoteInTodo(UpgradeCase):
     def prepare(self):
         if not util.module_installed(self.env.cr, "note"):
             self.skipTest("`note` module not installed")
+        self.env["project.task"].create(
+            {
+                "name": "Mystery task",
+            }
+        )
         personal_stages = self.env["project.task.type"].search([("user_id", "=", self.env.uid)])
         project_tag = self.env["project.tags"].create({"name": "UPGRADE_test_migrate_note_in_todo_test"})
         task = self.env["project.task"].create({"name": "Private Task", "personal_stage_id": personal_stages[-1].id})
