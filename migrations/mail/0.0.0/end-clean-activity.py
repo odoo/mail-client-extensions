2ac66bbda6ae80f7c16cc0f128048d8a1489a436
hpr-odoo <hpr@odoo.com>
2024-12-17 13:27:50 +0000

    [FIX] mail/0.0.0: clean orphan activities
    - it's pointless to keep faulty data
    
    - UPG-2395274
    - OPW: 4401882
    - TBG: 1591
    
    closes odoo/upgrade#6952
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/mail/0.0.0/end-clean-activity.py
@@ -0,0 +1,38 @@
+from odoo.addons.base.maintenance.migrations import util
+
+
+def migrate(cr, version):
+    if not util.table_exists(cr, "mail_activity"):
+        # as it's introduced in 11.0 after https://github.com/odoo/odoo/pull/14361
+        return
+    env = util.env(cr)
+    cr.execute("SELECT res_model FROM mail_activity GROUP BY res_model")
+    for (model,) in cr.fetchall():
+        if model not in env or not env[model]._auto:
+            continue
+        table = util.table_of_model(cr, model)
+        if not util.table_exists(cr, table):
+            continue
+        util.explode_execute(
+            cr,
+            cr.mogrify(
+                util.format_query(
+                    cr,
+                    """
+                    DELETE
+                      FROM mail_activity
+                     USING mail_activity ma
+                 LEFT JOIN {table} t
+                        ON ma.res_id = t.id
+                     WHERE mail_activity.id = ma.id
+                       AND ma.res_model = %s
+                       AND ma.res_id IS NOT NULL
+                       AND t.id IS NULL
+                    """,
+                    table=table,
+                ),
+                [model],
+            ).decode(),
+            table="mail_activity",
+            alias="ma",
+        )
