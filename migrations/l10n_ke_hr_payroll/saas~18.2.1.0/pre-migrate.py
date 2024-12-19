323aeafe135f5628ae6557e54549ba4876212b6c
lebm-odoo <lebm@odoo.com>
2024-12-19 10:14:14 +0100

    [IMP] l10n_ke_hr_payroll/saas~18.2.1.0: SHIF based on the GROSS
    See odoo/enterprise#73456
    
    Problem
    ----------
    The SHIF is based on the gross and not the basic.
    There is no SHIF Relief
    
    Objective
    ----------
    Correct the SHIF implementation
    
    Solution
    ----------
    Get GROSS in SHIF computation rather than BASIC
    Remove SHIF Relief Rule
    
    task-4414345
    
    closes odoo/upgrade#6868
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/l10n_ke_hr_payroll/saas~18.2.1.0/pre-migrate.py
+++ migrations/l10n_ke_hr_payroll/saas~18.2.1.0/pre-migrate.py
@@ -1,5 +1,8 @@
 from odoo.upgrade import util
 
+
 def migrate(cr, version):
     util.remove_model(cr, "l10n.ke.hr.payroll.nhif.report.line.wizard")
     util.remove_model(cr, "l10n.ke.hr.payroll.nhif.report.wizard")
+    util.delete_unused(cr, "l10n_ke_hr_payroll.l10n_ke_employees_salary_shif_amount_hidden")
+    util.delete_unused(cr, "l10n_ke_hr_payroll.l10n_ke_employees_shif_relief")
