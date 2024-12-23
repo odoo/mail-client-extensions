8fa3055fdeac17a7a996d3fd478e4f689ac3fd3f
Ricardo Gomes Rodrigues <rigr@odoo.com>
2024-12-23 15:08:34 +0100

    [IMP] *_extract/saas~18.2.1.0: generify extracted words
    We generify the manual correction of OCRed bills feature
    to allow manual corrections for bank statements too.
    For this, instead of having a simple One2many field
    of the `invoice_id` for each extracted word, we now
    have a `res_model` and `res_id` which allows us to
    specify any record we want (i.e. `account.bank.statement`).
    
    Entreprise PR: https://github.com/odoo/enterprise/pull/76066
    Documentation PR: https://github.com/odoo/documentation/pull/11634
    
    task-id 4260464
    
    closes odoo/upgrade#6971
    
    Related: odoo/enterprise#76066
    Related: odoo/documentation#11634
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/account_disallowed_expenses/saas~18.2.1.0/pre-migrate.py
@@ -0,0 +1,13 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    # Disallowed rate field becomes Deductible rate
+    cr.execute("UPDATE account_disallowed_expenses_rate SET rate = 100.0 - rate")
+    util.add_to_migration_reports(
+        message=""
+        "Disallowed Expenses: Disallowed rates are replaced by Deductibility rates,"
+        "allowing for deductiblity over 100%.",
+        category="Accounting",
+        format="md",
+    )
