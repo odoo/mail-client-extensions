c61ee532582f9500183d1ad9761fd40a2f2b3321
apan <apan@odoo.com>
2024-12-24 10:14:31 +0000

    [FIX] hr_expense_extract/saas~17.2: safe fields from removal
    When fields are removed from the account.move model, which inherits from
    `extract.mixin`, the `mail.thread.main.attachment` model fields are also
    affected as `extract.mixin` is inherited from it. As a result, related fields
    in the model are also removed.
    
    Moreover, since `account.payment` inherits from account.move, the removal of
    inherited fields also occurs there, causing the same removal of
    `mail.thread.main.attachment` fields, which should not happen.
    
    This PR ensures the prevention of the removal of `mail.thread.main.attachment`
    fields.
    
    UPG - 2394026
    OPW - 4407028
    
    closes odoo/upgrade#6975
    
    See: odoo/upgrade-util#184
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr_expense_extract/saas~17.2.1.0/pre-migrate.py
+++ migrations/hr_expense_extract/saas~17.2.1.0/pre-migrate.py
@@ -3,4 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     if not util.module_installed(cr, "account_invoice_extract"):
-        util.remove_inherit_from_model(cr, "account.move", "extract.mixin")
+        util.remove_inherit_from_model(cr, "account.move", "extract.mixin", with_inherit_parents=True)
