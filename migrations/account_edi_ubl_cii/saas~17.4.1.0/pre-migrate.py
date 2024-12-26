360a518a71a80cbf0d454e59b838c44da81f06f1
Jinane Maksoud <maji@odoo.com>
2024-12-26 13:32:00 +0100

    [FIX] account_edi_ubl_cii/saas~17.4: remove view
    The template ubl_20_InvoicePeriod was added in stable
    17.0 and removed in FW saas~17.4
    
    https://github.com/odoo/odoo/pull/191332
    
    closes odoo/upgrade#6982
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account_edi_ubl_cii/saas~17.4.1.0/pre-migrate.py
+++ migrations/account_edi_ubl_cii/saas~17.4.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "account.move.send", "ubl_partner_warning")
     util.remove_field(cr, "account.move.send", "show_ubl_company_warning")
+    util.remove_view(cr, "account_edi_ubl_cii.ubl_20_InvoicePeriod")
