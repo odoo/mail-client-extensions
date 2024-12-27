a43511c38859e658bac9dcc2fad9dff938c7e163
kajal-odoo <kago@odoo.com>
2024-12-27 13:06:37 +0000

    [FIX] web_studio/18.0: Add currency_id element
    When upgrading to Odoo 18, if the customer modified with studio a report
    that calls the report account.document_tax_totals_template,  or calls a
    report inheriting from it, like: account.document_tax_totals,
    sale.document_tax_totals, purchase.document_tax_totals),
    the report fails with a traceback after the upgrade.
    
    This is because of the way Studio deals with reports since 16.4
    (copying the entire arch of the report into a new view).
    
    On Odoo 18, the report account.document_tax_totals_template receives a
    new parameter currency, which was not there on previous versions.
    Raising the exception when not there
    
    [here](https://github.com/odoo/odoo/blob/2fe55ae592e5812c5ee5e39ed1e8332bde608cb2/addons/account/views/report_invoice.xml#L189-L192)
    opw-4418042
    upg-2404122
    unicorn-4417707
    
    closes odoo/upgrade#6994
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/web_studio/18.0.1.0/post-migrate.py
@@ -0,0 +1,29 @@
+import lxml.etree as et
+
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        r"""
+        SELECT view.id
+          FROM ir_ui_view view
+          JOIN ir_model_data imd
+            ON imd.res_id = view.id
+           AND imd.model = 'ir.ui.view'
+         WHERE imd.module = 'studio_customization'
+           AND view.arch_db->>'en_US' LIKE '%.tax\_totals%'
+         """
+    )
+    for (view,) in cr.fetchall():
+        with util.edit_view(cr, view_id=view, active=None) as arch:
+            for node in arch.xpath(
+                '//t[@t-set="tax_totals" and not(preceding-sibling::t[@t-set="currency"] or following-sibling::t[@t-set="currency"])]'
+            ):
+                t_value = node.get("t-value")
+                prefix, _, rest = t_value.partition(".")
+                if rest != "tax_totals":
+                    continue
+                currency_value = f"{prefix}.currency_id"
+                newnode = et.Element("t", {"t-set": "currency", "t-value": currency_value})
+                node.addnext(newnode)
