7bea607638583a302d77a2d3050545e509c52096
Julien Alardot (jual) <jual@odoo.com>
2024-12-27 14:08:47 +0100

    [IMP] *expense*/saas~18.2.*.*: Bye Bye reports
    Most of the time, expenses are created as "standalone"s and forcing
    the use of an expense report is adding frustrating unnecessary steps.
    
    A simplification was decided and the model hr.expense.sheet is removed,
    giving all of its remaining important logic to the `hr.expense`.
    
    In addition, approval flow was simplified with the implementation of
    an auto-validation process, where submitting an expense also approves it
    when no expense manager is set on the employee.
    
    See task for more details.
    
    task-id: 4481615
    
    Odoo PR: https://github.com/odoo/odoo/pull/189701
    Enterprise PR: https://github.com/odoo/enterprise/pull/75507
    
    closes odoo/upgrade#6990
    
    Related: odoo/odoo#189701
    Related: odoo/enterprise#75507
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/hr_expense/saas~18.2.2.0/pre-migrate.py
@@ -0,0 +1,96 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    eb = util.expand_braces
+
+    # Removing expense sheet records (as we don't just remove_model)
+    util.remove_record(cr, 'hr.expense.ir_rule_hr_expense_sheet_manager')
+    util.remove_record(cr, 'hr.expense.ir_rule_hr_expense_sheet_approver')
+    util.remove_record(cr, 'hr.expense.ir_rule_hr_expense_sheet_employee')
+    util.remove_record(cr, 'hr.expense.ir_rule_hr_expense_sheet_employee_not_draft')
+    util.remove_record(cr, 'hr.expense.hr_expense_report_comp_rule')
+    util.remove_view(cr, 'hr_expense.hr_expense_sheet_view_activity')
+    util.remove_view(cr, 'hr_expense.hr_expense_sheet_view_search')
+    util.remove_view(cr, 'hr_expense.view_hr_expense_sheet_graph')
+    util.remove_view(cr, 'hr_expense.view_hr_expense_sheet_pivot')
+    util.remove_view(cr, 'hr_expense.view_hr_expense_sheet_kanban_header')
+    util.remove_view(cr, 'hr_expense.view_hr_expense_sheet_kanban_no_header')
+    util.remove_view(cr, 'hr_expense.view_hr_expense_sheet_kanban')
+    util.remove_view(cr, 'hr_expense.view_hr_expense_sheet_form')
+    util.remove_view(cr, 'hr_expense.view_hr_expense_sheet_dashboard_tree_header')
+    util.remove_view(cr, 'hr_expense.view_hr_expense_sheet_tree')
+
+    util.create_column(cr, 'hr_expense', 'account_move_id', 'integer')
+    util.create_column(cr, 'hr_expense', 'department_id', 'integer')
+    util.create_column(cr, 'hr_expense', 'manager_id', 'integer')
+    util.create_column(cr, 'hr_expense', 'former_sheet_id', 'integer')
+    util.create_column(cr, 'hr_expense', 'payment_method_line_id', 'integer')
+    util.create_column(cr, 'hr_expense', 'approval_date', 'timestamp')
+    util.create_column(cr, 'hr_expense', 'untaxed_amount', 'float8')
+    util.create_column(cr, 'hr_expense', 'approval_state', 'varchar')
+
+    util.explode_execute(
+        cr,
+        """
+          WITH new_values AS (
+               SELECT expense.id AS expense_id,
+                      sheet.id AS former_sheet_id,
+                      ROUND((expense.total_amount - expense.tax_amount)::numeric, COALESCE(currency.rounding, company_currency.rounding, 2)::int) AS untaxed_amount,
+                      sheet.user_id AS manager_id,
+                      sheet.approval_date AS approval_date,
+                      CASE WHEN sheet.approval_state IS NULL THEN NULL
+                           WHEN sheet.approval_state = 'submit' THEN 'submitted'
+                           WHEN sheet.approval_state = 'approve' THEN 'approved'
+                      ELSE 'refused'  -- WHEN 'cancel'
+                      END AS approval_state, -- States were renamed for more consistency
+                      sheet.payment_method_line_id AS payment_method_line_id,
+                      employee.department_id AS department_id,
+                      move_line.move_id AS move_id  -- Because it's more reliable than the expense_sheet_id on the move
+                 FROM hr_expense AS expense
+                 JOIN hr_expense_sheet AS sheet
+                   ON expense.sheet_id = sheet.id
+                 JOIN res_company AS company
+                   ON expense.company_id = company.id
+                 JOIN hr_employee AS employee
+                   ON expense.employee_id = employee.id
+                 JOIN res_currency AS company_currency
+                   ON company.currency_id = company_currency.id
+            LEFT JOIN res_currency AS currency
+                   ON expense.currency_id = currency.id
+            LEFT JOIN account_move_line AS move_line
+                   ON move_line.expense_id = expense.id
+                WHERE {parallel_filter}
+        )
+        UPDATE hr_expense
+           SET manager_id = new_values.manager_id,
+               former_sheet_id = new_values.former_sheet_id,
+               approval_date = new_values.approval_date,
+               approval_state = new_values.approval_state,
+               payment_method_line_id = new_values.payment_method_line_id,
+               department_id = new_values.department_id,
+               account_move_id = new_values.move_id,
+               untaxed_amount = new_values.untaxed_amount
+         FROM new_values
+        WHERE hr_expense.id = new_values.expense_id
+        """,
+        'hr_expense',
+        alias='expense'
+    )
+    util.remove_field(cr, 'account.move', 'show_commercial_partner_warning')
+    util.remove_field(cr, 'hr.expense', 'approved_by')
+    util.remove_field(cr, 'hr.expense', 'approved_on')
+    util.remove_field(cr, 'hr.expense', 'accounting_date')
+
+    util.remove_field(cr, 'account.move', 'expense_sheet_id')
+    util.remove_field(cr, 'account.payment', 'expense_sheet_id')
+    util.remove_field(cr, 'hr.expense.approve.duplicate', 'sheet_ids')
+    util.remove_field(cr, 'hr.expense.refuse.wizard', 'sheet_ids')
+    util.remove_field(cr, 'hr.expense', 'sheet_id')
+
+    util.rename_field(cr, 'hr.department', 'expense_sheets_to_approve_count', 'expenses_to_approve_count')
+    util.rename_xmlid(cr, *eb('hr_expense.report_expense{_sheet,}_img'))
+    util.rename_xmlid(cr, *eb('hr_expense.report_expense{_sheet,}'))
+    util.rename_xmlid(cr, *eb('hr_expense.action_report_hr_expense{_sheet,}'))
+    util.rename_xmlid(cr, *eb('hr_expense.action_report_expense{_sheet,}_img'))
+    util.rename_xmlid(cr, *eb('hr_expense.hr_expense{_sheet,}_view_search_with_panel'))
---
+++ migrations/hr_payroll_expense/saas~18.2.1.0/pre-migrate.py
@@ -0,0 +1,21 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, 'hr_expense', 'payslip_id', 'integer')
+    util.create_column(cr, 'hr_expense', 'refund_in_payslip', 'boolean')
+    util.remove_view(cr, 'hr_payroll_expense.hr_expense_sheet_view_form_inherit_payroll')
+    util.remove_field(cr, 'hr.payslip', 'expense_sheet_ids')
+
+    if util.table_exists(cr, 'hr_expense_sheet'):
+        util.explode_execute(
+            cr,
+            """
+                UPDATE hr_expense
+                   SET payslip_id = hr_expense_sheet.payslip_id,
+                       refund_in_payslip = hr_expense_sheet.refund_in_payslip
+                  FROM hr_expense_sheet
+                 WHERE hr_expense.former_sheet_id = hr_expense_sheet.id
+            """,
+            'hr_expense',
+        )
