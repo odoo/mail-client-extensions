bfdaaa0f2a02fbc451b1c390d963511819c69efd
aamo-odoo <aamo@odoo.com>
2025-01-01 17:59:31 +0530

    [FIX] base/saas~17.5: set layout_background to default
    Issue:
    The 'layout_background' field got empty while configuring the document layout.
    
    Cause: In version 18.0, the 'layout_background' field of the 'res_company' model
    was updated, replacing the 'Geometric' option with 'Demo Logo.' If 'Geometric'
    was selected in earlier versions, an error occurs when configuring the report
    layout from the settings.
    
    Steps to reproduce:
    - Create a database in a lower version than 18.0 and install 'Accounting' module
    - Settings > Companies > Document Layout > Configure Document Layout
    - Upgrade to 18.0 and we will see 'layout_background' empty
    - Click 'Continue'
    
    Traceback:
    ValueError: Wrong value for res.company.layout_background: 'Geometric'
    
    Solution:
    - To resolve this issue, the value of 'layout_background' has been updated from
    'Geometric' to 'Custom' and set Background Image
    for that add previous background image in script and save it as
    attachment.
    - This change ensures that invalid selections are avoided, and the field
    maintains a valid default value, preventing errors during the 'report_layout'
    configuration.
    
    After this PR:
    https://github.com/odoo/odoo/commit/87258f05c7398173d3df6ec39869f52031502ede#diff-3d57dc213e6960bf9aaeaf637436ff6435776e50a55b35669baf8c0dbde7e1ecR78
    
    sentry-6117966601
    
    closes odoo/upgrade#7002
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/saas~17.5.1.3/post-migrate.py
+++ migrations/base/saas~17.5.1.3/post-migrate.py
@@ -1,5 +1,22 @@
+import base64
+from pathlib import Path
+
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
     util.if_unchanged(cr, "base.MVR", util.update_record_from_xml)
+    cr.execute(
+        """
+        SELECT id
+          FROM res_company
+         WHERE layout_background = 'Geometric'
+        """
+    )
+    if cr.rowcount:
+        cids = [cid for (cid,) in cr.fetchall()]
+        env = util.env(cr)
+        background_image = (Path(__file__).parent / "bg_background_template.jpg").read_bytes()
+        env["res.company"].browse(cids).write({"layout_background_image": base64.encodebytes(background_image)})
+
+    util.change_field_selection_values(cr, "res.company", "layout_background", {"Geometric": "Custom"})
