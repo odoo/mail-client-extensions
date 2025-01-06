8002aa9ebc9a548d681d21cd684fb211173c1412
Mahamadasif Ansari <maan@odoo.com>
2025-01-06 15:58:48 +0530

    [FIX] iap/saas~17.5: deleted uninstalled modules iap account
    An exception was generated when the user was trying to install
    `partner_autocomplete` after upgrading to Odoo 18.
    
    error:
    `UniqueViolation: duplicate key value violates unique constraint
    "iap_service_unique_technical_name"`
    
    Steps to produce an error:
    - Create a database in v17 and install `partner_autocomplete`
    - Uninstall `partner_autocomplete` and upgrade the database to V18
    - An error will occur when the user tries to install `partner_autocomplete`
    
    This is because:
    - In the previous version, the user was installed above the module and
      later uninstalled the module.
    - Changes with commit [1] refactored the IAP module for better registration
      flow of IAP and added iap_service module.
    - The upgrade script related to commit [1] was added, which converts
      iap_account records to iap_service with [2].
    - Now when the user tries to install the `partner_autocomplete` module,
      it causes an error because [3] tries to convert iap_service data into
      a record, but it already exists.
    
    This commit will fix the above issue by deleting iap_account records
    for which modules were uninstalled by the user.
    
    [1] - https://github.com/odoo/odoo/commit/ae35752ce7aeb6e62e7ef0ec46c15dd9d23571cc
    [2] - https://github.com/odoo/upgrade/commit/042565e439d515344b257c0f88d011e312ec6fe5#diff-709d4ac4ff8034659a21a0545b5374418526200322e731ef23f506c10f3030d8R18-R24
    [3] - https://github.com/odoo/odoo/blob/2723a1dad154297302ceb6379131e6e83c7a7836/addons/partner_autocomplete/data/iap_service_data.xml#L4
    
    sentry-6178052620
    
    closes odoo/upgrade#7024
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/iap/saas~17.5.1.1/pre-migrate.py
+++ migrations/iap/saas~17.5.1.1/pre-migrate.py
@@ -2,6 +2,25 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    module_iap_invoice_map = {
+        "iap_extract": "invoice_ocr",
+        "sign_itsme": "itsme_proxy",
+        "l10n_in_edi": "l10n_in_edi",
+        "l10n_br_avatax": "l10n_br_avatax_proxy",
+        "l10n_pe_edi": "l10n_pe_edi",
+        "partner_autocomplete": "partner_autocomplete",
+        "sms": "sms",
+        "snailmail": "snailmail",
+    }
+
+    iap_account_without_module = {
+        iap_account for module, iap_account in module_iap_invoice_map.items() if not util.module_installed(cr, module)
+    }
+
+    if iap_account_without_module:
+        cr.execute("SELECT id FROM iap_account WHERE service_name IN %s", [tuple(iap_account_without_module)])
+        util.remove_records(cr, "iap.account", [aid for (aid,) in cr.fetchall()])
+
     cr.execute("""
         CREATE TABLE iap_service (
             id SERIAL NOT NULL PRIMARY KEY,
