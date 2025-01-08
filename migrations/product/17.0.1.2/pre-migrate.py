5b875560ab1edd40de3c4a6ff58c96fc2aeb1dae
sagu-odoo <sagu@odoo.com>
2025-01-08 07:14:49 +0000

    [FIX] base/0.0.0: Fix warning(pricelist) coming in settings
    Step to reproduce:
    1. create db in 16.0 and install sale module
    2. In setting activate pricelist create product pricelist
       and then deactivate the pricelist setting
    3. upgrade database to 17.0 and open setting you will see
      warning popup
    
    From 16.2 new warning ``group_product_pricelist`` [introduced]
    (https://github.com/odoo/odoo/commit/83c52575d0ce3d7deb1737c622c6ce366db8c31b#diff-0ec802f7b85a746e7604f709c2a9aa0a287fff945f78a0f05e92e853b2cdb227R4)
    in product module regarding if priclist feature is disabled
    so related to that product priclist should be deactivated.
    
    So according to this fixing warning should not come after
    upgrading to 17.0 by deactivating pricelist.
    
    upg-2427534
    opw-4426941
    
    closes odoo/upgrade#7034
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/product/17.0.1.2/pre-migrate.py
+++ migrations/product/17.0.1.2/pre-migrate.py
@@ -3,3 +3,9 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "product_supplierinfo", "discount", "float8", default=0)
+    cr.execute(
+        "SELECT 1 FROM res_groups_implied_rel WHERE gid = %s AND hid = %s",
+        [util.ref(cr, "base.group_user"), util.ref(cr, "product.group_product_pricelist")],
+    )
+    if not cr.rowcount:
+        cr.execute("UPDATE product_pricelist SET active = False WHERE active")
