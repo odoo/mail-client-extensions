6b2069ad1c0db0c5f0122399c82a4f72d3d42a7d
aizu-odoo <aizu@odoo.com>
2025-01-09 14:32:19 +0100

    [IMP] account/saas~18.2: memo for grouped payments
    As the memo generated for grouped payments is being changed from "BATCH/XX" to
    "GROUP/XX", the prefix saved for existing companies needs to be changed.
    
    https://github.com/odoo/odoo/pull/192324
    task-4356414
    
    closes odoo/upgrade#7014
    
    Related: odoo/odoo#192324
    Signed-off-by: Quentin De Paoli (qdp) <qdp@odoo.com>

--- migrations/account/saas~18.2.1.4/pre-migrate.py
+++ migrations/account/saas~18.2.1.4/pre-migrate.py
@@ -3,3 +3,11 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "account.product_template_view_tree")
+
+    cr.execute("""
+      UPDATE ir_sequence
+         SET prefix = 'GROUP/%(year)s/'
+        FROM res_company c
+       WHERE ir_sequence.prefix = 'BATCH/%(year)s/'
+         AND c.batch_payment_sequence_id = ir_sequence.id
+    """)
