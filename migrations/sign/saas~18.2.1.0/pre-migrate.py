b0dfa236b0fd71be8dfde1e1b6f96b79dc0be7c1
Gabriel (gdpf) <gdpf@odoo.com>
2025-01-17 17:05:01 +0100

    [IMP] sign/saas~18.2: remove sign item role color field
    Sign item role colors are now configured on the js part
    due to the recent changes in the ui/ux so no need to store them.
    Update existing request's field "is_shared" checking the state selection.
    
    Enterprise PR: odoo/enterprise#75291
    
    closes odoo/upgrade#7076
    
    Task: 4387560
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/sign/saas~18.2.1.0/pre-migrate.py
+++ migrations/sign/saas~18.2.1.0/pre-migrate.py
@@ -4,3 +4,11 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "sign.cancel_sign_request_item_with_confirmation")
     util.remove_view(cr, "sign.canceled_sign_request_item")
+
+    # Drop the color field of roles as it is not used anymore.
+    util.remove_field(cr, "sign.item.role", "color")
+
+    # Remove unique name for roles constrait, menu and action.
+    util.remove_constraint(cr, "sign_item_role", "sign_item_role_name_uniq")
+    util.remove_menus(cr, [util.ref(cr, "sign.sign_item_role_menu")])
+    util.remove_record(cr, "sign.sign_item_role_action")
