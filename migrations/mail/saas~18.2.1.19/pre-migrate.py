0efdce9a1bf4d018f0da1626d42f479001caaa06
Sébastien Theys <seb@odoo.com>
2025-01-20 10:40:24 +0100

    [ADD] mail/saas~18.2: remove fold_state from discuss.channel.member
    The field was removed.
    
    See https://github.com/odoo/odoo/pull/193245
    See https://github.com/odoo/enterprise/pull/77022
    
    closes odoo/upgrade#7056
    
    Related: odoo/odoo#193245
    Related: odoo/enterprise#77022
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/mail/saas~18.2.1.19/pre-migrate.py
+++ migrations/mail/saas~18.2.1.19/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "discuss.channel", "allow_public_upload")
+    util.remove_field(cr, "discuss.channel.member", "fold_state")
