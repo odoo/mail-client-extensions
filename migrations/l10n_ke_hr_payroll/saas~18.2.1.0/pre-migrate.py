beaf55d9f7b1935ab6ff024bb453d588a39eb10b
Yannick Tivisse <yti@odoo.com>
2025-01-20 13:13:20 +0100

    [FIX] l10n_ke_hr_payroll/saas~18.2: Correctly delete salary rules
    Fixing: https://github.com/odoo/upgrade/blob/8fda3684fae06ae6ddfc6c2f32dadd965ace941f/migrations/l10n_ke_hr_payroll/saas~18.2.1.0/pre-migrate.py#L7-L8
    Taskid: 3673251
    Part-of: odoo/upgrade#7091
    Related: odoo/upgrade-util#197
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/l10n_ke_hr_payroll/saas~18.2.1.0/pre-migrate.py
+++ migrations/l10n_ke_hr_payroll/saas~18.2.1.0/pre-migrate.py
@@ -4,5 +4,7 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_model(cr, "l10n.ke.hr.payroll.nhif.report.line.wizard")
     util.remove_model(cr, "l10n.ke.hr.payroll.nhif.report.wizard")
-    util.delete_unused(cr, "l10n_ke_hr_payroll.l10n_ke_employees_salary_shif_amount_hidden")
-    util.delete_unused(cr, "l10n_ke_hr_payroll.l10n_ke_employees_shif_relief")
+
+    scripts = util.import_script("l10n_ke_hr_payroll/0.0.0/pre-migrate.py")
+    scripts.remove_salary_rule(cr, "l10n_ke_hr_payroll.l10n_ke_employees_salary_shif_amount_hidden")
+    scripts.remove_salary_rule(cr, "l10n_ke_hr_payroll.l10n_ke_employees_shif_relief")
