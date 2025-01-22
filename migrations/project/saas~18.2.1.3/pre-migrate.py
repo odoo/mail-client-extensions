64452e0322e5a166606ffe0c4ecfefae001de984
Vincent Larcin <vila@odoo.com>
2025-01-22 13:38:55 +0100

    [IMP] project/saas~18.2: move "email_from" from project_website
    This commit moves the "email_from" field on the "project.task" model
    from "website_project" to "project", so that it can later be used in
    features preventing infinite email loops
    
    Community: https://github.com/odoo/odoo/pull/194699
    
    Task-4461572
    
    closes odoo/upgrade#7106
    
    Related: odoo/odoo#194699
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/project/saas~18.2.1.3/pre-migrate.py
+++ migrations/project/saas~18.2.1.3/pre-migrate.py
@@ -3,3 +3,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "report.project.task.user", "active")
+    if util.module_installed(cr, "website_project"):
+        util.move_field_to_module(cr, "project.task", "email_from", "website_project", "project")
