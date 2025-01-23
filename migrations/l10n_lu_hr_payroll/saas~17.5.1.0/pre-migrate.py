29d2e0e35fdb1ad2395ba6b60f215b23c6f31d40
asno-odoo <asno@odoo.com>
2025-01-23 17:11:39 +0100

    [FIX] l10n_lu_hr_payroll/saas~17.5: remove/rename salary rule
    Salary rules should be removed using a special function as its not
    a normal record.
    
    upg-pr: https://github.com/odoo/upgrade/pull/6509
    enterprise-pr: https://github.com/odoo/enterprise/pull/67001
    
    Also rename the records that were forgotten during the pr
    mentioned above.
    
    closes odoo/upgrade#7120
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/l10n_lu_hr_payroll/saas~17.5.1.0/pre-migrate.py
+++ migrations/l10n_lu_hr_payroll/saas~17.5.1.0/pre-migrate.py
@@ -1,4 +1,5 @@
 from odoo.upgrade import util
+from odoo.upgrade.util.hr_payroll import remove_salary_rule
 
 
 def migrate(cr, version):
@@ -11,25 +12,33 @@ def migrate(cr, version):
     util.delete_unused(cr, "l10n_lu_hr_payroll.hr_salary_rule_category_total_tax_credit")
     util.delete_unused(cr, "l10n_lu_hr_payroll.hr_salary_rule_category_cotisation_base")
 
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_atn_transport")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_atn_transport_without_VAT")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_atn_transport_VAT")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_vpa")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_gross_tmp")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_cotisation_base")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_transport_fees")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_total_deduction")
     util.rename_xmlid(cr, *eb("l10n_lu_hr_payroll.l10n_lu_employees{_employee,}_cis"))
     util.rename_xmlid(cr, *eb("l10n_lu_hr_payroll.l10n_lu_employees{_employee,_cis}_ci_co2"))
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_total_tax_credit")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_employee_tax_credit")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_gratification")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_gratification_net_gratification")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_gross_meal_vouchers_2")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_atn_transport_without_VAT_2")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_atn_transport_VAT_2")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_benefit_various_2")
-    util.delete_unused(cr, "l10n_lu_hr_payroll.l10n_lu_employees_net_to_pay")
+    util.rename_xmlid(cr, *eb("l10n_lu_hr_payroll.l10n_lu_employees_{thirteen,13th}_month_health_fund"))
+    util.rename_xmlid(cr, *eb("l10n_lu_hr_payroll.l10n_lu_employees_{thirteen,13th}_month_retirement_fund"))
+    util.rename_xmlid(cr, *eb("l10n_lu_hr_payroll.l10n_lu_employees_{thirteen,13th}_month_gross_gratification"))
+    util.rename_xmlid(
+        cr, *eb("l10n_lu_hr_payroll.l10n_lu_employees_{thirteen,13th}_month_gross_gratification_rounding")
+    )
+    util.rename_xmlid(cr, *eb("l10n_lu_hr_payroll.l10n_lu_employees_{thirteen,13th}_month_tax_on_non_period_revenue"))
+
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_atn_transport")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_atn_transport_without_VAT")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_atn_transport_VAT")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_vpa")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_gross_tmp")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_cotisation_base")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_transport_fees")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_total_deduction")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_total_tax_credit")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_employee_tax_credit")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_gratification")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_gratification_net_gratification")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_gross_meal_vouchers_2")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_atn_transport_without_VAT_2")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_atn_transport_VAT_2")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_benefit_various_2")
+    remove_salary_rule(cr, "l10n_lu_hr_payroll.l10n_lu_employees_net_to_pay")
 
     util.rename_xmlid(cr, *eb("l10n_lu_hr_payroll.report_payslip_lux_{thirteen_month,gratification}"))
     util.rename_xmlid(cr, *eb("l10n_lu_hr_payroll.report_payslip_lux_lang_{thirteen_month,gratification}"))
