dec96e1a1fe50f7cd6bd7efa1dbe75da3fbeb0e4
Christophe Simonis <chs@odoo.com>
2025-01-23 11:54:08 +0100

    [chore] base/16.0: lint file
    - remove commented code
    - don't use f-string in logging
    - remove useless variable
    
    Part-of: odoo/upgrade#7111
    Related: odoo/upgrade-util#198
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/16.0.1.3/pre-90-convert-bootstrap5.py
+++ migrations/base/16.0.1.3/pre-90-convert-bootstrap5.py
@@ -23,35 +23,12 @@ def convert_views_bootstrap(cr, src_version, dst_version, views_ids):
     """
     if not views_ids:
         return
-    _logger.info(f"Converting {len(views_ids)} views/templates Bootstrap code from {src_version} to {dst_version}")
+    _logger.info("Converting %s views/templates Bootstrap code from %s to %s", len(views_ids), src_version, dst_version)
 
     for view_id in views_ids:
         with util.edit_view(cr, view_id=view_id, active=None) as arch:
             convert_tree(arch, src_version, dst_version, is_qweb=True)
         # TODO abt: maybe notify in the log or report that custom views with noupdate=False were converted?
-
-
-# def bootstrap_html_converter(src_version, dst_version):
-#     """
-#     Creates a function that converts HTML Bootstrap code from ``src_version`` to ``dst_version``.
-
-#     The returned function accepts a single argument, the html content to convert,
-#     and returns a tuple of (has_changed, converted_html), suitable for usage with
-#     ``util.snippets.convert_html_content``.
-
-#     :param str src_version: the Bootstrap version to convert from.
-#     :param str dst_version: the Bootstrap version to convert to.
-#     :rtype: callable[[str], tuple[bool, str]]
-#     """
-
-#     @lru_cache(maxsize=128)  # does avg 20~30% hits
-#     def convert(content):
-#         if not content:
-#             return False, content
-#         converted_content = convert_arch(content, src_version, dst_version, is_html=True, is_qweb=True)
-#         return content != converted_content, converted_content
-
-#     return convert
 
 
 def convert_views(cr):
@@ -105,7 +82,7 @@ def convert_views(cr):
         """,
         [tuple(standard_modules)],
     )
-    views_ids = [view_id for view_id, in cr.fetchall()]
+    views_ids = [view_id for (view_id,) in cr.fetchall()]
     if views_ids:
         convert_views_bootstrap(cr, "4.0", "5.0", views_ids)
 
@@ -148,23 +125,21 @@ def get_bs5_where_clause(cr, column):
     def add_word_delimiters(keyword):
         keyword = re.sub(r"^(?=\w)", r"\\m", keyword)
         keyword = re.sub(r"(?<=\w)$", r"\\M", keyword)
-        return keyword
+        return keyword  # noqa RET504
 
     column = ".".join(f'"{part}"' for part in column.split("."))
     classes, other_kwds = get_keyword_list()
-    clause = cr.mogrify(
+    return cr.mogrify(
         rf"(({column} ~ '\mclass\M\s*=' AND {column} ~ %(classes)s) OR {column} ~ %(others)s)",
         {
             "classes": "|".join(add_word_delimiters(cls) for cls in classes),
             "others": "|".join(add_word_delimiters(kw) for kw in other_kwds),
         },
     ).decode()
-    return clause
 
 
 def convert_html_fields(cr):
     _logger.info("Converting html fields data Bootstrap code from 4.0 to 5.0")
-    # converter_fn = bootstrap_html_converter("4.0", "5.0")
     converter_fn = BootstrapHTMLConverter("4.0", "5.0")
 
     html_fields = list(snippets.html_fields(cr))
@@ -175,18 +150,6 @@ def convert_html_fields(cr):
                 for column in columns
             )
             snippets.convert_html_columns(cr, table, columns, converter_fn, extra_where=extra_where)
-
-    # convert_cache_info = converter_fn.cache_info()
-    # processed_count = convert_cache_info.hits + convert_cache_info.misses
-    # if processed_count:
-    #     _logger.info(
-    #         f"Processed {processed_count} html fields values"
-    #         f" ({convert_cache_info.hits / processed_count:.2%} cache hits)"
-    #     )
-    # else:
-    #     _logger.info("No html fields values to process found")
-
-    # converter_fn.cache_clear()
 
 
 def migrate(cr, version):
