f18144a13446330d9150f934235934c10e035472
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-01-27 12:13:44 +0100

    [IMP] hr_appraisal/saas~18.2: make `employee_id` M2M
    See odoo/enterprise#75369
    
    - change the `employee_id` to `employee_ids` and adapt the db to it
    - change the `manager_id` to `manager_ids` and adapt the db to it
    - remove `manager_user_id` as it does not being used anywhere
    
    closes odoo/upgrade#6896
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/hr_appraisal/saas~18.2.1.1/pre-migrate.py
+++ migrations/hr_appraisal/saas~18.2.1.1/pre-migrate.py
@@ -36,3 +36,27 @@ def migrate(cr, version):
     )
 
     util.rename_field(cr, "hr.departure.wizard", "cancel_appraisal", "delete_appraisal")
+    util.create_m2m(cr, "hr_appraisal_goal_hr_employee_rel", "hr_appraisal_goal", "hr_employee")
+    # fill the many2many field with the employee_id first
+    cr.execute("""
+        INSERT INTO hr_appraisal_goal_hr_employee_rel (hr_appraisal_goal_id, hr_employee_id)
+        SELECT id, employee_id
+          FROM hr_appraisal_goal
+         WHERE employee_id IS NOT NULL
+    """)
+    # remove employee_id by renaming it to update the domains and then drop the col
+    util.rename_field(cr, "hr.appraisal.goal", "employee_id", "employee_ids")
+    util.remove_column(cr, "hr_appraisal_goal", "employee_ids")
+
+    util.create_m2m(cr, "hr_appraisal_goal_hr_employee_manager_rel", "hr_appraisal_goal", "hr_employee")
+    # fill the many2many field with the manager_id first
+    cr.execute("""
+        INSERT INTO hr_appraisal_goal_hr_employee_manager_rel (hr_appraisal_goal_id, hr_employee_id)
+        SELECT id, manager_id
+          FROM hr_appraisal_goal
+         WHERE manager_id IS NOT NULL
+    """)
+    # remove manager_id by renaming it to update the domains and then drop the col
+    util.rename_field(cr, "hr.appraisal.goal", "manager_id", "manager_ids")
+    util.remove_column(cr, "hr_appraisal_goal", "manager_ids")
+    util.remove_field(cr, "hr.appraisal.goal", "manager_user_id")
