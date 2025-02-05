383d77e7bce70bd5dc50b641135d67e696dedff6
Brieuc-brd <brd@odoo.com>
2025-02-05 13:56:56 +0100

    [IMP] website/saas~18.3: standardize s_tabs options
    `s_tabs_images` has been introduced and uses the same options as
    `s_tabs`. From now on, `s_tabs` options are no longer specific to this
    snippet. We moved them to the website.snippet_options template.
    
    task-4094407
    Part of task-4077427
    
    See odoo/odoo#185642 and odoo/design-themes#1001
    
    closes odoo/upgrade#6691
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/website/saas~18.3.1.0/pre-migrate.py
+++ migrations/website/saas~18.3.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade.util import remove_view
 def migrate(cr, version):
     remove_view(cr, "website.snippet_options_carousel_intro")
     remove_view(cr, "website.s_carousel_intro_options")
+    remove_view(cr, "website.s_tabs_options")
