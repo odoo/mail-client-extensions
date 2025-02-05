d0ff1e85ea7aaf4c055a113d3fb5b7a1580ceb54
shiyam kumar <shku@odoo.com>
2025-02-05 05:41:51 +0000

    [FIX] web_studio/18.0: Resolve duplicate invoice total
    Description:
    - In Odoo 18.0, the template
    'account.document_tax_totals_company_currency_template'
    is conditionally included based on whether the 'display_in_company_currency'
    flag is set in the 'tax_totals' object. Specifically, this condition checks if
    the company currency should be used for displaying tax totals on the invoice.
    
    - However, in Odoo 17.0, the same
    'account.document_tax_totals_company_currency_template' is included
    unconditionally.
    
    - When customers have customized their reports in Odoo 17 using Studio, the
    report templates may have included the
    'account.document_tax_totals_company_currency_template' without the condition
    introduced in Odoo 18. As a result, during the migration process, if the
    'display_in_company_currency' condition is not applied, the report will display
    the tax totals twice:
    1. Once from the 'account.document_tax_totals template'.
    2. Again from the 'account.document_tax_totals_company_currency_template'
    template.
    
    - This results in duplicate tax totals being shown on the invoice, which can
    confuse users.
    
    Fix:
    1. Wrap a t-if to check if o.tax_totals.get('display_in_company_currency') is
    true, ensuring tax totals are displayed only when necessary.
    2. Add a t-else block with an empty div (class oe_structure) to maintain the
    template structure and prevent layout issues if the condition isn't met.
    3. Ensure tax totals are displayed only once after migration by adding the
    standard conditional check and t-else block, eliminating duplicate lines.
    
    17.0: https://github.com/odoo/odoo/blob/17.0/addons/account/views/report_invoice.xml#L223
    18.0: https://github.com/odoo/odoo/blob/18.0/addons/account/views/report_invoice.xml#L229
    OPW - 4521432
    UPG - 2450524
    
    closes odoo/upgrade#7164
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/web_studio/18.0.1.0/post-migrate.py
+++ migrations/web_studio/18.0.1.0/post-migrate.py
@@ -12,11 +12,16 @@ def migrate(cr, version):
             ON imd.res_id = view.id
            AND imd.model = 'ir.ui.view'
          WHERE imd.module = 'studio_customization'
-           AND view.arch_db->>'en_US' LIKE '%.tax\_totals%'
-         """
+           AND (
+               view.arch_db->>'en_US' LIKE '%.tax\_totals%'
+               OR view.arch_db->>'en_US' LIKE '%account.document\_tax\_totals\_company\_currency\_template%'
+           )
+        """
     )
+
     for (view,) in cr.fetchall():
         with util.edit_view(cr, view_id=view, active=None) as arch:
+            # Process tax_totals modifications
             for node in arch.xpath(
                 '//t[@t-set="tax_totals" and not(preceding-sibling::t[@t-set="currency"] or following-sibling::t[@t-set="currency"])]'
             ):
@@ -27,3 +32,24 @@ def migrate(cr, version):
                 currency_value = f"{prefix}.currency_id"
                 newnode = et.Element("t", {"t-set": "currency", "t-value": currency_value})
                 node.addnext(newnode)
+
+            # Process company currency template modifications
+            for node in arch.xpath('//t[@t-call="account.document_tax_totals_company_currency_template"]'):
+                parent = node.getparent()
+                if parent.get("t-if") == "o.tax_totals.get('display_in_company_currency')":
+                    continue
+                new_nodes = et.fromstring(
+                    """
+                  <dummy>
+                      <t t-if="o.tax_totals.get('display_in_company_currency')">
+                          <t t-set="tax_totals" t-value="o.tax_totals"/>
+                          <t t-call="account.document_tax_totals_company_currency_template"/>
+                      </t>
+                      <t t-else="">
+                          <div class="oe_structure"/>
+                      </t>
+                  </dummy>
+                  """
+                )
+                idx = parent.index(node)
+                parent[idx : idx + 1] = new_nodes.getchildren()
