9d40a6dca7dabf713967b35f53982142970a97c6
Akash <apan@odoo.com>
2025-02-06 09:50:02 +0000

    [FIX] spreadsheet_dashboard_edition/saas~17.5: remove extra argument
    see: 1f76f7a
    As per the above commit calling `rename_field` didn't go as expected.
    But seems forgot to remove the extra argument.
    
    closes odoo/upgrade#7176
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/spreadsheet_dashboard_edition/saas~17.5.1.0/pre-migrate.py
+++ migrations/spreadsheet_dashboard_edition/saas~17.5.1.0/pre-migrate.py
@@ -2,4 +2,4 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.remove_field(cr, "spreadsheet.dashboard", "file_name", "spreadsheet_file_name")
+    util.remove_field(cr, "spreadsheet.dashboard", "file_name")
