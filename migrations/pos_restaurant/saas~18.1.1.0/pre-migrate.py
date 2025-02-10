c284ba44e380434b6c5517b3552be9a83be401eb
asno-odoo <asno@odoo.com>
2025-02-10 20:09:43 +0000

    [FIX] pos_restaurant/saas~18.1: make presets
    The presets for `Eat In` and 'Takeout` were moved from point_of_sale
    module to pos_restaurant module here: odoo/odoo#190159. The
    new file is not in the manifest and so its not loaded and
    update_record_from_xml won't work. Also adding xmlid ensures
    that we don't up with duplicate records when this function is called
    from ui:
    https://github.com/odoo/odoo/blob/saas-18.1/addons/point_of_sale/static/src/backend/pos_kanban_view/pos_kanban_view.js#L115
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/saas-18.1/odoo/service/server.py", line 1295, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "<decorator-gen-13>", line 2, in new
      File "/home/odoo/src/odoo/saas-18.1/odoo/tools/func.py", line 98, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/saas-18.1/odoo/orm/registry.py", line 143, in new
        load_modules(registry, force_demo, update_module=update_module)
      File "/home/odoo/src/odoo/saas-18.1/odoo/modules/loading.py", line 528, in load_modules
        processed_modules += load_marked_modules(
      File "/home/odoo/src/odoo/saas-18.1/odoo/modules/loading.py", line 405, in load_marked_modules
        loaded, processed = load_module_graph(
      File "/home/odoo/src/odoo/saas-18.1/odoo/modules/loading.py", line 205, in load_module_graph
        migrations.migrate_module(package, 'pre')
      File "/home/odoo/src/odoo/saas-18.1/odoo/modules/migration.py", line 263, in migrate_module
        mod.migrate(self.cr, installed_version)
      File "/tmp/tmp9bi49_9j/migrations/pos_restaurant/saas~18.1.1.0/pre-migrate.py", line 61, in migrate
        cr.execute(query, [takeout_preset_id, eat_in_preset_id])
      File "/home/odoo/src/odoo/saas-18.1/odoo/sql_db.py", line 421, in execute
        self._obj.execute(query, params)
    psycopg2.errors.NotNullViolation: null value in column "pos_preset_id" of relation "pos_config_pos_preset_rel" violates not-null constraint
    DETAIL:  Failing row contains (3, null).
    ```
    
    The original prs
    
    closes odoo/upgrade#7204
    
    Upgrade: #6743
    Community: odoo/odoo#186359
    Entperprise: odoo/enterprise#73348
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/pos_restaurant/saas~18.1.1.0/pre-migrate.py
+++ migrations/pos_restaurant/saas~18.1.1.0/pre-migrate.py
@@ -3,7 +3,28 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.delete_unused(cr, "pos_restaurant.product_category_pos_food")
-    eat_in_preset_id = util.ref(cr, "point_of_sale.pos_takein_preset")
+
+    # The pos_takein_preset and pos_takout_preset are only loaded from ui (or in demo mode) using a javascript function
+    # https://github.com/odoo/odoo/blob/saas-18.1/addons/point_of_sale/static/src/backend/pos_kanban_view/pos_kanban_view.js#L115
+    # in the backend data is loaded in init mode, thus info is updated always
+    # https://github.com/odoo/odoo/blob/8b2cf67e32c6584ce36acf952ad298072d863b43/addons/pos_restaurant/models/pos_config.py#L77
+    # we insert here the minimal mandatory info
+    cr.execute(
+        """
+        WITH _preset AS (
+            INSERT INTO pos_preset(
+                    name, identification, color, slots_per_interval, interval_time
+                    )
+              VALUES ('Eat In', 'none', '4', '5', '20')
+            RETURNING id
+        )
+        INSERT INTO ir_model_data (module, name, model, noupdate, res_id)
+           SELECT 'pos_restaurant', 'pos_takein_preset', 'pos.preset', TRUE,id
+             FROM _preset
+        RETURNING res_id
+        """
+    )
+    eat_in_preset_id = cr.fetchone()[0]
 
     if util.column_exists(cr, "pos_config", "self_ordering_takeaway"):
         cr.execute("UPDATE pos_config SET takeaway = true WHERE self_ordering_takeaway IS true")
@@ -44,7 +65,23 @@ def migrate(cr, version):
     """
     cr.execute(query, [eat_in_preset_id])
 
-    takeout_preset_id = util.ref(cr, "point_of_sale.pos_takeout_preset")
+    cr.execute(
+        """
+        WITH _preset AS (
+            INSERT INTO pos_preset(
+                    name, identification, color, slots_per_interval, interval_time
+                    )
+              VALUES ('Takeout', 'none', '3', '5', '20')
+            RETURNING id
+        )
+        INSERT INTO ir_model_data (module, name, model, noupdate, res_id)
+           SELECT 'pos_restaurant', 'pos_takeout_preset', 'pos.preset', TRUE, id
+             FROM _preset
+        RETURNING res_id
+        """
+    )
+    takeout_preset_id = cr.fetchone()[0]
+
     query = """
         WITH _upd AS (
            UPDATE pos_config c
