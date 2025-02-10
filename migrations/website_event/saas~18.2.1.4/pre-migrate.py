57d7e678fd43e73a4c0f446053ce4fd8117865a6
yagp-odoo <yagp@odoo.com>
2025-02-10 11:53:16 +0530

    [IMP] website_event/saas~18.2: remove old event creation view
    This commit removes old form view of event creation from frontend,
    as we use a new form view for that.
    
    See - https://github.com/odoo/odoo/pull/186609
    
    Task-4256878
    
    closes odoo/upgrade#7193
    
    Related: odoo/odoo#186609
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/website_event/saas~18.2.1.4/pre-migrate.py
+++ migrations/website_event/saas~18.2.1.4/pre-migrate.py
@@ -1,6 +1,8 @@
-from odoo.upgrade.util import remove_record
+from odoo.upgrade.util import remove_record, remove_view
 
 
 def migrate(cr, version):
     remove_record(cr, "website_event.s_events_000_js")
     remove_record(cr, "website_event.s_searchbar_000_js")
+
+    remove_view(cr, "website_event.event_event_view_form_add")
