973e1e053395e7ca6ea660841bbf6d0eca818f16
yagp-odoo <yagp@odoo.com>
2025-02-10 18:23:23 +0530

    [IMP] sales_team/saas~17.3: log removed sales team
    `team_id` field has been removed on res.partner model.
    However, data is lost at upgrade time and this could break
    valid flows for some customer.
    
    The purpose of this commit is to add chatter message of
    removed `Sales Team` on res.partner.
    
    Ref PR: https://github.com/odoo/odoo/pull/151768
    
    Task-4558500
    
    closes odoo/upgrade#7197
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/sales_team/saas~17.3.1.1/pre-migrate.py
+++ migrations/sales_team/saas~17.3.1.1/pre-migrate.py
@@ -3,4 +3,21 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "sales_team.res_partner_view_team")
+
+    query = """
+        INSERT INTO mail_message (
+                    res_id, model, author_id, message_type, date,
+                    body
+             )
+             SELECT r.id, 'res.partner', %s, 'notification', NOW() at time zone 'UTC',
+                    'Former Sales Team: ' || (t.name->>'en_US') || ' (id=' || r.team_id || ')'
+               FROM res_partner r
+               JOIN crm_team t
+                 ON r.team_id = t.id
+    """
+
+    util.explode_execute(
+        cr, cr.mogrify(query, [util.ref(cr, "base.partner_root")]).decode(), table="res_partner", alias="r"
+    )
+
     util.remove_field(cr, "res.partner", "team_id")
