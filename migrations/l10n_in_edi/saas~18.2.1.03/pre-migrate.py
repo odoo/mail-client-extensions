261ff0c9b7c69c585012ca8425499934ce818e4d
Harsh Modi <hamo@odoo.com>
2025-02-11 12:45:52 +0530

    [IMP] l10n_in_edi/saas~18.2: remove account_edi dependency
    In this commit we remove account_edi dependency
    for E-invoice and merge the `l10n_in_edi` into
    `l10n_in`
    
    see odoo/odoo#191920
    see odoo/enterprise#76621
    
    closes odoo/upgrade#7098
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/l10n_in_edi/saas~18.2.1.03/pre-migrate.py
+++ migrations/l10n_in_edi/saas~18.2.1.03/pre-migrate.py
@@ -4,3 +4,37 @@ from odoo.upgrade import util
 def migrate(cr, version):
     script = util.import_script("l10n_in/saas~18.2.2.0/pre-migrate.py")
     script._l10n_in_enable_feature(cr, "l10n_in_edi_feature")
+    util.remove_field(cr, "account.move", "l10n_in_edi_show_cancel")
+    util.create_column(cr, "account_move", "l10n_in_edi_status", "varchar")
+    cr.execute(
+        """
+        UPDATE account_move AS move
+           SET l10n_in_edi_status = CASE
+                  WHEN aed.state = 'to_cancel' THEN 'sent'
+                  ELSE aed.state
+               END
+          FROM account_edi_document AS aed
+          JOIN account_edi_format AS aef
+            ON aed.edi_format_id = aef.id
+         WHERE move.id = aed.move_id
+           AND aef.code = 'in_einvoice_1_03'
+        """
+    )
+    cr.execute(
+        """
+        UPDATE ir_attachment a
+           SET res_field = 'l10n_in_edi_attachment_file'
+          FROM account_edi_document aed
+          JOIN account_edi_format aef
+            ON aed.edi_format_id = aef.id
+         WHERE a.id = aed.attachment_id
+           AND aef.code = 'in_einvoice_1_03'
+        """
+    )
+    cr.execute("""
+        DELETE FROM account_edi_document aed
+              USING account_edi_format aef
+              WHERE aef.code = 'in_einvoice_1_03'
+                AND aef.id = aed.edi_format_id
+    """)
+    util.delete_unused(cr, "l10n_in.edi_in_einvoice_json_1_03")
