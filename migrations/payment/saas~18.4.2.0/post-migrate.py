d04ad2d33800d4da3c13947b82c6290f32a3b352
Shrey Mehta <shrm@odoo.com>
2025-02-12 16:15:09 +0530

    [IMP] payment/saas~18.4: add trustly to mollie
    In the linked Community PR, added 'payment.payment_method_trustly' to the
    'payment_provider_mollie'
    
    Related PR: https://github.com/odoo/odoo/pull/197463
    
    task-4555177
    
    closes odoo/upgrade#7211
    
    Related: odoo/odoo#197463
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/payment/saas~18.4.2.0/post-migrate.py
+++ migrations/payment/saas~18.4.2.0/post-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.if_unchanged(cr, "payment.payment_provider_razorpay", util.update_record_from_xml)
+    util.if_unchanged(cr, "payment.payment_provider_mollie", util.update_record_from_xml)
