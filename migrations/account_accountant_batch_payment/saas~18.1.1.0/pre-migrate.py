9a6daea0fd16c1c1b4aa0793cd93fafb51cc3662
asno-odoo <asno@odoo.com>
2025-02-14 23:27:10 +0000

    [FIX] account_accountant_batch_payment/saas~18.1: remove field
    The field was live in 18 for just a few days. From 18 release at
    odoo/odoo@b6fcb6d0 on Sep 25, 2024 (commit that introduced the field
    was odoo/enterprise@8a27188f on Jul 12, 2024 --aka 17.5)
    until odoo/enterprise@2c0e90db on Oct 2, 2024.
    
    closes odoo/upgrade#7233
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/account_accountant_batch_payment/saas~18.1.1.0/pre-migrate.py
+++ migrations/account_accountant_batch_payment/saas~18.1.1.0/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_model(cr, "account.batch.payment.rejection")
+    util.remove_field(cr, "bank.rec.widget.line", "batch_pay_content")
