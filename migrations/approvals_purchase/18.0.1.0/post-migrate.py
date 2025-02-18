c67fdaacfc964970d7fb2917dd0076653c7c1b6c
Edoardo Piroli <pied@odoo.com>
2025-02-18 16:52:38 +0100

    [IMP] approvals_purchase/18.0: verify uoms before recomputing seller_id
    tbg-1678
    
    `seller_id` computation will trigger the computation of uom quantities which, in
    the presence of uom category inconsistencies, will cause issues like the
    following:
    
    ```
    […]
    File "/home/odoo/src/odoo/18.0/odoo/modules/migration.py", line 254, in migrate_module
        mod.migrate(self.cr, installed_version)
      File "/tmp/tmp01x7_33k/migrations/approvals_purchase/18.0.1.0/post-migrate.py", line 5, in migrate
        util.recompute_fields(cr, "approval.product.line", ["seller_id"])
      File "/tmp/tmp01x7_33k/migrations/util/orm.py", line 248, in wrapper
        return f(*args, **kwargs)
      File "/tmp/tmp01x7_33k/migrations/util/orm.py", line 304, in recompute_fields
        recompute(records)
      File "/tmp/tmp01x7_33k/migrations/util/orm.py", line 218, in recompute
        (
      File "/home/odoo/src/odoo/18.0/odoo/api.py", line 827, in _recompute_all
        self[field.model_name]._recompute_field(field)
      File "/home/odoo/src/odoo/18.0/odoo/models.py", line 7279, in _recompute_field
        field.recompute(records)
      File "/home/odoo/src/odoo/18.0/odoo/fields.py", line 1451, in recompute
        apply_except_missing(self.compute_value, recs)
      File "/home/odoo/src/odoo/18.0/odoo/fields.py", line 1424, in apply_except_missing
        func(records)
      File "/home/odoo/src/odoo/18.0/odoo/fields.py", line 1473, in compute_value
        records._compute_field_value(self)
      File "/home/odoo/src/odoo/18.0/odoo/models.py", line 5238, in _compute_field_value
        fields.determine(field.compute, self)
      File "/home/odoo/src/odoo/18.0/odoo/fields.py", line 110, in determine
        return needle(*args)
      File "/home/odoo/src/enterprise/18.0/approvals_purchase/models/approval_product_line.py", line 50, in _compute_seller_id
        if line.product_id and line.po_uom_qty:
      File "/home/odoo/src/odoo/18.0/odoo/fields.py", line 1291, in __get__
        self.compute_value(recs)
      File "/home/odoo/src/odoo/18.0/odoo/fields.py", line 1473, in compute_value
        records._compute_field_value(self)
      File "/home/odoo/src/odoo/18.0/odoo/models.py", line 5238, in _compute_field_value
        fields.determine(field.compute, self)
      File "/home/odoo/src/odoo/18.0/odoo/fields.py", line 110, in determine
        return needle(*args)
      File "/home/odoo/src/enterprise/18.0/approvals_purchase/models/approval_product_line.py", line 40, in _compute_po_uom_qty
        line.po_uom_qty = uom._compute_quantity(
      File "/home/odoo/src/odoo/18.0/addons/uom/models/uom_uom.py", line 225, in _compute_quantity
        raise UserError(_(
    odoo.exceptions.UserError: The unit of measure kg defined on the order line doesn't belong to the same category as the unit of measure Pcs defined on the product. Please correct the unit of measure defined on the order line or on the product. They should belong to the same category.
    ```
    
    We override the `_compute_quantity` method to unset the raise_if_failure flag,
    preventing the issue from popping up during the upgrade.
    
    closes odoo/upgrade#7252
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/approvals_purchase/18.0.1.0/post-migrate.py
+++ migrations/approvals_purchase/18.0.1.0/post-migrate.py
@@ -2,4 +2,5 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.recompute_fields(cr, "approval.product.line", ["seller_id"])
+    apl = util.env(cr)["approval.product.line"].with_context(upg_seller_id_recompute_flag=True)
+    util.recompute_fields(cr, apl, ["seller_id"])
--- migrations/approvals_purchase/18.0.1.0/pre-migrate.py
+++ migrations/approvals_purchase/18.0.1.0/pre-migrate.py
@@ -1,4 +1,17 @@
+from odoo import models
+
 from odoo.upgrade import util
+
+
+class Uom(models.Model):
+    _name = "uom.uom"
+    _inherit = "uom.uom"
+    _module = "approvals_purchase"
+
+    def _compute_quantity(self, *args, **kwargs):
+        if self.env.context.get("upg_seller_id_recompute_flag"):
+            kwargs = dict(kwargs, raise_if_failure=False)
+        return super()._compute_quantity(*args, **kwargs)
 
 
 def migrate(cr, version):
