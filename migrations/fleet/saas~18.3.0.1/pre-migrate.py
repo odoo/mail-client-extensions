582f39383f0220ee37a7573331d265095b13ea9a
ahta <ahta@odoo.com>
2025-02-18 14:06:58 +0100

    [IMP] fleet/saas~18.3: remove unused res.partner fields
    The fields `plan_to_change_bike` and `plan_to_change_car` in the
    `fleet/res_partner` model is removed.
    The fields `plan_to_change_bike` and `plan_to_change_car` in the
    `fleet/vehicle` model are no longer  related fields.
    As a result, they are no longer computed from the fields in the
    `fleet/res_partner` model, which previously acted as a shared variable
    for all vehicles of the same user. Now, these fields are associated with
    each vehicle separately, and each vehicle's fields are set individually
    instead of being defined in the `fleet/res_partner` model.
    
    See odoo/odoo#197812
    
    closes odoo/upgrade#7250
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/fleet/saas~18.3.0.1/pre-migrate.py
+++ migrations/fleet/saas~18.3.0.1/pre-migrate.py
@@ -3,3 +3,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "fleet.fleet_vehicle_odometer_view_kanban")
+    util.remove_field(cr, "res.partner", "plan_to_change_car")
+    util.remove_field(cr, "res.partner", "plan_to_change_bike")
