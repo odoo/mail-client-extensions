a90be17e36da3eb633c3ddf3351219b4238e678d
dhka-odoo <dhka@odoo.com>
2025-02-20 12:03:39 +0530

    [IMP] l10n_sa_hr_payroll/saas~18.5: remove structure from sa payroll
    Removed Expat Employee Payroll Structure from Saudi Arabia Localization
    removed "Is Eligible for KSA Paid Compensation?" field and
    "l10n_sa_hr_payroll.hr_leave_type_view_form" view from hr.leave.tpye model
    removed "l10n_sa_leaves_count_compensable" field from hr.Employee model
    
    related community PR: https://github.com/odoo/odoo/pull/200372
    related enterprise PR: https://github.com/odoo/enterprise/pull/80783
    
    task-4460705
    
    closes odoo/upgrade#7333
    
    Related: odoo/odoo#200372
    Related: odoo/enterprise#80783
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>
    Co-authored-by: rmun-odoo <rmun@odoo.com>

--- migrations/l10n_sa_hr_payroll/saas~18.5.1.0/pre-migrate.py
+++ migrations/l10n_sa_hr_payroll/saas~18.5.1.0/pre-migrate.py
@@ -1,5 +1,14 @@
 from odoo.upgrade import util
+from odoo.upgrade.util.hr_payroll import remove_salary_rule
 
 
 def migrate(cr, version):
     util.remove_view(cr, "l10n_sa_hr_payroll.hr_payslip_run_view_form")
+    util.remove_view(cr, "l10n_sa_hr_payroll.hr_leave_type_view_form")
+
+    util.update_record_from_xml(cr, "l10n_sa_hr_payroll.ksa_saudi_employee_payroll_structure")
+    util.remove_field(cr, "hr.employee", "l10n_sa_leaves_count_compensable")
+    util.remove_field(cr, "hr.leave.type", "l10n_sa_is_compensable")
+
+    remove_salary_rule(cr, "l10n_sa_hr_payroll.ksa_saudi_annual_leave_compensation_salary_rule")
+    util.delete_unused(cr, "l10n_sa_hr_payroll.ksa_expat_employee_payroll_structure")
