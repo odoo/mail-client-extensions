d0fed077e4b555d40cf83b38a8bceddcfa5f789f
asno-odoo <asno@odoo.com>
2025-02-20 12:27:15 +0000

    [FIX] l10n_it/saas~18.2: move to end
    The record `l10n_it.tax_report_vat` is being used here: odoo/odoo#193662
    and being removed here: odoo/odoo#odoo/odoo#194580 and #7112. The
    script removing this record loads before file using it. remove in
    end script to ensure that we don't have a missing records traceback.
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/saas-18.2/odoo/tools/cache.py", line 118, in lookup
        r = d[key]
      File "<decorator-gen-6>", line 2, in __getitem__
      File "/home/odoo/src/odoo/saas-18.2/odoo/tools/func.py", line 98, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/saas-18.2/odoo/tools/lru.py", line 33, in __getitem__
        a = self.d[obj]
    KeyError: ('ir.model.data', <function IrModelData._xmlid_lookup at 0x734e87f49b40>, 'l10n_it.tax_report_vat')
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/saas-18.2/odoo/service/server.py", line 1343, in preload_registries
        registry = Registry.new(dbname, update_module=update_module, install_modules=config['init'], upgrade_modules=config['update'])
      File "<decorator-gen-14>", line 2, in new
      File "/home/odoo/src/odoo/saas-18.2/odoo/tools/func.py", line 98, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/saas-18.2/odoo/orm/registry.py", line 165, in new
        load_modules(
      File "/home/odoo/src/odoo/saas-18.2/odoo/modules/loading.py", line 456, in load_modules
        load_module_graph(
      File "/home/odoo/src/odoo/saas-18.2/odoo/modules/loading.py", line 238, in load_module_graph
        migrations.migrate_module(package, 'post')
      File "/home/odoo/src/odoo/saas-18.2/odoo/modules/migration.py", line 261, in migrate_module
        mod.migrate(self.cr, installed_version)
      File "/home/odoo/src/odoo/saas-18.2/addons/l10n_it/migrations/0.7/post-map_carryover_to_new_report.py", line 6, in migrate
        vat_report_id = env.ref('l10n_it.tax_report_vat').id
      File "/home/odoo/src/odoo/saas-18.2/odoo/orm/environments.py", line 148, in ref
        res_model, res_id = self['ir.model.data']._xmlid_to_res_model_res_id(
      File "/home/odoo/src/odoo/saas-18.2/odoo/addons/base/models/ir_model.py", line 2175, in _xmlid_to_res_model_res_id
        return self._xmlid_lookup(xmlid)
      File "<decorator-gen-37>", line 2, in _xmlid_lookup
      File "/home/odoo/src/odoo/saas-18.2/odoo/tools/cache.py", line 125, in lookup
        value = d[key] = self.method(*args, **kwargs)
      File "/home/odoo/src/odoo/saas-18.2/odoo/addons/base/models/ir_model.py", line 2168, in _xmlid_lookup
        raise ValueError('External ID not found in the system: %s' % xmlid)
    ValueError: External ID not found in the system: l10n_it.tax_report_vat
    ```
    
    closes odoo/upgrade#7264
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/l10n_it/saas~18.2.0.7/end-migrate.py
+++ migrations/l10n_it/saas~18.2.0.7/end-migrate.py
@@ -2,4 +2,5 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    # l10n_it.tax_report_vat is used in a local upgrade script in l10n_it
     util.remove_record(cr, "l10n_it.tax_report_vat")
