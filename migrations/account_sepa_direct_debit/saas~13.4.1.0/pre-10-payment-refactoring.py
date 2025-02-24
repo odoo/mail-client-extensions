c0f281e51da7a4f68643665080e3071beed00288
Agustin Castro Bugallo <agcb@odoo.com>
2025-02-24 08:03:54 +0000

    [IMP] account_sepa_direct_debit/saas~13.4: Explode execute query
    Use `explode_execute` to reduce the time from >1h to ~5 minutes.
    
    ```
    2025-02-17 21:54:58,731 31 INFO db_2561707 odoo.modules.migration: module account_sepa_direct_debit: Running migration [>saas~13.4.1.0] pre-10-payment-refactoring
    2025-02-17 22:55:01,369 31 INFO db_2561707 odoo.modules.registry: module account_sepa_direct_debit: creating or updating database tables
    ```
    
    vs
    
    ```
    2025-02-22 01:28:53,900 27 INFO agcb_2561707_15.0 odoo.modules.migration: module account_sepa_direct_debit: Running migration [>saas~13.4.1.0] pre-10-payment-refactoring
    2025-02-22 01:35:44,918 27 INFO agcb_2561707_15.0 odoo.modules.registry: module account_sepa_direct_debit: creating or updating database tables
    ```
    
    upg-2561707
    
    closes odoo/upgrade#7275
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/account_sepa_direct_debit/saas~13.4.1.0/pre-10-payment-refactoring.py
+++ migrations/account_sepa_direct_debit/saas~13.4.1.0/pre-10-payment-refactoring.py
@@ -1,25 +1,29 @@
-# -*- coding: utf-8 -*-
 from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-
     # ===========================================================
     # Payment-pocalypse (PR: 41301 & 7019)
     # ===========================================================
 
     # Migrate columns/fields.
 
-    util.remove_column(cr, 'account_payment', 'sdd_mandate_id')
+    util.remove_column(cr, "account_payment", "sdd_mandate_id")
 
-    util.rename_field(cr, 'account.move', 'sdd_paying_mandate_id', 'sdd_mandate_id')
+    util.rename_field(cr, "account.move", "sdd_paying_mandate_id", "sdd_mandate_id")
 
     # Update existing account.move.
 
-    cr.execute('''
+    util.explode_execute(
+        cr,
+        """
         UPDATE account_move move
-        SET sdd_mandate_id = pay_backup.sdd_mandate_id
-        FROM account_payment_pre_backup pay_backup
-        JOIN account_payment pay ON pay.id = pay_backup.id
-        WHERE move.id = pay.move_id
-    ''')
+           SET sdd_mandate_id = pay_backup.sdd_mandate_id
+          FROM account_payment_pre_backup pay_backup
+          JOIN account_payment pay
+            ON pay.id = pay_backup.id
+         WHERE move.id = pay.move_id
+        """,
+        table="account_move",
+        alias="move",
+    )
