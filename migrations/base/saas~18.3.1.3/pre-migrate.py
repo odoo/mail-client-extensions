fd48400bd980b5767402ec9898d95050b0526871
Paolo Gatti <pgi@odoo.com>
2025-02-24 16:23:45 +0100

    [FIX] base/saas~18.3: fixing Indonesia province XMLID
    Fixing province Papua Pegunungan from base.state_id_pp to base.state_id_pe
    
    See odoo/odoo#199078
    
    closes odoo/upgrade#7282
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/saas~18.3.1.3/pre-migrate.py
+++ migrations/base/saas~18.3.1.3/pre-migrate.py
@@ -6,3 +6,4 @@ def migrate(cr, version):
     util.remove_field(cr, "ir.actions.actions", "binding_invisible")
     # explicit removal for databases without odoo/odoo#199109
     util.remove_field(cr, "ir.cron", "binding_invisible")
+    util.rename_xmlid(cr, "base.state_id_pp", "base.state_id_pe", on_collision="merge")
