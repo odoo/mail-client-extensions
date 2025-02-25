72d96e1fed630c3dfb64f93599c5dcf8e044b76e
Alvaro Fuentes <afu@odoo.com>
2025-02-25 14:34:55 +0100

    [FIX] base/0.0.0: there is a loop by default
    By default commercial partner could be equal to self.
    
    Do not run the query recursively to avoid other weird loops.
    
    Add to report only the first entries.
    
    closes odoo/upgrade#7287
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/0.0.0/pre-match-partner_company.py
+++ migrations/base/0.0.0/pre-match-partner_company.py
@@ -1,5 +1,4 @@
 from odoo.addons.base.maintenance.migrations import util
-from odoo.addons.base.maintenance.migrations.util.inconsistencies import break_recursive_loops
 
 
 def migrate(cr, version):
@@ -13,38 +12,21 @@ def migrate(cr, version):
     """
     util.explode_execute(cr, query, table="res_partner", alias="p")
 
-    break_recursive_loops(cr, "res.partner", "commercial_partner_id")
     query = """
-        WITH RECURSIVE info AS (
-            SELECT id,
-                   company_id
-              FROM res_partner
-             WHERE (commercial_partner_id IS NULL
-                OR commercial_partner_id = id)
-               AND {parallel_filter}
-
-             UNION ALL
-
-            SELECT child.id,
-                   COALESCE(parent.company_id, child.company_id)
-              FROM res_partner child
-              JOIN info parent
-                ON child.commercial_partner_id = parent.id
-               AND child.commercial_partner_id != child.id
-        )
         UPDATE res_partner p
-           SET company_id = info.company_id
-          FROM info
-          JOIN res_company comp
-            ON info.company_id = comp.id
-         WHERE info.id = p.id
-           AND info.company_id IS NOT NULL
-           AND p.company_id IS DISTINCT FROM info.company_id
-     RETURNING p.id, p.name, comp.id, comp.name
+           SET company_id = cp.company_id
+          FROM res_partner cp
+          JOIN res_company c
+            ON cp.company_id = c.id
+         WHERE cp.id = p.commercial_partner_id
+           AND p.id != cp.commercial_partner_id
+           AND p.company_id IS DISTINCT FROM cp.company_id
+     RETURNING p.id, p.name, c.id, c.name
     """
-    util.explode_execute(cr, query, table="res_partner")
+    cr.execute(query)
 
-    rows = cr.fetchall()
+    total = cr.rowcount
+    rows = cr.fetchmany(20)
     if not rows:
         return
     util.add_to_migration_reports(
@@ -53,13 +35,16 @@ def migrate(cr, version):
         message="""
         <details>
             <summary>
-                Some partners were assigned to different companies than their
+                A total of {} partners were assigned to different companies than their
                 commercial partner. To avoid issues during the upgrade the following
                 parters were updated to match the company of their commercial partner.
+                (Showing only the first {})
             </summary>
             <ul>{}</ul>
         </details>
         """.format(
+            total,
+            len(rows),
             "\n".join(
                 "<li> Partner {} now belongs to company {}</li>".format(
                     util.get_anchor_link_to_record("res.partner", p_id, p_name),
