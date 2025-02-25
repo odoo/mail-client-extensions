b7f40731716f6a10c6a52b8c9e76b6edaf6315c1
Christophe Simonis <chs@odoo.com>
2025-02-25 15:37:45 +0100

    [FIX] base/0.0.0: correct cleaning of transients
    Exploding the cleaning of transient can actually slow down the whole
    process. Executing the cleaning queries in parallel should already
    speedup the execution.
    
    Part-of: odoo/upgrade#7290
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/0.0.0/pre-clean-transients.py
+++ migrations/base/0.0.0/pre-clean-transients.py
@@ -60,15 +60,7 @@ def migrate(cr, version):
             cr, "DELETE FROM {} WHERE {} AND {} IS NOT NULL", ir.table, ir.model_filter(), ir.res_id
         )
         util._logger.info("Cleaning references to transient models from %s", ir.table)
-        for queries in util.chunks(
-            itertools.chain.from_iterable(
-                util.explode_query_range(cr, cr.mogrify(query, data).decode(), table=ir.table, bucket_size=50000)
-                for data in models
-            ),
-            100000,
-            fmt=list,
-        ):
-            util.parallel_execute(cr, queries)
+        util.parallel_execute(cr, [cr.mogrify(query, data).decode() for data in models])
 
 
 def get_tables_of_models(cr, models):
