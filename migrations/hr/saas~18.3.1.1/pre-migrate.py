a69819802e314054d16de27539b7d165cff73aff
dhka-odoo <dhka@odoo.com>
2025-02-26 12:48:17 +0530

    [IMP] hr/saas~18.3: remove stored field
    removed the complete_name field in hr.department model ,as it was previously
    stored that prevented translations from being updated.
    
    related PR: https://github.com/odoo/odoo/pull/199848
    
    task-4578409
    
    closes odoo/upgrade#7311
    
    Related: odoo/odoo#199848
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/hr/saas~18.3.1.1/pre-migrate.py
+++ migrations/hr/saas~18.3.1.1/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "res.config.settings", "module_hr_homeworking")
+    util.remove_field(cr, "hr.department", "complete_name")
