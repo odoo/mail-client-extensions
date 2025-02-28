05fd694060d7e564e738a854a7ef3f93eca008ac
Ricardo Gomes Rodrigues <rigr@odoo.com>
2025-02-28 12:41:18 +0100

    [IMP] purchase/saas~18.3.1.2: remove view record
    In https://github.com/odoo/odoo/pull/199768, we move
    the "# Vendor Bills" smart button from the `purchase`
    module to the `account` module. Therefore, we can
    safely remove the useless view record that added this
    smart button in the `purchase` module.
    
    task-4584035
    
    closes odoo/upgrade#7309
    
    Related: odoo/odoo#199768
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/purchase/saas~18.3.1.2/pre-migrate.py
+++ migrations/purchase/saas~18.3.1.2/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_record(cr, "purchase.action_purchase_batch_bills")
+    util.remove_record(cr, "purchase.res_partner_view_purchase_account_buttons")
