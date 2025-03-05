21542051f40dcba3df9d5501dfdc6f7100d2a34e
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-03-05 08:36:35 +0100

    [IMP] hr_payroll/saas~18.3: fill new related stored fields
    see odoo/enterprise#79392
    
    - as `date_from, employee_id` is newely added to `hr_payslip_worked_days`
    model. they should be filled
    
    closes odoo/upgrade#7323
    
    Task: 4578442
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/hr_payroll/saas~18.3.1.0/pre-migrate.py
+++ migrations/hr_payroll/saas~18.3.1.0/pre-migrate.py
@@ -3,3 +3,14 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.convert_field_to_translatable(cr, "hr.payroll.structure", "name")
+
+    util.create_column(cr, "hr_payslip_worked_days", "date_from", "date")
+    util.create_column(cr, "hr_payslip_worked_days", "employee_id", "int4")
+    query = """
+        UPDATE hr_payslip_worked_days hpwd
+           SET date_from = hp.date_from,
+               employee_id = hp.employee_id
+          FROM hr_payslip hp
+         WHERE hpwd.payslip_id = hp.id
+    """
+    util.explode_execute(cr, query, table="hr_payslip_worked_days", alias="hpwd")
