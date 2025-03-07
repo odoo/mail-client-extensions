9929d738f095841fcac7747003dd6594a8b53744
Edoardo Piroli <pied@odoo.com>
2025-03-07 10:08:17 +0000

    [FIX] spreadsheet_edition/saas~17.3: fully parallelize revisions update
    tbg-1841
    
    Spreadsheet revisions' commands can vary dramatically in length:
    
    ```SQL
    pied@(none):pied_2596500> SELECT MAX(LENGTH(commands)), AVG(LENGTH(commands)), MIN(LENGTH(commands)) FROM spreadsheet_r
     evision WHERE commands LIKE '%\_PIVOT%' OR commands LIKE '%UPDATE\_CELL%'
    +----------+---------------------+-----+
    | max      | avg                 | min |
    |----------+---------------------+-----|
    | 58531147 | 125299.210037281331 | 97  |
    +----------+---------------------+-----+
    ```
    
    Which implies that a reasonable BATCH_SIZE (e.g. 1000) for most dbs is way too
    much for others, running into memory errors:
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/18.0/odoo/service/server.py", line 1318, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "<decorator-gen-13>", line 2, in new
      File "/home/odoo/src/odoo/18.0/odoo/tools/func.py", line 97, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/18.0/odoo/modules/registry.py", line 127, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/18.0/odoo/modules/loading.py", line 476, in load_modules
        processed_modules += load_marked_modules(env, graph,
      File "/home/odoo/src/odoo/18.0/odoo/modules/loading.py", line 364, in load_marked_modules
        loaded, processed = load_module_graph(
      File "/home/odoo/src/odoo/18.0/odoo/modules/loading.py", line 181, in load_module_graph
        migrations.migrate_module(package, 'pre')
      File "/home/odoo/src/odoo/18.0/odoo/modules/migration.py", line 254, in migrate_module
        mod.migrate(self.cr, installed_version)
      File "/tmp/tmptvdod22z/migrations/spreadsheet_edition/saas~17.3.1.0/pre-migrate.py", line 53, in migrate
        while chunk := ncr.fetchmany(MANY):
    MemoryError
    ```
    
    In this specific db it was found that `BATCH_SIZE` = 50 would still run into
    memory errors, while `BATCH_SIZE` = 40 would not. And even such a small value
    would not be safe against even bigger revisions.
    
    Therefore the logic was revised to avoid passing commands back and forth to and
    from the workers and letting them handle one command at a time. This fully
    parallelized version is simpler and ~3x faster, wrt the previous version, with
    batch size adjusted to 40.
    
    closes odoo/upgrade#7348
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/spreadsheet_edition/saas~17.3.1.0/pre-migrate.py
+++ migrations/spreadsheet_edition/saas~17.3.1.0/pre-migrate.py
@@ -1,15 +1,13 @@
+import collections
 import re
 import sys
 import uuid
 from concurrent.futures import ProcessPoolExecutor
 
-from psycopg2.extras import execute_batch
+from odoo.sql_db import db_connect
 
 from odoo.upgrade import util
 from odoo.upgrade.util import json
-
-BATCH_SIZE = 1000
-MANY = BATCH_SIZE * util.get_max_workers()
 
 
 def parse_dimension(dimension):
@@ -19,45 +17,54 @@ def parse_dimension(dimension):
     return {"name": values[0]}
 
 
-def update_data(query_row):
-    revision_id, data = query_row
-    data_loaded = json.loads(data)
+def update_command(id):
+    cr = update_command.cr
+    cr.execute("SELECT commands FROM spreadsheet_revision WHERE id = %s", [id])
+    data_loaded = json.loads(cr.fetchone()[0])
+    changed = False
     for command in data_loaded.get("commands", []):
         if command["type"] == "UPDATE_CELL" and command.get("content"):
             command["content"] = re.sub(r"\bODOO\.PIVOT\(", "PIVOT.VALUE(", command["content"], flags=re.I)
             command["content"] = re.sub(r"\bODOO\.PIVOT\.TABLE\b", "PIVOT", command["content"], flags=re.I)
             command["content"] = re.sub(r"\bODOO\.PIVOT\.HEADER\b", "PIVOT.HEADER", command["content"], flags=re.I)
+            changed = True
         elif command["type"] in ["ADD_PIVOT", "UPDATE_PIVOT"]:
             command["pivot"]["measures"] = [{"name": measure} for measure in command["pivot"]["measures"]]
             command["pivot"]["columns"] = [parse_dimension(col) for col in command["pivot"]["colGroupBys"]]
             command["pivot"]["rows"] = [parse_dimension(row) for row in command["pivot"]["rowGroupBys"]]
             del command["pivot"]["colGroupBys"]
             del command["pivot"]["rowGroupBys"]
+            changed = True
+    if changed:
+        cr.execute("UPDATE spreadsheet_revision SET commands = %s WHERE id = %s", [json.dumps(data_loaded), id])
 
-    return json.dumps(data_loaded), revision_id
+
+def init_worker(dbname):
+    conn = db_connect(dbname)
+    update_command.cr = conn.cursor()
+    update_command.cr._cnx.autocommit = True
 
 
 def migrate(cr, version):
     name = f"_upgrade_{uuid.uuid4().hex}"
     mod = sys.modules[name] = util.import_script(__file__, name=name)
-
-    query = r"""
-    SELECT id, commands
-      FROM spreadsheet_revision
-     WHERE commands LIKE '%UPDATE\_PIVOT%'
-        OR commands LIKE '%ADD\_PIVOT%'
-        OR (
-                commands LIKE '%UPDATE\_CELL%'
-            AND commands LIKE '%ODOO.PIVOT%'
-           )
-    """
-    with ProcessPoolExecutor(max_workers=util.get_max_workers()) as executor, util.named_cursor(cr, BATCH_SIZE) as ncr:
-        ncr.execute(query)
-
-        while chunk := ncr.fetchmany(MANY):
-            execute_batch(
-                cr._obj,
-                "UPDATE spreadsheet_revision SET commands=%s WHERE id=%s",
-                executor.map(mod.update_data, chunk, chunksize=BATCH_SIZE),
-                page_size=BATCH_SIZE,
-            )
+    cr.commit()
+    cr.execute(
+        r"""
+        SELECT id
+          FROM spreadsheet_revision
+         WHERE commands LIKE '%UPDATE\_PIVOT%'
+            OR commands LIKE '%ADD\_PIVOT%'
+            OR (
+                    commands LIKE '%UPDATE\_CELL%'
+                AND commands LIKE '%ODOO.PIVOT%'
+               )
+        """
+    )
+    with ProcessPoolExecutor(
+        max_workers=util.get_max_workers(), initializer=mod.init_worker, initargs=[cr.dbname]
+    ) as executor:
+        while ids := cr.fetchmany(50000):
+            # consume results to propagate workers' exceptions
+            collections.deque(executor.map(mod.update_command, (id for (id,) in ids)), maxlen=0)
+    cr.commit()
