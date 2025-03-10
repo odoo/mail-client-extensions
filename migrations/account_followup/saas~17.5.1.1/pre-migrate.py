aeda9998f41266d260d27955bdf39ab7cc959d3b
Jinane Maksoud <maji@odoo.com>
2025-03-10 18:02:03 +0100

    [FIX] account_followup/18.0: reset views
    The partner tree and search views changed
    from primary to extenions and got entirely
    rewritten. They should be recreated.
    
    closes odoo/upgrade#7358
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/account_followup/saas~17.5.1.1/pre-migrate.py
+++ migrations/account_followup/saas~17.5.1.1/pre-migrate.py
@@ -6,6 +6,8 @@ def migrate(cr, version):
     util.remove_field(cr, "account.move.line", "next_action_date")
 
     util.remove_view(cr, "account_followup.customer_statements_form_view")
+    util.remove_view(cr, "account_followup.customer_statements_tree_view")
+    util.remove_view(cr, "account_followup.customer_statements_search_view")
     util.remove_view(cr, "account_followup.account_followup_journal_dashboard_kanban_view")
     util.remove_record(cr, "account_followup.action_view_list_customer_statements")
     util.remove_menus(cr, [util.ref(cr, "account_followup.customer_statements_menu")])
