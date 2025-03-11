730b1842c422cd87900293e209b1c065b5bc8da4
Demesmaeker <edm@odoo.com>
2025-03-11 17:12:25 +0100

    [FIX] loyalty,sale/saas~18.3: remove unused fields
    See odoo/odoo#201208
    
    closes odoo/upgrade#7368
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/sale/saas~18.3.1.2/pre-migrate.py
+++ migrations/sale/saas~18.3.1.2/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.move_field_to_module(cr, "res.config.settings", "module_sale_gelato", "sale_gelato", "sale")
+    util.remove_field(cr, "sale.order.discount", "tax_ids")
