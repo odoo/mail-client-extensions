f7ead387e047395c578af96c2619ee48b54f82cb
Agustin Castro Bugallo <agcb@odoo.com>
2025-03-13 13:19:28 +0100

    [FIX] uom/saas~18.1: Keep changed factor for day and hour
    This script reverses the roles of day and hour. However, the factor for the day
    was hardcoded to 8 hours, squashing any change the client could have.
    
    upg-2587027
    opw-4630585
    
    closes odoo/upgrade#7379
    
    Signed-off-by: Jinane Maksoud (maji) <maji@odoo.com>

--- migrations/uom/saas~18.1.1.0/end-migrate.py
+++ migrations/uom/saas~18.1.1.0/end-migrate.py
@@ -6,12 +6,15 @@ def migrate(cr, version):
     break_recursive_loops(cr, "uom.uom", "relative_uom_id")
     env = util.env(cr)
     uom_hour = env.ref("uom.product_uom_hour", raise_if_not_found=False)
+    uom_day = env.ref("uom.product_uom_day", raise_if_not_found=False)
+    day_hour_factor = 8.0
     # add context to skip the constraint in stock module
     if uom_hour:
+        if uom_day and uom_day == uom_hour.relative_uom_id:
+            day_hour_factor = round(1 / uom_hour.relative_factor, 1)
         if uom_hour.relative_factor != 1.0 or uom_hour.relative_uom_id:
             uom_hour.with_context(_upg_swap_time_uom_ref=True).write({"relative_uom_id": False, "relative_factor": 1.0})
-        uom_day = env.ref("uom.product_uom_day", raise_if_not_found=False)
-        if uom_day and uom_day.relative_factor != 8.0:
+        if uom_day and not uom_day.relative_uom_id:
             uom_day.with_context(_upg_swap_time_uom_ref=True).write(
-                {"relative_uom_id": uom_hour, "relative_factor": 8.0}
+                {"relative_uom_id": uom_hour, "relative_factor": day_hour_factor}
             )
