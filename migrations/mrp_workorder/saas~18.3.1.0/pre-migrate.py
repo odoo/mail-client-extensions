f2b94d870f5733a72b355be091694f26e31af1e9
Pieter Claeys (clpi) <clpi@odoo.com>
2025-03-18 16:30:14 +0100

    [IMP] mrp_workorder/saas~18.3: remove quality check stock move view
    This view is no longer used as we will show the stock move( line)s
    directly on the cards in the shop floor.
    
    COM PR: odoo/odoo#200345
    ENT PR: odoo/enterprise#80469
    
    task-4484941
    
    closes odoo/upgrade#7331
    
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>

--- migrations/mrp_workorder/saas~18.3.1.0/pre-migrate.py
+++ migrations/mrp_workorder/saas~18.3.1.0/pre-migrate.py
@@ -10,3 +10,4 @@ def migrate(cr, version):
     util.remove_field(cr, "quality.check", "worksheet_page")
     util.remove_field(cr, "quality.check", "source_document")
     util.remove_field(cr, "stock.move", "worksheet_note")
+    util.remove_view(cr, "mrp_workorder.view_stock_move_operations_quality_check")
