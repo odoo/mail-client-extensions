7d9fb7b0349381ae4375f9461a5251906bdb3855
Thibault Delavallée <tde@odoo.com>
2025-03-21 08:25:02 +0100

    [FIX] various/saas~18.3: update calendar event wizard
    See odoo/odoo#202824 and odoo/enterprise#81895
    
    Task-4655022
    
    closes odoo/upgrade#7418
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/calendar/saas~18.3.1.1/pre-migrate.py
+++ migrations/calendar/saas~18.3.1.1/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     if util.module_installed(cr, "hr_calendar"):
         util.move_field_to_module(cr, "calendar.event", "unavailable_partner_ids", "hr_calendar", "calendar")
+    util.rename_field(cr, "calendar.popover.delete.wizard", "record", "calendar_event_id")
