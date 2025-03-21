960155f09c4791d0bb16a4f12397578f8b27477c
Jinane Maksoud <maji@odoo.com>
2025-03-21 11:02:17 +0100

    [FIX] base/saas~18.1: merge module correctly
    The module l10n_es_modelo130 is part of the community
    modules and should be merged regardless of the user
    having enterprise.
    
    closes odoo/upgrade#7420
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/base/saas~18.1.1.3/pre-10-modules.py
+++ migrations/base/saas~18.1.1.3/pre-10-modules.py
@@ -6,6 +6,7 @@ def migrate(cr, version):
     util.merge_module(cr, "l10n_in_reports_gstr_spreadsheet", "l10n_in_reports_gstr")
     util.merge_module(cr, "l10n_in_withholding_payment", "l10n_in_withholding")
     util.merge_module(cr, "auth_totp_mail_enforce", "auth_totp_mail")
+    util.merge_module(cr, "l10n_es_modelo130", "l10n_es")
     util.rename_module(cr, "l10n_in_edi_ewaybill", "l10n_in_ewaybill")
     if util.has_enterprise():
         util.merge_module(cr, "l10n_nl_reports_sbr", "l10n_nl_reports")
@@ -14,7 +15,6 @@ def migrate(cr, version):
         util.merge_module(cr, "l10n_nl_reports_sbr_icp", "l10n_nl_reports")
         util.rename_module(cr, "pos_urban_piper_swiggy", "l10n_in_pos_urban_piper")
         util.merge_module(cr, "pos_urban_piper_zomato", "l10n_in_pos_urban_piper")
-        util.merge_module(cr, "l10n_es_modelo130", "l10n_es")
         util.merge_module(cr, "l10n_es_reports_modelo130", "l10n_es_reports")
         util.merge_module(cr, "l10n_ee_rounding", "l10n_ee_reports")
         if not util.version_gte("saas~18.2"):  # module reintroduced in 18.2
