057e029b957a81d05c59284a3259be239745ec07
Christophe Simonis <chs@odoo.com>
2025-03-21 11:50:35 +0100

    [FIX] mrp/saas~17.3
    Don't use f-string to format query.
    
    closes odoo/upgrade#7421
    
    Related: odoo/upgrade-util#237
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/mrp/saas~17.3.2.0/end-migrate.py
+++ migrations/mrp/saas~17.3.2.0/end-migrate.py
@@ -2,23 +2,32 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    attached_on_sale, attached_on_sale_value = "", ""
-    prod_columns = set(util.get_columns(cr, "product_document"))
-    if "attached_on_sale" in prod_columns:
-        attached_on_sale = ", attached_on_sale"
-        attached_on_sale_value = ", 'hidden'"
-    common_column_names = [col for col in util.get_columns(cr, "mrp_document") if col in prod_columns]
-    common_column_joined = ", ".join(common_column_names)
+    if util.column_exists(cr, "product_document", "attached_on_sale"):
+        attached_on_sale = util.ColumnList.from_unquoted(cr, ["attached_on_sale"]).using(leading_comma=True)
+        attached_on_sale_value = util.SQLStr(", 'hidden'")
+    else:
+        attached_on_sale = attached_on_sale_value = util.SQLStr("")
+
+    columns = util.get_common_columns(
+        cr, "product_document", "mrp_document", ignore=["id", "attached_on_sale", "attached_on_mrp"]
+    )
+
     util.create_column(cr, "product_document", "_upg_mrp_id", "int4")
-    cr.execute(
-        f"""
+
+    query = util.format_query(
+        cr,
+        """
         INSERT INTO product_document (_upg_mrp_id, {common_column_joined}, attached_on_mrp {attached_on_sale})
              SELECT id, {common_column_joined}, 'bom' {attached_on_sale_value}
                FROM mrp_document
           RETURNING _upg_mrp_id,
                     product_document.id
-        """
+        """,
+        common_column_joined=columns,
+        attached_on_sale=attached_on_sale,
+        attached_on_sale_value=attached_on_sale_value,
     )
+    cr.execute(query)
     ids_mapped = dict(cr.fetchall())
     util.remove_column(cr, "product_document", "_upg_mrp_id")
     if ids_mapped:
