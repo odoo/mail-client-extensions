c77d2520d02d05331a57d5bf8fdff8bf9fed3b27
Akash Pandey <apan@odoo.com>
2025-03-25 12:48:58 +0530

    [FIX] account_reports/saas~17.4: replace str groupby with dict
    Commit: odoo/enterprise@a099a4db85c9eba0cf8bca59b4ebda951c252030
    
    This commit changes the groupby part from a str to a dict in line ids.
    Thus, we replace the old str-based groupby with a dict for records created
    before this commit.
    
    before fix:
    
    ```
    line_id            | ~account.report~9|~account.report.line~67|groupby:partner_id~res.partner~14|groupby:id~account.move.line~40
    ```
    
    After fix:
    
    ```
    line_id            | ~account.report~9|~account.report.line~67|{"groupby": "partner_id"}~res.partner~14|{"groupby": "id"}~account.move.line~40
    ```
    
    Steps to reproduce:-
    
    - Create a v17 database and install account_reports.
    - Open any report and add footnotes to report lines.
    (Note: Footnotes have been renamed to "annotates.")
    - Migrate the database to v18. The footnotes/annotates
      will no longer be visible.
    
    opw-4637101
    upg-2630564
    
    closes odoo/upgrade#7448
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account_reports/saas~17.4.1.0/pre-migrate.py
+++ migrations/account_reports/saas~17.4.1.0/pre-migrate.py
@@ -8,3 +8,19 @@ def migrate(cr, version):
     util.rename_xmlid(cr, *eb("account_reports.access_account_report_{footnote,annotation}_readonly"))
     util.rename_xmlid(cr, *eb("account_reports.access_account_report_{footnote,annotation}"))
     util.rename_xmlid(cr, *eb("account_reports.access_account_report_{footnote,annotation}_invoice"))
+
+    # see https://github.com/odoo/enterprise/pull/65087
+    util.explode_execute(
+        cr,
+        r"""
+        UPDATE account_report_annotation
+           SET line_id = regexp_replace(
+               line_id,
+               'groupby:([a-zA-Z0-9_]+)',
+               '{{"groupby": "\1"}}',
+               'g'
+               )
+         WHERE line_id LIKE '%groupby:%'
+        """,
+        table="account_report_annotation",
+    )
