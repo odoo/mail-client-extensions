67433fca0b18b54a2cdc36ca002ea4255c3e8ab9
rajh-odoo <rajh@odoo.com>
2025-03-25 12:33:58 +0530

    [IMP] hr_timesheet/saas~18.4: update security rules
    related https://github.com/odoo/odoo/pull/201996
    some domain is added in ir.rules which is noupdate=True so needs to updated.
    
    task-4586667
    
    closes odoo/upgrade#7438
    
    Related: odoo/odoo#201996
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/hr_timesheet/saas~18.4.1.0/post-migrate.py
@@ -0,0 +1,6 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.update_record_from_xml(cr, "hr_timesheet.timesheet_line_rule_user")
+    util.update_record_from_xml(cr, "hr_timesheet.timesheet_line_rule_approver")
