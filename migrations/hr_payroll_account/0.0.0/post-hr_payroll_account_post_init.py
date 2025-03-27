70c38b3a1fce8e31320b3e43c66df986b1964bc4
asno-odoo <asno@odoo.com>
2025-03-27 15:25:00 +0000

    [FIX] hr_payroll_account/0.0.0: try to import
    function added in 17.0 here:
    https://github.com/odoo/enterprise/pull/39256
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/15.0/odoo/service/server.py", line 1260, in preload_registries
        registry = Registry.new(dbname, update_module=update_module)
      File "/home/odoo/src/odoo/15.0/odoo/modules/registry.py", line 88, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/15.0/odoo/modules/loading.py", line 470, in load_modules
        processed_modules += load_marked_modules(cr, graph,
      File "/home/odoo/src/odoo/15.0/odoo/modules/loading.py", line 363, in load_marked_modules
        loaded, processed = load_module_graph(
      File "/home/odoo/src/odoo/15.0/odoo/modules/loading.py", line 227, in load_module_graph
        migrations.migrate_module(package, 'post')
      File "/home/odoo/src/odoo/15.0/odoo/modules/migration.py", line 177, in migrate_module
        mod = load_script(pyfile, name)
      File "/home/odoo/src/odoo/15.0/odoo/modules/migration.py", line 25, in load_script
        spec.loader.exec_module(module)
      File "<frozen importlib._bootstrap_external>", line 848, in exec_module
      File "<frozen importlib._bootstrap>", line 219, in _call_with_frames_removed
      File "/tmp/tmpr6vtrjhr/migrations/hr_payroll_account/0.0.0/post-hr_payroll_account_post_init.py", line 1, in <module>
        from odoo.addons.hr_payroll_account import _hr_payroll_account_post_init
    ImportError: cannot import name '_hr_payroll_account_post_init' from 'odoo.addons.hr_payroll_account' (/home/odoo/src/enterprise/15.0/hr_payroll_account/__init__.py)
    ```
    
    closes odoo/upgrade#7459
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/hr_payroll_account/0.0.0/post-hr_payroll_account_post_init.py
+++ migrations/hr_payroll_account/0.0.0/post-hr_payroll_account_post_init.py
@@ -1,4 +1,7 @@
-from odoo.addons.hr_payroll_account import _hr_payroll_account_post_init
+try:
+    from odoo.addons.hr_payroll_account import _hr_payroll_account_post_init
+except ImportError:
+    pass
 
 from odoo.addons.base.maintenance.migrations import util
 
