acbe7fe48b9906631ba351bd9a4f76fdde58acf5
Max Whale <mawh@odoo.com>
2025-03-27 16:11:59 +0100

    [ADD] event/saas~18.4: remove badge report view
    The ESC/label badge template was replaced with
    a new template with a custom report. The upgrade
    script removes the old template.
    
    See odoo/odoo#203274
    
    closes odoo/upgrade#7458
    
    Related: odoo/enterprise#83021
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/event/saas~18.4.1.9/pre-migrate.py
@@ -0,0 +1,13 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_view(cr, "event.event_report_template_esc_label_96x82_badge")
+
+    cr.execute("SELECT 4 FROM event_event WHERE badge_format = '96x82' LIMIT 1")
+    if cr.rowcount:
+        util.force_module_install(cr, "event_iot")
+        if util.module_installed(cr, "event_sale"):
+            util.force_module_install(cr, "event_sale_iot")
+        if util.module_installed(cr, "pos_event"):
+            util.force_module_install(cr, "pos_event_iot")
