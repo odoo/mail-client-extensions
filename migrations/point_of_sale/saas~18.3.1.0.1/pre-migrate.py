dd24197aa98b4df44e69c61e83772684816ff4a1
Rémy Voet (ryv) <ryv@odoo.com>
2025-04-01 08:40:27 +0200

    [IMP] point_of_sale/saas~18.3: store currency_id
    Store pos_config.currency_id field
    and update its content in SQL to avoid
    recomputing with the ORM.
    
    closes odoo/upgrade#7471
    
    Related: odoo/odoo#203901
    Related: odoo/enterprise#82415
    Signed-off-by: Rémy Voet (ryv) <ryv@odoo.com>

--- migrations/point_of_sale/saas~18.3.1.0.1/pre-migrate.py
+++ migrations/point_of_sale/saas~18.3.1.0.1/pre-migrate.py
@@ -3,3 +3,25 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.rename_field(cr, "res.config.settings", "module_pos_viva_wallet", "module_pos_viva_com")
+
+    util.create_column(cr, "pos_config", "currency_id", "integer")
+    util.explode_execute(
+        cr,
+        """
+         UPDATE pos_config AS c_up
+           SET currency_id = CASE
+                                 WHEN j.id IS NOT NULL THEN COALESCE(j.currency_id, com_j.currency_id)
+                                 ELSE com_c.currency_id
+                             END
+          FROM pos_config AS c
+          JOIN res_company AS com_c
+            ON (c.company_id = com_c.id)
+     LEFT JOIN account_journal AS j
+            ON (c.journal_id = j.id)
+     LEFT JOIN res_company AS com_j
+            ON (j.company_id = com_j.id)
+         WHERE c_up.id = c.id
+        """,
+        table="pos_config",
+        alias="c_up",
+    )
