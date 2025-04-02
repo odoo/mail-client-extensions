79ae346246d5d5416eb34ab5f9e8bc6f5b7f4b16
Max Whale <mawh@odoo.com>
2025-04-02 11:40:22 +0200

    [ADD] iot/saas~18.3: remove connected field from iot.device
    The `iot.device` model recently added a new field `connected_status`,
    resulting in 2 fields with very similar usages. The old `connected`
    field has now been removed and all its uses replaced with
    `connected_status`.
    
    See odoo/enterprise#82720
    
    task-4652455
    
    closes odoo/upgrade#7484
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/iot/saas~18.3.1.0/pre-migrate.py
+++ migrations/iot/saas~18.3.1.0/pre-migrate.py
@@ -3,3 +3,15 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "iot.box", "is_websocket_active")
+
+    util.create_column(cr, "iot_device", "connected_status", "varchar")
+    cr.execute(
+        """
+        UPDATE iot_device
+           SET connected_status = CASE
+                                      WHEN connected THEN 'connected'
+                                      ELSE 'disconnected'
+                                   END
+        """
+    )
+    util.remove_field(cr, "iot.device", "connected")
