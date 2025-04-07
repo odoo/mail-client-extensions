188d13c3233a4942455baea1c094350cb9144209
dhka-odoo <dhka@odoo.com>
2025-04-07 17:28:46 +0530

    [IMP] hr_referral/saas~18.3: set referral source id
    Replace the user source id with the referral source id,
    as a new referral source will now be used in the source field of hr_recruitment
    instead of the user sources
    
    related community PR - https://github.com/odoo/odoo/pull/202438
    related enterprise PR - https://github.com/odoo/enterprise/pull/76180
    
    task-4202786
    
    closes odoo/upgrade#7505
    
    Related: odoo/odoo#202438
    Related: odoo/enterprise#76180
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/hr_referral/saas~18.3.1.0/post-migrate.py
+++ migrations/hr_referral/saas~18.3.1.0/post-migrate.py
@@ -3,3 +3,9 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.update_record_from_xml(cr, "hr_recruitment.group_hr_recruitment_user", from_module="hr_referral")
+
+    utm_source_referral = util.ref(cr, "utm.utm_source_referral")
+    cr.execute(
+        "UPDATE hr_applicant SET source_id = %s WHERE ref_user_id IS NOT NULL",
+        [utm_source_referral],
+    )
