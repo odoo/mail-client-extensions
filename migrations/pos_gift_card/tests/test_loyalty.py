30a595c58ae9a431440fbbac8a3326b088bf76ef
Christophe Simonis <chs@odoo.com>
2025-04-08 17:59:43 +0200

    [FIX] pos_gift_card/tests
    Use UI API to create the POS order and close the POS session.
    
    Closing the session is needed to avoid the new tests added by
    odoo/upgrade-util@52a77321e80acb19314840d6939b3bfbc725e38b
    to fail to remove the partners because there is an open POS session[^1]
    
    [^1]: See https://github.com/odoo/odoo/blob/94a483a2ba0d978ddc5a01e115217ba8e52dd38f/addons/point_of_sale/models/res_partner.py#L60-L67
    
    closes odoo/upgrade#7516
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/pos_gift_card/tests/test_loyalty.py
+++ migrations/pos_gift_card/tests/test_loyalty.py
@@ -1,32 +1,28 @@
-# -*- coding: utf-8 -*-
+from odoo.fields import Datetime
 
-from odoo.addons.base.maintenance.migrations.testing import UpgradeCase, change_version
+from odoo.addons.base.maintenance.migrations.account.tests.test_common import TestAccountingSetupCommon
+from odoo.addons.base.maintenance.migrations.testing import change_version
 
 
 @change_version("saas~15.3")
-class TestLoyalty(UpgradeCase):
+class TestLoyalty(TestAccountingSetupCommon):
     def prepare(self):
         # Create a gift card and pay an order with it
-        company = self.env["res.company"].create(
-            {
-                "name": "UPGRADE_company",
-            }
+        super().prepare()
+        journal = self.env["account.journal"].create(
+            {"name": "UPG", "code": "UPG", "type": "general", "company_id": self.company.id}
         )
         gift_card = self.env["gift.card"].create(
             {
                 "initial_amount": 50,
-                "company_id": company.id,
+                "company_id": self.company.id,
             }
         )
         product = self.env["product.product"].create(
             {
                 "name": "UPGRADE_test_product",
                 "lst_price": 30,
-            }
-        )
-        partner = self.env["res.partner"].create(
-            {
-                "name": "UPGRADE MAN",
+                "property_account_income_id": self.account_income.id,
             }
         )
         sequence = self.env["ir.sequence"].create(
@@ -37,19 +33,20 @@ class TestLoyalty(UpgradeCase):
         )
         config = (
             self.env["pos.config"]
-            .with_company(company)
+            .with_company(self.company)
             .create(
                 {
                     "name": "UPGRADE pos config",
-                    "company_id": company.id,
+                    "company_id": self.company.id,
                     "sequence_id": sequence.id,
+                    "journal_id": journal.id,
                     "payment_method_ids": [
                         (
                             0,
                             0,
                             {
                                 "name": "UPGRADE method",
-                                "company_id": company.id,
+                                "company_id": self.company.id,
                                 "split_transactions": True,
                             },
                         )
@@ -60,43 +57,52 @@ class TestLoyalty(UpgradeCase):
         config.open_session_cb()
         session = config.current_session_id
         pay_with_gift_card = self.env.ref("gift_card.pay_with_gift_card_product")
-        so = self.env["pos.order"].create(
-            {
-                "name": "UPGRADE sol",
-                "partner_id": partner.id,
-                "session_id": session.id,
-                "amount_tax": 4.5,
-                "amount_total": 0,
-                "amount_paid": 0,
-                "amount_return": 0,
-                "lines": [
-                    (
-                        0,
-                        0,
-                        {
-                            "product_id": product.id,
-                            "qty": 1,
-                            "price_unit": 30,
-                            "price_subtotal": 30,
-                            "price_subtotal_incl": 34.5,
-                        },
-                    ),
-                    (
-                        0,
-                        0,
-                        {
-                            "product_id": pay_with_gift_card.id,
-                            "gift_card_id": gift_card.id,
-                            "qty": 1,
-                            "price_unit": -34.5,
-                            "price_subtotal": -34.5,
-                            "price_subtotal_incl": -34.5,
-                        },
-                    ),
-                ],
-            }
-        )
-        so.state = "paid"
+        ui_data = {
+            "name": "UPGRADE sol",
+            "user_id": self.env.user.id,
+            "pos_session_id": session.id,
+            "partner_id": self.partner.id,
+            "sequence_number": 1,
+            "pricelist_id": self.partner.property_product_pricelist.id,
+            "fiscal_position_id": session.config_id.default_fiscal_position_id.id,
+            "creation_date": str(Datetime.now()),
+            "amount_tax": 4.5,
+            "amount_total": 0,
+            "amount_paid": 0,
+            "amount_return": 0,
+            "lines": [
+                (
+                    0,
+                    0,
+                    {
+                        "product_id": product.id,
+                        "qty": 1,
+                        "price_unit": 30,
+                        "price_subtotal": 30,
+                        "price_subtotal_incl": 34.5,
+                    },
+                ),
+                (
+                    0,
+                    0,
+                    {
+                        "product_id": pay_with_gift_card.id,
+                        "gift_card_id": gift_card.id,
+                        "qty": 1,
+                        "price_unit": -34.5,
+                        "price_subtotal": -34.5,
+                        "price_subtotal_incl": -34.5,
+                    },
+                ),
+            ],
+            "statement_ids": [],
+        }
+
+        soid = self.env["pos.order"].create_from_ui([{"data": ui_data}])[0]["id"]
+        so = self.env["pos.order"].browse(soid)
+        so.action_pos_order_paid()
+        r = session.close_session_from_ui()
+        self.assertTrue(r["successful"], r.get("message"))
         return (so.id,)
 
     def check(self, init):
