6369fdb918a9d1d29cd46c9621fc1eee67517086
Bruno Boi <boi@odoo.com>
2025-04-08 12:49:02 +0000

    [FIX] website/saas~18.3: remove view
    A view has been removed in community PR.
    
    PR: odoo/odoo#205010
    Task id: opw-4684654
    
    closes odoo/upgrade#7523
    
    Signed-off-by: Jinane Maksoud (maji) <maji@odoo.com>

--- migrations/website/saas~18.3.1.0/pre-migrate.py
+++ migrations/website/saas~18.3.1.0/pre-migrate.py
@@ -5,3 +5,4 @@ def migrate(cr, version):
     remove_view(cr, "website.snippet_options_carousel_intro")
     remove_view(cr, "website.s_carousel_intro_options")
     remove_view(cr, "website.s_tabs_options")
+    remove_view(cr, "website.view_server_action_tree_website")
