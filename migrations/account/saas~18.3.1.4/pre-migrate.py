355662ebb4dcef84e3ebcb283b3434919855b84d
aizu-odoo <aizu@odoo.com>
2025-04-08 16:18:03 +0200

    [IMP] account/saas~18.3: make cash basis transfer accounts reconcilable
    Accounts used as the cash basis transition accounts now need to be reconcilable
    in order to generate the appropriate exchange difference entries. Furthermore,
    as the exchange moves are now only generated in the partial reconciliation we
    can remove the exchange_move_id field from the full reconcile model.
    
    odoo/odoo/pull/202713
    odoo/enterprise/pull/81847
    
    task-4404035
    
    closes odoo/upgrade#7513
    
    Related: odoo/odoo#202713
    Related: odoo/enterprise#81847
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/account/saas~18.3.1.4/pre-migrate.py
+++ migrations/account/saas~18.3.1.4/pre-migrate.py
@@ -40,3 +40,31 @@ def migrate(cr, version):
     util.remove_field(cr, "res.config.settings", "module_account_bank_statement_import_ofx")
     util.remove_field(cr, "res.config.settings", "module_account_bank_statement_import_csv")
     util.remove_field(cr, "res.config.settings", "module_account_bank_statement_import_camt")
+    util.remove_field(cr, "account.full.reconcile", "exchange_move_id")
+
+    cr.execute(
+        """
+        UPDATE account_account
+           SET reconcile = TRUE
+          FROM account_tax
+         WHERE account_account.id = account_tax.cash_basis_transition_account_id
+           AND account_account.reconcile IS NOT TRUE
+     RETURNING account_account.id
+        """
+    )
+    account_ids = [row[0] for row in cr.fetchall()]
+    if account_ids:
+
+        # Copy paste of account.account _toggle_reconcile_to_true
+        query = cr.mogrify(
+            """
+            UPDATE account_move_line
+               SET reconciled = CASE WHEN debit = 0 AND credit = 0 AND amount_currency = 0 THEN true ELSE false END,
+                   amount_residual = debit - credit,
+                   amount_residual_currency = amount_currency
+             WHERE full_reconcile_id IS NULL
+               AND account_id IN %s
+            """,
+            [tuple(account_ids)],
+        ).decode()
+        util.explode_execute(cr, query, table="account_move_line")
