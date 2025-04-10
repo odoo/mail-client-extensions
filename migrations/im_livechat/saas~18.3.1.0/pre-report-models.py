bdeb81d58bc55f75c0daa4c46507c5dcc6f61295
Giorgio Tocco (gito) <gito@odoo.com>
2025-04-10 18:42:42 +0200

    [IMP] im_livechat/saas~18.3: merged im_livechat report models
    This commit merges `im_livechat.report.operator` into
    `im_livechat.report.channel`.
    The pivot view will be used for operators reporting and
    the graph view will be used for sessions reporting.
    
    COM: https://github.com/odoo/odoo/pull/204772
    
    task-4619167
    
    closes odoo/upgrade#7497
    
    Related: odoo/odoo#204772
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/im_livechat/saas~18.3.1.0/pre-report-models.py
@@ -0,0 +1,10 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    eb = util.expand_braces
+    util.merge_model(cr, *eb("im_livechat.report.{operator,channel}"))
+    cr.execute("DROP VIEW IF EXISTS im_livechat_report_operator")
+    util.remove_view(cr, "im_livechat.im_livechat_report_operator_view_graph")
+    util.remove_view(cr, "im_livechat.im_livechat_report_operator_view_pivot")
+    util.remove_view(cr, "im_livechat.im_livechat_report_operator_view_search")
