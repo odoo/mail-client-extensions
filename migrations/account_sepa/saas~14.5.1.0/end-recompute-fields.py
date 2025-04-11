68e02b6f3d90d656b81800ad23d0d555c57be260
Alvaro Fuentes <afu@odoo.com>
2025-04-11 15:42:21 +0000

    [FIX] account_sepa/saas~14.5: compute field in SQL
    Recomputing `account.journal.sepa_pain_version` causes the ORM to load
    all linked payments. Loading millions of records causes a MemoryError.
    Even though it does nothing since the field
    `account.payment.sepa_pain_version` is non stored related.
    
    Links in 15.0:
    https://github.com/odoo/enterprise/blob/fd519260d93e76bafd5de08acf4bbfa6450e63f2/account_sepa/models/account_journal.py#L39
    https://github.com/odoo/enterprise/blob/fd519260d93e76bafd5de08acf4bbfa6450e63f2/account_sepa_pain_001_001_09/models/account_payment.py#L13
    
    The records are loaded as part of the triggers of modifications:
    ```
    > /home/odoo/src/odoo/15.0/odoo/models.py(6124)_modified_triggers()
    -> new_records = self.filtered(lambda r: not r.id)
    (Pdb) l
    6119                                if not any(self._ids):
    6120                                    # if self are new, records should be new as well
    6121                                    records = records.browse(it and NewId(it) for it in records._ids)
    6122                                break
    6123                    else:
    6124 ->                     new_records = self.filtered(lambda r: not r.id)
    6125                        real_records = self - new_records
    6126                        records = model.browse()
    6127                        if real_records:
    6128                            records |= model.search([(key.name, 'in', real_records.ids)], order='id')
    6129                        if new_records:
    ```
    
    Line 6128 triggers a huge load of records via `search`:
    ```
    (Pdb) p real_records
    account.journal(4, 5, 3, 8, 16, 15, 17, 6, 1, 13, 14, 9, 11, 10, 12, 7)
    (Pdb) model.search_count([(key.name, 'in', real_records.ids)])
    12340633
    ```
    
     We get to this place from:
    ```
    (Pdb) w
      /home/odoo/src/odoo/15.0/odoo-bin(8)<module>()
    -> odoo.cli.main()
      /home/odoo/src/odoo/15.0/odoo/cli/command.py(61)main()
    -> o.run(args)
      /home/odoo/src/odoo/15.0/odoo/cli/server.py(184)run()
    -> main(args)
      /home/odoo/src/odoo/15.0/odoo/cli/server.py(178)main()
    -> rc = odoo.service.server.start(preload=preload, stop=stop)
      /home/odoo/src/odoo/15.0/odoo/service/server.py(1356)start()
    -> rc = server.run(preload, stop)
      /home/odoo/src/odoo/15.0/odoo/service/server.py(557)run()
    -> rc = preload_registries(preload)
      /home/odoo/src/odoo/15.0/odoo/service/server.py(1260)preload_registries()
    -> registry = Registry.new(dbname, update_module=update_module)
      /home/odoo/src/odoo/15.0/odoo/modules/registry.py(88)new()
    -> odoo.modules.load_modules(registry, force_demo, status, update_module)
      /home/odoo/src/odoo/15.0/odoo/modules/loading.py(492)load_modules()
    -> migrations.migrate_module(package, 'end')
      /home/odoo/src/odoo/15.0/odoo/modules/migration.py(186)migrate_module()
    -> migrate(self.cr, installed_version)
      /tmp/tmpfvzpl5c4/migrations/account_sepa/saas~14.5.1.0/end-recompute-fields.py(8)migrate()
    -> util.recompute_fields(
      /tmp/tmpfvzpl5c4/migrations/util/orm.py(247)wrapper()
    -> return f(*args, **kwargs)
      /tmp/tmpfvzpl5c4/migrations/util/orm.py(319)recompute_fields()
    -> records.modified(fields)
      /home/odoo/src/odoo/15.0/odoo/models.py(6059)modified()
    -> for field, records, create in tocompute:
    > /home/odoo/src/odoo/15.0/odoo/models.py(6127)_modified_triggers()
    -> if real_records:
    ```
    
    upg-2561707
    opw-4563315
    
    closes odoo/upgrade#7547
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account_sepa/saas~14.5.1.0/end-recompute-fields.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo.upgrade import util
-
-
-def migrate(cr, version):
-    # ===========================================================
-    # Task 2567701 : Move Sepa Pain Version
-    # ===========================================================
-    util.recompute_fields(cr, "account.journal", ["sepa_pain_version"])
--- migrations/account_sepa/saas~14.5.1.0/pre-migrate.py
+++ migrations/account_sepa/saas~14.5.1.0/pre-migrate.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
@@ -9,3 +8,43 @@ def migrate(cr, version):
     util.remove_field(cr, "res.company", "sepa_pain_version")
     util.remove_field(cr, "res.config.settings", "sepa_pain_version")
     util.create_column(cr, "account_journal", "sepa_pain_version", "varchar")
+
+    # Ref: https://github.com/odoo/enterprise/blob/fd519260d93e76bafd5de08acf4bbfa6450e63f2/account_sepa/models/account_journal.py#L62-L77
+    # First try to retrieve the country_code from the IBAN
+    cr.execute(
+        """
+        UPDATE account_journal j
+           SET sepa_pain_version = CASE substring(b.acc_number, 1, 2)
+                   WHEN 'DE' THEN 'pain.001.003.03'
+                   WHEN 'CH' THEN 'pain.001.001.03.ch.02'
+                   WHEN 'SE' THEN 'pain.001.001.03.se'
+                   WHEN 'AT' THEN 'pain.001.001.03.austrian.004'
+                   ELSE 'pain.001.001.03'
+               END
+          FROM res_partner_bank b
+         WHERE j.bank_account_id = b.id
+           AND b.acc_number ~ '^[A-Z]{2}[0-9]{2}.*'
+        """
+    )
+    # Then try from the company's fiscal country, and finally from the company's country
+    cr.execute(
+        """
+        UPDATE account_journal j
+           SET sepa_pain_version = CASE COALESCE(fiscal_c.code, comp_c.code)
+                   WHEN 'DE' THEN 'pain.001.003.03'
+                   WHEN 'CH' THEN 'pain.001.001.03.ch.02'
+                   WHEN 'SE' THEN 'pain.001.001.03.se'
+                   WHEN 'AT' THEN 'pain.001.001.03.austrian.004'
+                   ELSE 'pain.001.001.03'
+               END
+          FROM res_company c
+          JOIN res_partner p
+            ON p.id = c.partner_id
+     LEFT JOIN res_country fiscal_c
+            ON c.account_fiscal_country_id = fiscal_c.id
+     LEFT JOIN res_country comp_c
+            ON p.country_id = comp_c.id
+         WHERE j.sepa_pain_version IS NULL
+           AND j.company_id = c.id
+        """
+    )
