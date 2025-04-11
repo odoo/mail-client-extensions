8ea6526e4c20920312c6fc81df391f00ab9e1207
tsm-odoo <tsm@odoo.com>
2025-04-11 15:34:10 +0200

    [IMP] im_livechat/saas~18.3: remove technical_name field
    The community counterpart of this PR removes the "technical_name"
    field from the channel report model.
    
    closes odoo/upgrade#7546
    
    Community: https://github.com/odoo/odoo/pull/205533
    Related: odoo/odoo#205533
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- migrations/im_livechat/saas~18.3.1.0/pre-migrate.py
+++ migrations/im_livechat/saas~18.3.1.0/pre-migrate.py
@@ -12,3 +12,4 @@ def migrate(cr, version):
         "im_livechat.ir_rule_discuss_channel_member_group_im_livechat_group_manager",
         "im_livechat.ir_rule_discuss_channel_member_im_livechat_group_user",
     )
+    util.remove_field(cr, "im_livechat.report.channel", "technical_name")
