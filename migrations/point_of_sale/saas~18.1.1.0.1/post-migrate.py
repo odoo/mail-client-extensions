76504c1da10768e5cbec5a3e6e195df886850630
Alvaro Fuentes <afu@odoo.com>
2025-04-14 14:19:02 +0200

    [FIX] point_of_sale/saas~18.1: do not warn for demo
    `pos_restaurant` adds a demo session with state `opening_control`
    
    https://github.com/odoo/odoo/blob/0754f70d3c9a586781d9d82b26d72a2224a8cccc/addons/pos_restaurant/data/restaurant_session_floor.xml#L497
    
    It doesn't block runbot because it upgrades without demo. It makes
    upgrade_partial CI red though. The latter is unblocking but it makes
    people nervous.
    
    closes odoo/upgrade#7556
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/point_of_sale/saas~18.1.1.0.1/post-migrate.py
+++ migrations/point_of_sale/saas~18.1.1.0.1/post-migrate.py
@@ -10,5 +10,9 @@ def migrate(cr, version):
     pos_ids = [r[0] for r in cr.fetchall()]
     if not pos_ids:
         return
-    _logger.warning("%s PoS sessions were already open, generating its sequences", len(pos_ids))
+    _logger.log(
+        util.NEARLYWARN if util.on_CI() else logging.WARNING,
+        "%s PoS sessions were already open, generating its sequences",
+        len(pos_ids),
+    )
     util.iter_browse(util.env(cr)["pos.session"], pos_ids)._create_sequences()
