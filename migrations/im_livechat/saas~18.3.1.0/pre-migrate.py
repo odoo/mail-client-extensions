259ec865a8369eca61fc19d8d8da88218c8ef2a4
Giorgio Tocco (gito) <gito@odoo.com>
2025-04-23 18:21:41 +0200

    [IMP] im_livechat/saas~18.3: removed session statistics measures
    COM: https://github.com/odoo/odoo/pull/207009
    
    This task removes some measures from the livechat report model.
    
    task-4589964
    
    closes odoo/upgrade#7595
    
    Related: odoo/odoo#207009
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/im_livechat/saas~18.3.1.0/pre-migrate.py
+++ migrations/im_livechat/saas~18.3.1.0/pre-migrate.py
@@ -12,5 +12,3 @@ def migrate(cr, version):
         "im_livechat.ir_rule_discuss_channel_member_group_im_livechat_group_manager",
         "im_livechat.ir_rule_discuss_channel_member_im_livechat_group_user",
     )
-    util.remove_field(cr, "im_livechat.report.channel", "technical_name")
-    util.remove_field(cr, "im_livechat.report.channel", "is_without_answer")
