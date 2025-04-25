409fdcaf8a8f8e409ba1311c6f2a076feacfae46
Agustin Castro Bugallo <agcb@odoo.com>
2025-04-25 09:38:53 +0000

    [FIX] documents_hr_recruitment/18.0: Ensure parent folder is correct
    The record `document_recruitment_folder` is changed in 18.0 to be moved to the
    folder `document_hr_folder`. However that folder can be archived or the change
    can create a loop, so we add a check before the update.
    
    upg-2709912
    upg-2646254
    ```
    raise UserError(_("It is not possible to move archived documents or documents to archived folders."))
    
    ```
    upg-2627419
    ```
    raise UserError(_("Recursion Detected."))
    ```
    
    closes odoo/upgrade#7613
    
    Signed-off-by: Jinane Maksoud (maji) <maji@odoo.com>

--- migrations/documents_hr_recruitment/18.0.1.0/pre-migrate.py
+++ migrations/documents_hr_recruitment/18.0.1.0/pre-migrate.py
@@ -2,4 +2,24 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.if_unchanged(cr, "documents_hr_recruitment.document_recruitment_folder", util.update_record_from_xml)
+    cr.execute(
+        """
+        SELECT 1
+          FROM documents_document hr,
+               documents_document recr
+         WHERE hr.id = %s
+           AND hr.active
+           AND recr.id = %s
+           AND recr.active
+           AND recr.id::text != ALL(STRING_TO_ARRAY(hr.parent_path, '/'))
+           AND recr.folder_id IS NULL
+        """,
+        [
+            util.ref(cr, "documents_hr.document_hr_folder"),
+            util.ref(cr, "documents_hr_recruitment.document_recruitment_folder"),
+        ],
+    )
+    if cr.rowcount:
+        util.update_record_from_xml(
+            cr, "documents_hr_recruitment.document_recruitment_folder", fields=("folder_id", "internal_access")
+        )
