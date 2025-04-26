0c374cb19e901be81d4dd813bcbfa9555f826966
Vivek Sinha <vksi@odoo.com>
2025-04-26 08:40:10 +0000

    [FIX] account_reports/0.0.0: Fix typo
    Recently d7a18ac
    this fix got deployed in this only line_id is take care of but this line_id
    column was renamed from saas~16.3 88a1358 earlier it was line so fixing it
    accordingly
    
    Traceback
    2025-04-25 17:37:52,613 28 INFO db_2775180 odoo.modules.migration: module
    account_reports: Running migration [0.0.0>] post-migrate
    2025-04-25 17:37:52,638 28 ERROR db_2775180 odoo.sql_db: bad query:
                UPDATE "account_report_footnote"
                   SET line_id = regexp_replace(line_id, '(?<!-)-', '~', 'g')
                 WHERE line_id LIKE '%-%'
                 AND account_report_footnote.id IS NOT NULL
    ERROR: column "line_id" does not exist
    LINE 4:              WHERE line_id LIKE '%-%'
                               ^
    HINT:  Perhaps you meant to reference the column "account_report_footnote.line".
    
    2025-04-25 17:37:52,645 28 WARNING db_2775180 odoo.modules.loading: Transient
     module states were reset
    2025-04-25 17:37:52,645 28 ERROR db_2775180 odoo.modules.registry: Failed to
    load registry
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/16.0/odoo/modules/registry.py", line 87, in new
        odoo.modules.load_modules(registry, force_demo, status, update_module)
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 489, in
    load_modules
        processed_modules += load_marked_modules(cr, graph,
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 374, in
    load_marked_modules
        loaded, processed = load_module_graph(
      File "/home/odoo/src/odoo/16.0/odoo/modules/loading.py", line 238, in
    load_module_graph
        migrations.migrate_module(package, 'post')
      File "/home/odoo/src/odoo/16.0/odoo/modules/migration.py", line 170, in
    migrate_module
        exec_script(self.cr, installed_version, pyfile, pkg.name, stage,
    stageformat[stage] % version)
      File "/home/odoo/src/odoo/16.0/odoo/modules/migration.py", line 188, in
    exec_script
        migrate(cr, installed_version)
      File "/tmp/tmp7qm_lt61/migrations/account_reports/0.0.0/post-migrate.py", line
    16,in migrate
        util.explode_execute(cr, query, table=table)
      File "/tmp/tmp7qm_lt61/migrations/util/pg.py", line 380, in explode_execute
        return parallel_execute(
      File "/tmp/tmp7qm_lt61/migrations/util/pg.py", line 188, in parallel_execute
        return parallel_execute_impl(cr, queries, logger=_logger)
      File "/tmp/tmp7qm_lt61/migrations/util/pg.py", line 104, in
    _parallel_execute_threaded
        cr.execute(queries[0])
      File "/home/odoo/src/odoo/16.0/odoo/sql_db.py", line 321, in execute
        res = self._obj.execute(query, params)
    psycopg2.errors.UndefinedColumn: column "line_id" does not exist
    LINE 4:              WHERE line_id LIKE '%-%'
                               ^
    HINT:  Perhaps you meant to reference the column "account_report_footnote.line".
    
    task-4753526
    tbg-1982
    
    closes odoo/upgrade#7618
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account_reports/0.0.0/post-migrate.py
+++ migrations/account_reports/0.0.0/post-migrate.py
@@ -3,14 +3,16 @@ from odoo.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     if util.version_between("16.0", "saas~18.3"):
+        column = "line_id" if util.version_gte("saas~16.3") else "line"
         table = "account_report_annotation" if util.version_gte("saas~17.4") else "account_report_footnote"
         query = util.format_query(
             cr,
             """
             UPDATE {table}
-               SET line_id = regexp_replace(line_id, '(?<!-)-', '~', 'g')
-             WHERE line_id LIKE '%-%'
+               SET {column} = regexp_replace({column}, '(?<!-)-', '~', 'g')
+             WHERE {column} LIKE '%-%'
             """,
             table=table,
+            column=column,
         )
         util.explode_execute(cr, query, table=table)
