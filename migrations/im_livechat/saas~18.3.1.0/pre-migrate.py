e72e566fd8216ea6ff72a1b64488a6a193c6ba89
Christophe Simonis <chs@odoo.com>
2025-04-30 17:37:01 +0200

    [FIX] im_livechat/saas~18.3
    Create empty column/m2m for new stored-computed fields.
    Those fields are computed with data from a mew model, so nothing to
    compute for existing databases.
    
    Part-of: odoo/upgrade#7694
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/im_livechat/saas~18.3.1.0/pre-migrate.py
+++ migrations/im_livechat/saas~18.3.1.0/pre-migrate.py
@@ -12,3 +12,11 @@ def migrate(cr, version):
         "im_livechat.ir_rule_discuss_channel_member_group_im_livechat_group_manager",
         "im_livechat.ir_rule_discuss_channel_member_im_livechat_group_user",
     )
+
+    util.create_m2m(
+        cr, "im_livechat_channel_member_history_discuss_channel_agent_rel", "discuss_channel", "res_partner"
+    )
+    util.create_m2m(
+        cr, "im_livechat_channel_member_history_discuss_channel_customer_rel", "discuss_channel", "res_partner"
+    )
+    util.create_column(cr, "discuss_channel", "livechat_is_escalated", "boolean", default=False)
