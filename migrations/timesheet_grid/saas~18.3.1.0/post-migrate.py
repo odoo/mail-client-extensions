e2477ab40e107deaf37cc4481065e9299fea0e9d
Christophe Simonis <chs@odoo.com>
2025-04-30 18:17:24 +0200

    [FIX] timesheet_grid/saas~18.3
    Process only timers of tasks of active employees linked to a project
    with analytic account.
    
    Part-of: odoo/upgrade#7694
    Signed-off-by: Nicolas Seinlet (nse) <nse@odoo.com>

--- migrations/timesheet_grid/saas~18.3.1.0/post-migrate.py
+++ migrations/timesheet_grid/saas~18.3.1.0/post-migrate.py
@@ -11,7 +11,18 @@ def migrate(cr, version):
     # because a existing timer could be existed in timesheet for a same user
     # and so to be sure only one timer in same model for a same user exists
     # those existing timers will be deleted.
-    cr.execute("SELECT id FROM timer_timer WHERE res_model='project.task'")
+    cr.execute("SELECT id FROM res_company")
+    all_companies = [cid for (cid,) in cr.fetchall()]
+    cr.execute(
+        """
+            SELECT t.id
+              FROM timer_timer t
+              JOIN hr_employee h
+             USING (user_id)
+             WHERE t.res_model = 'project.task'
+               AND h.active = true
+        """
+    )
     timer_ids = [tid for (tid,) in cr.fetchall()]
     if timer_ids:
         timesheet_vals_list = []
@@ -19,17 +30,21 @@ def migrate(cr, version):
         rounding = max(1, int(env["ir.config_parameter"].get_param("timesheet_grid.timesheet_rounding", 1)))
 
         for t in util.iter_browse(env["timer.timer"], timer_ids):
-            task = env["project.task"].browse(t.res_id)
+            task = env["project.task"].browse(t.res_id).exists()
+            if not task.project_id.account_id:
+                continue
             minutes_spent = t._get_minutes_spent()
             timesheet_vals_list.append(
                 {
                     "task_id": task.id,
                     "project_id": task.project_id.id,
+                    "account_id": task.project_id.account_id.id,
                     "date": t.timer_start.date(),
                     "name": "/",
                     "user_id": t.user_id.id,
                     "unit_amount": round_time_spent(minutes_spent, minimum_duration, rounding) / 60,
                 }
             )
-        util.iter_browse(env["account.analytic.line"], []).create(timesheet_vals_list)
+        AAL = env["account.analytic.line"].with_context(allowed_company_ids=all_companies)
+        util.iter_browse(AAL, []).create(timesheet_vals_list)
         util.iter_browse(env["timer.timer"], timer_ids).unlink()
