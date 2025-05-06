909f07fab8c65a1f8f76585693d19611844eec7a
jepbarasgarov <jeas@odoo.com>
2025-05-06 14:14:20 +0000

    [FIX] base/0.0.0: revert ensuring `sms` for `hr_recruitment`
    As you see here[^1] we have backported bridge module between
    `hr_recruitment` and `sms` starting from version `17.0`.
    So there is no need to force_install `sms`
    
    [^1]: https://github.com/odoo/odoo/pull/199238
    
    Part-of: odoo/upgrade#7646
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/base/0.0.0/pre-ensure-sms-for-hr_recruitment.py
+++
@@ -1,9 +0,0 @@
-from odoo.addons.base.maintenance.migrations import util
-
-
-def migrate(cr, version):
-    if util.version_between("saas~16.4", "saas~18.1"):
-        # There is an action in hr_recruitment that requires sms, the dependencies are wrong
-        # it was relying in the autoinstall of sms that could have been uninstalled by clients
-        # since we cannot change the dependencies in stable we reinstall sms.
-        util.force_install_module(cr, "sms", if_installed=("hr_recruitment",), reason="it's an implicit dependency")
