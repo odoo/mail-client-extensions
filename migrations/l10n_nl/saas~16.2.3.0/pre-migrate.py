a82788a7d193d7c0230b1045609682ebe62986a3
sagu-odoo <sagu@odoo.com>
2025-05-07 18:18:51 +0530

    [FIX] l10n_nl/saas~16.2: Fix typo
    according [this]
    https://github.com/odoo/odoo/blob/98ef54a1bb7a2c3085b7802efdb88a55a83622b6/addons/account_edi_ubl_cii/models/res_partner.py#L37
    code for ``l10n_nl_kvk`` it should be [``0106``]
    https://github.com/odoo/odoo/blob/d0ea51c5a84f0c082822bf00e2bd174c22a9f853/addons/account_edi_ubl_cii/models/res_partner.py#L54
    and same for ``l10n_nl_oin`` it should be [``0910``]
    https://github.com/odoo/odoo/blob/d0ea51c5a84f0c082822bf00e2bd174c22a9f853/addons/account_edi_ubl_cii/models/res_partner.py#L62
    
    https://docs.peppol.eu/poacc/billing/3.0/codelist/eas/
    
    opw-4764888
    upg-2755217
    
    closes odoo/upgrade#7651
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/l10n_nl/saas~16.2.3.0/pre-migrate.py
+++ migrations/l10n_nl/saas~16.2.3.0/pre-migrate.py
@@ -5,8 +5,8 @@ def migrate(cr, version):
     """
     Try to infer the value of the fields peppol_endpoint and peppol_eas.
     Copy the value of:
-    * l10n_nl_oin on the field peppol_endpoint and set peppol_eas to '0106' if it's set
-    * l10n_nl_kvk on the field peppol_endpoint and set peppol_eas to '0190' if it's set
+    * l10n_nl_oin on the field peppol_endpoint and set peppol_eas to '0190' if it's set
+    * l10n_nl_kvk on the field peppol_endpoint and set peppol_eas to '0106' if it's set
     * vat on the field peppol_endpoint and set peppol_eas to '9944' if it's set
     """
     query = """
@@ -14,8 +14,8 @@ def migrate(cr, version):
            SET peppol_endpoint = COALESCE(l10n_nl_kvk, l10n_nl_oin, vat),
                peppol_eas = (
                CASE
-                    WHEN l10n_nl_kvk IS NOT NULL THEN '0190'
-                    WHEN l10n_nl_oin IS NOT NULL THEN '0106'
+                    WHEN l10n_nl_kvk IS NOT NULL THEN '0106'
+                    WHEN l10n_nl_oin IS NOT NULL THEN '0190'
                     WHEN vat IS NOT NULL THEN '9944'
                END)
           FROM res_country c
