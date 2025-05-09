78ce1ecb775724ac8e36c4829d2fea14296f774f
Maximilien (malb) <malb@odoo.com>
2025-05-09 09:56:26 +0200

    [FIX] account_{accountant,check_printing}/saas~18.3: correct script
    Views have been reintroduced in https://github.com/odoo/enterprise/pull/85093
    
    closes odoo/upgrade#7662
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account_accountant/saas~18.3.1.1/pre-migrate.py
+++ migrations/account_accountant/saas~18.3.1.1/pre-migrate.py
@@ -4,4 +4,3 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_model(cr, "bank.rec.widget")
     util.remove_model(cr, "bank.rec.widget.line")
-    util.remove_view(cr, "account_accountant.view_account_move_line_search_bank_rec_widget")
--- migrations/account_check_printing/saas~18.3.1.0/pre-migrate.py
+++
@@ -1,5 +0,0 @@
-from odoo.upgrade import util
-
-
-def migrate(cr, version):
-    util.remove_view(cr, "account_accountant_check_printing.view_account_move_line_search_bank_rec_widget")
