94f537c4d9bfa4c0a028759449d4afb1b6ecb5ce
khaj-odoo <khaj@odoo.com>
2025-05-16 11:23:28 +0530

    [IMP] website_sale/saas~18.4: remove shipping address setting
    - Drops the group_delivery_invoice_address setting .
      Community PR: odoo/odoo#213720
      Task_ID:4746668
    
    closes odoo/upgrade#7705
    
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

---
+++ migrations/website_sale/saas~18.4.1.1/pre-migrate.py
@@ -0,0 +1,20 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "res.config.settings", "group_delivery_invoice_address")
+    hid = util.ref(cr, "account.group_delivery_invoice_address")
+    if not hid:
+        return
+
+    ptl = util.ref(cr, "base.group_portal")
+    pub = util.ref(cr, "base.group_public")
+
+    cr.execute(
+        """
+        DELETE FROM res_groups_implied_rel
+              WHERE hid = %s
+                AND gid IN %s
+        """,
+        (hid, (ptl, pub)),
+    )
