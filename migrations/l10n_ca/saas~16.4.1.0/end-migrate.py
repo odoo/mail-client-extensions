44e2a6d384ed46dde7fd8329f05072a477f86012
suresh <suba@odoo.com>
2025-05-16 10:53:19 +0000

    [FIX] l10n_ca/saas~16.4: Load Chart Template Data Properly
    Steps to reproduce:
    1. Create a database in a version before 16.4 and install `l10n_ca`.
    2. Migrate the database to version 17.0.
    3. Newly added chart of accounts are not loaded into the database.
    
    - Due to this, when we create a branch of the company and we are adding
       default account for sales journal, we get a traceback error.
    
    ```Traceback
    Traceback (most recent call last):
      File "/data/build/odoo/odoo/http.py", line 1976, in _transactioning
        return service_model.retrying(func, env=self.env)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/data/build/odoo/odoo/service/model.py", line 158, in retrying
        env.cr.flush()  # submit the changes to the database
        ^^^^^^^^^^^^^^
      File "/data/build/odoo/odoo/sql_db.py", line 160, in flush
        self.precommit.run()
      File "/data/build/odoo/odoo/tools/misc.py", line 1161, in run
        func()
      File "/data/build/odoo/addons/account/models/company.py", line 418, in try_loading
        self.env['account.chart.template']._load(
      File "/data/build/odoo/addons/account/models/chart_template.py", line 228, in _load
        self._post_load_data(template_code, company, template_data)
      File "/data/build/enterprise/hr_payroll_account/models/account_chart_template.py", line 17, in _post_load_data
        super()._post_load_data(template_code, company, template_data)
      File "/data/build/enterprise/account_reports/models/chart_template.py", line 10, in _post_load_data
        super()._post_load_data(template_code, company, template_data)
      File "/data/build/odoo/addons/stock_account/models/account_chart_template.py", line 12, in _post_load_data
        super()._post_load_data(template_code, company, template_data)
      File "/data/build/odoo/addons/account/models/chart_template.py", line 687, in _post_load_data
        sale_journal.default_account_id = self.ref(template_data.get('property_account_income_categ_id'))
                                          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/data/build/odoo/addons/account/models/chart_template.py", line 1162, in ref
        or self.env.ref(f"account.{self.env.company.parent_ids[0].id}_{xmlid}", raise_if_not_found)
           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/data/build/odoo/odoo/api.py", line 658, in ref
        res_model, res_id = self['ir.model.data']._xmlid_to_res_model_res_id(
                            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/data/build/odoo/odoo/addons/base/models/ir_model.py", line 2253, in _xmlid_to_res_model_res_id
        return self._xmlid_lookup(xmlid)
               ^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/usr/lib/python3/dist-packages/decorator.py", line 232, in fun
        return caller(func, *(extras + args), **kw)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/data/build/odoo/odoo/tools/cache.py", line 110, in lookup
        value = d[key] = self.method(*args, **kwargs)
                         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/data/build/odoo/odoo/addons/base/models/ir_model.py", line 2246, in _xmlid_lookup
        raise ValueError('External ID not found in the system: %s' % xmlid)
    ValueError: External ID not found in the system: account.13_l10n_ca_411100
    ```
    - To resolve this issue, I have loaded the chart template data.
      All the records will be added to the database after migration.
    
    OPW - 4737861
    UPG - 2700932
    
    closes odoo/upgrade#7700
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/l10n_ca/saas~16.4.1.0/end-migrate.py
@@ -0,0 +1,13 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute("UPDATE res_company SET chart_template = 'ca_2023' WHERE chart_template = 'ca' RETURNING id")
+    ids = [r[0] for r in cr.fetchall()]
+    if not ids:
+        return
+    env = util.env(cr)
+    companies = env["res.company"].search([("id", "in", ids)], order="parent_path")
+    companies.invalidate_recordset()  # necessary due to the update query above!
+    for company in companies:
+        env["account.chart.template"].try_loading("ca_2023", company)
--- migrations/l10n_ca/saas~16.4.1.0/post-migrate.py
+++
@@ -1,2 +0,0 @@
-def migrate(cr, version):
-    cr.execute("UPDATE res_company SET chart_template = 'ca_2023' WHERE chart_template = 'ca'")
