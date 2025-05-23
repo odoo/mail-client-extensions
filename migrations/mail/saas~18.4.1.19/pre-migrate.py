65df4ff76d0fdf52673aba426e473c809157281a
Thibault Delavallée <tde@odoo.com>
2025-05-23 12:01:47 +0200

    [FIX] mail/saas~18.4: remove outdated fields
    Followup of odoo/odoo#204895 : we can now remove dead fields and
    widgets in composer and scheduler. Those fields are not used anymore
    in chatter since another solution was implemented.
    
    See odoo/odoo#211442
    
    Task-4728522
    
    closes odoo/upgrade#7734
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/mail/saas~18.4.1.19/pre-migrate.py
+++ migrations/mail/saas~18.4.1.19/pre-migrate.py
@@ -4,3 +4,8 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_model(cr, "mail.resend.message")
     util.remove_model(cr, "mail.resend.partner")
+
+    util.remove_field(cr, "mail.compose.message", "notified_bcc")
+    util.remove_field(cr, "mail.compose.message", "show_notified_bcc")
+    util.remove_field(cr, "mail.scheduled.message", "notified_bcc")
+    util.remove_field(cr, "mail.scheduled.message", "show_notified_bcc")
