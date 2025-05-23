5c66432132642b73a575d1098fd8290d998c0d5c
igbe <igbe@odoo.com>
2025-05-23 10:16:02 +0200

    [IMP] account/saas~18.4.1.4: Remove autocheck_on_post field
    We reworked the Check feature on account moves. Now, we will auto check the
    moves based on the access right of the user, so we don't need the journal
    to autopost the moves anymore.
    
    See: https://github.com/odoo/odoo/pull/211598
    See: https://github.com/odoo/enterprise/pull/86386
    
    task-4807991
    
    closes odoo/upgrade#7740
    
    Related: odoo/odoo#211598
    Related: odoo/enterprise#86386
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account/saas~18.4.1.4/pre-migrate.py
+++ migrations/account/saas~18.4.1.4/pre-migrate.py
@@ -2,6 +2,7 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.remove_field(cr, "account.journal", "autocheck_on_post")
     util.change_field_selection_values(
         cr,
         "account.reconcile.model.line",
