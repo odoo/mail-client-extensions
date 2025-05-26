81c1de384d95d3f67484dbc216132db58e01aee7
jepbarasgarov <jeas@odoo.com>
2025-05-26 13:09:47 +0000

    [FIX] hr_recruitment/0.0.0: on_collision=`merge` while moving actions
    In some of the dbs, which has `sms` and `hr_recruitment` and bridge
    module already installed and the actions are exisiting in both
    `hr_recruitment` and `hr_recruitment_sms` modules. So during renaming
    the xmlids they are colliding.
    Most probably this case is happening because of module upgrade.
    
    Steps to reproduce:
    1. Checkout to commit which is before the bridge module in `18.0`
    2. Install hr_recruitment and sms (at this point we do not have bridge module)
    3. On new tab in terminal check out to the latest `18.0`  while the
       db is still running on the first tab.
    4. Log in to db and upgraded only module `sms` (you will get
       `hr_recruitment_sms` with state `uninstalled`)
    5. Activate the bridge module manually
    
    You will end up having both actions in both modules. Because
    `hr_recruitment` haven't been loaded after checking out to
    latest `18.0`.
    
    Now if you try to upgrade to `saas~18.2`, you will get
    error like this:
    
    ```
    Traceback (most recent call last):
      File "/home/odoo/src/odoo/saas-18.2/odoo/service/server.py", line 1352, in preload_registries
        registry = Registry.new(dbname, update_module=update_module, install_modules=config['init'], upgrade_modules=config['update'])
      File "<decorator-gen-14>", line 2, in new
      File "/home/odoo/src/odoo/saas-18.2/odoo/tools/func.py", line 98, in locked
        return func(inst, *args, **kwargs)
      File "/home/odoo/src/odoo/saas-18.2/odoo/orm/registry.py", line 165, in new
        load_modules(
      File "/home/odoo/src/odoo/saas-18.2/odoo/modules/loading.py", line 460, in load_modules
        load_module_graph(
      File "/home/odoo/src/odoo/saas-18.2/odoo/modules/loading.py", line 189, in load_module_graph
        migrations.migrate_module(package, 'pre')
      File "/home/odoo/src/odoo/saas-18.2/odoo/modules/migration.py", line 229, in migrate_module
        exec_script(self.cr, installed_version, pyfile, pkg.name, stage, stageformat[stage] % version)
      File "/home/odoo/src/odoo/saas-18.2/odoo/modules/migration.py", line 266, in exec_script
        mod.migrate(cr, installed_version)
      File "/tmp/tmpjceklbom/migrations/hr_recruitment/0.0.0/pre-move_actions_hr_recruitment_sms.py", line 8, in migrate
        util.rename_xmlid(cr, *eb("hr_recruitment{,_sms}.action_hr_applicant_mass_sms"))
      File "/tmp/tmpjceklbom/migrations/util/records.py", line 776, in rename_xmlid
        raise MigrationError("Can't rename {} to {} as it already exists".format(old, new))
    odoo.upgrade.util.exceptions.UpgradeError: Can't rename hr_recruitment.action_hr_applicant_mass_sms to hr_recruitment_sms.action_hr_applicant_mass_sms as it already exists
    ```
    
    tbg-2011
    
    closes odoo/upgrade#7745
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/hr_recruitment/0.0.0/pre-move_actions_hr_recruitment_sms.py
+++ migrations/hr_recruitment/0.0.0/pre-move_actions_hr_recruitment_sms.py
@@ -5,7 +5,7 @@ def migrate(cr, version):
     eb = util.expand_braces
     if util.version_between("17.0", "19.0"):
         if util.module_installed(cr, "hr_recruitment_sms"):
-            util.rename_xmlid(cr, *eb("hr_recruitment{,_sms}.action_hr_applicant_mass_sms"))
+            util.rename_xmlid(cr, *eb("hr_recruitment{,_sms}.action_hr_applicant_mass_sms"), on_collision="merge")
         else:
             util.delete_unused(cr, "hr_recruitment.action_hr_applicant_mass_sms")
 
@@ -13,6 +13,6 @@ def migrate(cr, version):
         if util.module_installed(cr, "hr_recruitment_sms"):
             # Action `action_hr_candidate_mass_sms` was introduced in `18.0`
             # and it was removed in `saas~18.2`
-            util.rename_xmlid(cr, *eb("hr_recruitment{,_sms}.action_hr_candidate_mass_sms"))
+            util.rename_xmlid(cr, *eb("hr_recruitment{,_sms}.action_hr_candidate_mass_sms"), on_collision="merge")
         else:
             util.delete_unused(cr, "hr_recruitment.action_hr_candidate_mass_sms")
