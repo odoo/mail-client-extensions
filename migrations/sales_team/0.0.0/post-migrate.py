2e72797ce5e36f65dcd3dcb949b22edf8a0fe67a
Jinane Maksoud <maji@odoo.com>
2025-05-27 13:31:49 +0200

    [FIX] sales_team/0.0.0: check column exists
    The condition on crm module being installed does
    not guarantee the column to exist, the module could
    be in state 'to install' during the upgrade.
    
    closes odoo/upgrade#7754
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/sales_team/0.0.0/post-migrate.py
+++ migrations/sales_team/0.0.0/post-migrate.py
@@ -2,5 +2,5 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    if util.version_gte("12.0") and util.module_installed(cr, "crm"):
+    if util.version_gte("12.0") and util.column_exists(cr, "crm_team", "use_opportunities"):
         cr.execute("UPDATE crm_team SET use_opportunities=True WHERE use_opportunities IS NULL")
