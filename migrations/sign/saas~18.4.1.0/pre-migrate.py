d8939a84d594eee545f8af2fb1c2af6fc4e9a399
Ahmad (alah) <alah@odoo.com>
2025-06-02 15:46:03 +0200

    [IMP] sign/saas~18.4: UI/UX cleanup
    This commit aims to:
    1. Remove unused transient model sign.duplicate.template.pdf
    
    2. Remove unused transient model sign.request.download
    
    3. Create the column `completion_date` on `sign_request`
    and update it, as we are making this column `stored`
    in the enterprise PR.
    
    Enterprise PR: odoo/enterprise#84142
    
    closes odoo/upgrade#7781
    
    Task: 4555838
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>

--- migrations/sign/saas~18.4.1.0/pre-migrate.py
+++ migrations/sign/saas~18.4.1.0/pre-migrate.py
@@ -3,3 +3,25 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "sign.sign_item_role_view_tree")
+    util.remove_model(cr, "sign.duplicate.template.pdf")
+    util.remove_model(cr, "sign.request.download")
+    util.create_column(cr, "sign_request", "completion_date", "timestamp")
+    util.explode_execute(
+        cr,
+        """
+        WITH latest_request_items AS (
+                SELECT sign_request_id, MAX(signing_date) AS max_signing_date
+                  FROM sign_request_item
+              GROUP BY sign_request_id
+        )
+        UPDATE sign_request sr
+           SET completion_date = CASE
+          WHEN sr.nb_wait > 0 THEN NULL
+          ELSE latest_request_items.max_signing_date
+           END
+          FROM latest_request_items
+         WHERE sr.id = latest_request_items.sign_request_id
+        """,
+        table="sign_request",
+        alias="sr",
+    )
