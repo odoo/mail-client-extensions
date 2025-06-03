88d9d6431c3f96030b7a260cf0f37426a1b4e371
Youssef <abyo@odoo.com>
2025-06-03 15:28:50 +0200

    [IMP] purchase/saas~18.4: remove has_alternatives field
    - Remove field has_alternatives from purchase order to match the PO line view
    
    See odoo/odoo#211005
    Task:4774453
    
    closes odoo/upgrade#7786
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/purchase_requisition/saas~18.4.0.1/pre-migrate.py
+++ migrations/purchase_requisition/saas~18.4.0.1/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "purchase.requisition.create.alternative", "partner_id")
+    util.remove_field(cr, "purchase.order", "has_alternatives")
