d4e537aa7f20d825ad37bf61dfa3449a7fca71fb
Christophe Simonis <chs@odoo.com>
2025-06-04 19:38:22 +0200

    [FIX] hr_recruitment_extract/saas~18.2
    Use model name instead of table name.
    
    Part-of: odoo/upgrade#7803
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/hr_recruitment_extract/saas~18.2.1.0/post-upgrade.py
+++ migrations/hr_recruitment_extract/saas~18.2.1.0/post-upgrade.py
@@ -4,4 +4,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     cr.execute("SELECT id FROM hr_applicant WHERE candidate_id IS NULL")
     if cr.rowcount:
-        util.recompute_fields(cr, "hr_applicant", ["is_in_extractable_state"], ids=[rid for (rid,) in cr.fetchall()])
+        util.recompute_fields(cr, "hr.applicant", ["is_in_extractable_state"], ids=[rid for (rid,) in cr.fetchall()])
