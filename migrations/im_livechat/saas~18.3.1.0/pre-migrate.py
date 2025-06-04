991586b05dafa37813e24b5268e30e8c3d650ffe
Christophe Simonis <chs@odoo.com>
2025-06-04 16:57:48 +0200

    [FIX] im_livechat/saas~18.3
    Fill the new m2m with know data.
    
    closes odoo/upgrade#7800
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/im_livechat/saas~18.3.1.0/pre-migrate.py
+++ migrations/im_livechat/saas~18.3.1.0/pre-migrate.py
@@ -16,7 +16,38 @@ def migrate(cr, version):
     util.create_m2m(
         cr, "im_livechat_channel_member_history_discuss_channel_agent_rel", "discuss_channel", "res_partner"
     )
+    query = """
+        INSERT INTO im_livechat_channel_member_history_discuss_channel_agent_rel(discuss_channel_id, res_partner_id)
+             SELECT id, livechat_operator_id
+               FROM discuss_channel
+              WHERE channel_type = 'livechat'
+                AND livechat_operator_id IS NOT NULL
+              UNION
+             SELECT c.id, sc.operator_partner_id
+               FROM chatbot_message m
+               JOIN discuss_channel c
+                 ON c.id = m.discuss_channel_id
+               JOIN chatbot_script_step st
+                 ON st.id = m.script_step_id
+               JOIN chatbot_script sc
+                 ON sc.id = st.chatbot_script_id
+              WHERE c.channel_type = 'livechat'
+    """
+    cr.execute(query)
+
     util.create_m2m(
         cr, "im_livechat_channel_member_history_discuss_channel_customer_rel", "discuss_channel", "res_partner"
     )
+    if util.column_exists(cr, "discuss_channel", "livechat_visitor_id"):  # from `website_livechat` module
+        query = """
+            INSERT INTO im_livechat_channel_member_history_discuss_channel_customer_rel(discuss_channel_id, res_partner_id)
+                 SELECT c.id, v.partner_id
+                   FROM discuss_channel c
+                   JOIN website_visitor v
+                     ON v.id = c.livechat_visitor_id
+                  WHERE c.channel_type = 'livechat'
+                    AND v.partner_id IS NOT NULL
+        """
+        cr.execute(query)
+
     util.create_column(cr, "discuss_channel", "livechat_is_escalated", "boolean", default=False)
