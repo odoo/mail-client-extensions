9e37df68fe72dc99b83fd980d4621ba4ad4254bf
Akash Pandey <apan@odoo.com>
2025-06-05 18:43:58 +0530

    [FIX] product/saas~17.4: migrate old pricelist discount behavior to v18
    In `v18.0`, the field `discount_policy` was removed. Now, discounts on sale
    order lines are only shown if the pricelist has `compute_price = 'percentage'`
    and the user has the group `'sale.group_discount_per_so_line'`.
    
    In `v17.0`, the visibility and application of discounts were controlled by
    the `discount_policy` field:
    
    1. `discount_policy = 'without_discount'`:
       - Unit price shows the public price
       - Discount is shown separately on the SO line
    
       → In `v18.0`, this must become `compute_price = 'percentage'`
    
    2. `discount_policy = 'with_discount'`:
       - Unit price includes the discount
       - No discount value is shown on the SO line
    
       → In `v18.0`, this must become `compute_price = 'formula'`
    
    This script updates existing pricelist items accordingly:
    
    - Converts compute_price from 'formula' to 'percentage' if
      discount_policy was 'without_discount'
    - Converts compute_price from 'percentage' to 'formula' if
      discount_policy was 'with_discount'
    - Also swaps price_discount and percent_price values as needed
    
    Steps to reproduce:
    
    - Install Sales in a v17.0 DB with discounts and advanced price rules
    - Create a pricelist with:
      - compute_price = 'formula'
      - discount_policy = 'without_discount'
    - Or create one with:
      - compute_price = 'percentage'
      - discount_policy = 'with_discount'
    - Upgrade to v18.0
    - Apply pricelist to a sale order and add a line
    
    opw-4846908
    upg-2844265
    
    closes odoo/upgrade#7815
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/product/saas~17.4.1.2/pre-migrate.py
+++ migrations/product/saas~17.4.1.2/pre-migrate.py
@@ -9,6 +9,43 @@ def migrate(cr, version):
     util.delete_unused(cr, "product.group_sale_pricelist")
     util.remove_field(cr, "res.config.settings", "group_sale_pricelist")
     util.remove_field(cr, "res.config.settings", "product_pricelist_setting")
+
+    formula_query = util.explode_query_range(
+        cr,
+        """
+        UPDATE product_pricelist_item item
+           SET compute_price = 'percentage',
+               percent_price = item.price_discount
+          FROM product_pricelist pp
+         WHERE pp.id = item.pricelist_id
+           AND item.compute_price = 'formula'
+           AND pp.discount_policy = 'without_discount'
+           AND COALESCE(item.price_surcharge, 0) = 0
+           AND COALESCE(item.price_round, 0) = 0
+           AND COALESCE(item.price_min_margin, 0) = 0
+           AND COALESCE(item.price_max_margin, 0) = 0
+           AND {parallel_filter}
+        """,
+        table="product_pricelist_item",
+        alias="item",
+    )
+    percentage_query = util.explode_query_range(
+        cr,
+        """
+        UPDATE product_pricelist_item item
+           SET compute_price = 'formula',
+               price_discount = item.percent_price
+          FROM product_pricelist pp
+         WHERE pp.id = item.pricelist_id
+           AND item.compute_price = 'percentage'
+           AND pp.discount_policy = 'with_discount'
+           AND {parallel_filter}
+        """,
+        table="product_pricelist_item",
+        alias="item",
+    )
+    util.parallel_execute(cr, formula_query + percentage_query)
+
     util.remove_field(cr, "product.pricelist", "discount_policy")
 
     util.create_column(cr, "product_pricelist_item", "price_markup", "float8")
