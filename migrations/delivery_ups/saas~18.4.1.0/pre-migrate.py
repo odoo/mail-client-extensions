041c6d1c42aced10da5f21e9db91b02dce2edd9e
Nisarg (nipl) <nipl@odoo.com>
2025-06-09 10:39:44 +0530

    [IMP] delivery_ups/saas~18.4: Restrict UPS Number to eCommerce checkout
    - Removed the `UPS Account Number` field from the main portal address form to
      avoid exposing it outside relevant use cases.
    - The field is now displayed only during the eCommerce checkout flow, when:
      - The user is creating or editing a delivery-type address,
      - `Bill My Account` is enabled, and
      - The selected or default delivery carrier on the partner is UPS or not set.
    
    task-4669485
    
    closes odoo/upgrade#7826
    
    Related: odoo/odoo#204726
    Related: odoo/enterprise#87200
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- migrations/delivery_ups/saas~18.4.1.0/pre-migrate.py
+++ migrations/delivery_ups/saas~18.4.1.0/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "delivery_ups.view_picking_withcarrier_out_form_inherit_delivery_ups")
+    util.remove_view(cr, "delivery_ups.portal_my_details")
