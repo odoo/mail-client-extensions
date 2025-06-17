97c6db31d5011da7e21078c22ea85faf1870dabe
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-06-17 11:59:12 +0200

    [IMP] sale_timesheet_enterprise/saas~18.4: remove employee public view
    See odoo/odoo#214129
    See odoo/enterprise#87592
    
    remove the employee_public_form_view_inherit
    
    closes odoo/upgrade#7862
    
    Task: 4568134
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/sale_timesheet_enterprise/saas~18.4.0.1/pre-migrate.py
+++ migrations/sale_timesheet_enterprise/saas~18.4.0.1/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "edit.billable.time.target", "job_title")
+    util.remove_view(cr, "sale_timesheet_enterprise.hr_employee_public_form_view_inherit")
