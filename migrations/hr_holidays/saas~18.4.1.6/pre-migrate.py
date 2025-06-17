b435065fbdcb9b74cc1a2fdcabc5be46a9e16728
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-06-17 11:55:58 +0200

    [IMP] hr_holidays/saas~18.4: remove employee public view
    See odoo/odoo#214129
    See odoo/enterprise#87592
    
    - remove the employee_public_form_view_inherit
    
    Task: 4568134
    Part-of: odoo/upgrade#7862
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/hr_holidays/saas~18.4.1.6/pre-migrate.py
+++ migrations/hr_holidays/saas~18.4.1.6/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "hr.leave", "manager_id")
+    util.remove_view(cr, "hr_holidays.hr_employee_public_form_view_inherit")
