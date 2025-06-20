86e95715b889e48efcd8d864868b11a2740338cb
tsm-odoo <tsm@odoo.com>
2025-06-20 14:48:37 +0200

    [IMP] im_livechat/saas~18.4: deduplicate member history
    The community counterpart of this commit, the member history
    is adapted in order to only have one record per channel/partner.
    A constraint is added to ensure no failing rows will be created
    afterwards. This commit deduplicates the already existing rows:
    - first member history is kept, because the `create_date` field
    is used to compute statistics such as session duration.
    - last history member is set on the first history as the last
    member is the relevant one.
    
    This will allow to use the member history model as reporting model.
    
    part of task-4796793
    
    closes odoo/upgrade#7908
    
    Community: https://github.com/odoo/odoo/pull/215002
    Related: odoo/odoo#215002
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/im_livechat/saas~18.4.1.0/pre-migrate.py
+++ migrations/im_livechat/saas~18.4.1.0/pre-migrate.py
@@ -9,3 +9,29 @@ def migrate(cr, version):
              SET block_assignment_during_call = True
            WHERE max_sessions_mode = 'limited'
         """)
+    if util.table_exists(cr, "im_livechat_channel_member_history"):
+        query = """
+          WITH duplicated_history_data AS (
+            SELECT ARRAY_AGG(history.id ORDER BY history.id) AS ids
+              FROM discuss_channel
+              JOIN im_livechat_channel_member_history history
+                ON discuss_channel.id = history.channel_id
+             WHERE partner_id IS NOT NULL
+               AND {parallel_filter}
+          GROUP BY history.channel_id,
+                  history.partner_id
+            HAVING COUNT(*) > 1
+          ),
+          _deleted AS (
+            DELETE FROM im_livechat_channel_member_history history
+             USING duplicated_history_data dups
+             WHERE history.id = ANY(dups.ids[1:ARRAY_LENGTH(dups.ids, 1) - 1])
+          )
+          UPDATE im_livechat_channel_member_history last_history
+             SET create_date = first_history.create_date
+            FROM duplicated_history_data dups
+            JOIN im_livechat_channel_member_history first_history
+              ON first_history.id = dups.ids[1]
+           WHERE last_history.id = dups.ids[ARRAY_LENGTH(dups.ids, 1)]
+        """
+        util.explode_execute(cr, query, "discuss_channel")
