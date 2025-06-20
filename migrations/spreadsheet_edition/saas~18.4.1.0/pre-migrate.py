23df6bd55397cc0174473740d9e757a4b6860ef9
Lucas Lefèvre (lul) <lul@odoo.com>
2025-06-20 08:15:23 +0200

    [IMP] spreadsheet_edition/saas~18.4: clean pivot sorted column
    A previous upgrade script for saas-18.1 (68219d8b) mistakenly forgot to change
    the sorted column field name to the measure id.
    
    The original upgrade script was fixed with 8506f12279ad87
    But we already had databases that had made the upgrade.
    We had to temporarily support both field name and measure id.
    
    This script cleanups things up, drops the support for
    field name and cleans the revisions that didn't go through
    the fixed upgrade script.
    
    odoo/o-spreadsheet#6637
    
    closes odoo/upgrade#7909
    
    Task: 4884349
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/spreadsheet_edition/saas~18.4.1.0/pre-migrate.py
+++ migrations/spreadsheet_edition/saas~18.4.1.0/pre-migrate.py
@@ -9,35 +9,47 @@ DEFAULT_VALUE_MAP = {
 
 
 def migrate(cr, version):
-    for commands in iter_commands(cr, like_any=[r"%ADD\_GLOBAL\_FILTER%", r"%EDIT\_GLOBAL\_FILTER%"]):
+    for commands in iter_commands(
+        cr, like_any=[r"%ADD\_GLOBAL\_FILTER%", r"%EDIT\_GLOBAL\_FILTER%", r"%ADD\_PIVOT%", r"%UPDATE\_PIVOT%"]
+    ):
         for command in commands:
-            if command["type"] not in ("ADD_GLOBAL_FILTER", "EDIT_GLOBAL_FILTER"):
-                continue
-            global_filter = command["filter"]
-            # If the defaultValue is an empty value, we remove it
-            if "defaultValue" in global_filter and not global_filter["defaultValue"]:
-                del global_filter["defaultValue"]
-            if global_filter["type"] == "text" and "defaultValue" in global_filter:
-                global_filter["defaultValue"] = [global_filter["defaultValue"]]
-            if global_filter["type"] == "text" and "rangeOfAllowedValues" in global_filter:
-                global_filter["rangesOfAllowedValues"] = [global_filter["rangeOfAllowedValues"]]
-                del global_filter["rangeOfAllowedValues"]
-            if (
-                global_filter["type"] == "date"
-                and global_filter["rangeType"] == "fixedPeriod"
-                and not isinstance(global_filter["defaultValue"], str)
-            ):
-                # If the defaultValue is not a string, it's probably a
-                # something very old that we do not support anymore
-                # See migration2to3(antepenultimate_year for example)
-                # from migration.js in odoo.
-                del global_filter["defaultValue"]
-            if global_filter["type"] == "date" and "defaultValue" in global_filter:
-                if global_filter["defaultValue"] in ("last_six_month", "last_three_years"):
+            if command["type"] in ("ADD_GLOBAL_FILTER", "EDIT_GLOBAL_FILTER"):
+                global_filter = command["filter"]
+                # If the defaultValue is an empty value, we remove it
+                if "defaultValue" in global_filter and not global_filter["defaultValue"]:
                     del global_filter["defaultValue"]
-                elif global_filter["defaultValue"] in DEFAULT_VALUE_MAP:
-                    global_filter["defaultValue"] = DEFAULT_VALUE_MAP[global_filter["defaultValue"]]
-            if "rangeType" in global_filter:
-                del global_filter["rangeType"]
-            if "disabledPeriods" in global_filter:
-                del global_filter["disabledPeriods"]
+                if global_filter["type"] == "text" and "defaultValue" in global_filter:
+                    global_filter["defaultValue"] = [global_filter["defaultValue"]]
+                if global_filter["type"] == "text" and "rangeOfAllowedValues" in global_filter:
+                    global_filter["rangesOfAllowedValues"] = [global_filter["rangeOfAllowedValues"]]
+                    del global_filter["rangeOfAllowedValues"]
+                if (
+                    global_filter["type"] == "date"
+                    and global_filter["rangeType"] == "fixedPeriod"
+                    and not isinstance(global_filter["defaultValue"], str)
+                ):
+                    # If the defaultValue is not a string, it's probably a
+                    # something very old that we do not support anymore
+                    # See migration2to3(antepenultimate_year for example)
+                    # from migration.js in odoo.
+                    del global_filter["defaultValue"]
+                if global_filter["type"] == "date" and "defaultValue" in global_filter:
+                    if global_filter["defaultValue"] in ("last_six_month", "last_three_years"):
+                        del global_filter["defaultValue"]
+                    elif global_filter["defaultValue"] in DEFAULT_VALUE_MAP:
+                        global_filter["defaultValue"] = DEFAULT_VALUE_MAP[global_filter["defaultValue"]]
+                if "rangeType" in global_filter:
+                    del global_filter["rangeType"]
+                if "disabledPeriods" in global_filter:
+                    del global_filter["disabledPeriods"]
+            elif command["type"] in ("ADD_PIVOT", "UPDATE_PIVOT"):
+                if command["pivot"].get("sortedColumn"):
+                    sorted_measure_id = command["pivot"]["sortedColumn"]["measure"]
+                    # a previous upgrade script for saas-18.1 mistakenly forgot to change
+                    # the sorted column field name to the measure id. We had to temporarily
+                    # support both. This script cleanups things up.
+                    new_measure_id = next(
+                        (m["id"] for m in command["pivot"]["measures"] if m["fieldName"] == sorted_measure_id), None
+                    )
+                    if new_measure_id:
+                        command["pivot"]["sortedColumn"]["measure"] = new_measure_id
--- migrations/spreadsheet_edition/tests/test_pivot_sorted_column.py
+++ migrations/spreadsheet_edition/tests/test_pivot_sorted_column.py
@@ -94,3 +94,92 @@ class TestPivotSortedColumn(UpgradeCase):
         )
         self.assertEqual(commands[2]["pivot"].get("sortedColumn"), None)
         self.assertEqual(commands[3]["pivot"].get("sortedColumn"), None)
+
+
+@change_version("saas~18.4")
+class TestCleanPivotSortedColumn(UpgradeCase):
+    def prepare(self):
+        if not util.version_gte("saas~18.3"):
+            self.skipTest("sorted column cleaning test is skipped for versions < saas~18.3")
+
+        pivot1 = {
+            "type": "ODOO",
+            "rows": [],
+            "columns": [],
+            "measures": [{"id": "MEASURE:sum", "fieldName": "MEASURE", "aggregator": "sum"}],
+            "model": "MODEL",
+            "name": "MODEL",
+            "sortedColumn": {
+                "order": "asc",
+                "measure": "MEASURE",  # not correct, should be "MEASURE:sum"
+                "domain": [],
+            },
+        }
+        pivot2 = {
+            **pivot1,
+            "sortedColumn": {
+                "order": "asc",
+                "measure": "MEASURE:sum",  # correct
+                "domain": [],
+            },
+        }
+        commands = {
+            "type": "REMOTE_REVISION",
+            "commands": [
+                {
+                    "type": "ADD_PIVOT",
+                    "pivot": pivot1,
+                    "pivotId": "1",
+                },
+                {
+                    "type": "UPDATE_PIVOT",
+                    "pivot": pivot1,
+                    "pivotId": "1",
+                },
+                {
+                    "type": "ADD_PIVOT",
+                    "pivot": pivot2,
+                    "pivotId": "2",
+                },
+                {
+                    "type": "UPDATE_PIVOT",
+                    "pivot": pivot2,
+                    "pivotId": "2",
+                },
+            ],
+        }
+
+        revision_id = self.env["spreadsheet.revision"].create(
+            [
+                {
+                    "res_id": 40009,
+                    "res_model": "spreadsheet",
+                    "commands": json.dumps(commands),
+                    "revision_uuid": "B",
+                    "parent_revision_id": False,
+                },
+            ]
+        )
+        return revision_id.id
+
+    def check(self, revision_id):
+        if not revision_id:
+            return
+        revisions = self.env["spreadsheet.revision"].browse(revision_id)
+        commands = json.loads(revisions[0].commands)["commands"]
+        self.assertEqual(
+            commands[0]["pivot"]["sortedColumn"]["measure"],
+            "MEASURE:sum",
+        )
+        self.assertEqual(
+            commands[1]["pivot"]["sortedColumn"]["measure"],
+            "MEASURE:sum",
+        )
+        self.assertEqual(
+            commands[2]["pivot"]["sortedColumn"]["measure"],
+            "MEASURE:sum",
+        )
+        self.assertEqual(
+            commands[3]["pivot"]["sortedColumn"]["measure"],
+            "MEASURE:sum",
+        )
