415ded1b9ee0624042c506b4e9ac01de779f83cb
Maira Salazar (srma) <srma@odoo.com>
2025-06-24 09:12:31 +0200

    [ADD] l10n_it_edi/saas~18.5: fill new field l10n_it_edi_attachment_name
    In the related commit, the many2one field l10n_it_edi_attachment_id is
    removed, leaving only the binary field l10n_it_edi_attachment_file.
    A new field l10n_it_edi_attachment_name is created to keep track of the
    attachment file name.
    
    In this upgrade commit, the field is filled with the existing name
    values of ir.attachment for IT EDI attachment files.
    
    See odoo/odoo#212726
    task-4771720
    
    closes odoo/upgrade#7785
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/l10n_it_edi/saas~18.5.0.4/pre-migrate.py
@@ -0,0 +1,17 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "account_move", "l10n_it_edi_attachment_name", "varchar")
+
+    query = """
+        UPDATE account_move
+           SET l10n_it_edi_attachment_name = ir_attachment.name
+          FROM ir_attachment
+         WHERE account_move.id = ir_attachment.res_id
+           AND ir_attachment.res_model = 'account.move'
+           AND ir_attachment.res_field = 'l10n_it_edi_attachment_file'
+    """
+    util.explode_execute(cr, query, table="account_move")
+
+    util.remove_field(cr, "account.move", "l10n_it_edi_attachment_id")
