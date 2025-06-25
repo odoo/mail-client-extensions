6c145647d128fbe768e0ef75328f3a31e3325dcb
Bruno Boi <boi@odoo.com>
2025-06-25 07:00:14 +0000

    [ADD] base/saas~18.5: initial actions code history
    Since a code history has been added to server actions of type 'Execute
    Python Code', we need a script to create the initial history for
    existing actions.
    
    Community PR: odoo/odoo#210338
    
    closes odoo/upgrade#7870
    
    Task: opw-4392608
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/base/saas~18.5.1.3/pre-99-migrate.py
@@ -0,0 +1,30 @@
+def migrate(cr, version):
+    cr.execute("""
+        CREATE TABLE ir_actions_server_history (
+            id SERIAL NOT NULL PRIMARY KEY,
+            action_id INTEGER,
+            code TEXT,
+            create_uid INTEGER,
+            write_uid INTEGER,
+            create_date TIMESTAMP WITHOUT TIME ZONE,
+            write_date TIMESTAMP WITHOUT TIME ZONE
+        )
+    """)
+    cr.execute("""
+        INSERT INTO ir_actions_server_history (
+                        action_id,
+                        code,
+                        create_uid,
+                        create_date,
+                        write_uid,
+                        write_date
+                    )
+             SELECT a.id,
+                    a.code,
+                    a.write_uid,
+                    a.write_date,
+                    a.write_uid,
+                    a.write_date
+               FROM ir_act_server a
+              WHERE a.state = 'code'
+    """)
