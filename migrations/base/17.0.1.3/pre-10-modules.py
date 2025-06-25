123e1b33e8e28c04cf0971c8802d30721fc621e8
Christophe Simonis <chs@odoo.com>
2025-06-25 19:48:35 +0200

    [FIX] base/17.0,web_map/saas~12.5
    Since odoo/odoo@1b8351038da2448b9d28dadd2e6db0652315e6df, the module
    `base_setup` is autoinstall, which in turn autoinstall the `web_map`
    module.
    
    Force the SQL compute of the added computed-stored field.
    
    This mainly useful for CI as the `base_setup` and `web_map` modules
    are installed in pretty much all databases.

--- migrations/base/17.0.1.3/pre-10-modules.py
+++ migrations/base/17.0.1.3/pre-10-modules.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.merge_module(cr, "account_sepa_direct_debit_008_001_08", "account_sepa_direct_debit")
+    util.force_upgrade_of_fresh_module(cr, "web_map")
