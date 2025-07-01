73341bcf92522fe348dac31ff33a083aa74125fe
Christophe Simonis <chs@odoo.com>
2025-07-01 19:01:16 +0200

    [FIX] {base,website_documents}/17.0
    Bootstrap new column of new autoinstall module.
    See odoo/enterprise@ce9ce608fdc5ac3b0d1a58eaaa5ae7780e6cd43f

--- migrations/base/17.0.1.3/pre-10-modules.py
+++ migrations/base/17.0.1.3/pre-10-modules.py
@@ -12,3 +12,4 @@ def migrate(cr, version):
     util.force_upgrade_of_fresh_module(cr, "l10n_in_ewaybill_stock")
     util.force_upgrade_of_fresh_module(cr, "l10n_rs_edi")
     util.force_upgrade_of_fresh_module(cr, "l10n_br_avatax_services")
+    util.force_upgrade_of_fresh_module(cr, "website_documents")
