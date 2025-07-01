7794322ce814177e53f3c18664430274bfc11603
Hesham Saleh <hsal@odoo.com>
2025-07-01 16:37:52 +0200

    [IMP] base/18.0: Merge l10n_fr_reports modules
    This commit merges l10n_fr_reports_extended module
    into l10n_fr_reports module.
    The extended module was added to backport some new fields
    in the regular one.
    
    Community PR: https://github.com/odoo/odoo/pull/202249
    Enterprise PR: https://github.com/odoo/enterprise/pull/81652
    
    task-4458309
    
    closes odoo/upgrade#7980
    
    Signed-off-by: Jinane Maksoud (maji) <maji@odoo.com>

--- migrations/base/18.0.1.3/pre-10-modules.py
+++ migrations/base/18.0.1.3/pre-10-modules.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.merge_module(cr, "l10n_es_edi_tbai_multi_refund", "l10n_es_edi_tbai")
+    util.merge_module(cr, "l10n_fr_reports_extended", "l10n_fr_reports")
