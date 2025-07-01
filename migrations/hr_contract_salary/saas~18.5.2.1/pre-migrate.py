ea4ed75b780e52a8552b0c3e4126191b80ed230b
adak-ahmed <adak@odoo.com>
2025-07-01 15:34:38 +0200

    [IMP] hr_contract_salary/saas~18.5: translate_fields_for salary_configurator
    Originally, some terms remain untranslated when chaging the
    language from English. Thus, those terms have been adjusted
    to adhere to automatic translation
    
    Ent PR: https://github.com/odoo/enterprise/pull/89132
    
    closes odoo/upgrade#7976
    
    Taskid: 4853199
    Related: odoo/enterprise#89132
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ migrations/hr_contract_salary/saas~18.5.2.1/pre-migrate.py
@@ -0,0 +1,8 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.convert_field_to_translatable(cr, "hr.contract.salary.benefit.type", "name")
+    util.convert_field_to_translatable(cr, "hr.contract.salary.personal.info.type", "name")
+    util.convert_field_to_translatable(cr, "hr.contract.salary.resume.category", "name")
+    util.convert_field_to_translatable(cr, "hr.contract.salary.resume", "name")
