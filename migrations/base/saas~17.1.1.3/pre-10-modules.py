6213514d22ba9003caf9d793bdb1b2e0083d53cd
Harsh Modi <hamo@odoo.com>
2025-07-02 18:04:00 +0530

    [IMP] base/saas~17.1: Remove Indian Debitnote Modules
    Since we are backporting the debit note support
    for `l10n_in` including the debit note sequence (generic account)
    and we can't add new dependency in stable,
    we are adding debit note modules only in `17.0`
    and the feature is already available from saas-17.1
    
    In this commit we remove the modules as the feature is
    already available from `saas-17.1`
    
    task-4910859
    
    see https://github.com/odoo/odoo/pull/217128 (main counter PR)
    see https://github.com/odoo/enterprise/pull/89276 (main counter PR)
    
    see https://github.com/odoo/odoo/pull/168844
    see https://github.com/odoo/enterprise/pull/66420
    see https://github.com/odoo/upgrade/pull/6251
    see https://github.com/odoo/odoo/pull/146922
    see https://github.com/odoo/enterprise/pull/53097
    
    closes odoo/upgrade#7992
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/saas~17.1.1.3/pre-10-modules.py
+++ migrations/base/saas~17.1.1.3/pre-10-modules.py
@@ -5,8 +5,9 @@ def migrate(cr, version):
     util.rename_module(cr, "mrp_subonctracting_landed_costs", "mrp_subcontracting_landed_costs")
     util.rename_module(cr, "website_form_project", "website_project")
     util.merge_module(cr, "l10n_es_edi_facturae_adm_centers", "l10n_es_edi_facturae")
-
+    util.merge_module(cr, "account_debit_note_sequence", "account_debit_note")
     if util.has_enterprise():
         util.rename_module(cr, "account_bacs", "l10n_uk_bacs")
         util.rename_module(cr, "l10n_au_keypay", "l10n_employment_hero")
         util.merge_module(cr, "l10n_be_codabox_bridge", "l10n_be_codabox")
+        util.remove_module(cr, "l10n_in_reports_debit_note")
