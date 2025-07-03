ba0ff47a8cc450da4be49d812bff22256c8786ad
romc-odoo <romc@odoo.com>
2025-07-03 13:43:37 +0200

    [ADD] hr_skills*/saas~18.5: remove fields/records and create column
    *=_slides
    
    See https://github.com/odoo/odoo/pull/205677
    
    task-4686721
    
    closes odoo/upgrade#8003
    
    Related: odoo/odoo#205677
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/hr_skills/saas~18.5.1.0/pre-migrate.py
+++ migrations/hr_skills/saas~18.5.1.0/pre-migrate.py
@@ -4,3 +4,7 @@ from odoo.upgrade import util
 def migrate(cr, version):
     if util.module_installed(cr, "hr_recruitment_skills"):
         util.move_field_to_module(cr, "hr.job", "skill_ids", "hr_recruitment_skills", "hr_skill")
+    util.delete_unused(cr, "hr_skills.resume_type_social_media")
+    util.remove_field(cr, "hr.resume.line", "display_type")
+    util.create_column(cr, "hr_resume_line", "external_url", "varchar")
+    util.create_column(cr, "hr_resume_line", "course_type", "varchar", default="external")
---
+++ migrations/hr_skills_slides/saas~18.5.1.0/pre-migrate.py
@@ -0,0 +1,16 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.delete_unused(cr, "hr_skills_slides.resume_type_training")
+    util.remove_view(cr, "hr_skills_slides.resume_slides_line_view_form")
+
+    cr.execute(
+        """
+        UPDATE hr_resume_line
+           SET duration = slide_channel.total_time,
+               course_type = 'elearning'
+          FROM slide_channel
+         WHERE slide_channel.id = hr_resume_line.channel_id
+        """
+    )
