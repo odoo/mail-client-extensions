d61d6c6e0c7c8712b0a6dff35738098da7a41a1d
Hesham Saleh <hsal@odoo.com>
2025-07-03 11:33:54 +0200

    [IMP] l10n_fr_reports/17.0: Uneeded view removed
    Removed dashboard kanban view extension from french reports localization module.
    This commit was needed because the community/enterprise PRs were backported
    they had this upgrade PR: https://github.com/odoo/upgrade/pull/7073
    
    Community PR: https://github.com/odoo/odoo/pull/202249
    Enterprise PR: https://github.com/odoo/enterprise/pull/81652
    
    task-4458309
    
    closes odoo/upgrade#7999
    
    Signed-off-by: Jinane Maksoud (maji) <maji@odoo.com>

--- migrations/l10n_fr_reports/0.0.0/pre-migrate.py
+++ migrations/l10n_fr_reports/0.0.0/pre-migrate.py
@@ -4,3 +4,5 @@ from odoo.addons.base.maintenance.migrations import util
 def migrate(cr, version):
     if util.version_between("saas~16.1", "17.0"):
         util.remove_record(cr, "l10n_fr_reports.account_financial_report_line_02_0_6_fr_bilan_passif_balance")
+    if util.version_between("17.0", "19.0"):
+        util.remove_view(cr, "l10n_fr_reports.inherit_view_account_journal_dashboard_kanban")
--- migrations/l10n_fr_reports/saas~18.2.1.2/pre-migrate.py
+++
@@ -1,5 +0,0 @@
-from odoo.upgrade import util
-
-
-def migrate(cr, version):
-    util.remove_view(cr, "l10n_fr_reports.inherit_view_account_journal_dashboard_kanban")
