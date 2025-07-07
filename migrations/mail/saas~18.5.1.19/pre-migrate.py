ec655410d5d6cf817b04b8840193e3e649d9af3b
Giorgio Tocco (gito) <gito@odoo.com>
2025-07-07 09:29:18 +0200

    [FIX] mail/saas~18.5: remove unused user settings mute
    community PR: https://github.com/odoo/odoo/pull/217638
    
    This commit removes the `mute_until_dt` field
    of `ResUsersSettings` after it was replaced by
    busy behavior by https://github.com/odoo/odoo/pull/215866.
    
    This commit also removes the cron job that
    called a method removed in said PR.
    
    closes odoo/upgrade#8018
    
    Related: odoo/odoo#217638
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/mail/saas~18.5.1.19/pre-migrate.py
@@ -0,0 +1,6 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.remove_field(cr, "res.users.settings", "mute_until_dt")
+    util.remove_record(cr, "mail.ir_cron_discuss_users_settings_unmute")
