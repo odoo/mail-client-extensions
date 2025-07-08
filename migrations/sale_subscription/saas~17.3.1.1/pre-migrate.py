d478e7bab97be36df148b78238e3ca949b326002
Akash Pandey <apan@odoo.com>
2025-07-08 18:03:23 +0530

    [FIX] sale_subscription/saas~17.3: fix violations to apply constraint
    Constraint was introduced in `saas~17.3`:
    odoo/enterprise@0f4a1f69aee0ddcc6448dbc100141c4a6f44535d
    This constraint prevents subscription records in `draft` or `sent` states
    from having a `progress` or `paused` `subscription_state`
    
    However, in `saas~17.2` and earlier, it was possible to create such records.
    
    Steps to reproduce:
    - In a v17 db, create a draft subscription.
    - Move it to the `In Progress` kanban stage (subscription_state = '3_progress')
    - Upgrade the db to v18.
    - The constraint will fail to be added due to existing inconsistent records.
    ```py
    2025-07-08 12:11:44,347 38746 WARNING sub_18 odoo.schema: Table 'sale_order': unable to add constraint 'sale_order_sale_subscription_state_coherence_2' as CHECK(NOT (is_subscription=TRUE AND state in ('draft', 'sent') AND subscription_state in ('3_progress', '4_paused')) )
    ```
    
    opw-4908844
    upg-2879623
    
    closes odoo/upgrade#8033
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/sale_subscription/saas~17.3.1.1/pre-migrate.py
+++ migrations/sale_subscription/saas~17.3.1.1/pre-migrate.py
@@ -4,3 +4,33 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "sale.order", "archived_product_ids")
     util.remove_field(cr, "sale.order", "archived_product_count")
+    cr.execute(
+        """
+        UPDATE sale_order
+           SET subscription_state =
+               CASE
+                   WHEN subscription_id IS NULL THEN '1_draft'
+                   ELSE '2_renewal'
+               END
+         WHERE is_subscription = TRUE
+           AND state IN ('draft', 'sent')
+           AND subscription_state IN ('3_progress', '4_paused')
+     RETURNING id, name
+        """
+    )
+    sub_ids = cr.fetchall()
+    if sub_ids:
+        util.add_to_migration_reports(
+            """
+            <details>
+                <summary>
+                    The following subscriptions have been updated because you cannot have a draft SO be a confirmed subscription.
+                </summary>
+                <ul>{}</ul>
+            </details>
+            """.format(
+                " ".join(f"<li>{util.get_anchor_link_to_record('sale.order', id, name)}</li>" for id, name in sub_ids)
+            ),
+            category="Subscription",
+            format="html",
+        )
