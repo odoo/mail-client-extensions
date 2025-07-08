e5d28f8130fe2a1e64e9428b44bfa366937c08a9
clesgow <quwo@odoo.com>
2025-07-08 10:11:50 +0200

    [IMP] stock_*/saas~18.5: allow multi-level packages
    Remove previously set fields that no longer have a meaning with the new
    pack in pack, as we no longer rely on the package_level.
    Also removes the old `choose.delivery.package` wizard as it's merged
    into the newer put in pack wizard.
    
    See odoo/odoo#203987
    See odoo/enterprise#82453
    
    Part-of: odoo/upgrade#7576
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/stock/saas~18.5.1.1/pre-migrate.py
+++ migrations/stock/saas~18.5.1.1/pre-migrate.py
@@ -25,3 +25,31 @@ def migrate(cr, version):
         cr, "stock.action_report_quant_package_barcode_small", "stock.action_report_package_barcode_small"
     )
     cr.execute("UPDATE ir_sequence SET code = 'stock.package' WHERE code = 'stock.quant.package'")
+
+    util.create_column(cr, "stock_package_type", "package_use", "varchar", default="disposable")
+    cr.execute("""
+        WITH reusable_types AS (
+                     SELECT sp.package_type_id AS type_id
+                       FROM stock_package sp
+                      WHERE sp.package_type_id IS NOT NULL
+                        AND sp.package_use = 'reusable'
+                   GROUP BY sp.package_type_id
+        )
+        UPDATE stock_package_type spt
+           SET package_use = 'reusable'
+          FROM reusable_types rt
+         WHERE spt.id = rt.type_id
+    """)
+    util.remove_field(cr, "stock.move", "picking_type_entire_packs")
+    util.remove_field(cr, "stock.move.line", "picking_type_entire_packs")
+    util.remove_field(cr, "stock.picking", "has_packages")
+    util.remove_field(cr, "stock.picking", "move_line_exist")
+    util.remove_field(cr, "stock.package", "package_use")
+
+    # compute new fields
+    util.create_column(cr, "stock_package", "complete_name", "varchar")
+    query = """
+        UPDATE stock_package sp
+           SET complete_name = sp.name
+    """
+    util.explode_execute(cr, query, table="stock_package", alias="sp")
--- migrations/stock_delivery/saas~18.5.1.0/pre-migrate.py
+++ migrations/stock_delivery/saas~18.5.1.0/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.rename_xmlid(cr, "stock_delivery.view_quant_package_weight_form", "stock_delivery.stock_package_view_form")
+    util.remove_model(cr, "choose.delivery.package")
