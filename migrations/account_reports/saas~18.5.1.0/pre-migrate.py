f0fa77ec7be6766e68fe7564557fde62de3d87d8
hupo-odoo <hupo@odoo.com>
2025-07-08 17:17:08 +0200

    [IMP] {l10n_be,account}_reports/saas~18.5: tax returns improvements
    In the related enterprise PR, we improve the views and model of
    the tax returns. for the return checks, we remove the chatter and
    some views.
    
    VAT returns now also have 4 stages instead of 3 before. The "Submit"
    stage is split into 2, making necessary an upgrade script to handle
    the 2 wizards that now exist.
    
    The field approver_ids on the return checks is also removed. It could
    contain several users. This field is replaced by 2 new fields, approver_id
    and supervisor_id. For the migration, if an admin was present in the
    original field, we set it as both the approver and supervisor, and
    if only a bookkeeper was present, it is set as approver.
    
    enterprise-https://github.com/odoo/enterprise/pull/89384
    task-4891073
    
    closes odoo/upgrade#8035
    
    Related: odoo/enterprise#89384
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account_reports/saas~18.5.1.0/pre-migrate.py
+++ migrations/account_reports/saas~18.5.1.0/pre-migrate.py
@@ -3,3 +3,64 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_record(cr, "account_reports.action_open_view_account_return")
+
+    util.remove_inherit_from_model(cr, "account.return.check", "mail.thread")
+    util.remove_inherit_from_model(cr, "account.return.check", "mail.activity.mixin")
+
+    util.remove_view(cr, "account_reports.view_attachment_kanban_inherit_return")
+    util.remove_view(cr, "account_reports.account_return_check_list_view")
+    util.remove_view(cr, "account_reports.account_return_check_form_view")
+
+    # Account Return model was introduced in 18.3
+    if util.table_exists(cr, "account_return"):
+        util.create_column(cr, "account_return", "date_lock", "date")
+        cr.execute(
+            """
+            UPDATE account_return
+               SET date_lock = date_submission
+             WHERE date_submission IS NOT NULL
+               AND amount_to_pay IS NOT NULL
+            """
+        )
+
+    # Removes approver_ids and dispatch values in new fields approver_id and supervisor_id
+
+    # | Scenario                | `approver_id` outcome | `supervisor_id` outcome |
+    # | ----------------------- | --------------------- | ----------------------- |
+    # | Admin(s) exist          | Set to admin          | Set to admin            |
+    # | Only non-admins exist   | Set to any approver   | Remains NULL            |
+    # | No approver_ids at all  | Not updated           | Not updated             |
+
+    # Return check model was introduced in 18.3
+    if not util.table_exists(cr, "account_return_check"):
+        return
+
+    util.create_column(cr, "account_return_check", "approver_id", "int4")
+    util.create_column(cr, "account_return_check", "supervisor_id", "int4")
+    cr.execute(
+        """
+            WITH check_admin_candidates AS (
+                SELECT return_check.id as return_check_id,
+                       MIN(checks_users.res_users_id) FILTER (WHERE admin.id IS NOT NULL) AS admin_user,
+                       MIN(checks_users.res_users_id) AS any_user
+                  FROM account_return_check return_check
+                  JOIN account_return_check_res_users_rel checks_users
+                    ON return_check.id = checks_users.account_return_check_id
+                  JOIN res_groups_users_rel r
+                    ON r.uid = checks_users.res_users_id
+             LEFT JOIN ir_model_data admin
+                    ON admin.res_id = r.gid
+                   AND admin.model = 'res.groups'
+                   AND admin.module = 'account'
+                   AND admin.name = 'group_account_manager'
+                 WHERE return_check.bypassed = TRUE
+              GROUP BY return_check.id
+            )
+            UPDATE account_return_check return_check
+               SET approver_id = COALESCE(candidates.admin_user, candidates.any_user),
+                   supervisor_id = candidates.admin_user
+              FROM check_admin_candidates candidates
+             WHERE candidates.return_check_id = return_check.id
+        """
+    )
+    util.remove_field(cr, "account.return.check", "approver_ids")
--- migrations/l10n_be_reports/saas~18.5.1.1/pre-migrate.py
+++ migrations/l10n_be_reports/saas~18.5.1.1/pre-migrate.py
@@ -3,3 +3,17 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "need_ec_sales_list")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "ask_restitution")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "is_prorata_necessary")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "show_prorata")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "prorata")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "prorata_year")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "prorata_at_100")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "prorata_at_0")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "special_prorata_deduction")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "special_prorata_1")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "special_prorata_2")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "special_prorata_3")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "special_prorata_4")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "special_prorata_5")
+    util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "submit_more")
