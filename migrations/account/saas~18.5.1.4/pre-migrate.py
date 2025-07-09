83ad9775366b3ad8cfe80208b4acab97c8b418ac
unka-odoo <unka@odoo.com>
2025-07-09 12:39:19 +0530

    [IMP] account/saas~18.5: remove field
    part of changing default tax ids to default fiscal position for receipts.
    
    Related Community PR: https://github.com/odoo/odoo/pull/218116
    
    closes odoo/upgrade#8036
    
    Task: 4907662
    Related: odoo/odoo#218116
    Signed-off-by: Jinane Maksoud (maji) <maji@odoo.com>

--- migrations/account/saas~18.5.1.4/pre-migrate.py
+++ migrations/account/saas~18.5.1.4/pre-migrate.py
@@ -3,6 +3,8 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, model="res.partner", fieldname="duplicated_bank_account_partners_count")
+    util.remove_field(cr, model="res.company", fieldname="account_sale_receipt_tax_id")
+    util.remove_field(cr, model="res.company", fieldname="account_purchase_receipt_tax_id")
 
     util.create_column(cr, "account_move_line", "no_followup", "bool")
     util.explode_execute(
