1ef630c4b0effe0d12a6cb14ee7d6423ba8cd139
Abdelrahman Mahmoud (amah) <amah@odoo.com>
2025-07-09 12:52:50 +0200

    [ADD] resource/saas~18.5: Fill duration_hours
    see odoo/odoo#217895
    
    -  As `duration_hours` became stored, filling it to avoid orm overhead
    
    closes odoo/upgrade#8030
    
    Task: 4313444
    Related: odoo/enterprise#92500
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ migrations/resource/saas~18.5.1.1/pre-migrate.py
@@ -0,0 +1,19 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    util.create_column(cr, "resource_calendar_attendance", "duration_hours", "float8")
+    query = """
+        UPDATE resource_calendar_attendance
+           SET duration_hours =
+              CASE
+                WHEN day_period != 'lunch' THEN hour_to - hour_from
+                ELSE 0
+              END
+         WHERE day_period != 'lunch'
+    """
+    util.explode_execute(cr, query, table="resource_calendar_attendance")
+
+    util.remove_field(cr, 'resource.calendar.attendance', 'date_from')
+    util.remove_field(cr, 'resource.calendar.attendance', 'date_to')
+    util.remove_field(cr, 'resource.calendar.attendance', 'resource_id')
