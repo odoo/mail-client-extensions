d3490863bb035d57900c2382011b09aa09702bad
Jinane Maksoud <maji@odoo.com>
2025-07-11 18:38:44 +0200

    [FIX] im_livechat/saas~18.4: add demo history xmlids
    The member history for demo data is now explicitly written
    with its own xmlid, and with the new unique constraints on
    the member history model, loading the demo xml files will
    cause a duplicate entry per channel-partner or channel-guest.
    This fix maps the existing demo history to the new xmlids.
    
    https://github.com/odoo/odoo/pull/215228/
    
    closes odoo/upgrade#8057
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/im_livechat/saas~18.4.1.0/pre-migrate-demo.py
@@ -0,0 +1,97 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    if not util.table_exists(cr, "im_livechat_channel_member_history"):
+        return
+    demo_history_guest = (
+        [
+            (
+                f"im_livechat.livechat_channel_chatbot_session_{i}_history_guest_demo",
+                f"im_livechat.livechat_channel_chatbot_session_{i}_demo",
+                f"im_livechat.livechat_channel_chatbot_session_{i}_guest_demo",
+            )
+            for i in (1, 2, 3)
+        ]
+        + [
+            (
+                f"im_livechat.livechat_channel_session_{i}_history_guest",
+                f"im_livechat.livechat_channel_session_{i}",
+                f"im_livechat.livechat_channel_session_{i}_guest",
+            )
+            for i in (1, 2)
+        ]
+        + [
+            (
+                f"im_livechat.livechat_channel_session_{i}_history_member_guest",
+                f"im_livechat.livechat_channel_session_{i}",
+                f"im_livechat.livechat_channel_session_{i}_guest",
+            )
+            for i in (5, 6, 8, 9, 10, 11, 12, 13, 14)
+        ]
+        + [
+            (
+                f"im_livechat.support_bot_session_{i}_history_member_guest_demo",
+                f"im_livechat.support_bot_session_{i}_demo",
+                f"im_livechat.support_bot_session_{i}_guest_demo",
+            )
+            for i in range(1, 8)
+        ]
+    )
+
+    demo_history_partner = (
+        [
+            (
+                f"im_livechat.livechat_channel_session_{i}_history_member_admin",
+                f"im_livechat.livechat_channel_session_{i}",
+                "base.partner_admin",
+            )
+            for i in (1, 3, 5, 6, 7, 9, 10, 11, 12, 13, 14)
+        ]
+        + [
+            (
+                f"im_livechat.livechat_channel_session_{i}_history_member_demo",
+                f"im_livechat.livechat_channel_session_{i}",
+                "base.partner_demo",
+            )
+            for i in (2, 4, 8, 14)
+        ]
+        + [
+            (
+                f"im_livechat.livechat_channel_session_{i}_history_member_portal",
+                f"im_livechat.livechat_channel_session_{i}",
+                "base.partner_demo_portal",
+            )
+            for i in (3, 4, 7)
+        ]
+        + [
+            (
+                f"im_livechat.support_bot_session_{i}_member_history_admin",
+                f"im_livechat.support_bot_session_{i}_demo",
+                "base.partner_admin",
+            )
+            for i in (1, 4, 6)
+        ]
+    )
+
+    for history_xid, channel_xid, guest_xid in demo_history_guest:
+        channel_id = util.ref(cr, channel_xid)
+        guest_id = util.ref(cr, guest_xid)
+        if channel_id and guest_id:
+            util.ensure_xmlid_match_record(
+                cr,
+                history_xid,
+                "im_livechat.channel.member.history",
+                {"channel_id": channel_id, "guest_id": guest_id},
+            )
+
+    for history_xid, channel_xid, partner_xid in demo_history_partner:
+        channel_id = util.ref(cr, channel_xid)
+        partner_id = util.ref(cr, partner_xid)
+        if channel_id and partner_id:
+            util.ensure_xmlid_match_record(
+                cr,
+                history_xid,
+                "im_livechat.channel.member.history",
+                {"channel_id": channel_id, "partner_id": partner_id},
+            )
