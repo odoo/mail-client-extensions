49eefd6ec4be5e25ff5d621dea0c590f4ae5c18a
Alvaro Fuentes <afu@odoo.com>
2025-07-15 13:21:51 +0200

    [FIX] base/saas~18.5: fix version folder
    Oversight of https://github.com/odoo/upgrade/pull/7636
    It was correct in 18.4 but the rebase on 18.5 came wrong.
    
    closes odoo/upgrade#8071
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/base/saas~18.5.1.0/pre-10-modules.py
+++
@@ -1,6 +0,0 @@
-from odoo.upgrade import util
-
-
-def migrate(cr, version):
-    if util.modules_installed(cr, "account_budget"):
-        util.force_install_module(cr, "account_budget_purchase")
--- migrations/base/saas~18.5.1.3/pre-10-modules.py
+++ migrations/base/saas~18.5.1.3/pre-10-modules.py
@@ -2,4 +2,6 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    if util.modules_installed(cr, "account_budget"):
+        util.force_install_module(cr, "account_budget_purchase")
     util.merge_module(cr, "l10n_be_reports_prorata", "l10n_be_reports")
