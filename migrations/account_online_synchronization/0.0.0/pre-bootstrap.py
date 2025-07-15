5c3def2df52bcad8e78203029a829fca2ac5c9f5
Christophe Simonis <chs@odoo.com>
2025-07-15 10:25:10 +0200

    [FIX] base/saas~16.4,account_online_synchronization/0.0.0
    Since saas~16.4, `sale_subscription` depends on `account_accountant`[^1].
    At installation, a bunch of other modules are auto-installed, including
    `account_online_synchronization`.
    Bootstrap column on `account_bank_statement_line`.
    
    [^1]: odoo/enterprise@f622d837119aba2c5666c9a315a93e8616840f03

--- migrations/account_online_synchronization/0.0.0/pre-bootstrap.py
+++ migrations/account_online_synchronization/0.0.0/pre-bootstrap.py
@@ -1,4 +1,5 @@
-from odoo.upgrade import util
+# -*- coding: utf-8 -*-
+from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
--- migrations/base/saas~16.4.1.3/pre-10-modules.py
+++ migrations/base/saas~16.4.1.3/pre-10-modules.py
@@ -26,3 +26,6 @@ def migrate(cr, version):
     # https://github.com/odoo/enterprise/pull/52580
     if util.module_installed(cr, "l10n_mx_edi_stock"):
         util.force_upgrade_of_fresh_module(cr, "l10n_mx_edi_stock_30")
+
+    if util.has_enterprise():
+        util.force_upgrade_of_fresh_module(cr, "account_online_synchronization")
