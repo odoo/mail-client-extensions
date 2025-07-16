02d208852de9c22c6e370c321adc321a6642d690
Sébastien Theys <seb@odoo.com>
2025-07-16 15:32:40 +0200

    [IMP] mail/saas~18.5: remove starred_message_ids
    The field was removed. The pivot table and the inverse are still there.
    
    https://github.com/odoo/odoo/pull/219282
    
    closes odoo/upgrade#8078
    
    Related: odoo/odoo#219282
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/mail/saas~18.5.1.19/pre-migrate.py
+++ migrations/mail/saas~18.5.1.19/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "res.users.settings", "mute_until_dt")
     util.remove_record(cr, "mail.ir_cron_discuss_users_settings_unmute")
+    util.remove_field(cr, "res.partner", "starred_message_ids")
