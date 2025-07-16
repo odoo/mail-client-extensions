4aeeb46056ddce93495919152497cca20d6a8ad8
Alessandro Lupo <alup@odoo.com>
2025-07-16 09:41:25 +0200

    [FIX] website/saas~18.5: use `html_builder` in shapeId
    **Problem**
    Some snippets contain shapes whose ID starts with `web_editor` instead of
    `html_builder`. These IDs are not listed in imageShapeDefinitions [1],
    and this generates a traceback when trying to apply shape tranformations.
    
    **How to reproduce**
    1. Insert the snippet `s_images_constellation`
    2. Click on the bottom left image having the "Double Pill" shape
    3. Click on any "Transform" button under the "Shape" row in the "Image" section
    4. Problem: traceback pops up
    
    **Solution**
    This commit introduces a migration script to properly update existing websites
    by replacing `web_editor` with `html_builder`. See also [2].
    
    [1] addons/html_builder/static/src/plugins/image/image_shapes_definition.js
    [2] https://github.com/odoo/odoo/pull/218837
    
    task-4367641
    
    closes odoo/upgrade#8076
    
    Related: odoo/odoo#218837
    Related: odoo/enterprise#90482
    Related: odoo/design-themes#1117
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/website/saas~18.5.1.0/pre-migrate.py
+++ migrations/website/saas~18.5.1.0/pre-migrate.py
@@ -1,4 +1,12 @@
 from odoo.upgrade import util
+from odoo.upgrade.util import snippets
+
+
+def fix_data_shape(el):
+    if el.attrib["data-shape"].startswith("web_editor/"):
+        el.attrib["data-shape"] = el.attrib["data-shape"].replace("web_editor/", "html_builder/", 1)
+        return True
+    return False
 
 
 def migrate(cr, version):
@@ -6,3 +14,9 @@ def migrate(cr, version):
     util.remove_view(cr, "website.new_page_template_gallery_0_s_text_block_h2")
     util.remove_view(cr, "website.new_page_template_gallery_1_s_text_block_h2")
     util.remove_view(cr, "website.new_page_template_gallery_3_s_text_block_h2")
+
+    snippets.convert_html_content(
+        cr,
+        snippets.html_converter(fix_data_shape, selector="//img[@data-shape]"),
+        where_column=r"~ '\ydata-shape=\"'",
+    )
