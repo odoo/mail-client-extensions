84ee25ba43ad4fb716c12ab77c2a7fe0d9830a3d
Jurgen Gjini <jugj@odoo.com>
2025-07-17 16:24:44 +0200

    [IMP] hr_payroll_account/saas~16.5: Add analytic distribution
    In this PR we switch from analytic_account_id to an analytic distribution
    on hr.version and salary rules
    
    Related enterprise PR : odoo/enterprise#90439
    
    task-4775857
    
    closes odoo/upgrade#8087
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/hr_payroll_account/saas~18.5.1.0/pre-migrate.py
+++ migrations/hr_payroll_account/saas~18.5.1.0/pre-migrate.py
@@ -3,3 +3,31 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "hr_payroll_account.hr_payslip_run_view_form")
+
+    util.create_column(cr, "hr_version", "analytic_distribution", "jsonb")
+    util.create_column(cr, "hr_salary_rule", "analytic_distribution", "jsonb")
+
+    util.explode_execute(
+        cr,
+        """
+        UPDATE hr_version hv
+           SET analytic_distribution = jsonb_build_object(hv.analytic_account_id::text, 100.0)
+         WHERE hv.analytic_account_id IS NOT NULL
+        """,
+        table="hr_version",
+        alias="hv",
+    )
+
+    util.explode_execute(
+        cr,
+        """
+        UPDATE hr_salary_rule hsr
+           SET analytic_distribution = jsonb_build_object(hsr.analytic_account_id::text, 100.0)
+         WHERE hsr.analytic_account_id IS NOT NULL
+        """,
+        table="hr_salary_rule",
+        alias="hsr",
+    )
+
+    util.remove_field(cr, "hr.version", "analytic_account_id")
+    util.remove_field(cr, "hr.salary.rule", "analytic_account_id")
