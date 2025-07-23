ae02732fa81ead2218f21b7650fee5df28d43528
kais-odoo <kais@odoo.com>
2025-07-23 08:05:36 +0530

    [IMP] l10n_in_hr_payroll/saas~18.5: remove unused fields and rules
    In this commit, remove fields which will replace with new payroll flow.
    - bonus ---> performance bonus
    - voluntary provident fund ---> pf percentage for employee and employer
    
    Enterprise PR: https://github.com/odoo/enterprise/pull/90795
    
    task-4922359
    
    closes odoo/upgrade#8111
    
    Related: odoo/enterprise#90795
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/l10n_in_hr_payroll/saas~18.5.1.0/pre-migrate.py
+++ migrations/l10n_in_hr_payroll/saas~18.5.1.0/pre-migrate.py
@@ -1,4 +1,5 @@
 from odoo.upgrade import util
+from odoo.upgrade.util.hr_payroll import remove_salary_rule
 
 
 def migrate(cr, version):
@@ -9,3 +10,15 @@ def migrate(cr, version):
         util.remove_record(cr, xmlid)
     else:
         util.delete_unused(cr, xmlid)
+
+    util.make_field_non_stored(cr, "hr.version", "l10n_in_provident_fund", selectable=True)
+    util.remove_field(cr, "hr.version", "l10n_in_voluntary_provident_fund")
+    util.remove_field(cr, "hr.version", "l10n_in_driver_salay")
+    util.rename_field(cr, "hr.version", "l10n_in_house_rent_allowance_metro_nonmetro", "l10n_in_hra_percentage")
+    util.rename_field(cr, "hr.version", "l10n_in_supplementary_allowance", "l10n_in_fixed_allowance")
+    util.rename_field(cr, "hr.version", "l10n_in_esic_amount", "l10n_in_esic_employee_amount")
+    remove_salary_rule(cr, "l10n_in_hr_payroll.l10n_in_hr_salary_rule_bonus")
+    remove_salary_rule(cr, "l10n_in_hr_payroll.hr_payslip_rule_vpf")
+    remove_salary_rule(cr, "l10n_in_hr_payroll.hr_salary_rule_driver")
+    remove_salary_rule(cr, "l10n_in_hr_payroll.l10n_in_hr_salary_rule_leave")
+    util.remove_field(cr, "hr.version", "l10n_in_leave_allowance")
