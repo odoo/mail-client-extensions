a8b6e1a831f1e2bcf464ea04e0f38d8fd45f1b93
Maryam Kia <maki@odoo.com>
2025-07-24 10:48:34 +0200

    [IMP] im_livechat/saas~18.5: remove anonymous_name field
    Over time, guest or user names have replaced the need for the anonymous_name
    field. Therefore, we can remove this field from `discuss.channel`.
    
    task-4675719
    
    closes odoo/upgrade#8114
    
    Community: https://github.com/odoo/odoo/pull/210785
    Related: odoo/odoo#210785
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- migrations/im_livechat/saas~18.5.1.0/pre-migrate.py
+++ migrations/im_livechat/saas~18.5.1.0/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "im_livechat.channel", "buffer_time")
+    util.remove_field(cr, "discuss.channel", "anonymous_name")
