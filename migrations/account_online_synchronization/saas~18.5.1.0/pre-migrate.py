9acb22449ade7a0c1de7703eb8384a3deebfb005
SaddemAmine <sana@odoo.com>
2025-07-30 21:57:45 +0200

    [IMP] account_online_synchronization/saas~18.5: migrate renewal_contact_email
    This field was moved from `account.journal` to `account.online.link`
    during odoo/enterprise#85772 which aims to streamline the KYC process
    for users.
    
    Task ID: 4487895
    
    closes odoo/upgrade#8141
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/account_online_synchronization/saas~18.5.1.0/pre-migrate.py
+++ migrations/account_online_synchronization/saas~18.5.1.0/pre-migrate.py
@@ -3,3 +3,13 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "account.online.link", "provider_data")
+
+    util.create_column(cr, "account_online_link", "renewal_contact_email", "varchar")
+    cr.execute("""
+        UPDATE account_online_link AS link
+           SET renewal_contact_email = journal.renewal_contact_email
+          FROM account_journal AS journal
+         WHERE journal.account_online_link_id = link.id
+           AND journal.renewal_contact_email IS NOT NULL
+    """)
+    util.remove_column(cr, "account_journal", "renewal_contact_email")
