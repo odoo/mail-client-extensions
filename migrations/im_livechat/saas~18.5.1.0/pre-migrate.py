9b1761334d945dbcb800ddf845b86fdba7ff7e5c
tsm-odoo <tsm@odoo.com>
2025-08-01 11:16:22 +0200

    [IMP] im_livechat/saas~18.5: introduce looking for help views
    The community counterpart of this commit introduces views that
    allow agents to easily find chats where help is requested. The
    search view allows to group by expertise/chat bot. However those
    two fields (`livechat_expertise_ids`/`livechat_bot_partner_ids`)
    are respectively related to a many field and a computed field thus
    should be stored to allow grouping.
    
    This commit creates the link tables and fill them to avoid costly
    computations.
    
    closes odoo/upgrade#8064
    
    Community: https://github.com/odoo/odoo/pull/218973.
    Related: odoo/odoo#218973
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- migrations/im_livechat/saas~18.5.1.0/pre-migrate.py
+++ migrations/im_livechat/saas~18.5.1.0/pre-migrate.py
@@ -4,3 +4,32 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "im_livechat.channel", "buffer_time")
     util.remove_field(cr, "discuss.channel", "anonymous_name")
+    util.create_m2m(
+        cr,
+        "im_livechat_channel_member_history_discuss_channel_bot_rel",
+        "discuss_channel",
+        "res_partner",
+    )
+    if util.table_exists(cr, "im_livechat_expertise"):
+        util.create_m2m(
+            cr,
+            "discuss_channel_im_livechat_expertise_rel",
+            "discuss_channel",
+            "im_livechat_expertise",
+        )
+    if util.table_exists(cr, "im_livechat_channel_member_history"):
+        populate_bot_rel_query = """
+          INSERT INTO im_livechat_channel_member_history_discuss_channel_bot_rel(discuss_channel_id, res_partner_id)
+               SELECT h.channel_id, h.partner_id
+                 FROM im_livechat_channel_member_history h
+                WHERE h.livechat_member_type = 'bot'
+        """
+        util.explode_execute(cr, populate_bot_rel_query, "im_livechat_channel_member_history", alias="h")
+        populate_expertise_rel_query = """
+          INSERT INTO discuss_channel_im_livechat_expertise_rel(discuss_channel_id, im_livechat_expertise_id)
+               SELECT h.channel_id, rel.im_livechat_expertise_id
+                 FROM im_livechat_channel_member_history_im_livechat_expertise_rel rel
+                 JOIN im_livechat_channel_member_history h
+                   ON h.id = rel.im_livechat_channel_member_history_id
+          """
+        util.explode_execute(cr, populate_expertise_rel_query, "im_livechat_channel_member_history", alias="h")
