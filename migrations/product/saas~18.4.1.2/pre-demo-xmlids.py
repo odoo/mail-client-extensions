0f0ff6e21e6d6c05822e15fed068c4a03dc06bdc
Jinane Maksoud <maji@odoo.com>
2025-08-01 13:58:59 +0200

    [FIX] product/saas~18.4: rename demo xmlids
    Some demo product templates were renamed using
    xmlid functions in demo files. Those functions
    are skipped in update mode and require an upgrade
    script.
    https://github.com/odoo/odoo/pull/221089
    
    closes odoo/upgrade#8151
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

---
+++ migrations/product/saas~18.4.1.2/pre-demo-xmlids.py
@@ -0,0 +1,78 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute("SELECT 1 FROM ir_module_module WHERE name = 'product' AND demo IS TRUE")
+    if not cr.rowcount:
+        return
+
+    eb = util.expand_braces
+    util.rename_xmlid(cr, *eb("product.{product_product_5_product_template,product_template_5}"))
+    util.rename_xmlid(cr, *eb("product.{product_product_8_product_template,product_template_8}"))
+    util.rename_xmlid(cr, *eb("product.{product_product_13_product_template,product_template_13}"))
+    util.rename_xmlid(cr, *eb("product.{consu_delivery_01_product_template,sofa_product_template}"))
+
+    # create 2 product variants
+    attribute_id = util.ref(cr, "product.fabric_attribute")
+    template_id = util.ref(cr, "product.sofa_product_template")
+    cr.execute(
+        """
+        WITH prod_att_value AS (
+             INSERT INTO product_attribute_value (attribute_id, name, active)
+             VALUES (%(att_id)s, '{"en_US": "Linen"}', TRUE),
+                    (%(att_id)s, '{"en_US": "Velvet"}', TRUE),
+                    (%(att_id)s, '{"en_US": "Leather"}', TRUE)
+             RETURNING id
+            ),
+            prod_temp_att_line AS (
+             INSERT INTO product_template_attribute_line (product_tmpl_id, attribute_id)
+             VALUES (%(template_id)s, %(att_id)s)
+             RETURNING id
+            )
+        INSERT INTO product_template_attribute_value (product_attribute_value_id, attribute_line_id,product_tmpl_id, attribute_id)
+        SELECT prod_att_value.id,
+               prod_temp_att_line.id,
+               %(template_id)s,
+               %(att_id)s
+          FROM prod_att_value,
+               prod_temp_att_line
+      ORDER BY prod_att_value.id,
+               prod_temp_att_line.id
+     RETURNING id
+        """,
+        {"att_id": attribute_id, "template_id": template_id},
+    )
+    attribute_values = [id for (id,) in cr.fetchall()]
+    util.ensure_xmlid_match_record(
+        cr,
+        "product.sofa_fabric_attribute_line",
+        "product.template.attribute.line",
+        {"product_tmpl_id": template_id, "attribute_id": attribute_id},
+    )
+    first_variant_id = util.ref(cr, "product.consu_delivery_01")
+    cols = util.get_columns(
+        cr,
+        "product_product",
+        ignore=(
+            "id",
+            "combination_indices",
+        ),
+    )
+    for xmlid in ("product.consu_delivery_01_velvet", "product.consu_delivery_01_leather"):
+        cr.execute(
+            util.format_query(
+                cr,
+                """
+                   INSERT INTO product_product({cols}, combination_indices)
+                   SELECT {p_cols}, %s
+                     FROM product_product p
+                    WHERE p.id = %s
+                RETURNING id
+                """,
+                cols=cols,
+                p_cols=cols.using(alias="p"),
+            ),
+            [attribute_values[1 if "velvet" in xmlid else 2], first_variant_id],
+        )
+        (new_id,) = cr.fetchone()
+        util.ensure_xmlid_match_record(cr, xmlid, "product.product", {"id": new_id})
