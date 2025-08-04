1069ee592c593ed4a01e401b79bfbad3de2e6ee4
Noe Antoine <nan@odoo.com>
2025-08-04 13:37:22 +0200

    [IMP] appointment/saas~18.5: merge single child menu and parent
    The reporting menu in the calendar app, (available only when
    appointment is installed), has a single child 'All Appointments'.
    
    In order to ease the access to reporting and user experience,
    we delete the child menu and link its action to the parent one.
    
    When pressing 'Reporting', it will go the the action previously
    linked to the 'All appointments' menu.
    
    COM PR: odoo/odoo#219677
    ENT PR: odoo/enterprise#90529
    
    Task-4929768
    
    closes odoo/upgrade#8160
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/appointment/saas~18.5.1.3/pre-migrate.py
+++ migrations/appointment/saas~18.5.1.3/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "appointment.invite", "has_identical_config")
+    util.remove_menus(cr, [util.ref(cr, "appointment.menu_schedule_report_all_events")])
