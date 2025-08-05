7480f35459944c61abb4ae23638d94271c0e9cab
Paweł Fertyk <pafe@odoo.com>
2025-08-05 11:59:05 +0200

    [IMP] purchase_stock/saas~18.5: remove unused Product Supplier field
    There is now a new field: `effective_vendor_id`. Its logic reflects the
    actual Odoo process of choosing the default value for the vendor, if
    none was provided. Because of this, the `product_supplier_id` is no
    longer necessary.
    
    closes odoo/upgrade#8171
    
    Task: 4643663
    Related: odoo/odoo#220067
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/purchase_stock/saas~18.5.1.2/pre-migrate.py
+++ migrations/purchase_stock/saas~18.5.1.2/pre-migrate.py
@@ -2,6 +2,8 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.remove_view(cr, "purchase_stock.stock_reorder_report_search_inherited_purchase_stock")
+    util.remove_field(cr, "stock.warehouse.orderpoint", "product_supplier_id")
     util.remove_view(cr, "purchase_stock.view_category_property_form")
     util.remove_view(cr, "purchase_stock.product_template_form_view")
     util.remove_field(cr, "product.template", "property_account_creditor_price_difference")
