0a520b63067369dab4816c0c5e9c91a745434aa6
higo-odoo <higo@odoo.com>
2025-08-07 09:49:53 +0000

    [IMP] purchase/saas~18.3: purchase order has done state removed
    Remove 'done' state from PO,
    Introduce 'locked' boolean field to indicate lock status.
    
    see
    https://github.com/odoo/odoo/pull/188374
    
    closes odoo/upgrade#8188
    
    Opw: 4967839
    Upg: 3056179
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/purchase/saas~18.3.1.2/pre-migrate.py
+++ migrations/purchase/saas~18.3.1.2/pre-migrate.py
@@ -7,3 +7,19 @@ def migrate(cr, version):
     util.remove_field(cr, "res.partner", "purchase_warn")
     util.remove_field(cr, "product.template", "purchase_line_warn")
     util.remove_view(cr, "purchase.purchase_partner_kanban_view")
+    util.remove_record(cr, "purchase.mt_rfq_done")
+    util.create_column(cr, "purchase_order", "locked", "boolean")
+    util.explode_execute(
+        cr,
+        """
+            UPDATE purchase_order o
+               SET locked = true
+              FROM res_company c
+             WHERE c.id = o.company_id
+               AND c.po_lock = 'lock'
+               AND o.state = 'done'
+        """,
+        alias="o",
+        table="purchase_order",
+    )
+    util.change_field_selection_values(cr, "purchase.order", "state", {"done": "purchase"})
