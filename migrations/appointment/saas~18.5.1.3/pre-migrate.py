e60c347c6be7900c9fc5f3b8f7d835ce81d5d7c0
Noe Antoine <nan@odoo.com>
2025-08-11 15:49:08 +0200

    [IMP] appointment/saas~18.5: remove bad configured slots, view and menu
    We add a constraint on the appointment.slot model to ensure
    start_datetime <= end_datetime as no check was done before.
    
    This only concerns 'unique' slots, as 'recurring' ones do not
    use those fields at all.
    
    We can remove them altogether because the slots are only linked
    to the appointment type, and used to generate slots in the
    booking flow. Removing those will not have any real negative
    impact as the verification checks on the slot would fail when
    trying to book them in the first place.
    
    Also remove a submenu and a linked view that are not needed and
    are replaced with a 'flexible' filter on the main appointment.type
    views that the user can use in order to only see its 'custom'
    appointments.
    
    ENT PR : odoo/enterprise#91824
    
    Task-4997980
    
    closes odoo/upgrade#8206
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/appointment/saas~18.5.1.3/pre-migrate.py
+++ migrations/appointment/saas~18.5.1.3/pre-migrate.py
@@ -4,3 +4,15 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_field(cr, "appointment.invite", "has_identical_config")
     util.remove_menus(cr, [util.ref(cr, "appointment.menu_schedule_report_all_events")])
+
+    util.remove_menus(cr, [util.ref(cr, "appointment.menu_appointment_type_custom")])
+    util.remove_record(cr, "appointment.appointment_type_action_custom")
+    util.remove_record(cr, "appointment.appointment_type_view_tree_invitation")
+
+    cr.execute("""
+        DELETE FROM appointment_slot
+              WHERE slot_type = 'unique'
+                AND ((start_datetime IS NULL OR end_datetime IS NULL) OR
+                     (start_datetime > end_datetime)
+                    )
+    """)
