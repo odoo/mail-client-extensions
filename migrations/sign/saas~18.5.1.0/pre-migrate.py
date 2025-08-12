ed0f3e47d164048e540bf1231b3446e35dd9dc01
FedericoBraidi <brfe@odoo.com>
2025-08-12 08:30:51 +0200

    [ADD] sign/saas~18.5.1.0: change default_{width, height} storing
    Since PR odoo/enterprise#89156 on enterprise gets rid
    of the default_{width, height} fields on the form view
    of sign.item.type and substitutes them with a
    new field field_size, here we set the old fields as non stored.
    Also, we set a default value for the new column.
    
    closes odoo/upgrade#7993
    
    Task: 4908814
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/sign/saas~18.5.1.0/pre-migrate.py
+++ migrations/sign/saas~18.5.1.0/pre-migrate.py
@@ -3,3 +3,19 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_column(cr, "sign_send_request", "signers_count")
+    util.create_column(cr, "sign_item_type", "field_size", "varchar", default="regular_text")
+
+    cr.execute(
+        """
+        UPDATE sign_item_type
+            SET field_size = CASE
+                WHEN default_width <= 0.14 THEN 'short_text'
+                WHEN default_width >= 0.24 THEN 'long_text'
+                ELSE 'regular_text'
+            END
+        WHERE item_type = 'text'
+        """
+    )
+
+    util.make_field_non_stored(cr, "sign.item.type", "default_width", selectable=False)
+    util.make_field_non_stored(cr, "sign.item.type", "default_height", selectable=False)
