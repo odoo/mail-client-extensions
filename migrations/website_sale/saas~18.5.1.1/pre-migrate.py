d46bd7a83bdf37b2a94fe0e4780ec0f2458117ec
stefanorigano (SRI) <sri@odoo.com>
2025-08-15 22:17:58 +0200

    [IMP] website_sale/saas~18.5: revamp ecommerce product page
    - Remove carousel's image-ratio templates. These were enabled by editor
      options to adjust carousel's ratio classes via xpath.
      The new system uses a selection field stored in the DB instead,
      allowing to easily reuse this information in other places.
    
    - Remove `website_sale.variants_separator`, that's not necessary
      anymore. The design provided by this templates is now shared by many
      other elements, and it's now controlled by a global editor option.
    
    - Remove `website_sale.shop_fullwidth` template: the options state is
      now tracked by a specific field and doesn't require a "flag" template
      anymore
    
    com: https://github.com/odoo/odoo/pull/222320/
    ent: https://github.com/odoo/enterprise/pull/92119
    
    task-4707065
    
    closes odoo/upgrade#8229
    
    Related: odoo/odoo#222320
    Related: odoo/enterprise#92119
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- migrations/website_sale/saas~18.5.1.1/pre-migrate.py
+++ migrations/website_sale/saas~18.5.1.1/pre-migrate.py
@@ -4,6 +4,8 @@ from odoo.upgrade import util
 def migrate(cr, version):
     env = util.env(cr)
     util.create_column(cr, "website", "shop_opt_products_design_classes", "varchar")
+    util.create_column(cr, "website", "product_page_image_ratio", "varchar")
+
     views_mapping = {
         "thumb_2_3": ["thumb_2_3"],
         "thumb_4_5": ["thumb_4_5"],
@@ -15,6 +17,14 @@ def migrate(cr, version):
         "add_to_cart": ["has_cta"],
         "description": ["has_description"],
     }
+
+    product_page_carousel_ratio_mapping = {
+        "products_carousel_4x3": "4_3",
+        "products_carousel_4x5": "4_5",
+        "products_carousel_16x9": "16_9",
+        "products_carousel_21x9": "21_9",
+    }
+
     for website in env["website"].search([]):
         # Default classes:
         product_design_classes = ["actions_onhover", "img_secondary_show"]
@@ -29,9 +39,18 @@ def migrate(cr, version):
                 product_design_classes.extend(classnames)
 
         classes = " ".join(f"o_wsale_products_opt_{o}" for o in product_design_classes)
+
+        # Product Page - Carousel image ratio
+        image_ratio = "1_1"  # Default value
+        for template_name, ratio_value in product_page_carousel_ratio_mapping.items():
+            if website.is_view_active(f"website_sale.{template_name}"):
+                image_ratio = ratio_value
+                break
+
+        # Update both product design classes and aspect-ratio columns
         cr.execute(
-            "UPDATE website SET shop_opt_products_design_classes = %s WHERE id = %s",
-            [classes, website.id],
+            "UPDATE website SET shop_opt_products_design_classes = %s, product_page_image_ratio = %s WHERE id = %s",
+            [classes, image_ratio, website.id],
         )
 
     views_to_remove = [
@@ -54,4 +73,9 @@ def migrate(cr, version):
     for view in views_to_remove:
         util.remove_view(cr, f"website_sale.dynamic_filter_template_product_product_{view}")
 
+    for template_name in product_page_carousel_ratio_mapping:
+        util.remove_view(cr, f"website_sale.{template_name}")
+
     util.remove_view(cr, "website_sale.snippets_options_web_editor")
+    util.remove_view(cr, "website_sale.variants_separator")
+    util.remove_view(cr, "website_sale.shop_fullwidth")
