0ea12366d64a536fc73d15492fd836a81964660b
Yannick Tivisse <yti@odoo.com>
2025-08-19 12:52:56 +0200

    [IMP] hr/saas~18.5: Remove menus and action
    ENT PR: https://github.com/odoo/odoo/pull/222659
    
    closes odoo/upgrade#8245
    
    Taskid: 4984195
    Related: odoo/odoo#222659
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/hr/saas~18.5.1.1/pre-migrate.py
+++ migrations/hr/saas~18.5.1.1/pre-migrate.py
@@ -3,3 +3,13 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, "res.config.settings", "module_hr_contract")
+
+    util.remove_menus(
+        cr,
+        [
+            util.ref(cr, "hr.hr_menu_hr_task"),
+            util.ref(cr, "hr.hr_menu_hr_my_activities"),
+            util.ref(cr, "hr.hr_menu_hr_all_activities"),
+        ],
+    )
+    util.remove_record(cr, "hr.action_hr_employee_my_activities")
