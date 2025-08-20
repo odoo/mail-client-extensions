bb74c182fc6ebb724a332a83cf338da69bcef9ca
MostafaTwfiq <ibmo@odoo.com>
2025-08-20 18:54:44 +0200

    [IMP] sign/saas~18.5.1.0: populate request validity on sign template
    Populate signature_request_validity field in sign.template with default value 60
    
    See odoo/enterprise#90515
    
    closes odoo/upgrade#8254
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/sign/saas~18.5.1.0/pre-migrate.py
+++ migrations/sign/saas~18.5.1.0/pre-migrate.py
@@ -4,6 +4,7 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_column(cr, "sign_send_request", "signers_count")
     util.create_column(cr, "sign_item_type", "field_size", "varchar", default="regular_text")
+    util.create_column(cr, "sign_template", "signature_request_validity", "int", default=60)
 
     cr.execute(
         """
