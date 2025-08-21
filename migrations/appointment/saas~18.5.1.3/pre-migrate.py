aaab7a6325bd6fd62d43ea5895cca4217d313f1c
Renaud Thiry <reth@odoo.com>
2025-08-21 11:09:58 +0200

    [IMP] appointment/saas~18.5.1.3: rename calendar view
    The calendar view is moved and made "primary" to be able to open a
    different form view from the one of base calendar.
    
    "_inherited_appointment" is not helpful anymore
    
    odoo/odoo#222741
    odoo/enterprise#92263
    
    task-4909129
    
    Part-of: odoo/upgrade#8262
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/appointment/saas~18.5.1.3/pre-migrate.py
+++ migrations/appointment/saas~18.5.1.3/pre-migrate.py
@@ -2,6 +2,10 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
+    util.rename_xmlid(
+        cr, "appointment.calendar_event_calendar_view_inherit_appointment", "appointment.calendar_event_view_calendar"
+    )
+
     util.remove_field(cr, "appointment.invite", "has_identical_config")
     util.remove_menus(cr, [util.ref(cr, "appointment.menu_schedule_report_all_events")])
 
