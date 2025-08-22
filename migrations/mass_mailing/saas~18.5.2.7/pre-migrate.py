10323dab61498ce082adfb98b0f00f6e38a3d4cd
abd-msyukyu-odoo <abd@odoo.com>
2025-08-22 11:24:32 +0200

    [IMP] mass_mailing/saas~18.5: remove snippet_options for mass_mailing
    `mass_mailing` now uses the `html_builder` and snippets options are defined as
    OWL templates. The old views are therefore removed. 2 obsolete snippets are also
    removed.
    
    See odoo/odoo#215364
    See odoo/enterprise#90405
    
    task-4247642
    
    closes odoo/upgrade#8274
    
    Signed-off-by: Damien Abeloos (abd) <abd@odoo.com>
    Co-authored-by: Damien Abeloos <abd@odoo.com>
    Co-authored-by: Thomas Josse <thjo@odoo.com>

--- migrations/mass_mailing/saas~18.5.2.7/pre-migrate.py
+++ migrations/mass_mailing/saas~18.5.2.7/pre-migrate.py
@@ -3,3 +3,9 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.create_column(cr, "mailing_mailing", "use_exclusion_list", "bool", default=True)
+    util.remove_view(cr, "mass_mailing.snippet_options")
+    util.remove_view(cr, "mass_mailing.snippet_options_background_options")
+    util.remove_view(cr, "mass_mailing.snippet_options_border_widgets")
+    util.remove_view(cr, "mass_mailing.snippet_options_border_line_widgets")
+    util.remove_view(cr, "mass_mailing.s_features")
+    util.remove_view(cr, "mass_mailing.s_mail_block_discount1")
