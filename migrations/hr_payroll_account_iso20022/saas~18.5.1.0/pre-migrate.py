31152d52de1720f7ae3445d92963b1cffb1693a7
3amo-magdy <ammg@odoo.com>
2025-08-22 13:06:01 +0200

    [IMP] hr_payroll_account_iso20022/saas~18.5: moving to standard
    Moved the trusted toggle button to the standard form view of employee.
    
    See https://github.com/odoo/odoo/pull/222226
    See https://github.com/odoo/enterprise/pull/91934
    Task-4908556
    
    Part-of: odoo/upgrade#8272
    Related: odoo/odoo#222226
    Related: odoo/enterprise#91934
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/hr_payroll_account_iso20022/saas~18.5.1.0/pre-migrate.py
+++ migrations/hr_payroll_account_iso20022/saas~18.5.1.0/pre-migrate.py
@@ -3,3 +3,4 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "hr_payroll_account_iso20022.hr_payslip_run_view_form")
+    util.remove_view(cr, "hr_payroll_account_iso20022.view_employee_form")
