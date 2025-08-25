dcab63b695505e8ac092b1c9751da3dcdc502b67
Arthur Pierrot (arpi) <arpi@odoo.com>
2025-08-25 10:57:48 +0200

    [FIX] l10n_mx_hr_payroll/saas~18.5: fix ISR payslip overlaps 2 months
    Bug:
    When a payslip is overlapping two months, the ISR can be negative and is not
    correctly computed.
    
    Cause:
    There is an adjustment made at the end of each month and each year. This works
    only if the payslip ends on the last day of the month.
    
    Fix:
    We remove the adjustment and add an input for adjusting the ISR manually.
    
    Task: 4790249
    
    See odoo/enterprise#92813
    
    closes odoo/upgrade#8284
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/l10n_mx_hr_payroll/saas~18.5.1.0/pre-migrate.py
+++ migrations/l10n_mx_hr_payroll/saas~18.5.1.0/pre-migrate.py
@@ -1,4 +1,5 @@
 from odoo.upgrade import util
+from odoo.upgrade.util.hr_payroll import remove_salary_rule
 
 
 def migrate(cr, version):
@@ -9,3 +10,7 @@ def migrate(cr, version):
     util.remove_field(cr, "hr.payroll.structure", "country_code")
     util.remove_view(cr, "l10n_mx_hr_payroll.hr_payroll_structure_type_view_form")
     util.remove_view(cr, "l10n_mx_hr_payroll.hr_payroll_structure_view_form")
+
+    util.remove_field(cr, "hr.version", "l10n_mx_external_annual_declaration")
+    util.remove_field(cr, "hr.employee", "l10n_mx_external_annual_declaration")
+    remove_salary_rule(cr, "l10n_mx_hr_payroll.l10n_mx_employees_salary_subsidy_adjustment")
