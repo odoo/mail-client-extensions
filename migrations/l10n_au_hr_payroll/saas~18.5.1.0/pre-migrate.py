bdaeda64b673116ab3a443db812bb4968dc934b6
mhch-odoo <mhch@odoo.com>
2025-08-25 11:01:12 +0800

    [IMP] l10n_au_hr_payroll/saas~18.5: Allowance adaptations
    Allow spcial computation for allowance with details on
    payslip instead of creating all possible input types.
    See odoo/enterprise#93031
    
    Task 4085719
    
    closes odoo/upgrade#8282
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/l10n_au_hr_payroll/saas~18.5.1.0/pre-migrate.py
+++ migrations/l10n_au_hr_payroll/saas~18.5.1.0/pre-migrate.py
@@ -6,3 +6,40 @@ from odoo.upgrade import util
 def migrate(cr, version):
     sql.rename_column(cr, "l10n_au_super_fund", "display_name", "name")
     util.make_field_non_stored(cr, "l10n_au.super.fund", "display_name", selectable=True)
+    util.remove_field(cr, "hr.payslip.input.type", "l10n_au_ato_rate_limit")
+
+    util.change_field_selection_values(cr, "hr.payslip.input.type", "l10n_au_paygw_treatment", {"special": "excess"})
+    util.change_field_selection_values(cr, "hr.payslip.input.type", "l10n_au_payment_type", {"other": "lump_sum"})
+
+    util.rename_xmlid(
+        cr,
+        "l10n_au_hr_payroll.input_bonus_commissions_overtime_prior",
+        "l10n_au_hr_payroll.input_bonus_commissions_overtime",
+    )
+    # Input types merged
+    input_types_to_switch = {
+        util.ref(cr, "l10n_au_hr_payroll.input_bonus_commissions_prior"): util.ref(
+            cr, "l10n_au_hr_payroll.input_bonus_commissions"
+        ),
+        util.ref(cr, "l10n_au_hr_payroll.input_cents_per_kilometer_1"): util.ref(
+            cr, "l10n_au_hr_payroll.input_cents_per_kilometer_2"
+        ),
+        util.ref(cr, "l10n_au_hr_payroll.input_laundry_1"): util.ref(cr, "l10n_au_hr_payroll.input_laundry_2"),
+        util.ref(cr, "l10n_au_hr_payroll.input_overseas_travel_allowance_1"): util.ref(
+            cr, "l10n_au_hr_payroll.input_overseas_travel_allowance_2"
+        ),
+        util.ref(cr, "l10n_au_hr_payroll.input_overtime_meal_allowance_1"): util.ref(
+            cr, "l10n_au_hr_payroll.input_overtime_meal_allowance_2"
+        ),
+        util.ref(cr, "l10n_au_hr_payroll.input_domestic_travel_allowance_3"): util.ref(
+            cr, "l10n_au_hr_payroll.input_domestic_travel_allowance_1"
+        ),
+    }
+    util.replace_record_references_batch(cr, input_types_to_switch, "hr.payslip.input.type")
+
+    # Input types not be used
+    util.delete_unused(cr, "l10n_au_hr_payroll.l10n_au_lumpsum_e", deactivate=True)
+    util.delete_unused(cr, "l10n_au_hr_payroll.input_living_away_from_home_allowance", deactivate=True)
+    util.delete_unused(cr, "l10n_au_hr_payroll.input_voluntary_super_contribution", deactivate=True)
+    util.delete_unused(cr, "l10n_au_hr_payroll.input_salary_sacrifice_superannuation", deactivate=True)
+    util.delete_unused(cr, "l10n_au_hr_payroll.input_salary_sacrifice_other", deactivate=True)
--- migrations/l10n_au_hr_payroll_account/saas~18.5.1.0/post-migrate.py
+++ migrations/l10n_au_hr_payroll_account/saas~18.5.1.0/post-migrate.py
@@ -5,3 +5,18 @@ def migrate(cr, version):
     env = util.env(cr)
     if fund := env.ref("l10n_au_hr_payroll_account.super_fund_example_super", raise_if_not_found=False):
         fund.usi = "1309011309"
+
+    cr.execute(
+        """
+        INSERT INTO l10n_au_payslip_ytd_input (res_id, res_model, name, l10n_au_payslip_ytd_id)
+             SELECT type.id, 'hr.payslip.input.type', type.name, ytd.id
+               FROM hr_payslip_input_type type
+         CROSS JOIN l10n_au_payslip_ytd ytd
+               JOIN hr_salary_rule rule
+                 ON ytd.rule_id = rule.id
+              WHERE type.l10n_au_payment_type = 'allowance'
+                AND type.l10n_au_paygw_treatment = 'special'
+                AND rule.code IN ('ALW', 'ALW.TAXFREE')
+        ON CONFLICT DO NOTHING;
+        """
+    )
