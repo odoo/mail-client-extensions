5127d49d637f6efe1c4e7743e151ee286f0b40aa
romc-odoo <romc@odoo.com>
2025-08-26 08:59:52 +0200

    [IMP] l10n_be_hr_payroll/saas~18.5: holiday attest rework
    The related PR is a rework of the holiday attest wizard.
    Related PR: https://github.com/odoo/enterprise/pull/92529
    
    task-5011670
    
    closes odoo/upgrade#8291
    
    Related: odoo/enterprise#92529
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- migrations/l10n_be_hr_payroll/saas~18.5.1.0/pre-migrate.py
+++ migrations/l10n_be_hr_payroll/saas~18.5.1.0/pre-migrate.py
@@ -3,3 +3,11 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_view(cr, "l10n_be_hr_payroll.hr_payslip_run_view_form")
+    util.remove_field(cr, "hr.payslip.employee.depature.holiday.attests", "unpaid_average_remunaration_n1")
+    util.remove_field(cr, "hr.payslip.employee.depature.holiday.attests", "unpaid_average_remunaration_n")
+    util.remove_field(cr, "hr.payslip.employee.depature.holiday.attests", "unpaid_time_off_n1")
+    util.remove_field(cr, "hr.payslip.employee.depature.holiday.attests", "unpaid_time_off_n")
+    util.remove_field(cr, "hr.payslip.employee.depature.holiday.attests", "time_off_allocated")
+    util.remove_field(cr, "hr.payslip.employee.depature.holiday.attests", "time_off_taken")
+    util.remove_field(cr, "hr.payslip.employee.depature.holiday.attests", "time_off_allocation_n_ids")
+    util.remove_field(cr, "hr.payslip.employee.depature.holiday.attests", "time_off_n_ids")
