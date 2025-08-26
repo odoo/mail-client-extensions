8b1db9935d369c7bf1d93b845296f5dab9b9f9d1
Alvaro Fuentes <afu@odoo.com>
2025-08-26 09:11:55 +0000

    [FIX] account_accountant/saas~18.3: avoid reco trigger
    We do not want a reco model to auto trigger when we apply them. The main
    purpose of #7974 was to ensure the reco model was linked to the right
    records. As a side effect we got that the automatic reco models trigger
    posting or removal of data, this affects the balances of some accounts.
    That breaks the invariant check for account balances.
    
    To avoid the issue we temporarily remove the auto trigger from all reco
    models when applying them.
    
    closes odoo/upgrade#8293
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account_accountant/saas~18.3.1.1/end-migrate.py
+++ migrations/account_accountant/saas~18.3.1.1/end-migrate.py
@@ -1,4 +1,32 @@
+import contextlib
+
 from odoo.upgrade import util
+
+
+@contextlib.contextmanager
+def no_reco_trigger(env):
+    env.flush_all()
+    env.cr.execute(
+        """
+        UPDATE account_reconcile_model
+           SET trigger = 'manual'
+         WHERE trigger = 'auto_reconcile'
+     RETURNING id
+        """
+    )
+    ids = [r[0] for r in env.cr.fetchall()]
+    env.invalidate_all()
+    yield
+    if ids:
+        env.cr.execute(
+            """
+            UPDATE account_reconcile_model
+               SET trigger = 'auto_reconcile'
+             WHERE id = ANY(%s)
+            """,
+            [ids],
+        )
+        env.invalidate_all()
 
 
 def migrate(cr, version):
@@ -20,15 +48,18 @@ def migrate(cr, version):
         reco_model_per_company[company_id] = reco_models_ids
 
     env = util.env(cr)
-    RecModel = env["account.reconcile.model"]
-    StLineModel = env["account.bank.statement.line"]
-    for company in env["res.company"].search([]):
-        if not reco_model_per_company.get(company.id) or not unreconciled_statement_lines_per_company.get(company.id):
-            continue
-
-        for reco_model in util.iter_browse(RecModel, reco_model_per_company[company.id]):
-            for st_line in util.iter_browse(
-                StLineModel,
-                unreconciled_statement_lines_per_company[company.id],
+    with no_reco_trigger(env):
+        RecModel = env["account.reconcile.model"]
+        StLineModel = env["account.bank.statement.line"]
+        for company in env["res.company"].search([]):
+            if not reco_model_per_company.get(company.id) or not unreconciled_statement_lines_per_company.get(
+                company.id
             ):
-                reco_model._apply_reconcile_models(st_line)
+                continue
+
+            for reco_model in util.iter_browse(RecModel, reco_model_per_company[company.id]):
+                for st_line in util.iter_browse(
+                    StLineModel,
+                    unreconciled_statement_lines_per_company[company.id],
+                ):
+                    reco_model._apply_reconcile_models(st_line)
