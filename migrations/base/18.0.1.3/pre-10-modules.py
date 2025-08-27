14183604ccfd7b3e5760075d90d16ed857b84d00
Alvaro Fuentes <afu@odoo.com>
2025-08-27 15:56:57 +0200

    [FIX] l10n_sa_hr_payroll/18.0: create column
    Since odoo/enterprise#63782 `l10n_sa_hr_payroll` depends on
    `hr_work_entry_holidays` which depends on `calendar` indirectly. The
    latter adds `calendar_last_notif_ack` field in `res.partner`.

--- migrations/base/18.0.1.3/pre-10-modules.py
+++ migrations/base/18.0.1.3/pre-10-modules.py
@@ -5,3 +5,8 @@ def migrate(cr, version):
     util.merge_module(cr, "l10n_es_edi_tbai_multi_refund", "l10n_es_edi_tbai")
     util.merge_module(cr, "l10n_fr_reports_extended", "l10n_fr_reports")
     util.force_upgrade_of_fresh_module(cr, "account_bank_statement_extract")
+
+    if util.module_installed(cr, "l10n_sa_hr_payroll") and not util.module_installed(cr, "calendar"):
+        # l10n_sa_hr_payroll now depends on hr_work_entry_holidays that depends (multi-level)
+        # on calendar module, the latter adds a new column to res.partner
+        util.create_column(cr, "res_partner", "calendar_last_notif_ack", "timestamp", default="now")
