b02d80d3ba6767622f0417a2807daf880589ee4f
Khushi Vakil <khva@odoo.com>
2025-08-28 11:56:15 +0530

    [IMP] l10n_in_reports/saas~18.5: remove enet template model
    This commit removes the model and the field related to `enet.template`.
    
    closes odoo/upgrade#8311
    
    Related: odoo/enterprise#93380
    Signed-off-by: Jinane Maksoud (maji) <maji@odoo.com>

--- migrations/l10n_in_reports/saas~18.5.1.1/pre-migrate.py
+++ migrations/l10n_in_reports/saas~18.5.1.1/pre-migrate.py
@@ -20,3 +20,7 @@ def migrate(cr, version):
     util.remove_records(cr, "account.report.expression", report_expression_records)
 
     util.remove_view(cr, "l10n_in_reports.l10n_in_gst_return_period_form_view_document_summary")
+
+    util.force_noupdate(cr, "l10n_in_reports.hdfc_bank_selection", noupdate=False)
+    util.remove_field(cr, "account.journal", "enet_template_field_ids")
+    util.remove_model(cr, "enet.template")
