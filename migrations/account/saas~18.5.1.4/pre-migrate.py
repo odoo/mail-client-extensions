5560aacb58bdc7a337052ea4ad93bbbaadf8cc68
Alvaro Fuentes <afu@odoo.com>
2025-08-29 15:30:12 +0200

    [FIX] account/saas~18.5: compute field
    Compute `no_followup` for move lines.
    
    See odoo/odoo#203329
    
    closes odoo/upgrade#8325
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/account/saas~18.5.1.4/pre-migrate.py
+++ migrations/account/saas~18.5.1.4/pre-migrate.py
@@ -3,3 +3,18 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     util.remove_field(cr, model="res.partner", fieldname="duplicated_bank_account_partners_count")
+
+    util.create_column(cr, "account_move_line", "no_followup", "bool")
+    util.explode_execute(
+        cr,
+        """
+        UPDATE account_move_line l
+           SET no_followup = True
+          FROM account_move m
+         WHERE l.move_id = m.id
+           AND m.move_type = 'entry'
+           AND m.origin_payment_id IS NULL
+        """,
+        table="account_move_line",
+        alias="l",
+    )
