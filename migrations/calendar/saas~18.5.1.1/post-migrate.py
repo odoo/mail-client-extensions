825b768553f7a62774786b0db198660a274451e3
Laetitia (ldau) <ldau@odoo.com>
2025-09-01 17:20:17 +0200

    [IMP] calendar/saas~18.5: update template mail
    Following odoo/odoo#224435, the email templates for the calendar app
    have been updated to give them a more modern look.
    
    task-4985981
    
    closes odoo/upgrade#8341
    
    Related: odoo/enterprise#93291
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- migrations/calendar/saas~18.5.1.1/post-migrate.py
+++ migrations/calendar/saas~18.5.1.1/post-migrate.py
@@ -2,6 +2,5 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    util.if_unchanged(
-        cr, "calendar.calendar_template_meeting_update", util.update_record_from_xml, fields=["description"]
-    )
+    for template in ["meeting_invitation", "meeting_changedate", "meeting_reminder", "meeting_update", "delete_event"]:
+        util.if_unchanged(cr, f"calendar.calendar_template_{template}", util.update_record_from_xml)
