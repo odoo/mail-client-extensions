256eaedde0019837ad9b9c3eeb02b601807c7992
depr-odoo <depr@odoo.com>
2025-09-01 13:03:01 +0000

    [FIX] delivery/saas~16.3.1.0: Add price rules for free_over flag
    - Before v16.2, we have allowed free delivery feature can be applied for
    all delivery type
    
    - After this
    [commit](https://github.com/odoo/odoo/commit/d3cddbe89aee672322524ff735b9f1742d4ca8cc#diff-55753758768fd6dededba39388cd1c7b0571b3c857e750bdb4a43de5735e4f87R113),
    the free delivery functionality is no longer available for the
    ```base_on_rule``` delivery type.
    - As a result orders above the specified amount are not receiving free
    delivery after migration for ```base_on_rule``` delivery type with
    ```free_over```.
    
    - To address this, we've converted it into a delivery price rule with
    sequence 1,ensuring that it is applied first to check the free_over feature,
    as it was in earlier version.
    
    OPW-5045791
    UPG-3114236
    
    closes odoo/upgrade#8346
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

---
+++ migrations/delivery/saas~16.3.1.0/pre-migrate.py
@@ -0,0 +1,61 @@
+from odoo.upgrade import util
+
+
+def migrate(cr, version):
+    cr.execute(
+        """
+        WITH updated AS (
+            UPDATE delivery_price_rule
+               SET sequence = delivery_price_rule.sequence + 10
+              FROM delivery_carrier dc
+             WHERE delivery_price_rule.carrier_id = dc.id
+               AND dc.delivery_type = 'base_on_rule'
+               AND dc.free_over
+               AND dc.amount > 0.00
+         RETURNING dc.id, dc.name
+        )
+        SELECT DISTINCT ON (id) id, name->>'en_US' FROM updated
+        """
+    )
+    carrier = cr.fetchall()
+    if carrier:
+        cr.execute(
+            """
+            INSERT INTO delivery_price_rule
+                        (sequence, carrier_id, variable, operator,
+                        max_value, list_base_price, list_price, variable_factor)
+                 SELECT 1, id, 'price', '>=',
+                        amount, 0.0, 0.0, 'weight'
+                   FROM delivery_carrier
+                  WHERE id IN %s
+            """,
+            [tuple(id[0] for id in carrier)],
+        )
+        util.add_to_migration_reports(
+            category="Delivery",
+            message=(
+                """
+                <details>
+                    <summary>
+                        Some shipping methods using the "Based on Rules" provider also have
+                        the "Free delivery for orders above a specified amount" option set.
+                        Since Odoo 17 the free delivery option is ignored for
+                        shipping methods based on rules.
+                        To keep the same behavior, a new Pricing Rule has been
+                        automatically created that ensures free delivery functionality
+                        for eligible orders.
+                        Please take a moment to review the affected shipping methods listed below.
+                        If you notice any inconsistency, feel free to adjust the Pricing rules or
+                        free delivery settings in your database and submit a new upgrade request.
+                    </summary>
+                    <ul>{}</ul>
+                </details>
+                """.format(
+                    " ".join(
+                        f"<li>{util.get_anchor_link_to_record('delivery.carrier', id, name)}</li>"
+                        for id, name in carrier
+                    )
+                )
+            ),
+            format="html",
+        )
