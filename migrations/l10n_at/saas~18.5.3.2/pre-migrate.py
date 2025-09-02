f655aceb47104989ca40f6f3b6eefd01d00ffb6a
dvsh-odoo <dvsh@odoo.com>
2025-09-02 17:29:20 +0530

    [IMP] l10n_at/saas~18.5: update austria tax report UI
    Following https://github.com/odoo/odoo/pull/224604, the Austria Tax
    report has been updated to comply with U30 form.
    
    TaskID-5046151
    
    closes odoo/upgrade#8357
    
    Related: odoo/odoo#224604
    Signed-off-by: William André (wan) <wan@odoo.com>

--- migrations/l10n_at/saas~18.5.3.2/pre-migrate.py
+++ migrations/l10n_at/saas~18.5.3.2/pre-migrate.py
@@ -233,3 +233,34 @@ def migrate(cr, version):
             """,
             format="html",
         )
+
+    # Remove obsolete report lines to avoid duplicates/conflicts when updating XML
+    older_records = [
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_14_base",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_15_base",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_16_base",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_17_base",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_18_base",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_19_base",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_14_tax",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_15_tax",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_16_tax",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_17_tax",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_18_tax",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_19_tax",
+        "l10n_at.tax_report_line_at_base_title_umsatz_base_4_14_19",
+        "l10n_at.tax_report_line_at_tax_title_4_14_19",
+        "l10n_at.tax_report_line_at_base_title_umsatz_base_4_28_31",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_28_base",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_29_base",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_30_base",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_31_base",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_28_tax",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_29_tax",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_30_tax",
+        "l10n_at.tax_report_line_l10n_at_tva_line_4_31_tax",
+        "l10n_at.tax_report_line_at_tax_title_4_28_31",
+        "l10n_at.tax_report_line_l10n_at_tva_line_5_13_tag",
+    ]
+    for xid in older_records:
+        util.remove_record(cr, xid)
