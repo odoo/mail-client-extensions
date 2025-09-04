efe6b3597a610f81d903010899c1cc25be3c3021
rame-odoo <rame@odoo.com>
2025-09-04 12:46:30 +0530

    [FIX] hr_holidays/saas~18.3: prevent constraint error
    Issue:
    
    In Odoo 18, there is one record 'hr_holidays.leave_type_unpaid' is used by demo
    leaves 'l10n_in_alisha_half_time_off', which i created and then validated.
    
    Now in 18.3, a new field 'hide_on_dashboard' was added in 'leave_type_unpaid'
    record. For that the upgrade script updated the record with:
    'if_unchanged(cr, 'hr_holidays.leave_type_unpaid', util.update_record_from_xml)'
    Related task-4317935
    
    Now, In master, a functional change was made: 'date_from' and 'date_to' fields
    of leave became computed fields. That computation is related to 'request_unit'
    field which is in our 'leave_type_unpaid' record and during upgrade if_unchanged
    will try to update that field and trigger re-computation of related 'date_from'
    and 'date_to' fields of those leave which are already validated.
    
    The re-computation hit the '_check_date_state' constraint, raising an error
    because validated leaves are not allowed to change dates.
    Related task-4765062
    
    Fix:
    
    Updating the full record with 'if_unchanged' works correctly up to 18.4, since
    no computation is involved. But starting from master, it causes errors due to
    recomputation of fields.
    
    To avoid this, we now restrict 'if_unchanged' to update only the
    'hide_on_dashboard' field with:
    update_record_from_xml(..., fields=['hide_on_dashboard'])
    for all affected leave types.
    
    This way, master upgrades no longer touch sensitive fields like request_unit,
    avoiding unnecessary recomputation of computed fields and preventing constraint
    errors on validated leaves.
    
    build_error-231279
    
    closes odoo/upgrade#8373
    
    Signed-off-by: Jinane Maksoud (maji) <maji@odoo.com>

--- migrations/hr_holidays/saas~18.3.1.6/post-migrate.py
+++ migrations/hr_holidays/saas~18.3.1.6/post-migrate.py
@@ -14,7 +14,11 @@ def migrate(cr, version):
 
     util.if_unchanged(cr, "hr_holidays.l10n_lu_leave_type_situational_unemployment", util.update_record_from_xml)
 
-    util.if_unchanged(cr, "hr_holidays.leave_type_sick_time_off", util.update_record_from_xml)
-    util.if_unchanged(cr, "hr_holidays.leave_type_unpaid", util.update_record_from_xml)
-    util.if_unchanged(cr, "hr_holidays.holiday_status_eto", util.update_record_from_xml)
-    util.if_unchanged(cr, "hr_holidays.holiday_status_extra_hours", util.update_record_from_xml)
+    util.if_unchanged(
+        cr, "hr_holidays.leave_type_sick_time_off", util.update_record_from_xml, fields=["hide_on_dashboard"]
+    )
+    util.if_unchanged(cr, "hr_holidays.leave_type_unpaid", util.update_record_from_xml, fields=["hide_on_dashboard"])
+    util.if_unchanged(cr, "hr_holidays.holiday_status_eto", util.update_record_from_xml, fields=["hide_on_dashboard"])
+    util.if_unchanged(
+        cr, "hr_holidays.holiday_status_extra_hours", util.update_record_from_xml, fields=["hide_on_dashboard"]
+    )
