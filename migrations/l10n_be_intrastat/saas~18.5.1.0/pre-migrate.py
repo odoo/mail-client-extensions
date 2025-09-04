1b69e9a714c190e3b87398d6c39e3b1b8b1e2735
Gauthier Wala (gawa) <gawa@odoo.com>
2025-09-04 12:05:36 +0200

    [IMP] l10n_be_intrastat/saas~18.5: Remove return view
    The button has been made generic, with a common method.
    This view can be deleted.
    
    Enterprise: https://github.com/odoo/enterprise/pull/93886
    Part-of: odoo/upgrade#8379
    Related: odoo/enterprise#93886
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- migrations/l10n_be_intrastat/saas~18.5.1.0/pre-migrate.py
+++ migrations/l10n_be_intrastat/saas~18.5.1.0/pre-migrate.py
@@ -4,3 +4,4 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_view(cr, "l10n_be_intrastat.vat_return_submission_wizard_form")
     util.remove_field(cr, "l10n_be_reports.vat.return.submission.wizard", "need_intrastat_goods_report")
+    util.remove_view(cr, "l10n_be_intrastat.account_return_kanban_view")
