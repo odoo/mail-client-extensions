d53a3867af8b830f5e823bdb21369c1622672593
msho-odoo <msho@odoo.com>
2025-09-08 12:48:53 +0200

    [FIX] quality_control/saas~18.5: fill column `spreadsheet_template_id`
    This commit now correctly fills column `spreadsheet_template_id` in the
    quality_control upgrade.
    
    Related upgrade script: https://github.com/odoo/upgrade/pull/7774
    
    closes odoo/upgrade#8402
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/quality_control/saas~18.5.1.0/pre-migrate.py
+++ migrations/quality_control/saas~18.5.1.0/pre-migrate.py
@@ -6,50 +6,53 @@ def migrate(cr, version):
     util.remove_field(cr, "quality.check", "spreadsheet_check_cell")
     util.create_column(cr, "quality_check", "spreadsheet_template_id", "int4")
 
-    query = False
-    if util.column_exists(cr, "quality_check", "production_id") and util.column_exists(
-        cr, "quality_check", "repair_id"
-    ):
-        query = """
-            SELECT qc.id, qc.name
-              FROM quality_check qc
-             WHERE (qc.picking_id IS NOT NULL AND qc.production_id IS NOT NULL)
-                OR (qc.picking_id IS NOT NULL AND qc.repair_id IS NOT NULL)
-                OR (qc.production_id IS NOT NULL AND qc.repair_id IS NOT NULL);
+    util.explode_execute(
+        cr,
         """
-    elif util.column_exists(cr, "quality_check", "production_id"):
-        query = """
-            SELECT qc.id, qc.name
-              FROM quality_check qc
-             WHERE (qc.picking_id IS NOT NULL AND qc.production_id IS NOT NULL);
-        """
-    elif util.column_exists(cr, "quality_check", "repair_id"):
-        query = """
-            SELECT qc.id, qc.name
-              FROM quality_check qc
-             WHERE (qc.picking_id IS NOT NULL AND qc.repair_id IS NOT NULL);
-        """
+        UPDATE quality_check qc
+           SET spreadsheet_template_id = qp.spreadsheet_template_id
+          FROM quality_point qp
+         WHERE qp.id = qc.point_id
+           AND qp.spreadsheet_template_id IS NOT NULL
+        """,
+        table="quality_check",
+        alias="qc",
+    )
 
-    if query:
+    cols = ["picking_id"]
+    if util.column_exists(cr, "quality_check", "production_id"):  # quality_mrp installed
+        cols.append("production_id")
+    if util.column_exists(cr, "quality_check", "repair_id"):  # quality_repair installed
+        cols.append("repair_id")
+    columns = util.ColumnList.from_unquoted(cr, cols)
+
+    if len(cols) > 1:
+        query = util.format_query(
+            cr,
+            """
+            SELECT qc.id,
+                   qc.name
+              FROM quality_check qc
+             WHERE cardinality(array_remove(array[{}], NULL)) > 1;
+            """,
+            columns,
+        )
         cr.execute(query)
+
         if cr.rowcount:
-            util.add_to_migration_reports(
-                category="Quality",
-                message=(
-                    f"""
-                <span>
-            There are some Quality Checks that have a combination of Transfers, Repair Orders,
-            and/or Manufacturing Orders linked to them. It is expected that only one at most of
-            these is linked to a Quality Check at a time. Quality Checks that have more than 1
-            assigned to them many have validation errors if the extra linkages are not removed
-            when modifying them after this upgrade.
-            The following are the faulty records:\n<ul>{
-                        " ".join(
-                            [util.get_anchor_link_to_record("quality.check", id, name) for (id, name) in cr.fetchall()]
-                        )
-                    }</ul>
-            </span>
-                """
-                ),
-                format="html",
-            )
+            li = " ".join([util.get_anchor_link_to_record("quality.check", id, name) for (id, name) in cr.fetchall()])
+            msg = f"""
+                <details>
+                <summary>
+                    Some Quality Checks have a combination of Transfers, Repair Orders,
+                    and/or Manufacturing Orders linked to them. At most one of them
+                    should be linked at a time. Quality Checks that have more than one
+                    may get validation errors when modifying them in Odoo 19.
+
+                    Below there is a list of the faulty records. Please check them and
+                    ensure the linked transfers or orders are correct.
+                </summary>
+                <ul>{li}</ul>
+                </details>
+            """
+            util.add_to_migration_reports(category="Quality", message=msg, format="html")
