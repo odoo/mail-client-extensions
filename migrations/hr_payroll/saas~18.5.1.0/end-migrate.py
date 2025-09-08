fda0a2ac9dd9b2c062a3a5c425cc460ddecce34e
Edoardo Piroli <pied@odoo.com>
2025-09-08 09:43:25 +0200

    [FIX] hr_payroll/saas~18.5: fix 1b06bb75e
    Applying suggestion in
    https://github.com/odoo/upgrade/pull/7958#discussion_r2322190205
    
    closes odoo/upgrade#8391
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/hr_payroll/saas~18.5.1.0/end-migrate.py
+++ migrations/hr_payroll/saas~18.5.1.0/end-migrate.py
@@ -4,3 +4,14 @@ from odoo.upgrade import util
 def migrate(cr, version):
     util.remove_column(cr, "hr_version", "time_credit")
     util.remove_column(cr, "hr_version", "time_credit_type_id")
+
+    util.recompute_fields(cr, "hr.payslip", ["error_count", "warning_count", "issues"])
+
+    query = """
+        UPDATE hr_payslip
+           SET state_display = CASE
+                WHEN error_count > 0 THEN 'error'
+                WHEN warning_count > 0 THEN 'warning'
+                ELSE state END
+    """
+    util.explode_execute(cr, query, table="hr_payslip")
