de796a8e5370164615d8993ecfc26351e219bb42
Alvaro Fuentes <afu@odoo.com>
2025-09-09 16:28:43 +0200

    [IMP] stock/16.0: recompute fields with query
    Avoid errors if there are too many records to update.
    
    Needs odoo/upgrade-util#317

--- migrations/stock/16.0.1.1/post-migrate.py
+++ migrations/stock/16.0.1.1/post-migrate.py
@@ -6,9 +6,4 @@ def migrate(cr, version):
     inconsistencies.verify_products(cr, "stock.move", "stock.move.line", "move_id")
     inconsistencies.verify_uoms(cr, "stock.move", uom_field="product_uom")
     inconsistencies.verify_uoms(cr, "stock.move.line", uom_field="product_uom_id")
-    with util.named_cursor(cr) as ncr:
-        ncr.execute("SELECT id FROM stock_move")
-        chunk = ncr.fetchmany(100000)  # avoid getting millions of ids
-        while chunk:
-            util.recompute_fields(cr, "stock.move", ["quantity_done"], ids=[i for (i,) in chunk], strategy="commit")
-            chunk = ncr.fetchmany(100000)
+    util.recompute_fields(cr, "stock.move", ["quantity_done"], strategy="commit")
