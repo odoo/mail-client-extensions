97e8799d125b0e91efd1512e55ee138da51440a5
Alvaro Fuentes <afu@odoo.com>
2025-09-09 16:27:46 +0200

    [IMP] sale_timesheet_margin/saas~14.5: recompute fields with query
    Avoid errors if there are too many records to update.

--- migrations/sale_timesheet_margin/saas~14.5.1.0/end-migrate.py
+++ migrations/sale_timesheet_margin/saas~14.5.1.0/end-migrate.py
@@ -1,14 +1,10 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    cr.execute(
-        """
+    query = """
         SELECT sol.id
           FROM sale_order_line sol
          WHERE sol.qty_delivered_method = 'timesheet'
         """
-    )
-    ids = [row[0] for row in cr.fetchall()]
-    util.recompute_fields(cr, "sale.order.line", ["purchase_price"], ids=ids)
+    util.recompute_fields(cr, "sale.order.line", ["purchase_price"], query=query)
