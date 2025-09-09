a06e135cdfb3d08d4710cadaa3a1bf426bb4a473
Alvaro Fuentes <afu@odoo.com>
2025-09-09 16:14:26 +0200

    [IMP] point_of_sale/saas~12.4: recompute fields with query
    Avoid errors if there are too many records to update.

--- migrations/point_of_sale/saas~12.4.1.0.1/post-migrate.py
+++ migrations/point_of_sale/saas~12.4.1.0.1/post-migrate.py
@@ -1,10 +1,6 @@
-# -*- coding: utf-8 -*-
-from operator import itemgetter
-
 from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    cr.execute("SELECT id FROM pos_order WHERE currency_rate IS NULL")
-    ids = list(map(itemgetter(0), cr.fetchall()))
-    util.recompute_fields(cr, "pos.order", fields=["currency_rate"], ids=ids)
+    query = "SELECT id FROM pos_order WHERE currency_rate IS NULL"
+    util.recompute_fields(cr, "pos.order", fields=["currency_rate"], query=query)
