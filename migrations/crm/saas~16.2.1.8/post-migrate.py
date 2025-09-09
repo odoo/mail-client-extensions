abc27514d50eba251780da4ad16c1473c5b791c5
Alvaro Fuentes <afu@odoo.com>
2025-09-09 15:54:48 +0200

    [IMP] crm/saas~16.2: recompute fields with query
    Avoid errors if there are too many records to update.

--- migrations/crm/saas~16.2.1.8/post-migrate.py
+++ migrations/crm/saas~16.2.1.8/post-migrate.py
@@ -2,7 +2,5 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    cr.execute("SELECT id FROM crm_lead WHERE email_normalized IS NOT NULL")
-    if cr.rowcount:
-        ids = [id_ for (id_,) in cr.fetchall()]
-        util.recompute_fields(cr, "crm.lead", ["email_domain_criterion"], ids=ids, chunk_size=1024)
+    query = "SELECT id FROM crm_lead WHERE email_normalized IS NOT NULL"
+    util.recompute_fields(cr, "crm.lead", ["email_domain_criterion"], query=query, chunk_size=1024)
