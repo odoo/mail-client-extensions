d406b4cf35082403e99f947a9c5a62bd50532571
Alvaro Fuentes <afu@odoo.com>
2025-09-09 16:10:04 +0200

    [IMP] loyalty/saas~17.5: recompute fields with query
    Avoid errors if there are too many records to update.

--- migrations/loyalty/saas~17.5.1.0/end-migrate.py
+++ migrations/loyalty/saas~17.5.1.0/end-migrate.py
@@ -2,5 +2,5 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    cr.execute("SELECT id FROM loyalty_reward WHERE description IS NULL")
-    util.recompute_fields(cr, "loyalty.reward", ["description"], ids=[row[0] for row in cr.fetchall()])
+    query = "SELECT id FROM loyalty_reward WHERE description IS NULL"
+    util.recompute_fields(cr, "loyalty.reward", ["description"], query=query)
