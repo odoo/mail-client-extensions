786f1971808eea8fd300c3588a6ce2894e1d2fa9
Alvaro Fuentes <afu@odoo.com>
2025-09-09 15:55:55 +0200

    [IMP] delivery/9.0: recompute fields with query
    Avoid errors if there are too many records to update.

--- migrations/delivery/9.0.1.0/end-compute-delivery_price.py
+++ migrations/delivery/9.0.1.0/end-compute-delivery_price.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from openerp.addons.base.maintenance.migrations import util
 
+
 def migrate(cr, version):
     # working on one record at a time seems faster... Maybe it should not work in chunks...
     # If slow, see below for a way to bootstrap these fields using a specific upgrade script
     # https://github.com/odoo/migration-scripts/commit/f4caab2e581d87d03a6f3d0399686dc4a411ad2b#diff-358428b7812a500c0a8a1f4be1978074
-    cr.execute("SELECT id FROM sale_order WHERE delivery_price IS NULL")
-    ids = [r[0] for r in cr.fetchall()]
-    util.recompute_fields(cr, 'sale.order', ['delivery_price'], chunk_size=1, ids=ids)
+    query = "SELECT id FROM sale_order WHERE delivery_price IS NULL"
+    util.recompute_fields(cr, "sale.order", ["delivery_price"], chunk_size=1, query=query)
