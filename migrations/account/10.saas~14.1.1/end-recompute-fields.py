b380fc1183978cdc5b95bd2390dfce798223ab04
Alvaro Fuentes <afu@odoo.com>
2025-09-09 15:45:29 +0200

    [IMP] account/10.saas~14: recompute fields with query
    Avoid errors if there are too many records to update.

--- migrations/account/10.saas~14.1.1/end-recompute-fields.py
+++ migrations/account/10.saas~14.1.1/end-recompute-fields.py
@@ -3,6 +3,5 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
-    cr.execute("SELECT id FROM account_invoice_tax WHERE base IS NULL")
-    ids = [row[0] for row in cr.fetchall()]
-    util.recompute_fields(cr, "account.invoice.tax", ["base"], ids=ids)
+    query = "SELECT id FROM account_invoice_tax WHERE base IS NULL"
+    util.recompute_fields(cr, "account.invoice.tax", ["base"], query=query)
