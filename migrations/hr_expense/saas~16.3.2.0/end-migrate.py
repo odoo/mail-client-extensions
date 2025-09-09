ca751595bdaa61b81fe5a6a7d7fb92ae3ae8bab8
Alvaro Fuentes <afu@odoo.com>
2025-09-09 16:01:48 +0200

    [IMP] account/saas~16.3: recompute fields with query
    Avoid errors if there are too many records to update.

--- migrations/hr_expense/saas~16.3.2.0/end-migrate.py
+++ migrations/hr_expense/saas~16.3.2.0/end-migrate.py
@@ -3,6 +3,5 @@ from odoo.upgrade import util
 
 def migrate(cr, version):
     # A new state is created, splitting the "reported" state into two new states
-    cr.execute("SELECT id FROM hr_expense WHERE state IN ('draft', 'reported')")
-    ids = [r[0] for r in cr.fetchall()]
-    util.recompute_fields(cr, "hr.expense", ["state"], ids=ids)
+    query = "SELECT id FROM hr_expense WHERE state IN ('draft', 'reported')"
+    util.recompute_fields(cr, "hr.expense", ["state"], query=query)
