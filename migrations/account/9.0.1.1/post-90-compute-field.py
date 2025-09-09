c67d131c0705c48be3f7982a4230a99e63b44f95
Alvaro Fuentes <afu@odoo.com>
2025-09-09 15:48:52 +0200

    [IMP] account/9.0: recompute fields with query
    Avoid errors if there are too many records to update.

--- migrations/account/9.0.1.1/post-90-compute-field.py
+++ migrations/account/9.0.1.1/post-90-compute-field.py
@@ -36,22 +36,20 @@ def migrate(cr, version):
     # Compute other fields via ORM
     # If slow, see below for a way to bootstrap these fields using a specific upgrade script
     # https://github.com/odoo/migration-scripts/commit/f4caab2e581d87d03a6f3d0399686dc4a411ad2b#diff-a9eb42ce1e369248e123f58aeb36443f
-    cr.execute("""SELECT id FROM account_move_line WHERE amount_residual IS NULL""")
-    ids = [row[0] for row in cr.fetchall()]
+    query = """SELECT id FROM account_move_line WHERE amount_residual IS NULL"""
     util.recompute_fields(
         cr, 'account.move.line',
         fields=('amount_residual', 'amount_residual_currency', 'reconciled'),
-        ids=ids,
+        query=query,
     )
 
-    cr.execute("""SELECT inv.id FROM account_invoice inv, res_company c
+    query = """SELECT inv.id FROM account_invoice inv, res_company c
                     WHERE c.id = inv.company_id and c.currency_id != inv.currency_id
-                """)
-    inv_ids = [row[0] for row in cr.fetchall()]
+                """
     util.recompute_fields(
         cr, 'account.invoice',
         fields=('amount_total_company_signed', 'amount_total_signed', 'amount_untaxed_signed'),
-        ids=inv_ids,
+        query=query,
     )
 
     # aml field will trigger the computation of residual field on invoice
