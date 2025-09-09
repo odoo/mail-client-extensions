32ae6bb515dca82d2dc93e5b401b43b9a43d5a72
Alvaro Fuentes <afu@odoo.com>
2025-09-09 15:54:03 +0200

    [IMP] account/saas~15.4: recompute fields with query
    Avoid errors if there are too many records to update.

--- migrations/account/saas~15.4.1.0/post-recompute.py
+++ migrations/account/saas~15.4.1.0/post-recompute.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
 from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    cr.execute("SELECT id FROM account_move WHERE state IN ('draft', 'cancel')")
-    line_ids = [id[0] for id in cr.fetchall()]
+    query = "SELECT id FROM account_move WHERE state IN ('draft', 'cancel')"
     util.recompute_fields(
-        cr, "account.move", ["amount_residual", "amount_residual_signed"], ids=line_ids, strategy="commit"
+        cr, "account.move", ["amount_residual", "amount_residual_signed"], query=query, strategy="commit"
     )
