1a161bd6657f6f6b905ee552d1c3ed1971db9be5
Alvaro Fuentes <afu@odoo.com>
2025-09-09 16:06:44 +0200

    [IMP] l10n_it_edi/saas~18.2: recompute fields with query
    Avoid errors if there are too many records to update.

--- migrations/l10n_it_edi/saas~18.2.0.4/end-migrate.py
+++ migrations/l10n_it_edi/saas~18.2.0.4/end-migrate.py
@@ -2,7 +2,7 @@ from odoo.upgrade import util
 
 
 def migrate(cr, version):
-    cr.execute("""
+    query = """
         SELECT m.id
           FROM account_move AS m
           JOIN res_company AS c ON (m.company_id = c.id)
@@ -10,27 +10,15 @@ def migrate(cr, version):
          WHERE m.state = 'posted'
            AND fiscal_cc.code = 'IT'
            AND m.l10n_it_document_type IS NULL
-    """)
-    move_ids_posted_wo_itdoctype = [r[0] for r in cr.fetchall()]
-    util.recompute_fields(
-        cr,
-        "account.move",
-        ["l10n_it_document_type"],
-        ids=move_ids_posted_wo_itdoctype,
-    )
+    """
+    util.recompute_fields(cr, "account.move", ["l10n_it_document_type"], query=query)
 
-    cr.execute("""
+    query = """
         SELECT m.id
           FROM account_move AS m
           JOIN res_company AS c ON (m.company_id = c.id)
           JOIN res_country AS fiscal_cc ON (c.account_fiscal_country_id = fiscal_cc.id)
          WHERE fiscal_cc.code = 'IT'
            AND (m.l10n_it_payment_method IS NULL OR m.l10n_it_payment_method = '')
-    """)
-    move_ids_wo_itpaymethod = [r[0] for r in cr.fetchall()]
-    util.recompute_fields(
-        cr,
-        "account.move",
-        ["l10n_it_payment_method"],
-        ids=move_ids_wo_itpaymethod,
-    )
+    """
+    util.recompute_fields(cr, "account.move", ["l10n_it_payment_method"], query=query)
