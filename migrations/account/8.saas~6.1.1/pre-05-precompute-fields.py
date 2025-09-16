fc8a9fdc8dd16dc3328345224e68bd407a8c2538
sagu-odoo <sagu@odoo.com>
2025-09-16 06:27:11 +0000

    [FIX] account/8.saas~6.1.1: Fix performance issue
    ``account_analytic_line`` have more records due to that while
    ``partner_id`` is updating it taking more then half hour.
    So, making bit faster using ``explode_execute``
    
    records
    ```
    
    select count(id) from account_analytic_line;
      count
    ----------
     70852161
    (1 row)
    
    ```
    before fix
    ```
    2025-09-09 17:30:54,822 24 INFO db_3128434 openerp.modules.migration: module account: Running migration [>8.saas~6.1.1] pre-05-precompute-fields
    2025-09-09 18:06:29,323 24 INFO db_3128434 openerp.modules.migration: module account: Running migration [>8.saas~6.1.1] pre-10-adapt-paymenttermline
    ```
    
    after fix
    ```
    2901:2025-09-16 06:18:43,478 23 INFO sagu_3128434_18.0 openerp.modules.migration: module account: Running migration [>8.saas~6.1.1] pre-05-precompute-fields
    2902-2025-09-16 06:19:43,732 23 INFO sagu_3128434_18.0 openerp.addons.base.maintenance.migrations.util.pg: [10.21%] 745/7300 queries processed in 0:01:00.073544
    2903-2025-09-16 06:20:43,830 23 INFO sagu_3128434_18.0 openerp.addons.base.maintenance.migrations.util.pg: [28.36%] 2070/7300 queries processed in 0:02:00.171033
    2904-2025-09-16 06:21:43,926 23 INFO sagu_3128434_18.0 openerp.addons.base.maintenance.migrations.util.pg: [45.14%] 3295/7300 queries processed in 0:03:00.266996
    2905-2025-09-16 06:22:44,043 23 INFO sagu_3128434_18.0 openerp.addons.base.maintenance.migrations.util.pg: [57.52%] 4199/7300 queries processed in 0:04:00.384057
    2906-2025-09-16 06:23:44,322 23 INFO sagu_3128434_18.0 openerp.addons.base.maintenance.migrations.util.pg: [69.21%] 5052/7300 queries processed in 0:05:00.663017
    2907-2025-09-16 06:24:44,333 23 INFO sagu_3128434_18.0 openerp.addons.base.maintenance.migrations.util.pg: [80.08%] 5846/7300 queries processed in 0:06:00.674176
    2908-2025-09-16 06:25:44,339 23 INFO sagu_3128434_18.0 openerp.addons.base.maintenance.migrations.util.pg: [91.77%] 6699/7300 queries processed in 0:07:00.680816
    2909-2025-09-16 06:26:05,817 23 INFO sagu_3128434_18.0 openerp.addons.base.maintenance.migrations.util.pg: [100.00%] 7300/7300 queries processed in 0:07:22.158624
    2910-2025-09-16 06:26:07,436 23 INFO sagu_3128434_18.0 openerp.modules.migration: module account: Running migration [>8.saas~6.1.1] pre-10-adapt-paymenttermline
    ```
    UPG-3128434
    
    Part-of: odoo/upgrade#8460
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/account/8.saas~6.1.1/pre-05-precompute-fields.py
+++ migrations/account/8.saas~6.1.1/pre-05-precompute-fields.py
@@ -3,11 +3,17 @@ from openerp.addons.base.maintenance.migrations import util
 
 def migrate(cr, version):
     util.create_column(cr, 'account_analytic_line', 'partner_id', 'int4')
-    cr.execute("""UPDATE account_analytic_line l
-                     SET partner_id = a.partner_id
-                    FROM account_analytic_account a
-                   WHERE a.id = l.account_id
-               """)
+    util.explode_execute(
+        cr,
+        """
+          UPDATE account_analytic_line l
+             SET partner_id = a.partner_id
+            FROM account_analytic_account a
+           WHERE a.id = l.account_id
+        """,
+        alias="l",
+        table="account_analytic_line",
+    )
 
     # speed up computing price_subtotal_signed + avoid writing on all invoice lines,
     # triggering lots of useless and dangerous re-computations!
