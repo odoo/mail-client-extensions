4fc1f83a0fbf21e077cfc610e2676d2011e0e34b
Lionel Piraux (lipi) <lipi@odoo.com>
2025-09-16 16:54:40 +0200

    [FIX] website_sale/saas~19.1: migrate to global GMC flag
    In [^1], a decision was made to introduce a new global group-feature
    flag to control the visibility of related menus. However, this flag was
    not enabled during the upgrade to version 19 (18.5).
    
    Additionally, in [^2], it was decided to merge the GMC feature flags
    into the global group-feature flag, removing the website-specific flag.
    This commit updates the GMC feature flag migration to reflect this
    decision.
    
    [^1]: https://github.com/odoo/odoo/pull/224889
    [^2]: https://github.com/odoo/odoo/pull/227629
    
    closes odoo/upgrade#8468
    
    Related: odoo/odoo#227629
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- migrations/website_sale/saas~18.5.1.0/pre-migrate.py
+++ migrations/website_sale/saas~18.5.1.0/pre-migrate.py
@@ -6,3 +6,19 @@ def migrate(cr, version):
         util.remove_field(cr, "res.config.settings", "enabled_gmc_src")
         util.remove_field(cr, "res.config.settings", "gmc_xml_url")
         util.remove_constraint(cr, "website", "website_check_gmc_ecommerce_access")
+
+        cr.execute("SELECT 1 FROM website WHERE enabled_gmc_src LIMIT 1")
+        if cr.rowcount:
+            # Pre-create the feature group and activate it if GMC was enabled on any website.
+            util.update_record_from_xml(cr, "website_sale.group_product_feed")
+            group_user_id = util.ref(cr, "base.group_user")
+            group_product_feed_id = util.ref(cr, "website_sale.group_product_feed")
+            cr.execute(
+                """
+                INSERT INTO res_groups_implied_rel (gid, hid)
+                VALUES (%s, %s)
+                """,
+                [group_user_id, group_product_feed_id],
+            )
+            # Ensure all the website share the same configuration
+            cr.execute("UPDATE website SET enabled_gmc_src = True")
