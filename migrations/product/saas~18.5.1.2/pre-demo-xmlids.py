283bbfeeb704351077695855ddc5dd124c602500
Jinane Maksoud <maji@odoo.com>
2025-09-23 12:58:45 +0200

    [FIX] product/saas~18.5: add demo xmlid
    Yet another demo product variant needs an xmlid
    
    https://github.com/odoo/odoo/pull/227346
    
    closes odoo/upgrade#8482
    
    Signed-off-by: Alvaro Fuentes Suarez (afu) <afu@odoo.com>

--- migrations/product/saas~18.5.1.2/pre-demo-xmlids.py
+++ migrations/product/saas~18.5.1.2/pre-demo-xmlids.py
@@ -8,7 +8,6 @@ def migrate_demo(cr):
     template_screen_id = util.ref(cr, "product.product_template_acoustic_bloc_screens")
     first_variant_id = util.ref(cr, "product.product_product_25")
     color_attribute_id = util.ref(cr, "product.pa_color")
-    att_black_id = util.ref(cr, "product.pav_color_black")
     cr.execute(
         """
         SELECT id FROM product_template_attribute_line WHERE product_tmpl_id = %s AND attribute_id = %s
@@ -16,37 +15,41 @@ def migrate_demo(cr):
         [template_screen_id, color_attribute_id],
     )
     (ptal_id,) = cr.fetchone()
-    cr.execute(
-        """
-        SELECT id FROM product_template_attribute_value WHERE attribute_line_id = %s AND product_attribute_value_id = %s
-        """,
-        [ptal_id, att_black_id],
-    )
-    if cr.rowcount:
-        (acoustic_ptav_black,) = cr.fetchone()
+    for color in ("white", "black"):
+        att_id = util.ref(cr, f"product.pav_color_{color}")
         cr.execute(
             """
-            SELECT id
-              FROM product_product
-             WHERE product_tmpl_id = %s
-               AND combination_indices = %s
+            SELECT id FROM product_template_attribute_value
+             WHERE attribute_line_id = %s
+               AND product_attribute_value_id = %s
             """,
-            [template_screen_id, str(acoustic_ptav_black)],
+            [ptal_id, att_id],
         )
-        (variant_id,) = cr.fetchone()
-        util.ensure_xmlid_match_record(
-            cr,
-            "product.product_product_acoustic_bloc_screens_black",
-            "product.product",
-            {"id": variant_id},
-        )
-    else:
-        acoustic_ptav_black = produc_demo_tools._create_tmpl_att_val(
-            cr, "acoustic_ptav_black", template_screen_id, ptal_id, color_attribute_id, att_black_id
-        )
-        produc_demo_tools._create_variants(
-            cr, "product_product_acoustic_bloc_screens_black", first_variant_id, [acoustic_ptav_black]
-        )
+        if cr.rowcount:
+            (acoustic_ptav,) = cr.fetchone()
+            cr.execute(
+                """
+                SELECT id
+                  FROM product_product
+                 WHERE product_tmpl_id = %s
+                   AND combination_indices = %s
+                """,
+                [template_screen_id, str(acoustic_ptav)],
+            )
+            (variant_id,) = cr.fetchone()
+            util.ensure_xmlid_match_record(
+                cr,
+                f"product.product_product_acoustic_bloc_screens_{color}",
+                "product.product",
+                {"id": variant_id},
+            )
+        else:
+            acoustic_ptav = produc_demo_tools._create_tmpl_att_val(
+                cr, f"acoustic_ptav_{color}", template_screen_id, ptal_id, color_attribute_id, att_id
+            )
+            produc_demo_tools._create_variants(
+                cr, f"product_product_acoustic_bloc_screens_{color}", first_variant_id, [acoustic_ptav]
+            )
 
 
 def migrate(cr, version):
